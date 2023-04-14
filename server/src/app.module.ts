5468442d1e7281336df8bcf76f843e9ff858e120
Sammy Teillet <sammy.teillet@gmail.com>
2023-04-14 16:49:23 +0200

    chore: remove static server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'front/build'),
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
