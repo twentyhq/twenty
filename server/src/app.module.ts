0d82751b9b6eb768e4538a597ffde57dee710517
Sammy Teillet <sammy.teillet@gmail.com>
2023-04-14 16:53:23 +0200

    feat: register health controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthController } from './health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TerminusModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
