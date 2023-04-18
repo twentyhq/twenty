0d9b04ea147e69a5a28d16b33370dac805538dc3
Sammy Teillet <sammy.teillet@gmail.com>
2023-04-18 18:07:55 +0200

    feature: set the right color for unchecked checkbox

--- front/src/components/form/Checkbox.tsx
+++ front/src/components/form/Checkbox.tsx
@@ -8,7 +8,23 @@ type OwnProps = {
 
 const StyledContainer = styled.span`
   input[type='checkbox'] {
-    accent-color: ${(props) => props.theme.purple};
+    accent-color: ${(props) => props.theme.blue};
+    margin: 8px;
+    height: 16px;
+    width: 16px;
+  }
+
+  input[type='checkbox']::before {
+    content: '';
+    border: 1px solid black;
+    width: 14px;
+    height: 14px;
+    border-radius: 2px;
+    display: block;
+  }
+
+  input[type='checkbox']:checked::before {
+    border: 1px solid ${(props) => props.theme.blue};
   }
 `;
 
