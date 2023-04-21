4885e3598ae93e30dd1b1d3278537c4226df87a3
Anders Borch <anders@cyborch.com>
2023-04-21 13:44:08 +0200

    Add cancel button.
    Add test to ensure that cancel button can remove an item.

--- front/src/components/table/table-header/__tests__/SortAndFilterBar.test.tsx
+++ front/src/components/table/table-header/__tests__/SortAndFilterBar.test.tsx
@@ -2,9 +2,8 @@ import { fireEvent, render } from '@testing-library/react';
 
 import { RegularSortAndFilterBar } from '../__stories__/SortAndFilterBar.stories';
 
-const removeFunction = jest.fn();
-
 it('Checks the SortAndFilterBar renders', async () => {
+  const removeFunction = jest.fn();
   const { getByText, getByTestId } = render(
     <RegularSortAndFilterBar removeFunction={removeFunction} />,
   );
@@ -12,6 +11,17 @@ it('Checks the SortAndFilterBar renders', async () => {
 
   const removeIcon = getByTestId('remove-icon-test_sort');
   fireEvent.click(removeIcon);
+
+  expect(removeFunction).toHaveBeenCalled();
+});
+
+it('Removes sorts when cancel is pressed', async () => {
+  const removeFunction = jest.fn();
+  const { getByTestId } = render(
+    <RegularSortAndFilterBar removeFunction={removeFunction} />,
+  );
+  const cancel = getByTestId('cancel-button');
+  fireEvent.click(cancel);
 
   expect(removeFunction).toHaveBeenCalled();
 });
