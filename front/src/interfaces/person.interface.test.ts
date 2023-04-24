4e517fa8f9c8e83d7664ded799e604dcb71760b6
Anders Borch <anders@cyborch.com>
2023-04-24 23:01:15 +0200

    Add map back to gql person

--- front/src/interfaces/person.interface.test.ts
+++ front/src/interfaces/person.interface.test.ts
@@ -1,4 +1,4 @@
-import { mapPerson } from './person.interface';
+import { mapGqlPerson, mapPerson } from './person.interface';
 
 describe('mapPerson', () => {
   it('should map person', () => {
@@ -19,5 +19,28 @@ describe('mapPerson', () => {
       __typename: '',
     });
     expect(person.fullName).toBe('John Doe');
+  });
+
+  it('should map person back', () => {
+    const person = mapGqlPerson({
+      id: 1,
+      fullName: 'John Doe',
+      email: '',
+      phone: '',
+      city: '',
+      company: {
+        id: 1,
+        name: '',
+        domain: '',
+      },
+      creationDate: new Date(),
+      pipe: {
+        id: 3,
+        name: '',
+        icon: '',
+      },
+      countryCode: '',
+    });
+    expect(person.firstname).toBe('John');
   });
 });
