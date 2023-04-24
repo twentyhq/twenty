38132749c2a9ba190e14afd41d9caafce3b4789c
Anders Borch <anders@cyborch.com>
2023-04-24 21:39:09 +0200

    Add updatePerson function

---
+++ front/src/services/people/update.ts
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client';
+import { Person, mapGqlPerson } from '../../interfaces/person.interface';
+import { apiClient } from '../../apollo';
+
+const UPDATE_PERSON = gql`
+  mutation UpdatePeople(
+    $id: Int
+    $firstname: String
+    $lastname: String
+    $phone: String
+    $city: String
+    $company_id: Int
+    $email: String
+  ) {
+    update_people(
+      where: { id: { _eq: $id } }
+      _set: {
+        city: $city
+        company_id: $company_id
+        email: $email
+        firstname: $firstname
+        id: $id
+        lastname: $lastname
+        phone: $phone
+      }
+    ) {
+      returning {
+        city
+        company {
+          company_domain
+          company_name
+          id
+        }
+        email
+        firstname
+        id
+        lastname
+        phone
+      }
+    }
+  }
+`;
+
+export async function updatePerson(person: Person) {
+  const result = await apiClient.mutate({
+    mutation: UPDATE_PERSON,
+    variables: mapGqlPerson(person),
+  });
+  return result;
+}
