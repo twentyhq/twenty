1931e829fa0d67cb4f9b4bdd64576ffd9ce6e5fb
Charles Bochet <charles@twenty.com>
2023-04-25 17:07:47 +0200

    Ease codegen use on FE

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'codegen.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
--- front/codegen.js
+++ front/codegen.js
@@ -1,11 +1,9 @@
 module.exports = {
   schema: [
     {
-      'https://api.twenty.com/v1/graphql': {
+      [process.env.HASURA_GRAPHQL_ENDPOINT]: {
         headers: {
-          Authorization:
-            'Bearer ' +
-            'eyJhbGciOiJIUzI1NiJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLXdvcmtzcGFjZS1pZCI6IjIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbIm1lIiwidXNlciJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtdXNlci1pZCI6IjU4NjdlMDBhLTY0MDUtNDY2Ni1hNDQzLTdhZTYzYWUzMDBlMiIsIngtaGFzdXJhLXVzZXItaXMtYW5vbnltb3VzIjoiZmFsc2UifSwic3ViIjoiNTg2N2UwMGEtNjQwNS00NjY2LWE0NDMtN2FlNjNhZTMwMGUyIiwiaWF0IjoxNjgyNDMzNTcxLCJleHAiOjE2ODI0MzQ0NzEsImlzcyI6Imhhc3VyYS1hdXRoIn0.qM1Fc1Sgb766UQ8Ez9_v6F9YOOKI1PTPq4NTscn9diM',
+          'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
         },
       },
     },
@@ -25,9 +23,6 @@ module.exports = {
         withHOC: false,
         withComponent: false,
       },
-    },
-    './graphql.schema.json': {
-      plugins: ['introspection'],
     },
   },
 };
