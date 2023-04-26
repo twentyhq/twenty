2ad87bbc64c6a684304ecb317c15d893b0c36339
Sammy Teillet <sammy.teillet@gmail.com>
2023-04-26 12:06:33 +0200

    feature: display filterOperand on top of dropdown

--- front/src/components/table/table-header/FilterDropdownButton.tsx
+++ front/src/components/table/table-header/FilterDropdownButton.tsx
@@ -3,6 +3,7 @@ import DropdownButton from './DropdownButton';
 import { FilterType } from './SortAndFilterBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+  faAngleDown,
   faBuildings,
   faCalendar,
   faEnvelope,
@@ -36,8 +37,20 @@ const filters: FilterType[] = [
   { id: 'city', label: 'City', icon: faMapPin },
 ];
 
+type FilterOperandType = { label: string; id: string };
+
+const filterOperands: FilterOperandType[] = [
+  { label: 'Include', id: 'include' },
+  { label: "Doesn't include", id: 'not-include' },
+];
+
 export function FilterDropdownButton() {
   const [isUnfolded, setIsUnfolded] = useState(false);
+
+  const [isOptionUnfolded, setIsOptionUnfolded] = useState(false);
+
+  const [selectedFilterOperand, setSelectedFilterOperand] =
+    useState<FilterOperandType>(filterOperands[0]);
 
   return (
     <DropdownButton
@@ -46,14 +59,36 @@ export function FilterDropdownButton() {
       isUnfolded={isUnfolded}
       setIsUnfolded={setIsUnfolded}
     >
-      {filters.map((filter, index) => (
-        <DropdownButton.StyledDropdownItem key={index + 1}>
-          <DropdownButton.StyledIcon>
-            {filter.icon && <FontAwesomeIcon icon={filter.icon} />}
-          </DropdownButton.StyledIcon>
-          {filter.label}
-        </DropdownButton.StyledDropdownItem>
-      ))}
+      {isOptionUnfolded
+        ? filterOperands.map((option, index) => (
+            <DropdownButton.StyledDropdownItem
+              key={index}
+              onClick={() => {
+                setSelectedFilterOperand(option);
+                setIsOptionUnfolded(false);
+              }}
+            >
+              {option.label}
+            </DropdownButton.StyledDropdownItem>
+          ))
+        : [
+            <DropdownButton.StyledDropdownTopOption
+              key={0}
+              onClick={() => setIsOptionUnfolded(true)}
+            >
+              {selectedFilterOperand.label}
+
+              <FontAwesomeIcon icon={faAngleDown} />
+            </DropdownButton.StyledDropdownTopOption>,
+            ...filters.map((filter, index) => (
+              <DropdownButton.StyledDropdownItem key={index + 1}>
+                <DropdownButton.StyledIcon>
+                  {filter.icon && <FontAwesomeIcon icon={filter.icon} />}
+                </DropdownButton.StyledIcon>
+                {filter.label}
+              </DropdownButton.StyledDropdownItem>
+            )),
+          ]}
     </DropdownButton>
   );
 }
