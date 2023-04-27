9e362cdffb4a10b3b500909d6d37bcab8a311203
Anders Borch <anders@cyborch.com>
2023-04-27 15:14:31 +0200

    Implicitly start hasura console

--- infra/dev/hasura/entrypoint.sh
+++ infra/dev/hasura/entrypoint.sh
@@ -8,4 +8,8 @@ done
 
 hasura deploy
 
+socat TCP-LISTEN:9695,fork,reuseaddr,bind=twenty-hasura TCP:127.0.0.1:9695 &
+socat TCP-LISTEN:9693,fork,reuseaddr,bind=twenty-hasura TCP:127.0.0.1:9693 &
+hasura console --log-level DEBUG --address "127.0.0.1" --no-browser &
+
 wait
