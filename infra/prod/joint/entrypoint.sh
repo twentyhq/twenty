a88b1fead0649bfd30fa973fa90f1bca1e6a837d
Felix Malfait <felix.malfait@gmail.com>
2023-04-27 19:12:04 +0200

    Deploy Hasura during build

--- infra/prod/joint/entrypoint.sh
+++ infra/prod/joint/entrypoint.sh
@@ -2,22 +2,22 @@
 
 envsubst '${PORT}' < /etc/nginx/nginx.conf.base > /etc/nginx/nginx.conf
 
-# Start the frontend
+echo "Starting the frontend"
 cd /app/front
 serve -s build -l 3001 &
 
-# Start the Hasura API
+echo "Starting the Hasura API"
 cd /app/hasura
 /usr/bin/graphql-engine serve --port 8080 &
 
-# Start the documentation
-# cd /app/docs
-# serve -s . &
- 
- # Start the server
+echo "Starting the server"
 cd /app/server
 SERVER_DATABASE_URL=${SERVER_DATABASE_URL} node dist/main &
 
+echo "Deploying Hasura (metadata, migrations...)"
+hasura deploy
+
+echo "Starting nginx"
 nginx
 
 # Keep the container running
