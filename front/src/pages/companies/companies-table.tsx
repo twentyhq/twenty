b168981fb9108f502b57c1d362b071c92edfd75e
Sammy Teillet <sammy.teillet@gmail.com>
2023-05-05 17:06:09 +0200

    feature: add creation date and address sorts

--- front/src/pages/companies/companies-table.tsx
+++ front/src/pages/companies/companies-table.tsx
@@ -40,9 +40,27 @@ export const sortsAvailable = [
     _type: 'default_sort',
   },
   {
+    key: 'employees',
+    label: 'Employees',
+    icon: <FaUsers />,
+    _type: 'default_sort',
+  },
+  {
     key: 'domain_name',
     label: 'Url',
     icon: <FaLink />,
+    _type: 'default_sort',
+  },
+  {
+    key: 'address',
+    label: 'Address',
+    icon: <FaMapPin />,
+    _type: 'default_sort',
+  },
+  {
+    key: 'created_at',
+    label: 'Creation',
+    icon: <FaCalendar />,
     _type: 'default_sort',
   },
 ] satisfies Array<SortType<Companies_Order_By>>;
