ad896183f4a52fb0d402b2983ef7f58e0fb95fa9
Charles Bochet <charles@twenty.com>
2023-05-18 15:56:42 +0200

    Add employees and address filter on companies table page

--- front/src/pages/companies/companies-filters.tsx
+++ front/src/pages/companies/companies-filters.tsx
@@ -1,48 +1,102 @@
 import { Company } from '../../interfaces/entities/company.interface';
-import { FaLink, FaBuilding } from 'react-icons/fa';
+import { FaLink, FaBuilding, FaMapPin, FaUsers } from 'react-icons/fa';
 import { FilterConfigType } from '../../interfaces/filters/interface';
 
+export const nameFilter = {
+  key: 'company_name',
+  label: 'Company',
+  icon: <FaBuilding />,
+  operands: [
+    {
+      label: 'Contains',
+      id: 'like',
+      whereTemplate: (searchString) => ({
+        name: { _ilike: `%${searchString}%` },
+      }),
+    },
+    {
+      label: 'Does not contain',
+      id: 'not_like',
+      whereTemplate: (searchString) => ({
+        _not: { name: { _ilike: `%${searchString}%` } },
+      }),
+    },
+  ],
+} satisfies FilterConfigType<Company, string>;
+
+export const urlFilter = {
+  key: 'company_domain_name',
+  label: 'Url',
+  icon: <FaLink />,
+  operands: [
+    {
+      label: 'Contains',
+      id: 'like',
+      whereTemplate: (searchString) => ({
+        domain_name: { _ilike: `%${searchString}%` },
+      }),
+    },
+    {
+      label: 'Does not contain',
+      id: 'not_like',
+      whereTemplate: (searchString) => ({
+        _not: { domain_name: { _ilike: `%${searchString}%` } },
+      }),
+    },
+  ],
+} satisfies FilterConfigType<Company, string>;
+
+export const addressFilter = {
+  key: 'company_address',
+  label: 'Address',
+  icon: <FaMapPin />,
+  operands: [
+    {
+      label: 'Contains',
+      id: 'like',
+      whereTemplate: (searchString) => ({
+        address: { _ilike: `%${searchString}%` },
+      }),
+    },
+    {
+      label: 'Does not contain',
+      id: 'not_like',
+      whereTemplate: (searchString) => ({
+        _not: { address: { _ilike: `%${searchString}%` } },
+      }),
+    },
+  ],
+} satisfies FilterConfigType<Company, string>;
+
+export const employeesFilter = {
+  key: 'company_employees',
+  label: 'Employees',
+  icon: <FaUsers />,
+  operands: [
+    {
+      label: 'Greater than',
+      id: 'greater_than',
+      whereTemplate: (searchString) => ({
+        employees: {
+          _gte: isNaN(Number(searchString)) ? undefined : Number(searchString),
+        },
+      }),
+    },
+    {
+      label: 'Less than',
+      id: 'less_than',
+      whereTemplate: (searchString) => ({
+        employees: {
+          _lte: isNaN(Number(searchString)) ? undefined : Number(searchString),
+        },
+      }),
+    },
+  ],
+} satisfies FilterConfigType<Company, string>;
+
 export const availableFilters = [
-  {
-    key: 'company_name',
-    label: 'Company',
-    icon: <FaBuilding />,
-    operands: [
-      {
-        label: 'Contains',
-        id: 'like',
-        whereTemplate: (searchString) => ({
-          name: { _ilike: `%${searchString}%` },
-        }),
-      },
-      {
-        label: 'Does not contain',
-        id: 'not_like',
-        whereTemplate: (searchString) => ({
-          _not: { name: { _ilike: `%${searchString}%` } },
-        }),
-      },
-    ],
-  } satisfies FilterConfigType<Company, string>,
-  {
-    key: 'company_domain_name',
-    label: 'Url',
-    icon: <FaLink />,
-    operands: [
-      {
-        label: 'Contains',
-        id: 'like',
-        whereTemplate: (searchString) => ({
-          domain_name: { _ilike: `%${searchString}%` },
-        }),
-      },
-      {
-        label: 'Does not contain',
-        id: 'not_like',
-        whereTemplate: (searchString) => ({
-          _not: { domain_name: { _ilike: `%${searchString}%` } },
-        }),
-      },
-    ],
-  } satisfies FilterConfigType<Company, string>,
+  nameFilter,
+  urlFilter,
+  addressFilter,
+  employeesFilter,
 ];
