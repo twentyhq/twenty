17b7edec9ad92c3d0532869062f3bfa16f116d96
Charles Bochet <charles@twenty.com>
2023-05-25 13:33:22 +0200

    Simplify local dev

--- .vscode/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/../infra/dev"
-
-docker-compose down 
-docker volume rm dev_twenty_node_modules_front
-docker volume rm dev_twenty_node_modules_server
-docker volume rm dev_twenty_node_modules_docs
--- .vscode/logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/../infra/dev"
-
-docker-compose logs -f
