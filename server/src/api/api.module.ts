20f1cbef24fddca4524c3692c3f1ab29d7f66841
Sammy Teillet <sammy.teillet@gmail.com>
2023-05-25 16:43:16 +0200

    feature: add use resolvers

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -6,6 +6,8 @@ import { PrismaClient } from '@prisma/client';
 import { CompanyRelationsResolver } from './local-graphql';
 import { PeopleResolvers } from './people.resolver';
 import { PersonRelationsResolver } from './people-relations.resolver';
+import { UserResolvers } from './user.resolver';
+import { UserRelationsResolver } from './user-relations.resolver';
 
 @Module({
   imports: [
@@ -20,6 +22,8 @@ import { PersonRelationsResolver } from './people-relations.resolver';
     CompanyRelationsResolver,
     PeopleResolvers,
     PersonRelationsResolver,
+    UserResolvers,
+    UserRelationsResolver,
   ],
 })
 export class ApiModule {}
