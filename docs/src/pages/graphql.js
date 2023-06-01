6b524425c34d717b5cbecc52eb8e9519e620c5c6
Felix Malfait <felix.malfait@gmail.com>
2023-06-01 15:42:53 +0200

    Add docs CI and fix docs build

--- docs/src/pages/graphql.js
+++ docs/src/pages/graphql.js
@@ -3,16 +3,24 @@ import { GraphiQL } from 'graphiql';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 import 'graphiql/graphiql.css';
 
-const fetcher = createGraphiQLFetcher({ url: 'https://api.twenty.com/graphql' });
+
+// Docusaurus does SSR for custom pages but we need to load GraphiQL in the browser
+function GraphiQLComponent() {
+  const fetcher = createGraphiQLFetcher({ url: 'https://api.twenty.com/graphql' });
+  return <GraphiQL fetcher={fetcher} />;
+}
 
 export default function graphQL() {
   return (
     <Layout title="Hello" description="Hello React Page">
-      <div class="fullHeightPlayground">
-        <GraphiQL fetcher={fetcher} />
+      <div className="fullHeightPlayground">
+        <BrowserOnly>
+          <GraphiQLComponent />
+        </BrowserOnly>
       </div>
     </Layout>
   );
