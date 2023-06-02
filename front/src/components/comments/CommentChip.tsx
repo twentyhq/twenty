54b8efbd8130e3e8f1d43a9566ad9dec8c689014
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-06-02 10:16:48 +0000

    Created ComponentChip and CellComponentChip and their stories

---
+++ front/src/components/comments/CommentChip.tsx
@@ -0,0 +1,63 @@
+import styled from '@emotion/styled';
+import { FaRegComment } from 'react-icons/fa';
+
+export type CommentChipProps = {
+  count: number;
+  onClick?: () => void;
+};
+
+const StyledChip = styled.div`
+  height: 26px;
+  min-width: 34px;
+
+  padding-left: 2px;
+  padding-right: 2px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  gap: 2px;
+
+  background: ${(props) => props.theme.secondaryBackgroundTransparent};
+  backdrop-filter: blur(6px);
+
+  border-radius: 5px;
+
+  cursor: pointer;
+
+  color: ${(props) => props.theme.text30};
+
+  &:hover {
+    background: ${(props) => props.theme.tertiaryBackground};
+    color: ${(props) => props.theme.text40};
+  }
+
+  user-select: none;
+`;
+
+const StyledCount = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+const StyledIcon = styled.div`
+  color: ${(props) => props.theme.text100};
+`;
+
+export function CommentChip({ count, onClick }: CommentChipProps) {
+  const formattedCount = count > 99 ? '99+' : count;
+
+  return (
+    <StyledChip onClick={onClick}>
+      <StyledCount>{formattedCount}</StyledCount>
+      <StyledIcon>
+        <FaRegComment size={12} />
+      </StyledIcon>
+    </StyledChip>
+  );
+}
---
+++ front/src/components/comments/__stories__/CommentChip.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { getRenderWrapperForComponent } from '../../../testing/renderWrappers';
+import { CellCommentChip } from '../CellCommentChip';
+import styled from '@emotion/styled';
+import { CellBaseContainer } from '../../editable-cell/CellBaseContainer';
+import { CommentChip } from '../CommentChip';
+
+const meta: Meta<typeof CellCommentChip> = {
+  title: 'Components/CellCommentChip',
+  component: CellCommentChip,
+};
+
+export default meta;
+type Story = StoryObj<typeof CellCommentChip>;
+
+const TestCellContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  min-width: 250px;
+  height: fit-content;
+
+  background: ${(props) => props.theme.primaryBackground};
+`;
+
+const StyledFakeCellText = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const OneComment: Story = {
+  render: getRenderWrapperForComponent(<CommentChip count={1} />),
+};
+
+export const TenComments: Story = {
+  render: getRenderWrapperForComponent(<CommentChip count={10} />),
+};
+
+export const TooManyComments: Story = {
+  render: getRenderWrapperForComponent(<CommentChip count={1000} />),
+};
+
+export const InCellDefault: Story = {
+  render: getRenderWrapperForComponent(
+    <TestCellContainer>
+      <CellBaseContainer>
+        <StyledFakeCellText>Fake short text</StyledFakeCellText>
+        <CellCommentChip count={12} />
+      </CellBaseContainer>
+    </TestCellContainer>,
+  ),
+};
+
+export const InCellOverlappingBlur: Story = {
+  render: getRenderWrapperForComponent(
+    <TestCellContainer>
+      <CellBaseContainer>
+        <StyledFakeCellText>
+          Fake long text to demonstrate blur effect
+        </StyledFakeCellText>
+        <CellCommentChip count={12} />
+      </CellBaseContainer>
+    </TestCellContainer>,
+  ),
+};
