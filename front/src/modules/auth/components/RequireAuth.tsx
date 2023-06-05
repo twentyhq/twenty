c70bea9513f8d851c1ea54cc5ff89c862b9c7a95
Sammy Teillet <sammy.teillet@gmail.com>
2023-06-05 17:55:45 +0200

    feature: display a message when user is not defined (#196)
    * feature: display a message when user is not defined
    
    * feature: display text in a fadein in case redirection is slow

--- front/src/modules/auth/components/RequireAuth.tsx
+++ front/src/modules/auth/components/RequireAuth.tsx
@@ -1,7 +1,31 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
 
 import { hasAccessToken } from '../services/AuthService';
+
+const EmptyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const FadeInStyle = styled.div`
+  opacity: 0;
+  animation: ${fadeIn} 1s forwards;
+`;
 
 export function RequireAuth({
   children,
@@ -16,5 +40,13 @@ export function RequireAuth({
     }
   }, [navigate]);
 
+  if (!hasAccessToken())
+    return (
+      <EmptyContainer>
+        <FadeInStyle>
+          Please hold on a moment, we're directing you to our login page...
+        </FadeInStyle>
+      </EmptyContainer>
+    );
   return children;
 }
