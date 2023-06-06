1276b067b71ad287a1e125a5263028fd694ce952
Sammy Teillet <sammy.teillet@gmail.com>
2023-06-06 16:27:39 +0200

    test: add story for table header

---
+++ front/src/modules/ui/components/table/table-header/__stories__/TableHeader.stories.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { FaList } from 'react-icons/fa';
+import { ApolloProvider } from '@apollo/client';
+import { expect } from '@storybook/jest';
+import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+
+import { FullHeightStorybookLayout } from '~/testing/FullHeightStorybookLayout';
+import { mockedClient } from '~/testing/mockedClient';
+
+import { availableFilters } from '../../../../../../pages/companies/companies-filters';
+import { availableSorts } from '../../../../../../pages/companies/companies-sorts';
+import { sleep } from '../../../../../../testing/sleep';
+import { TableHeader } from '../TableHeader';
+
+const meta: Meta<typeof TableHeader> = {
+  title: 'Components/Table/TableHeader',
+  component: TableHeader,
+};
+
+export default meta;
+type Story = StoryObj<typeof TableHeader>;
+
+export const Empty: Story = {
+  render: () => (
+    <ApolloProvider client={mockedClient}>
+      <FullHeightStorybookLayout>
+        <TableHeader
+          viewName="ViewName"
+          viewIcon={<FaList />}
+          availableSorts={availableSorts}
+          availableFilters={availableFilters}
+        />
+      </FullHeightStorybookLayout>
+    </ApolloProvider>
+  ),
+};
+
+export const WithSortsAndFilters: Story = {
+  render: () => (
+    <ApolloProvider client={mockedClient}>
+      <FullHeightStorybookLayout>
+        <TableHeader
+          viewName="ViewName"
+          viewIcon={<FaList />}
+          availableSorts={availableSorts}
+          availableFilters={availableFilters}
+        />
+      </FullHeightStorybookLayout>
+    </ApolloProvider>
+  ),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const outsideClick = await canvas.findByText('ViewName');
+
+    userEvent.click(await canvas.findByText('Filter'));
+    userEvent.click(await canvas.findByText('Name'));
+    const nameInput = await canvas.findByPlaceholderText('Name');
+    userEvent.type(nameInput, 'My name');
+    userEvent.click(outsideClick);
+
+    userEvent.click(await canvas.findByText('Sort'));
+    userEvent.click(await canvas.findByText('Name'));
+
+    userEvent.click(await canvas.findByText('Sort'));
+    userEvent.click(await canvas.findByText('Creation'));
+
+    userEvent.click(await canvas.findByText('Sort'));
+    userEvent.click(await canvas.findByText('Address'));
+
+    userEvent.click(await canvas.findByText('Filter'));
+    userEvent.click(await canvas.findByText('Employees'));
+    const employeesInput = await canvas.findByPlaceholderText('Employees');
+    userEvent.type(employeesInput, '12');
+
+    userEvent.click(await canvas.findByText('Sort'));
+    userEvent.click(await canvas.findByText('Url'));
+
+    userEvent.click(await canvas.findByText('Filter'));
+    userEvent.click(await canvas.findByText('Created At'));
+    userEvent.click(await canvas.findByText('6'));
+    userEvent.click(outsideClick);
+  },
+};
