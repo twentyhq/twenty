f0c353103d367cc58164a64f11524c7d2d7832dc
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-06-06 12:35:03 +0000

    wip

--- front/src/modules/comments/components/comments/CommentTextInput.tsx
+++ front/src/modules/comments/components/comments/CommentTextInput.tsx
@@ -6,10 +6,10 @@ import TextareaAutosize from 'react-textarea-autosize';
 import styled from '@emotion/styled';
 
 import { IconButton } from '@/ui/components/buttons/IconButton';
+import { AutosizeTextInput } from '@/ui/components/inputs/AutosizeTextInput';
 
 type OwnProps = {
-  onSend?: (text: string) => void;
-  placeholder?: string;
+  commentThreadId: string;
 };
 
 const StyledContainer = styled.div`
@@ -18,106 +18,16 @@ const StyledContainer = styled.div`
   width: 100%;
 `;
 
-const StyledTextArea = styled(TextareaAutosize)`
-  width: 100%;
-  padding: 8px;
-  font-size: 13px;
-  font-family: inherit;
-  font-weight: 400;
-  line-height: 16px;
-  border: none;
-  border-radius: 5px;
-  background: ${(props) => props.theme.tertiaryBackground};
-  color: ${(props) => props.theme.text80};
-  overflow: auto;
-  resize: none;
+export function CommentTextInput({ commentThreadId }: OwnProps) {
+  const asd = use;
 
-  &:focus {
-    outline: none;
-    border: none;
-  }
-
-  &::placeholder {
-    color: ${(props) => props.theme.text30};
-    font-weight: 400;
-  }
-`;
-
-const StyledBottomRightIconButton = styled.div`
-  width: 0px;
-  position: relative;
-  top: calc(100% - 26.5px);
-  right: 26px;
-`;
-
-export function CommentTextInput({ placeholder, onSend }: OwnProps) {
-  const [text, setText] = useState('');
-
-  const isSendButtonDisabled = !text;
-
-  useHotkeys(
-    ['shift+enter', 'enter'],
-    (event: KeyboardEvent, handler: HotkeysEvent) => {
-      if (handler.shift) {
-        return;
-      } else {
-        event.preventDefault();
-
-        onSend?.(text);
-
-        setText('');
-      }
-    },
-    {
-      enableOnContentEditable: true,
-      enableOnFormTags: true,
-    },
-    [onSend],
-  );
-
-  useHotkeys(
-    'esc',
-    (event: KeyboardEvent, handler: HotkeysEvent) => {
-      event.preventDefault();
-
-      setText('');
-    },
-    {
-      enableOnContentEditable: true,
-      enableOnFormTags: true,
-    },
-    [onSend],
-  );
-
-  function handleInputChange(event: React.FormEvent<HTMLTextAreaElement>) {
-    const newText = event.currentTarget.value;
-
-    setText(newText);
-  }
-
-  function handleOnClickSendButton() {
-    onSend?.(text);
-
-    setText('');
+  function handleSendComment(commentText: string) {
+    console.log({ commentText });
   }
 
   return (
     <>
-      <StyledContainer>
-        <StyledTextArea
-          placeholder={placeholder || 'Write something...'}
-          maxRows={5}
-          onChange={handleInputChange}
-          value={text}
-        />
-        <StyledBottomRightIconButton>
-          <IconButton
-            onClick={handleOnClickSendButton}
-            icon={<HiArrowSmRight size={15} />}
-            disabled={isSendButtonDisabled}
-          />
-        </StyledBottomRightIconButton>
-      </StyledContainer>
+      <AutosizeTextInput onSend={handleSendComment} />
     </>
   );
 }
