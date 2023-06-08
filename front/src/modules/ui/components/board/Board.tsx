8220aab2d4be266fc2517cd7e2d4cd88a467f0c6
Sammy Teillet <sammy.teillet@gmail.com>
2023-06-08 14:57:34 +0200

    feature: drag and drop elements between columns

--- front/src/modules/ui/components/board/Board.tsx
+++ front/src/modules/ui/components/board/Board.tsx
@@ -17,8 +17,20 @@ const StyledBoard = styled.div`
 `;
 
 type ItemKey = `item-${number}`;
+interface Item {
+  id: string;
+  content: string;
+}
+interface Items {
+  [key: string]: Item;
+}
+interface Column {
+  id: string;
+  title: string;
+  itemKeys: ItemKey[];
+}
 
-const items = {
+const items: Items = {
   'item-1': { id: 'item-1', content: 'Item 1' },
   'item-2': { id: 'item-2', content: 'Item 2' },
   'item-3': { id: 'item-3', content: 'Item 3' },
@@ -31,21 +43,54 @@ const initialBoard = [
   {
     id: 'column-1',
     title: 'Column 1',
-    itemKeys: ['item-1', 'item-2', 'item-3', 'item-4'] satisfies ItemKey[],
+    itemKeys: ['item-1', 'item-2', 'item-3', 'item-4'],
   },
   {
     id: 'column-2',
     title: 'Column 2',
-    itemKeys: ['item-5', 'item-6'] satisfies ItemKey[],
+    itemKeys: ['item-5', 'item-6'],
   },
-];
+] satisfies Column[];
 
 export const Board = () => {
-  const [board] = useState(initialBoard);
+  const [board, setBoard] = useState<Column[]>(initialBoard);
 
-  const onDragEnd: OnDragEndResponder = useCallback((result) => {
-    console.log(result);
-  }, []);
+  const onDragEnd: OnDragEndResponder = useCallback(
+    (result) => {
+      const { destination, source } = result;
+      if (!destination) return;
+      const sourceColumnIndex = board.findIndex(
+        (column) => column.id === source.droppableId,
+      );
+      const sourceColumn = board[sourceColumnIndex];
+      const destinationColumnIndex = board.findIndex(
+        (column) => column.id === destination.droppableId,
+      );
+      const destinationColumn = board[destinationColumnIndex];
+      if (!destinationColumn || !sourceColumn) return;
+      const sourceItems = sourceColumn.itemKeys;
+      const destinationItems = destinationColumn.itemKeys;
+
+      const [removed] = sourceItems.splice(source.index, 1);
+      destinationItems.splice(destination.index, 0, removed);
+
+      const newSourceColumn = {
+        ...sourceColumn,
+        itemKeys: sourceItems,
+      };
+
+      const newDestinationColumn = {
+        ...destinationColumn,
+        itemKeys: destinationItems,
+      };
+
+      const newBoard = [...board];
+      newBoard.splice(sourceColumnIndex, 1, newSourceColumn);
+      newBoard.splice(destinationColumnIndex, 1, newDestinationColumn);
+      setBoard(newBoard);
+    },
+    [board],
+  );
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
