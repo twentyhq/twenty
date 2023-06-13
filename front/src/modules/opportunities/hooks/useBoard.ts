1034f795796ba1aa45f4e9c995ead7bf59ab2b4b
Sammy Teillet <sammy.teillet@gmail.com>
2023-06-13 18:34:43 +0200

    refactor: extract useBoard in a hook

---
+++ front/src/modules/opportunities/hooks/useBoard.ts
@@ -0,0 +1,43 @@
+import {
+  useGetPeopleQuery,
+  useGetPipelinesQuery,
+} from '../../../generated/graphql';
+import { BoardItemKey, Column, Items } from '../../ui/components/board/Board';
+
+export const useBoard = () => {
+  const pipelines = useGetPipelinesQuery();
+  const initialBoard: Column[] =
+    pipelines.data?.findManyPipeline[0].pipelineStages?.map(
+      (pipelineStage) => ({
+        id: pipelineStage.name,
+        title: pipelineStage.name,
+        colorCode: pipelineStage.color,
+        itemKeys:
+          pipelineStage.pipelineProgresses?.map(
+            (item) => `item-${item.associableId}` as BoardItemKey,
+          ) || [],
+      }),
+    ) || [];
+
+  const pipelineEntityIds =
+    pipelines.data?.findManyPipeline[0].pipelineStages?.reduce(
+      (acc, pipelineStage) => [
+        ...acc,
+        ...(pipelineStage.pipelineProgresses?.map(
+          (item) => item.associableId,
+        ) || []),
+      ],
+      [] as string[],
+    );
+  const entities = useGetPeopleQuery({
+    variables: { where: { id: { in: pipelineEntityIds } } },
+  });
+
+  const items: Items =
+    entities.data?.people.reduce(
+      (acc, person) => ({ ...acc, [`item-${person.id}`]: person }),
+      {},
+    ) || {};
+
+  return { initialBoard, items, pipelines, entities };
+};
--- front/src/pages/opportunities/Opportunities.tsx
+++ front/src/pages/opportunities/Opportunities.tsx
@@ -3,54 +3,14 @@ import { FaBullseye } from 'react-icons/fa';
 import { WithTopBarContainer } from '@/ui/layout/containers/WithTopBarContainer';
 import { AppPage } from '~/AppPage';
 
-import {
-  useGetPeopleQuery,
-  useGetPipelinesQuery,
-} from '../../generated/graphql';
 import { Board } from '../../modules/opportunities/components/Board';
-import {
-  BoardItemKey,
-  Column,
-  Items,
-} from '../../modules/ui/components/board/Board';
+import { useBoard } from '../../modules/opportunities/hooks/useBoard';
 
 export function Opportunities() {
-  const pipelines = useGetPipelinesQuery();
-  const initialBoard: Column[] =
-    pipelines.data?.findManyPipeline[0].pipelineStages?.map(
-      (pipelineStage) => ({
-        id: pipelineStage.name,
-        title: pipelineStage.name,
-        colorCode: pipelineStage.color,
-        itemKeys:
-          pipelineStage.pipelineProgresses?.map(
-            (item) => `item-${item.associableId}` as BoardItemKey,
-          ) || [],
-      }),
-    ) || [];
+  const { initialBoard, items, pipelines, entities } = useBoard();
 
-  const pipelineEntityIds =
-    pipelines.data?.findManyPipeline[0].pipelineStages?.reduce(
-      (acc, pipelineStage) => [
-        ...acc,
-        ...(pipelineStage.pipelineProgresses?.map(
-          (item) => item.associableId,
-        ) || []),
-      ],
-      [] as string[],
-    );
-  const persons = useGetPeopleQuery({
-    variables: { where: { id: { in: pipelineEntityIds } } },
-  });
-
-  const items: Items =
-    persons.data?.people.reduce(
-      (acc, person) => ({ ...acc, [`item-${person.id}`]: person }),
-      {},
-    ) || {};
-
-  if (pipelines.loading || persons.loading) return <div>Loading...</div>;
-  if (pipelines.error || persons.error) return <div>Error...</div>;
+  if (pipelines.loading || entities.loading) return <div>Loading...</div>;
+  if (pipelines.error || entities.error) return <div>Error...</div>;
   return (
     <AppPage>
       <WithTopBarContainer title="Opportunities" icon={<FaBullseye />}>
