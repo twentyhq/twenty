d07b8ec61bc7a3f675862257be79c3bab8e61fe2
Sammy Teillet <sammy.teillet@gmail.com>
2023-06-13 14:35:06 +0200

    refactor: create method to get optimistic new board after update

--- front/src/modules/opportunities/components/Board.tsx
+++ front/src/modules/opportunities/components/Board.tsx
@@ -3,6 +3,7 @@ import {
   DragDropContext,
   Draggable,
   Droppable,
+  DropResult,
   OnDragEndResponder,
 } from '@hello-pangea/dnd';
 
@@ -23,41 +24,47 @@ type BoardProps = {
   items: Items;
 };
 
+const getOptimisticlyUpdatedBoard = (board: Column[], result: DropResult) => {
+  const { destination, source } = result;
+  if (!destination) return;
+  const sourceColumnIndex = board.findIndex(
+    (column) => column.id === source.droppableId,
+  );
+  const sourceColumn = board[sourceColumnIndex];
+  const destinationColumnIndex = board.findIndex(
+    (column) => column.id === destination.droppableId,
+  );
+  const destinationColumn = board[destinationColumnIndex];
+  if (!destinationColumn || !sourceColumn) return;
+  const sourceItems = sourceColumn.itemKeys;
+  const destinationItems = destinationColumn.itemKeys;
+
+  const [removed] = sourceItems.splice(source.index, 1);
+  destinationItems.splice(destination.index, 0, removed);
+
+  const newSourceColumn = {
+    ...sourceColumn,
+    itemKeys: sourceItems,
+  };
+
+  const newDestinationColumn = {
+    ...destinationColumn,
+    itemKeys: destinationItems,
+  };
+
+  const newBoard = [...board];
+  newBoard.splice(sourceColumnIndex, 1, newSourceColumn);
+  newBoard.splice(destinationColumnIndex, 1, newDestinationColumn);
+  return newBoard;
+};
+
 export const Board = ({ initialBoard, items }: BoardProps) => {
   const [board, setBoard] = useState<Column[]>(initialBoard);
 
   const onDragEnd: OnDragEndResponder = useCallback(
     (result) => {
-      const { destination, source } = result;
-      if (!destination) return;
-      const sourceColumnIndex = board.findIndex(
-        (column) => column.id === source.droppableId,
-      );
-      const sourceColumn = board[sourceColumnIndex];
-      const destinationColumnIndex = board.findIndex(
-        (column) => column.id === destination.droppableId,
-      );
-      const destinationColumn = board[destinationColumnIndex];
-      if (!destinationColumn || !sourceColumn) return;
-      const sourceItems = sourceColumn.itemKeys;
-      const destinationItems = destinationColumn.itemKeys;
-
-      const [removed] = sourceItems.splice(source.index, 1);
-      destinationItems.splice(destination.index, 0, removed);
-
-      const newSourceColumn = {
-        ...sourceColumn,
-        itemKeys: sourceItems,
-      };
-
-      const newDestinationColumn = {
-        ...destinationColumn,
-        itemKeys: destinationItems,
-      };
-
-      const newBoard = [...board];
-      newBoard.splice(sourceColumnIndex, 1, newSourceColumn);
-      newBoard.splice(destinationColumnIndex, 1, newDestinationColumn);
+      const newBoard = getOptimisticlyUpdatedBoard(board, result);
+      if (!newBoard) return;
       setBoard(newBoard);
     },
     [board],
