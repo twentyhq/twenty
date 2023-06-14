a49372250204b6b665b53b3a0d248bdc6f68a8cf
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-06-14 09:36:22 +0000

    Use lorem ipsum

--- front/src/modules/ui/components/menu/stories/DropdownMenu.stories.tsx
+++ front/src/modules/ui/components/menu/stories/DropdownMenu.stories.tsx
@@ -25,8 +25,10 @@ type Story = StoryObj<typeof DropdownMenu>;
 
 const FakeContentBelow = () => (
   <div style={{ position: 'absolute' }}>
-    askjdlaksjdlaksjdlakjsdlkj lkajsldkjalskd jalksdj alksjd alskjd alksjd
-    alksjd laksjd askjdlaksjdlaksjdlakjsdlkj lkajsldkjalskd jalksdj alksjd
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat.
   </div>
 );
 
