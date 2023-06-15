8fa3de505d4750e24afdebf49bc29e09581e7a1b
Charles Bochet <charles@twenty.com>
2023-06-15 08:25:03 +0200

    Add update, create and delete pipelineProgress endpoints

--- server/src/api/@generated/pipeline-stage/pipeline-stage-update-one-required-without-pipeline-progresses-nested.input.ts
+++ server/src/api/@generated/pipeline-stage/pipeline-stage-update-one-required-without-pipeline-progresses-nested.input.ts
@@ -1,39 +1,28 @@
 import { Field } from '@nestjs/graphql';
 import { InputType } from '@nestjs/graphql';
 import { PipelineStageCreateWithoutPipelineProgressesInput } from './pipeline-stage-create-without-pipeline-progresses.input';
-import { Type } from 'class-transformer';
+import { HideField } from '@nestjs/graphql';
 import { PipelineStageCreateOrConnectWithoutPipelineProgressesInput } from './pipeline-stage-create-or-connect-without-pipeline-progresses.input';
 import { PipelineStageUpsertWithoutPipelineProgressesInput } from './pipeline-stage-upsert-without-pipeline-progresses.input';
 import { PipelineStageWhereUniqueInput } from './pipeline-stage-where-unique.input';
+import { Type } from 'class-transformer';
 import { PipelineStageUpdateWithoutPipelineProgressesInput } from './pipeline-stage-update-without-pipeline-progresses.input';
 
 @InputType()
 export class PipelineStageUpdateOneRequiredWithoutPipelineProgressesNestedInput {
-  @Field(() => PipelineStageCreateWithoutPipelineProgressesInput, {
-    nullable: true,
-  })
-  @Type(() => PipelineStageCreateWithoutPipelineProgressesInput)
+  @HideField()
   create?: PipelineStageCreateWithoutPipelineProgressesInput;
 
-  @Field(() => PipelineStageCreateOrConnectWithoutPipelineProgressesInput, {
-    nullable: true,
-  })
-  @Type(() => PipelineStageCreateOrConnectWithoutPipelineProgressesInput)
+  @HideField()
   connectOrCreate?: PipelineStageCreateOrConnectWithoutPipelineProgressesInput;
 
-  @Field(() => PipelineStageUpsertWithoutPipelineProgressesInput, {
-    nullable: true,
-  })
-  @Type(() => PipelineStageUpsertWithoutPipelineProgressesInput)
+  @HideField()
   upsert?: PipelineStageUpsertWithoutPipelineProgressesInput;
 
   @Field(() => PipelineStageWhereUniqueInput, { nullable: true })
   @Type(() => PipelineStageWhereUniqueInput)
   connect?: PipelineStageWhereUniqueInput;
 
-  @Field(() => PipelineStageUpdateWithoutPipelineProgressesInput, {
-    nullable: true,
-  })
-  @Type(() => PipelineStageUpdateWithoutPipelineProgressesInput)
+  @HideField()
   update?: PipelineStageUpdateWithoutPipelineProgressesInput;
 }
