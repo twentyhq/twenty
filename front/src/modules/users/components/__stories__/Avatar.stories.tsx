a01e10bdfbb730a16d48bc318000e3ae68128278
Sammy Teillet <sammy.teillet@gmail.com>
2023-06-19 16:01:11 +0200

    chore: remove figma image url from storybook

--- front/src/modules/users/components/__stories__/Avatar.stories.tsx
+++ front/src/modules/users/components/__stories__/Avatar.stories.tsx
@@ -12,8 +12,7 @@ const meta: Meta<typeof Avatar> = {
 export default meta;
 type Story = StoryObj<typeof Avatar>;
 
-const avatarUrl =
-  'https://s3-alpha-sig.figma.com/img/bbb5/4905/f0a52cc2b9aaeb0a82a360d478dae8bf?Expires=1687132800&Signature=iVBr0BADa3LHoFVGbwqO-wxC51n1o~ZyFD-w7nyTyFP4yB-Y6zFawL-igewaFf6PrlumCyMJThDLAAc-s-Cu35SBL8BjzLQ6HymzCXbrblUADMB208PnMAvc1EEUDq8TyryFjRO~GggLBk5yR0EXzZ3zenqnDEGEoQZR~TRqS~uDF-GwQB3eX~VdnuiU2iittWJkajIDmZtpN3yWtl4H630A3opQvBnVHZjXAL5YPkdh87-a-H~6FusWvvfJxfNC2ZzbrARzXofo8dUFtH7zUXGCC~eUk~hIuLbLuz024lFQOjiWq2VKyB7dQQuGFpM-OZQEV8tSfkViP8uzDLTaCg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4';
+const avatarUrl = 'http://placekitten.com/300/300';
 
 export const Rounded: Story = {
   render: getRenderWrapperForComponent(
