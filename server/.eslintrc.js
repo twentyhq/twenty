2e0d41176a58b585ce4357b06f1642566f5cd405
amir khazama <amirkhazama1139@gmail.com>
2023-06-28 14:15:41 +0330

    eslint func-style rule added to server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'func-style':['warn', 'declaration', { 'allowArrowFunctions': true }],
   },
 };
