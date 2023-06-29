4ca273ba5d62dbb1b386dee564c5ad4d9fb899e9
Felix Malfait <felix.malfait@gmail.com>
2023-06-29 08:06:36 -0700

    Use Httpservice and add validation

--- server/src/core/event/event.resolver.ts
+++ server/src/core/event/event.resolver.ts
@@ -9,16 +9,19 @@ import { AuthWorkspace } from 'src/decorators/auth-workspace.decorator';
 import { User, Workspace } from '@prisma/client';
 import { AuthUser } from 'src/decorators/auth-user.decorator';
 import { anonymize } from 'src/utils/anonymize';
+import { HttpService } from '@nestjs/axios';
 
 @UseGuards(OptionalJwtAuthGuard)
 @Resolver(() => Event)
 export class EventResolver {
-  constructor(private readonly eventService: EventService) {}
+  constructor(
+    private readonly eventService: EventService,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Mutation(() => Event)
   createEvent(
     @Args() createEventInput: CreateEventInput,
-    @Context() context: any,
     @AuthWorkspace() workspace: Workspace | undefined,
     @AuthUser() user: User | undefined,
   ) {
@@ -36,13 +39,17 @@ export class EventResolver {
       },
     };
 
-    axios
+    this.httpService
       .post('https://t.twenty.com/api/v1/s2s/event', data, {
         headers: {
           'X-Auth-Token': 's2s.gs61um40nzhw7mh56uw1ft.4t4u3ku9ueu14cfg08yx91k',
         },
       })
-      .catch(() => null);
+      .subscribe({
+        // Non-breaking behavior,
+        // So no need to log this unless you're debugging this code specifically
+        error: (err) => err, // console.error('HTTP request failed:', error);
+      });
 
     return this.eventService.create(createEventInput);
   }
