848ccdbaee3c6b99a60ea6514211a880d034d560
Felix Malfait <felix.malfait@gmail.com>
2023-06-29 11:06:14 -0700

    change warn to error (server side)

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'func-style':['warn', 'declaration', { 'allowArrowFunctions': true }],
+    'func-style':['error', 'declaration', { 'allowArrowFunctions': true }],
   },
 };
