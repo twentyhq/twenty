7ebc23a10b629bf2499261f7136fb552cf05d6a7
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-07-04 09:56:05 +0200

    feat: protect against injections

--- server/src/core/file-upload/file-upload.controller.ts
+++ server/src/core/file-upload/file-upload.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Res } from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { Response } from 'express';
 import { join } from 'path';
+import { checkFilePath, checkFilename } from './file-upload.utils';
 
 @Controller('files')
 export class FileUploadController {
@@ -11,11 +12,13 @@ export class FileUploadController {
    */
   @Get('*/:filename')
   async getFile(@Param() params: string[], @Res() res: Response) {
+    const folderPath = checkFilePath(params[0]);
+    const filename = checkFilename(params['filename']);
     const filePath = join(
       process.cwd(),
       '.local-storage/',
-      params[0],
-      params['filename'],
+      folderPath,
+      filename,
     );
     const fileStream = createReadStream(filePath);
 
