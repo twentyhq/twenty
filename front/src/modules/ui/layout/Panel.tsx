fd664145d5e3cb6103324ffab60572d2dee55e6f
Félix Malfait <felix.malfait@gmail.com>
2023-07-08 22:47:48 +0200

    Bring back border radius inadvertently removed

--- front/src/modules/ui/layout/Panel.tsx
+++ front/src/modules/ui/layout/Panel.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 const StyledPanel = styled.div`
   background: ${({ theme }) => theme.background.primary};
   border: 1px solid ${({ theme }) => theme.border.color.light};
+  border-radius: 8px;
   display: flex;
   flex-direction: row;
   width: 100%;
