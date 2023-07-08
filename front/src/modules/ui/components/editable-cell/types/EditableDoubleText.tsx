10f2ee2d6287b0f7ea4b22e296c8e34fcc5c74ea
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-07-07 18:00:32 -0700

    Fix

--- front/src/modules/ui/components/editable-cell/types/EditableDoubleText.tsx
+++ front/src/modules/ui/components/editable-cell/types/EditableDoubleText.tsx
@@ -1,14 +1,8 @@
-import { ChangeEvent, ReactElement, useRef, useState } from 'react';
-import styled from '@emotion/styled';
-import { Key } from 'ts-key-enum';
+import { ReactElement } from 'react';
 
-import { useScopedHotkeys } from '@/hotkeys/hooks/useScopedHotkeys';
 import { InternalHotkeysScope } from '@/hotkeys/types/internal/InternalHotkeysScope';
-import { useMoveSoftFocus } from '@/ui/tables/hooks/useMoveSoftFocus';
-import { textInputStyle } from '@/ui/themes/effects';
 
 import { EditableCell } from '../EditableCell';
-import { useEditableCell } from '../hooks/useCloseEditableCell';
 
 import { EditableDoubleTextEditMode } from './EditableDoubleTextEditMode';
 
