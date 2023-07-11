f96e786bb69d8ae009a41eff8688198d2a315707
Emilien <emilien.chauvet.enpc@gmail.com>
2023-07-11 12:02:36 -0700

    Improve UX

--- front/src/modules/workspace/components/WorkspaceInviteLink.tsx
+++ front/src/modules/workspace/components/WorkspaceInviteLink.tsx
@@ -4,15 +4,15 @@ import styled from '@emotion/styled';
 
 import { Button } from '@/ui/components/buttons/Button';
 import { TextInput } from '@/ui/components/inputs/TextInput';
-import { IconLink } from '@/ui/icons';
+import { IconCheck, IconLink } from '@/ui/icons';
 
-const CopyLinkContainer = styled.div`
+const StyledContainer = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
 `;
 
-const LinkContainer = styled.div`
+const StyledLinkContainer = styled.div`
   flex: 1;
   margin-right: ${({ theme }) => theme.spacing(2)};
 `;
@@ -25,20 +25,25 @@ export function WorkspaceInviteLink({ inviteLink }: OwnProps) {
   const theme = useTheme();
   const [isCopied, setIsCopied] = useState(false);
   return (
-    <CopyLinkContainer>
-      <LinkContainer>
+    <StyledContainer>
+      <StyledLinkContainer>
         <TextInput value={inviteLink} disabled fullWidth />
-      </LinkContainer>
+      </StyledLinkContainer>
       <Button
-        icon={<IconLink size={theme.icon.size.md} />}
+        icon={
+          isCopied ? (
+            <IconCheck size={theme.icon.size.md} />
+          ) : (
+            <IconLink size={theme.icon.size.md} />
+          )
+        }
         variant="primary"
         title={isCopied ? 'Copied' : 'Copy link'}
-        disabled={isCopied}
         onClick={() => {
           setIsCopied(true);
           navigator.clipboard.writeText(inviteLink);
         }}
       />
-    </CopyLinkContainer>
+    </StyledContainer>
   );
 }
