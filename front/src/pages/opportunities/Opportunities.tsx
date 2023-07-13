4270dc3f2b68b311e64eeb04e8ccd325bedbd21c
Emilien <emilien.chauvet.enpc@gmail.com>
2023-07-12 18:25:35 -0700

    Make dnd and update logic part of the board

--- front/src/pages/opportunities/Opportunities.tsx
+++ front/src/pages/opportunities/Opportunities.tsx
@@ -4,15 +4,11 @@ import { CompanyBoardContext } from '@/companies/components/CompanyBoard/Company
 import { CompanyProgressBoard } from '@/companies/components/CompanyBoard/CompanyProgressBoard';
 import { BoardActionBarButtonDeletePipelineProgress } from '@/pipeline-progress/components/BoardActionBarButtonDeletePipelineProgress';
 import { EntityBoardActionBar } from '@/pipeline-progress/components/EntityBoardActionBar';
-import { useBoard } from '@/pipeline-progress/hooks/useBoard';
 import { RecoilScope } from '@/recoil-scope/components/RecoilScope';
 import { IconTargetArrow } from '@/ui/icons/index';
 import { WithTopBarContainer } from '@/ui/layout/containers/WithTopBarContainer';
 
-import {
-  useGetCompaniesQuery,
-  useGetPipelinesQuery,
-} from '../../generated/graphql';
+import { useGetPipelinesQuery } from '../../generated/graphql';
 
 export function Opportunities() {
   const theme = useTheme();
@@ -20,23 +16,14 @@ export function Opportunities() {
   const pipelines = useGetPipelinesQuery();
   const pipelineId = pipelines.data?.findManyPipeline[0]?.id;
 
-  const { initialBoard, items } = useBoard(
-    pipelineId || '',
-    useGetCompaniesQuery,
-  );
-
   return (
     <WithTopBarContainer
       title="Opportunities"
       icon={<IconTargetArrow size={theme.icon.size.md} />}
     >
-      {items && pipelineId ? (
+      {pipelineId ? (
         <RecoilScope SpecificContext={CompanyBoardContext}>
-          <CompanyProgressBoard
-            pipelineId={pipelineId}
-            initialBoard={initialBoard}
-            initialItems={items}
-          />
+          <CompanyProgressBoard pipelineId={pipelineId} />
           <EntityBoardActionBar>
             <BoardActionBarButtonDeletePipelineProgress />
           </EntityBoardActionBar>
