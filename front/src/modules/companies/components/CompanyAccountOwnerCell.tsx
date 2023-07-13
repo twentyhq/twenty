680c2acd6f226c3b303c46517099566f497fd3e1
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-07-13 09:25:57 -0700

    Fixed hotkeys scope

--- front/src/modules/companies/components/CompanyAccountOwnerCell.tsx
+++ front/src/modules/companies/components/CompanyAccountOwnerCell.tsx
@@ -1,3 +1,4 @@
+import { InternalHotkeysScope } from '@/hotkeys/types/internal/InternalHotkeysScope';
 import { PersonChip } from '@/people/components/PersonChip';
 import { EditableCell } from '@/ui/components/editable-cell/EditableCell';
 import { Company, User } from '~/generated/graphql';
@@ -15,6 +16,7 @@ export type OwnProps = {
 export function CompanyAccountOwnerCell({ company }: OwnProps) {
   return (
     <EditableCell
+      editHotkeysScope={{ scope: InternalHotkeysScope.RelationPicker }}
       editModeContent={<CompanyAccountOwnerPicker company={company} />}
       nonEditModeContent={
         company.accountOwner?.displayName ? (
