f4024b8723a6e2ad2ad28fdbe770d0fecb695648
Thaïs Guigon <guigon.thais@gmail.com>
2023-07-13 15:47:20 +0200

    docs: add EditableCellText stories

--- front/src/testing/decorators.tsx
+++ front/src/testing/decorators.tsx
@@ -2,6 +2,11 @@ import { ApolloProvider } from '@apollo/client';
 import { Decorator } from '@storybook/react';
 import { RecoilRoot } from 'recoil';
 
+import { RecoilScope } from '../modules/recoil-scope/components/RecoilScope';
+import { CellContext } from '../modules/ui/tables/states/CellContext';
+import { RowContext } from '../modules/ui/tables/states/RowContext';
+
+import { ComponentStorybookLayout } from './ComponentStorybookLayout';
 import { mockedClient } from './mockedClient';
 
 export const RootDecorator: Decorator = (Story) => (
@@ -10,4 +15,18 @@ export const RootDecorator: Decorator = (Story) => (
       <Story />
     </ApolloProvider>
   </RecoilRoot>
+);
+
+export const ComponentDecorator: Decorator = (Story) => (
+  <ComponentStorybookLayout>
+    <Story />
+  </ComponentStorybookLayout>
+);
+
+export const CellPositionDecorator: Decorator = (Story) => (
+  <RecoilScope SpecificContext={RowContext}>
+    <RecoilScope SpecificContext={CellContext}>
+      <Story />
+    </RecoilScope>
+  </RecoilScope>
 );
