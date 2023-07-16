d18c76c60f9c0d03e52b1ef1b0e28189dd251839
Félix Malfait <felix.malfait@gmail.com>
2023-07-16 08:30:24 +0200

    Begin - fix account owner not displayed

--- front/src/modules/companies/components/CompanyAccountOwnerCell.tsx
+++ front/src/modules/companies/components/CompanyAccountOwnerCell.tsx
@@ -7,7 +7,7 @@ import { Company, User } from '~/generated/graphql';
 import { CompanyAccountOwnerPicker } from './CompanyAccountOwnerPicker';
 
 export type CompanyAccountOnwer = Pick<Company, 'id'> & {
-  accountOwner?: Pick<User, 'id' | 'displayName'> | null;
+  accountOwner?: Pick<User, 'id' | 'displayName' | 'avatarUrl'> | null;
 };
 
 export type OwnProps = {
@@ -40,6 +40,7 @@ export function CompanyAccountOwnerCell({ company }: OwnProps) {
           <PersonChip
             id={company.accountOwner.id}
             name={company.accountOwner?.displayName ?? ''}
+            picture={company.accountOwner?.avatarUrl ?? ''}
           />
         ) : (
           <></>
