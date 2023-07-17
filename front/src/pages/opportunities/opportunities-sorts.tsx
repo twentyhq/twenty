022662b8bdd2726e756437ab84a30a0b4a001d08
Emilien <emilien.chauvet.enpc@gmail.com>
2023-07-17 16:54:23 -0700

    Add remainint sorts

--- front/src/pages/opportunities/opportunities-sorts.tsx
+++ front/src/pages/opportunities/opportunities-sorts.tsx
@@ -1,11 +1,21 @@
 import { SortType } from '@/ui/filter-n-sort/types/interface';
-import { IconCalendarEvent } from '@/ui/icon/index';
+import { IconCalendarEvent, IconCurrencyDollar } from '@/ui/icon/index';
 import { PipelineProgressOrderByWithRelationInput as PipelineProgresses_Order_By } from '~/generated/graphql';
 
 export const availableSorts = [
   {
+    key: 'createdAt',
+    label: 'Creation',
+    icon: <IconCalendarEvent size={16} />,
+  },
+  {
     key: 'amount',
     label: 'Amount',
+    icon: <IconCurrencyDollar size={16} />,
+  },
+  {
+    key: 'closeDate',
+    label: 'Expected close date',
     icon: <IconCalendarEvent size={16} />,
   },
 ] satisfies Array<SortType<PipelineProgresses_Order_By>>;
