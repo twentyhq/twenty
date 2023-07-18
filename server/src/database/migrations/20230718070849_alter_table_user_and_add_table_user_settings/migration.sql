83a86aed5a8e1f66ca5524c25d999c5f0748996c
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-07-18 10:16:39 -0700

    Fix from review

--- server/src/database/migrations/20230718070849_alter_table_user_and_add_table_user_settings/migration.sql
+++ server/src/database/migrations/20230718070849_alter_table_user_and_add_table_user_settings/migration.sql
@@ -1,6 +1,3 @@
--- Temporarily rename 'locale' to 'old_locale' to keep its values
-ALTER TABLE "users" RENAME COLUMN "locale" TO "old_locale";
-
 -- Adding 'settingsId' to 'users' table without NOT NULL constraint initially
 ALTER TABLE "users" ADD COLUMN "settingsId" TEXT;
 
@@ -16,32 +13,8 @@ CREATE TABLE "user_settings" (
     CONSTRAINT "user_settings_pkey" PRIMARY KEY ("id")
 );
 
--- Use a PL/pgSQL block to process each user one by one
-DO
-$$
-DECLARE
-  row users%rowtype;
-BEGIN
-   FOR row IN SELECT * FROM users
-  LOOP
-      -- Insert a new row into `user_settings` and capture the generated id
-      WITH new_setting AS (
-        INSERT INTO "user_settings"("id", "locale", "updatedAt")
-        VALUES((gen_random_uuid()::TEXT), row.old_locale, CURRENT_TIMESTAMP)
-        RETURNING "id"
-      )
-      -- Update the `settingsId` field in `users` table with the generated id
-      UPDATE "users"
-      SET "settingsId" = (SELECT "id" FROM new_setting)
-      WHERE "id" = row.id;
-  END LOOP;
-END
-$$;
-
 -- Applying constraints and indexes
 ALTER TABLE "users" ALTER COLUMN "settingsId" SET NOT NULL;
 CREATE UNIQUE INDEX "users_settingsId_key" ON "users"("settingsId");
 ALTER TABLE "users" ADD CONSTRAINT "users_settingsId_fkey" FOREIGN KEY ("settingsId") REFERENCES "user_settings"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
--- Finally, dropping 'old_locale'
-ALTER TABLE "users" DROP COLUMN "old_locale";
