a57fc8b0cca068f910863257a0e3b13cddb99187
Emilien <emilien.chauvet.enpc@gmail.com>
2023-07-20 12:04:30 -0700

    Remove useless code

---
+++ front/src/modules/ui/board/components/__stories__/BoardColumnEditTitleMenu.story.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { getRenderWrapperForComponent } from '~/testing/renderWrappers';
+
+import { BoardColumnEditTitleMenu } from '../BoardColumnEditTitleMenu';
+
+const meta: Meta<typeof BoardColumnEditTitleMenu> = {
+  title: 'UI/Accessories/Tag',
+  component: BoardColumnEditTitleMenu,
+};
+
+export default meta;
+type Story = StoryObj<typeof BoardColumnEditTitleMenu>;
+
+export const AllTags: Story = {
+  render: getRenderWrapperForComponent(
+    <BoardColumnEditTitleMenu
+      title={'Column title'}
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      onClose={() => {}}
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      onTitleEdit={() => {}}
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      onColumnColorEdit={() => {}}
+    />,
+  ),
+};
