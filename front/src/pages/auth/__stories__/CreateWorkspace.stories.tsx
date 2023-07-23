7638ba4136d3ae45c8868f578b5859663d8ba753
Charles Bochet <charles@twenty.com>
2023-07-22 23:00:25 -0700

    Fix tests

--- front/src/pages/auth/__stories__/CreateWorkspace.stories.tsx
+++ front/src/pages/auth/__stories__/CreateWorkspace.stories.tsx
@@ -1,10 +1,12 @@
+import { getOperationName } from '@apollo/client/utilities';
 import type { Meta, StoryObj } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import { graphql } from 'msw';
 
-import { AuthModal } from '@/auth/components/Modal';
-import { AuthLayout } from '@/ui/layout/components/AuthLayout';
-import { graphqlMocks } from '~/testing/graphqlMocks';
 import { getRenderWrapperForPage } from '~/testing/renderWrappers';
 
+import { GET_CURRENT_USER } from '../../../modules/users/queries';
+import { mockedOnboardingUsersData } from '../../../testing/mock-data/users';
 import { CreateWorkspace } from '../CreateWorkspace';
 
 const meta: Meta<typeof CreateWorkspace> = {
@@ -17,15 +19,23 @@ export default meta;
 export type Story = StoryObj<typeof CreateWorkspace>;
 
 export const Default: Story = {
-  render: getRenderWrapperForPage(
-    <AuthLayout>
-      <AuthModal>
-        <CreateWorkspace />
-      </AuthModal>
-    </AuthLayout>,
-    '/create-workspace',
-  ),
+  render: getRenderWrapperForPage(<CreateWorkspace />, '/create/workspace'),
   parameters: {
-    msw: graphqlMocks,
+    msw: [
+      graphql.query(
+        getOperationName(GET_CURRENT_USER) ?? '',
+        (req, res, ctx) => {
+          return res(
+            ctx.data({
+              currentUser: mockedOnboardingUsersData[0],
+            }),
+          );
+        },
+      ),
+    ],
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await canvas.findByText('Create your workspace');
   },
 };
