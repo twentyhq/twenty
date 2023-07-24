2b885f249600437340425c081c9a477ef201a82a
310387 <139059022+310387@users.noreply.github.com>
2023-07-24 03:02:21 +0300

    fix: 801 dont show if name empty (#854)
    * fix: 801 dont show if name empty
    
    * fix: 801 add same logic to multiple entity

--- front/src/modules/ui/relation-picker/components/SingleEntitySelectBase.tsx
+++ front/src/modules/ui/relation-picker/components/SingleEntitySelectBase.tsx
@@ -7,6 +7,7 @@ import { DropdownMenuSelectableItem } from '@/ui/dropdown/components/DropdownMen
 import { useScopedHotkeys } from '@/ui/hotkey/hooks/useScopedHotkeys';
 import { Avatar } from '@/users/components/Avatar';
 import { isDefined } from '~/utils/isDefined';
+import { isNonEmptyString } from '~/utils/isNonEmptyString';
 
 import { OverflowingTextWithTooltip } from '../../tooltip/OverflowingTextWithTooltip';
 import { useEntitySelectScroll } from '../hooks/useEntitySelectScroll';
@@ -35,9 +36,13 @@ export function SingleEntitySelectBase<
   onCancel?: () => void;
 }) {
   const containerRef = useRef<HTMLDivElement>(null);
-  const entitiesInDropdown = isDefined(entities.selectedEntity)
+  let entitiesInDropdown = isDefined(entities.selectedEntity)
     ? [entities.selectedEntity, ...(entities.entitiesToSelect ?? [])]
     : entities.entitiesToSelect ?? [];
+
+  entitiesInDropdown = entitiesInDropdown.filter((entity) =>
+    isNonEmptyString(entity.name),
+  );
 
   const { hoveredIndex, resetScroll } = useEntitySelectScroll({
     entities: entitiesInDropdown,
