ef35adcb2fcd264f3f8f079e35c77117da09b0c8
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-07-25 10:40:07 -0700

    Added sleep for failing test

--- front/src/modules/ui/table/editable-cell/components/__stories__/EditableCellText.stories.tsx
+++ front/src/modules/ui/table/editable-cell/components/__stories__/EditableCellText.stories.tsx
@@ -1,6 +1,7 @@
 import { expect } from '@storybook/jest';
 import type { Meta, StoryObj } from '@storybook/react';
 import { userEvent, within } from '@storybook/testing-library';
+import { sleep } from '@tanstack/query-core/build/lib/utils';
 
 import { CellPositionDecorator } from '~/testing/decorators/CellPositionDecorator';
 import { ComponentDecorator } from '~/testing/decorators/ComponentDecorator';
@@ -35,6 +36,8 @@ export const SoftFocusMode: Story = {
     await step('Escape', async () => {
       await userEvent.keyboard('{esc}');
     });
+
+    await sleep(1000);
 
     await step('Has soft focus mode', () => {
       expect(canvas.getByTestId('editable-cell-soft-focus-mode')).toBeDefined();
