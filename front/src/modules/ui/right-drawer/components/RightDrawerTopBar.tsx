0cc7d1cfd81de6846ef036a0a0b67cf6938326ca
gitstart-twenty <twenty@gitstart.com>
2023-07-31 14:26:56 +0000

    Move trash icon to the top bar of right drawer
    Co-authored-by: Matheus <matheus_benini@hotmail.com>

--- front/src/modules/ui/right-drawer/components/RightDrawerTopBar.tsx
+++ front/src/modules/ui/right-drawer/components/RightDrawerTopBar.tsx
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled';
+import { useRecoilValue } from 'recoil';
 
+import { ActivityActionBar } from '@/activities/right-drawer/components/ActivityActionBar';
+import { viewableActivityIdState } from '@/activities/states/viewableActivityIdState';
 import { useIsMobile } from '@/ui/utilities/responsive/hooks/useIsMobile';
+import { useGetActivityQuery } from '~/generated/graphql';
 
 import { RightDrawerTopBarCloseButton } from './RightDrawerTopBarCloseButton';
 import { RightDrawerTopBarExpandButton } from './RightDrawerTopBarExpandButton';
@@ -15,19 +19,35 @@ const StyledRightDrawerTopBar = styled.div`
   font-size: ${({ theme }) => theme.font.size.md};
   gap: ${({ theme }) => theme.spacing(1)};
   height: 56px;
-  justify-content: flex-start;
+  justify-content: space-between;
+  margin-right: ${({ theme }) => theme.spacing(2)};
   padding-left: ${({ theme }) => theme.spacing(2)};
-
   padding-right: ${({ theme }) => theme.spacing(2)};
+`;
+
+const TopBarWrapper = styled.div`
+  display: flex;
 `;
 
 export function RightDrawerTopBar() {
   const isMobile = useIsMobile();
+  const activityId = useRecoilValue(viewableActivityIdState);
+
+  const { data } = useGetActivityQuery({
+    variables: {
+      activityId: activityId ?? '',
+    },
+    skip: !activityId,
+  });
+  const activity = data?.findManyActivities[0];
 
   return (
     <StyledRightDrawerTopBar>
-      <RightDrawerTopBarCloseButton />
-      {!isMobile && <RightDrawerTopBarExpandButton />}
+      <TopBarWrapper>
+        <RightDrawerTopBarCloseButton />
+        {!isMobile && <RightDrawerTopBarExpandButton />}
+      </TopBarWrapper>
+      <ActivityActionBar activityId={activity?.id ?? ''} />
     </StyledRightDrawerTopBar>
   );
 }
