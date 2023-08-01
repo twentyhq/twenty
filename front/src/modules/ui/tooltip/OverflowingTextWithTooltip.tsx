8b8e4ac4a53b36ce898bc1ace401986cd4a330bd
Charles Bochet <charlesBochet@users.noreply.github.com>
2023-07-31 18:15:08 -0700

    A few polish on tasks (#1023)
    A few polishing on tasks

--- front/src/modules/ui/tooltip/OverflowingTextWithTooltip.tsx
+++ front/src/modules/ui/tooltip/OverflowingTextWithTooltip.tsx
@@ -11,12 +11,12 @@ const StyledOverflowingText = styled.div<{ cursorPointer: boolean }>`
   font-size: inherit;
 
   font-weight: inherit;
+  max-width: 100%;
   overflow: hidden;
   text-decoration: inherit;
-  text-overflow: ellipsis;
 
+  text-overflow: ellipsis;
   white-space: nowrap;
-  width: 100%;
 `;
 
 export function OverflowingTextWithTooltip({
