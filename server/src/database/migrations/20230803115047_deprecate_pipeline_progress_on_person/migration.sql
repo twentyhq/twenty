770d428aa09eaab863e5b84d67d0f8a05dc955c4
Emilien <emilien.chauvet.enpc@gmail.com>
2023-08-03 18:02:10 +0200

    Remove useless migrations

--- server/src/database/migrations/20230803115047_deprecate_pipeline_progress_on_person/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "pipeline_progresses" ADD COLUMN     "deprecatedPointOfContactId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "pipeline_progresses" ADD CONSTRAINT "pipeline_progresses_deprecatedPointOfContactId_fkey" FOREIGN KEY ("deprecatedPointOfContactId") REFERENCES "people"("id") ON DELETE SET NULL ON UPDATE CASCADE;
