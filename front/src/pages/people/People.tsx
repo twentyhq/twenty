c21e828ad4e3f9658b2c7348ddcb189ec5902d68
aditya pimpalkar <adipimpalkar@gmail.com>
2023-08-06 00:22:09 +0100

    removed favorite from index list

--- front/src/pages/people/People.tsx
+++ front/src/pages/people/People.tsx
@@ -1,9 +1,7 @@
 import { getOperationName } from '@apollo/client/utilities';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
-import { useRecoilValue } from 'recoil';
 
-import { GET_FAVORITES } from '@/favorites/queries/show';
 import { GET_PEOPLE } from '@/people/queries';
 import { PeopleTable } from '@/people/table/components/PeopleTable';
 import { TableActionBarButtonCreateActivityPeople } from '@/people/table/components/TableActionBarButtonCreateActivityPeople';
@@ -11,15 +9,9 @@ import { TableActionBarButtonDeletePeople } from '@/people/table/components/Tabl
 import { IconUser } from '@/ui/icon';
 import { WithTopBarContainer } from '@/ui/layout/components/WithTopBarContainer';
 import { EntityTableActionBar } from '@/ui/table/action-bar/components/EntityTableActionBar';
-import { useResetTableRowSelection } from '@/ui/table/hooks/useResetTableRowSelection';
-import { selectedRowIdsSelector } from '@/ui/table/states/selectedRowIdsSelector';
 import { TableContext } from '@/ui/table/states/TableContext';
 import { RecoilScope } from '@/ui/utilities/recoil-scope/components/RecoilScope';
 import { useInsertOnePersonMutation } from '~/generated/graphql';
-import {
-  FavoriteCreateManyInput,
-  useInsertManyFavoritesMutation,
-} from '~/generated/graphql';
 
 const StyledTableContainer = styled.div`
   display: flex;
@@ -28,9 +20,6 @@ const StyledTableContainer = styled.div`
 
 export function People() {
   const [insertOnePerson] = useInsertOnePersonMutation();
-  const [insertManyFavorites] = useInsertManyFavoritesMutation();
-  const selectedRowIds = useRecoilValue(selectedRowIdsSelector);
-  const resetRowSelection = useResetTableRowSelection();
 
   async function handleAddButtonClick() {
     await insertOnePerson({
@@ -44,25 +33,6 @@ export function People() {
     });
   }
 
-  async function handleAddFavorite() {
-    const rowIdsToFavorite = selectedRowIds;
-    if (rowIdsToFavorite.length > 0) {
-      resetRowSelection();
-      const favorites = rowIdsToFavorite.map((id) => {
-        return {
-          personId: id,
-        } as FavoriteCreateManyInput;
-      });
-
-      await insertManyFavorites({
-        variables: {
-          data: favorites,
-        },
-        refetchQueries: [getOperationName(GET_FAVORITES) ?? ''],
-      });
-    }
-  }
-
   const theme = useTheme();
 
   return (
@@ -71,7 +41,6 @@ export function People() {
         title="People"
         icon={<IconUser size={theme.icon.size.sm} />}
         onAddButtonClick={handleAddButtonClick}
-        onFavouriteButtonClick={handleAddFavorite}
       >
         <StyledTableContainer>
           <PeopleTable />
