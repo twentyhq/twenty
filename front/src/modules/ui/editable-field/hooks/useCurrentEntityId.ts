2a248088d39d6f93d7f1bee32cc64204c8da0734
corentin <corentin@twenty.com>
2023-08-09 15:55:48 -0700

    remove unused hook

--- front/src/modules/ui/editable-field/hooks/useCurrentEntityId.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import { EntityIdContext } from '../states/EditableFieldEntityIdContext';
-
-export function useCurrentEntityId() {
-  return useContext(EntityIdContext);
-}
