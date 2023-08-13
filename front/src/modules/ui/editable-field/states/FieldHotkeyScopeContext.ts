1680887a57c0a48db53ce92d9106c121ecac671a
corentin <corentin@twenty.com>
2023-08-12 17:09:08 -0700

    remove FieldHotkeyScopeContext.ts

--- front/src/modules/ui/editable-field/states/FieldHotkeyScopeContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-import { HotkeyScope } from '@/ui/utilities/hotkey/types/HotkeyScope';
-
-export const FieldHotkeyScopeContext = createContext<HotkeyScope | null>(null);
