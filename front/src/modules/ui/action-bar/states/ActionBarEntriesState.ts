537b227befb6268baac26dd0db150a2134555cd3
Charles Bochet <charles@twenty.com>
2023-08-14 17:18:51 -0700

    Fix lint

--- front/src/modules/ui/action-bar/states/ActionBarEntriesState.ts
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from 'react';
-import { atom } from 'recoil';
-
-export const actionBarEntriesState = atom<ReactElement[]>({
-  key: 'actionBarEntriesState',
-  default: [],
-});
