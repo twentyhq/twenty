0830fe18134104479c8569563914495fa6931fe9
Sunil Kumar <sunilk10099@gmail.com>
2023-08-17 18:42:26 +0530

    Revert "add states"
    This reverts commit 132aa5066a2405ce9f09eb5c504befae11dab00b.

--- front/src/modules/activities/states/draftActivityState.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-
-import { ActivityForDrawer } from '../types/ActivityForDrawer';
-
-export const DraftActivityState = atom<ActivityForDrawer | null>({
-  key: 'activities/draft-activity-state',
-  default: null,
-});
--- front/src/modules/activities/states/isActivityPersistedDBState.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const isActivityPersistedDBState = atom<boolean>({
-  key: 'activities/is-activity-persisted-db',
-  default: false,
-});
