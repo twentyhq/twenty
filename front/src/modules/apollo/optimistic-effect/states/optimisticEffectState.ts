0dd20ae44e303d04d563787c72b6b326271dd2d8
Charles Bochet <charles@twenty.com>
2023-09-01 17:31:17 +0200

    Implement optimistic effects

--- front/src/modules/apollo/optimistic-effect/states/optimisticEffectState.ts
+++ front/src/modules/apollo/optimistic-effect/states/optimisticEffectState.ts
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 import { OptimisticEffect } from '../types/OptimisticEffect';
 
 export const optimisticEffectState = atom<
-  Record<string, OptimisticEffect<any, any>>
+  Record<string, OptimisticEffect<unknown, unknown>>
 >({
   key: 'optimisticEffectState',
   default: {},
