c998c039ec9f40c99e718c84a1466c1f23147b85
Ragnar Laud <ragnar.laud@hotmail.com>
2023-09-04 12:30:43 +0300

    Fix URL validation on long/internationalized URLs (#1423)
    * Add URL validation tests
    
    Add test for longer TLDs
    Add test for internationalized TLDs
    
    * Fix URL validation with long TLDs
    
    * TLD max size match RFC 1034
    
    ---------
    
    Co-authored-by: Jérémy M <jeremy.magrin@gmail.com>

--- front/src/utils/is-url.ts
+++ front/src/utils/is-url.ts
@@ -4,7 +4,7 @@ export function isURL(url: string | undefined | null) {
   return (
     isDefined(url) &&
     url.match(
-      /^(https?:\/\/)?(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/i,
+      /^(https?:\/\/)?(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,63}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/i,
     )
   );
 }
