4a1049ab33abf91cbabb52fb5f479fbadf6d8386
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-09-04 17:46:08 +0200

    fix: smaller max size

--- front/src/modules/ui/table/options/components/TableViewsDropdownButton.tsx
+++ front/src/modules/ui/table/options/components/TableViewsDropdownButton.tsx
@@ -59,7 +59,7 @@ const StyledViewIcon = styled(IconList)`
 
 const StyledViewName = styled.span`
   display: inline-block;
-  max-width: 300px;
+  max-width: 200px;
   @media (max-width: 375px) {
     max-width: 90px;
   }
