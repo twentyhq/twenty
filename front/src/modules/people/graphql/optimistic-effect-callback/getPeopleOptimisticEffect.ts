a46210bb806479f926b0cea3916ac74766a7505b
Charles Bochet <charlesBochet@users.noreply.github.com>
2023-09-04 10:56:48 +0200

    Implement Optimistic Effects (#1415)
    * Fix person deletion not reflected on Opportunities POC
    
    * Fix companies, user deletion
    
    * Implement optimistic effects
    
    * Implement optimistic effects
    
    * Implement optimistic effects
    
    * Fix accoding to PR

---
+++ front/src/modules/people/graphql/optimistic-effect-callback/getPeopleOptimisticEffect.ts
@@ -0,0 +1,45 @@
+import { ApolloCache } from '@apollo/client';
+
+import {
+  GetPeopleQuery,
+  GetPeopleQueryVariables,
+  Person,
+} from '~/generated/graphql';
+
+import { GET_PEOPLE } from '../queries/getPeople';
+
+function optimisticEffectResolver({
+  cache,
+  entities,
+  variables,
+}: {
+  cache: ApolloCache<Person>;
+  entities: Person[];
+  variables: GetPeopleQueryVariables;
+}) {
+  const existingData = cache.readQuery<GetPeopleQuery>({
+    query: GET_PEOPLE,
+    variables: { orderBy: variables.orderBy, where: variables.where },
+  });
+
+  if (!existingData) {
+    return;
+  }
+
+  cache.writeQuery({
+    query: GET_PEOPLE,
+    variables: { orderBy: variables.orderBy, where: variables.where },
+    data: {
+      people: [...entities, ...existingData.people],
+    },
+  });
+}
+
+export function getPeopleOptimisticEffect(variables: GetPeopleQueryVariables) {
+  return {
+    key: 'generic-entity-table-data-person',
+    variables: variables,
+    typename: 'Person',
+    resolver: optimisticEffectResolver,
+  };
+}
