33e8d61ef28a5ba2c4d8ef89ab7c9bc3b6c6d32d
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2023-09-04 14:31:43 -0400

    add rule

--- packages/eslint-plugin-twenty/index.js
+++ packages/eslint-plugin-twenty/index.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const noHardcodedColors = require('./rules/no-hardcoded-colors');
-const cssAlphabetically = require('./rules/sort-css-properties-alphabetically');
-const styledComponentsPrefixedWithStyled = require('./rules/styled-components-prefixed-with-styled');
+const noHardcodedColors = require("./rules/no-hardcoded-colors");
+const cssAlphabetically = require("./rules/sort-css-properties-alphabetically");
+const styledComponentsPrefixedWithStyled = require("./rules/styled-components-prefixed-with-styled");
+const useScopedHotkeysInstead = require("./rules/use-scopedhotkeys-instead");
 
 module.exports = {
-  rules: {
-    'no-hardcoded-colors': noHardcodedColors,
-    'sort-css-properties-alphabetically': cssAlphabetically,
-    'styled-components-prefixed-with-styled': styledComponentsPrefixedWithStyled,
-  },
+	rules: {
+		"no-hardcoded-colors": noHardcodedColors,
+		"sort-css-properties-alphabetically": cssAlphabetically,
+		"styled-components-prefixed-with-styled":
+			styledComponentsPrefixedWithStyled,
+		"use-scopedhotkeys-instead": useScopedHotkeysInstead,
+	},
 };
