efc45f8663afe54a0aa05c3b2d1b2397f741bf6c
brendanlaschke <brendan.laschke@googlemail.com>
2023-09-16 22:05:55 +0300

    Add company relation for person table (#1612)
    * - add company relation for person table
    
    * - also for context menu
    
    * - fix yarn.lock
    
    * - fix newline missing
    
    * - fixed tab
    
    * fix

--- front/src/modules/people/hooks/usePersonTableActionBarEntries.tsx
+++ front/src/modules/people/hooks/usePersonTableActionBarEntries.tsx
@@ -1,8 +1,6 @@
 import { getOperationName } from '@apollo/client/utilities';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
-import { useOpenCreateActivityDrawerForSelectedRowIds } from '@/activities/hooks/useOpenCreateActivityDrawerForSelectedRowIds';
-import { ActivityTargetableEntityType } from '@/activities/types/ActivityTargetableEntity';
 import { actionBarEntriesState } from '@/ui/action-bar/states/actionBarEntriesState';
 import { IconCheckbox, IconNotes, IconTrash } from '@/ui/icon';
 import { useResetTableRowSelection } from '@/ui/table/hooks/useResetTableRowSelection';
@@ -12,18 +10,13 @@ import { ActivityType, useDeleteManyPersonMutation } from '~/generated/graphql';
 
 import { GET_PEOPLE } from '../graphql/queries/getPeople';
 
+import { useCreateActivityForPeople } from './useCreateActivityForPeople';
+
 export const usePersonTableActionBarEntries = () => {
-  const setActionBarEntries = useSetRecoilState(actionBarEntriesState);
-
-  const openCreateActivityRightDrawer =
-    useOpenCreateActivityDrawerForSelectedRowIds();
-
-  const handleActivityClick = async (type: ActivityType) => {
-    openCreateActivityRightDrawer(type, ActivityTargetableEntityType.Person);
-  };
-
   const selectedRowIds = useRecoilValue(selectedRowIdsSelector);
   const [tableRowIds, setTableRowIds] = useRecoilState(tableRowIdsState);
+  const setActionBarEntries = useSetRecoilState(actionBarEntriesState);
+  const createActivityForPeople = useCreateActivityForPeople();
 
   const resetRowSelection = useResetTableRowSelection();
 
@@ -64,12 +57,12 @@ export const usePersonTableActionBarEntries = () => {
         {
           label: 'Note',
           Icon: IconNotes,
-          onClick: () => handleActivityClick(ActivityType.Note),
+          onClick: () => createActivityForPeople(ActivityType.Note),
         },
         {
           label: 'Task',
           Icon: IconCheckbox,
-          onClick: () => handleActivityClick(ActivityType.Task),
+          onClick: () => createActivityForPeople(ActivityType.Task),
         },
         {
           label: 'Delete',
