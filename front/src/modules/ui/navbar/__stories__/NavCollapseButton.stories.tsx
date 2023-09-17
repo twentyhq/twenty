b473f119d9cf7daf7ba922598c1ff5977e83e6a0
gitstart-twenty <twenty@gitstart.com>
2023-09-17 18:11:38 +0000

    Write Storybook test for @/ui/navbar
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>

---
+++ front/src/modules/ui/navbar/__stories__/NavCollapseButton.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ComponentDecorator } from '~/testing/decorators/ComponentDecorator';
+
+import NavCollapseButton from '../components/NavCollapseButton';
+
+const meta: Meta<typeof NavCollapseButton> = {
+  title: 'UI/Navbar/NavCollapseButton',
+  component: NavCollapseButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof NavCollapseButton>;
+
+export const Default: Story = {
+  decorators: [ComponentDecorator],
+};
+
+export const Hidden: Story = {
+  args: { show: false },
+  decorators: [ComponentDecorator],
+};
---
+++ front/src/modules/ui/navbar/__stories__/NavItem.stories.tsx
@@ -0,0 +1,94 @@
+import styled from '@emotion/styled';
+import type { Decorator, Meta, StoryObj } from '@storybook/react';
+
+import { IconSearch, IconSettings } from '@/ui/icon';
+import { CatalogDecorator } from '~/testing/decorators/CatalogDecorator';
+import { ComponentWithRouterDecorator } from '~/testing/decorators/ComponentWithRouterDecorator';
+
+import NavItem from '../components/NavItem';
+
+const meta: Meta<typeof NavItem> = {
+  title: 'UI/Navbar/NavItem',
+  component: NavItem,
+};
+
+const StyledNavItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+`;
+
+const ComponentDecorator: Decorator = (Story) => (
+  <StyledNavItemContainer>
+    <Story />
+  </StyledNavItemContainer>
+);
+
+export default meta;
+type Story = StoryObj<typeof NavItem>;
+
+export const Default: Story = {
+  args: {
+    label: 'Search',
+    Icon: IconSearch,
+    onClick: () => console.log('clicked'),
+    active: true,
+  },
+  argTypes: { Icon: { control: false }, onClick: { control: false } },
+  decorators: [ComponentDecorator, ComponentWithRouterDecorator],
+};
+
+export const Catalog: Story = {
+  args: Default.args,
+  decorators: [
+    ComponentDecorator,
+    CatalogDecorator,
+    ComponentWithRouterDecorator,
+  ],
+  parameters: {
+    pseudo: { hover: ['button:has(svg.tabler-icon-settings)'] },
+    catalog: {
+      dimensions: [
+        {
+          name: 'active',
+          values: [true, false],
+          props: (active: boolean) => ({ active }),
+          labels: (active: boolean) => (active ? 'Active' : 'Inactive'),
+        },
+        {
+          name: 'danger',
+          values: [true, false],
+          props: (danger: boolean) => ({ danger }),
+          labels: (danger: boolean) => (danger ? 'Danger' : 'No Danger'),
+        },
+        {
+          name: 'states',
+          values: ['Default', 'Hover'],
+          props: (state: string) =>
+            state === 'Default'
+              ? {}
+              : { label: 'Settings', Icon: IconSettings },
+        },
+      ],
+    },
+  },
+};
+
+export const Soon: Story = {
+  args: {
+    ...Default.args,
+    active: false,
+    soon: true,
+  },
+  argTypes: { Icon: { control: false }, onClick: { control: false } },
+  decorators: [ComponentDecorator, ComponentWithRouterDecorator],
+};
+
+export const Count: Story = {
+  args: {
+    ...Default.args,
+    count: 3,
+  },
+  argTypes: { Icon: { control: false }, onClick: { control: false } },
+  decorators: [ComponentDecorator, ComponentWithRouterDecorator],
+};
