3bbd1dbff989757754ffa6a33e10b9e15c79d9f7
corentin <corentin@twenty.com>
2023-09-19 16:35:28 +0200

    fix tests

--- server/src/tenant/metadata/data-source/data-source.service.spec.ts
+++ server/src/tenant/metadata/data-source/data-source.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { EnvironmentService } from 'src/integrations/environment/environment.service';
+import { DataSourceMetadataService } from 'src/tenant/metadata/data-source-metadata/data-source-metadata.service';
+import { EntitySchemaGeneratorService } from 'src/tenant/metadata/entity-schema-generator/entity-schema-generator.service';
+
 import { DataSourceService } from './data-source.service';
 
 describe('DataSourceService', () => {
@@ -7,7 +11,23 @@ describe('DataSourceService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DataSourceService],
+      providers: [
+        DataSourceService,
+        {
+          provide: EnvironmentService,
+          useValue: {
+            getPGDatabaseUrl: () => '',
+          },
+        },
+        {
+          provide: DataSourceMetadataService,
+          useValue: {},
+        },
+        {
+          provide: EntitySchemaGeneratorService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<DataSourceService>(DataSourceService);
