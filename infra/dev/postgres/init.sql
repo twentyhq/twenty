e634866452806c1326e879ed02e02901fa6bba4c
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-09-20 16:28:25 +0200

    feat: wip add pg_graphql

--- infra/dev/postgres/init.sql
+++ infra/dev/postgres/init.sql
@@ -1,3 +1,39 @@
+-- Create extension
+CREATE EXTENSION pg_graphql;
+
+create role anon;
+
+grant usage on schema public to anon;
+alter default privileges in schema public grant all on tables to anon;
+alter default privileges in schema public grant all on functions to anon;
+alter default privileges in schema public grant all on sequences to anon;
+
+grant usage on schema graphql to anon;
+grant all on function graphql.resolve to anon;
+
+alter default privileges in schema graphql grant all on tables to anon;
+alter default privileges in schema graphql grant all on functions to anon;
+alter default privileges in schema graphql grant all on sequences to anon;
+
+
+-- GraphQL Entrypoint
+create function graphql(
+    "operationName" text default null,
+    query text default null,
+    variables jsonb default null,
+    extensions jsonb default null
+)
+    returns jsonb
+    language sql
+as $$
+    select graphql.resolve(
+        query := query,
+        variables := coalesce(variables, '{}'),
+        "operationName" := "operationName",
+        extensions := extensions
+    );
+$$;
+
 -- Create the default database for development
 CREATE DATABASE "default";
 
