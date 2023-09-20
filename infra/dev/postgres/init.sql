444e64d47c1afa4361e39a335147bd442aba1080
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-09-20 14:54:25 +0200

    feat: create metadata schema in default database

--- infra/dev/postgres/init.sql
+++ infra/dev/postgres/init.sql
@@ -7,3 +7,9 @@ CREATE DATABASE "test";
 -- Create a twenty user
 CREATE USER twenty PASSWORD 'twenty';
 ALTER USER twenty CREATEDB;
+
+-- Connect to the "default" database
+\c "default";
+
+-- Create the metadata schema if it doesn't exist
+CREATE SCHEMA IF NOT EXISTS "metadata";
