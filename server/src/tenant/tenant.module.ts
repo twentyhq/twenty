be8e924dc1dc53b0d5ab246d05f9beacb3c1083b
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-09-20 14:35:27 +0200

    feat: rename custom into universal

--- server/src/tenant/tenant.module.ts
+++ server/src/tenant/tenant.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { MetadataModule } from './metadata/metadata.module';
-import { UniversalModule } from './custom/universal.module';
+import { UniversalModule } from './universal/universal.module';
 
 @Module({
   imports: [MetadataModule, UniversalModule],
