0baca654a9f1763422b4a1e42002caf5860020af
Charles Bochet <charles@twenty.com>
2023-09-21 12:39:34 -0700

    Fix tests

--- front/src/modules/ui/input/relation-picker/hooks/useEntitySelectSearch.ts
+++ front/src/modules/ui/input/relation-picker/hooks/useEntitySelectSearch.ts
@@ -4,12 +4,12 @@ import debounce from 'lodash.debounce';
 import { useRecoilScopedState } from '@/ui/utilities/recoil-scope/hooks/useRecoilScopedState';
 
 import { RelationPickerRecoilScopeContext } from '../states/recoil-scope-contexts/RelationPickerRecoilScopeContext';
-import { relationPickerHoveredIdScopedState } from '../states/relationPickerPreselectedIdScopedState';
+import { relationPickerPreselectedIdScopedState } from '../states/relationPickerPreselectedIdScopedState';
 import { relationPickerSearchFilterScopedState } from '../states/relationPickerSearchFilterScopedState';
 
 export const useEntitySelectSearch = () => {
-  const [, setRelationPickerHoveredId] = useRecoilScopedState(
-    relationPickerHoveredIdScopedState,
+  const [, setRelationPickerPreselectedId] = useRecoilScopedState(
+    relationPickerPreselectedIdScopedState,
     RelationPickerRecoilScopeContext,
   );
 
@@ -28,7 +28,7 @@ export const useEntitySelectSearch = () => {
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
     debouncedSetSearchFilter(event.currentTarget.value);
-    setRelationPickerHoveredId('');
+    setRelationPickerPreselectedId('');
   };
 
   useEffect(() => {
