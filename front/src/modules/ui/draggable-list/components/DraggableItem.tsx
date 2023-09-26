6370d842da69e9f40e90b4b2dd9d481c91972cb0
aditya pimpalkar <adipimpalkar@gmail.com>
2023-09-26 22:32:42 +0100

    Draggablee item component

---
+++ front/src/modules/ui/draggable-list/components/DraggableItem.tsx
@@ -0,0 +1,44 @@
+import { Draggable } from '@hello-pangea/dnd';
+
+type DraggableItemProps = {
+  key: string;
+  draggableId: string;
+  index: number;
+  itemsComponent: JSX.Element;
+};
+
+export const DraggableItem = ({
+  key,
+  draggableId,
+  index,
+  itemsComponent,
+}: DraggableItemProps) => {
+  return (
+    <Draggable key={key} draggableId={draggableId} index={index}>
+      {(draggableProvided) => {
+        const draggableStyle = draggableProvided.draggableProps.style;
+
+        return (
+          <div
+            ref={draggableProvided.innerRef}
+            {...{
+              ...draggableProvided.draggableProps,
+              style: {
+                ...draggableStyle,
+                left: 'auto',
+                top: 'auto',
+                transform: draggableStyle?.transform?.replace(
+                  /\(-?\d+px,/,
+                  '(0,',
+                ),
+              },
+            }}
+            {...draggableProvided.dragHandleProps}
+          >
+            {itemsComponent}
+          </div>
+        );
+      }}
+    </Draggable>
+  );
+};
