6c06abee004fe04116e785915dde3090ec1e7272
gitstart-twenty <twenty@gitstart.com>
2023-10-03 01:16:19 +0000

    Chore: Move `useCreateActivity` hook to /hooks
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>

--- front/src/modules/activities/components/ActivityEditor.tsx
+++ front/src/modules/activities/components/ActivityEditor.tsx
@@ -3,24 +3,19 @@ import { useApolloClient } from '@apollo/client';
 import { getOperationName } from '@apollo/client/utilities';
 import styled from '@emotion/styled';
 import debounce from 'lodash.debounce';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 import { ActivityBodyEditor } from '@/activities/components/ActivityBodyEditor';
 import { ActivityComments } from '@/activities/components/ActivityComments';
 import { ActivityTypeDropdown } from '@/activities/components/ActivityTypeDropdown';
 import { GET_ACTIVITIES } from '@/activities/graphql/queries/getActivities';
-import { currentUserState } from '@/auth/states/currentUserState';
-import { GET_COMPANIES } from '@/companies/graphql/queries/getCompanies';
-import { GET_PEOPLE } from '@/people/graphql/queries/getPeople';
 import { PropertyBox } from '@/ui/editable-field/property-box/components/PropertyBox';
 import { RecoilScope } from '@/ui/utilities/recoil-scope/components/RecoilScope';
 import { useIsMobile } from '@/ui/utilities/responsive/hooks/useIsMobile';
 import {
   Activity,
-  ActivityCreateInput,
   ActivityTarget,
   ActivityType,
-  useCreateActivityMutation,
   User,
   useUpdateActivityMutation,
 } from '~/generated/graphql';
@@ -29,13 +24,9 @@ import { ActivityAssigneeEditableField } from '../editable-fields/components/Act
 import { ActivityEditorDateField } from '../editable-fields/components/ActivityEditorDateField';
 import { ActivityRelationEditableField } from '../editable-fields/components/ActivityRelationEditableField';
 import { ACTIVITY_UPDATE_FRAGMENT } from '../graphql/fragments/activityUpdateFragment';
-import { GET_ACTIVITIES_BY_TARGETS } from '../graphql/queries/getActivitiesByTarget';
-import { GET_ACTIVITY } from '../graphql/queries/getActivity';
-import { activityTargetableEntityArrayState } from '../states/activityTargetableEntityArrayState';
+import { useCreateActivity } from '../hooks/useCreateActivity';
 import { currentActivityState } from '../states/currentActivityState';
-import { viewableActivityIdState } from '../states/viewableActivityIdState';
 import { CommentForDrawer } from '../types/CommentForDrawer';
-import { getRelationData } from '../utils/getRelationData';
 
 import { ActivityTitle } from './ActivityTitle';
 
@@ -88,63 +79,6 @@ type OwnProps = {
   };
   showComment?: boolean;
   autoFillTitle?: boolean;
-};
-
-// this hook can be used to create an activity and set other fields (data) at the same time
-const useCreateActivity = () => {
-  const currentUser = useRecoilValue(currentUserState);
-  const currentActivity = useRecoilValue(currentActivityState);
-  const [createActivityMutation] = useCreateActivityMutation();
-
-  const [, setViewableActivityId] = useRecoilState(viewableActivityIdState);
-  const [, setActivityTargetableEntityArray] = useRecoilState(
-    activityTargetableEntityArrayState,
-  );
-
-  return (data: Partial<ActivityCreateInput>) => {
-    const now = new Date().toISOString();
-
-    return createActivityMutation({
-      variables: {
-        data: {
-          id: currentActivity?.id,
-          createdAt: now,
-          updatedAt: now,
-          author: { connect: { id: currentUser?.id ?? '' } },
-          workspaceMemberAuthor: {
-            connect: { id: currentUser?.workspaceMember?.id ?? '' },
-          },
-          workspaceMemberAssignee: {
-            connect: { id: currentUser?.workspaceMember?.id ?? '' },
-          },
-          activityTargets: {
-            createMany: {
-              data: currentActivity?.targetableEntities
-                ? getRelationData(currentActivity.targetableEntities)
-                : [],
-              skipDuplicates: true,
-            },
-          },
-          type: currentActivity?.type,
-          assignee: currentActivity?.assignee,
-          ...data,
-        },
-      },
-      refetchQueries: [
-        getOperationName(GET_COMPANIES) ?? '',
-        getOperationName(GET_PEOPLE) ?? '',
-        getOperationName(GET_ACTIVITY) ?? '',
-        getOperationName(GET_ACTIVITIES_BY_TARGETS) ?? '',
-        getOperationName(GET_ACTIVITIES) ?? '',
-      ],
-      onCompleted: (data) => {
-        setViewableActivityId(data.createOneActivity.id);
-        setActivityTargetableEntityArray(
-          currentActivity?.targetableEntities ?? [],
-        );
-      },
-    });
-  };
 };
 
 export const ActivityEditor = ({
---
+++ front/src/modules/activities/hooks/useCreateActivity.ts
@@ -0,0 +1,75 @@
+import { getOperationName } from '@apollo/client/utilities';
+import { useRecoilState, useRecoilValue } from 'recoil';
+
+import { currentUserState } from '@/auth/states/currentUserState';
+import { GET_COMPANIES } from '@/companies/graphql/queries/getCompanies';
+import { GET_PEOPLE } from '@/people/graphql/queries/getPeople';
+import {
+  ActivityCreateInput,
+  useCreateActivityMutation,
+} from '~/generated/graphql';
+
+import { GET_ACTIVITIES } from '../graphql/queries/getActivities';
+import { GET_ACTIVITIES_BY_TARGETS } from '../graphql/queries/getActivitiesByTarget';
+import { GET_ACTIVITY } from '../graphql/queries/getActivity';
+import { activityTargetableEntityArrayState } from '../states/activityTargetableEntityArrayState';
+import { currentActivityState } from '../states/currentActivityState';
+import { viewableActivityIdState } from '../states/viewableActivityIdState';
+import { getRelationData } from '../utils/getRelationData';
+
+// this hook can be used to create an activity and set other fields (data) at the same time
+export const useCreateActivity = () => {
+  const currentUser = useRecoilValue(currentUserState);
+  const currentActivity = useRecoilValue(currentActivityState);
+  const [createActivityMutation] = useCreateActivityMutation();
+
+  const [, setViewableActivityId] = useRecoilState(viewableActivityIdState);
+  const [, setActivityTargetableEntityArray] = useRecoilState(
+    activityTargetableEntityArrayState,
+  );
+
+  return (data: Partial<ActivityCreateInput>) => {
+    const now = new Date().toISOString();
+
+    return createActivityMutation({
+      variables: {
+        data: {
+          id: currentActivity?.id,
+          createdAt: now,
+          updatedAt: now,
+          author: { connect: { id: currentUser?.id ?? '' } },
+          workspaceMemberAuthor: {
+            connect: { id: currentUser?.workspaceMember?.id ?? '' },
+          },
+          workspaceMemberAssignee: {
+            connect: { id: currentUser?.workspaceMember?.id ?? '' },
+          },
+          activityTargets: {
+            createMany: {
+              data: currentActivity?.targetableEntities
+                ? getRelationData(currentActivity.targetableEntities)
+                : [],
+              skipDuplicates: true,
+            },
+          },
+          type: currentActivity?.type,
+          assignee: currentActivity?.assignee,
+          ...data,
+        },
+      },
+      refetchQueries: [
+        getOperationName(GET_COMPANIES) ?? '',
+        getOperationName(GET_PEOPLE) ?? '',
+        getOperationName(GET_ACTIVITY) ?? '',
+        getOperationName(GET_ACTIVITIES_BY_TARGETS) ?? '',
+        getOperationName(GET_ACTIVITIES) ?? '',
+      ],
+      onCompleted: (data) => {
+        setViewableActivityId(data.createOneActivity.id);
+        setActivityTargetableEntityArray(
+          currentActivity?.targetableEntities ?? [],
+        );
+      },
+    });
+  };
+};
