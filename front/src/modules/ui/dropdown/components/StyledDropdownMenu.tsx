cbbc8ee7f96ab5eee6da7e1b2389a6b6466f8bd2
dev-DTECH <debdeep.dt@gmail.com>
2023-10-08 00:44:41 +0530

    Fixed #1855

--- front/src/modules/ui/dropdown/components/StyledDropdownMenu.tsx
+++ front/src/modules/ui/dropdown/components/StyledDropdownMenu.tsx
@@ -7,7 +7,7 @@ export const StyledDropdownMenu = styled.div<{
   backdrop-filter: ${({ disableBlur }) =>
     disableBlur ? 'none' : 'blur(20px)'};
 
-  background: ${({ theme }) => theme.background.transparent.secondary};
+  background: ${({ theme }) => theme.background.secondary};
   border: 1px solid ${({ theme }) => theme.border.color.medium};
   border-radius: ${({ theme }) => theme.border.radius.md};
   box-shadow: ${({ theme }) => theme.boxShadow.strong};
