92a0814c18cc558366593f643951435ae2b1fe46
corentin <corentin@twenty.com>
2023-10-08 18:45:39 +0200

    Add metadata queries resolvers

--- server/src/metadata/object-metadata/object-metadata.module.ts
+++ server/src/metadata/object-metadata/object-metadata.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { NestjsQueryGraphQLModule, PagingStrategies } from '@ptc-org/nestjs-query-graphql';
+import { NestjsQueryTypeOrmModule } from '@ptc-org/nestjs-query-typeorm';
 
 import { ObjectMetadataService } from './object-metadata.service';
 import { ObjectMetadata } from './object-metadata.entity';
+import { JwtAuthGuard } from 'src/guards/jwt.auth.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ObjectMetadata], 'metadata')],
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [
+        NestjsQueryTypeOrmModule.forFeature([ObjectMetadata], 'metadata'),
+      ],
+      resolvers: [
+        {
+          EntityClass: ObjectMetadata,
+          DTOClass: ObjectMetadata,
+          enableTotalCount: true,
+          pagingStrategy: PagingStrategies.CURSOR,
+          create: { disabled: true },
+          update: { disabled: true },
+          delete: { disabled: true },
+          guards: [JwtAuthGuard],
+        },
+      ],
+    }),
+  ],
   providers: [ObjectMetadataService],
   exports: [ObjectMetadataService],
 })
