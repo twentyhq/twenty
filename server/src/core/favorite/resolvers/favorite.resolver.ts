f5e7534965a451e7e736d848764179ad3bdc1df4
abhithory <abhithory@gmail.com>
2023-10-08 13:34:56 -0500

    favorites reoder funtion added in backend

--- server/src/core/favorite/resolvers/favorite.resolver.ts
+++ server/src/core/favorite/resolvers/favorite.resolver.ts
@@ -1,5 +1,5 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
-import { UseGuards } from '@nestjs/common';
+import { NotFoundException, UseGuards } from '@nestjs/common';
 import { InputType } from '@nestjs/graphql';
 import { Field } from '@nestjs/graphql';
 
@@ -21,6 +21,7 @@ import {
 import { AuthWorkspace } from 'src/decorators/auth-workspace.decorator';
 import { FavoriteService } from 'src/core/favorite/services/favorite.service';
 import { FavoriteWhereInput } from 'src/core/@generated/favorite/favorite-where.input';
+import { assert } from 'src/utils/assert';
 
 @InputType()
 class FavoriteMutationForPersonArgs {
@@ -32,6 +33,15 @@ class FavoriteMutationForPersonArgs {
 class FavoriteMutationForCompanyArgs {
   @Field(() => String)
   companyId: string;
+}
+
+@InputType()
+class FavoriteMutationForUpdatingOrder {
+  @Field(() => String)
+  favoriteId: string;
+
+  @Field(() => Number)
+  toIndex: number;
 }
 
 @UseGuards(JwtAuthGuard)
@@ -118,6 +128,62 @@ export class FavoriteResolver {
       },
       select: prismaSelect.value,
     });
+  }
+
+  @Mutation(() => Boolean, {
+    nullable: false,
+  })
+  @UseGuards(AbilityGuard)
+  @CheckAbilities(CreateFavoriteAbilityHandler)
+  async updateFavoritesOrder(
+    @Args('data') args: FavoriteMutationForUpdatingOrder,
+    @AuthWorkspace() workspace: Workspace,
+  ): Promise<boolean> {
+    const { favoriteId, toIndex } = args;
+
+    const allFavorites = await this.favoriteService.findMany({
+      where: {
+        workspaceId: workspace.id,
+      },
+      include: {
+        person: true,
+        company: {
+          include: {
+            accountOwner: true,
+          },
+        },
+      },
+    });
+    const currentIndex = allFavorites.findIndex(
+      (favorite) => favorite.id === favoriteId,
+    );
+
+    assert(
+      currentIndex !== -1,
+      'Favorite Item with this Id not found',
+      NotFoundException,
+    );
+    // Remove 'favoriteId' favorite from its current position
+    const FavoriteItemToReorder = allFavorites.splice(currentIndex, 1)[0];
+
+    // Insert 'favoriteId' favorite at the 'toIndex'
+    allFavorites.splice(toIndex, 0, FavoriteItemToReorder);
+    // Delete all old favorites
+    await this.favoriteService.deleteMany({
+      where: { workspaceId: workspace.id },
+    });
+
+    const updatedFavoritesData = allFavorites.map((favorite) => ({
+      workspaceId: workspace.id,
+      ...(favorite?.personId ? { personId: favorite.personId } : {}),
+      ...(favorite?.companyId ? { companyId: favorite.companyId } : {}),
+    }));
+
+    await this.favoriteService.createMany({
+      data: updatedFavoritesData,
+    });
+
+    return true;
   }
 
   @Mutation(() => Favorite, {
