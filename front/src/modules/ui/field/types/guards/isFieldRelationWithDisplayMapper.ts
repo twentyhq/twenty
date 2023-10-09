59c45edcb164da42bfb673a70a0fc0b48d254cfb
bosiraphael <raphael.bosi@gmail.com>
2023-10-09 18:41:05 +0200

    removed unused temporary type

--- front/src/modules/ui/field/types/guards/isFieldRelationWithDisplayMapper.ts
+++
@@ -1,6 +0,0 @@
-import { FieldDefinition } from '../FieldDefinition';
-import { FieldMetadata, FieldRelationMetadata } from '../FieldMetadata';
-
-export const isFieldRelation = (
-  field: FieldDefinition<FieldMetadata>,
-): field is FieldDefinition<FieldRelationMetadata> => field.type === 'relation';
