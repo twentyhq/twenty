b6d2c4f395ee031e5d0c46c08877a509fa3f04f4
bosiraphael <raphael.bosi@gmail.com>
2023-10-10 12:25:35 +0200

    add ellipsis to links

--- front/src/modules/ui/field/meta-types/display/components/EmailFieldDisplay.tsx
+++ front/src/modules/ui/field/meta-types/display/components/EmailFieldDisplay.tsx
@@ -1,13 +1,8 @@
 import { useEmailField } from '../../hooks/useEmailField';
-import { EllipsisDisplay } from '../content-display/components/EllipsisDisplay';
 import { EmailDisplay } from '../content-display/components/EmailDisplay';
 
 export const EmailFieldDisplay = () => {
   const { fieldValue } = useEmailField();
 
-  return (
-    <EllipsisDisplay>
-      <EmailDisplay value={fieldValue} />
-    </EllipsisDisplay>
-  );
+  return <EmailDisplay value={fieldValue} />;
 };
--- front/src/modules/ui/field/meta-types/display/components/PhoneFieldDisplay.tsx
+++ front/src/modules/ui/field/meta-types/display/components/PhoneFieldDisplay.tsx
@@ -1,14 +1,9 @@
 import { PhoneDisplay } from '@/ui/field/meta-types/display/content-display/components/PhoneDisplay';
 
 import { usePhoneField } from '../../hooks/usePhoneField';
-import { EllipsisDisplay } from '../content-display/components/EllipsisDisplay';
 
 export const PhoneFieldDisplay = () => {
   const { fieldValue } = usePhoneField();
 
-  return (
-    <EllipsisDisplay>
-      <PhoneDisplay value={fieldValue} />
-    </EllipsisDisplay>
-  );
+  return <PhoneDisplay value={fieldValue} />;
 };
