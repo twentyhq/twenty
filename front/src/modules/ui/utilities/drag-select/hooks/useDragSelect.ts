a39b6766e89bba2507d2ab36922ce1f2b718b233
Ayush Agrawal <54364088+AyushAgrawal-A2@users.noreply.github.com>
2023-10-10 17:13:35 +0530

    Fix: All cell re-rendering

--- front/src/modules/ui/utilities/drag-select/hooks/useDragSelect.ts
+++ front/src/modules/ui/utilities/drag-select/hooks/useDragSelect.ts
@@ -1,10 +1,10 @@
-import { useRecoilCallback, useRecoilState } from 'recoil';
+import { useRecoilCallback, useSetRecoilState } from "recoil";
 
 import { isDragSelectionStartEnabledState } from '../states/internal/isDragSelectionStartEnabledState';
 
 export const useDragSelect = () => {
-  const [, setIsDragSelectionStartEnabled] = useRecoilState(
-    isDragSelectionStartEnabledState,
+  const setIsDragSelectionStartEnabled = useSetRecoilState(
+    isDragSelectionStartEnabledState
   );
 
   const setDragSelectionStartEnabled = (isEnabled: boolean) => {
