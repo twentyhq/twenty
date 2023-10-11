a37b620df323c99a36d9cbb8ba8f381b67b68b26
bosiraphael <raphael.bosi@gmail.com>
2023-10-11 18:17:33 +0200

    wip

--- front/src/modules/ui/icon-section/components/IconSection.tsx
+++ front/src/modules/ui/icon-section/components/IconSection.tsx
@@ -1,21 +1,42 @@
+import { useState } from 'react';
+
+import { IconButton } from '@/ui/button/components/IconButton';
 import { IconArrowRight } from '@/ui/icon';
 import { IconPicker } from '@/ui/input/components/IconPicker';
 import { Section } from '@/ui/section/components/Section';
 import { H2Title } from '@/ui/typography/components/H2Title';
 
 export const IconSection = () => {
+  const [iconPickerOpen, setIconPickerOpen] = useState(false);
+
+  const [selectedIcon, setSelectedIcon] = useState(() => IconArrowRight);
+  const [selectedIconKey, setSelectedIconKey] = useState('IconSettings');
+
   return (
     <Section>
       <H2Title
         title="Icon"
         description="The icon that will be displayed in the sidebar."
       />
-      <IconPicker
-        selectedIconKey="IconSettings"
-        onChange={() => {
-          return;
+      <IconButton
+        Icon={selectedIcon}
+        onClick={() => {
+          setIconPickerOpen(true);
         }}
       />
+      {iconPickerOpen && (
+        <IconPicker
+          selectedIconKey={selectedIconKey}
+          onChange={(icon) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            setSelectedIcon(icon.Icon);
+            setSelectedIconKey(icon.iconKey);
+            setIconPickerOpen(false);
+          }}
+        />
+      )}
+
       <IconArrowRight />
     </Section>
   );
