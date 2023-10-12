13febd60f4a88ecfa9425f32dbd88cf12a88bca8
bosiraphael <raphael.bosi@gmail.com>
2023-10-12 12:33:17 +0200

    remove hardcoded values and use theme values

--- front/src/modules/settings/components/IconWithLabel.tsx
+++ front/src/modules/settings/components/IconWithLabel.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { IconComponent } from '@/ui/icon/types/IconComponent';
@@ -11,33 +12,34 @@ const StyledContainer = styled.div`
   align-items: center;
   align-self: stretch;
   display: flex;
-  gap: 12px;
-  padding: var(--spacing-4-px, 4px);
+  gap: ${({ theme }) => theme.spacing(3)};
+  padding: ${({ theme }) => theme.spacing(1)};
 `;
 
 const StyledSubContainer = styled.div`
   align-items: center;
   display: flex;
-  gap: var(--spacing-4-px, 4px);
+  gap: ${({ theme }) => theme.spacing(1)};
 `;
 const StyledItemLabel = styled.div`
-  color: ${(props) => props.theme.font.color.secondary};
-  font-family: Inter;
-  font-size: 13px;
+  color: ${({ theme }) => theme.font.color.secondary};
+  font-size: ${({ theme }) => theme.font.size.sm};
 
   /* Base/Regular */
   font-style: normal;
-  font-weight: 400;
+  font-weight: ${({ theme }) => theme.font.size.md};
   leading-trim: both;
-  line-height: 150%;
+  line-height: ${({ theme }) => theme.text.lineHeight.md};
   text-edge: cap; /* 19.5px */
 `;
 
 export const IconWithLabel = ({ Icon, label }: IconWithLabelProps) => {
+  const theme = useTheme();
+
   return (
     <StyledContainer>
       <StyledSubContainer>
-        <Icon size={16} stroke={2} />
+        <Icon size={theme.icon.size.md} stroke={theme.icon.stroke.sm} />
         <StyledItemLabel>{label}</StyledItemLabel>
       </StyledSubContainer>
     </StyledContainer>
