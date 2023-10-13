5e76ceac1d7060355999f04d15285a58f525f35b
Rus Khakimov <396223+khakimov@users.noreply.github.com>
2023-10-13 15:10:34 +0700

    Fix bug where "metadata" scheme was not created automatically (#1971)

--- server/src/metadata/metadata.datasource.ts
+++ server/src/metadata/metadata.datasource.ts
@@ -4,6 +4,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 
+import { InitMetadataTables1695214465080 } from './migrations/1695214465080-InitMetadataTables';
+import { AlterFieldMetadataTable1695717691800 } from './migrations/1695717691800-alter-field-metadata-table';
+import { AddTargetColumnMap1696409050890 } from './migrations/1696409050890-add-target-column-map';
+import { MetadataNameLabelRefactoring1697126636202 } from './migrations/1697126636202-MetadataNameLabelRefactoring';
+
 config();
 
 const configService = new ConfigService();
@@ -17,7 +22,12 @@ export const typeORMMetadataModuleOptions: TypeOrmModuleOptions = {
   synchronize: false,
   migrationsRun: true,
   migrationsTableName: '_typeorm_migrations',
-  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  migrations: [
+    InitMetadataTables1695214465080,
+    AlterFieldMetadataTable1695717691800,
+    AddTargetColumnMap1696409050890,
+    MetadataNameLabelRefactoring1697126636202,
+  ],
 };
 
 export const connectionSource = new DataSource(
