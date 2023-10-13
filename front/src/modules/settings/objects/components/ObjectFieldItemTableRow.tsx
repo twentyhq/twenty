c813b6ef720a4bca24b2890ec4818924cba3e2d6
Thaïs Guigon <guigon.thais@gmail.com>
2023-10-13 11:18:04 +0200

    refactor: add ObjectFieldDataType

--- front/src/modules/settings/objects/components/ObjectFieldItemTableRow.tsx
+++ front/src/modules/settings/objects/components/ObjectFieldItemTableRow.tsx
@@ -1,20 +1,13 @@
-import { css, useTheme } from '@emotion/react';
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 
-import {
-  IconCheck,
-  IconDotsVertical,
-  IconLink,
-  IconNumbers,
-  IconPlug,
-  IconSocial,
-  IconUserCircle,
-} from '@/ui/icon';
-import { IconComponent } from '@/ui/icon/types/IconComponent';
+import { IconDotsVertical } from '@/ui/icon';
 import { TableCell } from '@/ui/table/components/TableCell';
 import { TableRow } from '@/ui/table/components/TableRow';
 
 import { ObjectFieldItem } from '../types/ObjectFieldItem';
+
+import { ObjectFieldDataType } from './ObjectFieldDataType';
 
 export const StyledObjectFieldTableRow = styled(TableRow)`
   grid-template-columns: 180px 148px 148px 36px;
@@ -23,25 +16,6 @@ export const StyledObjectFieldTableRow = styled(TableRow)`
 const StyledNameTableCell = styled(TableCell)`
   color: ${({ theme }) => theme.font.color.primary};
   gap: ${({ theme }) => theme.spacing(2)};
-`;
-
-const StyledDataType = styled.div<{ value: ObjectFieldItem['dataType'] }>`
-  align-items: center;
-  border: 1px solid transparent;
-  border-radius: ${({ theme }) => theme.border.radius.sm};
-  display: flex;
-  font-size: ${({ theme }) => theme.font.size.sm};
-  gap: ${({ theme }) => theme.spacing(1)};
-  height: 20px;
-  padding: 0 ${({ theme }) => theme.spacing(2)};
-
-  ${({ theme, value }) =>
-    value === 'relation'
-      ? css`
-          border-color: ${theme.color.purple20};
-          color: ${theme.color.purple};
-        `
-      : ''}
 `;
 
 const StyledIconTableCell = styled(TableCell)`
@@ -53,25 +27,12 @@ const StyledIconDotsVertical = styled(IconDotsVertical)`
   color: ${({ theme }) => theme.font.color.tertiary};
 `;
 
-const dataTypes: Record<
-  ObjectFieldItem['dataType'],
-  { label: string; Icon: IconComponent }
-> = {
-  boolean: { label: 'True/False', Icon: IconCheck },
-  number: { label: 'Number', Icon: IconNumbers },
-  relation: { label: 'Relation', Icon: IconPlug },
-  social: { label: 'Social', Icon: IconSocial },
-  teammate: { label: 'Teammate', Icon: IconUserCircle },
-  text: { label: 'Text', Icon: IconLink },
-};
-
 export const ObjectFieldItemTableRow = ({
   fieldItem,
 }: {
   fieldItem: ObjectFieldItem;
 }) => {
   const theme = useTheme();
-  const dataTypeItem = dataTypes[fieldItem.dataType];
 
   return (
     <StyledObjectFieldTableRow>
@@ -83,10 +44,7 @@ export const ObjectFieldItemTableRow = ({
         {fieldItem.type === 'standard' ? 'Standard' : 'Custom'}
       </TableCell>
       <TableCell>
-        <StyledDataType value={fieldItem.dataType}>
-          <dataTypeItem.Icon size={theme.icon.size.sm} />
-          {dataTypeItem.label}
-        </StyledDataType>
+        <ObjectFieldDataType value={fieldItem.dataType} />
       </TableCell>
       <StyledIconTableCell>
         <StyledIconDotsVertical
