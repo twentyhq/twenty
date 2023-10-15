ba3094b4486c5d2fe62ab15a694a292c8c3e1cf9
Charles Bochet <charles@twenty.com>
2023-10-15 16:45:17 +0200

    Fix script windows init.sql

--- infra/dev/postgres/init.sql
+++ infra/dev/postgres/init.sql
@@ -1,10 +1,10 @@
 SELECT 'CREATE DATABASE "default"'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'default');
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'default')\gexec
 
 SELECT 'CREATE DATABASE "test"'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test');
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test')\gexec
 
 SELECT 'CREATE USER twenty PASSWORD ''twenty'''
-WHERE NOT EXISTS (SELECT FROM pg_user WHERE usename = 'twenty');
+WHERE NOT EXISTS (SELECT FROM pg_user WHERE usename = 'twenty')\gexec
 
-SELECT 'ALTER ROLE twenty superuser';
+SELECT 'ALTER ROLE twenty superuser'\gexec
