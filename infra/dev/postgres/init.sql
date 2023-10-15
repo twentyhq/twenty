9296443e344f672828922c04260c996e58713ba9
Arijit <81144422+ArijitCloud@users.noreply.github.com>
2023-10-15 16:31:56 +0200

    Fix: alter role code (#2029)
    fix: alter role code

--- infra/dev/postgres/init.sql
+++ infra/dev/postgres/init.sql
@@ -7,4 +7,4 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test');
 SELECT 'CREATE USER twenty PASSWORD ''twenty'''
 WHERE NOT EXISTS (SELECT FROM pg_user WHERE usename = 'twenty');
 
-ALTER ROLE twenty superuser;
+SELECT 'ALTER ROLE twenty superuser';
