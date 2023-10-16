525603227aad4bc4c793551dfdc06b0842dcd4e4
Weiko <corentin@twenty.com>
2023-10-16 16:59:46 +0200

    Add default db schema for seeded workspace (#2061)
    * Add default db schema for seeded workspace
    
    * fix
    
    * add if not exists
    
    * add metadata seeds
    
    * use setup-db for e2e
    
    * fix tests

--- server/scripts/run-integration.sh
+++ server/scripts/run-integration.sh
@@ -4,16 +4,5 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/set-env-test.sh
 
-npx ts-node ./test/utils/check-db.ts
-EXIT_CODE=$?
-
-if [ $EXIT_CODE -ne 0 ]; then
-    echo '🟡 - Database is not initialized. Running migrations...'
-    npx ts-node ./test/utils/setup-db.ts
-    npx prisma migrate reset --force && yarn prisma:generate
-    yarn typeorm:migrate
-else
-    echo "🟢 - Database is already initialized."
-fi
-
+yarn database:setup && yarn database:reset
 yarn jest --config ./test/jest-e2e.json
