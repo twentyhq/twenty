462a5a673876ad8a6b2046df4e4d48b9858dc682
Abhishek Thory <76877003+abhithory@users.noreply.github.com>
2023-10-17 14:10:47 -0500

    danger zone section added in object edit page (#2093)

--- front/src/pages/settings/data-model/SettingsObjectEdit.tsx
+++ front/src/pages/settings/data-model/SettingsObjectEdit.tsx
@@ -6,8 +6,11 @@ import { SettingsObjectFormSection } from '@/settings/data-model/components/Sett
 import { activeObjectItems } from '@/settings/data-model/constants/mockObjects';
 import { SettingsObjectIconSection } from '@/settings/data-model/object-edit/SettingsObjectIconSection';
 import { AppPath } from '@/types/AppPath';
-import { IconSettings } from '@/ui/display/icon';
+import { IconArchive, IconSettings } from '@/ui/display/icon';
+import { H2Title } from '@/ui/display/typography/components/H2Title';
+import { Button } from '@/ui/input/button/components/Button';
 import { SubMenuTopBarContainer } from '@/ui/layout/page/SubMenuTopBarContainer';
+import { Section } from '@/ui/layout/section/components/Section';
 import { Breadcrumb } from '@/ui/navigation/bread-crumb/components/Breadcrumb';
 
 export const SettingsObjectEdit = () => {
@@ -47,6 +50,15 @@ export const SettingsObjectEdit = () => {
             />
           </>
         )}
+        <Section>
+          <H2Title title="Danger zone" description={`Disable object`} />
+          <Button
+            Icon={IconArchive}
+            title="Disable"
+            size="small"
+            onClick={() => {}}
+          />
+        </Section>
       </SettingsPageContainer>
     </SubMenuTopBarContainer>
   );
