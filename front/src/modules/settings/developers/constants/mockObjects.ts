ebd2affced1ea8761eb11fa82a53735f053681fe
Saba Shavidze <sabashavidze@gmail.com>
2023-10-17 18:56:04 +0400

    update pl based on comments

--- front/src/modules/settings/developers/constants/mockObjects.ts
+++ front/src/modules/settings/developers/constants/mockObjects.ts
@@ -1,38 +1,34 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 } from 'uuid';
 
 import { ApisFiedlItem } from '../types/ApisFieldItem';
 
-const generateUniqueID = () => {
-  return uuidv4(); // Generate a unique string ID
-};
-
 export const activeApiKeyItems: ApisFiedlItem[] = [
   {
-    id: generateUniqueID(),
+    id: v4(),
     name: 'Zapier key',
     type: 'internal',
     expiration: 'In 80 days',
   },
   {
-    id: generateUniqueID(),
+    id: v4(),
     name: 'Notion',
     type: 'internal',
     expiration: 'Expired',
   },
   {
-    id: generateUniqueID(),
+    id: v4(),
     name: 'Trello',
     type: 'internal',
     expiration: 'In 1 year',
   },
   {
-    id: generateUniqueID(),
+    id: v4(),
     name: 'Cargo',
     type: 'published',
     expiration: 'Never',
   },
   {
-    id: generateUniqueID(),
+    id: v4(),
     name: 'Backoffice',
     type: 'published',
     expiration: 'In 32 days',
