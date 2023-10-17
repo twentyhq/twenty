01dfa015c44a6b9bfab49a366c7b45411e3ed06e
bosiraphael <raphael.bosi@gmail.com>
2023-10-17 18:32:06 +0200

    add close button

--- front/src/modules/settings/data-model/objects/SettingsObjectCoverImage.tsx
+++ front/src/modules/settings/data-model/objects/SettingsObjectCoverImage.tsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled';
 
+import { IconX } from '@/ui/display/icon';
+import { LightIconButton } from '@/ui/input/button/components/LightIconButton';
+
 import CoverImage from '../assets/build-your-business-logic.jpg';
 
 const StyledCoverImageContainer = styled.div`
@@ -7,6 +10,7 @@ const StyledCoverImageContainer = styled.div`
   border-radius: ${({ theme }) => theme.border.radius.md};
   height: 153px;
   overflow: hidden;
+  position: relative;
   width: 100%;
 `;
 
@@ -16,10 +20,17 @@ const StyledCoverImage = styled.img`
   width: 100%;
 `;
 
+const StyledLighIconButton = styled(LightIconButton)`
+  position: absolute;
+  right: ${({ theme }) => theme.spacing(2)};
+  top: ${({ theme }) => theme.spacing(2)};
+`;
+
 export const SettingsObjectCoverImage = () => {
   return (
     <StyledCoverImageContainer>
       <StyledCoverImage src={CoverImage} alt="Build your business logic" />
+      <StyledLighIconButton Icon={IconX} accent="tertiary" size="small" />
     </StyledCoverImageContainer>
   );
 };
