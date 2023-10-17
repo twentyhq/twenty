637c01f1f1b75c81f66f19ac71a11b60cfba710b
Charles Bochet <charles@twenty.com>
2023-10-17 11:40:45 +0200

    Move shadow style from TableCell and InlineCell to FieldInputs

--- front/src/modules/ui/data/data-table/table-cell/components/TableCellEditMode.tsx
+++ front/src/modules/ui/data/data-table/table-cell/components/TableCellEditMode.tsx
@@ -1,27 +1,15 @@
 import { ReactElement } from 'react';
 import styled from '@emotion/styled';
 
-import { overlayBackground } from '@/ui/theme/constants/effects';
-
 const StyledEditableCellEditModeContainer = styled.div<TableCellEditModeProps>`
   align-items: center;
-  border: ${({ transparent, theme }) =>
-    transparent ? 'none' : `1px solid ${theme.border.color.light}`};
-  border-radius: ${({ transparent, theme }) =>
-    transparent ? 'none' : theme.border.radius.sm};
   display: flex;
-  left: ${(props) =>
-    props.editModeHorizontalAlign === 'right' ? 'auto' : '0'};
   margin: -1px;
   max-width: ${({ maxContentWidth }) =>
     maxContentWidth ? `${maxContentWidth}px` : 'none'};
   min-height: 100%;
   min-width: ${({ maxContentWidth }) => (maxContentWidth ? `none` : '100%')};
   position: absolute;
-  right: ${(props) =>
-    props.editModeHorizontalAlign === 'right' ? '0' : 'auto'};
-  top: ${(props) => (props.editModeVerticalPosition === 'over' ? '0' : '100%')};
-  ${({ transparent }) => (transparent ? '' : overlayBackground)};
   z-index: 1;
 `;
 
