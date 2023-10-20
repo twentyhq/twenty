8d8e8b78ee172827af2b7d0fe89f4d481de4c619
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-10-20 14:15:32 +0530

    Reposition DataTable column head menu

--- front/src/modules/ui/data/data-table/components/ColumnHeadWithDropdown.tsx
+++ front/src/modules/ui/data/data-table/components/ColumnHeadWithDropdown.tsx
@@ -32,8 +32,9 @@ export const ColumnHeadWithDropdown = ({
             primaryColumnKey={primaryColumnKey}
           />
         }
+        dropdownPlacement="bottom-start"
+        dropdownOffset={{ x: 0, y: 0 }}
         dropdownHotkeyScope={{ scope: column.key + '-header' }}
-        dropdownOffset={{ x: 0, y: -8 }}
       />
     </DropdownScope>
   );
