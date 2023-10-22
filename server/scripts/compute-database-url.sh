e67b2d23ae90066954d1e6b0ef94ae9214312f3a
Charles Bochet <charles@twenty.com>
2023-10-22 12:42:52 +0200

    Fix port missing while building pg_database_url for render

--- server/scripts/compute-database-url.sh
+++ server/scripts/compute-database-url.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-export PG_DATABASE_URL=postgres://twenty:twenty@$PG_DATABASE_HOST:5432/default
+export PG_DATABASE_URL=postgres://twenty:twenty@$PG_DATABASE_HOST:$PG_DATABASE_PORT/default
