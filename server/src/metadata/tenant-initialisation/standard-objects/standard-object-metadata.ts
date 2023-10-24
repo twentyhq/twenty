0e13161d42b957af3e4ee656a292dc4a9367ee87
gitstart-twenty <twenty@gitstart.com>
2023-10-24 11:38:53 +0000

    Chore(server): Add V2 objects initialisation using TenantInitialisation service
    Co-authored-by: Benjamin Mayanja V <vibenjamin6@gmail.com>

--- server/src/metadata/tenant-initialisation/standard-objects/standard-object-metadata.ts
+++ server/src/metadata/tenant-initialisation/standard-objects/standard-object-metadata.ts
@@ -1,5 +1,7 @@
-import companyObject from './companies.metadata.json';
+import companyObject from './companies/companies.metadata.json';
+import userObject from './users/users.metadata.json';
 
 export const standardObjectsMetadata = {
   companyV2: companyObject,
+  userV2: userObject,
 };
--- server/src/metadata/tenant-initialisation/standard-objects/standard-object-seeds.ts
+++ server/src/metadata/tenant-initialisation/standard-objects/standard-object-seeds.ts
@@ -1,5 +1,7 @@
-import companySeeds from './companies.seeds.json';
+import companySeeds from './companies/companies.seeds.json';
+import userSeeds from './users/users.seeds.json';
 
 export const standardObjectsSeeds = {
   companyV2: companySeeds,
+  userV2: userSeeds,
 };
