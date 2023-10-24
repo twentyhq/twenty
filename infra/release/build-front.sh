d58b85df54facbb571c6abea83179f12f6c7a4ed
Charles Bochet <charles@twenty.com>
2023-10-24 16:50:55 +0200

    Complete deploy to render work

--- infra/release/build-front.sh
+++ infra/release/build-front.sh
@@ -1,5 +1,6 @@
 
 docker buildx build \
 --push \
+--no-cache \
 --platform linux/amd64,linux/arm64 \
 -f ./infra/build/front/Dockerfile -t twentycrm/twenty-front:0.1.5 -t twentycrm/twenty-front:latest .
