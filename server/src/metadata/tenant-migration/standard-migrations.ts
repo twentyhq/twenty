b4e7b161c946485e51560bb5c511f572a4fbdb92
gitstart-twenty <twenty@gitstart.com>
2023-10-25 11:03:24 +0000

    Update standard migrations
    Co-authored-by: Benjamin Mayanja V <vibenjamin6@gmail.com>

--- server/src/metadata/tenant-migration/standard-migrations.ts
+++ server/src/metadata/tenant-migration/standard-migrations.ts
@@ -1,6 +1,10 @@
 import { addCompanyTable } from './migrations/1697618009-addCompanyTable';
+import { updateCompanyTable } from './migrations/1698229753-updateCompanyTable';
+import { addUserTable } from './migrations/1698230772-addUserTable';
 
 // TODO: read the folder and return all migrations
 export const standardMigrations = {
   '1697618009-addCompanyTable': addCompanyTable,
+  '1698229753-updateCompanyTable': updateCompanyTable,
+  '1698230772-addUserTable': addUserTable,
 };
