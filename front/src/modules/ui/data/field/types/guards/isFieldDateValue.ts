2faac6368c458f3bb3f049930a19fbc674323c5e
brendanlaschke <brendan.laschke@googlemail.com>
2023-10-26 23:24:53 +0200

    use zod instead of yup

--- front/src/modules/ui/data/field/types/guards/isFieldDateValue.ts
+++ front/src/modules/ui/data/field/types/guards/isFieldDateValue.ts
@@ -2,7 +2,7 @@ import { isNull, isString } from '@sniptt/guards';
 
 import { FieldDateValue } from '../FieldMetadata';
 
-// TODO: add yup
+// TODO: add zod
 export const isFieldDateValue = (
   fieldValue: unknown,
 ): fieldValue is FieldDateValue => isNull(fieldValue) || isString(fieldValue);
