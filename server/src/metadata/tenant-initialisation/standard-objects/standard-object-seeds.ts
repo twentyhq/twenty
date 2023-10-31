a1f4512c2b3ff74615757ecf3e90b41f9b8074e7
gitstart-twenty <twenty@gitstart.com>
2023-10-31 08:16:39 +0000

    Add more seeds
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>

--- server/src/metadata/tenant-initialisation/standard-objects/standard-object-seeds.ts
+++ server/src/metadata/tenant-initialisation/standard-objects/standard-object-seeds.ts
@@ -1,9 +1,21 @@
 import companySeeds from './companies/companies.seeds.json';
 import viewSeeds from './views/views.seeds.json';
 import viewFieldSeeds from './view-fields/view-fields.seeds.json';
+import commentSeeds from './comment/comment.seeds.json';
+import personSeeds from './person/person.seeds.json';
+import pipelineSeeds from './pipeline/pipeline.seeds.json';
+import pipelineStageSeeds from './pipeline-stage/pipeline-stage.seeds.json';
+import userSettingSeeds from './user-settings/user-settings.seeds.json';
+import workspaceMemberSeeds from './workspace-member/workspace-member.seeds.json';
 
 export const standardObjectsSeeds = {
   companyV2: companySeeds,
   viewV2: viewSeeds,
   viewFieldV2: viewFieldSeeds,
+  commentV2: commentSeeds,
+  personV2: personSeeds,
+  pipelineV2: pipelineSeeds,
+  pipelineStageV2: pipelineStageSeeds,
+  userSettingV2: userSettingSeeds,
+  workspaceMemberV2: workspaceMemberSeeds,
 };
