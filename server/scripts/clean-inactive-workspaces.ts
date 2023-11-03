ffa00350162036e7def54079e0e12fffea49f515
martmull <martmull@hotmail.fr>
2023-11-03 13:06:49 +0100

    Factorize functions

--- server/scripts/clean-inactive-workspaces.ts
+++ server/scripts/clean-inactive-workspaces.ts
@@ -1,30 +1,6 @@
-import { ConfigService } from '@nestjs/config';
-
 import console from 'console';
 
-import { DataSource } from 'typeorm';
-
-const configService = new ConfigService();
-
-export const connectionSource = new DataSource({
-  type: 'postgres',
-  logging: false,
-  url: configService.get<string>('PG_DATABASE_URL'),
-});
-
-const performQuery = async (
-  query: string,
-  consoleDescription: string,
-  withLog = true,
-) => {
-  try {
-    const result = await connectionSource.query(query);
-    withLog && console.log(`Performed '${consoleDescription}' successfully`);
-    return result;
-  } catch (err) {
-    withLog && console.error(`Failed to perform '${consoleDescription}':`, err);
-  }
-};
+import { connectionSource, performQuery } from './utils';
 
 const getWorkspacesFromSchema = async () => {
   return await performQuery(
