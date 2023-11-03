b30160a9108e4cf4a4b74ca37225604b38277f26
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-11-03 16:36:51 +0100

    Fix

--- front/src/modules/ui/object/field/hooks/useGetButtonIcon.ts
+++ front/src/modules/ui/object/field/hooks/useGetButtonIcon.ts
@@ -4,27 +4,21 @@ import { IconPencil } from '@/ui/display/icon';
 import { IconComponent } from '@/ui/display/icon/types/IconComponent';
 
 import { FieldContext } from '../contexts/FieldContext';
-import { FieldDefinition } from '../types/FieldDefinition';
-import { FieldMetadata } from '../types/FieldMetadata';
 import { isFieldEmail } from '../types/guards/isFieldEmail';
 import { isFieldPhone } from '../types/guards/isFieldPhone';
 import { isFieldRelation } from '../types/guards/isFieldRelation';
 import { isFieldURL } from '../types/guards/isFieldURL';
 
-export const useGetButtonIcon = (
-  customFieldDefinition?: FieldDefinition<FieldMetadata> | null | undefined,
-): IconComponent | undefined => {
+export const useGetButtonIcon = (): IconComponent | undefined => {
   const { fieldDefinition } = useContext(FieldContext);
 
-  const usedFieldDefinition = customFieldDefinition ?? fieldDefinition;
-
-  if (!usedFieldDefinition) return undefined;
+  if (!fieldDefinition) return undefined;
 
   if (
-    isFieldURL(usedFieldDefinition) ||
-    isFieldEmail(usedFieldDefinition) ||
-    isFieldPhone(usedFieldDefinition) ||
-    isFieldRelation(usedFieldDefinition)
+    isFieldURL(fieldDefinition) ||
+    isFieldEmail(fieldDefinition) ||
+    isFieldPhone(fieldDefinition) ||
+    isFieldRelation(fieldDefinition)
   ) {
     return IconPencil;
   }
