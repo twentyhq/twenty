410d7ceb15b36aabf05fcfb3bc2ea296e5767cf2
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-11-06 15:29:02 +0100

    fix: small tenant refactor fixes

--- server/src/tenant/resolver-builder/factories/find-many-resolver.factory.ts
+++ server/src/tenant/resolver-builder/factories/find-many-resolver.factory.ts
@@ -5,13 +5,15 @@ import {
   Resolver,
 } from 'src/tenant/resolver-builder/interfaces/resolvers-builder.interface';
 import { SchemaBuilderContext } from 'src/tenant/schema-builder/interfaces/schema-builder-context.interface';
-import { FactoryInterface } from 'src/tenant/resolver-builder/interfaces/factory.interface';
+import { ResolverBuilderFactoryInterface } from 'src/tenant/resolver-builder/interfaces/resolver-builder-factory.interface';
 
 import { DataSourceService } from 'src/metadata/data-source/data-source.service';
 import { PGGraphQLQueryRunner } from 'src/tenant/resolver-builder/pg-graphql/pg-graphql-query-runner';
 
 @Injectable()
-export class FindManyResolverFactory implements FactoryInterface {
+export class FindManyResolverFactory
+  implements ResolverBuilderFactoryInterface
+{
   public static methodName = 'findMany' as const;
 
   constructor(private readonly dataSourceService: DataSourceService) {}
