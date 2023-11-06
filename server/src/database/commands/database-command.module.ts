6edcd8a4c62509c2751874b76ac222624e1913c4
martmull <martmull@hotmail.fr>
2023-11-06 10:42:28 +0100

    Remove workspaces from database

--- server/src/database/commands/database-command.module.ts
+++ server/src/database/commands/database-command.module.ts
@@ -3,8 +3,14 @@ import { Module } from '@nestjs/common';
 import { DataCleanInactiveCommand } from 'src/database/commands/clean-inactive-workspaces.command';
 import { PrismaService } from 'src/database/prisma.service';
 import { ConfirmationQuestion } from 'src/database/commands/confirmation.question';
+import { WorkspaceService } from 'src/core/workspace/services/workspace.service';
 
 @Module({
-  providers: [DataCleanInactiveCommand, ConfirmationQuestion, PrismaService],
+  providers: [
+    DataCleanInactiveCommand,
+    ConfirmationQuestion,
+    PrismaService,
+    WorkspaceService,
+  ],
 })
 export class DatabaseCommandModule {}
