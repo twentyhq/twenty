290e2e812031369b65fccfe051a0ad12ab77e3c6
martmull <martmull@hotmail.fr>
2023-11-06 10:01:37 +0100

    Add a dry-run option

--- server/src/database/commands/database-command.module.ts
+++ server/src/database/commands/database-command.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { DataCleanInactiveCommand } from 'src/database/commands/clean-inactive-workspaces.command';
 import { PrismaService } from 'src/database/prisma.service';
+import { ConfirmationQuestion } from 'src/database/commands/confirmation.question';
 
 @Module({
-  providers: [DataCleanInactiveCommand, PrismaService],
+  providers: [DataCleanInactiveCommand, ConfirmationQuestion, PrismaService],
 })
 export class DatabaseCommandModule {}
