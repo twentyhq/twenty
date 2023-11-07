7aa6b204189312911a9582debd461169c83f169a
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-11-07 12:33:40 +0100

    Adding the possibility to add multiple ui components in the live code editor in the docs (#2381)
    * working
    
    * forgot docs folder
    
    * modify according to comments

--- front/tsup.ui.index.tsx
+++ front/tsup.ui.index.tsx
@@ -1,12 +1,8 @@
-import { ThemeProvider as EmotionThemProvider } from '@emotion/react';
+import {  ThemeType } from './src/modules/ui/theme/constants/theme';
 
-import { lightTheme, ThemeType } from './src/modules/ui/theme/constants/theme';
+export {ThemeProvider} from '@emotion/react';
 
-export const ThemeProvider = ({ children }: { children: any }) => {
-  return (
-    <EmotionThemProvider theme={lightTheme}>{children}</EmotionThemProvider>
-  );
-};
+export {lightTheme, darkTheme} from './src/modules/ui/theme/constants/theme';
 
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -14,3 +10,4 @@ declare module '@emotion/react' {
 }
 
 export * from './src/modules/ui/display/checkmark/components/Checkmark';
+export * from './src/modules/ui/input/button/components/Button';
