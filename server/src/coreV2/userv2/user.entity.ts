56dc87a60f9ada57be5e0729a70b0daa84849161
gitstart-twenty <140154534+gitstart-twenty@users.noreply.github.com>
2023-11-08 16:11:52 +0545

    chore: create a new TypeORM config using @nestjs/typeorm for public schema (#2241)
    * chore: create a new TypeORM config using @nestjs/typeorm for public schema
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    * Refactor according to review
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    * Refactor according to review
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    * Refactor according to review
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    * Refactor according to review
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    * Remove unnecessary changes
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    * Refactor imports
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    ---------
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>

---
+++ server/src/coreV2/userv2/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column({ default: false })
+  emailVerified: boolean;
+}
