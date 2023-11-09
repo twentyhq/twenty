c8eda6170427711454e68ba436d887255c98f82c
Pallavi Varshney <h20190029g@alumni.bits-pilani.ac.in>
2023-11-09 07:34:54 -0800

    #2133 Add comments icon and count to Timeline card (#2205)
    * fix
    
    * #2133 added comments icon and count on notes tab
    
    * reverted changes in people-filters.tsx
    
    * added comment icon and count on timeline in People/Companies
    
    * removed infra/dev/scripts/hashicorp.gpg
    
    * added comment count and icon on timeline cards with seperate react component
    
    * used isNonEmptyArray typeguard for array length check
    
    * updated review feedback
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

---
+++ front/src/modules/activities/comment/CommentCounter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { IconComment } from '@/ui/display/icon';
+
+const StyledCommentIcon = styled.div`
+  align-items: center;
+  color: ${({ theme }) => theme.font.color.light};
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(1)};
+`;
+
+const CommentCounter = ({ commentCount }: { commentCount: number }) => {
+  const theme = useTheme();
+  return (
+    <div>
+      {commentCount > 0 && (
+        <StyledCommentIcon>
+          <IconComment size={theme.icon.size.md} />
+          {commentCount}
+        </StyledCommentIcon>
+      )}
+    </div>
+  );
+};
+
+export default CommentCounter;
