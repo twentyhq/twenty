5deda38d6505e7c4a279a8ccc7e7c1f0e554367b
corentin <corentin@twenty.com>
2023-11-15 19:18:28 +0100

    fix tenantMigration order

--- server/src/metadata/tenant-migration/tenant-migration.service.ts
+++ server/src/metadata/tenant-migration/tenant-migration.service.ts
@@ -62,7 +62,7 @@ export class TenantMigrationService {
     workspaceId: string,
   ): Promise<TenantMigrationEntity[]> {
     return await this.tenantMigrationRepository.find({
-      order: { createdAt: 'ASC' },
+      order: { name: 'ASC', createdAt: 'ASC' },
       where: {
         appliedAt: IsNull(),
         workspaceId,
