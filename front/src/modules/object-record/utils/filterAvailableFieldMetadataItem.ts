7da18a13e810ac11a432e18448de027827062814
Lucas Bordeau <bordeau.lucas@gmail.com>
2023-11-16 17:09:50 +0100

    Feat/filter available field definition v2 (#2547)
    * Added react-dev-inspector
    
    * Add field relation type parsing and filter available fields for record table and show page
    
    * Revert "Added react-dev-inspector"
    
    This reverts commit 7a78964c2c3996ce2e27f6f8d0c0b5e1f3283e17.

---
+++ front/src/modules/object-record/utils/filterAvailableFieldMetadataItem.ts
@@ -0,0 +1,21 @@
+import { FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
+import { parseFieldRelationType } from '@/object-metadata/utils/parseFieldRelationType';
+
+export const filterAvailableFieldMetadataItem = (
+  fieldMetadataItem: FieldMetadataItem,
+): boolean => {
+  if (fieldMetadataItem.type === 'RELATION') {
+    const fieldMetadataItemRelationType =
+      parseFieldRelationType(fieldMetadataItem);
+
+    if (fieldMetadataItemRelationType !== 'TO_ONE_OBJECT') {
+      return false;
+    }
+  }
+
+  if (fieldMetadataItem.type === 'UUID') {
+    return false;
+  }
+
+  return true;
+};
