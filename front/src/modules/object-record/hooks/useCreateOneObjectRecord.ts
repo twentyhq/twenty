15b3408edd8377422f0e94880af05c38ed15cac0
Charles Bochet <charles@twenty.com>
2023-11-17 14:34:57 +0100

    Fix Timeline

--- front/src/modules/object-record/hooks/useCreateOneObjectRecord.ts
+++ front/src/modules/object-record/hooks/useCreateOneObjectRecord.ts
@@ -4,26 +4,7 @@ import { v4 } from 'uuid';
 import { useOptimisticEffect } from '@/apollo/optimistic-effect/hooks/useOptimisticEffect';
 import { useFindOneObjectMetadataItem } from '@/object-metadata/hooks/useFindOneObjectMetadataItem';
 import { ObjectMetadataItemIdentifier } from '@/object-metadata/types/ObjectMetadataItemIdentifier';
-import { CurrencyCode, FieldMetadataType } from '~/generated-metadata/graphql';
 import { capitalize } from '~/utils/string/capitalize';
-
-const defaultFieldValues: Record<FieldMetadataType, unknown> = {
-  [FieldMetadataType.Currency]: {
-    amountMicros: null,
-    currencyCode: CurrencyCode.Usd,
-  },
-  [FieldMetadataType.Boolean]: false,
-  [FieldMetadataType.Date]: null,
-  [FieldMetadataType.Email]: '',
-  [FieldMetadataType.Enum]: null,
-  [FieldMetadataType.Number]: null,
-  [FieldMetadataType.Probability]: null,
-  [FieldMetadataType.Relation]: null,
-  [FieldMetadataType.Phone]: '',
-  [FieldMetadataType.Text]: '',
-  [FieldMetadataType.Link]: { url: '', label: '' },
-  [FieldMetadataType.Uuid]: '',
-};
 
 export const useCreateOneObjectRecord = ({
   objectNamePlural,
