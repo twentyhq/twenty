4e3c3be5a4bdfcf219b142aa9c211733f9ca696b
Abdullah <125115953+mabdullahabaid@users.noreply.github.com>
2023-11-18 13:24:10 +0500

    feat: inject the add to twenty button into linkedin company page

--- packages/twenty-chrome-extension/src/contentScript/index.ts
+++ packages/twenty-chrome-extension/src/contentScript/index.ts
@@ -9,9 +9,9 @@ function createNewButton(): HTMLButtonElement {
     color: 'white',
     fontSize: '1.5rem',
     fontWeight: '600',
-    marginRight: '0.5em',
     padding: '0.45em 1em',
     width: '15rem',
+    height: '32px', // Default LinkedIn buttons height.
   };
 
   // Apply styles to the button.
@@ -44,14 +44,36 @@ function createNewButton(): HTMLButtonElement {
   return newButton;
 }
 
-function insertButtonBeforeOpenToButton(): void {
+function insertButtonForUser(): void {
   const parentDiv: HTMLDivElement | null = document.querySelector('.pv-top-card-v2-ctas');
 
   if (parentDiv) {
-    const newButton: HTMLButtonElement = createNewButton();
-    parentDiv.prepend(newButton);
+    const newButtonUser: HTMLButtonElement = createNewButton();
+    parentDiv.prepend(newButtonUser);
+
+    const buttonSpecificStyles = {
+      marginRight: '0.5em',
+    };
+
+    Object.assign(newButtonUser.style, buttonSpecificStyles);
+  }
+}
+
+function insertButtonForCompany(): void {
+  const parentDiv: HTMLDivElement | null = document.querySelector('.org-top-card-primary-actions__inner');
+
+  if (parentDiv) {
+    const newButtonCompany: HTMLButtonElement = createNewButton();
+    parentDiv.prepend(newButtonCompany);
+
+    const buttonSpecificStyles = {
+      alignSelf: 'end',
+    };
+
+    Object.assign(newButtonCompany.style, buttonSpecificStyles);
   }
 }
 
 // Execute the insertion of the button when the content script loads
-insertButtonBeforeOpenToButton();
+insertButtonForUser();
+insertButtonForCompany();
