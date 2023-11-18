35181635d2297cd78fa0d617896ee01072c08e75
Abdullah <125115953+mabdullahabaid@users.noreply.github.com>
2023-11-18 12:43:13 +0500

    feat: create the api key form ui and render it on the options page

--- packages/twenty-chrome-extension/src/options/Options.tsx
+++ packages/twenty-chrome-extension/src/options/Options.tsx
@@ -1,8 +1,20 @@
+import styled from '@emotion/styled';
+import { ApiKeyForm } from '../modules/api-key/components/ApiKeyForm';
+
+const StyledContainer = styled.div`
+  background: ${({ theme }) => theme.background.noisy};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
 const Options = () => {
   return (
-    <>
-      <div>Options</div>
-    </>
+    <StyledContainer>
+      <ApiKeyForm />
+    </StyledContainer>
   );
 };
 
