c795db33b2d3115e963ac1678d32e2619ed843fb
Weiko <corentin@twenty.com>
2023-11-23 16:26:33 +0100

    Add activityTarget relation after custom object creation (#2670)
    * Add activityTarget relation after custom object creation
    
    * add isCustom check for relations

--- server/src/metadata/object-metadata/object-metadata.service.ts
+++ server/src/metadata/object-metadata/object-metadata.service.ts
@@ -11,9 +11,16 @@ import {
   WorkspaceMigrationColumnCreate,
   WorkspaceMigrationTableAction,
 } from 'src/metadata/workspace-migration/workspace-migration.entity';
-import { FieldMetadataType } from 'src/metadata/field-metadata/field-metadata.entity';
+import {
+  FieldMetadataEntity,
+  FieldMetadataType,
+} from 'src/metadata/field-metadata/field-metadata.entity';
 import { TypeORMService } from 'src/database/typeorm/typeorm.service';
 import { DataSourceService } from 'src/metadata/data-source/data-source.service';
+import {
+  RelationMetadataEntity,
+  RelationMetadataType,
+} from 'src/metadata/relation-metadata/relation-metadata.entity';
 
 import { ObjectMetadataEntity } from './object-metadata.entity';
 
@@ -24,6 +31,12 @@ export class ObjectMetadataService extends TypeOrmQueryService<ObjectMetadataEnt
   constructor(
     @InjectRepository(ObjectMetadataEntity, 'metadata')
     private readonly objectMetadataRepository: Repository<ObjectMetadataEntity>,
+
+    @InjectRepository(FieldMetadataEntity, 'metadata')
+    private readonly fieldMetadataRepository: Repository<FieldMetadataEntity>,
+
+    @InjectRepository(RelationMetadataEntity, 'metadata')
+    private readonly relationMetadataRepository: Repository<RelationMetadataEntity>,
 
     private readonly dataSourceService: DataSourceService,
     private readonly typeORMService: TypeORMService,
@@ -117,6 +130,86 @@ export class ObjectMetadataService extends TypeOrmQueryService<ObjectMetadataEnt
         ],
     });
 
+    const activityTargetObjectMetadata =
+      await this.objectMetadataRepository.findOneByOrFail({
+        nameSingular: 'activityTarget',
+        workspaceId: record.workspaceId,
+      });
+
+    const activityTargetRelationFieldMetadata =
+      await this.fieldMetadataRepository.save([
+        // FROM
+        {
+          objectMetadataId: createdObjectMetadata.id,
+          workspaceId: record.workspaceId,
+          isCustom: true,
+          isActive: true,
+          type: FieldMetadataType.RELATION,
+          name: 'activityTargets',
+          label: 'Activities',
+          targetColumnMap: {},
+          description: `Activities tied to the ${record.labelSingular}`,
+          icon: 'IconCheckbox',
+          isNullable: true,
+        },
+        // TO
+        {
+          objectMetadataId: activityTargetObjectMetadata.id,
+          workspaceId: record.workspaceId,
+          isCustom: true,
+          isActive: true,
+          type: FieldMetadataType.RELATION,
+          name: record.nameSingular,
+          label: record.labelSingular,
+          targetColumnMap: {},
+          description: `ActivityTarget ${record.labelSingular}`,
+          icon: 'IconBuildingSkyscraper',
+          isNullable: true,
+        },
+        // Foreign key
+        {
+          objectMetadataId: activityTargetObjectMetadata.id,
+          workspaceId: record.workspaceId,
+          isCustom: true,
+          isActive: true,
+          type: FieldMetadataType.UUID,
+          name: `${createdObjectMetadata.targetTableName}Id`,
+          label: `${record.labelSingular} ID (foreign key)`,
+          targetColumnMap: {},
+          description: `ActivityTarget ${record.labelSingular} id foreign key`,
+          icon: undefined,
+          isNullable: true,
+          isSystem: true,
+          defaultValue: undefined,
+        },
+      ]);
+
+    const activityTargetRelationFieldMetadataMap =
+      activityTargetRelationFieldMetadata.reduce(
+        (acc, fieldMetadata: FieldMetadataEntity) => {
+          if (fieldMetadata.type === FieldMetadataType.RELATION) {
+            acc[fieldMetadata.objectMetadataId] = fieldMetadata;
+          }
+          return acc;
+        },
+        {},
+      );
+
+    await this.relationMetadataRepository.save([
+      {
+        workspaceId: record.workspaceId,
+        relationType: RelationMetadataType.ONE_TO_MANY,
+        fromObjectMetadataId: createdObjectMetadata.id,
+        toObjectMetadataId: activityTargetObjectMetadata.id,
+        fromFieldMetadataId:
+          activityTargetRelationFieldMetadataMap[createdObjectMetadata.id].id,
+        toFieldMetadataId:
+          activityTargetRelationFieldMetadataMap[
+            activityTargetObjectMetadata.id
+          ].id,
+      },
+    ]);
+
     await this.workspaceMigrationService.createCustomMigration(
       createdObjectMetadata.workspaceId,
       [
@@ -124,6 +217,30 @@ export class ObjectMetadataService extends TypeOrmQueryService<ObjectMetadataEnt
           name: createdObjectMetadata.targetTableName,
           action: 'create',
         } satisfies WorkspaceMigrationTableAction,
+        // Add activity target relation
+        {
+          name: activityTargetObjectMetadata.targetTableName,
+          action: 'alter',
+          columns: [
+            {
+              action: WorkspaceMigrationColumnActionType.CREATE,
+              columnName: `${createdObjectMetadata.targetTableName}Id`,
+              columnType: 'uuid',
+            } satisfies WorkspaceMigrationColumnCreate,
+          ],
+        },
+        {
+          name: activityTargetObjectMetadata.targetTableName,
+          action: 'alter',
+          columns: [
+            {
+              action: WorkspaceMigrationColumnActionType.RELATION,
+              columnName: `${createdObjectMetadata.targetTableName}Id`,
+              referencedTableName: createdObjectMetadata.targetTableName,
+              referencedTableColumnName: 'id',
+            },
+          ],
+        },
         // This is temporary until we implement mainIdentifier
         {
           name: createdObjectMetadata.targetTableName,
