28b3a11c596216cbce6c593769ff10469ff70caa
Rus Khakimov <396223+khakimov@users.noreply.github.com>
2023-11-30 22:13:25 +0700

    added workspaceId to seedFeatureFlags()

--- server/src/database/typeorm-seeds/core/feature-flags.ts
+++ server/src/database/typeorm-seeds/core/feature-flags.ts
@@ -2,11 +2,12 @@ import { DataSource } from 'typeorm';
 
 const tableName = 'featureFlag';
 
-import { SeedWorkspaceId } from 'src/database/typeorm-seeds/core/workspaces';
+// import { SeedWorkspaceId } from 'src/database/typeorm-seeds/core/workspaces';
 
 export const seedFeatureFlags = async (
   workspaceDataSource: DataSource,
   schemaName: string,
+  workspaceId: string,
 ) => {
   await workspaceDataSource
     .createQueryBuilder()
@@ -16,7 +17,7 @@ export const seedFeatureFlags = async (
     .values([
       {
         key: 'IS_RELATION_FIELD_TYPE_ENABLED',
-        workspaceId: SeedWorkspaceId,
+        workspaceId: workspaceId,
         value: true,
       },
     ])
