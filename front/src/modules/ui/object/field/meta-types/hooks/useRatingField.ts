8bd567c2b4fd5a59a7a410ca2aa56f0bca1c072b
Charles Bochet <charles@twenty.com>
2023-12-01 16:29:49 +0100

    Fix post merge conflict on Rating field hook

--- front/src/modules/ui/object/field/meta-types/hooks/useRatingField.ts
+++ front/src/modules/ui/object/field/meta-types/hooks/useRatingField.ts
@@ -5,9 +5,9 @@ import { FieldMetadataType } from '~/generated-metadata/graphql';
 
 import { FieldContext } from '../../contexts/FieldContext';
 import { entityFieldsFamilySelector } from '../../states/selectors/entityFieldsFamilySelector';
+import { FieldRatingValue } from '../../types/FieldMetadata';
 import { assertFieldMetadata } from '../../types/guards/assertFieldMetadata';
 import { isFieldRating } from '../../types/guards/isFieldRating';
-import { FieldRatingValue } from '../../types/FieldMetadata';
 
 export const useRatingField = () => {
   const { entityId, fieldDefinition, hotkeyScope } = useContext(FieldContext);
