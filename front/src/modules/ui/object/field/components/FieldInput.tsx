93e4f795514522b9ede899196667a099983edfe8
Thaïs <guigon.thais@gmail.com>
2023-12-01 15:31:01 +0100

    feat: rename Probability field type to Rating and update preview (#2770)
    Closes #2593

--- front/src/modules/ui/object/field/components/FieldInput.tsx
+++ front/src/modules/ui/object/field/components/FieldInput.tsx
@@ -12,7 +12,7 @@ import { EmailFieldInput } from '../meta-types/input/components/EmailFieldInput'
 import { LinkFieldInput } from '../meta-types/input/components/LinkFieldInput';
 import { NumberFieldInput } from '../meta-types/input/components/NumberFieldInput';
 import { PhoneFieldInput } from '../meta-types/input/components/PhoneFieldInput';
-import { ProbabilityFieldInput } from '../meta-types/input/components/ProbabilityFieldInput';
+import { RatingFieldInput } from '../meta-types/input/components/RatingFieldInput';
 import { RelationFieldInput } from '../meta-types/input/components/RelationFieldInput';
 import { TextFieldInput } from '../meta-types/input/components/TextFieldInput';
 import { FieldInputEvent } from '../types/FieldInputEvent';
@@ -23,7 +23,7 @@ import { isFieldEmail } from '../types/guards/isFieldEmail';
 import { isFieldLink } from '../types/guards/isFieldLink';
 import { isFieldNumber } from '../types/guards/isFieldNumber';
 import { isFieldPhone } from '../types/guards/isFieldPhone';
-import { isFieldProbability } from '../types/guards/isFieldProbability';
+import { isFieldRating } from '../types/guards/isFieldRating';
 import { isFieldRelation } from '../types/guards/isFieldRelation';
 import { isFieldText } from '../types/guards/isFieldText';
 
@@ -120,8 +120,8 @@ export const FieldInput = ({
         />
       ) : isFieldBoolean(fieldDefinition) ? (
         <BooleanFieldInput onSubmit={onSubmit} />
-      ) : isFieldProbability(fieldDefinition) ? (
-        <ProbabilityFieldInput onSubmit={onSubmit} />
+      ) : isFieldRating(fieldDefinition) ? (
+        <RatingFieldInput onSubmit={onSubmit} />
       ) : (
         <></>
       )}
