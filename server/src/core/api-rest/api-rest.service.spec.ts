df7427832ee139e0714e931b26c4a6f656b78d92
martmull <martmull@hotmail.fr>
2023-12-04 13:54:56 +0100

    Fix test

--- server/src/core/api-rest/api-rest.service.spec.ts
+++ server/src/core/api-rest/api-rest.service.spec.ts
@@ -6,7 +6,20 @@ import { EnvironmentService } from 'src/integrations/environment/environment.ser
 
 describe('ApiRestService', () => {
   let service: ApiRestService;
-  const objectMetadataItem = { fields: [{ name: 'field', type: 'NUMBER' }] };
+  const objectMetadataItem = {
+    fields: [
+      {
+        name: 'fieldNumber',
+        type: 'NUMBER',
+        targetColumnMap: { value: 'fieldNumber' },
+      },
+      {
+        name: 'fieldString',
+        type: 'TEXT',
+        targetColumnMap: { value: 'fieldString' },
+      },
+    ],
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -35,18 +48,18 @@ describe('ApiRestService', () => {
       expect(() => service.checkFilterQuery('())')).toThrow();
       expect(() =>
         service.checkFilterQuery(
-          'and(or(field[eq]:1,field[eq]:2)),field[eq]:3)',
+          'and(or(fieldNumber[eq]:1,fieldNumber[eq]:2)),fieldNumber[eq]:3)',
         ),
       ).toThrow();
       expect(() =>
         service.checkFilterQuery(
-          'and(or(field[eq]:1,field[eq]:2),field[eq]:3)',
+          'and(or(fieldNumber[eq]:1,fieldNumber[eq]:2),fieldNumber[eq]:3)',
         ),
       ).not.toThrow();
     });
   });
   describe('formatFieldValue', () => {
-    it('should format field value', () => {
+    it('should format fieldNumber value', () => {
       expect(service.formatFieldValue('1', 'NUMBER')).toEqual(1);
       expect(service.formatFieldValue('a', 'NUMBER')).toEqual(NaN);
       expect(service.formatFieldValue('true', 'BOOLEAN')).toEqual(true);
@@ -73,93 +86,127 @@ describe('ApiRestService', () => {
   });
   describe('parseFilterQueryContent', () => {
     it('should parse query filter test 1', () => {
-      expect(service.parseFilterQueryContent('and(field[eq]:1)')).toEqual([
-        'field[eq]:1',
-      ]);
+      expect(service.parseFilterQueryContent('and(fieldNumber[eq]:1)')).toEqual(
+        ['fieldNumber[eq]:1'],
+      );
     });
     it('should parse query filter test 2', () => {
       expect(
-        service.parseFilterQueryContent('and(field[eq]:1,field[eq]:2)'),
-      ).toEqual(['field[eq]:1', 'field[eq]:2']);
+        service.parseFilterQueryContent(
+          'and(fieldNumber[eq]:1,fieldNumber[eq]:2)',
+        ),
+      ).toEqual(['fieldNumber[eq]:1', 'fieldNumber[eq]:2']);
     });
     it('should parse query filter test 3', () => {
       expect(
         service.parseFilterQueryContent(
-          'and(field[eq]:1,or(field[eq]:2,field[eq]:3))',
+          'and(fieldNumber[eq]:1,or(fieldNumber[eq]:2,fieldNumber[eq]:3))',
         ),
-      ).toEqual(['field[eq]:1', 'or(field[eq]:2,field[eq]:3)']);
+      ).toEqual([
+        'fieldNumber[eq]:1',
+        'or(fieldNumber[eq]:2,fieldNumber[eq]:3)',
+      ]);
     });
     it('should parse query filter test 4', () => {
       expect(
         service.parseFilterQueryContent(
-          'and(field[eq]:1,or(field[eq]:2,not(field[eq]:3)),field[eq]:4,not(field[eq]:5))',
+          'and(fieldNumber[eq]:1,or(fieldNumber[eq]:2,not(fieldNumber[eq]:3)),fieldNumber[eq]:4,not(fieldNumber[eq]:5))',
         ),
       ).toEqual([
-        'field[eq]:1',
-        'or(field[eq]:2,not(field[eq]:3))',
-        'field[eq]:4',
-        'not(field[eq]:5)',
+        'fieldNumber[eq]:1',
+        'or(fieldNumber[eq]:2,not(fieldNumber[eq]:3))',
+        'fieldNumber[eq]:4',
+        'not(fieldNumber[eq]:5)',
       ]);
     });
     it('should parse query filter test 5', () => {
       expect(
-        service.parseFilterQueryContent('and(field[in]:[1,2],field[eq]:4)'),
-      ).toEqual(['field[in]:[1,2]', 'field[eq]:4']);
+        service.parseFilterQueryContent(
+          'and(fieldNumber[in]:[1,2],fieldNumber[eq]:4)',
+        ),
+      ).toEqual(['fieldNumber[in]:[1,2]', 'fieldNumber[eq]:4']);
     });
   });
   describe('parseStringFilter', () => {
     it('should parse string filter test 1', () => {
       expect(
         service.parseStringFilter(
-          'and(field[eq]:1,field[eq]:2)',
+          'and(fieldNumber[eq]:1,fieldNumber[eq]:2)',
           objectMetadataItem,
         ),
-      ).toEqual({ and: [{ field: { eq: 1 } }, { field: { eq: 2 } }] });
+      ).toEqual({
+        and: [{ fieldNumber: { eq: 1 } }, { fieldNumber: { eq: 2 } }],
+      });
     });
     it('should parse string filter test 2', () => {
       expect(
         service.parseStringFilter(
-          'and(field[eq]:1,or(field[eq]:2,field[eq]:3))',
+          'and(fieldNumber[eq]:1,or(fieldNumber[eq]:2,fieldNumber[eq]:3))',
           objectMetadataItem,
         ),
       ).toEqual({
         and: [
-          { field: { eq: 1 } },
-          { or: [{ field: { eq: 2 } }, { field: { eq: 3 } }] },
+          { fieldNumber: { eq: 1 } },
+          { or: [{ fieldNumber: { eq: 2 } }, { fieldNumber: { eq: 3 } }] },
         ],
       });
     });
     it('should parse string filter test 3', () => {
       expect(
         service.parseStringFilter(
-          'and(field[eq]:1,or(field[eq]:2,field[eq]:3,and(field[eq]:6,field[eq]:7)),or(field[eq]:4,field[eq]:5))',
+          'and(fieldNumber[eq]:1,or(fieldNumber[eq]:2,fieldNumber[eq]:3,and(fieldNumber[eq]:6,fieldNumber[eq]:7)),or(fieldNumber[eq]:4,fieldNumber[eq]:5))',
           objectMetadataItem,
         ),
       ).toEqual({
         and: [
-          { field: { eq: 1 } },
+          { fieldNumber: { eq: 1 } },
           {
             or: [
-              { field: { eq: 2 } },
-              { field: { eq: 3 } },
-              { and: [{ field: { eq: 6 } }, { field: { eq: 7 } }] },
+              { fieldNumber: { eq: 2 } },
+              { fieldNumber: { eq: 3 } },
+              { and: [{ fieldNumber: { eq: 6 } }, { fieldNumber: { eq: 7 } }] },
             ],
           },
-          { or: [{ field: { eq: 4 } }, { field: { eq: 5 } }] },
+          { or: [{ fieldNumber: { eq: 4 } }, { fieldNumber: { eq: 5 } }] },
+        ],
+      });
+    });
+    it('should parse string filter test 4', () => {
+      expect(
+        service.parseStringFilter(
+          'and(fieldString[gt]:"value",or(fieldNumber[is]:NOT_NULL,not(fieldString[startsWith]:"val"),and(fieldNumber[eq]:6,fieldString[ilike]:"%val%")),or(fieldNumber[eq]:4,fieldString[is]:NULL))',
+          objectMetadataItem,
+        ),
+      ).toEqual({
+        and: [
+          { fieldString: { gt: 'value' } },
+          {
+            or: [
+              { fieldNumber: { is: 'NOT_NULL' } },
+              { not: { fieldString: { startsWith: 'val' } } },
+              {
+                and: [
+                  { fieldNumber: { eq: 6 } },
+                  { fieldString: { ilike: '%val%' } },
+                ],
+              },
+            ],
+          },
+          { or: [{ fieldNumber: { eq: 4 } }, { fieldString: { is: 'NULL' } }] },
         ],
       });
     });
     it('should handler not', () => {
       expect(
         service.parseStringFilter(
-          'and(field[eq]:1,not(field[eq]:2))',
+          'and(fieldNumber[eq]:1,not(fieldNumber[eq]:2))',
           objectMetadataItem,
         ),
       ).toEqual({
         and: [
-          { field: { eq: 1 } },
+          { fieldNumber: { eq: 1 } },
           {
-            not: { field: { eq: 2 } },
+            not: { fieldNumber: { eq: 2 } },
           },
         ],
       });
@@ -184,9 +231,9 @@ describe('ApiRestService', () => {
     });
     it('should parse simple filter string test 3', () => {
       expect(
-        service.parseSimpleFilterString('field[gt]:valStart]:[valEnd'),
+        service.parseSimpleFilterString('fieldNumber[gt]:valStart]:[valEnd'),
       ).toEqual({
-        fields: ['field'],
+        fields: ['fieldNumber'],
         comparator: 'gt',
         value: 'valStart]:[valEnd',
       });
@@ -199,7 +246,7 @@ describe('ApiRestService', () => {
       ).toEqual({
         fields: ['person', 'createdAt'],
         comparator: 'gt',
-        value: '2023-12-01T14:23:23.914Z',
+        value: '"2023-12-01T14:23:23.914Z"',
       });
     });
     it('should parse simple filter string test 5', () => {
