2c46d9abde65d26c48b3ff7c325dd3fb69e1dc49
martmull <martmull@hotmail.fr>
2023-12-05 12:34:24 +0100

    Add tests for limit parser

---
+++ server/src/core/api-rest/api-rest-query-builder/factories/parsers/__tests__/limit-parser.factory.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { LimitParserFactory } from 'src/core/api-rest/api-rest-query-builder/factories/parsers/limit-parser.factory';
+
+describe('LimitParserFactory', () => {
+  let service: LimitParserFactory;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LimitParserFactory],
+    }).compile();
+    service = module.get<LimitParserFactory>(LimitParserFactory);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('create', () => {
+    it('should return default if limit missing', () => {
+      const request: any = { query: {} };
+      expect(service.create(request)).toEqual(60);
+    });
+    it('should return limit', () => {
+      const request: any = { query: { limit: '10' } };
+      expect(service.create(request)).toEqual(10);
+    });
+    it('should throw if not integer', () => {
+      const request: any = { query: { limit: 'aaa' } };
+      expect(() => service.create(request)).toThrow();
+    });
+  });
+});
