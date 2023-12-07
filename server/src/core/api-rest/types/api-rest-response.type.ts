88f5950ddf16f9a6787f21e81abbd51a81dfdaba
martmull <martmull@hotmail.fr>
2023-12-07 12:27:47 +0100

    Handle graphql errors

--- server/src/core/api-rest/types/api-rest-response.type.ts
+++ server/src/core/api-rest/types/api-rest-response.type.ts
@@ -1,1 +1,5 @@
-export type ApiRestResponse = { data: { error?: string } };
+import { HttpException } from '@nestjs/common';
+
+export type ApiRestResponse = {
+  data: { error?: HttpException | string; status?: number };
+};
