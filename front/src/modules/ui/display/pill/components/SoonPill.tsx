62fa55eae6f92661e9f3ef1a0912a30ce7871757
Thaïs <guigon.thais@gmail.com>
2023-12-07 12:43:38 +0100

    feat: add Settings/Accounts Settings section (#2853)
    Closes #2818

--- front/src/modules/ui/display/pill/components/SoonPill.tsx
+++ front/src/modules/ui/display/pill/components/SoonPill.tsx
@@ -1,5 +1,9 @@
 import styled from '@emotion/styled';
 
+type SoonPillProps = {
+  className?: string;
+};
+
 const StyledSoonPill = styled.span`
   align-items: center;
   background: ${({ theme }) => theme.background.transparent.light};
@@ -13,8 +17,9 @@ const StyledSoonPill = styled.span`
   height: ${({ theme }) => theme.spacing(4)};
   justify-content: flex-end;
   line-height: ${({ theme }) => theme.text.lineHeight.lg};
-  margin-left: auto;
   padding: ${({ theme }) => `0 ${theme.spacing(2)}`};
 `;
 
-export const SoonPill = () => <StyledSoonPill>Soon</StyledSoonPill>;
+export const SoonPill = ({ className }: SoonPillProps) => (
+  <StyledSoonPill className={className}>Soon</StyledSoonPill>
+);
