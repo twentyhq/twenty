d4613c87f6b8852ed421afa52cc0ce657f8f0672
Charles Bochet <charles@twenty.com>
2023-12-08 13:00:59 +0100

    Fix infinite loop on sign in

--- front/src/modules/object-record/hooks/useObjectRecordTable.ts
+++ front/src/modules/object-record/hooks/useObjectRecordTable.ts
@@ -1,16 +1,19 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
+import { currentWorkspaceState } from '@/auth/states/currentWorkspaceState';
 import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
 import { useObjectNameSingularFromPlural } from '@/object-metadata/hooks/useObjectNameSingularFromPlural';
 import { turnFiltersIntoWhereClause } from '@/object-record/object-filter-dropdown/utils/turnFiltersIntoWhereClause';
 import { turnSortsIntoOrderBy } from '@/object-record/object-sort-dropdown/utils/turnSortsIntoOrderBy';
 import { useRecordTableScopedStates } from '@/object-record/record-table/hooks/internal/useRecordTableScopedStates';
 import { useRecordTable } from '@/object-record/record-table/hooks/useRecordTable';
+import { signInBackgroundMockCompanies } from '@/sign-in-background-mock/constants/signInBackgroundMockCompanies';
 
 import { useFindManyRecords } from './useFindManyRecords';
 
 export const useObjectRecordTable = () => {
   const { scopeId: objectNamePlural, setRecordTableData } = useRecordTable();
+  const currentWorkspace = useRecoilValue(currentWorkspaceState);
 
   const { objectNameSingular } = useObjectNameSingularFromPlural({
     objectNamePlural,
@@ -48,7 +51,7 @@ export const useObjectRecordTable = () => {
     });
 
   return {
-    records,
+    records: currentWorkspace ? records : signInBackgroundMockCompanies,
     loading,
     fetchMoreRecords,
     queryStateIdentifier,
--- front/src/modules/object-record/record-table/hooks/internal/useSetRecordTableData.ts
+++ front/src/modules/object-record/record-table/hooks/internal/useSetRecordTableData.ts
@@ -1,13 +1,12 @@
 import { useRecoilCallback } from 'recoil';
 
 import { entityFieldsFamilyState } from '@/object-record/field/states/entityFieldsFamilyState';
+import { useResetTableRowSelection } from '@/object-record/record-table/hooks/internal/useResetTableRowSelection';
+import { isFetchingRecordTableDataState } from '@/object-record/record-table/states/isFetchingRecordTableDataState';
+import { numberOfTableRowsState } from '@/object-record/record-table/states/numberOfTableRowsState';
 import { isDeeplyEqual } from '~/utils/isDeeplyEqual';
 
-import { isFetchingRecordTableDataState } from '../../states/isFetchingRecordTableDataState';
-import { numberOfTableRowsState } from '../../states/numberOfTableRowsState';
 import { tableRowIdsState } from '../../states/tableRowIdsState';
-
-import { useResetTableRowSelection } from './useResetTableRowSelection';
 
 type useSetRecordTableDataProps = {
   onEntityCountChange: (entityCount: number) => void;
