7045fb1a83d3650c97b8917fa7b6224239427d35
bosiraphael <raphael.bosi@gmail.com>
2023-12-12 18:13:06 +0100

    update description

--- packages/twenty-server/src/workspace/messaging/commands/fetch-workspace-messages.command.ts
+++ packages/twenty-server/src/workspace/messaging/commands/fetch-workspace-messages.command.ts
@@ -6,8 +6,7 @@ interface FetchWorkspaceMessagesOptions {
 
 @Command({
   name: 'workspace:fetch-messages',
-  description:
-    'Seed workspace with initial data. This command is intended for development only.',
+  description: 'Fetch messages of all workspaceMembers in a workspace.',
 })
 export class FetchWorkspaceMessagesCommand extends CommandRunner {
   async run(
