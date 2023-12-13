5cf1343c6ebbd5381b0397c2a9f90501e354919c
Charles Bochet <charles@twenty.com>
2023-12-13 14:10:03 +0100

    Fix

--- packages/twenty-front/.storybook/preview.ts
+++ packages/twenty-front/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
 import { Preview, ReactRenderer } from '@storybook/react';
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
-import { initialize, mswLoader, mswDecorator } from 'msw-storybook-addon';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 
 import { RootDecorator } from '../src/testing/decorators/RootDecorator';
 import { mockedUserJWT } from '../src/testing/mock-data/jwt';
@@ -40,7 +40,6 @@ const preview: Preview = {
     cookie: {
       tokenPair: `{%22accessToken%22:{%22token%22:%22${mockedUserJWT}%22%2C%22expiresAt%22:%222023-07-18T15:06:40.704Z%22%2C%22__typename%22:%22AuthToken%22}%2C%22refreshToken%22:{%22token%22:%22${mockedUserJWT}%22%2C%22expiresAt%22:%222023-10-15T15:06:41.558Z%22%2C%22__typename%22:%22AuthToken%22}%2C%22__typename%22:%22AuthTokenPair%22}`,
     },
-    loaders: [mswLoader],
   },
 };
 
