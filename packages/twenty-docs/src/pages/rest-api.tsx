c4e01a6dce70f61c764154b16a17e20af29f21ad
martmull <martmull@hotmail.fr>
2023-12-14 13:07:19 +0100

    Add ugly form to fetch open api schema

--- packages/twenty-docs/src/pages/rest-api.tsx
+++ packages/twenty-docs/src/pages/rest-api.tsx
@@ -1,14 +1,42 @@
 import Layout from "@theme/Layout";
 import BrowserOnly from "@docusaurus/BrowserOnly";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { API } from '@stoplight/elements';
 import '@stoplight/elements/styles.min.css';
 
 const RestApiComponent = () => {
+  const [token, setToken] = useState('')
+  const [openApiJson, setOpenApiJson] = useState({})
+  const updateToken = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setToken(event.target.value)
+  }
+  const submitToken = async (e) => {
+    e.preventDefault();
+    const json = await fetch(
+      "http://localhost:3000/open-api",
+      {headers: {Authorization: `Bearer ${token}`}}
+    ).then((res)=> res.json())
+    setOpenApiJson(json)
+  }
+  useEffect(()=> {
+     (async ()=> {
+      const initialJson = await fetch(
+        "http://localhost:3000/open-api",
+      ).then((res)=> res.json())
+     setOpenApiJson(initialJson)
+    })()
+  },[])
   return (
     <div className="App">
+      <div>
+        <form>
+          <input type='text' value={token} onChange={updateToken}/>
+          <button type='submit' onClick={submitToken}>Load Schema</button>
+        </form>
+      </div>
       <API
-        apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml"
+        apiDescriptionDocument={JSON.stringify(openApiJson)}
+        router="hash"
       />
     </div>
   )
