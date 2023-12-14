67631f2b81d1c1dd17db68c540d20a2d6923738d
Kanav Arora <kanavarora1515@gmail.com>
2023-12-14 16:51:11 +0530

    2980-fix: CommandGroup background

--- packages/twenty-front/src/modules/command-menu/components/CommandGroup.tsx
+++ packages/twenty-front/src/modules/command-menu/components/CommandGroup.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 
 const StyledGroup = styled.div`
   align-items: center;
+  background: ${({ theme }) => theme.background.transparent};
   color: ${({ theme }) => theme.font.color.light};
   font-size: ${({ theme }) => theme.font.size.xs};
   font-weight: ${({ theme }) => theme.font.weight.semiBold};
