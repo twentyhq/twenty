64fba08639a156615f50548acbf870bf2c0f75ec
Charles Bochet <charles@twenty.com>
2023-12-15 19:36:17 +0100

    Fix spacing

--- packages/twenty-docs/src/pages/rest-api.tsx
+++ packages/twenty-docs/src/pages/rest-api.tsx
@@ -2,7 +2,7 @@ import Layout from "@theme/Layout";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import React, { useEffect, useState } from "react";
 import { API } from '@stoplight/elements';
-import spotlightTheme from  '!css-loader!@stoplight/elements/styles.min.css';
+import spotlightTheme from '!css-loader!@stoplight/elements/styles.min.css';
 import './rest-api.css'
 import { parseJson } from "nx/src/utils/json";
 import { TbLoader2 } from "react-icons/tb";
