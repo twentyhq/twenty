1e47994315fee750a329a707eaabbc62b5e13d7c
martmull <martmull@hotmail.fr>
2023-12-15 12:16:00 +0100

    Update design

--- packages/twenty-docs/src/pages/rest-api.tsx
+++ packages/twenty-docs/src/pages/rest-api.tsx
@@ -14,25 +14,29 @@ interface TokenFormPropsI {
 
 const TokenForm = ({onSubmit, isTokenValid, token}: TokenFormPropsI)=> {
   const updateToken = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onSubmit(event.target.value)
     localStorage.setItem('TryIt_securitySchemeValues', JSON.stringify({bearerAuth: event.target.value}))
+    onSubmit(event.target.value)
   }
 
   return !isTokenValid && (
     <div>
+      <div className='container'>
       <form className="form">
         <label>
           To load all your API endpoints, <a className='link' href='https://app.twenty.com/settings/developers/api-keys'>generate an API key</a> and paste it here:
         </label>
-        <input
-          className={token ? "input invalid" : "input"}
-          type='text'
-          placeholder='123'
-          value={token}
-          onChange={updateToken}
-        />
-        {token && <span style={{ color: 'red', fontSize: '12px' }}>Token invalid</span>}
+        <p>
+          <input
+            className={token ? "input invalid" : "input"}
+            type='text'
+            placeholder='123'
+            value={token}
+            onChange={updateToken}
+          />
+          <p className={`token-invalid`} style={{visibility: !token ? 'hidden' : 'visible'}} >Token invalid</p>
+        </p>
       </form>
+      </div>
     </div>
     )
 }
