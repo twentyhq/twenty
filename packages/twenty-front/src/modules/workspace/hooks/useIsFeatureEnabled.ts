ac3c517c82433c23a3b9e03a447b19472b5c780a
Thaïs <guigon.thais@gmail.com>
2023-12-15 16:31:03 +0100

    feat: add Show Page Emails tab (#2962)
    * feat: add Show Page Emails tab
    
    Closes #2926, Closes #2927
    
    * feat: review - disable Emails tab if messaging not enabled
    
    * refactor: review - add FeatureFlagKey type
    
    ---------
    
    Co-authored-by: Thais GUIGON <thaisguigon@macbook-pro.home>

--- packages/twenty-front/src/modules/workspace/hooks/useIsFeatureEnabled.ts
+++ packages/twenty-front/src/modules/workspace/hooks/useIsFeatureEnabled.ts
@@ -1,17 +1,14 @@
 import { useRecoilValue } from 'recoil';
 
 import { currentWorkspaceState } from '@/auth/states/currentWorkspaceState';
+import { FeatureFlagKey } from '@/workspace/types/FeatureFlagKey';
 
-export const useIsFeatureEnabled = (featureKey: string): boolean => {
+export const useIsFeatureEnabled = (featureKey: FeatureFlagKey) => {
   const currentWorkspace = useRecoilValue(currentWorkspaceState);
 
   const featureFlag = currentWorkspace?.featureFlags?.find(
     (flag) => flag.key === featureKey,
   );
 
-  if (!featureFlag) {
-    return false;
-  }
-
-  return featureFlag.value;
+  return !!featureFlag?.value;
 };
