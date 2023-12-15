69b7fcce47d1a4c5ea537310d87f30a007fd7939
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-12-15 16:35:56 +0100

    2880 timebox create a poc to fetch emails from the gmail api (#2993)
    * create empty service
    
    * getting threads is working
    
    * insert message channel
    
    * save threads in the db
    
    * clean
    
    * fetch messages
    
    * create a service to fetch a batch of messages
    
    * batch messages
    
    * use httpService instead
    
    * parse batch
    
    * base 64 decoding working
    
    * solve parsing bug
    
    * saving messages is working
    
    * bug to fix in fetchAllByBatches
    
    * fetching all messages is working but not saving yet
    
    * fecth 500 messages and threads is working
    
    * remove unused package and console log
    
    * set direction to incoming
    
    * fix bug after merging main

--- packages/twenty-server/src/workspace/messaging/commands/fetch-workspace-messages-commands.module.ts
+++ packages/twenty-server/src/workspace/messaging/commands/fetch-workspace-messages-commands.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { FetchWorkspaceMessagesCommand } from 'src/workspace/messaging/commands/fetch-workspace-messages.command';
 import { MessagingModule } from 'src/workspace/messaging/messaging.module';
+import { FetchWorkspaceMessagesModule } from 'src/workspace/messaging/services/fetch-workspace-messages.module';
 
 @Module({
-  imports: [MessagingModule],
+  imports: [MessagingModule, FetchWorkspaceMessagesModule],
   providers: [FetchWorkspaceMessagesCommand],
 })
 export class FetchWorkspaceMessagesCommandsModule {}
