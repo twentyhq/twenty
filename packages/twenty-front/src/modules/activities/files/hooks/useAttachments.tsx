5f7442cf2377568a084af74e18e5f0469014ff48
gitstart-twenty <140154534+gitstart-twenty@users.noreply.github.com>
2023-12-15 17:53:20 +0800

    Add jest tests for twenty-front (#2983)
    * Add jest tests for twenty-front
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    
    * Fix tests
    
    ---------
    
    Co-authored-by: gitstart-twenty <gitstart-twenty@users.noreply.github.com>
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- packages/twenty-front/src/modules/activities/files/hooks/useAttachments.tsx
+++ packages/twenty-front/src/modules/activities/files/hooks/useAttachments.tsx
@@ -3,6 +3,7 @@ import { useFindManyRecords } from '@/object-record/hooks/useFindManyRecords';
 
 import { ActivityTargetableEntity } from '../../types/ActivityTargetableEntity';
 
+// do we need to test this?
 export const useAttachments = (entity: ActivityTargetableEntity) => {
   const { records: attachments } = useFindManyRecords({
     objectNameSingular: 'attachment',
---
+++ packages/twenty-front/src/modules/companies/utils/__tests__/mapBoardFieldDefinitionsToViewFields.test.ts
@@ -0,0 +1,49 @@
+import { mapBoardFieldDefinitionsToViewFields } from '@/companies/utils/mapBoardFieldDefinitionsToViewFields';
+import { FieldMetadata } from '@/object-record/field/types/FieldMetadata';
+import { BoardFieldDefinition } from '@/object-record/record-board/types/BoardFieldDefinition';
+
+describe('mapBoardFieldDefinitionsToViewFields', () => {
+  it('should map board field definitions to view fields', () => {
+    const fieldDefinitions: BoardFieldDefinition<FieldMetadata>[] = [
+      {
+        fieldMetadataId: 'fieldMetadataId',
+        label: 'label',
+        iconName: 'iconName',
+        type: 'BOOLEAN',
+        metadata: {
+          objectMetadataNameSingular: 'objectMetadataNameSingular',
+          fieldName: 'fieldName',
+        },
+        position: 0,
+        isVisible: true,
+        viewFieldId: 'viewFieldId',
+      },
+      {
+        fieldMetadataId: 'fieldMetadataId1',
+        label: 'label1',
+        iconName: 'iconName1',
+        type: 'NUMBER',
+        metadata: {
+          objectMetadataNameSingular: 'objectMetadataNameSingular1',
+          fieldName: 'fieldName1',
+          placeHolder: 'placeHolder1',
+          isPositive: true,
+        },
+        position: 1,
+        isVisible: false,
+        viewFieldId: 'viewFieldId1',
+      },
+    ];
+    const viewFields = mapBoardFieldDefinitionsToViewFields(fieldDefinitions);
+
+    expect(viewFields).toHaveLength(2);
+
+    expect(viewFields[0]).toHaveProperty('id');
+    expect(viewFields[0]).toHaveProperty('size');
+    expect(viewFields[0]).toHaveProperty('position');
+    expect(viewFields[0]).toHaveProperty('isVisible');
+
+    expect(viewFields[0].definition).toEqual(fieldDefinitions[0]);
+    expect(viewFields[1].definition).toEqual(fieldDefinitions[1]);
+  });
+});
