9fe17cbf8856f3661057f77cbd88045e65ba8d85
martmull <martmull@hotmail.fr>
2023-12-15 10:44:45 +0100

    Use local storage

--- packages/twenty-docs/src/pages/rest-api.tsx
+++ packages/twenty-docs/src/pages/rest-api.tsx
@@ -4,29 +4,34 @@ import React, { useEffect, useState } from "react";
 import { API } from '@stoplight/elements';
 import '@stoplight/elements/styles.min.css';
 import './graphql.css'
+import { parseJson } from "nx/src/utils/json";
 
 interface TokenFormPropsI {
-  onSubmit: (token: string) => void
+  onSubmit: (token: string) => void,
+  isTokenValid: boolean | undefined,
+  token: string,
 }
 
-const TokenForm = ({onSubmit}: TokenFormPropsI)=> {
+const TokenForm = ({onSubmit, isTokenValid, token}: TokenFormPropsI)=> {
   const updateToken = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault()
     onSubmit(event.target.value)
+    localStorage.setItem('TryIt_securitySchemeValues', JSON.stringify({bearerAuth: event.target.value}))
   }
 
-  return (
+  return !isTokenValid && (
     <div>
       <form className="form">
         <label>
           To load all your API endpoints, <a className='link' href='https://app.twenty.com/settings/developers/api-keys'>generate an API key</a> and paste it here:
         </label>
         <input
-          className="input"
+          className={token ? "input invalid" : "input"}
           type='text'
           placeholder='123'
+          value={token}
           onChange={updateToken}
         />
+        {token && <span style={{ color: 'red', fontSize: '12px' }}>Token invalid</span>}
       </form>
     </div>
     )
@@ -34,34 +39,49 @@ const TokenForm = ({onSubmit}: TokenFormPropsI)=> {
 
 const RestApiComponent = () => {
   const [openApiJson, setOpenApiJson] = useState({})
+  const [isTokenValid, setIsTokenValid] = useState<TokenFormPropsI['isTokenValid']>(undefined)
+  const storedToken = parseJson(localStorage.getItem('TryIt_securitySchemeValues'))?.bearerAuth ?? ''
+
+  const validateToken = (openApiJson) => {
+    if(openApiJson.tags) {
+      setIsTokenValid(true)
+    } else {
+      setIsTokenValid(false)
+    }
+  }
 
   const getJson = async (token: string ) => {
     return await fetch(
       "http://localhost:3000/open-api",
       {headers: {Authorization: `Bearer ${token}`}}
-    ).then((res)=> res.json())
+    ).then((res)=> res.json()).then((result)=> {
+      validateToken(result)
+      return result
+    })
   }
 
   const submitToken = async (token) => {
     const json = await getJson(token)
-
     setOpenApiJson(json)
   }
+
   useEffect(()=> {
      (async ()=> {
-       const initialJson = await getJson('')
-
-       setOpenApiJson(initialJson)
+       await submitToken(storedToken)
     })()
   },[])
 
-  return (
+  return isTokenValid !== undefined && (
     <>
-      <TokenForm onSubmit={submitToken} />
-      <API
-        apiDescriptionDocument={JSON.stringify(openApiJson)}
-        router="hash"
-      />
+      <TokenForm onSubmit={submitToken} isTokenValid={isTokenValid} token={storedToken} />
+      {
+        isTokenValid && (
+        <API
+          apiDescriptionDocument={JSON.stringify(openApiJson)}
+          router="hash"
+        />
+        )
+      }
     </>
   )
 }
