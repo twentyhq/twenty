ec0747f1ca7ed37fec480c9c3309153976a7e47c
martmull <martmull@hotmail.fr>
2023-12-18 17:46:16 +0100

    Fix graphiql theme

--- packages/twenty-docs/src/pages/graphql.tsx
+++ packages/twenty-docs/src/pages/graphql.tsx
@@ -1,5 +1,6 @@
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
 import { GraphiQL } from "graphiql";
+import { useTheme, Theme } from "@graphiql/react";
 import React, { useState } from "react";
 import "graphiql/graphiql.css";
 import Layout from "@theme/Layout";
@@ -8,6 +9,16 @@ import Playground from "../components/playground";
 
 
 const graphQL = () => {
+  window.localStorage.setItem("graphiql:theme", window.localStorage.getItem("theme"));
+
+  const { setTheme } = useTheme();
+
+  addEventListener('storage', (ev)=> {
+    if(ev.key === 'theme') {
+      setTheme(ev.newValue as Theme);
+    }
+  })
+
   const [token , setToken] = useState()
 
   const fetcher = createGraphiQLFetcher({
