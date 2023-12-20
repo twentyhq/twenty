0a877055365ee7d84a6ab48e6f8d17e00abb3bba
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-12-20 16:21:17 +0100

    fix: delete test command

--- packages/twenty-server/src/workspace/workspace-sync-metadata/commands/workspace-sync-metadata-commands.module.ts
+++ packages/twenty-server/src/workspace/workspace-sync-metadata/commands/workspace-sync-metadata-commands.module.ts
@@ -4,10 +4,9 @@ import { DataSourceModule } from 'src/metadata/data-source/data-source.module';
 import { WorkspaceSyncMetadataModule } from 'src/workspace/workspace-sync-metadata/workspace-sync-metadata.module';
 
 import { SyncWorkspaceMetadataCommand } from './sync-workspace-metadata.command';
-import { TestWorkspaceMetadataCommand } from './test.command';
 
 @Module({
   imports: [WorkspaceSyncMetadataModule, DataSourceModule],
-  providers: [SyncWorkspaceMetadataCommand, TestWorkspaceMetadataCommand],
+  providers: [SyncWorkspaceMetadataCommand],
 })
 export class WorkspaceSyncMetadataCommandsModule {}
