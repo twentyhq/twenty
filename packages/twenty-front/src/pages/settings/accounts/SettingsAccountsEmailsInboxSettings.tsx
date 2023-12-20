5bbd1a7c495c0983526f573fbcb22f594c6e3687
Thaïs <guigon.thais@gmail.com>
2023-12-20 15:59:02 +0100

    feat: add Settings/Accounts/Emails/Inbox Settings synchronization sec… (#3071)
    * feat: add Settings/Accounts/Emails/Inbox Settings page
    
    Closes #3013
    
    * feat: add Settings/Accounts/Emails/Inbox Settings synchronization section
    
    Closes #3014
    
    ---------
    
    Co-authored-by: Lucas Bordeau <bordeau.lucas@gmail.com>

--- packages/twenty-front/src/pages/settings/accounts/SettingsAccountsEmailsInboxSettings.tsx
+++ packages/twenty-front/src/pages/settings/accounts/SettingsAccountsEmailsInboxSettings.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
+import { SettingsAccountsInboxSettingsSynchronizationSection } from '@/settings/accounts/components/SettingsAccountsInboxSettingsSynchronizationSection';
 import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
 import { AppPath } from '@/types/AppPath';
 import { IconSettings } from '@/ui/display/icon';
@@ -19,6 +20,10 @@ export const SettingsAccountsEmailsInboxSettings = () => {
     if (!account) navigate(AppPath.NotFound);
   }, [account, navigate]);
 
+  const handleToggle = (_value: boolean) => {};
+
+  if (!account) return <></>;
+
   return (
     <SubMenuTopBarContainer Icon={IconSettings} title="Settings">
       <SettingsPageContainer>
@@ -28,6 +33,10 @@ export const SettingsAccountsEmailsInboxSettings = () => {
             { children: 'Emails', href: '/settings/accounts/emails' },
             { children: account?.email || '' },
           ]}
+        />
+        <SettingsAccountsInboxSettingsSynchronizationSection
+          account={account}
+          onToggle={handleToggle}
         />
       </SettingsPageContainer>
     </SubMenuTopBarContainer>
