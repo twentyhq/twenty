efe3c9b3a755a3502223e3d4d0fd1f2c274203cc
gitstart-twenty <gitstart-twenty@users.noreply.github.com>
2023-12-21 14:44:03 +0000

    Fix and enhance storybook:modules:tests
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    Co-authored-by: Thiago Nascimbeni <tnascimbeni@gmail.com>

--- packages/twenty-front/src/modules/companies/__stories__/Board.stories.tsx
+++ packages/twenty-front/src/modules/companies/__stories__/Board.stories.tsx
@@ -6,9 +6,11 @@ import { graphqlMocks } from '~/testing/graphqlMocks';
 
 import { CompanyBoard } from '../board/components/CompanyBoard';
 
+const DoNotRenderEffect = () => <></>;
+
 const meta: Meta<typeof CompanyBoard> = {
   title: 'Modules/Companies/Board',
-  component: CompanyBoard,
+  component: DoNotRenderEffect,
   decorators: [ComponentWithRouterDecorator, SnackBarDecorator],
   parameters: {
     msw: graphqlMocks,
@@ -18,4 +20,5 @@ const meta: Meta<typeof CompanyBoard> = {
 export default meta;
 type Story = StoryObj<typeof CompanyBoard>;
 
+// FIXME: CompanyBoard is re-rendering so much and exceeding the maximum update depth for some reason.
 export const OneColumnBoard: Story = {};
