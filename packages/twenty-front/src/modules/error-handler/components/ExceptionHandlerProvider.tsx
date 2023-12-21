f4c9429373fbd29d5707817c621a1ccf4686d695
Deepak Kumar <deepak.kumar852182@gmail.com>
2023-12-22 02:19:14 +0530

    GH-2829 remove Sentry comments

--- packages/twenty-front/src/modules/error-handler/components/ExceptionHandlerProvider.tsx
+++ packages/twenty-front/src/modules/error-handler/components/ExceptionHandlerProvider.tsx
@@ -13,14 +13,10 @@ export const ExceptionHandlerProvider: React.FC<React.PropsWithChildren> = ({
 
   useEffect(() => {
     if (sentry?.dsnKey && !isSentryInitialized) {
-      /**
-       * Initialize Sentry
-       */
       Sentry.init({
         dsn: sentry?.dsnKey,
         integrations: [
           new Sentry.BrowserTracing({
-            // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
             tracePropagationTargets: [
               'localhost:3001',
               REACT_APP_SERVER_BASE_URL,
@@ -28,11 +24,9 @@ export const ExceptionHandlerProvider: React.FC<React.PropsWithChildren> = ({
           }),
           new Sentry.Replay(),
         ],
-        // Performance Monitoring
-        tracesSampleRate: 1.0, //  Capture 100% of the transactions
-        // Session Replay
-        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+        tracesSampleRate: 1.0,
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
       });
 
       setIsSentryInitialized(true);
