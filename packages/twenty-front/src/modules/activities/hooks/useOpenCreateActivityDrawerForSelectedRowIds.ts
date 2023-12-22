641297b6fa6755a443c057812f95de8c9b70a134
Deepak Kumar <deepak.kumar852182@gmail.com>
2023-12-22 23:53:23 +0530

    GH-3106 fix activity drawer opener for selected rows hook

--- packages/twenty-front/src/modules/activities/hooks/useOpenCreateActivityDrawerForSelectedRowIds.ts
+++ packages/twenty-front/src/modules/activities/hooks/useOpenCreateActivityDrawerForSelectedRowIds.ts
@@ -1,4 +1,4 @@
-import { useRecoilCallback } from 'recoil';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 
 import { ActivityType } from '@/activities/types/Activity';
 import { selectedRowIdsSelector } from '@/object-record/record-table/states/selectors/selectedRowIdsSelector';
@@ -13,18 +13,17 @@ import { useOpenCreateActivityDrawer } from './useOpenCreateActivityDrawer';
 export const useOpenCreateActivityDrawerForSelectedRowIds = () => {
   const openCreateActivityDrawer = useOpenCreateActivityDrawer();
 
+  const selectedRowIds = useRecoilValue(selectedRowIdsSelector);
+
   return useRecoilCallback(
-    ({ snapshot }) =>
+    () =>
       (
         type: ActivityType,
         entityType: ActivityTargetableEntityType,
         relatedEntities?: ActivityTargetableEntity[],
       ) => {
-        const selectedRowIds = Object.keys(
-          snapshot.getLoadable(selectedRowIdsSelector).getValue(),
-        );
         let activityTargetableEntityArray: ActivityTargetableEntity[] =
-          selectedRowIds.map((id) => ({
+          selectedRowIds.map((id: string) => ({
             type: entityType,
             id,
           }));
