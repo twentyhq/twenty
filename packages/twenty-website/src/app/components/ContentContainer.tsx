c422045ea62ea82303329c5e87492a909756c406
Félix Malfait <felix.malfait@gmail.com>
2023-12-29 11:17:32 +0100

    Marketing improvements 3 (#3175)
    * Improve marketing website
    
    * User guide with icons
    
    * Add TOC
    
    * Linter
    
    * Basic GraphQL playground
    
    * Very basic contributors page
    
    * Failed attempt to integrate REST playground
    
    * Yarn
    
    * Begin contributors DB
    
    * Improve contributors page

--- packages/twenty-website/src/app/components/ContentContainer.tsx
+++ packages/twenty-website/src/app/components/ContentContainer.tsx
@@ -1,18 +1,22 @@
-'use client'
+'use client';
 
-import styled from '@emotion/styled'
+import styled from '@emotion/styled';
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 600px;
-     @media(max-width: 809px) {
-        width: 100%;
-    }`;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0px 96px 0px 96px;
+  @media (max-width: 809px) {
+    width: 100%;
+    padding: 0px 12px 0px 12px;
+  }
+`;
 
-
-export const ContentContainer = ({children}: {children?: React.ReactNode}) => {
-  return (
-      <Container>{children}</Container>
-  )
-}
+export const ContentContainer = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}) => {
+  return <Container>{children}</Container>;
+};
--- packages/twenty-website/src/app/emotion-root-style-registry.js
+++ packages/twenty-website/src/app/emotion-root-style-registry.js
@@ -1,37 +1,37 @@
-'use client'
+'use client';
 
-import { CacheProvider } from '@emotion/react'
-import createCache from '@emotion/cache'
-import { useServerInsertedHTML } from 'next/navigation'
-import { useState } from 'react'
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { useServerInsertedHTML } from 'next/navigation';
+import { useState } from 'react';
 
 export default function RootStyleRegistry({ children }) {
   const [{ cache, flush }] = useState(() => {
-    const cache = createCache({ key: 'emotion-cache' })
-    cache.compat = true
-    const prevInsert = cache.insert
-    let inserted = []
+    const cache = createCache({ key: 'emotion-cache' });
+    cache.compat = true;
+    const prevInsert = cache.insert;
+    let inserted = [];
     cache.insert = (...args) => {
-      const serialized = args[1]
+      const serialized = args[1];
       if (cache.inserted[serialized.name] === undefined) {
-        inserted.push(serialized.name)
+        inserted.push(serialized.name);
       }
-      return prevInsert(...args)
-    }
+      return prevInsert(...args);
+    };
     const flush = () => {
-      const prevInserted = inserted
-      inserted = []
-      return prevInserted
-    }
-    return { cache, flush }
-  })
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+    return { cache, flush };
+  });
 
   useServerInsertedHTML(() => {
-    const names = flush()
-    if (names.length === 0) return null
-    let styles = ''
+    const names = flush();
+    if (names.length === 0) return null;
+    let styles = '';
     for (const name of names) {
-      styles += cache.inserted[name]
+      styles += cache.inserted[name];
     }
     return (
       <style
@@ -40,8 +40,8 @@ export default function RootStyleRegistry({ children }) {
           __html: styles,
         }}
       />
-    )
-  })
+    );
+  });
 
-  return <CacheProvider value={cache}>{children}</CacheProvider>
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
 }
