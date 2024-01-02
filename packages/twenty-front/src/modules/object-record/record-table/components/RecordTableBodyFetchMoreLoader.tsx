aeeb9c59fa399784f868e69e9fe143044c66e537
Suman Sahoo <ssamsomu@gmail.com>
2024-01-02 16:44:10 +0530

    style update

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableBodyFetchMoreLoader.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableBodyFetchMoreLoader.tsx
@@ -1,4 +1,5 @@
 import { useInView } from 'react-intersection-observer';
+import styled from '@emotion/styled';
 import { useRecoilCallback, useRecoilValue } from 'recoil';
 
 import { useObjectRecordTable } from '@/object-record/hooks/useObjectRecordTable';
@@ -6,6 +7,7 @@ import { StyledRow } from '@/object-record/record-table/components/RecordTableRo
 import { useRecordTable } from '@/object-record/record-table/hooks/useRecordTable';
 import { getRecordTableScopedStates } from '@/object-record/record-table/utils/getRecordTableScopedStates';
 import { isFetchingMoreRecordsFamilyState } from '@/object-record/states/isFetchingMoreRecordsFamilyState';
+import { grayScale } from '@/ui/theme/constants/colors';
 
 export const RecordTableBodyFetchMoreLoader = () => {
   const { queryStateIdentifier } = useObjectRecordTable();
@@ -30,11 +32,24 @@ export const RecordTableBodyFetchMoreLoader = () => {
     onChange: onLastRowVisible,
   });
 
+  const StyledTransparentBox = styled.div`
+    height: 32px;
+    width: 32px;
+  `;
+
   return (
     <tbody ref={tbodyRef}>
       {isFetchingMoreObjects ? (
         <StyledRow selected={false}>
-          <td style={{ height: 50 }} colSpan={1000}>
+          <StyledTransparentBox />
+          <td
+            style={{
+              color: grayScale.gray40,
+              height: 32,
+              paddingLeft: 8,
+            }}
+            colSpan={1000}
+          >
             Loading more...
           </td>
         </StyledRow>
