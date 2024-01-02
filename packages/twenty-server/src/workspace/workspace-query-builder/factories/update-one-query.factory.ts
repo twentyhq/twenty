585efb157c2586d3e9103c2ecd708818ae25f665
aditya pimpalkar <adipimpalkar@gmail.com>
2024-01-02 22:24:23 +0000

    fix: updatedAt param

--- packages/twenty-server/src/workspace/workspace-query-builder/factories/update-one-query.factory.ts
+++ packages/twenty-server/src/workspace/workspace-query-builder/factories/update-one-query.factory.ts
@@ -31,13 +31,16 @@ export class UpdateOneQueryFactory {
       options.fieldMetadataCollection,
     );
 
+    const argsData = {
+      ...computedArgs.data, 
+      updatedAt: new Date().toISOString()
+    }
+
     return `
       mutation {
         update${
           options.targetTableName
-        }Collection(set: ${stringifyWithoutKeyQuote(
-      computedArgs.data,
-    )}, filter: { id: { eq: "${computedArgs.id}" } }) {
+        }Collection(set: ${stringifyWithoutKeyQuote(argsData)}, filter: { id: { eq: "${computedArgs.id}" } }) {
           affectedCount
           records {
             ${fieldsString}
