72e3e4de1fc5b77251a233d84a5dd2b883eb939a
martmull <martmull@hotmail.fr>
2024-01-03 09:33:48 +0100

    Fix paths and commands for render deploy

--- packages/twenty-server/scripts/render-run.sh
+++ packages/twenty-server/scripts/render-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PG_DATABASE_URL=postgres://twenty:twenty@$PG_DATABASE_HOST:$PG_DATABASE_PORT/default
-yarn database:setup
+yarn nx run twenty-server:database:setup
 node dist/src/main
