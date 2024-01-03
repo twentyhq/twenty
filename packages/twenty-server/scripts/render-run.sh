d79aca6aaa1763290973630df20e56a959ea6b60
martmull <martmull@hotmail.fr>
2024-01-03 09:56:20 +0100

    Remove breaking change

--- packages/twenty-server/scripts/render-run.sh
+++ packages/twenty-server/scripts/render-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PG_DATABASE_URL=postgres://twenty:twenty@$PG_DATABASE_HOST:$PG_DATABASE_PORT/default
-yarn nx run twenty-server:database:setup
+yarn database:setup
 node dist/src/main
