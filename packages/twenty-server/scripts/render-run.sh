912026a257ce146555e7b796c995b3d71325dcb2
martmull <martmull@hotmail.fr>
2024-01-03 11:43:30 +0100

    Fix render script

--- packages/twenty-server/scripts/render-run.sh
+++ packages/twenty-server/scripts/render-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PG_DATABASE_URL=postgres://twenty:twenty@$PG_DATABASE_HOST:$PG_DATABASE_PORT/default
-yarn database:setup
+npx ts-node ./scripts/setup-db.ts && yarn typeorm:migrate && yarn command workspace:seed:dev
 node dist/src/main
