ea2cb8938f7948ec5ce3b9319b65afa03ea611ec
Weiko <corentin@twenty.com>
2024-01-08 18:24:39 +0100

    Add fetch connected account job (#3313)
    * Add fetch connected account job
    
    * add featureFlag check

--- packages/twenty-server/src/workspace/messaging/commands/fetch-workspace-messages-commands.module.ts
+++ packages/twenty-server/src/workspace/messaging/commands/fetch-workspace-messages-commands.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { FeatureFlagEntity } from 'src/core/feature-flag/feature-flag.entity';
+import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
+import { DataSourceModule } from 'src/metadata/data-source/data-source.module';
 import { FetchWorkspaceMessagesCommand } from 'src/workspace/messaging/commands/fetch-workspace-messages.command';
 import { MessagingModule } from 'src/workspace/messaging/messaging.module';
-import { FetchWorkspaceMessagesModule } from 'src/workspace/messaging/services/fetch-workspace-messages.module';
 
 @Module({
-  imports: [MessagingModule, FetchWorkspaceMessagesModule],
+  imports: [
+    MessagingModule,
+    DataSourceModule,
+    TypeORMModule,
+    TypeOrmModule.forFeature([FeatureFlagEntity], 'core'),
+  ],
   providers: [FetchWorkspaceMessagesCommand],
 })
 export class FetchWorkspaceMessagesCommandsModule {}
