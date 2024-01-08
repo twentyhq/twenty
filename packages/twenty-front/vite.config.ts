f0c5824235a67ff6372737d8a0c430a869f70981
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-01-08 11:48:52 +0100

    Added vite-plugin-checker to twenty-front

--- packages/twenty-front/vite.config.ts
+++ packages/twenty-front/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,6 +19,11 @@ export default defineConfig(({ mode }) => {
     react({ jsxImportSource: "@emotion/react" }),
     tsconfigPaths(),
     svgr(),
+    checker({
+      typescript: {
+        tsconfigPath: "tsconfig.app.json"
+      }
+    }),
   ]
 
   return {
