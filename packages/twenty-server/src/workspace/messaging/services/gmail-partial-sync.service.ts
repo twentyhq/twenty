ce447c46c5ad3e799e8c34c72c3c96f5ab09739e
bosiraphael <raphael.bosi@gmail.com>
2024-01-09 15:22:45 +0100

    getLastSyncHistoryId

--- packages/twenty-server/src/workspace/messaging/services/gmail-partial-sync.service.ts
+++ packages/twenty-server/src/workspace/messaging/services/gmail-partial-sync.service.ts
@@ -1,16 +1,43 @@
 import { Injectable } from '@nestjs/common';
 
-import { TypeORMService } from 'src/database/typeorm/typeorm.service';
-import { DataSourceService } from 'src/metadata/data-source/data-source.service';
 import { FetchBatchMessagesService } from 'src/workspace/messaging/services/fetch-batch-messages.service';
 import { GmailClientProvider } from 'src/workspace/messaging/providers/gmail/gmail-client.provider';
+import { Utils } from 'src/workspace/messaging/services/utils.service';
+import { MessagingProducer } from 'src/workspace/messaging/producers/messaging-producer';
 
 @Injectable()
 export class GmailPartialSync {
   constructor(
     private readonly gmailClientProvider: GmailClientProvider,
-    private readonly dataSourceService: DataSourceService,
-    private readonly typeORMService: TypeORMService,
     private readonly fetchBatchMessagesService: FetchBatchMessagesService,
+    private readonly utils: Utils,
+    private readonly messagingProducer: MessagingProducer,
   ) {}
+
+  private async getLastSyncHistoryId(
+    workspaceId: string,
+    connectedAccountId: string,
+  ): Promise<string> {
+    const { connectedAccount } =
+      await this.utils.getDataSourceMetadataWorkspaceMetadataAndConnectedAccount(
+        workspaceId,
+        connectedAccountId,
+      );
+
+    if (!connectedAccount) {
+      throw new Error('No connected account found');
+    }
+
+    const lastSyncHistoryId = connectedAccount.lastSyncHistoryId;
+
+    if (!lastSyncHistoryId) {
+      // Fall back to full sync
+      await this.messagingProducer.enqueueFetchAllMessagesFromConnectedAccount(
+        { workspaceId, connectedAccountId: connectedAccountId },
+        `${workspaceId}-${connectedAccount.id}`,
+      );
+    }
+
+    return lastSyncHistoryId;
+  }
 }
