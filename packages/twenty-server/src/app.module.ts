a72e6843ff08b168c1efab1cc7d2a83219f6686b
corentin <corentin@twenty.com>
2024-01-09 17:12:33 +0100

    Fix capture exception for metadata and core

--- packages/twenty-server/src/app.module.ts
+++ packages/twenty-server/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
 
 import { YogaDriver, YogaDriverConfig } from '@graphql-yoga/nestjs';
 
 import { GraphQLConfigService } from 'src/graphql-config.service';
-import { GlobalExceptionFilter } from 'src/filters/global-exception.filter';
-
-import { AppService } from './app.service';
 
 import { CoreModule } from './core/core.module';
 import { IntegrationsModule } from './integrations/integrations.module';
@@ -29,13 +25,6 @@ import { WorkspaceModule } from './workspace/workspace.module';
     IntegrationsModule,
     CoreModule,
     WorkspaceModule,
-  ],
-  providers: [
-    AppService,
-    {
-      provide: APP_FILTER,
-      useValue: GlobalExceptionFilter,
-    },
   ],
 })
 export class AppModule {}
