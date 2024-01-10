818c97314ecd163c060de04e4d07b7598ac50eb2
Charles Bochet <charles@twenty.com>
2024-01-10 17:07:28 +0100

    Fix

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableColumnDropdownMenu.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableColumnDropdownMenu.tsx
@@ -1,5 +1,4 @@
 import { FieldMetadata } from '@/object-record/field/types/FieldMetadata';
-import { BoardOptionsDropdownId } from '@/object-record/record-board/constants/BoardOptionsDropdownId';
 import { IconArrowLeft, IconArrowRight, IconEyeOff } from '@/ui/display/icon';
 import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/DropdownMenuItemsContainer';
 import { useDropdown } from '@/ui/layout/dropdown/hooks/useDropdown';
@@ -24,7 +23,7 @@ export const RecordTableColumnDropdownMenu = ({
   const { handleColumnVisibilityChange, handleMoveTableColumn } =
     useTableColumns();
 
-  const { closeDropdown } = useDropdown(BoardOptionsDropdownId);
+  const { closeDropdown } = useDropdown(column.fieldMetadataId + '-header');
 
   const handleColumnMoveLeft = () => {
     closeDropdown();
