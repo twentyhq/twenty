6f46a42ff5fc5d2e79e2685930b5e36f6376ab4b
Jérémy Magrin <jeremy.magrin@gmail.com>
2024-01-12 08:45:05 +0100

    fix: auth user decorator cannot destruct property of undefined

--- packages/twenty-server/src/core/analytics/analytics.resolver.ts
+++ packages/twenty-server/src/core/analytics/analytics.resolver.ts
@@ -21,7 +21,7 @@ export class AnalyticsResolver {
   createEvent(
     @Args() createEventInput: CreateAnalyticsInput,
     @AuthWorkspace() workspace: Workspace | undefined,
-    @AuthUser() user: User | undefined,
+    @AuthUser({ required: false }) user: User | undefined,
   ) {
     return this.analyticsService.create(createEventInput, user, workspace);
   }
--- packages/twenty-server/src/decorators/auth-user.decorator.ts
+++ packages/twenty-server/src/decorators/auth-user.decorator.ts
@@ -1,10 +1,22 @@
-import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import {
+  ExecutionContext,
+  ForbiddenException,
+  createParamDecorator,
+} from '@nestjs/common';
 
 import { getRequest } from 'src/utils/extract-request';
 
+interface DecoratorOptions {
+  required?: boolean;
+}
+
 export const AuthUser = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext) => {
+  (options: DecoratorOptions = { required: true }, ctx: ExecutionContext) => {
     const request = getRequest(ctx);
+
+    if (options.required && (!request.user || !request.user.user)) {
+      throw new ForbiddenException("You're not authorized to do this");
+    }
 
     return request.user ? request.user.user : undefined;
   },
