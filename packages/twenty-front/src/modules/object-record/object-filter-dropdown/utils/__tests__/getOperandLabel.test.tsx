03b5c40e8a658137019da873f2d190473e788fbc
gitstart-twenty <140154534+gitstart-twenty@users.noreply.github.com>
2024-01-12 18:08:01 +0100

    Add tests for `modules/object-record/object-filter-dropdown` (#3399)
    * Add tests for `modules/object-record/object-filter-dropdown`
    
    Co-authored-by: KlingerMatheus <klinger.matheus@gitstart.dev>
    
    * Fix linter
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    Co-authored-by: KlingerMatheus <klinger.matheus@gitstart.dev>
    
    * Revert unwanted changes
    
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    Co-authored-by: KlingerMatheus <klinger.matheus@gitstart.dev>
    
    ---------
    
    Co-authored-by: gitstart-twenty <gitstart-twenty@users.noreply.github.com>
    Co-authored-by: KlingerMatheus <klinger.matheus@gitstart.dev>
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>

---
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/utils/__tests__/getOperandLabel.test.tsx
@@ -0,0 +1,48 @@
+import { ViewFilterOperand } from '@/views/types/ViewFilterOperand';
+import { capitalize } from '~/utils/string/capitalize';
+
+import { getOperandLabel, getOperandLabelShort } from '../getOperandLabel';
+
+describe('getOperandLabel', () => {
+  const testCases = [
+    [ViewFilterOperand.Contains, 'Contains'],
+    [ViewFilterOperand.DoesNotContain, "Doesn't contain"],
+    [ViewFilterOperand.GreaterThan, 'Greater than'],
+    [ViewFilterOperand.LessThan, 'Less than'],
+    [ViewFilterOperand.Is, 'Is'],
+    [ViewFilterOperand.IsNot, 'Is not'],
+    [ViewFilterOperand.IsNotNull, 'Is not null'],
+    [undefined, ''], // undefined operand
+  ];
+
+  testCases.forEach(([operand, expectedLabel]) => {
+    const formattedOperand = capitalize(operand || 'undefined');
+
+    it(`should return correct label for ViewFilterOperand.${formattedOperand}`, () => {
+      const result = getOperandLabel(operand as ViewFilterOperand);
+      expect(result).toBe(expectedLabel);
+    });
+  });
+});
+
+describe('getOperandLabelShort', () => {
+  const testCases = [
+    [ViewFilterOperand.Is, ': '],
+    [ViewFilterOperand.Contains, ': '],
+    [ViewFilterOperand.IsNot, ': Not'],
+    [ViewFilterOperand.DoesNotContain, ': Not'],
+    [ViewFilterOperand.IsNotNull, ': NotNull'],
+    [ViewFilterOperand.GreaterThan, '\u00A0> '],
+    [ViewFilterOperand.LessThan, '\u00A0< '],
+    [undefined, ': '], // undefined operand
+  ];
+
+  testCases.forEach(([operand, expectedLabel]) => {
+    const formattedOperand = capitalize(operand || 'undefined');
+
+    it(`should return correct short label for ViewFilterOperand.${formattedOperand}`, () => {
+      const result = getOperandLabelShort(operand as ViewFilterOperand);
+      expect(result).toBe(expectedLabel);
+    });
+  });
+});
