c7433f36567f9c7bbf78aebabe5977b12ea8aa72
Charles Bochet <charles@twenty.com>
2024-01-13 13:56:20 +0100

    Stop switching to a different datasource per workspace

--- packages/twenty-server/src/workspace/workspace-migration-runner/workspace-migration-runner.service.ts
+++ packages/twenty-server/src/workspace/workspace-migration-runner/workspace-migration-runner.service.ts
@@ -298,6 +298,7 @@ export class WorkspaceMigrationRunnerService {
         columnNames: [migrationColumn.columnName],
         referencedColumnNames: [migrationColumn.referencedTableColumnName],
         referencedTableName: migrationColumn.referencedTableName,
+        referencedSchema: schemaName,
         onDelete: 'CASCADE',
       }),
     );
