03049f5f2c019a9df70c891979d45191f30679b1
martmull <martmull@hotmail.fr>
2024-01-15 08:58:36 +0100

    Merge creates

--- packages/twenty-zapier/src/utils/handleQueryParams.ts
+++ packages/twenty-zapier/src/utils/handleQueryParams.ts
@@ -1,5 +1,7 @@
-const handleQueryParams = (inputData: { [x: string]: any }): string => {
-  const formattedInputData: { [x: string]: any } = {};
+import { InputData } from '../utils/inputData.type';
+
+const handleQueryParams = (inputData: InputData): string => {
+  const formattedInputData: InputData = {};
   Object.keys(inputData).forEach((key) => {
     if (key.includes('__')) {
       const [objectKey, nestedObjectKey] = key.split('__');
---
+++ packages/twenty-zapier/src/utils/inputData.type.ts
@@ -0,0 +1,1 @@
+export type InputData = { [x: string]: any };
