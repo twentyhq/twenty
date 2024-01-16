6a0347afeece586d42b2db60d776b6b6551f6f1d
Thaïs Guigon <guigon.thais@gmail.com>
2024-01-16 13:55:29 -0300

    perf: get record from cache before fetching 'findOneRecord'

--- packages/twenty-front/src/modules/object-record/hooks/useFindOneRecord.ts
+++ packages/twenty-front/src/modules/object-record/hooks/useFindOneRecord.ts
@@ -1,11 +1,12 @@
+import { useMemo } from 'react';
 import { useQuery } from '@apollo/client';
 
 import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
 import { ObjectMetadataItemIdentifier } from '@/object-metadata/types/ObjectMetadataItemIdentifier';
+import { useGetRecordFromCache } from '@/object-record/hooks/useGetRecordFromCache';
+import { ObjectRecord } from '@/object-record/types/ObjectRecord';
 
-export const useFindOneRecord = <
-  ObjectType extends { id: string } & Record<string, any>,
->({
+export const useFindOneRecord = <ObjectType extends ObjectRecord>({
   objectNameSingular,
   objectRecordId = '',
   onCompleted,
@@ -22,17 +23,25 @@ export const useFindOneRecord = <
     depth,
   );
 
+  const getRecordFromCache = useGetRecordFromCache<ObjectType>({
+    objectMetadataItem,
+  });
+  const recordFromCache = useMemo(
+    () => getRecordFromCache(objectRecordId),
+    [getRecordFromCache, objectRecordId],
+  );
+
   const { data, loading, error } = useQuery<
     { [nameSingular: string]: ObjectType },
     { objectRecordId: string }
   >(findOneRecordQuery, {
-    skip: !objectMetadataItem || !objectRecordId || skip,
+    skip: !objectMetadataItem || !objectRecordId || !!recordFromCache || skip,
     variables: { objectRecordId },
     onCompleted: (data) => onCompleted?.(data[objectNameSingular]),
   });
 
   return {
-    record: data?.[objectNameSingular] || undefined,
+    record: recordFromCache || data?.[objectNameSingular] || undefined,
     loading,
     error,
   };
