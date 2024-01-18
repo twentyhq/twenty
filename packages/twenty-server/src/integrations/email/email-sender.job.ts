768ea1bae6cab0216577b36cb5751c74c3a7c8ea
martmull <martmull@hotmail.fr>
2024-01-18 13:52:53 +0100

    Update logging for smtp emails

--- packages/twenty-server/src/integrations/email/email-sender.job.ts
+++ packages/twenty-server/src/integrations/email/email-sender.job.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 import { SendMailOptions } from 'nodemailer';
 
@@ -8,11 +8,9 @@ import { EmailSenderService } from 'src/integrations/email/email-sender.service'
 
 @Injectable()
 export class EmailSenderJob implements MessageQueueJob<SendMailOptions> {
-  private readonly logger = new Logger(EmailSenderJob.name);
   constructor(private readonly emailSenderService: EmailSenderService) {}
 
   async handle(data: SendMailOptions): Promise<void> {
     await this.emailSenderService.send(data);
-    this.logger.log(`Email to ${data.to} sent`);
   }
 }
