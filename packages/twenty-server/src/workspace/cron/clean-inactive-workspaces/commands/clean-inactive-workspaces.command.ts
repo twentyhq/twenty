b991790f629ab9a3b67724ca390a13ee73f4d776
martmull <martmull@hotmail.fr>
2024-01-24 12:51:42 +0100

    Update clean inactive workspaces (#3600)
    * Fix typo
    
    * Add dry-run option in clean inactive workspaces
    
    * Add logs
    
    * Chunk workspace metadata
    
    * Add BCC to clean workspace notification email
    
    * Send workspace to delete ids in one email
    
    * Update example
    
    * Update function naming

--- packages/twenty-server/src/workspace/cron/clean-inactive-workspaces/commands/clean-inactive-workspaces.command.ts
+++ packages/twenty-server/src/workspace/cron/clean-inactive-workspaces/commands/clean-inactive-workspaces.command.ts
@@ -1,10 +1,14 @@
 import { Inject } from '@nestjs/common';
 
-import { Command, CommandRunner } from 'nest-commander';
+import { Command, CommandRunner, Option } from 'nest-commander';
 
 import { MessageQueue } from 'src/integrations/message-queue/message-queue.constants';
 import { MessageQueueService } from 'src/integrations/message-queue/services/message-queue.service';
 import { CleanInactiveWorkspaceJob } from 'src/workspace/cron/clean-inactive-workspaces/clean-inactive-workspace.job';
+
+export type CleanInactiveWorkspacesCommandOptions = {
+  dryRun: boolean;
+};
 
 @Command({
   name: 'clean-inactive-workspaces',
@@ -18,10 +22,22 @@ export class CleanInactiveWorkspacesCommand extends CommandRunner {
     super();
   }
 
-  async run(): Promise<void> {
-    await this.messageQueueService.add<any>(
+  @Option({
+    flags: '-d, --dry-run [dry run]',
+    description: 'Dry run: Log cleaning actions without executing them.',
+    required: false,
+  })
+  dryRun(value: string): boolean {
+    return Boolean(value);
+  }
+
+  async run(
+    _passedParam: string[],
+    options: CleanInactiveWorkspacesCommandOptions,
+  ): Promise<void> {
+    await this.messageQueueService.add<CleanInactiveWorkspacesCommandOptions>(
       CleanInactiveWorkspaceJob.name,
-      {},
+      options,
       { retryLimit: 3 },
     );
   }
