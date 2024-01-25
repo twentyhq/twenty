c575e58f2d6b612ad29b53946f0a4fa27d418462
bosiraphael <raphael.bosi@gmail.com>
2024-01-25 15:30:45 +0100

    remove mock

--- packages/twenty-front/src/testing/mock-data/activities.ts
+++ packages/twenty-front/src/testing/mock-data/activities.ts
@@ -210,20 +210,3 @@ export const mockedActivities: Array<MockedActivity> = [
     __typename: 'Activity',
   },
 ];
-
-// export const mockedEmailThreads: TimelineThread[] = [
-//   {
-//     lastMessageBody:
-//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dignissim nisi eu tellus dapibus, egestas placerat risus placerat. Praesent eget arcu consectetur, efficitur felis.',
-//     numberOfMessagesInThread: 3,
-//     read: true,
-//     lastMessageReceivedAt: new Date('11/04/2023').toISOString(),
-//     firstParticipant: {
-//       id: '1',
-//       handle: 'steve@gmail.com',
-//     },
-//     subject: 'Next step',
-//     lastTwoParticipants: [],
-//     participantCount: 2,
-//   },
-// ];
