27259ace889b51ab447ceb86c6fff604f1e71c22
Charles Bochet <charles@twenty.com>
2024-01-28 23:15:32 +0100

    Fix

--- packages/twenty-front/src/modules/activities/inline-cell/components/ActivityTargetsInlineCell.tsx
+++ packages/twenty-front/src/modules/activities/inline-cell/components/ActivityTargetsInlineCell.tsx
@@ -3,6 +3,8 @@ import { useActivityTargetObjectRecords } from '@/activities/hooks/useActivityTa
 import { ActivityTargetInlineCellEditMode } from '@/activities/inline-cell/components/ActivityTargetInlineCellEditMode';
 import { ActivityTarget } from '@/activities/types/ActivityTarget';
 import { GraphQLActivity } from '@/activities/types/GraphQLActivity';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { useFieldContext } from '@/object-record/hooks/useFieldContext';
 import { RecordFieldInputScope } from '@/object-record/record-field/scopes/RecordFieldInputScope';
 import { RecordInlineCellContainer } from '@/object-record/record-inline-cell/components/RecordInlineCellContainer';
 import { RelationPickerHotkeyScope } from '@/object-record/relation-picker/types/RelationPickerHotkeyScope';
@@ -25,28 +27,39 @@ export const ActivityTargetsInlineCell = ({
     activityId: activity?.id ?? '',
   });
 
+  const { FieldContextProvider } = useFieldContext({
+    objectNameSingular: CoreObjectNameSingular.Activity,
+    objectRecordId: activity?.id ?? '',
+    fieldMetadataName: 'activityTargets',
+    fieldPosition: 2,
+  });
+
+  if (!FieldContextProvider) return null;
+
   return (
     <RecordFieldInputScope recordFieldInputScopeId={activity?.id ?? ''}>
-      <RecordInlineCellContainer
-        buttonIcon={IconPencil}
-        customEditHotkeyScope={{
-          scope: RelationPickerHotkeyScope.RelationPicker,
-        }}
-        IconLabel={IconArrowUpRight}
-        editModeContent={
-          <ActivityTargetInlineCellEditMode
-            activityId={activity?.id ?? ''}
-            activityTargetObjectRecords={activityTargetObjectRecords as any}
-          />
-        }
-        label="Relations"
-        displayModeContent={
-          <ActivityTargetChips
-            activityTargetObjectRecords={activityTargetObjectRecords}
-          />
-        }
-        isDisplayModeContentEmpty={activityTargetObjectRecords.length === 0}
-      />
+      <FieldContextProvider>
+        <RecordInlineCellContainer
+          buttonIcon={IconPencil}
+          customEditHotkeyScope={{
+            scope: RelationPickerHotkeyScope.RelationPicker,
+          }}
+          IconLabel={IconArrowUpRight}
+          editModeContent={
+            <ActivityTargetInlineCellEditMode
+              activityId={activity?.id ?? ''}
+              activityTargetObjectRecords={activityTargetObjectRecords as any}
+            />
+          }
+          label="Relations"
+          displayModeContent={
+            <ActivityTargetChips
+              activityTargetObjectRecords={activityTargetObjectRecords}
+            />
+          }
+          isDisplayModeContentEmpty={activityTargetObjectRecords.length === 0}
+        />
+      </FieldContextProvider>
     </RecordFieldInputScope>
   );
 };
