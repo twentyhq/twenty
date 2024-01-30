03e5c792f0290acfb37954079b3afc7e8cda4fc7
Weiko <corentin@twenty.com>
2024-01-30 19:35:41 +0100

    [messaging] fix message-channel-message-association field name as dependencies (#3712)

--- packages/twenty-server/src/workspace/workspace-sync-metadata/types/object-record.ts
+++ packages/twenty-server/src/workspace/workspace-sync-metadata/types/object-record.ts
@@ -1,6 +1,5 @@
 import { BaseObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/base.object-metadata';
 
-// Note: This is actually not enterely correct, id field should only be added if the relation is MANY_TO_ONE or ONE_TO_ONE
 export type ObjectRecord<T extends BaseObjectMetadata> = {
   [K in keyof T as T[K] extends BaseObjectMetadata
     ? `${Extract<K, string>}Id`
