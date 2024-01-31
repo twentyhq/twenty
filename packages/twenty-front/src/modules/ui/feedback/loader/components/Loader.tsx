cbca10e5e45ef2eca0304dc13fed2c2f9a23323f
bosiraphael <raphael.bosi@gmail.com>
2024-01-31 15:46:35 +0100

    wip

--- packages/twenty-front/src/modules/ui/feedback/loader/components/Loader.tsx
+++ packages/twenty-front/src/modules/ui/feedback/loader/components/Loader.tsx
@@ -1,13 +1,38 @@
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
 
 const StyledLoaderContainer = styled.div`
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
   display: flex;
   gap: ${({ theme }) => theme.spacing(2)};
   width: ${({ theme }) => theme.spacing(6)};
   height: ${({ theme }) => theme.spacing(3)};
   border-radius: ${({ theme }) => theme.border.radius.pill};
   border: 1px solid ${({ theme }) => theme.border.color.strong};
+  overflow: hidden;
 `;
 
-export const Loader = () => <StyledLoaderContainer></StyledLoaderContainer>;
+const StyledLoader = styled(motion.div)`
+  background-color: ${({ theme }) => theme.font.color.light};
+  border-radius: ${({ theme }) => theme.border.radius.pill};
+  height: 8px;
+  width: 8px;
+`;
+
+export const Loader = () => (
+  <StyledLoaderContainer>
+    <StyledLoader
+      animate={{
+        x: [-16, 0, 16],
+        width: [8, 12, 8],
+        height: [8, 2, 8],
+      }}
+      transition={{
+        duration: 1,
+        times: [0, 0.3, 0.6],
+        repeat: Infinity,
+      }}
+    />
+  </StyledLoaderContainer>
+);
