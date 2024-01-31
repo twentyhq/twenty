4290db3566d30b82ca517759f1dfcac829291930
Charles Bochet <charlesBochet@users.noreply.github.com>
2024-01-31 17:21:17 +0100

    Fix new board bugs (#3730)
    * Fix freeze on field visibility change on board
    
    * Fix
    
    * Fix lint

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableRow.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableRow.tsx
@@ -18,10 +18,6 @@ type RecordTableRowProps = {
   rowIndex: number;
 };
 
-const StyledPlaceholder = styled.td`
-  height: 30px;
-`;
-
 const StyledTd = styled.td`
   background-color: ${({ theme }) => theme.background.primary};
 `;
@@ -56,31 +52,27 @@ export const RecordTableRow = ({ recordId, rowIndex }: RecordTableRowProps) => {
         data-testid={`row-id-${recordId}`}
         data-selectable-id={recordId}
       >
-        {inView ? (
-          <>
-            <StyledTd>
-              <CheckboxCell />
-            </StyledTd>
-            {[...visibleTableColumns]
-              .sort((columnA, columnB) => columnA.position - columnB.position)
-              .map((column, columnIndex) => {
-                return (
-                  <RecordTableCellContext.Provider
-                    value={{
-                      columnDefinition: column,
-                      columnIndex,
-                    }}
-                    key={column.fieldMetadataId}
-                  >
-                    <RecordTableCellContainer />
-                  </RecordTableCellContext.Provider>
-                );
-              })}
-            <td></td>
-          </>
-        ) : (
-          <StyledPlaceholder />
-        )}
+        <StyledTd>
+          <CheckboxCell />
+        </StyledTd>
+        {[...visibleTableColumns]
+          .sort((columnA, columnB) => columnA.position - columnB.position)
+          .map((column, columnIndex) => {
+            return inView ? (
+              <RecordTableCellContext.Provider
+                value={{
+                  columnDefinition: column,
+                  columnIndex,
+                }}
+                key={column.fieldMetadataId}
+              >
+                <RecordTableCellContainer />
+              </RecordTableCellContext.Provider>
+            ) : (
+              <td></td>
+            );
+          })}
+        <td></td>
       </tr>
     </RecordTableRowContext.Provider>
   );
