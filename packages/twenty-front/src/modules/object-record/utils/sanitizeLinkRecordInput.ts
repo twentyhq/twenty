170eed7be215f94309e83c2eea9d7c19d91cc129
Thaïs Guigon <guigon.thais@gmail.com>
2024-02-06 13:06:15 -0300

    fix: fix useSpreadsheetPersonImport tests

--- packages/twenty-front/src/modules/object-record/utils/sanitizeLinkRecordInput.ts
+++ packages/twenty-front/src/modules/object-record/utils/sanitizeLinkRecordInput.ts
@@ -1,7 +1,15 @@
+import { z } from 'zod';
+
 export const sanitizeLink = (url: string) => {
   const hostname = getUrlHostName(url) || getUrlHostName(`https://${url}`);
   return hostname.replace(/\/+$/, '').replace(/\?.*$/, '');
 };
 
-const getUrlHostName = (url: string) =>
-  URL.canParse(url) ? new URL(url).hostname.replace(/^www\./i, '') : '';
+const getUrlHostName = (url: string) => {
+  const urlSchema = z.string().url();
+  const validation = urlSchema.safeParse(url);
+
+  return validation.success
+    ? new URL(validation.data).hostname.replace(/^www\./i, '')
+    : '';
+};
