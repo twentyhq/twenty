c03e1b4dee2399b13e6c1a610bb12d7f1c55f6a6
bosiraphael <raphael.bosi@gmail.com>
2024-02-06 15:38:30 +0100

    update dto

--- packages/twenty-server/src/core/messaging/dtos/timeline-thread.dto.ts
+++ packages/twenty-server/src/core/messaging/dtos/timeline-thread.dto.ts
@@ -3,6 +3,8 @@ import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { IDField } from '@ptc-org/nestjs-query-graphql';
 
 import { TimelineThreadParticipant } from 'src/core/messaging/dtos/timeline-thread-participant.dto';
+import { FieldMetadataType } from 'src/metadata/field-metadata/field-metadata.entity';
+import { FieldMetadata } from 'src/workspace/workspace-sync-metadata/decorators/field-metadata.decorator';
 
 @ObjectType('TimelineThread')
 export class TimelineThread {
@@ -11,6 +13,25 @@ export class TimelineThread {
 
   @Field()
   read: boolean;
+
+  @FieldMetadata({
+    type: FieldMetadataType.SELECT,
+    label: 'Visibility',
+    description: 'Visibility',
+    icon: 'IconEyeglass',
+    options: [
+      { value: 'metadata', label: 'Metadata', position: 0, color: 'green' },
+      { value: 'subject', label: 'Subject', position: 1, color: 'blue' },
+      {
+        value: 'share_everything',
+        label: 'Share Everything',
+        position: 2,
+        color: 'orange',
+      },
+    ],
+    defaultValue: { value: 'share_everything' },
+  })
+  visibility: string;
 
   @Field()
   firstParticipant: TimelineThreadParticipant;
