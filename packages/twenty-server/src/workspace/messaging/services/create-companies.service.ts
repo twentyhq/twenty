6cf4de3fc0c8e8387f1cfcfc867c5ee63f910475
bosiraphael <raphael.bosi@gmail.com>
2024-02-07 16:34:22 +0100

    wip

--- packages/twenty-server/src/workspace/messaging/services/create-companies.service.ts
+++ packages/twenty-server/src/workspace/messaging/services/create-companies.service.ts
@@ -1,6 +1,75 @@
-export const createCompanyFromDomainName = async (
-  domainName: string,
-  workspaceId: string,
-  userId: string,
-  companyId?: string,
-) => {};
+import { Injectable } from '@nestjs/common';
+
+import { v4 } from 'uuid';
+
+import { ObjectMetadataInterface } from 'src/metadata/field-metadata/interfaces/object-metadata.interface';
+import { Record } from 'src/workspace/workspace-query-builder/interfaces/record.interface';
+
+import { capitalize } from 'src/utils/capitalize';
+import { stringifyWithoutKeyQuote } from 'src/workspace/workspace-query-builder/utils/stringify-without-key-quote.util';
+import { WorkspaceQueryRunnerService } from 'src/workspace/workspace-query-runner/workspace-query-runner.service';
+@Injectable()
+export class CreateCompaniesService {
+  constructor(
+    private readonly workspaceQueryRunnunerService: WorkspaceQueryRunnerService,
+  ) {}
+
+  async createCompanyFromDomainName(
+    domainName: string,
+    workspaceId: string,
+    objectMetadataItemCollection: ObjectMetadataInterface[],
+  ) {
+    const companyName = capitalize(domainName.split('.')[0]);
+
+    const companyObjectMetadata = objectMetadataItemCollection.find(
+      (item) => item.nameSingular === 'company',
+    );
+
+    if (!companyObjectMetadata) {
+      return;
+    }
+
+    let relatedCompanyId = v4();
+
+    const existingCompany =
+      await this.workspaceQueryRunnunerService.executeAndParse<Record>(
+        `query {companyCollection(filter: {domainName: {eq: "${domainName}"}}) {
+                edges {
+                  node {
+                    id
+                  }
+                }
+              }
+            }
+          `,
+        companyObjectMetadata,
+        '',
+        workspaceId,
+      );
+
+    if (existingCompany.edges?.length) {
+      relatedCompanyId = existingCompany.edges[0].node.id;
+    }
+
+    await this.workspaceQueryRunnunerService.execute(
+      `mutation {
+          insertIntocompanyCollection(objects: ${stringifyWithoutKeyQuote([
+            {
+              id: relatedCompanyId,
+              name: companyName,
+              domainName: domainName,
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString(),
+            },
+          ])}) {
+            affectedCount
+            records {
+              id
+            }
+          }
+        }
+      `,
+      workspaceId,
+    );
+  }
+}
