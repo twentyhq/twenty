f1b0ee8e512304369359704509e80991e2e802ea
Thomas Trompette <thomast@twenty.com>
2024-02-07 10:20:43 +0100

    Fix opportunity type

--- packages/twenty-server/src/workspace/workspace-sync-metadata/standard-objects/attachment.object-metadata.ts
+++ packages/twenty-server/src/workspace/workspace-sync-metadata/standard-objects/attachment.object-metadata.ts
@@ -6,6 +6,7 @@ import { ObjectMetadata } from 'src/workspace/workspace-sync-metadata/decorators
 import { ActivityObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/activity.object-metadata';
 import { BaseObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/base.object-metadata';
 import { CompanyObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/company.object-metadata';
+import { OpportunityObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/opportunity.object-metadata';
 import { PersonObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/person.object-metadata';
 import { WorkspaceMemberObjectMetadata } from 'src/workspace/workspace-sync-metadata/standard-objects/workspace-member.object-metadata';
 
@@ -89,5 +90,5 @@ export class AttachmentObjectMetadata extends BaseObjectMetadata {
     joinColumn: 'opportunityId',
   })
   @IsNullable()
-  opportunity: CompanyObjectMetadata;
+  opportunity: OpportunityObjectMetadata;
 }
