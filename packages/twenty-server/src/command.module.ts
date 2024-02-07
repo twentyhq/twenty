7bdf92e6da3f5cc500ea665053664f9fe18819db
martmull <martmull@hotmail.fr>
2024-02-07 18:19:58 +0100

    Delete src/workspace/cron

--- packages/twenty-server/src/command.module.ts
+++ packages/twenty-server/src/command.module.ts
@@ -3,8 +3,6 @@ import { Module } from '@nestjs/common';
 import { DatabaseCommandModule } from 'src/database/commands/database-command.module';
 import { FetchWorkspaceMessagesCommandsModule } from 'src/workspace/messaging/commands/fetch-workspace-messages-commands.module';
 import { WorkspaceHealthCommandModule } from 'src/workspace/workspace-health/commands/workspace-health-command.module';
-import { StartFetchAllWorkspacesMessagesCronCommand } from 'src/workspace/cron/fetch-all-workspaces-messages/commands/start-fetch-all-workspaces-messages.cron.command';
-import { StopFetchAllWorkspacesMessagesCronCommand } from 'src/workspace/cron/fetch-all-workspaces-messages/commands/stop-fetch-all-workspaces-messages.cron.command';
 import { WorkspaceCleanerModule } from 'src/workspace/workspace-cleaner/workspace-cleaner.module';
 
 import { AppModule } from './app.module';
@@ -21,8 +19,6 @@ import { WorkspaceMigrationRunnerCommandsModule } from './workspace/workspace-mi
     WorkspaceCleanerModule,
     WorkspaceHealthCommandModule,
     WorkspaceMigrationRunnerCommandsModule,
-    StartFetchAllWorkspacesMessagesCronCommand,
-    StopFetchAllWorkspacesMessagesCronCommand,
   ],
 })
 export class CommandModule {}
