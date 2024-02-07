a908353955adf90d68c806329159e4e6fc6a070e
Jérémy M <jeremy.magrin@gmail.com>
2024-02-07 15:38:23 +0100

    feat: add deletion support on sync metadata command (#3826)
    * feat: add deletion support on sync metadata command
    
    * fix: remove debug

--- packages/twenty-server/src/workspace/workspace-sync-metadata/factories/index.ts
+++ packages/twenty-server/src/workspace/workspace-sync-metadata/factories/index.ts
@@ -1,11 +1,15 @@
 import { FeatureFlagFactory } from './feature-flags.factory';
 import { StandardObjectFactory } from './standard-object.factory';
 import { StandardRelationFactory } from './standard-relation.factory';
-import { WorkspaceSyncFactory } from './workspace-sync.factory';
+import { ObjectWorkspaceMigrationFactory } from './object-workspace-migration.factory';
+import { FieldWorkspaceMigrationFactory } from './field-workspace-migration.factory';
+import { RelationWorkspaceMigrationFactory } from './relation-workspace-migration.factory';
 
 export const workspaceSyncMetadataFactories = [
   FeatureFlagFactory,
   StandardObjectFactory,
   StandardRelationFactory,
-  WorkspaceSyncFactory,
+  ObjectWorkspaceMigrationFactory,
+  FieldWorkspaceMigrationFactory,
+  RelationWorkspaceMigrationFactory,
 ];
