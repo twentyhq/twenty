7ec968d5a2701e0553f043159c8698ed1e9493ff
Jérémy M <jeremy.magrin@gmail.com>
2024-02-08 18:59:17 +0100

    feat: workspace health type fix (#3890)
    * feat: workspace health type fix
    
    * Fix
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- packages/twenty-server/src/workspace/workspace-health/services/workspace-fix.service.ts
+++ packages/twenty-server/src/workspace/workspace-health/services/workspace-fix.service.ts
@@ -7,14 +7,19 @@ import { WorkspaceHealthIssue } from 'src/workspace/workspace-health/interfaces/
 
 import { WorkspaceMigrationEntity } from 'src/metadata/workspace-migration/workspace-migration.entity';
 import { ObjectMetadataEntity } from 'src/metadata/object-metadata/object-metadata.entity';
-import { isWorkspaceHealthNullableIssue } from 'src/workspace/workspace-health/utils/is-workspace-health-issue-type.util';
+import {
+  isWorkspaceHealthNullableIssue,
+  isWorkspaceHealthTypeIssue,
+} from 'src/workspace/workspace-health/utils/is-workspace-health-issue-type.util';
 
 import { WorkspaceFixNullableService } from './workspace-fix-nullable.service';
+import { WorkspaceFixTypeService } from './workspace-fix-type.service';
 
 @Injectable()
 export class WorkspaceFixService {
   constructor(
     private readonly workspaceFixNullableService: WorkspaceFixNullableService,
+    private readonly workspaceFixTypeService: WorkspaceFixTypeService,
   ) {}
 
   async fix(
@@ -28,6 +33,12 @@ export class WorkspaceFixService {
         service: this.workspaceFixNullableService,
         issues: issues.filter((issue) =>
           isWorkspaceHealthNullableIssue(issue.type),
+        ),
+      },
+      [WorkspaceHealthFixKind.Type]: {
+        service: this.workspaceFixTypeService,
+        issues: issues.filter((issue) =>
+          isWorkspaceHealthTypeIssue(issue.type),
         ),
       },
     };
