448c2af2d4937f69ccbc90363feda936eae5f756
bosiraphael <raphael.bosi@gmail.com>
2024-02-08 13:45:18 +0100

    Add name

--- packages/twenty-server/src/workspace/messaging/services/create-contact.service.ts
+++ packages/twenty-server/src/workspace/messaging/services/create-contact.service.ts
@@ -7,23 +7,27 @@ import { DataSourceEntity } from 'src/metadata/data-source/data-source.entity';
 export class CreateContactService {
   constructor() {}
 
-  async createContactFromHandle(
+  async createContactFromHandleAndDisplayName(
     handle: string,
+    displayName: string,
     dataSourceMetadata: DataSourceEntity,
     manager: EntityManager,
-  ) {
-    const existingPerson = await manager.query(
+  ): Promise<void> {
+    const existingContact = await manager.query(
       `SELECT * FROM ${dataSourceMetadata.schema}.person WHERE handle = $1`,
       [handle],
     );
 
-    if (existingPerson.length > 0) {
-      return existingPerson[0];
+    if (existingContact.length > 0) {
+      return existingContact[0];
     }
 
+    const contactFirstName = displayName.split(' ')[0];
+    const contactLastName = displayName.split(' ')[1];
+
     await manager.query(
-      `INSERT INTO ${dataSourceMetadata.schema}.person (email) VALUES ($1)`,
-      [handle],
+      `INSERT INTO ${dataSourceMetadata.schema}.person (email, nameFirstName, nameLastName) VALUES ($1, $2, $3)`,
+      [handle, contactFirstName, contactLastName],
     );
   }
 }
