2444e6d72eec69a72acb9ffd152787b1a21f60bf
bosiraphael <raphael.bosi@gmail.com>
2024-02-08 11:30:11 +0100

    Add HTTP service to fetch company info from domain name

--- packages/twenty-server/src/workspace/messaging/services/create-companies.service.ts
+++ packages/twenty-server/src/workspace/messaging/services/create-companies.service.ts
@@ -2,20 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 import { v4 } from 'uuid';
 import { EntityManager } from 'typeorm';
+import axios, { AxiosInstance } from 'axios';
 
-import { capitalize } from 'src/utils/capitalize';
 import { DataSourceEntity } from 'src/metadata/data-source/data-source.entity';
+import { capitalize } from 'src/utils/capitalize';
 @Injectable()
 export class CreateCompaniesService {
-  constructor() {}
+  private readonly httpService: AxiosInstance;
+
+  constructor() {
+    this.httpService = axios.create({
+      baseURL: 'https://companies.twenty.com/',
+    });
+  }
 
   async createCompanyFromDomainName(
     domainName: string,
     dataSourceMetadata: DataSourceEntity,
     manager: EntityManager,
   ) {
-    const companyName = capitalize(domainName.split('.')[0]);
-
     const companyId = v4();
 
     const existingCompany = await manager.query(
@@ -26,12 +31,32 @@ export class CreateCompaniesService {
       return existingCompany[0].id;
     }
 
+    const { name, city } = await this.getCompanyInfoFromDomainName(domainName);
+
+    const companyNameFromDomainName = capitalize(domainName.split('.')[0]);
+
+    const companyName = name || companyNameFromDomainName;
+
     await manager.query(
-      `INSERT INTO ${dataSourceMetadata.schema}.company (id, name, "domainName")
+      `INSERT INTO ${dataSourceMetadata.schema}.company (id, name, "domainName", address)
       VALUES ($1, $2, $3)`,
-      [companyId, companyName, domainName],
+      [companyId, companyName, domainName, city],
     );
 
     return companyId;
+  }
+
+  async getCompanyInfoFromDomainName(domainName: string): Promise<{
+    name: string;
+    city: string;
+  }> {
+    const response = await this.httpService.get(`/${domainName}`);
+
+    const data = response.data;
+
+    return {
+      name: data.name,
+      city: data.city,
+    };
   }
 }
