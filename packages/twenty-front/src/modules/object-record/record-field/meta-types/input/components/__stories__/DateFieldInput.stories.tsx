b13cecbf624f90fd8f10934c834d55e34c0cc031
Mohamed Houssein Douici <douici1@gmail.com>
2024-02-12 21:23:35 +0000

    fix: set a specifc date to date field input on the story and set the state on record store family selector

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/__stories__/DateFieldInput.stories.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/__stories__/DateFieldInput.stories.tsx
@@ -8,7 +8,7 @@ import { FieldContextProvider } from '../../../__stories__/FieldContextProvider'
 import { useDateTimeField } from '../../../hooks/useDateTimeField';
 import { DateFieldInput, DateFieldInputProps } from '../DateFieldInput';
 
-const formattedDate = new Date();
+const formattedDate = new Date(2022, 1, 1);
 
 const DateFieldValueSetterEffect = ({ value }: { value: Date }) => {
   const { setFieldValue } = useDateTimeField();
