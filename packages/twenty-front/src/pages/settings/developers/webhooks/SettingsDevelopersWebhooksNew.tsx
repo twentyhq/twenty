5bf4c72b82313a88aaa3e669fa7c58d6eb06bc66
Jonathanlemon <lemonjt02@gmail.com>
2024-02-13 15:45:48 -0500

    Added functionality for onKeyDown for new webhook and new API key, to save when the user presses the [Enter] key

--- packages/twenty-front/src/pages/settings/developers/webhooks/SettingsDevelopersWebhooksNew.tsx
+++ packages/twenty-front/src/pages/settings/developers/webhooks/SettingsDevelopersWebhooksNew.tsx
@@ -60,6 +60,10 @@ export const SettingsDevelopersWebhooksNew = () => {
           <TextInput
             placeholder="URL"
             value={formValues.targetUrl}
+            onKeyDown={(e) => {
+              if (e.key === "Enter")
+                  handleSave();
+            }}
             onChange={(value) => {
               setFormValues((prevState) => ({
                 ...prevState,
