87fafae9be84a9c8551753954541caa84df5a369
Weiko <corentin@twenty.com>
2024-02-13 19:19:57 +0100

    [messaging] Removing TO requirement for email import (#3949)

--- packages/twenty-server/src/workspace/messaging/services/fetch-messages-by-batches.service.ts
+++ packages/twenty-server/src/workspace/messaging/services/fetch-messages-by-batches.service.ts
@@ -189,7 +189,6 @@ export class FetchMessagesByBatchesService {
           } = parsed;
 
           if (!from) throw new Error('From value is missing');
-          if (!to) throw new Error('To value is missing');
 
           const participants = [
             ...this.formatAddressObjectAsParticipants(from, 'from'),
