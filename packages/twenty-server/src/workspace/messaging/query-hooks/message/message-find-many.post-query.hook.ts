c136338d962ee0a5a0c704177b9511f5486e0ee7
corentin <corentin@twenty.com>
2024-02-15 11:25:39 +0100

    fix

--- packages/twenty-server/src/workspace/messaging/query-hooks/message/message-find-many.post-query.hook.ts
+++ packages/twenty-server/src/workspace/messaging/query-hooks/message/message-find-many.post-query.hook.ts
@@ -1,24 +1,115 @@
 import { Injectable } from '@nestjs/common';
 
+import { groupBy } from 'lodash';
+
 import { IConnection } from 'src/utils/pagination/interfaces/connection.interface';
 import { IEdge } from 'src/utils/pagination/interfaces/edge.interface';
 import { WorkspacePostQueryHook } from 'src/workspace/workspace-query-runner/workspace-query-hook/interfaces/workspace-post-query-hook.interface';
+import { Record as IRecord } from 'src/workspace/workspace-query-builder/interfaces/record.interface';
+
+import { MessageChannelMessageAssociationService } from 'src/workspace/messaging/repositories/message-channel-message-association/message-channel-message-association.service';
+import { MessageChannelService } from 'src/workspace/messaging/repositories/message-channel/message-channel.service';
+import { WorkspaceMemberService } from 'src/workspace/messaging/repositories/workspace-member/workspace-member.service';
+import { ConnectedAccountService } from 'src/workspace/messaging/repositories/connected-account/connected-account.service';
 
 @Injectable()
 export class MessageFindManyPostQueryHook implements WorkspacePostQueryHook {
-  async execute<Record>(
-    _userId: string | undefined,
-    _workspaceId: string,
+  constructor(
+    private readonly messageChannelMessageAssociationService: MessageChannelMessageAssociationService,
+    private readonly messageChannelService: MessageChannelService,
+    private readonly workspaceMemberService: WorkspaceMemberService,
+    private readonly connectedAccountService: ConnectedAccountService,
+  ) {}
+
+  async execute<Record extends IRecord = IRecord>(
+    userId: string,
+    workspaceId: string,
     result: IConnection<Record, IEdge<Record>>,
   ): Promise<IConnection<Record, IEdge<Record>>> {
+    const messagesWithHiddenUnsharedMessages = await this.hideUnsharedMessages(
+      userId,
+      workspaceId,
+      result.edges,
+    );
+
     return {
       ...result,
-      edges: result.edges.map((edge) => ({
-        ...edge,
-        node: {
-          ...edge.node,
-        },
-      })),
+      edges: messagesWithHiddenUnsharedMessages,
     };
+  }
+
+  private async hideUnsharedMessages<Record extends IRecord = IRecord>(
+    userId: string,
+    workspaceId: string,
+    messages: IEdge<Record>[],
+  ) {
+    const messageChannelMessageAssociations =
+      await this.messageChannelMessageAssociationService.getByMessageIds(
+        messages.map((message) => message.node.id),
+        workspaceId,
+      );
+
+    const messageChannelMessageAssociationsByMessageIds = groupBy(
+      messageChannelMessageAssociations,
+      (messageChannelMessageAssociation) =>
+        messageChannelMessageAssociation.messageId,
+    );
+
+    const messageChannelIds = messageChannelMessageAssociations.map(
+      (association) => association.messageChannelId,
+    );
+
+    const messageChannels = await this.messageChannelService.getByIds(
+      messageChannelIds,
+      workspaceId,
+    );
+
+    const messageChannelsByIds = groupBy(
+      messageChannels,
+      (channel) => channel.id,
+    );
+
+    const currentWorkspaceMember =
+      await this.workspaceMemberService.getByIdOrFail(userId, workspaceId);
+
+    const connectedAccounts = await this.connectedAccountService.getByIds(
+      messageChannels.map((channel) => channel.connectedAccountId),
+      workspaceId,
+    );
+
+    const connectedAccountsByIds = groupBy(
+      connectedAccounts,
+      (account) => account.id,
+    );
+
+    const messagesWithHiddenUnsharedMessages = messages.map((message) => {
+      const messageChannelMessageAssociations =
+        messageChannelMessageAssociationsByMessageIds[message.node.id];
+
+      const shouldShowMessage = messageChannelMessageAssociations.find(
+        (association) => {
+          const messageChannel =
+            messageChannelsByIds[association.messageChannelId][0];
+
+          messageChannel.visibility === 'share_everything' ||
+            connectedAccountsByIds[messageChannel.connectedAccountId][0]
+              .accountOwnerId === currentWorkspaceMember.id;
+        },
+      );
+
+      if (shouldShowMessage) {
+        return message;
+      }
+
+      return {
+        ...message,
+        node: {
+          ...message.node,
+          hidden: true,
+        },
+      };
+    });
+
+    return messagesWithHiddenUnsharedMessages;
   }
 }
--- packages/twenty-server/src/workspace/workspace-query-runner/workspace-query-hook/interfaces/workspace-post-query-hook.interface.ts
+++ packages/twenty-server/src/workspace/workspace-query-runner/workspace-query-hook/interfaces/workspace-post-query-hook.interface.ts
@@ -1,13 +1,14 @@
 import { IConnection } from 'src/utils/pagination/interfaces/connection.interface';
 import { IEdge } from 'src/utils/pagination/interfaces/edge.interface';
 import { PGGraphQLMutation } from 'src/workspace/workspace-query-runner/interfaces/pg-graphql.interface';
+import { Record as IRecord } from 'src/workspace/workspace-query-builder/interfaces/record.interface';
 
 type Result<Record> =
   | IConnection<Record, IEdge<Record>>
   | PGGraphQLMutation<Record>;
 
 export interface WorkspacePostQueryHook {
-  execute<Record>(
+  execute<Record extends IRecord = IRecord>(
     userId: string | undefined,
     workspaceId: string,
     result: Result<Record>,
