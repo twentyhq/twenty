cba84be43cdfba8713eda35d64ecf492f07ba307
martmull <martmull@hotmail.fr>
2024-02-15 10:46:43 +0100

    Remove isCurrentWorkspaceActiveSelector

--- packages/twenty-front/src/modules/object-metadata/components/ObjectMetadataItemsProvider.tsx
+++ packages/twenty-front/src/modules/object-metadata/components/ObjectMetadataItemsProvider.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { useRecoilValue } from 'recoil';
 
-import { isCurrentWorkspaceActiveSelector } from '@/auth/states/selectors/isCurrentWorkspaceActiveSelector';
+import { currentWorkspaceMemberState } from '@/auth/states/currentWorkspaceMemberState.ts';
 import { ObjectMetadataItemsLoadEffect } from '@/object-metadata/components/ObjectMetadataItemsLoadEffect';
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
 import { RelationPickerScope } from '@/object-record/relation-picker/scopes/RelationPickerScope';
@@ -9,14 +10,12 @@ export const ObjectMetadataItemsProvider = ({
   children,
 }: React.PropsWithChildren) => {
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
-  const isCurrentWorkspaceActive = useRecoilValue(
-    isCurrentWorkspaceActiveSelector,
-  );
+  const currentWorkspaceMember = useRecoilValue(currentWorkspaceMemberState);
 
   return (
     <>
       <ObjectMetadataItemsLoadEffect />
-      {(!isCurrentWorkspaceActive || !!objectMetadataItems.length) && (
+      {(!currentWorkspaceMember || !!objectMetadataItems.length) && (
         <RelationPickerScope relationPickerScopeId="relation-picker">
           {children}
         </RelationPickerScope>
