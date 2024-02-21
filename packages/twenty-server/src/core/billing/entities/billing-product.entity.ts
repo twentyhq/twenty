2177ba978b4e15d2356acf0bbe50b55c91dad9d5
martmull <martmull@hotmail.fr>
2024-02-21 17:38:01 +0100

    Remove graphql decorators

--- packages/twenty-server/src/core/billing/entities/billing-product.entity.ts
+++ packages/twenty-server/src/core/billing/entities/billing-product.entity.ts
@@ -1,18 +1,12 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
-
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { IDField } from '@ptc-org/nestjs-query-graphql';
 
 import { BillingSubscription } from 'src/core/billing/entities/billing-subscription.entity';
 
 @Entity({ name: 'billingProduct', schema: 'core' })
-@ObjectType('BillingProduct')
 export class BillingProduct {
-  @IDField(() => ID)
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Field()
   @Column({ nullable: false })
   billingSubscriptionId: string;
 
@@ -25,15 +19,12 @@ export class BillingProduct {
   )
   billingSubscription: BillingSubscription;
 
-  @Field()
   @Column({ nullable: false })
   stripeProductId: string;
 
-  @Field()
   @Column({ nullable: false })
   stripePriceId: string;
 
-  @Field()
   @Column({ nullable: false })
   quantity: number;
 }
--- packages/twenty-server/src/core/billing/entities/billing-subscription.entity.ts
+++ packages/twenty-server/src/core/billing/entities/billing-subscription.entity.ts
@@ -1,5 +1,3 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
-
 import {
   Column,
   Entity,
@@ -8,16 +6,13 @@ import {
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { IDField } from '@ptc-org/nestjs-query-graphql';
 import Stripe from 'stripe';
 
 import { Workspace } from 'src/core/workspace/workspace.entity';
 import { BillingProduct } from 'src/core/billing/entities/billing-product.entity';
 
 @Entity({ name: 'billingSubscription', schema: 'core' })
-@ObjectType('BillingSubscription')
 export class BillingSubscription {
-  @IDField(() => ID)
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
@@ -27,19 +22,15 @@ export class BillingSubscription {
   @JoinColumn()
   workspace: Workspace;
 
-  @Field()
   @Column({ nullable: false, type: 'uuid' })
   workspaceId: string;
 
-  @Field()
   @Column({ unique: true, nullable: false })
   stripeCustomerId: string;
 
-  @Field()
   @Column({ unique: true, nullable: false })
   stripeSubscriptionId: string;
 
-  @Field()
   @Column({ nullable: false })
   status: Stripe.Subscription.Status;
 
