0b46fae6ae35c9e4bc734bf34c8d12c26dab2467
Thaïs Guigon <guigon.thais@gmail.com>
2024-02-21 15:14:09 -0300

    feat: add Accounts List Card to Calendar Settings
    Closes #4061

--- packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsEmailsSyncSection.tsx
+++ packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsEmailsSyncSection.tsx
@@ -1,18 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
 import { useRecoilValue } from 'recoil';
+import { LightIconButton } from 'tsup.ui.index';
 
 import { ConnectedAccount } from '@/accounts/types/ConnectedAccount';
 import { MessageChannel } from '@/accounts/types/MessageChannel';
 import { currentWorkspaceMemberState } from '@/auth/states/currentWorkspaceMemberState';
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { useFindManyRecords } from '@/object-record/hooks/useFindManyRecords';
-import { SettingsAccountsEmailsCard } from '@/settings/accounts/components/SettingsAccountsEmailsCard';
-import { SettingsAccountsListEmptyStateCard } from '@/settings/accounts/components/SettingsAccountsListEmptyStateCard';
-import { SettingsAccountsListSkeletonCard } from '@/settings/accounts/components/SettingsAccountsListSkeletonCard';
+import { SettingsAccountsListCard } from '@/settings/accounts/components/SettingsAccountsListCard';
+import { SettingsAccountsSynchronizationStatus } from '@/settings/accounts/components/SettingsAccountsSynchronizationStatus';
+import { IconChevronRight } from '@/ui/display/icon';
+import { IconGmail } from '@/ui/display/icon/components/IconGmail';
 import { H2Title } from '@/ui/display/typography/components/H2Title';
 import { Section } from '@/ui/layout/section/components/Section';
 
+const StyledRowRightContainer = styled.div`
+  align-items: center;
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(1)};
+`;
+
 export const SettingsAccountsEmailsSyncSection = () => {
   const currentWorkspaceMember = useRecoilValue(currentWorkspaceMemberState);
+  const navigate = useNavigate();
 
   const { records: accounts, loading: accountsLoading } =
     useFindManyRecords<ConnectedAccount>({
@@ -26,7 +37,7 @@ export const SettingsAccountsEmailsSyncSection = () => {
 
   const { records: messageChannels, loading: messageChannelsLoading } =
     useFindManyRecords<MessageChannel>({
-      objectNameSingular: 'messageChannel',
+      objectNameSingular: CoreObjectNameSingular.MessageChannel,
       filter: {
         connectedAccountId: {
           in: accounts.map((account) => account.id),
@@ -41,24 +52,28 @@ export const SettingsAccountsEmailsSyncSection = () => {
     }),
   );
 
-  const loading = accountsLoading || messageChannelsLoading;
-
   return (
     <Section>
       <H2Title
         title="Emails sync"
         description="Sync your inboxes and set your privacy settings"
       />
-
-      {loading ? (
-        <SettingsAccountsListSkeletonCard />
-      ) : accounts.length ? (
-        <SettingsAccountsEmailsCard
-          messageChannels={messageChannelsWithSyncedEmails}
-        />
-      ) : (
-        <SettingsAccountsListEmptyStateCard />
-      )}
+      <SettingsAccountsListCard
+        accounts={messageChannelsWithSyncedEmails}
+        isLoading={accountsLoading || messageChannelsLoading}
+        onRowClick={(messageChannel) =>
+          navigate(`/settings/accounts/emails/${messageChannel.id}`)
+        }
+        RowIcon={IconGmail}
+        RowRightComponent={({ account: messageChannel }) => (
+          <StyledRowRightContainer>
+            <SettingsAccountsSynchronizationStatus
+              synced={messageChannel.isSynced}
+            />
+            <LightIconButton Icon={IconChevronRight} accent="tertiary" />
+          </StyledRowRightContainer>
+        )}
+      />
     </Section>
   );
 };
