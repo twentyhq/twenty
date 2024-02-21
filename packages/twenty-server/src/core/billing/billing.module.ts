abf37966c8d1c49dd276879602b4a17faecd9060
martmull <martmull@hotmail.fr>
2024-02-21 17:08:20 +0100

    Remove useless imports

--- packages/twenty-server/src/core/billing/billing.module.ts
+++ packages/twenty-server/src/core/billing/billing.module.ts
@@ -1,21 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { NestjsQueryGraphQLModule } from '@ptc-org/nestjs-query-graphql';
-import { NestjsQueryTypeOrmModule } from '@ptc-org/nestjs-query-typeorm';
-
-import { BillingSubscription } from 'src/core/billing/entities/billing-subscription.entity';
-import { BillingProduct } from 'src/core/billing/entities/billing-product.entity';
-
 @Module({
-  imports: [
-    NestjsQueryGraphQLModule.forFeature({
-      imports: [
-        NestjsQueryTypeOrmModule.forFeature(
-          [BillingSubscription, BillingProduct],
-          'core',
-        ),
-      ],
-    }),
-  ],
+  imports: [],
 })
 export class BillingModule {}
