df3d4f67bc6e79f76a7eac57eefb420a8c47080b
Charles Bochet <charles@twenty.com>
2024-02-24 19:02:26 +0100

    Fix

--- packages/twenty-front/nyc.config.cjs
+++ packages/twenty-front/nyc.config.cjs
@@ -6,8 +6,8 @@ const globalCoverage = {
 };
 
 const modulesCoverage = {
-  statements: 80,
-  lines: 80,
+  statements: 75,
+  lines: 75,
   functions: 70,
   include: ['src/modules/**/*'],
   exclude: ['src/**/*.ts'],
