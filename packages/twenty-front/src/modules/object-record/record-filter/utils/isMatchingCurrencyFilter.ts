7e2117a450c5d35728db25e89efbf6b16d5f2f11
Charles Bochet <charles@twenty.com>
2024-02-25 22:19:24 +0100

    Fix

--- packages/twenty-front/src/modules/object-record/record-filter/utils/isMatchingCurrencyFilter.ts
+++ packages/twenty-front/src/modules/object-record/record-filter/utils/isMatchingCurrencyFilter.ts
@@ -7,43 +7,41 @@ export const isMatchingCurrencyFilter = ({
   currencyFilter: CurrencyFilter;
   value: number;
 }) => {
-  if (currencyFilter?.amountMicros !== undefined) {
-    switch (true) {
-      case currencyFilter.amountMicros.eq !== undefined: {
-        return value === currencyFilter.amountMicros.eq;
+  switch (true) {
+    case currencyFilter.amountMicros?.eq !== undefined: {
+      return value === currencyFilter.amountMicros.eq;
+    }
+    case currencyFilter.amountMicros?.neq !== undefined: {
+      return value !== currencyFilter.amountMicros.neq;
+    }
+    case currencyFilter.amountMicros?.gt !== undefined: {
+      return value > currencyFilter.amountMicros.gt;
+    }
+    case currencyFilter.amountMicros?.gte !== undefined: {
+      return value >= currencyFilter.amountMicros.gte;
+    }
+    case currencyFilter.amountMicros?.lt !== undefined: {
+      return value < currencyFilter.amountMicros.lt;
+    }
+    case currencyFilter.amountMicros?.lte !== undefined: {
+      return value <= currencyFilter.amountMicros.lte;
+    }
+    case currencyFilter.amountMicros?.in !== undefined: {
+      return currencyFilter.amountMicros.in.includes(value);
+    }
+    case currencyFilter.amountMicros?.is !== undefined: {
+      if (currencyFilter.amountMicros.is === 'NULL') {
+        return value === null;
+      } else {
+        return value !== null;
       }
-      case currencyFilter.amountMicros.neq !== undefined: {
-        return value !== currencyFilter.amountMicros.neq;
-      }
-      case currencyFilter.amountMicros.gt !== undefined: {
-        return value > currencyFilter.amountMicros.gt;
-      }
-      case currencyFilter.amountMicros.gte !== undefined: {
-        return value >= currencyFilter.amountMicros.gte;
-      }
-      case currencyFilter.amountMicros.lt !== undefined: {
-        return value < currencyFilter.amountMicros.lt;
-      }
-      case currencyFilter.amountMicros.lte !== undefined: {
-        return value <= currencyFilter.amountMicros.lte;
-      }
-      case currencyFilter.amountMicros.in !== undefined: {
-        return currencyFilter.amountMicros.in.includes(value);
-      }
-      case currencyFilter.amountMicros.is !== undefined: {
-        if (currencyFilter.amountMicros.is === 'NULL') {
-          return value === null;
-        } else {
-          return value !== null;
-        }
-      }
-      default: {
-        throw new Error(
-          `Unexpected amountMicros for currency filter : ${JSON.stringify(
-            currencyFilter.amountMicros,
-          )}`,
-        );
-      }
+    }
+    default: {
+      throw new Error(
+        `Unexpected amountMicros for currency filter : ${JSON.stringify(
+          currencyFilter.amountMicros,
+        )}`,
+      );
     }
   }
 };
