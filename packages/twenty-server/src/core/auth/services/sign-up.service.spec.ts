fb1db14662f629e2596b9febd0cb06776bd51cd4
Charles Bochet <charles@twenty.com>
2024-02-25 11:47:24 +0100

    Fix tests

--- packages/twenty-server/src/core/auth/services/sign-up.service.spec.ts
+++ packages/twenty-server/src/core/auth/services/sign-up.service.spec.ts
@@ -2,13 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { HttpService } from '@nestjs/axios';
 
-import { FileUploadService } from 'packages/twenty-server/dist/src/core/file/services/file-upload.service';
-import { UserWorkspaceService } from 'packages/twenty-server/dist/src/core/user-workspace/user-workspace.service';
-
 import { Workspace } from 'src/core/workspace/workspace.entity';
 import { User } from 'src/core/user/user.entity';
 import { EnvironmentService } from 'src/integrations/environment/environment.service';
 import { SignUpService } from 'src/core/auth/services/sign-up.service';
+import { FileUploadService } from 'src/core/file/services/file-upload.service';
+import { UserWorkspaceService } from 'src/core/user-workspace/user-workspace.service';
 
 describe('SignUpService', () => {
   let service: SignUpService;
