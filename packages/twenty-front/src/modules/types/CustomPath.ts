33e526dc149418a1d1cdd88282558f5bb76c2dbd
Shubham Pawar <82868470+srp-pawar@users.noreply.github.com>
2024-02-26 19:07:45 +0530

    Added Custom Paths
    eg: Campaign Page, template pages

---
+++ packages/twenty-front/src/modules/types/CustomPath.ts
@@ -0,0 +1,17 @@
+export enum CustomPath {
+  TemplatesPage = '/templatelist',
+
+  AudioTemplatePage = '/audiotemplate',
+
+  VideoTemplatePage = '/videotemplate',
+
+  DocumentTemplatePage = '/documenttemplate',
+
+  ImageTemplatePage = '/imagetemplate',
+
+  TextTemplatePage = '/texttemplate',
+
+  CampaignsPage = '/campaigns',
+
+  DatePickerPage = '/datepicker',
+}
