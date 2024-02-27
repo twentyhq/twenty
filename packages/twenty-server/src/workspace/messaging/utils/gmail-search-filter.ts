06cd3056b3dd2b661366fdaa9d1297665608b320
bosiraphael <raphael.bosi@gmail.com>
2024-02-27 16:35:02 +0100

    gmail filter is working

--- packages/twenty-server/src/workspace/messaging/utils/gmail-search-filter.ts
+++ packages/twenty-server/src/workspace/messaging/utils/gmail-search-filter.ts
@@ -2,7 +2,9 @@ export const gmailSearchFilterNonPersonalEmails =
   'noreply|no-reply|do_not_reply|no.reply|accounts@|info@|admin@|contact@|hello@|support@|sales@|feedback@|service@|help@|mailer-daemon|notifications|digest|auto|apps|assign|comments|customer-success|enterprise|esign|express|forum|gc@|learn|mailer|marketing|messages|news|notification|payments|receipts|recrutement|security|service|support|team';
 
 export const gmailSearchFilterExcludeEmails = (emails: string[]): string => {
-  return `from:-(${gmailSearchFilterNonPersonalEmails}|${emails.join(
+  return `(in:inbox from:-(${gmailSearchFilterNonPersonalEmails}|${emails.join(
     '|',
-  )}) to:-(${gmailSearchFilterNonPersonalEmails}|${emails.join('|')})`;
+  )})|(in:sent to:-(${gmailSearchFilterNonPersonalEmails}|${emails.join(
+    '|',
+  )}))`;
 };
