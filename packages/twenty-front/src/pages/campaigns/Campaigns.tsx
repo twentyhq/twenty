d000f9470f0beba2ccac5ec475b7dc524f6ea0ca
Sanjana <sanjana@synapsenet.co>
2024-02-29 10:29:06 +0530

    updated checkbox in campaigns

--- packages/twenty-front/src/pages/campaigns/Campaigns.tsx
+++ packages/twenty-front/src/pages/campaigns/Campaigns.tsx
@@ -1,22 +1,18 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable @nx/workspace-styled-components-prefixed-with-styled */
-import { useState } from 'react';
-import { useMutation, useQuery } from '@apollo/client';
-import styled from '@emotion/styled';
-import { Select, TextInput } from 'tsup.ui.index';
-import { v4 as uuidv4 } from 'uuid';
-
-import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
-import { IconMail } from '@/ui/display/icon';
-import { H1Title } from '@/ui/display/typography/components/H1Title';
-import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
-import { Button } from '@/ui/input/button/components/Button';
-import DateTimePicker from '@/ui/input/components/internal/date/components/DateTimePicker';
-import { Radio } from '@/ui/input/components/Radio';
-import { SubMenuTopBarContainer } from '@/ui/layout/page/SubMenuTopBarContainer';
 import { Section } from '@/ui/layout/section/components/Section';
-import { ADD_CAMPAIGN } from '@/users/graphql/queries/addCampaign';
+import { Checkbox, CheckboxVariant,CheckboxSize, CheckboxShape, Select, TextInput } from 'tsup.ui.index';
+import { CurrencyCode } from '@/object-record/record-field/types/CurrencyCode';
+import { Nullable } from '~/types/Nullable';
+import { SubMenuTopBarContainer } from '@/ui/layout/page/SubMenuTopBarContainer';
+import { IconMail } from '@/ui/display/icon';
+import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
+import styled from '@emotion/styled';
+import { H1Title } from '@/ui/display/typography/components/H1Title';
+import { Button } from '@/ui/input/button/components/Button';
+import { Radio } from '@/ui/input/components/Radio';
+import { SetStateAction, useState } from 'react';
+import { useQuery } from '@apollo/client';
 import { GET_SPECIALTY } from '@/users/graphql/queries/getSpecialtyDetails';
+import DateTimePicker from '../modules/ui/input/components/internal/date/components/DateTimePicker';
 
 const StyledH1Title = styled(H1Title)`
   margin-bottom: 0;
@@ -27,17 +23,17 @@ const StyledRadio = styled(Radio)`
 `;
 
 const StyledSection = styled(Section)`
-  align-items: center;
-  display: flex;
-  justify-content: space-around;
-  margin-bottom: 16px;
-  margin-left: 0;
+margin-bottom: 16px;
+margin-left: 0;
+display: flex;
+justify-content: space-around;
+align-items: center;
 `;
 
 const SaveButtonContainer = styled.div`
+  width: auto;
   display: flex;
   justify-content: flex-end;
-  width: auto;
 `;
 
 const StyledLabel = styled.span`
@@ -48,144 +44,118 @@ const StyledLabel = styled.span`
   text-transform: uppercase;
 `;
 
-export const Campaigns = () => {
-  let Specialty: any = [];
+const StyledCheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+`;
 
-  const SpecialtyTypes: any = {};
+
+export type SpecilatySelectValues = {
+  currencyCode: CurrencyCode;
+};
+
+export type DateInputProps = {
+  value: Nullable<Date>;
+  onEnter: (newDate: Nullable<Date>) => void;
+  onEscape: (newDate: Nullable<Date>) => void;
+  onClickOutside: (
+    event: MouseEvent | TouchEvent,
+    newDate: Nullable<Date>,
+  ) => void;
+  hotkeyScope: string;
+  clearable?: boolean;
+  onChange?: (newDate: Nullable<Date>) => void;
+};
+
+
+export const Campaigns = () => {
+  var Specialty:any = [
+  ];
+
+  const SpecialtyTypes:any={
+  }
 
   const [campaignName, setCampaignName] = useState('');
   const [description, setDescription] = useState('');
-  const [specialty, setSpecialty] = useState('');
+  const [specialty, setSpecialty] = useState();
   const [subSpecialty, setSubSpecialty] = useState('');
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const [selectedMessaging, setSelectedMessaging] = useState('');
+  const [selectedStartDate,setSelectedStartDate]=useState(new Date())
+  const { loading: queryLoading, data: queryData } = useQuery(
+    GET_SPECIALTY,
+  );
 
-  const { loading: queryLoading, data: queryData } = useQuery(GET_SPECIALTY);
-
-  if (!queryLoading) {
-    const specialtyTypes = queryData.subspecialties.edges.map(
-      (edge: { node: { specialtyType: { name: any } } }) =>
-        edge?.node?.specialtyType?.name,
-    );
-    const uniqueSpecialtyTypes = Array.from(new Set(specialtyTypes));
-    Specialty = uniqueSpecialtyTypes.map((specialtyType) => ({
+  if(!queryLoading){
+    const specialtyTypes=queryData.subSpecialties.edges.map((edge: { node: { specialtyType: { name: any; }; }; })=> edge?.node?.specialtyType?.name);
+    const uniqueSpecialtyTypes=Array.from(new Set(specialtyTypes));
+    Specialty = uniqueSpecialtyTypes.map(specialtyType => ({
       value: specialtyType,
-      label: specialtyType,
+      label: specialtyType
     }));
+    
+    queryData.subSpecialties.edges.forEach((edge: { node: { specialtyType: { name: any; }; name: any; }; }) => {
+      const specialtyType = edge.node.specialtyType.name;
+      const subSpecialty = edge.node.name;
+    
+      // If the specialty type is already a key in the dictionary, push the sub-specialty to its array
+      if (SpecialtyTypes[specialtyType]) {
+        SpecialtyTypes[specialtyType].push({value: subSpecialty,label:subSpecialty});
+      } else {
+        // If the specialty type is not yet a key, create a new array with the sub-specialty as its first element
+        SpecialtyTypes[specialtyType] = [];
+        SpecialtyTypes[specialtyType].push({value: subSpecialty,label:subSpecialty});
 
-    queryData.subspecialties.edges.forEach(
-      (edge: { node: { specialtyType: { name: any }; name: any } }) => {
-        const specialtyType = edge.node.specialtyType.name;
-        const subSpecialty = edge.node.name;
-
-        // If the specialty type is already a key in the dictionary, push the sub-specialty to its array
-        if (SpecialtyTypes[specialtyType]) {
-          SpecialtyTypes[specialtyType].push({
-            value: subSpecialty,
-            label: subSpecialty,
-          });
-        } else {
-          // If the specialty type is not yet a key, create a new array with the sub-specialty as its first element
-          SpecialtyTypes[specialtyType] = [];
-          SpecialtyTypes[specialtyType].push({
-            value: subSpecialty,
-            label: subSpecialty,
-          });
-        }
-      },
-    );
+      }
+    });
   }
-
-  const [addCampaigns, { loading, error }] = useMutation(ADD_CAMPAIGN);
-  const { enqueueSnackBar } = useSnackBar();
-
-  const handleCampaignChange = (e: any) => {
-    setCampaignName(e.target.value);
+    
+  const handleCampaignChange = (e:any) => {
+      setCampaignName(e.target.value);
   };
 
-  const handleDescriptionChange = (e: any) => {
-    setDescription(e.target.value);
+  const handleDescriptionChange = (e:any) => {
+      setDescription(e.target.value);
   };
 
-  const handleSpecialtySelectChange = (selectedValue: any) => {
-    setSpecialty(selectedValue);
-  };
+    const handleSpecialtySelectChange = (selectedValue:any) => {
+      setSpecialty(selectedValue);
+    };
+  
+    const handleSubSpecialtySelectChange = (selectedValue:any) => {
+      setSubSpecialty(selectedValue);
+    };
 
-  const handleSubSpecialtySelectChange = (selectedValue: any) => {
-    setSubSpecialty(selectedValue);
-  };
+    const [date, setDate] = useState(new Date());    
 
-  const handleRadioChange = (value: string) => {
-    setSelectedMessaging(value);
-  };
-  const resetCampaignData = () => {
-    setCampaignName('');
-    setDescription('');
-    setStartDate(new Date());
-    setSelectedMessaging('');
-    setSpecialty('');
-    setSubSpecialty('');
-  };
-  const handleSave = async () => {
-    // console.log('Date', date.toISOString());
+    const [selectedMessaging, setSelectedMessaging] = useState("");
 
-    try {
-      console.log('Start Date', startDate);
+    const handleRadioChange = (value: string) => {
+      setSelectedMessaging(value)
+    };
 
-      console.log('End Date', endDate);
-      const variables = {
-        input: {
-          id: uuidv4(),
-          campaignName: campaignName,
-          specialtyType: specialty,
-          description: description,
-          subSpecialtyType: subSpecialty,
-          startDate: startDate,
-          endDate: endDate,
-          messagingMedia: selectedMessaging,
-        },
-      };
-      const { data } = await addCampaigns({
-        variables: variables,
-      });
-      enqueueSnackBar('Campaign added successfully', {
-        variant: 'success',
-      });
-      resetCampaignData();
-    } catch (errors: any) {
-      console.error('Error updating user:', error);
-      enqueueSnackBar(errors.message + 'Error while Submitting Campaign', {
-        variant: 'error',
-      });
-    }
-  };
+
   return (
-    <SubMenuTopBarContainer Icon={IconMail} title="Campaign">
-      <SettingsPageContainer>
+  <SubMenuTopBarContainer Icon={IconMail} title="Campaign">
+    <SettingsPageContainer>
         <StyledH1Title title="Campaign" />
         <Section>
-          <TextInput
-            label="Campaign Name"
-            value={campaignName}
-            // eslint-disable-next-line no-restricted-globals
-            onChange={() => handleCampaignChange(event)}
-            placeholder="Campaign Name"
-            name="campaignName"
-            required
-            fullWidth
-          />
+            <TextInput
+                    label="Campaign Name"
+                    value={campaignName}
+                    onChange={()=>handleCampaignChange(event)}
+                    placeholder="Campaign Name"
+                    name='campaignName'
+                    fullWidth
+                />
         </Section>
         <Section>
-          <TextInput
-            label="Description"
-            value={description}
-            onChange={() => handleDescriptionChange(event)}
-            placeholder="Description about campaign"
-            name="description"
-            required
-            fullWidth
-          />
+            <TextInput
+                    label="Description"
+                    value={description}
+                    onChange={()=>handleDescriptionChange(event)}
+                    placeholder="Description about campaign"
+                    name='description'
+                    fullWidth
+                />
         </Section>
         <Section>
           <Select
@@ -198,73 +168,93 @@ export const Campaigns = () => {
             onChange={handleSpecialtySelectChange}
           />
         </Section>
-        {specialty && (
-          <Section>
+        {specialty &&
+        <Section>
             <Select
-              fullWidth
-              // disabled
-              label="Sub Specialty Type"
-              dropdownId="Sub Specialty Type"
-              value={subSpecialty}
-              options={SpecialtyTypes[specialty]}
-              onChange={handleSubSpecialtySelectChange}
-            />
-          </Section>
-        )}
+                fullWidth
+                // disabled
+                label="Sub Specialty Type"
+                dropdownId="Sub Specialty Type"
+                value={subSpecialty}
+                options={SpecialtyTypes[specialty]}
+                onChange={handleSubSpecialtySelectChange}
+                />
+        </Section>}
         <Section>
           <StyledLabel>Start Date</StyledLabel>
-          <DateTimePicker
-            onChange={(startDate) => setStartDate(startDate)}
-            minDate={new Date()}
-          />
+          <DateTimePicker onChange={(date)=>setDate(date)} minDate={date} />
         </Section>
         <Section>
           <StyledLabel>End Date</StyledLabel>
-          <DateTimePicker
-            onChange={(endDate) => setEndDate(endDate)}
-            minDate={startDate}
-          />
+          <DateTimePicker onChange={(date)=>setDate(date)}  minDate={date} selected={null}/>
         </Section>
 
         <StyledLabel>Messaging</StyledLabel>
         <StyledSection>
-          <StyledRadio
-            value="SMS"
-            label="SMS"
-            onChange={(checked) => handleRadioChange('SMS')}
-            checked={selectedMessaging === 'SMS'}
+        <StyledCheckboxLabel htmlFor="sms-checkbox">
+          <Checkbox
+            // id="sms-checkbox"
+            checked={false}
+            indeterminate={false}
+            onChange={(checked) => handleRadioChange("SMS")}
+            variant={CheckboxVariant.Primary}
+            size={CheckboxSize.Small}
+            shape={CheckboxShape.Squared}
           />
-          <StyledRadio
-            value="WhastApp"
-            label="WhastApp"
-            onChange={(checked) => handleRadioChange('WhastApp')}
-            checked={selectedMessaging === 'WhastApp'}
-          />
+          SMS
+          </StyledCheckboxLabel>
 
-          <StyledRadio
-            value="GBM"
-            label="GBM"
-            onChange={(checked) => handleRadioChange('GBM')}
-            checked={selectedMessaging === 'GBM'}
-          />
+          <StyledCheckboxLabel htmlFor="whatsapp-checkbox">
+            <Checkbox
+              // id="whatsapp-checkbox"
+              checked={false}
+              indeterminate={false}
+              onChange={(checked) => handleRadioChange("WhastApp")}
+              variant={CheckboxVariant.Primary}
+              size={CheckboxSize.Small}
+              shape={CheckboxShape.Squared}
+            />
+            WhastApp
+          </StyledCheckboxLabel>
 
-          <StyledRadio
-            value="Call"
-            label="Call"
-            onChange={() => handleRadioChange('Call')}
-            checked={selectedMessaging === 'Call'}
-          />
+          <StyledCheckboxLabel htmlFor="gbm-checkbox">
+            <Checkbox
+              // id="gbm-checkbox"
+              checked={false}
+              indeterminate={false}
+              onChange={(checked) => handleRadioChange("GBM")}
+              variant={CheckboxVariant.Primary}
+              size={CheckboxSize.Small}
+              shape={CheckboxShape.Squared}
+            />
+            GBM
+          </StyledCheckboxLabel>
+          
+          <StyledCheckboxLabel htmlFor="call-checkbox">
+            <Checkbox
+              // id="call-checkbox"
+              checked={false}
+              indeterminate={false}
+              onChange={(checked) => handleRadioChange("Call")}
+              variant={CheckboxVariant.Primary}
+              size={CheckboxSize.Small}
+              shape={CheckboxShape.Squared}
+            />
+            Call
+            </StyledCheckboxLabel>
         </StyledSection>
         <SaveButtonContainer>
-          <Button
-            title="Save"
-            variant="primary"
-            accent="blue"
-            size="medium"
-            onClick={(event) => handleSave()}
-          />
-        </SaveButtonContainer>
-      </SettingsPageContainer>
-    </SubMenuTopBarContainer>
-  );
+        <Button
+        title="Save"
+        variant="primary"
+        accent="blue"
+        size="medium"
+        />
+    </SaveButtonContainer>
+    </SettingsPageContainer>
+    
+</SubMenuTopBarContainer>
+
+)
+  
 };
