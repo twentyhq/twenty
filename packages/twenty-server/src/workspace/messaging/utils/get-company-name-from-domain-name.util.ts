eb71ce9986756544ac654128789a9d33c07f6323
bosiraphael <raphael.bosi@gmail.com>
2024-03-01 15:35:27 +0100

    add domain parsing library

--- packages/twenty-server/src/workspace/messaging/utils/get-company-name-from-domain-name.util.ts
+++ packages/twenty-server/src/workspace/messaging/utils/get-company-name-from-domain-name.util.ts
@@ -1,5 +1,15 @@
+import { parseDomain, fromUrl, ParseResultType } from 'parse-domain';
+
 import { capitalize } from 'src/utils/capitalize';
 
-export const getCompanyNameFromDomainName = (domainName: string): string => {
-  return capitalize(domainName.split('.').slice(-2, -1)[0]);
+export const getCompanyNameFromDomainName = (domainName: string) => {
+  const parseResult = parseDomain(fromUrl(domainName));
+
+  if (parseResult.type === ParseResultType.Listed) {
+    const { domain } = parseResult;
+
+    return domain ? capitalize(domain) : '';
+  }
+
+  return '';
 };
--- packages/twenty-server/src/workspace/messaging/utils/get-domain-name-from-handle.util.ts
+++ packages/twenty-server/src/workspace/messaging/utils/get-domain-name-from-handle.util.ts
@@ -1,3 +1,34 @@
+import { fromUrl, parseDomain, ParseResultType } from 'parse-domain';
+
+import { capitalize } from 'src/utils/capitalize';
+
 export function getDomainNameFromHandle(handle: string): string {
-  return handle.split('@')?.[1].split('.').slice(-2).join('.').toLowerCase();
+  const wholeDomain = handle.split('@')?.[1];
+
+  const parseResult = parseDomain(fromUrl(wholeDomain));
+
+  if (parseResult.type === ParseResultType.Listed) {
+    const { domain } = parseResult;
+
+    return domain || '';
+  } else {
+    return '';
+  }
+}
+
+export function getCompanyNameAndDomainNameFromHandle(handle: string) {
+  const wholeDomain = handle.split('@')?.[1];
+
+  const parseResult = parseDomain(fromUrl(wholeDomain));
+
+  if (parseResult.type === ParseResultType.Listed) {
+    const { domain, topLevelDomains } = parseResult;
+
+    return {
+      domainName: `${domain}.${topLevelDomains.join('.')}`,
+      companyName: domain ? capitalize(domain) : '',
+    };
+  } else {
+    return { domain: '', companyName: '' };
+  }
 }
