78df6de3ff9a60d70c0845e3ea6faa77280498e0
martmull <martmull@hotmail.fr>
2024-03-05 14:39:27 +0100

    WIP: add menu page

---
+++ packages/twenty-front/src/pages/settings/SettingsBilling.tsx
@@ -0,0 +1,9 @@
+import { IconCurrencyDollar } from '@tabler/icons-react';
+
+import { SubMenuTopBarContainer } from '@/ui/layout/page/SubMenuTopBarContainer';
+export const SettingsBilling = () => (
+  <SubMenuTopBarContainer
+    Icon={IconCurrencyDollar}
+    title="Settings"
+  ></SubMenuTopBarContainer>
+);
