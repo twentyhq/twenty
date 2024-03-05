f4e934bd810ea01e0690e1009e3989a3c20d57fc
Hyunsu <hs9880@gmail.com>
2024-03-06 05:36:12 +0900

    fix: state consistency issue while closing the email thread right drawer (#4205)

--- packages/twenty-front/src/modules/activities/emails/hooks/useEmailThread.ts
+++ packages/twenty-front/src/modules/activities/emails/hooks/useEmailThread.ts
@@ -1,22 +1,37 @@
-import { useRecoilState } from 'recoil';
+import { useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import { useOpenEmailThreadRightDrawer } from '@/activities/emails/right-drawer/hooks/useOpenEmailThreadRightDrawer';
 import { viewableEmailThreadIdState } from '@/activities/emails/state/viewableEmailThreadIdState';
+import { useRightDrawer } from '@/ui/layout/right-drawer/hooks/useRightDrawer';
+import { isRightDrawerOpenState } from '@/ui/layout/right-drawer/states/isRightDrawerOpenState';
 
 export const useEmailThread = () => {
-  const [, setViewableEmailThreadId] = useRecoilState(
+  const [viewableEmailThreadId, setViewableEmailThreadId] = useRecoilState(
     viewableEmailThreadIdState,
   );
-
+  const { closeRightDrawer } = useRightDrawer();
   const openEmailThredRightDrawer = useOpenEmailThreadRightDrawer();
+  const isRightDrawerOpen = useRecoilValue(isRightDrawerOpenState);
+  const [isDrawerActive, setIsDrawerActive] = useState(false);
+
+  useEffect(() => {
+    if (isRightDrawerOpen) {
+      setIsDrawerActive(true);
+      return;
+    }
+    setIsDrawerActive(false);
+  }, [isRightDrawerOpen]);
 
   const openEmailThread = (threadId: string) => {
+    if (viewableEmailThreadId === threadId && isDrawerActive) {
+      setViewableEmailThreadId(null);
+      closeRightDrawer();
+      return;
+    }
     openEmailThredRightDrawer();
-
     setViewableEmailThreadId(threadId);
   };
 
-  return {
-    openEmailThread,
-  };
+  return { openEmailThread };
 };
