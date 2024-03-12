a9a76388b94a1a4813f11de587efd005d79fce61
procrastinatorMuffin <danil.sholudko@icloud.com>
2024-03-12 22:07:52 +0100

    Update backdrop-filter in OverlayBackground.ts

--- packages/twenty-front/src/modules/ui/theme/constants/OverlayBackground.ts
+++ packages/twenty-front/src/modules/ui/theme/constants/OverlayBackground.ts
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 import { ThemeType } from './ThemeLight';
 
 export const OVERLAY_BACKGROUND = (props: { theme: ThemeType }) => css`
-  backdrop-filter: blur(8px);
+  backdrop-filter: blur(12px) saturate(200%) contrast(50%) brightness(130%)
   background: ${props.theme.background.transparent.secondary};
   box-shadow: ${props.theme.boxShadow.strong};
 `;
