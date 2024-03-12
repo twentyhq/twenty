62d414ee666f0849eb89ba044970b88925560dbe
martmull <martmull@hotmail.fr>
2024-03-12 18:10:27 +0100

    40 remove self billing feature flag (#4379)
    * Define quantity at checkout
    
    * Remove billing submenu when not isBillingEnabled
    
    * Remove feature flag
    
    * Log warning when missing subscription active workspace add or remove member
    
    * Display subscribe cta for free usage of twenty
    
    * Authorize all settings when subscription canceled or unpaid
    
    * Display subscribe cta for workspace with canceled subscription
    
    * Replace OneToOne by OneToMany
    
    * Add a currentBillingSubscriptionField
    
    * Handle multiple subscriptions by workspace
    
    * Fix redirection
    
    * Fix test
    
    * Fix billingState

--- packages/twenty-server/src/core/billing/entities/billing-subscription.entity.ts
+++ packages/twenty-server/src/core/billing/entities/billing-subscription.entity.ts
@@ -1,20 +1,25 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
 import {
   Column,
   CreateDateColumn,
   Entity,
   JoinColumn,
+  ManyToOne,
   OneToMany,
-  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import Stripe from 'stripe';
+import { IDField } from '@ptc-org/nestjs-query-graphql';
 
 import { Workspace } from 'src/core/workspace/workspace.entity';
 import { BillingSubscriptionItem } from 'src/core/billing/entities/billing-subscription-item.entity';
 
 @Entity({ name: 'billingSubscription', schema: 'core' })
+@ObjectType('BillingSubscription')
 export class BillingSubscription {
+  @IDField(() => ID)
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
@@ -27,7 +32,7 @@ export class BillingSubscription {
   @UpdateDateColumn({ type: 'timestamp with time zone' })
   updatedAt: Date;
 
-  @OneToOne(() => Workspace, (workspace) => workspace.billingSubscription, {
+  @ManyToOne(() => Workspace, (workspace) => workspace.billingSubscriptions, {
     onDelete: 'CASCADE',
   })
   @JoinColumn()
@@ -36,12 +41,13 @@ export class BillingSubscription {
   @Column({ nullable: false, type: 'uuid' })
   workspaceId: string;
 
-  @Column({ unique: true, nullable: false })
+  @Column({ nullable: false })
   stripeCustomerId: string;
 
   @Column({ unique: true, nullable: false })
   stripeSubscriptionId: string;
 
+  @Field()
   @Column({ nullable: false })
   status: Stripe.Subscription.Status;
 
