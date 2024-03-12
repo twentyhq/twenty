490b81360b1bb881182809ada49de19eeafb63ad
Félix Malfait <felix.malfait@gmail.com>
2024-03-12 14:54:34 +0100

    Add Dockerfile and stop injecting env-config

--- packages/twenty-front/src/config/index.ts
+++ packages/twenty-front/src/config/index.ts
@@ -1,11 +1,18 @@
 declare global {
   interface Window {
-    _env_?: Record<string, string>;
     __APOLLO_CLIENT__?: any;
   }
 }
 
+const getBaseUrl = () => {
+  if (window.location.hostname === 'localhost') {
+    return 'http://localhost:3000';
+  } else {
+    return `${window.location.protocol}//${window.location.hostname}${
+      window.location.port ? `:${window.location.port}` : ''
+    }`;
+  }
+};
+
 export const REACT_APP_SERVER_BASE_URL =
-  window._env_?.REACT_APP_SERVER_BASE_URL ||
-  process.env.REACT_APP_SERVER_BASE_URL ||
-  'http://localhost:3000';
+  process.env.REACT_APP_SERVER_BASE_URL || getBaseUrl();
