a02e11f81a285fc212e1fd2b320ad73bbc484dd5
Thomas Trompette <thomas.trompette@sfr.fr>
2024-03-13 14:47:54 +0100

    Use prepared statements + add tests for record position (#4451)
    Use prepared statements + add tests
    
    Co-authored-by: Thomas Trompette <thomast@twenty.com>

--- packages/twenty-server/src/workspace/workspace-query-builder/factories/__tests__/record-position-query.factory.spec.ts
+++ packages/twenty-server/src/workspace/workspace-query-builder/factories/__tests__/record-position-query.factory.spec.ts
@@ -16,35 +16,73 @@ describe('RecordPositionQueryFactory', () => {
   });
 
   describe('create', () => {
-    it('should return a string with the position when positionValue is first', async () => {
-      const positionValue = 'first';
+    describe('createForGet', () => {
+      it('should return a string with the position when positionValue is first', async () => {
+        const positionValue = 'first';
 
-      const result = await factory.create(
-        RecordPositionQueryType.GET,
-        positionValue,
-        objectMetadataItem,
-        dataSourceSchema,
-      );
+        const result = await factory.create(
+          RecordPositionQueryType.GET,
+          positionValue,
+          objectMetadataItem,
+          dataSourceSchema,
+        );
 
-      expect(result).toEqual(
-        `SELECT position FROM workspace_test."company"
+        expect(result).toEqual(
+          `SELECT position FROM workspace_test."company"
             WHERE "position" IS NOT NULL ORDER BY "position" ASC LIMIT 1`,
-      );
+        );
+      });
+
+      it('should return a string with the position when positionValue is last', async () => {
+        const positionValue = 'last';
+
+        const result = await factory.create(
+          RecordPositionQueryType.GET,
+          positionValue,
+          objectMetadataItem,
+          dataSourceSchema,
+        );
+
+        expect(result).toEqual(
+          `SELECT position FROM workspace_test."company"
+            WHERE "position" IS NOT NULL ORDER BY "position" DESC LIMIT 1`,
+        );
+      });
     });
 
-    it('should return a string with the position when positionValue is last', async () => {
-      const positionValue = 'last';
+    it('should return a string with the position when positionValue is a number', async () => {
+      const positionValue = 1;
+
+      try {
+        await factory.create(
+          RecordPositionQueryType.GET,
+          positionValue,
+          objectMetadataItem,
+          dataSourceSchema,
+        );
+      } catch (error) {
+        expect(error.message).toEqual(
+          'RecordPositionQueryType.GET requires positionValue to be a number',
+        );
+      }
+    });
+  });
+
+  describe('createForUpdate', () => {
+    it('should return a string when RecordPositionQueryType is UPDATE', async () => {
+      const positionValue = 1;
 
       const result = await factory.create(
-        RecordPositionQueryType.GET,
+        RecordPositionQueryType.UPDATE,
         positionValue,
         objectMetadataItem,
         dataSourceSchema,
       );
 
       expect(result).toEqual(
-        `SELECT position FROM workspace_test."company"
-            WHERE "position" IS NOT NULL ORDER BY "position" DESC LIMIT 1`,
+        `UPDATE workspace_test."company"
+            SET "position" = $1
+            WHERE "id" = $2`,
       );
     });
   });
