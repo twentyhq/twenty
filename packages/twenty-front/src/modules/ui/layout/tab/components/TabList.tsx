e3d29fd51081d28c6923c2da11b0aa9ff099b3f4
brendanlaschke <brendan.laschke@googlemail.com>
2024-03-16 14:22:40 +0100

    fix overlay scroll leaving gap

--- packages/twenty-front/src/modules/ui/layout/tab/components/TabList.tsx
+++ packages/twenty-front/src/modules/ui/layout/tab/components/TabList.tsx
@@ -5,6 +5,7 @@ import { useRecoilValue } from 'recoil';
 import { IconComponent } from '@/ui/display/icon/types/IconComponent';
 import { useTabList } from '@/ui/layout/tab/hooks/useTabList';
 import { TabListScope } from '@/ui/layout/tab/scopes/TabListScope';
+import { ScrollWrapper } from '@/ui/utilities/scroll/components/ScrollWrapper';
 
 import { Tab } from './Tab';
 
@@ -30,7 +31,6 @@ const StyledContainer = styled.div`
   height: 40px;
   padding-left: ${({ theme }) => theme.spacing(2)};
   user-select: none;
-  overflow: auto;
 `;
 
 export const TabList = ({ tabs, tabListId }: TabListProps) => {
@@ -46,24 +46,26 @@ export const TabList = ({ tabs, tabListId }: TabListProps) => {
 
   return (
     <TabListScope tabListScopeId={tabListId}>
-      <StyledContainer>
-        {tabs
-          .filter((tab) => !tab.hide)
-          .map((tab) => (
-            <Tab
-              id={tab.id}
-              key={tab.id}
-              title={tab.title}
-              Icon={tab.Icon}
-              active={tab.id === activeTabId}
-              onClick={() => {
-                setActiveTabId(tab.id);
-              }}
-              disabled={tab.disabled}
-              hasBetaPill={tab.hasBetaPill}
-            />
-          ))}
-      </StyledContainer>
+      <ScrollWrapper hideY>
+        <StyledContainer>
+          {tabs
+            .filter((tab) => !tab.hide)
+            .map((tab) => (
+              <Tab
+                id={tab.id}
+                key={tab.id}
+                title={tab.title}
+                Icon={tab.Icon}
+                active={tab.id === activeTabId}
+                onClick={() => {
+                  setActiveTabId(tab.id);
+                }}
+                disabled={tab.disabled}
+                hasBetaPill={tab.hasBetaPill}
+              />
+            ))}
+        </StyledContainer>
+      </ScrollWrapper>
     </TabListScope>
   );
 };
