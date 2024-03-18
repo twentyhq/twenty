9a6d9bc163eed95c74055e9969590184afb7c1d9
bosiraphael <raphael.bosi@gmail.com>
2024-03-18 12:26:30 +0100

    working on getCalendarEventsFromPersonIds

--- packages/twenty-server/src/engine/modules/calendar/timeline-calendar-event.resolver.ts
+++ packages/twenty-server/src/engine/modules/calendar/timeline-calendar-event.resolver.ts
@@ -2,7 +2,8 @@ import { UseGuards } from '@nestjs/common';
 import { Resolver } from '@nestjs/graphql';
 
 import { JwtAuthGuard } from 'src/engine/guards/jwt.auth.guard';
+import { TimelineCalendarEventsWithTotal } from 'src/engine/modules/calendar/dtos/timeline-calendar-events-with-total.dto';
 
 @UseGuards(JwtAuthGuard)
-@Resolver(() => null)
+@Resolver(() => TimelineCalendarEventsWithTotal)
 export class TimelineCalendarEventResolver {}
--- packages/twenty-server/src/engine/modules/calendar/timeline-calendar-event.service.ts
+++ packages/twenty-server/src/engine/modules/calendar/timeline-calendar-event.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+import { groupBy } from 'lodash.groupBy';
+
+import { TIMELINE_EVENTS_DEFAULT_PAGE_SIZE } from 'src/engine/modules/calendar/constants/calendar.constants';
+import { TimelineCalendarEventsWithTotal } from 'src/engine/modules/calendar/dtos/timeline-calendar-events-with-total.dto';
 import { WorkspaceDataSourceService } from 'src/engine/workspace-datasource/workspace-datasource.service';
 
 @Injectable()
@@ -7,4 +11,62 @@ export class TimelineCalendarEventService {
   constructor(
     private readonly workspaceDataSourceService: WorkspaceDataSourceService,
   ) {}
+
+  async getCalendarEventsFromPersonIds(
+    workspaceMemberId: string,
+    workspaceId: string,
+    personIds: string[],
+    page: number = 1,
+    pageSize: number = TIMELINE_EVENTS_DEFAULT_PAGE_SIZE,
+  ): Promise<TimelineCalendarEventsWithTotal> {
+    const offset = (page - 1) * pageSize;
+
+    const dataSourceSchema =
+      this.workspaceDataSourceService.getSchemaName(workspaceId);
+
+    const calendarEvents =
+      await this.workspaceDataSourceService.executeRawQuery(
+        `SELECT
+      calendarEvent.*,
+      FROM ${dataSourceSchema}.calendarEvent calendarEvent
+      INNER JOIN ${dataSourceSchema}.calendarEventAttendee calendarEventAttendee ON calendarEvent.id = calendarEventAttendee.calendarEventId
+      WHERE calendarEventAttendee.personId IN ANY($1)
+      GROUP BY calendarEvent.id,
+      ORDER BY calendarEvent.startDateTime DESC
+      LIMIT $2
+      OFFSET $3`,
+        [personIds, pageSize, offset],
+        workspaceId,
+      );
+
+    if (!calendarEvents) {
+      return {
+        totalNumberOfCalendarEvents: 0,
+        timelineCalendarEvents: [],
+      };
+    }
+
+    const calendarEventAttendees =
+      await this.workspaceDataSourceService.executeRawQuery(
+        `SELECT *
+      FROM ${dataSourceSchema}.calendarEventAttendee calendarEventAttendee
+      WHERE calendarEventAttendee.calendarEventId IN ANY($1)`,
+        [calendarEvents.map((event) => event.id)],
+        workspaceId,
+      );
+
+    const calendarEventAttendeesByEventId = groupBy(
+      calendarEventAttendees,
+      'calendarEventId',
+    );
+
+    const timelineCalendarEvents = calendarEvents.map((event) => {
+      const attendees = calendarEventAttendeesByEventId[event.id] || [];
+
+      return {
+        ...event,
+        attendees,
+      };
+    });
+  }
 }
