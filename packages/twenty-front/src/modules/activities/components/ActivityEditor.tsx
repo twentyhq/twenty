71f34b0467e6eaa36c4f697608cae883a2bedfe2
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-03-19 07:11:00 +0000

    fix panel opening wrapping glitch (#4204)

--- packages/twenty-front/src/modules/activities/components/ActivityEditor.tsx
+++ packages/twenty-front/src/modules/activities/components/ActivityEditor.tsx
@@ -18,6 +18,7 @@ const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
+  min-width: 300px;
   overflow-y: auto;
   gap: ${({ theme }) => theme.spacing(4)};
 `;
