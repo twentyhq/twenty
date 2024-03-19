d3dd05916ca1c753f103f464629b2908e2350c22
Félix Malfait <felix.malfait@gmail.com>
2024-03-19 12:35:50 +0100

    Simplify code

--- packages/twenty-server/src/engine/api/graphql/workspace-query-runner/listeners/entity-events-to-db.listener.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-query-runner/listeners/entity-events-to-db.listener.ts
@@ -3,8 +3,6 @@ import { OnEvent } from '@nestjs/event-emitter';
 
 import { MessageQueue } from 'src/engine/integrations/message-queue/message-queue.constants';
 import { MessageQueueService } from 'src/engine/integrations/message-queue/services/message-queue.service';
-import { ObjectRecordUpdateEvent } from 'src/engine/integrations/event-emitter/types/object-record-update.event';
-import { ObjectRecordDeleteEvent } from 'src/engine/integrations/event-emitter/types/object-record-delete.event';
 import { ObjectRecordCreateEvent } from 'src/engine/integrations/event-emitter/types/object-record-create.event';
 import {
   SaveEventToDbJobData,
@@ -18,30 +16,8 @@ export class EntityEventsToDbListener {
     private readonly messageQueueService: MessageQueueService,
   ) {}
 
-  @OnEvent('*.created')
-  async handleAllCreate(payload: ObjectRecordCreateEvent<any>) {
-    this.messageQueueService.add<SaveEventToDbJobData>(SaveEventToDbJob.name, {
-      workspaceId: payload.workspaceId,
-      recordId: payload.recordId,
-      objectName: payload.objectMetadata.nameSingular,
-      operation: payload.operation,
-      details: payload.details,
-    });
-  }
-
-  @OnEvent('*.updated')
-  async handleAllUpdate(payload: ObjectRecordUpdateEvent<any>) {
-    this.messageQueueService.add<SaveEventToDbJobData>(SaveEventToDbJob.name, {
-      workspaceId: payload.workspaceId,
-      recordId: payload.recordId,
-      objectName: payload.objectMetadata.nameSingular,
-      operation: payload.operation,
-      details: payload.details,
-    });
-  }
-
-  @OnEvent('*.deleted')
-  async handleAllDelete(payload: ObjectRecordDeleteEvent<any>) {
+  @OnEvent('*.*')
+  async handle(payload: ObjectRecordCreateEvent<any>) {
     this.messageQueueService.add<SaveEventToDbJobData>(SaveEventToDbJob.name, {
       workspaceId: payload.workspaceId,
       recordId: payload.recordId,
