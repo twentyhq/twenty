e908aeb0293ab65fc441efbd9e38398629cfb329
Jérémy Magrin <jeremy.magrin@gmail.com>
2024-03-20 15:39:44 +0100

    fix: merge

--- packages/twenty-server/src/engine/core-modules/user/services/user.service.spec.ts
+++ packages/twenty-server/src/engine/core-modules/user/services/user.service.spec.ts
@@ -4,7 +4,7 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/engine/core-modules/user/user.entity';
 import { DataSourceService } from 'src/engine/metadata-modules/data-source/data-source.service';
 import { TypeORMService } from 'src/database/typeorm/typeorm.service';
-import { UserWorkspace } from 'src/engine/modules/user-workspace/user-workspace.entity';
+import { UserWorkspace } from 'src/engine/core-modules/user-workspace/user-workspace.entity';
 
 import { UserService } from './user.service';
 
