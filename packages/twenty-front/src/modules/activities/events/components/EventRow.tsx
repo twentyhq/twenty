b059612aaa1fb0de0feff17fb99702e2151dda40
Félix Malfait <felix.malfait@gmail.com>
2024-03-22 10:50:48 +0100

    Add frontend tests

--- packages/twenty-front/src/modules/activities/events/components/EventRow.tsx
+++ packages/twenty-front/src/modules/activities/events/components/EventRow.tsx
@@ -2,8 +2,11 @@ import { Event } from '@/activities/events/types/Event';
 
 export const EventRow = ({ event }: { event: Event }) => {
   return (
-    <p>
-      {event.name}:<pre>{event.properties}</pre>
-    </p>
+    <>
+      <p>{JSON.stringify(event)}</p>
+      <p>
+        {event.name}:<pre>{event.properties}</pre>
+      </p>
+    </>
   );
 };
