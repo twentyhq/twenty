a9dd44e8e7da72f150bd1e9eb58dbe59ca75478e
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-03-23 01:02:15 +0530

    Add turnstile and hcaptcha integration

--- packages/twenty-front/src/modules/auth/hooks/useGenerateCaptchaToken.ts
+++ packages/twenty-front/src/modules/auth/hooks/useGenerateCaptchaToken.ts
@@ -7,11 +7,13 @@ import { CaptchaDriverType } from '~/generated-metadata/graphql';
 declare global {
   interface Window {
     grecaptcha: any;
+    turnstile: any;
+    hcaptcha: any;
   }
 }
 
 export const useGenerateCaptchaToken = () => {
-  const captchaProvider = useRecoilValue(captchaProviderState());
+  const captchaProvider = useRecoilValue(captchaProviderState);
 
   const generateGoogleRecaptchaToken = useCallback(async () => {
     if (
@@ -23,6 +25,32 @@ export const useGenerateCaptchaToken = () => {
     }
   }, [captchaProvider?.provider, captchaProvider?.siteKey]);
 
+  const generateTurnstileCaptchaToken = useCallback(async () => {
+    if ( 'turnstile' in window && captchaProvider?.provider && captchaProvider.siteKey) {
+      return new Promise((resolve) => {
+        (window.turnstile as any).render('#captcha-widget', {
+          sitekey: captchaProvider.siteKey,
+          callback: (token: string) => {
+            resolve(token);
+          }
+        });
+      });
+    }
+  }, [captchaProvider?.provider, captchaProvider?.siteKey]);
+
+  const generateHcaptchaToken = useCallback(async () => {
+    if ( 'hcaptcha' in window && captchaProvider?.provider && captchaProvider.siteKey) {
+      return new Promise((resolve) => {
+        (window.hcaptcha as any).render('#captcha-widget', {
+          sitekey: captchaProvider.siteKey,
+          callback: (token: string) => {
+            resolve(token);
+          }
+        });
+      });
+    }
+  }, []);
+
   const generateCaptchaToken = useCallback(() => {
     if (!captchaProvider?.provider || !captchaProvider.siteKey) {
       return;
@@ -30,7 +58,10 @@ export const useGenerateCaptchaToken = () => {
     switch (captchaProvider.provider) {
       case CaptchaDriverType.GoogleRecatpcha:
         return generateGoogleRecaptchaToken();
-        break;
+      case CaptchaDriverType.Turnstile:
+        return generateTurnstileCaptchaToken();
+      case CaptchaDriverType.HCaptcha:
+        return generateHcaptchaToken();
     }
   }, [captchaProvider?.provider, captchaProvider?.siteKey]);
 
