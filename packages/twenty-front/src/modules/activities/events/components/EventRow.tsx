6571025b865661aaaeba240bf237b27dafa7fcde
Félix Malfait <felix.malfait@gmail.com>
2024-03-22 09:21:01 +0100

    Fix handling of json field on frontend

--- packages/twenty-front/src/modules/activities/events/components/EventRow.tsx
+++ packages/twenty-front/src/modules/activities/events/components/EventRow.tsx
@@ -3,7 +3,7 @@ import { Event } from '@/activities/events/types/Event';
 export const EventRow = ({ event }: { event: Event }) => {
   return (
     <p>
-      {event.name}: {JSON.stringify(event.properties)}
+      {event.name}:<pre>{event.properties}</pre>
     </p>
   );
 };
