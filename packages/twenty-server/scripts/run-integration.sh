e576fe0d67b315e993fd0f3699dd18931beaf348
Charles Bochet <charlesBochet@users.noreply.github.com>
2024-03-25 12:31:18 +0100

    Update yarn commands (#4644)
    * Simplify commands
    
    * Simplify commands
    
    * Migrate all dev commands to project.json
    
    * Fix tests

--- packages/twenty-server/scripts/run-integration.sh
+++ packages/twenty-server/scripts/run-integration.sh
@@ -4,5 +4,5 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/set-env-test.sh
 
-yarn database:init
-yarn jest --config ./test/jest-e2e.json
+yarn nx database:reset
+yarn nx jest --config ./test/jest-e2e.json
