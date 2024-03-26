046a8fcb0cedb2caa64a40336bc79527ee1393c8
martmull <martmull@hotmail.fr>
2024-03-26 12:13:41 +0100

    Revert "Use isSvg package"
    This reverts commit 05014b51076505d8766ca8c4c3233f6563869b17.

--- packages/twenty-server/src/engine/core-modules/file/file-upload/services/file-upload.service.ts
+++ packages/twenty-server/src/engine/core-modules/file/file-upload/services/file-upload.service.ts
@@ -4,7 +4,6 @@ import sharp from 'sharp';
 import { v4 as uuidV4 } from 'uuid';
 import { JSDOM } from 'jsdom';
 import DOMPurify from 'dompurify';
-import isSvg from 'is-svg';
 
 import { FileFolder } from 'src/engine/core-modules/file/interfaces/file-folder.interface';
 
@@ -35,12 +34,22 @@ export class FileUploadService {
     });
   }
 
-  private _sanitizeFile(
-    file: Buffer | Uint8Array | string,
-  ): Buffer | Uint8Array | string {
+  private _sanitizeFile({
+    file,
+    ext,
+    mimeType,
+  }: {
+    file: Buffer | Uint8Array | string;
+    ext: string;
+    mimeType: string | undefined;
+  }): Buffer | Uint8Array | string {
     const fileString = file.toString();
 
-    if (isSvg(fileString)) {
+    if (
+      fileString.includes('svg') &&
+      ext === 'svg' &&
+      mimeType === 'image/svg+xml'
+    ) {
       const window = new JSDOM('').window;
       const purify = DOMPurify(window);
 
@@ -66,7 +75,7 @@ export class FileUploadService {
     const name = `${id}${ext ? `.${ext}` : ''}`;
 
     await this._uploadFile({
-      file: this._sanitizeFile(file),
+      file: this._sanitizeFile({ file, ext, mimeType }),
       filename: name,
       mimeType,
       fileFolder,
