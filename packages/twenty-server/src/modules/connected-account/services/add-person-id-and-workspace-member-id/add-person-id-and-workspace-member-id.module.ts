68977dc6753097b969d2807d16c56fe32607eea8
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2024-03-29 10:03:00 +0100

    Calendar event fixes and improvements (#4690)
    * fixes
    
    * saving workspaceMemberId and personId when saving attendees
    
    * add typing
    
    * use Map
    
    * improve saveMessageParticipants
    
    * fix role type
    
    * move logic in a service
    
    * create new service
    
    * use new service in calendar-event-attendee.service
    
    * modify service to include more common logic
    
    * add defaumt value to isOrganizer in calendar-event-attendee.object-metadata
    
    * rename folder
    
    * renaming

--- packages/twenty-server/src/modules/connected-account/services/add-person-id-and-workspace-member-id/add-person-id-and-workspace-member-id.module.ts
+++ packages/twenty-server/src/modules/connected-account/services/add-person-id-and-workspace-member-id/add-person-id-and-workspace-member-id.module.ts
@@ -2,17 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
 import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
-import { AddPersonIdAndWorkspaceMemberIdModule } from 'src/modules/connected-account/services/add-person-id-and-workspace-member-id/add-person-id-and-workspace-member-id.module';
-import { MessageParticipantService } from 'src/modules/messaging/services/message-participant/message-participant.service';
+import { AddPersonIdAndWorkspaceMemberIdService } from 'src/modules/connected-account/services/add-person-id-and-workspace-member-id/add-person-id-and-workspace-member-id.service';
 import { PersonObjectMetadata } from 'src/modules/person/standard-objects/person.object-metadata';
 
 @Module({
   imports: [
     WorkspaceDataSourceModule,
     ObjectMetadataRepositoryModule.forFeature([PersonObjectMetadata]),
-    AddPersonIdAndWorkspaceMemberIdModule,
   ],
-  providers: [MessageParticipantService],
-  exports: [MessageParticipantService],
+  providers: [AddPersonIdAndWorkspaceMemberIdService],
+  exports: [AddPersonIdAndWorkspaceMemberIdService],
 })
-export class MessageParticipantModule {}
+export class AddPersonIdAndWorkspaceMemberIdModule {}
