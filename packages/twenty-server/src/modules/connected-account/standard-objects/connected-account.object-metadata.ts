bc794c73d82104658ba76007b90b742c6c0e895e
bosiraphael <raphael.bosi@gmail.com>
2024-03-29 15:10:22 +0100

    add IsNullable decorator

--- packages/twenty-server/src/modules/connected-account/standard-objects/connected-account.object-metadata.ts
+++ packages/twenty-server/src/modules/connected-account/standard-objects/connected-account.object-metadata.ts
@@ -8,6 +8,7 @@ import { connectedAccountStandardFieldIds } from 'src/engine/workspace-manager/w
 import { standardObjectIds } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-object-ids';
 import { FieldMetadata } from 'src/engine/workspace-manager/workspace-sync-metadata/decorators/field-metadata.decorator';
 import { Gate } from 'src/engine/workspace-manager/workspace-sync-metadata/decorators/gate.decorator';
+import { IsNullable } from 'src/engine/workspace-manager/workspace-sync-metadata/decorators/is-nullable.decorator';
 import { IsSystem } from 'src/engine/workspace-manager/workspace-sync-metadata/decorators/is-system.decorator';
 import { ObjectMetadata } from 'src/engine/workspace-manager/workspace-sync-metadata/decorators/object-metadata.decorator';
 import { RelationMetadata } from 'src/engine/workspace-manager/workspace-sync-metadata/decorators/relation-metadata.decorator';
@@ -88,6 +89,7 @@ export class ConnectedAccountObjectMetadata extends BaseObjectMetadata {
     description: 'Auth failed at',
     icon: 'IconX',
   })
+  @IsNullable()
   authFailedAt: Date;
 
   @FieldMetadata({
