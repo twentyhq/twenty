f0bfa245273aadd7b76c8324dc43d47515b436ed
Ady Beraud <a.beraud96@gmail.com>
2024-04-02 22:38:58 +0200

    Making Contributors Background Responsive

--- packages/twenty-website/src/app/_components/oss-friends/Background.tsx
+++ packages/twenty-website/src/app/_components/oss-friends/Background.tsx
@@ -1,44 +1,83 @@
 'use client';
-
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
+
+const TOP_GRADIENT_STOP = 350;
+const MIDDLE_GRADIENT_STOP = 1200;
+const BOTTOM_GRADIENT_STOP = 300;
 
 const BackgroundContainer = styled.div`
   position: absolute;
-  top: 100%;
-  left: 0px;
-  width: 100%;
+  top: 0;
+  left: -50%;
+  right: -50%;
+  width: 220%;
   height: 100%;
-  max-height: 200%;
   background-image: url(https://framerusercontent.com/images/nqEmdwe7yDXNsOZovuxG5zvj2E.png);
   background-size: auto 20px;
   background-repeat: repeat;
   transform-origin: center center;
+  transform: rotate(-2deg);
   z-index: -2;
 `;
 
 const Gradient = styled.div`
   position: absolute;
-  width: 100%;
+  top: 0;
+  left: -50%;
+  right: -50%;
+  width: 220%;
   height: 100%;
-  max-height: 200%;
-  top: 100%;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  transform: rotate(-2deg);
   background: linear-gradient(
     185deg,
-    #fff 8.33%,
-    rgba(255, 255, 255, 0.08) 48.95%,
-    #fff 92.18%
+    #fff ${TOP_GRADIENT_STOP}px,
+    rgba(255, 255, 255, 0.08) ${MIDDLE_GRADIENT_STOP}px,
+    #fff calc(100% - ${BOTTOM_GRADIENT_STOP}px)
   );
   z-index: -1;
 `;
 
+type ContainerProps = {
+  containerHeight: number; // Specify that containerHeight is a number
+};
+
+const Container = styled.div<ContainerProps>`
+  z-index: -2;
+  top: 0;
+  position: absolute;
+  overflow: hidden;
+  width: 100vw;
+  height: ${({ containerHeight }) => containerHeight}px;
+`;
+
 export const Background = () => {
+  const [containerHeight, setContainerHeight] = useState(0);
+
+  useEffect(() => {
+    const updateHeight = () => {
+      const height = Math.max(
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight,
+        document.body.scrollHeight,
+        document.body.offsetHeight,
+      );
+
+      setContainerHeight(height);
+    };
+
+    updateHeight();
+
+    window.addEventListener('resize', updateHeight);
+
+    return () => window.removeEventListener('resize', updateHeight);
+  }, []);
+
   return (
-    <>
+    <Container containerHeight={containerHeight}>
       <BackgroundContainer />
       <Gradient />
-    </>
+    </Container>
   );
 };
