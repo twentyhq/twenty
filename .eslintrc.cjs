a3c8448077afe11634b2a91a208e24a7b1c8ae79
Thaïs Guigon <guigon.thais@gmail.com>
2024-04-02 19:22:01 +0200

    refactor: move storybook related rules to .eslintrc.react.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -118,21 +118,6 @@ module.exports = {
       },
     },
     {
-      files: ['*.stories.@(ts|tsx|js|jsx)'],
-      rules: {
-        '@typescript-eslint/no-non-null-assertion': 'off',
-      },
-    },
-    {
-      files: ['.storybook/main.@(js|cjs|mjs|ts)'],
-      rules: {
-        'storybook/no-uninstalled-addons': [
-          'error',
-          { packageJsonLocation: '../../package.json' },
-        ],
-      },
-    },
-    {
       files: ['**/constants/*.ts', '**/*.constants.ts'],
       rules: {
         '@typescript-eslint/naming-convention': [
