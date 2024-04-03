d566a53852f61695e889bc7ce8a29a6f90371583
Thomas Trompette <thomast@twenty.com>
2024-04-03 14:12:29 +0200

    Add comment on user mapping

--- packages/twenty-server/src/engine/api/graphql/workspace-query-builder/factories/foreign-data-wrapper-query.factory.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-query-builder/factories/foreign-data-wrapper-query.factory.ts
@@ -25,6 +25,7 @@ export class ForeignDataWrapperQueryFactory {
     foreignDataWrapperId: string,
     userMappingOptions: UserMappingOptions,
   ) {
+    // CURRENT_USER works for now since we are using only one user. But if we switch to a user per workspace, we need to change this.
     return `CREATE USER MAPPING IF NOT EXISTS FOR CURRENT_USER SERVER "${foreignDataWrapperId}" OPTIONS (user '${userMappingOptions.username}', password '${userMappingOptions.password}')`;
   }
 
