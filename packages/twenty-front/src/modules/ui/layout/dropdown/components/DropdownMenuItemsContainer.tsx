957aac55a81e42ba1e8b621b8f9b3de541c5f565
tp <tirth4ur@gmail.com>
2024-04-03 13:25:27 -0400

    adjust window size for scroll

--- packages/twenty-front/src/modules/ui/layout/dropdown/components/DropdownMenuItemsContainer.tsx
+++ packages/twenty-front/src/modules/ui/layout/dropdown/components/DropdownMenuItemsContainer.tsx
@@ -13,7 +13,7 @@ const StyledDropdownMenuItemsExternalContainer = styled.div<{
   flex-direction: column;
   gap: 2px;
   height: 100%;
-  max-height: ${({ hasMaxHeight }) => (hasMaxHeight ? '180px' : 'none')};
+  max-height: ${({ hasMaxHeight }) => (hasMaxHeight ? '155px' : 'none')};
   overflow-y: auto;
 
   padding: var(--padding);
