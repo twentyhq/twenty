7c106361771a28ff0ebd62fc101406cafed2aaa5
martmull <martmull@hotmail.fr>
2024-04-04 13:13:25 +0200

    Update table display

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/MultiSelectFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/MultiSelectFieldDisplay.tsx
@@ -1,6 +1,12 @@
+import styled from '@emotion/styled';
+
 import { useMultiSelectField } from '@/object-record/record-field/meta-types/hooks/useMultiSelectField.ts';
 import { Tag } from '@/ui/display/tag/components/Tag';
 
+const StyledTagContainer = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(1)};
+`;
 export const MultiSelectFieldDisplay = () => {
   const { fieldValue, fieldDefinition } = useMultiSelectField();
 
@@ -11,11 +17,11 @@ export const MultiSelectFieldDisplay = () => {
     : [];
 
   return selectedOptions ? (
-    <>
+    <StyledTagContainer>
       {selectedOptions.map((selectedOption) => (
         <Tag color={selectedOption.color} text={selectedOption.label} />
       ))}
-    </>
+    </StyledTagContainer>
   ) : (
     <></>
   );
