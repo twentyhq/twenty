ff54df513273f4c8feb338adc0a6a924b35c8ac7
Thomas Trompette <thomast@twenty.com>
2024-04-05 13:37:54 +0200

    Create mutation folder

--- packages/twenty-front/src/modules/databases/hooks/useCreateOneDatabaseConnection.ts
+++ packages/twenty-front/src/modules/databases/hooks/useCreateOneDatabaseConnection.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, useMutation } from '@apollo/client';
 import { getOperationName } from '@apollo/client/utilities';
 
-import { CREATE_ONE_DATABASE_CONNECTION } from '@/databases/graphql/queries/createOneDatabaseConnection';
+import { CREATE_ONE_DATABASE_CONNECTION } from '@/databases/graphql/mutations/createOneDatabaseConnection';
 import { GET_MANY_DATABASE_CONNECTIONS } from '@/databases/graphql/queries/findManyDatabaseConnections';
 import { useApolloMetadataClient } from '@/object-metadata/hooks/useApolloMetadataClient';
 import {
