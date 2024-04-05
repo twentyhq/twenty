f0d39c0fa17bd1ea755d165a5ce6302e095ef08c
bosiraphael <raphael.bosi@gmail.com>
2024-04-05 16:06:28 +0200

    Add CalendarEventParticipantService to MatchParticipantJob

--- packages/twenty-server/src/modules/connected-account/jobs/match-participant.job.ts
+++ packages/twenty-server/src/modules/connected-account/jobs/match-participant.job.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { MessageQueueJob } from 'src/engine/integrations/message-queue/interfaces/message-queue-job.interface';
 
+import { CalendarEventParticipantService } from 'src/modules/calendar/services/calendar-event-participant/calendar-event-participant.service';
 import { MessageParticipantService } from 'src/modules/messaging/services/message-participant/message-participant.service';
 
 export type MatchParticipantJobData = {
@@ -17,12 +18,20 @@ export class MatchParticipantJob
 {
   constructor(
     private readonly messageParticipantService: MessageParticipantService,
+    private readonly calendarEventParticipantService: CalendarEventParticipantService,
   ) {}
 
   async handle(data: MatchParticipantJobData): Promise<void> {
     const { workspaceId, email, personId, workspaceMemberId } = data;
 
     await this.messageParticipantService.matchMessageParticipants(
+      workspaceId,
+      email,
+      personId,
+      workspaceMemberId,
+    );
+
+    await this.calendarEventParticipantService.matchCalendarEventParticipants(
       workspaceId,
       email,
       personId,
