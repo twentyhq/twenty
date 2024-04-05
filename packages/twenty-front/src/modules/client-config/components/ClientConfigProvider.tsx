9f2c9ee76e7872698a92bc477926e511a101103f
Charles Bochet <charlesBochet@users.noreply.github.com>
2024-04-05 20:33:02 +0200

    Remove repetitive query of ClientConfig and CurrentWorkspace member (#4859)
    ## Removing repetitive queries (impacting performance on page load)
    
    We have recently introduced the capability to detect schema version
    mismatch. To do that, we add a new header in all our queries. On page
    load, this header is added once we know the currentUser and especially
    its currentWorkspace and related schema version.
    However, applying this header to apollo client will re-trigger all
    queries that have been already performed (GetClientConfig and
    GetCurrentUser). To avoid re-triggering them, I'm introducing two new
    "isLoaded" states and skip the query if the query has already been
    performed
    
    ## Fixing Relation Detail not displaying data on show page
    
    Small bug introduced in a previous PR

--- packages/twenty-front/src/modules/client-config/components/ClientConfigProvider.tsx
+++ packages/twenty-front/src/modules/client-config/components/ClientConfigProvider.tsx
@@ -1,64 +1,11 @@
-import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
-import { authProvidersState } from '@/client-config/states/authProvidersState';
-import { billingState } from '@/client-config/states/billingState';
-import { isDebugModeState } from '@/client-config/states/isDebugModeState';
-import { isSignInPrefilledState } from '@/client-config/states/isSignInPrefilledState';
-import { isSignUpDisabledState } from '@/client-config/states/isSignUpDisabledState';
-import { sentryConfigState } from '@/client-config/states/sentryConfigState';
-import { supportChatState } from '@/client-config/states/supportChatState';
-import { telemetryState } from '@/client-config/states/telemetryState';
-import { useGetClientConfigQuery } from '~/generated/graphql';
-import { isDefined } from '~/utils/isDefined';
+import { isClientConfigLoadedState } from '@/client-config/states/isClientConfigLoadedState';
 
 export const ClientConfigProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const setAuthProviders = useSetRecoilState(authProvidersState);
-  const setIsDebugMode = useSetRecoilState(isDebugModeState);
+  const isClientConfigLoaded = useRecoilValue(isClientConfigLoadedState);
 
-  const setIsSignInPrefilled = useSetRecoilState(isSignInPrefilledState);
-  const setIsSignUpDisabled = useSetRecoilState(isSignUpDisabledState);
-
-  const setBilling = useSetRecoilState(billingState);
-  const setTelemetry = useSetRecoilState(telemetryState);
-  const setSupportChat = useSetRecoilState(supportChatState);
-
-  const setSentryConfig = useSetRecoilState(sentryConfigState);
-
-  const { data, loading } = useGetClientConfigQuery();
-
-  useEffect(() => {
-    if (isDefined(data?.clientConfig)) {
-      setAuthProviders({
-        google: data?.clientConfig.authProviders.google,
-        password: data?.clientConfig.authProviders.password,
-        magicLink: false,
-      });
-      setIsDebugMode(data?.clientConfig.debugMode);
-      setIsSignInPrefilled(data?.clientConfig.signInPrefilled);
-      setIsSignUpDisabled(data?.clientConfig.signUpDisabled);
-
-      setBilling(data?.clientConfig.billing);
-      setTelemetry(data?.clientConfig.telemetry);
-      setSupportChat(data?.clientConfig.support);
-
-      setSentryConfig({
-        dsn: data?.clientConfig?.sentry?.dsn,
-      });
-    }
-  }, [
-    data,
-    setAuthProviders,
-    setIsDebugMode,
-    setIsSignInPrefilled,
-    setIsSignUpDisabled,
-    setTelemetry,
-    setSupportChat,
-    setBilling,
-    setSentryConfig,
-  ]);
-
-  return loading ? <></> : <>{children}</>;
+  return isClientConfigLoaded ? <>{children}</> : <></>;
 };
