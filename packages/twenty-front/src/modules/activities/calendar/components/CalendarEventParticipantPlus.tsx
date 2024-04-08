f7077db5529dd079a814ce6b608b701e37c8b8c9
bosiraphael <raphael.bosi@gmail.com>
2024-04-08 18:37:51 +0200

    improve plus button

--- packages/twenty-front/src/modules/activities/calendar/components/CalendarEventParticipantPlus.tsx
+++ packages/twenty-front/src/modules/activities/calendar/components/CalendarEventParticipantPlus.tsx
@@ -2,17 +2,29 @@ import styled from '@emotion/styled';
 
 const StyledCalendarEventParticipantPlusContainer = styled.div`
   align-items: center;
+  background: ${({ theme }) => theme.background.tertiary};
+  border-radius: 4px;
   display: flex;
+  height: 20px;
   justify-content: space-between;
+  padding: 3px 4px;
+  box-sizing: border-box;
+  cursor: pointer;
+
+  &:hover {
+    background: ${({ theme }) => theme.background.quaternary};
+  }
 `;
 
 export const CalendarEventParticipantPlus = ({
   number,
+  onClick,
 }: {
   number: number;
+  onClick?: () => void;
 }) => {
   return (
-    <StyledCalendarEventParticipantPlusContainer>
+    <StyledCalendarEventParticipantPlusContainer onClick={onClick}>
       +{number}
     </StyledCalendarEventParticipantPlusContainer>
   );
