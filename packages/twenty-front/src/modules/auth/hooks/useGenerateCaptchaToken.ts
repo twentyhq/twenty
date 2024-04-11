687dd33157fa070c3677d3467111b69ee277da87
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-11 22:02:52 +0530

    Refactor useGenerateCaptchaToken hook

--- packages/twenty-front/src/modules/auth/hooks/useGenerateCaptchaToken.ts
+++ packages/twenty-front/src/modules/auth/hooks/useGenerateCaptchaToken.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @nx/workspace-explicit-boolean-predicates-in-if */
 import { useCallback, useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 
@@ -18,112 +17,66 @@ export const useGenerateCaptchaToken = () => {
   // eslint-disable-next-line @nx/workspace-no-state-useref
   const captchaWidgetId = useRef<string | null>(null);
 
-  const generateGoogleRecaptchaToken = useCallback(async (): Promise<
-    string | undefined
-  > => {
-    if (
-      'grecaptcha' in window &&
-      captchaProvider?.provider &&
-      captchaProvider?.siteKey
-    ) {
-      if (captchaWidgetId.current === null) {
-        captchaWidgetId.current = (window.grecaptcha as any).render(
-          'captcha-widget',
-          {
-            sitekey: captchaProvider.siteKey,
-          },
-        );
-      }
-
-      return new Promise((resolve, reject) => {
-        (window.grecaptcha as any)
-          .execute(captchaWidgetId.current, { action: 'submit' })
-          .then((token: string) => resolve(token))
-          .catch((error: any) => reject(error));
-      });
-    }
-  }, [captchaProvider?.provider, captchaProvider?.siteKey]);
-
-  const generateTurnstileCaptchaToken = useCallback(async (): Promise<
-    string | undefined
-  > => {
-    if (
-      'turnstile' in window &&
-      captchaProvider?.provider &&
-      captchaProvider.siteKey
-    ) {
-      if (captchaWidgetId.current === null) {
-        captchaWidgetId.current = (window.turnstile as any).render(
-          // eslint-disable-next-line @nx/workspace-no-hardcoded-colors
-          '#captcha-widget',
-          {
-            sitekey: captchaProvider.siteKey,
-          },
-        );
-      }
-
-      return new Promise((resolve, reject) => {
-        window.turnstile.reset(captchaWidgetId.current);
-        window.turnstile.execute(captchaWidgetId.current, {
-          callback: (token: string) => {
-            resolve(token);
-          },
-          'error-callback': (error: any) => {
-            reject(error);
-          },
-        });
-      });
-    }
-  }, [captchaProvider?.provider, captchaProvider?.siteKey]);
-
-  const generateHcaptchaToken = useCallback(async (): Promise<
-    string | undefined
-  > => {
-    if (
-      'hcaptcha' in window &&
-      captchaProvider?.provider &&
-      captchaProvider.siteKey
-    ) {
-      if (captchaWidgetId.current === null) {
-        captchaWidgetId.current = (window.hcaptcha as any).render(
-          'captcha-widget',
-          {
-            sitekey: captchaProvider.siteKey,
-          },
-        );
-      }
-
-      return new Promise((resolve, reject) => {
-        window.hcaptcha
-          .execute(captchaWidgetId.current, { async: true })
-          .then(({ response }: any) => resolve(response))
-          .catch((error: any) => reject(error));
-      });
-    }
-  }, [captchaProvider?.provider, captchaProvider?.siteKey]);
-
   const generateCaptchaToken = useCallback(
-    (isLoaded: boolean) => {
+    async (isLoaded: boolean): Promise<string | undefined> => {
       if (!isLoaded || !captchaProvider?.provider || !captchaProvider.siteKey) {
         return;
       }
 
+      const renderCaptchaWidget = (renderFn: any, widgetId: string) => {
+        if (captchaWidgetId.current === null) {
+          captchaWidgetId.current = renderFn(widgetId, {
+            sitekey: captchaProvider.siteKey,
+          });
+        }
+      };
+
+      const executeCaptcha = (executeFn: any): Promise<string | undefined> => {
+        return new Promise((resolve, reject) => {
+          executeFn(captchaWidgetId.current, { action: 'submit' })
+            .then((token: string) => resolve(token))
+            .catch((error: any) => reject(error));
+        });
+      };
+
       switch (captchaProvider.provider) {
         case CaptchaDriverType.GoogleRecatpcha:
-          return generateGoogleRecaptchaToken();
+          if ('grecaptcha' in window) {
+            renderCaptchaWidget(window.grecaptcha.render, 'captcha-widget');
+            return executeCaptcha(window.grecaptcha.execute);
+          }
+          break;
         case CaptchaDriverType.Turnstile:
-          return generateTurnstileCaptchaToken();
+          if ('turnstile' in window) {
+            // eslint-disable-next-line @nx/workspace-no-hardcoded-colors
+            renderCaptchaWidget(window.turnstile.render, '#captcha-widget');
+            window.turnstile.reset(captchaWidgetId.current);
+            return new Promise((resolve, reject) => {
+              window.turnstile.execute(captchaWidgetId.current, {
+                callback: (token: string) => {
+                  resolve(token);
+                },
+                'error-callback': (error: any) => {
+                  reject(error);
+                },
+              });
+            });
+          }
+          break;
         case CaptchaDriverType.HCaptcha:
-          return generateHcaptchaToken();
+          if ('hcaptcha' in window) {
+            renderCaptchaWidget(window.hcaptcha.render, 'captcha-widget');
+            return new Promise((resolve, reject) => {
+              window.hcaptcha
+                .execute(captchaWidgetId.current, { async: true })
+                .then(({ response }: any) => resolve(response))
+                .catch((error: any) => reject(error));
+            });
+          }
+          break;
       }
     },
-    [
-      captchaProvider?.provider,
-      captchaProvider?.siteKey,
-      generateGoogleRecaptchaToken,
-      generateTurnstileCaptchaToken,
-      generateHcaptchaToken,
-    ],
+    [captchaProvider?.provider, captchaProvider?.siteKey],
   );
 
   return { generateCaptchaToken };
