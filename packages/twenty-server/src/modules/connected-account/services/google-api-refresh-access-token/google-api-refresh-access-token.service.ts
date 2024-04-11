fc56775c2a7d9418589402ae7d017bf55b8b968e
Weiko <corentin@twenty.com>
2024-04-11 17:57:48 +0200

    [calendar/messaging] fix connected account auth failed should skip sync (#4920)
    - AuthFailedAt is set when a refreshToken is not valid and an
    accessToken can't be generated, meaning it will need a manual action
    from the user to provide a new refresh token.
    - Calendar/messaging jobs should not be executed if authFailedAt is not
    null.

--- packages/twenty-server/src/modules/connected-account/services/google-api-refresh-access-token/google-api-refresh-access-token.service.ts
+++ packages/twenty-server/src/modules/connected-account/services/google-api-refresh-access-token/google-api-refresh-access-token.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
 import { EnvironmentService } from 'src/engine/integrations/environment/environment.service';
-import { ExceptionHandlerService } from 'src/engine/integrations/exception-handler/exception-handler.service';
 import { InjectObjectMetadataRepository } from 'src/engine/object-metadata-repository/object-metadata-repository.decorator';
 import { ConnectedAccountRepository } from 'src/modules/connected-account/repositories/connected-account.repository';
 import { ConnectedAccountObjectMetadata } from 'src/modules/connected-account/standard-objects/connected-account.object-metadata';
@@ -14,7 +13,6 @@ export class GoogleAPIRefreshAccessTokenService {
     private readonly environmentService: EnvironmentService,
     @InjectObjectMetadataRepository(ConnectedAccountObjectMetadata)
     private readonly connectedAccountRepository: ConnectedAccountRepository,
-    private readonly exceptionHandlerService: ExceptionHandlerService,
   ) {}
 
   async refreshAndSaveAccessToken(
@@ -29,6 +27,12 @@ export class GoogleAPIRefreshAccessTokenService {
     if (!connectedAccount) {
       throw new Error(
         `No connected account found for ${connectedAccountId} in workspace ${workspaceId}`,
+      );
+    }
+
+    if (connectedAccount.authFailedAt) {
+      throw new Error(
+        `Skipping refresh of access token for connected account ${connectedAccountId} in workspace ${workspaceId} because auth already failed, a new refresh token is needed`,
       );
     }
 
@@ -82,11 +86,7 @@ export class GoogleAPIRefreshAccessTokenService {
         connectedAccountId,
         workspaceId,
       );
-      this.exceptionHandlerService.captureExceptions([error], {
-        user: {
-          workspaceId,
-        },
-      });
+
       throw new Error(`Error refreshing access token: ${error.message}`);
     }
   }
