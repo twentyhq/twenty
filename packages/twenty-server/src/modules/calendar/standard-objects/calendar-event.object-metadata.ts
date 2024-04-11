3bc2f8051a82e2216f7c228d77047e3b7e005a7d
bosiraphael <raphael.bosi@gmail.com>
2024-04-11 11:26:52 +0200

    rename eventParticipants in calendarEventParticipants

--- packages/twenty-server/src/modules/calendar/standard-objects/calendar-event.object-metadata.ts
+++ packages/twenty-server/src/modules/calendar/standard-objects/calendar-event.object-metadata.ts
@@ -171,7 +171,7 @@ export class CalendarEventObjectMetadata extends BaseObjectMetadata {
   calendarChannelEventAssociations: CalendarChannelEventAssociationObjectMetadata[];
 
   @FieldMetadata({
-    standardId: calendarEventStandardFieldIds.eventParticipants,
+    standardId: calendarEventStandardFieldIds.calendarEventParticipants,
     type: FieldMetadataType.RELATION,
     label: 'Event Participants',
     description: 'Event Participants',
