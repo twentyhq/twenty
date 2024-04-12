3caa3eb0f4a1aeb7071cb7c7c9e0cf78a589be71
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-13 02:03:38 +0530

    Update sync color & text

--- packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsSynchronizationStatus.tsx
+++ packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsSynchronizationStatus.tsx
@@ -1,27 +1,46 @@
+import { MessageChannelSyncStatus } from '@/accounts/types/MessageChannel';
 import { Status } from '@/ui/display/status/components/Status';
 
 export type SettingsAccountsSynchronizationStatusProps = {
-  syncStatus: 'synced' | 'failed' | 'notSynced';
+  syncStatus: MessageChannelSyncStatus;
+};
+
+const getColor = (syncStatus?: MessageChannelSyncStatus | null) => {
+  switch (syncStatus) {
+    case MessageChannelSyncStatus.FAILED:
+      return 'red';
+    case MessageChannelSyncStatus.PENDING:
+      return 'gray';
+    case MessageChannelSyncStatus.ONGOING:
+      return 'yellow';
+    case MessageChannelSyncStatus.SUCCEEDED:
+      return 'green';
+    default:
+      return 'gray';
+  }
+};
+
+const getText = (syncStatus?: MessageChannelSyncStatus | null) => {
+  switch (syncStatus) {
+    case MessageChannelSyncStatus.FAILED:
+      return 'Sync failed';
+    case MessageChannelSyncStatus.PENDING:
+      return 'Sync pending';
+    case MessageChannelSyncStatus.ONGOING:
+      return 'Syncing';
+    case MessageChannelSyncStatus.SUCCEEDED:
+      return 'Synced';
+    default:
+      return 'Sync pending';
+  }
 };
 
 export const SettingsAccountsSynchronizationStatus = ({
   syncStatus,
 }: SettingsAccountsSynchronizationStatusProps) => (
   <Status
-    color={
-      syncStatus === 'synced'
-        ? 'green'
-        : syncStatus === 'failed'
-          ? 'red'
-          : 'gray'
-    }
-    text={
-      syncStatus === 'synced'
-        ? 'Synced'
-        : syncStatus === 'failed'
-          ? 'Sync failed'
-          : 'Not synced'
-    }
+    color={getColor(syncStatus)}
+    text={getText(syncStatus)}
     weight="medium"
   />
 );
