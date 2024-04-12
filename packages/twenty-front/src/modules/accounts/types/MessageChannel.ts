63c15f3675aa848e91803db3b6efb0d3d91de2d7
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-13 02:02:31 +0530

    Add sync status enum & type

--- packages/twenty-front/src/modules/accounts/types/MessageChannel.ts
+++ packages/twenty-front/src/modules/accounts/types/MessageChannel.ts
@@ -1,9 +1,17 @@
 import { InboxSettingsVisibilityValue } from '@/settings/accounts/components/SettingsAccountsInboxVisibilitySettingsCard';
 
+export enum MessageChannelSyncStatus {
+  PENDING = 'PENDING',
+  ONGOING = 'ONGOING',
+  SUCCEEDED = 'SUCCEEDED',
+  FAILED = 'FAILED',
+}
+
 export type MessageChannel = {
   id: string;
   handle: string;
   isContactAutoCreationEnabled?: boolean;
   isSynced?: boolean;
   visibility: InboxSettingsVisibilityValue;
+  syncStatus: MessageChannelSyncStatus;
 };
