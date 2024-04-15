1c3775e4a0343dcb5147f47309a91f90671e34ae
martmull <martmull@hotmail.fr>
2024-04-15 20:05:59 +0200

    Fix Never api key expiration dates (#4965)
    closes #4714 
    
    We cannot set null expiration dates for api keys. So we will set to 100
    years instead of null. If apiKey expires in more that 10 years, it is
    displayed as "Never expires"

--- packages/twenty-front/src/modules/settings/developers/utils/__tests__/format-expiration.test.ts
+++ packages/twenty-front/src/modules/settings/developers/utils/__tests__/format-expiration.test.ts
@@ -3,6 +3,13 @@ import { formatExpiration } from '@/settings/developers/utils/format-expiration'
 jest.useFakeTimers().setSystemTime(new Date('2024-01-01T00:00:00.000Z'));
 
 describe('formatExpiration', () => {
+  it('should format properly when expiresAt is great', () => {
+    const expiresAt = '2044-01-10T00:00:00.000Z';
+    const result = formatExpiration(expiresAt);
+    expect(result).toEqual('Never');
+    const resultWithExpiresMention = formatExpiration(expiresAt, true);
+    expect(resultWithExpiresMention).toEqual('Never expires');
+  });
   it('should format properly when expiresAt is null', () => {
     const expiresAt = null;
     const result = formatExpiration(expiresAt);
@@ -18,17 +25,17 @@ describe('formatExpiration', () => {
     expect(resultWithExpiresMention).toEqual('Expires in 9 days');
   });
   it('should format properly when expiresAt is large', () => {
-    const expiresAt = '2034-01-10T00:00:00.000Z';
+    const expiresAt = '2032-01-10T00:00:00.000Z';
     const result = formatExpiration(expiresAt);
-    expect(result).toEqual('In 10 years');
+    expect(result).toEqual('In 8 years');
     const resultWithExpiresMention = formatExpiration(expiresAt, true);
-    expect(resultWithExpiresMention).toEqual('Expires in 10 years');
+    expect(resultWithExpiresMention).toEqual('Expires in 8 years');
   });
   it('should format properly when expiresAt is large and long version', () => {
-    const expiresAt = '2034-01-10T00:00:00.000Z';
+    const expiresAt = '2032-01-10T00:00:00.000Z';
     const result = formatExpiration(expiresAt, undefined, false);
-    expect(result).toEqual('In 10 years and 9 days');
+    expect(result).toEqual('In 8 years and 9 days');
     const resultWithExpiresMention = formatExpiration(expiresAt, true, false);
-    expect(resultWithExpiresMention).toEqual('Expires in 10 years and 9 days');
+    expect(resultWithExpiresMention).toEqual('Expires in 8 years and 9 days');
   });
 });
