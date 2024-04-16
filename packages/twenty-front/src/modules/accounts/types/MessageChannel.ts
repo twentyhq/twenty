60b732311fdd66406ca01945caecf8d685f5a41c
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-16 23:00:36 +0530

    remove static type

--- packages/twenty-front/src/modules/accounts/types/MessageChannel.ts
+++ packages/twenty-front/src/modules/accounts/types/MessageChannel.ts
@@ -1,17 +1,10 @@
 import { InboxSettingsVisibilityValue } from '@/settings/accounts/components/SettingsAccountsInboxVisibilitySettingsCard';
 
-export enum MessageChannelSyncStatus {
-  PENDING = 'PENDING',
-  ONGOING = 'ONGOING',
-  SUCCEEDED = 'SUCCEEDED',
-  FAILED = 'FAILED',
-}
-
 export type MessageChannel = {
   id: string;
   handle: string;
   isContactAutoCreationEnabled?: boolean;
   isSynced?: boolean;
   visibility: InboxSettingsVisibilityValue;
-  syncStatus: MessageChannelSyncStatus;
+  syncStatus: string;
 };
