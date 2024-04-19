877b81f19e9c96bcc6995d5deddc24f54ba29515
rathodvinod5 <rathodvinod5@gmail.com>
2024-04-19 21:49:19 +0530

    added code to search fields in filter menu

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/ObjectFilterDropdownFilterSelect.tsx
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/ObjectFilterDropdownFilterSelect.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+import styled from '@emotion/styled';
 import { useRecoilValue } from 'recoil';
 import { useIcons } from 'twenty-ui';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { useFilterDropdown } from '@/object-record/object-filter-dropdown/hooks/useFilterDropdown';
 import { RelationPickerHotkeyScope } from '@/object-record/relation-picker/types/RelationPickerHotkeyScope';
@@ -9,7 +12,34 @@ import { useSetHotkeyScope } from '@/ui/utilities/hotkey/hooks/useSetHotkeyScope
 
 import { getOperandsForFilterType } from '../utils/getOperandsForFilterType';
 
+export const StyledInput = styled.input`
+  background: ${({ theme }) => theme.background.secondary};
+  border: none;
+  border-top: 1px solid ${({ theme }) => theme.border.color.light};
+  border-bottom: 1px solid ${({ theme }) => theme.border.color.light};
+  border-radius: 0;
+  border-top-left-radius: ${({ theme }) => theme.border.radius.md};
+  border-top-right-radius: ${({ theme }) => theme.border.radius.md};
+  color: ${({ theme }) => theme.font.color.primary};
+  margin: 0;
+  outline: none;
+  padding: ${({ theme }) => theme.spacing(2)};
+  height: 20px;
+  font-family: inherit;
+  font-size: ${({ theme }) => theme.font.size.sm};
+
+  font-weight: inherit;
+  max-width: 100%;
+  overflow: hidden;
+  text-decoration: none;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.font.color.light};
+  }
+`;
+
 export const ObjectFilterDropdownFilterSelect = () => {
+  const [searchText, setSearchText] = useState('');
   const {
     setFilterDefinitionUsedInDropdown,
     setSelectedOperandInDropdown,
@@ -25,10 +55,28 @@ export const ObjectFilterDropdownFilterSelect = () => {
 
   const setHotkeyScope = useSetHotkeyScope();
 
+  const debouncedSetSearchFilter = useDebouncedCallback(setSearchText, 100, {
+    leading: true,
+  });
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSetSearchFilter(event.target.value);
+  };
+
   return (
+    <>
+    <StyledInput
+      value={searchText}
+      placeholder="Search fields"
+      onChange={handleSearchChange}
+    />
     <DropdownMenuItemsContainer>
       {[...availableFilterDefinitions]
         .sort((a, b) => a.label.localeCompare(b.label))
+        .filter((item) =>
+          item.label
+            .toLocaleLowerCase()
+            .includes(searchText.toLocaleLowerCase()),
+        )
         .map((availableFilterDefinition, index) => (
           <MenuItem
             key={`select-filter-${index}`}
@@ -51,5 +99,6 @@ export const ObjectFilterDropdownFilterSelect = () => {
           />
         ))}
     </DropdownMenuItemsContainer>
+    </>
   );
 };
