59898abc8f2c3151c99b889397d822a8ac92c45b
Marie Stoppa <marie.stoppa@essec.edu>
2024-04-19 15:01:59 +0200

    Format object, fields, relation names before create and save

---
+++ packages/twenty-server/src/engine/metadata-modules/object-metadata/utils/format-object-metadata-input.util.ts
@@ -0,0 +1,31 @@
+import { BadRequestException } from '@nestjs/common';
+
+import { ChararactersNotSupportedException } from 'src/engine/metadata-modules/errors/CharactersNotSupportedException';
+import { CreateObjectInput } from 'src/engine/metadata-modules/object-metadata/dtos/create-object.input';
+import { UpdateObjectInput } from 'src/engine/metadata-modules/object-metadata/dtos/update-object.input';
+import { formatString } from 'src/engine/metadata-modules/utils/format-string.util';
+
+export const formatObjectMetadataInput = (
+  objectMetadataInput: CreateObjectInput | UpdateObjectInput,
+) => {
+  try {
+    objectMetadataInput = {
+      ...objectMetadataInput,
+      nameSingular: objectMetadataInput.nameSingular
+        ? formatString(objectMetadataInput.nameSingular)
+        : objectMetadataInput.nameSingular,
+      namePlural: objectMetadataInput.namePlural
+        ? formatString(objectMetadataInput.namePlural)
+        : objectMetadataInput.namePlural,
+    };
+  } catch (error) {
+    if (error instanceof ChararactersNotSupportedException) {
+      console.error(error.message);
+      throw new BadRequestException(
+        `Characters used in name "${objectMetadataInput.nameSingular}" or "${objectMetadataInput.namePlural}" are not supported`,
+      );
+    } else {
+      throw error;
+    }
+  }
+};
