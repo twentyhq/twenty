2b9de4c4670db1d86f17c90b7c5e50dd1b2b8b75
Ady Beraud <a.beraud96@gmail.com>
2024-04-22 12:15:23 +0300

    separated database init and github sync

--- packages/twenty-website/src/database/init-database.ts
+++ packages/twenty-website/src/database/init-database.ts
@@ -1,7 +1,9 @@
-import { githubSynch } from '@/github-synch/github-synch';
+import { migrate } from '@/database/database';
+import { fetchAndSaveGithubData } from '@/github-sync/fetch-and-save-github-data';
 
 export const initDatabase = async () => {
-  await githubSynch();
+  await migrate();
+  await fetchAndSaveGithubData();
   process.exit(0);
 };
 
