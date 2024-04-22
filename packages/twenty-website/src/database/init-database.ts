0e74ec7de96abf54b1645a2d6c5e1046a099fba9
Félix Malfait <felix.malfait@gmail.com>
2024-04-22 11:33:22 +0200

    Remove github sync from db init

--- packages/twenty-website/src/database/init-database.ts
+++ packages/twenty-website/src/database/init-database.ts
@@ -1,9 +1,7 @@
 import { migrate } from '@/database/database';
-import { fetchAndSaveGithubData } from '@/github-sync/fetch-and-save-github-data';
 
 export const initDatabase = async () => {
   await migrate();
-  await fetchAndSaveGithubData();
   process.exit(0);
 };
 
