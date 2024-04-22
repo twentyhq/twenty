b7ebc2be2cdbc1e08b9b6f984e3aa541bdd2519c
Marie Stoppa <marie.stoppa@essec.edu>
2024-04-22 17:53:03 +0200

    Fix test file for format-string

--- packages/twenty-front/src/pages/settings/data-model/utils/__tests__/format-string.spec.ts
+++ packages/twenty-front/src/pages/settings/data-model/utils/__tests__/format-string.spec.ts
@@ -1,21 +1,19 @@
-import { ChararactersNotSupportedException } from 'src/engine/metadata-modules/errors/CharactersNotSupportedException';
-import {
-  formatString,
-  validPattern,
-} from 'src/engine/metadata-modules/utils/format-string.util';
+import { formatString } from '~/pages/settings/data-model/utils/format-string.util';
+
+const VALID_STRING_PATTERN = /^[a-zA-Z][a-zA-Z0-9 ]*$/;
 
 describe('formatString', () => {
   it('leaves strings unchanged if only latin characters', () => {
     const input = 'testName';
 
-    expect(formatString(input).match(validPattern)?.length).toBe(1);
+    expect(formatString(input).match(VALID_STRING_PATTERN)?.length).toBe(1);
     expect(formatString(input)).toEqual(input);
   });
 
   it('leaves strings unchanged if only latin characters and digits', () => {
     const input = 'testName123';
 
-    expect(formatString(input).match(validPattern)?.length).toBe(1);
+    expect(formatString(input).match(VALID_STRING_PATTERN)?.length).toBe(1);
     expect(formatString(input)).toEqual(input);
   });
 
@@ -23,7 +21,7 @@ describe('formatString', () => {
     const input = 'בְרִבְרִ';
     const expected = 'bRibRi';
 
-    expect(formatString(input).match(validPattern)?.length).toBe(1);
+    expect(formatString(input).match(VALID_STRING_PATTERN)?.length).toBe(1);
     expect(formatString(input)).toEqual(expected);
   });
 
@@ -31,20 +29,7 @@ describe('formatString', () => {
     const input = 'aa2בְרִבְרִ';
     const expected = 'aa2BRibRi';
 
-    expect(formatString(input).match(validPattern)?.length).toBe(1);
+    expect(formatString(input).match(VALID_STRING_PATTERN)?.length).toBe(1);
     expect(formatString(input)).toEqual(expected);
-  });
-
-  it('throws error if starts with digits', () => {
-    const input = '123string';
-
-    try {
-      formatString(input);
-    } catch (error: any) {
-      expect(error.name).toBe(ChararactersNotSupportedException.name);
-
-      return;
-    }
-    throw new Error('formatString should have thrown');
   });
 });
