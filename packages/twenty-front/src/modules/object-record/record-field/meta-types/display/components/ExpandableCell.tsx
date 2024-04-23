ebd95b245f84d700c84ffca8bf41cd78f2a2753e
martmull <martmull@hotmail.fr>
2024-04-23 15:20:40 +0200

    Update design

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/ExpandableCell.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/ExpandableCell.tsx
@@ -1,14 +1,28 @@
 import { ReactElement } from 'react';
 import styled from '@emotion/styled';
-import { IconPencil } from 'twenty-ui';
+import { Chip, ChipVariant, IconPencil } from 'twenty-ui';
 
 import { FloatingIconButton } from '@/ui/input/button/components/FloatingIconButton';
-const StyledContainer = styled.div<{ isHovered?: boolean }>`
+const StyledContainer = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(1)};
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const StyledChildrenContainer = styled.div`
   align-items: center;
   display: flex;
   gap: ${({ theme }) => theme.spacing(1)};
-  overflow: ${({ isHovered }) => (isHovered ? 'hidden' : 'none')};
+  overflow: hidden;
 `;
+
+const StyledChipContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing(1)};
+`;
+
 export const ExpandableCell = ({
   children,
   isHovered,
@@ -17,9 +31,14 @@ export const ExpandableCell = ({
   isHovered?: boolean;
 }) => {
   return (
-    <StyledContainer isHovered={isHovered}>
-      {children}
-      {isHovered && <FloatingIconButton Icon={IconPencil} />}
+    <StyledContainer>
+      <StyledChildrenContainer>{children}</StyledChildrenContainer>
+      {isHovered && (
+        <StyledChipContainer>
+          <Chip label={`+3`} variant={ChipVariant.Highlighted} />
+          <FloatingIconButton Icon={IconPencil} />
+        </StyledChipContainer>
+      )}
     </StyledContainer>
   );
 };
