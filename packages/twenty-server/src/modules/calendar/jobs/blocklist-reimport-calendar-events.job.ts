ecf1ed2b07182c17e458cbabacf71290049db09f
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2024-04-24 14:07:27 +0200

    4747 create deleted listener on blocklist calendar (#5128)
    Closes #4747 for calendar events

--- packages/twenty-server/src/modules/calendar/jobs/blocklist-reimport-calendar-events.job.ts
+++ packages/twenty-server/src/modules/calendar/jobs/blocklist-reimport-calendar-events.job.ts
@@ -3,33 +3,33 @@ import { Injectable, Logger } from '@nestjs/common';
 import { MessageQueueJob } from 'src/engine/integrations/message-queue/interfaces/message-queue-job.interface';
 
 import { InjectObjectMetadataRepository } from 'src/engine/object-metadata-repository/object-metadata-repository.decorator';
+import { GoogleCalendarSyncService } from 'src/modules/calendar/services/google-calendar-sync/google-calendar-sync.service';
 import { ConnectedAccountRepository } from 'src/modules/connected-account/repositories/connected-account.repository';
 import { ConnectedAccountObjectMetadata } from 'src/modules/connected-account/standard-objects/connected-account.object-metadata';
-import { GmailFullSyncService } from 'src/modules/messaging/services/gmail-full-sync/gmail-full-sync.service';
 
-export type BlocklistReimportMessagesJobData = {
+export type BlocklistReimportCalendarEventsJobData = {
   workspaceId: string;
   workspaceMemberId: string;
   handle: string;
 };
 
 @Injectable()
-export class BlocklistReimportMessagesJob
-  implements MessageQueueJob<BlocklistReimportMessagesJobData>
+export class BlocklistReimportCalendarEventsJob
+  implements MessageQueueJob<BlocklistReimportCalendarEventsJobData>
 {
-  private readonly logger = new Logger(BlocklistReimportMessagesJob.name);
+  private readonly logger = new Logger(BlocklistReimportCalendarEventsJob.name);
 
   constructor(
     @InjectObjectMetadataRepository(ConnectedAccountObjectMetadata)
     private readonly connectedAccountRepository: ConnectedAccountRepository,
-    private readonly gmailFullSyncService: GmailFullSyncService,
+    private readonly googleCalendarSyncService: GoogleCalendarSyncService,
   ) {}
 
-  async handle(data: BlocklistReimportMessagesJobData): Promise<void> {
+  async handle(data: BlocklistReimportCalendarEventsJobData): Promise<void> {
     const { workspaceId, workspaceMemberId, handle } = data;
 
     this.logger.log(
-      `Reimporting messages from handle ${handle} in workspace ${workspaceId} for workspace member ${workspaceMemberId}`,
+      `Reimporting calendar events from handle ${handle} in workspace ${workspaceId} for workspace member ${workspaceMemberId}`,
     );
 
     const connectedAccount =
@@ -46,14 +46,14 @@ export class BlocklistReimportMessagesJob
       return;
     }
 
-    await this.gmailFullSyncService.fetchConnectedAccountThreads(
+    await this.googleCalendarSyncService.startGoogleCalendarSync(
       workspaceId,
       connectedAccount[0].id,
-      [handle],
+      handle,
     );
 
     this.logger.log(
-      `Reimporting messages from ${handle} in workspace ${workspaceId} for workspace member ${workspaceMemberId} done`,
+      `Reimporting calendar events from ${handle} in workspace ${workspaceId} for workspace member ${workspaceMemberId} done`,
     );
   }
 }
