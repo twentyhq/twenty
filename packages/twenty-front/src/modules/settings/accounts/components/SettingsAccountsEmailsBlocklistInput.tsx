27eba6af27449ce83fc6825ea2e128cfacb16c71
bosiraphael <raphael.bosi@gmail.com>
2024-04-24 17:08:37 +0200

    refactor

--- packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsEmailsBlocklistInput.tsx
+++ packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsEmailsBlocklistInput.tsx
@@ -1,8 +1,9 @@
 import { Controller, useForm } from 'react-hook-form';
 import styled from '@emotion/styled';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Key } from 'ts-key-enum';
+import { z } from 'zod';
 
-import { validateMetadataLabel } from '@/object-metadata/utils/validateMetadataLabel';
 import { Button } from '@/ui/input/button/components/Button';
 import { TextInput } from '@/ui/input/components/TextInput';
 
@@ -16,19 +17,24 @@ const StyledLinkContainer = styled.div`
   margin-right: ${({ theme }) => theme.spacing(2)};
 `;
 
-type FormInput = {
-  email: string;
-};
-
 type SettingsAccountsEmailsBlocklistInputProps = {
   updateBlockedEmailList: (email: string) => void;
 };
 
+const validationSchema = z
+  .object({
+    email: z.string(),
+  })
+  .required();
+
+type FormInput = z.infer<typeof validationSchema>;
+
 export const SettingsAccountsEmailsBlocklistInput = ({
   updateBlockedEmailList,
 }: SettingsAccountsEmailsBlocklistInputProps) => {
-  const { reset, handleSubmit, control } = useForm<FormInput>({
+  const { reset, handleSubmit, control, formState } = useForm<FormInput>({
     mode: 'onChange',
+    resolver: zodResolver(validationSchema),
   });
 
   const submit = handleSubmit((data) => {
@@ -49,17 +55,14 @@ export const SettingsAccountsEmailsBlocklistInput = ({
           <Controller
             name="email"
             control={control}
-            render={({ field: { onChange, value } }) => (
+            render={({ field: { value, onChange } }) => (
               <TextInput
                 placeholder="eddy@gmail.com, @apple.com"
                 value={value}
-                onChange={(value) => {
-                  if (!value || validateMetadataLabel(value)) {
-                    onChange?.(value);
-                  }
-                }}
-                fullWidth
+                onChange={onChange}
+                error={formState.errors.email?.message}
                 onKeyDown={handleKeyDown}
+                fullWidth
               />
             )}
           />
