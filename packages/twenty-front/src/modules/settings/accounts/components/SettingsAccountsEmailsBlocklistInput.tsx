aea7de04a86803832c8436d17e67e56e63d3ed21
bosiraphael <raphael.bosi@gmail.com>
2024-04-24 16:40:27 +0200

    use react hook forms

--- packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsEmailsBlocklistInput.tsx
+++ packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsEmailsBlocklistInput.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import styled from '@emotion/styled';
 import { Key } from 'ts-key-enum';
 
+import { validateMetadataLabel } from '@/object-metadata/utils/validateMetadataLabel';
 import { Button } from '@/ui/input/button/components/Button';
 import { TextInput } from '@/ui/input/components/TextInput';
 
@@ -15,6 +16,10 @@ const StyledLinkContainer = styled.div`
   margin-right: ${({ theme }) => theme.spacing(2)};
 `;
 
+type FormInput = {
+  email: string;
+};
+
 type SettingsAccountsEmailsBlocklistInputProps = {
   updateBlockedEmailList: (email: string) => void;
 };
@@ -22,42 +27,45 @@ type SettingsAccountsEmailsBlocklistInputProps = {
 export const SettingsAccountsEmailsBlocklistInput = ({
   updateBlockedEmailList,
 }: SettingsAccountsEmailsBlocklistInputProps) => {
-  const [formValues, setFormValues] = useState<{
-    email: string;
-  }>({
-    email: '',
+  const { setValue, handleSubmit, control } = useForm<FormInput>({
+    mode: 'onChange',
+  });
+
+  const submit = handleSubmit((data) => {
+    updateBlockedEmailList(data.email);
+    setValue('email', '');
   });
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === Key.Enter) {
-      updateBlockedEmailList(formValues.email);
-      setFormValues({ email: '' });
+      submit();
     }
   };
 
   return (
-    <StyledContainer>
-      <StyledLinkContainer>
-        <TextInput
-          placeholder="eddy@gmail.com, @apple.com"
-          value={formValues.email}
-          onChange={(value) => {
-            setFormValues((prevState) => ({
-              ...prevState,
-              email: value,
-            }));
-          }}
-          fullWidth
-          onKeyDown={handleKeyDown}
-        />
-      </StyledLinkContainer>
-      <Button
-        title="Add to blocklist"
-        onClick={() => {
-          updateBlockedEmailList(formValues.email);
-          setFormValues({ email: '' });
-        }}
-      />
-    </StyledContainer>
+    <form onSubmit={submit}>
+      <StyledContainer>
+        <StyledLinkContainer>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                placeholder="eddy@gmail.com, @apple.com"
+                value={value}
+                onChange={(value) => {
+                  if (!value || validateMetadataLabel(value)) {
+                    onChange?.(value);
+                  }
+                }}
+                fullWidth
+                onKeyDown={handleKeyDown}
+              />
+            )}
+          />
+        </StyledLinkContainer>
+        <Button title="Add to blocklist" type="submit" />
+      </StyledContainer>
+    </form>
   );
 };
