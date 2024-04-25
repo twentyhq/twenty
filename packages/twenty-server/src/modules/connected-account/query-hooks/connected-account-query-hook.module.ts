8926798a82ef93a633f4b1ee0603b0397fa34e5a
bosiraphael <raphael.bosi@gmail.com>
2024-04-25 17:41:37 +0200

    import module

--- packages/twenty-server/src/modules/connected-account/query-hooks/connected-account-query-hook.module.ts
+++ packages/twenty-server/src/modules/connected-account/query-hooks/connected-account-query-hook.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { BlocklistCreateManyPreQueryHook } from 'src/modules/connected-account/query-hooks/blocklist/blocklist-create-many.pre-query.hook';
+import { BlocklistValidationModule } from 'src/modules/connected-account/services/blocklist/blocklist-validation.module';
 
 @Module({
-  imports: [],
+  imports: [BlocklistValidationModule],
   providers: [
     {
       provide: BlocklistCreateManyPreQueryHook.name,
