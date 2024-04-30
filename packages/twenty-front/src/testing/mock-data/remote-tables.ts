c46feecf9ad79ea6381545af0ac61d52a5383251
Marie Stoppa <marie.stoppa@essec.edu>
2024-04-30 17:45:18 +0200

    Use UUIDs in mocks

--- packages/twenty-front/src/testing/mock-data/remote-tables.ts
+++ packages/twenty-front/src/testing/mock-data/remote-tables.ts
@@ -1,14 +1,14 @@
 export const mockedRemoteTables = [
   {
     __typename: 'RemoteTable',
-    id: 1,
+    id: 'bb06a39e-a792-424e-ac29-c54a395fdb4f',
     name: 'Cars',
     schema: 'public',
     status: 'SYNCED',
   },
   {
     __typename: 'RemoteTable',
-    id: 2,
+    id: '994b68c7-148d-4ee0-b557-c688c3f53f73',
     name: 'Flowers',
     schema: 'public',
     status: 'NOT_SYNCED',
