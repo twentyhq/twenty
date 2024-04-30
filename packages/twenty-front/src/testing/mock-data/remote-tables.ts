6976e7c63f7274dc2819baab3822c32814f6d3c4
Marie Stoppa <marie.stoppa@essec.edu>
2024-04-30 17:29:47 +0200

    Add stories

---
+++ packages/twenty-front/src/testing/mock-data/remote-tables.ts
@@ -0,0 +1,16 @@
+export const mockedRemoteTables = [
+  {
+    __typename: 'RemoteTable',
+    id: 1,
+    name: 'Cars',
+    schema: 'public',
+    status: 'SYNCED',
+  },
+  {
+    __typename: 'RemoteTable',
+    id: 2,
+    name: 'Flowers',
+    schema: 'public',
+    status: 'NOT_SYNCED',
+  },
+];
