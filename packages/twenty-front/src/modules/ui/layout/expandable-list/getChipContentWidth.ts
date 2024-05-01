a00216b676cfc3aa310344349e5a9e61d1af9355
martmull <martmull@hotmail.fr>
2024-05-01 10:40:49 +0200

    Add tests

--- packages/twenty-front/src/modules/ui/layout/expandable-list/getChipContentWidth.ts
+++ packages/twenty-front/src/modules/ui/layout/expandable-list/getChipContentWidth.ts
@@ -2,14 +2,5 @@ export const getChipContentWidth = (numberOfChildren: number) => {
   if (numberOfChildren <= 1) {
     return 0;
   }
-  if (numberOfChildren <= 10) {
-    return 17;
-  }
-  if (numberOfChildren <= 100) {
-    return 17 + 8;
-  }
-  if (numberOfChildren <= 1000) {
-    return 17 + 8 * 2;
-  }
-  return 17 + 8 * (Math.trunc(Math.log10(numberOfChildren)) - 1);
+  return 17 + 8 * Math.trunc(Math.log10(numberOfChildren));
 };
