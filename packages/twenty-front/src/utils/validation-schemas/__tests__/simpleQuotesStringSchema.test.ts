ba9dc27b0b6bf7fcf921e506c3dbe29d513ef655
Thaïs Guigon <guigon.thais@gmail.com>
2024-05-09 18:43:43 +0200

    refactor: improve simpleQuotesStringSchema typings and tests

--- packages/twenty-front/src/utils/validation-schemas/__tests__/simpleQuotesStringSchema.test.ts
+++ packages/twenty-front/src/utils/validation-schemas/__tests__/simpleQuotesStringSchema.test.ts
@@ -4,19 +4,28 @@ import { simpleQuotesStringSchema } from '../simpleQuotesStringSchema';
 
 describe('simpleQuotesStringSchema', () => {
   it('validates a string with simple quotes', () => {
-    const result = simpleQuotesStringSchema.safeParse("'with simple quotes'");
-    expect(result.success).toBe(true);
+    // Given
+    const input = "'with simple quotes'";
+
+    // When
+    const result = simpleQuotesStringSchema.parse(input);
+
+    // Then
+    expect(result).toBe(input);
   });
 
   it.each([
+    // Given
     ['no simple quotes'],
     ["'only at start"],
     ["only at end'"],
     ["mid'dle"],
     [''],
-  ])('fails for strings not wrapped in simple quotes (%s)', (value) => {
-    const result = simpleQuotesStringSchema.safeParse(value);
+  ])('fails for strings not wrapped in simple quotes (%s)', (input) => {
+    // When
+    const result = simpleQuotesStringSchema.safeParse(input);
 
+    // Then
     expect(result.success).toBe(false);
     expect((result as SafeParseError<string>).error.errors).toEqual([
       {
