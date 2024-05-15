eee983832c4c4985ed2b84518f0aa51dd85d086d
Charles Bochet <charles@twenty.com>
2024-05-15 19:06:59 +0200

    Refactor to use operation signature

---
+++ packages/twenty-front/src/modules/activities/calendar/graphql/operation-signatures/FindOneCalendarEventOperationSignature.ts
@@ -0,0 +1,33 @@
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { RecordGqlOperationSignature } from '@/object-record/graphql/types/RecordGqlOperationSignature';
+
+export const FIND_ONE_CALENDAR_EVENT_OPERATION_SIGNATURE: RecordGqlOperationSignature =
+  {
+    objectNameSingular: CoreObjectNameSingular.CalendarEvent,
+    variables: {},
+    fields: {
+      conferenceLink: true,
+      description: true,
+      endsAt: true,
+      externalCreatedAt: true,
+      id: true,
+      isCanceled: true,
+      isFullDay: true,
+      location: true,
+      startsAt: true,
+      title: true,
+      visibility: true,
+      calendarEventParticipants: {
+        id: true,
+        person: true,
+        workspaceMember: true,
+        isOrganizer: true,
+        responseStatus: true,
+        handle: true,
+        createdAt: true,
+        calendarEventId: true,
+        updatedAt: true,
+        displayName: true,
+      },
+    },
+  };
