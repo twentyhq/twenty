9a21e30b5bf5d689dd245bc7e0b81e6237ec66ec
Marie Stoppa <marie.stoppa@essec.edu>
2024-05-15 16:56:04 +0200

    Add tests on regex

---
+++ packages/twenty-front/src/pages/settings/data-model/utils/__tests__/constants.utils.test.ts
@@ -0,0 +1,65 @@
+import {
+  METADATA_LABEL_VALID_STRING_PATTERN,
+  METADATA_NAME_VALID_STRING_PATTERN,
+  OPTION_VALUE_VALID_STRING_PATTERN,
+} from '~/pages/settings/data-model/utils/constants.utils';
+
+describe('METADATA_NAME_VALID_STRING_PATTERN', () => {
+  it('does not match empty string', () => {
+    const input = ' ';
+
+    expect(input.match(METADATA_NAME_VALID_STRING_PATTERN)).toBeNull();
+  });
+
+  it('does not match string starting with digits', () => {
+    const input = '1string';
+
+    expect(input.match(METADATA_NAME_VALID_STRING_PATTERN)).toBeNull();
+  });
+
+  it('does not match string with non-latin char', () => {
+    const input = 'λλλ';
+
+    expect(input.match(METADATA_NAME_VALID_STRING_PATTERN)).toBeNull();
+  });
+});
+
+describe('METADATA_LABEL_VALID_STRING_PATTERN', () => {
+  it('does not match empty string', () => {
+    const input = ' ';
+
+    expect(input.match(METADATA_LABEL_VALID_STRING_PATTERN)).toBeNull();
+  });
+
+  it('does not match string starting with digits', () => {
+    const input = '1string';
+
+    expect(input.match(METADATA_LABEL_VALID_STRING_PATTERN)).toBeNull();
+  });
+
+  it('matches string with non-latin char', () => {
+    const input = 'λλλ';
+
+    expect(input.match(METADATA_LABEL_VALID_STRING_PATTERN)).toHaveLength(1);
+  });
+});
+
+describe('OPTION_VALUE_VALID_STRING_PATTERN', () => {
+  it('does not match empty string', () => {
+    const input = ' ';
+
+    expect(input.match(OPTION_VALUE_VALID_STRING_PATTERN)).toBeNull();
+  });
+
+  it('matches string starting with digits', () => {
+    const input = '1string';
+
+    expect(input.match(OPTION_VALUE_VALID_STRING_PATTERN)).toBeNull();
+  });
+
+  it('matches string with non-latin char', () => {
+    const input = 'λλλ';
+
+    expect(input.match(OPTION_VALUE_VALID_STRING_PATTERN)).toHaveLength(1);
+  });
+});
