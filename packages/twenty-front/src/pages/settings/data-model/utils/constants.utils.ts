286c7b198e520c83d03843c872a3ff624c0b4be5
Marie Stoppa <marie.stoppa@essec.edu>
2024-05-15 16:50:10 +0200

    Update regex

--- packages/twenty-front/src/pages/settings/data-model/utils/constants.utils.ts
+++ packages/twenty-front/src/pages/settings/data-model/utils/constants.utils.ts
@@ -1,3 +1,3 @@
 export const METADATA_NAME_VALID_STRING_PATTERN = /^[a-zA-Z][a-zA-Z0-9 ]*$/;
-export const METADATA_LABEL_VALID_STRING_PATTERN = /^[^0-9].+$/;
-export const OPTION_VALUE_VALID_STRING_PATTERN = /^.+$/;
+export const METADATA_LABEL_VALID_STRING_PATTERN = /^[^0-9].*$/;
+export const OPTION_VALUE_VALID_STRING_PATTERN = /^(?!\s*$).+$/;
