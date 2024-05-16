a75eb0a5692f0f31b0b78ce4fbc2907f700ce520
Fabio Nettis <nettisfabio@gmail.com>
2024-05-16 17:09:46 +0200

    feat: add creation date to notes panel (#5432)
    ## Description
    
    Adds a view for creation date and author to notes and tasks panel. Here
    is a preview of the new `ActivityCreationDate` component:
    
    
    ![image](https://github.com/twentyhq/twenty/assets/36916632/8adfa584-5f0c-464a-9d69-753f89c19c28)
    
    Closes #5424
    
    ### Type of change
    
    <!-- Please delete options that are not relevant. -->
    
    - [x] New feature (non-breaking change which adds functionality)
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] Any dependent changes have been merged and published in downstream
    modules
    
    ---------
    
    Co-authored-by: Félix Malfait <felix.malfait@gmail.com>

---
+++ packages/twenty-front/src/modules/activities/components/ActivityCreationDate.tsx
@@ -0,0 +1,43 @@
+import styled from '@emotion/styled';
+import { useRecoilState } from 'recoil';
+
+import { Activity } from '@/activities/types/Activity';
+import { recordStoreFamilyState } from '@/object-record/record-store/states/recordStoreFamilyState';
+import { beautifyPastDateRelativeToNow } from '~/utils/date-utils';
+
+const StyledCreationDisplay = styled.span`
+  color: ${({ theme }) => theme.font.color.light};
+  display: flex;
+  font-size: ${({ theme }) => theme.font.size.md};
+  user-select: none;
+  width: 100%;
+`;
+
+type ActivityCreationDateProps = {
+  activityId: string;
+};
+
+export const ActivityCreationDate = ({
+  activityId,
+}: ActivityCreationDateProps) => {
+  const [activityInStore] = useRecoilState(recordStoreFamilyState(activityId));
+  const activity = activityInStore as Activity;
+
+  const beautifiedDate = activity.createdAt
+    ? beautifyPastDateRelativeToNow(activity.createdAt)
+    : null;
+
+  const authorName = activity.author?.name
+    ? `${activity.author.name.firstName} ${activity.author.name.lastName}`
+    : null;
+
+  if (!activity.createdAt || !authorName) {
+    return <></>;
+  }
+
+  return (
+    <StyledCreationDisplay>
+      Created {beautifiedDate} by {authorName}
+    </StyledCreationDisplay>
+  );
+};
