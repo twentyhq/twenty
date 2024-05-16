62a2fdaeb73d760a3e2aa3ac53c2ba55ad146ced
martmull <martmull@hotmail.fr>
2024-05-16 13:57:00 +0200

    Position created records first

--- packages/twenty-server/src/engine/api/rest/rest-api-core-query-builder/factories/create-variables.factory.ts
+++ packages/twenty-server/src/engine/api/rest/rest-api-core-query-builder/factories/create-variables.factory.ts
@@ -7,8 +7,14 @@ import { QueryVariables } from 'src/engine/api/rest/types/query-variables.type';
 @Injectable()
 export class CreateVariablesFactory {
   create(request: Request): QueryVariables {
+    const data = Array.isArray(request.body)
+      ? request.body.map((recordData) => {
+          return { position: 'first', ...recordData };
+        })
+      : { position: 'first', ...request.body };
+
     return {
-      data: request.body,
+      data,
     };
   }
 }
