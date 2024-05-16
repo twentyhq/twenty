1a555ef76528324c39e0980b5fb9bbae42b58372
Fabio Nettis <nettisfabio@gmail.com>
2024-05-16 10:47:03 +0200

    lib(comp): add activity creation data component

---
+++ packages/twenty-front/src/modules/activities/components/ActivityCreationDate.tsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+import { useRecoilState } from 'recoil';
+
+import { Activity } from '@/activities/types/Activity';
+import { recordStoreFamilyState } from '@/object-record/record-store/states/recordStoreFamilyState';
+import { beautifyPastDateRelativeToNow } from '~/utils/date-utils';
+
+const StyledCreationDisplay = styled.span`
+  color: ${({ theme }) => theme.font.color.light};
+  display: flex;
+  font-size: 15px;
+  margin-top: ${({ theme }) => theme.spacing(2)};
+  user-select: none;
+  width: 100%;
+`;
+
+type ActivityCreationDateProps = {
+  activityId: string;
+};
+
+export const ActivityCreationDate = ({
+  activityId,
+}: ActivityCreationDateProps) => {
+  const [activityInStore] = useRecoilState(recordStoreFamilyState(activityId));
+  const activity = activityInStore as Activity;
+
+  const beautifiedDate = activity.createdAt
+    ? beautifyPastDateRelativeToNow(activity.createdAt)
+    : null;
+
+  const authorName = activity.author?.name
+    ? `${activity.author.name.firstName} ${activity.author.name.lastName}`
+    : null;
+
+  if (!activity.createdAt || !authorName) {
+    return <></>;
+  }
+
+  return (
+    <StyledCreationDisplay>
+      Created {beautifiedDate} by {authorName}
+    </StyledCreationDisplay>
+  );
+};
