4fcdfbff7dfaa1fdc2e71abd39edd58c93e06c5f
martmull <martmull@hotmail.fr>
2024-05-21 11:31:03 +0200

    Fix unhandled exception (#5474)
    Solves exception.getStatus is not a function error logs in twenty-server
    
    Catch all errors in order to have no error log at all

--- packages/twenty-server/src/utils/apply-cors-to-exceptions.ts
+++ packages/twenty-server/src/utils/apply-cors-to-exceptions.ts
@@ -1,4 +1,9 @@
-import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
 
 import { Response } from 'express';
 
@@ -25,6 +30,9 @@ export class ApplyCorsToExceptions implements ExceptionFilter {
       'Origin, X-Requested-With, Content-Type, Accept',
     );
 
-    response.status(exception.getStatus()).json(exception.response);
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    response.status(status).json(exception.response);
   }
 }
