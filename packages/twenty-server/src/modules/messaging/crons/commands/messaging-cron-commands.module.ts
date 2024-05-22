fe0507678516156165e39c709ae156e2aabe48ed
bosiraphael <raphael.bosi@gmail.com>
2024-05-22 16:26:25 +0200

    renaming

--- packages/twenty-server/src/modules/messaging/crons/commands/messaging-cron-commands.module.ts
+++ packages/twenty-server/src/modules/messaging/crons/commands/messaging-cron-commands.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
 import { ConnectedAccountWorkspaceEntity } from 'src/modules/connected-account/standard-objects/connected-account.workspace-entity';
 import { GmailMessagesImportCronCommand } from 'src/modules/messaging/crons/commands/gmail-messages-import.cron.command';
-import { GmailPartialSyncCronCommand } from 'src/modules/messaging/crons/commands/gmail-partial-sync.cron.command';
+import { GmailPartialSyncCronCommand } from 'src/modules/messaging/crons/commands/gmail-partial-message-list-fetch.cron.command';
 @Module({
   imports: [
     ObjectMetadataRepositoryModule.forFeature([
