a890f5b6e606d5f5a2dac5d25f014ac7819b977f
José Pereira <jose.a.pereira@tecnico.ulisboa.pt>
2024-05-22 16:23:07 +0100

    feat: add initial messageThreadMembersChip boilerplate
    Co-authored-by: Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>

--- packages/twenty-front/src/modules/activities/emails/components/EmailThreadMembersChip.tsx
+++ packages/twenty-front/src/modules/activities/emails/components/EmailThreadMembersChip.tsx
@@ -1,14 +1,42 @@
 import styled from '@emotion/styled';
+import { Avatar, Chip, ChipAccent, ChipSize, ChipVariant } from 'twenty-ui';
 
-const StyledContainer = styled.div`
-  align-items: flex-start;
-  display: flex;
-`;
+const useChipN = 'private';
+
 
 export const EmailThreadMembersChip = () => {
   return (
-    <StyledContainer>
-      <h1> Test </h1>
-    </StyledContainer>
+    <>
+      {useChipN === 'private' && (
+        <Chip
+          label="Private"
+          leftComponent={
+            <Avatar
+              avatarUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png"
+              placeholder="private"
+            />
+          }
+          size={ChipSize.Large}
+          accent={ChipAccent.TextSecondary}
+          variant={ChipVariant.Highlighted}
+        />
+      )}
+      {useChipN === 'everyone' && (
+        <Chip
+          label="Everyone"
+          size={ChipSize.Large}
+          accent={ChipAccent.TextSecondary}
+          variant={ChipVariant.Highlighted}
+        />
+      )}
+      {useChipN === 'shared' && (
+        <Chip
+          label="Shared With 1 person"
+          size={ChipSize.Large}
+          accent={ChipAccent.TextSecondary}
+          variant={ChipVariant.Highlighted}
+        />
+      )}
+    </>
   );
 };
