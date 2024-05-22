a92ab884305dc675d54d6a1e93665ca941c8f8fe
bosiraphael <raphael.bosi@gmail.com>
2024-05-22 14:12:30 +0200

    create full sync service v2

--- packages/twenty-server/src/modules/messaging/jobs/gmail-full-sync.job.ts
+++ packages/twenty-server/src/modules/messaging/jobs/gmail-full-sync.job.ts
@@ -1,9 +1,17 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
 
 import { MessageQueueJob } from 'src/engine/integrations/message-queue/interfaces/message-queue-job.interface';
 
 import { GoogleAPIRefreshAccessTokenService } from 'src/modules/connected-account/services/google-api-refresh-access-token/google-api-refresh-access-token.service';
 import { GmailFullSyncService } from 'src/modules/messaging/services/gmail-full-sync/gmail-full-sync.service';
+import {
+  FeatureFlagEntity,
+  FeatureFlagKeys,
+} from 'src/engine/core-modules/feature-flag/feature-flag.entity';
+import { GmailFullSyncV2Service } from 'src/modules/messaging/services/gmail-full-sync/gmail-full-sync-v2.service';
 
 export type GmailFullSyncJobData = {
   workspaceId: string;
@@ -17,6 +25,9 @@ export class GmailFullSyncJob implements MessageQueueJob<GmailFullSyncJobData> {
   constructor(
     private readonly googleAPIsRefreshAccessTokenService: GoogleAPIRefreshAccessTokenService,
     private readonly gmailFullSyncService: GmailFullSyncService,
+    private readonly gmailFullSyncV2Service: GmailFullSyncV2Service,
+    @InjectRepository(FeatureFlagEntity, 'core')
+    private readonly featureFlagRepository: Repository<FeatureFlagEntity>,
   ) {}
 
   async handle(data: GmailFullSyncJobData): Promise<void> {
@@ -38,9 +49,25 @@ export class GmailFullSyncJob implements MessageQueueJob<GmailFullSyncJobData> {
       return;
     }
 
-    await this.gmailFullSyncService.fetchConnectedAccountThreads(
-      data.workspaceId,
-      data.connectedAccountId,
-    );
+    const isGmailSyncV2EnabledFeatureFlag =
+      await this.featureFlagRepository.findOneBy({
+        workspaceId: data.workspaceId,
+        key: FeatureFlagKeys.IsGmailSyncV2Enabled,
+        value: true,
+      });
+
+    const isGmailSyncV2Enabled = isGmailSyncV2EnabledFeatureFlag?.value;
+
+    if (isGmailSyncV2Enabled) {
+      await this.gmailFullSyncV2Service.fetchConnectedAccountThreads(
+        data.workspaceId,
+        data.connectedAccountId,
+      );
+    } else {
+      await this.gmailFullSyncService.fetchConnectedAccountThreads(
+        data.workspaceId,
+        data.connectedAccountId,
+      );
+    }
   }
 }
