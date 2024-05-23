f89f0834b575291413649375d699a4859f76ac02
bosiraphael <raphael.bosi@gmail.com>
2024-05-23 16:41:02 +0200

    remove cron and command

--- packages/twenty-server/src/modules/messaging/crons/commands/messaging-cron-commands.module.ts
+++ packages/twenty-server/src/modules/messaging/crons/commands/messaging-cron-commands.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
 import { ConnectedAccountWorkspaceEntity } from 'src/modules/connected-account/standard-objects/connected-account.workspace-entity';
 import { GmailFetchMessagesFromCacheCronCommand } from 'src/modules/messaging/crons/commands/gmail-fetch-messages-from-cache.cron.command';
-import { GmailFullSyncCronCommand } from 'src/modules/messaging/crons/commands/gmail-full-sync.cron.command';
 import { GmailPartialSyncCronCommand } from 'src/modules/messaging/crons/commands/gmail-partial-sync.cron.command';
 @Module({
   imports: [
@@ -13,7 +12,6 @@ import { GmailPartialSyncCronCommand } from 'src/modules/messaging/crons/command
   ],
   providers: [
     GmailPartialSyncCronCommand,
-    GmailFullSyncCronCommand,
     GmailFetchMessagesFromCacheCronCommand,
   ],
 })
