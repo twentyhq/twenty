22f0e64fdf64a3f2a10b47b83ce9e7fd9e8fa095
Thaïs Guigon <guigon.thais@gmail.com>
2024-05-24 10:49:37 +0200

    fix: fix missing import

--- packages/twenty-front/src/modules/settings/data-model/fields/forms/boolean/components/SettingsDataModelFieldBooleanSettingsFormCard.tsx
+++ packages/twenty-front/src/modules/settings/data-model/fields/forms/boolean/components/SettingsDataModelFieldBooleanSettingsFormCard.tsx
@@ -3,7 +3,10 @@ import styled from '@emotion/styled';
 
 import { FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { SettingsDataModelPreviewFormCard } from '@/settings/data-model/components/SettingsDataModelPreviewFormCard';
-import { SettingsDataModelFieldBooleanForm } from '@/settings/data-model/fields/forms/boolean/components/SettingsDataModelFieldBooleanForm';
+import {
+  SettingsDataModelFieldBooleanForm,
+  SettingsDataModelFieldBooleanFormValues,
+} from '@/settings/data-model/fields/forms/boolean/components/SettingsDataModelFieldBooleanForm';
 import { useBooleanSettingsFormInitialValues } from '@/settings/data-model/fields/forms/boolean/hooks/useBooleanSettingsFormInitialValues';
 import {
   SettingsDataModelFieldPreviewCard,
