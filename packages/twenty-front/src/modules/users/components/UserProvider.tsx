2a8b044794effa437860a76d086a4568dcaf8981
martmull <martmull@hotmail.fr>
2024-05-27 11:51:59 +0200

    Remove flash after create workspace

--- packages/twenty-front/src/modules/users/components/UserProvider.tsx
+++ packages/twenty-front/src/modules/users/components/UserProvider.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { isCurrentUserLoadedState } from '@/auth/states/isCurrentUserLoadingState';
+import { AppPath } from '@/types/AppPath';
+import { useIsMatchingLocation } from '~/hooks/useIsMatchingLocation';
 import { UserOrMetadataLoader } from '~/loading/components/UserOrMetadataLoader';
 
 export const UserProvider = ({ children }: React.PropsWithChildren) => {
   const isCurrentUserLoaded = useRecoilValue(isCurrentUserLoadedState);
+  const isMatchingLocation = useIsMatchingLocation();
 
-  return !isCurrentUserLoaded ? <UserOrMetadataLoader /> : <>{children}</>;
+  return !isCurrentUserLoaded &&
+    !isMatchingLocation(AppPath.CreateWorkspace) ? (
+    <UserOrMetadataLoader />
+  ) : (
+    <>{children}</>
+  );
 };
