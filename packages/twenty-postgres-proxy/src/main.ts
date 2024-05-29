1a56056aba45cd2eb8de689b712a559814dc872b
Félix Malfait <felix.malfait@gmail.com>
2024-05-29 15:00:35 +0200

    Temporarily simplify script

--- packages/twenty-postgres-proxy/src/main.ts
+++ packages/twenty-postgres-proxy/src/main.ts
@@ -1,5 +1,6 @@
 import net from 'net';
 import dotenv from 'dotenv';
+import { on } from 'events';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -8,80 +9,32 @@ const PG_HOST = process.env.PG_DATABASE_HOST || 'localhost'; // PostgreSQL serve
 const PG_PORT = parseInt(process.env.PG_DATABASE_PORT || '5432'); // PostgreSQL server port
 const PROXY_PORT = parseInt(process.env.PROXY_PORT || '5432') ; // Port for proxy server to listen on
 
-const ALLOWED_HOSTS = (process.env.ALLOWED_HOSTS || '').split(',');
-const ALLOWED_IPS = (process.env.ALLOWED_IPS || '').split(',');
-const ALLOWED_USERS = (process.env.ALLOWED_USERS || '').split(',');
 
-function isAllowedIP(ip: string): boolean {
-    return ALLOWED_IPS.includes('*') || ALLOWED_IPS.includes(ip);
-}
-
-function isAllowedHost(hostname: string): boolean {
-    return ALLOWED_HOSTS.includes('*') || ALLOWED_HOSTS.includes(hostname);
-}
-
-function isAllowedUser(user: string): boolean {
-    return ALLOWED_USERS.includes('*') || ALLOWED_USERS.includes(user);
-}
 
 const server = net.createServer((clientSocket) => {
     console.log('Client connected.');
 
     const clientIP = clientSocket.remoteAddress || '';
-    if (!isAllowedIP(clientIP)) {
-        console.log('Security check failed: IP not allowed');
-        clientSocket.end('Security check failed: IP not allowed\n');
-        return;
-    }
+    console.log('clientIP', clientIP)
 
-    let serverSocket: net.Socket | null = null;
+    const serverSocket = net.connect({ host: PG_HOST, port: PG_PORT }, () => {
+        console.log('Proxy connected to PostgreSQL server.');
+    });
 
-    clientSocket.once('data', (data) => {
-        const dataString = data.toString();
-        console.log('Received Data:', dataString);
+    serverSocket.on('data', (data) => {
+        console.log('PostgreSQL Response:', data.toString());
+        clientSocket.write(data);
+    });
 
-        const hostnameMatch = dataString.match(/Host=([\w\.-]+)/);
-        if (!hostnameMatch || !isAllowedHost(hostnameMatch[1])) {
-            console.log('Security check failed: Host not allowed');
-            clientSocket.end('Security check failed: Host not allowed\n');
-            return;
-        }
+    serverSocket.on('end', () => {
+        console.log('PostgreSQL server connection closed.');
+        clientSocket.end();
+    });
 
-        const userMatch = dataString.match(/user=(\w+)\0/);
-        if (userMatch) {
-            const user = userMatch[1];
-            console.log('Extracted User:', user);
-
-            if (!isAllowedUser(user)) {
-                console.log('Security check failed: User not allowed');
-                clientSocket.end('Security check failed: User not allowed\n');
-                return;
-            }
-
-            serverSocket = net.connect({ host: PG_HOST, port: PG_PORT }, () => {
-                console.log('Proxy connected to PostgreSQL server.');
-                serverSocket?.write(data);
-            });
-
-            serverSocket.on('data', (data) => {
-                console.log('PostgreSQL Response:', data.toString());
-                clientSocket.write(data);
-            });
-
-            serverSocket.on('end', () => {
-                console.log('PostgreSQL server connection closed.');
-                clientSocket.end();
-            });
-
-            serverSocket.on('error', (err) => {
-                console.log('Server Error:', err.message);
-                serverSocket.destroy();
-                clientSocket.end();
-            });
-        } else {
-            console.log('User not found in initial data.');
-            clientSocket.end('Authentication details missing\n');
-        }
+    serverSocket.on('error', (err) => {
+        console.log('Server Error:', err.message);
+        serverSocket.destroy();
+        clientSocket.end();
     });
 
     clientSocket.on('end', () => {
