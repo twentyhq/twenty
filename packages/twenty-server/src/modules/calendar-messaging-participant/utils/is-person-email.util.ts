cef4f215e9ae245a53c480de66f9549f3b9aae65
bosiraphael <raphael.bosi@gmail.com>
2024-05-31 17:59:04 +0200

    remove file

--- packages/twenty-server/src/modules/calendar-messaging-participant/utils/is-person-email.util.ts
+++
@@ -1,6 +0,0 @@
-export const isPersonEmail = (email: string): boolean => {
-  const nonPersonalPattern =
-    /noreply|no-reply|do_not_reply|no\.reply|^(info@|contact@|hello@|support@|feedback@|service@|help@)/;
-
-  return !nonPersonalPattern.test(email);
-};
