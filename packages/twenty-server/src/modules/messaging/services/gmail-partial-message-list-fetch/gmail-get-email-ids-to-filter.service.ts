94d0914c68eb845dd42efa3f2cac7c959ca025e4
bosiraphael <raphael.bosi@gmail.com>
2024-05-31 17:55:18 +0200

    create GmailGetEmailIdsToFilterService

---
+++ packages/twenty-server/src/modules/messaging/services/gmail-partial-message-list-fetch/gmail-get-email-ids-to-filter.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+
+import { gmail_v1 } from 'googleapis';
+
+import { GmailGetHistoryService } from 'src/modules/messaging/services/gmail-partial-message-list-fetch/gmail-get-history.service';
+import { excludedLabelIds } from 'src/modules/messaging/utils/gmail-search-filter.util';
+
+@Injectable()
+export class GmailGetEmailIdsToFilterService {
+  constructor(
+    private readonly gmailGetHistoryService: GmailGetHistoryService,
+  ) {}
+
+  public async fetchFilteredEmailIds(
+    gmailClient: gmail_v1.Gmail,
+    lastSyncHistoryId: string,
+  ): Promise<string[]> {
+    const fullHistory: gmail_v1.Schema$History[] = [];
+
+    for (const labelId of excludedLabelIds) {
+      const { history, error } = await this.gmailGetHistoryService.getHistory(
+        gmailClient,
+        lastSyncHistoryId,
+        ['messageAdded'],
+        labelId,
+      );
+
+      if (error) {
+        return [];
+      }
+
+      fullHistory.push(...history);
+    }
+
+    const emailIds = fullHistory
+      .map((history) => history.messagesAdded)
+      .flat()
+      .map((message) => message?.message?.id)
+      .filter((id) => id) as string[];
+
+    return emailIds;
+  }
+}
--- packages/twenty-server/src/modules/messaging/utils/gmail-search-filter.util.ts
+++ packages/twenty-server/src/modules/messaging/utils/gmail-search-filter.util.ts
@@ -1,6 +1,3 @@
-// export const gmailSearchFilterNonPersonalEmails =
-//   '*noreply@|*no-reply@|*do_not_reply@|*no.reply@|*info@|*contact@|*hello@|*support@|*feedback@|*service@|*help@';
-
 export const excludedCategories = ['promotions', 'social', 'forums'];
 
 export const excludedLabelIds = excludedCategories.map(
