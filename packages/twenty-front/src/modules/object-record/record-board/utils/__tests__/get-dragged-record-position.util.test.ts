44a40f4a1e0773f825b587216ecc343e4b26eb6f
Thomas Trompette <thomas.trompette@sfr.fr>
2024-05-31 14:07:14 +0200

    Improve tests

--- packages/twenty-front/src/modules/object-record/record-board/utils/__tests__/get-dragged-record-position.util.test.ts
+++ packages/twenty-front/src/modules/object-record/record-board/utils/__tests__/get-dragged-record-position.util.test.ts
@@ -1,19 +1,27 @@
 import { getDraggedRecordPosition } from '../get-dragged-record-position.util';
 
 describe('getDraggedRecordPosition', () => {
-  it('should return the average of the two positions', () => {
+  it('when both records defined and positive, should return the average of the two positions', () => {
     expect(getDraggedRecordPosition(1, 3)).toBe(2);
   });
 
-  it('should return the position before the record after', () => {
+  it('when both records defined and negative, should return the average of the two positions', () => {
+    expect(getDraggedRecordPosition(-3, -1)).toBe(-2);
+  });
+
+  it('when both records defined and one negative, should return the average of the two positions', () => {
+    expect(getDraggedRecordPosition(-1, 3)).toBe(1);
+  });
+
+  it('when only record after, should return the position - 1', () => {
     expect(getDraggedRecordPosition(undefined, 3)).toBe(2);
   });
 
-  it('should return the position after the record before', () => {
+  it('when only record before, should return the position + 1', () => {
     expect(getDraggedRecordPosition(1, undefined)).toBe(2);
   });
 
-  it('should return 1 if no positions are defined', () => {
+  it('when both records undefined, should return 1', () => {
     expect(getDraggedRecordPosition(undefined, undefined)).toBe(1);
   });
 });
