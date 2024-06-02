45f18aac08e162aae1e11e956e56d25a8570d7f1
aditya pimpalkar <adipimpalkar@gmail.com>
2024-06-03 00:10:25 +0100

    update WorkspaceMember type on front end

--- packages/twenty-front/src/modules/workspace-member/types/WorkspaceMember.ts
+++ packages/twenty-front/src/modules/workspace-member/types/WorkspaceMember.ts
@@ -1,3 +1,6 @@
+import { DateFormat } from '@/workspace-member/constants/DateFormat';
+import { TimeFormat } from '@/workspace-member/constants/TimeFormat';
+
 export type ColorScheme = 'Dark' | 'Light' | 'System';
 
 export type WorkspaceMember = {
@@ -9,6 +12,9 @@ export type WorkspaceMember = {
   };
   avatarUrl?: string | null;
   locale: string;
+  timeZone: string;
+  dateFormat: DateFormat;
+  timeFormat: TimeFormat;
   colorScheme?: ColorScheme;
   createdAt: string;
   updatedAt: string;
