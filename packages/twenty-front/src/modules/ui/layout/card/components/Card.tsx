3660dc60f562737fcc1f8d954d109e68408a7a80
martmull <martmull@hotmail.fr>
2024-06-02 08:57:13 +0200

    Fix style

--- packages/twenty-front/src/modules/ui/layout/card/components/Card.tsx
+++ packages/twenty-front/src/modules/ui/layout/card/components/Card.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 const StyledCard = styled.div<{ fullWidth?: boolean }>`
   border: 1px solid ${({ theme }) => theme.border.color.medium};
-  border-radius: ${({ theme }) => theme.border.radius.sm};
+  border-radius: ${({ theme }) => theme.border.radius.md};
   color: ${({ theme }) => theme.font.color.secondary};
   overflow: hidden;
   width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
