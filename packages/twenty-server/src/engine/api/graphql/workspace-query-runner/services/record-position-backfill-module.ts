82aca1acb659c01fc5685e3d16e5b2a392772ecd
Thomas Trompette <thomas.trompette@sfr.fr>
2024-06-03 18:15:48 +0200

    Add backfill position job

--- packages/twenty-server/src/engine/api/graphql/workspace-query-runner/services/record-position-backfill-module.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-query-runner/services/record-position-backfill-module.ts
@@ -4,9 +4,10 @@ import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/works
 import { RecordPositionQueryFactory } from 'src/engine/api/graphql/workspace-query-builder/factories/record-position-query.factory';
 import { RecordPositionFactory } from 'src/engine/api/graphql/workspace-query-runner/factories/record-position.factory';
 import { RecordPositionBackfillService } from 'src/engine/api/graphql/workspace-query-runner/services/record-position-backfill-service';
+import { ObjectMetadataModule } from 'src/engine/metadata-modules/object-metadata/object-metadata.module';
 
 @Module({
-  imports: [WorkspaceDataSourceModule],
+  imports: [WorkspaceDataSourceModule, ObjectMetadataModule],
   providers: [
     RecordPositionFactory,
     RecordPositionQueryFactory,
--- packages/twenty-server/src/engine/api/graphql/workspace-query-runner/services/record-position-backfill-service.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-query-runner/services/record-position-backfill-service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 import { ObjectMetadataInterface } from 'src/engine/metadata-modules/field-metadata/interfaces/object-metadata.interface';
 
@@ -8,44 +8,92 @@ import {
   RecordPositionQueryType,
 } from 'src/engine/api/graphql/workspace-query-builder/factories/record-position-query.factory';
 import { RecordPositionFactory } from 'src/engine/api/graphql/workspace-query-runner/factories/record-position.factory';
+import { ObjectMetadataService } from 'src/engine/metadata-modules/object-metadata/object-metadata.service';
 
 @Injectable()
 export class RecordPositionBackfillService {
+  private readonly logger = new Logger(RecordPositionBackfillService.name);
   constructor(
+    private readonly objectMetadataService: ObjectMetadataService,
     private readonly recordPositionFactory: RecordPositionFactory,
     private readonly recordPositionQueryFactory: RecordPositionQueryFactory,
     private readonly workspaceDataSourceService: WorkspaceDataSourceService,
   ) {}
 
-  async backfill(
-    workspaceId: string,
-    objectMetadata: { nameSingular: string; isCustom: boolean },
-    recordId: string,
-  ) {
-    const position = await this.recordPositionFactory.create(
-      'last',
-      objectMetadata as ObjectMetadataInterface,
-      workspaceId,
-    );
-
+  async backfill(workspaceId: string, dryRun: boolean) {
     const dataSourceSchema =
       this.workspaceDataSourceService.getSchemaName(workspaceId);
 
-    const [query, params] = await this.recordPositionQueryFactory.create(
-      {
-        recordPositionQueryType: RecordPositionQueryType.UPDATE_POSITION,
-        recordId,
-        positionValue: position,
-      },
-      objectMetadata as ObjectMetadataInterface,
-      dataSourceSchema,
-    );
+    const objectMetadataEntities =
+      await this.objectMetadataService.findManyWithinWorkspace(workspaceId, {
+        where: { isSystem: false },
+      });
 
-    this.workspaceDataSourceService.executeRawQuery(
-      query,
-      params,
-      workspaceId,
-      undefined,
-    );
+    const objectMetadataWithPosition =
+      objectMetadataEntities.filter(hasPositionField);
+
+    for (const objectMetadata of objectMetadataWithPosition) {
+      const [recordsWithoutPositionQuery, recordsWithoutPositionQueryParams] =
+        await this.recordPositionQueryFactory.create(
+          {
+            recordPositionQueryType: RecordPositionQueryType.FIND_BY_POSITION,
+            positionValue: null,
+          },
+          objectMetadata,
+          dataSourceSchema,
+        );
+
+      const recordsWithoutPosition =
+        await this.workspaceDataSourceService.executeRawQuery(
+          recordsWithoutPositionQuery,
+          recordsWithoutPositionQueryParams,
+          workspaceId,
+          undefined,
+        );
+
+      const position = await this.recordPositionFactory.create(
+        'last',
+        objectMetadata as ObjectMetadataInterface,
+        workspaceId,
+      );
+
+      for (
+        let recordIndex = 0;
+        recordIndex < recordsWithoutPosition.length;
+        recordIndex++
+      ) {
+        const recordId = recordsWithoutPosition[recordIndex].id;
+        const backfilledPosition = position + recordIndex;
+
+        this.logger.log(
+          `Backfilling position ${backfilledPosition} for ${objectMetadata.nameSingular} ${recordId}`,
+        );
+
+        if (dryRun) {
+          continue;
+        }
+
+        const [query, params] = await this.recordPositionQueryFactory.create(
+          {
+            recordPositionQueryType: RecordPositionQueryType.UPDATE_POSITION,
+            recordId: recordsWithoutPosition[recordIndex].id,
+            positionValue: position + recordIndex,
+          },
+          objectMetadata,
+          dataSourceSchema,
+        );
+
+        await this.workspaceDataSourceService.executeRawQuery(
+          query,
+          params,
+          workspaceId,
+          undefined,
+        );
+      }
+    }
   }
 }
+
+const hasPositionField = (objectMetadata: ObjectMetadataInterface) =>
+  ['company', 'opportunity', 'person'].includes(objectMetadata.nameSingular) ||
+  objectMetadata.isCustom;
