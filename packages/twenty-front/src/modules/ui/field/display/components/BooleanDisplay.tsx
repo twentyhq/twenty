65c9d8c33d13bf77c54635909e7fa69367f57e67
Marie Stoppa <marie.stoppa@essec.edu>
2024-06-04 11:45:20 +0200

    Fix boolean field in table view

---
+++ packages/twenty-front/src/modules/ui/field/display/components/BooleanDisplay.tsx
@@ -0,0 +1,29 @@
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { IconCheck, IconX } from 'twenty-ui';
+
+const StyledBooleanFieldValue = styled.div`
+  margin-left: ${({ theme }) => theme.spacing(1)};
+`;
+
+type BooleanDisplayProps = {
+  value: boolean | null;
+};
+
+export const BooleanDisplay = ({ value }: BooleanDisplayProps) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      {' '}
+      {value ? (
+        <IconCheck size={theme.icon.size.sm} />
+      ) : (
+        <IconX size={theme.icon.size.sm} />
+      )}
+      <StyledBooleanFieldValue>
+        {value ? 'True' : 'False'}
+      </StyledBooleanFieldValue>
+    </>
+  );
+};
