3a1297c1967aac4f52736b800ae93653141cddea
Thomas Trompette <thomas.trompette@sfr.fr>
2024-06-06 18:47:57 +0200

    Add endpoint to create credentials

---
+++ packages/twenty-server/src/engine/core-modules/postgres-credentials/postgres-credentials.service.ts
@@ -0,0 +1,103 @@
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { randomBytes } from 'crypto';
+
+import { Repository } from 'typeorm';
+
+import {
+  decryptText,
+  encryptText,
+} from 'src/engine/core-modules/auth/auth.util';
+import { EnvironmentService } from 'src/engine/integrations/environment/environment.service';
+import { PostgresCredentials } from 'src/engine/core-modules/postgres-credentials/postgres-credentials.entity';
+import { NotFoundError } from 'src/engine/utils/graphql-errors.util';
+import { PostgresCredentialsDTO } from 'src/engine/core-modules/postgres-credentials/dtos/postgres-credentials.dto';
+
+export class PostgresCredentialsService {
+  constructor(
+    @InjectRepository(PostgresCredentials, 'core')
+    private readonly postgresCredentialsRepository: Repository<PostgresCredentials>,
+    private readonly environmentService: EnvironmentService,
+  ) {}
+
+  async enablePostgresProxy(
+    workspaceId: string,
+  ): Promise<PostgresCredentialsDTO> {
+    const user = `user_${randomBytes(4).toString('hex')}`;
+    const password = randomBytes(16).toString('hex');
+
+    const key = this.environmentService.get('LOGIN_TOKEN_SECRET');
+    const passwordHash = encryptText(password, key);
+
+    const postgresCredentials = await this.postgresCredentialsRepository.create(
+      {
+        user,
+        passwordHash,
+        workspaceId,
+      },
+    );
+
+    await this.postgresCredentialsRepository.save(postgresCredentials);
+
+    return {
+      id: postgresCredentials.id,
+      user,
+      password,
+      workspaceId,
+    };
+  }
+
+  async disablePostgresProxy(
+    workspaceId: string,
+  ): Promise<PostgresCredentialsDTO> {
+    const postgresCredentials =
+      await this.postgresCredentialsRepository.findOne({
+        where: {
+          workspaceId,
+        },
+      });
+
+    if (!postgresCredentials?.id) {
+      throw new NotFoundError(
+        'No valid Postgres credentials not found for this workspace',
+      );
+    }
+
+    await this.postgresCredentialsRepository.delete({
+      id: postgresCredentials.id,
+    });
+
+    const key = this.environmentService.get('LOGIN_TOKEN_SECRET');
+
+    return {
+      id: postgresCredentials.id,
+      user: postgresCredentials.user,
+      password: decryptText(postgresCredentials.passwordHash, key),
+      workspaceId: postgresCredentials.workspaceId,
+    };
+  }
+
+  async getPostgresCredentials(
+    workspaceId: string,
+  ): Promise<PostgresCredentialsDTO | null> {
+    const postgresCredentials =
+      await this.postgresCredentialsRepository.findOne({
+        where: {
+          workspaceId,
+        },
+      });
+
+    if (!postgresCredentials) {
+      return null;
+    }
+
+    const key = this.environmentService.get('LOGIN_TOKEN_SECRET');
+
+    return {
+      id: postgresCredentials.id,
+      user: postgresCredentials.user,
+      password: decryptText(postgresCredentials.passwordHash, key),
+      workspaceId: postgresCredentials.workspaceId,
+    };
+  }
+}
