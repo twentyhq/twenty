7df681bba346a1fe6d3fe75da78ff35c0c7c915e
ad-elias <elias@autodiligence.com>
2024-06-08 00:27:09 +0200

    Move feature flag check to resolver

--- packages/twenty-server/src/engine/core-modules/text-to-sql/text-to-sql.resolver.ts
+++ packages/twenty-server/src/engine/core-modules/text-to-sql/text-to-sql.resolver.ts
@@ -1,11 +1,18 @@
 import { Args, Query, Resolver, ArgsType, Field } from '@nestjs/graphql';
-import { UseGuards } from '@nestjs/common';
+import { ForbiddenException, UseGuards } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
 
 import { JwtAuthGuard } from 'src/engine/guards/jwt.auth.guard';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { AuthWorkspace } from 'src/engine/decorators/auth/auth-workspace.decorator';
 import { TextToSQLService } from 'src/engine/core-modules/text-to-sql/text-to-sql.service';
 import { TextToSQLQueryResult } from 'src/engine/core-modules/text-to-sql/dtos/text-to-sql-query-result.dto';
+import {
+  FeatureFlagEntity,
+  FeatureFlagKeys,
+} from 'src/engine/core-modules/feature-flag/feature-flag.entity';
 
 @ArgsType()
 class GetTextToSQLArgs {
@@ -16,13 +23,30 @@ class GetTextToSQLArgs {
 @UseGuards(JwtAuthGuard)
 @Resolver(() => TextToSQLQueryResult)
 export class TextToSQLResolver {
-  constructor(private readonly textToSQLService: TextToSQLService) {}
+  constructor(
+    private readonly textToSQLService: TextToSQLService,
+    @InjectRepository(FeatureFlagEntity, 'core')
+    private readonly featureFlagRepository: Repository<FeatureFlagEntity>,
+  ) {}
 
   @Query(() => TextToSQLQueryResult)
   async getTextToSQL(
     @AuthWorkspace() { id: workspaceId }: Workspace,
     @Args() { text }: GetTextToSQLArgs,
   ) {
+    const isAskAIEnabledFeatureFlag =
+      await this.featureFlagRepository.findOneBy({
+        workspaceId,
+        key: FeatureFlagKeys.IsAskAIEnabled,
+        value: true,
+      });
+
+    if (!isAskAIEnabledFeatureFlag?.value) {
+      throw new ForbiddenException(
+        `${FeatureFlagKeys.IsAskAIEnabled} feature flag is disabled`,
+      );
+    }
+
     return this.textToSQLService.query(workspaceId, text);
   }
 }
