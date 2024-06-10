9d285f17d4e8cfe0cc3cc644e676b4aff64fc744
martmull <martmull@hotmail.fr>
2024-06-10 17:46:54 +0200

    Add authentication optional api url parameter

--- packages/twenty-zapier/src/authentication.ts
+++ packages/twenty-zapier/src/authentication.ts
@@ -23,6 +23,15 @@ export default {
       helpText:
         'Create an API key in [your twenty workspace](https://app.twenty.com/settings/developers)',
     },
+    {
+      computed: false,
+      key: 'apiUrl',
+      required: false,
+      label: 'Api Url',
+      type: 'string',
+      helpText:
+        'Leave blank if you use app.twenty.com. If you self-host Twenty, please set your Twenty server url (same as variable REACT_APP_SERVER_BASE_URL in https://docs.twenty.com/start/self-hosting/).',
+    },
   ],
   connectionLabel: '{{data.currentWorkspace.displayName}}',
   customConfig: {},
