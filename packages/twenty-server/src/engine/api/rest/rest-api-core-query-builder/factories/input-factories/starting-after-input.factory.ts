374237a9888a35206b40aa9a998c3873e39f5dd8
Félix Malfait <felix.malfait@gmail.com>
2024-06-12 21:54:33 +0200

    Refacto rest api, fix graphl playground, improve analytics (#5844)
    - Improve the rest api by introducing startingAfter/endingBefore (we
    previously had lastCursor), and moving pageInfo/totalCount outside of
    the data object.
    - Fix broken GraphQL playground on website
    - Improve analytics by sending server url

--- packages/twenty-server/src/engine/api/rest/rest-api-core-query-builder/factories/input-factories/starting-after-input.factory.ts
+++ packages/twenty-server/src/engine/api/rest/rest-api-core-query-builder/factories/input-factories/starting-after-input.factory.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
 
 @Injectable()
-export class LastCursorInputFactory {
+export class StartingAfterInputFactory {
   create(request: Request): string | undefined {
-    const cursorQuery = request.query.last_cursor;
+    const cursorQuery = request.query.starting_after;
 
     if (typeof cursorQuery !== 'string') {
       return undefined;
