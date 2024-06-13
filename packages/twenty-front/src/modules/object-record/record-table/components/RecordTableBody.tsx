247b3c7950565c947b4022976d177a5b94221ba6
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-06-13 17:18:09 +0200

    Fixed bug with negative position values and refactored a bit

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableBody.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableBody.tsx
@@ -1,12 +1,9 @@
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 import { RecordTableBodyFetchMoreLoader } from '@/object-record/record-table/components/RecordTableBodyFetchMoreLoader';
 import { RecordTableRow } from '@/object-record/record-table/components/RecordTableRow';
 import { useRecordTableStates } from '@/object-record/record-table/hooks/internal/useRecordTableStates';
-import { useTableRowDragHandler } from '@/object-record/record-table/hooks/useTableRowDragHandler';
-import { isRemoveSortingModalOpenState } from '@/object-record/record-table/states/isRemoveSortingModalOpenState';
 import { DraggableTableBody } from '@/ui/layout/draggable-list/components/DraggableTableBody';
-import { useGetCurrentView } from '@/views/hooks/useGetCurrentView';
 
 type RecordTableBodyProps = {
   objectNameSingular: string;
@@ -21,24 +18,11 @@ export const RecordTableBody = ({
 
   const tableRowIds = useRecoilValue(tableRowIdsState);
 
-  const { currentViewWithCombinedFiltersAndSorts } =
-    useGetCurrentView(recordTableId);
-  const viewSorts = currentViewWithCombinedFiltersAndSorts?.viewSorts || [];
-
-  const setIsRemoveSortingModalOpenState = useSetRecoilState(
-    isRemoveSortingModalOpenState,
-  );
-
-  const handleDrag = useTableRowDragHandler(
-    objectNameSingular,
-    viewSorts,
-    setIsRemoveSortingModalOpenState,
-  );
-
   return (
     <>
       <DraggableTableBody
-        onDragEnd={handleDrag}
+        objectNameSingular={objectNameSingular}
+        recordTableId={recordTableId}
         draggableItems={
           <>
             {tableRowIds.map((recordId, rowIndex) => {
--- packages/twenty-front/src/modules/ui/layout/draggable-list/components/DraggableTableBody.tsx
+++ packages/twenty-front/src/modules/ui/layout/draggable-list/components/DraggableTableBody.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
-import {
-  DragDropContext,
-  Droppable,
-  OnDragEndResponder,
-} from '@hello-pangea/dnd';
+import { DragDropContext, Droppable, DropResult } from '@hello-pangea/dnd';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { v4 } from 'uuid';
 
+import { useUpdateOneRecord } from '@/object-record/hooks/useUpdateOneRecord';
 import { RecordTablePendingRow } from '@/object-record/record-table/components/RecordTablePendingRow';
+import { useRecordTableStates } from '@/object-record/record-table/hooks/internal/useRecordTableStates';
+import { useComputeNewRowPosition } from '@/object-record/record-table/hooks/useComputeNewRowPosition';
+import { isRemoveSortingModalOpenState } from '@/object-record/record-table/states/isRemoveSortingModalOpenState';
+import { useGetCurrentView } from '@/views/hooks/useGetCurrentView';
+import { isDefined } from '~/utils/isDefined';
 
 type DraggableTableBodyProps = {
   draggableItems: React.ReactNode;
-  onDragEnd: OnDragEndResponder;
+  objectNameSingular: string;
+  recordTableId: string;
 };
 
 const StyledTbody = styled.tbody`
@@ -19,13 +23,52 @@ const StyledTbody = styled.tbody`
 `;
 
 export const DraggableTableBody = ({
+  objectNameSingular,
   draggableItems,
-  onDragEnd,
+  recordTableId,
 }: DraggableTableBodyProps) => {
   const [v4Persistable] = useState(v4());
 
+  const { tableRowIdsState } = useRecordTableStates();
+
+  const tableRowIds = useRecoilValue(tableRowIdsState);
+
+  const { updateOneRecord: updateOneRow } = useUpdateOneRecord({
+    objectNameSingular,
+  });
+
+  const { currentViewWithCombinedFiltersAndSorts } =
+    useGetCurrentView(recordTableId);
+
+  const viewSorts = currentViewWithCombinedFiltersAndSorts?.viewSorts || [];
+
+  const setIsRemoveSortingModalOpenState = useSetRecoilState(
+    isRemoveSortingModalOpenState,
+  );
+  const computeNewRowPosition = useComputeNewRowPosition();
+
+  const handleDragEnd = (result: DropResult) => {
+    if (viewSorts.length > 0) {
+      setIsRemoveSortingModalOpenState(true);
+      return;
+    }
+
+    const computeResult = computeNewRowPosition(result, tableRowIds);
+
+    if (!isDefined(computeResult)) {
+      return;
+    }
+
+    updateOneRow({
+      idToUpdate: computeResult.draggedRecordId,
+      updateOneRecordInput: {
+        position: computeResult.newPosition,
+      },
+    });
+  };
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <Droppable droppableId={v4Persistable}>
         {(provided) => (
           <StyledTbody
