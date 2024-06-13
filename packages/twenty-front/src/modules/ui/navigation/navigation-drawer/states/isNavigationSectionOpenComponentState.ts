e5c0d75c8aa096d42d6ea4627f7750c6e83dcbbe
Félix Malfait <felix.malfait@gmail.com>
2024-06-13 16:32:29 +0200

    Recoil family state

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/states/isNavigationSectionOpenComponentState.ts
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/states/isNavigationSectionOpenComponentState.ts
@@ -1,7 +1,15 @@
+import { ComponentStateKey } from '@/ui/utilities/state/component-state/types/ComponentStateKey';
 import { createComponentState } from '@/ui/utilities/state/component-state/utils/createComponentState';
+import { localStorageEffect } from '~/utils/recoil-effects';
 
-export const isNavigationSectionOpenComponentState =
-  createComponentState<boolean>({
+export const isNavigationSectionOpenComponentState = ({
+  scopeId,
+}: ComponentStateKey) => {
+  return createComponentState<boolean>({
     key: 'isNavigationSectionOpenComponentState',
     defaultValue: true,
-  });
+    effects: [
+      localStorageEffect('isNavigationSectionOpenComponentState' + scopeId),
+    ],
+  })({ scopeId });
+};
