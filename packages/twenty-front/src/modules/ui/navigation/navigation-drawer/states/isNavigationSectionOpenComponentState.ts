25a2faf5919c0fe4a345693072478a7f543bc720
Félix Malfait <felix.malfait@gmail.com>
2024-06-13 22:24:43 +0200

    fix family state

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/states/isNavigationSectionOpenComponentState.ts
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/states/isNavigationSectionOpenComponentState.ts
@@ -1,15 +1,9 @@
-import { ComponentStateKey } from '@/ui/utilities/state/component-state/types/ComponentStateKey';
 import { createComponentState } from '@/ui/utilities/state/component-state/utils/createComponentState';
 import { localStorageEffect } from '~/utils/recoil-effects';
 
-export const isNavigationSectionOpenComponentState = ({
-  scopeId,
-}: ComponentStateKey) => {
-  return createComponentState<boolean>({
+export const isNavigationSectionOpenComponentState =
+  createComponentState<boolean>({
     key: 'isNavigationSectionOpenComponentState',
     defaultValue: true,
-    effects: [
-      localStorageEffect('isNavigationSectionOpenComponentState' + scopeId),
-    ],
-  })({ scopeId });
-};
+    effects: [localStorageEffect()],
+  });
