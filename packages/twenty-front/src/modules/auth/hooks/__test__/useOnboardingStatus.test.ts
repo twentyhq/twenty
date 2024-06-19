29bc0bb2e83b9c2c4de7bc4f3c3d8b5fdeb842a7
martmull <martmull@hotmail.fr>
2024-06-19 15:53:12 +0200

    Fix tests

--- packages/twenty-front/src/modules/auth/hooks/__test__/useOnboardingStatus.test.ts
+++ packages/twenty-front/src/modules/auth/hooks/__test__/useOnboardingStatus.test.ts
@@ -4,11 +4,6 @@ import { RecoilRoot, useSetRecoilState } from 'recoil';
 
 import { useOnboardingStatus } from '@/auth/hooks/useOnboardingStatus';
 import { CurrentUser, currentUserState } from '@/auth/states/currentUserState';
-import {
-  CurrentWorkspace,
-  currentWorkspaceState,
-} from '@/auth/states/currentWorkspaceState';
-import { isVerifyPendingState } from '@/auth/states/isVerifyPendingState';
 import { tokenPairState } from '@/auth/states/tokenPairState';
 import { OnboardingStatus } from '~/generated/graphql';
 
@@ -16,41 +11,22 @@ const tokenPair = {
   accessToken: { token: 'accessToken', expiresAt: 'expiresAt' },
   refreshToken: { token: 'refreshToken', expiresAt: 'expiresAt' },
 };
-const billing = {
-  billingUrl: 'testing.com',
-  isBillingEnabled: true,
-};
 const currentUser = {
   id: '1',
-  email: 'test@test',
-  supportUserHash: '1',
-  canImpersonate: false,
   onboardingStatus: null,
 } as CurrentUser;
-const currentWorkspace = {
-  activationStatus: 'active',
-  id: '1',
-  allowImpersonation: true,
-  currentBillingSubscription: {
-    status: 'trialing',
-  },
-} as CurrentWorkspace;
 
 const renderHooks = () => {
   const { result } = renderHook(
     () => {
       const onboardingStatus = useOnboardingStatus();
-      const setCurrentWorkspace = useSetRecoilState(currentWorkspaceState);
       const setCurrentUser = useSetRecoilState(currentUserState);
       const setTokenPair = useSetRecoilState(tokenPairState);
-      const setVerifyPending = useSetRecoilState(isVerifyPendingState);
 
       return {
         onboardingStatus,
         setCurrentUser,
-        setCurrentWorkspace,
         setTokenPair,
-        setVerifyPending,
       };
     },
     {
@@ -61,192 +37,29 @@ const renderHooks = () => {
 };
 
 describe('useOnboardingStatus', () => {
-  it('should return "ongoing_user_creation" when user is not logged in', async () => {
+  it(`should return "${OnboardingStatus.UserCreation}" when user is not logged in`, async () => {
     const { result } = renderHooks();
-
-    expect(result.current.onboardingStatus).toBe('ongoing_user_creation');
+    expect(result.current.onboardingStatus).toBe(OnboardingStatus.UserCreation);
   });
 
-  it('should return "incomplete"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
+  Object.values(OnboardingStatus)
+    .filter(
+      (onboardingStatus) => onboardingStatus !== OnboardingStatus.UserCreation,
+    )
+    .forEach((onboardingStatus) => {
+      it(`should return "${onboardingStatus}"`, async () => {
+        const { result } = renderHooks();
+        const { setTokenPair, setCurrentUser } = result.current;
 
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.SubscriptionIncomplete,
-      });
-      setCurrentWorkspace(currentWorkspace);
-    });
+        act(() => {
+          setTokenPair(tokenPair);
+          setCurrentUser({
+            ...currentUser,
+            onboardingStatus,
+          });
+        });
 
-    expect(result.current.onboardingStatus).toBe('incomplete');
-  });
-
-  it('should return "canceled"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.SubscriptionCanceled,
-      });
-      setCurrentWorkspace(currentWorkspace);
-    });
-
-    expect(result.current.onboardingStatus).toBe('canceled');
-  });
-
-  it('should return "ongoing_workspace_activation"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.WorkspaceActivation,
-      });
-      setCurrentWorkspace(currentWorkspace);
-    });
-
-    expect(result.current.onboardingStatus).toBe(
-      'ongoing_workspace_activation',
-    );
-  });
-
-  it('should return "ongoing_profile_creation"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.ProfileCreation,
-      });
-      setCurrentWorkspace({
-        ...currentWorkspace,
-        subscriptionStatus: 'active',
+        expect(result.current.onboardingStatus).toBe(onboardingStatus);
       });
     });
-
-    expect(result.current.onboardingStatus).toBe('ongoing_profile_creation');
-  });
-
-  it('should return "ongoing_sync_email"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.SyncEmail,
-      });
-      setCurrentWorkspace({
-        ...currentWorkspace,
-        subscriptionStatus: 'active',
-      });
-    });
-
-    expect(result.current.onboardingStatus).toBe('ongoing_sync_email');
-  });
-
-  it('should return "ongoing_invite_team"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.InviteTeam,
-      });
-      setCurrentWorkspace({
-        ...currentWorkspace,
-        subscriptionStatus: 'active',
-      });
-    });
-
-    expect(result.current.onboardingStatus).toBe('ongoing_invite_team');
-  });
-
-  it('should return "completed"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser(currentUser);
-      setCurrentWorkspace({
-        ...currentWorkspace,
-        subscriptionStatus: 'active',
-      });
-    });
-
-    expect(result.current.onboardingStatus).toBe('completed');
-  });
-
-  it('should return "past_due"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.SubscriptionPastDue,
-      });
-      setCurrentWorkspace(currentWorkspace);
-    });
-
-    expect(result.current.onboardingStatus).toBe('past_due');
-  });
-
-  it('should return "unpaid"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.SubscriptionUnpaid,
-      });
-      setCurrentWorkspace(currentWorkspace);
-    });
-
-    expect(result.current.onboardingStatus).toBe('unpaid');
-  });
-
-  it('should return "completed_without_subscription"', async () => {
-    const { result } = renderHooks();
-    const { setTokenPair, setCurrentUser, setCurrentWorkspace } =
-      result.current;
-
-    act(() => {
-      setTokenPair(tokenPair);
-      setCurrentUser({
-        ...currentUser,
-        onboardingStatus: OnboardingStatus.CompletedWithoutSubscription,
-      });
-      setCurrentWorkspace(currentWorkspace);
-    });
-
-    expect(result.current.onboardingStatus).toBe(
-      'completed_without_subscription',
-    );
-  });
 });
