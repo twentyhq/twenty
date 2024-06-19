88bfc1284f75c5cce1934ff1c679e47bf6fd1647
aditya pimpalkar <adipimpalkar@gmail.com>
2024-06-19 07:53:41 +0100

    format date as per timezone

--- packages/twenty-front/src/modules/ui/field/display/components/DateTimeDisplay.tsx
+++ packages/twenty-front/src/modules/ui/field/display/components/DateTimeDisplay.tsx
@@ -1,7 +1,11 @@
-import format from 'date-fns-tz/format';
+import { useState } from 'react';
+import formatInTimeZone from 'date-fns-tz/formatInTimeZone';
 import { useRecoilValue } from 'recoil';
 
 import { currentWorkspaceMemberState } from '@/auth/states/currentWorkspaceMemberState';
+import { DateFormat } from '@/workspace-member/constants/DateFormat';
+import { TimeFormat } from '@/workspace-member/constants/TimeFormat';
+import { detectTimeZone } from '@/workspace-member/utils/detectTimeZone';
 import { parseDate } from '~/utils/date-utils';
 
 import { EllipsisDisplay } from './EllipsisDisplay';
@@ -13,13 +17,22 @@ type DateTimeDisplayProps = {
 export const DateTimeDisplay = ({ value }: DateTimeDisplayProps) => {
   const currentWorkspaceMember = useRecoilValue(currentWorkspaceMemberState);
 
+  const [timeZone] = useState(
+    currentWorkspaceMember?.timeZone ?? detectTimeZone(),
+  );
+  const [dateFormat] = useState(
+    (currentWorkspaceMember?.dateFormat as DateFormat) ??
+      DateFormat.MONTH_FIRST,
+  );
+  const [timeFormat] = useState(
+    (currentWorkspaceMember?.timeFormat as TimeFormat) ?? TimeFormat.MILITARY,
+  );
+
   const formatDatetime = (date: Date | string) => {
-    return format(
+    return formatInTimeZone(
       parseDate(date).toJSDate(),
-      `${currentWorkspaceMember?.dateFormat} ${currentWorkspaceMember?.timeFormat}`,
-      {
-        timeZone: currentWorkspaceMember?.timeZone,
-      },
+      timeZone,
+      `${dateFormat} ${timeFormat}`,
     );
   };
   return <EllipsisDisplay>{value && formatDatetime(value)}</EllipsisDisplay>;
