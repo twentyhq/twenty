64b456912acae0990dd442bb199fb06e3e29f400
gitstart-twenty <140154534+gitstart-twenty@users.noreply.github.com>
2024-06-19 07:34:42 -0400

    Favicons are being re-rendered on hover (#5849)
    ### Description
    
    Favicons are being re-rendered on hover
    
    ### Refs
    
    #3523
    
    ### Demo
    
    
    https://www.loom.com/share/e3944d940a014283af8c26baac1fed57?sid=e3e96a81-3a54-4969-8602-99c64bb3ffe7
    
    Fixes #3523
    
    ---------
    
    Co-authored-by: gitstart-twenty <gitstart-twenty@users.noreply.github.com>
    Co-authored-by: v1b3m <vibenjamin6@gmail.com>
    Co-authored-by: Thiago Nascimbeni <tnascimbeni@gmail.com>
    Co-authored-by: Lucas Bordeau <bordeau.lucas@gmail.com>

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/ChipFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/ChipFieldDisplay.tsx
@@ -1,7 +1,6 @@
 import { EntityChip } from 'twenty-ui';
 
 import { useChipFieldDisplay } from '@/object-record/record-field/meta-types/hooks/useChipFieldDisplay';
-import { getImageAbsoluteURIOrBase64 } from '~/utils/image/getImageAbsoluteURIOrBase64';
 
 export const ChipFieldDisplay = () => {
   const { recordValue, generateRecordChipData } = useChipFieldDisplay();
@@ -17,7 +16,7 @@ export const ChipFieldDisplay = () => {
       entityId={recordValue.id}
       name={recordChipData.name as any}
       avatarType={recordChipData.avatarType}
-      avatarUrl={getImageAbsoluteURIOrBase64(recordChipData.avatarUrl) ?? ''}
+      avatarUrl={recordChipData.avatarUrl ?? ''}
       linkToEntity={recordChipData.linkToShowPage}
     />
   );
