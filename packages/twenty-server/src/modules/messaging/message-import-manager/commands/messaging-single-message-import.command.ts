7de72b95fb33fd761fca6de268eea75b23492e69
bosiraphael <raphael.bosi@gmail.com>
2024-06-19 18:14:47 +0200

    add options

--- packages/twenty-server/src/modules/messaging/message-import-manager/commands/messaging-single-message-import.command.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/commands/messaging-single-message-import.command.ts
@@ -1,9 +1,12 @@
-import { Command, CommandRunner } from 'nest-commander';
+import { Command, CommandRunner, Option } from 'nest-commander';
 
 import { InjectMessageQueue } from 'src/engine/integrations/message-queue/decorators/message-queue.decorator';
 import { MessageQueue } from 'src/engine/integrations/message-queue/message-queue.constants';
 import { MessageQueueService } from 'src/engine/integrations/message-queue/services/message-queue.service';
-import { MessagingAddSingleMessageToCacheForImportJob } from 'src/modules/messaging/message-import-manager/jobs/messaging-add-single-message-to-cache-for-import.job';
+import {
+  MessagingAddSingleMessageToCacheForImportJob,
+  MessagingAddSingleMessageToCacheForImportJobData,
+} from 'src/modules/messaging/message-import-manager/jobs/messaging-add-single-message-to-cache-for-import.job';
 
 type MessagingSingleMessageImportCommandOptions = {
   messageId: string;
@@ -27,7 +30,7 @@ export class MessagingSingleMessageImportCommand extends CommandRunner {
     _passedParam: string[],
     options: MessagingSingleMessageImportCommandOptions,
   ): Promise<void> {
-    await this.messageQueueService.add(
+    await this.messageQueueService.add<MessagingAddSingleMessageToCacheForImportJobData>(
       MessagingAddSingleMessageToCacheForImportJob.name,
       {
         messageId: options.messageId,
@@ -35,5 +38,32 @@ export class MessagingSingleMessageImportCommand extends CommandRunner {
         workspaceId: options.workspaceId,
       },
     );
+  }
+
+  @Option({
+    flags: '-m, --message-id [message_id]',
+    description: 'Message ID',
+    required: true,
+  })
+  parseMessageId(value: string): string {
+    return value;
+  }
+
+  @Option({
+    flags: '-n, --message-channel-id [message_channel_id]',
+    description: 'Message channel ID',
+    required: true,
+  })
+  parseMessageChannelId(value: string): string {
+    return value;
+  }
+
+  @Option({
+    flags: '-w, --workspace-id [workspace_id]',
+    description: 'Workspace ID',
+    required: true,
+  })
+  parseWorkspaceId(value: string): string {
+    return value;
   }
 }
