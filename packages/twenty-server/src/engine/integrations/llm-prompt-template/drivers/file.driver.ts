3c080ef89ee9f823f1f425bc091f9d05a4121640
ad-elias <elias@autodiligence.com>
2024-06-20 22:23:49 +0200

    Create file prompt template driver

--- packages/twenty-server/src/engine/integrations/llm-prompt-template/drivers/file.driver.ts
+++ packages/twenty-server/src/engine/integrations/llm-prompt-template/drivers/file.driver.ts
@@ -1,4 +1,5 @@
-import { NotImplementedException } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 import { PromptTemplate } from '@langchain/core/prompts';
 
@@ -6,12 +7,36 @@ import { LLMPromptTemplateDriver } from 'src/engine/integrations/llm-prompt-temp
 import { LLMPromptTemplateEnvVar } from 'src/engine/integrations/llm-prompt-template/interfaces/llm-prompt-template-name.interface';
 import { PromptTemplateInput } from 'src/engine/integrations/llm-prompt-template/interfaces/llm-prompt-template.interface';
 
+import { resolveAbsolutePath } from 'src/utils/resolve-absolute-path';
+
 export class FileDriver implements LLMPromptTemplateDriver {
   constructor() {}
 
   async getPromptTemplate<T extends LLMPromptTemplateEnvVar>(
     promptTemplateName: string,
   ): Promise<PromptTemplate<PromptTemplateInput<T>, any>> {
-    throw new NotImplementedException();
+    const filePath = join(
+      resolveAbsolutePath('.llm-prompt-templates'),
+      `${promptTemplateName}.txt`,
+    );
+
+    let promptTemplateString: string;
+
+    try {
+      promptTemplateString = readFileSync(filePath, {
+        encoding: 'utf-8',
+      });
+    } catch (e) {
+      throw new Error(
+        `Missing LLM prompt template at the following path: ${filePath}`,
+      );
+    }
+
+    const promptTemplate = PromptTemplate.fromTemplate<any>(
+      promptTemplateString,
+      { templateFormat: 'mustache' },
+    );
+
+    return promptTemplate;
   }
 }
