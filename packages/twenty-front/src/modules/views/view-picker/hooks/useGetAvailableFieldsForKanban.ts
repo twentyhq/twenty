51c24df7b3d955b66b7db12e86a9390df1abe84f
Achsan <achsanh@gmail.com>
2024-06-20 05:25:55 -0400

    feat(view): Reset `dirty` state of `view picker` before navigating to settings

--- packages/twenty-front/src/modules/views/view-picker/hooks/useGetAvailableFieldsForKanban.ts
+++ packages/twenty-front/src/modules/views/view-picker/hooks/useGetAvailableFieldsForKanban.ts
@@ -1,16 +1,19 @@
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
 import { useViewStates } from '@/views/hooks/internal/useViewStates';
+import { useViewPickerStates } from '@/views/view-picker/hooks/useViewPickerStates';
 import { FieldMetadataType } from '~/generated-metadata/graphql';
 
 export const useGetAvailableFieldsForKanban = () => {
   const { viewObjectMetadataIdState } = useViewStates();
+  const { viewPickerIsDirtyState } = useViewPickerStates();
 
   const viewObjectMetadataId = useRecoilValue(viewObjectMetadataIdState);
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
+  const setViewPickerIsDirty = useSetRecoilState(viewPickerIsDirtyState);
 
   const objectMetadataItem = objectMetadataItems.find(
     (objectMetadata) => objectMetadata.id === viewObjectMetadataId,
@@ -24,8 +27,9 @@ export const useGetAvailableFieldsForKanban = () => {
   const navigate = useNavigate();
 
   const navigateToSelectSettings = useCallback(() => {
+    setViewPickerIsDirty(false);
     navigate(`/settings/objects/${objectMetadataItem?.namePlural}`);
-  }, [navigate, objectMetadataItem?.namePlural]);
+  }, [navigate, objectMetadataItem?.namePlural, setViewPickerIsDirty]);
 
   return {
     availableFieldsForKanban,
