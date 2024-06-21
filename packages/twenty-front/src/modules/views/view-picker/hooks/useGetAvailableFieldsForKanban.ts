7bc5c0fcc4aa6fff96740ba18efd28357f6058cd
Achsan <achsanh@gmail.com>
2024-06-21 10:45:43 -0400

    Revert "Fix(view): `Create` Button is not visible when creating `Kanban` View (#5969)"
    This reverts commit 7a0f097df4bf680dc79108f2eff9228060821ae5.

--- packages/twenty-front/src/modules/views/view-picker/hooks/useGetAvailableFieldsForKanban.ts
+++ packages/twenty-front/src/modules/views/view-picker/hooks/useGetAvailableFieldsForKanban.ts
@@ -1,24 +1,16 @@
 import { useCallback } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
-import { navigationMemorizedUrlState } from '@/ui/navigation/states/navigationMemorizedUrlState';
 import { useViewStates } from '@/views/hooks/internal/useViewStates';
-import { useViewPickerStates } from '@/views/view-picker/hooks/useViewPickerStates';
 import { FieldMetadataType } from '~/generated-metadata/graphql';
 
 export const useGetAvailableFieldsForKanban = () => {
   const { viewObjectMetadataIdState } = useViewStates();
-  const { viewPickerIsDirtyState } = useViewPickerStates();
 
   const viewObjectMetadataId = useRecoilValue(viewObjectMetadataIdState);
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
-  const setViewPickerIsDirty = useSetRecoilState(viewPickerIsDirtyState);
-  const setNavigationMemorizedUrl = useSetRecoilState(
-    navigationMemorizedUrlState,
-  );
-  const location = useLocation();
 
   const objectMetadataItem = objectMetadataItems.find(
     (objectMetadata) => objectMetadata.id === viewObjectMetadataId,
@@ -32,18 +24,8 @@ export const useGetAvailableFieldsForKanban = () => {
   const navigate = useNavigate();
 
   const navigateToSelectSettings = useCallback(() => {
-    setViewPickerIsDirty(false);
-
-    setNavigationMemorizedUrl(location.pathname + location.search);
-
     navigate(`/settings/objects/${objectMetadataItem?.namePlural}`);
-  }, [
-    navigate,
-    objectMetadataItem?.namePlural,
-    setViewPickerIsDirty,
-    setNavigationMemorizedUrl,
-    location,
-  ]);
+  }, [navigate, objectMetadataItem?.namePlural]);
 
   return {
     availableFieldsForKanban,
