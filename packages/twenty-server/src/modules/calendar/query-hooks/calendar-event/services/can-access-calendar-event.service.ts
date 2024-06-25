cbbace8cff5070f486de59e3ce79f9cee7e5e86b
Weiko <corentin@twenty.com>
2024-06-25 11:57:53 +0200

    fix missing relation

--- packages/twenty-server/src/modules/calendar/query-hooks/calendar-event/services/can-access-calendar-event.service.ts
+++ packages/twenty-server/src/modules/calendar/query-hooks/calendar-event/services/can-access-calendar-event.service.ts
@@ -40,6 +40,7 @@ export class CanAccessCalendarEventService {
           ),
         ),
       },
+      relations: ['connectedAccount'],
     });
 
     const calendarChannelsGroupByVisibility = groupBy(
