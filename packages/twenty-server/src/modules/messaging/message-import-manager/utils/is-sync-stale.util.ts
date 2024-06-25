f8a2460153bcd3f319398eebf32fcdf44337c486
bosiraphael <raphael.bosi@gmail.com>
2024-06-25 11:28:28 +0200

    throw in isSyncStale if date format is invalid

--- packages/twenty-server/src/modules/messaging/message-import-manager/utils/is-sync-stale.util.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/utils/is-sync-stale.util.ts
@@ -1,17 +1,13 @@
 import { MESSAGING_IMPORT_ONGOING_SYNC_TIMEOUT } from 'src/modules/messaging/message-import-manager/constants/messaging-import-ongoing-sync-timeout.constant';
 
 export const isSyncStale = (syncStageStartedAt: string): boolean => {
-  try {
-    const syncStageStartedTime = new Date(syncStageStartedAt).getTime();
+  const syncStageStartedTime = new Date(syncStageStartedAt).getTime();
 
-    if (isNaN(syncStageStartedTime)) {
-      throw new Error('Invalid date format');
-    }
-
-    return (
-      Date.now() - syncStageStartedTime > MESSAGING_IMPORT_ONGOING_SYNC_TIMEOUT
-    );
-  } catch (error) {
-    return false;
+  if (isNaN(syncStageStartedTime)) {
+    throw new Error('Invalid date format');
   }
+
+  return (
+    Date.now() - syncStageStartedTime > MESSAGING_IMPORT_ONGOING_SYNC_TIMEOUT
+  );
 };
