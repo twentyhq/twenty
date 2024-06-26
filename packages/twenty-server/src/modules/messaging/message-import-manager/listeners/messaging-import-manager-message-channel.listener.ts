70df40824fbc504413fe714e3f9da735c464a328
bosiraphael <raphael.bosi@gmail.com>
2024-06-26 14:43:36 +0200

    fix event listener

--- packages/twenty-server/src/modules/messaging/message-import-manager/listeners/messaging-import-manager-message-channel.listener.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/listeners/messaging-import-manager-message-channel.listener.ts
@@ -10,6 +10,12 @@ import {
   MessagingCleanCacheJob,
   MessagingCleanCacheJobData,
 } from 'src/modules/messaging/message-import-manager/jobs/messaging-clean-cache';
+import { ObjectRecordUpdateEvent } from 'src/engine/integrations/event-emitter/types/object-record-update.event';
+import { objectRecordChangedProperties } from 'src/engine/integrations/event-emitter/utils/object-record-changed-properties.util';
+import {
+  MessagingCreateCompanyAndContactAfterSyncJobData,
+  MessagingCreateCompanyAndContactAfterSyncJob,
+} from 'src/modules/messaging/message-participants-manager/jobs/messaging-create-company-and-contact-after-sync.job';
 
 @Injectable()
 export class MessagingMessageImportManagerMessageChannelListener {
@@ -29,5 +35,26 @@ export class MessagingMessageImportManagerMessageChannelListener {
         messageChannelId: payload.recordId,
       },
     );
+  }
+
+  @OnEvent('messageChannel.updated')
+  async handleUpdatedEvent(
+    payload: ObjectRecordUpdateEvent<MessageChannelWorkspaceEntity>,
+  ) {
+    if (
+      objectRecordChangedProperties(
+        payload.properties.before,
+        payload.properties.after,
+      ).includes('isContactAutoCreationEnabled') &&
+      payload.properties.after.isContactAutoCreationEnabled
+    ) {
+      await this.messageQueueService.add<MessagingCreateCompanyAndContactAfterSyncJobData>(
+        MessagingCreateCompanyAndContactAfterSyncJob.name,
+        {
+          workspaceId: payload.workspaceId,
+          messageChannelId: payload.recordId,
+        },
+      );
+    }
   }
 }
