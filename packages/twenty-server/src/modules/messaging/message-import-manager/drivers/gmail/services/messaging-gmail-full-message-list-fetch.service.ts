1a66db5bffa5da4fa025f94fd498808576e5607d
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2024-06-27 17:07:45 +0200

    Refactor messaging refresh access token (#6034)
    - Put error handling outside of `refreshAndSaveAccessToken`
    - return after failing to refresh access token in
    `processMessageBatchImport`
    - remove unnecessary token refresh in `processMessageListFetch`

--- packages/twenty-server/src/modules/messaging/message-import-manager/drivers/gmail/services/messaging-gmail-full-message-list-fetch.service.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/drivers/gmail/services/messaging-gmail-full-message-list-fetch.service.ts
@@ -22,8 +22,6 @@ import { MessagingChannelSyncStatusService } from 'src/modules/messaging/common/
 import { MessagingGmailClientProvider } from 'src/modules/messaging/message-import-manager/drivers/gmail/providers/messaging-gmail-client.provider';
 import { MESSAGING_GMAIL_USERS_MESSAGES_LIST_MAX_RESULT } from 'src/modules/messaging/message-import-manager/drivers/gmail/constants/messaging-gmail-users-messages-list-max-result.constant';
 import { MESSAGING_GMAIL_EXCLUDED_CATEGORIES } from 'src/modules/messaging/message-import-manager/drivers/gmail/constants/messaging-gmail-excluded-categories';
-import { GoogleAPIRefreshAccessTokenService } from 'src/modules/connected-account/services/google-api-refresh-access-token/google-api-refresh-access-token.service';
-import { ConnectedAccountRepository } from 'src/modules/connected-account/repositories/connected-account.repository';
 
 @Injectable()
 export class MessagingGmailFullMessageListFetchService {
@@ -41,11 +39,8 @@ export class MessagingGmailFullMessageListFetchService {
       MessageChannelMessageAssociationWorkspaceEntity,
     )
     private readonly messageChannelMessageAssociationRepository: MessageChannelMessageAssociationRepository,
-    @InjectObjectMetadataRepository(ConnectedAccountWorkspaceEntity)
-    private readonly connectedAccountRepository: ConnectedAccountRepository,
     private readonly messagingChannelSyncStatusService: MessagingChannelSyncStatusService,
     private readonly gmailErrorHandlingService: MessagingErrorHandlingService,
-    private readonly googleAPIsRefreshAccessTokenService: GoogleAPIRefreshAccessTokenService,
   ) {}
 
   public async processMessageListFetch(
@@ -58,26 +53,9 @@ export class MessagingGmailFullMessageListFetchService {
       workspaceId,
     );
 
-    await this.googleAPIsRefreshAccessTokenService.refreshAndSaveAccessToken(
-      workspaceId,
-      connectedAccount.id,
-    );
-
-    const refreshedConnectedAccount =
-      await this.connectedAccountRepository.getById(
-        connectedAccount.id,
-        workspaceId,
-      );
-
-    if (!refreshedConnectedAccount) {
-      throw new Error(
-        `Connected account ${connectedAccount.id} not found in workspace ${workspaceId}`,
-      );
-    }
-
     const gmailClient: gmail_v1.Gmail =
       await this.gmailClientProvider.getGmailClient(
-        refreshedConnectedAccount.refreshToken,
+        connectedAccount.refreshToken,
       );
 
     const { error: gmailError } =
