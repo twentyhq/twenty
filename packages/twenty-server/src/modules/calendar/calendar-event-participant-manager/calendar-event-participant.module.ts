50c8e2618ebc00bc6781df9c015ab531b68053ab
bosiraphael <raphael.bosi@gmail.com>
2024-07-01 14:10:11 +0200

    reorder imports

--- packages/twenty-server/src/modules/calendar/calendar-event-participant-manager/calendar-event-participant.module.ts
+++ packages/twenty-server/src/modules/calendar/calendar-event-participant-manager/calendar-event-participant.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
-
 import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
 import { TwentyORMModule } from 'src/engine/twenty-orm/twenty-orm.module';
 import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
