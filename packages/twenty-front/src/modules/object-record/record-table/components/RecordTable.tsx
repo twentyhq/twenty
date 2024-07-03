2965438cd61d96330aa6035be7d802b63be78d93
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-07-03 16:05:38 +0200

    Ok for soft focus

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTable.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTable.tsx
@@ -1,8 +1,5 @@
-import { useRef } from 'react';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useRecoilValue } from 'recoil';
-import { RGBA } from 'twenty-ui';
 
 import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
 import { RecordTableBody } from '@/object-record/record-table/components/RecordTableBody';
@@ -21,8 +18,6 @@ import {
 import { useTriggerContextMenu } from '@/object-record/record-table/record-table-cell/hooks/useTriggerContextMenu';
 import { useUpsertRecordV2 } from '@/object-record/record-table/record-table-cell/hooks/useUpsertRecordV2';
 import { RecordTableScope } from '@/object-record/record-table/scopes/RecordTableScope';
-import { isRecordTableScrolledLeftState } from '@/object-record/record-table/states/isRecordTableScrolledLeftState';
-import { isRecordTableScrolledTopState } from '@/object-record/record-table/states/isRecordTableScrolledTopState';
 import { MoveFocusDirection } from '@/object-record/record-table/types/MoveFocusDirection';
 import { TableCellPosition } from '@/object-record/record-table/types/TableCellPosition';
 
@@ -36,58 +31,6 @@ const StyledTable = styled.table<{
   table-layout: fixed;
 
   width: calc(100% - ${({ theme }) => theme.table.horizontalCellMargin} * 2);
-
-  &.freeze-first-columns thead th:nth-of-type(3)::after,
-  &.freeze-first-columns tbody td:nth-of-type(3)::after {
-    content: '';
-    height: calc(100% + 1px);
-    position: absolute;
-    width: 4px;
-    right: -4px;
-    top: 0;
-    border-right: none;
-
-    ${({ theme }) => css`
-      box-shadow: 4px 0px 4px -4px ${theme.name === 'dark'
-          ? RGBA(theme.grayScale.gray50, 0.8)
-          : RGBA(theme.grayScale.gray100, 0.25)} inset;
-    `}
-  }
-
-  &.freeze-first-row th::after {
-    content: '';
-    width: calc(100%);
-    position: absolute;
-    height: 4px;
-    bottom: -4px;
-    left: 0;
-    z-index: 0;
-
-    ${({ theme }) => css`
-      box-shadow: 0px 4px 4px -4px ${theme.name === 'dark'
-          ? RGBA(theme.grayScale.gray50, 0.8)
-          : RGBA(theme.grayScale.gray100, 0.25)} inset;
-    `}
-  }
-
-  &.freeze-first-row.freeze-first-columns th:nth-of-type(3)::after {
-    content: '';
-    width: calc(100%);
-    position: absolute;
-    height: 4px;
-    bottom: -4px;
-    left: 0;
-    z-index: 0;
-
-    right: auto;
-    top: auto;
-
-    ${({ theme }) => css`
-      box-shadow: 0px 4px 4px -4px ${theme.name === 'dark'
-          ? RGBA(theme.grayScale.gray50, 0.8)
-          : RGBA(theme.grayScale.gray100, 0.25)} inset;
-    `}
-  }
 `;
 
 type RecordTableProps = {
@@ -103,7 +46,6 @@ export const RecordTable = ({
   onColumnsChange,
   createRecord,
 }: RecordTableProps) => {
-  const tableRef = useRef<HTMLTableElement>(null);
   const { scopeId, visibleTableColumnsSelector } =
     useRecordTableStates(recordTableId);
 
@@ -166,68 +108,6 @@ export const RecordTable = ({
 
   const visibleTableColumns = useRecoilValue(visibleTableColumnsSelector());
 
-  // const scrollWrapper = useContext(ScrollWrapperContext);
-
-  // const handleScroll = useDebouncedCallback(
-  //   useCallback(() => {
-  //     const styledTableElement = tableRef.current;
-
-  //     const scrollWrapperElement = scrollWrapper.current;
-
-  //     const currentScrollLeft =
-  //       scrollWrapperElement?.children.item(1)?.scrollLeft ?? 0;
-  //     const currentScrollTop =
-  //       scrollWrapperElement?.children.item(1)?.scrollTop ?? 0;
-
-  //     if (currentScrollLeft > 0) {
-  //       styledTableElement?.classList.add('freeze-first-columns');
-  //     } else {
-  //       styledTableElement?.classList.remove('freeze-first-columns');
-  //     }
-
-  //     if (currentScrollTop > 0) {
-  //       styledTableElement?.classList.add('freeze-first-row');
-  //     } else {
-  //       styledTableElement?.classList.remove('freeze-first-row');
-  //     }
-  //   }, [scrollWrapper]),
-  //   0,
-  //   {
-  //     leading: true,
-  //   },
-  // );
-
-  // useLayoutEffect(() => {
-  //   const scrollWrapperElement = scrollWrapper.current;
-
-  //   scrollWrapperElement?.addEventListener('wheel', handleScroll, {
-  //     capture: true,
-  //     passive: true,
-  //   });
-  //   scrollWrapperElement?.addEventListener('scroll', handleScroll, {
-  //     capture: true,
-  //     passive: true,
-  //   });
-
-  //   // scrollWrapperElement?.addEventListener('touchmove', handleScroll, {
-  //   //   capture: true,
-  //   //   passive: true,
-  //   // });
-
-  //   return () => {
-  //     scrollWrapperElement?.removeEventListener('wheel', handleScroll);
-  //     scrollWrapperElement?.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, [scrollWrapper, handleScroll]);
-
-  const isRecordTableScrolledTop = useRecoilValue(
-    isRecordTableScrolledTopState,
-  );
-
-  const isRecordTableScrolledLeft = useRecoilValue(
-    isRecordTableScrolledLeftState,
-  );
-
   return (
     <RecordTableScope
       recordTableScopeId={scopeId}
@@ -247,12 +127,7 @@ export const RecordTable = ({
             visibleTableColumns,
           }}
         >
-          <StyledTable
-            ref={tableRef}
-            className="entity-table-cell"
-            freezeFirstColumns={!isRecordTableScrolledLeft}
-            freezeFirstRow={!isRecordTableScrolledTop}
-          >
+          <StyledTable className="entity-table-cell">
             <RecordTableHeader createRecord={createRecord} />
             <RecordTableBodyEffect objectNameSingular={objectNameSingular} />
             <RecordTableBody
