5ca0e9b0bacb94e5081915c95513dd858a0f2e7c
RobertoSimonini1 <roberto.simonini@endor.studio>
2024-07-08 20:17:35 +0200

    truncate the string if overlow the text

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/FullNameFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/FullNameFieldDisplay.tsx
@@ -2,13 +2,25 @@ import { isNonEmptyString } from '@sniptt/guards';
 
 import { useFullNameFieldDisplay } from '@/object-record/record-field/meta-types/hooks/useFullNameFieldDisplay';
 import { TextDisplay } from '@/ui/field/display/components/TextDisplay';
+import { useIsMobile } from '@/ui/utilities/responsive/hooks/useIsMobile';
+
+const MOBILE_MAX_LENGTH = 15;
+const DESKTOP_MAX_LENGTH = 20;
 
 export const FullNameFieldDisplay = () => {
   const { fieldValue } = useFullNameFieldDisplay();
+  const isMobile = useIsMobile(); 
+
+  const truncateTheContent = (contentToTruncate: string, maxLength: number) => {
+   return contentToTruncate.length > maxLength ? `${contentToTruncate.slice(0, maxLength)}...` : contentToTruncate;
+  }
 
   const content = [fieldValue?.firstName, fieldValue?.lastName]
     .filter(isNonEmptyString)
     .join(' ');
 
-  return <TextDisplay text={content} />;
+    const maxLength = isMobile ? MOBILE_MAX_LENGTH : DESKTOP_MAX_LENGTH;
+    const truncatedContent = truncateTheContent(content, maxLength);
+
+    return <TextDisplay text={truncatedContent} />;
 };
