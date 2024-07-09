ee7b6bf099bd106a82464c2cf34b06488fb37b82
Charles Bochet <charlesBochet@users.noreply.github.com>
2024-07-09 14:57:59 +0200

    Display table record creation row when clicking on Add new from table empty state (#6174)
    As per title

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTable.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTable.tsx
@@ -1,12 +1,15 @@
 import styled from '@emotion/styled';
-import { isNonEmptyString } from '@sniptt/guards';
+import { isNonEmptyString, isNull } from '@sniptt/guards';
 
+import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
+import { RecordTableContextProvider } from '@/object-record/record-table/components/RecordTableContextProvider';
+import { RecordTableEmptyState } from '@/object-record/record-table/components/RecordTableEmptyState';
+import { useRecordTableStates } from '@/object-record/record-table/hooks/internal/useRecordTableStates';
 import { RecordTableBody } from '@/object-record/record-table/record-table-body/components/RecordTableBody';
 import { RecordTableBodyEffect } from '@/object-record/record-table/record-table-body/components/RecordTableBodyEffect';
-import { RecordTableContextProvider } from '@/object-record/record-table/components/RecordTableContextProvider';
 import { RecordTableHeader } from '@/object-record/record-table/record-table-header/components/RecordTableHeader';
-import { useRecordTableStates } from '@/object-record/record-table/hooks/internal/useRecordTableStates';
 import { RecordTableScope } from '@/object-record/record-table/scopes/RecordTableScope';
+import { useRecoilValue } from 'recoil';
 
 const StyledTable = styled.table`
   border-radius: ${({ theme }) => theme.border.radius.sm};
@@ -32,6 +35,27 @@ export const RecordTable = ({
 }: RecordTableProps) => {
   const { scopeId } = useRecordTableStates(recordTableId);
 
+  const {
+    isRecordTableInitialLoadingState,
+    tableRowIdsState,
+    pendingRecordIdState,
+  } = useRecordTableStates(recordTableId);
+
+  const isRecordTableInitialLoading = useRecoilValue(
+    isRecordTableInitialLoadingState,
+  );
+
+  const tableRowIds = useRecoilValue(tableRowIdsState);
+
+  const pendingRecordId = useRecoilValue(pendingRecordIdState);
+
+  const { objectMetadataItem: foundObjectMetadataItem } = useObjectMetadataItem(
+    { objectNameSingular },
+  );
+
+  const objectLabel = foundObjectMetadataItem?.labelSingular;
+  const isRemote = foundObjectMetadataItem?.isRemote ?? false;
+
   if (!isNonEmptyString(objectNameSingular)) {
     return <></>;
   }
@@ -45,11 +69,22 @@ export const RecordTable = ({
         objectNameSingular={objectNameSingular}
         recordTableId={recordTableId}
       >
-        <StyledTable className="entity-table-cell">
-          <RecordTableHeader createRecord={createRecord} />
-          <RecordTableBodyEffect />
-          <RecordTableBody />
-        </StyledTable>
+        <RecordTableBodyEffect />
+        {!isRecordTableInitialLoading &&
+        tableRowIds.length === 0 &&
+        isNull(pendingRecordId) ? (
+          <RecordTableEmptyState
+            objectNameSingular={objectNameSingular}
+            objectLabel={objectLabel}
+            createRecord={createRecord}
+            isRemote={isRemote}
+          />
+        ) : (
+          <StyledTable className="entity-table-cell">
+            <RecordTableHeader createRecord={createRecord} />
+            <RecordTableBody />
+          </StyledTable>
+        )}
       </RecordTableContextProvider>
     </RecordTableScope>
   );
