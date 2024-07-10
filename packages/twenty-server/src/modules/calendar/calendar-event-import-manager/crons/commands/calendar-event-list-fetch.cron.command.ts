18a287416aa821daaff447841286f4c27a11cdf3
bosiraphael <raphael.bosi@gmail.com>
2024-07-10 18:00:51 +0200

    fix cron pattern

--- packages/twenty-server/src/modules/calendar/calendar-event-import-manager/crons/commands/calendar-event-list-fetch.cron.command.ts
+++ packages/twenty-server/src/modules/calendar/calendar-event-import-manager/crons/commands/calendar-event-list-fetch.cron.command.ts
@@ -5,7 +5,7 @@ import { MessageQueue } from 'src/engine/integrations/message-queue/message-queu
 import { MessageQueueService } from 'src/engine/integrations/message-queue/services/message-queue.service';
 import { CalendarEventListFetchCronJob } from 'src/modules/calendar/calendar-event-import-manager/crons/jobs/calendar-event-list-fetch.cron.job';
 
-const CALENDAR_EVENTS_IMPORT_CRON_PATTERN = '*/1 * * * *';
+const CALENDAR_EVENTS_IMPORT_CRON_PATTERN = '*/5 * * * *';
 
 @Command({
   name: 'cron:calendar:calendar-event-list-fetch',
