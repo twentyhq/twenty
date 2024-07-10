a414092fa2e9e8393da80d26ad2a5822d2ed5f76
martmull <martmull@hotmail.fr>
2024-07-10 15:54:01 +0200

    Create function record at function upsert

--- packages/twenty-server/src/engine/core-modules/function/function.module.ts
+++ packages/twenty-server/src/engine/core-modules/function/function.module.ts
@@ -3,9 +3,16 @@ import { Module } from '@nestjs/common';
 import { FunctionService } from 'src/engine/core-modules/function/function.service';
 import { FunctionResolver } from 'src/engine/core-modules/function/function.resolver';
 import { FileUploadModule } from 'src/engine/core-modules/file/file-upload/file-upload.module';
+import { UserModule } from 'src/engine/core-modules/user/user.module';
+import { TwentyORMModule } from 'src/engine/twenty-orm/twenty-orm.module';
+import { FunctionWorkspaceEntity } from 'src/modules/function/stadard-objects/function.workspace-entity';
 
 @Module({
-  imports: [FileUploadModule],
+  imports: [
+    FileUploadModule,
+    UserModule,
+    TwentyORMModule.forFeature([FunctionWorkspaceEntity]),
+  ],
   providers: [FunctionService, FunctionResolver],
 })
 export class FunctionModule {}
--- packages/twenty-server/src/engine/core-modules/function/function.service.ts
+++ packages/twenty-server/src/engine/core-modules/function/function.service.ts
@@ -7,10 +7,22 @@ import { FileFolder } from 'src/engine/core-modules/file/interfaces/file-folder.
 
 import { User } from 'src/engine/core-modules/user/user.entity';
 import { FileUploadService } from 'src/engine/core-modules/file/file-upload/services/file-upload.service';
+import { UserService } from 'src/engine/core-modules/user/services/user.service';
+import { InjectWorkspaceRepository } from 'src/engine/twenty-orm/decorators/inject-workspace-repository.decorator';
+import { WorkspaceRepository } from 'src/engine/twenty-orm/repository/workspace.repository';
+import {
+  FunctionSyncStatus,
+  FunctionWorkspaceEntity,
+} from 'src/modules/function/stadard-objects/function.workspace-entity';
 
 @Injectable()
 export class FunctionService {
-  constructor(private readonly fileUploadService: FileUploadService) {}
+  constructor(
+    private readonly fileUploadService: FileUploadService,
+    private readonly userService: UserService,
+    @InjectWorkspaceRepository(FunctionWorkspaceEntity)
+    private readonly functionRepository: WorkspaceRepository<FunctionWorkspaceEntity>,
+  ) {}
 
   async upsertFunction(
     workspaceId: string,
@@ -23,24 +35,30 @@ export class FunctionService {
     const typescriptCode = fileContent.toString('utf8');
     const javascriptCode = this.compileTypeScript(typescriptCode);
 
-    const { path: sourcePath } = await this.fileUploadService.uploadFile({
+    const { path: sourceCodePath } = await this.fileUploadService.uploadFile({
       file: typescriptCode,
       filename,
       mimeType: mimetype,
       fileFolder: FileFolder.Function,
     });
 
-    const { path: compiledPath } = await this.fileUploadService.uploadFile({
+    const { path: builtSourcePath } = await this.fileUploadService.uploadFile({
       file: javascriptCode,
       filename: '.js',
       mimeType: mimetype,
       fileFolder: FileFolder.Function,
     });
 
-    console.log(sourcePath);
-    console.log(compiledPath);
+    const workspaceMember = await this.userService.loadWorkspaceMember(user);
+    const createdFunction = await this.functionRepository.save({
+      name,
+      author: workspaceMember,
+      sourceCodePath,
+      builtSourcePath,
+      syncStatus: FunctionSyncStatus.READY,
+    });
 
-    return sourcePath;
+    return createdFunction.sourceCodePath;
   }
 
   private async readStreamToBuffer(
