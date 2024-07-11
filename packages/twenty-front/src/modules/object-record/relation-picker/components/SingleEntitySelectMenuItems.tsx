3e664720898dd39680d615438cea03cfd46a47bc
aditya pimpalkar <adipimpalkar@gmail.com>
2024-07-11 19:28:23 +0100

    handle key selection for select all and select none buttons

--- packages/twenty-front/src/modules/object-record/relation-picker/components/SingleEntitySelectMenuItems.tsx
+++ packages/twenty-front/src/modules/object-record/relation-picker/components/SingleEntitySelectMenuItems.tsx
@@ -1,14 +1,17 @@
 import { useRef } from 'react';
 import { isNonEmptyString } from '@sniptt/guards';
+import { useRecoilValue } from 'recoil';
 import { Key } from 'ts-key-enum';
 import { IconComponent, IconPlus } from 'twenty-ui';
 
 import { SelectableMenuItemSelect } from '@/object-record/relation-picker/components/SelectableMenuItemSelect';
+import { SINGLE_ENTITY_SELECT_BASE_LIST } from '@/object-record/relation-picker/constants/SingleEntitySelectBaseList';
 import { CreateNewButton } from '@/ui/input/relation-picker/components/CreateNewButton';
 import { DropdownMenuSkeletonItem } from '@/ui/input/relation-picker/components/skeletons/DropdownMenuSkeletonItem';
 import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/DropdownMenuItemsContainer';
 import { DropdownMenuSeparator } from '@/ui/layout/dropdown/components/DropdownMenuSeparator';
 import { SelectableList } from '@/ui/layout/selectable-list/components/SelectableList';
+import { useSelectableList } from '@/ui/layout/selectable-list/hooks/useSelectableList';
 import { MenuItem } from '@/ui/navigation/menu-item/components/MenuItem';
 import { MenuItemSelect } from '@/ui/navigation/menu-item/components/MenuItemSelect';
 import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
@@ -54,18 +57,64 @@ export const SingleEntitySelectMenuItems = ({
 }: SingleEntitySelectMenuItemsProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const entitiesInDropdown = [selectedEntity, ...entitiesToSelect].filter(
+  const createNewRecord = showCreateButton
+    ? {
+        __typename: '',
+        id: 'add-new',
+        name: 'Add New',
+      }
+    : null;
+
+  const selectNone = emptyLabel
+    ? {
+        __typename: '',
+        id: 'select-none',
+        name: emptyLabel,
+      }
+    : null;
+
+  const selectAll = isAllEntitySelectShown
+    ? {
+        __typename: '',
+        id: 'select-all',
+        name: selectAllLabel,
+      }
+    : null;
+
+  const entitiesInDropdown = [
+    selectAll,
+    selectNone,
+    selectedEntity,
+    ...entitiesToSelect,
+    createNewRecord,
+  ].filter(
     (entity): entity is EntityForSelect =>
       isDefined(entity) && isNonEmptyString(entity.name),
+  );
+
+  const { isSelectedItemIdSelector, handleResetSelectedPosition } =
+    useSelectableList(SINGLE_ENTITY_SELECT_BASE_LIST);
+
+  const isSelectedAddNewButton = useRecoilValue(
+    isSelectedItemIdSelector('add-new'),
+  );
+
+  const isSelectedSelectNoneButton = useRecoilValue(
+    isSelectedItemIdSelector('select-none'),
+  );
+
+  const isSelectedSelectAllButton = useRecoilValue(
+    isSelectedItemIdSelector('select-all'),
   );
 
   useScopedHotkeys(
     [Key.Escape],
     () => {
+      handleResetSelectedPosition();
       onCancel?.();
     },
     hotkeyScope,
-    [onCancel],
+    [onCancel, handleResetSelectedPosition],
   );
 
   const selectableItemIds = entitiesInDropdown.map((entity) => entity.id);
@@ -73,16 +122,16 @@ export const SingleEntitySelectMenuItems = ({
   return (
     <div ref={containerRef}>
       <SelectableList
-        selectableListId="single-entity-select-base-list"
+        selectableListId={SINGLE_ENTITY_SELECT_BASE_LIST}
         selectableItemIdArray={selectableItemIds}
         hotkeyScope={hotkeyScope}
         onEnter={(itemId) => {
-          const entityIndex = entitiesInDropdown.findIndex(
-            (entity) => entity.id === itemId,
-          );
-          if (entityIndex < 0 && showCreateButton === true) {
+          if (itemId === 'add-new' && showCreateButton === true) {
             onCreate?.();
           } else {
+            const entityIndex = entitiesInDropdown.findIndex(
+              (entity) => entity.id === itemId,
+            );
             onEntitySelected(entitiesInDropdown[entityIndex]);
           }
         }}
@@ -91,48 +140,76 @@ export const SingleEntitySelectMenuItems = ({
           {loading ? (
             <DropdownMenuSkeletonItem />
           ) : entitiesInDropdown.length === 0 && !isAllEntitySelectShown ? (
-            <MenuItem text="No result" />
-          ) : (
             <>
-              {isAllEntitySelectShown &&
-                selectAllLabel &&
-                onAllEntitySelected && (
-                  <MenuItemSelect
-                    key="select-all"
-                    onClick={() => onAllEntitySelected()}
-                    LeftIcon={SelectAllIcon}
-                    text={selectAllLabel}
-                    selected={!!isAllEntitySelected}
-                  />
-                )}
-              {emptyLabel && (
-                <MenuItemSelect
-                  key="select-none"
-                  onClick={() => onEntitySelected()}
-                  LeftIcon={EmptyIcon}
-                  text={emptyLabel}
-                  selected={!selectedEntity}
-                />
-              )}
-            </>
-          )}
-          {entitiesInDropdown?.map((entity) => (
-            <SelectableMenuItemSelect
-              key={entity.id}
-              entity={entity}
-              onEntitySelected={onEntitySelected}
-              selectedEntity={selectedEntity}
-            />
-          ))}
-          {showCreateButton && (
-            <>
+              <MenuItem text="No result" />
               {entitiesToSelect.length > 0 && <DropdownMenuSeparator />}
               <CreateNewButton
+                key="add-new"
                 onClick={onCreate}
                 LeftIcon={IconPlus}
                 text="Add New"
+                hovered={isSelectedAddNewButton}
               />
             </>
+          ) : (
+            entitiesInDropdown?.map((entity) => {
+              switch (entity.id) {
+                case 'add-new': {
+                  return (
+                    <>
+                      {entitiesToSelect.length > 0 && <DropdownMenuSeparator />}
+                      <CreateNewButton
+                        key={entity.id}
+                        onClick={onCreate}
+                        LeftIcon={IconPlus}
+                        text="Add New"
+                        hovered={isSelectedAddNewButton}
+                      />
+                    </>
+                  );
+                }
+                case 'select-none': {
+                  return (
+                    emptyLabel && (
+                      <MenuItemSelect
+                        key={entity.id}
+                        onClick={() => onEntitySelected()}
+                        LeftIcon={EmptyIcon}
+                        text={emptyLabel}
+                        selected={!selectedEntity}
+                        hovered={isSelectedSelectNoneButton}
+                      />
+                    )
+                  );
+                }
+                case 'select-all': {
+                  return (
+                    isAllEntitySelectShown &&
+                    selectAllLabel &&
+                    onAllEntitySelected && (
+                      <MenuItemSelect
+                        key={entity.id}
+                        onClick={() => onAllEntitySelected()}
+                        LeftIcon={SelectAllIcon}
+                        text={selectAllLabel}
+                        selected={!!isAllEntitySelected}
+                        hovered={isSelectedSelectAllButton}
+                      />
+                    )
+                  );
+                }
+                default: {
+                  return (
+                    <SelectableMenuItemSelect
+                      key={entity.id}
+                      entity={entity}
+                      onEntitySelected={onEntitySelected}
+                      selectedEntity={selectedEntity}
+                    />
+                  );
+                }
+              }
+            })
           )}
         </DropdownMenuItemsContainer>
       </SelectableList>
