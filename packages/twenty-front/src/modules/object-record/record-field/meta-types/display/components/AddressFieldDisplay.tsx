a789a249defa8a7831ed84884e5db9923bcef8d6
Charles Bochet <charles@twenty.com>
2024-07-13 20:42:57 +0200

    Fixes

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/AddressFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/AddressFieldDisplay.tsx
@@ -1,5 +1,19 @@
+import { isNonEmptyString } from '@sniptt/guards';
+
+import { useAddressFieldDisplay } from '@/object-record/record-field/meta-types/hooks/useAddressFieldDisplay';
 import { TextDisplay } from '@/ui/field/display/components/TextDisplay';
 
 export const AddressFieldDisplay = () => {
-  return <TextDisplay text={'toto'} />;
+  const { fieldValue } = useAddressFieldDisplay();
+
+  const content = [
+    fieldValue?.addressStreet1,
+    fieldValue?.addressStreet2,
+    fieldValue?.addressCity,
+    fieldValue?.addressCountry,
+  ]
+    .filter(isNonEmptyString)
+    .join(', ');
+
+  return <TextDisplay text={content} />;
 };
