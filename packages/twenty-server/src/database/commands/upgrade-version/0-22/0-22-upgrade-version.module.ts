928f8aa1c63875222fc2b00c6734d1349c2ea1dd
Weiko <corentin@twenty.com>
2024-07-16 16:08:21 +0200

    Add missing commands to 0.22

--- packages/twenty-server/src/database/commands/upgrade-version/0-22/0-22-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-22/0-22-upgrade-version.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AddNewAddressFieldToViewsWithDeprecatedAddressFieldCommand } from 'src/database/commands/upgrade-version/0-22/0-22-add-new-address-field-to-views-with-deprecated-address.command';
 import { FixObjectMetadataIdStandardIdCommand } from 'src/database/commands/upgrade-version/0-22/0-22-fix-object-metadata-id-standard-id.command';
 import { UpdateBooleanFieldsNullDefaultValuesAndNullValuesCommand } from 'src/database/commands/upgrade-version/0-22/0-22-update-boolean-fields-null-default-values-and-null-values.command';
+import { UpdateMessageChannelSyncStageEnumCommand } from 'src/database/commands/upgrade-version/0-22/0-22-update-message-channel-sync-stage-enum.command';
+import { UpdateMessageChannelSyncStatusEnumCommand } from 'src/database/commands/upgrade-version/0-22/0-22-update-message-channel-sync-status-enum.command';
 import { UpgradeTo0_22Command } from 'src/database/commands/upgrade-version/0-22/0-22-upgrade-version.command';
 import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
@@ -19,6 +22,9 @@ import { WorkspaceCacheVersionModule } from 'src/engine/metadata-modules/workspa
   providers: [
     FixObjectMetadataIdStandardIdCommand,
     UpdateBooleanFieldsNullDefaultValuesAndNullValuesCommand,
+    UpdateMessageChannelSyncStatusEnumCommand,
+    UpdateMessageChannelSyncStageEnumCommand,
+    AddNewAddressFieldToViewsWithDeprecatedAddressFieldCommand,
     UpgradeTo0_22Command,
   ],
 })
