bb548dda138f237ccf63dd7a2a795ad2c7ca0196
bosiraphael <raphael.bosi@gmail.com>
2024-07-16 12:03:53 +0200

    add try catch

--- packages/twenty-server/src/database/commands/0-22-update-message-channel-sync-stage-enum.command.ts
+++ packages/twenty-server/src/database/commands/0-22-update-message-channel-sync-stage-enum.command.ts
@@ -71,27 +71,28 @@ export class UpdateMessageChannelSyncStageEnumCommand extends CommandRunner {
     }
 
     for (const workspaceId of workspaceIds) {
-      const dataSourceMetadatas =
-        await this.dataSourceService.getDataSourcesMetadataFromWorkspaceId(
-          workspaceId,
-        );
+      try {
+        const dataSourceMetadatas =
+          await this.dataSourceService.getDataSourcesMetadataFromWorkspaceId(
+            workspaceId,
+          );
 
-      for (const dataSourceMetadata of dataSourceMetadatas) {
-        const workspaceDataSource =
-          await this.typeORMService.connectToDataSource(dataSourceMetadata);
+        for (const dataSourceMetadata of dataSourceMetadatas) {
+          const workspaceDataSource =
+            await this.typeORMService.connectToDataSource(dataSourceMetadata);
 
-        if (workspaceDataSource) {
-          const queryRunner = workspaceDataSource.createQueryRunner();
+          if (workspaceDataSource) {
+            const queryRunner = workspaceDataSource.createQueryRunner();
 
-          await queryRunner.connect();
-          await queryRunner.startTransaction();
+            await queryRunner.connect();
+            await queryRunner.startTransaction();
 
-          try {
-            await queryRunner.query(
-              `ALTER TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum" RENAME TO "messageChannel_syncStage_enum_old"`,
-            );
-            await queryRunner.query(
-              `CREATE TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum" AS ENUM (
+            try {
+              await queryRunner.query(
+                `ALTER TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum" RENAME TO "messageChannel_syncStage_enum_old"`,
+              );
+              await queryRunner.query(
+                `CREATE TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum" AS ENUM (
               'FULL_MESSAGE_LIST_FETCH_PENDING',
               'PARTIAL_MESSAGE_LIST_FETCH_PENDING',
               'MESSAGE_LIST_FETCH_ONGOING',
@@ -99,124 +100,128 @@ export class UpdateMessageChannelSyncStageEnumCommand extends CommandRunner {
               'MESSAGES_IMPORT_ONGOING',
               'FAILED'
             )`,
-            );
+              );
 
-            await queryRunner.query(
-              `ALTER TABLE "${dataSourceMetadata.schema}"."messageChannel" ALTER COLUMN "syncStage" DROP DEFAULT`,
-            );
-            await queryRunner.query(
-              `ALTER TABLE "${dataSourceMetadata.schema}"."messageChannel" ALTER COLUMN "syncStage" TYPE text`,
-            );
+              await queryRunner.query(
+                `ALTER TABLE "${dataSourceMetadata.schema}"."messageChannel" ALTER COLUMN "syncStage" DROP DEFAULT`,
+              );
+              await queryRunner.query(
+                `ALTER TABLE "${dataSourceMetadata.schema}"."messageChannel" ALTER COLUMN "syncStage" TYPE text`,
+              );
 
-            await queryRunner.query(
-              `ALTER TABLE "${dataSourceMetadata.schema}"."messageChannel" ALTER COLUMN "syncStage" TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum" USING "syncStage"::text::"${dataSourceMetadata.schema}"."messageChannel_syncStage_enum"`,
-            );
+              await queryRunner.query(
+                `ALTER TABLE "${dataSourceMetadata.schema}"."messageChannel" ALTER COLUMN "syncStage" TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum" USING "syncStage"::text::"${dataSourceMetadata.schema}"."messageChannel_syncStage_enum"`,
+              );
 
-            await queryRunner.query(
-              `DROP TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum_old"`,
-            );
-            await queryRunner.commitTransaction();
-          } catch (error) {
-            await queryRunner.rollbackTransaction();
-            this.logger.log(
-              chalk.red(`Running command on workspace ${workspaceId} failed`),
-            );
-            throw error;
-          } finally {
-            await queryRunner.release();
+              await queryRunner.query(
+                `DROP TYPE "${dataSourceMetadata.schema}"."messageChannel_syncStage_enum_old"`,
+              );
+              await queryRunner.commitTransaction();
+            } catch (error) {
+              await queryRunner.rollbackTransaction();
+              this.logger.log(
+                chalk.red(`Running command on workspace ${workspaceId} failed`),
+              );
+              throw error;
+            } finally {
+              await queryRunner.release();
+            }
           }
         }
-      }
 
-      const messageChannelObjectMetadata =
-        await this.objectMetadataRepository.findOne({
-          where: { nameSingular: 'messageChannel', workspaceId },
+        const messageChannelObjectMetadata =
+          await this.objectMetadataRepository.findOne({
+            where: { nameSingular: 'messageChannel', workspaceId },
+          });
+
+        if (!messageChannelObjectMetadata) {
+          this.logger.log(
+            chalk.yellow(
+              `Object metadata for messageChannel not found in workspace ${workspaceId}`,
+            ),
+          );
+
+          continue;
+        }
+
+        const syncStageFieldMetadata =
+          await this.fieldMetadataRepository.findOne({
+            where: {
+              name: 'syncStage',
+              workspaceId,
+              objectMetadataId: messageChannelObjectMetadata.id,
+            },
+          });
+
+        if (!syncStageFieldMetadata) {
+          this.logger.log(
+            chalk.yellow(
+              `Field metadata for syncStage not found in workspace ${workspaceId}`,
+            ),
+          );
+
+          continue;
+        }
+
+        const newOptions = [
+          {
+            id: v4(),
+            value: MessageChannelSyncStage.FULL_MESSAGE_LIST_FETCH_PENDING,
+            label: 'Full messages list fetch pending',
+            position: 0,
+            color: 'blue',
+          },
+          {
+            id: v4(),
+            value: MessageChannelSyncStage.PARTIAL_MESSAGE_LIST_FETCH_PENDING,
+            label: 'Partial messages list fetch pending',
+            position: 1,
+            color: 'blue',
+          },
+          {
+            id: v4(),
+            value: MessageChannelSyncStage.MESSAGE_LIST_FETCH_ONGOING,
+            label: 'Messages list fetch ongoing',
+            position: 2,
+            color: 'orange',
+          },
+          {
+            id: v4(),
+            value: MessageChannelSyncStage.MESSAGES_IMPORT_PENDING,
+            label: 'Messages import pending',
+            position: 3,
+            color: 'blue',
+          },
+          {
+            id: v4(),
+            value: MessageChannelSyncStage.MESSAGES_IMPORT_ONGOING,
+            label: 'Messages import ongoing',
+            position: 4,
+            color: 'orange',
+          },
+          {
+            id: v4(),
+            value: MessageChannelSyncStage.FAILED,
+            label: 'Failed',
+            position: 5,
+            color: 'red',
+          },
+        ];
+
+        await this.fieldMetadataRepository.update(syncStageFieldMetadata.id, {
+          options: newOptions,
         });
 
-      if (!messageChannelObjectMetadata) {
+        await this.workspaceCacheVersionService.incrementVersion(workspaceId);
+
         this.logger.log(
-          chalk.yellow(
-            `Object metadata for messageChannel not found in workspace ${workspaceId}`,
-          ),
+          chalk.green(`Running command on workspace ${workspaceId} done`),
         );
-
-        continue;
-      }
-
-      const syncStageFieldMetadata = await this.fieldMetadataRepository.findOne(
-        {
-          where: {
-            name: 'syncStage',
-            workspaceId,
-            objectMetadataId: messageChannelObjectMetadata.id,
-          },
-        },
-      );
-
-      if (!syncStageFieldMetadata) {
-        this.logger.log(
-          chalk.yellow(
-            `Field metadata for syncStage not found in workspace ${workspaceId}`,
-          ),
+      } catch (error) {
+        this.logger.error(
+          `Migration failed for workspace ${workspaceId}: ${error.message}`,
         );
-
-        continue;
       }
-
-      const newOptions = [
-        {
-          id: v4(),
-          value: MessageChannelSyncStage.FULL_MESSAGE_LIST_FETCH_PENDING,
-          label: 'Full messages list fetch pending',
-          position: 0,
-          color: 'blue',
-        },
-        {
-          id: v4(),
-          value: MessageChannelSyncStage.PARTIAL_MESSAGE_LIST_FETCH_PENDING,
-          label: 'Partial messages list fetch pending',
-          position: 1,
-          color: 'blue',
-        },
-        {
-          id: v4(),
-          value: MessageChannelSyncStage.MESSAGE_LIST_FETCH_ONGOING,
-          label: 'Messages list fetch ongoing',
-          position: 2,
-          color: 'orange',
-        },
-        {
-          id: v4(),
-          value: MessageChannelSyncStage.MESSAGES_IMPORT_PENDING,
-          label: 'Messages import pending',
-          position: 3,
-          color: 'blue',
-        },
-        {
-          id: v4(),
-          value: MessageChannelSyncStage.MESSAGES_IMPORT_ONGOING,
-          label: 'Messages import ongoing',
-          position: 4,
-          color: 'orange',
-        },
-        {
-          id: v4(),
-          value: MessageChannelSyncStage.FAILED,
-          label: 'Failed',
-          position: 5,
-          color: 'red',
-        },
-      ];
-
-      await this.fieldMetadataRepository.update(syncStageFieldMetadata.id, {
-        options: newOptions,
-      });
-
-      await this.workspaceCacheVersionService.incrementVersion(workspaceId);
-
-      this.logger.log(
-        chalk.green(`Running command on workspace ${workspaceId} done`),
-      );
     }
 
     this.logger.log(chalk.green(`Command completed!`));
