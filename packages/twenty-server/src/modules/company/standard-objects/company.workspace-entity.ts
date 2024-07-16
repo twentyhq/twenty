9dec5076ba8a726c4e4c68774c1450f7b2eae204
Marie Stoppa <marie.stoppa@essec.edu>
2024-07-16 14:32:44 +0200

    wip

--- packages/twenty-server/src/modules/company/standard-objects/company.workspace-entity.ts
+++ packages/twenty-server/src/modules/company/standard-objects/company.workspace-entity.ts
@@ -67,7 +67,7 @@ export class CompanyWorkspaceEntity extends BaseWorkspaceEntity {
 
   @WorkspaceField({
     standardId: COMPANY_STANDARD_FIELD_IDS.linkedinLink,
-    type: FieldMetadataType.LINK,
+    type: FieldMetadataType.LINKS,
     label: 'Linkedin',
     description: 'The company Linkedin account',
     icon: 'IconBrandLinkedin',
@@ -77,7 +77,7 @@ export class CompanyWorkspaceEntity extends BaseWorkspaceEntity {
 
   @WorkspaceField({
     standardId: COMPANY_STANDARD_FIELD_IDS.xLink,
-    type: FieldMetadataType.LINK,
+    type: FieldMetadataType.LINKS,
     label: 'X',
     description: 'The company Twitter/X account',
     icon: 'IconBrandX',
