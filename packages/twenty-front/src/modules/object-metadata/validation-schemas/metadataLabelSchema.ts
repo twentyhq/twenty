94c2358c89e8ff5a2397d342e55cae53b1c18e79
Deval Minocha <deval.minocha24@gmail.com>
2024-07-17 15:13:57 +0530

    #6094 Prevent creating a custom field with an existing name (#6100)
    Fixes #6094 
    Description: Added logic inside SettingsObjectNewFieldStep2.tsx to
    prevent form submission
    Current Behaviours:
    <img width="947" alt="Screenshot 2024-07-03 at 1 45 31 PM"
    src="https://github.com/twentyhq/twenty/assets/95612797/bef54bc4-fc83-48f3-894a-34445ec64723">
    
    ---------
    
    Co-authored-by: Marie Stoppa <marie.stoppa@essec.edu>

--- packages/twenty-front/src/modules/object-metadata/validation-schemas/metadataLabelSchema.ts
+++ packages/twenty-front/src/modules/object-metadata/validation-schemas/metadataLabelSchema.ts
@@ -1,23 +1,42 @@
+import { errors } from '@/settings/data-model/fields/forms/utils/errorMessages';
 import { z } from 'zod';
 
 import { METADATA_LABEL_VALID_PATTERN } from '~/pages/settings/data-model/constants/MetadataLabelValidPattern';
 import { computeMetadataNameFromLabelOrThrow } from '~/pages/settings/data-model/utils/compute-metadata-name-from-label.utils';
-
-export const metadataLabelSchema = z
-  .string()
-  .trim()
-  .min(1)
-  .regex(METADATA_LABEL_VALID_PATTERN)
-  .refine(
-    (label) => {
-      try {
-        computeMetadataNameFromLabelOrThrow(label);
-        return true;
-      } catch (error) {
-        return false;
-      }
-    },
-    {
-      message: 'Label is not formattable',
-    },
-  ); // allows non-latin char
+export const metadataLabelSchema = (existingLabels?: string[]) => {
+  return z
+    .string()
+    .trim()
+    .min(1, errors.LabelEmpty)
+    .regex(METADATA_LABEL_VALID_PATTERN, errors.LabelNotFormattable)
+    .refine(
+      (label) => {
+        try {
+          computeMetadataNameFromLabelOrThrow(label);
+          return true;
+        } catch (error) {
+          return false;
+        }
+      },
+      {
+        message: errors.LabelNotFormattable,
+      },
+    )
+    .refine(
+      (label) => {
+        try {
+          if (!existingLabels || !label?.length) {
+            return true;
+          }
+          return !existingLabels.includes(
+            computeMetadataNameFromLabelOrThrow(label),
+          );
+        } catch (error) {
+          return false;
+        }
+      },
+      {
+        message: errors.LabelNotUnique,
+      },
+    );
+};
