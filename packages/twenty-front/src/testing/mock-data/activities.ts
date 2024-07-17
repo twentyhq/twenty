2d3c2419906dc86be068f0c22d51eb8676c6bd4a
Charles Bochet <charles@twenty.com>
2024-07-17 14:06:11 +0200

    Fixes

--- packages/twenty-front/src/testing/mock-data/activities.ts
+++ packages/twenty-front/src/testing/mock-data/activities.ts
@@ -1,8 +1,7 @@
 import { Activity } from '@/activities/types/Activity';
 import { ActivityTarget } from '@/activities/types/ActivityTarget';
 import { Comment } from '@/activities/types/Comment';
-import { Company } from '@/companies/types/Company';
-import { Person } from '@/people/types/Person';
+import { ObjectRecord } from '@/object-record/types/ObjectRecord';
 import { WorkspaceMember } from '@/workspace-member/types/WorkspaceMember';
 
 type MockedActivity = Pick<
@@ -36,11 +35,8 @@ type MockedActivity = Pick<
       | 'targetObjectNameSingular'
     > & {
       activity: Pick<Activity, 'id' | 'createdAt' | 'updatedAt' | '__typename'>;
-      person?: Pick<Person, 'id' | 'name' | 'avatarUrl' | '__typename'> | null;
-      company?: Pick<
-        Company,
-        'id' | 'name' | 'domainName' | '__typename'
-      > | null;
+      person?: ObjectRecord | null;
+      company?: ObjectRecord | null;
     }
   >;
 };
@@ -108,9 +104,33 @@ export const mockedActivities: Array<MockedActivity> = [
         companyId: '89bb825c-171e-4bcc-9cf7-43448d6fb280',
         company: {
           __typename: 'Company',
-          id: '89bb825c-171e-4bcc-9cf7-43448d6fb280',
-          name: 'Airbnb',
-          domainName: 'airbnb.com',
+          id: '20202020-1455-4c57-afaf-dd5dc086361d',
+          employees: null,
+          createdAt: '2024-06-05T09:00:20.412Z',
+          updatedAt: '2024-06-05T09:00:20.412Z',
+          name: 'Algolia',
+          idealCustomerProfile: false,
+          accountOwner: null,
+          accountOwnerId: null,
+          domainName: 'algolia.com',
+          address: '',
+          previousEmployees: null,
+          annualRecurringRevenue: {
+            __typename: 'Currency',
+            amountMicros: null,
+            currencyCode: '',
+          },
+          position: 13,
+          xLink: {
+            __typename: 'Link',
+            label: '',
+            url: '',
+          },
+          linkedinLink: {
+            __typename: 'Link',
+            label: '',
+            url: '',
+          },
         },
         person: null,
         activityId: '89bb825c-171e-4bcc-9cf7-43448d6fb230',
@@ -131,9 +151,33 @@ export const mockedActivities: Array<MockedActivity> = [
         companyId: 'b396e6b9-dc5c-4643-bcff-61b6cf7523ae',
         company: {
           __typename: 'Company',
-          id: '89bb825c-171e-4bcc-9cf7-43448d6fb278',
-          name: 'Aircall',
-          domainName: 'aircall.io',
+          id: '20202020-1455-4c57-afaf-dd5dc086361d',
+          employees: null,
+          createdAt: '2024-06-05T09:00:20.412Z',
+          updatedAt: '2024-06-05T09:00:20.412Z',
+          name: 'Algolia',
+          idealCustomerProfile: false,
+          accountOwner: null,
+          accountOwnerId: null,
+          domainName: 'algolia.com',
+          address: '',
+          previousEmployees: null,
+          annualRecurringRevenue: {
+            __typename: 'Currency',
+            amountMicros: null,
+            currencyCode: '',
+          },
+          position: 13,
+          xLink: {
+            __typename: 'Link',
+            label: '',
+            url: '',
+          },
+          linkedinLink: {
+            __typename: 'Link',
+            label: '',
+            url: '',
+          },
         },
         person: null,
         activityId: 'b396e6b9-dc5c-4643-bcff-61b6cf7523ae',
@@ -175,12 +219,97 @@ export const mockedActivities: Array<MockedActivity> = [
         personId: '7dfbc3f7-6e5e-4128-957e-8d86808cdf6b', // Alexandre
         person: {
           __typename: 'Person',
-          id: '7dfbc3f7-6e5e-4128-957e-8d86808cdf6b',
-          name: {
-            firstName: 'Alexandre',
-            lastName: 'Test',
+          id: '20202020-ac73-4797-824e-87a1f5aea9e0',
+          email: 'sylvie.palmer@linkedin.com',
+          bestCompany: null,
+          position: 2,
+          testJson: null,
+          testRating: 'RATING_5',
+          testMultiSelect: ['OPTION_2', 'OPTION_3', 'OPTION_4'],
+          testBoolean: false,
+          testSelect: null,
+          testDateOnly: '2024-06-06T00:00:00.000Z',
+          phone: '+33780123456',
+          createdAt: '2024-06-05T09:00:20.412Z',
+          city: 'Los Angeles',
+          testPhone: '',
+          jobTitle: 'CEO',
+          testCurrency: {
+            __typename: 'Currency',
+            amountMicros: 20323000000,
+            currencyCode: 'USD',
           },
-          avatarUrl: '',
+          xLink: {
+            __typename: 'Link',
+            label: '',
+            url: 'twitter.com',
+          },
+          testLinks: {
+            __typename: 'Links',
+            primaryLinkUrl: 'https://algolia.com',
+            primaryLinkLabel: '',
+            secondaryLinks: [
+              {
+                url: 'https://paris.com',
+                label: '',
+              },
+            ],
+          },
+          name: {
+            __typename: 'FullName',
+            firstName: 'Sylvie',
+            lastName: 'Palmer',
+          },
+          linkedinLink: {
+            __typename: 'Link',
+            label: '',
+            url: 'linkedin.com',
+          },
+          testAddress: {
+            __typename: 'Address',
+            addressStreet1: '',
+            addressStreet2: '',
+            addressCity: '',
+            addressState: '',
+            addressCountry: '',
+            addressPostcode: '',
+            addressLat: null,
+            addressLng: null,
+          },
+          testLink: {
+            __typename: 'Link',
+            label: '',
+            url: '',
+          },
+          company: {
+            __typename: 'Company',
+            id: '20202020-3ec3-4fe3-8997-b76aa0bfa408',
+            employees: null,
+            previousEmployeesId: '20202020-2d40-4e49-8df4-9c6a049191de',
+            accountOwnerId: null,
+            updatedAt: '2024-06-05T09:39:33.886Z',
+            idealCustomerProfile: false,
+            createdAt: '2024-06-05T09:00:20.412Z',
+            name: 'Linkedin',
+            domainName: 'linkedin.com',
+            address: '',
+            position: 1,
+            linkedinLink: {
+              __typename: 'Link',
+              label: '',
+              url: '',
+            },
+            annualRecurringRevenue: {
+              __typename: 'Currency',
+              amountMicros: null,
+              currencyCode: '',
+            },
+            xLink: {
+              __typename: 'Link',
+              label: '',
+              url: '',
+            },
+          },
         },
         company: null,
         companyId: null,
