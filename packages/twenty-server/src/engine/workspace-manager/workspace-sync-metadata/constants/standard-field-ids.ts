6db50ee34f678f7e0b57d65ecbda31f78cf7e100
martmull <martmull@hotmail.fr>
2024-07-17 15:57:43 +0200

    Remove useless parameter

--- packages/twenty-server/src/engine/workspace-manager/workspace-sync-metadata/constants/standard-field-ids.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-sync-metadata/constants/standard-field-ids.ts
@@ -351,7 +351,6 @@ export const WORKSPACE_MEMBER_STANDARD_FIELD_IDS = {
   calendarEventParticipants: '20202020-0dbc-4841-9ce1-3e793b5b3512',
   timelineActivities: '20202020-e15b-47b8-94fe-8200e3c66615',
   auditLogs: '20202020-2f54-4739-a5e2-99563385e83d',
-  authoredFunctions: '20202020-2f54-4739-a5e2-99563385e83e',
 };
 
 export const CUSTOM_OBJECT_STANDARD_FIELD_IDS = {
