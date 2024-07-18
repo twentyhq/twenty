a54ae1537749bd25c7e82d6acf50cd33f23abdb8
Antar <antardas2334@gmail.com>
2024-07-18 09:45:38 +0600

    fix: icon visibility on horizontal scroll #6304

--- packages/twenty-front/src/modules/object-record/record-table/record-table-header/components/RecordTableHeaderDragDropColumn.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-header/components/RecordTableHeaderDragDropColumn.tsx
@@ -1,10 +1,15 @@
 import { styled } from '@linaria/react';
+import { useContext } from 'react';
+import { ThemeContext } from 'twenty-ui';
 
-const StyledTh = styled.th`
+const StyledTh = styled.th<{ backgroundColor: string;}>`
   border-bottom: none;
   border-top: none;
+	background: ${({ backgroundColor }) => backgroundColor};
 `;
 
 export const RecordTableHeaderDragDropColumn = () => {
-  return <StyledTh></StyledTh>;
+	const { theme } = useContext(ThemeContext);
+	
+  return <StyledTh backgroundColor={theme.background.primary}></StyledTh>;
 };
