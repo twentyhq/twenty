41211dc1a642b2e4e12fbec2a650736ab7c96517
Charles Bochet <charles@twenty.com>
2024-07-19 18:57:38 +0200

    Fix linter and yarn.lock not being synced

--- packages/twenty-front/src/modules/object-record/record-table/record-table-header/components/RecordTableHeaderDragDropColumn.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-header/components/RecordTableHeaderDragDropColumn.tsx
@@ -2,14 +2,14 @@ import { styled } from '@linaria/react';
 import { useContext } from 'react';
 import { ThemeContext } from 'twenty-ui';
 
-const StyledTh = styled.th<{ backgroundColor: string;}>`
+const StyledTh = styled.th<{ backgroundColor: string }>`
+  background: ${({ backgroundColor }) => backgroundColor};
   border-bottom: none;
   border-top: none;
-	background: ${({ backgroundColor }) => backgroundColor};
 `;
 
 export const RecordTableHeaderDragDropColumn = () => {
-	const { theme } = useContext(ThemeContext);
-	
+  const { theme } = useContext(ThemeContext);
+
   return <StyledTh backgroundColor={theme.background.primary}></StyledTh>;
 };
