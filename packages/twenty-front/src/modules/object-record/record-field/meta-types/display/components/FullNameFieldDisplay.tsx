0395462bb9cba1875715808eed0890a7cc6ab244
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-07-19 13:09:47 +0200

    Multiple fixes

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/FullNameFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/FullNameFieldDisplay.tsx
@@ -1,28 +1,14 @@
 import { isNonEmptyString } from '@sniptt/guards';
 
 import { useFullNameFieldDisplay } from '@/object-record/record-field/meta-types/hooks/useFullNameFieldDisplay';
-import { TextDisplay } from '@/ui/field/display/components/TextDisplay';
-import { useIsMobile } from '@/ui/utilities/responsive/hooks/useIsMobile';
-
-const MOBILE_MAX_LENGTH = 15;
-const DESKTOP_MAX_LENGTH = 20;
+import { OverflowingTextWithTooltip } from 'twenty-ui';
 
 export const FullNameFieldDisplay = () => {
   const { fieldValue } = useFullNameFieldDisplay();
-  const isMobile = useIsMobile();
-
-  const truncateTheContent = (contentToTruncate: string, maxLength: number) => {
-    return contentToTruncate.length > maxLength
-      ? `${contentToTruncate.slice(0, maxLength)}...`
-      : contentToTruncate;
-  };
 
   const content = [fieldValue?.firstName, fieldValue?.lastName]
     .filter(isNonEmptyString)
     .join(' ');
 
-  const maxLength = isMobile ? MOBILE_MAX_LENGTH : DESKTOP_MAX_LENGTH;
-  const truncatedContent = truncateTheContent(content, maxLength);
-
-  return <TextDisplay text={truncatedContent} />;
+  return <OverflowingTextWithTooltip text={content} />;
 };
