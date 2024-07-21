92e8c1b55c25f0a15cddce4fdf394ab0bc8929a6
Ciara Hatcher <social@purplemoose.net>
2024-07-21 13:14:07 -0500

    Updates for v20+ Refactored to use vars instead of locals Added variable descriptions Misc bug fixed for terraform

--- packages/twenty-docker/k8s/terraform/pvc-db.tf
+++ packages/twenty-docker/k8s/terraform/pvc-db.tf
@@ -1,13 +1,13 @@
 resource "kubernetes_persistent_volume_claim" "db" {
   metadata {
-    name      = "${local.twentycrm_app_name}-db-pvc"
+    name      = "${var.twentycrm_app_name}-db-pvc"
     namespace = kubernetes_namespace.twentycrm.metadata.0.name
   }
   spec {
     access_modes = ["ReadWriteOnce"]
     resources {
       requests = {
-        storage = local.twentycrm_db_pvc_requests
+        storage = var.twentycrm_db_pvc_requests
       }
     }
     volume_name = kubernetes_persistent_volume.db.metadata.0.name
