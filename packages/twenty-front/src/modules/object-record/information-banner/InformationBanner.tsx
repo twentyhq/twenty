629a2bf0c84c167bf7a7f59aa5c87497d354a5f0
bosiraphael <raphael.bosi@gmail.com>
2024-07-22 16:12:18 +0200

    store the account ids to reconnect

--- packages/twenty-front/src/modules/object-record/information-banner/InformationBanner.tsx
+++ packages/twenty-front/src/modules/object-record/information-banner/InformationBanner.tsx
@@ -1,4 +1,7 @@
+import { ConnectedAccount } from '@/accounts/types/ConnectedAccount';
 import { currentUserState } from '@/auth/states/currentUserState';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { useFindOneRecord } from '@/object-record/hooks/useFindOneRecord';
 import { Button } from '@/ui/input/button/components/Button';
 import { useRecoilValue } from 'recoil';
 import { Banner, IconRefresh } from 'twenty-ui';
@@ -8,16 +11,21 @@ export const InformationBanner = () => {
 
   const userVars = currentUser?.userVars;
 
-  const accountsToReconnect = userVars?.['ACCOUNTS_TO_RECONNECT'] ?? [];
+  const accountIdsToReconnect = userVars?.['ACCOUNTS_TO_RECONNECT'] ?? [];
 
-  if (accountsToReconnect.length === 0) {
+  const accountToReconnect = useFindOneRecord<ConnectedAccount>({
+    objectNameSingular: CoreObjectNameSingular.ConnectedAccount,
+    objectRecordId: accountIdsToReconnect[0],
+  });
+
+  if (!accountToReconnect?.record) {
     return null;
   }
 
   return (
     <Banner>
-      Sync lost with mailbox {accountsToReconnect[0]}. Please reconnect for
-      updates:
+      Sync lost with mailbox {accountToReconnect?.record?.handle}. Please
+      reconnect for updates:
       <Button
         variant="secondary"
         title="Reconnect"
