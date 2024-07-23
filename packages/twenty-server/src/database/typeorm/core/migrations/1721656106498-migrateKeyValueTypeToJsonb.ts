85ada3940ff19eaac6aea4d930e4213cc5922771
bosiraphael <raphael.bosi@gmail.com>
2024-07-23 12:13:09 +0200

    fix lint

--- packages/twenty-server/src/database/typeorm/core/migrations/1721656106498-migrateKeyValueTypeToJsonb.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/1721656106498-migrateKeyValueTypeToJsonb.ts
@@ -1,16 +1,25 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class MigrateKeyValueTypeToJsonb1721656106498 implements MigrationInterface {
-    name = 'MigrateKeyValueTypeToJsonb1721656106498'
+export class MigrateKeyValueTypeToJsonb1721656106498
+  implements MigrationInterface
+{
+  name = 'MigrateKeyValueTypeToJsonb1721656106498';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "core"."keyValuePair" DROP COLUMN "value"`);
-        await queryRunner.query(`ALTER TABLE "core"."keyValuePair" ADD "value" jsonb`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "core"."keyValuePair" DROP COLUMN "value"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."keyValuePair" ADD "value" jsonb`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "core"."keyValuePair" DROP COLUMN "value"`);
-        await queryRunner.query(`ALTER TABLE "core"."keyValuePair" ADD "value" text`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "core"."keyValuePair" DROP COLUMN "value"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."keyValuePair" ADD "value" text`,
+    );
+  }
 }
