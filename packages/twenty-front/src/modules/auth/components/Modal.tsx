c8c7dcb4196cece90bc86ee55643489fb6e2ed97
Nitin Koche <nitinkoche03@gmail.com>
2024-07-24 13:59:14 +0530

    Fixed SignInUp Modal misalignment for devices smaller than 400px width

--- packages/twenty-front/src/modules/auth/components/Modal.tsx
+++ packages/twenty-front/src/modules/auth/components/Modal.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import styled from '@emotion/styled';
+import React from 'react';
 
 import { ModalLayout } from '@/ui/layout/modal/components/ModalLayout';
 
 const StyledContent = styled(ModalLayout.Content)`
   align-items: center;
-  width: calc(400px - ${({ theme }) => theme.spacing(10 * 2)});
 `;
 
 type AuthModalProps = { children: React.ReactNode };
