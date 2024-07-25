55ccf0fbc871cdb0ca468e8bb629f8992a1f8b26
martmull <martmull@hotmail.fr>
2024-07-25 18:01:21 +0200

    Remove useEffect from component

--- packages/twenty-front/src/modules/settings/serverless-functions/forms/useServerlessFunctionFormValues.ts
+++ packages/twenty-front/src/modules/settings/serverless-functions/forms/useServerlessFunctionFormValues.ts
@@ -1,4 +1,7 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { getFileAbsoluteURI } from '~/utils/file/getFileAbsoluteURI';
+import { isDefined } from '~/utils/isDefined';
+import { useGetOneServerlessFunction } from '@/settings/serverless-functions/hooks/useGetOneServerlessFunction';
 
 export type ServerlessFunctionFormValues = {
   name: string;
@@ -10,15 +13,41 @@ export type SetServerlessFunctionFormValues = Dispatch<
   SetStateAction<ServerlessFunctionFormValues>
 >;
 
-export const useServerlessFunctionFormValues = (): [
-  ServerlessFunctionFormValues,
-  SetServerlessFunctionFormValues,
-] => {
+export const useServerlessFunctionFormValues = (
+  serverlessFunctionId: string,
+): [ServerlessFunctionFormValues, SetServerlessFunctionFormValues] => {
+  const { serverlessFunction } =
+    useGetOneServerlessFunction(serverlessFunctionId);
   const [formValues, setFormValues] = useState<ServerlessFunctionFormValues>({
     name: '',
     description: '',
     code: '',
   });
+
+  useEffect(() => {
+    const getFileContent = async () => {
+      const resp = await fetch(
+        getFileAbsoluteURI(serverlessFunction?.sourceCodeFullPath),
+      );
+      if (resp.status !== 200) {
+        throw new Error('Network response was not ok');
+      } else {
+        const result = await resp.text();
+        const newState = {
+          code: result,
+          name: serverlessFunction?.name || '',
+          description: serverlessFunction?.description || '',
+        };
+        setFormValues((prevState) => ({
+          ...prevState,
+          ...newState,
+        }));
+      }
+    };
+    if (isDefined(serverlessFunction?.sourceCodeFullPath)) {
+      getFileContent();
+    }
+  }, [serverlessFunction, setFormValues]);
 
   return [formValues, setFormValues];
 };
--- packages/twenty-front/src/pages/settings/serverless-functions/SettingsServerlessFunctionDetail.tsx
+++ packages/twenty-front/src/pages/settings/serverless-functions/SettingsServerlessFunctionDetail.tsx
@@ -1,15 +1,11 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { IconCode, IconSettings, IconTestPipe } from 'twenty-ui';
 import { SubMenuTopBarContainer } from '@/ui/layout/page/SubMenuTopBarContainer';
 import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
 import { Breadcrumb } from '@/ui/navigation/bread-crumb/components/Breadcrumb';
 import { SettingsHeaderContainer } from '@/settings/components/SettingsHeaderContainer';
-import { useGetOneServerlessFunction } from '@/settings/serverless-functions/hooks/useGetOneServerlessFunction';
 import { Section } from '@/ui/layout/section/components/Section';
 import { TabList } from '@/ui/layout/tab/components/TabList';
-import { isDefined } from '~/utils/isDefined';
-import { getFileAbsoluteURI } from '~/utils/file/getFileAbsoluteURI';
 import { useTabList } from '@/ui/layout/tab/hooks/useTabList';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { SettingsServerlessFunctionCodeEditorTab } from '@/settings/serverless-functions/components/tabs/SettingsServerlessFunctionCodeEditorTab';
@@ -34,12 +30,10 @@ export const SettingsServerlessFunctionDetail = () => {
     TAB_LIST_COMPONENT_ID,
   );
   const activeTabId = useRecoilValue(activeTabIdState);
-  const { serverlessFunction } =
-    useGetOneServerlessFunction(serverlessFunctionId);
   const { executeOneServerlessFunction } = useExecuteOneServerlessFunction();
   const { updateOneServerlessFunction } = useUpdateOneServerlessFunction();
-  const [savedCode, setSavedCode] = useState<string>('');
-  const [formValues, setFormValues] = useServerlessFunctionFormValues();
+  const [formValues, setFormValues] =
+    useServerlessFunctionFormValues(serverlessFunctionId);
   const setSettingsServerlessFunctionOutput = useSetRecoilState(
     settingsServerlessFunctionOutputState,
   );
@@ -47,31 +41,14 @@ export const SettingsServerlessFunctionDetail = () => {
     settingsServerlessFunctionInputState,
   );
 
-  const canSave =
-    savedCode !== formValues.code ||
-    serverlessFunction?.name !== formValues.name ||
-    serverlessFunction?.description !== formValues.description;
-
   const save = async () => {
     try {
-      const { data } = await updateOneServerlessFunction({
-        id: serverlessFunction?.id,
+      await updateOneServerlessFunction({
+        id: serverlessFunctionId,
         name: formValues.name,
         description: formValues.description,
         code: formValues.code,
       });
-      const newState = {
-        ...(isDefined(data?.updateOneServerlessFunction?.name) && {
-          name: data?.updateOneServerlessFunction?.name,
-        }),
-        ...(isDefined(data?.updateOneServerlessFunction?.description) && {
-          description: data?.updateOneServerlessFunction?.description,
-        }),
-      };
-      setFormValues((prevState) => ({
-        ...prevState,
-        ...newState,
-      }));
     } catch (err) {
       enqueueSnackBar(
         (err as Error)?.message || 'An error occurred while updating function',
@@ -95,12 +72,10 @@ export const SettingsServerlessFunctionDetail = () => {
   };
 
   const handleExecute = async () => {
-    if (canSave) {
-      await handleSave();
-    }
+    await handleSave();
     try {
       const result = await executeOneServerlessFunction(
-        serverlessFunction?.id,
+        serverlessFunctionId,
         JSON.parse(settingsServerlessFunctionInput),
       );
       setSettingsServerlessFunctionOutput(
@@ -121,36 +96,6 @@ export const SettingsServerlessFunctionDetail = () => {
     }
     setActiveTabId('test');
   };
-
-  useEffect(() => {
-    const getFileContent = async () => {
-      const resp = await fetch(
-        getFileAbsoluteURI(serverlessFunction?.sourceCodeFullPath),
-      );
-      if (resp.status !== 200) {
-        throw new Error('Network response was not ok');
-      } else {
-        const result = await resp.text();
-        setSavedCode(result);
-        const newState = {
-          code: result,
-          ...(isDefined(serverlessFunction?.name) && {
-            name: serverlessFunction?.name,
-          }),
-          ...(isDefined(serverlessFunction?.description) && {
-            description: serverlessFunction?.description,
-          }),
-        };
-        setFormValues((prevState) => ({
-          ...prevState,
-          ...newState,
-        }));
-      }
-    };
-    if (isDefined(serverlessFunction?.sourceCodeFullPath)) {
-      getFileContent();
-    }
-  }, [serverlessFunction, setFormValues]);
 
   const tabs = [
     { id: 'editor', title: 'Editor', Icon: IconCode },
@@ -189,14 +134,14 @@ export const SettingsServerlessFunctionDetail = () => {
   };
 
   return (
-    serverlessFunction?.name && (
+    formValues.name && (
       <SubMenuTopBarContainer Icon={IconSettings} title="Settings">
         <SettingsPageContainer>
           <SettingsHeaderContainer>
             <Breadcrumb
               links={[
                 { children: 'Functions', href: '/settings/functions' },
-                { children: `${serverlessFunction?.name}` },
+                { children: `${formValues.name}` },
               ]}
             />
           </SettingsHeaderContainer>
