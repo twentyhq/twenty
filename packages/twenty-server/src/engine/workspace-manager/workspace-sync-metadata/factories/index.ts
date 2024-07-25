7b943457c85bba0f0c732d8b8c353648bf7a9f26
Charles Bochet <charles@twenty.com>
2024-07-25 20:43:12 +0200

    Update sync-metadata to use feature-flag core module

--- packages/twenty-server/src/engine/workspace-manager/workspace-sync-metadata/factories/index.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-sync-metadata/factories/index.ts
@@ -1,12 +1,10 @@
 import { StandardIndexFactory } from 'src/engine/workspace-manager/workspace-sync-metadata/factories/standard-index.factory';
 
-import { FeatureFlagFactory } from '../../../core-modules/feature-flag/services/feature-flags.factory';
 import { StandardFieldFactory } from './standard-field.factory';
 import { StandardObjectFactory } from './standard-object.factory';
 import { StandardRelationFactory } from './standard-relation.factory';
 
 export const workspaceSyncMetadataFactories = [
-  FeatureFlagFactory,
   StandardFieldFactory,
   StandardObjectFactory,
   StandardRelationFactory,
