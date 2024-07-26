d7664f172a7abc8a29e92ac4c71944a462f6ab8d
Marie Stoppa <marie.stoppa@essec.edu>
2024-07-26 16:11:00 +0200

    Fix demo seeds

--- packages/twenty-server/src/engine/workspace-manager/demo-objects-prefill-data/company.ts
+++ packages/twenty-server/src/engine/workspace-manager/demo-objects-prefill-data/company.ts
@@ -11,7 +11,7 @@ export const companyPrefillDemoData = async (
     .insert()
     .into(`${schemaName}.company`, [
       'name',
-      'domainName',
+      'domainNamePrimaryLinkUrl',
       'addressAddressCity',
       'employees',
       'linkedinLinkPrimaryLinkUrl',
