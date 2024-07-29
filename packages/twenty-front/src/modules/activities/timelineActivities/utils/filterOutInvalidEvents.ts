ba49a9c2a6e8cc0a7286ee9fbd2dbcb9fb7cd0a1
Weiko <corentin@twenty.com>
2024-07-29 14:47:39 +0200

    move to util

--- packages/twenty-front/src/modules/activities/timelineActivities/utils/filterOutInvalidEvents.ts
+++ packages/twenty-front/src/modules/activities/timelineActivities/utils/filterOutInvalidEvents.ts
@@ -1,48 +1,35 @@
 import { TimelineActivity } from '@/activities/timelineActivities/types/TimelineActivity';
-import { FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
-import { isDefined } from '~/utils/isDefined';
 
 export const filterOutInvalidEvents = (
   events: TimelineActivity[],
   mainObjectMetadataItem: ObjectMetadataItem,
-) => {
-  const fieldMetadataItemMap: Record<string, FieldMetadataItem> =
-    mainObjectMetadataItem.fields.reduce(
-      (acc, field) => ({ ...acc, [field.name]: field }),
-      {},
-    );
-
-  const filteredEvents = events.reduce(
-    (acc: TimelineActivity[], event: TimelineActivity) => {
-      const { properties } = event;
-      const diff: Record<string, { before: any; after: any }> =
-        properties?.diff;
-
-      if (!isDefined(diff)) {
-        acc.push(event);
-
-        return acc;
-      }
-
-      const diffEntriesWithoutDeletedFields = Object.entries(diff).filter(
-        ([diffKey, _diffValue]) => fieldMetadataItemMap[diffKey],
-      );
-
-      if (diffEntriesWithoutDeletedFields.length > 0) {
-        acc.push({
-          ...event,
-          properties: {
-            ...properties,
-            diff: Object.fromEntries(diffEntriesWithoutDeletedFields),
-          },
-        });
-      }
-
-      return acc;
-    },
-    [],
+): TimelineActivity[] => {
+  const fieldMetadataItemMap = new Map(
+    mainObjectMetadataItem.fields.map((field) => [field.name, field]),
   );
 
-  return filteredEvents;
+  return events.filter((event) => {
+    const diff = event.properties?.diff;
+    const canSkipValidation = !diff;
+
+    if (canSkipValidation) {
+      return true;
+    }
+
+    const validDiffEntries = Object.entries(diff).filter(([diffKey]) =>
+      fieldMetadataItemMap.has(diffKey),
+    );
+
+    if (validDiffEntries.length === 0) {
+      return false;
+    }
+
+    event.properties = {
+      ...event.properties,
+      diff: Object.fromEntries(validDiffEntries),
+    };
+
+    return true;
+  });
 };
