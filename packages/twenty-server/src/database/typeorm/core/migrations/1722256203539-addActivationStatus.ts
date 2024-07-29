9d8a00e29d55e7c68f5602d26c83ac2339af19de
bosiraphael <raphael.bosi@gmail.com>
2024-07-29 15:24:58 +0200

    fix migration

--- packages/twenty-server/src/database/typeorm/core/migrations/1722256203539-addActivationStatus.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/1722256203539-addActivationStatus.ts
@@ -1,18 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddActivationStatus1722256203539 implements MigrationInterface {
-    name = 'AddActivationStatus1722256203539'
+  name = 'AddActivationStatus1722256203539';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "core"."workspace_activationstatus_enum" AS ENUM('ACTIVE', 'INACTIVE')`);
-        await queryRunner.query(`ALTER TABLE "core"."workspace" ADD "activationStatus" "core"."workspace_activationstatus_enum" NOT NULL DEFAULT 'INACTIVE'`);
-        await queryRunner.query(`ALTER TABLE "core"."keyValuePair" ALTER COLUMN "textValueDeprecated" SET NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "core"."workspace_activationstatus_enum" AS ENUM('ACTIVE', 'INACTIVE')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."workspace" ADD "activationStatus" "core"."workspace_activationstatus_enum" NOT NULL DEFAULT 'INACTIVE'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "core"."keyValuePair" ALTER COLUMN "textValueDeprecated" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "core"."workspace" DROP COLUMN "activationStatus"`);
-        await queryRunner.query(`DROP TYPE "core"."workspace_activationstatus_enum"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "core"."workspace" DROP COLUMN "activationStatus"`,
+    );
+    await queryRunner.query(
+      `DROP TYPE "core"."workspace_activationstatus_enum"`,
+    );
+  }
 }
