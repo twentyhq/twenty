69c4ee6ec80a428bde0100d8623fb59aa8076029
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-07-30 17:45:01 +0530

    fixes 6116

--- packages/twenty-front/src/modules/object-record/record-index/options/components/RecordIndexOptionsDropdownContent.tsx
+++ packages/twenty-front/src/modules/object-record/record-index/options/components/RecordIndexOptionsDropdownContent.tsx
@@ -123,7 +123,7 @@ export const RecordIndexOptionsDropdownContent = ({
     objectNameSingular,
     recordIndexId,
   });
-  console.log(currentMenu);
+
   return (
     <>
       {!currentMenu && (
