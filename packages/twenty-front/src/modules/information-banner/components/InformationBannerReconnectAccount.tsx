68880502984f0b9945677a3dcb238c220039bc97
bosiraphael <raphael.bosi@gmail.com>
2024-07-30 14:05:00 +0200

    put all logic inside InformationBannerAccountToReconnect

--- packages/twenty-front/src/modules/information-banner/components/InformationBannerReconnectAccount.tsx
+++ packages/twenty-front/src/modules/information-banner/components/InformationBannerReconnectAccount.tsx
@@ -1,18 +1,25 @@
 import { ConnectedAccount } from '@/accounts/types/ConnectedAccount';
+import { currentUserState } from '@/auth/states/currentUserState';
+import { InformationBannerKeys } from '@/information-banner/components/InformationBannerWrapper';
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { useFindOneRecord } from '@/object-record/hooks/useFindOneRecord';
 import { useTriggerGoogleApisOAuth } from '@/settings/accounts/hooks/useTriggerGoogleApisOAuth';
 import { Button } from '@/ui/input/button/components/Button';
+import { useRecoilValue } from 'recoil';
 import { Banner, IconRefresh } from 'twenty-ui';
 
-export const InformationBannerAccountToReconnect = ({
-  accountIdToReconnect,
-}: {
-  accountIdToReconnect: string;
-}) => {
+export const InformationBannerAccountToReconnect = () => {
+  const currentUser = useRecoilValue(currentUserState);
+
+  const userVars = currentUser?.userVars;
+
+  const firstAccountIdToReconnect =
+    userVars?.[InformationBannerKeys.ACCOUNTS_TO_RECONNECT]?.[0];
+
   const accountToReconnect = useFindOneRecord<ConnectedAccount>({
     objectNameSingular: CoreObjectNameSingular.ConnectedAccount,
-    objectRecordId: accountIdToReconnect,
+    objectRecordId: firstAccountIdToReconnect,
+    skip: !firstAccountIdToReconnect,
   });
 
   const { triggerGoogleApisOAuth } = useTriggerGoogleApisOAuth();
--- packages/twenty-front/src/modules/information-banner/components/InformationBannerWrapper.tsx
+++ packages/twenty-front/src/modules/information-banner/components/InformationBannerWrapper.tsx
@@ -1,26 +1,13 @@
-import { currentUserState } from '@/auth/states/currentUserState';
 import { InformationBannerAccountToReconnect } from '@/information-banner/components/InformationBannerReconnectAccount';
-import { useRecoilValue } from 'recoil';
 
 export enum InformationBannerKeys {
   ACCOUNTS_TO_RECONNECT = 'ACCOUNTS_TO_RECONNECT',
 }
 
 export const InformationBannerWrapper = () => {
-  const currentUser = useRecoilValue(currentUserState);
-
-  const userVars = currentUser?.userVars;
-
-  const firstAccountIdToReconnect =
-    userVars?.[InformationBannerKeys.ACCOUNTS_TO_RECONNECT]?.[0];
-
   return (
     <>
-      {firstAccountIdToReconnect && (
-        <InformationBannerAccountToReconnect
-          accountIdToReconnect={firstAccountIdToReconnect}
-        />
-      )}
+      <InformationBannerAccountToReconnect />
     </>
   );
 };
