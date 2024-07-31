88f323f6d02d1d9ae244316ec663bc0109c260bc
martmull <martmull@hotmail.fr>
2024-07-31 19:55:23 +0200

    Simplify code

--- packages/twenty-server/src/modules/workflow/workflow-runner/workflow-runner.service.ts
+++ packages/twenty-server/src/modules/workflow/workflow-runner/workflow-runner.service.ts
@@ -26,25 +26,37 @@ export class WorkflowRunnerService {
       return payload;
     }
 
-    let result: object | undefined = undefined;
-
     const workflowActionRunner = this.workflowActionRunnerFactory.get(
       action.type,
     );
 
-    const executionResult = await workflowActionRunner.execute({
+    const result = await workflowActionRunner.execute({
       action,
       workspaceId,
       payload,
     });
 
-    if (executionResult.data) {
-      result = executionResult.data;
-    } else if (!executionResult.error) {
+    if (result.data) {
+      return await this.run({
+        action: action.nextAction,
+        workspaceId,
+        payload: result.data,
+      });
+    }
+
+    if (!result.error) {
       throw new Error('Execution result error, no data or error');
-    } else if (action.settings.errorHandlingOptions.continueOnFailure.value) {
-      result = payload;
-    } else if (
+    }
+
+    if (action.settings.errorHandlingOptions.continueOnFailure.value) {
+      return await this.run({
+        action: action.nextAction,
+        workspaceId,
+        payload,
+      });
+    }
+
+    if (
       action.settings.errorHandlingOptions.retryOnFailure.value &&
       attemptCount < MAX_RETRIES_ON_FAILURE
     ) {
@@ -54,14 +66,8 @@ export class WorkflowRunnerService {
         payload,
         attemptCount: attemptCount + 1,
       });
-    } else {
-      return executionResult.error;
     }
 
-    return await this.run({
-      action: action.nextAction,
-      workspaceId,
-      payload: result,
-    });
+    return result.error;
   }
 }
