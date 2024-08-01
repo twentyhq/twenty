8c8f1927653823f9db3a720131c6a0dbcc84eadc
Thomas Trompette <thomas.trompette@sfr.fr>
2024-08-01 11:57:44 +0200

    Trigger workflow on database event (#6480)
    - Add global listener on database event
    - Fetch event listener associated
    - Trigger associated workflow
    
    Also updated the runner so it expects the input to be in the payload
    rather than the trigger

--- packages/twenty-server/src/engine/core-modules/workflow/workflow-trigger.resolver.ts
+++ packages/twenty-server/src/engine/core-modules/workflow/workflow-trigger.resolver.ts
@@ -1,12 +1,13 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 
+import { RunWorkflowVersionInput } from 'src/engine/core-modules/workflow/dtos/run-workflow-version-input.dto';
+import { WorkflowTriggerResultDTO } from 'src/engine/core-modules/workflow/dtos/workflow-trigger-result.dto';
 import { workflowTriggerGraphqlApiExceptionHandler } from 'src/engine/core-modules/workflow/utils/workflow-trigger-graphql-api-exception-handler.util';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { AuthWorkspace } from 'src/engine/decorators/auth/auth-workspace.decorator';
 import { JwtAuthGuard } from 'src/engine/guards/jwt.auth.guard';
 import { WorkflowTriggerService } from 'src/modules/workflow/workflow-trigger/workflow-trigger.service';
-import { WorkflowTriggerResultDTO } from 'src/engine/core-modules/workflow/dtos/workflow-trigger-result.dto';
 
 @UseGuards(JwtAuthGuard)
 @Resolver()
@@ -31,15 +32,16 @@ export class WorkflowTriggerResolver {
   }
 
   @Mutation(() => WorkflowTriggerResultDTO)
-  async triggerWorkflow(
+  async runWorkflowVersion(
     @AuthWorkspace() { id: workspaceId }: Workspace,
-    @Args('workflowVersionId') workflowVersionId: string,
+    @Args('input') { workflowVersionId, payload }: RunWorkflowVersionInput,
   ) {
     try {
       return {
-        result: await this.workflowTriggerService.runWorkflow(
+        result: await this.workflowTriggerService.runWorkflowVersion(
           workspaceId,
           workflowVersionId,
+          payload ?? {},
         ),
       };
     } catch (error) {
