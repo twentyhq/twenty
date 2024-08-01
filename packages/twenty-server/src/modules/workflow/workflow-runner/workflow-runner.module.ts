ae423f5e7509d04353c1be11034ed72584dc6ada
martmull <martmull@hotmail.fr>
2024-08-01 11:38:31 +0200

    Improve typing definition (#6481)
    - added typing for workflow-runner results
    - fix workflow typing
    - add a `workflow-action-runner` submodule that contains factories for
    action runners
    - added code-action-runner
    - simplified code

--- packages/twenty-server/src/modules/workflow/workflow-runner/workflow-runner.module.ts
+++ packages/twenty-server/src/modules/workflow/workflow-runner/workflow-runner.module.ts
@@ -3,10 +3,10 @@ import { Module } from '@nestjs/common';
 import { WorkflowRunnerService } from 'src/modules/workflow/workflow-runner/workflow-runner.service';
 import { WorkflowRunnerJob } from 'src/modules/workflow/workflow-runner/workflow-runner.job';
 import { WorkflowCommonModule } from 'src/modules/workflow/common/workflow-common.module';
-import { ServerlessFunctionModule } from 'src/engine/metadata-modules/serverless-function/serverless-function.module';
+import { WorkflowActionRunnerModule } from 'src/modules/workflow/workflow-action-runner/workflow-action-runner.module';
 
 @Module({
-  imports: [WorkflowCommonModule, ServerlessFunctionModule],
+  imports: [WorkflowCommonModule, WorkflowActionRunnerModule],
   providers: [WorkflowRunnerService, WorkflowRunnerJob],
   exports: [WorkflowRunnerService],
 })
