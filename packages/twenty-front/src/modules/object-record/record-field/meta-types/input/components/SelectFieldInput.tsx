1f5c25ac0a7bc206b46c34466c23bb03ae5c4da1
Aditya Pimpalkar <adipimpalkar@gmail.com>
2024-08-02 19:12:46 +0100

    fix: navigate with arrow keys in select/multi-select (#5983)
    closes: #4977
    
    
    
    https://github.com/twentyhq/twenty/assets/13139771/8121814c-9a8a-4a8d-9290-1aebe145220f
    
    ---------
    
    Co-authored-by: Félix Malfait <felix.malfait@gmail.com>
    Co-authored-by: Lucas Bordeau <bordeau.lucas@gmail.com>

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/SelectFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/SelectFieldInput.tsx
@@ -1,14 +1,19 @@
 import styled from '@emotion/styled';
 import { useRef, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 import { Key } from 'ts-key-enum';
 
 import { useClearField } from '@/object-record/record-field/hooks/useClearField';
 import { useSelectField } from '@/object-record/record-field/meta-types/hooks/useSelectField';
 import { FieldInputEvent } from '@/object-record/record-field/types/FieldInputEvent';
+import { SINGLE_ENTITY_SELECT_BASE_LIST } from '@/object-record/relation-picker/constants/SingleEntitySelectBaseList';
 import { DropdownMenu } from '@/ui/layout/dropdown/components/DropdownMenu';
 import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/DropdownMenuItemsContainer';
 import { DropdownMenuSearchInput } from '@/ui/layout/dropdown/components/DropdownMenuSearchInput';
 import { DropdownMenuSeparator } from '@/ui/layout/dropdown/components/DropdownMenuSeparator';
+import { SelectableList } from '@/ui/layout/selectable-list/components/SelectableList';
+import { useSelectableListStates } from '@/ui/layout/selectable-list/hooks/internal/useSelectableListStates';
+import { useSelectableList } from '@/ui/layout/selectable-list/hooks/useSelectableList';
 import { MenuItemSelectTag } from '@/ui/navigation/menu-item/components/MenuItemSelectTag';
 import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
 import { useListenClickOutside } from '@/ui/utilities/pointer-event/hooks/useListenClickOutside';
@@ -31,8 +36,15 @@ export const SelectFieldInput = ({
 }: SelectFieldInputProps) => {
   const { persistField, fieldDefinition, fieldValue, hotkeyScope } =
     useSelectField();
+  const { selectedItemIdState } = useSelectableListStates({
+    selectableListScopeId: SINGLE_ENTITY_SELECT_BASE_LIST,
+  });
+  const { handleResetSelectedPosition } = useSelectableList(
+    SINGLE_ENTITY_SELECT_BASE_LIST,
+  );
   const clearField = useClearField();
 
+  const selectedItemId = useRecoilValue(selectedItemIdState);
   const [searchFilter, setSearchFilter] = useState('');
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -69,9 +81,20 @@ export const SelectFieldInput = ({
       );
       if (weAreNotInAnHTMLInput && isDefined(onCancel)) {
         onCancel();
+        handleResetSelectedPosition();
       }
     },
   });
+
+  useScopedHotkeys(
+    Key.Escape,
+    () => {
+      onCancel?.();
+      handleResetSelectedPosition();
+    },
+    hotkeyScope,
+    [onCancel, handleResetSelectedPosition],
+  );
 
   useScopedHotkeys(
     Key.Enter,
@@ -83,45 +106,71 @@ export const SelectFieldInput = ({
       if (isDefined(selectedOption)) {
         onSubmit?.(() => persistField(selectedOption.value));
       }
+      handleResetSelectedPosition();
     },
     hotkeyScope,
   );
 
+  const optionIds = [
+    `No ${fieldDefinition.label}`,
+    ...optionsInDropDown.map((option) => option.value),
+  ];
+
   return (
-    <StyledRelationPickerContainer ref={containerRef}>
-      <DropdownMenu data-select-disable>
-        <DropdownMenuSearchInput
-          value={searchFilter}
-          onChange={(event) => setSearchFilter(event.currentTarget.value)}
-          autoFocus
-        />
-        <DropdownMenuSeparator />
+    <SelectableList
+      selectableListId={SINGLE_ENTITY_SELECT_BASE_LIST}
+      selectableItemIdArray={optionIds}
+      hotkeyScope={hotkeyScope}
+      onEnter={(itemId) => {
+        const option = optionsInDropDown.find(
+          (option) => option.value === itemId,
+        );
+        if (isDefined(option)) {
+          onSubmit?.(() => persistField(option.value));
+          handleResetSelectedPosition();
+        }
+      }}
+    >
+      <StyledRelationPickerContainer ref={containerRef}>
+        <DropdownMenu data-select-disable>
+          <DropdownMenuSearchInput
+            value={searchFilter}
+            onChange={(event) => setSearchFilter(event.currentTarget.value)}
+            autoFocus
+          />
+          <DropdownMenuSeparator />
 
-        <DropdownMenuItemsContainer hasMaxHeight>
-          {fieldDefinition.metadata.isNullable && (
-            <MenuItemSelectTag
-              key={`No ${fieldDefinition.label}`}
-              selected={false}
-              text={`No ${fieldDefinition.label}`}
-              color="transparent"
-              variant="outline"
-              onClick={handleClearField}
-            />
-          )}
-
-          {optionsInDropDown.map((option) => {
-            return (
+          <DropdownMenuItemsContainer hasMaxHeight>
+            {fieldDefinition.metadata.isNullable ?? (
               <MenuItemSelectTag
-                key={option.value}
-                selected={option.value === fieldValue}
-                text={option.label}
-                color={option.color}
-                onClick={() => onSubmit?.(() => persistField(option.value))}
+                key={`No ${fieldDefinition.label}`}
+                selected={false}
+                text={`No ${fieldDefinition.label}`}
+                color="transparent"
+                variant="outline"
+                onClick={handleClearField}
+                isKeySelected={selectedItemId === `No ${fieldDefinition.label}`}
               />
-            );
-          })}
-        </DropdownMenuItemsContainer>
-      </DropdownMenu>
-    </StyledRelationPickerContainer>
+            )}
+
+            {optionsInDropDown.map((option) => {
+              return (
+                <MenuItemSelectTag
+                  key={option.value}
+                  selected={option.value === fieldValue}
+                  text={option.label}
+                  color={option.color}
+                  onClick={() => {
+                    onSubmit?.(() => persistField(option.value));
+                    handleResetSelectedPosition();
+                  }}
+                  isKeySelected={selectedItemId === option.value}
+                />
+              );
+            })}
+          </DropdownMenuItemsContainer>
+        </DropdownMenu>
+      </StyledRelationPickerContainer>
+    </SelectableList>
   );
 };
