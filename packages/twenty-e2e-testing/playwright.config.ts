843fb0cfdd355f8ad3fc49c0821f9ddeba5c7658
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-08-05 15:20:54 +0530

    fix: to have config for baseUrl and remove configs for ci

--- packages/twenty-e2e-testing/playwright.config.ts
+++ packages/twenty-e2e-testing/playwright.config.ts
@@ -1,34 +1,21 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import { config } from 'dotenv';
+config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
+ * See https://playwright.dev/docs/trace-viewer to Collect trace when retrying the failed test
  */
 export default defineConfig({
   testDir: './.',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    baseURL: process.env.FRONTEND_BASE_URL ?? 'http://localhost:3001',
   },
 
   /* Configure projects for major browsers */
