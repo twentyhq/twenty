11b7ac1d04997e520b440dace1daca7e3cce96c8
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-08-05 17:42:54 +0530

    fix: remove running dev server

--- packages/twenty-e2e-testing/playwright.config.ts
+++ packages/twenty-e2e-testing/playwright.config.ts
@@ -55,11 +55,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
