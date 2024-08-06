87a79b181ce7ceceea3e0930723cdac13a534f40
Thomas Trompette <thomas.trompette@sfr.fr>
2024-08-06 11:58:06 +0200

    Make bool fields editable again

--- packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellFieldInput.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 
 import { FieldInput } from '@/object-record/record-field/components/FieldInput';
 import { FieldContext } from '@/object-record/record-field/contexts/FieldContext';
+import { useIsFieldReadOnly } from '@/object-record/record-field/hooks/useIsFieldReadOnly';
 import { FieldInputEvent } from '@/object-record/record-field/types/FieldInputEvent';
 import { RecordTableContext } from '@/object-record/record-table/contexts/RecordTableContext';
 
@@ -9,6 +10,7 @@ export const RecordTableCellFieldInput = () => {
   const { onUpsertRecord, onMoveFocus, onCloseTableCell } =
     useContext(RecordTableContext);
   const { entityId, fieldDefinition } = useContext(FieldContext);
+  const isFieldReadOnly = useIsFieldReadOnly();
 
   const handleEnter: FieldInputEvent = (persistField) => {
     onUpsertRecord({
@@ -87,7 +89,7 @@ export const RecordTableCellFieldInput = () => {
       onShiftTab={handleShiftTab}
       onSubmit={handleSubmit}
       onTab={handleTab}
-      isReadOnly={true}
+      isReadOnly={isFieldReadOnly}
     />
   );
 };
