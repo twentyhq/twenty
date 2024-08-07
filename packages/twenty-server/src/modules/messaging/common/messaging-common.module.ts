a1c4868a1463b11075db6d393ab9f2933fdeb562
bosiraphael <raphael.bosi@gmail.com>
2024-08-07 18:33:19 +0200

    merge main

--- packages/twenty-server/src/modules/messaging/common/messaging-common.module.ts
+++ packages/twenty-server/src/modules/messaging/common/messaging-common.module.ts
@@ -2,16 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { FeatureFlagEntity } from 'src/engine/core-modules/feature-flag/feature-flag.entity';
-import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
 import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
 import { ConnectedAccountModule } from 'src/modules/connected-account/connected-account.module';
 import { MessagingChannelSyncStatusService } from 'src/modules/messaging/common/services/messaging-channel-sync-status.service';
-import { PersonWorkspaceEntity } from 'src/modules/person/standard-objects/person.workspace-entity';
 
 @Module({
   imports: [
     WorkspaceDataSourceModule,
-    ObjectMetadataRepositoryModule.forFeature([PersonWorkspaceEntity]),
     TypeOrmModule.forFeature([FeatureFlagEntity], 'core'),
     ConnectedAccountModule,
   ],
