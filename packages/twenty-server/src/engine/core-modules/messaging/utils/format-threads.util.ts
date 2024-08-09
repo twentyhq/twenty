448f148594f4df52c424b31320891f6a740b805c
bosiraphael <raphael.bosi@gmail.com>
2024-08-09 16:47:25 +0200

    improve typing

--- packages/twenty-server/src/engine/core-modules/messaging/utils/format-threads.util.ts
+++ packages/twenty-server/src/engine/core-modules/messaging/utils/format-threads.util.ts
@@ -1,5 +1,7 @@
 import { TimelineThread } from 'src/engine/core-modules/messaging/dtos/timeline-thread.dto';
 import { extractParticipantSummary } from 'src/engine/core-modules/messaging/utils/extract-participant-summary.util';
+import { MessageChannelVisibility } from 'src/modules/messaging/common/standard-objects/message-channel.workspace-entity';
+import { MessageParticipantWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-participant.workspace-entity';
 
 export const formatThreads = (
   threads: Omit<
@@ -10,8 +12,12 @@ export const formatThreads = (
     | 'read'
     | 'visibility'
   >[],
-  threadParticipantsByThreadId: any,
-  threadVisibilityByThreadId: any,
+  threadParticipantsByThreadId: {
+    [key: string]: MessageParticipantWorkspaceEntity[];
+  },
+  threadVisibilityByThreadId: {
+    [key: string]: MessageChannelVisibility;
+  },
 ): TimelineThread[] => {
   return threads.map((thread) => ({
     ...thread,
