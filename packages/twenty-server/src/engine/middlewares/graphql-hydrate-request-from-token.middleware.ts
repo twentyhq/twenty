6f76337cdebd2fbae83590c7790ae46e262070d9
Weiko <corentin@twenty.com>
2024-08-09 12:38:03 +0200

    Fix auth exceptions

--- packages/twenty-server/src/engine/middlewares/graphql-hydrate-request-from-token.middleware.ts
+++ packages/twenty-server/src/engine/middlewares/graphql-hydrate-request-from-token.middleware.ts
@@ -1,12 +1,31 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
+import { AuthGraphqlApiExceptionFilter } from 'src/engine/core-modules/auth/filters/auth-graphql-api-exception.filter';
 import { TokenService } from 'src/engine/core-modules/auth/services/token.service';
 import { AuthContext } from 'src/engine/core-modules/auth/types/auth-context.type';
 import { ExceptionHandlerService } from 'src/engine/integrations/exception-handler/exception-handler.service';
 import { WorkspaceCacheVersionService } from 'src/engine/metadata-modules/workspace-cache-version/workspace-cache-version.service';
 import { handleExceptionAndConvertToGraphQLError } from 'src/engine/utils/global-exception-handler.util';
+
+class GraphqlTokenValidationProxy {
+  private tokenService: TokenService;
+
+  constructor(tokenService: TokenService) {
+    this.tokenService = tokenService;
+  }
+
+  async validateToken(req: Request) {
+    try {
+      return await this.tokenService.validateToken(req);
+    } catch (error) {
+      const authGraphqlApiExceptionFilter = new AuthGraphqlApiExceptionFilter();
+
+      throw authGraphqlApiExceptionFilter.catch(error);
+    }
+  }
+}
 
 @Injectable()
 export class GraphQLHydrateRequestFromTokenMiddleware
@@ -48,7 +67,11 @@ export class GraphQLHydrateRequestFromTokenMiddleware
     let data: AuthContext;
 
     try {
-      data = await this.tokenService.validateToken(req);
+      const graphqlTokenValidationProxy = new GraphqlTokenValidationProxy(
+        this.tokenService,
+      );
+
+      data = await graphqlTokenValidationProxy.validateToken(req);
       const cacheVersion = await this.workspaceCacheVersionService.getVersion(
         data.workspace.id,
       );
