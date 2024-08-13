9d3328888e0cd2b2b72b62b535348bf480a317eb
Devessier <baptiste@devessier.fr>
2024-08-13 18:16:01 +0200

    feat: automatically position nodes and add + buttons to unlinked nodes

--- packages/twenty-front/src/pages/workflows/WorkflowShowPage.tsx
+++ packages/twenty-front/src/pages/workflows/WorkflowShowPage.tsx
@@ -1,23 +1,34 @@
 import styled from '@emotion/styled';
 import { useParams } from 'react-router-dom';
 
+import { IconButton } from '@/ui/input/button/components/IconButton';
 import { PageBody } from '@/ui/layout/page/PageBody';
 import { PageContainer } from '@/ui/layout/page/PageContainer';
 import { PageTitle } from '@/ui/utilities/page-title/PageTitle';
+import Dagre from '@dagrejs/dagre';
 import {
   Background,
+  Edge,
   Handle,
   MarkerType,
   Node,
   Position,
   ReactFlow,
+  useEdgesState,
+  useNodesState,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { GRAY_SCALE, IconSettingsAutomation } from 'twenty-ui';
+import { useMemo } from 'react';
+import { GRAY_SCALE, IconPlus, IconSettingsAutomation } from 'twenty-ui';
 import { WorkflowShowPageHeader } from '~/pages/workflows/WorkflowShowPageHeader';
 
-type NodeData = {
+type WorkflowNodeData = {
   nodeType: 'trigger' | 'condition' | 'action';
+  label: string;
+};
+
+const generateId = () => {
+  return Math.random().toString(16).slice(2);
 };
 
 const StyledFlowContainer = styled.div`
@@ -38,29 +49,150 @@ const StyledFlowContainer = styled.div`
   --xy-node-boxshadow-selected: none;
 `;
 
+const addCreateStepNodes = (
+  nodes: Array<Node<WorkflowNodeData>>,
+  edges: Array<Edge>,
+) => {
+  const nodesWithoutTargets = nodes.filter((n) =>
+    edges.every((e) => e.source !== n.id),
+  );
+
+  console.log({ nodesWithoutTargets });
+
+  const updatedNodes: typeof nodes = nodes.slice();
+  const updatedEdges: typeof edges = edges.slice();
+
+  for (const n of nodesWithoutTargets) {
+    const newCreateStepNode: Node<WorkflowNodeData> = {
+      id: generateId(),
+      type: 'create-step',
+      data: {},
+      position: { x: 0, y: 0 },
+    };
+
+    updatedNodes.push(newCreateStepNode);
+
+    updatedEdges.push({
+      id: generateId(),
+      source: n.id,
+      target: newCreateStepNode.id,
+      markerEnd: {
+        type: MarkerType.ArrowClosed,
+      },
+    });
+  }
+
+  console.log({ updatedNodes, updatedEdges });
+
+  return {
+    nodes: updatedNodes,
+    edges: updatedEdges,
+  };
+};
+
+const getLayoutedElements = (
+  nodes: Array<Node<WorkflowNodeData>>,
+  edges: Array<Edge>,
+) => {
+  const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+  g.setGraph({ rankdir: 'TB' });
+
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target));
+  nodes.forEach((node) =>
+    g.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    }),
+  );
+
+  Dagre.layout(g);
+
+  return {
+    nodes: nodes.map((node) => {
+      const position = g.node(node.id);
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      const x = position.x - (node.measured?.width ?? 0) / 2;
+      const y = position.y - (node.measured?.height ?? 0) / 2;
+
+      return { ...node, position: { x, y } };
+    }),
+    edges,
+  };
+};
+
+const initialNodes: Array<Node<WorkflowNodeData>> = [
+  {
+    id: '1',
+    data: {
+      nodeType: 'trigger',
+      label: 'Person is Created',
+    },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '2',
+    data: {
+      nodeType: 'action',
+      label: 'Find Stripe Customer',
+    },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '3',
+    data: {
+      nodeType: 'action',
+      label: 'Update People',
+    },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '4',
+    data: {
+      nodeType: 'action',
+      label: 'Split First & Last Name',
+    },
+    position: { x: 0, y: 0 },
+  },
+];
+const initialEdges: Array<Edge> = [
+  {
+    id: 'edge-1',
+    source: '1',
+    target: '2',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'edge-2',
+    source: '2',
+    target: '3',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'edge-3',
+    source: '2',
+    target: '4',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+];
+
 export const WorkflowShowPage = () => {
-  const nodes: Array<Node<NodeData>> = [
-    {
-      id: '1',
-      data: {
-        nodeType: 'trigger',
-      },
-      position: {
-        x: 0,
-        y: 0,
-      },
-    },
-    {
-      id: '2',
-      data: {
-        nodeType: 'action',
-      },
-      position: {
-        x: 0,
-        y: 100,
-      },
-    },
-  ];
+  const { nodes: n, edges: e } = useMemo(
+    () => addCreateStepNodes(initialNodes, initialEdges),
+    [initialNodes, initialEdges],
+  );
+
+  const [rawNodes, setRawNodes, onRawNodesChange] = useNodesState(n);
+  const [rawEdges, setRawEdges, onRawEdgesChange] = useEdgesState(e);
+
+  const { nodes, edges } = getLayoutedElements(rawNodes, rawEdges);
 
   const parameters = useParams<{
     workflowId: string;
@@ -79,19 +211,13 @@ export const WorkflowShowPage = () => {
           <ReactFlow
             nodeTypes={{
               default: StepNode,
+              'create-step': CreateStepNode,
             }}
             fitView
-            defaultNodes={nodes}
-            defaultEdges={[
-              {
-                id: 'edge-1',
-                source: '1',
-                target: '2',
-                markerEnd: {
-                  type: MarkerType.ArrowClosed,
-                },
-              },
-            ]}
+            nodes={nodes.map((n) => ({ ...n, draggable: false }))}
+            edges={edges}
+            onNodesChange={onRawNodesChange}
+            onEdgesChange={onRawEdgesChange}
           >
             <Background color={GRAY_SCALE.gray25} size={2} />
           </ReactFlow>
@@ -105,21 +231,23 @@ const StyledStepNodeContainer = styled.div`
   display: flex;
   flex-direction: column;
 
-  padding: 12px 0;
+  padding-bottom: 12px;
+  padding-top: 6px;
 `;
 
 const StyledStepNodeType = styled.div`
-  align-self: flex-start;
-
   background-color: ${({ theme }) => theme.background.tertiary};
   border-radius: ${({ theme }) => theme.border.radius.sm}
     ${({ theme }) => theme.border.radius.sm} 0 0;
-  margin-left: ${({ theme }) => theme.spacing(2)};
-  padding: ${({ theme }) => theme.spacing(1)} ${({ theme }) => theme.spacing(2)};
 
-  font-weight: ${({ theme }) => theme.font.weight.semiBold};
-  font-size: ${({ theme }) => theme.font.size.xs};
   color: #b3b3b3;
+  font-size: ${({ theme }) => theme.font.size.xs};
+  font-weight: ${({ theme }) => theme.font.weight.semiBold};
+
+  padding: ${({ theme }) => theme.spacing(1)} ${({ theme }) => theme.spacing(2)};
+  position: absolute;
+  top: 0;
+  transform: translateY(-100%);
 `;
 
 const StyledStepNodeInnerContainer = styled.div`
@@ -130,6 +258,7 @@ const StyledStepNodeInnerContainer = styled.div`
   gap: ${({ theme }) => theme.spacing(2)};
   padding: ${({ theme }) => theme.spacing(2)};
 
+  position: relative;
   box-shadow: ${({ theme }) => theme.boxShadow.superHeavy};
 
   .selectable.selected &,
@@ -144,20 +273,38 @@ const StyledStepNodeLabel = styled.div`
   font-weight: ${({ theme }) => theme.font.weight.medium};
 `;
 
-const StepNode = ({ data }: { data: NodeData }) => {
+const StyledTargetHandle = styled(Handle)`
+  visibility: hidden;
+`;
+
+const StyledSourceHandle = styled(Handle)`
+  background-color: #b3b3b3;
+`;
+
+const StepNode = ({ data }: { data: WorkflowNodeData }) => {
   return (
     <StyledStepNodeContainer>
       {data.nodeType !== 'trigger' ? (
-        <Handle type="target" position={Position.Top} />
+        <StyledTargetHandle type="target" position={Position.Top} />
       ) : null}
 
-      <StyledStepNodeType>{data.nodeType}</StyledStepNodeType>
-
       <StyledStepNodeInnerContainer>
-        <StyledStepNodeLabel>Yolo</StyledStepNodeLabel>
+        <StyledStepNodeType>{data.nodeType}</StyledStepNodeType>
+
+        <StyledStepNodeLabel>{data.label}</StyledStepNodeLabel>
       </StyledStepNodeInnerContainer>
 
-      <Handle type="source" position={Position.Bottom} />
+      <StyledSourceHandle type="source" position={Position.Bottom} />
     </StyledStepNodeContainer>
+  );
+};
+
+const CreateStepNode = () => {
+  return (
+    <div>
+      <StyledTargetHandle type="target" position={Position.Top} />
+
+      <IconButton Icon={IconPlus} />
+    </div>
   );
 };
