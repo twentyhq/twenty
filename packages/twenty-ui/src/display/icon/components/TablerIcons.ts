d5fc22af1773835d070da87c7d376a6b66067f78
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-08-16 20:50:42 +0200

    Fixed lint

--- packages/twenty-ui/src/display/icon/components/TablerIcons.ts
+++ packages/twenty-ui/src/display/icon/components/TablerIcons.ts
@@ -173,7 +173,7 @@ export {
   IconVideo,
   IconWand,
   IconWorld,
-  IconX
+  IconX,
 } from '@tabler/icons-react';
-export type { TablerIconsProps } from '@tabler/icons-react';
 
+export type { TablerIconsProps } from '@tabler/icons-react';
