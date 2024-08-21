b786a427f40282990ca5cadf7179c102a545a501
Thomas Trompette <thomas.trompette@sfr.fr>
2024-08-21 16:05:02 +0200

    Fix modules

--- packages/twenty-server/src/modules/workflow/workflow-runner/workflow-runner.module.ts
+++ packages/twenty-server/src/modules/workflow/workflow-runner/workflow-runner.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { WorkflowCommonModule } from 'src/modules/workflow/common/workflow-common.module';
+import { WorkflowExecutorModule } from 'src/modules/workflow/workflow-executor/workflow-executor.module';
+import { RunWorkflowJob } from 'src/modules/workflow/workflow-runner/jobs/run-workflow.job';
 import { WorkflowRunModule } from 'src/modules/workflow/workflow-runner/workflow-run/workflow-run.module';
 import { WorkflowRunnerWorkspaceService } from 'src/modules/workflow/workflow-runner/workflow-runner.workspace-service';
 
 @Module({
-  imports: [WorkflowRunModule],
-  providers: [WorkflowRunnerWorkspaceService],
+  imports: [WorkflowRunModule, WorkflowCommonModule, WorkflowExecutorModule],
+  providers: [WorkflowRunnerWorkspaceService, RunWorkflowJob],
   exports: [WorkflowRunnerWorkspaceService],
 })
 export class WorkflowRunnerModule {}
