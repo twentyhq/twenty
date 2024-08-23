80db2042e06d5cf903eaabc2af56af0ae1fc0414
gitstart-twenty <gitstart-twenty@users.noreply.github.com>
2024-08-23 16:36:05 +0000

    Fix currency field edition form

--- packages/twenty-front/src/modules/settings/data-model/fields/forms/currency/hooks/useCurrencySettingsFormInitialValues.ts
+++ packages/twenty-front/src/modules/settings/data-model/fields/forms/currency/hooks/useCurrencySettingsFormInitialValues.ts
@@ -12,11 +12,10 @@ export const useCurrencySettingsFormInitialValues = ({
 }) => {
   const initialAmountMicrosValue =
     (fieldMetadataItem?.defaultValue?.amountMicros as number | null) ?? null;
-  const initialCurrencyCode =
-    (fieldMetadataItem?.defaultValue?.currencyCode as CurrencyCode) ??
-    CurrencyCode.USD;
   const initialCurrencyCodeValue =
-    applySimpleQuotesToString(initialCurrencyCode);
+    (fieldMetadataItem?.defaultValue?.currencyCode as `'${CurrencyCode}'`) ??
+    applySimpleQuotesToString(CurrencyCode.USD);
+
   const initialDefaultValue = {
     amountMicros: initialAmountMicrosValue,
     currencyCode: initialCurrencyCodeValue,
