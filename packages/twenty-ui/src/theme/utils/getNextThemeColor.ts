f5136c3cfa8fe48abc67738a26308ad4dfd6f3e4
Marie Stoppa <marie.stoppa@essec.edu>
2024-08-23 11:35:50 +0200

    Improve code quality

--- packages/twenty-ui/src/theme/utils/getNextThemeColor.ts
+++ packages/twenty-ui/src/theme/utils/getNextThemeColor.ts
@@ -1,9 +1,13 @@
 import { MAIN_COLOR_NAMES, ThemeColor } from '@ui/theme';
+import { isDefined } from '@ui/utilities';
 
 export const getNextThemeColor = (currentColor?: ThemeColor): ThemeColor => {
-  const currentColorIndex = currentColor
-    ? MAIN_COLOR_NAMES.findIndex((color) => color === currentColor)
-    : -1;
+  if (!isDefined(currentColor)) {
+    return MAIN_COLOR_NAMES[0];
+  }
+  const currentColorIndex = MAIN_COLOR_NAMES.findIndex(
+    (color) => color === currentColor,
+  );
   const nextColorIndex = (currentColorIndex + 1) % MAIN_COLOR_NAMES.length;
   return MAIN_COLOR_NAMES[nextColorIndex];
 };
