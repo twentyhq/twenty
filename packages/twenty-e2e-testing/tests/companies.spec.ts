52f4a1dcfc4c3069ddbe0a2a61026abcfda1875d
BOHEUS <nothin_spe@2abc.hub.pl>
2024-08-26 12:34:40 +0200

    Added check for Continue button

--- packages/twenty-e2e-testing/tests/companies.spec.ts
+++ packages/twenty-e2e-testing/tests/companies.spec.ts
@@ -9,7 +9,7 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/');
   await page.getByRole('button', { name: 'Continue With Email' }).click();
   await page.getByPlaceholder('Email').fill(process.env.DEFAULT_LOGIN);
-  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('button', { name: 'Continue', exact: true }).click();
   await page.getByPlaceholder('Password').fill(process.env.DEFAULT_PASSWORD);
   await page.getByRole('button', { name: 'Sign in' }).click();
 });
--- packages/twenty-e2e-testing/tests/workspaces.spec.ts
+++ packages/twenty-e2e-testing/tests/workspaces.spec.ts
@@ -23,7 +23,7 @@ test.describe('', () => {
     await page.goto('/');
     await page.getByRole('button', { name: 'Continue With Email' }).click();
     await page.getByPlaceholder('Email').fill('tim@apple.dev');
-    await page.getByRole('button', { name: 'Continue' }).click();
+    await page.getByRole('button', { name: 'Continue', exact: true }).click();
     await page.getByPlaceholder('Password').fill('Applecar2025');
     await page.getByRole('button', { name: 'Sign in' }).click();
     expect(page.url()).not.toContain('/welcome');
