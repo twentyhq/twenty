7b9e9607058c1a7590f28931231fb4e7d31a293b
Devessier <baptiste@devessier.fr>
2024-08-27 16:51:17 +0200

    feat: get the last version by sorting all of them by creation datetime

--- packages/twenty-front/src/modules/workflow/utils/getWorkflowLastVersion.ts
+++ packages/twenty-front/src/modules/workflow/utils/getWorkflowLastVersion.ts
@@ -3,5 +3,8 @@ import { Workflow, WorkflowVersion } from '@/workflow/types/Workflow';
 export const getWorkflowLastVersion = (
   workflow: Workflow,
 ): WorkflowVersion | undefined => {
-  return workflow.versions.at(-1);
+  return workflow.versions
+    .slice()
+    .sort((a, b) => (a.createdAt < b.createdAt ? -1 : 1))
+    .at(-1);
 };
