974703f0981763b576cf6ddd27958dc41cc27f1a
Nitin Koche <nitinkoche03@gmail.com>
2024-08-30 02:09:05 +0530

    lint fix

--- packages/twenty-front/src/modules/settings/data-model/constants/SettingsFieldTypeCategoryDescriptions.ts
+++ packages/twenty-front/src/modules/settings/data-model/constants/SettingsFieldTypeCategoryDescriptions.ts
@@ -1,5 +1,4 @@
-import { SettingsFieldTypeCategoryType } from "@/settings/data-model/types/SettingsFieldTypeCategoryType";
-
+import { SettingsFieldTypeCategoryType } from '@/settings/data-model/types/SettingsFieldTypeCategoryType';
 
 export const SETTINGS_FIELD_TYPE_CATEGORY_DESCRIPTIONS: Record<
   SettingsFieldTypeCategoryType,
