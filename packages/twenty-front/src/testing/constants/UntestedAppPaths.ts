2a943e72ed28919ed0a746914637108ebdf22830
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-08-30 15:37:16 +0200

    Fix lint and test

--- packages/twenty-front/src/testing/constants/UntestedAppPaths.ts
+++ packages/twenty-front/src/testing/constants/UntestedAppPaths.ts
@@ -1,3 +1,8 @@
 import { AppPath } from '@/types/AppPath';
 
-export const UNTESTED_APP_PATHS = [AppPath.Settings, AppPath.Developers];
+export const UNTESTED_APP_PATHS = [
+  AppPath.Settings,
+  AppPath.Developers,
+  // TODO: add this page to tests ?
+  AppPath.WorkflowShowPage,
+];
