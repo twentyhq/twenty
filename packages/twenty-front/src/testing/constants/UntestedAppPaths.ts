67a4a49905f24dadcefaf344dc614a9689651ace
Charles Bochet <charles@twenty.com>
2024-08-31 01:02:55 +0200

    Fix

--- packages/twenty-front/src/testing/constants/UntestedAppPaths.ts
+++ packages/twenty-front/src/testing/constants/UntestedAppPaths.ts
@@ -1,6 +1,3 @@
 import { AppPath } from '@/types/AppPath';
 
-export const UNTESTED_APP_PATHS = [
-  AppPath.Settings,
-  AppPath.Developers,
-];
+export const UNTESTED_APP_PATHS = [AppPath.Settings, AppPath.Developers];
