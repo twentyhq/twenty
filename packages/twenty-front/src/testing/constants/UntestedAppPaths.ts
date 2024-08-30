8b49d5e0bb7cdfdde1ff06c7ae918e5e2d9a315e
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-08-30 15:45:49 +0200

    Fixed comment

--- packages/twenty-front/src/testing/constants/UntestedAppPaths.ts
+++ packages/twenty-front/src/testing/constants/UntestedAppPaths.ts
@@ -3,6 +3,5 @@ import { AppPath } from '@/types/AppPath';
 export const UNTESTED_APP_PATHS = [
   AppPath.Settings,
   AppPath.Developers,
-  // TODO: add this page to tests ?
   AppPath.WorkflowShowPage,
 ];
