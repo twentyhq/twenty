a9da5ba193efa7e1f89216add8db3edd69b99f14
Charles Bochet <charles@twenty.com>
2024-08-30 22:54:10 +0200

    lazy load Settings routes to ease build

--- packages/twenty-front/src/App.tsx
+++ packages/twenty-front/src/App.tsx
@@ -5,7 +5,6 @@ import {
   Outlet,
   Route,
   RouterProvider,
-  Routes,
   useLocation,
 } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
@@ -24,7 +23,6 @@ import { ApolloMetadataClientProvider } from '@/object-metadata/components/Apoll
 import { ObjectMetadataItemsProvider } from '@/object-metadata/components/ObjectMetadataItemsProvider';
 import { PrefetchDataProvider } from '@/prefetch/components/PrefetchDataProvider';
 import { AppPath } from '@/types/AppPath';
-import { SettingsPath } from '@/types/SettingsPath';
 import { DialogManager } from '@/ui/feedback/dialog-manager/components/DialogManager';
 import { DialogManagerScope } from '@/ui/feedback/dialog-manager/scopes/DialogManagerScope';
 import { SnackBarProvider } from '@/ui/feedback/snack-bar-manager/components/SnackBarProvider';
@@ -52,39 +50,8 @@ import { CreateWorkspace } from '~/pages/onboarding/CreateWorkspace';
 import { InviteTeam } from '~/pages/onboarding/InviteTeam';
 import { PaymentSuccess } from '~/pages/onboarding/PaymentSuccess';
 import { SyncEmails } from '~/pages/onboarding/SyncEmails';
-import { SettingsAccounts } from '~/pages/settings/accounts/SettingsAccounts';
-import { SettingsAccountsCalendars } from '~/pages/settings/accounts/SettingsAccountsCalendars';
-import { SettingsAccountsEmails } from '~/pages/settings/accounts/SettingsAccountsEmails';
-import { SettingsNewAccount } from '~/pages/settings/accounts/SettingsNewAccount';
-import { SettingsCRMMigration } from '~/pages/settings/crm-migration/SettingsCRMMigration';
-import { SettingsNewObject } from '~/pages/settings/data-model/SettingsNewObject';
-import { SettingsObjectDetailPage } from '~/pages/settings/data-model/SettingsObjectDetailPage';
-import { SettingsObjectEdit } from '~/pages/settings/data-model/SettingsObjectEdit';
-import { SettingsObjectFieldEdit } from '~/pages/settings/data-model/SettingsObjectFieldEdit';
-import { SettingsObjectNewFieldStep1 } from '~/pages/settings/data-model/SettingsObjectNewField/SettingsObjectNewFieldStep1';
-import { SettingsObjectNewFieldStep2 } from '~/pages/settings/data-model/SettingsObjectNewField/SettingsObjectNewFieldStep2';
-import { SettingsObjectOverview } from '~/pages/settings/data-model/SettingsObjectOverview';
-import { SettingsObjects } from '~/pages/settings/data-model/SettingsObjects';
-import { SettingsDevelopersApiKeyDetail } from '~/pages/settings/developers/api-keys/SettingsDevelopersApiKeyDetail';
-import { SettingsDevelopersApiKeysNew } from '~/pages/settings/developers/api-keys/SettingsDevelopersApiKeysNew';
-import { SettingsDevelopers } from '~/pages/settings/developers/SettingsDevelopers';
-import { SettingsDevelopersWebhooksDetail } from '~/pages/settings/developers/webhooks/SettingsDevelopersWebhookDetail';
-import { SettingsDevelopersWebhooksNew } from '~/pages/settings/developers/webhooks/SettingsDevelopersWebhooksNew';
-import { SettingsIntegrationDatabase } from '~/pages/settings/integrations/SettingsIntegrationDatabase';
-import { SettingsIntegrationEditDatabaseConnection } from '~/pages/settings/integrations/SettingsIntegrationEditDatabaseConnection';
-import { SettingsIntegrationNewDatabaseConnection } from '~/pages/settings/integrations/SettingsIntegrationNewDatabaseConnection';
-import { SettingsIntegrations } from '~/pages/settings/integrations/SettingsIntegrations';
-import { SettingsIntegrationShowDatabaseConnection } from '~/pages/settings/integrations/SettingsIntegrationShowDatabaseConnection';
-import { SettingsAppearance } from '~/pages/settings/profile/appearance/components/SettingsAppearance';
-import { Releases } from '~/pages/settings/Releases';
-import { SettingsServerlessFunctionDetailWrapper } from '~/pages/settings/serverless-functions/SettingsServerlessFunctionDetailWrapper';
-import { SettingsServerlessFunctions } from '~/pages/settings/serverless-functions/SettingsServerlessFunctions';
-import { SettingsServerlessFunctionsNew } from '~/pages/settings/serverless-functions/SettingsServerlessFunctionsNew';
-import { SettingsBilling } from '~/pages/settings/SettingsBilling';
-import { SettingsProfile } from '~/pages/settings/SettingsProfile';
-import { SettingsWorkspace } from '~/pages/settings/SettingsWorkspace';
-import { SettingsWorkspaceMembers } from '~/pages/settings/SettingsWorkspaceMembers';
 import { WorkflowShowPage } from '~/pages/workflows/WorkflowShowPage';
+import { SettingsRoutes } from '~/SettingsRoutes';
 import { getPageTitleFromPath } from '~/utils/title-utils';
 
 const ProvidersThatNeedRouterContext = () => {
@@ -164,159 +131,22 @@ const createRouter = (
           <Route path={AppPath.Impersonate} element={<ImpersonateEffect />} />
           <Route path={AppPath.RecordIndexPage} element={<RecordIndexPage />} />
           <Route path={AppPath.RecordShowPage} element={<RecordShowPage />} />
-
           {isWorkflowEnabled === true ? (
             <Route
               path={AppPath.WorkflowShowPage}
               element={<WorkflowShowPage />}
             />
           ) : null}
-
           <Route
             path={AppPath.SettingsCatchAll}
             element={
-              <Routes>
-                <Route
-                  path={SettingsPath.ProfilePage}
-                  element={<SettingsProfile />}
-                />
-                <Route
-                  path={SettingsPath.Appearance}
-                  element={<SettingsAppearance />}
-                />
-                <Route
-                  path={SettingsPath.Accounts}
-                  element={<SettingsAccounts />}
-                />
-                <Route
-                  path={SettingsPath.NewAccount}
-                  element={<SettingsNewAccount />}
-                />
-                <Route
-                  path={SettingsPath.AccountsCalendars}
-                  element={<SettingsAccountsCalendars />}
-                />
-                <Route
-                  path={SettingsPath.AccountsEmails}
-                  element={<SettingsAccountsEmails />}
-                />
-                {isBillingEnabled && (
-                  <Route
-                    path={SettingsPath.Billing}
-                    element={<SettingsBilling />}
-                  />
-                )}
-                <Route
-                  path={SettingsPath.WorkspaceMembersPage}
-                  element={<SettingsWorkspaceMembers />}
-                />
-                <Route
-                  path={SettingsPath.Workspace}
-                  element={<SettingsWorkspace />}
-                />
-                <Route
-                  path={SettingsPath.Objects}
-                  element={<SettingsObjects />}
-                />
-                <Route
-                  path={SettingsPath.ObjectOverview}
-                  element={<SettingsObjectOverview />}
-                />
-                <Route
-                  path={SettingsPath.ObjectDetail}
-                  element={<SettingsObjectDetailPage />}
-                />
-                <Route
-                  path={SettingsPath.ObjectEdit}
-                  element={<SettingsObjectEdit />}
-                />
-                <Route
-                  path={SettingsPath.NewObject}
-                  element={<SettingsNewObject />}
-                />
-                <Route
-                  path={SettingsPath.Developers}
-                  element={<SettingsDevelopers />}
-                />
-                {isCRMMigrationEnabled && (
-                  <Route
-                    path={SettingsPath.CRMMigration}
-                    element={<SettingsCRMMigration />}
-                  />
-                )}
-                <Route
-                  path={AppPath.DevelopersCatchAll}
-                  element={
-                    <Routes>
-                      <Route
-                        path={SettingsPath.DevelopersNewApiKey}
-                        element={<SettingsDevelopersApiKeysNew />}
-                      />
-                      <Route
-                        path={SettingsPath.DevelopersApiKeyDetail}
-                        element={<SettingsDevelopersApiKeyDetail />}
-                      />
-                      <Route
-                        path={SettingsPath.DevelopersNewWebhook}
-                        element={<SettingsDevelopersWebhooksNew />}
-                      />
-                      <Route
-                        path={SettingsPath.DevelopersNewWebhookDetail}
-                        element={<SettingsDevelopersWebhooksDetail />}
-                      />
-                    </Routes>
-                  }
-                />
-                {isServerlessFunctionSettingsEnabled && (
-                  <>
-                    <Route
-                      path={SettingsPath.ServerlessFunctions}
-                      element={<SettingsServerlessFunctions />}
-                    />
-                    <Route
-                      path={SettingsPath.NewServerlessFunction}
-                      element={<SettingsServerlessFunctionsNew />}
-                    />
-                    <Route
-                      path={SettingsPath.ServerlessFunctionDetail}
-                      element={<SettingsServerlessFunctionDetailWrapper />}
-                    />
-                  </>
-                )}
-                <Route
-                  path={SettingsPath.Integrations}
-                  element={<SettingsIntegrations />}
-                />
-                <Route
-                  path={SettingsPath.IntegrationDatabase}
-                  element={<SettingsIntegrationDatabase />}
-                />
-                <Route
-                  path={SettingsPath.IntegrationNewDatabaseConnection}
-                  element={<SettingsIntegrationNewDatabaseConnection />}
-                />
-                <Route
-                  path={SettingsPath.IntegrationEditDatabaseConnection}
-                  element={<SettingsIntegrationEditDatabaseConnection />}
-                />
-                <Route
-                  path={SettingsPath.IntegrationDatabaseConnection}
-                  element={<SettingsIntegrationShowDatabaseConnection />}
-                />
-                <Route
-                  path={SettingsPath.ObjectNewFieldStep1}
-                  element={<SettingsObjectNewFieldStep1 />}
-                />
-                <Route
-                  path={SettingsPath.ObjectNewFieldStep2}
-                  element={<SettingsObjectNewFieldStep2 />}
-                />
-                <Route
-                  path={SettingsPath.ObjectFieldEdit}
-                  element={<SettingsObjectFieldEdit />}
-                />
-                <Route path={SettingsPath.Releases} element={<Releases />} />
-              </Routes>
+              <SettingsRoutes
+                isBillingEnabled={isBillingEnabled}
+                isCRMMigrationEnabled={isCRMMigrationEnabled}
+                isServerlessFunctionSettingsEnabled={
+                  isServerlessFunctionSettingsEnabled
+                }
+              />
             }
           />
           <Route path={AppPath.NotFoundWildcard} element={<NotFound />} />
