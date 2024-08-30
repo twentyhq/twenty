8f20dcf9db3cd81b8a28532075a9a819ff92b82a
Weiko <corentin@twenty.com>
2024-08-30 18:48:36 +0200

    add missing deletedAt fields

--- packages/twenty-server/src/database/commands/upgrade-version/0-24/0-24-set-custom-object-is-soft-deletable.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-24/0-24-set-custom-object-is-soft-deletable.command.ts
@@ -8,7 +8,12 @@ import {
   ActiveWorkspacesCommandRunner,
 } from 'src/database/commands/active-workspaces.command';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import {
+  FieldMetadataEntity,
+  FieldMetadataType,
+} from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
+import { BASE_OBJECT_STANDARD_FIELD_IDS } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-field-ids';
 
 type SetCustomObjectIsSoftDeletableCommandOptions =
   ActiveWorkspacesCommandOptions;
@@ -23,6 +28,8 @@ export class SetCustomObjectIsSoftDeletableCommand extends ActiveWorkspacesComma
     protected readonly workspaceRepository: Repository<Workspace>,
     @InjectRepository(ObjectMetadataEntity, 'metadata')
     private readonly objectMetadataRepository: Repository<ObjectMetadataEntity>,
+    @InjectRepository(FieldMetadataEntity, 'metadata')
+    private readonly fieldMetadataRepository: Repository<FieldMetadataEntity>,
   ) {
     super(workspaceRepository);
   }
@@ -32,29 +39,128 @@ export class SetCustomObjectIsSoftDeletableCommand extends ActiveWorkspacesComma
     options: SetCustomObjectIsSoftDeletableCommandOptions,
     workspaceIds: string[],
   ): Promise<void> {
+    for (const workspaceId of workspaceIds) {
+      await this.updateCustomObjectMetadata(
+        workspaceId,
+        options.dryRun ?? false,
+      );
+      await this.createDeletedAtFields(workspaceId, options.dryRun ?? false);
+    }
+  }
+
+  private async updateCustomObjectMetadata(
+    workspaceId: string,
+    dryRun: boolean,
+  ): Promise<void> {
     const updateCriteria = {
-      workspaceId: In(workspaceIds),
+      workspaceId: workspaceId,
       isCustom: true,
       isSoftDeletable: false,
     };
 
-    if (options.dryRun) {
-      const entitiesToUpdate = await this.objectMetadataRepository.find({
+    const objectsMetadataToUpdateIds = (
+      await this.objectMetadataRepository.find({
         select: ['id'],
         where: updateCriteria,
-      });
+      })
+    ).map((entity) => entity.id);
 
+    if (dryRun) {
       this.logger.log(
-        `Dry run: ${entitiesToUpdate.length} entities would be updated`,
+        `Dry run: ${objectsMetadataToUpdateIds.length} objects would be updated for workspace ${workspaceId}`,
       );
 
       return;
     }
 
-    const result = await this.objectMetadataRepository.update(updateCriteria, {
-      isSoftDeletable: true,
+    if (objectsMetadataToUpdateIds.length === 0) {
+      this.logger.log(
+        `No objects found for workspace ${workspaceId} to update`,
+      );
+
+      return;
+    }
+
+    const updatedObjects = await this.objectMetadataRepository.update(
+      objectsMetadataToUpdateIds,
+      {
+        isSoftDeletable: true,
+      },
+    );
+
+    this.logger.log(
+      `Updated ${updatedObjects.affected} objects for workspace ${workspaceId}`,
+    );
+  }
+
+  private async createDeletedAtFields(workspaceId: string, dryRun: boolean) {
+    const customObjectsMetadata = await this.objectMetadataRepository.find({
+      select: ['id'],
+      where: {
+        workspaceId,
+        isCustom: true,
+      },
     });
 
-    this.logger.log(`Updated ${result.affected} entities`);
+    const customObjectsMetadataIds = customObjectsMetadata.map(
+      (entity) => entity.id,
+    );
+
+    if (dryRun) {
+      this.logger.log(
+        `Dry run: ${customObjectsMetadataIds.length} deletedAt fields would be created for workspace ${workspaceId}`,
+      );
+
+      return;
+    }
+
+    const existingDeletedAtFields = await this.fieldMetadataRepository.find({
+      select: ['objectMetadataId'],
+      where: {
+        workspaceId,
+        objectMetadataId: In(customObjectsMetadataIds),
+        standardId: BASE_OBJECT_STANDARD_FIELD_IDS.deletedAt,
+      },
+    });
+
+    const existingDeletedAtFieldObjectIds = new Set(
+      existingDeletedAtFields.map((field) => field.objectMetadataId),
+    );
+
+    const objectsNeedingDeletedAtFields = customObjectsMetadataIds.filter(
+      (id) => !existingDeletedAtFieldObjectIds.has(id),
+    );
+
+    const newDeletedAtFields = objectsNeedingDeletedAtFields.map(
+      (objectId) =>
+        ({
+          standardId: BASE_OBJECT_STANDARD_FIELD_IDS.deletedAt,
+          type: FieldMetadataType.DATE_TIME,
+          name: 'deletedAt',
+          label: 'Deleted at',
+          icon: 'IconCalendarClock',
+          description: 'Deletion date',
+          isNullable: true,
+          isActive: true,
+          isCustom: false,
+          isSystem: false,
+          workspaceId,
+          defaultValue: null,
+          objectMetadataId: objectId,
+        }) satisfies Partial<FieldMetadataEntity>,
+    );
+
+    if (newDeletedAtFields.length > 0) {
+      const createdDeletedAtFields =
+        await this.fieldMetadataRepository.insert(newDeletedAtFields);
+
+      this.logger.log(
+        `Created ${createdDeletedAtFields.identifiers.length} deletedAt fields for workspace ${workspaceId}`,
+      );
+    } else {
+      this.logger.log(
+        `No new deletedAt fields needed for workspace ${workspaceId}`,
+      );
+    }
   }
 }
