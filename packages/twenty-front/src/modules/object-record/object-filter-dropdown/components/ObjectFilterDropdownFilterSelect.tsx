e06fa04166ee91bf76e344a4c353b45a87f74084
Pacifique Linjanja <pacilinja2@gmail.com>
2024-09-02 11:31:01 +0400

    feat(UI): update the UI to allow filtering by composite sub-fields

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/ObjectFilterDropdownFilterSelect.tsx
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/ObjectFilterDropdownFilterSelect.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import { useIcons } from 'twenty-ui';
+import { IconApps, IconChevronLeft, useIcons } from 'twenty-ui';
 
 import { useFilterDropdown } from '@/object-record/object-filter-dropdown/hooks/useFilterDropdown';
 import { RelationPickerHotkeyScope } from '@/object-record/relation-picker/types/RelationPickerHotkeyScope';
@@ -9,6 +9,9 @@ import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/Drop
 import { MenuItem } from '@/ui/navigation/menu-item/components/MenuItem';
 import { useSetHotkeyScope } from '@/ui/utilities/hotkey/hooks/useSetHotkeyScope';
 
+import { FilterDefinition } from '@/object-record/object-filter-dropdown/types/FilterDefinition';
+import { FilterType } from '@/object-record/object-filter-dropdown/types/FilterType';
+import { DropdownMenuHeader } from '@/ui/layout/dropdown/components/DropdownMenuHeader';
 import { getOperandsForFilterType } from '../utils/getOperandsForFilterType';
 
 export const StyledInput = styled.input`
@@ -37,8 +40,131 @@ export const StyledInput = styled.input`
   }
 `;
 
+type SubMenuProps = {
+  currentSubMenu: FilterType;
+  setCurrentSubMenu: (currentSubMenu: FilterType | null) => void;
+  parent: FilterDefinition | null;
+};
+
+const SubMenu = ({
+  currentSubMenu,
+  setCurrentSubMenu,
+  parent,
+}: SubMenuProps) => {
+  const [searchText, setSearchText] = useState('');
+
+  const getHeaderTitle = (subMenu: FilterType | null) => {
+    switch (subMenu) {
+      case 'ADDRESS':
+        return 'Address';
+      case 'FULL_NAME':
+        return 'Full Name';
+      case 'LINKS':
+        return 'Links';
+      default:
+        return;
+    }
+  };
+
+  const { getIcon } = useIcons();
+
+  const getMenuOptions = (subMenu: FilterType | null) => {
+    switch (subMenu) {
+      case 'ADDRESS':
+        return [
+          'Address 1',
+          'Address 2',
+          'City',
+          'Post Code',
+          'State',
+          'Country',
+        ];
+      case 'FULL_NAME':
+        return ['First Name', 'Last Name'];
+      case 'LINKS':
+        return ['Link URL', 'Link Label'];
+      default:
+        return [];
+    }
+  };
+
+  const {
+    setFilterDefinitionUsedInDropdown,
+    setSelectedOperandInDropdown,
+    setObjectFilterDropdownSearchInput,
+  } = useFilterDropdown();
+
+  const handleFilterSelect = (definition: FilterDefinition | null) => {
+    if (definition !== null) {
+      setFilterDefinitionUsedInDropdown(definition);
+
+      setSelectedOperandInDropdown(
+        getOperandsForFilterType(definition.type)?.[0],
+      );
+
+      setObjectFilterDropdownSearchInput('');
+    }
+  };
+
+  return (
+    <>
+      <DropdownMenuHeader
+        StartIcon={IconChevronLeft}
+        onClick={() => {
+          setCurrentSubMenu(null);
+        }}
+      >
+        {getHeaderTitle(currentSubMenu)}
+      </DropdownMenuHeader>
+      <StyledInput
+        value={searchText}
+        autoFocus
+        placeholder="Search fields"
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          setSearchText(event.target.value)
+        }
+      />
+      <DropdownMenuItemsContainer>
+        <MenuItem
+          key={`select-filter-${-1}`}
+          testId={`select-filter-${-1}`}
+          onClick={() => {
+            handleFilterSelect(parent);
+          }}
+          LeftIcon={IconApps}
+          text={`Any ${getHeaderTitle(currentSubMenu)} field`}
+        />
+        {getMenuOptions(currentSubMenu)
+          .sort((a, b) => a.localeCompare(b))
+          .filter((item) =>
+            item.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()),
+          )
+          .map((menuOption, index) => (
+            <MenuItem
+              key={`select-filter-${index}`}
+              testId={`select-filter-${index}`}
+              onClick={() =>
+                parent &&
+                handleFilterSelect({
+                  ...parent,
+                  type: 'TEXT',
+                  label: menuOption,
+                })
+              }
+              text={menuOption}
+              LeftIcon={getIcon(parent?.iconName)}
+            />
+          ))}
+      </DropdownMenuItemsContainer>
+    </>
+  );
+};
+
 export const ObjectFilterDropdownFilterSelect = () => {
   const [searchText, setSearchText] = useState('');
+  const [currentSubMenu, setCurrentSubMenu] = useState<FilterType | null>(null);
+  const [currentParentFilterDefinition, setCurrentParentFilterDefinition] =
+    useState<FilterDefinition | null>(null);
   const {
     setFilterDefinitionUsedInDropdown,
     setSelectedOperandInDropdown,
@@ -54,49 +180,74 @@ export const ObjectFilterDropdownFilterSelect = () => {
 
   const setHotkeyScope = useSetHotkeyScope();
 
+  const handleSelectFilter = (availableFilterDefinition: FilterDefinition) => {
+    setFilterDefinitionUsedInDropdown(availableFilterDefinition);
+
+    if (
+      availableFilterDefinition.type === 'RELATION' ||
+      availableFilterDefinition.type === 'SELECT'
+    ) {
+      setHotkeyScope(RelationPickerHotkeyScope.RelationPicker);
+    }
+
+    setSelectedOperandInDropdown(
+      getOperandsForFilterType(availableFilterDefinition.type)?.[0],
+    );
+
+    setObjectFilterDropdownSearchInput('');
+  };
+
+  const isCompositeField = (type: FilterType) =>
+    ['ADDRESS', 'FULL_NAME', 'LINKS'].includes(type);
+
   return (
     <>
-      <StyledInput
-        value={searchText}
-        autoFocus
-        placeholder="Search fields"
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setSearchText(event.target.value)
-        }
-      />
-      <DropdownMenuItemsContainer>
-        {[...availableFilterDefinitions]
-          .sort((a, b) => a.label.localeCompare(b.label))
-          .filter((item) =>
-            item.label
-              .toLocaleLowerCase()
-              .includes(searchText.toLocaleLowerCase()),
-          )
-          .map((availableFilterDefinition, index) => (
-            <MenuItem
-              key={`select-filter-${index}`}
-              testId={`select-filter-${index}`}
-              onClick={() => {
-                setFilterDefinitionUsedInDropdown(availableFilterDefinition);
-
-                if (
-                  availableFilterDefinition.type === 'RELATION' ||
-                  availableFilterDefinition.type === 'SELECT'
-                ) {
-                  setHotkeyScope(RelationPickerHotkeyScope.RelationPicker);
-                }
-
-                setSelectedOperandInDropdown(
-                  getOperandsForFilterType(availableFilterDefinition.type)?.[0],
-                );
-
-                setObjectFilterDropdownSearchInput('');
-              }}
-              LeftIcon={getIcon(availableFilterDefinition.iconName)}
-              text={availableFilterDefinition.label}
-            />
-          ))}
-      </DropdownMenuItemsContainer>
+      {!currentSubMenu ? (
+        <>
+          <StyledInput
+            value={searchText}
+            autoFocus
+            placeholder="Search fields"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchText(event.target.value)
+            }
+          />
+          <DropdownMenuItemsContainer>
+            {[...availableFilterDefinitions]
+              .sort((a, b) => a.label.localeCompare(b.label))
+              .filter((item) =>
+                item.label
+                  .toLocaleLowerCase()
+                  .includes(searchText.toLocaleLowerCase()),
+              )
+              .map((availableFilterDefinition, index) => (
+                <MenuItem
+                  key={`select-filter-${index}`}
+                  testId={`select-filter-${index}`}
+                  onClick={() => {
+                    if (isCompositeField(availableFilterDefinition.type)) {
+                      setCurrentSubMenu(availableFilterDefinition.type);
+                      setCurrentParentFilterDefinition(
+                        availableFilterDefinition,
+                      );
+                    } else {
+                      handleSelectFilter(availableFilterDefinition);
+                    }
+                  }}
+                  LeftIcon={getIcon(availableFilterDefinition.iconName)}
+                  text={availableFilterDefinition.label}
+                  hasSubMenu={isCompositeField(availableFilterDefinition.type)}
+                />
+              ))}
+          </DropdownMenuItemsContainer>
+        </>
+      ) : (
+        <SubMenu
+          currentSubMenu={currentSubMenu}
+          parent={currentParentFilterDefinition}
+          setCurrentSubMenu={setCurrentSubMenu}
+        />
+      )}
     </>
   );
 };
