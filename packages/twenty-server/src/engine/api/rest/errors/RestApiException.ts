24b500fb3b164d778a8273e4fc454a19f05b1bf3
martmull <martmull@hotmail.fr>
2024-09-02 18:32:53 +0200

    Update error messages

--- packages/twenty-server/src/engine/api/rest/errors/RestApiException.ts
+++ packages/twenty-server/src/engine/api/rest/errors/RestApiException.ts
@@ -3,21 +3,35 @@ import { BadRequestException } from '@nestjs/common';
 import { BaseGraphQLError } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
 
 const formatMessage = (message: BaseGraphQLError) => {
-  if (message.extensions) {
-    return message.extensions.response.message || message.extensions.response;
+  let formattedMessage = message.extensions
+    ? message.extensions.response.message || message.extensions.response
+    : message.message;
+
+  formattedMessage = formattedMessage
+    .replace(/"/g, "'")
+    .replace("Variable '$data' got i", 'I');
+
+  const regex =
+    /Field '(.*?)' is not defined by type '(.*?)'\. Did you mean '(.*?)'\?/;
+
+  const match = formattedMessage.match(regex);
+
+  if (match) {
+    const field = match[1];
+    const type = match[2];
+    const suggestion = match[3];
+
+    formattedMessage = `Field '${field}' is not defined by type '${type}'. Did you mean '${suggestion}'?`;
   }
 
-  return message.message;
+  return formattedMessage;
 };
 
 export class RestApiException extends BadRequestException {
   constructor(errors: BaseGraphQLError[]) {
     super({
       statusCode: 400,
-      message:
-        errors.length === 1
-          ? formatMessage(errors[0])
-          : JSON.stringify(errors.map((error) => formatMessage(error))),
+      messages: errors.map((error) => formatMessage(error)),
       error: 'Bad Request',
     });
   }
