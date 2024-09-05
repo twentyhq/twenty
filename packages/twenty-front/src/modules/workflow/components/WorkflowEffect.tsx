ee9c463fecffd476d36bd20fb49f2f19783d6d26
Devessier <baptiste@devessier.fr>
2024-09-05 18:46:10 +0200

    feat: create a first workflow version if a workflow has no version yet

--- packages/twenty-front/src/modules/workflow/components/WorkflowEffect.tsx
+++ packages/twenty-front/src/modules/workflow/components/WorkflowEffect.tsx
@@ -1,9 +1,12 @@
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { useCreateOneRecord } from '@/object-record/hooks/useCreateOneRecord';
 import { useWorkflowWithCurrentVersion } from '@/workflow/hooks/useWorkflowWithCurrentVersion';
 import { workflowDiagramState } from '@/workflow/states/workflowDiagramState';
 import { workflowIdState } from '@/workflow/states/workflowIdState';
+import { WorkflowVersion } from '@/workflow/types/Workflow';
 import { addCreateStepNodes } from '@/workflow/utils/addCreateStepNodes';
 import { getWorkflowVersionDiagram } from '@/workflow/utils/getWorkflowVersionDiagram';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { isDefined } from 'twenty-ui';
 
@@ -17,14 +20,45 @@ export const WorkflowEffect = ({ workflowId }: WorkflowEffectProps) => {
   const setWorkflowId = useSetRecoilState(workflowIdState);
   const setWorkflowDiagram = useSetRecoilState(workflowDiagramState);
 
+  const [isCreatingInitialVersionState, setIsCreatingInitialVersionState] =
+    useState<boolean>(false);
+
+  const { createOneRecord: createOneWorkflowVersion } =
+    useCreateOneRecord<WorkflowVersion>({
+      objectNameSingular: CoreObjectNameSingular.WorkflowVersion,
+    });
+
   useEffect(() => {
     setWorkflowId(workflowId);
   }, [setWorkflowId, workflowId]);
 
   useEffect(() => {
-    const currentVersion = workflowWithCurrentVersion?.currentVersion;
+    setIsCreatingInitialVersionState(false);
+  }, [workflowId]);
+
+  useEffect(() => {
+    if (!isDefined(workflowWithCurrentVersion)) {
+      setWorkflowDiagram(undefined);
+
+      return;
+    }
+
+    const currentVersion = workflowWithCurrentVersion.currentVersion;
     if (!isDefined(currentVersion)) {
       setWorkflowDiagram(undefined);
+
+      if (isCreatingInitialVersionState) {
+        return;
+      }
+
+      setIsCreatingInitialVersionState(true);
+
+      createOneWorkflowVersion({
+        status: 'DRAFT',
+        workflowId,
+      }).finally(() => {
+        setIsCreatingInitialVersionState(false);
+      });
 
       return;
     }
@@ -34,7 +68,13 @@ export const WorkflowEffect = ({ workflowId }: WorkflowEffectProps) => {
       addCreateStepNodes(lastWorkflowDiagram);
 
     setWorkflowDiagram(workflowDiagramWithCreateStepNodes);
-  }, [setWorkflowDiagram, workflowWithCurrentVersion?.currentVersion]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    isCreatingInitialVersionState,
+    setWorkflowDiagram,
+    workflowId,
+    workflowWithCurrentVersion,
+  ]);
 
   return null;
 };
