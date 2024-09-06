cbc45d6616289c55ec34c86edc3e3ed9f9c6695b
gitstart-twenty <gitstart-twenty@users.noreply.github.com>
2024-09-06 16:03:47 +0000

    remove unused file and update zod schema

--- packages/twenty-front/src/modules/object-record/record-field/types/guards/isFieldArrayValue.ts
+++ packages/twenty-front/src/modules/object-record/record-field/types/guards/isFieldArrayValue.ts
@@ -1,11 +1,7 @@
 import { FieldArrayValue } from '@/object-record/record-field/types/FieldMetadata';
 import { z } from 'zod';
 
-export const arraySchema = z.union([
-  z.null(),
-  z.array(z.any()),
-  z.record(z.any()),
-]);
+export const arraySchema = z.union([z.null(), z.array(z.string())]);
 
 export const isFieldArrayValue = (
   fieldValue: unknown,
