2bff5cb9a525d11f1f99b59a33ef22a279f3ff45
Devessier <baptiste@devessier.fr>
2024-09-10 18:29:06 +0200

    feat: allow activating workflow version and refetch its data after mutation

--- packages/twenty-front/src/pages/object-record/RecordShowPage.tsx
+++ packages/twenty-front/src/pages/object-record/RecordShowPage.tsx
@@ -1,16 +1,20 @@
 import { useParams } from 'react-router-dom';
 
 import { TimelineActivityContext } from '@/activities/timelineActivities/contexts/TimelineActivityContext';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { RecordShowContainer } from '@/object-record/record-show/components/RecordShowContainer';
 import { useRecordShowPage } from '@/object-record/record-show/hooks/useRecordShowPage';
 import { RecordValueSetterEffect } from '@/object-record/record-store/components/RecordValueSetterEffect';
 import { RecordFieldValueSelectorContextProvider } from '@/object-record/record-store/contexts/RecordFieldValueSelectorContext';
+import { Button } from '@/ui/input/button/components/Button';
 import { PageBody } from '@/ui/layout/page/PageBody';
 import { PageContainer } from '@/ui/layout/page/PageContainer';
 import { PageFavoriteButton } from '@/ui/layout/page/PageFavoriteButton';
 import { ShowPageAddButton } from '@/ui/layout/show-page/components/ShowPageAddButton';
 import { ShowPageMoreButton } from '@/ui/layout/show-page/components/ShowPageMoreButton';
 import { PageTitle } from '@/ui/utilities/page-title/PageTitle';
+import { useActivateWorkflowVersion } from '@/workflow/hooks/useActivateWorkflowVersion';
+import { useWorkflowWithCurrentVersion } from '@/workflow/hooks/useWorkflowWithCurrentVersion';
 import { RecordShowPageHeader } from '~/pages/object-record/RecordShowPageHeader';
 
 export const RecordShowPage = () => {
@@ -35,6 +39,11 @@ export const RecordShowPage = () => {
     parameters.objectRecordId ?? '',
   );
 
+  const workflowWithCurrentVersion = useWorkflowWithCurrentVersion(
+    parameters.objectRecordId,
+  );
+  const { activateWorkflowVersion } = useActivateWorkflowVersion();
+
   return (
     <RecordFieldValueSelectorContextProvider>
       <RecordValueSetterEffect recordId={objectRecordId} />
@@ -46,6 +55,22 @@ export const RecordShowPage = () => {
           headerIcon={headerIcon}
         >
           <>
+            {objectNameSingular === CoreObjectNameSingular.Workflow ? (
+              <Button
+                title="Activate"
+                onClick={() => {
+                  console.log(
+                    'activate',
+                    workflowWithCurrentVersion?.currentVersion?.id,
+                  );
+
+                  return activateWorkflowVersion(
+                    workflowWithCurrentVersion?.currentVersion?.id,
+                  );
+                }}
+              />
+            ) : null}
+
             <PageFavoriteButton
               isFavorite={isFavorite}
               onClick={handleFavoriteButtonClick}
