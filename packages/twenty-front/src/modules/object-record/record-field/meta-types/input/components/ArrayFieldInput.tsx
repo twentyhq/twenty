3c395c83be85349436060abf51607929680161e4
Charles Bochet <charles@twenty.com>
2024-09-11 14:30:35 +0200

    Fix conflict

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/ArrayFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/ArrayFieldInput.tsx
@@ -2,6 +2,7 @@ import { useArrayField } from '@/object-record/record-field/meta-types/hooks/use
 import { ArrayFieldMenuItem } from '@/object-record/record-field/meta-types/input/components/ArrayFieldMenuItem';
 import { MultiItemFieldInput } from '@/object-record/record-field/meta-types/input/components/MultiItemFieldInput';
 import { useMemo } from 'react';
+import { FieldMetadataType } from '~/generated-metadata/graphql';
 
 type ArrayFieldInputProps = {
   onCancel?: () => void;
@@ -23,6 +24,7 @@ export const ArrayFieldInput = ({ onCancel }: ArrayFieldInputProps) => {
       onPersist={persistArrayField}
       onCancel={onCancel}
       placeholder="Enter value"
+      fieldMetadataType={FieldMetadataType.Array}
       renderItem={({ value, index, handleEdit, handleDelete }) => (
         <ArrayFieldMenuItem
           key={index}
