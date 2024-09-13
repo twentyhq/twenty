a0ed0912e8f45a10f2b6537b96f89bb084adbe2b
anamarn <ana.marn@uc.cl>
2024-09-13 11:45:16 +0200

    clean wrapper and illustration icon

--- packages/twenty-ui/src/display/icon/components/IllustrationIconCalendarEvent.tsx
+++ packages/twenty-ui/src/display/icon/components/IllustrationIconCalendarEvent.tsx
@@ -1,19 +1,8 @@
 import { useTheme } from '@emotion/react';
-
-import styled from '@emotion/styled';
 import IllustrationIconCalendarEventRaw from '@ui/display/icon/assets/illustration-calendar-event.svg?react';
+import { IllustrationIconWrapper } from '@ui/display/icon/components/IllustrationIconWrapper';
 import { IconComponentProps } from '@ui/display/icon/types/IconComponent';
 type IllustrationIconCalendarEventProps = Pick<IconComponentProps, 'size'>;
-
-const StyledTestRect = styled('div')`
-  align-items: center;
-  background-color: ${({ theme }) => theme.background.primary};
-  border: 0.75px solid ${({ theme }) => theme.border.color.medium};
-  border-radius: ${({ theme }) => theme.border.radius.sm};
-  display: flex;
-  justify-content: center;
-  size: auto;
-`;
 
 export const IllustrationIconCalendarEvent = (
   props: IllustrationIconCalendarEventProps,
@@ -22,13 +11,13 @@ export const IllustrationIconCalendarEvent = (
   const size = props.size ?? theme.icon.size.lg;
   const { color, fill } = theme.IllustrationIcon;
   return (
-    <StyledTestRect>
+    <IllustrationIconWrapper>
       <IllustrationIconCalendarEventRaw
         fill={fill}
         color={color}
         height={size}
         width={size}
       />
-    </StyledTestRect>
+    </IllustrationIconWrapper>
   );
 };
--- packages/twenty-ui/src/display/icon/components/IllustrationIconWrapper.tsx
+++ packages/twenty-ui/src/display/icon/components/IllustrationIconWrapper.tsx
@@ -2,11 +2,12 @@ import styled from '@emotion/styled';
 
 const StyledRectangleIllustrationIcon = styled('div')`
   background-color: ${({ theme }) => theme.background.primary};
-  border: 0.75px solid ${({ theme }) => theme.border.color.medium};
+  border: solid ${({ theme }) => theme.border.color.medium};
   border-radius: ${({ theme }) => theme.border.radius.sm};
   display: flex;
   justify-content: center;
   size: auto;
+  box-sizing: content-box;
 `;
 
 export const IllustrationIconWrapper = StyledRectangleIllustrationIcon;
