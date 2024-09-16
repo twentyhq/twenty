dcac8326167732af723a9e6c4e34b9fb28061b7c
Marie <51697796+ijreilly@users.noreply.github.com>
2024-09-16 19:35:13 +0200

    [Emails migration] Fix email field migration (#7065)
    Fix email field migration
    
    - Remove deprecated field of type Email
    - Add standard emails field on person to person views in position 4

--- packages/twenty-server/src/database/commands/upgrade-version/0-30/0-30-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-30/0-30-upgrade-version.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { FixEmailFieldsToEmailsCommand } from 'src/database/commands/upgrade-version/0-30/0-30-fix-email-field-migration.command';
 import { MigrateEmailFieldsToEmailsCommand } from 'src/database/commands/upgrade-version/0-30/0-30-migrate-email-fields-to-emails.command';
 import { SetStaleMessageSyncBackToPendingCommand } from 'src/database/commands/upgrade-version/0-30/0-30-set-stale-message-sync-back-to-pending';
 import { UpgradeTo0_30Command } from 'src/database/commands/upgrade-version/0-30/0-30-upgrade-version.command';
@@ -32,6 +33,7 @@ import { ViewModule } from 'src/modules/view/view.module';
     UpgradeTo0_30Command,
     MigrateEmailFieldsToEmailsCommand,
     SetStaleMessageSyncBackToPendingCommand,
+    FixEmailFieldsToEmailsCommand,
   ],
 })
 export class UpgradeTo0_30CommandModule {}
