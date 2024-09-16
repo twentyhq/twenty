49ea1e7e2dca4121b52b7c5c265e5bf22b4b0ab4
bosiraphael <raphael.bosi@gmail.com>
2024-09-16 16:58:08 +0200

    fixes

--- packages/twenty-front/src/modules/ui/layout/page/PageHeader.tsx
+++ packages/twenty-front/src/modules/ui/layout/page/PageHeader.tsx
@@ -30,8 +30,8 @@ const StyledTopBarContainer = styled.div<{ width?: number }>`
   padding: ${({ theme }) => theme.spacing(2)};
   padding-left: 0;
   padding-right: ${({ theme }) => theme.spacing(3)};
-  width: ${({ width }) => width + 'px' || '100%'};
   z-index: 20;
+  width: ${({ width }) => width + 'px' || '100%'};
 
   @media (max-width: ${MOBILE_VIEWPORT}px) {
     width: 100%;
