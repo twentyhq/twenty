c6c062e4d20132606d275574727ac9b9698a3e7d
Weiko <corentin@twenty.com>
2024-09-17 19:20:48 +0200

    add trigger webhook + emit events

--- packages/twenty-server/src/engine/api/graphql/graphql-query-runner/graphql-query-runner.service.ts
+++ packages/twenty-server/src/engine/api/graphql/graphql-query-runner/graphql-query-runner.service.ts
@@ -14,20 +14,32 @@ import {
   FindOneResolverArgs,
   ResolverArgsType,
 } from 'src/engine/api/graphql/workspace-resolver-builder/interfaces/workspace-resolvers-builder.interface';
+import { ObjectMetadataInterface } from 'src/engine/metadata-modules/field-metadata/interfaces/object-metadata.interface';
 
 import { GraphqlQueryCreateManyResolverService } from 'src/engine/api/graphql/graphql-query-runner/resolvers/graphql-query-create-many-resolver.service';
 import { GraphqlQueryFindManyResolverService } from 'src/engine/api/graphql/graphql-query-runner/resolvers/graphql-query-find-many-resolver.service';
 import { GraphqlQueryFindOneResolverService } from 'src/engine/api/graphql/graphql-query-runner/resolvers/graphql-query-find-one-resolver.service';
 import { QueryRunnerArgsFactory } from 'src/engine/api/graphql/workspace-query-runner/factories/query-runner-args.factory';
+import {
+  CallWebhookJobsJob,
+  CallWebhookJobsJobData,
+  CallWebhookJobsJobOperation,
+} from 'src/engine/api/graphql/workspace-query-runner/jobs/call-webhook-jobs.job';
 import { assertIsValidUuid } from 'src/engine/api/graphql/workspace-query-runner/utils/assert-is-valid-uuid.util';
 import { WorkspaceQueryHookService } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-hook/workspace-query-hook.service';
 import {
   WorkspaceQueryRunnerException,
   WorkspaceQueryRunnerExceptionCode,
 } from 'src/engine/api/graphql/workspace-query-runner/workspace-query-runner.exception';
+import { AuthContext } from 'src/engine/core-modules/auth/types/auth-context.type';
+import { ObjectRecordCreateEvent } from 'src/engine/core-modules/event-emitter/types/object-record-create.event';
+import { InjectMessageQueue } from 'src/engine/core-modules/message-queue/decorators/message-queue.decorator';
+import { MessageQueue } from 'src/engine/core-modules/message-queue/message-queue.constants';
+import { MessageQueueService } from 'src/engine/core-modules/message-queue/services/message-queue.service';
 import { LogExecutionTime } from 'src/engine/decorators/observability/log-execution-time.decorator';
 import { assertMutationNotOnRemoteObject } from 'src/engine/metadata-modules/object-metadata/utils/assert-mutation-not-on-remote-object.util';
 import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
+import { WorkspaceEventEmitter } from 'src/engine/workspace-event-emitter/workspace-event-emitter';
 
 @Injectable()
 export class GraphqlQueryRunnerService {
@@ -35,6 +47,9 @@ export class GraphqlQueryRunnerService {
     private readonly twentyORMGlobalManager: TwentyORMGlobalManager,
     private readonly workspaceQueryHookService: WorkspaceQueryHookService,
     private readonly queryRunnerArgsFactory: QueryRunnerArgsFactory,
+    private readonly workspaceEventEmitter: WorkspaceEventEmitter,
+    @InjectMessageQueue(MessageQueue.webhookQueue)
+    private readonly messageQueueService: MessageQueueService,
   ) {}
 
   @LogExecutionTime()
@@ -140,9 +155,21 @@ export class GraphqlQueryRunnerService {
       ResolverArgsType.CreateMany,
     )) as CreateManyResolverArgs<ObjectRecord>;
 
-    const results = await graphqlQueryCreateManyResolverService.createMany(
+    const results = (await graphqlQueryCreateManyResolverService.createMany(
       computedArgs,
       options,
+    )) as ObjectRecord[];
+
+    await this.triggerWebhooks<ObjectRecord>(
+      results,
+      CallWebhookJobsJobOperation.create,
+      options,
+    );
+
+    this.emitCreateEvents<ObjectRecord>(
+      results,
+      authContext,
+      objectMetadataItem,
     );
 
     return results?.[0] as ObjectRecord;
@@ -180,11 +207,74 @@ export class GraphqlQueryRunnerService {
       ResolverArgsType.CreateMany,
     )) as CreateManyResolverArgs<ObjectRecord>;
 
-    const results = await graphqlQueryCreateManyResolverService.createMany(
+    const results = (await graphqlQueryCreateManyResolverService.createMany(
       computedArgs,
+      options,
+    )) as ObjectRecord[];
+
+    await this.workspaceQueryHookService.executePostQueryHooks(
+      authContext,
+      objectMetadataItem.nameSingular,
+      'createMany',
+      results,
+    );
+
+    await this.triggerWebhooks<ObjectRecord>(
+      results,
+      CallWebhookJobsJobOperation.create,
       options,
     );
 
-    return results as ObjectRecord[];
+    this.emitCreateEvents<ObjectRecord>(
+      results,
+      authContext,
+      objectMetadataItem,
+    );
+
+    return results;
+  }
+
+  private emitCreateEvents<BaseRecord extends IRecord = IRecord>(
+    records: BaseRecord[],
+    authContext: AuthContext,
+    objectMetadataItem: ObjectMetadataInterface,
+  ) {
+    this.workspaceEventEmitter.emit(
+      `${objectMetadataItem.nameSingular}.created`,
+      records.map(
+        (record) =>
+          ({
+            userId: authContext.user?.id,
+            recordId: record.id,
+            objectMetadata: objectMetadataItem,
+            properties: {
+              after: record,
+            },
+          }) satisfies ObjectRecordCreateEvent<any>,
+      ),
+      authContext.workspace.id,
+    );
+  }
+
+  private async triggerWebhooks<Record>(
+    jobsData: Record[] | undefined,
+    operation: CallWebhookJobsJobOperation,
+    options: WorkspaceQueryRunnerOptions,
+  ) {
+    if (!Array.isArray(jobsData)) {
+      return;
+    }
+    jobsData.forEach((jobData) => {
+      this.messageQueueService.add<CallWebhookJobsJobData>(
+        CallWebhookJobsJob.name,
+        {
+          record: jobData,
+          workspaceId: options.authContext.workspace.id,
+          operation,
+          objectMetadataItem: options.objectMetadataItem,
+        },
+        { retryLimit: 3 },
+      );
+    });
   }
 }
