8f2b0055e5ddeec3eee7765c97db8eb2f1dd9409
Charles Bochet <charles@twenty.com>
2024-09-18 23:16:43 +0200

    Fix tests

--- packages/twenty-server/src/engine/core-modules/workspace-invitation/services/workspace-invitation.service.spec.ts
+++ packages/twenty-server/src/engine/core-modules/workspace-invitation/services/workspace-invitation.service.spec.ts
@@ -2,8 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
 import { AppToken } from 'src/engine/core-modules/app-token/app-token.entity';
-import { EnvironmentService } from 'src/engine/core-modules/environment/environment.service';
+import { TokenService } from 'src/engine/core-modules/auth/token/services/token.service';
 import { EmailService } from 'src/engine/core-modules/email/email.service';
+import { EnvironmentService } from 'src/engine/core-modules/environment/environment.service';
+import { OnboardingService } from 'src/engine/core-modules/onboarding/onboarding.service';
+import { UserWorkspace } from 'src/engine/core-modules/user-workspace/user-workspace.entity';
 
 import { WorkspaceInvitationService } from './workspace-invitation.service';
 
@@ -24,6 +27,18 @@ describe('WorkspaceInvitationService', () => {
         },
         {
           provide: EmailService,
+          useValue: {},
+        },
+        {
+          provide: TokenService,
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(UserWorkspace, 'core'),
+          useValue: {},
+        },
+        {
+          provide: OnboardingService,
           useValue: {},
         },
       ],
