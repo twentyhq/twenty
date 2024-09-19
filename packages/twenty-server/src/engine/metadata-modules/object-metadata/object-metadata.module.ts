8964d26d5bc3f07e68dff63a01948a58a006ed13
Thomas Trompette <thomas.trompette@sfr.fr>
2024-09-19 18:13:07 +0200

    Clean views without object metadata (#7153)
    Add command for cleaning + clean on object deletion

--- packages/twenty-server/src/engine/metadata-modules/object-metadata/object-metadata.module.ts
+++ packages/twenty-server/src/engine/metadata-modules/object-metadata/object-metadata.module.ts
@@ -10,7 +10,6 @@ import { NestjsQueryTypeOrmModule } from '@ptc-org/nestjs-query-typeorm';
 
 import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 import { FeatureFlagEntity } from 'src/engine/core-modules/feature-flag/feature-flag.entity';
-import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-flag.module';
 import { WorkspaceAuthGuard } from 'src/engine/guards/workspace-auth.guard';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
@@ -44,7 +43,6 @@ import { UpdateObjectPayload } from './dtos/update-object.input';
         WorkspaceMigrationModule,
         WorkspaceMigrationRunnerModule,
         WorkspaceMetadataVersionModule,
-        FeatureFlagModule,
         RemoteTableRelationsModule,
       ],
       services: [ObjectMetadataService],
