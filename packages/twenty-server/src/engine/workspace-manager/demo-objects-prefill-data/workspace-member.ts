75d701607dc2e8971f62467165a29d21b4d3f46b
Félix Malfait <felix@twenty.com>
2024-09-20 13:30:22 +0200

    Change demo account email

--- packages/twenty-server/src/engine/workspace-manager/demo-objects-prefill-data/workspace-member.ts
+++ packages/twenty-server/src/engine/workspace-manager/demo-objects-prefill-data/workspace-member.ts
@@ -5,7 +5,7 @@ import { DEMO_SEED_USER_IDS } from 'src/database/typeorm-seeds/core/demo/users';
 export const DEMO_SEED_WORKSPACE_MEMBER_IDS = {
   NOAH: '20202020-0687-4c41-b707-ed1bfca972a2',
   HUGO: '20202020-77d5-4cb6-b60a-f4a835a85d62',
-  JULIA: '20202020-1553-45c6-a028-5a9064cce07e',
+  TIM: '20202020-1553-45c6-a028-5a9064cce07e',
 };
 
 export const workspaceMemberPrefillData = async (
@@ -42,12 +42,12 @@ export const workspaceMemberPrefillData = async (
         userId: DEMO_SEED_USER_IDS.HUGO,
       },
       {
-        id: DEMO_SEED_WORKSPACE_MEMBER_IDS.JULIA,
-        nameFirstName: 'Julia',
-        nameLastName: 'S',
+        id: DEMO_SEED_WORKSPACE_MEMBER_IDS.TIM,
+        nameFirstName: 'Tim',
+        nameLastName: 'Apple',
         locale: 'en',
         colorScheme: 'Light',
-        userId: DEMO_SEED_USER_IDS.JULIA,
+        userId: DEMO_SEED_USER_IDS.TIM,
       },
     ])
     .execute();
