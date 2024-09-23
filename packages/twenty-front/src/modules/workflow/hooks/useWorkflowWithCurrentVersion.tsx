883e6359281ec7773827eeebf7bf527260b24c3b
Devessier <baptiste@devessier.fr>
2024-09-23 18:16:05 +0200

    feat: don't fetch all versions data when we only need the count of workflow versions

--- packages/twenty-front/src/modules/workflow/hooks/useWorkflowWithCurrentVersion.tsx
+++ packages/twenty-front/src/modules/workflow/hooks/useWorkflowWithCurrentVersion.tsx
@@ -19,7 +19,9 @@ export const useWorkflowWithCurrentVersion = (
       id: true,
       name: true,
       statuses: true,
-      versions: true,
+      versions: {
+        totalCount: true,
+      },
     },
     skip: !isDefined(workflowId),
   });
