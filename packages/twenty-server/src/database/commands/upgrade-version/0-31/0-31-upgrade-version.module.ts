bd8e3c4b8bac3dee86ee8d0cf5af3ce0cec76d29
Thomas Trompette <thomas.trompette@sfr.fr>
2024-09-24 18:42:07 +0200

    Add index key to all tasks and all notes views

--- packages/twenty-server/src/database/commands/upgrade-version/0-31/0-31-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-31/0-31-upgrade-version.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddIndexKeyToTasksAndNotesViewsCommand } from 'src/database/commands/upgrade-version/0-31/0-31-add-index-key-to-tasks-and-notes-views.command';
 
 import { BackfillWorkspaceFavoritesCommand } from 'src/database/commands/upgrade-version/0-31/0-31-backfill-workspace-favorites.command';
 import { CleanViewsWithDeletedObjectMetadataCommand } from 'src/database/commands/upgrade-version/0-31/0-31-clean-views-with-deleted-object-metadata.command';
@@ -18,6 +19,7 @@ import { WorkspaceSyncMetadataCommandsModule } from 'src/engine/workspace-manage
     UpgradeTo0_31Command,
     BackfillWorkspaceFavoritesCommand,
     CleanViewsWithDeletedObjectMetadataCommand,
+    AddIndexKeyToTasksAndNotesViewsCommand,
   ],
 })
 export class UpgradeTo0_31CommandModule {}
