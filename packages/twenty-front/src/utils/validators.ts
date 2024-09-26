8c9ca8d2c6c6d4ff513eb42f49d0457c3e47613e
sid0-0 <a@b.com>
2024-09-26 23:07:54 +0530

    using zod validator instead of regex

--- packages/twenty-front/src/utils/validators.ts
+++ packages/twenty-front/src/utils/validators.ts
@@ -1,6 +1,6 @@
+import { z } from 'zod';
+
 export const VALIDATORS = {
   EMAIL: (valueToValidate: string) =>
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      valueToValidate.toLowerCase(),
-    ),
+    z.string().email().safeParse(valueToValidate).success,
 };
