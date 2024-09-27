ca906bbf6b83af751406a3d960efcd4e5a83fb27
nitin <142569587+ehconitin@users.noreply.github.com>
2024-09-27 17:50:21 +0530

    5922 - UI Overlap and State Persistence in Filter Menus (#7270)
    fixes #5922 
    
    
    https://github.com/user-attachments/assets/07d088da-cefb-4d87-9016-e14cef18567d

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/MultipleFiltersDropdownContent.tsx
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/MultipleFiltersDropdownContent.tsx
@@ -1,11 +1,9 @@
-import { useRecoilValue } from 'recoil';
-
+import { ObjectFilterDropdownRatingInput } from '@/object-record/object-filter-dropdown/components/ObjectFilterDropdownRatingInput';
 import { ObjectFilterDropdownSearchInput } from '@/object-record/object-filter-dropdown/components/ObjectFilterDropdownSearchInput';
 import { useFilterDropdown } from '@/object-record/object-filter-dropdown/hooks/useFilterDropdown';
-import { DropdownMenuSeparator } from '@/ui/layout/dropdown/components/DropdownMenuSeparator';
 import { ViewFilterOperand } from '@/views/types/ViewFilterOperand';
-
-import { ObjectFilterDropdownRatingInput } from '@/object-record/object-filter-dropdown/components/ObjectFilterDropdownRatingInput';
+import styled from '@emotion/styled';
+import { useRecoilValue } from 'recoil';
 import { MultipleFiltersDropdownFilterOnFilterChangedEffect } from './MultipleFiltersDropdownFilterOnFilterChangedEffect';
 import { ObjectFilterDropdownDateInput } from './ObjectFilterDropdownDateInput';
 import { ObjectFilterDropdownFilterSelect } from './ObjectFilterDropdownFilterSelect';
@@ -16,6 +14,21 @@ import { ObjectFilterDropdownOptionSelect } from './ObjectFilterDropdownOptionSe
 import { ObjectFilterDropdownRecordSelect } from './ObjectFilterDropdownRecordSelect';
 import { ObjectFilterDropdownTextSearchInput } from './ObjectFilterDropdownTextSearchInput';
 
+const StyledContainer = styled.div`
+  position: relative;
+`;
+
+const StyledOperandSelectContainer = styled.div`
+  background: ${({ theme }) => theme.background.secondary};
+  box-shadow: ${({ theme }) => theme.boxShadow.light};
+  border-radius: ${({ theme }) => theme.border.radius.md};
+  left: 10px;
+  position: absolute;
+  top: 10px;
+  width: 100%;
+  z-index: 1000;
+`;
+
 type MultipleFiltersDropdownContentProps = {
   filterDropdownId?: string;
 };
@@ -24,20 +37,23 @@ export const MultipleFiltersDropdownContent = ({
   filterDropdownId,
 }: MultipleFiltersDropdownContentProps) => {
   const {
-    isObjectFilterDropdownOperandSelectUnfoldedState,
     filterDefinitionUsedInDropdownState,
     selectedOperandInDropdownState,
+    isObjectFilterDropdownOperandSelectUnfoldedState,
   } = useFilterDropdown({ filterDropdownId });
 
   const isObjectFilterDropdownOperandSelectUnfolded = useRecoilValue(
     isObjectFilterDropdownOperandSelectUnfoldedState,
   );
+
   const filterDefinitionUsedInDropdown = useRecoilValue(
     filterDefinitionUsedInDropdownState,
   );
+
   const selectedOperandInDropdown = useRecoilValue(
     selectedOperandInDropdownState,
   );
+
   const isEmptyOperand =
     selectedOperandInDropdown &&
     [ViewFilterOperand.IsEmpty, ViewFilterOperand.IsNotEmpty].includes(
@@ -45,64 +61,64 @@ export const MultipleFiltersDropdownContent = ({
     );
 
   return (
-    <>
+    <StyledContainer>
       {!filterDefinitionUsedInDropdown ? (
         <ObjectFilterDropdownFilterSelect />
-      ) : isObjectFilterDropdownOperandSelectUnfolded ? (
-        <ObjectFilterDropdownOperandSelect />
-      ) : isEmptyOperand ? (
-        <ObjectFilterDropdownOperandButton />
       ) : (
-        selectedOperandInDropdown && (
-          <>
-            <ObjectFilterDropdownOperandButton />
-            <DropdownMenuSeparator />
-            {[
-              'TEXT',
-              'EMAIL',
-              'EMAILS',
-              'PHONE',
-              'FULL_NAME',
-              'LINK',
-              'LINKS',
-              'ADDRESS',
-              'ACTOR',
-              'ARRAY',
-              'PHONES',
-            ].includes(filterDefinitionUsedInDropdown.type) && (
-              <ObjectFilterDropdownTextSearchInput />
-            )}
-            {['NUMBER', 'CURRENCY'].includes(
-              filterDefinitionUsedInDropdown.type,
-            ) && <ObjectFilterDropdownNumberInput />}
-            {filterDefinitionUsedInDropdown.type === 'RATING' && (
-              <ObjectFilterDropdownRatingInput />
-            )}
-            {['DATE_TIME', 'DATE'].includes(
-              filterDefinitionUsedInDropdown.type,
-            ) && <ObjectFilterDropdownDateInput />}
-            {filterDefinitionUsedInDropdown.type === 'RELATION' && (
-              <>
-                <ObjectFilterDropdownSearchInput />
-                <DropdownMenuSeparator />
-                <ObjectFilterDropdownRecordSelect />
-              </>
-            )}
-            {filterDefinitionUsedInDropdown.type === 'SELECT' && (
-              <>
-                <ObjectFilterDropdownSearchInput />
-                <DropdownMenuSeparator />
-                <ObjectFilterDropdownOptionSelect />
-              </>
-            )}
-          </>
-        )
+        <>
+          <ObjectFilterDropdownOperandButton />
+          {isObjectFilterDropdownOperandSelectUnfolded && (
+            <StyledOperandSelectContainer>
+              <ObjectFilterDropdownOperandSelect />
+            </StyledOperandSelectContainer>
+          )}
+          {!isEmptyOperand && selectedOperandInDropdown && (
+            <>
+              {[
+                'TEXT',
+                'EMAIL',
+                'EMAILS',
+                'PHONE',
+                'FULL_NAME',
+                'LINK',
+                'LINKS',
+                'ADDRESS',
+                'ACTOR',
+                'ARRAY',
+                'PHONES',
+              ].includes(filterDefinitionUsedInDropdown.type) && (
+                <ObjectFilterDropdownTextSearchInput />
+              )}
+              {['NUMBER', 'CURRENCY'].includes(
+                filterDefinitionUsedInDropdown.type,
+              ) && <ObjectFilterDropdownNumberInput />}
+              {filterDefinitionUsedInDropdown.type === 'RATING' && (
+                <ObjectFilterDropdownRatingInput />
+              )}
+              {['DATE_TIME', 'DATE'].includes(
+                filterDefinitionUsedInDropdown.type,
+              ) && <ObjectFilterDropdownDateInput />}
+              {filterDefinitionUsedInDropdown.type === 'RELATION' && (
+                <>
+                  <ObjectFilterDropdownSearchInput />
+                  <ObjectFilterDropdownRecordSelect />
+                </>
+              )}
+              {filterDefinitionUsedInDropdown.type === 'SELECT' && (
+                <>
+                  <ObjectFilterDropdownSearchInput />
+                  <ObjectFilterDropdownOptionSelect />
+                </>
+              )}
+            </>
+          )}
+        </>
       )}
       <MultipleFiltersDropdownFilterOnFilterChangedEffect
         filterDefinitionUsedInDropdownType={
           filterDefinitionUsedInDropdown?.type
         }
       />
-    </>
+    </StyledContainer>
   );
 };
