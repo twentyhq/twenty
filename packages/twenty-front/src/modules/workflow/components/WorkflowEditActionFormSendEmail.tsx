b0b39f569c8ee4d55fcea227a42b782e5ef34a04
Devessier <baptiste@devessier.fr>
2024-09-27 17:13:37 +0200

    feat: be sure the settings are saved when exiting the drawer

--- packages/twenty-front/src/modules/workflow/components/WorkflowEditActionFormSendEmail.tsx
+++ packages/twenty-front/src/modules/workflow/components/WorkflowEditActionFormSendEmail.tsx
@@ -54,6 +54,12 @@ export const WorkflowEditActionFormSendEmail = ({
     });
   }, 1_000);
 
+  useEffect(() => {
+    return () => {
+      saveAction.flush();
+    };
+  }, [saveAction]);
+
   const handleSave = form.handleSubmit(saveAction);
 
   return (
