a3b0a3f81ad96662f9d19f8f179ec994652c089c
Marie Stoppa <marie.stoppa@essec.edu>
2024-09-27 13:57:17 +0200

    Fix standardId issues with phones field migration

--- packages/twenty-server/src/database/commands/upgrade-version/0-30/0-30-migrate-phone-fields-to-phones.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-30/0-30-migrate-phone-fields-to-phones.command.ts
@@ -168,6 +168,12 @@ export class MigratePhoneFieldsToPhonesCommand extends ActiveWorkspacesCommandRu
           name: 'phones',
         } satisfies CreateFieldInput);
 
+        // StandardId is not exposed in CreateFieldInput
+        await this.metadataDataSource.query(
+          `UPDATE "metadata"."fieldMetadata" SET "standardId" = $1 where "id"=$2`,
+          [PERSON_STANDARD_FIELD_IDS.phones, standardPersonPhoneField.id],
+        );
+
         await this.viewService.removeFieldFromViews({
           workspaceId: workspaceId,
           fieldId: standardPersonPhonesField.id,
