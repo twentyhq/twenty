8ad4774194d1ed7d1e7716078b4130619202bf8c
Félix Malfait <felix@twenty.com>
2024-09-27 15:31:12 +0200

    Fix merge

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/MultipleFiltersDropdownContent.tsx
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/MultipleFiltersDropdownContent.tsx
@@ -81,7 +81,7 @@ export const MultipleFiltersDropdownContent = ({
               <ObjectFilterDropdownOperandSelect />
             </StyledOperandSelectContainer>
           )}
-          {!isConfigurable && selectedOperandInDropdown && (
+          {isConfigurable && selectedOperandInDropdown && (
             <>
               {[
                 'TEXT',
