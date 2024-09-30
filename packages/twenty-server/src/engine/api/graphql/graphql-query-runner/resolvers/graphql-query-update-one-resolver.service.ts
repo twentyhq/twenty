03dfd374e15c0f98de6629b54e48bea09edb3f1c
Weiko <corentin@twenty.com>
2024-09-30 10:57:04 +0200

    fix update

--- packages/twenty-server/src/engine/api/graphql/graphql-query-runner/resolvers/graphql-query-update-one-resolver.service.ts
+++ packages/twenty-server/src/engine/api/graphql/graphql-query-runner/resolvers/graphql-query-update-one-resolver.service.ts
@@ -1,5 +1,3 @@
-import graphqlFields from 'graphql-fields';
-
 import { ResolverService } from 'src/engine/api/graphql/graphql-query-runner/interfaces/resolver-service.interface';
 import { Record as IRecord } from 'src/engine/api/graphql/workspace-query-builder/interfaces/record.interface';
 import { WorkspaceQueryRunnerOptions } from 'src/engine/api/graphql/workspace-query-runner/interfaces/query-runner-option.interface';
@@ -9,9 +7,7 @@ import {
   GraphqlQueryRunnerException,
   GraphqlQueryRunnerExceptionCode,
 } from 'src/engine/api/graphql/graphql-query-runner/errors/graphql-query-runner.exception';
-import { GraphqlQueryParser } from 'src/engine/api/graphql/graphql-query-runner/graphql-query-parsers/graphql-query.parser';
 import { ObjectRecordsToGraphqlConnectionMapper } from 'src/engine/api/graphql/graphql-query-runner/orm-mappers/object-records-to-graphql-connection.mapper';
-import { getObjectMetadataOrThrow } from 'src/engine/api/graphql/graphql-query-runner/utils/get-object-metadata-or-throw.util';
 import { assertIsValidUuid } from 'src/engine/api/graphql/workspace-query-runner/utils/assert-is-valid-uuid.util';
 import { assertMutationNotOnRemoteObject } from 'src/engine/metadata-modules/object-metadata/utils/assert-mutation-not-on-remote-object.util';
 import { generateObjectMetadataMap } from 'src/engine/metadata-modules/utils/generate-object-metadata-map.util';
@@ -30,7 +26,7 @@ export class GraphqlQueryUpdateOneResolverService
     args: UpdateOneResolverArgs<Partial<ObjectRecord>>,
     options: WorkspaceQueryRunnerOptions,
   ): Promise<ObjectRecord> {
-    const { authContext, objectMetadataItem, objectMetadataCollection, info } =
+    const { authContext, objectMetadataItem, objectMetadataCollection } =
       options;
     const repository =
       await this.twentyORMGlobalManager.getRepositoryForWorkspace(
@@ -40,21 +36,6 @@ export class GraphqlQueryUpdateOneResolverService
 
     const objectMetadataMap = generateObjectMetadataMap(
       objectMetadataCollection,
-    );
-    const objectMetadata = getObjectMetadataOrThrow(
-      objectMetadataMap,
-      objectMetadataItem.nameSingular,
-    );
-    const graphqlQueryParser = new GraphqlQueryParser(
-      objectMetadata.fields,
-      objectMetadataMap,
-    );
-
-    const selectedFields = graphqlFields(info);
-
-    const { select, relations } = graphqlQueryParser.parseSelectedFields(
-      objectMetadataItem,
-      selectedFields,
     );
 
     const queryBuilder = repository.createQueryBuilder(
