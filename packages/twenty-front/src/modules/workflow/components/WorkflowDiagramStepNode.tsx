35361093bf4e151d1009a3775a99d3d81cebeb16
Baptiste Devessier <baptiste@devessier.fr>
2024-10-01 18:14:54 +0200

    Delete workflow step (#7373)
    - Allows the deletion of triggers and steps in workflows. If the
    workflow can not be edited right now, we create a new draft version.
    - The workflow right drawer can now render nothing. It's necessary to
    behave that way because a deleted step will still be displayed for a
    short amount of time in the drawer. The drawer will be filled with blank
    content when it disappears.
    
    
    https://github.com/user-attachments/assets/abd5184e-d3db-4fe7-8870-ccc78ff23d41
    
    Closes #7057

--- packages/twenty-front/src/modules/workflow/components/WorkflowDiagramStepNode.tsx
+++ packages/twenty-front/src/modules/workflow/components/WorkflowDiagramStepNode.tsx
@@ -1,9 +1,15 @@
+import { FloatingIconButton } from '@/ui/input/button/components/FloatingIconButton';
 import { WorkflowDiagramBaseStepNode } from '@/workflow/components/WorkflowDiagramBaseStepNode';
+import { useDeleteOneStep } from '@/workflow/hooks/useDeleteOneStep';
+import { useWorkflowWithCurrentVersion } from '@/workflow/hooks/useWorkflowWithCurrentVersion';
+import { workflowIdState } from '@/workflow/states/workflowIdState';
 import { WorkflowDiagramStepNodeData } from '@/workflow/types/WorkflowDiagram';
 import { assertUnreachable } from '@/workflow/utils/assertUnreachable';
+import { assertWorkflowWithCurrentVersionIsDefined } from '@/workflow/utils/assertWorkflowWithCurrentVersionIsDefined';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
-import { IconCode, IconMail, IconPlaylistAdd } from 'twenty-ui';
+import { useRecoilValue } from 'recoil';
+import { IconCode, IconMail, IconPlaylistAdd, IconTrash } from 'twenty-ui';
 
 const StyledStepNodeLabelIconContainer = styled.div`
   align-items: center;
@@ -15,11 +21,25 @@ const StyledStepNodeLabelIconContainer = styled.div`
 `;
 
 export const WorkflowDiagramStepNode = ({
+  id,
   data,
+  selected,
 }: {
+  id: string;
   data: WorkflowDiagramStepNodeData;
+  selected?: boolean;
 }) => {
   const theme = useTheme();
+
+  const workflowId = useRecoilValue(workflowIdState);
+
+  const workflowWithCurrentVersion = useWorkflowWithCurrentVersion(workflowId);
+  assertWorkflowWithCurrentVersionIsDefined(workflowWithCurrentVersion);
+
+  const { deleteOneStep } = useDeleteOneStep({
+    workflow: workflowWithCurrentVersion,
+    stepId: id,
+  });
 
   const renderStepIcon = () => {
     switch (data.nodeType) {
@@ -67,6 +87,16 @@ export const WorkflowDiagramStepNode = ({
       nodeType={data.nodeType}
       label={data.label}
       Icon={renderStepIcon()}
+      RightFloatingElement={
+        selected ? (
+          <FloatingIconButton
+            Icon={IconTrash}
+            onClick={() => {
+              return deleteOneStep();
+            }}
+          />
+        ) : undefined
+      }
     />
   );
 };
