564d03d6f2b94182626f67f6271f4bc085def590
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-10-02 15:18:01 +0530

    fix: having twenty logo

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/ActorFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/ActorFieldDisplay.tsx
@@ -1,5 +1,6 @@
 import { useActorFieldDisplay } from '@/object-record/record-field/meta-types/hooks/useActorFieldDisplay';
 import { ActorDisplay } from '@/ui/field/display/components/ActorDisplay';
+import { DEFAULT_WORKSPACE_LOGO } from '@/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo';
 import { isNonEmptyString } from '@sniptt/guards';
 
 export const ActorFieldDisplay = () => {
@@ -18,7 +19,11 @@ export const ActorFieldDisplay = () => {
     <ActorDisplay
       name={name}
       source={fieldValue.source}
-      avatarUrl={fieldValue.workspaceMember?.avatarUrl}
+      avatarUrl={
+        fieldValue.workspaceMemberId
+          ? fieldValue.workspaceMember?.avatarUrl
+          : DEFAULT_WORKSPACE_LOGO
+      }
       workspaceMemberId={fieldValue.workspaceMemberId}
     />
   );
