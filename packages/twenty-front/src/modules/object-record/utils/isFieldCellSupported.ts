cb90b83aabcf6f99d008218b4976a8235b84bc2b
Félix Malfait <felix@twenty.com>
2024-10-03 11:44:18 +0200

    Remove comment

--- packages/twenty-front/src/modules/object-record/utils/isFieldCellSupported.ts
+++ packages/twenty-front/src/modules/object-record/utils/isFieldCellSupported.ts
@@ -47,7 +47,6 @@ export const isFieldCellSupported = (
 
     if (
       !fieldMetadataItem.relationDefinition ||
-      // Remove Many to many as it's no longer supported
       !relationObjectMetadataItem ||
       !isObjectMetadataAvailableForRelation(relationObjectMetadataItem)
     ) {
