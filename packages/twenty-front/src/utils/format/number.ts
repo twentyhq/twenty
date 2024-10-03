4acbb14d5f01d0d0f09513c76eb0ff5d436ed01b
Marie Stoppa <marie.stoppa@essec.edu>
2024-10-03 18:56:56 +0200

    Always show decimals

--- packages/twenty-front/src/utils/format/number.ts
+++ packages/twenty-front/src/utils/format/number.ts
@@ -2,7 +2,7 @@ export const DEFAULT_DECIMAL_VALUE = 0;
 
 export const formatNumber = (value: number, decimals?: number): string => {
   return value.toLocaleString('en-US', {
-    minimumFractionDigits: 0,
+    minimumFractionDigits: decimals ?? DEFAULT_DECIMAL_VALUE,
     maximumFractionDigits: decimals ?? DEFAULT_DECIMAL_VALUE,
   });
 };
