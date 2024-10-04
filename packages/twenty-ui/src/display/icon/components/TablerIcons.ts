ae2bd66f45f1079564c92085347c46e6ec992100
bmbaji <116686135+bmbaji@users.noreply.github.com>
2024-10-04 12:57:15 -0400

    changed the createdByName to Twenty(Sample data). (#7424)
    I changed the createdByName from' system' to 'Twenty(Sample Data)'.
    
    ---------
    
    Co-authored-by: Félix Malfait <felix@twenty.com>

--- packages/twenty-ui/src/display/icon/components/TablerIcons.ts
+++ packages/twenty-ui/src/display/icon/components/TablerIcons.ts
@@ -139,10 +139,11 @@ export {
   IconPilcrow,
   IconPlayerPlay,
   IconPlayerStop,
-  IconPower,
+  IconPlaylistAdd,
   IconPlaystationSquare,
   IconPlug,
   IconPlus,
+  IconPower,
   IconPresentation,
   IconProgressCheck,
   IconPuzzle,
@@ -154,6 +155,7 @@ export {
   IconReload,
   IconRepeat,
   IconRestore,
+  IconRobot,
   IconRocket,
   IconRotate,
   IconRotate2,
@@ -183,8 +185,7 @@ export {
   IconVideo,
   IconWand,
   IconWorld,
-  IconX,
-  IconPlaylistAdd,
+  IconX
 } from '@tabler/icons-react';
 
 export type { TablerIconsProps } from '@tabler/icons-react';
