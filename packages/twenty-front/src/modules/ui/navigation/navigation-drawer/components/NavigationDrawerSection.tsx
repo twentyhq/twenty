fbb5b3dfd4d064ec89ab9a9807393a47611da13b
Thomas Trompette <thomas.trompette@sfr.fr>
2024-10-07 18:19:00 +0200

    Fix settings navigation advanced design (#7472)
    This PR moved the settings navigation to the left and bottom
    https://github.com/twentyhq/twenty/pull/7130
    
    Updating the logic to:
    -remove logic that move the existing
    -add the setting icon to absolute
    
    <img width="264" alt="Capture d’écran 2024-10-07 à 18 04 05"
    src="https://github.com/user-attachments/assets/b848a5dd-50e9-48c2-bb50-1dcffa9481ac">
    <img width="264" alt="Capture d’écran 2024-10-07 à 18 04 11"
    src="https://github.com/user-attachments/assets/3812929c-dce0-410b-8caa-3ea1210af958">

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawerSection.tsx
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawerSection.tsx
@@ -4,7 +4,6 @@ const StyledSection = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${({ theme }) => theme.betweenSiblingsGap};
-  width: 100%;
 `;
 
 export { StyledSection as NavigationDrawerSection };
