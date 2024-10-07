1baffb6895488ade5099dc21da423e005307c3be
bosiraphael <raphael.bosi@gmail.com>
2024-10-07 13:34:54 +0200

    remove unused dependency from dependency array

--- packages/twenty-front/src/modules/action-menu/components/ActionMenuEffect.tsx
+++ packages/twenty-front/src/modules/action-menu/components/ActionMenuEffect.tsx
@@ -34,7 +34,6 @@ export const ActionMenuEffect = ({ actionMenuId }: ActionMenuEffectProps) => {
   }, [
     contextStoreTargetedRecordIds,
     openActionBar,
-    closeActionMenuDropdown,
     closeActionBar,
     isDropdownOpen,
   ]);
