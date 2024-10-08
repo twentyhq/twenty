6c84125bad33e086f10c2a3839e9e16a2be04502
ehconitin <nitinkoche03@gmail.com>
2024-10-08 17:02:16 +0530

    conflicts?

--- packages/twenty-front/src/modules/settings/data-model/fields/forms/validation-schemas/settingsFieldFormSchema.ts
+++ packages/twenty-front/src/modules/settings/data-model/fields/forms/validation-schemas/settingsFieldFormSchema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 import { settingsDataModelFieldDescriptionFormSchema } from '@/settings/data-model/fields/forms/components/SettingsDataModelFieldDescriptionForm';
 import { settingsDataModelFieldIconLabelFormSchema } from '@/settings/data-model/fields/forms/components/SettingsDataModelFieldIconLabelForm';
 import { settingsDataModelFieldSettingsFormSchema } from '@/settings/data-model/fields/forms/components/SettingsDataModelFieldSettingsFormCard';
-import { settingsDataModelFieldTypeFormSchema } from '@/settings/data-model/fields/forms/components/SettingsObjectNewFieldSelector';
+import { settingsDataModelFieldTypeFormSchema } from '@/settings/data-model/fields/forms/components/SettingsDataModelFieldTypeSelect';
 
 export const settingsFieldFormSchema = (existingOtherLabels?: string[]) => {
   return z
