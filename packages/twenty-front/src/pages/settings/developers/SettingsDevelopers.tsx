9a1f9a2e02d7c3ad9d1318816a2275433de769e0
harshit078 <harshitsingh467@gmail.com>
2024-10-08 18:16:30 +0530

    improved padding and gap

--- packages/twenty-front/src/pages/settings/developers/SettingsDevelopers.tsx
+++ packages/twenty-front/src/pages/settings/developers/SettingsDevelopers.tsx
@@ -15,12 +15,16 @@ const StyledButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
   padding-top: ${({ theme }) => theme.spacing(2)};
+  @media (max-width: ${MOBILE_VIEWPORT}px) {
+    padding-top: ${({ theme }) => theme.spacing(5)};
+  }
 `;
 
 const StyledContainer = styled.div<{ isMobile: boolean }>`
   display: flex;
   flex-direction: column;
   overflow: hidden;
+  gap: ${({ theme }) => theme.spacing(2)};
 `;
 
 export const SettingsDevelopers = () => {
