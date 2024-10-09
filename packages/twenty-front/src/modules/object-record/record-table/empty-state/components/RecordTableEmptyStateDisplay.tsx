de50b600c1305c442322b4ecc5b46d21284b08cd
Thomas Trompette <thomas.trompette@sfr.fr>
2024-10-09 17:08:44 +0200

    Remove add button on empty state + set delete cascade on workflow sub objects

--- packages/twenty-front/src/modules/object-record/record-table/empty-state/components/RecordTableEmptyStateDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/empty-state/components/RecordTableEmptyStateDisplay.tsx
@@ -8,7 +8,10 @@ import {
   AnimatedPlaceholderEmptyTitle,
 } from '@/ui/layout/animated-placeholder/components/EmptyPlaceholderStyled';
 
+import { isReadOnlyObject } from '@/object-metadata/utils/isReadOnlyObject';
+import { RecordTableContext } from '@/object-record/record-table/contexts/RecordTableContext';
 import { Button } from '@/ui/input/button/components/Button';
+import { useContext } from 'react';
 import { IconComponent } from 'twenty-ui';
 
 type RecordTableEmptyStateDisplayProps = {
@@ -28,6 +31,9 @@ export const RecordTableEmptyStateDisplay = ({
   subTitle,
   title,
 }: RecordTableEmptyStateDisplayProps) => {
+  const { objectMetadataItem } = useContext(RecordTableContext);
+  const isReadOnly = isReadOnlyObject(objectMetadataItem);
+
   return (
     <AnimatedPlaceholderEmptyContainer>
       <AnimatedPlaceholder type={animatedPlaceholderType} />
@@ -37,12 +43,14 @@ export const RecordTableEmptyStateDisplay = ({
           {subTitle}
         </AnimatedPlaceholderEmptySubTitle>
       </AnimatedPlaceholderEmptyTextContainer>
-      <Button
-        Icon={Icon}
-        title={buttonTitle}
-        variant={'secondary'}
-        onClick={onClick}
-      />
+      {!isReadOnly && (
+        <Button
+          Icon={Icon}
+          title={buttonTitle}
+          variant={'secondary'}
+          onClick={onClick}
+        />
+      )}
     </AnimatedPlaceholderEmptyContainer>
   );
 };
