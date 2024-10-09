f75cd8f1c76fcc4c8cdfebf8a9819088650bcbf2
Antoine Moreaux <antoine@specifyapp.com>
2024-10-09 17:34:57 +0200

    feat(sso): trigger auth on switch on sso workspace

--- packages/twenty-front/src/modules/auth/graphql/mutations/generateJWT.ts
+++ packages/twenty-front/src/modules/auth/graphql/mutations/generateJWT.ts
@@ -3,8 +3,28 @@ import { gql } from '@apollo/client';
 export const GENERATE_JWT = gql`
   mutation GenerateJWT($workspaceId: String!) {
     generateJWT(workspaceId: $workspaceId) {
-      tokens {
-        ...AuthTokensFragment
+      ... on GenerateJWTOutputWithAuthTokens {
+        success
+        reason
+        authTokens: payload {
+          tokens {
+            ...AuthTokensFragment
+          }
+        }
+      }
+      ... on GenerateJWTOutputWithSSOAUTH {
+        success
+        reason
+        availableSSOIDPs: payload {
+          id
+          issuer
+          name
+          status
+          workspace {
+            id
+            displayName
+          }
+        }
       }
     }
   }
