66f0d318b3a10b4ad12bbcf3afe18695eaaffb41
bosiraphael <raphael.bosi@gmail.com>
2024-10-10 11:10:11 +0200

    add tests

--- packages/twenty-front/src/modules/object-metadata/hooks/__tests__/useObjectMetadataItemById.test.ts
+++ packages/twenty-front/src/modules/object-metadata/hooks/__tests__/useObjectMetadataItemById.test.ts
@@ -1,7 +1,7 @@
 import { renderHook } from '@testing-library/react';
 
 import { ObjectMetadataItemNotFoundError } from '@/object-metadata/errors/ObjectMetadataNotFoundError';
-import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
+import { useObjectMetadataItemById } from '@/object-metadata/hooks/useObjectMetadataItemById';
 import { getJestMetadataAndApolloMocksWrapper } from '~/testing/jest/getJestMetadataAndApolloMocksWrapper';
 import { generatedMockObjectMetadataItems } from '~/testing/mock-data/generatedMockObjectMetadataItems';
 
@@ -9,14 +9,21 @@ const Wrapper = getJestMetadataAndApolloMocksWrapper({
   apolloMocks: [],
 });
 
-// Split into tests for each new hook
-describe('useObjectMetadataItem', () => {
+describe('useObjectMetadataItemById', () => {
   const opportunityObjectMetadata = generatedMockObjectMetadataItems.find(
     (item) => item.nameSingular === 'opportunity',
   );
+
+  if (!opportunityObjectMetadata) {
+    throw new Error('Opportunity object metadata not found');
+  }
+
   it('should return correct properties', async () => {
     const { result } = renderHook(
-      () => useObjectMetadataItem({ objectNameSingular: 'opportunity' }),
+      () =>
+        useObjectMetadataItemById({
+          objectId: opportunityObjectMetadata.id,
+        }),
       {
         wrapper: Wrapper,
       },
@@ -24,17 +31,14 @@ describe('useObjectMetadataItem', () => {
 
     const { objectMetadataItem } = result.current;
 
-    expect(objectMetadataItem.id).toBe(opportunityObjectMetadata?.id);
+    expect(objectMetadataItem.id).toBe(opportunityObjectMetadata.id);
   });
 
-  it('should throw an error when invalid object name singular is provided', async () => {
+  it('should throw an error when invalid ID is provided', async () => {
     expect(() =>
-      renderHook(
-        () => useObjectMetadataItem({ objectNameSingular: 'invalid-object' }),
-        {
-          wrapper: Wrapper,
-        },
-      ),
+      renderHook(() => useObjectMetadataItemById({ objectId: 'invalid-id' }), {
+        wrapper: Wrapper,
+      }),
     ).toThrow(ObjectMetadataItemNotFoundError);
   });
 });
