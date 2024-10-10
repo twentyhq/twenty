56e232f4c57459340a7d9524e652ba207b19bcd5
bosiraphael <raphael.bosi@gmail.com>
2024-10-10 10:54:06 +0200

    remove objectMetadataItemFamilySelectorById

--- packages/twenty-front/src/modules/object-metadata/hooks/useObjectMetadataItemById.ts
+++ packages/twenty-front/src/modules/object-metadata/hooks/useObjectMetadataItemById.ts
@@ -4,20 +4,16 @@ import { ObjectMetadataItemNotFoundError } from '@/object-metadata/errors/Object
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
 import { isDefined } from '~/utils/isDefined';
 
-import { objectMetadataItemFamilySelectorById } from '@/object-metadata/states/objectMetadataItemFamilySelectorById';
-
 export const useObjectMetadataItemById = ({
   objectId,
 }: {
   objectId: string;
 }) => {
-  const objectMetadataItem = useRecoilValue(
-    objectMetadataItemFamilySelectorById({
-      objectId,
-    }),
-  );
-
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
+
+  const objectMetadataItem = objectMetadataItems.find(
+    (objectMetadataItem) => objectMetadataItem.id === objectId,
+  );
 
   if (!isDefined(objectMetadataItem)) {
     throw new ObjectMetadataItemNotFoundError(objectId, objectMetadataItems);
