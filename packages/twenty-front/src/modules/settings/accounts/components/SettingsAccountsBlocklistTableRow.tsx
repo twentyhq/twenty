70fa9d1c92e8021d9869aa312fe78cfd3d8c510b
harshit078 <harshitsingh467@gmail.com>
2024-10-12 17:14:52 +0530

    fixed table contents and prevented overflowing

--- packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsBlocklistTableRow.tsx
+++ packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsBlocklistTableRow.tsx
@@ -1,29 +1,68 @@
-import { IconX } from 'twenty-ui';
+import { IconX, MOBILE_VIEWPORT } from 'twenty-ui';
 
 import { BlocklistItem } from '@/accounts/types/BlocklistItem';
 import { IconButton } from '@/ui/input/button/components/IconButton';
 import { TableCell } from '@/ui/layout/table/components/TableCell';
 import { TableRow } from '@/ui/layout/table/components/TableRow';
 import { formatToHumanReadableDate } from '~/utils/date-utils';
+import styled from '@emotion/styled';
 
 type SettingsAccountsBlocklistTableRowProps = {
   blocklistItem: BlocklistItem;
   onRemove: (id: string) => void;
 };
+const StyledTableRow = styled(TableRow)`
+  grid-template-columns: 4fr;
+  white-space: pre;
+  @media (max-width: ${MOBILE_VIEWPORT}px) {
+    grid-template-columns: 3fr;
+    width: 100%;
+  }
+`;
 
+const StyledTableCell = styled(TableCell)`
+  @media (max-width: ${MOBILE_VIEWPORT}px) {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+`;
+
+const StyledDateCell = styled(TableCell)`
+  @media (max-width: ${MOBILE_VIEWPORT}px) {
+    max-width: 100%;
+    position: relative;
+  }
+`;
+
+const StyledIconCell = styled(TableCell)`
+  text-align: right;
+`;
+
+const StyledScrollableTextContainer = styled.div`
+  @media (max-width: ${MOBILE_VIEWPORT}px) {
+    max-width: 100%;
+    overflow-x: auto;
+    white-space: pre-line;
+  }
+`;
 export const SettingsAccountsBlocklistTableRow = ({
   blocklistItem,
   onRemove,
 }: SettingsAccountsBlocklistTableRowProps) => {
   return (
-    <TableRow key={blocklistItem.id}>
-      <TableCell>{blocklistItem.handle}</TableCell>
-      <TableCell>
+    <StyledTableRow key={blocklistItem.id}>
+      <StyledTableCell>
+        <StyledScrollableTextContainer>
+          {blocklistItem.handle}
+        </StyledScrollableTextContainer>
+      </StyledTableCell>
+      <StyledDateCell align={'right'}>
         {blocklistItem.createdAt
           ? formatToHumanReadableDate(blocklistItem.createdAt)
           : ''}
-      </TableCell>
-      <TableCell align="right">
+      </StyledDateCell>
+      <StyledIconCell align="right">
         <IconButton
           onClick={() => {
             onRemove(blocklistItem.id);
@@ -32,7 +71,7 @@ export const SettingsAccountsBlocklistTableRow = ({
           size="small"
           Icon={IconX}
         />
-      </TableCell>
-    </TableRow>
+      </StyledIconCell>
+    </StyledTableRow>
   );
 };
