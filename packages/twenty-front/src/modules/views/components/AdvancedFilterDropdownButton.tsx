3b011dc0864e6cbe919358b451ac7e7408c70ffc
ad-elias <elias@autodiligence.com>
2024-10-13 23:37:41 +0200

    Support removing advanced filter

--- packages/twenty-front/src/modules/views/components/AdvancedFilterDropdownButton.tsx
+++ packages/twenty-front/src/modules/views/components/AdvancedFilterDropdownButton.tsx
@@ -3,8 +3,11 @@ import { useCallback } from 'react';
 import { Dropdown } from '@/ui/layout/dropdown/components/Dropdown';
 
 import { AdvancedFilterViewFilterGroup } from '@/object-record/advanced-filter/components/AdvancedFilterViewFilterGroup';
+import { useDeleteCombinedViewFilterGroup } from '@/object-record/advanced-filter/hooks/useDeleteCombinedViewFilterGroup';
 import { AdvancedFilterChip } from '@/views/components/AdvancedFilterChip';
 import { ADVANCED_FILTER_DROPDOWN_ID } from '@/views/constants/AdvancedFilterDropdownId';
+import { useDeleteCombinedViewFilters } from '@/views/hooks/useDeleteCombinedViewFilters';
+import { useGetCurrentView } from '@/views/hooks/useGetCurrentView';
 
 interface AdvancedFilterDropdownButtonProps {
   viewBarId: string;
@@ -13,11 +16,39 @@ interface AdvancedFilterDropdownButtonProps {
 export const AdvancedFilterDropdownButton = (
   props: AdvancedFilterDropdownButtonProps,
 ) => {
+  const { deleteCombinedViewFilter } = useDeleteCombinedViewFilters();
+  const { deleteCombinedViewFilterGroup } = useDeleteCombinedViewFilterGroup();
+
+  const { currentViewWithCombinedFiltersAndSorts } = useGetCurrentView();
+
+  const viewFilters =
+    currentViewWithCombinedFiltersAndSorts?.viewFilters.filter(
+      (viewFilter) => !!viewFilter.viewFilterGroupId,
+    );
+  const viewFilterGroups =
+    currentViewWithCombinedFiltersAndSorts?.viewFilterGroups;
+
+  const viewFilterIds = viewFilters?.map((viewFilter) => viewFilter.id) ?? [];
+  const viewFilterGroupIds =
+    viewFilterGroups?.map((viewFilter) => viewFilter.id) ?? [];
+
   const handleDropdownClickOutside = useCallback(() => {}, []);
 
   const handleDropdownClose = () => {};
 
-  const removeAdvancedFilter = useCallback(() => {}, []);
+  const removeAdvancedFilter = useCallback(async () => {
+    for (const viewFilterGroupId of viewFilterGroupIds) {
+      await deleteCombinedViewFilterGroup(viewFilterGroupId);
+    }
+    for (const viewFilterId of viewFilterIds) {
+      await deleteCombinedViewFilter(viewFilterId);
+    }
+  }, [
+    viewFilterIds,
+    viewFilterGroupIds,
+    deleteCombinedViewFilter,
+    deleteCombinedViewFilterGroup,
+  ]);
 
   return (
     <Dropdown
