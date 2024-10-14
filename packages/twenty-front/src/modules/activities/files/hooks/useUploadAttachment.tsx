9cd74f9a79e181c07d393acd70e8e54350848edd
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-10-14 20:56:41 +0530

    Update useUploadAttachment.tsx

--- packages/twenty-front/src/modules/activities/files/hooks/useUploadAttachment.tsx
+++ packages/twenty-front/src/modules/activities/files/hooks/useUploadAttachment.tsx
@@ -7,7 +7,7 @@ const useUploadAttachment = () => {
   const [addAttachmentMutation] = useAddAttachmentMutation();
 
   const uploadAttachment = async (file: File, fileFolder: FileFolder) => {
-    // Perform the file upload
+    
     const result = await uploadFile({
       variables: {
         file,
@@ -17,15 +17,15 @@ const useUploadAttachment = () => {
 
     const uploadedFileData = result?.data?.uploadFile;
 
-    // Check if the upload result exists
+   
     if (!uploadedFileData) {
       throw new Error("Couldn't upload the image.");
     }
 
-    // Get the absolute URL of the uploaded file
+   
     const uploadedFileUrl = getFileAbsoluteURI(uploadedFileData);
 
-    // Add the file metadata to the attachment table
+    
     await addAttachmentMutation({
       variables: {
         fileId: uploadedFileData, // Assuming file ID is part of the upload response
