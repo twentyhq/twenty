79dfb6171438f12157ae805bbebd63525fa2614b
Antoine Moreaux <antoine@specifyapp.com>
2024-10-15 12:51:14 +0200

    fix(sso): fix tests

--- packages/twenty-front/src/modules/settings/security/utils/__tests__/parseSAMLMetadataFromXMLFile.test.ts
+++ packages/twenty-front/src/modules/settings/security/utils/__tests__/parseSAMLMetadataFromXMLFile.test.ts
@@ -2,20 +2,20 @@ import { parseSAMLMetadataFromXMLFile } from '../parseSAMLMetadataFromXMLFile';
 
 describe('parseSAMLMetadataFromXMLFile', () => {
   it('should parse SAML metadata from XML file', () => {
-    const xmlString = `
-      <md:EntityDescriptor entityID="https://test.com">
-        <md:IDPSSODescriptor>
-          <md:KeyDescriptor>
-            <ds:KeyInfo>
-              <ds:X509Data>
-                <ds:X509Certificate>test</ds:X509Certificate>
-              </ds:X509Data>
+    const xmlString = `<?xml version="1.0" encoding="UTF-8"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="https://test.com" validUntil="2026-02-04T17:46:23.000Z">
+    <md:IDPSSODescriptor WantAuthnRequestsSigned="false" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
+        <md:KeyDescriptor use="signing">
+            <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+                <ds:X509Data>
+                    <ds:X509Certificate>test</ds:X509Certificate>
+                </ds:X509Data>
             </ds:KeyInfo>
-          </md:KeyDescriptor>
-          <md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://test.com" />
-        </md:IDPSSODescriptor>
-      </md:EntityDescriptor>
-    `;
+        </md:KeyDescriptor>
+        <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress</md:NameIDFormat>
+        <md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://test.com"/>
+        <md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="https://test.com"/>
+    </md:IDPSSODescriptor>
+</md:EntityDescriptor>`;
     const result = parseSAMLMetadataFromXMLFile(xmlString);
     expect(result).toEqual({
       success: true,
