d5bf442ac29b68b98b9000426e4ed0dcf2f40eca
Ana Sofia Marin Alexandre <ana.marn@uc.cl>
2024-10-15 17:41:52 +0200

    clean unused code and comments

--- packages/twenty-front/src/modules/settings/developers/webhook/states/webhookGraphOptionsState.ts
+++
@@ -1,8 +0,0 @@
-import { createState } from 'twenty-ui';
-
-export type WebhookGraphOptions = '7D' | '1D' | '12H' | '4H';
-
-export const webhookGraphOptionsState = createState<WebhookGraphOptions>({
-  key: 'webhookGraphOptions',
-  defaultValue: '7D',
-});
