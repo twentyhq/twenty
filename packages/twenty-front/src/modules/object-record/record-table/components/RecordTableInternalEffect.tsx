5657fc73d3ffe91f603cd5cfafec79e794887736
bosiraphael <raphael.bosi@gmail.com>
2024-10-18 11:04:13 +0200

    update RecordTableInternalEffect

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableInternalEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableInternalEffect.tsx
@@ -1,29 +1,21 @@
 import { Key } from 'ts-key-enum';
 
-import { SOFT_FOCUS_CLICK_OUTSIDE_LISTENER_ID } from '@/object-record/record-table/constants/SoftFocusClickOutsideListenerId';
 import { useRecordTable } from '@/object-record/record-table/hooks/useRecordTable';
 import { TableHotkeyScope } from '@/object-record/record-table/types/TableHotkeyScope';
 import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
-import { useClickOutsideListener } from '@/ui/utilities/pointer-event/hooks/useClickOutsideListener';
 import { useListenClickOutsideByClassName } from '@/ui/utilities/pointer-event/hooks/useListenClickOutside';
 
 type RecordTableInternalEffectProps = {
   recordTableId: string;
-  tableBodyRef: React.RefObject<HTMLDivElement>;
 };
 
 export const RecordTableInternalEffect = ({
   recordTableId,
-  tableBodyRef,
 }: RecordTableInternalEffectProps) => {
   const { leaveTableFocus, resetTableRowSelection, useMapKeyboardToSoftFocus } =
     useRecordTable({ recordTableId });
 
   useMapKeyboardToSoftFocus();
-
-  const { useListenClickOutside } = useClickOutsideListener(
-    SOFT_FOCUS_CLICK_OUTSIDE_LISTENER_ID,
-  );
 
   useScopedHotkeys(
     [Key.Escape],
@@ -37,7 +29,6 @@ export const RecordTableInternalEffect = ({
     classNames: ['entity-table-cell'],
     excludeClassNames: ['bottom-bar', 'action-menu-dropdown'],
     callback: () => {
-      console.log('click outside');
       leaveTableFocus();
     },
   });
