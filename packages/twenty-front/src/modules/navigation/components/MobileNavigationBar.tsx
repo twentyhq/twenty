fe0ae489a25e01e9e5fba1c56cc691a81ad95dff
Hitarth Sheth <hitarthsheth0@gmail.com>
2024-10-20 22:23:14 -0400

    horizontal mobile scroll added

--- packages/twenty-front/src/modules/navigation/components/MobileNavigationBar.tsx
+++ packages/twenty-front/src/modules/navigation/components/MobileNavigationBar.tsx
@@ -1,11 +1,20 @@
+import { currentWorkspaceMemberState } from '@/auth/states/currentWorkspaceMemberState';
 import { useCommandMenu } from '@/command-menu/hooks/useCommandMenu';
 import { isCommandMenuOpenedState } from '@/command-menu/states/isCommandMenuOpenedState';
+import { useFavorites } from '@/favorites/hooks/useFavorites';
+import { useFilteredObjectMetadataItemsForWorkspaceFavorites } from '@/navigation/hooks/useObjectMetadataItemsInWorkspaceFavorites';
 import { NavigationBar } from '@/ui/navigation/navigation-bar/components/NavigationBar';
 import { isNavigationDrawerExpandedState } from '@/ui/navigation/states/isNavigationDrawerExpanded';
-import { useRecoilState } from 'recoil';
-import { IconComponent, IconList, IconSearch, IconSettings } from 'twenty-ui';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { AvatarType, IconComponent, IconList, IconSearch, IconSettings, useIcons } from 'twenty-ui';
 import { useIsSettingsPage } from '../hooks/useIsSettingsPage';
 import { currentMobileNavigationDrawerState } from '../states/currentMobileNavigationDrawerState';
+import { getObjectMetadataItemViews } from '@/views/utils/getObjectMetadataItemViews';
+import { usePrefetchedData } from '@/prefetch/hooks/usePrefetchedData';
+import { PrefetchKey } from '@/prefetch/types/PrefetchKey';
+import { View } from '@/views/types/View';
+import { useLastVisitedView } from '@/navigation/hooks/useLastVisitedView';
 
 type NavigationBarItemName = 'main' | 'search' | 'tasks' | 'settings';
 
@@ -17,53 +26,111 @@ export const MobileNavigationBar = () => {
     useRecoilState(isNavigationDrawerExpandedState);
   const [currentMobileNavigationDrawer, setCurrentMobileNavigationDrawer] =
     useRecoilState(currentMobileNavigationDrawerState);
-
+  const { favorites, handleReorderFavorite } = useFavorites();
+  const currentWorkspaceMember = useRecoilValue(currentWorkspaceMemberState);
+  const navigate = useNavigate();
+  const { records: views } = usePrefetchedData<View>(PrefetchKey.AllViews);
+  const { getLastVisitedViewIdFromObjectMetadataItemId } = useLastVisitedView();
+  const { getIcon } = useIcons();
+  
   const activeItemName = isNavigationDrawerExpanded
-    ? currentMobileNavigationDrawer
-    : isCommandMenuOpened
-      ? 'search'
-      : isSettingsPage
-        ? 'settings'
-        : 'main';
-
-  const items: {
-    name: NavigationBarItemName;
-    Icon: IconComponent;
+  ? currentMobileNavigationDrawer
+  : isCommandMenuOpened
+  ? 'search'
+  : isSettingsPage
+  ? 'settings'
+  : 'main';
+  
+  const currentWorkspaceMemberFavorites = favorites.filter(
+    (favorite) => favorite.workspaceMemberId === currentWorkspaceMember?.id,
+  );
+  
+  const favoriteItems: {
+    id: string,
+    name: string,
+    labelIdentifier: string,
+    avatarUrl: string,
+    avatarType: AvatarType,
+    link: string,
+    recordId: string,
     onClick: () => void;
-  }[] = [
-    {
-      name: 'main',
-      Icon: IconList,
-      onClick: () => {
-        closeCommandMenu();
-        setIsNavigationDrawerExpanded(
-          (previousIsOpen) => activeItemName !== 'main' || !previousIsOpen,
-        );
-        setCurrentMobileNavigationDrawer('main');
-      },
-    },
-    {
-      name: 'search',
-      Icon: IconSearch,
-      onClick: () => {
-        if (!isCommandMenuOpened) {
-          openCommandMenu();
-        }
-        setIsNavigationDrawerExpanded(false);
-      },
-    },
-    {
-      name: 'settings',
-      Icon: IconSettings,
-      onClick: () => {
-        closeCommandMenu();
-        setIsNavigationDrawerExpanded(
-          (previousIsOpen) => activeItemName !== 'settings' || !previousIsOpen,
-        );
-        setCurrentMobileNavigationDrawer('settings');
-      },
-    },
-  ];
+  }[] = currentWorkspaceMemberFavorites.map(favItem => ({
+    id: favItem.id,
+    name: favItem.labelIdentifier,
+    labelIdentifier: favItem.labelIdentifier,
+    avatarUrl: favItem.avatarUrl,
+    avatarType: favItem.avatarType,
+    link: favItem.link,
+    recordId: favItem.recordId,
+    onClick: () => navigate(favItem.link)
+  }))
 
-  return <NavigationBar activeItemName={activeItemName} items={items} />;
+  
+const ORDERED_STANDARD_OBJECTS = [
+  'person',
+  'company',
+  'opportunity',
+  'task',
+  'note',
+];
+
+const { activeObjectMetadataItems: objectMetadataItemsToDisplay } =
+useFilteredObjectMetadataItemsForWorkspaceFavorites();
+
+const sortedStandardObjectMetadataItems = [...objectMetadataItemsToDisplay]
+.filter((item) => ORDERED_STANDARD_OBJECTS.includes(item.nameSingular))
+.sort((objectMetadataItemA, objectMetadataItemB) => {
+  const indexA = ORDERED_STANDARD_OBJECTS.indexOf(
+    objectMetadataItemA.nameSingular,
+  );
+  const indexB = ORDERED_STANDARD_OBJECTS.indexOf(
+    objectMetadataItemB.nameSingular,
+  );
+  if (indexA === -1 || indexB === -1) {
+    return objectMetadataItemA.nameSingular.localeCompare(
+      objectMetadataItemB.nameSingular,
+    );
+  }
+  return indexA - indexB;
+});
+
+const sortedCustomObjectMetadataItems = [...objectMetadataItemsToDisplay]
+.filter((item) => !ORDERED_STANDARD_OBJECTS.includes(item.nameSingular))
+.sort((objectMetadataItemA, objectMetadataItemB) => {
+  return new Date(objectMetadataItemA.createdAt) <
+    new Date(objectMetadataItemB.createdAt)
+    ? 1
+    : -1;
+});
+
+const objectMetadataItemsForNavigationItems: {
+  id: string,
+  Icon: IconComponent,
+  isActive: boolean,
+  onclick: () => void
+}[] = [
+  ...sortedStandardObjectMetadataItems,
+  ...sortedCustomObjectMetadataItems,
+].map(objectMetadata => {
+  const objectMetadataViews = getObjectMetadataItemViews(
+    objectMetadata.id,
+    views,
+  );
+  const lastVisitedViewId = getLastVisitedViewIdFromObjectMetadataItemId(
+    objectMetadata.id,
+  );
+  const viewId = lastVisitedViewId ?? objectMetadataViews[0]?.id;
+  const navigationPath = `/objects/${objectMetadata.namePlural}${
+    viewId ? `?view=${viewId}` : ''
+  }`;
+
+  return{
+    id: objectMetadata.id,
+    Icon: getIcon(objectMetadata.icon),
+    isActive: objectMetadata.id == viewId,
+    onclick: () => navigate(navigationPath)
+  }
+})
+
+  return <NavigationBar activeItemName={activeItemName} favorites={favoriteItems} objectMetaData={objectMetadataItemsForNavigationItems}/>;
 };
--- packages/twenty-front/src/modules/ui/navigation/navigation-bar/components/NavigationBar.tsx
+++ packages/twenty-front/src/modules/ui/navigation/navigation-bar/components/NavigationBar.tsx
@@ -1,33 +1,104 @@
+import { currentWorkspaceState } from '@/auth/states/currentWorkspaceState';
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
-import { IconComponent } from 'twenty-ui';
-
+import { useRecoilValue } from 'recoil';
+import { Avatar, AvatarType, getImageAbsoluteURI, IconComponent } from 'twenty-ui';
 import { NavigationBarItem } from './NavigationBarItem';
+import { useNavigate } from 'react-router-dom';
+import { workspaceLogoUrl } from '~/testing/mock-data/users';
+import { getAbsoluteUrl } from '~/utils/url/getAbsoluteUrl';
 
+const theme = useTheme()
 const StyledContainer = styled.div`
-  display: flex;
-  gap: ${({ theme }) => theme.spacing(4)};
-  justify-content: center;
+display: flex;
+gap: ${({ theme }) => theme.spacing(4)};
+  justify-content: left;
   padding: ${({ theme }) => theme.spacing(3)};
   z-index: 1001;
+  overflow: scroll;
+  `;
+
+const StyledAvatar = styled(Avatar)`
+  :hover {
+    cursor: grab;
+  }
 `;
+
+const FavoritesHighlight = styled.div`
+    width: 24,
+    backgroundColor: theme.background.transparent.lighter,
+    border: 1px solid ${theme.background.transparent.lighter},
+    borderRadius: theme.border.radius.sm,
+`;
+
+const currentWorkspace = useRecoilValue(currentWorkspaceState);
+const navigate = useNavigate()
 
 type NavigationBarProps = {
   activeItemName: string;
-  items: { name: string; Icon: IconComponent; onClick: () => void }[];
+  favorites: {
+    id: string;
+    name: string;
+    labelIdentifier: string;
+    avatarUrl: string;
+    avatarType: AvatarType;
+    link: string;
+    recordId: string;
+    onClick: () => void;
+  }[];
+  objectMetaData: {
+    id: string,
+    Icon: IconComponent,
+    isActive: boolean,
+    onclick: () => void
+  }[];
 };
 
 export const NavigationBar = ({
   activeItemName,
-  items,
-}: NavigationBarProps) => (
+  favorites,
+  objectMetaData
+}: NavigationBarProps) => {
+
+  return (
   <StyledContainer>
-    {items.map(({ Icon, name, onClick }) => (
+    <FavoritesHighlight>
+      <NavigationBarItem 
+        key="workspaceLogo" 
+        Icon={() => (
+          <StyledAvatar
+            avatarUrl={getAbsoluteUrl(currentWorkspace?.logo as string) ?? undefined}
+            placeholder="Workspace Logo"
+            className="fav-avatar"
+          />
+        )}
+        isActive={false}
+        onClick={() => navigate("")}
+      />
+    {favorites.map((favItem) => (
       <NavigationBarItem
-        key={name}
-        Icon={Icon}
-        isActive={activeItemName === name}
-        onClick={onClick}
+        key={favItem.id}
+        Icon={() => (
+          <StyledAvatar
+            placeholderColorSeed={favItem.recordId}
+            avatarUrl={favItem.avatarUrl}
+            type={favItem.avatarType}
+            placeholder={favItem.labelIdentifier}
+            className="fav-avatar"
+          />
+        )}
+        isActive={false}
+        onClick={favItem.onClick}
+      />
+    ))}
+    </FavoritesHighlight>
+    {objectMetaData.map((object) => (
+      <NavigationBarItem
+      key={object.id}
+      Icon={object.Icon}
+      isActive={object.isActive}
+      onClick={object.onclick}
       />
     ))}
   </StyledContainer>
-);
+)};
