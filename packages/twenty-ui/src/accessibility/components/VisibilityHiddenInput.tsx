debca260cba6020b1838e492e4743e214f15befe
Devessier <baptiste@devessier.fr>
2024-10-21 16:24:08 +0200

    refactor: remove manual forwardRef

--- packages/twenty-ui/src/accessibility/components/VisibilityHiddenInput.tsx
+++ packages/twenty-ui/src/accessibility/components/VisibilityHiddenInput.tsx
@@ -1,17 +1,7 @@
 import styled from '@emotion/styled';
 import { VISIBILITY_HIDDEN } from '@ui/accessibility/utils/visibility-hidden';
-import { forwardRef } from 'react';
 
-const StyledInput = styled.input`
+// eslint-disable-next-line @nx/workspace-styled-components-prefixed-with-styled
+export const VisibilityHiddenInput = styled.input`
   ${VISIBILITY_HIDDEN}
 `;
-
-const _VisibilityHiddenInput = (
-  props: React.InputHTMLAttributes<HTMLInputElement>,
-  ref: React.Ref<HTMLInputElement>,
-) => {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <StyledInput ref={ref} {...props} />;
-};
-
-export const VisibilityHiddenInput = forwardRef(_VisibilityHiddenInput);
