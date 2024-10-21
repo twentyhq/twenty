6ff5d485c1a6dec6c27a3dee41e63857092176ec
Weiko <corentin@twenty.com>
2024-10-21 17:10:14 +0200

    Add simplify-search-vector-expression to upgrade 0-32 command

--- packages/twenty-server/src/database/commands/upgrade-version/0-32/0-32-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-32/0-32-upgrade-version.module.ts
@@ -4,14 +4,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnforceUniqueConstraintsCommand } from 'src/database/commands/upgrade-version/0-32/0-32-enforce-unique-constraints.command';
 import { UpgradeTo0_32Command } from 'src/database/commands/upgrade-version/0-32/0-32-upgrade-version.command';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
+import { SearchModule } from 'src/engine/metadata-modules/search/search.module';
+import { WorkspaceMigrationRunnerModule } from 'src/engine/workspace-manager/workspace-migration-runner/workspace-migration-runner.module';
 import { WorkspaceSyncMetadataCommandsModule } from 'src/engine/workspace-manager/workspace-sync-metadata/commands/workspace-sync-metadata-commands.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Workspace], 'core'),
-    TypeOrmModule.forFeature([ObjectMetadataEntity], 'metadata'),
+    TypeOrmModule.forFeature(
+      [ObjectMetadataEntity, FieldMetadataEntity],
+      'metadata',
+    ),
     WorkspaceSyncMetadataCommandsModule,
+    SearchModule,
+    WorkspaceMigrationRunnerModule,
   ],
   providers: [UpgradeTo0_32Command, EnforceUniqueConstraintsCommand],
 })
