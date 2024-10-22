297175e36598e8473cd34470550c5eab8b1274ab
gitstart-twenty <gitstart-twenty@users.noreply.github.com>
2024-10-22 10:47:37 +0000

    Migrate to twenty-ui - utilities/dimensions

--- packages/twenty-ui/src/utilities/index.ts
+++ packages/twenty-ui/src/utilities/index.ts
@@ -1,4 +1,5 @@
 export * from './color/utils/stringToHslColor';
+export * from './dimensions/components/ComputeNodeDimensions';
 export * from './image/getImageAbsoluteURI';
 export * from './isDefined';
 export * from './state/utils/createState';
