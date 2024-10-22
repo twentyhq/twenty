aff1044fce2a32a50140782703d47ec0cf35c882
bosiraphael <raphael.bosi@gmail.com>
2024-10-22 15:07:34 +0200

    fix storybook test

--- packages/twenty-front/src/modules/action-menu/components/__stories__/RecordIndexActionMenuBar.stories.tsx
+++ packages/twenty-front/src/modules/action-menu/components/__stories__/RecordIndexActionMenuBar.stories.tsx
@@ -5,6 +5,7 @@ import { RecoilRoot } from 'recoil';
 import { RecordIndexActionMenuBar } from '@/action-menu/components/RecordIndexActionMenuBar';
 import { actionMenuEntriesComponentState } from '@/action-menu/states/actionMenuEntriesComponentState';
 import { ActionMenuComponentInstanceContext } from '@/action-menu/states/contexts/ActionMenuComponentInstanceContext';
+import { ContextStoreComponentInstanceContext } from '@/context-store/states/contexts/ContextStoreComponentInstanceContext';
 import { contextStoreNumberOfSelectedRecordsComponentState } from '@/context-store/states/contextStoreNumberOfSelectedRecordsComponentState';
 import { contextStoreTargetedRecordsRuleComponentState } from '@/context-store/states/contextStoreTargetedRecordsRuleComponentState';
 import { isBottomBarOpenedComponentState } from '@/ui/layout/bottom-bar/states/isBottomBarOpenedComponentState';
@@ -19,64 +20,68 @@ const meta: Meta<typeof RecordIndexActionMenuBar> = {
   component: RecordIndexActionMenuBar,
   decorators: [
     (Story) => (
-      <RecoilRoot
-        initializeState={({ set }) => {
-          set(
-            contextStoreTargetedRecordsRuleComponentState.atomFamily({
-              instanceId: 'story-action-menu',
-            }),
-            {
-              mode: 'selection',
-              selectedRecordIds: ['1', '2', '3'],
-            },
-          );
-          set(
-            contextStoreNumberOfSelectedRecordsComponentState.atomFamily({
-              instanceId: 'story-action-menu',
-            }),
-            3,
-          );
-          set(
-            actionMenuEntriesComponentState.atomFamily({
-              instanceId: 'story-action-menu',
-            }),
-            new Map([
-              [
-                'delete',
-                {
-                  key: 'delete',
-                  label: 'Delete',
-                  position: 0,
-                  Icon: IconTrash,
-                  onClick: deleteMock,
-                },
-              ],
-              [
-                'markAsDone',
-                {
-                  key: 'markAsDone',
-                  label: 'Mark as done',
-                  position: 1,
-                  Icon: IconCheckbox,
-                  onClick: markAsDoneMock,
-                },
-              ],
-            ]),
-          );
-          set(
-            isBottomBarOpenedComponentState.atomFamily({
-              instanceId: 'action-bar-story-action-menu',
-            }),
-            true,
-          );
-        }}
+      <ContextStoreComponentInstanceContext.Provider
+        value={{ instanceId: 'story-action-menu' }}
       >
-        <ActionMenuComponentInstanceContext.Provider
-          value={{ instanceId: 'story-action-menu' }}
+        <RecoilRoot
+          initializeState={({ set }) => {
+            set(
+              contextStoreTargetedRecordsRuleComponentState.atomFamily({
+                instanceId: 'story-action-menu',
+              }),
+              {
+                mode: 'selection',
+                selectedRecordIds: ['1', '2', '3'],
+              },
+            );
+            set(
+              contextStoreNumberOfSelectedRecordsComponentState.atomFamily({
+                instanceId: 'story-action-menu',
+              }),
+              3,
+            );
+            set(
+              actionMenuEntriesComponentState.atomFamily({
+                instanceId: 'story-action-menu',
+              }),
+              new Map([
+                [
+                  'delete',
+                  {
+                    key: 'delete',
+                    label: 'Delete',
+                    position: 0,
+                    Icon: IconTrash,
+                    onClick: deleteMock,
+                  },
+                ],
+                [
+                  'markAsDone',
+                  {
+                    key: 'markAsDone',
+                    label: 'Mark as done',
+                    position: 1,
+                    Icon: IconCheckbox,
+                    onClick: markAsDoneMock,
+                  },
+                ],
+              ]),
+            );
+            set(
+              isBottomBarOpenedComponentState.atomFamily({
+                instanceId: 'action-bar-story-action-menu',
+              }),
+              true,
+            );
+          }}
         >
-          <Story />
-        </ActionMenuComponentInstanceContext.Provider>
-      </RecoilRoot>
+          <ActionMenuComponentInstanceContext.Provider
+            value={{ instanceId: 'story-action-menu' }}
+          >
+            <Story />
+          </ActionMenuComponentInstanceContext.Provider>
+        </RecoilRoot>
+      </ContextStoreComponentInstanceContext.Provider>
     ),
   ],
   args: {
