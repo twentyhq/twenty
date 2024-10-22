44018d65151b77eff23c1023289dc1572ed1a16d
Charles Bochet <charles@twenty.com>
2024-10-22 10:51:08 +0200

    Fix

--- packages/twenty-ui/src/utilities/index.ts
+++ packages/twenty-ui/src/utilities/index.ts
@@ -2,6 +2,6 @@ export * from './color/utils/stringToHslColor';
 export * from './getDisplayValueByUrlType';
 export * from './image/getImageAbsoluteURI';
 export * from './isDefined';
+export * from './screen-size/hooks/useScreenSize';
 export * from './state/utils/createState';
 export * from './types/Nullable';
-export * from './screen-size/hooks/useScreenSize';
