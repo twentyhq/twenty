91fb51a4f376541b6fd17e543d1e6793a79c6747
gitstart-twenty <gitstart-twenty@users.noreply.github.com>
2024-10-22 10:50:34 +0000

    Migrate to twenty-ui - utilities/animation

--- packages/twenty-ui/src/utilities/animation/components/AnimatedEaseInOut.tsx
+++ packages/twenty-ui/src/utilities/animation/components/AnimatedEaseInOut.tsx
@@ -1,36 +1,41 @@
 import { useTheme } from '@emotion/react';
+import { AnimationDuration } from '@ui/theme';
 import { AnimatePresence, motion } from 'framer-motion';
-import { AnimationDuration } from 'twenty-ui';
 
-type AnimatedFadeOutProps = {
+type AnimatedEaseInOutProps = {
   isOpen: boolean;
   children: React.ReactNode;
   duration?: AnimationDuration;
   marginBottom?: string;
   marginTop?: string;
+  initial?: boolean;
 };
 
-export const AnimatedFadeOut = ({
-  isOpen,
+export const AnimatedEaseInOut = ({
   children,
-  duration = 'normal',
+  isOpen,
   marginBottom,
   marginTop,
-}: AnimatedFadeOutProps) => {
+  duration = 'normal',
+  initial = true,
+}: AnimatedEaseInOutProps) => {
   const theme = useTheme();
+
   return (
-    <AnimatePresence>
+    <AnimatePresence initial={initial}>
       {isOpen && (
         <motion.div
           initial={{
-            opacity: 1,
             marginBottom: marginBottom ?? 0,
             marginTop: marginTop ?? 0,
+            height: 0,
+            opacity: 0,
           }}
-          exit={{ opacity: 0, height: 0, marginBottom: 0, marginTop: 0 }}
+          animate={{ height: 'fit-content', opacity: 1 }}
+          exit={{ height: 0, opacity: 0, marginBottom: 0, marginTop: 0 }}
           transition={{
             duration: theme.animation.duration[duration],
-            ease: 'easeOut',
+            ease: 'easeInOut',
           }}
         >
           {children}
