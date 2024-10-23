7bec4f55b190e8240a9056be5a166d9d3dfc70b2
Félix Malfait <felix@twenty.com>
2024-10-23 12:10:45 +0200

    Forgot to add unicity constraint

--- packages/twenty-server/src/database/typeorm/metadata/migrations/1729676165199-add-object-shortcut.ts
+++ packages/twenty-server/src/database/typeorm/metadata/migrations/1729676165199-add-object-shortcut.ts
@@ -7,9 +7,16 @@ export class AddObjectShortcut1729676165199 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "metadata"."objectMetadata" ADD "shortcut" character varying`,
     );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_objectMetadata_shortcut_upper_workspace" ON "metadata"."objectMetadata" (UPPER("shortcut"), "workspaceId")`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "metadata"."IDX_objectMetadata_shortcut_upper_workspace"`,
+    );
+
     await queryRunner.query(
       `ALTER TABLE "metadata"."objectMetadata" DROP COLUMN "shortcut"`,
     );
