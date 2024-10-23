5f8f47b966516ea31be2d51d6b60bc546436cb5c
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2024-10-23 22:28:49 +0530

    Use 'role' = button for chip navigation

--- packages/twenty-ui/src/display/chip/components/Chip.tsx
+++ packages/twenty-ui/src/display/chip/components/Chip.tsx
@@ -39,7 +39,7 @@ const StyledContainer = withTheme(styled.div<
   Pick<
     ChipProps,
     'accent' | 'clickable' | 'disabled' | 'maxWidth' | 'size' | 'variant'
-  > & { theme: Theme }
+  > & { theme: Theme } & { role: 'button' }
 >`
   --chip-horizontal-padding: ${({ theme }) => theme.spacing(1)};
   --chip-vertical-padding: ${({ theme }) => theme.spacing(1)};
@@ -133,6 +133,7 @@ export const Chip = ({
       size={size}
       variant={variant}
       onClick={onClick}
+      role="button"
     >
       {leftComponent}
       <OverflowingTextWithTooltip
