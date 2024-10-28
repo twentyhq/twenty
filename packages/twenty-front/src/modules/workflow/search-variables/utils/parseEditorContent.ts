68200be4461e9a047991f1a6fcbda9d3d0b14349
Thomas Trompette <thomas.trompette@sfr.fr>
2024-10-28 15:31:38 +0100

    Update syntax

--- packages/twenty-front/src/modules/workflow/search-variables/utils/parseEditorContent.ts
+++ packages/twenty-front/src/modules/workflow/search-variables/utils/parseEditorContent.ts
@@ -12,7 +12,7 @@ export const parseEditorContent = (json: JSONContent): string => {
 
     if (node.type === 'text') {
       // Replace &nbsp; with regular space
-      return (node.text || '').replace(/\u00A0/g, ' ');
+      return node?.text?.replace(/\u00A0/g, ' ') ?? '';
     }
 
     if (node.type === 'hardBreak') {
