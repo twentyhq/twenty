c1129bcd130a564a37a0e3cdf51c3f96a4d44b84
Devessier <baptiste@devessier.fr>
2024-10-28 15:54:56 +0100

    feat: simplify things by using a grid layout and use OverflowingTextWithTooltip

--- packages/twenty-front/src/modules/ui/input/components/SelectControl.tsx
+++ packages/twenty-front/src/modules/ui/input/components/SelectControl.tsx
@@ -1,24 +1,31 @@
 import { SelectOption } from '@/ui/input/components/Select';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
-import { IconChevronDown, isDefined } from 'twenty-ui';
+import {
+  IconChevronDown,
+  isDefined,
+  OverflowingTextWithTooltip,
+} from 'twenty-ui';
 
-const StyledControlContainer = styled.div<{ disabled?: boolean }>`
+const StyledControlContainer = styled.div<{
+  disabled?: boolean;
+  hasIcon: boolean;
+}>`
+  display: grid;
+  grid-template-columns: ${({ hasIcon }) =>
+    hasIcon ? 'auto 1fr auto' : '1fr auto'};
   align-items: center;
+  gap: ${({ theme }) => theme.spacing(1)};
+  box-sizing: border-box;
+  height: ${({ theme }) => theme.spacing(8)};
+  max-width: 100%;
+  padding: 0 ${({ theme }) => theme.spacing(2)};
   background-color: ${({ theme }) => theme.background.transparent.lighter};
   border: 1px solid ${({ theme }) => theme.border.color.medium};
-  box-sizing: border-box;
   border-radius: ${({ theme }) => theme.border.radius.sm};
   color: ${({ disabled, theme }) =>
     disabled ? theme.font.color.tertiary : theme.font.color.primary};
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
-  display: flex;
-  gap: ${({ theme }) => theme.spacing(1)};
-  height: ${({ theme }) => theme.spacing(8)};
-  max-width: 100%;
-  padding: 0 ${({ theme }) => theme.spacing(2)};
-  flex-grow: 1;
-  flex-shrink: 1;
 `;
 
 const StyledIconChevronDown = styled(IconChevronDown)<{
@@ -26,20 +33,6 @@ const StyledIconChevronDown = styled(IconChevronDown)<{
 }>`
   color: ${({ disabled, theme }) =>
     disabled ? theme.font.color.extraLight : theme.font.color.tertiary};
-  flex-shrink: 0;
-`;
-
-const StyledIconContainer = styled.span`
-  display: flex;
-  flex-shrink: 0;
-`;
-
-const StyledLabel = styled.span`
-  flex-grow: 1;
-  flex-shrink: 1;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
 `;
 
 type SelectControlProps = {
@@ -54,19 +47,18 @@ export const SelectControl = ({
   const theme = useTheme();
 
   return (
-    <StyledControlContainer disabled={isDisabled}>
+    <StyledControlContainer
+      disabled={isDisabled}
+      hasIcon={isDefined(selectedOption.Icon)}
+    >
       {isDefined(selectedOption.Icon) ? (
-        <StyledIconContainer>
-          <selectedOption.Icon
-            color={
-              isDisabled ? theme.font.color.light : theme.font.color.primary
-            }
-            size={theme.icon.size.md}
-            stroke={theme.icon.stroke.sm}
-          />
-        </StyledIconContainer>
+        <selectedOption.Icon
+          color={isDisabled ? theme.font.color.light : theme.font.color.primary}
+          size={theme.icon.size.md}
+          stroke={theme.icon.stroke.sm}
+        />
       ) : null}
-      <StyledLabel>{selectedOption.label}</StyledLabel>
+      <OverflowingTextWithTooltip text={selectedOption.label} />
       <StyledIconChevronDown disabled={isDisabled} size={theme.icon.size.md} />
     </StyledControlContainer>
   );
