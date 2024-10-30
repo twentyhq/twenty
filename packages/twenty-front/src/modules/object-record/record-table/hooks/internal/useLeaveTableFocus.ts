0bdcc1065c827acf40c6fba3f1af58a64a7533ea
Devessier <baptiste@devessier.fr>
2024-10-30 17:21:39 +0100

    fix: disable record table click outside listener when cell is edited

--- packages/twenty-front/src/modules/object-record/record-table/hooks/internal/useLeaveTableFocus.ts
+++ packages/twenty-front/src/modules/object-record/record-table/hooks/internal/useLeaveTableFocus.ts
@@ -1,10 +1,7 @@
 import { useRecoilCallback } from 'recoil';
 
 import { useRecordTableStates } from '@/object-record/record-table/hooks/internal/useRecordTableStates';
-import { currentHotkeyScopeState } from '@/ui/utilities/hotkey/states/internal/currentHotkeyScopeState';
 import { getSnapshotValue } from '@/ui/utilities/recoil-scope/utils/getSnapshotValue';
-
-import { TableHotkeyScope } from '../../types/TableHotkeyScope';
 
 import { useResetTableRowSelection } from '@/object-record/record-table/hooks/internal/useResetTableRowSelection';
 import { useDisableSoftFocus } from './useDisableSoftFocus';
@@ -24,17 +21,9 @@ export const useLeaveTableFocus = (recordTableId?: string) => {
           isSoftFocusActiveState,
         );
 
-        const currentHotkeyScope = snapshot
-          .getLoadable(currentHotkeyScopeState)
-          .getValue();
-
         resetTableRowSelection();
 
         if (!isSoftFocusActive) {
-          return;
-        }
-
-        if (currentHotkeyScope?.scope === TableHotkeyScope.Table) {
           return;
         }
 
