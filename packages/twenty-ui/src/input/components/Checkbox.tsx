10c58de5e0bd8ad9fdabda7658590e801a9006ba
Karan Parmar <karan.parmar@PE-HO-MAC-185.local>
2024-10-31 16:57:43 +0530

    updated the checks based on the hover conditions

--- packages/twenty-ui/src/input/components/Checkbox.tsx
+++ packages/twenty-ui/src/input/components/Checkbox.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
-import * as React from 'react';
 import { IconCheck, IconMinus } from '@ui/display/icon/components/TablerIcons';
+import * as React from 'react';
 
 export enum CheckboxVariant {
   Primary = 'primary',
@@ -52,8 +52,13 @@ const StyledInputContainer = styled.div<InputProps>`
 
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   display: flex;
-  padding: ${({ theme, checkboxSize }) =>
-    checkboxSize === CheckboxSize.Large ? theme.spacing(1.5) : 0};
+  padding: ${({ theme, checkboxSize, hoverable }) => {
+    if (!hoverable) return 0;
+    
+    return checkboxSize === CheckboxSize.Large 
+      ? theme.spacing(1.5)
+      : theme.spacing(1.25);
+  }};
   position: relative;
   ${({ hoverable, isChecked, theme, indeterminate, disabled }) => {
     if (!hoverable || disabled === true) return '';
