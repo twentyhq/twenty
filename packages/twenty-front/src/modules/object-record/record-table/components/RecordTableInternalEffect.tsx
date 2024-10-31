d0dc8bae4f8f9a9bc1d3bd9dc92f7d9b84cb3168
Baptiste Devessier <baptiste@devessier.fr>
2024-10-31 11:45:40 +0100

    Fix save record table cell when clicking outside table (#8230)

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableInternalEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableInternalEffect.tsx
@@ -1,19 +1,26 @@
 import { Key } from 'ts-key-enum';
 
+import { RECORD_TABLE_CLICK_OUTSIDE_LISTENER_ID } from '@/object-record/record-table/constants/RecordTableClickOutsideListenerId';
+import { useLeaveTableFocus } from '@/object-record/record-table/hooks/internal/useLeaveTableFocus';
 import { useRecordTable } from '@/object-record/record-table/hooks/useRecordTable';
 import { TableHotkeyScope } from '@/object-record/record-table/types/TableHotkeyScope';
 import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
-import { useListenClickOutsideByClassName } from '@/ui/utilities/pointer-event/hooks/useListenClickOutside';
+import { useListenClickOutsideV2 } from '@/ui/utilities/pointer-event/hooks/useListenClickOutsideV2';
 
 type RecordTableInternalEffectProps = {
   recordTableId: string;
+  tableBodyRef: React.RefObject<HTMLDivElement>;
 };
 
 export const RecordTableInternalEffect = ({
   recordTableId,
+  tableBodyRef,
 }: RecordTableInternalEffectProps) => {
-  const { leaveTableFocus, resetTableRowSelection, useMapKeyboardToSoftFocus } =
-    useRecordTable({ recordTableId });
+  const leaveTableFocus = useLeaveTableFocus(recordTableId);
+
+  const { resetTableRowSelection, useMapKeyboardToSoftFocus } = useRecordTable({
+    recordTableId,
+  });
 
   useMapKeyboardToSoftFocus();
 
@@ -25,9 +32,9 @@ export const RecordTableInternalEffect = ({
     TableHotkeyScope.Table,
   );
 
-  useListenClickOutsideByClassName({
-    classNames: ['entity-table-cell'],
-    excludeClassNames: ['bottom-bar', 'action-menu-dropdown', 'command-menu'],
+  useListenClickOutsideV2({
+    listenerId: RECORD_TABLE_CLICK_OUTSIDE_LISTENER_ID,
+    refs: [tableBodyRef],
     callback: () => {
       leaveTableFocus();
     },
