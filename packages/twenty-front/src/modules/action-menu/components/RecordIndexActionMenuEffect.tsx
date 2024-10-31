a75526cb3aca028b036bf3c7a9f5a7e467a6e1a0
bosiraphael <raphael.bosi@gmail.com>
2024-10-31 16:16:25 +0100

    Fix hotkey scope when opening command menu

--- packages/twenty-front/src/modules/action-menu/components/RecordIndexActionMenuEffect.tsx
+++ packages/twenty-front/src/modules/action-menu/components/RecordIndexActionMenuEffect.tsx
@@ -2,10 +2,12 @@ import { useActionMenu } from '@/action-menu/hooks/useActionMenu';
 import { ActionMenuComponentInstanceContext } from '@/action-menu/states/contexts/ActionMenuComponentInstanceContext';
 import { isCommandMenuOpenedState } from '@/command-menu/states/isCommandMenuOpenedState';
 import { contextStoreNumberOfSelectedRecordsComponentState } from '@/context-store/states/contextStoreNumberOfSelectedRecordsComponentState';
+import { isBottomBarOpenedComponentState } from '@/ui/layout/bottom-bar/states/isBottomBarOpenedComponentState';
 import { isDropdownOpenComponentState } from '@/ui/layout/dropdown/states/isDropdownOpenComponentState';
 import { useRightDrawer } from '@/ui/layout/right-drawer/hooks/useRightDrawer';
 import { useAvailableComponentInstanceIdOrThrow } from '@/ui/utilities/state/component-state/hooks/useAvailableComponentInstanceIdOrThrow';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
+import { useSetRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentStateV2';
 import { extractComponentState } from '@/ui/utilities/state/component-state/utils/extractComponentState';
 import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
@@ -20,6 +22,13 @@ export const RecordIndexActionMenuEffect = () => {
   );
 
   const { openActionBar, closeActionBar } = useActionMenu(actionMenuId);
+
+  // Using closeActionBar here was causing a bug because it goes back to the
+  // previous hotkey scope, and we don't want that here.
+  const setIsBottomBarOpened = useSetRecoilComponentStateV2(
+    isBottomBarOpenedComponentState,
+    `action-bar-${actionMenuId}`,
+  );
 
   const isDropdownOpen = useRecoilValue(
     extractComponentState(
@@ -50,9 +59,9 @@ export const RecordIndexActionMenuEffect = () => {
 
   useEffect(() => {
     if (isRightDrawerOpen || isCommandMenuOpened) {
-      closeActionBar();
+      setIsBottomBarOpened(false);
     }
-  }, [closeActionBar, isRightDrawerOpen, isCommandMenuOpened]);
+  }, [isRightDrawerOpen, isCommandMenuOpened, setIsBottomBarOpened]);
 
   return null;
 };
