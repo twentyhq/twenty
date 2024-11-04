90bf5253e54a799113a43014713e5bbbc3e5c6fa
Marie Stoppa <marie.stoppa@essec.edu>
2024-11-04 16:08:08 +0400

    Exclude relations from prefillRecord

--- packages/twenty-front/src/modules/object-record/utils/prefillRecord.ts
+++ packages/twenty-front/src/modules/object-record/utils/prefillRecord.ts
@@ -3,7 +3,7 @@ import { isUndefined } from '@sniptt/guards';
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 import { ObjectRecord } from '@/object-record/types/ObjectRecord';
 import { generateDefaultFieldValue } from '@/object-record/utils/generateDefaultFieldValue';
-import { FieldMetadataType, RelationDefinitionType } from '~/generated/graphql';
+import { FieldMetadataType } from '~/generated-metadata/graphql';
 import { isDefined } from '~/utils/isDefined';
 
 export const prefillRecord = <T extends ObjectRecord>({
@@ -15,24 +15,9 @@ export const prefillRecord = <T extends ObjectRecord>({
 }) => {
   return Object.fromEntries(
     objectMetadataItem.fields
+      .filter((field) => field.type !== FieldMetadataType.Relation)
       .map((fieldMetadataItem) => {
-        let inputValue = input[fieldMetadataItem.name];
-        if (
-          !inputValue &&
-          fieldMetadataItem.type === FieldMetadataType.Relation &&
-          fieldMetadataItem.relationDefinition?.direction ===
-            RelationDefinitionType.ManyToOne
-        ) {
-          const relationIdFieldName = `${fieldMetadataItem.name}Id`;
-          const relationIdFieldMetadataItem = objectMetadataItem.fields.find(
-            (field) => field.name === relationIdFieldName,
-          );
-          inputValue = input[relationIdFieldName];
-
-          return relationIdFieldMetadataItem
-            ? [relationIdFieldName, inputValue]
-            : undefined;
-        }
+        const inputValue = input[fieldMetadataItem.name];
 
         return [
           fieldMetadataItem.name,
