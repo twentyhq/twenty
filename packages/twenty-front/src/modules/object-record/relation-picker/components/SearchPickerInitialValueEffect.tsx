b6fd35a44ff46d3475c80bc3ba90efe94d798d7b
Marie Stoppa <marie.stoppa@essec.edu>
2024-11-04 12:53:01 +0400

    wip

---
+++ packages/twenty-front/src/modules/object-record/relation-picker/components/SearchPickerInitialValueEffect.tsx
@@ -0,0 +1,29 @@
+import { getRelationPickerScopedStates } from '@/object-record/relation-picker/utils/getRelationPickerScopedStates';
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+
+export const SearchPickerInitialValueEffect = ({
+  initialValueForSearchFilter,
+  relationPickerScopeId,
+}: {
+  initialValueForSearchFilter?: string | null;
+  relationPickerScopeId: string;
+}) => {
+  const { relationPickerSearchFilterState } = getRelationPickerScopedStates({
+    relationPickerScopeId: relationPickerScopeId,
+  });
+
+  const setRelationPickerSearchFilter = useSetRecoilState(
+    relationPickerSearchFilterState,
+  );
+
+  useEffect(() => {
+    console.log(
+      'setRelationPickerSearchFilter from Effect',
+      initialValueForSearchFilter ?? '',
+    );
+    setRelationPickerSearchFilter(initialValueForSearchFilter);
+  }, [initialValueForSearchFilter, setRelationPickerSearchFilter]);
+
+  return <></>;
+};
--- packages/twenty-front/src/modules/object-record/relation-picker/hooks/useEntitySelectSearch.ts
+++ packages/twenty-front/src/modules/object-record/relation-picker/hooks/useEntitySelectSearch.ts
@@ -26,6 +26,7 @@ export const useEntitySelectSearch = ({
   const handleSearchFilterChange = (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
+    console.log('set search filter', event.currentTarget.value);
     debouncedSetSearchFilter(event.currentTarget.value);
     setRelationPickerPreselectedId('');
   };
