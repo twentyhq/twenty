d35f4fe36f5a148cd156c40f3a2a5766e1a63853
Marie Stoppa <marie.stoppa@essec.edu>
2024-11-04 17:02:57 +0400

    Update prefillrecord api to expect full relation data

--- packages/twenty-front/src/modules/object-record/utils/prefillRecord.ts
+++ packages/twenty-front/src/modules/object-record/utils/prefillRecord.ts
@@ -1,9 +1,10 @@
 import { isUndefined } from '@sniptt/guards';
 
+import { FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 import { ObjectRecord } from '@/object-record/types/ObjectRecord';
 import { generateDefaultFieldValue } from '@/object-record/utils/generateDefaultFieldValue';
-import { FieldMetadataType } from '~/generated-metadata/graphql';
+import { FieldMetadataType, RelationDefinitionType } from '~/generated/graphql';
 import { isDefined } from '~/utils/isDefined';
 
 export const prefillRecord = <T extends ObjectRecord>({
@@ -15,9 +16,15 @@ export const prefillRecord = <T extends ObjectRecord>({
 }) => {
   return Object.fromEntries(
     objectMetadataItem.fields
-      .filter((field) => field.type !== FieldMetadataType.Relation)
       .map((fieldMetadataItem) => {
         const inputValue = input[fieldMetadataItem.name];
+        if (
+          fieldMetadataItem.type === FieldMetadataType.Relation &&
+          fieldMetadataItem.relationDefinition?.direction ===
+            RelationDefinitionType.ManyToOne
+        ) {
+          throwIfInputRelationDataIsInconsistent(input, fieldMetadataItem);
+        }
 
         return [
           fieldMetadataItem.name,
@@ -28,4 +35,17 @@ export const prefillRecord = <T extends ObjectRecord>({
       })
       .filter(isDefined),
   ) as T;
+};
+
+const throwIfInputRelationDataIsInconsistent = (
+  input: Record<string, unknown>,
+  fieldMetadataItem: FieldMetadataItem,
+) => {
+  const inputValue = input[fieldMetadataItem.name];
+  const relationIdFieldName = `${fieldMetadataItem.name}Id`;
+  if (isDefined(inputValue) && !isDefined(input[relationIdFieldName])) {
+    throw new Error(
+      `Inconsistent input: ${fieldMetadataItem.name} is specified but ${relationIdFieldName} is missing`,
+    );
+  }
 };
