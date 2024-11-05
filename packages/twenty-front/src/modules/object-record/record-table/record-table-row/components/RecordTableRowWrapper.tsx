3793f6c4512bdf10efb7a4e51189f58dbf663ee0
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2024-11-05 20:33:24 +0530

    fix: content getting hide on drag and drop in stage view cards (#7621)
    ## ISSUE
    
    -  Closes #7388 
    
    ## Demo
    
    
    
    https://github.com/user-attachments/assets/193813aa-def9-406b-9fe7-397627bb1242
    
    - [ ] Table Row Drag WIP
    
    ---------
    
    Co-authored-by: Lucas Bordeau <bordeau.lucas@gmail.com>

--- packages/twenty-front/src/modules/object-record/record-table/record-table-row/components/RecordTableRowWrapper.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-row/components/RecordTableRowWrapper.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from '@emotion/react';
 import { Draggable } from '@hello-pangea/dnd';
-import { ReactNode, useContext, useEffect } from 'react';
+import { ReactNode, useContext, useEffect, useRef } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { getBasePathToShowPage } from '@/object-metadata/utils/getBasePathToShowPage';
@@ -9,8 +9,10 @@ import { RecordTableContext } from '@/object-record/record-table/contexts/Record
 import { RecordTableRowContext } from '@/object-record/record-table/contexts/RecordTableRowContext';
 import { RecordTableTr } from '@/object-record/record-table/record-table-row/components/RecordTableTr';
 import { isRowSelectedComponentFamilyState } from '@/object-record/record-table/record-table-row/states/isRowSelectedComponentFamilyState';
+import { tableCellWidthsComponentState } from '@/object-record/record-table/states/tableCellWidthsComponentState';
 import { RecordTableWithWrappersScrollWrapperContext } from '@/ui/utilities/scroll/contexts/ScrollWrapperContexts';
 import { useRecoilComponentFamilyValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentFamilyValueV2';
+import { useRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentStateV2';
 
 export const RecordTableRowWrapper = ({
   recordId,
@@ -23,6 +25,8 @@ export const RecordTableRowWrapper = ({
   isPendingRow?: boolean;
   children: ReactNode;
 }) => {
+  const trRef = useRef<HTMLTableRowElement>(null);
+
   const { objectMetadataItem } = useContext(RecordTableContext);
   const { onIndexRecordsLoaded } = useContext(RecordIndexRootPropsContext);
 
@@ -44,6 +48,24 @@ export const RecordTableRowWrapper = ({
     rootMargin: '1000px',
   });
 
+  const [, setTableCellWidths] = useRecoilComponentStateV2(
+    tableCellWidthsComponentState,
+  );
+
+  useEffect(() => {
+    if (rowIndex === 0) {
+      const tdArray = Array.from(
+        trRef.current?.getElementsByTagName('td') ?? [],
+      );
+
+      const tdWidths = tdArray.map((td) => {
+        return td.getBoundingClientRect().width;
+      });
+
+      setTableCellWidths(tdWidths);
+    }
+  }, [trRef, rowIndex, setTableCellWidths]);
+
   // TODO: find a better way to emit this event
   useEffect(() => {
     if (inView) {
@@ -56,6 +78,8 @@ export const RecordTableRowWrapper = ({
       {(draggableProvided, draggableSnapshot) => (
         <RecordTableTr
           ref={(node) => {
+            // @ts-expect-error - TS doesn't know that node.current is assignable
+            trRef.current = node;
             elementRef(node);
             draggableProvided.innerRef(node);
           }}
