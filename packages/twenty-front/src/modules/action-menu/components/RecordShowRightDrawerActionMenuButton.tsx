bc59d9c8ab15a671aa0b33ae8bb37407b1a38b4e
bosiraphael <raphael.bosi@gmail.com>
2024-11-06 11:54:06 +0100

    add shortcut

--- packages/twenty-front/src/modules/action-menu/components/RecordShowRightDrawerActionMenuButton.tsx
+++ packages/twenty-front/src/modules/action-menu/components/RecordShowRightDrawerActionMenuButton.tsx
@@ -5,9 +5,11 @@ import { Dropdown } from '@/ui/layout/dropdown/components/Dropdown';
 import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/DropdownMenuItemsContainer';
 import { useDropdownV2 } from '@/ui/layout/dropdown/hooks/useDropdownV2';
 import { MenuItem } from '@/ui/navigation/menu-item/components/MenuItem';
+import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
 import { useAvailableComponentInstanceIdOrThrow } from '@/ui/utilities/state/component-state/hooks/useAvailableComponentInstanceIdOrThrow';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useTheme } from '@emotion/react';
+import { Key } from 'ts-key-enum';
 import { Button } from 'twenty-ui';
 
 export const RecordShowRightDrawerActionMenuButton = () => {
@@ -19,9 +21,31 @@ export const RecordShowRightDrawerActionMenuButton = () => {
     ActionMenuComponentInstanceContext,
   );
 
-  const { closeDropdown } = useDropdownV2();
+  const { closeDropdown, toggleDropdown } = useDropdownV2();
 
   const theme = useTheme();
+
+  useScopedHotkeys(
+    [Key.Escape],
+    () => {
+      closeDropdown(
+        `record-show-right-drawer-action-menu-dropdown-${actionMenuId}`,
+      );
+    },
+    RecordShowRightDrawerActionMenuButtonHotkeyScope.RecordShowRightDrawerActionMenuButton,
+    [],
+  );
+
+  useScopedHotkeys(
+    ['ctrl+o,meta+o'],
+    () => {
+      toggleDropdown(
+        `record-show-right-drawer-action-menu-dropdown-${actionMenuId}`,
+      );
+    },
+    RecordShowRightDrawerActionMenuButtonHotkeyScope.RecordShowRightDrawerActionMenuButton,
+    [],
+  );
 
   return (
     <Dropdown
