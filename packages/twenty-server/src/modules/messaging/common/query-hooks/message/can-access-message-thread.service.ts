92848f6e4799adc8417ce26c6239d5839e05dea5
bosiraphael <raphael.bosi@gmail.com>
2024-11-07 16:47:22 +0100

    add limit to the query

--- packages/twenty-server/src/modules/messaging/common/query-hooks/message/can-access-message-thread.service.ts
+++ packages/twenty-server/src/modules/messaging/common/query-hooks/message/can-access-message-thread.service.ts
@@ -56,6 +56,7 @@ export class CanAccessMessageThreadService {
           accountOwnerId: currentWorkspaceMember.id,
         },
       ],
+      take: 1,
     });
 
     if (connectedAccounts.length === 0) {
