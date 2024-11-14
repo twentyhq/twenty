add4a58f4e6a71e516ad227e11970c2fbac05c07
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-11-14 13:23:34 +0100

    Fix

--- packages/twenty-server/src/engine/api/graphql/workspace-query-runner/factories/query-result-getters/utils/is-query-result-field-value-a-connection.guard.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-query-runner/factories/query-result-getters/utils/is-query-result-field-value-a-connection.guard.ts
@@ -1,5 +1,3 @@
-import { isDefined } from 'class-validator';
-
 import { Record as ObjectRecord } from 'src/engine/api/graphql/workspace-query-builder/interfaces/record.interface';
 import { QueryResultFieldValue } from 'src/engine/api/graphql/workspace-query-runner/factories/query-result-getters/interfaces/query-result-field-value';
 import { IConnection } from 'src/engine/api/graphql/workspace-query-runner/interfaces/connection.interface';
@@ -8,5 +6,5 @@ import { IEdge } from 'src/engine/api/graphql/workspace-query-runner/interfaces/
 export const isQueryResultFieldValueAConnection = (
   result: QueryResultFieldValue,
 ): result is IConnection<ObjectRecord, IEdge<ObjectRecord>> => {
-  return isDefined((result as any).edges);
+  return 'edges' in result && Array.isArray(result.edges);
 };
