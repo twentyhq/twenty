541edde8cc635577e0230137227f8e4c132efc15
Félix Malfait <felix@twenty.com>
2024-11-14 22:06:13 +0100

    Fix entrypoint

--- packages/twenty-docker/twenty/entrypoint.sh
+++ packages/twenty-docker/twenty/entrypoint.sh
@@ -7,8 +7,9 @@ if [ "${ENABLE_DB_MIGRATIONS}" = "true" ] && [ ! -f /app/docker-data/db_status ]
     # Creating the database if it doesn't exist
     PGUSER=$(echo $PG_DATABASE_URL | awk -F '//' '{print $2}' | awk -F ':' '{print $1}')
     PGPASS=$(echo $PG_DATABASE_URL | awk -F ':' '{print $3}' | awk -F '@' '{print $1}')
-    PGPASSWORD=${PGPASS} psql -h localhost -p 5432 -U ${PGUSER} -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'default'" | grep -q 1 || \
-    PGPASSWORD=${PGPASS} psql -h localhost -p 5432 -U ${PGUSER} -d postgres -c "CREATE DATABASE \"default\""
+    PGHOST=$(echo $PG_DATABASE_URL | awk -F '@' '{print $2}' | awk -F '/' '{print $1}')
+    PGPASSWORD=${PGPASSWORD_SUPERUSER} psql -h ${PGHOST} -p 5432 -U ${PGUSER_SUPERUSER} -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'default'" | grep -q 1 || \
+    PGPASSWORD=${PGPASSWORD_SUPERUSER} psql -h ${PGHOST} -p 5432 -U ${PGUSER_SUPERUSER} -d postgres -c "CREATE DATABASE \"default\""
 
     # Run setup and migration scripts
     NODE_OPTIONS="--max-old-space-size=1500" tsx ./scripts/setup-db.ts
