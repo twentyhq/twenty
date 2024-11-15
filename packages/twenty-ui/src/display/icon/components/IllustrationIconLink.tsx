cac20d72387cc23de2e0a9032dcbad67e440583f
ehconitin <nitinkoche03@gmail.com>
2024-11-15 15:34:42 +0530

    fix

--- packages/twenty-ui/src/display/icon/components/IllustrationIconLink.tsx
+++ packages/twenty-ui/src/display/icon/components/IllustrationIconLink.tsx
@@ -4,25 +4,20 @@ import { IllustrationIconWrapper } from '@ui/display/icon/components/Illustratio
 import IllustrationIconLinkRaw from '@ui/display/icon/assets/illustration-link.svg?react';
 import { IconComponentProps } from '@ui/display/icon/types/IconComponent';
 
-type IllustrationColorType = 'grey' | 'blue';
-
-type IllustrationIconLinkProps = Pick<IconComponentProps, 'size'> & {
-  colorScheme?: IllustrationColorType;
-};
+type IllustrationIconLinkProps = Pick<IconComponentProps, 'size'>;
 
 export const IllustrationIconLink = (props: IllustrationIconLinkProps) => {
   const theme = useTheme();
   const size = props.size ?? theme.icon.size.lg;
   const { color, fill } = theme.IllustrationIcon;
-  const colorScheme = props.colorScheme ?? 'blue';
 
   return (
     <IllustrationIconWrapper>
       <IllustrationIconLinkRaw
         height={size}
         width={size}
-        fill={fill[colorScheme]}
-        color={color[colorScheme]}
+        fill={fill.blue}
+        color={color.blue}
       />
     </IllustrationIconWrapper>
   );
