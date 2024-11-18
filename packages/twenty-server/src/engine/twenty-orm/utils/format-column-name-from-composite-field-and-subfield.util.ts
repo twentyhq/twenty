c925525809c19289ac9e94ae688efbeca3c37b57
Weiko <corentin@twenty.com>
2024-11-18 11:25:31 +0100

    add type

--- packages/twenty-server/src/engine/twenty-orm/utils/format-column-name-from-composite-field-and-subfield.util.ts
+++ packages/twenty-server/src/engine/twenty-orm/utils/format-column-name-from-composite-field-and-subfield.util.ts
@@ -4,7 +4,7 @@ import { isDefined } from 'src/utils/is-defined';
 export const formatColumnNameFromCompositeFieldAndSubfield = (
   fieldName: string,
   subFieldName?: string,
-) => {
+): string => {
   if (isDefined(subFieldName)) {
     return `${fieldName}${capitalize(subFieldName)}`;
   }
