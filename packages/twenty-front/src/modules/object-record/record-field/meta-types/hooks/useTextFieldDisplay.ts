2f3c41620c0d0ae3b3963da3c8f0c689eaa08967
Guillim <guillim@users.noreply.github.com>
2024-11-18 17:36:19 +0100

    Wrap Long text fields (textarea) (#8557)
    Here we add the option for Text inputs to be wrapped, and to select on
    how many lines text should be displayed.
    
    Fix #7552
    
    ---------
    
    Co-authored-by: guillim <guillaume@twenty.com>

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/hooks/useTextFieldDisplay.ts
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/hooks/useTextFieldDisplay.ts
@@ -2,11 +2,15 @@ import { useContext } from 'react';
 
 import { useRecordFieldValue } from '@/object-record/record-store/contexts/RecordFieldValueSelectorContext';
 
+import { assertFieldMetadata } from '@/object-record/record-field/types/guards/assertFieldMetadata';
+import { isFieldText } from '@/object-record/record-field/types/guards/isFieldText';
+import { FieldMetadataType } from '~/generated-metadata/graphql';
 import { FieldContext } from '../../contexts/FieldContext';
 
 export const useTextFieldDisplay = () => {
   const { recordId, fieldDefinition, hotkeyScope } = useContext(FieldContext);
 
+  assertFieldMetadata(FieldMetadataType.Text, isFieldText, fieldDefinition);
   const fieldName = fieldDefinition.metadata.fieldName;
 
   const fieldValue =
