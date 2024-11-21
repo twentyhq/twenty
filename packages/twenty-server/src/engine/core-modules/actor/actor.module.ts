763deb45a4f6b03144551c4fcde67045b7aa9af4
Weiko <corentin@twenty.com>
2024-11-21 22:45:36 +0100

    fix createdBy prehook

--- packages/twenty-server/src/engine/core-modules/actor/actor.module.ts
+++ packages/twenty-server/src/engine/core-modules/actor/actor.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { CreatedByPreQueryHook } from 'src/engine/core-modules/actor/query-hooks/created-by.pre-query-hook';
+import { CreatedByCreateManyPreQueryHook } from 'src/engine/core-modules/actor/query-hooks/created-by.create-many.pre-query-hook';
+import { CreatedByCreateOnePreQueryHook } from 'src/engine/core-modules/actor/query-hooks/created-by.create-one.pre-query-hook';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([FieldMetadataEntity], 'metadata')],
-  providers: [CreatedByPreQueryHook],
-  exports: [CreatedByPreQueryHook],
+  providers: [CreatedByCreateManyPreQueryHook, CreatedByCreateOnePreQueryHook],
+  exports: [CreatedByCreateManyPreQueryHook, CreatedByCreateOnePreQueryHook],
 })
 export class ActorModule {}
