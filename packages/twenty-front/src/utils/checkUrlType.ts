bd110a5beaf3ec0f7d5b33944cddb8ce5ea460a7
martmull <martmull@hotmail.fr>
2024-11-22 11:16:12 +0100

    Fix test

--- packages/twenty-front/src/utils/checkUrlType.ts
+++ packages/twenty-front/src/utils/checkUrlType.ts
@@ -1,10 +1,13 @@
 import { LinkType } from 'twenty-ui';
 
 export const checkUrlType = (url: string) => {
-  if (/^(https?:\/\/)?(www\.)?linkedin\.com(\/[\w\-/?#:.%=&]*)?$/.test(url)) {
+  if (/^(https?:\/\/)?(www\.)?linkedin\.com\/.+$/.test(url)) {
     return LinkType.LinkedIn;
   }
-  if (/^(https?:\/\/)?(www\.)?twitter\.com(\/[\w\-/?#:.%=&]*)?$/.test(url)) {
+  if (/^(https?:\/\/)?(www\.)?twitter\.com\/.+$/.test(url)) {
+    return LinkType.Twitter;
+  }
+  if (/^(https?:\/\/)?(www\.)?x\.com\/.+$/.test(url)) {
     return LinkType.Twitter;
   }
 
