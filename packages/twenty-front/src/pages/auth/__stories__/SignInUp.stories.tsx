8e6e42b3c4e5232c31ac05ad5751c7acacb7087a
Antoine Moreaux <antoine@specifyapp.com>
2024-11-22 11:29:22 +0100

    feat(auth): Add mock handler for workspace data query
    Added `GET_PUBLIC_WORKSPACE_DATA_BY_SUBDOMAIN` query mock handler to return mock workspace data. Also updated the button text check in SignInUp stories for consistency.

--- packages/twenty-front/src/pages/auth/__stories__/SignInUp.stories.tsx
+++ packages/twenty-front/src/pages/auth/__stories__/SignInUp.stories.tsx
@@ -12,6 +12,7 @@ import { graphqlMocks } from '~/testing/graphqlMocks';
 
 import { AppPath } from '@/types/AppPath';
 import { SignInUp } from '../SignInUp';
+import { GET_PUBLIC_WORKSPACE_DATA_BY_SUBDOMAIN } from '@/auth/graphql/queries/getPublicWorkspaceDataBySubdomain';
 
 const meta: Meta<PageDecoratorArgs> = {
   title: 'Pages/Auth/SignInUp',
@@ -21,6 +22,25 @@ const meta: Meta<PageDecoratorArgs> = {
   parameters: {
     msw: {
       handlers: [
+        graphql.query(
+          getOperationName(GET_PUBLIC_WORKSPACE_DATA_BY_SUBDOMAIN) ?? '',
+          () => {
+            return HttpResponse.json({
+              data: {
+                id: 'id',
+                logo: 'logo',
+                displayName: 'displayName',
+                authProviders: {
+                  google: true,
+                  microsoft: false,
+                  password: true,
+                  magicLink: false,
+                  sso: [],
+                },
+              },
+            });
+          },
+        ),
         graphql.query(getOperationName(GET_CURRENT_USER) ?? '', () => {
           return HttpResponse.json({
             data: null,
@@ -52,7 +72,9 @@ export type Story = StoryObj<typeof SignInUp>;
 export const Default: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const continueWithEmailButton = await canvas.findByText('Continue');
+    const continueWithEmailButton = await canvas.findByText(
+      'Continue With Email',
+    );
 
     await fireEvent.click(continueWithEmailButton);
   },
