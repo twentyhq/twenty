36a9bf879251fd05090652a99514431c58e812c3
Antoine Moreaux <antoine@specifyapp.com>
2024-11-25 10:27:15 +0100

    refactor(Logo): remove size prop and use theme spacing
    Simplified the Logo component by removing the size prop and replacing it with theme-based spacing values. This change reduces complexity and improves consistency with other styled components that use theme spacing.

--- packages/twenty-front/src/modules/auth/components/Logo.tsx
+++ packages/twenty-front/src/modules/auth/components/Logo.tsx
@@ -1,48 +1,39 @@
-import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { getImageAbsoluteURI } from '~/utils/image/getImageAbsoluteURI';
-import { Spacing } from 'twenty-ui';
 
 type LogoProps = {
   primaryLogo?: string | null;
   secondaryLogo?: string | null;
-  size?: Spacing | null;
 };
 
-const StyledContainer = styled.div<{ size: number }>`
-  height: ${({ size }) => size}px;
+const StyledContainer = styled.div`
+  height: ${({ theme }) => theme.spacing(4)};
   margin-bottom: ${({ theme }) => theme.spacing(4)};
   margin-top: ${({ theme }) => theme.spacing(4)};
 
   position: relative;
-  width: ${({ size }) => size}px;
+  width: ${({ theme }) => theme.spacing(4)};
 `;
 
-const StyledSecondaryLogo = styled.img<{ size: number }>`
+const StyledSecondaryLogo = styled.img`
   border-radius: ${({ theme }) => theme.border.radius.xs};
-  height: calc(${({ size }) => size}px / 2);
-  width: calc(${({ size }) => size}px / 2);
+  height: calc(${({ theme }) => theme.spacing(4)} / 2);
+  width: calc(${({ theme }) => theme.spacing(4)} / 2);
 `;
 
-const StyledSecondaryLogoContainer = styled.div<{ size: number }>`
+const StyledSecondaryLogoContainer = styled.div`
   align-items: center;
   background-color: ${({ theme }) => theme.background.primary};
   border-radius: ${({ theme }) => theme.border.radius.sm};
-  bottom: calc(
-    (${({ theme }) => theme.spacing(3)} * -1) * ${({ size }) => size} / 48
-  );
+  bottom: ${({ theme }) => `-${theme.spacing(3)}`};
   display: flex;
-  height: calc(
-    (${({ theme }) => theme.spacing(7)} * ${({ size }) => size}) / 48
-  );
+  height: 28px;
   justify-content: center;
 
   position: absolute;
-  right: calc(
-    (${({ theme }) => theme.spacing(3)} * -1) * ${({ size }) => size} / 48
-  );
-  width: calc(${({ theme }) => theme.spacing(7)} * ${({ size }) => size} / 48);
+  right: ${({ theme }) => `-${theme.spacing(3)}`};
+  width: 28px;
 `;
 
 const StyledPrimaryLogo = styled.div<{ src: string }>`
@@ -53,10 +44,6 @@ const StyledPrimaryLogo = styled.div<{ src: string }>`
 `;
 
 export const Logo = (props: LogoProps) => {
-  const theme = useTheme();
-
-  const size = props.size ?? theme.spacing(12);
-
   const defaultPrimaryLogoUrl = `${window.location.origin}/icons/android/android-launchericon-192-192.png`;
 
   const primaryLogoUrl = getImageAbsoluteURI(
@@ -64,14 +51,12 @@ export const Logo = (props: LogoProps) => {
   );
   const secondaryLogoUrl = getImageAbsoluteURI(props.secondaryLogo);
 
-  const sizeInNumber = parseInt(size, 10);
-
   return (
-    <StyledContainer size={sizeInNumber}>
+    <StyledContainer>
       <StyledPrimaryLogo src={primaryLogoUrl} />
       {secondaryLogoUrl && (
-        <StyledSecondaryLogoContainer size={sizeInNumber}>
-          <StyledSecondaryLogo size={sizeInNumber} src={secondaryLogoUrl} />
+        <StyledSecondaryLogoContainer>
+          <StyledSecondaryLogo src={secondaryLogoUrl} />
         </StyledSecondaryLogoContainer>
       )}
     </StyledContainer>
