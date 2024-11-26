d3ffbaba30bc92fba0c32d2acdfa774be08b76e6
Weiko <corentin@twenty.com>
2024-11-26 11:03:27 +0100

    Set missing labelIdentifier to custom objects (#8750)
    ## Context
    Following https://github.com/twentyhq/twenty/pull/8729
    
    This command backfills missing labelIdentifier for custom objects

--- packages/twenty-server/src/database/commands/upgrade-version/0-33/0-33-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-33/0-33-upgrade-version.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DeleteViewFieldsWithoutViewsCommand } from 'src/database/commands/upgrade-version/0-33/0-33-delete-view-fields-without-views.command';
 import { EnforceUniqueConstraintsCommand } from 'src/database/commands/upgrade-version/0-33/0-33-enforce-unique-constraints.command';
+import { SetMissingLabelIdentifierToCustomObjectsCommand } from 'src/database/commands/upgrade-version/0-33/0-33-set-missing-label-identifier-to-custom-objects.command';
 import { UpdateRichTextSearchVectorCommand } from 'src/database/commands/upgrade-version/0-33/0-33-update-rich-text-search-vector-expression';
 import { UpgradeTo0_33Command } from 'src/database/commands/upgrade-version/0-33/0-33-upgrade-version.command';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
@@ -28,6 +29,7 @@ import { WorkspaceSyncMetadataCommandsModule } from 'src/engine/workspace-manage
     UpdateRichTextSearchVectorCommand,
     EnforceUniqueConstraintsCommand,
     DeleteViewFieldsWithoutViewsCommand,
+    SetMissingLabelIdentifierToCustomObjectsCommand,
   ],
 })
 export class UpgradeTo0_33CommandModule {}
