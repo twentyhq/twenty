726bb56bb63c09cea4af5b944e9cd5ba3e9c6a8b
Weiko <corentin@twenty.com>
2024-11-26 10:40:17 +0100

    add delete view fields without views command (#8728)
    ## Context
    We recently added a command to ensure uniqueness on the viewId column in
    the viewField table. This created some issues for some old workspaces
    that had viewFields with an empty viewId.
    This command should get rid of those and set the column as non-nullable.
    Also updating the onDelete action accordingly and set one missing for
    FavoriteFolder

--- packages/twenty-server/src/database/commands/upgrade-version/0-33/0-33-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-33/0-33-upgrade-version.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { DeleteViewFieldsWithoutViewsCommand } from 'src/database/commands/upgrade-version/0-33/0-33-delete-view-fields-without-views.command';
 import { EnforceUniqueConstraintsCommand } from 'src/database/commands/upgrade-version/0-33/0-33-enforce-unique-constraints.command';
 import { UpdateRichTextSearchVectorCommand } from 'src/database/commands/upgrade-version/0-33/0-33-update-rich-text-search-vector-expression';
 import { UpgradeTo0_33Command } from 'src/database/commands/upgrade-version/0-33/0-33-upgrade-version.command';
@@ -26,6 +27,7 @@ import { WorkspaceSyncMetadataCommandsModule } from 'src/engine/workspace-manage
     UpgradeTo0_33Command,
     UpdateRichTextSearchVectorCommand,
     EnforceUniqueConstraintsCommand,
+    DeleteViewFieldsWithoutViewsCommand,
   ],
 })
 export class UpgradeTo0_33CommandModule {}
