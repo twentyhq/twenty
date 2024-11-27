7ede48b28b5ffd269216b9830b454ab6028c9c62
guillim <guillaume@twenty.com>
2024-11-27 14:45:10 +0100

    removing useless fct

--- packages/twenty-front/src/utils/string/applySimpleQuotesToString.ts
+++ packages/twenty-front/src/utils/string/applySimpleQuotesToString.ts
@@ -1,35 +1,3 @@
 export const applySimpleQuotesToString = <T extends string>(
   value: T,
 ): `'${T}'` => `'${value}'`;
-
-export const applySimpleQuotesToStringRecursive = (obj: any): any => {
-  if (typeof obj === 'string') {
-    if (obj === '') {
-      return "''";
-    }
-    if (obj === "''") {
-      return "''";
-    }
-
-    obj = applySimpleQuotesToString(obj);
-
-    if (obj.startsWith("''") === true) {
-      obj = obj.slice(1);
-    }
-    if (obj.endsWith("''") === true) {
-      obj = obj.slice(0, -1);
-    }
-    return obj;
-  } else if (Array.isArray(obj)) {
-    return obj.map(applySimpleQuotesToStringRecursive);
-  } else if (typeof obj === 'object' && obj !== null) {
-    const newObj: any = {};
-    for (const key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key) === true) {
-        newObj[key] = applySimpleQuotesToStringRecursive(obj[key]);
-      }
-    }
-    return newObj;
-  }
-  return obj;
-};
