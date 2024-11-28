846042203b3009fb67e27fe8e83f61b526080371
Devessier <baptiste@devessier.fr>
2024-11-28 17:29:32 +0100

    refactor: write some more tests

---
+++ packages/twenty-front/src/utils/__tests__/castToString.test.ts
@@ -0,0 +1,25 @@
+import { castToString } from '../castToString';
+
+it('returns an empty string when undefined is provided', () => {
+  expect(castToString(undefined)).toBe('');
+});
+
+it('returns an empty string when null is provided', () => {
+  expect(castToString(undefined)).toBe('');
+});
+
+it('returns an empty string when an empty string is provided', () => {
+  expect(castToString('')).toBe('');
+});
+
+it('preserves strings', () => {
+  expect(castToString('test')).toBe('test');
+});
+
+it('casts numbers to strings', () => {
+  expect(castToString(21)).toBe('21');
+});
+
+it('casts booleans to strings', () => {
+  expect(castToString(true)).toBe('true');
+});
