4fc5f5810e4e645c9297c9c9d3d079b475bf23ab
martmull <martmull@hotmail.fr>
2024-12-02 21:45:20 +0100

    Fix delete trigger

--- packages/twenty-front/src/modules/workflow/hooks/useDeleteStep.ts
+++ packages/twenty-front/src/modules/workflow/hooks/useDeleteStep.ts
@@ -1,7 +1,13 @@
-import { WorkflowWithCurrentVersion } from '@/workflow/types/Workflow';
+import {
+  WorkflowWithCurrentVersion,
+  WorkflowVersion,
+} from '@/workflow/types/Workflow';
 import { useDeleteWorkflowVersionStep } from '@/workflow/hooks/useDeleteWorkflowVersionStep';
 import { useGetUpdatableWorkflowVersion } from '@/workflow/hooks/useGetUpdatableWorkflowVersion';
 import { useRightDrawer } from '@/ui/layout/right-drawer/hooks/useRightDrawer';
+import { TRIGGER_STEP_ID } from '@/workflow/constants/TriggerStepId';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { useUpdateOneRecord } from '@/object-record/hooks/useUpdateOneRecord';
 
 export const useDeleteStep = ({
   workflow,
@@ -9,6 +15,10 @@ export const useDeleteStep = ({
   workflow: WorkflowWithCurrentVersion;
 }) => {
   const { deleteWorkflowVersionStep } = useDeleteWorkflowVersionStep();
+  const { updateOneRecord: updateOneWorkflowVersion } =
+    useUpdateOneRecord<WorkflowVersion>({
+      objectNameSingular: CoreObjectNameSingular.WorkflowVersion,
+    });
 
   const { getUpdatableWorkflowVersion } = useGetUpdatableWorkflowVersion();
   const { closeRightDrawer } = useRightDrawer();
@@ -16,6 +26,15 @@ export const useDeleteStep = ({
   const deleteStep = async (stepId: string) => {
     closeRightDrawer();
     const workflowVersion = await getUpdatableWorkflowVersion(workflow);
+    if (stepId === TRIGGER_STEP_ID) {
+      await updateOneWorkflowVersion({
+        idToUpdate: workflow.currentVersion.id,
+        updateOneRecordInput: {
+          trigger: null,
+        },
+      });
+      return;
+    }
     await deleteWorkflowVersionStep({
       workflowVersionId: workflowVersion.id,
       stepId,
