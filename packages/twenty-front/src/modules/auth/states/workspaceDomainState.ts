b02097baf0c5eaa70f34ab14fa90699d7aebbfb0
Antoine Moreaux <moreaux.antoine@gmail.com>
2024-12-03 18:00:04 +0100

    refactor(auth, domain): rename lastAuthenticateWorkspaceState
    Renamed `lastAuthenticateWorkspaceState` to `workspaceDomainState` for clearer context and consistency across modules. Updated imports, variable references, and renamed related files. This change improves code readability by aligning state names with their purpose.

--- packages/twenty-front/src/modules/auth/states/workspaceDomainState.ts
+++ packages/twenty-front/src/modules/auth/states/workspaceDomainState.ts
@@ -2,16 +2,16 @@ import { cookieStorageEffect } from '~/utils/recoil-effects';
 import { Workspace } from '~/generated/graphql';
 import { createState } from 'twenty-ui';
 
-export const lastAuthenticateWorkspaceState = createState<
+export const workspaceDomainState = createState<
   | (Pick<Workspace, 'id' | 'subdomain'> & {
       cookieAttributes?: Cookies.CookieAttributes;
     })
   | null
 >({
-  key: 'lastAuthenticateWorkspaceState',
+  key: 'workspaceDomainState',
   defaultValue: null,
   effects: [
-    cookieStorageEffect('lastAuthenticateWorkspace', {
+    cookieStorageEffect('lastWorkspaceDomainState', {
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 year
     }),
   ],
