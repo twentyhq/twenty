eacb00b49e294f954584766eeac22dd50ff50be3
Devessier <baptiste@devessier.fr>
2024-12-03 11:15:37 +0100

    fix: as we rely on a single React state, calling onCancel will override the next draft value we set in the clearField function

--- packages/twenty-front/src/modules/object-record/record-field/form-types/components/FormSelectFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/form-types/components/FormSelectFieldInput.tsx
@@ -131,7 +131,8 @@ export const FormSelectFieldInput = ({
 
   const handleClearField = () => {
     clearField();
-    onCancel();
+
+    goBackToPreviousHotkeyScope();
   };
 
   const handleSubmit = (option: SelectOption) => {
