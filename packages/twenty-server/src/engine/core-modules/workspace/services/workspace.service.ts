a973188ab1a0dc121499df476f15e16a8ecdba29
Antoine Moreaux <moreaux.antoine@gmail.com>
2024-12-03 16:26:52 +0100

    [refactor]: Extract getDomainNameByEmail to a separate file
    Refactored the getDomainNameByEmail function into its own file for
    better separation of concerns and reusability. The addition of unit
    tests ensures that the function behaves correctly, improving code
    reliability. Also corrected a typo in the self-hosting documentation.

--- packages/twenty-server/src/engine/core-modules/workspace/services/workspace.service.ts
+++ packages/twenty-server/src/engine/core-modules/workspace/services/workspace.service.ts
@@ -18,9 +18,9 @@ import {
 } from 'src/engine/core-modules/workspace/workspace.entity';
 import { WorkspaceManagerService } from 'src/engine/workspace-manager/workspace-manager.service';
 import { DEFAULT_FEATURE_FLAGS } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/default-feature-flags';
-import { EnvironmentService } from 'src/engine/core-modules/environment/environment.service';
-import { getDomainNameByEmail, isWorkEmail } from 'src/utils/is-work-email';
+import { isWorkEmail } from 'src/utils/is-work-email';
 import { isDefined } from 'src/utils/is-defined';
+import { getDomainNameByEmail } from 'src/utils/get-domain-name-by-email';
 
 @Injectable()
 // eslint-disable-next-line @nx/workspace-inject-workspace-repository
@@ -34,7 +34,6 @@ export class WorkspaceService extends TypeOrmQueryService<Workspace> {
     private readonly userWorkspaceRepository: Repository<UserWorkspace>,
     private readonly workspaceManagerService: WorkspaceManagerService,
     private readonly featureFlagService: FeatureFlagService,
-    private readonly environmentService: EnvironmentService,
     private readonly billingSubscriptionService: BillingSubscriptionService,
     private readonly userWorkspaceService: UserWorkspaceService,
   ) {
