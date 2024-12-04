22690f2885215efe570d699b60388a484badd275
Félix Malfait <felix@twenty.com>
2024-12-04 15:39:04 +0100

    Lint

--- packages/twenty-server/src/database/typeorm/metadata/migrations/1733318004066-missing-shortcut-migration.ts
+++ packages/twenty-server/src/database/typeorm/metadata/migrations/1733318004066-missing-shortcut-migration.ts
@@ -1,14 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class MissingShortcutMigration1733318004066 implements MigrationInterface {
-    name = 'MissingShortcutMigration1733318004066'
+export class MissingShortcutMigration1733318004066
+  implements MigrationInterface
+{
+  name = 'MissingShortcutMigration1733318004066';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "metadata"."IDX_objectMetadata_shortcut_upper_workspace"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "metadata"."IDX_objectMetadata_shortcut_upper_workspace"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_objectMetadata_shortcut_upper_workspace" ON "metadata"."objectMetadata" ("workspaceId") `);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_objectMetadata_shortcut_upper_workspace" ON "metadata"."objectMetadata" ("workspaceId") `,
+    );
+  }
 }
