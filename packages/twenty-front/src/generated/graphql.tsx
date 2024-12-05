87b803beb9ed37e49b2d77277cfd2e0655477a13
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-12-05 15:42:54 +0100

    Fix

--- packages/twenty-front/src/generated/graphql.tsx
+++ packages/twenty-front/src/generated/graphql.tsx
@@ -1,5 +1,5 @@
-import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
+import { gql } from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -1279,6 +1279,8 @@ export type UpdateServerlessFunctionInput = {
 export type UpdateWorkflowVersionStepInput = {
   /** Step to update in JSON format */
   step: Scalars['JSON'];
+  /** Boolean to check if we need to update stepOutput */
+  shouldUpdateStepOutput?: InputMaybe<Scalars['Boolean']>;
   /** Workflow version ID */
   workflowVersionId: Scalars['String'];
 };
