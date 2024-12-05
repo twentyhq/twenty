36e4357bb1edb08121b0c07c55e84707c9601bde
Thomas Trompette <thomas.trompette@sfr.fr>
2024-12-05 10:48:34 +0100

    Select full record in variable dropdown (#8851)
    Output schema is now separated in two sections:
    - object, that gather all informations on the selectable object
    - fields, that display object fields in a record context, or simply the
    available fields from the previous steps
    
    The dropdown variable has now a new mode:
    - if objectNameSingularToSelect is defined, it goes into an object mode.
    Only objects of the right type will be shown
    - if not set, it will use the already existing mode, to select a field
    
    When an object is selected, it actually set the id of the object
    
    
    
    https://github.com/user-attachments/assets/1c95f8fd-10f0-4c1c-aeb7-c7d847e89536

--- packages/twenty-ui/src/display/text/components/HorizontalSeparator.tsx
+++ packages/twenty-ui/src/display/text/components/HorizontalSeparator.tsx
@@ -1,13 +1,14 @@
-import { JSX } from 'react';
 import styled from '@emotion/styled';
+import { JSX } from 'react';
 
 type HorizontalSeparatorProps = {
   visible?: boolean;
   text?: string;
   noMargin?: boolean;
+  color?: string;
 };
 const StyledSeparator = styled.div<HorizontalSeparatorProps>`
-  background-color: ${({ theme }) => theme.border.color.medium};
+  background-color: ${({ theme, color }) => color ?? theme.border.color.medium};
   height: ${({ visible }) => (visible ? '1px' : 0)};
   margin-bottom: ${({ theme, noMargin }) => (noMargin ? 0 : theme.spacing(3))};
   margin-top: ${({ theme, noMargin }) => (noMargin ? 0 : theme.spacing(3))};
@@ -38,6 +39,7 @@ export const HorizontalSeparator = ({
   visible = true,
   text = '',
   noMargin = false,
+  color,
 }: HorizontalSeparatorProps): JSX.Element => (
   <>
     {text ? (
@@ -47,7 +49,7 @@ export const HorizontalSeparator = ({
         <StyledLine visible={visible} />
       </StyledSeparatorContainer>
     ) : (
-      <StyledSeparator visible={visible} noMargin={noMargin} />
+      <StyledSeparator visible={visible} noMargin={noMargin} color={color} />
     )}
   </>
 );
