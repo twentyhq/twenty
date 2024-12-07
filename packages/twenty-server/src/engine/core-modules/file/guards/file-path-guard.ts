4c0c2529a8dd0a8919ad21daa87636a84220aadb
Charles Bochet <charles@twenty.com>
2024-12-07 12:57:37 +0100

    Fix broken image url

--- packages/twenty-server/src/engine/core-modules/file/guards/file-path-guard.ts
+++ packages/twenty-server/src/engine/core-modules/file/guards/file-path-guard.ts
@@ -15,6 +15,7 @@ export class FilePathGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const query = request.query;
+    const urlParamsV2 = new URLSearchParams(request.originalUrl);
 
     if (!query || !query['token']) {
       return false;
