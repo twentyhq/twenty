995809e87076e0a52802c5a57059375b76eff632
guillim <guigloo@msn.com>
2024-12-09 18:01:04 +0100

    rounded

--- packages/twenty-front/src/modules/settings/security/components/SettingsSecurityOptionsList.tsx
+++ packages/twenty-front/src/modules/settings/security/components/SettingsSecurityOptionsList.tsx
@@ -1,20 +1,20 @@
 import { currentWorkspaceState } from '@/auth/states/currentWorkspaceState';
 import { SettingsOptionCardContentToggle } from '@/settings/components/SettingsOptions/SettingsOptionCardContentToggle';
+import { SSOIdentitiesProvidersState } from '@/settings/security/states/SSOIdentitiesProvidersState';
 import { SnackBarVariant } from '@/ui/feedback/snack-bar-manager/components/SnackBar';
 import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
 import styled from '@emotion/styled';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import {
-  IconLink,
   Card,
   IconGoogle,
+  IconLink,
   IconMicrosoft,
   IconPassword,
 } from 'twenty-ui';
-import { useUpdateWorkspaceMutation } from '~/generated/graphql';
 import { AuthProviders } from '~/generated-metadata/graphql';
+import { useUpdateWorkspaceMutation } from '~/generated/graphql';
 import { capitalize } from '~/utils/string/capitalize';
-import { SSOIdentitiesProvidersState } from '@/settings/security/states/SSOIdentitiesProvidersState';
 
 const StyledSettingsSecurityOptionsList = styled.div`
   display: flex;
@@ -122,7 +122,7 @@ export const SettingsSecurityOptionsList = () => {
     <StyledSettingsSecurityOptionsList>
       {currentWorkspace && (
         <>
-          <Card>
+          <Card rounded>
             <SettingsOptionCardContentToggle
               Icon={IconGoogle}
               title="Google"
