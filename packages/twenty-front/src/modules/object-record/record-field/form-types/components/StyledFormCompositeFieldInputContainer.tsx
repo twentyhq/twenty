d4983f888838985dc667e6f44d2b18a06c13b42d
Thomas Trompette <thomas.trompette@sfr.fr>
2024-12-10 17:16:12 +0100

    Factorize composite type and imput

--- packages/twenty-front/src/modules/object-record/record-field/form-types/components/StyledFormCompositeFieldInputContainer.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/form-types/components/StyledFormCompositeFieldInputContainer.tsx
@@ -3,8 +3,6 @@ import styled from '@emotion/styled';
 export const StyledFormCompositeFieldInputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  align-self: stretch;
   background: ${({ theme }) => theme.background.secondary};
   border: 1px solid ${({ theme }) => theme.border.color.medium};
   border-radius: ${({ theme }) => theme.border.radius.sm};
