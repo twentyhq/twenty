d55144ab6b0848e39ce4ec97673a0a5d5078a63e
martmull <martmull@hotmail.fr>
2024-12-11 11:44:55 +0100

    Fix outputSchema computing

--- packages/twenty-front/src/modules/serverless-functions/utils/__tests__/getFunctionOutputSchema.test.ts
+++ packages/twenty-front/src/modules/serverless-functions/utils/__tests__/getFunctionOutputSchema.test.ts
@@ -2,15 +2,15 @@ import { getFunctionOutputSchema } from '@/serverless-functions/utils/getFunctio
 
 describe('getFunctionOutputSchema', () => {
   it('should compute outputSchema properly', () => {
-    const testResult = { a: null, b: 'b', c: { cc: 1 } };
+    const testResult = { a: null, b: 'b', c: { cc: 1 }, d: true };
     const expectedOutputSchema = {
-      a: { isLeaf: true, type: 'object', value: null },
+      a: { isLeaf: true, type: 'unknown', value: null },
       b: { isLeaf: true, type: 'string', value: 'b' },
       c: {
-        isLeaf: true,
-        type: 'object',
-        value: { cc: 1 },
+        isLeaf: false,
+        value: { cc: { isLeaf: true, type: 'number', value: 1 } },
       },
+      d: { isLeaf: true, type: 'boolean', value: true },
     };
     expect(getFunctionOutputSchema(testResult)).toEqual(expectedOutputSchema);
   });
--- packages/twenty-front/src/modules/serverless-functions/utils/getFunctionOutputSchema.ts
+++ packages/twenty-front/src/modules/serverless-functions/utils/getFunctionOutputSchema.ts
@@ -1,18 +1,46 @@
-import { InputSchemaPropertyType } from '@/workflow/types/InputSchema';
 import { BaseOutputSchema } from '@/workflow/search-variables/types/StepOutputSchema';
+import { isObject } from '@sniptt/guards';
+import { InputSchemaPropertyType } from '@/workflow/types/InputSchema';
+import { isDefined } from 'twenty-ui';
 
-export const getFunctionOutputSchema = (
-  serverlessFunctionTestResult: object,
-) => {
-  return serverlessFunctionTestResult
-    ? Object.entries(serverlessFunctionTestResult).reduce(
+const getValueType = (value: any): InputSchemaPropertyType => {
+  if (!isDefined(value) || value === null) {
+    return 'unknown';
+  }
+  if (typeof value === 'string') {
+    return 'string';
+  }
+  if (typeof value === 'number') {
+    return 'number';
+  }
+  if (typeof value === 'boolean') {
+    return 'boolean';
+  }
+  if (typeof value === 'object') {
+    return 'object';
+  }
+  if (Array.isArray(value)) {
+    return 'array';
+  }
+  return 'unknown';
+};
+
+export const getFunctionOutputSchema = (testResult: object) => {
+  return testResult
+    ? Object.entries(testResult).reduce(
         (acc: BaseOutputSchema, [key, value]) => {
-          acc[key] = {
-            isLeaf: true,
-            value,
-            type: typeof value as InputSchemaPropertyType,
-          };
-
+          if (isObject(value)) {
+            acc[key] = {
+              isLeaf: false,
+              value: getFunctionOutputSchema(value),
+            };
+          } else {
+            acc[key] = {
+              isLeaf: true,
+              value,
+              type: getValueType(value),
+            };
+          }
           return acc;
         },
         {},
