7b14190961ce4eb22037e6da1c8294f4a5289206
Antoine Moreaux <moreaux.antoine@gmail.com>
2024-12-16 10:49:44 +0100

    fix(): sleep before redirect

--- packages/twenty-front/src/modules/domain-manager/hooks/useRedirect.ts
+++ packages/twenty-front/src/modules/domain-manager/hooks/useRedirect.ts
@@ -1,6 +1,8 @@
 // Don't use this hook directly! Prefer the high level hooks like:
 // useRedirectToDefaultDomain and useRedirectToWorkspaceDomain
 
+import { useDebouncedCallback } from 'use-debounce';
+
 export const useRedirect = () => {
   const redirect = useDebouncedCallback((url: string) => {
     window.location.href = url;
