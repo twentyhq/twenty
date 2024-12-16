8b3fd3277d233b0cc3cbd2b5c22b3b34512a876d
Jérémy Magrin <jeremy.magrin@gmail.com>
2024-12-16 15:07:02 +0100

    fix: add create cursor util

---
+++ packages/twenty-front/src/modules/apollo/utils/createCursor.ts
@@ -0,0 +1,20 @@
+import { ObjectRecord } from '@/object-record/types/ObjectRecord';
+
+export const createCursor = (record: ObjectRecord) => {
+  if (!('id' in record)) {
+    throw new Error('Record does not have an id');
+  }
+
+  const payload: {
+    id: string;
+    position?: number;
+  } = {
+    id: record.id,
+  };
+
+  if ('position' in record) {
+    payload.position = record.position;
+  }
+
+  return Buffer.from(JSON.stringify(payload), 'utf-8').toString('base64');
+};
