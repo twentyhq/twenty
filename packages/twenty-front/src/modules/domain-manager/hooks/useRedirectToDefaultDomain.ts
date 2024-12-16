11e8ec335408ab88eb14adf13be6df1940581f97
Antoine Moreaux <moreaux.antoine@gmail.com>
2024-12-16 10:40:08 +0100

    fix(): sleep before redirect

--- packages/twenty-front/src/modules/domain-manager/hooks/useRedirectToDefaultDomain.ts
+++ packages/twenty-front/src/modules/domain-manager/hooks/useRedirectToDefaultDomain.ts
@@ -1,12 +1,17 @@
 import { useReadDefaultDomainFromConfiguration } from '@/domain-manager/hooks/useReadDefaultDomainFromConfiguration';
 import { useRedirect } from '@/domain-manager/hooks/useRedirect';
+import { useLastAuthenticatedWorkspaceDomain } from '@/domain-manager/hooks/useLastAuthenticatedWorkspaceDomain';
 
 export const useRedirectToDefaultDomain = () => {
   const { defaultDomain } = useReadDefaultDomainFromConfiguration();
+  const { setLastAuthenticateWorkspaceDomain } =
+    useLastAuthenticatedWorkspaceDomain();
+
   const { redirect } = useRedirect();
   const redirectToDefaultDomain = () => {
     const url = new URL(window.location.href);
     if (url.hostname !== defaultDomain) {
+      setLastAuthenticateWorkspaceDomain(null);
       url.hostname = defaultDomain;
       redirect(url.toString());
     }
