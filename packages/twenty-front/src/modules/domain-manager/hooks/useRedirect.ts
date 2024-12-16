2679816bc7378236f970de52defe9ddf6f4f7e48
Antoine Moreaux <moreaux.antoine@gmail.com>
2024-12-16 10:47:22 +0100

    fix(): sleep before redirect

--- packages/twenty-front/src/modules/domain-manager/hooks/useRedirect.ts
+++ packages/twenty-front/src/modules/domain-manager/hooks/useRedirect.ts
@@ -1,14 +1,10 @@
 // Don't use this hook directly! Prefer the high level hooks like:
 // useRedirectToDefaultDomain and useRedirectToWorkspaceDomain
-import { sleep } from '~/utils/sleep';
 
 export const useRedirect = () => {
-  const redirect = (url: string) => {
-    // This hacky workaround is necessary to ensure the tokens stored in the cookie are updated correctly.
-    sleep(0).then(() => {
-      window.location.href = url;
-    });
-  };
+  const redirect = useDebouncedCallback((url: string) => {
+    window.location.href = url;
+  }, 100);
 
   return {
     redirect,
