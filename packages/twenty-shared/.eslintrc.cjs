8c39f131e2ed2cf53ff947a8197e5ebc4d421489
Charles Bochet <charles@twenty.com>
2024-12-16 17:00:36 +0100

    Fix

--- packages/twenty-shared/.eslintrc.cjs
+++ packages/twenty-shared/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['../../.eslintrc.cjs', '../../.eslintrc.react.cjs'],
+  extends: ['../../.eslintrc.cjs'],
   ignorePatterns: ['!**/*'],
   overrides: [
     {
--- packages/twenty-shared/src/utils/image/__tests__/getImageAbsoluteURI.test.ts
+++ packages/twenty-shared/src/utils/image/__tests__/getImageAbsoluteURI.test.ts
@@ -4,42 +4,42 @@ describe('getImageAbsoluteURI', () => {
   it('should return null if imageUrl is empty', () => {
     const imageUrl = '';
     const baseUrl = 'http://localhost:3000';
-    const result = getImageAbsoluteURI({imageUrl, baseUrl});
+    const result = getImageAbsoluteURI({ imageUrl, baseUrl });
     expect(result).toBeNull();
   });
 
   it('should return null if baseUrl is empty', () => {
     const imageUrl = 'https://XXX';
     const baseUrl = '';
-    const result = getImageAbsoluteURI({imageUrl, baseUrl});
+    const result = getImageAbsoluteURI({ imageUrl, baseUrl });
     expect(result).toBeNull();
   });
 
   it('should return absolute url if the imageUrl is an absolute url', () => {
     const imageUrl = 'https://XXX';
     const baseUrl = 'http://localhost:3000';
-    const result = getImageAbsoluteURI({imageUrl, baseUrl});
+    const result = getImageAbsoluteURI({ imageUrl, baseUrl });
     expect(result).toBe(imageUrl);
   });
 
   it('should return fully formed url if imageUrl is a url', () => {
     const imageUrl = 'pic.png?token=XXX';
     const baseUrl = 'http://localhost:3000';
-    const result = getImageAbsoluteURI({imageUrl, baseUrl});
+    const result = getImageAbsoluteURI({ imageUrl, baseUrl });
     expect(result).toBe('http://localhost:3000/files/pic.png?token=XXX');
   });
 
   it('should return fully formed url if imageUrl is a relative url', () => {
     const imageUrl = '/pic.png?token=XXX';
     const baseUrl = 'http://localhost:3000';
-    const result = getImageAbsoluteURI({imageUrl, baseUrl});
+    const result = getImageAbsoluteURI({ imageUrl, baseUrl });
     expect(result).toBe('http://localhost:3000/files/pic.png?token=XXX');
   });
 
   it('should return null if imageUrl is an empty string', () => {
     const imageUrl = '';
     const baseUrl = 'http://localhost:3000';
-    const result = getImageAbsoluteURI({imageUrl, baseUrl});
+    const result = getImageAbsoluteURI({ imageUrl, baseUrl });
     expect(result).toBeNull();
   });
 });
