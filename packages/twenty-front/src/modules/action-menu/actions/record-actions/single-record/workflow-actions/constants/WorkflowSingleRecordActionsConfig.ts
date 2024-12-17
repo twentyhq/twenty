5e7de35f09cb7ea298560b73b821e194ff563d2b
bosiraphael <raphael.bosi@gmail.com>
2024-12-17 15:16:31 +0100

    add navigation to workflow actions

--- packages/twenty-front/src/modules/action-menu/actions/record-actions/single-record/workflow-actions/constants/WorkflowSingleRecordActionsConfig.ts
+++ packages/twenty-front/src/modules/action-menu/actions/record-actions/single-record/workflow-actions/constants/WorkflowSingleRecordActionsConfig.ts
@@ -1,3 +1,5 @@
+import { useNavigateToNextRecordSingleRecordAction } from '@/action-menu/actions/record-actions/single-record/hooks/useNavigateToNextRecordSingleRecordAction';
+import { useNavigateToPreviousRecordSingleRecordAction } from '@/action-menu/actions/record-actions/single-record/hooks/useNavigateToPreviousRecordSingleRecordAction';
 import { useActivateDraftWorkflowSingleRecordAction } from '@/action-menu/actions/record-actions/single-record/workflow-actions/hooks/useActivateDraftWorkflowSingleRecordAction';
 import { useActivateLastPublishedVersionWorkflowSingleRecordAction } from '@/action-menu/actions/record-actions/single-record/workflow-actions/hooks/useActivateLastPublishedVersionWorkflowSingleRecordAction';
 import { useDeactivateWorkflowSingleRecordAction } from '@/action-menu/actions/record-actions/single-record/workflow-actions/hooks/useDeactivateWorkflowSingleRecordAction';
@@ -14,6 +16,8 @@ import {
   ActionMenuEntryType,
 } from '@/action-menu/types/ActionMenuEntry';
 import {
+  IconChevronDown,
+  IconChevronUp,
   IconHistory,
   IconHistoryToggle,
   IconPlayerPause,
@@ -147,5 +151,27 @@ export const WORKFLOW_SINGLE_RECORD_ACTIONS_CONFIG: Record<
       ActionAvailableOn.INDEX_PAGE_SINGLE_RECORD_SELECTION,
     ],
     actionHook: useTestWorkflowSingleRecordAction,
+  },
+  navigateToPreviousRecord: {
+    type: ActionMenuEntryType.Standard,
+    scope: ActionMenuEntryScope.RecordSelection,
+    key: 'navigate-to-previous-record',
+    label: 'Navigate to previous workflow',
+    shortLabel: '',
+    position: 9,
+    Icon: IconChevronUp,
+    availableOn: [ActionAvailableOn.SHOW_PAGE],
+    actionHook: useNavigateToPreviousRecordSingleRecordAction,
+  },
+  navigateToNextRecord: {
+    type: ActionMenuEntryType.Standard,
+    scope: ActionMenuEntryScope.RecordSelection,
+    key: 'navigate-to-next-record',
+    label: 'Navigate to next workflow',
+    shortLabel: '',
+    position: 10,
+    Icon: IconChevronDown,
+    availableOn: [ActionAvailableOn.SHOW_PAGE],
+    actionHook: useNavigateToNextRecordSingleRecordAction,
   },
 };
