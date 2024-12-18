twentyhq/twenty

commit e0f1db2ed3388e91f2eb672b0128b300ed5220bf

fix: recordBoard sticky header + scrollThumb (#8946)
Fixes: #8944 

[Screencast from 2024-12-08
00-43-19.webm](https://github.com/user-attachments/assets/b6e15fcc-46a1-4ab0-aacf-86056248bb69)

---------

Co-authored-by: ehconitin <nitinkoche03@gmail.com>

packages/twenty-front/src/modules/object-record/record-board/components/RecordBoard.tsx
@@ -46,7 +46,6 @@ const StyledColumnContainer = styled.div`
 const StyledContainerContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
 `;
 
 const StyledBoardContentContainer = styled.div`

packages/twenty-front/src/modules/ui/utilities/scroll/components/ScrollWrapper.tsx
@@ -17,7 +17,7 @@ import 'overlayscrollbars/overlayscrollbars.css';
 
 const StyledScrollWrapper = styled.div<{ scrollHide?: boolean }>`
   display: flex;
-  height: fit-content;
+  height: 100%;
   width: 100%;
 
   .os-scrollbar-handle {

packages/twenty-front/src/modules/object-record/record-index/components/RecordIndexContainer.tsx
@@ -54,9 +54,9 @@ const StyledContainer = styled.div`
 `;
 
 const StyledContainerWithPadding = styled.div`
-  height: calc(100% - 40px);
+  box-sizing: border-box;
+  height: calc(100% - ${({ theme }) => theme.spacing(10)});
   margin-left: ${({ theme }) => theme.spacing(2)};
-  width: 100%;
 `;
 
 export const RecordIndexContainer = () => {

commit ea614773fae56708d1517efb81ea11b711c91e3d

fix: group by menu display bug (broken in #8946)

packages/twenty-front/src/modules/object-record/record-board/components/RecordBoard.tsx
@@ -196,6 +196,7 @@ export const RecordBoard = () => {
         <ScrollWrapper
           contextProviderName="recordBoard"
           componentInstanceId={`scroll-wrapper-record-board-${recordBoardId}`}
+          fullHeight
         >
           <RecordBoardStickyHeaderEffect />
           <StyledContainerContainer>

packages/twenty-front/src/modules/ui/utilities/scroll/components/ScrollWrapper.tsx
@@ -15,9 +15,12 @@ import { scrollWrapperScrollTopComponentState } from '@/ui/utilities/scroll/stat
 import { useSetRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentStateV2';
 import 'overlayscrollbars/overlayscrollbars.css';
 
-const StyledScrollWrapper = styled.div<{ scrollHide?: boolean }>`
+const StyledScrollWrapper = styled.div<{
+  scrollHide?: boolean;
+  fullHeight?: boolean;
+}>`
   display: flex;
-  height: 100%;
+  height: ${({ fullHeight }) => (fullHeight ? '100%' : 'fit-content')};
   width: 100%;
 
   .os-scrollbar-handle {
@@ -33,6 +36,7 @@ const StyledInnerContainer = styled.div`
 export type ScrollWrapperProps = {
   children: React.ReactNode;
   className?: string;
+  fullHeight?: boolean;
   defaultEnableXScroll?: boolean;
   defaultEnableYScroll?: boolean;
   contextProviderName: ContextProviderName;
@@ -44,6 +48,7 @@ export const ScrollWrapper = ({
   componentInstanceId,
   children,
   className,
+  fullHeight,
   defaultEnableXScroll = true,
   defaultEnableYScroll = true,
   contextProviderName,
@@ -110,6 +115,7 @@ export const ScrollWrapper = ({
           ref={scrollableRef}
           className={className}
           scrollHide={scrollHide}
+          fullHeight={fullHeight}
         >
           <StyledInnerContainer>{children}</StyledInnerContainer>
         </StyledScrollWrapper>