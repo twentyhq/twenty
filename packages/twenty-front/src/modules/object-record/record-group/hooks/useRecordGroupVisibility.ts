7a9a4d9a7a186cd746129623f34bec2357466064
Marie Stoppa <marie.stoppa@essec.edu>
2024-12-18 15:48:11 +0100

    Revert "feat: hide empty record group default true"
    This reverts commit 8918d05b92bb3bee9dd02cf0bd20d6d63870ced9.

--- packages/twenty-front/src/modules/object-record/record-group/hooks/useRecordGroupVisibility.ts
+++ packages/twenty-front/src/modules/object-record/record-group/hooks/useRecordGroupVisibility.ts
@@ -1,10 +1,15 @@
 import { recordGroupDefinitionFamilyState } from '@/object-record/record-group/states/recordGroupDefinitionFamilyState';
+import { recordGroupIdsComponentState } from '@/object-record/record-group/states/recordGroupIdsComponentState';
 import { RecordGroupDefinition } from '@/object-record/record-group/types/RecordGroupDefinition';
 import { recordIndexRecordGroupHideComponentState } from '@/object-record/record-index/states/recordIndexRecordGroupHideComponentState';
+import { recordIndexRecordIdsByGroupComponentFamilyState } from '@/object-record/record-index/states/recordIndexRecordIdsByGroupComponentFamilyState';
 import { useRecoilComponentCallbackStateV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentCallbackStateV2';
+import { getSnapshotValue } from '@/ui/utilities/state/utils/getSnapshotValue';
 import { useSaveCurrentViewGroups } from '@/views/hooks/useSaveCurrentViewGroups';
+import { mapRecordGroupDefinitionsToViewGroups } from '@/views/utils/mapRecordGroupDefinitionsToViewGroups';
 import { recordGroupDefinitionToViewGroup } from '@/views/utils/recordGroupDefinitionToViewGroup';
 import { useRecoilCallback } from 'recoil';
+import { isDefined } from '~/utils/isDefined';
 
 type UseRecordGroupVisibilityParams = {
   viewBarId: string;
@@ -13,10 +18,20 @@ type UseRecordGroupVisibilityParams = {
 export const useRecordGroupVisibility = ({
   viewBarId,
 }: UseRecordGroupVisibilityParams) => {
+  const recordIndexRecordGroupIdsState = useRecoilComponentCallbackStateV2(
+    recordGroupIdsComponentState,
+  );
+
+  const recordIndexRecordIdsByGroupFamilyState =
+    useRecoilComponentCallbackStateV2(
+      recordIndexRecordIdsByGroupComponentFamilyState,
+      viewBarId,
+    );
+
   const objectOptionsDropdownRecordGroupHideState =
     useRecoilComponentCallbackStateV2(recordIndexRecordGroupHideComponentState);
 
-  const { saveViewGroup } = useSaveCurrentViewGroups(viewBarId);
+  const { saveViewGroup, saveViewGroups } = useSaveCurrentViewGroups(viewBarId);
 
   const handleVisibilityChange = useRecoilCallback(
     ({ set }) =>
@@ -35,14 +50,66 @@ export const useRecordGroupVisibility = ({
   );
 
   const handleHideEmptyRecordGroupChange = useRecoilCallback(
-    ({ set }) =>
+    ({ snapshot, set }) =>
       async () => {
-        set(
+        const updatedRecordGroupDefinitions: RecordGroupDefinition[] = [];
+        const recordGroupIds = getSnapshotValue(
+          snapshot,
+          recordIndexRecordGroupIdsState,
+        );
+
+        const currentHideState = getSnapshotValue(
+          snapshot,
           objectOptionsDropdownRecordGroupHideState,
-          (currentHideState) => !currentHideState,
+        );
+        const newHideState = !currentHideState;
+
+        set(objectOptionsDropdownRecordGroupHideState, newHideState);
+
+        for (const recordGroupId of recordGroupIds) {
+          const recordGroup = getSnapshotValue(
+            snapshot,
+            recordGroupDefinitionFamilyState(recordGroupId),
+          );
+
+          if (!isDefined(recordGroup)) {
+            throw new Error(
+              `Record group with id ${recordGroupId} not found in snapshot`,
+            );
+          }
+
+          const recordGroupRowIds = getSnapshotValue(
+            snapshot,
+            recordIndexRecordIdsByGroupFamilyState(recordGroupId),
+          );
+
+          if (recordGroupRowIds.length > 0) {
+            continue;
+          }
+
+          const updatedRecordGroup = {
+            ...recordGroup,
+            isVisible: !newHideState,
+          };
+
+          set(
+            recordGroupDefinitionFamilyState(recordGroupId),
+            updatedRecordGroup,
+          );
+
+          updatedRecordGroupDefinitions.push(updatedRecordGroup);
+        }
+
+        saveViewGroups(
+          mapRecordGroupDefinitionsToViewGroups(updatedRecordGroupDefinitions),
         );
       },
-    [objectOptionsDropdownRecordGroupHideState],
+    [
+      recordIndexRecordGroupIdsState,
+      objectOptionsDropdownRecordGroupHideState,
+      saveViewGroups,
+      recordIndexRecordIdsByGroupFamilyState,
+    ],
   );
 
   return {
