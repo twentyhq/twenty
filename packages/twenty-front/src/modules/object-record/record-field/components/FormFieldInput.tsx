894a7947e8b5b24d38e2211f267d6c961fb30905
Lucas Bordeau <bordeau.lucas@gmail.com>
2024-12-18 15:46:53 +0100

    Removed unused field types

--- packages/twenty-front/src/modules/object-record/record-field/components/FormFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/components/FormFieldInput.tsx
@@ -1,14 +1,13 @@
 import { FormAddressFieldInput } from '@/object-record/record-field/form-types/components/FormAddressFieldInput';
+import { FormBooleanFieldInput } from '@/object-record/record-field/form-types/components/FormBooleanFieldInput';
 import { FormDateFieldInput } from '@/object-record/record-field/form-types/components/FormDateFieldInput';
 import { FormEmailsFieldInput } from '@/object-record/record-field/form-types/components/FormEmailsFieldInput';
 import { FormFullNameFieldInput } from '@/object-record/record-field/form-types/components/FormFullNameFieldInput';
 import { FormLinksFieldInput } from '@/object-record/record-field/form-types/components/FormLinksFieldInput';
 import { FormMultiSelectFieldInput } from '@/object-record/record-field/form-types/components/FormMultiSelectFieldInput';
 import { FormNumberFieldInput } from '@/object-record/record-field/form-types/components/FormNumberFieldInput';
-import { FormRawJsonFieldInput } from '@/object-record/record-field/form-types/components/FormRawJsonFieldInput';
 import { FormSelectFieldInput } from '@/object-record/record-field/form-types/components/FormSelectFieldInput';
 import { FormTextFieldInput } from '@/object-record/record-field/form-types/components/FormTextFieldInput';
-import { FormUuidFieldInput } from '@/object-record/record-field/form-types/components/FormUuidFieldInput';
 import { VariablePickerComponent } from '@/object-record/record-field/form-types/types/VariablePickerComponent';
 import { FieldDefinition } from '@/object-record/record-field/types/FieldDefinition';
 import {
@@ -20,16 +19,15 @@ import {
   FieldMultiSelectValue,
 } from '@/object-record/record-field/types/FieldMetadata';
 import { isFieldAddress } from '@/object-record/record-field/types/guards/isFieldAddress';
+import { isFieldBoolean } from '@/object-record/record-field/types/guards/isFieldBoolean';
 import { isFieldDate } from '@/object-record/record-field/types/guards/isFieldDate';
 import { isFieldEmails } from '@/object-record/record-field/types/guards/isFieldEmails';
 import { isFieldFullName } from '@/object-record/record-field/types/guards/isFieldFullName';
 import { isFieldLinks } from '@/object-record/record-field/types/guards/isFieldLinks';
 import { isFieldMultiSelect } from '@/object-record/record-field/types/guards/isFieldMultiSelect';
 import { isFieldNumber } from '@/object-record/record-field/types/guards/isFieldNumber';
-import { isFieldRawJson } from '@/object-record/record-field/types/guards/isFieldRawJson';
 import { isFieldSelect } from '@/object-record/record-field/types/guards/isFieldSelect';
 import { isFieldText } from '@/object-record/record-field/types/guards/isFieldText';
-import { isFieldUuid } from '@/object-record/record-field/types/guards/isFieldUuid';
 import { JsonValue } from 'type-fest';
 
 type FormFieldInputProps = {
@@ -51,6 +49,13 @@ export const FormFieldInput = ({
       defaultValue={defaultValue as string | number | undefined}
       onPersist={onPersist}
       placeholder={field.label}
+      VariablePicker={VariablePicker}
+    />
+  ) : isFieldBoolean(field) ? (
+    <FormBooleanFieldInput
+      label={field.label}
+      defaultValue={defaultValue as string | boolean | undefined}
+      onPersist={onPersist}
       VariablePicker={VariablePicker}
     />
   ) : isFieldText(field) ? (
@@ -112,22 +117,6 @@ export const FormFieldInput = ({
       onPersist={onPersist}
       VariablePicker={VariablePicker}
       options={field.metadata.options}
-    />
-  ) : isFieldRawJson(field) ? (
-    <FormRawJsonFieldInput
-      label={field.label}
-      defaultValue={defaultValue as string | undefined}
-      onPersist={onPersist}
-      placeholder={field.label}
-      VariablePicker={VariablePicker}
-    />
-  ) : isFieldUuid(field) ? (
-    <FormUuidFieldInput
-      label={field.label}
-      defaultValue={defaultValue as string | null | undefined}
-      onPersist={onPersist}
-      placeholder={field.label}
-      VariablePicker={VariablePicker}
     />
   ) : null;
 };
