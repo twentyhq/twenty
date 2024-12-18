3b489203149b3eb64b2ffa548fc1f5824dca0d49
Jérémy M <jeremy.magrin@gmail.com>
2024-12-18 12:26:38 +0100

    feat: NoValue is bot properly created the backend (#9110)
    `No Value` view groups wasn't properly created when we select a group by
    field metadata, this PR fix the issue.
    Also a script is added to backfill the current view groups.
    
    ---------
    
    Co-authored-by: Marie <51697796+ijreilly@users.noreply.github.com>

--- packages/twenty-server/src/database/commands/upgrade-version/0-40/0-40-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-40/0-40-upgrade-version.module.ts
@@ -3,16 +3,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RecordPositionBackfillCommand } from 'src/database/commands/upgrade-version/0-40/0-40-record-position-backfill.command';
 import { UpgradeTo0_40Command } from 'src/database/commands/upgrade-version/0-40/0-40-upgrade-version.command';
+import { ViewGroupNoValueBackfillCommand } from 'src/database/commands/upgrade-version/0-40/0-40-view-group-no-value-backfill.command';
 import { RecordPositionBackfillModule } from 'src/engine/api/graphql/workspace-query-runner/services/record-position-backfill-module';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
+import { FieldMetadataModule } from 'src/engine/metadata-modules/field-metadata/field-metadata.module';
 import { WorkspaceSyncMetadataCommandsModule } from 'src/engine/workspace-manager/workspace-sync-metadata/commands/workspace-sync-metadata-commands.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Workspace], 'core'),
+    TypeOrmModule.forFeature([FieldMetadataEntity], 'metadata'),
     WorkspaceSyncMetadataCommandsModule,
     RecordPositionBackfillModule,
+    FieldMetadataModule,
   ],
-  providers: [UpgradeTo0_40Command, RecordPositionBackfillCommand],
+  providers: [
+    UpgradeTo0_40Command,
+    RecordPositionBackfillCommand,
+    ViewGroupNoValueBackfillCommand,
+  ],
 })
 export class UpgradeTo0_40CommandModule {}
