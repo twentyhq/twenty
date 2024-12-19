c801d97e059176e6a2d5610da002e55ffa9b4eaf
Antoine Moreaux <moreaux.antoine@gmail.com>
2024-12-19 15:02:54 +0100

    fix test

--- packages/twenty-server/src/engine/core-modules/auth/token/services/refresh-token.service.ts
+++ packages/twenty-server/src/engine/core-modules/auth/token/services/refresh-token.service.ts
@@ -90,7 +90,6 @@ export class RefreshTokenService {
       );
     }
 
-    console.log('>>>>>>>>>>>>>>', token);
     // TODO: Delete this useless condition and error after March 31st 2025
     if (!token.workspaceId) {
       throw new AuthException(
