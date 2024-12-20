f1a616c77e9a6c13fe48fd2f47bdabb01297b53b
Félix Malfait <felix@twenty.com>
2024-12-20 17:18:27 +0100

    Simplify

--- packages/twenty-server/src/engine/core-modules/company-enrichment/company-enrichment.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { CompanyEnrichmentService } from './company-enrichment.service';
-
-@Module({
-  providers: [CompanyEnrichmentService],
-  exports: [CompanyEnrichmentService],
-})
-export class CompanyEnrichmentModule {}
