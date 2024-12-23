b2b6644784513c3c22ee6c9d2810c16bbec47c38
Félix Malfait <felix@twenty.com>
2024-12-23 11:37:34 +0100

    Use uuid package instead of crypto package

--- packages/twenty-front/src/modules/settings/security/utils/sSOIdentityProviderDefaultValues.ts
+++ packages/twenty-front/src/modules/settings/security/utils/sSOIdentityProviderDefaultValues.ts
@@ -1,6 +1,7 @@
 /* @license Enterprise */
 
 import { SettingSecurityNewSSOIdentityFormValues } from '@/settings/security/types/SSOIdentityProvider';
+import { v4 } from 'uuid';
 import { IdentityProviderType } from '~/generated/graphql';
 
 export const sSOIdentityProviderDefaultValues: Record<
@@ -11,15 +12,7 @@ export const sSOIdentityProviderDefaultValues: Record<
     type: 'SAML',
     ssoURL: '',
     name: '',
-    id:
-      window.location.protocol === 'https:'
-        ? crypto.randomUUID()
-        : '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
-            (
-              +c ^
-              (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
-            ).toString(16),
-          ),
+    id: v4(),
     certificate: '',
     issuer: '',
   }),
