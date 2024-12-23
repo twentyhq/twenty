dee6046881f7e2dd7ce01c644fefdbe868ba2eb4
Devessier <baptiste@devessier.fr>
2024-12-23 18:14:14 +0100

    fix: select node when current workflow version changes

--- packages/twenty-front/src/modules/workflow/components/WorkflowDiagramEffect.tsx
+++ packages/twenty-front/src/modules/workflow/components/WorkflowDiagramEffect.tsx
@@ -1,5 +1,6 @@
 import { getSnapshotValue } from '@/ui/utilities/state/utils/getSnapshotValue';
 import { workflowDiagramState } from '@/workflow/states/workflowDiagramState';
+import { workflowLastCreatedStepIdState } from '@/workflow/states/workflowLastCreatedStepIdState';
 import {
   WorkflowVersion,
   WorkflowWithCurrentVersion,
@@ -37,6 +38,26 @@ export const WorkflowDiagramEffect = ({
             previousWorkflowDiagram,
             nextWorkflowDiagram,
           );
+        }
+
+        const selectedNode = getSnapshotValue(
+          snapshot,
+          workflowLastCreatedStepIdState,
+        );
+        if (isDefined(selectedNode)) {
+          mergedWorkflowDiagram.nodes = mergedWorkflowDiagram.nodes.map(
+            (node) => {
+              if (node.id === selectedNode) {
+                return {
+                  ...node,
+                  selected: true,
+                };
+              }
+              return node;
+            },
+          );
+
+          set(workflowLastCreatedStepIdState, undefined);
         }
 
         set(workflowDiagramState, mergedWorkflowDiagram);
