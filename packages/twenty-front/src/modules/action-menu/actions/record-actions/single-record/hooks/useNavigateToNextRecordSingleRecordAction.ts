28b6e6fc8756deb0a70daabe835ad5e4bb88a8dd
bosiraphael <raphael.bosi@gmail.com>
2024-12-26 11:57:58 +0100

    mask navigate actions inside right drawer

--- packages/twenty-front/src/modules/action-menu/actions/record-actions/single-record/hooks/useNavigateToNextRecordSingleRecordAction.ts
+++ packages/twenty-front/src/modules/action-menu/actions/record-actions/single-record/hooks/useNavigateToNextRecordSingleRecordAction.ts
@@ -1,15 +1,19 @@
 import { SingleRecordActionHookWithObjectMetadataItem } from '@/action-menu/actions/types/SingleRecordActionHook';
+import { ActionMenuContext } from '@/action-menu/contexts/ActionMenuContext';
 import { useRecordShowPagePagination } from '@/object-record/record-show/hooks/useRecordShowPagePagination';
+import { useContext } from 'react';
 
 export const useNavigateToNextRecordSingleRecordAction: SingleRecordActionHookWithObjectMetadataItem =
   ({ recordId, objectMetadataItem }) => {
+    const { isInRightDrawer } = useContext(ActionMenuContext);
+
     const { navigateToNextRecord } = useRecordShowPagePagination(
       objectMetadataItem.nameSingular,
       recordId,
     );
 
     return {
-      shouldBeRegistered: true,
+      shouldBeRegistered: !isInRightDrawer,
       onClick: navigateToNextRecord,
     };
   };
