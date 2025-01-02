feb318bcc7d8f76e800f5c28b0b54ab6017c0652
Thomas Trompette <thomas.trompette@sfr.fr>
2025-01-02 11:39:17 +0100

    Put actions in steps + move hooks

--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramStepNodeEditable.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramStepNodeEditable.tsx
@@ -1,9 +1,9 @@
-import { useDeleteStep } from '@/workflow/hooks/useDeleteStep';
 import { useWorkflowWithCurrentVersion } from '@/workflow/hooks/useWorkflowWithCurrentVersion';
 import { workflowIdState } from '@/workflow/states/workflowIdState';
 import { assertWorkflowWithCurrentVersionIsDefined } from '@/workflow/utils/assertWorkflowWithCurrentVersionIsDefined';
 import { WorkflowDiagramStepNodeBase } from '@/workflow/workflow-diagram/components/WorkflowDiagramStepNodeBase';
 import { WorkflowDiagramStepNodeData } from '@/workflow/workflow-diagram/types/WorkflowDiagram';
+import { useDeleteStep } from '@/workflow/workflow-steps/hooks/useDeleteStep';
 import { useRecoilValue } from 'recoil';
 import { FloatingIconButton, IconTrash } from 'twenty-ui';
 
