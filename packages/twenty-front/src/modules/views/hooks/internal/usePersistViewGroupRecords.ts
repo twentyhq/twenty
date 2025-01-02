35eaf0ff9028fcf0000e21c964633bb693a1364e
Jérémy Magrin <jeremy.magrin@gmail.com>
2025-01-02 16:05:10 +0100

    fix: multiple record created in cache when calling useUpdateOneRecord

--- packages/twenty-front/src/modules/views/hooks/internal/usePersistViewGroupRecords.ts
+++ packages/twenty-front/src/modules/views/hooks/internal/usePersistViewGroupRecords.ts
@@ -3,17 +3,20 @@ import { useCallback } from 'react';
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { useCreateManyRecords } from '@/object-record/hooks/useCreateManyRecords';
 import { useDestroyManyRecords } from '@/object-record/hooks/useDestroyManyRecords';
-import { useUpdateOneRecord } from '@/object-record/hooks/useUpdateOneRecord';
+import { useUpdateOneRecordMutation } from '@/object-record/hooks/useUpdateOneRecordMutation';
 import { GraphQLView } from '@/views/types/GraphQLView';
 import { ViewGroup } from '@/views/types/ViewGroup';
+import { useApolloClient } from '@apollo/client';
 
 export const usePersistViewGroupRecords = () => {
+  const apolloClient = useApolloClient();
+
   const { createManyRecords } = useCreateManyRecords({
     objectNameSingular: CoreObjectNameSingular.ViewGroup,
     shouldMatchRootQueryFilter: true,
   });
 
-  const { updateOneRecord } = useUpdateOneRecord({
+  const { updateOneRecordMutation } = useUpdateOneRecordMutation({
     objectNameSingular: CoreObjectNameSingular.ViewGroup,
   });
 
@@ -41,19 +44,43 @@ export const usePersistViewGroupRecords = () => {
     async (viewGroupsToUpdate: ViewGroup[]) => {
       if (!viewGroupsToUpdate.length) return;
 
-      return Promise.all(
-        viewGroupsToUpdate.map((viewGroup) =>
-          updateOneRecord({
+      const mutationPromises = viewGroupsToUpdate.map((viewGroup) =>
+        apolloClient.mutate<{ updateViewGroup: ViewGroup }>({
+          mutation: updateOneRecordMutation,
+          variables: {
             idToUpdate: viewGroup.id,
-            updateOneRecordInput: {
+            input: {
               isVisible: viewGroup.isVisible,
               position: viewGroup.position,
             },
-          }),
-        ),
+          },
+          // Avoid cache being updated with stale data
+          fetchPolicy: 'no-cache',
+        }),
       );
+
+      const mutationResults = await Promise.all(mutationPromises);
+
+      // FixMe: Using useUpdateOneRecord hook that call triggerUpdateRecordsOptimisticEffect is actaully causing multiple records to be created
+      // This is a temporary fix
+      mutationResults.forEach(({ data }) => {
+        const record = data?.['updateViewGroup'];
+
+        if (!record) return;
+
+        apolloClient.cache.modify({
+          id: apolloClient.cache.identify({
+            __typename: 'ViewGroup',
+            id: record.id,
+          }),
+          fields: {
+            isVisible: () => record.isVisible,
+            position: () => record.position,
+          },
+        });
+      });
     },
-    [updateOneRecord],
+    [apolloClient, updateOneRecordMutation],
   );
 
   const deleteViewGroupRecords = useCallback(
