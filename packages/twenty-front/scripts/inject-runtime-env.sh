b2eceebdebafa7b961ef9b11887ca93782a18922
Félix Malfait <felix@twenty.com>
2025-01-02 22:51:23 +0100

    Remove env-config.js

--- packages/twenty-front/scripts/inject-runtime-env.sh
+++ packages/twenty-front/scripts/inject-runtime-env.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-echo "Generating env-config.js file from runtime environment variables..."
 
-BASE_FILENAME="build/env-config.js"
-mkdir -p build
-rm -rf "./$BASE_FILENAME"
+echo "Injecting runtime environment variables into index.html..."
 
-{
-	echo "window._env_ = {"
-	echo "  REACT_APP_SERVER_BASE_URL: \"$REACT_APP_SERVER_BASE_URL\","
-	echo "}"
-} > "./$BASE_FILENAME"
+CONFIG_BLOCK=$(cat << EOF
+<!-- BEGIN: Twenty Config -->
+    <script id="twenty-env-config">
+      window._env_ = {
+        REACT_APP_SERVER_BASE_URL: "$REACT_APP_SERVER_BASE_URL"
+      };
+    </script>
+<!-- END: Twenty Config -->
+EOF
+)
+
+# Use sed to replace the config block in index.html
+# The .* is non-greedy in sed by default
+sed -i.bak "s|<!-- BEGIN: Twenty Config -->.*<!-- END: Twenty Config -->|$CONFIG_BLOCK|" build/index.html
+rm -f build/index.html.bak
