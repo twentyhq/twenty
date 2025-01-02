c90d9a66ad959d5d575a731e5eec2d02c4293a20
guillim <guigloo@msn.com>
2025-01-02 18:13:44 +0100

    "'TODO'" is the default value from the database and must be escaped in the frontend

--- packages/twenty-front/src/modules/object-record/utils/generateDefaultFieldValue.ts
+++ packages/twenty-front/src/modules/object-record/utils/generateDefaultFieldValue.ts
@@ -2,6 +2,7 @@ import { FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { isFieldValueEmpty } from '@/object-record/record-field/utils/isFieldValueEmpty';
 import { generateEmptyFieldValue } from '@/object-record/utils/generateEmptyFieldValue';
 import { v4 } from 'uuid';
+import { stripSimpleQuotesFromString } from '~/utils/string/stripSimpleQuotesFromString';
 
 export const generateDefaultFieldValue = (
   fieldMetadataItem: Pick<FieldMetadataItem, 'defaultValue' | 'type'>,
@@ -11,7 +12,7 @@ export const generateDefaultFieldValue = (
     fieldDefinition: fieldMetadataItem,
   })
     ? generateEmptyFieldValue(fieldMetadataItem)
-    : fieldMetadataItem.defaultValue;
+    : stripSimpleQuotesFromString(fieldMetadataItem.defaultValue);
 
   switch (defaultValue) {
     case 'uuid':
