d8e2550aa49f65bf9db008ceb4bed3c2b4ddb869
Félix Malfait <felix@twenty.com>
2025-01-02 11:20:48 +0100

    Hide tests

--- packages/twenty-e2e-testing/tests/all/workspaces.e2e-spec.ts
+++ packages/twenty-e2e-testing/tests/all/workspaces.e2e-spec.ts
@@ -1,16 +1,18 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import { sh } from '../../drivers/shell_driver';
 
 test.describe('', () => {
   test.use({ storageState: { cookies: [], origins: [] } });
 
-  test('Creating new workspace with multi workspaces not enabled', async ({
-    page,
-    browserName,
-  }) => {
+  /*
+
+  test('Creating new workspace', async ({ page, browserName }) => {
     // this test must use only 1 browser, otherwise it will lead to success and fail (1 workspace is created instead of x workspaces)
-    await sh('npx nx run twenty-server:database:reset --configuration=no-seed');
     if (browserName == 'chromium') {
+      await sh(
+        'npx nx run twenty-server:database:reset --configuration=no-seed',
+      );
+
       await page.goto('/');
       await page.getByRole('button', { name: 'Continue With Email' }).click();
       await page.getByPlaceholder('Email').fill('test@apple.dev'); // email must be changed each time test is run
@@ -27,9 +29,10 @@ test.describe('', () => {
       await page.getByText('Continue without sync').click();
       await page.getByRole('button', { name: 'Finish' }).click();
       await expect(page.locator('table')).toBeVisible({ timeout: 1000 });
-    }
-    await sh('npx nx run twenty-server:database:reset');
+      await sh('npx nx run twenty-server:database:reset');
+   }
   });
+  */
 
   test('Syncing all workspaces', async () => {
     await sh('npx nx run twenty-server:command workspace:sync-metadata -f');
