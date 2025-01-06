71e8b5103d6a929c95d055e4a951504a2f34771e
Eddie <eddie_aparecido@hotmail.com>
2025-01-06 17:09:47 -0300

    Update: header roles overview

--- packages/twenty-front/src/pages/settings/roles/SettingsRolesOverview.tsx
+++ packages/twenty-front/src/pages/settings/roles/SettingsRolesOverview.tsx
@@ -4,17 +4,23 @@ import { getSettingsPagePath } from '@/settings/utils/getSettingsPagePath';
 import { SettingsPath } from '@/types/SettingsPath';
 import { SubMenuTopBarContainer } from '@/ui/layout/page/components/SubMenuTopBarContainer';
 import { ReactFlowProvider } from '@xyflow/react';
+import { useTranslation } from 'react-i18next';
 
 export const SettingsRolesOverview = () => {
+  const { t } = useTranslation();
   return (
     <SubMenuTopBarContainer 
-        links={[
-            {
-                children: 'New',
-                href: getSettingsPagePath(SettingsPath.NewRole),
-            },
-        ]}  
-        title="Settings"
+    links={[
+      {
+          children: 'Workspace',
+          href: getSettingsPagePath(SettingsPath.Workspace),
+      },
+      {
+        children: 'Roles',
+        href: getSettingsPagePath(SettingsPath.MembersRoles),
+      },
+      ]} 
+      title={""}
     >
       <ReactFlowProvider>
         <SettingsDataRoleOverview />
