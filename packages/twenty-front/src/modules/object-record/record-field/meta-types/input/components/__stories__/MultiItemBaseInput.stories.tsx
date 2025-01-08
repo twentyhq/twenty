af46d1894fc9d14ca8ad69bb9be96b47849dc337
etiennejouan <jouan.etienne@gmail.com>
2025-01-08 15:58:12 +0100

    fix: update storybook component path

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/__stories__/MultiItemBaseInput.stories.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/__stories__/MultiItemBaseInput.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentDecorator } from 'twenty-ui';
 import { MultiItemBaseInput } from '../MultiItemBaseInput';
 
 const meta: Meta<typeof MultiItemBaseInput> = {
-  title: 'UI/Data/Field/Input/Base',
+  title: 'UI/Data/Field/Input/BaseFieldInput',
   component: MultiItemBaseInput,
   decorators: [ComponentDecorator],
   args: { value: 'Lorem ipsum' },
