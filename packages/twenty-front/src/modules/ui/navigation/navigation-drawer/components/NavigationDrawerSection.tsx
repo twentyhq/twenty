bec7911d59bc465e0cef563c947491d65e0e1385
nitin <142569587+ehconitin@users.noreply.github.com>
2025-01-08 19:33:47 +0530

    Navigation drawer scroll padding fix (#9141)
    closes https://github.com/twentyhq/twenty/issues/9026
    fixes #9312
    
    
    
    https://github.com/user-attachments/assets/3d7df3ec-8a5e-4308-8993-82c715edc683
    
    ---------
    
    Co-authored-by: Lucas Bordeau <bordeau.lucas@gmail.com>

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawerSection.tsx
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawerSection.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { useIsMobile } from 'twenty-ui';
 
 const StyledSection = styled.div`
   display: flex;
@@ -9,4 +10,25 @@ const StyledSection = styled.div`
   flex-shrink: 1;
 `;
 
-export { StyledSection as NavigationDrawerSection };
+const StyledSectionInnerContainerMinusScrollPadding = styled.div<{
+  isMobile: boolean;
+}>`
+  width: calc(
+    100% - ${({ isMobile, theme }) => (isMobile ? 0 : theme.spacing(2))}
+  );
+`;
+
+export const NavigationDrawerSection = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const isMobile = useIsMobile();
+  return (
+    <StyledSection>
+      <StyledSectionInnerContainerMinusScrollPadding isMobile={isMobile}>
+        {children}
+      </StyledSectionInnerContainerMinusScrollPadding>
+    </StyledSection>
+  );
+};
