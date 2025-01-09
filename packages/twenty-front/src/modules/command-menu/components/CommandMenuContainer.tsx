7c23add0592eb5cfdaf13b3a09cc14c909963160
bosiraphael <raphael.bosi@gmail.com>
2025-01-09 18:19:05 +0100

    add animation to command menu on open and on close

--- packages/twenty-front/src/modules/command-menu/components/CommandMenuContainer.tsx
+++ packages/twenty-front/src/modules/command-menu/components/CommandMenuContainer.tsx
@@ -3,6 +3,8 @@ import { RecordAgnosticActionsSetterEffect } from '@/action-menu/actions/record-
 import { ActionMenuConfirmationModals } from '@/action-menu/components/ActionMenuConfirmationModals';
 import { ActionMenuContext } from '@/action-menu/contexts/ActionMenuContext';
 import { ActionMenuComponentInstanceContext } from '@/action-menu/states/contexts/ActionMenuComponentInstanceContext';
+import { CommandMenuAnimationVariant } from '@/command-menu/constants/CommandMenuAnimationVariant';
+import { COMMAND_MENU_ANIMATION_VARIANTS } from '@/command-menu/constants/CommandMenuAnimationVariants';
 import { useCommandMenu } from '@/command-menu/hooks/useCommandMenu';
 import { useCommandMenuHotKeys } from '@/command-menu/hooks/useCommandMenuHotKeys';
 import { isCommandMenuOpenedState } from '@/command-menu/states/isCommandMenuOpenedState';
@@ -10,13 +12,15 @@ import { ContextStoreComponentInstanceContext } from '@/context-store/states/con
 import { AppHotkeyScope } from '@/ui/utilities/hotkey/types/AppHotkeyScope';
 import { useListenClickOutside } from '@/ui/utilities/pointer-event/hooks/useListenClickOutside';
 import { useIsFeatureEnabled } from '@/workspace/hooks/useIsFeatureEnabled';
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useRef } from 'react';
 import { useRecoilValue } from 'recoil';
 import { useIsMobile } from 'twenty-ui';
 import { FeatureFlagKey } from '~/generated/graphql';
 
-const StyledCommandMenu = styled.div`
+const StyledCommandMenu = styled(motion.div)`
   background: ${({ theme }) => theme.background.secondary};
   border-left: 1px solid ${({ theme }) => theme.border.color.medium};
   box-shadow: ${({ theme }) => theme.boxShadow.strong};
@@ -27,7 +31,6 @@ const StyledCommandMenu = styled.div`
   position: fixed;
   right: 0%;
   top: 0%;
-  width: ${() => (useIsMobile() ? '100%' : '500px')};
   z-index: 30;
 `;
 
@@ -54,6 +57,14 @@ export const CommandMenuContainer = ({
     hotkeyScope: AppHotkeyScope.CommandMenuOpen,
   });
 
+  const isMobile = useIsMobile();
+
+  const targetVariantForAnimation: CommandMenuAnimationVariant = isMobile
+    ? 'fullScreen'
+    : 'normal';
+
+  const theme = useTheme();
+
   return (
     <ContextStoreComponentInstanceContext.Provider
       value={{ instanceId: 'command-menu' }}
@@ -70,11 +81,23 @@ export const CommandMenuContainer = ({
           <RecordActionMenuEntriesSetter />
           {isWorkflowEnabled && <RecordAgnosticActionsSetterEffect />}
           <ActionMenuConfirmationModals />
-          {isCommandMenuOpened && (
-            <StyledCommandMenu ref={commandMenuRef} className="command-menu">
-              {children}
-            </StyledCommandMenu>
-          )}
+          <AnimatePresence>
+            {isCommandMenuOpened && (
+              <StyledCommandMenu
+                ref={commandMenuRef}
+                className="command-menu"
+                animate={targetVariantForAnimation}
+                initial="closed"
+                exit="closed"
+                variants={COMMAND_MENU_ANIMATION_VARIANTS}
+                transition={{
+                  duration: theme.animation.duration.normal,
+                }}
+              >
+                {children}
+              </StyledCommandMenu>
+            )}
+          </AnimatePresence>
         </ActionMenuContext.Provider>
       </ActionMenuComponentInstanceContext.Provider>
     </ContextStoreComponentInstanceContext.Provider>
