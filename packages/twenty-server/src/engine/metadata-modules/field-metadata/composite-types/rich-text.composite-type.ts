513a6236554571109c4b21e555dc8f89aecc0462
ad-elias <elias@autodiligence.com>
2025-01-10 14:06:14 +0100

    Fix import

--- packages/twenty-server/src/engine/metadata-modules/field-metadata/composite-types/rich-text.composite-type.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/composite-types/rich-text.composite-type.ts
@@ -1,8 +1,7 @@
+import { FieldMetadataType } from 'twenty-shared';
 import { z } from 'zod';
 
 import { CompositeType } from 'src/engine/metadata-modules/field-metadata/interfaces/composite-type.interface';
-
-import { FieldMetadataType } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 
 export const richTextCompositeType: CompositeType = {
   type: FieldMetadataType.RICH_TEXT,
