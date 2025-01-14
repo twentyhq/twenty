c2964e116454ef21f4cb62fb22d7f8eb561d60a4
prastoin <paul@twenty.com>
2025-01-14 16:40:13 +0100

    chore: to drop

--- packages/twenty-front/src/index.tsx
+++ packages/twenty-front/src/index.tsx
@@ -5,7 +5,7 @@ import '@emotion/react';
 import { App } from '@/app/components/App';
 import 'react-loading-skeleton/dist/skeleton.css';
 import './index.css';
-
+// Trigger ci-front
 const root = ReactDOM.createRoot(
   document.getElementById('root') ?? document.body,
 );
