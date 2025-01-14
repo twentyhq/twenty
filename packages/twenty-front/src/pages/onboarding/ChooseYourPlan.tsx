e239fc105270f678d8a7ebaa37d21314b0f889ad
martmull <martmull@hotmail.fr>
2025-01-14 11:27:58 +0100

    Fix flash on checkoutUrl loading

--- packages/twenty-front/src/pages/onboarding/ChooseYourPlan.tsx
+++ packages/twenty-front/src/pages/onboarding/ChooseYourPlan.tsx
@@ -156,7 +156,7 @@ export const ChooseYourPlan = () => {
     handleCheckoutSession();
   }
 
-  if (billingCheckoutSession.skipPlanPage || isSubmitting) {
+  if (billingCheckoutSession.skipPlanPage && isSubmitting) {
     return <Loader />;
   }
 
