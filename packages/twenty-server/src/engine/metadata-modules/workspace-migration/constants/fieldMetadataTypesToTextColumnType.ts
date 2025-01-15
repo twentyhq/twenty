911ea791faa3b03003703356c7e4868c5de3c98a
Marie Stoppa <marie.stoppa@essec.edu>
2025-01-15 16:21:22 +0100

    Fix empty / not empty operations on composite fields

--- packages/twenty-server/src/engine/metadata-modules/workspace-migration/constants/fieldMetadataTypesToTextColumnType.ts
+++ packages/twenty-server/src/engine/metadata-modules/workspace-migration/constants/fieldMetadataTypesToTextColumnType.ts
@@ -4,4 +4,9 @@ export const FIELD_METADATA_TYPES_TO_TEXT_COLUMN_TYPE = [
   FieldMetadataType.TEXT,
   FieldMetadataType.RICH_TEXT,
   FieldMetadataType.ARRAY,
+  FieldMetadataType.ADDRESS,
+  FieldMetadataType.LINKS,
+  FieldMetadataType.FULL_NAME,
+  FieldMetadataType.PHONES,
+  FieldMetadataType.EMAILS,
 ];
