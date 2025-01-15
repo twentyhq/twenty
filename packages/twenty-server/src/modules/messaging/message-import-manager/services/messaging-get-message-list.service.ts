76a0f6741c32182ad0ceec1c3297c670e5a7160b
guillim <guigloo@msn.com>
2025-01-15 18:16:40 +0100

    creation of the future outlook icon

--- packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-get-message-list.service.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-get-message-list.service.ts
@@ -29,7 +29,7 @@ export class MessagingGetMessageListService {
   public async getFullMessageList(
     connectedAccount: Pick<
       ConnectedAccountWorkspaceEntity,
-      'provider' | 'refreshToken' | 'id'
+      'provider' | 'refreshToken' | 'id' | 'handle'
     >,
   ): Promise<GetFullMessageListResponse> {
     switch (connectedAccount.provider) {
