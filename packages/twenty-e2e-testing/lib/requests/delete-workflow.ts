5421eee1414d97da8312ffc215265b971083e06b
Devessier <baptiste@devessier.fr>
2025-01-16 18:40:38 +0100

    test: centralize backend url declaration and rename first test

--- packages/twenty-e2e-testing/lib/requests/delete-workflow.ts
+++ packages/twenty-e2e-testing/lib/requests/delete-workflow.ts
@@ -1,21 +1,19 @@
 import { Page } from '@playwright/test';
 import { getAuthToken } from '../utils/getAuthToken';
+import { backendGraphQLUrl } from './backend';
 
 export const deleteWorkflow = async (page: Page, workflowId: string) => {
-  const { authToken } = await getAuthToken(page)
+  const { authToken } = await getAuthToken(page);
 
-  return page.request.post(
-    new URL('/graphql', process.env.BACKEND_BASE_URL).toString(),
-    {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-      data: {
-        operationName: 'DeleteOneWorkflow',
-        variables: { idToDelete: workflowId },
-        query:
-          'mutation DeleteOneWorkflow($idToDelete: ID!) {\n  deleteWorkflow(id: $idToDelete) {\n    __typename\n    deletedAt\n    id\n  }\n}',
-      },
+  return page.request.post(backendGraphQLUrl, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
     },
-  );
+    data: {
+      operationName: 'DeleteOneWorkflow',
+      variables: { idToDelete: workflowId },
+      query:
+        'mutation DeleteOneWorkflow($idToDelete: ID!) {\n  deleteWorkflow(id: $idToDelete) {\n    __typename\n    deletedAt\n    id\n  }\n}',
+    },
+  });
 };
