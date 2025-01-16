9f319e38663c6126b38280ae4974e721018589cb
ehconitin <nitinkoche03@gmail.com>
2025-01-17 00:38:36 +0530

    test fix

--- packages/twenty-server/src/engine/core-modules/labs/utils/is-public-feature-flag.util.spec.ts
+++ packages/twenty-server/src/engine/core-modules/labs/utils/is-public-feature-flag.util.spec.ts
@@ -1,15 +1,20 @@
+import { PUBLIC_FEATURE_FLAGS } from 'src/engine/core-modules/feature-flag/constants/public-feature-flag.const';
 import { FeatureFlagKey } from 'src/engine/core-modules/feature-flag/enums/feature-flag-key.enum';
 import { isPublicFeatureFlag } from 'src/engine/core-modules/labs/utils/is-public-feature-flag.util';
 
 describe('isPublicFeatureFlag', () => {
   it('should return true for public flags', () => {
-    expect(isPublicFeatureFlag(FeatureFlagKey.IsCommandMenuV2Enabled)).toBe(
-      true,
-    );
+    const publicFlag = PUBLIC_FEATURE_FLAGS[0];
+
+    expect(isPublicFeatureFlag(publicFlag)).toBe(true);
   });
 
   it('should return false for non-public flags', () => {
-    expect(isPublicFeatureFlag(FeatureFlagKey.IsWorkflowEnabled)).toBe(false);
+    const nonPublicFlag = Object.values(FeatureFlagKey).find(
+      (flag) => !PUBLIC_FEATURE_FLAGS.includes(flag as any),
+    );
+
+    expect(isPublicFeatureFlag(nonPublicFlag as FeatureFlagKey)).toBe(false);
   });
 
   it('should return false for undefined/null', () => {
