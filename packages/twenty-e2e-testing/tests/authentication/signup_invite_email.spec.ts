a5880afdf8a7ba4407c02afe2fabe3aca790dbad
Devessier <baptiste@devessier.fr>
2025-01-17 12:17:19 +0100

    test: generate a new user for every test
    Deleting an account doesn't clear the user from the database, it just removes the user from the workspace.
    The test used to fail when ran several times.

--- packages/twenty-e2e-testing/tests/authentication/signup_invite_email.spec.ts
+++ packages/twenty-e2e-testing/tests/authentication/signup_invite_email.spec.ts
@@ -1,64 +1,60 @@
+import { randomUUID } from 'crypto';
 import { expect, test } from './fixture';
 
-test.describe('Authentication test', () => {
-  const email = 'test@apple.dev';
+test('Sign up with invite link via email', async ({
+  page,
+  loginPage,
+  leftMenu,
+  membersSection,
+  settingsPage,
+  profileSection,
+  confirmationModal,
+}) => {
+  const email = `test${randomUUID().replaceAll('-', '')}@apple.dev`;
   const firstName = 'John';
   const lastName = 'Doe';
-  let testCompleted = false;
 
-  test('Sign up with invite link via email', async ({
-    page,
-    loginPage,
-    leftMenu,
-    membersSection,
-    settingsPage,
-  }) => {
-    const inviteLink: string =
-      await test.step('Go to Settings and copy invite link', async () => {
-        await page.goto(process.env.LINK); // skip login page (and redirect) when running on environments with multi-workspace enabled
-        await leftMenu.goToSettings();
-        await settingsPage.goToMembersSection();
-        await membersSection.copyInviteLink();
-        return await page.evaluate('navigator.clipboard.readText()');
-      });
-    await test.step('Go to invite link', async () => {
-      await settingsPage.logout();
-      await page.goto(inviteLink);
+  const inviteLink: string =
+    await test.step('Go to Settings and copy invite link', async () => {
+      await page.goto(process.env.LINK); // skip login page (and redirect) when running on environments with multi-workspace enabled
+      await leftMenu.goToSettings();
+      await settingsPage.goToMembersSection();
+      await membersSection.copyInviteLink();
+      return await page.evaluate('navigator.clipboard.readText()');
     });
-    await test.step('Create new account', async () => {
-      await loginPage.clickLoginWithEmail();
-      await loginPage.typeEmail(email);
-      await loginPage.clickContinueButton();
-      await loginPage.typePassword(process.env.DEFAULT_PASSWORD);
-      await loginPage.clickSignUpButton();
-      await loginPage.typeFirstName(firstName);
-      await loginPage.typeLastName(lastName);
-      await loginPage.clickContinueButton();
-      await loginPage.noSyncWithGoogle();
-      testCompleted = true;
-    });
+
+  await test.step('Go to invite link', async () => {
+    await settingsPage.logout();
+
+    await Promise.all([
+      expect(page.getByText(/Join .+ team/)).toBeVisible(),
+
+      page.goto(inviteLink),
+    ]);
   });
 
-  test.afterEach(
-    async ({
-      page,
-      confirmationModal,
-      leftMenu,
-      profileSection,
-      settingsPage,
-    }) => {
-      if (testCompleted) {
-        // security measurement to clean up only after test is completed,
-        // otherwise default account used for tests may be deleted and resetting database will be necessary
-        await test.step('Cleanup - deleting account', async () => {
-          await leftMenu.goToSettings();
-          await settingsPage.goToProfileSection();
-          await profileSection.deleteAccount();
-          await confirmationModal.typePlaceholderToInput();
-          await confirmationModal.clickConfirmButton();
-          expect(page.url()).toContain('/welcome');
-        });
-      }
-    },
-  );
+  await test.step('Create new account', async () => {
+    await loginPage.clickLoginWithEmail();
+    await loginPage.typeEmail(email);
+    await loginPage.clickContinueButton();
+    await loginPage.typePassword(process.env.DEFAULT_PASSWORD);
+    await loginPage.clickSignUpButton();
+    await loginPage.typeFirstName(firstName);
+    await loginPage.typeLastName(lastName);
+    await loginPage.clickContinueButton();
+    await loginPage.noSyncWithGoogle();
+  });
+
+  await test.step('Delete account from workspace', async () => {
+    await leftMenu.goToSettings();
+    await settingsPage.goToProfileSection();
+    await profileSection.deleteAccount();
+    await confirmationModal.typePlaceholderToInput();
+
+    await Promise.all([
+      page.waitForURL('/welcome'),
+
+      confirmationModal.clickConfirmButton(),
+    ]);
+  });
 });
