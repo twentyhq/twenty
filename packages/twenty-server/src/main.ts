95cf1da44fffd328c2d7c96d8b0cee4077a5d6db
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-01-17 17:20:49 +0100

    refactor(server): move session middleware initialization
    Relocated the session middleware initialization to an earlier point in the code for better alignment and clarity. This change ensures proper setup and avoids redundancy in the middleware application.

--- packages/twenty-server/src/main.ts
+++ packages/twenty-server/src/main.ts
@@ -38,6 +38,8 @@ const bootstrap = async () => {
   const logger = app.get(LoggerService);
   const environmentService = app.get(EnvironmentService);
 
+  app.use(session(getSessionStorageOptions(environmentService)));
+
   // TODO: Double check this as it's not working for now, it's going to be helpful for durable trees in twenty "orm"
   // // Apply context id strategy for durable trees
   // ContextIdFactory.apply(new AggregateByWorkspaceContextIdStrategy());
@@ -82,9 +84,6 @@ const bootstrap = async () => {
 
   // Inject the server url in the frontend page
   generateFrontConfig();
-
-  // Enable session - Today it's used only for SSO
-  app.use(session(getSessionStorageOptions(environmentService)));
 
   // Trust the first proxy to ensure correct client IP and protocol detection.
   app.set('trust proxy', 1);
