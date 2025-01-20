f685df9f762db77dabde3ea69e51584bd63cd6b5
Charles Bochet <charles@twenty.com>
2025-01-20 22:19:43 +0100

    Fix

--- packages/twenty-front/jest.config.ts
+++ packages/twenty-front/jest.config.ts
@@ -27,9 +27,7 @@ const jestConfig: JestConfigWithTsJest = {
     '\\.(jpg|jpeg|png|gif|webp|svg|svg\\?react)$':
       '<rootDir>/__mocks__/imageMock.js',
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
-    ...pathsToModuleNameMapper(tsConfig.compilerOptions.paths, {
-      prefix: '<rootDir>/../..',
-    }),
+    ...pathsToModuleNameMapper(tsConfig.compilerOptions.paths),
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
