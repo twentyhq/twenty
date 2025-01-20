cb2bee2df834c408759f4e9222c921fb36549c54
Devessier <baptiste@devessier.fr>
2025-01-20 12:04:44 +0100

    test: delete redundant test

--- packages/twenty-e2e-testing/tests/workflow-visualizer.spec.ts
+++ packages/twenty-e2e-testing/tests/workflow-visualizer.spec.ts
@@ -1,42 +1,6 @@
 import { expect } from '@playwright/test';
 import { test } from '../lib/fixtures/blank-workflow';
 
-test('Create simple workflow', async ({
-  workflowVisualizer: _workflowVisualizer,
-  page,
-}) => {
-  const addTriggerButton = page.getByText('Add a Trigger');
-  await addTriggerButton.click();
-
-  const triggerOption = page.getByText('Record is Created');
-  await triggerOption.click();
-
-  await expect(
-    page.getByTestId('command-menu').getByRole('textbox'),
-  ).toHaveValue('Record is Created');
-
-  const triggerNode = page.getByTestId('rf__node-trigger');
-  await expect(triggerNode).toHaveClass(/selected/);
-  await expect(triggerNode).toHaveText(/Record is Created/);
-
-  const addStepButton = page.getByLabel('Add a step');
-  await addStepButton.click();
-
-  const createRecordOption = page.getByText('Create Record');
-
-  await createRecordOption.click();
-
-  await expect(
-    page.getByTestId('command-menu').getByRole('textbox').first(),
-  ).toHaveValue('Create Record');
-
-  const createRecordNode = page
-    .locator('.react-flow__node.selected')
-    .getByText('Create Record');
-  await expect(createRecordNode).toBeVisible();
-  await expect(triggerNode).not.toHaveClass(/selected/);
-});
-
 test('Create workflow with every possible step', async ({
   workflowVisualizer,
   page,
