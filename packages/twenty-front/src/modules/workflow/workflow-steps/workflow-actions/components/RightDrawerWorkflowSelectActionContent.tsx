efe34a4a15d1f64a8b58aa61c8c4df7966c7309f
Thomas Trompette <thomas.trompette@sfr.fr>
2025-01-20 17:48:14 +0100

    Split paddings

--- packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/components/RightDrawerWorkflowSelectActionContent.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/components/RightDrawerWorkflowSelectActionContent.tsx
@@ -19,8 +19,10 @@ const StyledSectionTitle = styled.span`
   color: ${({ theme }) => theme.font.color.tertiary};
   font-size: ${({ theme }) => theme.font.size.sm};
   font-weight: ${({ theme }) => theme.font.weight.medium};
-  padding: ${({ theme }) => theme.spacing(2)} ${({ theme }) => theme.spacing(1)}
-    ${({ theme }) => theme.spacing(1)} ${({ theme }) => theme.spacing(1)};
+  padding-top: ${({ theme }) => theme.spacing(2)};
+  padding-bottom: ${({ theme }) => theme.spacing(1)};
+  padding-left: ${({ theme }) => theme.spacing(1)};
+  padding-right: ${({ theme }) => theme.spacing(1)};
 `;
 
 export const RightDrawerWorkflowSelectActionContent = ({
