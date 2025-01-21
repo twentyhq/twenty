0b5fbc27bf9d3602c613c7615aa4714c1db9e13b
Thomas Trompette <thomas.trompette@sfr.fr>
2025-01-21 14:49:04 +0100

    Hide workflows and favorites behind feature flag

--- packages/twenty-front/src/modules/object-metadata/hooks/useRefreshObjectMetadataItem.ts
+++ packages/twenty-front/src/modules/object-metadata/hooks/useRefreshObjectMetadataItem.ts
@@ -2,10 +2,13 @@ import { FIND_MANY_OBJECT_METADATA_ITEMS } from '@/object-metadata/graphql/queri
 import { useApolloMetadataClient } from '@/object-metadata/hooks/useApolloMetadataClient';
 import { isAppWaitingForFreshObjectMetadataState } from '@/object-metadata/states/isAppWaitingForFreshObjectMetadataState';
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
+import { isWorkflowSubObjectMetadata } from '@/object-metadata/utils/isWorkflowSubObjectMetadata';
 import { mapPaginatedObjectMetadataItemsToObjectMetadataItems } from '@/object-metadata/utils/mapPaginatedObjectMetadataItemsToObjectMetadataItems';
+import { useIsFeatureEnabled } from '@/workspace/hooks/useIsFeatureEnabled';
 import { useRecoilCallback } from 'recoil';
-import { ObjectMetadataItemsQuery } from '~/generated-metadata/graphql';
+import { FeatureFlagKey, ObjectMetadataItemsQuery } from '~/generated-metadata/graphql';
 import { isDeeplyEqual } from '~/utils/isDeeplyEqual';
 
 type FetchPolicy = 'network-only' | 'cache-first';
@@ -14,6 +17,9 @@ export const useRefreshObjectMetadataItems = (
   fetchPolicy: FetchPolicy = 'cache-first',
 ) => {
   const client = useApolloMetadataClient();
+  const isWorkflowEnabled = useIsFeatureEnabled(
+    FeatureFlagKey.IsWorkflowEnabled,
+  );
 
   const refreshObjectMetadataItems = async () => {
     const result = await client.query<ObjectMetadataItemsQuery>({
@@ -27,7 +33,14 @@ export const useRefreshObjectMetadataItems = (
         pagedObjectMetadataItems: result.data,
       });
 
-    replaceObjectMetadataItemIfDifferent(objectMetadataItems);
+    const filteredObjectMetadataItems = objectMetadataItems.filter((object) => {
+      return isWorkflowEnabled || (
+        object.nameSingular !== CoreObjectNameSingular.Workflow && 
+        !isWorkflowSubObjectMetadata(object.nameSingular)
+      );
+    });
+
+    replaceObjectMetadataItemIfDifferent(filteredObjectMetadataItems);
   };
 
   const replaceObjectMetadataItemIfDifferent = useRecoilCallback(
