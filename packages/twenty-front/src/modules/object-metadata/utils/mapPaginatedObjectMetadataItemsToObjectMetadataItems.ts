613da26546e043b273e09c804de4be64777564a0
prastoin <paul@twenty.com>
2025-01-21 19:10:32 +0100

    refactor(front): validate instead of force cassting wip

--- packages/twenty-front/src/modules/object-metadata/utils/mapPaginatedObjectMetadataItemsToObjectMetadataItems.ts
+++ packages/twenty-front/src/modules/object-metadata/utils/mapPaginatedObjectMetadataItemsToObjectMetadataItems.ts
@@ -1,3 +1,4 @@
+import { isString } from '@sniptt/guards';
 import { ObjectMetadataItemsQuery } from '~/generated-metadata/graphql';
 import { ObjectMetadataItem } from '../types/ObjectMetadataItem';
 
@@ -7,18 +8,25 @@ export const mapPaginatedObjectMetadataItemsToObjectMetadataItems = ({
   pagedObjectMetadataItems: ObjectMetadataItemsQuery | undefined;
 }) => {
   const formattedObjects: ObjectMetadataItem[] =
-    pagedObjectMetadataItems?.objects.edges.map((object) => ({
-      ...object.node,
-      fields: object.node.fields.edges.map((field) => field.node),
-      labelIdentifierFieldMetadataId: object.node
-        .labelIdentifierFieldMetadataId as string,
-      indexMetadatas: object.node.indexMetadatas?.edges.map((index) => ({
-        ...index.node,
-        indexFieldMetadatas: index.node.indexFieldMetadatas?.edges.map(
-          (indexField) => indexField.node,
-        ),
-      })),
-    })) ?? [];
+    pagedObjectMetadataItems?.objects.edges.map((object) => {
+      const {labelIdentifierFieldMetadataId} = object.node;
+      // Could be zod parse tbh to gain grain on uuid parse
+      if (!isString(labelIdentifierFieldMetadataId)) {
+        throw new Error("Should never occurs, labelIdentifierFieldMetadataId is not a string")
+      };
+
+      return {
+        ...object.node,
+        fields: object.node.fields.edges.map((field) => field.node),
+        labelIdentifierFieldMetadataId,
+        indexMetadatas: object.node.indexMetadatas?.edges.map((index) => ({
+          ...index.node,
+          indexFieldMetadatas: index.node.indexFieldMetadatas?.edges.map(
+            (indexField) => indexField.node,
+          ),
+        }))
+      }
+    }) ?? [];
 
   return formattedObjects;
 };
