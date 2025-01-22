a661b62d0a505cfa6111af05c67d7f9e7f563c18
Baptiste Devessier <baptiste@devessier.fr>
2025-01-22 11:03:36 +0100

    Align the workflow visualizer's nodes on the left (#9776)
    **This PR implements a new layout for the visualizer, but the dimensions
    of the nodes will change soon. I used hard-coded dimensions, like
    `40px`, but I'll update them when I work on fixing the nodes' design. I
    think we can merge this PR first and then fix the nodes' design.**
    
    https://github.com/user-attachments/assets/580fa812-ee8e-4452-b6ac-ca55ecb31759

--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramCreateStepNode.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramCreateStepNode.tsx
@@ -1,19 +1,15 @@
-import { StyledHandle } from '@/workflow/workflow-diagram/components/WorkflowDiagramStepNodeBase';
-import { NODE_BORDER_WIDTH } from '@/workflow/workflow-diagram/constants/NodeBorderWidth';
-import { NODE_ICON_LEFT_MARGIN } from '@/workflow/workflow-diagram/constants/NodeIconLeftMargin';
-import { NODE_ICON_WIDTH } from '@/workflow/workflow-diagram/constants/NodeIconWidth';
 import styled from '@emotion/styled';
-import { Position } from '@xyflow/react';
+import { Handle, Position } from '@xyflow/react';
 import { IconButton, IconPlus } from 'twenty-ui';
 
 const StyledContainer = styled.div`
-  padding-top: ${({ theme }) => theme.spacing(3)};
-  transform: translateX(-50%);
+  padding-left: ${({ theme }) => theme.spacing(6)};
+  padding-top: ${({ theme }) => theme.spacing(1)};
   position: relative;
-  left: ${NODE_ICON_WIDTH + NODE_ICON_LEFT_MARGIN + NODE_BORDER_WIDTH}px;
 `;
 
-const StyledTargetHandle = styled(StyledHandle)`
+export const StyledTargetHandle = styled(Handle)`
+  left: ${({ theme }) => theme.spacing(10)};
   visibility: hidden;
 `;
 
