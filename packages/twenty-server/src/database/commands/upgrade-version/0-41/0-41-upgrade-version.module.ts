99fbd2f2bed059858844b70d9a3cf99b600a3ec8
Jérémy M <jeremy.magrin@gmail.com>
2025-01-22 17:01:54 +0100

    feat: add targetFieldMetadataId and migration script for relations (#9793)
    Fix https://github.com/twentyhq/core-team-issues/issues/238 and
    https://github.com/twentyhq/core-team-issues/issues/239

--- packages/twenty-server/src/database/commands/upgrade-version/0-41/0-41-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-41/0-41-upgrade-version.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { MigrateRelationsToFieldMetadataCommand } from 'src/database/commands/upgrade-version/0-41/0-41-migrate-relations-to-field-metadata.command';
 import { SeedWorkflowViewsCommand } from 'src/database/commands/upgrade-version/0-41/0-41-seed-workflow-views.command';
 import { UpgradeTo0_41Command } from 'src/database/commands/upgrade-version/0-41/0-41-upgrade-version.command';
 import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
+import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { ObjectMetadataModule } from 'src/engine/metadata-modules/object-metadata/object-metadata.module';
 import { WorkspaceHealthModule } from 'src/engine/workspace-manager/workspace-health/workspace-health.module';
 import { SyncWorkspaceLoggerService } from 'src/engine/workspace-manager/workspace-sync-metadata/commands/services/sync-workspace-logger.service';
@@ -16,6 +18,7 @@ import { WorkspaceSyncMetadataModule } from 'src/engine/workspace-manager/worksp
 @Module({
   imports: [
     TypeOrmModule.forFeature([Workspace], 'core'),
+    TypeOrmModule.forFeature([FieldMetadataEntity], 'metadata'),
     TypeORMModule,
     DataSourceModule,
     ObjectMetadataModule,
@@ -28,6 +31,7 @@ import { WorkspaceSyncMetadataModule } from 'src/engine/workspace-manager/worksp
     SyncWorkspaceMetadataCommand,
     SeedWorkflowViewsCommand,
     UpgradeTo0_41Command,
+    MigrateRelationsToFieldMetadataCommand,
   ],
 })
 export class UpgradeTo0_41CommandModule {}
--- packages/twenty-server/src/engine/metadata-modules/field-metadata/interfaces/field-metadata-settings.interface.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/interfaces/field-metadata-settings.interface.ts
@@ -1,5 +1,8 @@
 import { FieldMetadataType } from 'twenty-shared';
 
+import { RelationOnDeleteAction } from 'src/engine/metadata-modules/field-metadata/interfaces/relation-on-delete-action.interface';
+import { RelationType } from 'src/engine/metadata-modules/field-metadata/interfaces/relation-type.interface';
+
 export enum NumberDataType {
   FLOAT = 'float',
   INT = 'int',
@@ -30,11 +33,17 @@ export type FieldMetadataDateTimeSettings = {
   displayAsRelativeDate?: boolean;
 };
 
+export type FieldMetadataRelationSettings = {
+  relationType: RelationType;
+  onDelete?: RelationOnDeleteAction;
+};
+
 type FieldMetadataSettingsMapping = {
   [FieldMetadataType.NUMBER]: FieldMetadataNumberSettings;
   [FieldMetadataType.DATE]: FieldMetadataDateSettings;
   [FieldMetadataType.DATE_TIME]: FieldMetadataDateTimeSettings;
   [FieldMetadataType.TEXT]: FieldMetadataTextSettings;
+  [FieldMetadataType.RELATION]: FieldMetadataRelationSettings;
 };
 
 type SettingsByFieldMetadata<T extends FieldMetadataType | 'default'> =
