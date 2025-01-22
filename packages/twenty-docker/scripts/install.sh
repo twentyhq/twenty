6578d4eac741248b1c38e3f034f882fe2a07f16c
Félix Malfait <felix@twenty.com>
2025-01-22 11:04:34 +0100

    Charles PR comment

--- packages/twenty-docker/scripts/install.sh
+++ packages/twenty-docker/scripts/install.sh
@@ -90,11 +90,11 @@ else
 fi
 
 # Generate random strings for secrets
-echo "# === Randomly generated secret ===" >>.env
-echo "APP_SECRET=$(openssl rand -base64 32)" >>.env
+echo "# === Randomly generated secret ===" >> .env
+echo "APP_SECRET=$(openssl rand -base64 32)" >> .env
 
-echo "" >>.env
-echo "PGPASSWORD_SUPERUSER=$(openssl rand -hex 16)" >>.env
+echo "" >> .env
+echo "PGPASSWORD_SUPERUSER=$(openssl rand -hex 16)" >> .env
 
 echo -e "\t• .env configuration completed"
 
