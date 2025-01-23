881a8e00a1248a1793861ada82b2eb2a2a242f93
Félix Malfait <felix@twenty.com>
2025-01-23 15:00:13 +0100

    Improve typing

--- packages/twenty-server/src/modules/timeline/services/timeline-activity.service.ts
+++ packages/twenty-server/src/modules/timeline/services/timeline-activity.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
-import { ObjectRecordBaseEvent } from 'src/engine/core-modules/event-emitter/types/object-record.base.event';
+import { ObjectRecordNonDestructiveEvent } from 'src/engine/core-modules/event-emitter/types/object-record-non-destructive-event';
 import { InjectObjectMetadataRepository } from 'src/engine/object-metadata-repository/object-metadata-repository.decorator';
 import { WorkspaceDataSourceService } from 'src/engine/workspace-datasource/workspace-datasource.service';
 import { TimelineActivityRepository } from 'src/modules/timeline/repositiories/timeline-activity.repository';
 import { TimelineActivityWorkspaceEntity } from 'src/modules/timeline/standard-objects/timeline-activity.workspace-entity';
 
-type TimelineActivity =
-  ObjectRecordBaseEvent<TimelineActivityWorkspaceEntity> & {
-    name: string;
-    objectName?: string;
-    linkedRecordCachedName?: string;
-    linkedRecordId?: string;
-    linkedObjectMetadataId?: string;
-  };
+type TimelineActivity = ObjectRecordNonDestructiveEvent & {
+  name: string;
+  objectName?: string;
+  linkedRecordCachedName?: string;
+  linkedRecordId?: string;
+  linkedObjectMetadataId?: string;
+};
 
 @Injectable()
 export class TimelineActivityService {
@@ -33,7 +32,7 @@ export class TimelineActivityService {
     eventName,
     workspaceId,
   }: {
-    event: ObjectRecordBaseEvent<TimelineActivityWorkspaceEntity>;
+    event: ObjectRecordNonDestructiveEvent;
     eventName: string;
     workspaceId: string;
   }) {
@@ -65,7 +64,7 @@ export class TimelineActivityService {
     workspaceId,
     eventName,
   }: {
-    event: ObjectRecordBaseEvent<TimelineActivityWorkspaceEntity>;
+    event: ObjectRecordNonDestructiveEvent;
     workspaceId: string;
     eventName: string;
   }): Promise<TimelineActivity[] | undefined> {
@@ -78,7 +77,10 @@ export class TimelineActivityService {
 
       // 2 timelines, one for the linked object and one for the task/note
       if (linkedTimelineActivities && linkedTimelineActivities?.length > 0)
-        return [...linkedTimelineActivities, { ...event, name: eventName }];
+        return [
+          ...linkedTimelineActivities,
+          { ...event, name: eventName },
+        ] satisfies TimelineActivity[];
     }
 
     if (
@@ -93,7 +95,7 @@ export class TimelineActivityService {
       });
     }
 
-    return [{ ...event, name: eventName }];
+    return [{ ...event, name: eventName }] satisfies TimelineActivity[];
   }
 
   private async getLinkedTimelineActivities({
@@ -101,7 +103,7 @@ export class TimelineActivityService {
     workspaceId,
     eventName,
   }: {
-    event: ObjectRecordBaseEvent<TimelineActivityWorkspaceEntity>;
+    event: ObjectRecordNonDestructiveEvent;
     workspaceId: string;
     eventName: string;
   }): Promise<TimelineActivity[] | undefined> {
@@ -146,7 +148,7 @@ export class TimelineActivityService {
     eventName,
     workspaceId,
   }: {
-    event: ObjectRecordBaseEvent<TimelineActivityWorkspaceEntity>;
+    event: ObjectRecordNonDestructiveEvent;
     dataSourceSchema: string;
     activityType: string;
     eventName: string;
@@ -195,7 +197,7 @@ export class TimelineActivityService {
           linkedRecordCachedName: activity[0].title,
           linkedRecordId: activity[0].id,
           linkedObjectMetadataId: event.objectMetadata.id,
-        } as TimelineActivity;
+        } satisfies TimelineActivity;
       })
       .filter((event): event is TimelineActivity => event !== undefined);
   }
@@ -207,7 +209,7 @@ export class TimelineActivityService {
     eventName,
     workspaceId,
   }: {
-    event: ObjectRecordBaseEvent<TimelineActivityWorkspaceEntity>;
+    event: ObjectRecordNonDestructiveEvent;
     dataSourceSchema: string;
     activityType: string;
     eventName: string;
@@ -258,7 +260,7 @@ export class TimelineActivityService {
         linkedRecordCachedName: activity[0].title,
         linkedRecordId: activity[0].id,
         linkedObjectMetadataId: activityObjectMetadataId,
-      } as TimelineActivity,
+      } satisfies TimelineActivity,
     ];
   }
 }
