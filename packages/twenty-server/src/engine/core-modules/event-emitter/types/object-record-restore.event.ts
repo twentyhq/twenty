266963b6b8268cb55f96004c51aeb5f5df016a35
Charles Bochet <charles@twenty.com>
2025-01-23 17:11:50 +0100

    Fix

--- packages/twenty-server/src/engine/core-modules/event-emitter/types/object-record-restore.event.ts
+++ packages/twenty-server/src/engine/core-modules/event-emitter/types/object-record-restore.event.ts
@@ -1,8 +1,8 @@
-import { ObjectRecordCreateEvent } from 'src/engine/core-modules/event-emitter/types/object-record-create.event';
+import { ObjectRecordBaseEvent } from 'src/engine/core-modules/event-emitter/types/object-record.base.event';
 
 export class ObjectRecordRestoreEvent<
   T = object,
-> extends ObjectRecordCreateEvent<T> {
+> extends ObjectRecordBaseEvent<T> {
   properties: {
     after: T;
   };
