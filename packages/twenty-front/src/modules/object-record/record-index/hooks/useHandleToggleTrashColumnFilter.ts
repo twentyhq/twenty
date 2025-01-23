bddca094511d0355d956d88d92c4b22b201d6d51
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-01-23 11:09:44 +0100

    Refactored table filters to consume new currentRecordFilters component state (#9652)
    This PR implements a first real use case, now currentRecordFilters
    component state acts as the global record filter reference.
    
    It is set by the view initially and can be reset to view filters state
    at any point.
    
    This new state is also modified by two new upsertRecordFilter /
    removeRecordFilter hooks that will be drop-in replacement of the actual
    upsertCombinedViewFilter and removeCombinediewFilter hooks.
    
    This PR implements the logic to manipulate record filters but only reads
    it to make the table find many request, all other features are still
    relying on the old view filter implementation.
    
    Advanced filters are ignored because they are hidden and because this
    effort is made precisely to allow the completion of the advanced filters
    feature.

--- packages/twenty-front/src/modules/object-record/record-index/hooks/useHandleToggleTrashColumnFilter.ts
+++ packages/twenty-front/src/modules/object-record/record-index/hooks/useHandleToggleTrashColumnFilter.ts
@@ -4,6 +4,7 @@ import { v4 } from 'uuid';
 import { useColumnDefinitionsFromFieldMetadata } from '@/object-metadata/hooks/useColumnDefinitionsFromFieldMetadata';
 import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
 import { getFilterTypeFromFieldType } from '@/object-metadata/utils/formatFieldMetadataItemsAsFilterDefinitions';
+import { useUpsertRecordFilter } from '@/object-record/record-filter/hooks/useUpsertRecordFilter';
 import { RecordFilter } from '@/object-record/record-filter/types/RecordFilter';
 import { isSoftDeleteFilterActiveComponentState } from '@/object-record/record-table/states/isSoftDeleteFilterActiveComponentState';
 import { useRecoilComponentCallbackStateV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentCallbackStateV2';
@@ -35,6 +36,8 @@ export const useHandleToggleTrashColumnFilter = ({
       isSoftDeleteFilterActiveComponentState,
       viewBarId,
     );
+
+  const { upsertRecordFilter } = useUpsertRecordFilter();
 
   const handleToggleTrashColumnFilter = useCallback(() => {
     const trashFieldMetadata = objectMetadataItem.fields.find(
@@ -69,8 +72,14 @@ export const useHandleToggleTrashColumnFilter = ({
       value: '',
     };
 
+    upsertRecordFilter(newFilter);
     upsertCombinedViewFilter(newFilter);
-  }, [columnDefinitions, objectMetadataItem, upsertCombinedViewFilter]);
+  }, [
+    columnDefinitions,
+    objectMetadataItem,
+    upsertCombinedViewFilter,
+    upsertRecordFilter,
+  ]);
 
   const toggleSoftDeleteFilterState = useRecoilCallback(
     ({ set }) =>
