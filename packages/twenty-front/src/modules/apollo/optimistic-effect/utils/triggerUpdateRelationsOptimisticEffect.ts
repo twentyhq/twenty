9451bea6d92fdd03b66591925ee7183dfd53a78f
prastoin <paul@twenty.com>
2025-01-23 17:55:23 +0100

    refactor(front): consume metadatas to compute if current value is recordConnection

--- packages/twenty-front/src/modules/apollo/optimistic-effect/utils/triggerUpdateRelationsOptimisticEffect.ts
+++ packages/twenty-front/src/modules/apollo/optimistic-effect/utils/triggerUpdateRelationsOptimisticEffect.ts
@@ -5,28 +5,106 @@ import { triggerDestroyRecordsOptimisticEffect } from '@/apollo/optimistic-effec
 import { triggerDetachRelationOptimisticEffect } from '@/apollo/optimistic-effect/utils/triggerDetachRelationOptimisticEffect';
 import { CORE_OBJECT_NAMES_TO_DELETE_ON_TRIGGER_RELATION_DETACH } from '@/apollo/types/coreObjectNamesToDeleteOnRelationDetach';
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
-import { isObjectRecordConnection } from '@/object-record/cache/utils/isObjectRecordConnection';
 import { RecordGqlConnection } from '@/object-record/graphql/types/RecordGqlConnection';
 import { RecordGqlNode } from '@/object-record/graphql/types/RecordGqlNode';
 import { ObjectRecord } from '@/object-record/types/ObjectRecord';
-import { FieldMetadataType } from '~/generated-metadata/graphql';
+import { assertUnreachable } from '@/workflow/utils/assertUnreachable';
+import isEmpty from 'lodash.isempty';
+import {
+  FieldMetadataType,
+  RelationDefinitionType,
+} from '~/generated-metadata/graphql';
 import { isDeeplyEqual } from '~/utils/isDeeplyEqual';
 import { isDefined } from '~/utils/isDefined';
 
+// TODO should be extracted outside of the file
+type ToNameNode = RecordGqlConnection | RecordGqlNode | null;
+type GetDetachMetricsArgs = {
+  relationDefinition: NonNullable<FieldMetadataItem['relationDefinition']>;
+  currentFieldValueOnSourceRecord: ToNameNode;
+  updatedFieldValueOnSourceRecord: ToNameNode;
+};
+type GetDetachMetricsReport = {
+  targetRecordsToDetachFrom: RecordGqlNode[];
+  targetRecordsToAttachTo: RecordGqlNode[];
+};
+const getDetachMetricsToRename = ({
+  currentFieldValueOnSourceRecord,
+  relationDefinition,
+  updatedFieldValueOnSourceRecord,
+}: GetDetachMetricsArgs): GetDetachMetricsReport => {
+  // console.log(
+  //   relationDefinition,
+  //   currentFieldValueOnSourceRecord,
+  //   updatedFieldValueOnSourceRecord,
+  // );
+  const computeIsRecordConnection = () => {
+    switch (relationDefinition.direction) {
+      case RelationDefinitionType.MANY_TO_MANY:
+      case RelationDefinitionType.ONE_TO_MANY: {
+        return true;
+      }
+      case RelationDefinitionType.MANY_TO_ONE:
+      case RelationDefinitionType.ONE_TO_ONE: {
+        return false;
+      }
+      default: {
+        return assertUnreachable(relationDefinition.direction);
+      }
+    }
+  };
+  const isRecordConnection = computeIsRecordConnection();
+  const functionToGiveANameTo = (value: ToNameNode): RecordGqlNode[] => {
+    console.log(value, relationDefinition.direction);
+    // I don't understand typing here, we sometimes received empty array ?
+    // Zod schema validation was stripping this use case before
+    if (!isDefined(value) || isEmpty(value)) {
+      return [];
+    }
+
+    if (isRecordConnection) {
+      return value?.edges.map(({ node }: any) => node as RecordGqlNode);
+    }
+
+    return [value] as RecordGqlNode[];
+  };
+
+  const targetRecordsToDetachFrom = functionToGiveANameTo(
+    currentFieldValueOnSourceRecord,
+  );
+  const targetRecordsToAttachTo = functionToGiveANameTo(
+    updatedFieldValueOnSourceRecord,
+  );
+
+  return {
+    targetRecordsToDetachFrom,
+    targetRecordsToAttachTo,
+  };
+};
+
+type triggerUpdateRelationsOptimisticEffectArgs = {
+  cache: ApolloCache<unknown>;
+  sourceObjectMetadataItem: ObjectMetadataItem;
+  currentSourceRecord: ObjectRecord | null;
+  updatedSourceRecord: ObjectRecord | null;
+  objectMetadataItems: ObjectMetadataItem[];
+};
 export const triggerUpdateRelationsOptimisticEffect = ({
   cache,
   sourceObjectMetadataItem,
   currentSourceRecord,
   updatedSourceRecord,
   objectMetadataItems,
-}: {
-  cache: ApolloCache<unknown>;
-  sourceObjectMetadataItem: ObjectMetadataItem;
-  currentSourceRecord: ObjectRecord | null;
-  updatedSourceRecord: ObjectRecord | null;
-  objectMetadataItems: ObjectMetadataItem[];
-}) => {
+}: triggerUpdateRelationsOptimisticEffectArgs) => {
+  console.log({
+    cache,
+    sourceObjectMetadataItem,
+    currentSourceRecord,
+    updatedSourceRecord,
+    objectMetadataItems,
+  });
   return sourceObjectMetadataItem.fields.forEach(
     (fieldMetadataItemOnSourceRecord) => {
       const notARelationField =
@@ -81,37 +159,17 @@ export const triggerUpdateRelationsOptimisticEffect = ({
       ) {
         return;
       }
-
-      // TODO: replace this by a relation type check, if it's one to many,
-      //   it's an object record connection (we can still check it though as a safeguard)
-      const currentFieldValueOnSourceRecordIsARecordConnection =
-        isObjectRecordConnection(
-          targetObjectMetadata.nameSingular,
+      const { targetRecordsToAttachTo, targetRecordsToDetachFrom } =
+        getDetachMetricsToRename({
           currentFieldValueOnSourceRecord,
-        );
+          relationDefinition,
+          updatedFieldValueOnSourceRecord,
+        });
 
-      // const targetRecordsToDetachFrom =
-      //   currentFieldValueOnSourceRecordIsARecordConnection
-      //     ? currentFieldValueOnSourceRecord.edges.map(
-      //         ({ node }) => node as RecordGqlNode,
-      //       )
-      //     : [currentFieldValueOnSourceRecord].filter(isDefined);
-
-      // const updatedFieldValueOnSourceRecordIsARecordConnection =
-      //   isObjectRecordConnection(
-      //     targetObjectMetadata.nameSingular,
-      //     updatedFieldValueOnSourceRecord,
-      //   );
-
-      // const targetRecordsToAttachTo =
-      //   updatedFieldValueOnSourceRecordIsARecordConnection
-      //     ? updatedFieldValueOnSourceRecord.edges.map(
-      //         ({ node }) => node as RecordGqlNode,
-      //       )
-      //     : [updatedFieldValueOnSourceRecord].filter(isDefined);
-
-      // const shouldDetachSourceFromAllTargets =
-      //   isDefined(currentSourceRecord) && targetRecordsToDetachFrom.length > 0;
+      // From my understanding could be named
+      // hasCachedSourceThatCouldBeDetach
+      const shouldDetachSourceFromAllTargets =
+        isDefined(currentSourceRecord) && targetRecordsToDetachFrom.length > 0;
 
       if (shouldDetachSourceFromAllTargets) {
         // TODO: see if we can de-hardcode this, put cascade delete in relation metadata item
@@ -146,14 +204,14 @@ export const triggerUpdateRelationsOptimisticEffect = ({
         isDefined(updatedSourceRecord) && targetRecordsToAttachTo.length > 0;
 
       if (shouldAttachSourceToAllTargets) {
-        targetRecordsToAttachTo.forEach((targetRecordToAttachTo) =>
+        targetRecordsToAttachTo.forEach((targetRecordsToAttachTo) =>
           triggerAttachRelationOptimisticEffect({
             cache,
             sourceObjectNameSingular: sourceObjectMetadataItem.nameSingular,
             sourceRecordId: updatedSourceRecord.id,
             fieldNameOnTargetRecord: targetFieldMetadata.name,
             targetObjectNameSingular: targetObjectMetadata.nameSingular,
-            targetRecordId: targetRecordToAttachTo.id,
+            targetRecordId: targetRecordsToAttachTo.id,
           }),
         );
       }
