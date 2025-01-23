27d5a6a8a9fd9ff1597724c5cb9d1b0d43f1605f
Guillim <guillim@users.noreply.github.com>
2025-01-23 12:08:08 +0100

    Microsoft-multi-tenant (#9801)
    Microsoft fixes

--- packages/twenty-server/src/engine/core-modules/auth/guards/microsoft-apis-oauth-exchange-code-for-token.guard.ts
+++ packages/twenty-server/src/engine/core-modules/auth/guards/microsoft-apis-oauth-exchange-code-for-token.guard.ts
@@ -8,16 +8,12 @@ import {
 import { MicrosoftAPIsOauthExchangeCodeForTokenStrategy } from 'src/engine/core-modules/auth/strategies/microsoft-apis-oauth-exchange-code-for-token.auth.strategy';
 import { setRequestExtraParams } from 'src/engine/core-modules/auth/utils/google-apis-set-request-extra-params.util';
 import { EnvironmentService } from 'src/engine/core-modules/environment/environment.service';
-import { GuardRedirectService } from 'src/engine/core-modules/guard-redirect/services/guard-redirect.service';
 
 @Injectable()
 export class MicrosoftAPIsOauthExchangeCodeForTokenGuard extends AuthGuard(
   'microsoft-apis',
 ) {
-  constructor(
-    private readonly guardRedirectService: GuardRedirectService,
-    private readonly environmentService: EnvironmentService,
-  ) {
+  constructor(private readonly environmentService: EnvironmentService) {
     super();
   }
 
@@ -25,16 +21,6 @@ export class MicrosoftAPIsOauthExchangeCodeForTokenGuard extends AuthGuard(
     try {
       const request = context.switchToHttp().getRequest();
       const state = JSON.parse(request.query.state);
-
-      if (
-        !this.environmentService.get('MESSAGING_PROVIDER_MICROSOFT_ENABLED') &&
-        !this.environmentService.get('CALENDAR_PROVIDER_MICROSOFT_ENABLED')
-      ) {
-        throw new AuthException(
-          'Microsoft apis auth is not enabled',
-          AuthExceptionCode.MICROSOFT_API_AUTH_DISABLED,
-        );
-      }
 
       new MicrosoftAPIsOauthExchangeCodeForTokenStrategy(
         this.environmentService,
@@ -49,18 +35,12 @@ export class MicrosoftAPIsOauthExchangeCodeForTokenGuard extends AuthGuard(
 
       return (await super.canActivate(context)) as boolean;
     } catch (error) {
-      this.guardRedirectService.dispatchErrorFromGuard(
-        context,
-        error?.oauthError?.statusCode === 403
-          ? new AuthException(
-              `Insufficient privileges to access this microsoft resource. Make sure you have the correct scopes or ask your admin to update your scopes. ${error?.message}`,
-              AuthExceptionCode.INSUFFICIENT_SCOPES,
-            )
-          : error,
-        this.guardRedirectService.getSubdomainAndCustomDomainFromContext(
-          context,
-        ),
-      );
+      if (error?.oauthError?.statusCode === 403) {
+        throw new AuthException(
+          `Insufficient privileges to access this microsoft resource. Make sure you have the correct scopes or ask your admin to update your scopes. ${error?.message}`,
+          AuthExceptionCode.INSUFFICIENT_SCOPES,
+        );
+      }
 
       return false;
     }
