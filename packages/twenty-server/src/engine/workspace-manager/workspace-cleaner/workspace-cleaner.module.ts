3302f02d9f8bf01d5272af2fde129e7db5e587c2
etiennejouan <jouan.etienne@gmail.com>
2025-01-23 09:36:35 +0100

    fix: fix after greptile review

--- packages/twenty-server/src/engine/workspace-manager/workspace-cleaner/workspace-cleaner.module.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-cleaner/workspace-cleaner.module.ts
@@ -7,9 +7,9 @@ import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-s
 import { CleanInactiveWorkspacesCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/clean-inactive-workspaces.command';
 import { DeleteWorkspacesCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/delete-workspaces.command';
 import { StartCleanInactiveWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/start-clean-inactive-workspaces.cron.command';
-import { StartCleanSuspendedWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/start-clean-suspended-workspaces.cron.commands';
+import { StartCleanSuspendedWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/start-clean-suspended-workspaces.cron.command';
 import { StopCleanInactiveWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/stop-clean-inactive-workspaces.cron.command';
-import { StopCleanSuspendedWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/stop-clean-suspended-workspaces.cron.commands';
+import { StopCleanSuspendedWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/stop-clean-suspended-workspaces.cron.command';
 
 @Module({
   imports: [
