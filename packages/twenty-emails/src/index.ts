fe891b7419296b4596e2556e6e4d18d6e080c5f9
etiennejouan <jouan.etienne@gmail.com>
2025-01-24 11:55:15 +0100

    remove old clean inactive workspace commands + job

--- packages/twenty-emails/src/index.ts
+++ packages/twenty-emails/src/index.ts
@@ -1,5 +1,3 @@
-export * from './emails/clean-inactive-workspaces.email';
-export * from './emails/delete-inactive-workspaces.email';
 export * from './emails/password-reset-link.email';
 export * from './emails/password-update-notify.email';
 export * from './emails/send-email-verification-link.email';
--- packages/twenty-server/src/engine/workspace-manager/workspace-cleaner/workspace-cleaner.module.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-cleaner/workspace-cleaner.module.ts
@@ -4,11 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { WorkspaceModule } from 'src/engine/core-modules/workspace/workspace.module';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
-import { CleanInactiveWorkspacesCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/clean-inactive-workspaces.command';
 import { CleanSuspendedWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/clean-suspended-workspaces.cron.command';
 import { DeleteWorkspacesCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/delete-workspaces.command';
-import { StartCleanInactiveWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/start-clean-inactive-workspaces.cron.command';
-import { StopCleanInactiveWorkspacesCronCommand } from 'src/engine/workspace-manager/workspace-cleaner/commands/stop-clean-inactive-workspaces.cron.command';
 
 @Module({
   imports: [
@@ -16,12 +13,6 @@ import { StopCleanInactiveWorkspacesCronCommand } from 'src/engine/workspace-man
     WorkspaceModule,
     DataSourceModule,
   ],
-  providers: [
-    DeleteWorkspacesCommand,
-    CleanInactiveWorkspacesCommand,
-    StartCleanInactiveWorkspacesCronCommand,
-    StopCleanInactiveWorkspacesCronCommand,
-    CleanSuspendedWorkspacesCronCommand,
-  ],
+  providers: [DeleteWorkspacesCommand, CleanSuspendedWorkspacesCronCommand],
 })
 export class WorkspaceCleanerModule {}
