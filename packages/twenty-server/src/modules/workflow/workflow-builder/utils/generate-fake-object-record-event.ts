f605641ebb1f5459639d78f8285650ead15e7a0e
Thomas Trompette <thomas.trompette@sfr.fr>
2025-01-24 17:51:37 +0100

    Provide better labels for events

--- packages/twenty-server/src/modules/workflow/workflow-builder/utils/generate-fake-object-record-event.ts
+++ packages/twenty-server/src/modules/workflow/workflow-builder/utils/generate-fake-object-record-event.ts
@@ -48,7 +48,7 @@ export const generateFakeObjectRecordEvent = (
       ...baseResult,
       properties: {
         isLeaf: false,
-        value: { after: { isLeaf: false, value: after, label: 'After' } },
+        value: { after: { isLeaf: false, value: after, label: 'After Creation' } },
         label: 'Record Fields',
       },
     };
@@ -62,8 +62,8 @@ export const generateFakeObjectRecordEvent = (
       properties: {
         isLeaf: false,
         value: {
-          before: { isLeaf: false, value: before, label: 'Before' },
-          after: { isLeaf: false, value: after, label: 'After' },
+          before: { isLeaf: false, value: before, label: 'Before Update' },
+          after: { isLeaf: false, value: after, label: 'After Update' },
         },
         label: 'Record Fields',
       },
@@ -76,7 +76,7 @@ export const generateFakeObjectRecordEvent = (
       properties: {
         isLeaf: false,
         value: {
-          before: { isLeaf: false, value: before, label: 'Before' },
+          before: { isLeaf: false, value: before, label: 'Before Deletion' },
         },
         label: 'Record Fields',
       },
@@ -89,7 +89,7 @@ export const generateFakeObjectRecordEvent = (
       properties: {
         isLeaf: false,
         value: {
-          before: { isLeaf: false, value: before, label: 'Before' },
+          before: { isLeaf: false, value: before, label: 'Before Deletion' },
         },
         label: 'Record Fields',
       },
