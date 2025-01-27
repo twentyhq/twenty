7aa4a3df2cf0d2843c27699675585234600bcfca
bosiraphael <raphael.bosi@gmail.com>
2025-01-27 11:25:31 +0100

    update component

--- packages/twenty-front/src/modules/command-menu/actions-list/components/CommandMenuList.tsx
+++ packages/twenty-front/src/modules/command-menu/actions-list/components/CommandMenuList.tsx
@@ -1,28 +1,26 @@
-import { ActionMenuEntry } from '@/action-menu/types/ActionMenuEntry';
+import { CommandMenuItem } from '@/command-menu/components/CommandMenuItem';
 import { COMMAND_MENU_SEARCH_BAR_HEIGHT } from '@/command-menu/constants/CommandMenuSearchBarHeight';
 import { COMMAND_MENU_SEARCH_BAR_PADDING } from '@/command-menu/constants/CommandMenuSearchBarPadding';
+import { useMatchCommands } from '@/command-menu/hooks/useMatchCommands';
+import { commandMenuSearchState } from '@/command-menu/states/commandMenuSearchState';
+import { Command } from '@/command-menu/types/Command';
 import { SelectableItem } from '@/ui/layout/selectable-list/components/SelectableItem';
 import { SelectableList } from '@/ui/layout/selectable-list/components/SelectableList';
 import { AppHotkeyScope } from '@/ui/utilities/hotkey/types/AppHotkeyScope';
 import { ScrollWrapper } from '@/ui/utilities/scroll/components/ScrollWrapper';
 import styled from '@emotion/styled';
-import { Button, useIsMobile } from 'twenty-ui';
+import { useRecoilValue } from 'recoil';
+import { useIsMobile } from 'twenty-ui';
 
 const MOBILE_NAVIGATION_BAR_HEIGHT = 64;
 
-export type ActionsListProps = {
-  entries: ActionMenuEntry[];
+export type CommandMenuListProps = {
+  commands: Command[];
   filtering: boolean;
   isLoading: boolean;
-  isShowingDetail: boolean;
-  navigationTitle: string;
   pagination: { hasMore: boolean; pageSize: number; onLoadMore: () => void };
   searchBarPlaceholder: string;
-  searchText: string;
-  selectedItemId: string;
   throttle: boolean;
-  onSearchTextChange: (text: string) => void;
-  onSelectionChange: (id: string) => void;
 };
 
 const StyledList = styled.div`
@@ -47,21 +45,23 @@ const StyledInnerList = styled.div<{ isMobile: boolean }>`
   width: calc(100% - ${({ theme }) => theme.spacing(4)});
 `;
 
-export const ActionsList = ({
-  entries,
-  filtering,
-  isLoading,
-  isShowingDetail,
-  navigationTitle,
+export const CommandMenuList = ({
+  commands,
+  filtering = true,
+  isLoading = false,
   pagination,
-  searchBarPlaceholder,
-  searchText,
-  selectedItemId,
-  throttle,
-  onSearchTextChange,
-  onSelectionChange,
-}: ActionsListProps) => {
+  //searchBarPlaceholder = '',
+  //throttle,
+}: CommandMenuListProps) => {
   const isMobile = useIsMobile();
+
+  const commandMenuSearch = useRecoilValue(commandMenuSearchState);
+
+  const { matchCommands } = useMatchCommands({
+    commandMenuSearch,
+  });
+
+  const filteredCommands = filtering ? matchCommands(commands) : commands;
 
   return (
     <StyledList>
@@ -71,12 +71,23 @@ export const ActionsList = ({
       >
         <StyledInnerList isMobile={isMobile}>
           <SelectableList
-            selectableListId="actions-list"
+            selectableListId="command-menu-list"
             hotkeyScope={AppHotkeyScope.CommandMenuOpen}
           >
-            <SelectableItem itemId="">
-              <Button>Test</Button>
-            </SelectableItem>
+            {filteredCommands.map((command) => (
+              <SelectableItem itemId={command.id} key={command.id}>
+                <CommandMenuItem
+                  id={command.id}
+                  Icon={command.Icon}
+                  label={command.label}
+                  onClick={command.onCommandClick}
+                  to={command.to}
+                  shouldCloseCommandMenuOnClick={
+                    command.shouldCloseCommandMenuOnClick
+                  }
+                />
+              </SelectableItem>
+            ))}
           </SelectableList>
         </StyledInnerList>
       </ScrollWrapper>
