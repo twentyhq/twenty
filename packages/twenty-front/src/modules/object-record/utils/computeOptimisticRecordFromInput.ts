b1823e0756b220aef3c29c7fa0c42753740cc08d
prastoin <paul@twenty.com>
2025-01-27 18:22:04 +0100

    feat(front): retrieve from cache relation in optimistic behavior sanitizer

--- packages/twenty-front/src/modules/object-record/utils/computeOptimisticRecordFromInput.ts
+++ packages/twenty-front/src/modules/object-record/utils/computeOptimisticRecordFromInput.ts
@@ -1,22 +1,32 @@
 import { isNull, isString } from '@sniptt/guards';
 
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
+import {
+  getRecordFromCache,
+  GetRecordFromCacheArgs,
+} from '@/object-record/cache/utils/getRecordFromCache';
+import { generateDepthOneRecordGqlFields } from '@/object-record/graphql/utils/generateDepthOneRecordGqlFields';
 import { ObjectRecord } from '@/object-record/types/ObjectRecord';
 import { RelationDefinitionType } from '~/generated-metadata/graphql';
 import { FieldMetadataType } from '~/generated/graphql';
 import { isDefined } from '~/utils/isDefined';
 import { getUrlHostName } from '~/utils/url/getUrlHostName';
+
 type ComputeOptimisticCacheRecordInputArgs = {
+  // Could be renamed
   objectMetadataItem: ObjectMetadataItem;
   recordInput: Partial<ObjectRecord>;
-};
+} & Pick<GetRecordFromCacheArgs, 'cache' | 'objectMetadataItems'>;
 // TODO refactor everything here my dude
 export const computeOptimisticRecordFromInput = ({
   objectMetadataItem,
   recordInput,
+  cache,
+  objectMetadataItems,
 }: ComputeOptimisticCacheRecordInputArgs) => {
   console.log({ recordInput });
-  const recordInputEntries = Object.entries<Partial<ObjectRecord>>(recordInput);
+  // TODO strictly type as possible
+  const recordInputEntries = Object.entries(recordInput);
   const sanitizedEntries = objectMetadataItem.fields.flatMap(
     (fieldMetadataItem) => {
       const isRelationField =
@@ -67,13 +77,52 @@ export const computeOptimisticRecordFromInput = ({
             [fieldIdName, null],
             [fieldMetadataItem.name, null],
           ];
-        } else {
-          const fromCacheStuff = { from: 'cache', id: fieldIdValue };
-          return [
-            [fieldIdName, recordInput[fieldIdName] ?? null],
-            [fieldMetadataItem.name, fromCacheStuff],
-          ];
         }
+        // Is it the best way to retrieve the related objectMetadaItem it's log(n)
+        const targetNameSingular = fieldMetadataItem.relationDefinition?.targetObjectMetadata.nameSingular;
+        const relationObjectMetataDataItem = objectMetadataItems.find(({nameSingular}) => nameSingular === targetNameSingular);
+        // Could relationObjectMetataDataItem in case we haven't fetch all the related objectDataItems ? should silent error on this one ?
+        if (!isDefined(targetNameSingular) || !isDefined(relationObjectMetataDataItem)) {
+          throw new Error("Should never occurs invalid relation TODO TEXT")
+        }
+        const recordGqlFields = generateDepthOneRecordGqlFields({objectMetadataItem: relationObjectMetataDataItem})
+        const cachedRecord = getRecordFromCache({
+          cache,
+          objectMetadataItem: relationObjectMetataDataItem,
+          objectMetadataItems,
+          recordId: fieldIdValue,
+          recordGqlFields,
+        });
+        console.log({fieldIdValue, cachedRecord})
+        if(!isDefined(cachedRecord) || Object.keys(cachedRecord).length <= 0) {
+          console.log("NOPE", {cachedRecord})
+          return []
+        }
+
+        return [
+          [fieldIdName, fieldIdValue],
+          [fieldMetadataItem.name, cachedRecord],
+        ];
+        // We kinda have a race condition here
+        // Should we sort and filter by relation desc ? and use reduce ? => OOF
+        // Here same id but will it stay the case in the future ?
+        // Should be integ tested
+        /*
+          [
+            [
+                "companyId",
+                "20202020-f86b-419f-b794-02319abe8637"
+            ],
+            [
+                "companyId",
+                "20202020-f86b-419f-b794-02319abe8637"
+            ],
+            [
+                "company",
+                {}
+            ]
+          ]
+        */
       }
 
       if (!isDefined(recordInputMatch)) {
@@ -88,6 +137,7 @@ export const computeOptimisticRecordFromInput = ({
       return [[fieldName, fieldValue]];
     },
   );
+  console.log({sanitizedEntries})
   const sanitizedResultRecord = Object.fromEntries(sanitizedEntries);
   const recordDomainNameIsDefined =
     isDefined(sanitizedResultRecord?.domainName) &&
