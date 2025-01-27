09416fe6334f19dbe654c2766d569468b93214ab
Charles Bochet <charlesBochet@users.noreply.github.com>
2025-01-27 21:25:02 +0100

    Fix object metadata view creation issue (#9875)
    Fixes https://github.com/twentyhq/core-team-issues/issues/26
    Fixes https://github.com/twentyhq/twenty/issues/9350

--- packages/twenty-front/src/modules/ui/input/components/TextInputV2.tsx
+++ packages/twenty-front/src/modules/ui/input/components/TextInputV2.tsx
@@ -63,7 +63,7 @@ const StyledInput = styled.input<
   padding: ${({ theme, sizeVariant }) =>
     sizeVariant === 'sm' ? `${theme.spacing(2)} 0` : theme.spacing(2)};
   padding-left: ${({ theme, LeftIcon }) =>
-    LeftIcon ? `px` : theme.spacing(2)};
+    LeftIcon ? `calc(${theme.spacing(4)} + 16px)` : theme.spacing(2)};
   width: ${({ theme, width }) =>
     width ? `calc(${width}px + ${theme.spacing(0.5)})` : '100%'};
   max-width: ${({ autoGrow }) => (autoGrow ? '100%' : 'none')};
