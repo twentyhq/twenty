408154719f3162a21598cb03fb39a4cc5e163a1a
etiennejouan <jouan.etienne@gmail.com>
2025-01-28 18:51:41 +0100

    add command to run cleaning suspended workspaces job

---
+++ packages/twenty-server/src/engine/workspace-manager/workspace-cleaner/commands/clean-suspended-workspaces.command.ts
@@ -0,0 +1,35 @@
+import { Command, CommandRunner, Option } from 'nest-commander';
+
+import { CleanWorkspaceService } from 'src/engine/workspace-manager/workspace-cleaner/services/clean.workspace-service';
+
+type CleanSuspendedWorkspacesCommandOptions = {
+  workspaceIds: string[];
+};
+
+@Command({
+  name: 'workspaces:clean',
+  description: 'Clean suspended workspaces',
+})
+export class CleanSuspendedWorkspacesCommand extends CommandRunner {
+  constructor(private readonly cleanWorkspaceService: CleanWorkspaceService) {
+    super();
+  }
+
+  @Option({
+    flags: '-w, --workspaceIds [workspaceIds]',
+    description: 'comma separated workspace ids',
+    required: true,
+  })
+  parseWorkspaceIds(value: string): string[] {
+    return value.split(',');
+  }
+
+  async run(
+    _passedParam: string[],
+    options: CleanSuspendedWorkspacesCommandOptions,
+  ): Promise<void> {
+    await this.cleanWorkspaceService.batchWarnOrCleanWorkspaces(
+      options.workspaceIds,
+    );
+  }
+}
