779034fb0c941dbf9c1ff47734f10fe98cd3f323
Pacifique Linjanja <pacilinja2@gmail.com>
2025-01-28 15:27:32 +0200

    chore: minor updates

--- packages/twenty-server/src/engine/middlewares/graphql-hydrate-request-from-token.middleware.ts
+++ packages/twenty-server/src/engine/middlewares/graphql-hydrate-request-from-token.middleware.ts
@@ -5,10 +5,8 @@ import { NextFunction, Request, Response } from 'express';
 import { AuthGraphqlApiExceptionFilter } from 'src/engine/core-modules/auth/filters/auth-graphql-api-exception.filter';
 import { AccessTokenService } from 'src/engine/core-modules/auth/token/services/access-token.service';
 import { AuthContext } from 'src/engine/core-modules/auth/types/auth-context.type';
-import { ExceptionHandlerService } from 'src/engine/core-modules/exception-handler/exception-handler.service';
 import { MiddlewareService } from 'src/engine/middlewares/middleware.service';
 import { bindDataToRequestObject } from 'src/engine/middlewares/utils/bind-data-to-request-object.utils';
-import { handleExceptionAndConvertToGraphQLError } from 'src/engine/utils/global-exception-handler.util';
 import { WorkspaceCacheStorageService } from 'src/engine/workspace-cache-storage/workspace-cache-storage.service';
 class GraphqlTokenValidationProxy {
   private accessTokenService: AccessTokenService;
@@ -35,7 +33,6 @@ export class GraphQLHydrateRequestFromTokenMiddleware
   constructor(
     private readonly accessTokenService: AccessTokenService,
     private readonly workspaceStorageCacheService: WorkspaceCacheStorageService,
-    private readonly exceptionHandlerService: ExceptionHandlerService,
     private readonly middlewareService: MiddlewareService,
   ) {}
 
@@ -59,21 +56,7 @@ export class GraphQLHydrateRequestFromTokenMiddleware
 
       bindDataToRequestObject(data, req, metadataVersion);
     } catch (error) {
-      const errors = [
-        handleExceptionAndConvertToGraphQLError(
-          error,
-          this.exceptionHandlerService,
-        ),
-      ];
-
-      this.middlewareService.writeResponseOnExceptionCaught(
-        res,
-        'graphql',
-        error,
-        errors,
-      );
-
-      res.end();
+      this.middlewareService.writeGraphqlResponseOnExceptionCaught(res, error);
 
       return;
     }
---
+++ packages/twenty-server/test/integration/rest/suites/rest-api-core-delete.integration-spec.ts
@@ -0,0 +1,77 @@
+import {
+  FAKE_PERSON_ID,
+  PERSON_1_ID,
+} from 'test/integration/constants/mock-person-ids.constants';
+import { PERSON_GQL_FIELDS } from 'test/integration/constants/person-gql-fields.constants';
+import { createManyOperationFactory } from 'test/integration/graphql/utils/create-many-operation-factory.util';
+import { makeGraphqlAPIRequest } from 'test/integration/graphql/utils/make-graphql-api-request.util';
+import { makeRestAPIRequest } from 'test/integration/rest/utils/make-rest-api-request.util';
+import { generateRecordName } from 'test/integration/utils/generate-record-name';
+
+describe('Core REST API Delete One endpoint', () => {
+  let people: any;
+
+  beforeAll(async () => {
+    const personCity1 = generateRecordName(PERSON_1_ID);
+
+    // TODO: move this creation to REST API when the POST method is migrated
+    const graphqlOperation = createManyOperationFactory({
+      objectMetadataSingularName: 'person',
+      objectMetadataPluralName: 'people',
+      gqlFields: PERSON_GQL_FIELDS,
+      data: [
+        {
+          id: PERSON_1_ID,
+          city: personCity1,
+        },
+      ],
+    });
+
+    const response = await makeGraphqlAPIRequest(graphqlOperation);
+
+    people = response.body.data.createPeople;
+    expect(people.length).toBe(1);
+  });
+
+  it('1a. should delete one person', async () => {
+    const response = await makeRestAPIRequest(
+      'delete',
+      `/people/${PERSON_1_ID}`,
+    );
+
+    expect(response.body.data.deletePerson.id).toBe(PERSON_1_ID);
+  });
+
+  it('1.b. should return a BadRequestException when trying to delete a non-existing person', async () => {
+    await makeRestAPIRequest('delete', `/people/${FAKE_PERSON_ID}`)
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.errors).toBeTruthy();
+        expect(res.body.errors[0].message).toContain(
+          `Could not find any entity of type "person"`,
+        );
+      });
+  });
+
+  it('1.c. should return an UnauthorizedException when no token is provided', async () => {
+    await makeRestAPIRequest('delete', `/people/${PERSON_1_ID}`, {
+      authorization: '',
+    })
+      .expect(500)
+      .expect((res) => {
+        expect(res.body.errors).toBeTruthy();
+        expect(res.body.errors[0].code).toBe('UNAUTHENTICATED');
+      });
+  });
+
+  it('1.d. should return an UnauthorizedException when an invalid token is provided', async () => {
+    await makeRestAPIRequest('delete', `/people/${PERSON_1_ID}`, {
+      authorization: 'Bearer invalid-token',
+    })
+      .expect(500)
+      .expect((res) => {
+        expect(res.body.errors).toBeTruthy();
+        expect(res.body.errors[0].code).toBe('UNAUTHENTICATED');
+      });
+  });
+});
