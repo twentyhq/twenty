d0129969998f5c34faba1b6e50677dbf75be7f75
prastoin <paul@twenty.com>
2025-01-28 11:28:06 +0100

    refactor(front): from flatMap to for loop

--- packages/twenty-front/src/modules/object-record/utils/computeOptimisticRecordFromInput.ts
+++ packages/twenty-front/src/modules/object-record/utils/computeOptimisticRecordFromInput.ts
@@ -27,130 +27,128 @@ export const computeOptimisticRecordFromInput = ({
 }: ComputeOptimisticCacheRecordInputArgs) => {
   console.log('computeOptimisticRecordFromInput', { recordInput });
   const recordInputEntries = Object.entries(recordInput);
-  const sanitizedEntries = objectMetadataItem.fields.flatMap(
-    (fieldMetadataItem) => {
-      const isUuidField = fieldMetadataItem.type === FieldMetadataType.UUID;
-      if (isUuidField) {
-        const isRelationFieldId = objectMetadataItem.fields.some(
-          ({ type, relationDefinition }) => {
-            if (type !== FieldMetadataType.RELATION) {
-              return;
-            }
+  const accumulator: Partial<ObjectRecord> = {};
+  for (const fieldMetadataItem of objectMetadataItem.fields) {
+    const isUuidField = fieldMetadataItem.type === FieldMetadataType.UUID;
+    if (isUuidField) {
+      // Could be a util ?
+      const isRelationFieldId = objectMetadataItem.fields.some(
+        ({ type, relationDefinition }) => {
+          if (type !== FieldMetadataType.RELATION) {
+            return;
+          }
 
-            if (!isDefined(relationDefinition)) {
-              throw new Error('Should never occurs ? TODO TEXT');
-            }
+          if (!isDefined(relationDefinition)) {
+            throw new Error('Should never occurs ? TODO TEXT');
+          }
 
-            const sourceFieldName = relationDefinition.sourceFieldMetadata.name;
-            // TODO we should create utils to centralized this logic
-            return `${sourceFieldName}Id` === fieldMetadataItem.name;
-          },
-        );
-
-        if(isRelationFieldId) {
-          return []
-        }
-      }
-
-      const isRelationField =
-        fieldMetadataItem.type === FieldMetadataType.RELATION;
-      const recordInputMatch = recordInputEntries.find(
-        ([fieldName]) => fieldName === fieldMetadataItem.name,
+          const sourceFieldName = relationDefinition.sourceFieldMetadata.name;
+          // TODO we should create utils to centralized this logic
+          return `${sourceFieldName}Id` === fieldMetadataItem.name;
+        },
       );
-      // Maybe we could ? consume the inputs if defined and if not try the cache idk
-      if (isDefined(recordInputMatch) && isRelationField) {
-        throw new Error(
-          'Should never provide relation mutation through anything else than the fieldId e.g companyId',
-        );
+
+      if (isRelationFieldId) {
+        continue;
+      }
+    }
+
+    const isRelationField =
+      fieldMetadataItem.type === FieldMetadataType.RELATION;
+    const recordInputMatch = recordInputEntries.find(
+      ([fieldName]) => fieldName === fieldMetadataItem.name,
+    );
+    // Maybe we could ? consume the inputs if defined and if not try the cache idk
+    if (isDefined(recordInputMatch) && isRelationField) {
+      throw new Error(
+        'Should never provide relation mutation through anything else than the fieldId e.g companyId',
+      );
+    }
+
+    if (
+      isRelationField &&
+      fieldMetadataItem.relationDefinition?.direction ===
+        RelationDefinitionType.ONE_TO_MANY
+    ) {
+      continue;
+    }
+
+    const isManyToOneRelation =
+      fieldMetadataItem.relationDefinition?.direction ===
+      RelationDefinitionType.MANY_TO_ONE;
+    if (isRelationField && isManyToOneRelation) {
+      const relationIdFieldName = `${fieldMetadataItem.name}Id`;
+      const recordInputMatchId = recordInputEntries.find(
+        ([fieldName]) => fieldName === relationIdFieldName,
+      );
+      if (!isDefined(recordInputMatchId)) {
+        continue;
       }
 
-      if (
-        isRelationField &&
-        fieldMetadataItem.relationDefinition?.direction ===
-          RelationDefinitionType.ONE_TO_MANY
-      ) {
-        return [];
+      const [fieldIdName, fieldIdValue] = recordInputMatchId;
+      const relationIdFieldMetadataItem = objectMetadataItem.fields.find(
+        (field) => field.name === relationIdFieldName,
+      );
+      if (!isDefined(relationIdFieldMetadataItem)) {
+        continue;
       }
 
-      const isManyToOneRelation =
-        fieldMetadataItem.relationDefinition?.direction ===
-        RelationDefinitionType.MANY_TO_ONE;
-      if (isRelationField && isManyToOneRelation) {
-        const relationIdFieldName = `${fieldMetadataItem.name}Id`;
-        const recordInputMatchId = recordInputEntries.find(
-          ([fieldName]) => fieldName === relationIdFieldName,
-        );
-        if (!isDefined(recordInputMatchId)) {
-          return [];
-        }
-        const [fieldIdName, fieldIdValue] = recordInputMatchId;
-
-        const relationIdFieldMetadataItem = objectMetadataItem.fields.find(
-          (field) => field.name === relationIdFieldName,
-        );
-        if (!isDefined(relationIdFieldMetadataItem)) {
-          return [];
-        }
-
-        const isRecordInputFieldIdNull = isNull(fieldIdValue);
-        if (isRecordInputFieldIdNull) {
-          return [
-            [fieldIdName, null],
-            [fieldMetadataItem.name, null],
-          ];
-        }
-        const targetNameSingular =
-          fieldMetadataItem.relationDefinition?.targetObjectMetadata
-            .nameSingular;
-        const relationObjectMetataDataItem = objectMetadataItems.find(
-          ({ nameSingular }) => nameSingular === targetNameSingular,
-        );
-        if (
-          !isDefined(targetNameSingular) ||
-          !isDefined(relationObjectMetataDataItem)
-        ) {
-          throw new Error('Should never occurs invalid relation TODO TEXT');
-        }
-        const cachedRecord = getRecordFromCache({
-          cache,
-          objectMetadataItem: relationObjectMetataDataItem,
-          objectMetadataItems,
-          recordId: fieldIdValue,
-        });
-        if (!isDefined(cachedRecord) || Object.keys(cachedRecord).length <= 0) {
-          return [];
-        }
-
+      const isRecordInputFieldIdNull = isNull(fieldIdValue);
+      if (isRecordInputFieldIdNull) {
         return [
-          [fieldIdName, fieldIdValue],
-          [fieldMetadataItem.name, cachedRecord],
+          [fieldIdName, null],
+          [fieldMetadataItem.name, null],
         ];
       }
-
-      if (!isDefined(recordInputMatch)) {
-        return [];
+      const targetNameSingular =
+        fieldMetadataItem.relationDefinition?.targetObjectMetadata.nameSingular;
+      const relationObjectMetataDataItem = objectMetadataItems.find(
+        ({ nameSingular }) => nameSingular === targetNameSingular,
+      );
+      if (
+        !isDefined(targetNameSingular) ||
+        !isDefined(relationObjectMetataDataItem)
+      ) {
+        throw new Error('Should never occurs invalid relation TODO TEXT');
       }
-      const [fieldName, fieldValue] = recordInputMatch;
 
-      if (!fieldMetadataItem.isNullable && fieldValue == null) {
-        return [];
+      const cachedRecord = getRecordFromCache({
+        cache,
+        objectMetadataItem: relationObjectMetataDataItem,
+        objectMetadataItems,
+        recordId: fieldIdValue,
+      });
+      if (!isDefined(cachedRecord) || Object.keys(cachedRecord).length <= 0) {
+        continue;
       }
 
-      return [[fieldName, fieldValue]];
-    },
-  );
-  console.log({ sanitizedEntries });
-  const sanitizedResultRecord = Object.fromEntries(sanitizedEntries);
+      accumulator[fieldIdName] = fieldIdValue;
+      accumulator[fieldMetadataItem.name] = cachedRecord;
+      continue;
+    }
+
+    if (!isDefined(recordInputMatch)) {
+      continue;
+    }
+
+    const [fieldName, fieldValue] = recordInputMatch;
+    if (!fieldMetadataItem.isNullable && fieldValue == null) {
+      continue;
+    }
+
+    accumulator[fieldName] = fieldValue;
+  }
+
   const recordDomainNameIsDefined =
-    isDefined(sanitizedResultRecord?.domainName) &&
-    isString(sanitizedResultRecord.domainName);
+    isDefined(accumulator?.domainName) &&
+    isString(accumulator.domainName);
   if (!recordDomainNameIsDefined) {
-    return sanitizedResultRecord;
+    return accumulator;
   }
 
   return {
-    ...sanitizedResultRecord,
+    ...accumulator,
     // We should have a look to latest typeScript version that should infer typing from isDefined and IsString here
-    domainName: getUrlHostName(sanitizedResultRecord.domainName as string),
+    domainName: getUrlHostName(accumulator.domainName as string),
   };
 };
