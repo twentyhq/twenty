5bd820c4b68a2a0325f6ccb70851f52d4eed51ae
Thomas Trompette <thomas.trompette@sfr.fr>
2025-01-28 15:16:36 +0100

    Fix workflows empty states (#9878)
    Versions and runs cannot be added manually. Updating message.
    
    <img width="545" alt="Capture d’écran 2025-01-28 à 11 33 47"
    src="https://github.com/user-attachments/assets/36b7ade8-3061-4b72-bfa5-c262e4d7d25e"
    />
    <img width="614" alt="Capture d’écran 2025-01-28 à 11 34 02"
    src="https://github.com/user-attachments/assets/9b524eb4-f40c-4330-a37f-fb33e08d22f4"
    />

--- packages/twenty-front/src/modules/object-record/record-table/empty-state/utils/getEmptyStateSubTitle.ts
+++ packages/twenty-front/src/modules/object-record/record-table/empty-state/utils/getEmptyStateSubTitle.ts
@@ -1,17 +1,16 @@
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
-import { t } from '@lingui/core/macro';
 
 export const getEmptyStateSubTitle = (
   objectNameSingular: string,
   objectLabel: string,
 ) => {
   if (objectNameSingular === CoreObjectNameSingular.WorkflowVersion) {
-    return t`Create a workflow and return here to view its versions`;
+    return 'Create a workflow and return here to view its versions';
   }
 
   if (objectNameSingular === CoreObjectNameSingular.WorkflowRun) {
-    return t`Run a workflow and return here to view its executions`;
+    return 'Run a workflow and return here to view its executions';
   }
 
-  return t`Use our API or add your first ${objectLabel} manually`;
+  return `Use our API or add your first ${objectLabel} manually`;
 };
--- packages/twenty-front/src/modules/object-record/record-table/empty-state/utils/getEmptyStateTitle.ts
+++ packages/twenty-front/src/modules/object-record/record-table/empty-state/utils/getEmptyStateTitle.ts
@@ -1,17 +1,16 @@
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
-import { t } from '@lingui/core/macro';
 
 export const getEmptyStateTitle = (
   objectNameSingular: string,
   objectLabel: string,
 ) => {
   if (objectNameSingular === CoreObjectNameSingular.WorkflowVersion) {
-    return t`No workflow versions yet`;
+    return 'No workflow versions yet';
   }
 
   if (objectNameSingular === CoreObjectNameSingular.WorkflowRun) {
-    return t`No workflow runs yet`;
+    return 'No workflow runs yet';
   }
 
-  return t`Add your first ${objectLabel}`;
+  return `Add your first ${objectLabel}`;
 };
