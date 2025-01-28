6501f9fcb951f5eee7675351aa94bd2c4f980155
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-01-28 16:17:55 +0100

    refactor(domain-manager): make DTO fields nullable where needed
    Updated OwnershipVerification and related DTO fields to be nullable where appropriate for better alignment with API specifications. This ensures compatibility with optional data returned by the external API.

--- packages/twenty-server/src/engine/core-modules/domain-manager/dtos/custom-hostname-details.ts
+++ packages/twenty-server/src/engine/core-modules/domain-manager/dtos/custom-hostname-details.ts
@@ -2,27 +2,30 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 import { CustomHostname } from 'src/engine/core-modules/domain-manager/types/custom-hostname.type';
 
+// Type come from: https://developers.cloudflare.com/api/node/resources/custom_hostnames/methods/list/#(merged%20schema)%20%3E%20(property)%20result%20%3E%20(items)%20%3E%20(property)%20ownership_verification
 @ObjectType()
-export class OwnershipVerification {
-  @Field(() => String)
+class OwnershipVerification {
+  @Field(() => String, { nullable: true })
   name?: string;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   type?: 'txt';
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   value?: string;
 }
 
+// Type come from: https://developers.cloudflare.com/api/node/resources/custom_hostnames/methods/list/#(merged%20schema)%20%3E%20(property)%20result%20%3E%20(items)%20%3E%20(property)%20ownership_verification_http
 @ObjectType()
-export class OwnershipVerificationHttp {
-  @Field(() => String)
+class OwnershipVerificationHttp {
+  @Field(() => String, { nullable: true })
   http_body?: string;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   http_url?: string;
 }
 
+// Type come from: https://developers.cloudflare.com/api/node/resources/custom_hostnames/methods/list/#(merged%20schema)%20%3E%20(property)%20result%20%3E%20(items)
 @ObjectType()
 export class CustomHostnameDetails implements Omit<CustomHostname, 'id'> {
   @Field(() => String)
@@ -34,7 +37,7 @@ export class CustomHostnameDetails implements Omit<CustomHostname, 'id'> {
   @Field(() => OwnershipVerificationHttp, { nullable: true })
   ownership_verification_http?: OwnershipVerificationHttp;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   status?:
     | 'active'
     | 'pending'
