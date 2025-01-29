baf55b45c7ebb2625515c160b06f10f49d442c57
Jérémy Magrin <jeremy.magrin@gmail.com>
2025-01-29 10:50:47 +0100

    fix: use custom exceptions

--- packages/twenty-server/src/engine/api/graphql/graphql-query-runner/utils/get-target-object-metadata.util.ts
+++ packages/twenty-server/src/engine/api/graphql/graphql-query-runner/utils/get-target-object-metadata.util.ts
@@ -1,5 +1,9 @@
 import { FieldMetadataInterface } from 'src/engine/metadata-modules/field-metadata/interfaces/field-metadata.interface';
 
+import {
+  GraphqlQueryRunnerException,
+  GraphqlQueryRunnerExceptionCode,
+} from 'src/engine/api/graphql/graphql-query-runner/errors/graphql-query-runner.exception';
 import { ObjectMetadataMaps } from 'src/engine/metadata-modules/types/object-metadata-maps';
 
 export const getTargetObjectMetadata = (
@@ -7,8 +11,9 @@ export const getTargetObjectMetadata = (
   objectMetadataMaps: ObjectMetadataMaps,
 ) => {
   if (!fieldMetadata.relationTargetObjectMetadataId) {
-    throw new Error(
+    throw new GraphqlQueryRunnerException(
       `Relation target object metadata id not found for field ${fieldMetadata.name}`,
+      GraphqlQueryRunnerExceptionCode.RELATION_TARGET_OBJECT_METADATA_NOT_FOUND,
     );
   }
 
@@ -16,8 +21,9 @@ export const getTargetObjectMetadata = (
     objectMetadataMaps.byId[fieldMetadata.relationTargetObjectMetadataId];
 
   if (!targetObjectMetadata) {
-    throw new Error(
+    throw new GraphqlQueryRunnerException(
       `Target object metadata not found for field ${fieldMetadata.name}`,
+      GraphqlQueryRunnerExceptionCode.RELATION_TARGET_OBJECT_METADATA_NOT_FOUND,
     );
   }
 
