72760cc6f5c26897603c24c9cb7824a45d887a81
bosiraphael <raphael.bosi@gmail.com>
2025-01-29 10:39:46 +0100

    improve CommandMenuSearchRecordsPage performance

--- packages/twenty-front/src/modules/command-menu/pages/components/CommandMenuSearchRecordsPage.tsx
+++ packages/twenty-front/src/modules/command-menu/pages/components/CommandMenuSearchRecordsPage.tsx
@@ -1,15 +1,18 @@
 import { CommandMenuList } from '@/command-menu/components/CommandMenuList';
 import { useSearchRecords } from '@/command-menu/hooks/useSearchRecords';
+import { useMemo } from 'react';
 
 export const CommandMenuSearchRecordsPage = () => {
   const { commandGroups, loading, noResults } = useSearchRecords();
 
+  const selectableItemIds = useMemo(() => {
+    return commandGroups.flatMap((group) => group.items).map((item) => item.id);
+  }, [commandGroups]);
+
   return (
     <CommandMenuList
       commandGroups={commandGroups}
-      selectableItemIds={commandGroups.flatMap((group) =>
-        group.items.map((item) => item.id),
-      )}
+      selectableItemIds={selectableItemIds}
       loading={loading}
       noResults={noResults}
     />
