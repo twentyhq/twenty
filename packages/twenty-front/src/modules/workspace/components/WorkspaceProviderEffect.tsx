37543c8eb740a22e1ad24028ffea92d7516f2f01
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-01-29 17:56:55 +0100

    refactor(workspace): remove unused auth-related code
    Cleaned up unused imports and redundant logic related to SSO and sign-in-up states in WorkspaceProviderEffect. This improves code clarity and reduces unnecessary dependencies in the workspace module.

--- packages/twenty-front/src/modules/workspace/components/WorkspaceProviderEffect.tsx
+++ packages/twenty-front/src/modules/workspace/components/WorkspaceProviderEffect.tsx
@@ -1,4 +1,4 @@
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 import { isMultiWorkspaceEnabledState } from '@/client-config/states/isMultiWorkspaceEnabledState';
 import { useReadWorkspaceSubdomainFromCurrentLocation } from '@/domain-manager/hooks/useReadWorkspaceSubdomainFromCurrentLocation';
@@ -7,17 +7,11 @@ import { lastAuthenticatedWorkspaceDomainState } from '@/domain-manager/states/l
 import { useEffect } from 'react';
 import { isDefined } from '~/utils/isDefined';
 
-import { useSSO } from '@/auth/sign-in-up/hooks/useSSO';
-import { signInUpStepState } from '@/auth/states/signInUpStepState';
 import { useGetPublicWorkspaceDataBySubdomain } from '@/domain-manager/hooks/useGetPublicWorkspaceDataBySubdomain';
 import { useIsCurrentLocationOnDefaultDomain } from '@/domain-manager/hooks/useIsCurrentLocationOnDefaultDomain';
 export const WorkspaceProviderEffect = () => {
   const { data: getPublicWorkspaceData } =
     useGetPublicWorkspaceDataBySubdomain();
-
-  const { redirectToSSOLoginPage } = useSSO();
-
-  const setSignInUpStep = useSetRecoilState(signInUpStepState);
 
   const lastAuthenticatedWorkspaceDomain = useRecoilValue(
     lastAuthenticatedWorkspaceDomainState,
