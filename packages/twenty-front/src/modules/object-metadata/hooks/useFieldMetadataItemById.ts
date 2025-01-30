de2d1970ec38ac82e278daa83eec78702e13653f
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-01-30 17:28:15 +0100

    Used flatMap everywhere

--- packages/twenty-front/src/modules/object-metadata/hooks/useFieldMetadataItemById.ts
+++ packages/twenty-front/src/modules/object-metadata/hooks/useFieldMetadataItemById.ts
@@ -5,14 +5,9 @@ import { isDefined } from 'twenty-ui';
 export const useFieldMetadataItemById = (fieldMetadataId: string) => {
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
 
-  const correspondingObjectMetadataItem = objectMetadataItems.find(
-    (objectMetadataItem) =>
-      objectMetadataItem.fields.some((field) => field.id === fieldMetadataId),
-  );
-
-  const fieldMetadataItem = correspondingObjectMetadataItem?.fields.find(
-    (field) => field.id === fieldMetadataId,
-  );
+  const fieldMetadataItem = objectMetadataItems
+    .flatMap((objectMetadataItem) => objectMetadataItem.fields)
+    .find((field) => field.id === fieldMetadataId);
 
   if (!isDefined(fieldMetadataItem)) {
     throw new Error(`Field metadata item not found for id ${fieldMetadataId}`);
--- packages/twenty-front/src/modules/object-metadata/hooks/useGetFieldMetadataItemById.ts
+++ packages/twenty-front/src/modules/object-metadata/hooks/useGetFieldMetadataItemById.ts
@@ -1,18 +1,20 @@
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
 import { useRecoilValue } from 'recoil';
+import { isDefined } from 'twenty-ui';
 
 export const useGetFieldMetadataItemById = () => {
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
 
   const getFieldMetadataItemById = (fieldMetadataId: string) => {
-    const correspondingObjectMetadataItem = objectMetadataItems.find(
-      (objectMetadataItem) =>
-        objectMetadataItem.fields.some((field) => field.id === fieldMetadataId),
-    );
+    const fieldMetadataItem = objectMetadataItems
+      .flatMap((objectMetadataItem) => objectMetadataItem.fields)
+      .find((field) => field.id === fieldMetadataId);
 
-    const fieldMetadataItem = correspondingObjectMetadataItem?.fields.find(
-      (field) => field.id === fieldMetadataId,
-    );
+    if (!isDefined(fieldMetadataItem)) {
+      throw new Error(
+        `Field metadata item not found for id ${fieldMetadataId}`,
+      );
+    }
 
     return fieldMetadataItem;
   };
