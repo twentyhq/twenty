0fe28b90d07206f184f69525b8a0201c7448775b
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-01-30 18:05:46 +0100

    refactor(domain-manager): fix type casing in hook function
    Updated the type name from 'workspaceUrls' to 'WorkspaceUrls' to align with proper casing conventions. This change ensures consistency and accuracy in the type definitions.

--- packages/twenty-front/src/modules/domain-manager/hooks/useGetWorkspaceUrlFromWorkspaceUrls.ts
+++ packages/twenty-front/src/modules/domain-manager/hooks/useGetWorkspaceUrlFromWorkspaceUrls.ts
@@ -1,10 +1,10 @@
 import { useSearchParams } from 'react-router-dom';
-import { workspaceUrls } from '~/generated/graphql';
+import { WorkspaceUrls } from '~/generated/graphql';
 
 export const useGetWorkspaceUrlFromWorkspaceUrls = () => {
   const [searchParams] = useSearchParams();
 
-  const getWorkspaceUrl = (workspaceUrls: workspaceUrls) => {
+  const getWorkspaceUrl = (workspaceUrls: WorkspaceUrls) => {
     return searchParams.get('force-subdomain-url')
       ? workspaceUrls.subdomainUrl
       : (workspaceUrls.customUrl ?? workspaceUrls.subdomainUrl);
