9ccff36eb16c86db310d83759bf97cdfa57cc75b
Marie Stoppa <marie.stoppa@essec.edu>
2025-02-03 18:46:28 +0100

    Remove @Column from DTO

--- packages/twenty-server/src/engine/metadata-modules/role/dtos/role.dto.ts
+++ packages/twenty-server/src/engine/metadata-modules/role/dtos/role.dto.ts
@@ -1,6 +1,6 @@
 import { Field, HideField, ObjectType } from '@nestjs/graphql';
 
-import { PrimaryGeneratedColumn, Relation } from 'typeorm';
+import { Relation } from 'typeorm';
 
 import { WorkspaceMember } from 'src/engine/core-modules/user/dtos/workspace-member.dto';
 import { UserWorkspaceRoleEntity } from 'src/engine/metadata-modules/role/user-workspace-role.entity';
@@ -8,7 +8,6 @@ import { UserWorkspaceRoleEntity } from 'src/engine/metadata-modules/role/user-w
 @ObjectType()
 export class RoleDTO {
   @Field({ nullable: false })
-  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Field({ nullable: false })
