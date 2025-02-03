54b2da6263e9602c9e97592589381dffdb8b7f01
bosiraphael <raphael.bosi@gmail.com>
2025-02-03 17:55:15 +0100

    create state

---
+++ packages/twenty-front/src/modules/command-menu/states/commandMenuNavigationStackState.ts
@@ -0,0 +1,19 @@
+import { CommandMenuPages } from '@/command-menu/types/CommandMenuPages';
+import { IconComponent, createState } from 'twenty-ui';
+
+type CommandMenuNavigationStackItem = {
+  page: CommandMenuPages;
+  pageTitle?: string;
+  pageIcon?: IconComponent;
+};
+
+export const commandMenuNavigationStackState = createState<
+  CommandMenuNavigationStackItem[]
+>({
+  key: 'command-menu/commandMenuNavigationStackState',
+  defaultValue: [
+    {
+      page: CommandMenuPages.Root,
+    },
+  ],
+});
