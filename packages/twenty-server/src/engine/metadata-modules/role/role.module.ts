351e768038323b6c140f6e8c007ccffc854ae46b
Marie <51697796+ijreilly@users.noreply.github.com>
2025-02-03 19:14:18 +0100

    [Permissions] Implement getRoles (#9955)
    In this PR
    
    - introducing roles module to separate roles logic (assign a Role, get a
    workspace's roles etc.) from permission logic (check if a user has a
    permission)
    - Introduces getRoles endpoint to fetch a workspace's roles
    - introduces the first permission check: getRoles in only accessible to
    users with permission on ROLE setting. Implemented
    validatesUserHasWorkspaceSettingPermissionOrThrow

--- packages/twenty-server/src/engine/metadata-modules/role/role.module.ts
+++ packages/twenty-server/src/engine/metadata-modules/role/role.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { EnvironmentModule } from 'src/engine/core-modules/environment/environment.module';
-import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-flag.module';
 import { UserWorkspace } from 'src/engine/core-modules/user-workspace/user-workspace.entity';
-import { PermissionsService } from 'src/engine/metadata-modules/permissions/permissions.service';
+import { PermissionsModule } from 'src/engine/metadata-modules/permissions/permissions.module';
 import { RoleEntity } from 'src/engine/metadata-modules/role/role.entity';
+import { RoleResolver } from 'src/engine/metadata-modules/role/role.resolver';
+import { RoleService } from 'src/engine/metadata-modules/role/role.service';
 import { UserWorkspaceRoleEntity } from 'src/engine/metadata-modules/role/user-workspace-role.entity';
 import { UserRoleModule } from 'src/engine/metadata-modules/userRole/userRole.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([RoleEntity, UserWorkspaceRoleEntity], 'metadata'),
-    FeatureFlagModule,
     TypeOrmModule.forFeature([UserWorkspace], 'core'),
-    EnvironmentModule,
     UserRoleModule,
+    PermissionsModule,
   ],
-  providers: [PermissionsService],
-  exports: [PermissionsService],
+  providers: [RoleService, RoleResolver],
+  exports: [RoleService],
 })
-export class PermissionsModule {}
+export class RoleModule {}
