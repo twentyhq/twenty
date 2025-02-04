c458afef4c327174f7bcbaf3438f12807b2f5371
Thomas Trompette <thomas.trompette@sfr.fr>
2025-02-04 14:09:58 +0100

    Enable workflow is lab

--- packages/twenty-front/src/modules/object-metadata/hooks/useFilteredObjectMetadataItems.ts
+++ packages/twenty-front/src/modules/object-metadata/hooks/useFilteredObjectMetadataItems.ts
@@ -1,12 +1,29 @@
 import { useRecoilValue } from 'recoil';
 
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { isWorkflowSubObjectMetadata } from '@/object-metadata/utils/isWorkflowSubObjectMetadata';
+import { useIsFeatureEnabled } from '@/workspace/hooks/useIsFeatureEnabled';
+import { FeatureFlagKey } from '~/generated-metadata/graphql';
 
 export const useFilteredObjectMetadataItems = () => {
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
 
+  const isWorkflowEnabled = useIsFeatureEnabled(
+    FeatureFlagKey.IsWorkflowEnabled,
+  );
+
+  const isWorkflowToBeFiltered = (nameSingular: string) => {
+    return (
+      !isWorkflowEnabled &&
+      (nameSingular === CoreObjectNameSingular.Workflow ||
+        isWorkflowSubObjectMetadata(nameSingular))
+    );
+  };
+
   const activeObjectMetadataItems = objectMetadataItems.filter(
-    ({ isActive, isSystem }) => isActive && !isSystem,
+    ({ isActive, isSystem, nameSingular }) =>
+      isActive && !isSystem && !isWorkflowToBeFiltered(nameSingular),
   );
 
   const alphaSortedActiveObjectMetadataItems = activeObjectMetadataItems.sort(
