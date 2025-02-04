dfc1bb7c295cdf53fe1683a1384b0f32b82efeab
Raphaël Bosi <71827178+bosiraphael@users.noreply.github.com>
2025-02-04 15:47:43 +0100

    361 create a navigation stack for the command menu (#9995)
    Closes https://github.com/twentyhq/core-team-issues/issues/361
    
    - Created navigation stack state
    - Created navigation functions inside the `useCommandMenu` hook
    - Added tests

---
+++ packages/twenty-front/src/modules/command-menu/states/commandMenuNavigationStackState.ts
@@ -0,0 +1,15 @@
+import { CommandMenuPages } from '@/command-menu/types/CommandMenuPages';
+import { IconComponent, createState } from 'twenty-ui';
+
+export type CommandMenuNavigationStackItem = {
+  page: CommandMenuPages;
+  pageTitle?: string;
+  pageIcon?: IconComponent;
+};
+
+export const commandMenuNavigationStackState = createState<
+  CommandMenuNavigationStackItem[]
+>({
+  key: 'command-menu/commandMenuNavigationStackState',
+  defaultValue: [],
+});
