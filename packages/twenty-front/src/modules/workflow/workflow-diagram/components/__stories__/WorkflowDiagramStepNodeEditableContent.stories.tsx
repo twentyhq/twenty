125a0c34198cb3bda498d8464740ac9fa79a7e2c
Baptiste Devessier <baptiste@devessier.fr>
2025-02-04 18:38:38 +0100

    Create variants for workflow visualizer nodes (#10006)
    Closes https://github.com/twentyhq/core-team-issues/issues/332
    
    - Create the success and failed variants
    - Introduce the first responsive color
    - Creating stories for the new variants
    
    These components are not yet in use in the source code. If you want to
    see them, launch Storybook.
    
    | Success | Failure |
    |--------|--------|
    | ![CleanShot 2025-02-04 at 16 24
    43@2x](https://github.com/user-attachments/assets/0dd68a8f-3914-4b6e-b2d8-43108c2f5e8c)
    | ![CleanShot 2025-02-04 at 16 24
    59@2x](https://github.com/user-attachments/assets/e4e408d3-29fb-4fbc-a277-044aec9b0f4b)
    |
    | ![CleanShot 2025-02-04 at 16 24
    54@2x](https://github.com/user-attachments/assets/d565ee47-1476-475d-adf6-dadfff9c6719)
    | ![CleanShot 2025-02-04 at 16 25
    05@2x](https://github.com/user-attachments/assets/9a0aabcc-84d1-41e2-a5a1-7c8cb05f963f)
    |

--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/__stories__/WorkflowDiagramStepNodeEditableContent.stories.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/__stories__/WorkflowDiagramStepNodeEditableContent.stories.tsx
@@ -2,9 +2,9 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import { WorkflowDiagramStepNodeData } from '@/workflow/workflow-diagram/types/WorkflowDiagram';
 import { fn } from '@storybook/test';
-import { ReactFlowProvider } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { CatalogDecorator, CatalogStory } from 'twenty-ui';
+import { ReactflowDecorator } from '~/testing/decorators/ReactflowDecorator';
 import { graphqlMocks } from '~/testing/graphqlMocks';
 import { WorkflowDiagramStepNodeEditableContent } from '../WorkflowDiagramStepNodeEditableContent';
 
@@ -17,12 +17,43 @@ export default meta;
 
 type Story = StoryObj<typeof WorkflowDiagramStepNodeEditableContent>;
 
+const ALL_STEPS = [
+  {
+    nodeType: 'trigger',
+    triggerType: 'DATABASE_EVENT',
+    name: 'Record is Created',
+  },
+  { nodeType: 'trigger', triggerType: 'MANUAL', name: 'Manual' },
+  {
+    nodeType: 'action',
+    actionType: 'CREATE_RECORD',
+    name: 'Create Record',
+  },
+  {
+    nodeType: 'action',
+    actionType: 'UPDATE_RECORD',
+    name: 'Update Record',
+  },
+  {
+    nodeType: 'action',
+    actionType: 'DELETE_RECORD',
+    name: 'Delete Record',
+  },
+  {
+    nodeType: 'action',
+    actionType: 'SEND_EMAIL',
+    name: 'Send Email',
+  },
+  { nodeType: 'action', actionType: 'CODE', name: 'Code' },
+] satisfies WorkflowDiagramStepNodeData[];
+
 export const All: CatalogStory<
   Story,
   typeof WorkflowDiagramStepNodeEditableContent
 > = {
   args: {
     onDelete: fn(),
+    variant: 'default',
     selected: false,
   },
   parameters: {
@@ -37,57 +68,22 @@ export const All: CatalogStory<
       dimensions: [
         {
           name: 'step type',
-          values: [
-            {
-              nodeType: 'trigger',
-              triggerType: 'DATABASE_EVENT',
-              name: 'Record is Created',
-            },
-            { nodeType: 'trigger', triggerType: 'MANUAL', name: 'Manual' },
-            {
-              nodeType: 'action',
-              actionType: 'CREATE_RECORD',
-              name: 'Create Record',
-            },
-            {
-              nodeType: 'action',
-              actionType: 'UPDATE_RECORD',
-              name: 'Update Record',
-            },
-            {
-              nodeType: 'action',
-              actionType: 'DELETE_RECORD',
-              name: 'Delete Record',
-            },
-            {
-              nodeType: 'action',
-              actionType: 'SEND_EMAIL',
-              name: 'Send Email',
-            },
-            { nodeType: 'action', actionType: 'CODE', name: 'Code' },
-          ] satisfies WorkflowDiagramStepNodeData[],
+          values: ALL_STEPS,
           props: (data: WorkflowDiagramStepNodeData) => ({ data }),
         },
       ],
     },
   },
-  decorators: [
-    CatalogDecorator,
-    (Story) => {
-      return (
-        <ReactFlowProvider>
-          <Story />
-        </ReactFlowProvider>
-      );
-    },
-  ],
+  decorators: [CatalogDecorator, ReactflowDecorator],
 };
+
 export const AllSelected: CatalogStory<
   Story,
   typeof WorkflowDiagramStepNodeEditableContent
 > = {
   args: {
     onDelete: fn(),
+    variant: 'default',
     selected: true,
   },
   parameters: {
@@ -103,48 +99,131 @@ export const AllSelected: CatalogStory<
       dimensions: [
         {
           name: 'step type',
-          values: [
-            {
-              nodeType: 'trigger',
-              triggerType: 'DATABASE_EVENT',
-              name: 'Record is Created',
-            },
-            { nodeType: 'trigger', triggerType: 'MANUAL', name: 'Manual' },
-            {
-              nodeType: 'action',
-              actionType: 'CREATE_RECORD',
-              name: 'Create Record',
-            },
-            {
-              nodeType: 'action',
-              actionType: 'UPDATE_RECORD',
-              name: 'Update Record',
-            },
-            {
-              nodeType: 'action',
-              actionType: 'DELETE_RECORD',
-              name: 'Delete Record',
-            },
-            {
-              nodeType: 'action',
-              actionType: 'SEND_EMAIL',
-              name: 'Send Email',
-            },
-            { nodeType: 'action', actionType: 'CODE', name: 'Code' },
-          ] satisfies WorkflowDiagramStepNodeData[],
+          values: ALL_STEPS,
           props: (data: WorkflowDiagramStepNodeData) => ({ data }),
         },
       ],
     },
   },
-  decorators: [
-    CatalogDecorator,
-    (Story) => {
-      return (
-        <ReactFlowProvider>
-          <Story />
-        </ReactFlowProvider>
-      );
+  decorators: [CatalogDecorator, ReactflowDecorator],
+};
+
+export const AllSuccess: CatalogStory<
+  Story,
+  typeof WorkflowDiagramStepNodeEditableContent
+> = {
+  args: {
+    onDelete: fn(),
+    variant: 'success',
+  },
+  parameters: {
+    msw: graphqlMocks,
+    catalog: {
+      options: {
+        elementContainer: {
+          width: 250,
+          style: { position: 'relative' },
+        },
+      },
+      dimensions: [
+        {
+          name: 'step type',
+          values: ALL_STEPS,
+          props: (data: WorkflowDiagramStepNodeData) => ({ data }),
+        },
+      ],
     },
-  ],
+  },
+  decorators: [CatalogDecorator, ReactflowDecorator],
+};
+
+export const AllSuccessSelected: CatalogStory<
+  Story,
+  typeof WorkflowDiagramStepNodeEditableContent
+> = {
+  args: {
+    onDelete: fn(),
+    variant: 'success',
+    selected: true,
+  },
+  parameters: {
+    msw: graphqlMocks,
+    catalog: {
+      options: {
+        elementContainer: {
+          width: 250,
+          style: { position: 'relative' },
+          className: 'selectable selected',
+        },
+      },
+      dimensions: [
+        {
+          name: 'step type',
+          values: ALL_STEPS,
+          props: (data: WorkflowDiagramStepNodeData) => ({ data }),
+        },
+      ],
+    },
+  },
+  decorators: [CatalogDecorator, ReactflowDecorator],
+};
+
+export const AllFailure: CatalogStory<
+  Story,
+  typeof WorkflowDiagramStepNodeEditableContent
+> = {
+  args: {
+    onDelete: fn(),
+    variant: 'failure',
+  },
+  parameters: {
+    msw: graphqlMocks,
+    catalog: {
+      options: {
+        elementContainer: {
+          width: 250,
+          style: { position: 'relative' },
+        },
+      },
+      dimensions: [
+        {
+          name: 'step type',
+          values: ALL_STEPS,
+          props: (data: WorkflowDiagramStepNodeData) => ({ data }),
+        },
+      ],
+    },
+  },
+  decorators: [CatalogDecorator, ReactflowDecorator],
+};
+
+export const AllFailureSelected: CatalogStory<
+  Story,
+  typeof WorkflowDiagramStepNodeEditableContent
+> = {
+  args: {
+    onDelete: fn(),
+    variant: 'failure',
+    selected: true,
+  },
+  parameters: {
+    msw: graphqlMocks,
+    catalog: {
+      options: {
+        elementContainer: {
+          width: 250,
+          style: { position: 'relative' },
+          className: 'selectable selected',
+        },
+      },
+      dimensions: [
+        {
+          name: 'step type',
+          values: ALL_STEPS,
+          props: (data: WorkflowDiagramStepNodeData) => ({ data }),
+        },
+      ],
+    },
+  },
+  decorators: [CatalogDecorator, ReactflowDecorator],
 };
