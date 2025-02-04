53b51c8bbac2961c61bb7dfda803d5c975307d15
Guillim <guillim@users.noreply.github.com>
2025-02-04 15:20:35 +0100

    Fix-issue-370 (#9996)
    Fixes the issue from introduced when alowing gmail and outlook.
    
    fixes https://github.com/twentyhq/core-team-issues/issues/370

---
+++ packages/twenty-front/src/modules/client-config/states/isMicrosoftMessagingEnabledState.ts
@@ -0,0 +1,6 @@
+import { createState } from '@ui/utilities/state/utils/createState';
+
+export const isMicrosoftMessagingEnabledState = createState<boolean>({
+  key: 'isMicrosoftMessagingEnabled',
+  defaultValue: false,
+});
