60aac86179f6fdf5d8e6523c2bc7500324e4172f
prastoin <paul@twenty.com>
2025-02-05 21:41:26 +0100

    comment

--- packages/twenty-shared/src/workflow.ts
+++ packages/twenty-shared/src/workflow.ts
@@ -1,1 +1,4 @@
+// Index required here for correct import suggestion
+// Seems to be specific to workflow, maybe due to folder structure or the fact its a standalone barrel which is not the default main
+// sourcemap still half broken redirects to .d.ts
 export * from "./workflow/index";
