704654f4c99b902b319011a2441d9b02f8c4944d
Devessier <baptiste@devessier.fr>
2025-02-06 16:28:23 +0100

    refactor: make the ai code mine

--- packages/twenty-front/src/modules/workflow/workflow-diagram/utils/markLeafNodes.ts
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/utils/markLeafNodes.ts
@@ -1,8 +1,9 @@
 import { WorkflowDiagram } from '@/workflow/workflow-diagram/types/WorkflowDiagram';
 
-export const markLeafNodes = (diagram: WorkflowDiagram): WorkflowDiagram => {
-  const { nodes, edges } = diagram;
-
+export const markLeafNodes = ({
+  nodes,
+  edges,
+}: WorkflowDiagram): WorkflowDiagram => {
   const sourceNodeIds = new Set(edges.map((edge) => edge.source));
 
   const updatedNodes = nodes.map((node) => ({
@@ -15,6 +16,6 @@ export const markLeafNodes = (diagram: WorkflowDiagram): WorkflowDiagram => {
 
   return {
     nodes: updatedNodes,
-    edges: edges,
+    edges,
   };
 };
