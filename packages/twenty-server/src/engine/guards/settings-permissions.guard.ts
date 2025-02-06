b7d1f8ed5576a3ff62dbb044f71d13b719e6eb59
Marie Stoppa <marie.stoppa@essec.edu>
2025-02-06 17:23:53 +0100

    create SettingsPermissionsGuard

---
+++ packages/twenty-server/src/engine/guards/settings-permissions.guard.ts
@@ -0,0 +1,78 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Type,
+} from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+import { SettingsFeatures } from 'twenty-shared';
+
+import { FeatureFlagKey } from 'src/engine/core-modules/feature-flag/enums/feature-flag-key.enum';
+import { FeatureFlagService } from 'src/engine/core-modules/feature-flag/services/feature-flag.service';
+import {
+  PermissionsException,
+  PermissionsExceptionCode,
+} from 'src/engine/metadata-modules/permissions/permissions.exception';
+import { PermissionsService } from 'src/engine/metadata-modules/permissions/permissions.service';
+
+export const SettingsPermissionsGuard = (
+  requiredPermission: SettingsFeatures,
+): Type<CanActivate> => {
+  @Injectable()
+  class SettingsGuardMixin extends SettingsPermissions {
+    constructor(
+      featureFlagService: FeatureFlagService,
+      permissionsService: PermissionsService,
+    ) {
+      super(requiredPermission, featureFlagService, permissionsService);
+    }
+  }
+
+  return SettingsGuardMixin;
+};
+
+@Injectable()
+class SettingsPermissions implements CanActivate {
+  constructor(
+    protected readonly requiredPermission: SettingsFeatures,
+    protected readonly featureFlagService: FeatureFlagService,
+    protected readonly permissionsService: PermissionsService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ctx = GqlExecutionContext.create(context);
+
+    const workspaceId = ctx.getContext().req.workspace.id;
+
+    const permissionsEnabled = await this.featureFlagService.isFeatureEnabled(
+      FeatureFlagKey.IsPermissionsEnabled,
+      workspaceId,
+    );
+
+    if (!permissionsEnabled) {
+      return true;
+    }
+
+    const userWorkspaceId = ctx.getContext().req.userWorkspaceId;
+
+    try {
+      await this.permissionsService.validateUserHasWorkspaceSettingPermissionOrThrow(
+        {
+          userWorkspaceId,
+          setting: this.requiredPermission,
+        },
+      );
+
+      return true;
+    } catch (error) {
+      if (
+        error instanceof PermissionsException &&
+        error.code === PermissionsExceptionCode.PERMISSION_DENIED
+      ) {
+        return false;
+      }
+      throw error;
+    }
+  }
+}
