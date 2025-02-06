ed23b49952e6d2cc8cd8fe97993c34d73c685a17
bosiraphael <raphael.bosi@gmail.com>
2025-02-06 15:59:25 +0100

    for the workflows the navigation was done twice on node creation

--- packages/twenty-front/src/modules/workflow/workflow-steps/hooks/useCreateStep.ts
+++ packages/twenty-front/src/modules/workflow/workflow-steps/hooks/useCreateStep.ts
@@ -1,5 +1,4 @@
 import { useRightDrawer } from '@/ui/layout/right-drawer/hooks/useRightDrawer';
-import { RightDrawerPages } from '@/ui/layout/right-drawer/types/RightDrawerPages';
 import { useGetUpdatableWorkflowVersion } from '@/workflow/hooks/useGetUpdatableWorkflowVersion';
 import { workflowLastCreatedStepIdState } from '@/workflow/states/workflowLastCreatedStepIdState';
 import {
@@ -7,7 +6,6 @@ import {
   WorkflowWithCurrentVersion,
 } from '@/workflow/types/Workflow';
 import { workflowSelectedNodeState } from '@/workflow/workflow-diagram/states/workflowSelectedNodeState';
-import { getWorkflowNodeIconKey } from '@/workflow/workflow-diagram/utils/getWorkflowNodeIconKey';
 import { useCreateWorkflowVersionStep } from '@/workflow/workflow-steps/hooks/useCreateWorkflowVersionStep';
 import { workflowCreateStepFromParentStepIdState } from '@/workflow/workflow-steps/states/workflowCreateStepFromParentStepIdState';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
@@ -54,17 +52,6 @@ export const useCreateStep = ({
 
     setWorkflowSelectedNode(createdStep.id);
     setWorkflowLastCreatedStepId(createdStep.id);
-
-    const stepIcon = getWorkflowNodeIconKey({
-      nodeType: 'action',
-      actionType: createdStep.type as WorkflowStepType,
-      name: createdStep.name,
-    });
-
-    openRightDrawer(RightDrawerPages.WorkflowStepEdit, {
-      title: createdStep.name,
-      Icon: getIcon(stepIcon),
-    });
   };
 
   return {
