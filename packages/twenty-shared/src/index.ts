2f47fd50c847b83f2384af2b6216d593c5d07cbe
Paul Rastoin <45004772+prastoin@users.noreply.github.com>
2025-02-06 06:25:53 +0100

    chore(shared): Update index.ts remove comment

--- packages/twenty-shared/src/index.ts
+++ packages/twenty-shared/src/index.ts
@@ -2,5 +2,3 @@ export * from './constants';
 export * from './types';
 export * from './utils';
 export * from './workspace';
-// export * from './workflow';
-// TODO use barrel.js script to handle and version index.ts files
