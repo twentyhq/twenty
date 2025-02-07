945203d2c4aca2a3f95b181544767a9bdb7ccae6
martmull <martmull@hotmail.fr>
2025-02-07 10:32:58 +0100

    Fix close right drawer on return key

--- packages/twenty-front/src/modules/object-record/record-field/form-types/components/FormFieldInputContainer.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/form-types/components/FormFieldInputContainer.tsx
@@ -1,4 +1,7 @@
 import styled from '@emotion/styled';
+import { ReactNode } from 'react';
+import { usePreviousHotkeyScope } from '@/ui/utilities/hotkey/hooks/usePreviousHotkeyScope';
+import { FormFieldInputHotKeyScope } from '@/object-record/record-field/form-types/constants/FormFieldInputHotKeyScope';
 
 const StyledFormFieldInputContainer = styled.div`
   display: flex;
@@ -6,4 +9,29 @@ const StyledFormFieldInputContainer = styled.div`
   width: 100%;
 `;
 
-export const FormFieldInputContainer = StyledFormFieldInputContainer;
+export const FormFieldInputContainer = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const {
+    goBackToPreviousHotkeyScope,
+    setHotkeyScopeAndMemorizePreviousScope,
+  } = usePreviousHotkeyScope();
+
+  const onFocus = () => {
+    setHotkeyScopeAndMemorizePreviousScope(
+      FormFieldInputHotKeyScope.FormFieldInput,
+    );
+  };
+
+  const onBlur = () => {
+    goBackToPreviousHotkeyScope();
+  };
+
+  return (
+    <StyledFormFieldInputContainer onFocus={onFocus} onBlur={onBlur}>
+      {children}
+    </StyledFormFieldInputContainer>
+  );
+};
