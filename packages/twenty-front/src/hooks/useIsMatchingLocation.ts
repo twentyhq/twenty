7e7505dc59cd6f530dbaf3eae572c69f5ffad87d
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-02-07 10:40:19 +0100

    refactor(auth): simplify continueWithEmail and remove useCallback
    Refactored continueWithEmail to remove unnecessary dependencies and simplified the useIsMatchingLocation hook by eliminating useCallback. This reduces complexity and addresses potential infinite loop issues.

--- packages/twenty-front/src/hooks/useIsMatchingLocation.ts
+++ packages/twenty-front/src/hooks/useIsMatchingLocation.ts
@@ -3,36 +3,26 @@ import { matchPath, useLocation } from 'react-router-dom';
 import { AppBasePath } from '@/types/AppBasePath';
 import { isNonEmptyString } from '@sniptt/guards';
 import { isDefined } from 'twenty-shared';
-import { useCallback } from 'react';
 
 export const useIsMatchingLocation = () => {
   const location = useLocation();
 
-  // Infinite loop issue caused by `checkUserExistsQuery` in `useSignInUp`.
-  // Without executing this query, there is no infinite loop.
-  // I also noticed that in `isMatchingLocation` inside `continueWithEmail`, no loop occurs.
-  // Both functions are called within the `useEffect` of `SignInUpWorkspaceScopeFormEffect`.
-  // This led me to conclude that the issue comes from `useIsMatchingLocation`.
-  // Using `useCallback` prevent the loop.
-  const isMatchingLocation = useCallback(
-    (path: string, basePath?: AppBasePath) => {
-      const addTrailingSlash = (path: string) =>
-        path.endsWith('/') ? path : path + '/';
+  const addTrailingSlash = (path: string) =>
+    path.endsWith('/') ? path : path + '/';
 
-      const getConstructedPath = (path: string, basePath?: AppBasePath) => {
-        if (!isNonEmptyString(basePath)) return path;
+  const getConstructedPath = (path: string, basePath?: AppBasePath) => {
+    if (!isNonEmptyString(basePath)) return path;
 
-        return addTrailingSlash(basePath) + path;
-      };
+    return addTrailingSlash(basePath) + path;
+  };
 
-      const match = matchPath(
-        getConstructedPath(path, basePath),
-        location.pathname,
-      );
-      return isDefined(match);
-    },
-    [location.pathname],
-  );
+  const isMatchingLocation = (path: string, basePath?: AppBasePath) => {
+    const match = matchPath(
+      getConstructedPath(path, basePath),
+      location.pathname,
+    );
+    return isDefined(match);
+  };
 
   return {
     isMatchingLocation,
