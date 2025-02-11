61fe833ab4b044604282ae05c0ea4ad8dd5b72a5
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-02-11 19:00:23 +0100

    Fixed naming

--- packages/twenty-front/src/modules/views/components/ViewBarRecordFilterEffect.tsx
+++ packages/twenty-front/src/modules/views/components/ViewBarRecordFilterEffect.tsx
@@ -7,7 +7,7 @@ import { useRecoilComponentFamilyStateV2 } from '@/ui/utilities/state/component-
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useSetRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentStateV2';
 import { currentViewIdComponentState } from '@/views/states/currentViewIdComponentState';
-import { hasInitializedViewFiltersComponentFamilyState } from '@/views/states/hasInitializedViewFiltersComponentFamilyState';
+import { hasInitializedCurrentRecordFiltersComponentFamilyState } from '@/views/states/hasInitializedCurrentRecordFiltersComponentFamilyState';
 import { View } from '@/views/types/View';
 import { mapViewFiltersToFilters } from '@/views/utils/mapViewFiltersToFilters';
 import { useEffect } from 'react';
@@ -20,13 +20,15 @@ export const ViewBarRecordFilterEffect = () => {
 
   const currentViewId = useRecoilComponentValueV2(currentViewIdComponentState);
 
-  const [hasInitializedViewFilters, setHasInitializedViewFilters] =
-    useRecoilComponentFamilyStateV2(
-      hasInitializedViewFiltersComponentFamilyState,
-      {
-        viewId: currentViewId,
-      },
-    );
+  const [
+    hasInitializedCurrentRecordFilters,
+    setHasInitializedCurrentRecordFilters,
+  ] = useRecoilComponentFamilyStateV2(
+    hasInitializedCurrentRecordFiltersComponentFamilyState,
+    {
+      viewId: currentViewId,
+    },
+  );
 
   const setCurrentRecordFilters = useSetRecoilComponentStateV2(
     currentRecordFiltersComponentState,
@@ -40,7 +42,7 @@ export const ViewBarRecordFilterEffect = () => {
     useFilterableFieldMetadataItemsInRecordIndexContext();
 
   useEffect(() => {
-    if (isDataPrefetched && !hasInitializedViewFilters) {
+    if (isDataPrefetched && !hasInitializedCurrentRecordFilters) {
       const currentView = views.find((view) => view.id === currentViewId);
 
       const filterDefinitions = filterableFieldMetadataItems.map(
@@ -54,7 +56,7 @@ export const ViewBarRecordFilterEffect = () => {
         setCurrentRecordFilters(
           mapViewFiltersToFilters(currentView.viewFilters, filterDefinitions),
         );
-        setHasInitializedViewFilters(true);
+        setHasInitializedCurrentRecordFilters(true);
       }
     }
   }, [
@@ -64,8 +66,8 @@ export const ViewBarRecordFilterEffect = () => {
     setCurrentRecordFilters,
     filterableFieldMetadataItems,
     currentRecordFilters,
-    hasInitializedViewFilters,
-    setHasInitializedViewFilters,
+    hasInitializedCurrentRecordFilters,
+    setHasInitializedCurrentRecordFilters,
   ]);
 
   return null;
--- packages/twenty-front/src/modules/views/states/hasInitializedViewFiltersComponentFamilyState.ts
+++
@@ -1,9 +0,0 @@
-import { createComponentFamilyStateV2 } from '@/ui/utilities/state/component-state/utils/createComponentFamilyStateV2';
-import { ViewComponentInstanceContext } from '@/views/states/contexts/ViewComponentInstanceContext';
-
-export const hasInitializedViewFiltersComponentFamilyState =
-  createComponentFamilyStateV2<boolean, { viewId?: string }>({
-    key: 'hasInitializedViewFiltersComponentFamilyState',
-    defaultValue: false,
-    componentInstanceContext: ViewComponentInstanceContext,
-  });
