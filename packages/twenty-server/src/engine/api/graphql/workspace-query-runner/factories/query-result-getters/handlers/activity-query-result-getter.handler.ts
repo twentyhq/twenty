2786c2c4526f2e8f90cbd69cb3791c7ab4b767cd
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-02-11 11:02:34 +0100

    Fix temporary console.warn

--- packages/twenty-server/src/engine/api/graphql/workspace-query-runner/factories/query-result-getters/handlers/activity-query-result-getter.handler.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-query-runner/factories/query-result-getters/handlers/activity-query-result-getter.handler.ts
@@ -41,9 +41,11 @@ export class ActivityQueryResultGetterHandler
       blocknote = JSON.parse(blocknoteJson);
     } catch (error) {
       // TODO: Remove this once we have removed the old rich text
+      // eslint-disable-next-line no-console
       console.warn(
         `Failed to parse body for activity ${activity.id} in workspace ${workspaceId}, for rich text version ${isRichTextV2Enabled ? 'v2' : 'v1'}`,
       );
+      // eslint-disable-next-line no-console
       console.warn(blocknoteJson);
     }
 
