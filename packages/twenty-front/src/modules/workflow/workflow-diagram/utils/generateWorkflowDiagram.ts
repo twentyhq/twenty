601d305b34019c469d00953ce1dcd65cae2725c6
Devessier <baptiste@devessier.fr>
2025-02-11 18:15:00 +0100

    refactor: extract how we get the trigger node definition into a separate function

--- packages/twenty-front/src/modules/workflow/workflow-diagram/utils/generateWorkflowDiagram.ts
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/utils/generateWorkflowDiagram.ts
@@ -1,18 +1,15 @@
 import { WorkflowStep, WorkflowTrigger } from '@/workflow/types/Workflow';
-import { assertUnreachable } from '@/workflow/utils/assertUnreachable';
-import { splitWorkflowTriggerEventName } from '@/workflow/utils/splitWorkflowTriggerEventName';
+import { WORKFLOW_DIAGRAM_EMPTY_TRIGGER_NODE_DEFINITION } from '@/workflow/workflow-diagram/constants/WorkflowDiagramEmptyTriggerNodeDefinition';
 import { WORKFLOW_VISUALIZER_EDGE_DEFAULT_CONFIGURATION } from '@/workflow/workflow-diagram/constants/WorkflowVisualizerEdgeDefaultConfiguration';
 import {
   WorkflowDiagram,
   WorkflowDiagramEdge,
-  WorkflowDiagramEmptyTriggerNodeData,
   WorkflowDiagramNode,
   WorkflowDiagramStepNodeData,
 } from '@/workflow/workflow-diagram/types/WorkflowDiagram';
-import { DATABASE_TRIGGER_TYPES } from '@/workflow/workflow-trigger/constants/DatabaseTriggerTypes';
+import { getWorkflowDiagramTriggerNode } from '@/workflow/workflow-diagram/utils/getWorkflowDiagramTriggerNode';
 
 import { TRIGGER_STEP_ID } from '@/workflow/workflow-trigger/constants/TriggerStepId';
-import { getTriggerIcon } from '@/workflow/workflow-trigger/utils/getTriggerIcon';
 import { isDefined } from 'twenty-shared';
 import { v4 } from 'uuid';
 
@@ -58,84 +55,13 @@ export const generateWorkflowDiagram = ({
     return nodeId;
   };
 
-  const triggerNodeId = TRIGGER_STEP_ID;
-
   if (isDefined(trigger)) {
-    let triggerDefaultLabel: string;
-    let triggerIcon: string | undefined;
-
-    switch (trigger.type) {
-      case 'MANUAL': {
-        triggerDefaultLabel = 'Manual Trigger';
-        triggerIcon = getTriggerIcon({
-          type: 'MANUAL',
-        });
-
-        break;
-      }
-      case 'CRON': {
-        triggerDefaultLabel = 'On a Schedule';
-        triggerIcon = getTriggerIcon({
-          type: 'CRON',
-        });
-
-        break;
-      }
-      case 'DATABASE_EVENT': {
-        const triggerEvent = splitWorkflowTriggerEventName(
-          trigger.settings.eventName,
-        );
-
-        triggerDefaultLabel =
-          DATABASE_TRIGGER_TYPES.find(
-            (item) => item.event === triggerEvent.event,
-          )?.defaultLabel ?? '';
-
-        triggerIcon = getTriggerIcon({
-          type: 'DATABASE_EVENT',
-          eventName: triggerEvent.event,
-        });
-
-        break;
-      }
-      default: {
-        return assertUnreachable(
-          trigger,
-          `Expected the trigger "${JSON.stringify(trigger)}" to be supported.`,
-        );
-      }
-    }
-
-    nodes.push({
-      id: triggerNodeId,
-      data: {
-        nodeType: 'trigger',
-        triggerType: trigger.type,
-        name: isDefined(trigger.name) ? trigger.name : triggerDefaultLabel,
-        icon: triggerIcon,
-        isLeafNode: false,
-      } satisfies WorkflowDiagramStepNodeData,
-      position: {
-        x: 0,
-        y: 0,
-      },
-    });
+    nodes.push(getWorkflowDiagramTriggerNode({ trigger }));
   } else {
-    nodes.push({
-      id: triggerNodeId,
-      type: 'empty-trigger',
-      data: {
-        nodeType: 'empty-trigger',
-        isLeafNode: false,
-      } satisfies WorkflowDiagramEmptyTriggerNodeData,
-      position: {
-        x: 0,
-        y: 0,
-      },
-    });
+    nodes.push(WORKFLOW_DIAGRAM_EMPTY_TRIGGER_NODE_DEFINITION);
   }
 
-  let lastStepId = triggerNodeId;
+  let lastStepId = TRIGGER_STEP_ID;
 
   for (const step of steps) {
     lastStepId = processNode(step, lastStepId, 150, 100);
