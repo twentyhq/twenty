bed0430a4d66089ed09e7f63a4425bf28ba6f31b
martmull <martmull@hotmail.fr>
2025-02-12 15:55:58 +0100

    Add info log

--- packages/twenty-server/src/modules/workflow/workflow-status/jobs/workflow-statuses-update.job.ts
+++ packages/twenty-server/src/modules/workflow/workflow-status/jobs/workflow-statuses-update.job.ts
@@ -191,6 +191,8 @@ export class WorkflowStatusesUpdateJob {
             serverlessFunction.latestVersion;
 
           newStep.settings = newStepSettings;
+
+          this.logger.log(`New step computed for code step -> ${newStep}`);
         }
         newSteps.push(newStep);
       }
