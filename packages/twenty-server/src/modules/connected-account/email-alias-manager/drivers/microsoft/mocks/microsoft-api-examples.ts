3f2e47be5b62bf619a6aba07bf6cfb42a1ba26e7
guillim <guigloo@msn.com>
2025-02-12 12:46:16 +0100

    adding unit test

---
+++ packages/twenty-server/src/modules/connected-account/email-alias-manager/drivers/microsoft/mocks/microsoft-api-examples.ts
@@ -0,0 +1,9 @@
+export const microsoftGraphMeResponseWithProxyAddresses = {
+  '@odata.context':
+    'https://graph.microsoft.com/v1.0/$metadata#users(proxyAddresses)/$entity',
+  proxyAddresses: [
+    'SMTP:bertrand1@domain.onmicrosoft.com',
+    'smtp:betrand2@domain.onmicrosoft.com',
+    'smtp:betrand3@otherdomain.com',
+  ],
+};
