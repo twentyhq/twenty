b0da46e62f1666a428eaebcaed8ee847e58b6c78
guillim <guigloo@msn.com>
2025-02-13 15:51:04 +0100

    marie better naming

--- packages/twenty-server/src/engine/core-modules/workspace/utils/workspace-graphql-api-exception-handler.util.ts
+++ packages/twenty-server/src/engine/core-modules/workspace/utils/workspace-graphql-api-exception-handler.util.ts
@@ -1,5 +1,6 @@
 import {
   ConflictError,
+  ForbiddenError,
   InternalServerError,
   NotFoundError,
 } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
@@ -13,10 +14,11 @@ export const workspaceGraphqlApiExceptionHandler = (error: Error) => {
     switch (error.code) {
       case WorkspaceExceptionCode.SUBDOMAIN_NOT_FOUND:
       case WorkspaceExceptionCode.WORKSPACE_NOT_FOUND:
-      case WorkspaceExceptionCode.ENVIRONMENT_VAR_NOT_ENABLED:
         throw new NotFoundError(error.message);
       case WorkspaceExceptionCode.SUBDOMAIN_ALREADY_TAKEN:
         throw new ConflictError(error.message);
+      case WorkspaceExceptionCode.ENVIRONMENT_VAR_NOT_ENABLED:
+        throw new ForbiddenError(error.message);
       default:
         throw new InternalServerError(error.message);
     }
