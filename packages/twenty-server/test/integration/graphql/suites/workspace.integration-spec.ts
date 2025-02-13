8760a926ffd785acc15a3e1a020b81afecb5f4a2
Marie Stoppa <marie.stoppa@essec.edu>
2025-02-13 15:22:46 +0100

    wip

--- packages/twenty-server/test/integration/graphql/suites/workspace.integration-spec.ts
+++ packages/twenty-server/test/integration/graphql/suites/workspace.integration-spec.ts
@@ -1,12 +1,16 @@
 import request from 'supertest';
+import { ADMIN_ACCESS_TOKEN } from 'test/integration/constants/admin-access-token.constants';
 import { updateFeatureFlagFactory } from 'test/integration/graphql/utils/update-feature-flag-factory.util';
+
+import { SEED_APPLE_WORKSPACE_ID } from 'src/database/typeorm-seeds/core/workspaces';
+import { PermissionsExceptionMessage } from 'src/engine/metadata-modules/permissions/permissions.exception';
 
 const client = request(`http://localhost:${APP_PORT}`);
 
 describe('WorkspaceResolver', () => {
   beforeEach(async () => {
     const query = updateFeatureFlagFactory(
-      'a43da6fa-8792-492f-8113-7a911b05bc14',
+      SEED_APPLE_WORKSPACE_ID,
       'IsPermissionsEnabled',
       true,
     );
@@ -15,7 +19,7 @@ describe('WorkspaceResolver', () => {
   });
   afterEach(async () => {
     const query = updateFeatureFlagFactory(
-      'a43da6fa-8792-492f-8113-7a911b05bc14',
+      SEED_APPLE_WORKSPACE_ID,
       'IsPermissionsEnabled',
       false,
     );
@@ -23,6 +27,62 @@ describe('WorkspaceResolver', () => {
     await client.post('/graphql').send(query);
   });
   describe('security permissions', () => {
+    it('should update workspace when user has permission (admin role)', async () => {
+      const queryData = {
+        query: `
+        mutation updateWorkspace {
+          updateWorkspace(data: { isMicrosoftAuthEnabled: false }) {
+            id
+            isMicrosoftAuthEnabled
+          }
+        }
+      `,
+      };
+
+      return client
+        .post('/graphql')
+        .set('Authorization', `Bearer ${ADMIN_ACCESS_TOKEN}`)
+        .send(queryData)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data).toBeDefined();
+          expect(res.body.errors).toBeUndefined();
+        })
+        .expect((res) => {
+          const data = res.body.data.updateWorkspace;
+
+          expect(data).toBeDefined();
+          expect(data.isMicrosoftAuthEnabled).toBe(false);
+        });
+    });
+
+    it('should throw a permission error when user does not have permission (no admin role)', async () => {
+      const queryData = {
+        query: `
+        mutation updateWorkspace {
+          updateWorkspace(data: { isMicrosoftAuthEnabled: false }) {
+            id
+            isMicrosoftAuthEnabled
+          }
+        }
+      `,
+      };
+
+      return client
+        .post('/graphql')
+        .set('Authorization', `Bearer ${ACCESS_TOKEN}`)
+        .send(queryData)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data).toBeNull();
+          expect(res.body.errors).toBeDefined();
+          expect(res.body.errors[0].message).toBe(
+            PermissionsExceptionMessage.PERMISSION_DENIED,
+          );
+        });
+    });
+  });
+  describe('workspace permissions', () => {
     it('should update workspace when permissions are enabled and user has permission', async () => {
       const queryData = {
         query: `
