d6739825109fceac7b6a3dbe7eda6bb666342681
ehconitin <nitinkoche03@gmail.com>
2025-02-13 16:43:22 +0530

    oops

--- packages/twenty-server/src/engine/core-modules/health/health.module.ts
+++ packages/twenty-server/src/engine/core-modules/health/health.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 
 import { HealthCacheService } from 'src/engine/core-modules/health/health-cache.service';
 import { HealthController } from 'src/engine/core-modules/health/health.controller';
 import { RedisClientModule } from 'src/engine/core-modules/redis-client/redis-client.module';
 
 @Module({
-  imports: [RedisClientModule],
+  imports: [TerminusModule, RedisClientModule],
   controllers: [HealthController],
   providers: [HealthCacheService],
   exports: [HealthCacheService],
