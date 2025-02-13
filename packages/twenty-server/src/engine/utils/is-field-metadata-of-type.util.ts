0b42f67d388a68d4af1d8e2a663f90f20ac3aeda
Jérémy Magrin <jeremy.magrin@gmail.com>
2025-02-13 12:27:28 +0100

    wip

--- packages/twenty-server/src/engine/utils/is-field-metadata-of-type.util.ts
+++ packages/twenty-server/src/engine/utils/is-field-metadata-of-type.util.ts
@@ -2,9 +2,17 @@ import { FieldMetadataType } from 'twenty-shared';
 
 import { FieldMetadataInterface } from 'src/engine/metadata-modules/field-metadata/interfaces/field-metadata.interface';
 
-export function isFieldMetadataOfType<Type extends FieldMetadataType>(
-  fieldMetadata: FieldMetadataInterface<any>,
+export function isFieldMetadataOfType<
+  T extends FieldMetadataInterface<FieldMetadataType>,
+  Type extends T['type'],
+>(
+  fieldMetadata: T,
   type: Type,
-): fieldMetadata is FieldMetadataInterface<Type> {
+): fieldMetadata is T & {
+  type: Type;
+  // defaultValue?: FieldMetadataDefaultValue<Type>;
+  // options?: FieldMetadataOptions<Type>;
+  // settings?: FieldMetadataSettings<Type>;
+} {
   return fieldMetadata.type === type;
 }
