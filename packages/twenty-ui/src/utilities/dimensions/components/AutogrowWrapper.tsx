895f9796e987971d7156e7b6f8e34e873dff4501
bosiraphael <raphael.bosi@gmail.com>
2025-02-14 16:33:02 +0100

    remove unnecessary intersection observer

---
+++ packages/twenty-ui/src/utilities/dimensions/components/AutogrowWrapper.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+import { ReactNode, useRef } from 'react';
+
+type AutogrowWrapperProps = {
+  children: ReactNode;
+  node?: ReactNode;
+  className?: string;
+};
+
+const StyledNodeWrapper = styled.span`
+  pointer-events: none;
+  visibility: hidden;
+`;
+
+const StyledDiv = styled.div`
+  max-width: 100%;
+  position: relative;
+`;
+
+const StyledChildWrapper = styled.div`
+  left: 0;
+  top: 0;
+  position: absolute;
+`;
+
+export const AutogrowWrapper = ({
+  children,
+  node = children,
+  className,
+}: AutogrowWrapperProps) => {
+  const nodeWrapperRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <StyledDiv ref={nodeWrapperRef} className={className}>
+      <StyledNodeWrapper>{node}</StyledNodeWrapper>
+      <StyledChildWrapper>{children}</StyledChildWrapper>
+    </StyledDiv>
+  );
+};
