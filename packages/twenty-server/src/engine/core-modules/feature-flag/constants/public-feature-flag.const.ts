5fe101545ded7f45205640ceb467654286504171
Thomas Trompette <thomas.trompette@sfr.fr>
2025-02-14 14:59:27 +0100

    Add side panel in lab (#10183)
    <img width="908" alt="Capture d’écran 2025-02-13 à 11 15 22"
    src="https://github.com/user-attachments/assets/642b181c-8a20-4935-bb32-97d9d2b0f4c9"
    />

--- packages/twenty-server/src/engine/core-modules/feature-flag/constants/public-feature-flag.const.ts
+++ packages/twenty-server/src/engine/core-modules/feature-flag/constants/public-feature-flag.const.ts
@@ -7,7 +7,10 @@ type FeatureFlagMetadata = {
 };
 
 export type PublicFeatureFlag = {
-  key: Extract<FeatureFlagKey, FeatureFlagKey.IsWorkflowEnabled>;
+  key: Extract<
+    FeatureFlagKey,
+    FeatureFlagKey.IsWorkflowEnabled | FeatureFlagKey.IsCommandMenuV2Enabled
+  >;
   metadata: FeatureFlagMetadata;
 };
 
@@ -18,6 +21,15 @@ export const PUBLIC_FEATURE_FLAGS: PublicFeatureFlag[] = [
       label: 'Workflows',
       description: 'Create custom workflows to automate your work.',
       imagePath: 'https://twenty.com/images/lab/is-workflow-enabled.png',
+    },
+  },
+  {
+    key: FeatureFlagKey.IsCommandMenuV2Enabled,
+    metadata: {
+      label: 'Side Panel',
+      description:
+        'Click on the 3 dots menu at the top right or press command K to open your new side panel.',
+      imagePath: 'https://twenty.com/images/lab/side-panel.png',
     },
   },
 ];
