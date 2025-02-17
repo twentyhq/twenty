687f692c34324959e87b129ae382b1ad8d22b07b
JarWarren <43893126+JarWarren@users.noreply.github.com>
2025-02-16 20:58:16 -0800

    Update page.tsx
    Fix typo in Cloudflare

--- packages/twenty-website/src/app/page.tsx
+++ packages/twenty-website/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div style={{ minHeight: '60vh', marginTop: '50px' }}>
         Part of the website is built directly with Framer, including the
         homepage. <br />
-        We use Clouflare to split the traffic between the two sites.
+        We use Cloudflare to split the traffic between the two sites.
       </div>
     </ContentContainer>
   );
