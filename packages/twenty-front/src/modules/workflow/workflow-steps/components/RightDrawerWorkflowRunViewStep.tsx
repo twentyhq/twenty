6568a35c1e8bf9e215e2123dcc610407beeef554
Devessier <baptiste@devessier.fr>
2025-02-20 17:39:59 +0100

    feat: start creating a tab list for the workflow run right drawer

--- packages/twenty-front/src/modules/workflow/workflow-steps/components/RightDrawerWorkflowRunViewStep.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-steps/components/RightDrawerWorkflowRunViewStep.tsx
@@ -1,8 +1,25 @@
+import { SingleTabProps, TabList } from '@/ui/layout/tab/components/TabList';
+import { useTabList } from '@/ui/layout/tab/hooks/useTabList';
 import { useFlowOrThrow } from '@/workflow/hooks/useFlowOrThrow';
 import { workflowSelectedNodeState } from '@/workflow/workflow-diagram/states/workflowSelectedNodeState';
 import { WorkflowStepDetail } from '@/workflow/workflow-steps/components/WorkflowStepDetail';
+import styled from '@emotion/styled';
 import { useRecoilValue } from 'recoil';
 import { isDefined } from 'twenty-shared';
+import { IconCode, IconStepInto } from 'twenty-ui';
+
+const StyledTabListContainer = styled.div`
+  align-items: center;
+  padding-left: ${({ theme }) => theme.spacing(2)};
+  border-bottom: ${({ theme }) => `1px solid ${theme.border.color.light}`};
+  box-sizing: border-box;
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(2)};
+  height: ${({ theme }) => theme.spacing(10)};
+  background-color: ${({ theme }) => theme.background.secondary};
+`;
+
+type TabId = 'node' | 'other';
 
 export const RightDrawerWorkflowRunViewStep = () => {
   const flow = useFlowOrThrow();
@@ -14,14 +31,32 @@ export const RightDrawerWorkflowRunViewStep = () => {
     );
   }
 
+  const tabListId = `${'workflow-run-right-drawer'}`;
+  const { activeTabId } = useTabList<TabId>(tabListId);
+
+  const tabs: SingleTabProps<TabId>[] = [
+    { id: 'node', title: 'Node', Icon: IconStepInto },
+    { id: 'other', title: 'Code', Icon: IconCode },
+  ];
+
   return (
     <>
-      <WorkflowStepDetail
-        stepId={workflowSelectedNode}
-        trigger={flow.trigger}
-        steps={flow.steps}
-        readonly
-      />
+      <StyledTabListContainer>
+        <TabList
+          tabListInstanceId={tabListId}
+          tabs={tabs}
+          behaveAsLinks={false}
+        />
+      </StyledTabListContainer>
+
+      {activeTabId === 'node' ? (
+        <WorkflowStepDetail
+          stepId={workflowSelectedNode}
+          trigger={flow.trigger}
+          steps={flow.steps}
+          readonly
+        />
+      ) : null}
     </>
   );
 };
