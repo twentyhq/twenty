41ca5242ce9ba51fabe9c80eec529bce43e27f17
bosiraphael <raphael.bosi@gmail.com>
2025-02-20 15:42:21 +0100

    create script to update all default views on workflow objects

--- packages/twenty-server/src/database/commands/upgrade-version/0-43/0-43-upgrade-version.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-43/0-43-upgrade-version.command.ts
@@ -6,6 +6,8 @@ import { Repository } from 'typeorm';
 import { ActiveWorkspacesCommandRunner } from 'src/database/commands/active-workspaces.command';
 import { BaseCommandOptions } from 'src/database/commands/base.command';
 import { AddTasksAssignedToMeViewCommand } from 'src/database/commands/upgrade-version/0-43/0-43-add-tasks-assigned-to-me-view.command';
+import { MigrateSearchVectorOnNoteAndTaskEntitiesCommand } from 'src/database/commands/upgrade-version/0-43/0-43-migrate-search-vector-on-note-and-task-entities.command';
+import { UpdateDefaultViewRecordOpeningOnWorkflowObjectsCommand } from 'src/database/commands/upgrade-version/0-43/0-43-update-default-view-record-opening-on-workflow-objects.command';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 
 @Command({
@@ -17,6 +19,8 @@ export class UpgradeTo0_43Command extends ActiveWorkspacesCommandRunner {
     @InjectRepository(Workspace, 'core')
     protected readonly workspaceRepository: Repository<Workspace>,
     private readonly addTasksAssignedToMeViewCommand: AddTasksAssignedToMeViewCommand,
+    private readonly migrateSearchVectorOnNoteAndTaskEntitiesCommand: MigrateSearchVectorOnNoteAndTaskEntitiesCommand,
+    private readonly updateDefaultViewRecordOpeningOnWorkflowObjectsCommand: UpdateDefaultViewRecordOpeningOnWorkflowObjectsCommand,
   ) {
     super(workspaceRepository);
   }
@@ -29,6 +33,18 @@ export class UpgradeTo0_43Command extends ActiveWorkspacesCommandRunner {
     this.logger.log('Running command to upgrade to 0.43');
 
     await this.addTasksAssignedToMeViewCommand.executeActiveWorkspacesCommand(
+      passedParam,
+      options,
+      workspaceIds,
+    );
+
+    await this.migrateSearchVectorOnNoteAndTaskEntitiesCommand.executeActiveWorkspacesCommand(
+      passedParam,
+      options,
+      workspaceIds,
+    );
+
+    await this.updateDefaultViewRecordOpeningOnWorkflowObjectsCommand.executeActiveWorkspacesCommand(
       passedParam,
       options,
       workspaceIds,
