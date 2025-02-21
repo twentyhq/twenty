7463d090791a10cac6f280ac656bf898a14c9761
oliver <8559757+oliverqx@users.noreply.github.com>
2025-02-20 20:30:04 -0600

    feat: update fields

--- packages/twenty-front/src/modules/settings/api/playground/form/components/ApiPlaygroundSetupForm.tsx
+++ packages/twenty-front/src/modules/settings/api/playground/form/components/ApiPlaygroundSetupForm.tsx
@@ -1,14 +1,23 @@
-import { TextInput } from '@/ui/input/components/TextInput';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { useFindManyRecords } from '@/object-record/hooks/useFindManyRecords';
+import { ApiKey } from '@/settings/developers/types/api-key/ApiKey';
+import { Select } from '@/ui/input/components/Select';
 import styled from '@emotion/styled';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useLingui } from '@lingui/react/macro';
+import { IconBrandGraphql } from '@tabler/icons-react';
 import { Controller, useForm } from 'react-hook-form';
+import {
+  IconApi,
+  IconBracketsAngle,
+  IconFolderRoot
+} from 'twenty-ui';
 import { z } from 'zod';
 
 export const apiPlaygroundSetupFormSchema = z.object({
     apiKey: z.string(),
     schema: z.string(),
-    playground: z.string()
+    apiPlayground: z.string()
 })
 
 type ApiPlaygroundSetupFormValues = z.infer<
@@ -31,65 +40,64 @@ export const ApiPlaygroundSetupForm = ({
 }: ApiPlaygroundFormProps) => {
   const { t } = useLingui();
 
-  const { control } =
-    useForm<ApiPlaygroundSetupFormValues>({
-        mode: 'onTouched',
-        resolver: zodResolver(apiPlaygroundSetupFormSchema),
-    });
+  const { control } = useForm<ApiPlaygroundSetupFormValues>({
+      mode: 'onTouched',
+      resolver: zodResolver(apiPlaygroundSetupFormSchema),
+  });
+
+  const { records: apiKeys } = useFindManyRecords<ApiKey>({
+      objectNameSingular: CoreObjectNameSingular.ApiKey,
+      filter: { revokedAt: { is: 'NULL' } },
+  });
 
   return (
     <StyledInputsContainer>
         <Controller
             name={'apiKey'}
             control={control}
-            // defaultValue={objectMetadataItem?.icon ?? 'IconListNumbers'}
             render={({ field: { onChange, value } }) => (
-                <TextInput
-                    label={'API Key'}
-                    placeholder={'Listing'}
-                    value={value}
-                    onChange={(value) => {
-                        onChange(value);
-                    }}
-                    onBlur={onBlur}
-                //   disabled={disableEdition}
-                    fullWidth
+                <Select
+                  dropdownId="schema"
+                  label="Schema"
+                  options={[
+                    { value: 'core', label: 'Core' },
+                    { value: 'metadata', label: 'Metadata' },
+                  ]}
+                  value={value}
+                  onChange={onChange}
                 />
             )}
         />
         <Controller
             name={'schema'}
             control={control}
-        //   defaultValue={objectMetadataItem?.labelSingular}
             render={({ field: { onChange, value } }) => (
-            <TextInput
-                label={'Schema'}
-                placeholder={'Listing'}
-                value={value}
-                onChange={(value) => {
-                onChange(value);
-                }}
-                onBlur={onBlur}
-            //   disabled={disableEdition}
-                fullWidth
-            />
+                <Select
+                  dropdownId="schema"
+                  label="Schema"
+                  options={[
+                    { value: 'core', label: 'Core', Icon: IconFolderRoot },
+                    { value: 'metadata', label: 'Metadata', Icon: IconBracketsAngle },
+                  ]}
+                  value={value}
+                  onChange={onChange}
+                />
             )}
         />
         <Controller
-            name={'playground'}
+            name={'apiPlayground'}
             control={control}
-        //   defaultValue={objectMetadataItem?.labelPlural}
             render={({ field: { onChange, value } }) => (
-            <TextInput
-                label={t`Plural`}
-                placeholder={t`Listings`}
-                value={value}
-                onChange={(value) => {
-                onChange(value);
-                }}
-            //   disabled={disableEdition}
-                fullWidth
-            />
+                <Select
+                    dropdownId="apiPlaygroundType"
+                    label="API"
+                    options={[
+                        { value: 'rest', label: 'REST', Icon: IconApi },
+                        { value: 'graphql', label: 'GraphQL', Icon: IconBrandGraphql },
+                    ]}
+                    value={value}
+                    onChange={onChange}
+                />
             )}
         />
     </StyledInputsContainer>
