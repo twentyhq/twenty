f0ae47bafc17a0d08c69d4737253ee69c1f17b17
Paribesh01 <nepalparibesh01@gmail.com>
2025-02-25 00:19:41 +0530

    fix: Active

--- packages/twenty-front/src/pages/object-record/RecordIndexPage.tsx
+++ packages/twenty-front/src/pages/object-record/RecordIndexPage.tsx
@@ -2,11 +2,14 @@ import { contextStoreCurrentObjectMetadataItemComponentState } from '@/context-s
 import { contextStoreCurrentViewIdComponentState } from '@/context-store/states/contextStoreCurrentViewIdComponentState';
 import { ContextStoreComponentInstanceContext } from '@/context-store/states/contexts/ContextStoreComponentInstanceContext';
 import { RecordIndexContainerGater } from '@/object-record/record-index/components/RecordIndexContainerGater';
+import { AppPath } from '@/types/AppPath';
 import { PageContainer } from '@/ui/layout/page/components/PageContainer';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { isNonEmptyString, isUndefined } from '@sniptt/guards';
+import { useNavigateApp } from '~/hooks/useNavigateApp';
 
 export const RecordIndexPage = () => {
+  const navigateApp = useNavigateApp();
   const contextStoreCurrentViewId = useRecoilComponentValueV2(
     contextStoreCurrentViewIdComponentState,
     'main-context-store',
@@ -16,6 +19,10 @@ export const RecordIndexPage = () => {
     contextStoreCurrentObjectMetadataItemComponentState,
     'main-context-store',
   );
+
+  if (!objectMetadataItem?.isActive) {
+    navigateApp(AppPath.NotFound);
+  }
 
   if (
     isUndefined(objectMetadataItem) ||
