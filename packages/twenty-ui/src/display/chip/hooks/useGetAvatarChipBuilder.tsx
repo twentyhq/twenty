54264f867306eb5067f1726f1eabb55835edd60a
prastoin <paul@twenty.com>
2025-02-24 15:17:08 +0100

    lint(ui): fix

--- packages/twenty-ui/src/display/chip/hooks/useGetAvatarChipBuilder.tsx
+++ packages/twenty-ui/src/display/chip/hooks/useGetAvatarChipBuilder.tsx
@@ -43,7 +43,7 @@ export const useGetAvatarChipsBuilder = ({
   LeftIconColor,
   avatarType,
   avatarUrl,
-  isIconInverted,
+  isIconInverted = false,
   placeholderColorSeed,
   variant: propsVariant,
 }: AvatarChipsCommonProps) => {
