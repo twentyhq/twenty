fa2b3ec31430c75ed45131f78a07b886a7dc4223
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-02-24 16:13:13 +0100

    - Created useSaveRecordSortsToViewSorts - Added getViewSortsToCreate and its unit test - Added getViewSortsToDelete and its unit test - Added getViewSortsToUpdate and its unit test - Added areViewSortsEqual and its unit test - Implemented saveRecordSortsToViewSorts instead of previous saveViewSorts

--- packages/twenty-front/src/modules/views/hooks/useAreViewSortsDifferentFromRecordSorts.ts
+++ packages/twenty-front/src/modules/views/hooks/useAreViewSortsDifferentFromRecordSorts.ts
@@ -1,14 +1,46 @@
-import { useRecoilComponentFamilyValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentFamilyValueV2';
+import { currentRecordSortsComponentState } from '@/object-record/record-sort/states/currentRecordSortsComponentState';
+import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useGetCurrentViewOnly } from '@/views/hooks/useGetCurrentViewOnly';
-import { areViewSortsDifferentFromRecordSortsSelector } from '@/views/states/selectors/areViewSortsDifferentFromRecordSortsFamilySelector';
+import { getViewSortsToCreate } from '@/views/utils/getViewSortsToCreate';
+import { getViewSortsToDelete } from '@/views/utils/getViewSortsToDelete';
+import { getViewSortsToUpdate } from '@/views/utils/getViewSortsToUpdate';
+import { mapRecordSortToViewSort } from '@/views/utils/mapRecordSortToViewSort';
+import { useMemo } from 'react';
 
 export const useAreViewSortsDifferentFromRecordSorts = () => {
   const { currentView } = useGetCurrentViewOnly();
-
-  const viewSortsAreDifferentFromRecordSorts = useRecoilComponentFamilyValueV2(
-    areViewSortsDifferentFromRecordSortsSelector,
-    { viewId: currentView?.id },
+  const currentRecordSorts = useRecoilComponentValueV2(
+    currentRecordSortsComponentState,
   );
+
+  const viewSortsAreDifferentFromRecordSorts = useMemo(() => {
+    const currentViewSorts = currentView?.viewSorts ?? [];
+    const viewSortsFromCurrentRecordSorts = currentRecordSorts.map(
+      mapRecordSortToViewSort,
+    );
+
+    const viewSortsToCreate = getViewSortsToCreate(
+      currentViewSorts,
+      viewSortsFromCurrentRecordSorts,
+    );
+
+    const viewSortsToDelete = getViewSortsToDelete(
+      currentViewSorts,
+      viewSortsFromCurrentRecordSorts,
+    );
+
+    const viewSortsToUpdate = getViewSortsToUpdate(
+      currentViewSorts,
+      viewSortsFromCurrentRecordSorts,
+    );
+
+    const sortsHaveChanged =
+      viewSortsToCreate.length > 0 ||
+      viewSortsToDelete.length > 0 ||
+      viewSortsToUpdate.length > 0;
+
+    return sortsHaveChanged;
+  }, [currentRecordSorts, currentView]);
 
   return { viewSortsAreDifferentFromRecordSorts };
 };
