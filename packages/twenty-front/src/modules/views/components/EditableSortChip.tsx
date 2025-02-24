92b8bfca7565bd185b53590b836592c4b3532f38
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-02-24 21:40:13 +0100

    - Removed combinedFilter and combinedSort related hooks and utils - Removed unsaved states for view filters and view sorts - Use record filter and record sort related hooks and utils where needed instead

--- packages/twenty-front/src/modules/views/components/EditableSortChip.tsx
+++ packages/twenty-front/src/modules/views/components/EditableSortChip.tsx
@@ -5,24 +5,17 @@ import { useRemoveRecordSort } from '@/object-record/record-sort/hooks/useRemove
 import { useUpsertRecordSort } from '@/object-record/record-sort/hooks/useUpsertRecordSort';
 import { RecordSort } from '@/object-record/record-sort/types/RecordSort';
 import { SortOrFilterChip } from '@/views/components/SortOrFilterChip';
-import { useDeleteCombinedViewSorts } from '@/views/hooks/useDeleteCombinedViewSorts';
-import { useUpsertCombinedViewSorts } from '@/views/hooks/useUpsertCombinedViewSorts';
 
 type EditableSortChipProps = {
   recordSort: RecordSort;
 };
 
 export const EditableSortChip = ({ recordSort }: EditableSortChipProps) => {
-  const { deleteCombinedViewSort } = useDeleteCombinedViewSorts();
-
   const { removeRecordSort } = useRemoveRecordSort();
-
-  const { upsertCombinedViewSort } = useUpsertCombinedViewSorts();
 
   const { upsertRecordSort } = useUpsertRecordSort();
 
   const handleRemoveClick = () => {
-    deleteCombinedViewSort(recordSort.fieldMetadataId);
     removeRecordSort(recordSort.fieldMetadataId);
   };
 
@@ -36,7 +29,6 @@ export const EditableSortChip = ({ recordSort }: EditableSortChipProps) => {
       direction: recordSort.direction === 'asc' ? 'desc' : 'asc',
     };
 
-    upsertCombinedViewSort(newSort);
     upsertRecordSort(newSort);
   };
 
