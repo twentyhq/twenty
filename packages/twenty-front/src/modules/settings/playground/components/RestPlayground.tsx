371b7a8550510fbf6221b37100f049414c96f488
oliver <8559757+oliverqx@users.noreply.github.com>
2025-02-24 17:06:16 -0600

    fix: REST Playground padding
    - add scroll wrapper
    - add nested container. used to apply max height to vue app. otherwise, the bottom padding is not seen

--- packages/twenty-front/src/modules/settings/playground/components/RestPlayground.tsx
+++ packages/twenty-front/src/modules/settings/playground/components/RestPlayground.tsx
@@ -1,6 +1,9 @@
 import { PlaygroundPage } from '@/settings/playground/components/PlaygroundPage';
 import { openAPIReferenceState } from '@/settings/playground/states/openAPIReference';
 import { SettingsPath } from '@/types/SettingsPath';
+import { PAGE_BAR_MIN_HEIGHT } from '@/ui/layout/page/components/PageHeader';
+import { ScrollWrapper } from '@/ui/utilities/scroll/components/ScrollWrapper';
+import styled from '@emotion/styled';
 import { Trans } from '@lingui/react/macro';
 import { ApiReferenceReact } from '@scalar/api-reference-react';
 import '@scalar/api-reference-react/style.css';
@@ -8,6 +11,20 @@ import { useContext } from 'react';
 import { useRecoilState } from 'recoil';
 import { ThemeContext } from 'twenty-ui';
 import { getSettingsPath } from '~/utils/navigation/getSettingsPath';
+
+const StyledNestedContainer = styled.div<{ pageBarHeight: number }>`
+  height: 100%;
+  width: 100%;
+
+  > * {
+    max-height: ${({ theme, pageBarHeight }) =>
+      `calc(
+        100dvh 
+        - ${pageBarHeight * 3}px 
+        - ${theme.spacing(3)}
+      ) !important`};
+  }
+`;
 
 export const RestPlayground = () => {
   const [openAPIReference] = useRecoilState(openAPIReferenceState);
@@ -27,14 +44,21 @@ export const RestPlayground = () => {
         { children: <Trans>Rest API Playground</Trans> },
       ]}
     >
-      <ApiReferenceReact
-        configuration={{
-          spec: {
-            content: openAPIReference,
-          },
-          forceDarkModeState: theme.name as 'dark' | 'light',
-        }}
-      />
+      <ScrollWrapper
+        contextProviderName="playgroundPageContainer"
+        componentInstanceId={'scroll-wrapper-playground-page-container'}
+      >
+        <StyledNestedContainer pageBarHeight={PAGE_BAR_MIN_HEIGHT}>
+          <ApiReferenceReact
+            configuration={{
+              spec: {
+                content: openAPIReference,
+              },
+              forceDarkModeState: theme.name as 'dark' | 'light',
+            }}
+          />
+        </StyledNestedContainer>
+      </ScrollWrapper>
     </PlaygroundPage>
   );
 };
