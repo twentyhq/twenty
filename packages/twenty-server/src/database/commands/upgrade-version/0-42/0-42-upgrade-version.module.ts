7a90909bb46bf796ea2f7a525cbe92aa7d8911ec
Jérémy Magrin <jeremy.magrin@gmail.com>
2025-02-25 11:04:37 +0100

    fix

--- packages/twenty-server/src/database/commands/upgrade-version/0-42/0-42-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-42/0-42-upgrade-version.module.ts
@@ -6,7 +6,6 @@ import { FixBodyV2ViewFieldPositionCommand } from 'src/database/commands/upgrade
 import { LimitAmountOfViewFieldCommand } from 'src/database/commands/upgrade-version/0-42/0-42-limit-amount-of-view-field';
 import { MigrateRichTextFieldCommand } from 'src/database/commands/upgrade-version/0-42/0-42-migrate-rich-text-field.command';
 import { StandardizationOfActorCompositeContextTypeCommand } from 'src/database/commands/upgrade-version/0-42/0-42-standardization-of-actor-composite-context-type';
-import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 import { FeatureFlag } from 'src/engine/core-modules/feature-flag/feature-flag.entity';
 import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-flag.module';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
@@ -27,7 +26,6 @@ import { WorkspaceSyncMetadataCommandsModule } from 'src/engine/workspace-manage
           [ObjectMetadataEntity, FieldMetadataEntity],
           'metadata',
         ),
-        TypeORMModule,
         WorkspaceSyncMetadataCommandsModule,
         WorkspaceMigrationRunnerModule,
         WorkspaceMigrationModule,
