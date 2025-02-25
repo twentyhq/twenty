0b03863e002fd2f70a7fb6b806e61c397943c682
Devessier <baptiste@devessier.fr>
2025-02-25 11:59:29 +0100

    chore: rollback refactoring

--- packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/components/WorkflowEditActionFormServerlessFunctionFields.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/components/WorkflowEditActionFormServerlessFunctionFields.tsx
@@ -5,64 +5,80 @@ import { InputLabel } from '@/ui/input/components/InputLabel';
 import { FunctionInput } from '@/workflow/workflow-steps/workflow-actions/types/FunctionInput';
 import styled from '@emotion/styled';
 import { isObject } from '@sniptt/guards';
-import path from 'path';
+import { ReactNode } from 'react';
 
 const StyledContainer = styled.div`
   display: inline-flex;
   flex-direction: column;
 `;
 
-type WorkflowEditActionFormServerlessFunctionFieldsProps = {
+export const WorkflowEditActionFormServerlessFunctionFields = ({
+  functionInput,
+  path = [],
+  VariablePicker,
+  onInputChange,
+  readonly = false,
+}: {
   functionInput: FunctionInput;
   path?: string[];
-} & (
-  | {
-      VariablePicker: VariablePickerComponent;
-      onInputChange: (value: any, path: string[]) => void;
-      readonly: false;
-    }
-  | {
-      VariablePicker?: never;
-      onInputChange?: never;
-      readonly: true;
-    }
-);
+  VariablePicker?: VariablePickerComponent;
+  onInputChange: (value: any, path: string[]) => void;
+  readonly?: boolean;
+}) => {
+  const renderFields = ({
+    functionInput,
+    path = [],
+    VariablePicker,
+    onInputChange,
+    readonly = false,
+  }: {
+    functionInput: FunctionInput;
+    path?: string[];
+    VariablePicker?: VariablePickerComponent;
+    onInputChange: (value: any, path: string[]) => void;
+    readonly?: boolean;
+  }): ReactNode[] => {
+    return Object.entries(functionInput).map(([inputKey, inputValue]) => {
+      const currentPath = [...path, inputKey];
+      const pathKey = currentPath.join('.');
+      if (inputValue !== null && isObject(inputValue)) {
+        return (
+          <StyledContainer key={pathKey}>
+            <InputLabel>{inputKey}</InputLabel>
+            <FormNestedFieldInputContainer>
+              {renderFields({
+                functionInput: inputValue,
+                path: currentPath,
+                VariablePicker,
+                onInputChange,
+              })}
+            </FormNestedFieldInputContainer>
+          </StyledContainer>
+        );
+      } else {
+        return (
+          <FormTextFieldInput
+            key={pathKey}
+            label={inputKey}
+            placeholder="Enter value"
+            defaultValue={inputValue ? `${inputValue}` : ''}
+            readonly={readonly}
+            onPersist={(value) => onInputChange(value, currentPath)}
+            VariablePicker={VariablePicker}
+          />
+        );
+      }
+    });
+  };
 
-export const WorkflowEditActionFormServerlessFunctionFields = (
-  props: WorkflowEditActionFormServerlessFunctionFieldsProps,
-) => {
   return (
     <>
-      {Object.entries(props.functionInput).map(([inputKey, inputValue]) => {
-        const currentPath = [...props.path, inputKey];
-        const pathKey = currentPath.join('.');
-        if (inputValue !== null && isObject(inputValue)) {
-          return (
-            <StyledContainer key={pathKey}>
-              <InputLabel>{inputKey}</InputLabel>
-              <FormNestedFieldInputContainer>
-                {renderFields({
-                  functionInput: inputValue,
-                  path: currentPath,
-                  VariablePicker,
-                  onInputChange,
-                })}
-              </FormNestedFieldInputContainer>
-            </StyledContainer>
-          );
-        } else {
-          return (
-            <FormTextFieldInput
-              key={pathKey}
-              label={inputKey}
-              placeholder="Enter value"
-              defaultValue={inputValue ? `${inputValue}` : ''}
-              readonly={props.readonly}
-              onPersist={(value) => onInputChange(value, currentPath)}
-              VariablePicker={VariablePicker}
-            />
-          );
-        }
+      {renderFields({
+        functionInput,
+        path,
+        VariablePicker,
+        onInputChange,
+        readonly,
       })}
     </>
   );
