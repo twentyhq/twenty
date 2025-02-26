078b9573cdcf474822adb9ae93c46f1a507ea14f
prastoin <paul@twenty.com>
2025-02-26 16:02:27 +0100

    test(front): refactor and fix

--- packages/twenty-front/src/modules/object-record/object-sort-dropdown/utils/__tests__/turnSortsIntoOrderBy.test.ts
+++ packages/twenty-front/src/modules/object-record/object-sort-dropdown/utils/__tests__/turnSortsIntoOrderBy.test.ts
@@ -1,11 +1,23 @@
 import { FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
+import { RecordGqlOperationOrderBy } from '@/object-record/graphql/types/RecordGqlOperationOrderBy';
 import { turnSortsIntoOrderBy } from '@/object-record/object-sort-dropdown/utils/turnSortsIntoOrderBy';
 import { RecordSort } from '@/object-record/record-sort/types/RecordSort';
+import { v4 } from 'uuid';
+import { FieldMetadataType } from '~/generated-metadata/graphql';
 
-const objectMetadataItem: ObjectMetadataItem = {
+const objectMetadataItemWithPosition: ObjectMetadataItem = {
   id: 'object1',
-  fields: [],
+  fields: [
+    {
+      name: 'name',
+      updatedAt: '2021-01-01',
+      createdAt: '2021-01-01',
+      id: '20202020-18b3-4099-86e3-c46b2d5d42f2',
+      type: FieldMetadataType.POSITION,
+      label: 'label',
+    },
+  ],
   indexMetadatas: [],
   createdAt: '2021-01-01',
   updatedAt: '2021-01-01',
@@ -22,81 +34,109 @@ const objectMetadataItem: ObjectMetadataItem = {
   isLabelSyncedWithName: true,
 };
 
+const getMockFieldMetadataItem = (
+  overrides: Partial<FieldMetadataItem>,
+): FieldMetadataItem => ({
+  name: 'name',
+  updatedAt: '2021-01-01',
+  createdAt: '2021-01-01',
+  id: `20202020-${v4().split('-').slice(1).join('-')}`,
+  type: FieldMetadataType.TEXT,
+  label: 'label',
+  ...overrides,
+});
+
 describe('turnSortsIntoOrderBy', () => {
-  it('should sort by recordPosition if no sorts', () => {
-    const fields = [{ id: 'field1', name: 'createdAt' }] as FieldMetadataItem[];
-    expect(turnSortsIntoOrderBy({ ...objectMetadataItem, fields }, [])).toEqual(
-      [
+  it.each<{
+    title: string;
+    fields: Partial<FieldMetadataItem>[];
+    expected: RecordGqlOperationOrderBy;
+    sort: RecordSort[];
+    objectMetadataItemOverrides?: Partial<Omit<ObjectMetadataItem, 'fields'>>;
+  }>([
+    {
+      title: 'It should sort by recordPosition if no sorts',
+      fields: [{ id: 'field1', name: 'field1' }],
+      sort: [],
+      expected: [
         {
           position: 'AscNullsFirst',
         },
       ],
+    },
+    {
+      title: 'It should create OrderByField with single sort',
+      fields: [{ id: 'field1', name: 'field1' }],
+      sort: [
+        {
+          id: 'id',
+          fieldMetadataId: 'field1',
+          direction: 'asc',
+        },
+      ],
+      expected: [{ field1: 'AscNullsFirst' }, { position: 'AscNullsFirst' }],
+    },
+    {
+      title: 'It should create OrderByField with multiple sorts',
+      fields: [
+        { id: 'field1', name: 'field1' },
+        { id: 'field2', name: 'field2' },
+      ],
+      sort: [
+        {
+          id: 'id',
+          fieldMetadataId: 'field1',
+          direction: 'asc',
+        },
+        {
+          id: 'id',
+          fieldMetadataId: 'field2',
+          direction: 'desc',
+        },
+      ],
+      expected: [
+        { field1: 'AscNullsFirst' },
+        { field2: 'DescNullsLast' },
+        { position: 'AscNullsFirst' },
+      ],
+    },
+    {
+      title: 'It should ignore if field not found',
+      fields: [],
+      sort: [
+        {
+          id: 'id',
+          fieldMetadataId: 'invalidField',
+          direction: 'asc',
+        },
+      ],
+      expected: [{ position: 'AscNullsFirst' }],
+    },
+    {
+      title: 'It should not return position for remotes',
+      fields: [],
+      sort: [
+        {
+          id: 'id',
+          fieldMetadataId: 'invalidField',
+          direction: 'asc',
+        },
+      ],
+      expected: [],
+      objectMetadataItemOverrides: {
+        isRemote: true,
+      },
+    },
+  ])('.$title', ({ fields, sort, expected, objectMetadataItemOverrides }) => {
+    const newFields = fields.map(getMockFieldMetadataItem);
+    const objectMetadataItemWithNewFields = {
+      ...objectMetadataItemWithPosition,
+      ...objectMetadataItemOverrides,
+      fields: [...objectMetadataItemWithPosition.fields, ...newFields],
+    };
+
+    expect(turnSortsIntoOrderBy(objectMetadataItemWithNewFields, sort)).toEqual(
+      expected,
     );
-  });
-
-  it('should create OrderByField with single sort', () => {
-    const sorts: RecordSort[] = [
-      {
-        id: 'id',
-        fieldMetadataId: 'field1',
-        direction: 'asc',
-      },
-    ];
-    const fields = [{ id: 'field1', name: 'field1' }] as FieldMetadataItem[];
-    expect(
-      turnSortsIntoOrderBy({ ...objectMetadataItem, fields }, sorts),
-    ).toEqual([{ field1: 'AscNullsFirst' }, { position: 'AscNullsFirst' }]);
-  });
-
-  it('should create OrderByField with multiple sorts', () => {
-    const sorts: RecordSort[] = [
-      {
-        id: 'id',
-        fieldMetadataId: 'field1',
-        direction: 'asc',
-      },
-      {
-        id: 'id',
-        fieldMetadataId: 'field2',
-        direction: 'desc',
-      },
-    ];
-    const fields = [
-      { id: 'field1', name: 'field1' },
-      { id: 'field2', name: 'field2' },
-    ] as FieldMetadataItem[];
-    expect(
-      turnSortsIntoOrderBy({ ...objectMetadataItem, fields }, sorts),
-    ).toEqual([
-      { field1: 'AscNullsFirst' },
-      { field2: 'DescNullsLast' },
-      { position: 'AscNullsFirst' },
-    ]);
-  });
-
-  it('should ignore if field not found', () => {
-    const sorts: RecordSort[] = [
-      {
-        id: 'id',
-        fieldMetadataId: 'invalidField',
-        direction: 'asc',
-      },
-    ];
-    expect(turnSortsIntoOrderBy(objectMetadataItem, sorts)).toEqual([
-      { position: 'AscNullsFirst' },
-    ]);
-  });
-
-  it('should not return position for remotes', () => {
-    const sorts: RecordSort[] = [
-      {
-        id: 'id',
-        fieldMetadataId: 'invalidField',
-        direction: 'asc',
-      },
-    ];
-    expect(
-      turnSortsIntoOrderBy({ ...objectMetadataItem, isRemote: true }, sorts),
-    ).toEqual([]);
   });
 });
