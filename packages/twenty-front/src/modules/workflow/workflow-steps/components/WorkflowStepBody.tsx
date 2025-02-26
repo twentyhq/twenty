c00582a70f13e58fac25f5d1e91f91fc08ff60d5
Devessier <baptiste@devessier.fr>
2025-02-26 12:44:54 +0100

    fix: ensure the background of the body of the actions is primary

--- packages/twenty-front/src/modules/workflow/workflow-steps/components/WorkflowStepBody.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-steps/components/WorkflowStepBody.tsx
@@ -1,13 +1,14 @@
 import styled from '@emotion/styled';
 
 const StyledWorkflowStepBody = styled.div`
+  background: ${({ theme }) => theme.background.primary};
   display: flex;
+  flex: 1 1 auto;
   flex-direction: column;
+  height: 100%;
   overflow-y: scroll;
   padding: ${({ theme }) => theme.spacing(4)};
   row-gap: ${({ theme }) => theme.spacing(6)};
-  flex: 1 1 auto;
-  height: 100%;
 `;
 
 export { StyledWorkflowStepBody as WorkflowStepBody };
