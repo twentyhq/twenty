a2fc1096d72dd27a6f3ba1da1139d1368bf84ae5
Devessier <baptiste@devessier.fr>
2025-02-26 17:19:29 +0100

    feat: parse the workflow run and return undefined if it's not valid

--- packages/twenty-front/src/modules/workflow/hooks/useWorkflowRunUnsafe.ts
+++ packages/twenty-front/src/modules/workflow/hooks/useWorkflowRunUnsafe.ts
@@ -1,23 +1,16 @@
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { useFindOneRecord } from '@/object-record/hooks/useFindOneRecord';
 import { WorkflowRun } from '@/workflow/types/Workflow';
-import { workflowRunSchema } from '@/workflow/validation-schemas/workflowSchema';
 
-export const useWorkflowRun = ({
+export const useWorkflowRunUnsafe = ({
   workflowRunId,
 }: {
   workflowRunId: string;
-}): WorkflowRun | undefined => {
-  const { record: rawRecord } = useFindOneRecord({
+}) => {
+  const { record } = useFindOneRecord<WorkflowRun>({
     objectNameSingular: CoreObjectNameSingular.WorkflowRun,
     objectRecordId: workflowRunId,
   });
-
-  const { success, data: record } = workflowRunSchema.safeParse(rawRecord);
-
-  if (!success) {
-    return undefined;
-  }
 
   return record;
 };
