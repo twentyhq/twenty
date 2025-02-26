87f3f775b09a01d51d01e9d2db217a5e386c226b
prastoin <paul@twenty.com>
2025-02-26 15:25:31 +0100

    lint(front): fix

--- packages/twenty-front/src/modules/object-metadata/utils/hasObjectMetadataItemPositionField.ts
+++ packages/twenty-front/src/modules/object-metadata/utils/hasObjectMetadataItemPositionField.ts
@@ -2,4 +2,6 @@ import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 
 export const hasObjectMetadataItemPositionField = (
   objectMetadataItem: ObjectMetadataItem,
-) => !objectMetadataItem.isRemote && objectMetadataItem.fields.some((field) => field.name === 'position');
+) =>
+  !objectMetadataItem.isRemote &&
+  objectMetadataItem.fields.some((field) => field.name === 'position');
