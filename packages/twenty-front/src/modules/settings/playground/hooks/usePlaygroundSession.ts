0626ebf25c7fb727d0c9960ec851a252f6b8d043
oliver <8559757+oliverqx@users.noreply.github.com>
2025-02-27 14:45:29 -0600

    feat: create playground hooks
    - useSetPlayground
    - usePlaygroundSession

---
+++ packages/twenty-front/src/modules/settings/playground/hooks/usePlaygroundSession.ts
@@ -0,0 +1,38 @@
+import { PlaygroundSchemas } from '@/settings/playground/types/PlaygroundTypes';
+import { PlaygroundSessionKeys } from '@/settings/playground/types/SessionTypes';
+import { PlaygroundSessionService } from '@/settings/playground/utils/playgroundSessionService';
+import { isDefined } from 'twenty-shared';
+
+export const PLAYGROUND_API_KEY = 'apiKeyForPlayground';
+export const PLAYGROUND_SCHEMA = 'apiSchemaForPlayground';
+
+type PlaygroundSessionValid = {
+  apiKey: string;
+  schema: PlaygroundSchemas;
+  isValid: true;
+};
+
+type PlaygroundSessionInvalid = {
+  apiKey: null;
+  schema: null;
+  isValid: false;
+};
+
+type PlaygroundSession = PlaygroundSessionValid | PlaygroundSessionInvalid;
+
+export const usePlaygroundSession = (): PlaygroundSession => {
+  const apiKey = PlaygroundSessionService.get(PlaygroundSessionKeys.API_KEY);
+  const schema = PlaygroundSessionService.get(PlaygroundSessionKeys.SCHEMA);
+
+  const isValid = isDefined(apiKey) && isDefined(schema);
+
+  if (isValid) {
+    return {
+      apiKey: apiKey as string,
+      schema: schema as PlaygroundSchemas,
+      isValid: true,
+    };
+  }
+
+  return { apiKey: null, schema: null, isValid: false };
+};
---
+++ packages/twenty-front/src/modules/settings/playground/hooks/useSetPlaygroundSession.ts
@@ -0,0 +1,15 @@
+import { PlaygroundSchemas } from '@/settings/playground/types/PlaygroundTypes';
+import { PlaygroundSessionKeys } from '@/settings/playground/types/SessionTypes';
+import { PlaygroundSessionService } from '@/settings/playground/utils/playgroundSessionService';
+
+export const useSetPlaygroundSession = () => {
+  const setSchema = (schema: PlaygroundSchemas) =>
+    PlaygroundSessionService.set(PlaygroundSessionKeys.SCHEMA, schema);
+  const setApiKey = (apiKey: string) =>
+    PlaygroundSessionService.set(PlaygroundSessionKeys.API_KEY, apiKey);
+
+  return {
+    setSchema,
+    setApiKey,
+  };
+};
