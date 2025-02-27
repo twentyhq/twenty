933a23ddbe563eb385771eb73c14b169ca5a4a36
Hamed Niroomand <hamed@niroomand.dev>
2025-02-27 19:59:18 +0330

    feat: add keyboard shortcut for text direction switching

--- packages/twenty-front/src/index.tsx
+++ packages/twenty-front/src/index.tsx
@@ -11,3 +11,14 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(<App />);
+
+document.addEventListener('keydown', (event) => {
+  console.log('event', event);
+  const target = event.target as HTMLElement;
+  const isTypeable = target.getAttribute('contenteditable') === 'true' || ['INPUT', 'TEXTAREA'].includes(target.tagName);
+  if (event.ctrlKey && event.shiftKey && event.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+    target.setAttribute('dir', 'rtl');
+  } else if (event.ctrlKey && event.shiftKey && event.location === KeyboardEvent.DOM_KEY_LOCATION_LEFT) {
+    target.setAttribute('dir', 'ltr');
+  }
+})
