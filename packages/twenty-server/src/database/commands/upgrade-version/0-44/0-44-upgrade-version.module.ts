281a648fa578da0e981e7b3b79b9f756e00f5519
Marie Stoppa <marie.stoppa@essec.edu>
2025-02-28 16:21:02 +0400

    export command to init permissions

--- packages/twenty-server/src/database/commands/upgrade-version/0-44/0-44-upgrade-version.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version/0-44/0-44-upgrade-version.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { MigrationCommandModule } from 'src/database/commands/migration-command/migration-command.module';
+import { InitializePermissionsCommand } from 'src/database/commands/upgrade-version/0-44/0-44-initialize-permissions.command';
 import { UserWorkspace } from 'src/engine/core-modules/user-workspace/user-workspace.entity';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { RoleModule } from 'src/engine/metadata-modules/role/role.module';
@@ -15,7 +16,7 @@ import { UserRoleModule } from 'src/engine/metadata-modules/user-role/user-role.
         TypeOrmModule.forFeature([Workspace, UserWorkspace], 'core'),
         UserRoleModule,
       ],
-      providers: [],
+      providers: [InitializePermissionsCommand],
     }),
   ],
 })
