eb053cce21bed4b86a2313c51a017ff0db885304
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-02-28 18:06:12 +0100

    - Implemented useSaveRecordFilterGroupsToViewFilterGroups - Removed useDeleteCombinedViewFilterGroup - Removed useUpsertCombinedViewFilterGroup - Removed useResetUnsavedViewStates - Removed useGetViewFilterGroupsCombined - Removed unsavedToDeleteViewFilterGroupIdsComponentFamilyState - Removed unsavedToUpsertViewFilterGroupsComponentFamilyState - Replaced useGetCurrentView by useGetCurrentViewOnly everywhere viewFilterGroup where used - Renamed viewFilterGroup by recordFilterGroup everywhere view isn't relevant

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/AdvancedFilterButton.tsx
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/AdvancedFilterButton.tsx
@@ -1,16 +1,17 @@
 import { useObjectMetadataItemById } from '@/object-metadata/hooks/useObjectMetadataItemById';
 import { availableFieldMetadataItemsForFilterFamilySelector } from '@/object-metadata/states/availableFieldMetadataItemsForFilterFamilySelector';
 import { getFilterTypeFromFieldType } from '@/object-metadata/utils/formatFieldMetadataItemsAsFilterDefinitions';
-import { useUpsertCombinedViewFilterGroup } from '@/object-record/advanced-filter/hooks/useUpsertCombinedViewFilterGroup';
 import { OBJECT_FILTER_DROPDOWN_ID } from '@/object-record/object-filter-dropdown/constants/ObjectFilterDropdownId';
 import { useUpsertRecordFilterGroup } from '@/object-record/record-filter-group/hooks/useUpsertRecordFilterGroup';
+import { currentRecordFilterGroupsComponentState } from '@/object-record/record-filter-group/states/currentRecordFilterGroupsComponentState';
 import { RecordFilterGroupLogicalOperator } from '@/object-record/record-filter-group/types/RecordFilterGroupLogicalOperator';
 import { useUpsertRecordFilter } from '@/object-record/record-filter/hooks/useUpsertRecordFilter';
 import { getRecordFilterOperands } from '@/object-record/record-filter/utils/getRecordFilterOperands';
 
 import { useDropdown } from '@/ui/layout/dropdown/hooks/useDropdown';
+import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { ADVANCED_FILTER_DROPDOWN_ID } from '@/views/constants/AdvancedFilterDropdownId';
-import { useGetCurrentView } from '@/views/hooks/useGetCurrentView';
+import { useGetCurrentViewOnly } from '@/views/hooks/useGetCurrentViewOnly';
 import { ViewFilterGroupLogicalOperator } from '@/views/types/ViewFilterGroupLogicalOperator';
 import styled from '@emotion/styled';
 import { useLingui } from '@lingui/react/macro';
@@ -56,16 +57,13 @@ export const AdvancedFilterButton = () => {
     OBJECT_FILTER_DROPDOWN_ID,
   );
 
-  const { currentViewId, currentViewWithCombinedFiltersAndSorts } =
-    useGetCurrentView();
+  const { currentView } = useGetCurrentViewOnly();
 
-  const { upsertCombinedViewFilterGroup } = useUpsertCombinedViewFilterGroup();
   const { upsertRecordFilterGroup } = useUpsertRecordFilterGroup();
 
   const { upsertRecordFilter } = useUpsertRecordFilter();
 
-  const objectMetadataId =
-    currentViewWithCombinedFiltersAndSorts?.objectMetadataId;
+  const objectMetadataId = currentView?.objectMetadataId;
 
   if (!objectMetadataId) {
     throw new Error('Object metadata id is missing from current view');
@@ -81,23 +79,23 @@ export const AdvancedFilterButton = () => {
     }),
   );
 
+  const currentRecordFilterGroups = useRecoilComponentValueV2(
+    currentRecordFilterGroupsComponentState,
+  );
+
   const handleClick = () => {
-    if (!currentViewId) {
+    if (!isDefined(currentView)) {
       throw new Error('Missing current view id');
     }
 
-    const alreadyHasAdvancedFilterGroup =
-      (currentViewWithCombinedFiltersAndSorts?.viewFilterGroups?.length ?? 0) >
-      0;
+    const alreadyHasAdvancedFilterGroup = currentRecordFilterGroups.length > 0;
 
     if (!alreadyHasAdvancedFilterGroup) {
       const newViewFilterGroup = {
         id: v4(),
-        viewId: currentViewId,
+        viewId: currentView.id,
         logicalOperator: ViewFilterGroupLogicalOperator.AND,
       };
-
-      upsertCombinedViewFilterGroup(newViewFilterGroup);
 
       upsertRecordFilterGroup({
         id: newViewFilterGroup.id,
@@ -129,7 +127,7 @@ export const AdvancedFilterButton = () => {
         operand: firstOperand,
         value: '',
         displayValue: '',
-        viewFilterGroupId: newViewFilterGroup.id,
+        recordFilterGroupId: newViewFilterGroup.id,
         type: getFilterTypeFromFieldType(defaultFieldMetadataItem.type),
         label: defaultFieldMetadataItem.label,
       });
