4d11891034952c16c105129b1e6f181a4944d454
Marie Stoppa <marie.stoppa@essec.edu>
2025-02-28 15:59:07 +0400

    wip

---
+++ packages/twenty-server/src/database/commands/upgrade-version/0-44/0-44-upgrade-version.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { MigrationCommandModule } from 'src/database/commands/migration-command/migration-command.module';
+import { UserWorkspace } from 'src/engine/core-modules/user-workspace/user-workspace.entity';
+import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import { RoleModule } from 'src/engine/metadata-modules/role/role.module';
+import { UserRoleModule } from 'src/engine/metadata-modules/user-role/user-role.module';
+
+@Module({
+  imports: [
+    MigrationCommandModule.register('0.44', {
+      imports: [
+        RoleModule,
+        TypeOrmModule.forFeature([Workspace, UserWorkspace], 'core'),
+        UserRoleModule,
+      ],
+      providers: [],
+    }),
+  ],
+})
+export class UpgradeTo0_44CommandModule {}
