0baae81162c80ed7a507a86f32ce187b6ba063a3
Devessier <baptiste@devessier.fr>
2025-03-03 18:34:17 +0100

    chore: setup keystatic for the developers content

--- packages/twenty-website/keystatic.config.ts
+++ packages/twenty-website/keystatic.config.ts
@@ -1,18 +1,30 @@
-import { config, fields, collection } from '@keystatic/core';
+import { collection, config, fields } from '@keystatic/core';
+import { wrapper } from '@keystatic/core/content-components';
 
 export default config({
   storage: {
     kind: 'local',
   },
   collections: {
-    posts: collection({
-      label: 'Posts',
+    developers: collection({
+      label: 'Technical documentation',
       slugField: 'title',
-      path: 'src/content/posts/*',
+      path: 'src/content/developers/**',
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({ label: 'Content' }),
+        icon: fields.text({ label: 'Icon' }),
+        image: fields.text({ label: 'Image' }),
+        info: fields.text({ label: 'Info' }),
+        content: fields.mdx({
+          label: 'Content',
+          components: {
+            ArticleEditContent: wrapper({
+              label: 'ArticleEditContent',
+              schema: {},
+            }),
+          },
+        }),
       },
     }),
   },
