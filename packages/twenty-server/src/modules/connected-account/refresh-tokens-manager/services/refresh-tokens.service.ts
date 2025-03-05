179043f76a1e7793c35d71fc4b3bc661cd69e419
guillim <guigloo@msn.com>
2025-03-05 14:35:26 +0100

    paul's usggestions

--- packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/services/refresh-tokens.service.ts
+++ packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/services/refresh-tokens.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
-import { assertUnreachable, ConnectedAccountProvider } from 'twenty-shared';
+import {
+  assertUnreachable,
+  ConnectedAccountProvider,
+  isDefined,
+} from 'twenty-shared';
 
 import { TwentyORMManager } from 'src/engine/twenty-orm/twenty-orm.manager';
 import { GoogleAPIRefreshAccessTokenService } from 'src/modules/connected-account/refresh-tokens-manager/drivers/google/services/google-api-refresh-access-token.service';
@@ -37,77 +41,69 @@ export class RefreshTokensService {
       );
     }
 
-    switch (connectedAccount.provider) {
-      case 'microsoft':
-      case 'google': {
-        let newAccessToken: string;
-        let newRefreshToken: string | undefined;
+    const connectedAccountToken = await this.refreshTokens(
+      connectedAccount,
+      refreshToken,
+      workspaceId,
+    );
 
-        try {
-          const tokenObject = await this.refreshTokens(
-            connectedAccount,
-            refreshToken,
-          );
-
-          newAccessToken = tokenObject.newAccessToken;
-          newRefreshToken = tokenObject.newRefreshToken;
-        } catch (error) {
-          throw new RefreshAccessTokenException(
-            `Error refreshing access token for connected account ${connectedAccount.id} in workspace ${workspaceId}: ${error.message} ${error?.response?.data?.error_description}`,
-            RefreshAccessTokenExceptionCode.REFRESH_ACCESS_TOKEN_FAILED,
-          );
-        }
-
-        const connectedAccountRepository =
-          await this.twentyORMManager.getRepository<ConnectedAccountWorkspaceEntity>(
-            'connectedAccount',
-          );
-
-        if (newRefreshToken) {
-          await connectedAccountRepository.update(
-            { id: connectedAccount.id },
-            {
-              accessToken: newAccessToken,
-              refreshToken: newRefreshToken,
-            },
-          );
-        } else {
-          await connectedAccountRepository.update(
-            { id: connectedAccount.id },
-            {
-              accessToken: newAccessToken,
-            },
-          );
-        }
-
-        return newAccessToken;
-      }
-      default:
-        return assertUnreachable(
-          connectedAccount.provider,
-          `Provider ${connectedAccount.provider} not supported for access token refresh`,
+    try {
+      const connectedAccountRepository =
+        await this.twentyORMManager.getRepository<ConnectedAccountWorkspaceEntity>(
+          'connectedAccount',
         );
+
+      if (isDefined(connectedAccountToken.newRefreshToken)) {
+        await connectedAccountRepository.update(
+          { id: connectedAccount.id },
+          {
+            accessToken: connectedAccountToken.newAccessToken,
+            refreshToken: connectedAccountToken.newRefreshToken,
+          },
+        );
+      } else {
+        await connectedAccountRepository.update(
+          { id: connectedAccount.id },
+          {
+            accessToken: connectedAccountToken.newAccessToken,
+          },
+        );
+      }
+    } catch (error) {
+      throw new Error(
+        `Error saving the new tokens for connected account ${connectedAccount.id} in workspace ${workspaceId}: ${error.message} `,
+      );
     }
+
+    return connectedAccountToken.newAccessToken;
   }
 
   async refreshTokens(
     connectedAccount: ConnectedAccountWorkspaceEntity,
     refreshToken: string,
+    workspaceId: string,
   ): Promise<NewTokens> {
-    switch (connectedAccount.provider) {
-      case ConnectedAccountProvider.GOOGLE:
-        return this.googleAPIRefreshAccessTokenService.refreshAccessToken(
-          refreshToken,
-        );
-      case ConnectedAccountProvider.MICROSOFT:
-        return this.microsoftAPIRefreshAccessTokenService.refreshTokens(
-          refreshToken,
-        );
-      default:
-        return assertUnreachable(
-          connectedAccount.provider,
-          `Provider ${connectedAccount.provider} not supported`,
-        );
+    try {
+      switch (connectedAccount.provider) {
+        case ConnectedAccountProvider.GOOGLE:
+          return this.googleAPIRefreshAccessTokenService.refreshAccessToken(
+            refreshToken,
+          );
+        case ConnectedAccountProvider.MICROSOFT:
+          return this.microsoftAPIRefreshAccessTokenService.refreshTokens(
+            refreshToken,
+          );
+        default:
+          return assertUnreachable(
+            connectedAccount.provider,
+            `Provider ${connectedAccount.provider} not supported`,
+          );
+      }
+    } catch (error) {
+      throw new RefreshAccessTokenException(
+        `Error refreshing tokens for connected account ${connectedAccount.id} in workspace ${workspaceId}: ${error.message} ${error?.response?.data?.error_description}`,
+        RefreshAccessTokenExceptionCode.REFRESH_ACCESS_TOKEN_FAILED,
+      );
     }
   }
 }
