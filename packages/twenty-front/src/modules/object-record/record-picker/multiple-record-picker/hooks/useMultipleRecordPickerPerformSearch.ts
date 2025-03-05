8e8bd9f764d26ce3dd7f672b6778ea946e20e11c
Charles Bochet <charles@twenty.com>
2025-03-05 11:09:27 +0100

    Fix

--- packages/twenty-front/src/modules/object-record/record-picker/multiple-record-picker/hooks/useMultipleRecordPickerPerformSearch.ts
+++ packages/twenty-front/src/modules/object-record/record-picker/multiple-record-picker/hooks/useMultipleRecordPickerPerformSearch.ts
@@ -1,19 +1,16 @@
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 import { CombinedFindManyRecordsQueryResult } from '@/object-record/multiple-objects/types/CombinedFindManyRecordsQueryResult';
 import { generateCombinedSearchRecordsQuery } from '@/object-record/multiple-objects/utils/generateCombinedSearchRecordsQuery';
+import { multipleRecordPickerPickableMorphItemsComponentState } from '@/object-record/record-picker/multiple-record-picker/states/multipleRecordPickerPickableMorphItemsComponentState';
 import { multipleRecordPickerSearchFilterComponentState } from '@/object-record/record-picker/multiple-record-picker/states/multipleRecordPickerSearchFilterComponentState';
 import { multipleRecordPickerSearchableObjectMetadataItemsComponentState } from '@/object-record/record-picker/multiple-record-picker/states/multipleRecordPickerSearchableObjectMetadataItemsComponentState';
+import { multipleRecordPickerformatQueryResultAsRecordsWithObjectMetadataId } from '@/object-record/record-picker/multiple-record-picker/utils/multipleRecordPickerformatQueryResultAsRecordWithObjectMetadataId';
+import { recordStoreFamilyState } from '@/object-record/record-store/states/recordStoreFamilyState';
 import { useApolloClient } from '@apollo/client';
 import { useRecoilCallback } from 'recoil';
 import { capitalize, isDefined } from 'twenty-shared';
 
 export const useMultipleRecordPickerPerformSearch = () => {
-  // const { objectRecordForSelectArray } =
-  //   useMultipleRecordPickerQueryResultFormattedAsObjectRecordForSelectArray({
-  //     multiObjectRecordsQueryResult:
-  //       matchesSearchFilterObjectRecordsQueryResult,
-  //   });
-
   const client = useApolloClient();
 
   const performSearch = useRecoilCallback(
@@ -77,10 +74,27 @@ export const useMultipleRecordPickerPerformSearch = () => {
             },
           });
 
-        console.log(
-          'combinedSearchRecordsQueryResult',
-          combinedSearchRecordsQueryResult,
+        const { recordsWithObjectMetadataId } =
+          multipleRecordPickerformatQueryResultAsRecordsWithObjectMetadataId({
+            objectMetadataItems: searchableObjectMetadataItems,
+            searchQueryResult: combinedSearchRecordsQueryResult,
+          });
+
+        set(
+          multipleRecordPickerPickableMorphItemsComponentState.atomFamily({
+            instanceId: multipleRecordPickerInstanceId,
+          }),
+          recordsWithObjectMetadataId.map(({ record, objectMetadataItem }) => ({
+            isMatchingSearchFilter: true,
+            isSelected: false,
+            objectMetadataId: objectMetadataItem.id,
+            recordId: record.id,
+          })),
         );
+
+        recordsWithObjectMetadataId.forEach(({ record }) => {
+          set(recordStoreFamilyState(record.id), record);
+        });
       },
     [client],
   );
