55a45c50cc0c2e4e1e83d22dee3c79cd8f5a73ca
Guillim <guillim@users.noreply.github.com>
2025-03-05 16:22:51 +0100

    microsoft sync failed (#10381)
    This PR is supposed to solve an issue with the syncrhonisation of
    messages, specifically with microsoft driver. Microsoft calls don't need
    access_Token so refreshing toekns was not implemented.
    
    However, microsoft rely on its client which calls its refresfh_token,
    and I might have missed some underlying dependency from microsoft
    impelemtation so I setup the access token process to refresh it
    
    Needs a talk before to be merged
    
    Fix : https://github.com/twentyhq/twenty/issues/10367
    
    EDIT:
    it was a problem with microsoft making refreshtoken expire (contrarily
    to google) which needs to be handled.

--- packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/exceptions/connected-account-refresh-tokens.exception.ts
+++ packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/exceptions/connected-account-refresh-tokens.exception.ts
@@ -1,12 +1,15 @@
 import { CustomException } from 'src/utils/custom-exception';
 
-export class RefreshAccessTokenException extends CustomException {
-  constructor(message: string, code: RefreshAccessTokenExceptionCode) {
+export class ConnectedAccountRefreshAccessTokenException extends CustomException {
+  constructor(
+    message: string,
+    code: ConnectedAccountRefreshAccessTokenExceptionCode,
+  ) {
     super(message, code);
   }
 }
 
-export enum RefreshAccessTokenExceptionCode {
+export enum ConnectedAccountRefreshAccessTokenExceptionCode {
   REFRESH_TOKEN_NOT_FOUND = 'REFRESH_TOKEN_NOT_FOUND',
   REFRESH_ACCESS_TOKEN_FAILED = 'REFRESH_ACCESS_TOKEN_FAILED',
   PROVIDER_NOT_SUPPORTED = 'PROVIDER_NOT_SUPPORTED',
