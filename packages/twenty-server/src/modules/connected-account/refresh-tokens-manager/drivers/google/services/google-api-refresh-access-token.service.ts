97baa00f0c5af2bd514e600244386b94675b2e3a
guillim <guigloo@msn.com>
2025-03-05 11:38:25 +0100

    paul comment on typing

--- packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/drivers/google/services/google-api-refresh-access-token.service.ts
+++ packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/drivers/google/services/google-api-refresh-access-token.service.ts
@@ -5,12 +5,19 @@ import axios from 'axios';
 import { EnvironmentService } from 'src/engine/core-modules/environment/environment.service';
 import { NewTokens } from 'src/modules/connected-account/refresh-tokens-manager/services/refresh-tokens.service';
 
+interface GoogleRefreshTokenResponse {
+  access_token: string;
+  id_token?: string;
+  token_type?: string;
+  expires_in?: number;
+  scope?: string;
+}
 @Injectable()
 export class GoogleAPIRefreshAccessTokenService {
   constructor(private readonly environmentService: EnvironmentService) {}
 
   async refreshAccessToken(refreshToken: string): Promise<NewTokens> {
-    const response = await axios.post(
+    const response = await axios.post<GoogleRefreshTokenResponse>(
       'https://oauth2.googleapis.com/token',
       {
         client_id: this.environmentService.get('AUTH_GOOGLE_CLIENT_ID'),
