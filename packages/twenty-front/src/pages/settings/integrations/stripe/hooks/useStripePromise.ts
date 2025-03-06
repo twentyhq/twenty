dc68a51b3a0caa3ed9cde2e7fee05135dba5cf68
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-03-06 14:20:57 -0300

    [log] FIX:  Log stripe key

--- packages/twenty-front/src/pages/settings/integrations/stripe/hooks/useStripePromise.ts
+++ packages/twenty-front/src/pages/settings/integrations/stripe/hooks/useStripePromise.ts
@@ -3,9 +3,9 @@ import { loadStripe } from '@stripe/stripe-js';
 export const useStripePromise = () => {
   const key = import.meta.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 
-  if (!key) throw new Error('Missing REACT_APP_STRIPE_PUBLISHABLE_KEY .env');
-
   console.log(key);
+
+  if (!key) throw new Error('Missing REACT_APP_STRIPE_PUBLISHABLE_KEY .env');
 
   const stripePromise = loadStripe(key);
 
