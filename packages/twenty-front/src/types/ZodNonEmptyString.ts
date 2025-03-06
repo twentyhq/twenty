5f43eecc04909f1c036d9fab57c7785bda40e5d4
prastoin <paul@twenty.com>
2025-03-06 12:32:50 +0100

    chore(front): remove min string translation which is not reactive

--- packages/twenty-front/src/types/ZodNonEmptyString.ts
+++ packages/twenty-front/src/types/ZodNonEmptyString.ts
@@ -1,6 +1,3 @@
-import { t } from '@lingui/core/macro';
 import { z } from 'zod';
 
-export const zodNonEmptyString = z
-  .string()
-  .min(1, t`String must contain at least 1 character`);
+export const zodNonEmptyString = z.string().min(1);
