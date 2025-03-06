c2cf373809d821790bbe2f74d6e981a1be6b6339
Charles Bochet <charles@twenty.com>
2025-03-06 08:31:33 +0100

    Fix hotkeyscope

--- packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellFieldContextWrapper.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellFieldContextWrapper.tsx
@@ -2,16 +2,20 @@ import { ReactNode, useContext } from 'react';
 
 import { isLabelIdentifierField } from '@/object-metadata/utils/isLabelIdentifierField';
 import { FieldContext } from '@/object-record/record-field/contexts/FieldContext';
+import { FieldMetadata } from '@/object-record/record-field/types/FieldMetadata';
 import { isFieldRelation } from '@/object-record/record-field/types/guards/isFieldRelation';
 import { isFieldSelect } from '@/object-record/record-field/types/guards/isFieldSelect';
 import { useRecordIndexContextOrThrow } from '@/object-record/record-index/contexts/RecordIndexContext';
+import { MultipleRecordPickerHotkeyScope } from '@/object-record/record-picker/multiple-record-picker/types/MultipleRecordPickerHotkeyScope';
 import { SingleRecordPickerHotkeyScope } from '@/object-record/record-picker/single-record-picker/types/SingleRecordPickerHotkeyScope';
 import { RecordUpdateContext } from '@/object-record/record-table/contexts/EntityUpdateMutationHookContext';
 import { RecordTableCellContext } from '@/object-record/record-table/contexts/RecordTableCellContext';
 import { useRecordTableContextOrThrow } from '@/object-record/record-table/contexts/RecordTableContext';
 import { useRecordTableRowContextOrThrow } from '@/object-record/record-table/contexts/RecordTableRowContext';
+import { ColumnDefinition } from '@/object-record/record-table/types/ColumnDefinition';
 import { TableHotkeyScope } from '@/object-record/record-table/types/TableHotkeyScope';
 import { SelectFieldHotkeyScope } from '@/object-record/select/types/SelectFieldHotkeyScope';
+import { RelationDefinitionType } from '~/generated-metadata/graphql';
 import { isUndefinedOrNull } from '~/utils/isUndefinedOrNull';
 
 export const RecordTableCellFieldContextWrapper = ({
@@ -33,11 +37,35 @@ export const RecordTableCellFieldContextWrapper = ({
     return null;
   }
 
-  const customHotkeyScope = isFieldRelation(columnDefinition)
-    ? SingleRecordPickerHotkeyScope.SingleRecordPicker
-    : isFieldSelect(columnDefinition)
-      ? SelectFieldHotkeyScope.SelectField
-      : TableHotkeyScope.CellEditMode;
+  const computedHotkeyScope = (
+    columnDefinition: ColumnDefinition<FieldMetadata>,
+  ) => {
+    if (isFieldRelation(columnDefinition)) {
+      if (
+        columnDefinition.metadata.relationType ===
+        RelationDefinitionType.MANY_TO_ONE
+      ) {
+        return SingleRecordPickerHotkeyScope.SingleRecordPicker;
+      }
+
+      if (
+        columnDefinition.metadata.relationType ===
+        RelationDefinitionType.ONE_TO_MANY
+      ) {
+        return MultipleRecordPickerHotkeyScope.MultipleRecordPicker;
+      }
+
+      return SingleRecordPickerHotkeyScope.SingleRecordPicker;
+    }
+
+    if (isFieldSelect(columnDefinition)) {
+      return SelectFieldHotkeyScope.SelectField;
+    }
+
+    return TableHotkeyScope.CellEditMode;
+  };
+
+  const customHotkeyScope = computedHotkeyScope(columnDefinition);
 
   return (
     <FieldContext.Provider
