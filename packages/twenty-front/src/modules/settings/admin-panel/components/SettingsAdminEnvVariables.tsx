ccfd7f6dd829e99123a1be4e671b35734b96dd5f
ehconitin <nitinkoche03@gmail.com>
2025-03-07 15:16:33 +0530

    WIP: environment variables banner and documentation link

--- packages/twenty-front/src/modules/settings/admin-panel/components/SettingsAdminEnvVariables.tsx
+++ packages/twenty-front/src/modules/settings/admin-panel/components/SettingsAdminEnvVariables.tsx
@@ -1,8 +1,18 @@
 import { SettingsAdminEnvVariablesTable } from '@/settings/admin-panel/components/SettingsAdminEnvVariablesTable';
 import { SettingsAdminTabSkeletonLoader } from '@/settings/admin-panel/components/SettingsAdminTabSkeletonLoader';
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useState } from 'react';
-import { Button, H1Title, H1TitleFontColor, H2Title, Section } from 'twenty-ui';
+import {
+  Banner,
+  Button,
+  H1Title,
+  H1TitleFontColor,
+  H2Title,
+  IconExternalLink,
+  IconInfoCircle,
+  Section,
+} from 'twenty-ui';
 import { useGetEnvironmentVariablesGroupedQuery } from '~/generated/graphql';
 
 const StyledGroupContainer = styled.div`
@@ -28,7 +38,28 @@ const StyledShowMoreButton = styled(Button)<{ isSelected?: boolean }>`
   `}
 `;
 
+const StyledBanner = styled(Banner)`
+  align-items: center;
+  background-color: ${({ theme }) => theme.color.blueAccent20};
+  border-radius: ${({ theme }) => theme.border.radius.md};
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(2)};
+  justify-content: space-between;
+  padding-right: ${({ theme }) => theme.spacing(1)};
+`;
+
+const StyledBannerContent = styled.div`
+  align-items: center;
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(2)};
+`;
+
+const StyledBannerText = styled.div`
+  color: ${({ theme }) => theme.color.blue};
+`;
+
 export const SettingsAdminEnvVariables = () => {
+  const theme = useTheme();
   const { data: environmentVariables, loading: environmentVariablesLoading } =
     useGetEnvironmentVariablesGroupedQuery({
       fetchPolicy: 'network-only',
@@ -62,9 +93,32 @@ export const SettingsAdminEnvVariables = () => {
   return (
     <>
       <Section>
-        These are only the server values. Ensure your worker environment has the
-        same variables and values, this is required for asynchronous tasks like
-        email sync.
+        <StyledBanner>
+          <StyledBannerContent>
+            <IconInfoCircle
+              color={theme.color.blue}
+              size={theme.icon.size.md}
+            />
+            <StyledBannerText>
+              Worker variables are not defined here
+            </StyledBannerText>
+          </StyledBannerContent>
+          <Button
+            variant="tertiary"
+            title="Go to Documentation"
+            onClick={() =>
+              window.open(
+                'https://twenty.com/developers/section/self-hosting/setup#setup-environment-variables',
+                '_blank',
+              )
+            }
+            size="small"
+            accent="blue"
+            Icon={IconExternalLink}
+          >
+            Go to Documentation
+          </Button>
+        </StyledBanner>
       </Section>
       <Section>
         {visibleGroups.map((group) => (
