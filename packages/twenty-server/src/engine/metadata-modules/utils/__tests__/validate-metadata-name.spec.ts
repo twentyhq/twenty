2e3d14d779b63e8c9c99f54cdff1ecc41cc38759
prastoin <paul@twenty.com>
2025-03-10 19:04:29 +0100

    test(server): use snapshot

--- packages/twenty-server/src/engine/metadata-modules/utils/__tests__/validate-metadata-name.spec.ts
+++ packages/twenty-server/src/engine/metadata-modules/utils/__tests__/validate-metadata-name.spec.ts
@@ -1,15 +1,10 @@
 import { EachTestingContext } from 'twenty-shared';
 
-import { InvalidStringException } from 'src/engine/metadata-modules/utils/exceptions/invalid-string.exception';
-import { NameIsReservedKeywordException } from 'src/engine/metadata-modules/utils/exceptions/name-is-reserved-keyword.exception';
-import { NameIsNotInCamelCase } from 'src/engine/metadata-modules/utils/exceptions/name-not-camel-case.exception';
-import { NameTooLongException } from 'src/engine/metadata-modules/utils/exceptions/name-too-long.exception';
-import { NameTooShortException } from 'src/engine/metadata-modules/utils/exceptions/name-too-short.exception';
 import { validateMetadataNameOrThrow } from 'src/engine/metadata-modules/utils/validate-metadata-name.utils';
 
 type ValidateMetadataNameTestContext = EachTestingContext<{
   input: string;
-  expectedError?: any;
+  shouldNotThrow?: true;
 }>;
 
 const validateMetadataNameTestCases: ValidateMetadataNameTestContext[] = [
@@ -17,80 +12,77 @@ const validateMetadataNameTestCases: ValidateMetadataNameTestContext[] = [
     title: 'validates when string is valid',
     context: {
       input: 'testName',
+      shouldNotThrow: true,
     },
   },
   {
     title: 'throw error when string is not in camel case',
     context: {
       input: 'TestName',
-      expectedError: NameIsNotInCamelCase,
     },
   },
   {
     title: 'throws error when string has spaces',
     context: {
       input: 'name with spaces',
-      expectedError: NameIsNotInCamelCase,
     },
   },
   {
     title: 'throws error when string is a reserved word',
     context: {
       input: 'role',
-      expectedError: NameIsReservedKeywordException,
     },
   },
   {
     title: 'throws error when string starts with capital letter',
     context: {
       input: 'StringStartingWithCapitalLetter',
-      expectedError: NameIsNotInCamelCase,
     },
   },
   {
     title: 'throws error when string has non latin characters',
     context: {
       input: 'בְרִבְרִ',
-      expectedError: InvalidStringException,
     },
   },
   {
     title: 'throws error when starts with digits',
     context: {
       input: '123string',
-      expectedError: NameIsNotInCamelCase,
     },
   },
   {
     title: 'validates when string is less than 63 characters',
     context: {
       input: 'a'.repeat(63),
+      shouldNotThrow: true,
     },
   },
   {
     title: 'throws error when string is above 63 characters',
     context: {
       input: 'a'.repeat(64),
-      expectedError: NameTooLongException,
     },
   },
   {
     title: 'throws error when string is empty',
     context: {
       input: '',
-      expectedError: NameTooShortException,
     },
   },
 ];
 
 describe('validateMetadataNameOrThrow', () => {
-  it.each(validateMetadataNameTestCases)('$title', ({ context }) => {
-    if (context.expectedError) {
-      expect(() => validateMetadataNameOrThrow(context.input)).toThrow(
-        context.expectedError,
-      );
-    } else {
-      expect(() => validateMetadataNameOrThrow(context.input)).not.toThrow();
-    }
-  });
+  it.each(validateMetadataNameTestCases)(
+    '$title',
+    ({ context: { input, shouldNotThrow } }) => {
+      if (shouldNotThrow) {
+        expect(() => validateMetadataNameOrThrow(input)).not.toThrow();
+      } else {
+        expect(() =>
+          validateMetadataNameOrThrow(input),
+        ).toThrowErrorMatchingSnapshot();
+      }
+    },
+  );
 });
