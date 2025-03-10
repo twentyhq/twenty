cceac16329866245979b480779e741b84bd5643b
prastoin <paul@twenty.com>
2025-03-10 15:44:58 +0100

    chore(server): move each testing context to twenty-shared

--- packages/twenty-shared/src/index.ts
+++ packages/twenty-shared/src/index.ts
@@ -1,3 +1,4 @@
+export * from './EachTestingContext'; //TMP as will be removed later when merging object metadata item server validation pr
 export * from './constants';
 export * from './i18n';
 export * from './types';
