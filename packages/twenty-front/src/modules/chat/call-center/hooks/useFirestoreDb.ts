24df37190eb7e7e80ebd12aad8454c1db67a2136
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-03-11 10:14:14 -0300

    [refactor] firestore environments
    Moved firestore environments to config, exposed firestore environments to window on server build for production

--- packages/twenty-front/src/modules/chat/call-center/hooks/useFirestoreDb.ts
+++ packages/twenty-front/src/modules/chat/call-center/hooks/useFirestoreDb.ts
@@ -1,14 +1,34 @@
 import { initializeApp } from 'firebase/app';
 import { Firestore, getFirestore } from 'firebase/firestore';
+import z from 'zod';
+import {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+} from '~/config';
+
+const firebaseCredentialsSchema = z.array(z.string()).nonempty().length(6);
 
 export const useFirestoreDb = (): { firestoreDb: Firestore } => {
+  firebaseCredentialsSchema.parse([
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+  ]);
+
   const app = initializeApp({
-    apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
   });
 
   return {
