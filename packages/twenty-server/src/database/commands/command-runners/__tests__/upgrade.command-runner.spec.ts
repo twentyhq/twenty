c29cc19c691dc93d504a66e65648625fef434e73
prastoin <paul@twenty.com>
2025-03-11 17:41:05 +0100

    test(server): wip refactor whole layout

--- packages/twenty-server/src/database/commands/command-runners/__tests__/upgrade.command-runner.spec.ts
+++ packages/twenty-server/src/database/commands/command-runners/__tests__/upgrade.command-runner.spec.ts
@@ -3,10 +3,10 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { SemVer } from 'semver';
 import { Repository } from 'typeorm';
 
+import { errors } from 'openid-client';
 import { EnvironmentVariables } from 'src/engine/core-modules/environment/environment-variables';
 import { EnvironmentService } from 'src/engine/core-modules/environment/environment.service';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
-import { WorkspaceDataSource } from 'src/engine/twenty-orm/datasource/workspace.datasource';
 import { WorkspaceRepository } from 'src/engine/twenty-orm/repository/workspace.repository';
 import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
 import { SyncWorkspaceMetadataCommand } from 'src/engine/workspace-manager/workspace-sync-metadata/commands/sync-workspace-metadata.command';
@@ -49,116 +49,176 @@ describe('fromVersion validation', () => {
   });
 });
 
+const genereateMockWorkspace = (overrides?: Partial<Workspace>) =>
+  ({
+    id: 'workspace-id',
+    version: '1.0.0',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    allowImpersonation: false,
+    isPublicInviteLinkEnabled: false,
+    displayName: 'Test Workspace',
+    domainName: 'test',
+    inviteHash: 'hash',
+    logo: null,
+    deletedAt: null,
+    activationStatus: 'active',
+    workspaceMembersCount: 1,
+    ...overrides,
+  }) as Workspace;
+
+type BuildUpgradeCommandModuleArgs = {
+  workspaces: Workspace[];
+  appVersion: string;
+};
+const buildUpgradeCommandModule = async ({
+  workspaces,
+  appVersion,
+}: BuildUpgradeCommandModuleArgs) => {
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [
+      TestUpgradeCommandRunner,
+      {
+        provide: getRepositoryToken(Workspace, 'core'),
+        useValue: {
+          findOneByOrFail: jest
+            .fn()
+            .mockImplementation((args) =>
+              workspaces.find((el) => el.id === args.id),
+            ),
+          update: jest.fn(),
+          find: jest.fn().mockResolvedValue(workspaces),
+        },
+      },
+      {
+        provide: EnvironmentService,
+        useValue: {
+          get: jest
+            .fn()
+            .mockImplementation((key: keyof EnvironmentVariables) => {
+              switch (key) {
+                case 'APP_VERSION': {
+                  return appVersion;
+                }
+                default: {
+                  return;
+                }
+              }
+            }),
+        },
+      },
+      {
+        provide: TwentyORMGlobalManager,
+        useValue: {
+          connect: jest.fn(),
+          destroyDataSourceForWorkspace: jest.fn(),
+          getDataSourceForWorkspace: jest.fn(),
+        },
+      },
+      {
+        provide: SyncWorkspaceMetadataCommand,
+        useValue: {
+          runOnWorkspace: jest.fn(),
+        },
+      },
+    ],
+  }).compile();
+
+  return module;
+};
+
 describe('UpgradeCommandRunner', () => {
   let upgradeCommandRunner: TestUpgradeCommandRunner;
   let workspaceRepository: Repository<Workspace>;
   let syncWorkspaceMetadataCommand: jest.Mocked<SyncWorkspaceMetadataCommand>;
+  let afterSyncMetadataUpgradeCommandsToRun: jest.SpyInstance; //TODO improve typing ?
+  let beforeSyncMetadataUpgradeCommandsToRun: jest.SpyInstance; //TODO improve typing ?
   let errors: Error[] = [];
 
-  const genereateMockWorkspace = (overrides?: Partial<Workspace>) =>
-    ({
-      id: 'workspace-id',
-      version: '1.0.0',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      allowImpersonation: false,
-      isPublicInviteLinkEnabled: false,
-      displayName: 'Test Workspace',
-      domainName: 'test',
-      inviteHash: 'hash',
-      logo: null,
-      deletedAt: null,
-      activationStatus: 'active',
-      workspaceMembersCount: 1,
-      ...overrides,
-    }) as Workspace;
+  type BuildModuleAndSetupSpiesArgs = {
+    numberOfWorkspace: number;
+    appVersion?: string;
+  };
+  const buildModuleAndSetupSpies = async ({
+    numberOfWorkspace,
+    appVersion = '1.0.0',
+  }: BuildModuleAndSetupSpiesArgs) => {
+    const workspaces = Array.from({ length: numberOfWorkspace }, (_v, index) =>
+      genereateMockWorkspace({ id: `workspace_${index}` }),
+    );
+    const module = await buildUpgradeCommandModule({
+      appVersion,
+      workspaces,
+    });
 
-  const findByOneMockWorkspace = genereateMockWorkspace();
-  const ALL_MOCKED_WORKSPACE: Workspace[] = Array.from(
-    { length: 42 },
-    (_v, index) => genereateMockWorkspace({ id: `workspace_${index}` }),
-  ).concat([findByOneMockWorkspace]);
-  const NUMBER_OF_MOCK_WORKSPACE = ALL_MOCKED_WORKSPACE.length;
-  const mockDataSource = {
-    // Add minimal required DataSource implementation
-  } as unknown as WorkspaceDataSource; // TODO ?
-
-  beforeEach(async () => {
     errors = [];
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        TestUpgradeCommandRunner,
-        {
-          provide: getRepositoryToken(Workspace, 'core'),
-          useValue: {
-            findOneByOrFail: jest.fn().mockImplementation((args) => {
-              console.log({ args });
-              return findByOneMockWorkspace;
-            }),
-            update: jest.fn(),
-            find: jest.fn().mockResolvedValue(ALL_MOCKED_WORKSPACE),
-          },
-        },
-        {
-          provide: EnvironmentService,
-          useValue: {
-            get: jest
-              .fn()
-              .mockImplementation((key: keyof EnvironmentVariables) => {
-                switch (key) {
-                  case 'APP_VERSION': {
-                    return '1.0.0';
-                  }
-                  default: {
-                    return;
-                  }
-                }
-              }),
-          },
-        },
-        {
-          provide: TwentyORMGlobalManager,
-          useValue: {
-            connect: jest.fn(),
-            destroyDataSourceForWorkspace: jest.fn(),
-            getDataSourceForWorkspace: jest.fn(),
-          },
-        },
-        {
-          provide: SyncWorkspaceMetadataCommand,
-          useValue: {
-            runOnWorkspace: jest.fn(),
-          },
-        },
-      ],
-    }).compile();
-
     upgradeCommandRunner = module.get<TestUpgradeCommandRunner>(
       TestUpgradeCommandRunner,
     );
-    jest.spyOn(upgradeCommandRunner, 'beforeSyncMetadataUpgradeCommandsToRun');
-    jest.spyOn(upgradeCommandRunner, 'afterSyncMetadataUpgradeCommandsToRun');
+    beforeSyncMetadataUpgradeCommandsToRun = jest.spyOn(
+      upgradeCommandRunner,
+      'beforeSyncMetadataUpgradeCommandsToRun',
+    );
+    afterSyncMetadataUpgradeCommandsToRun = jest.spyOn(
+      upgradeCommandRunner,
+      'afterSyncMetadataUpgradeCommandsToRun',
+    );
     jest.spyOn(upgradeCommandRunner, 'runOnWorkspace');
 
     workspaceRepository = module.get<Repository<Workspace>>(
       getRepositoryToken(Workspace, 'core'),
     );
     syncWorkspaceMetadataCommand = module.get(SyncWorkspaceMetadataCommand);
+  };
+
+  it('should run upgrade over all workspaces', async () => {
+    const numberOfWorkspace = 42;
+    await buildModuleAndSetupSpies({
+      numberOfWorkspace,
+    });
+    const passedParams = [];
+    const options = {};
+    await upgradeCommandRunner.run(passedParams, options);
+
+    [
+      upgradeCommandRunner.runOnWorkspace,
+      upgradeCommandRunner.beforeSyncMetadataUpgradeCommandsToRun,
+      upgradeCommandRunner.afterSyncMetadataUpgradeCommandsToRun,
+      syncWorkspaceMetadataCommand.runOnWorkspace,
+      workspaceRepository.update,
+    ].forEach((fn) => expect(fn).toHaveBeenCalledTimes(numberOfWorkspace));
   });
 
-  describe('runOnWorkspace', () => {
-    const runArgs = {
-      appVersion: '1.1.0',
-      workspaceId: 'workspace-id',
-      index: 0,
-      total: 1,
-      options: {
-        dryRun: false,
-        workspaceIds: ['workspace-id'],
-      },
-      dataSource: mockDataSource,
-    };
+  it('should run syncMetadataCommand betwen beforeSyncMetadataUpgradeCommandsToRun and afterSyncMetadataUpgradeCommandsToRun', async () => {
+    await buildModuleAndSetupSpies({
+      numberOfWorkspace: 1,
+    });
+    const passedParams = [];
+    const options = {};
+    await upgradeCommandRunner.run(passedParams, options);
 
+    [
+      upgradeCommandRunner.runOnWorkspace,
+      upgradeCommandRunner.beforeSyncMetadataUpgradeCommandsToRun,
+      upgradeCommandRunner.afterSyncMetadataUpgradeCommandsToRun,
+      syncWorkspaceMetadataCommand.runOnWorkspace,
+    ].forEach((fn) => expect(fn).toHaveBeenCalledTimes(1));
+
+    // Verify order of execution
+    const beforeSyncCall =
+      beforeSyncMetadataUpgradeCommandsToRun.mock.invocationCallOrder[0];
+    const afterSyncCall =
+      afterSyncMetadataUpgradeCommandsToRun.mock.invocationCallOrder[0];
+    const syncMetadataCall =
+      syncWorkspaceMetadataCommand.runOnWorkspace.mock.invocationCallOrder[0];
+
+    expect(beforeSyncCall).toBeLessThan(syncMetadataCall);
+    expect(syncMetadataCall).toBeLessThan(afterSyncCall);
+  });
+});
+
+describe('WIP_UpgradeCommandRunner', () => {
+  describe('runOnWorkspace', () => {
     it('should run sync metadata between commands', async () => {
       const beforeSyncSpy = jest.spyOn(
         upgradeCommandRunner,
@@ -221,30 +281,11 @@ describe('UpgradeCommandRunner', () => {
 
     it('should fail if app version is not defined', async () => {
       await expect(
-        upgradeCommandRunner.runOnWorkspace({
+        upgradeCommandRunner.run({
           ...runArgs,
           appVersion: undefined,
         }),
       ).rejects.toThrow('Should never occur, APP_VERSION_NOT_DEFINED');
-    });
-
-    it('should update workspace version after successful upgrade', async () => {
-      const passedParams = [];
-      const options = {};
-      await upgradeCommandRunner.run(passedParams, options);
-
-      [
-        upgradeCommandRunner.runOnWorkspace,
-        upgradeCommandRunner.beforeSyncMetadataUpgradeCommandsToRun,
-        upgradeCommandRunner.afterSyncMetadataUpgradeCommandsToRun,
-        syncWorkspaceMetadataCommand.runOnWorkspace,
-      ].forEach((fn) =>
-        expect(fn).toHaveBeenCalledTimes(NUMBER_OF_MOCK_WORKSPACE),
-      );
-
-      expect(workspaceRepository.update).toHaveBeenCalledTimes(
-        NUMBER_OF_MOCK_WORKSPACE,
-      );
     });
   });
 
