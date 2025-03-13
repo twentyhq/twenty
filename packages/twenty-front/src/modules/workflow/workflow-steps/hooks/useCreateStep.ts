ced7c747038b9a2f98508c80e473a97092918cb3
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-03-13 14:22:20 -0300

    Post twenty sync fix: * Regenerated graphql types; * Fix incorrect import; * Incresed buld node memory size to prevent OOM Error;

--- packages/twenty-front/src/modules/workflow/workflow-steps/hooks/useCreateStep.ts
+++ packages/twenty-front/src/modules/workflow/workflow-steps/hooks/useCreateStep.ts
@@ -1,5 +1,3 @@
-import { useRightDrawer } from '@/ui/layout/right-drawer/hooks/useRightDrawer';
-import { RightDrawerPages } from '@/ui/layout/right-drawer/types/RightDrawerPages';
 import { useGetUpdatableWorkflowVersion } from '@/workflow/hooks/useGetUpdatableWorkflowVersion';
 import { workflowLastCreatedStepIdState } from '@/workflow/states/workflowLastCreatedStepIdState';
 import {
@@ -7,7 +5,6 @@ import {
   WorkflowWithCurrentVersion,
 } from '@/workflow/types/Workflow';
 import { workflowSelectedNodeState } from '@/workflow/workflow-diagram/states/workflowSelectedNodeState';
-import { getWorkflowNodeIcon } from '@/workflow/workflow-diagram/utils/getWorkflowNodeIcon';
 import { useCreateWorkflowVersionStep } from '@/workflow/workflow-steps/hooks/useCreateWorkflowVersionStep';
 import { workflowCreateStepFromParentStepIdState } from '@/workflow/workflow-steps/states/workflowCreateStepFromParentStepIdState';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
@@ -30,8 +27,6 @@ export const useCreateStep = ({
 
   const { getUpdatableWorkflowVersion } = useGetUpdatableWorkflowVersion();
 
-  const { openRightDrawer } = useRightDrawer();
-
   const createStep = async (newStepType: WorkflowStepType) => {
     if (!isDefined(workflowCreateStepFromParentStepId)) {
       throw new Error('Select a step to create a new step from first.');
@@ -52,14 +47,6 @@ export const useCreateStep = ({
 
     setWorkflowSelectedNode(createdStep.id);
     setWorkflowLastCreatedStepId(createdStep.id);
-
-    openRightDrawer(RightDrawerPages.WorkflowStepEdit, {
-      title: createdStep.name,
-      Icon: getWorkflowNodeIcon({
-        nodeType: 'action',
-        actionType: createdStep.type as WorkflowStepType,
-      }),
-    });
   };
 
   return {
