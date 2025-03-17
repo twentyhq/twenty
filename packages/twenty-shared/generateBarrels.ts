bb51f842dfcb44b74b0ed4f5a96c59da6b34665c
prastoin <paul@twenty.com>
2025-03-17 17:03:42 +0100

    refactor(shared): generate barrel updates package.json

--- packages/twenty-shared/generateBarrels.ts
+++ packages/twenty-shared/generateBarrels.ts
@@ -1,9 +1,8 @@
 import * as fs from 'fs';
 import path from 'path';
 import slash from 'slash';
-
-const extensions = ['.ts', '.tsx'];
-const excludedExtensions = [
+const INCLUDED_EXTENSIONS = ['.ts', '.tsx'];
+const EXCLUDED_EXTENSIONS = [
   '.test.ts',
   '.test.tsx',
   '.spec.ts',
@@ -11,14 +10,17 @@ const excludedExtensions = [
   '.stories.ts',
   '.stories.tsx',
 ];
-const excludedDirectories = [
+const EXCLUDED_DIRECTORIES = [
   '__tests__',
   '__mocks__',
   '__stories__',
   'internal',
 ];
+const INDEX_FILENAME = 'index.ts';
+const PACKAGE_JSON = 'package.json';
 // TODO refactor to be config ?
-const srcPath = path.resolve('packages/twenty-shared/src');
+const PACKAGE_PATH = path.resolve('packages/twenty-shared');
+const SRC_PATH = path.resolve(`${PACKAGE_PATH}/src`);
 
 type createTypeScriptFileArgs = {
   path: string;
@@ -28,7 +30,11 @@ const createTypeScriptIndexFile = ({
   content,
   path: filePath,
 }: createTypeScriptFileArgs) =>
-  fs.writeFileSync(path.join(filePath, 'index.ts'), `${content}\n`, 'utf-8');
+  fs.writeFileSync(
+    path.join(filePath, INDEX_FILENAME),
+    `${content}\n`,
+    'utf-8',
+  );
 
 const getSubDirectoryPaths = (directoryPath: string): string[] =>
   fs
@@ -42,7 +48,7 @@ const getSubDirectoryPaths = (directoryPath: string): string[] =>
       }
 
       const isExcludedDirectory =
-        excludedDirectories.includes(fileOrDirectoryName);
+        EXCLUDED_DIRECTORIES.includes(fileOrDirectoryName);
       return !isExcludedDirectory;
     })
     .map((subDirectoryName) => path.join(directoryPath, subDirectoryName));
@@ -59,63 +65,109 @@ const getFilesPaths = (directoryPath: string): string[] =>
       return false;
     }
 
-    const isIndexFile = filePath.startsWith('index.');
+    const isIndexFile = filePath === INDEX_FILENAME; // Should be starts with ?
     if (isIndexFile) {
       return false;
     }
 
-    const isWhiteListedExtension = extensions.some((extension) =>
+    const isWhiteListedExtension = INCLUDED_EXTENSIONS.some((extension) =>
       filePath.endsWith(extension),
     );
-    const isExcludedExtension = excludedExtensions.every(
+    const isExcludedExtension = EXCLUDED_EXTENSIONS.every(
       (excludedExtension) => !filePath.endsWith(excludedExtension),
     );
     return isWhiteListedExtension && isExcludedExtension;
   });
 
+const generateModuleIndexFiles = (moduleDirectories: string[]) => {
+  return moduleDirectories.map<createTypeScriptFileArgs>(
+    (moduleDirectoryPath) => {
+      const directoryPaths = getDirectoryPathsRecursive(moduleDirectoryPath);
+      const content = directoryPaths
+        .flatMap((directoryPath) => {
+          const directFilesPaths = getFilesPaths(directoryPath);
+
+          return directFilesPaths.map((filePath) => {
+            const fileNameWithoutExtension = filePath
+              .split('.')
+              .slice(0, -1)
+              .join('.');
+            const pathToImport = slash(
+              path.relative(
+                moduleDirectoryPath,
+                path.join(directoryPath, fileNameWithoutExtension),
+              ),
+            );
+            return `export * from './${pathToImport}';`;
+          });
+        })
+        .sort((a, b) => a.localeCompare(b))
+        .join('\n');
+
+      return {
+        content,
+        path: moduleDirectoryPath,
+      };
+    },
+  );
+};
+
+type ExportOccurence = {
+  types: string;
+  import: string;
+  require: string;
+};
+type ExportsConfig = Record<string, ExportOccurence>;
+const generateModulePackageExports = (moduleDirectories: string[]) => {
+  return moduleDirectories.reduce<ExportsConfig>((acc, moduleDirectory) => {
+    const moduleName = moduleDirectory.split('/').pop();
+    if (moduleName === undefined) {
+      throw new Error(
+        `Should never occur, moduleName is undefined ${moduleDirectory}`,
+      );
+    }
+
+    return {
+      ...acc,
+      [moduleName]: {
+        types: `./dist/${moduleName}.d.ts`,
+        import: `./dist/${moduleName}.mjs`,
+        require: `./dist/${moduleName}.js`,
+      },
+    };
+  }, {});
+};
+
+const readPackageJson = (): Record<string, unknown> => {
+  const rawPackageJson = fs.readFileSync(
+    path.join(PACKAGE_PATH, PACKAGE_JSON),
+    'utf-8',
+  );
+  return JSON.parse(rawPackageJson);
+};
+
+const writePackageJson = (packageJson: unknown) =>
+  fs.writeFileSync(
+    path.join(PACKAGE_PATH, PACKAGE_JSON),
+    JSON.stringify(packageJson),
+    'utf-8',
+  );
+
+const writeExportsInPackageJson = (exports: ExportsConfig) => {
+  const initialPackage = readPackageJson();
+  const updatedPackage = {
+    ...initialPackage,
+    exports,
+  };
+  writePackageJson(updatedPackage);
+};
+
 const main = () => {
-  const moduleDirectories = getSubDirectoryPaths(srcPath);
+  const moduleDirectories = getSubDirectoryPaths(SRC_PATH);
+  const moduleIndexFiles = generateModuleIndexFiles(moduleDirectories);
+  const modulePackageExports = generateModulePackageExports(moduleDirectories);
 
-  moduleDirectories.forEach((moduleDirectoryPath) => {
-    const directoryPaths = getDirectoryPathsRecursive(moduleDirectoryPath);
-    const moduleExports = directoryPaths
-      .flatMap((directoryPath) => {
-        const directFilesPaths = getFilesPaths(directoryPath);
-
-        return directFilesPaths.map((filePath) => {
-          const fileNameWithoutExtension = filePath
-            .split('.')
-            .slice(0, -1)
-            .join('.');
-          const pathToImport = slash(
-            path.relative(
-              moduleDirectoryPath,
-              path.join(directoryPath, fileNameWithoutExtension),
-            ),
-          );
-          return `export * from './${pathToImport}';`;
-        });
-      })
-      .sort((a, b) => a.localeCompare(b))
-      .join('\n');
-
-    createTypeScriptIndexFile({
-      content: moduleExports,
-      path: moduleDirectoryPath,
-    });
-  });
-
-  const mainBarrelExports = moduleDirectories
-    .map(
-      (moduleDirectoryPath) =>
-        `export * from './${slash(path.relative(srcPath, moduleDirectoryPath))}';`,
-    )
-    .sort((a, b) => a.localeCompare(b))
-    .join('\n');
-
-  createTypeScriptIndexFile({
-    content: mainBarrelExports,
-    path: srcPath,
-  });
+  writeExportsInPackageJson(modulePackageExports);
+  moduleIndexFiles.forEach(createTypeScriptIndexFile);
 };
 main();
