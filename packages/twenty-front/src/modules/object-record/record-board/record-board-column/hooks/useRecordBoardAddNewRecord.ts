ad3f01c84c37f195ab378a7ed2ebe2382a899981
Charles Bochet <charles@twenty.com>
2025-03-18 18:12:05 +0100

    Fix

--- packages/twenty-front/src/modules/object-record/record-board/record-board-column/hooks/useRecordBoardAddNewRecord.ts
+++ packages/twenty-front/src/modules/object-record/record-board/record-board-column/hooks/useRecordBoardAddNewRecord.ts
@@ -1,18 +1,68 @@
+import { useOpenRecordInCommandMenu } from '@/command-menu/hooks/useOpenRecordInCommandMenu';
 import { RecordBoardContext } from '@/object-record/record-board/contexts/RecordBoardContext';
 import { RecordBoardColumnContext } from '@/object-record/record-board/record-board-column/contexts/RecordBoardColumnContext';
+import { recordIndexOpenRecordInState } from '@/object-record/record-index/states/recordIndexOpenRecordInState';
+import { useRecordTitleCell } from '@/object-record/record-title-cell/hooks/useRecordTitleCell';
+import { AppPath } from '@/types/AppPath';
+import { ViewOpenRecordInType } from '@/views/types/ViewOpenRecordInType';
 import { useContext } from 'react';
+import { useRecoilCallback } from 'recoil';
+import { v4 } from 'uuid';
+import { useNavigateApp } from '~/hooks/useNavigateApp';
 
 export const useRecordBoardAddNewRecord = () => {
   const columnContext = useContext(RecordBoardColumnContext);
-  const { createOneRecord, selectFieldMetadataItem } =
+  const { createOneRecord, selectFieldMetadataItem, objectMetadataItem } =
     useContext(RecordBoardContext);
 
-  const createNewBoardRecord = (position: 'first' | 'last') => {
-    createOneRecord({
-      [selectFieldMetadataItem.name]: columnContext?.columnDefinition.value,
-      position,
-    });
-  };
+  const { openRecordInCommandMenu } = useOpenRecordInCommandMenu();
+  const { openRecordTitleCell } = useRecordTitleCell();
+  const navigate = useNavigateApp();
+
+  const createNewBoardRecord = useRecoilCallback(
+    ({ snapshot }) =>
+      async (position: 'first' | 'last' | number) => {
+        const recordId = v4();
+
+        const recordIndexOpenRecordIn = snapshot
+          .getLoadable(recordIndexOpenRecordInState)
+          .getValue();
+
+        await createOneRecord({
+          id: recordId,
+          [selectFieldMetadataItem.name]: columnContext?.columnDefinition.value,
+          position: position,
+        });
+
+        if (recordIndexOpenRecordIn === ViewOpenRecordInType.SIDE_PANEL) {
+          openRecordInCommandMenu({
+            recordId,
+            objectNameSingular: objectMetadataItem.nameSingular,
+            isNewRecord: true,
+          });
+
+          openRecordTitleCell({
+            recordId,
+            fieldMetadataId: objectMetadataItem.labelIdentifierFieldMetadataId,
+          });
+        } else {
+          navigate(AppPath.RecordShowPage, {
+            objectNameSingular: objectMetadataItem.nameSingular,
+            objectRecordId: recordId,
+          });
+        }
+      },
+    [
+      columnContext?.columnDefinition.value,
+      createOneRecord,
+      navigate,
+      objectMetadataItem.labelIdentifierFieldMetadataId,
+      objectMetadataItem.nameSingular,
+      openRecordInCommandMenu,
+      openRecordTitleCell,
+      selectFieldMetadataItem.name,
+    ],
+  );
 
   return {
     createNewBoardRecord,
