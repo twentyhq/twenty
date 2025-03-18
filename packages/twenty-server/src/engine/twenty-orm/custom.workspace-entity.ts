1b2b2e548937e96cbe6e276f2faba78834a38ae6
martmull <martmull@hotmail.fr>
2025-03-18 14:41:18 +0100

    Add updatedBy field to objects

--- packages/twenty-server/src/engine/twenty-orm/custom.workspace-entity.ts
+++ packages/twenty-server/src/engine/twenty-orm/custom.workspace-entity.ts
@@ -64,6 +64,15 @@ export class CustomWorkspaceEntity extends BaseWorkspaceEntity {
   })
   createdBy: ActorMetadata;
 
+  @WorkspaceField({
+    standardId: CUSTOM_OBJECT_STANDARD_FIELD_IDS.updatedBy,
+    type: FieldMetadataType.ACTOR,
+    label: msg`Updated by`,
+    icon: 'IconCreativeCommonsSa',
+    description: msg`The updater of the record`,
+  })
+  updatedBy: ActorMetadata;
+
   @WorkspaceRelation({
     standardId: CUSTOM_OBJECT_STANDARD_FIELD_IDS.noteTargets,
     label: msg`Notes`,
