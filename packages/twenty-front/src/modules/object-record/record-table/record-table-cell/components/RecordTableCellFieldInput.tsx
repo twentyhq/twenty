841d48b704666ebc7c997519f6d55b45b7083630
Guillim <guillim@users.noreply.github.com>
2025-03-18 19:34:50 +0100

    Creating records in side panel not working (#10999)
    Fixes https://github.com/twentyhq/core-team-issues/issues/624
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellFieldInput.tsx
@@ -5,10 +5,7 @@ import { FieldContext } from '@/object-record/record-field/contexts/FieldContext
 import { useIsFieldValueReadOnly } from '@/object-record/record-field/hooks/useIsFieldValueReadOnly';
 import { FieldInputEvent } from '@/object-record/record-field/types/FieldInputEvent';
 import { useRecordTableBodyContextOrThrow } from '@/object-record/record-table/contexts/RecordTableBodyContext';
-import { getDropdownFocusIdForRecordField } from '@/object-record/utils/getDropdownFocusIdForRecordField';
 import { getRecordFieldInputId } from '@/object-record/utils/getRecordFieldInputId';
-import { activeDropdownFocusIdState } from '@/ui/layout/dropdown/states/activeDropdownFocusIdState';
-import { useRecoilCallback } from 'recoil';
 
 export const RecordTableCellFieldInput = () => {
   const { recordId, fieldDefinition } = useContext(FieldContext);
@@ -43,35 +40,20 @@ export const RecordTableCellFieldInput = () => {
     onCloseTableCell();
   };
 
-  const handleClickOutside = useRecoilCallback(
-    ({ snapshot }) =>
-      (persistField: () => void, event: MouseEvent | TouchEvent) => {
-        const dropdownFocusId = getDropdownFocusIdForRecordField(
-          recordId,
-          fieldDefinition.fieldMetadataId,
-          'table-cell',
-        );
+  const handleClickOutside = (
+    persistField: () => void,
+    event: MouseEvent | TouchEvent,
+  ) => {
+    event.stopImmediatePropagation();
 
-        const activeDropdownFocusId = snapshot
-          .getLoadable(activeDropdownFocusIdState)
-          .getValue();
+    onUpsertRecord({
+      persistField,
+      recordId,
+      fieldName: fieldDefinition.metadata.fieldName,
+    });
 
-        if (activeDropdownFocusId !== dropdownFocusId) {
-          return;
-        }
-
-        event.stopImmediatePropagation();
-
-        onUpsertRecord({
-          persistField,
-          recordId,
-          fieldName: fieldDefinition.metadata.fieldName,
-        });
-
-        onCloseTableCell();
-      },
-    [fieldDefinition, onCloseTableCell, onUpsertRecord, recordId],
-  );
+    onCloseTableCell();
+  };
 
   const handleEscape: FieldInputEvent = (persistField) => {
     onUpsertRecord({
