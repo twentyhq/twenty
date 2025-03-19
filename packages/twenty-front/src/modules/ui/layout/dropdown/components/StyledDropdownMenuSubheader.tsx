d99d2845cbb652379ace389e03968587d3e81bc2
Félix Malfait <felix@twenty.com>
2025-03-19 12:58:28 +0100

    More cleanup

--- packages/twenty-front/src/modules/ui/layout/dropdown/components/StyledDropdownMenuSubheader.tsx
+++ packages/twenty-front/src/modules/ui/layout/dropdown/components/StyledDropdownMenuSubheader.tsx
@@ -6,8 +6,3 @@ export const StyledDropdownMenuSubheader = styled(Label)`
   padding: ${({ theme }) => `${theme.spacing(1)} ${theme.spacing(2)}`};
   width: 100%;
 `;
-
-export const DropdownMenuSubheader = (
-  props: React.ComponentProps<typeof StyledDropdownMenuSubheader>,
-  // eslint-disable-next-line react/jsx-props-no-spreading
-) => <StyledDropdownMenuSubheader variant="small" {...props} />;
