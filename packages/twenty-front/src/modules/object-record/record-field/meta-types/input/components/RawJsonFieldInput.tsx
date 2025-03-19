02bd45a0683566ea0f616733553d09075a3e37c6
Devessier <baptiste@devessier.fr>
2025-03-19 18:14:07 +0100

    feat: start integrating the json tree component and monaco for raw json field input

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/RawJsonFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/input/components/RawJsonFieldInput.tsx
@@ -1,9 +1,16 @@
-import { TextAreaInput } from '@/ui/field/input/components/TextAreaInput';
+import styled from '@emotion/styled';
 
 import {
   FieldInputClickOutsideEvent,
   FieldInputEvent,
 } from '@/object-record/record-field/types/FieldInputEvent';
+import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
+import { useListenClickOutside } from '@/ui/utilities/pointer-event/hooks/useListenClickOutside';
+import { useRef, useState } from 'react';
+import { Key } from 'ts-key-enum';
+import { IconPencil } from 'twenty-ui/display';
+import { CodeEditor, FloatingIconButton } from 'twenty-ui/input';
+import { isTwoFirstDepths, JsonTree } from 'twenty-ui/json-visualizer';
 import { useJsonField } from '../../hooks/useJsonField';
 
 type RawJsonFieldInputProps = {
@@ -13,6 +20,20 @@ type RawJsonFieldInputProps = {
   onTab?: FieldInputEvent;
   onShiftTab?: FieldInputEvent;
 };
+
+const StyledJsonTreeContainer = styled.div`
+  height: 300px;
+  width: 400px;
+  padding: ${({ theme }) => theme.spacing(2)};
+  overflow: auto;
+  position: relative;
+`;
+
+const StyledSwitchModeButtonContainer = styled.div`
+  position: fixed;
+  top: ${({ theme }) => theme.spacing(2)};
+  right: ${({ theme }) => theme.spacing(2)};
+`;
 
 export const RawJsonFieldInput = ({
   onEnter,
@@ -28,6 +49,10 @@ export const RawJsonFieldInput = ({
     setDraftValue,
     persistJsonField,
   } = useJsonField();
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleEnter = (newText: string) => {
     onEnter?.(() => persistJsonField(newText));
@@ -56,19 +81,80 @@ export const RawJsonFieldInput = ({
     setDraftValue(newText);
   };
 
+  const handleSwitchMode = () => {
+    setIsEditing(!isEditing);
+  };
+
+  useListenClickOutside({
+    refs: [containerRef],
+    callback: (event) => {
+      handleClickOutside(event, draftValue ?? '');
+    },
+    listenerId: hotkeyScope,
+  });
+
+  useScopedHotkeys(
+    Key.Enter,
+    () => {
+      handleEnter(draftValue ?? '');
+    },
+    hotkeyScope,
+    [handleEnter, draftValue],
+  );
+
+  useScopedHotkeys(
+    [Key.Escape],
+    () => {
+      handleEscape(draftValue ?? '');
+    },
+    hotkeyScope,
+    [handleEscape, draftValue],
+  );
+
+  useScopedHotkeys(
+    'tab',
+    () => {
+      handleTab(draftValue ?? '');
+    },
+    hotkeyScope,
+    [handleTab, draftValue],
+  );
+
+  useScopedHotkeys(
+    'shift+tab',
+    () => {
+      handleShiftTab(draftValue ?? '');
+    },
+    hotkeyScope,
+    [handleShiftTab, draftValue],
+  );
+
   return (
-    <TextAreaInput
-      placeholder={fieldDefinition.metadata.placeHolder}
-      autoFocus
-      value={draftValue ?? ''}
-      onClickOutside={handleClickOutside}
-      onEnter={handleEnter}
-      onEscape={handleEscape}
-      onShiftTab={handleShiftTab}
-      onTab={handleTab}
-      hotkeyScope={hotkeyScope}
-      onChange={handleChange}
-      maxRows={25}
-    />
+    <StyledJsonTreeContainer ref={containerRef}>
+      {isEditing ? (
+        <CodeEditor
+          value={draftValue}
+          language="application/json"
+          height={292}
+          options={{
+            lineNumbers: 'off',
+          }}
+        />
+      ) : (
+        <JsonTree
+          value={JSON.parse(draftValue ?? '')}
+          arrowButtonCollapsedLabel=""
+          arrowButtonExpandedLabel=""
+          emptyArrayLabel=""
+          emptyObjectLabel=""
+          emptyStringLabel=""
+          shouldExpandNodeInitially={isTwoFirstDepths}
+        />
+      )}
+
+      <StyledSwitchModeButtonContainer>
+        <FloatingIconButton Icon={IconPencil} onClick={handleSwitchMode} />
+      </StyledSwitchModeButtonContainer>
+    </StyledJsonTreeContainer>
   );
 };
