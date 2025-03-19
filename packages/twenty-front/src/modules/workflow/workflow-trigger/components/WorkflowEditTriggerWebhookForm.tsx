704ccd454b15adeef273c4ae7c637f9f3b8bb10c
martmull <martmull@hotmail.fr>
2025-03-19 16:42:16 +0100

    Fix TextInputV2 design

--- packages/twenty-front/src/modules/workflow/workflow-trigger/components/WorkflowEditTriggerWebhookForm.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-trigger/components/WorkflowEditTriggerWebhookForm.tsx
@@ -1,10 +1,19 @@
 import { WorkflowWebhookTrigger } from '@/workflow/types/Workflow';
 import { useTheme } from '@emotion/react';
-import { useIcons } from 'twenty-ui';
+import { useIcons, IconCopy } from 'twenty-ui';
 import { getTriggerIcon } from '@/workflow/workflow-trigger/utils/getTriggerIcon';
 import { isDefined } from 'twenty-shared';
 import { WorkflowStepHeader } from '@/workflow/workflow-steps/components/WorkflowStepHeader';
 import { WorkflowStepBody } from '@/workflow/workflow-steps/components/WorkflowStepBody';
+import { TextInputV2 } from '@/ui/input/components/TextInputV2';
+import { SnackBarVariant } from '@/ui/feedback/snack-bar-manager/components/SnackBar';
+import { useDebouncedCallback } from 'use-debounce';
+import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
+import { useLingui } from '@lingui/react/macro';
+import { useRecoilValue } from 'recoil';
+import { workflowIdState } from '@/workflow/states/workflowIdState';
+import { currentWorkspaceState } from '@/auth/states/currentWorkspaceState';
+import { REACT_APP_SERVER_BASE_URL } from '~/config';
 
 type WorkflowEditTriggerWebhookFormProps = {
   trigger: WorkflowWebhookTrigger;
@@ -22,13 +31,35 @@ export const WorkflowEditTriggerWebhookForm = ({
   trigger,
   triggerOptions,
 }: WorkflowEditTriggerWebhookFormProps) => {
+  const { enqueueSnackBar } = useSnackBar();
   const theme = useTheme();
+  const { t } = useLingui();
   const { getIcon } = useIcons();
+  const workflowId = useRecoilValue(workflowIdState);
+  const currentWorkspace = useRecoilValue(currentWorkspaceState);
+
   const headerTitle = isDefined(trigger.name) ? trigger.name : 'Webhook';
 
   const headerIcon = getTriggerIcon({
     type: 'WEBHOOK',
   });
+
+  const webhookUrl = `${REACT_APP_SERVER_BASE_URL}/webhooks/workflows/${currentWorkspace?.id}/${workflowId}`;
+  const displayWebhookUrl = webhookUrl.replace(/^(https?:\/\/)?(www\.)?/, '');
+
+  const copyToClipboard = async () => {
+    await navigator.clipboard.writeText(webhookUrl);
+    enqueueSnackBar(t`Copied to clipboard!`, {
+      variant: SnackBarVariant.Success,
+      icon: <IconCopy size={theme.icon.size.md} />,
+    });
+  };
+
+  const copyToClipboardDebounced = useDebouncedCallback(copyToClipboard, 200);
+
+  if (!isDefined(currentWorkspace)) {
+    return <></>;
+  }
 
   return (
     <>
@@ -49,7 +80,20 @@ export const WorkflowEditTriggerWebhookForm = ({
         headerType="Trigger · Webhook"
         disabled={triggerOptions.readonly}
       />
-      <WorkflowStepBody></WorkflowStepBody>
+      <WorkflowStepBody>
+        <TextInputV2
+          label="Live URL"
+          value={displayWebhookUrl}
+          RightIcon={() => (
+            <IconCopy
+              size={theme.icon.size.md}
+              color={theme.font.color.secondary}
+            />
+          )}
+          onRightIconClick={copyToClipboardDebounced}
+          readOnly
+        />
+      </WorkflowStepBody>
     </>
   );
 };
