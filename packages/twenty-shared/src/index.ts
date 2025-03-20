22022e8444a5f3838ce89ad3a9d62f99401e3f4b
prastoin <paul@twenty.com>
2025-03-20 15:55:47 +0100

    refactor(shared): one for all barrel for the cjs

--- packages/twenty-shared/src/index.ts
+++ packages/twenty-shared/src/index.ts
@@ -1,1 +1,15 @@
-export default {};
+/*
+ * _____                    _
+ *|_   _|_      _____ _ __ | |_ _   _
+ *  | | \ \ /\ / / _ \ '_ \| __| | | | Auto-genreated file
+ *  | |  \ V  V /  __/ | | | |_| |_| | Any edition to this will be override
+ *  |_|   \_/\_/ \___|_| |_|\__|\__, |
+ *                              |___/
+ */
+
+export * from './constants';
+export * from './testing';
+export * from './translations';
+export * from './types';
+export * from './utils';
+export * from './workspace';
