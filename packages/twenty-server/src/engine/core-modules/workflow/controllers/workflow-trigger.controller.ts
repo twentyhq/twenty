4c8e2b3ef3647bc99674a33a40e7e2aabbdb43ee
martmull <martmull@hotmail.fr>
2025-03-20 10:06:06 +0100

    Code review returns: Thomas

--- packages/twenty-server/src/engine/core-modules/workflow/controllers/workflow-trigger.controller.ts
+++ packages/twenty-server/src/engine/core-modules/workflow/controllers/workflow-trigger.controller.ts
@@ -11,6 +11,8 @@ import {
   WorkflowTriggerException,
   WorkflowTriggerExceptionCode,
 } from 'src/modules/workflow/workflow-trigger/exceptions/workflow-trigger.exception';
+import { WorkflowTriggerType } from 'src/modules/workflow/workflow-trigger/types/workflow-trigger.type';
+import { WorkflowVersionWorkspaceEntity } from 'src/modules/workflow/common/standard-objects/workflow-version.workspace-entity';
 
 @Controller('webhooks')
 @UseFilters(WorkflowTriggerRestApiExceptionFilter)
@@ -48,6 +50,28 @@ export class WorkflowTriggerController {
       throw new WorkflowTriggerException(
         'Workflow not activated',
         WorkflowTriggerExceptionCode.INVALID_WORKFLOW_STATUS,
+      );
+    }
+
+    const workflowVersionRepository =
+      await this.twentyORMManager.getRepository<WorkflowVersionWorkspaceEntity>(
+        'workflowVersion',
+      );
+    const workflowVersion = await workflowVersionRepository.findOne({
+      where: { id: workflow.lastPublishedVersionId },
+    });
+
+    if (!isDefined(workflowVersion)) {
+      throw new WorkflowTriggerException(
+        'Workflow version not found',
+        WorkflowTriggerExceptionCode.INVALID_WORKFLOW_VERSION,
+      );
+    }
+
+    if (workflowVersion.trigger?.type !== WorkflowTriggerType.WEBHOOK) {
+      throw new WorkflowTriggerException(
+        'Workflow does not have a Webhook trigger',
+        WorkflowTriggerExceptionCode.INVALID_WORKFLOW_TRIGGER,
       );
     }
 
