bac268eb1d2f02d712108ef6ee1ed9550cd84634
prastoin <paul@twenty.com>
2025-03-20 23:12:03 +0100

    chore(random): it builds

--- packages/twenty-random/src/utils/validation/isDefined.ts
+++
@@ -1,4 +0,0 @@
-import { isNull, isUndefined } from '@sniptt/guards';
-
-export const isDefined = <T>(value: T | null | undefined): value is T =>
-  !isUndefined(value) && !isNull(value);
--- packages/twenty-random/src/utils/validation/isValidLocale.ts
+++
@@ -1,5 +0,0 @@
-import { APP_LOCALES } from '@/translations';
-
-export const isValidLocale = (
-  value: string | null,
-): value is keyof typeof APP_LOCALES => value !== null && value in APP_LOCALES;
