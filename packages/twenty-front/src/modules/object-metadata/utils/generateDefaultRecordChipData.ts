3528d725c3d6305cadbfbb177939f96702f222e4
prastoin <paul@twenty.com>
2025-03-20 16:51:06 +0100

    fix(front): default record chip data value

--- packages/twenty-front/src/modules/object-metadata/utils/generateDefaultRecordChipData.ts
+++ packages/twenty-front/src/modules/object-metadata/utils/generateDefaultRecordChipData.ts
@@ -1,15 +1,19 @@
 import { isFieldFullNameValue } from '@/object-record/record-field/types/guards/isFieldFullNameValue';
 import { ObjectRecord } from '@/object-record/types/ObjectRecord';
 
-export const generateDefaultRecordChipData = (record: ObjectRecord) => {
+export const generateDefaultRecordChipData = (
+  record: ObjectRecord,
+) => {
   const name = isFieldFullNameValue(record.name)
-    ? record.name.firstName + ' ' + record.name.lastName
+    ? `${record.name.firstName} ${record.name.lastName}`
     : (record.name ?? '');
 
   return {
+    __typename: '',
+    id: '',
     name,
     avatarUrl: name,
     avatarType: 'rounded',
     linkToShowPage: false,
-  };
+  } satisfies ObjectRecord;
 };
