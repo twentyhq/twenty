0ea30fd12d618fed3002f46fed8e461feddeff08
bosiraphael <raphael.bosi@gmail.com>
2025-03-24 17:22:56 +0100

    add see workflow single record action to workflow versions

---
+++ packages/twenty-front/src/modules/action-menu/actions/record-actions/single-record/workflow-version-actions/hooks/useSeeWorkflowWorkflowVersionSingleRecordAction.ts
@@ -0,0 +1,33 @@
+import { useSelectedRecordIdOrThrow } from '@/action-menu/actions/record-actions/single-record/hooks/useSelectedRecordIdOrThrow';
+import { ActionHookWithoutObjectMetadataItem } from '@/action-menu/actions/types/ActionHook';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { recordStoreFamilyState } from '@/object-record/record-store/states/recordStoreFamilyState';
+import { AppPath } from '@/types/AppPath';
+import { useRecoilValue } from 'recoil';
+import { isDefined } from 'twenty-shared/utils';
+import { useNavigateApp } from '~/hooks/useNavigateApp';
+
+export const useSeeWorkflowVersionSingleRecordAction: ActionHookWithoutObjectMetadataItem =
+  () => {
+    const recordId = useSelectedRecordIdOrThrow();
+
+    const workflowVersion = useRecoilValue(recordStoreFamilyState(recordId));
+
+    const navigateApp = useNavigateApp();
+
+    const shouldBeRegistered = isDefined(workflowVersion?.workflow?.id);
+
+    const onClick = () => {
+      if (!shouldBeRegistered || !workflowVersion?.workflow?.id) return;
+
+      navigateApp(AppPath.RecordShowPage, {
+        objectNameSingular: CoreObjectNameSingular.Workflow,
+        objectRecordId: workflowVersion.workflow.id,
+      });
+    };
+
+    return {
+      shouldBeRegistered,
+      onClick,
+    };
+  };
