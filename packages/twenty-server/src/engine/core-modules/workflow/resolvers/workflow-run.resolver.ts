96fce4365fdec538a90874c77a8fb54de41191a7
martmull <martmull@hotmail.fr>
2025-03-27 11:21:52 +0100

    Add graphql-redis-subscriptions package

--- packages/twenty-server/src/engine/core-modules/workflow/resolvers/workflow-run.resolver.ts
+++ packages/twenty-server/src/engine/core-modules/workflow/resolvers/workflow-run.resolver.ts
@@ -3,11 +3,13 @@ import { Inject } from '@nestjs/common';
 
 import { PubSub } from 'graphql-subscriptions';
 
+import { WorkflowRunDTO } from 'src/engine/core-modules/workflow/dtos/workflow-run.dto';
+
 @Resolver()
 export class WorkflowRunResolver {
   constructor(@Inject('PUB_SUB') private readonly pubSub: PubSub) {}
 
-  @Subscription(() => String)
+  @Subscription(() => WorkflowRunDTO)
   workflowRunUpdated() {
     return this.pubSub.asyncIterator('workflowRunUpdated');
   }
