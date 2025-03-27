7df7c6552c15ce40dc07195b864d3fa06c761338
martmull <martmull@hotmail.fr>
2025-03-27 10:36:16 +0100

    Emit when workflow run updated

--- packages/twenty-server/src/modules/workflow/workflow-runner/workflow-run/workflow-run.module.ts
+++ packages/twenty-server/src/modules/workflow/workflow-runner/workflow-run/workflow-run.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 
+import { NestjsQueryTypeOrmModule } from '@ptc-org/nestjs-query-typeorm';
+
 import { WorkflowCommonModule } from 'src/modules/workflow/common/workflow-common.module';
 import { WorkflowRunWorkspaceService } from 'src/modules/workflow/workflow-runner/workflow-run/workflow-run.workspace-service';
 import { SubscriptionsModule } from 'src/engine/subscriptions/subscriptions.module';
 import { WorkflowRunListener } from 'src/modules/workflow/workflow-runner/workflow-run/workflow-run.listener';
+import { ScopedWorkspaceContextFactory } from 'src/engine/twenty-orm/factories/scoped-workspace-context.factory';
+import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 
 @Module({
-  imports: [WorkflowCommonModule, SubscriptionsModule],
-  providers: [WorkflowRunWorkspaceService, WorkflowRunListener],
+  imports: [
+    WorkflowCommonModule,
+    SubscriptionsModule,
+    NestjsQueryTypeOrmModule.forFeature([ObjectMetadataEntity], 'metadata'),
+  ],
+  providers: [
+    WorkflowRunWorkspaceService,
+    WorkflowRunListener,
+    ScopedWorkspaceContextFactory,
+  ],
   exports: [WorkflowRunWorkspaceService],
 })
 export class WorkflowRunModule {}
