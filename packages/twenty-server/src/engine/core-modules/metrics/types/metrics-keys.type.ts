391392dd87d22226353f985c30d578a275a61a58
Etienne <45695613+etiennejouan@users.noreply.github.com>
2025-03-28 08:45:24 +0100

    set up metrics collecting with open telemetry (#11236)
    Done :  
    - move metrics and health cache services from health module to metrics
    module
    - refactor metrics counter from specific method to set up from enum keys
    - add OpenTelemetry (Otel) instrumentation for metrics
    - set up Otel SDK to send metrics to Otel collector
    
    To do later : 
    - implement Otel instrumentation for traces + plug Sentry on top

---
+++ packages/twenty-server/src/engine/core-modules/metrics/types/metrics-keys.type.ts
@@ -0,0 +1,9 @@
+export enum MetricsKeys {
+  MessageChannelSyncJobActive = 'message-channel-sync-job/active',
+  MessageChannelSyncJobFailedInsufficientPermissions = 'message-channel-sync-job/failed-insufficient-permissions',
+  MessageChannelSyncJobFailedUnknown = 'message-channel-sync-job/failed-unknown',
+  CalendarEventSyncJobActive = 'calendar-event-sync-job/active',
+  CalendarEventSyncJobFailedInsufficientPermissions = 'calendar-event-sync-job/failed-insufficient-permissions',
+  CalendarEventSyncJobFailedUnknown = 'calendar-event-sync-job/failed-unknown',
+  InvalidCaptcha = 'invalid-captcha',
+}
