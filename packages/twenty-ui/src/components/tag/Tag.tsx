fc8e4bbad40f9be706729f42af86033d0964efbf
prastoin <paul@twenty.com>
2025-03-31 16:41:13 +0200

    conflicts(shared): latest updates on twenty-ui conflicts
    conflicts(shared): buttons

--- packages/twenty-ui/src/components/tag/Tag.tsx
+++ packages/twenty-ui/src/components/tag/Tag.tsx
@@ -33,6 +33,7 @@ const StyledTag = styled.h3<{
       const themeColor = theme.tag.background[color];
 
       if (!isDefined(themeColor)) {
+        // eslint-disable-next-line no-console
         console.warn(`Tag color ${color} is not defined in the theme`);
         return theme.tag.background.gray;
       } else {
