91bd86b4a56633e463c597e50da52622412e30b3
prastoin <paul@twenty.com>
2025-03-31 11:26:05 +0200

    test(front): fix failing test due to timezone

--- packages/twenty-front/src/modules/localization/utils/__tests__/findAvailableTimeZoneOption.test.ts
+++ packages/twenty-front/src/modules/localization/utils/__tests__/findAvailableTimeZoneOption.test.ts
@@ -4,11 +4,14 @@ jest.useFakeTimers().setSystemTime(new Date('2024-01-01T00:00:00.000Z'));
 describe('findAvailableTimeZoneOption', () => {
   it('should find the matching available IANA time zone select option from a given IANA time zone', () => {
     const ianaTimeZone = 'Europe/Paris';
-    const value = 'Europe/Paris';
-    const label = '(GMT+01:00) Central European Standard Time - Paris';
 
     const option = findAvailableTimeZoneOption(ianaTimeZone);
 
-    expect(option).toEqual({ value, label });
+    expect(option).toMatchInlineSnapshot(`
+{
+  "label": "(GMT+02:00) Central European Summer Time - Paris",
+  "value": "Europe/Paris",
+}
+`);
   });
 });
