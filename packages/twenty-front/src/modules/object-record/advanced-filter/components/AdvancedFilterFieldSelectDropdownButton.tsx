b13be7bd2e7b368d1340c2f06f7b9be6d4d62f5e
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-03-31 10:32:04 +0200

    Advanced filter fix placeholder and icon in dropdown buttons (#11286)
    This PR fixes a UI issue that brings a lot more robustness to the
    advanced filter look and feel.
    
    It adds the icon of the field metadata item in the filter field select.
    
    It adds a custom placeholder in the filter input select, depending on
    the field metadata item type.
    
    <img width="661" alt="image"
    src="https://github.com/user-attachments/assets/8bf2044f-52cf-447d-909d-3312089c0df5"
    />

--- packages/twenty-front/src/modules/object-record/advanced-filter/components/AdvancedFilterFieldSelectDropdownButton.tsx
+++ packages/twenty-front/src/modules/object-record/advanced-filter/components/AdvancedFilterFieldSelectDropdownButton.tsx
@@ -1,11 +1,9 @@
+import { AdvancedFilterFieldSelectDropdownButtonClickableSelect } from '@/object-record/advanced-filter/components/AdvancedFilterFieldSelectDropdownButtonClickableSelect';
 import { AdvancedFilterFieldSelectDropdownContent } from '@/object-record/advanced-filter/components/AdvancedFilterFieldSelectDropdownContent';
 import { DEFAULT_ADVANCED_FILTER_DROPDOWN_OFFSET } from '@/object-record/advanced-filter/constants/DefaultAdvancedFilterDropdownOffset';
 import { useAdvancedFilterFieldSelectDropdown } from '@/object-record/advanced-filter/hooks/useAdvancedFilterFieldSelectDropdown';
 
-import { currentRecordFiltersComponentState } from '@/object-record/record-filter/states/currentRecordFiltersComponentState';
-import { SelectControl } from '@/ui/input/components/SelectControl';
 import { Dropdown } from '@/ui/layout/dropdown/components/Dropdown';
-import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import styled from '@emotion/styled';
 
 const StyledContainer = styled.div`
@@ -22,26 +20,13 @@ export const AdvancedFilterFieldSelectDropdownButton = ({
   const { advancedFilterFieldSelectDropdownId } =
     useAdvancedFilterFieldSelectDropdown(recordFilterId);
 
-  const currentRecordFilters = useRecoilComponentValueV2(
-    currentRecordFiltersComponentState,
-  );
-
-  const recordFilter = currentRecordFilters.find(
-    (recordFilter) => recordFilter.id === recordFilterId,
-  );
-
-  const selectedFieldLabel = recordFilter?.label ?? '';
-
   return (
     <StyledContainer>
       <Dropdown
         dropdownId={advancedFilterFieldSelectDropdownId}
         clickableComponent={
-          <SelectControl
-            selectedOption={{
-              label: selectedFieldLabel,
-              value: null,
-            }}
+          <AdvancedFilterFieldSelectDropdownButtonClickableSelect
+            recordFilterId={recordFilterId}
           />
         }
         dropdownComponents={
