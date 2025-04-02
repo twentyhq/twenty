e0e4cfd5d72a02b4527aaeae966d9b0c1c351f68
prastoin <paul@twenty.com>
2025-04-02 16:58:27 +0200

    restore(front): some previous configuration

--- packages/twenty-front/vite.config.ts
+++ packages/twenty-front/vite.config.ts
@@ -63,7 +63,8 @@ export default defineConfig(({ command, mode }) => {
   if (VITE_DISABLE_ESLINT_CHECKER !== 'true') {
     checkers['eslint'] = {
       lintCommand:
-        'eslint . --report-unused-disable-directives --max-warnings 0 --config .eslintrc.cjs',
+        // Appended to packages/twenty-front/.eslintrc.cjs
+        'cd  ../.. && eslint packages/twenty-front --report-unused-disable-directives --max-warnings 0 --config .eslintrc.cjs',
     };
   }
 
