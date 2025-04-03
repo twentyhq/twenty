e041e5954cc297b4cc65bbbe4382797b012dca1c
khathuynh-ws <kha.huynh@whitespace.us>
2025-04-03 12:45:47 -0400

    [SMS text view] Show sms messages with most recent at bottom
    [SMS text view] Auto-scroll to bottom, most recent messages
    
    [SMS text view] Retrieve person's phone number to call API
    
    Add custom readme
    
    [SMS text view] Call API for messages to or from given phone number
    
    Refactor twilio api call to use useEffect pattern
    
    Display sms texts from twilio api call
    
    First pass at sms text card

--- packages/twenty-front/src/modules/activities/types/SMSText.ts
+++ packages/twenty-front/src/modules/activities/types/SMSText.ts
@@ -1,13 +1,13 @@
 export type SMSText = {
-    id: string;
-    sender: string;
-    body: string;
-    date: Date;
+  id: string;
+  sender: string;
+  body: string;
+  date: Date;
 };
 
 export type TwilioMessage = {
-    sid: string;
-    from: string;
-    body: string;
-    date_sent: string | Date; 
+  sid: string;
+  from: string;
+  body: string;
+  date_sent: string | Date;
 };
