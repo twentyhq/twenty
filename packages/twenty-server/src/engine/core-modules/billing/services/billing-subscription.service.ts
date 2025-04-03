752eb938365dc938db28749478387415af79ad0f
Etienne <45695613+etiennejouan@users.noreply.github.com>
2025-04-03 16:18:44 +0200

    disable workflow execution if billing issue (#11374)
    closes https://github.com/twentyhq/core-team-issues/issues/404

--- packages/twenty-server/src/engine/core-modules/billing/services/billing-subscription.service.ts
+++ packages/twenty-server/src/engine/core-modules/billing/services/billing-subscription.service.ts
@@ -14,7 +14,6 @@ import {
 } from 'src/engine/core-modules/billing/billing.exception';
 import { BillingEntitlement } from 'src/engine/core-modules/billing/entities/billing-entitlement.entity';
 import { BillingPrice } from 'src/engine/core-modules/billing/entities/billing-price.entity';
-import { BillingProduct } from 'src/engine/core-modules/billing/entities/billing-product.entity';
 import { BillingSubscriptionItem } from 'src/engine/core-modules/billing/entities/billing-subscription-item.entity';
 import { BillingSubscription } from 'src/engine/core-modules/billing/entities/billing-subscription.entity';
 import { BillingEntitlementKey } from 'src/engine/core-modules/billing/enums/billing-entitlement-key.enum';
@@ -38,8 +37,6 @@ export class BillingSubscriptionService {
     private readonly billingEntitlementRepository: Repository<BillingEntitlement>,
     @InjectRepository(BillingSubscription, 'core')
     private readonly billingSubscriptionRepository: Repository<BillingSubscription>,
-    @InjectRepository(BillingProduct, 'core')
-    private readonly billingProductRepository: Repository<BillingProduct>,
   ) {}
 
   async getCurrentBillingSubscriptionOrThrow(criteria: {
