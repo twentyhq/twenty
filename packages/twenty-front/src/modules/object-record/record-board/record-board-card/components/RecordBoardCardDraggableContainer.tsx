af7e8643b301ccc33a458f793fe362f60bbe984e
Charles Bochet <charles@twenty.com>
2025-04-04 15:57:31 +0200

    Fix

--- packages/twenty-front/src/modules/object-record/record-board/record-board-card/components/RecordBoardCardDraggableContainer.tsx
+++ packages/twenty-front/src/modules/object-record/record-board/record-board-card/components/RecordBoardCardDraggableContainer.tsx
@@ -1,6 +1,8 @@
 import { Draggable } from '@hello-pangea/dnd';
 
 import { RecordBoardCard } from '@/object-record/record-board/record-board-card/components/RecordBoardCard';
+import { RecordBoardCardContext } from '@/object-record/record-board/record-board-card/contexts/RecordBoardCardContext';
+import { useIsRecordReadOnly } from '@/object-record/record-field/hooks/useIsRecordReadOnly';
 
 export const RecordBoardCardDraggableContainer = ({
   recordId,
@@ -9,22 +11,30 @@ export const RecordBoardCardDraggableContainer = ({
   recordId: string;
   index: number;
 }) => {
+  const isRecordReadOnly = useIsRecordReadOnly({
+    recordId,
+  });
+
   return (
-    <Draggable key={recordId} draggableId={recordId} index={index}>
-      {(draggableProvided) => (
-        <div
-          ref={draggableProvided?.innerRef}
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...draggableProvided?.dragHandleProps}
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...draggableProvided?.draggableProps}
-          className="record-board-card"
-          data-selectable-id={recordId}
-          data-select-disable
-        >
-          <RecordBoardCard />
-        </div>
-      )}
-    </Draggable>
+    <RecordBoardCardContext.Provider
+      value={{ recordId, isRecordReadOnly: false }}
+    >
+      <Draggable key={recordId} draggableId={recordId} index={index}>
+        {(draggableProvided) => (
+          <div
+            ref={draggableProvided?.innerRef}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...draggableProvided?.dragHandleProps}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...draggableProvided?.draggableProps}
+            className="record-board-card"
+            data-selectable-id={recordId}
+            data-select-disable
+          >
+            <RecordBoardCard />
+          </div>
+        )}
+      </Draggable>
+    </RecordBoardCardContext.Provider>
   );
 };
