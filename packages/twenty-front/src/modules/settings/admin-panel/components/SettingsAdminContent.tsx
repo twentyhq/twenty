a9ff6971fe5b44a7d050d6821165c61269b931db
ehconitin <nitinkoche03@gmail.com>
2025-04-04 16:10:55 +0530

    conflicts

--- packages/twenty-front/src/modules/settings/admin-panel/components/SettingsAdminContent.tsx
+++ packages/twenty-front/src/modules/settings/admin-panel/components/SettingsAdminContent.tsx
@@ -5,11 +5,12 @@ import { SETTINGS_ADMIN_TABS_ID } from '@/settings/admin-panel/constants/Setting
 import { TabList } from '@/ui/layout/tab/components/TabList';
 import { t } from '@lingui/core/macro';
 import { useRecoilValue } from 'recoil';
-<<<<<<< HEAD
-import { IconCode, IconHeart, IconSettings2, IconVariable } from 'twenty-ui';
-=======
-import { IconHeart, IconSettings2, IconVariable } from 'twenty-ui/display';
->>>>>>> upstream/main
+import {
+  IconCode,
+  IconHeart,
+  IconSettings2,
+  IconVariable,
+} from 'twenty-ui/display';
 
 export const SettingsAdminContent = () => {
   const currentUser = useRecoilValue(currentUserState);
