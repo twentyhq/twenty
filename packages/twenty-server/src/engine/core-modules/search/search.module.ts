f8cf857f8e74e6e21f0a1f5501d70fc1fa38f418
vaibhav-devere <vaibhavdevere@gmail.com>
2025-04-04 11:57:21 +0530

    Update Search Service to properly handle worksapce member avatar url

--- packages/twenty-server/src/engine/core-modules/search/search.module.ts
+++ packages/twenty-server/src/engine/core-modules/search/search.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-flag.module';
+import { FileModule } from 'src/engine/core-modules/file/file.module';
 import { SearchResolver } from 'src/engine/core-modules/search/search.resolver';
 import { SearchService } from 'src/engine/core-modules/search/services/search.service';
 import { WorkspaceCacheStorageModule } from 'src/engine/workspace-cache-storage/workspace-cache-storage.module';
 
 @Module({
-  imports: [WorkspaceCacheStorageModule, FeatureFlagModule],
+  imports: [WorkspaceCacheStorageModule, FeatureFlagModule, FileModule],
   providers: [SearchResolver, SearchService],
 })
 export class SearchModule {}
