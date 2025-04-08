b8e9ad40fdde76729017dce4cbcf87f7b543791c
bosiraphael <raphael.bosi@gmail.com>
2025-04-08 18:40:50 +0200

    fix duplicate type

--- packages/twenty-front/src/modules/action-menu/contexts/ActionConfigContext.tsx
+++ packages/twenty-front/src/modules/action-menu/contexts/ActionConfigContext.tsx
@@ -1,6 +1,4 @@
 import { ActionConfig } from '@/action-menu/actions/types/ActionConfig';
 import { createContext } from 'react';
 
-export const ActionConfigContext = createContext<
-  ActionConfig | ActionConfig | null
->(null);
+export const ActionConfigContext = createContext<ActionConfig | null>(null);
