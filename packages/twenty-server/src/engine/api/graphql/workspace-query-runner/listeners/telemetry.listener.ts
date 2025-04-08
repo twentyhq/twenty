2311712ecfc9ecf0bae1cf13480b756ef05ccf51
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-04-08 14:34:35 +0200

    feat(telemetry): integrate telemetry service into listener
    Added TelemetryService to capture user signup and creation events in addition to analytics. This enhances event tracking by including telemetry data for better insights.

--- packages/twenty-server/src/engine/api/graphql/workspace-query-runner/listeners/telemetry.listener.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-query-runner/listeners/telemetry.listener.ts
@@ -4,13 +4,17 @@ import { OnDatabaseBatchEvent } from 'src/engine/api/graphql/graphql-query-runne
 import { DatabaseEventAction } from 'src/engine/api/graphql/graphql-query-runner/enums/database-event-action';
 import { AnalyticsService } from 'src/engine/core-modules/analytics/services/analytics.service';
 import { ObjectRecordCreateEvent } from 'src/engine/core-modules/event-emitter/types/object-record-create.event';
+import { TelemetryService } from 'src/engine/core-modules/telemetry/telemetry.service';
 import { WorkspaceEventBatch } from 'src/engine/workspace-event-emitter/types/workspace-event.type';
 import { USER_SIGNUP_EVENT_NAME } from 'src/engine/api/graphql/workspace-query-runner/constants/user-signup-event-name.constants';
 import { OnCustomBatchEvent } from 'src/engine/api/graphql/graphql-query-runner/decorators/on-custom-batch-event.decorator';
 
 @Injectable()
 export class TelemetryListener {
-  constructor(private readonly analyticsService: AnalyticsService) {}
+  constructor(
+    private readonly analyticsService: AnalyticsService,
+    private readonly telemetryService: TelemetryService,
+  ) {}
 
   @OnDatabaseBatchEvent('*', DatabaseEventAction.CREATED)
   async handleAllCreate(payload: WorkspaceEventBatch<ObjectRecordCreateEvent>) {
@@ -23,6 +27,7 @@ export class TelemetryListener {
           })
           .sendUnknownEvent({
             action: payload.name,
+            payload: {},
           }),
       ),
     );
@@ -33,7 +38,7 @@ export class TelemetryListener {
     payload: WorkspaceEventBatch<ObjectRecordCreateEvent>,
   ) {
     await Promise.all(
-      payload.events.map((eventPayload) =>
+      payload.events.map(async (eventPayload) => {
         this.analyticsService
           .createAnalyticsContext({
             userId: eventPayload.userId,
@@ -41,8 +46,22 @@ export class TelemetryListener {
           })
           .sendUnknownEvent({
             action: USER_SIGNUP_EVENT_NAME,
-          }),
-      ),
+            payload: {},
+          });
+
+        this.telemetryService.create(
+          {
+            action: USER_SIGNUP_EVENT_NAME,
+            payload: {
+              payload,
+              userId: undefined,
+              workspaceId: undefined,
+            },
+          },
+          eventPayload.userId,
+          payload.workspaceId,
+        );
+      }),
     );
   }
 }
