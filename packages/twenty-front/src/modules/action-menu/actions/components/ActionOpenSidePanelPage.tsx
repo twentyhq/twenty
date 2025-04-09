2d0dd6f68038e7abdd5f1cd31b046e9d5efe1b3b
bosiraphael <raphael.bosi@gmail.com>
2025-04-09 17:17:32 +0200

    reorder imports

--- packages/twenty-front/src/modules/action-menu/actions/components/ActionOpenSidePanelPage.tsx
+++ packages/twenty-front/src/modules/action-menu/actions/components/ActionOpenSidePanelPage.tsx
@@ -1,10 +1,9 @@
-import { useContext } from 'react';
-
 import { ActionDisplay } from '@/action-menu/actions/display/components/ActionDisplay';
 import { ActionConfigContext } from '@/action-menu/contexts/ActionConfigContext';
 import { useNavigateCommandMenu } from '@/command-menu/hooks/useNavigateCommandMenu';
 import { commandMenuSearchState } from '@/command-menu/states/commandMenuSearchState';
 import { CommandMenuPages } from '@/command-menu/types/CommandMenuPages';
+import { useContext } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { IconComponent } from 'twenty-ui/display';
 
