9e0402e6915a3dac74cd99be1072c81109c860cd
Raphaël Bosi <71827178+bosiraphael@users.noreply.github.com>
2025-04-09 15:12:49 +0200

    Action menu refactoring (#11454)
    # Description
    
    Closes [#696](https://github.com/twentyhq/core-team-issues/issues/696)
    
    - `useAction` hooks have been removed for all actions
    - Every action can now declare a react component
    - Some standard action components have been introduced: `Action`,
    `ActionLink` and `ActionModal`
    - The `ActionDisplay` component uses the new `displayType` prop of the
    `ActionMenuContext` to render the right component for the action
    according to its container: `ActionButton`, `ActionDropdownItem` or
    `ActionListItem`
    - The `ActionDisplayer` wraps the action component inside a context
    which gives it all the information about the action
    -`actionMenuEntriesComponenState` has been removed and now all actions
    are computed directly using `useRegisteredAction`
    - This computation is done inside `ActionMenuContextProvider` and the
    actions are passed inside a context
    - `actionMenuType` gives information about the container of the action,
    so the action can know wether or not to close this container upon
    execution

---
+++ packages/twenty-front/src/modules/action-menu/actions/types/ActionConfig.ts
@@ -0,0 +1,24 @@
+import { ActionScope } from '@/action-menu/actions/types/ActionScope';
+import { ActionType } from '@/action-menu/actions/types/ActionType';
+import { ActionViewType } from '@/action-menu/actions/types/ActionViewType';
+import { ShouldBeRegisteredFunctionParams } from '@/action-menu/actions/types/ShouldBeRegisteredFunctionParams';
+import { MessageDescriptor } from '@lingui/core';
+import { IconComponent } from 'twenty-ui/display';
+import { MenuItemAccent } from 'twenty-ui/navigation';
+
+export type ActionConfig = {
+  type: ActionType;
+  scope: ActionScope;
+  key: string;
+  label: MessageDescriptor | string;
+  shortLabel?: MessageDescriptor | string;
+  description?: MessageDescriptor | string;
+  position: number;
+  Icon: IconComponent;
+  isPinned?: boolean;
+  accent?: MenuItemAccent;
+  availableOn?: ActionViewType[];
+  shouldBeRegistered: (params: ShouldBeRegisteredFunctionParams) => boolean;
+  component: React.ReactNode;
+  hotKeys?: string[];
+};
