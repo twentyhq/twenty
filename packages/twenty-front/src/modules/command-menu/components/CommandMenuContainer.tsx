b0db9e2caf8b5e53146500b0134735dfa3a42dc5
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-04-09 18:00:43 +0200

    Fixed

--- packages/twenty-front/src/modules/command-menu/components/CommandMenuContainer.tsx
+++ packages/twenty-front/src/modules/command-menu/components/CommandMenuContainer.tsx
@@ -6,15 +6,12 @@ import { RecordAgnosticActionsKeys } from '@/action-menu/actions/record-agnostic
 import { ActionMenuConfirmationModals } from '@/action-menu/components/ActionMenuConfirmationModals';
 import { ActionMenuContext } from '@/action-menu/contexts/ActionMenuContext';
 import { ActionMenuComponentInstanceContext } from '@/action-menu/states/contexts/ActionMenuComponentInstanceContext';
-import { COMMAND_MENU_ANIMATION_VARIANTS } from '@/command-menu/constants/CommandMenuAnimationVariants';
+import { CommandMenuOpenContainer } from '@/command-menu/components/CommandMenuOpenContainer';
 import { COMMAND_MENU_COMPONENT_INSTANCE_ID } from '@/command-menu/constants/CommandMenuComponentInstanceId';
 import { useCommandMenu } from '@/command-menu/hooks/useCommandMenu';
 import { useCommandMenuCloseAnimationCompleteCleanup } from '@/command-menu/hooks/useCommandMenuCloseAnimationCompleteCleanup';
-import { useCommandMenuHotKeys } from '@/command-menu/hooks/useCommandMenuHotKeys';
 import { commandMenuSearchState } from '@/command-menu/states/commandMenuSearchState';
 import { isCommandMenuOpenedState } from '@/command-menu/states/isCommandMenuOpenedState';
-import { CommandMenuAnimationVariant } from '@/command-menu/types/CommandMenuAnimationVariant';
-import { CommandMenuHotkeyScope } from '@/command-menu/types/CommandMenuHotkeyScope';
 import { contextStoreCurrentObjectMetadataItemIdComponentState } from '@/context-store/states/contextStoreCurrentObjectMetadataItemIdComponentState';
 import { contextStoreCurrentViewIdComponentState } from '@/context-store/states/contextStoreCurrentViewIdComponentState';
 import { ContextStoreComponentInstanceContext } from '@/context-store/states/contexts/ContextStoreComponentInstanceContext';
@@ -23,79 +20,23 @@ import { RecordFilterGroupsComponentInstanceContext } from '@/object-record/reco
 import { RecordFiltersComponentInstanceContext } from '@/object-record/record-filter/states/context/RecordFiltersComponentInstanceContext';
 import { RecordSortsComponentInstanceContext } from '@/object-record/record-sort/states/context/RecordSortsComponentInstanceContext';
 import { getRecordIndexIdFromObjectNamePluralAndViewId } from '@/object-record/utils/getRecordIndexIdFromObjectNamePluralAndViewId';
-import { RootStackingContextZIndices } from '@/ui/layout/constants/RootStackingContextZIndices';
-import { currentHotkeyScopeState } from '@/ui/utilities/hotkey/states/internal/currentHotkeyScopeState';
-import { useListenClickOutside } from '@/ui/utilities/pointer-event/hooks/useListenClickOutside';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useIsFeatureEnabled } from '@/workspace/hooks/useIsFeatureEnabled';
-import { useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useRef } from 'react';
-import { useRecoilCallback, useRecoilValue, useSetRecoilState } from 'recoil';
+import { AnimatePresence } from 'framer-motion';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { FeatureFlagKey } from '~/generated-metadata/graphql';
-import { useIsMobile } from 'twenty-ui/utilities';
-
-const StyledCommandMenu = styled(motion.div)`
-  background: ${({ theme }) => theme.background.primary};
-  border-left: 1px solid ${({ theme }) => theme.border.color.medium};
-  box-shadow: ${({ theme }) => theme.boxShadow.strong};
-  font-family: ${({ theme }) => theme.font.family};
-  height: 100%;
-  overflow: hidden;
-  padding: 0;
-  position: fixed;
-  right: 0%;
-  top: 0%;
-  z-index: ${RootStackingContextZIndices.CommandMenu};
-  display: flex;
-  flex-direction: column;
-`;
 
 export const CommandMenuContainer = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const { toggleCommandMenu, closeCommandMenu } = useCommandMenu();
+  const { toggleCommandMenu } = useCommandMenu();
 
   const { commandMenuCloseAnimationCompleteCleanup } =
     useCommandMenuCloseAnimationCompleteCleanup();
 
   const isCommandMenuOpened = useRecoilValue(isCommandMenuOpenedState);
-
-  const commandMenuRef = useRef<HTMLDivElement>(null);
-
-  useCommandMenuHotKeys();
-
-  const handleClickOutside = useRecoilCallback(
-    ({ snapshot }) =>
-      () => {
-        const hotkeyScope = snapshot
-          .getLoadable(currentHotkeyScopeState)
-          .getValue();
-
-        if (hotkeyScope?.scope === CommandMenuHotkeyScope.CommandMenuFocused) {
-          closeCommandMenu();
-        }
-      },
-    [closeCommandMenu],
-  );
-
-  useListenClickOutside({
-    refs: [commandMenuRef],
-    callback: handleClickOutside,
-    listenerId: 'COMMAND_MENU_LISTENER_ID',
-    excludeClassNames: ['page-header-command-menu-button'],
-  });
-
-  const isMobile = useIsMobile();
-
-  const targetVariantForAnimation: CommandMenuAnimationVariant = isMobile
-    ? 'fullScreen'
-    : 'normal';
-
-  const theme = useTheme();
 
   const isWorkflowEnabled = useIsFeatureEnabled(
     FeatureFlagKey.IsWorkflowEnabled,
@@ -172,18 +113,9 @@ export const CommandMenuContainer = ({
                   onExitComplete={commandMenuCloseAnimationCompleteCleanup}
                 >
                   {isCommandMenuOpened && (
-                    <StyledCommandMenu
-                      data-testid="command-menu"
-                      ref={commandMenuRef}
-                      className="command-menu"
-                      animate={targetVariantForAnimation}
-                      initial="closed"
-                      exit="closed"
-                      variants={COMMAND_MENU_ANIMATION_VARIANTS}
-                      transition={{ duration: theme.animation.duration.normal }}
-                    >
+                    <CommandMenuOpenContainer>
                       {children}
-                    </StyledCommandMenu>
+                    </CommandMenuOpenContainer>
                   )}
                 </AnimatePresence>
               </ActionMenuContext.Provider>
