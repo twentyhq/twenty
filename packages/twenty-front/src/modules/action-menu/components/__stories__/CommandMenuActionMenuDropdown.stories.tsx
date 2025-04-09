da98bd5357bb66aaa77c3bf75ee72423424a519b
bosiraphael <raphael.bosi@gmail.com>
2025-04-09 11:45:41 +0200

    create mock file

--- packages/twenty-front/src/modules/action-menu/components/__stories__/CommandMenuActionMenuDropdown.stories.tsx
+++ packages/twenty-front/src/modules/action-menu/components/__stories__/CommandMenuActionMenuDropdown.stories.tsx
@@ -2,19 +2,13 @@ import { expect, jest } from '@storybook/jest';
 import { Meta, StoryObj } from '@storybook/react';
 import { RecoilRoot } from 'recoil';
 
-import { Action } from '@/action-menu/actions/components/Action';
-import { SingleRecordActionKeys } from '@/action-menu/actions/record-actions/single-record/types/SingleRecordActionsKey';
-import { ActionScope } from '@/action-menu/actions/types/ActionScope';
-import { ActionType } from '@/action-menu/actions/types/ActionType';
-import { ActionViewType } from '@/action-menu/actions/types/ActionViewType';
 import { CommandMenuActionMenuDropdown } from '@/action-menu/components/CommandMenuActionMenuDropdown';
 import { ActionMenuContext } from '@/action-menu/contexts/ActionMenuContext';
+import { createMockActionMenuActions } from '@/action-menu/mock/action-menu-actions.mock';
 import { ActionMenuComponentInstanceContext } from '@/action-menu/states/contexts/ActionMenuComponentInstanceContext';
 import { contextStoreNumberOfSelectedRecordsComponentState } from '@/context-store/states/contextStoreNumberOfSelectedRecordsComponentState';
 import { contextStoreTargetedRecordsRuleComponentState } from '@/context-store/states/contextStoreTargetedRecordsRuleComponentState';
-import { msg } from '@lingui/core/macro';
 import { userEvent, waitFor, within } from '@storybook/test';
-import { IconFileExport, IconHeart, IconTrash } from 'twenty-ui/display';
 import {
   ComponentDecorator,
   getCanvasElementForDropdownTesting,
@@ -59,58 +53,11 @@ const meta: Meta<typeof CommandMenuActionMenuDropdown> = {
               isInRightDrawer: true,
               displayType: 'dropdownItem',
               actionMenuType: 'command-menu-show-page-action-menu-dropdown',
-              actions: [
-                {
-                  type: ActionType.Standard,
-                  scope: ActionScope.RecordSelection,
-                  key: SingleRecordActionKeys.ADD_TO_FAVORITES,
-                  label: msg`Add to favorites`,
-                  shortLabel: msg`Add to favorites`,
-                  position: 2,
-                  isPinned: true,
-                  Icon: IconHeart,
-                  shouldBeRegistered: () => true,
-                  availableOn: [
-                    ActionViewType.INDEX_PAGE_SINGLE_RECORD_SELECTION,
-                    ActionViewType.SHOW_PAGE,
-                  ],
-                  component: <Action onClick={addToFavoritesMock} />,
-                },
-                {
-                  type: ActionType.Standard,
-                  scope: ActionScope.RecordSelection,
-                  key: SingleRecordActionKeys.EXPORT,
-                  label: msg`Export`,
-                  shortLabel: msg`Export`,
-                  position: 4,
-                  Icon: IconFileExport,
-                  accent: 'default',
-                  isPinned: false,
-                  shouldBeRegistered: () => true,
-                  availableOn: [
-                    ActionViewType.SHOW_PAGE,
-                    ActionViewType.INDEX_PAGE_SINGLE_RECORD_SELECTION,
-                  ],
-                  component: <Action onClick={exportMock} />,
-                },
-                {
-                  type: ActionType.Standard,
-                  scope: ActionScope.RecordSelection,
-                  key: SingleRecordActionKeys.DELETE,
-                  label: msg`Delete`,
-                  shortLabel: msg`Delete`,
-                  position: 7,
-                  Icon: IconTrash,
-                  accent: 'default',
-                  isPinned: true,
-                  shouldBeRegistered: () => true,
-                  availableOn: [
-                    ActionViewType.INDEX_PAGE_SINGLE_RECORD_SELECTION,
-                    ActionViewType.SHOW_PAGE,
-                  ],
-                  component: <Action onClick={deleteMock} />,
-                },
-              ],
+              actions: createMockActionMenuActions({
+                deleteMock,
+                addToFavoritesMock,
+                exportMock,
+              }),
             }}
           >
             <Story />
--- packages/twenty-front/src/modules/action-menu/components/__stories__/RecordIndexActionMenuDropdown.stories.tsx
+++ packages/twenty-front/src/modules/action-menu/components/__stories__/RecordIndexActionMenuDropdown.stories.tsx
@@ -3,19 +3,13 @@ import { Meta, StoryObj } from '@storybook/react';
 import { userEvent, waitFor, within } from '@storybook/testing-library';
 import { RecoilRoot } from 'recoil';
 
-import { Action } from '@/action-menu/actions/components/Action';
-import { SingleRecordActionKeys } from '@/action-menu/actions/record-actions/single-record/types/SingleRecordActionsKey';
-import { ActionScope } from '@/action-menu/actions/types/ActionScope';
-import { ActionType } from '@/action-menu/actions/types/ActionType';
-import { ActionViewType } from '@/action-menu/actions/types/ActionViewType';
 import { RecordIndexActionMenuDropdown } from '@/action-menu/components/RecordIndexActionMenuDropdown';
 import { ActionMenuContext } from '@/action-menu/contexts/ActionMenuContext';
+import { createMockActionMenuActions } from '@/action-menu/mock/action-menu-actions.mock';
 import { ActionMenuComponentInstanceContext } from '@/action-menu/states/contexts/ActionMenuComponentInstanceContext';
 import { recordIndexActionMenuDropdownPositionComponentState } from '@/action-menu/states/recordIndexActionMenuDropdownPositionComponentState';
 import { isDropdownOpenComponentState } from '@/ui/layout/dropdown/states/isDropdownOpenComponentState';
 import { extractComponentState } from '@/ui/utilities/state/component-state/utils/extractComponentState';
-import { msg } from '@lingui/core/macro';
-import { IconFileExport, IconHeart, IconTrash } from 'twenty-ui/display';
 import { getCanvasElementForDropdownTesting } from 'twenty-ui/testing';
 import { ContextStoreDecorator } from '~/testing/decorators/ContextStoreDecorator';
 import { I18nFrontDecorator } from '~/testing/decorators/I18nFrontDecorator';
@@ -57,58 +51,11 @@ const meta: Meta<typeof RecordIndexActionMenuDropdown> = {
               isInRightDrawer: true,
               displayType: 'dropdownItem',
               actionMenuType: 'index-page-action-menu-dropdown',
-              actions: [
-                {
-                  type: ActionType.Standard,
-                  scope: ActionScope.RecordSelection,
-                  key: SingleRecordActionKeys.ADD_TO_FAVORITES,
-                  label: msg`Add to favorites`,
-                  shortLabel: msg`Add to favorites`,
-                  position: 2,
-                  isPinned: true,
-                  Icon: IconHeart,
-                  shouldBeRegistered: () => true,
-                  availableOn: [
-                    ActionViewType.INDEX_PAGE_SINGLE_RECORD_SELECTION,
-                    ActionViewType.SHOW_PAGE,
-                  ],
-                  component: <Action onClick={addToFavoritesMock} />,
-                },
-                {
-                  type: ActionType.Standard,
-                  scope: ActionScope.RecordSelection,
-                  key: SingleRecordActionKeys.EXPORT,
-                  label: msg`Export`,
-                  shortLabel: msg`Export`,
-                  position: 4,
-                  Icon: IconFileExport,
-                  accent: 'default',
-                  isPinned: false,
-                  shouldBeRegistered: () => true,
-                  availableOn: [
-                    ActionViewType.SHOW_PAGE,
-                    ActionViewType.INDEX_PAGE_SINGLE_RECORD_SELECTION,
-                  ],
-                  component: <Action onClick={exportMock} />,
-                },
-                {
-                  type: ActionType.Standard,
-                  scope: ActionScope.RecordSelection,
-                  key: SingleRecordActionKeys.DELETE,
-                  label: msg`Delete`,
-                  shortLabel: msg`Delete`,
-                  position: 7,
-                  Icon: IconTrash,
-                  accent: 'default',
-                  isPinned: true,
-                  shouldBeRegistered: () => true,
-                  availableOn: [
-                    ActionViewType.INDEX_PAGE_SINGLE_RECORD_SELECTION,
-                    ActionViewType.SHOW_PAGE,
-                  ],
-                  component: <Action onClick={deleteMock} />,
-                },
-              ],
+              actions: createMockActionMenuActions({
+                deleteMock,
+                addToFavoritesMock,
+                exportMock,
+              }),
             }}
           >
             <Story />
