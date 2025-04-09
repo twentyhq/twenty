a39349dc09ac259bef4fc762caec2e6c90872538
martmull <martmull@hotmail.fr>
2025-04-09 16:10:38 +0200

    Fix when outputTab is disabled

--- packages/twenty-front/src/modules/command-menu/pages/workflow/step/view-run/components/CommandMenuWorkflowRunViewStep.tsx
+++ packages/twenty-front/src/modules/command-menu/pages/workflow/step/view-run/components/CommandMenuWorkflowRunViewStep.tsx
@@ -87,7 +87,9 @@ export const CommandMenuWorkflowRunViewStep = () => {
       <StyledContainer>
         <StyledTabList
           tabs={tabs}
-          initialActiveTabId={WorkflowRunTabId.OUTPUT}
+          initialActiveTabId={
+            !areInputAndOutputTabsDisabled ? WorkflowRunTabId.OUTPUT : undefined
+          }
           behaveAsLinks={false}
           componentInstanceId={
             WORKFLOW_RUN_STEP_SIDE_PANEL_TAB_LIST_COMPONENT_ID
