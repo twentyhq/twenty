da6978b9b884ee27f9a0459608ec68635a352036
Charles Bochet <charles@twenty.com>
2025-04-09 17:30:50 +0200

    Fix

--- packages/twenty-front/src/modules/settings/accounts/constants/AvailableTimezoneOptions.ts
+++ packages/twenty-front/src/modules/settings/accounts/constants/AvailableTimezoneOptions.ts
@@ -5,7 +5,6 @@ import { AVAILABLE_TIME_ZONE_OPTIONS_BY_LABEL } from '@/settings/accounts/consta
 export const AVAILABLE_TIMEZONE_OPTIONS = Object.values(
   AVAILABLE_TIME_ZONE_OPTIONS_BY_LABEL,
 ).sort((optionA, optionB) => {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   const difference =
     getTimezoneOffset(optionA.value) - getTimezoneOffset(optionB.value);
 
