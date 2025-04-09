7e531507e88dcdb580c6fe9d168f80ae05a9daa6
bosiraphael <raphael.bosi@gmail.com>
2025-04-09 17:06:08 +0200

    create ActionOpenSidePanelPage component

---
+++ packages/twenty-front/src/modules/action-menu/actions/components/ActionOpenSidePanelPage.tsx
@@ -0,0 +1,45 @@
+import { useContext } from 'react';
+
+import { ActionDisplay } from '@/action-menu/actions/display/components/ActionDisplay';
+import { ActionConfigContext } from '@/action-menu/contexts/ActionConfigContext';
+import { useNavigateCommandMenu } from '@/command-menu/hooks/useNavigateCommandMenu';
+import { commandMenuSearchState } from '@/command-menu/states/commandMenuSearchState';
+import { CommandMenuPages } from '@/command-menu/types/CommandMenuPages';
+import { useSetRecoilState } from 'recoil';
+import { IconComponent } from 'twenty-ui/display';
+
+export const ActionOpenSidePanelPage = ({
+  page,
+  pageTitle,
+  pageIcon,
+  shouldResetSearchState = false,
+}: {
+  page: CommandMenuPages;
+  pageTitle: string;
+  pageIcon: IconComponent;
+  shouldResetSearchState?: boolean;
+}) => {
+  const actionConfig = useContext(ActionConfigContext);
+
+  const { navigateCommandMenu } = useNavigateCommandMenu();
+
+  const setCommandMenuSearchState = useSetRecoilState(commandMenuSearchState);
+
+  if (!actionConfig) {
+    return null;
+  }
+
+  const handleClick = () => {
+    navigateCommandMenu({
+      page,
+      pageTitle,
+      pageIcon,
+    });
+
+    if (shouldResetSearchState) {
+      setCommandMenuSearchState('');
+    }
+  };
+
+  return <ActionDisplay action={actionConfig} onClick={handleClick} />;
+};
