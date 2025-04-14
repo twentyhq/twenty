ace515eea7c9da83423a785426571d97ed882b8b
Raphaël Bosi <71827178+bosiraphael@users.noreply.github.com>
2025-04-14 11:03:29 +0200

    Fix workflow with manual triggers and no selection not appearing in the command menu (#11544)
    Fix workflow with manual triggers and no selection not appearing in the
    command menu
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- packages/twenty-front/src/modules/action-menu/contexts/ActionMenuContextProviderWorkflowsEnabled.tsx
+++ packages/twenty-front/src/modules/action-menu/contexts/ActionMenuContextProviderWorkflowsEnabled.tsx
@@ -1,10 +1,14 @@
+import { useRunWorkflowRecordActions } from '@/action-menu/actions/record-actions/run-workflow-actions/hooks/useRunWorkflowRecordActions';
+import { useRunWorkflowRecordAgnosticActions } from '@/action-menu/actions/record-agnostic-actions/run-workflow-actions/hooks/useRunWorkflowRecordAgnosticActions';
 import {
   ActionMenuContext,
   ActionMenuContextType,
 } from '@/action-menu/contexts/ActionMenuContext';
 import { useRegisteredActions } from '@/action-menu/hooks/useRegisteredActions';
 import { useShouldActionBeRegisteredParams } from '@/action-menu/hooks/useShouldActionBeRegisteredParams';
+import { contextStoreTargetedRecordsRuleComponentState } from '@/context-store/states/contextStoreTargetedRecordsRuleComponentState';
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
+import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useWorkflowWithCurrentVersion } from '@/workflow/hooks/useWorkflowWithCurrentVersion';
 
 export const ActionMenuContextProviderWorkflowsEnabled = ({
@@ -35,13 +39,33 @@ export const ActionMenuContextProviderWorkflowsEnabled = ({
 
   const actions = useRegisteredActions(shouldBeRegisteredParams);
 
+  const contextStoreTargetedRecordsRule = useRecoilComponentValueV2(
+    contextStoreTargetedRecordsRuleComponentState,
+  );
+
+  const isSingleRecordSelection =
+    contextStoreTargetedRecordsRule.mode === 'selection' &&
+    contextStoreTargetedRecordsRule.selectedRecordIds.length === 1;
+
+  const runWorkflowRecordActions = useRunWorkflowRecordActions({
+    objectMetadataItem,
+    skip: !isSingleRecordSelection,
+  });
+
+  const runWorkflowRecordAgnosticActions =
+    useRunWorkflowRecordAgnosticActions();
+
   return (
     <ActionMenuContext.Provider
       value={{
         isInRightDrawer,
         displayType,
         actionMenuType,
-        actions,
+        actions: [
+          ...actions,
+          ...runWorkflowRecordActions,
+          ...runWorkflowRecordAgnosticActions,
+        ],
       }}
     >
       {children}
