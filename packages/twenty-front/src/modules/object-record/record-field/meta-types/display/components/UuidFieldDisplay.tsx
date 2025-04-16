e9e5768979a3cbf5034976a2dfa3a34020302d3c
guillim <guigloo@msn.com>
2025-04-16 14:44:22 +0200

    part 5

--- packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/UuidFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/meta-types/display/components/UuidFieldDisplay.tsx
@@ -1,8 +1,19 @@
 import { useUuidField } from '@/object-record/record-field/meta-types/hooks/useUuidField';
 import { TextDisplay } from '@/ui/field/display/components/TextDisplay';
+import styled from '@emotion/styled';
+
+const StyledTextDisplay = styled.div`
+  align-items: center;
+  display: flex;
+  height: 20px;
+`;
 
 export const UuidFieldDisplay = () => {
   const { fieldValue } = useUuidField();
 
-  return <TextDisplay text={fieldValue} />;
+  return (
+    <StyledTextDisplay>
+      <TextDisplay text={fieldValue} />
+    </StyledTextDisplay>
+  );
 };
