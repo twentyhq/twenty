3caa76017e0c586b3128188947b1e0c6753b56b0
bosiraphael <raphael.bosi@gmail.com>
2025-04-16 11:36:19 +0200

    improve scroll into view

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableScrollToFocusedElementEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableScrollToFocusedElementEffect.tsx
@@ -1,6 +1,7 @@
 import { focusPositionComponentState } from '@/object-record/record-table/states/focusPositionComponentState';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useEffect } from 'react';
+import { isDefined } from 'twenty-shared/utils';
 
 export const RecordTableScrollToFocusedElementEffect = () => {
   const focusPosition = useRecoilComponentValueV2(focusPositionComponentState);
@@ -13,7 +14,21 @@ export const RecordTableScrollToFocusedElementEffect = () => {
     const focusElement = document.getElementById(
       `record-table-cell-${focusPosition.column}-${focusPosition.row}`,
     );
-    focusElement?.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+
+    if (!focusElement) {
+      return;
+    }
+
+    const firstColumnCell = document.getElementById(`record-table-cell-0-0`);
+    const secondColumnCell = document.getElementById(`record-table-cell-1-0`);
+
+    if (isDefined(firstColumnCell) && isDefined(secondColumnCell)) {
+      const firstColumnWidth = firstColumnCell.offsetWidth;
+      const secondColumnWidth = secondColumnCell.offsetWidth;
+      focusElement.style.scrollMarginLeft = `${firstColumnWidth + secondColumnWidth}px`;
+    }
+
+    focusElement.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
   }, [focusPosition]);
 
   return null;
