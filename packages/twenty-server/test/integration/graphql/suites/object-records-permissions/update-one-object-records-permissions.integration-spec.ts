5d8613bce3a043b5b749f69907af45d2f672ac72
Marie Stoppa <marie.stoppa@essec.edu>
2025-04-17 19:18:12 +0200

    add a first test for permissions V2

--- packages/twenty-server/test/integration/graphql/suites/object-records-permissions/update-one-object-records-permissions.integration-spec.ts
+++ packages/twenty-server/test/integration/graphql/suites/object-records-permissions/update-one-object-records-permissions.integration-spec.ts
@@ -4,65 +4,146 @@ import { PERSON_GQL_FIELDS } from 'test/integration/constants/person-gql-fields.
 import { createOneOperationFactory } from 'test/integration/graphql/utils/create-one-operation-factory.util';
 import { makeGraphqlAPIRequestWithGuestRole } from 'test/integration/graphql/utils/make-graphql-api-request-with-guest-role.util';
 import { makeGraphqlAPIRequest } from 'test/integration/graphql/utils/make-graphql-api-request.util';
+import { updateFeatureFlagFactory } from 'test/integration/graphql/utils/update-feature-flag-factory.util';
 import { updateOneOperationFactory } from 'test/integration/graphql/utils/update-one-operation-factory.util';
 
+import { SEED_APPLE_WORKSPACE_ID } from 'src/database/typeorm-seeds/core/workspaces';
 import { ErrorCode } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
 import { PermissionsExceptionMessage } from 'src/engine/metadata-modules/permissions/permissions.exception';
 
 describe('updateOneObjectRecordsPermissions', () => {
-  const personId = randomUUID();
-
-  beforeAll(async () => {
-    const createPersonOperation = createOneOperationFactory({
-      objectMetadataSingularName: 'person',
-      gqlFields: PERSON_GQL_FIELDS,
-      data: {
-        id: personId,
-        jobTitle: 'Software Engineer',
-      },
-    });
-
-    await makeGraphqlAPIRequest(createPersonOperation);
-  });
-
-  it('should throw a permission error when user does not have permission (guest role)', async () => {
+  describe('permissions V2 disabled', () => {
     const personId = randomUUID();
-    const graphqlOperation = updateOneOperationFactory({
-      objectMetadataSingularName: 'person',
-      gqlFields: PERSON_GQL_FIELDS,
-      recordId: personId,
-      data: {
-        jobTitle: 'Senior Software Engineer',
-      },
+
+    beforeAll(async () => {
+      const createPersonOperation = createOneOperationFactory({
+        objectMetadataSingularName: 'person',
+        gqlFields: PERSON_GQL_FIELDS,
+        data: {
+          id: personId,
+          jobTitle: 'Software Engineer',
+        },
+      });
+
+      await makeGraphqlAPIRequest(createPersonOperation);
     });
 
-    const response = await makeGraphqlAPIRequestWithGuestRole(graphqlOperation);
+    it('should throw a permission error when user does not have permission (guest role)', async () => {
+      const personId = randomUUID();
+      const graphqlOperation = updateOneOperationFactory({
+        objectMetadataSingularName: 'person',
+        gqlFields: PERSON_GQL_FIELDS,
+        recordId: personId,
+        data: {
+          jobTitle: 'Senior Software Engineer',
+        },
+      });
 
-    expect(response.body.data).toStrictEqual({ updatePerson: null });
-    expect(response.body.errors).toBeDefined();
-    expect(response.body.errors[0].message).toBe(
-      PermissionsExceptionMessage.PERMISSION_DENIED,
-    );
-    expect(response.body.errors[0].extensions.code).toBe(ErrorCode.FORBIDDEN);
+      const response =
+        await makeGraphqlAPIRequestWithGuestRole(graphqlOperation);
+
+      expect(response.body.data).toStrictEqual({ updatePerson: null });
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toBe(
+        PermissionsExceptionMessage.PERMISSION_DENIED,
+      );
+      expect(response.body.errors[0].extensions.code).toBe(ErrorCode.FORBIDDEN);
+    });
+
+    it('should update an object record when user has permission (admin role)', async () => {
+      const graphqlOperation = updateOneOperationFactory({
+        objectMetadataSingularName: 'person',
+        gqlFields: PERSON_GQL_FIELDS,
+        recordId: personId,
+        data: {
+          jobTitle: 'Senior Software Engineer',
+        },
+      });
+
+      const response = await makeGraphqlAPIRequest(graphqlOperation);
+
+      expect(response.body.data).toBeDefined();
+      expect(response.body.data.updatePerson).toBeDefined();
+      expect(response.body.data.updatePerson.id).toBe(personId);
+      expect(response.body.data.updatePerson.jobTitle).toBe(
+        'Senior Software Engineer',
+      );
+    });
   });
 
-  it('should update an object record when user has permission (admin role)', async () => {
-    const graphqlOperation = updateOneOperationFactory({
-      objectMetadataSingularName: 'person',
-      gqlFields: PERSON_GQL_FIELDS,
-      recordId: personId,
-      data: {
-        jobTitle: 'Senior Software Engineer',
-      },
+  describe('permissions V2 enabled', () => {
+    const personId = randomUUID();
+
+    beforeAll(async () => {
+      const createPersonOperation = createOneOperationFactory({
+        objectMetadataSingularName: 'person',
+        gqlFields: PERSON_GQL_FIELDS,
+        data: {
+          id: personId,
+          jobTitle: 'Software Engineer',
+        },
+      });
+
+      await makeGraphqlAPIRequest(createPersonOperation);
+
+      const enablePermissionsQuery = updateFeatureFlagFactory(
+        SEED_APPLE_WORKSPACE_ID,
+        'IsPermissionsV2Enabled',
+        true,
+      );
+
+      await makeGraphqlAPIRequest(enablePermissionsQuery);
     });
 
-    const response = await makeGraphqlAPIRequest(graphqlOperation);
+    afterAll(async () => {
+      const disablePermissionsQuery = updateFeatureFlagFactory(
+        SEED_APPLE_WORKSPACE_ID,
+        'IsPermissionsV2Enabled',
+        false,
+      );
 
-    expect(response.body.data).toBeDefined();
-    expect(response.body.data.updatePerson).toBeDefined();
-    expect(response.body.data.updatePerson.id).toBe(personId);
-    expect(response.body.data.updatePerson.jobTitle).toBe(
-      'Senior Software Engineer',
-    );
+      await makeGraphqlAPIRequest(disablePermissionsQuery);
+    });
+
+    it('should throw a permission error when user does not have permission (guest role)', async () => {
+      const graphqlOperation = updateOneOperationFactory({
+        objectMetadataSingularName: 'person',
+        gqlFields: PERSON_GQL_FIELDS,
+        recordId: personId,
+        data: {
+          jobTitle: 'Senior Software Engineer',
+        },
+      });
+
+      const response =
+        await makeGraphqlAPIRequestWithGuestRole(graphqlOperation);
+
+      expect(response.body.data).toStrictEqual({ updatePerson: null });
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toBe(
+        PermissionsExceptionMessage.PERMISSION_DENIED,
+      );
+      expect(response.body.errors[0].extensions.code).toBe(ErrorCode.FORBIDDEN);
+    });
+
+    it('should update an object record when user has permission (admin role)', async () => {
+      const graphqlOperation = updateOneOperationFactory({
+        objectMetadataSingularName: 'person',
+        gqlFields: PERSON_GQL_FIELDS,
+        recordId: personId,
+        data: {
+          jobTitle: 'Senior Software Engineer',
+        },
+      });
+
+      const response = await makeGraphqlAPIRequest(graphqlOperation);
+
+      expect(response.body.data).toBeDefined();
+      expect(response.body.data.updatePerson).toBeDefined();
+      expect(response.body.data.updatePerson.id).toBe(personId);
+      expect(response.body.data.updatePerson.jobTitle).toBe(
+        'Senior Software Engineer',
+      );
+    });
   });
 });
