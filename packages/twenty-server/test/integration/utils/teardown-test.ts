f73a252e89352bed93688e2d050015030402fece
martmull <martmull@hotmail.fr>
2025-04-17 15:20:31 +0200

    Fix ci

--- packages/twenty-server/test/integration/utils/teardown-test.ts
+++ packages/twenty-server/test/integration/utils/teardown-test.ts
@@ -1,10 +1,6 @@
 import 'tsconfig-paths/register';
 
 export default async () => {
-  const pubSub = global.app.get('PUB_SUB');
-
-  pubSub.redisPublisher.quit();
-  pubSub.redisSubscriber.quit();
-  pubSub.close();
-  global.app.close();
+  await global.app.get('PUB_SUB').close();
+  await global.app.close();
 };
