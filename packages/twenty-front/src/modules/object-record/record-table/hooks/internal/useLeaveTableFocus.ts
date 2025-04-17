322cf107c6713e567f692434ffefa2fca06bc666
bosiraphael <raphael.bosi@gmail.com>
2025-04-17 11:40:17 +0200

    simplify useLeaveTableFocus

--- packages/twenty-front/src/modules/object-record/record-table/hooks/internal/useLeaveTableFocus.ts
+++ packages/twenty-front/src/modules/object-record/record-table/hooks/internal/useLeaveTableFocus.ts
@@ -1,23 +1,12 @@
-import { useRecoilCallback } from 'recoil';
-
-import { getSnapshotValue } from '@/ui/utilities/recoil-scope/utils/getSnapshotValue';
-
 import { useResetTableRowSelection } from '@/object-record/record-table/hooks/internal/useResetTableRowSelection';
 import { useSetIsFocusActive } from '@/object-record/record-table/record-table-cell/hooks/useSetIsFocusActive';
 import { RecordTableComponentInstanceContext } from '@/object-record/record-table/states/context/RecordTableComponentInstanceContext';
-import { isFocusActiveComponentState } from '@/object-record/record-table/states/isFocusActiveComponentState';
 import { useAvailableComponentInstanceIdOrThrow } from '@/ui/utilities/state/component-state/hooks/useAvailableComponentInstanceIdOrThrow';
-import { useRecoilComponentCallbackStateV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentCallbackStateV2';
 
 export const useLeaveTableFocus = (recordTableId?: string) => {
   const recordTableIdFromContext = useAvailableComponentInstanceIdOrThrow(
     RecordTableComponentInstanceContext,
     recordTableId,
-  );
-
-  const isFocusActiveState = useRecoilComponentCallbackStateV2(
-    isFocusActiveComponentState,
-    recordTableIdFromContext,
   );
 
   const resetTableRowSelection = useResetTableRowSelection(
@@ -28,19 +17,9 @@ export const useLeaveTableFocus = (recordTableId?: string) => {
     recordTableIdFromContext,
   );
 
-  return useRecoilCallback(
-    ({ snapshot }) =>
-      () => {
-        const isFocusActive = getSnapshotValue(snapshot, isFocusActiveState);
+  return () => {
+    resetTableRowSelection();
 
-        resetTableRowSelection();
-
-        setIsFocusActiveForCurrentPosition(false);
-      },
-    [
-      isFocusActiveState,
-      resetTableRowSelection,
-      setIsFocusActiveForCurrentPosition,
-    ],
-  );
+    setIsFocusActiveForCurrentPosition(false);
+  };
 };
