78ad220055f6c4fb57840fff3c5735be073e04ea
martmull <martmull@hotmail.fr>
2025-04-17 16:56:36 +0200

    Code review returns: greptile

--- packages/twenty-server/src/engine/core-modules/exception-handler/mocks/exception-handler-mock.service.ts
+++ packages/twenty-server/src/engine/core-modules/exception-handler/mocks/exception-handler-mock.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 import { ExceptionHandlerOptions } from 'src/engine/core-modules/exception-handler/interfaces/exception-handler-options.interface';
 
+import { ExceptionHandlerDriverInterface } from 'src/engine/core-modules/exception-handler/interfaces';
+
 @Injectable()
-export class ExceptionHandlerMockService {
+export class ExceptionHandlerMockService
+  implements ExceptionHandlerDriverInterface
+{
   captureExceptions(
-    exceptions: ReadonlyArray<any>,
-    options?: ExceptionHandlerOptions,
+    exceptions: readonly any[],
+    _?: ExceptionHandlerOptions | undefined,
   ): string[] {
     return exceptions.map(() => 'mocked-exception-id');
   }
