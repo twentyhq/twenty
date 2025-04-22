0f782051b81b381f901368b6203ed3034a97d13a
Charles Bochet <charles@twenty.com>
2025-04-22 23:52:49 +0200

    Fix stories

--- packages/twenty-front/src/modules/command-menu/hooks/useFilterActionsWithCommandMenuSearch.ts
+++ packages/twenty-front/src/modules/command-menu/hooks/useFilterActionsWithCommandMenuSearch.ts
@@ -1,38 +1,39 @@
 import { ActionConfig } from '@/action-menu/actions/types/ActionConfig';
 import { getActionLabel } from '@/action-menu/utils/getActionLabel';
 import { isNonEmptyString } from '@sniptt/guards';
-import { useDebounce } from 'use-debounce';
+import { useCallback } from 'react';
+
+const checkInShortcuts = (action: ActionConfig, search: string) => {
+  const concatenatedString = action.hotKeys?.join('') ?? '';
+  return concatenatedString.toLowerCase().includes(search.toLowerCase().trim());
+};
+
+const checkInLabels = (action: ActionConfig, search: string) => {
+  const actionLabel = getActionLabel(action.label);
+  if (isNonEmptyString(actionLabel)) {
+    return actionLabel.toLowerCase().includes(search.toLowerCase());
+  }
+  return false;
+};
+
+type UseFilterActionsWithCommandMenuSearchProps = {
+  commandMenuSearch: string;
+};
 
 export const useFilterActionsWithCommandMenuSearch = ({
   commandMenuSearch,
-}: {
-  commandMenuSearch: string;
-}) => {
-  const [deferredCommandMenuSearch] = useDebounce(commandMenuSearch, 300); // 200ms - 500ms
-
-  const checkInShortcuts = (action: ActionConfig, search: string) => {
-    const concatenatedString = action.hotKeys?.join('') ?? '';
-    return concatenatedString
-      .toLowerCase()
-      .includes(search.toLowerCase().trim());
-  };
-
-  const checkInLabels = (action: ActionConfig, search: string) => {
-    const actionLabel = getActionLabel(action.label);
-    if (isNonEmptyString(actionLabel)) {
-      return actionLabel.toLowerCase().includes(search.toLowerCase());
-    }
-    return false;
-  };
-
-  const filterActionsWithCommandMenuSearch = (actions: ActionConfig[]) => {
-    return actions.filter((action) =>
-      deferredCommandMenuSearch.length > 0
-        ? checkInShortcuts(action, deferredCommandMenuSearch) ||
-          checkInLabels(action, deferredCommandMenuSearch)
-        : true,
-    );
-  };
+}: UseFilterActionsWithCommandMenuSearchProps) => {
+  const filterActionsWithCommandMenuSearch = useCallback(
+    (actions: ActionConfig[]) => {
+      return actions.filter((action) =>
+        commandMenuSearch.length > 0
+          ? checkInShortcuts(action, commandMenuSearch) ||
+            checkInLabels(action, commandMenuSearch)
+          : true,
+      );
+    },
+    [commandMenuSearch],
+  );
 
   return {
     filterActionsWithCommandMenuSearch,
