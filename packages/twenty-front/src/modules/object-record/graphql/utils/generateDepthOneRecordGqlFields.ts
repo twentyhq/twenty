69679cca69512aad555b982403622b6913116f53
Charles Bochet <charles@twenty.com>
2025-04-22 11:59:27 +0200

    Fix

--- packages/twenty-front/src/modules/object-record/graphql/utils/generateDepthOneRecordGqlFields.ts
+++ packages/twenty-front/src/modules/object-record/graphql/utils/generateDepthOneRecordGqlFields.ts
@@ -1,4 +1,5 @@
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
+import { isDefined } from 'twenty-shared/utils';
 
 export type GenerateDepthOneRecordGqlFields = {
   objectMetadataItem: ObjectMetadataItem;
@@ -9,6 +10,11 @@ export const generateDepthOneRecordGqlFields = ({
   objectMetadataItem.fields.reduce<Record<string, true>>((acc, field) => {
     return {
       ...acc,
+      ...(isDefined(field.settings?.joinColumnName)
+        ? {
+            [field.settings.joinColumnName]: true,
+          }
+        : {}),
       [field.name]: true,
     };
   }, {});
