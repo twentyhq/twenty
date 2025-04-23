7a5abf2386eacf564cbe9e068d9e45c19f948b43
Devessier <baptiste@devessier.fr>
2025-04-23 15:16:11 +0200

    refactor: move everything in recoil callback and document why we exit

--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowRunDiagramCanvasEffect.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowRunDiagramCanvasEffect.tsx
@@ -3,69 +3,59 @@ import { getSnapshotValue } from '@/ui/utilities/state/utils/getSnapshotValue';
 import { workflowIdState } from '@/workflow/states/workflowIdState';
 import { workflowDiagramStatusState } from '@/workflow/workflow-diagram/states/workflowDiagramStatusState';
 import { workflowSelectedNodeState } from '@/workflow/workflow-diagram/states/workflowSelectedNodeState';
-import {
-  WorkflowDiagramNode,
-  WorkflowRunDiagramStepNodeData,
-} from '@/workflow/workflow-diagram/types/WorkflowDiagram';
+import { WorkflowRunDiagramNode } from '@/workflow/workflow-diagram/types/WorkflowDiagram';
 import { getWorkflowNodeIconKey } from '@/workflow/workflow-diagram/utils/getWorkflowNodeIconKey';
 import { OnSelectionChangeParams, useOnSelectionChange } from '@xyflow/react';
-import { useRecoilCallback, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilCallback } from 'recoil';
 import { isDefined } from 'twenty-shared/utils';
 import { useIcons } from 'twenty-ui/display';
 
 export const WorkflowRunDiagramCanvasEffect = () => {
   const { getIcon } = useIcons();
 
-  const workflowId = useRecoilValue(workflowIdState);
-  const setWorkflowSelectedNode = useSetRecoilState(workflowSelectedNodeState);
   const { openWorkflowRunViewStepInCommandMenu } = useWorkflowCommandMenu();
 
   const handleSelectionChange = useRecoilCallback(
-    ({ snapshot }) =>
+    ({ snapshot, set }) =>
       ({ nodes }: OnSelectionChangeParams) => {
+        const workflowId = getSnapshotValue(snapshot, workflowIdState);
+
+        if (!isDefined(workflowId)) {
+          throw new Error('Expected the workflowId to be defined.');
+        }
+
         const workflowDiagramStatus = getSnapshotValue(
           snapshot,
           workflowDiagramStatusState,
         );
 
-        console.log('handle selection change', {
-          nodes,
-          workflowDiagramStatus,
-        });
-
+        // The `handleSelectionChange` function is called when the diagram initializes and
+        // a node is selected. In this case, we don't want to execute the rest of this function.
+        // We open the Side Panel® synchronously after ReactFlow is initialized and a node is selected,
+        // animations perform better that way.
         if (workflowDiagramStatus !== 'done') {
-          console.log('handle selection change [aborted]');
-
           return;
         }
 
-        const selectedNode = nodes[0] as WorkflowDiagramNode | undefined;
+        const selectedNode = nodes[0] as WorkflowRunDiagramNode | undefined;
 
         if (!isDefined(selectedNode)) {
           return;
         }
 
-        setWorkflowSelectedNode(selectedNode.id);
+        set(workflowSelectedNodeState, selectedNode.id);
 
-        const selectedNodeData =
-          selectedNode.data as WorkflowRunDiagramStepNodeData;
+        const selectedNodeData = selectedNode.data;
 
-        if (isDefined(workflowId)) {
-          openWorkflowRunViewStepInCommandMenu({
-            workflowId,
-            title: selectedNodeData.name,
-            icon: getIcon(getWorkflowNodeIconKey(selectedNodeData)),
-            workflowSelectedNode: selectedNode.id,
-            stepExecutionStatus: selectedNodeData.runStatus,
-          });
-        }
+        openWorkflowRunViewStepInCommandMenu({
+          workflowId,
+          title: selectedNodeData.name,
+          icon: getIcon(getWorkflowNodeIconKey(selectedNodeData)),
+          workflowSelectedNode: selectedNode.id,
+          stepExecutionStatus: selectedNodeData.runStatus,
+        });
       },
-    [
-      getIcon,
-      openWorkflowRunViewStepInCommandMenu,
-      setWorkflowSelectedNode,
-      workflowId,
-    ],
+    [getIcon, openWorkflowRunViewStepInCommandMenu],
   );
 
   useOnSelectionChange({
