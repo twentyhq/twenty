0326ad6f9d37aefa7abb4024869320cfc2d3b6b3
bosiraphael <raphael.bosi@gmail.com>
2025-04-24 12:14:04 +0200

    make object options menu selectable

--- packages/twenty-front/src/modules/object-record/object-options-dropdown/components/ObjectOptionsDropdownMenuContent.tsx
+++ packages/twenty-front/src/modules/object-record/object-options-dropdown/components/ObjectOptionsDropdownMenuContent.tsx
@@ -1,6 +1,7 @@
 import { Key } from 'ts-key-enum';
 
 import { ObjectOptionsDropdownMenuViewName } from '@/object-record/object-options-dropdown/components/ObjectOptionsDropdownMenuViewName';
+import { OBJECT_OPTIONS_DROPDOWN_ID } from '@/object-record/object-options-dropdown/constants/ObjectOptionsDropdownId';
 import { useObjectOptionsForBoard } from '@/object-record/object-options-dropdown/hooks/useObjectOptionsForBoard';
 import { useOptionsDropdown } from '@/object-record/object-options-dropdown/hooks/useOptionsDropdown';
 import { recordGroupFieldMetadataComponentState } from '@/object-record/record-group/states/recordGroupFieldMetadataComponentState';
@@ -9,6 +10,9 @@ import { SnackBarVariant } from '@/ui/feedback/snack-bar-manager/components/Snac
 import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
 import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/DropdownMenuItemsContainer';
 import { DropdownMenuSeparator } from '@/ui/layout/dropdown/components/DropdownMenuSeparator';
+import { SelectableListItem } from '@/ui/layout/selectable-list/components/SelectableItem';
+import { SelectableList } from '@/ui/layout/selectable-list/components/SelectableList';
+import { selectedItemIdComponentState } from '@/ui/layout/selectable-list/states/selectedItemIdComponentState';
 import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useSetRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentStateV2';
@@ -75,92 +79,155 @@ export const ObjectOptionsDropdownMenuContent = () => {
 
   const isDefaultView = currentView?.key === 'INDEX';
 
+  const selectableItemIdArray = [
+    'Layout',
+    'Fields',
+    ...(isDefaultView ? [] : ['Group', 'Compact view']),
+    'Copy link to view',
+    ...(isDefaultView ? [] : ['Delete view']),
+  ];
+
+  const selectedItemId = useRecoilComponentValueV2(
+    selectedItemIdComponentState,
+    OBJECT_OPTIONS_DROPDOWN_ID,
+  );
+
   return (
     <>
       {currentView && (
         <ObjectOptionsDropdownMenuViewName currentView={currentView} />
       )}
       <DropdownMenuSeparator />
-      <DropdownMenuItemsContainer scrollable={false}>
-        <MenuItem
-          onClick={() => onContentChange('layout')}
-          LeftIcon={viewTypeIconMapping(currentView?.type ?? ViewType.Table)}
-          text={t`Layout`}
-          contextualText={`${capitalize(currentView?.type ?? '')}`}
-          hasSubMenu
-        />
-      </DropdownMenuItemsContainer>
-      <DropdownMenuSeparator />
-
-      <DropdownMenuItemsContainer scrollable={false}>
-        <MenuItem
-          onClick={() => onContentChange('fields')}
-          LeftIcon={IconListDetails}
-          text={t`Fields`}
-          contextualText={`${visibleBoardFields.length} shown`}
-          hasSubMenu
-        />
-
-        <div id="group-by-menu-item">
-          <MenuItem
-            onClick={() =>
-              isDefined(recordGroupFieldMetadata)
-                ? onContentChange('recordGroups')
-                : onContentChange('recordGroupFields')
-            }
-            LeftIcon={IconLayoutList}
-            text={t`Group`}
-            contextualText={
-              isDefaultView
-                ? t`Not available on Default View`
-                : recordGroupFieldMetadata?.label
-            }
-            hasSubMenu
-            disabled={isDefaultView}
-          />
-        </div>
-        {!isGroupByEnabled && (
-          <AppTooltip
-            anchorSelect={`#group-by-menu-item`}
-            content={t`Not available on Default View`}
-            noArrow
-            place="bottom"
-            width="100%"
-          />
-        )}
+      <SelectableList
+        selectableListInstanceId={OBJECT_OPTIONS_DROPDOWN_ID}
+        hotkeyScope={TableOptionsHotkeyScope.Dropdown}
+        selectableItemIdArray={selectableItemIdArray}
+      >
+        <DropdownMenuItemsContainer scrollable={false}>
+          <SelectableListItem
+            itemId="Layout"
+            onEnter={() => onContentChange('layout')}
+          >
+            <MenuItem
+              selected={selectedItemId === 'Layout'}
+              onClick={() => onContentChange('layout')}
+              LeftIcon={viewTypeIconMapping(
+                currentView?.type ?? ViewType.Table,
+              )}
+              text={t`Layout`}
+              contextualText={`${capitalize(currentView?.type ?? '')}`}
+              hasSubMenu
+            />
+          </SelectableListItem>
+        </DropdownMenuItemsContainer>
         <DropdownMenuSeparator />
 
-        <MenuItem
-          onClick={() => {
-            const currentUrl = window.location.href;
-            navigator.clipboard.writeText(currentUrl);
-            enqueueSnackBar('Link copied to clipboard', {
-              variant: SnackBarVariant.Success,
-              icon: <IconCopy size={theme.icon.size.md} />,
-              duration: 2000,
-            });
-          }}
-          LeftIcon={IconCopy}
-          text={t`Copy link to view`}
-        />
-        <div id="delete-view-menu-item">
-          <MenuItem
-            onClick={() => handleDelete()}
-            LeftIcon={IconTrash}
-            text={t`Delete view`}
-            disabled={currentView?.key === 'INDEX'}
-          />
-        </div>
-        {currentView?.key === 'INDEX' && (
-          <AppTooltip
-            anchorSelect={`#delete-view-menu-item`}
-            content={t`Not available on Default View`}
-            noArrow
-            place="bottom"
-            width="100%"
-          />
-        )}
-      </DropdownMenuItemsContainer>
+        <DropdownMenuItemsContainer scrollable={false}>
+          <SelectableListItem
+            itemId="Fields"
+            onEnter={() => onContentChange('fields')}
+          >
+            <MenuItem
+              selected={selectedItemId === 'Fields'}
+              onClick={() => onContentChange('fields')}
+              LeftIcon={IconListDetails}
+              text={t`Fields`}
+              contextualText={`${visibleBoardFields.length} shown`}
+              hasSubMenu
+            />
+          </SelectableListItem>
+
+          <div id="group-by-menu-item">
+            <SelectableListItem
+              itemId="Group"
+              onEnter={() =>
+                isDefined(recordGroupFieldMetadata)
+                  ? onContentChange('recordGroups')
+                  : onContentChange('recordGroupFields')
+              }
+            >
+              <MenuItem
+                selected={selectedItemId === 'Group'}
+                onClick={() =>
+                  isDefined(recordGroupFieldMetadata)
+                    ? onContentChange('recordGroups')
+                    : onContentChange('recordGroupFields')
+                }
+                LeftIcon={IconLayoutList}
+                text={t`Group`}
+                contextualText={
+                  isDefaultView
+                    ? t`Not available on Default View`
+                    : recordGroupFieldMetadata?.label
+                }
+                hasSubMenu
+                disabled={isDefaultView}
+              />
+            </SelectableListItem>
+          </div>
+          {!isGroupByEnabled && (
+            <AppTooltip
+              anchorSelect={`#group-by-menu-item`}
+              content={t`Not available on Default View`}
+              noArrow
+              place="bottom"
+              width="100%"
+            />
+          )}
+          <DropdownMenuSeparator />
+
+          <SelectableListItem
+            itemId="Copy link to view"
+            onEnter={() => {
+              const currentUrl = window.location.href;
+              navigator.clipboard.writeText(currentUrl);
+              enqueueSnackBar('Link copied to clipboard', {
+                variant: SnackBarVariant.Success,
+                icon: <IconCopy size={theme.icon.size.md} />,
+                duration: 2000,
+              });
+            }}
+          >
+            <MenuItem
+              selected={selectedItemId === 'Copy link to view'}
+              onClick={() => {
+                const currentUrl = window.location.href;
+                navigator.clipboard.writeText(currentUrl);
+                enqueueSnackBar('Link copied to clipboard', {
+                  variant: SnackBarVariant.Success,
+                  icon: <IconCopy size={theme.icon.size.md} />,
+                  duration: 2000,
+                });
+              }}
+              LeftIcon={IconCopy}
+              text={t`Copy link to view`}
+            />
+          </SelectableListItem>
+          <div id="delete-view-menu-item">
+            <SelectableListItem
+              itemId="Delete view"
+              onEnter={() => handleDelete()}
+            >
+              <MenuItem
+                selected={selectedItemId === 'Delete view'}
+                onClick={() => handleDelete()}
+                LeftIcon={IconTrash}
+                text={t`Delete view`}
+                disabled={currentView?.key === 'INDEX'}
+              />
+            </SelectableListItem>
+          </div>
+          {currentView?.key === 'INDEX' && (
+            <AppTooltip
+              anchorSelect={`#delete-view-menu-item`}
+              content={t`Not available on Default View`}
+              noArrow
+              place="bottom"
+              width="100%"
+            />
+          )}
+        </DropdownMenuItemsContainer>
+      </SelectableList>
     </>
   );
 };
