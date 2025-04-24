a66d6a54249e59a71548cad6f74fb68344957b8b
Weiko <corentin@twenty.com>
2025-04-24 11:19:26 +0200

    remove object-level during creation until better fix

--- packages/twenty-front/src/modules/settings/roles/role/components/SettingsRole.tsx
+++ packages/twenty-front/src/modules/settings/roles/role/components/SettingsRole.tsx
@@ -306,6 +306,7 @@ export const SettingsRole = ({ roleId, isCreateMode }: SettingsRoleProps) => {
           <SettingsRolePermissions
             roleId={roleId}
             isEditable={isRoleEditable}
+            isCreateMode={isCreateMode}
           />
         )}
         {activeTabId === SETTINGS_ROLE_DETAIL_TABS.TABS_IDS.SETTINGS && (
