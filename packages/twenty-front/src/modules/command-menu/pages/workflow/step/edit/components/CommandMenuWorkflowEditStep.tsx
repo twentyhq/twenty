6afd39da37526f4f99e1e72c319f5fddc24c8fca
Devessier <baptiste@devessier.fr>
2025-04-28 11:19:55 +0200

    refactor: one component per file for CommandMenuWorkflowEditStep

--- packages/twenty-front/src/modules/command-menu/pages/workflow/step/edit/components/CommandMenuWorkflowEditStep.tsx
+++ packages/twenty-front/src/modules/command-menu/pages/workflow/step/edit/components/CommandMenuWorkflowEditStep.tsx
@@ -1,4 +1,3 @@
-import { ActionMenuContext } from '@/action-menu/contexts/ActionMenuContext';
 import { workflowIdComponentState } from '@/command-menu/pages/workflow/states/workflowIdComponentState';
 import { CommandMenuWorkflowEditStepContent } from '@/command-menu/pages/workflow/step/edit/components/CommandMenuWorkflowEditStepContent';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
@@ -6,7 +5,6 @@ import { useWorkflowWithCurrentVersion } from '@/workflow/hooks/useWorkflowWithC
 import { WorkflowStepContextProvider } from '@/workflow/states/context/WorkflowStepContext';
 import { getWorkflowVisualizerComponentInstanceId } from '@/workflow/utils/getWorkflowVisualizerComponentInstanceId';
 import { WorkflowVisualizerComponentInstanceContext } from '@/workflow/workflow-diagram/states/contexts/WorkflowVisualizerComponentInstanceContext';
-import { useContext } from 'react';
 import { isDefined } from 'twenty-shared/utils';
 
 export const CommandMenuWorkflowEditStep = () => {
@@ -15,25 +13,6 @@ export const CommandMenuWorkflowEditStep = () => {
     throw new Error('Expected workflowIdComponentState to be defined');
   }
 
-  const { isInRightDrawer } = useContext(ActionMenuContext);
-
-  console.log({ isInRightDrawer });
-
-  return (
-    <WorkflowVisualizerComponentInstanceContext.Provider
-      value={{
-        instanceId: getWorkflowVisualizerComponentInstanceId({
-          id: workflowId,
-          isInRightDrawer,
-        }),
-      }}
-    >
-      <Child workflowId={workflowId} />
-    </WorkflowVisualizerComponentInstanceContext.Provider>
-  );
-};
-
-const Child = ({ workflowId }: { workflowId: string }) => {
   const workflow = useWorkflowWithCurrentVersion(workflowId);
 
   if (!isDefined(workflow)) {
@@ -41,10 +20,18 @@ const Child = ({ workflowId }: { workflowId: string }) => {
   }
 
   return (
-    <WorkflowStepContextProvider
-      value={{ workflowVersionId: workflow.currentVersion.id }}
+    <WorkflowVisualizerComponentInstanceContext.Provider
+      value={{
+        instanceId: getWorkflowVisualizerComponentInstanceId({
+          recordId: workflowId,
+        }),
+      }}
     >
-      <CommandMenuWorkflowEditStepContent workflow={workflow} />
-    </WorkflowStepContextProvider>
+      <WorkflowStepContextProvider
+        value={{ workflowVersionId: workflow.currentVersion.id }}
+      >
+        <CommandMenuWorkflowEditStepContent workflow={workflow} />
+      </WorkflowStepContextProvider>
+    </WorkflowVisualizerComponentInstanceContext.Provider>
   );
 };
