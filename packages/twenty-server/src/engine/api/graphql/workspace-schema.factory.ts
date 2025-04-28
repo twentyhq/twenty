271ab78cc461d1607b71dc5bcb9992bfda211910
Marie Stoppa <marie.stoppa@essec.edu>
2025-04-28 17:43:45 +0200

    use recomputed cache version

--- packages/twenty-server/src/engine/api/graphql/workspace-schema.factory.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-schema.factory.ts
@@ -89,6 +89,7 @@ export class WorkspaceSchemaFactory {
           });
 
         objectMetadataMaps = recomputed?.recomputedObjectMetadataMaps;
+        currentCacheVersion = recomputed?.recomputedMetadataVersion;
       }
     }
 
