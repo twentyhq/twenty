5d8ee9a4f7b35ee13f0f907d22a97672223be1cf
guillim <guigloo@msn.com>
2025-04-28 18:13:55 +0200

    fix-default-picture-url

--- packages/twenty-front/src/modules/settings/profile/components/ProfilePictureUploader.tsx
+++ packages/twenty-front/src/modules/settings/profile/components/ProfilePictureUploader.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 import { currentWorkspaceMemberState } from '@/auth/states/currentWorkspaceMemberState';
 import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { useUpdateOneRecord } from '@/object-record/hooks/useUpdateOneRecord';
 import { ImageInput } from '@/ui/input/components/ImageInput';
+import { getImageAbsoluteURI, isDefined } from 'twenty-shared/utils';
+import { REACT_APP_SERVER_BASE_URL } from '~/config';
 import { useUploadProfilePictureMutation } from '~/generated/graphql';
 import { isUndefinedOrNull } from '~/utils/isUndefinedOrNull';
-import { isDefined } from 'twenty-shared/utils';
 
 export const ProfilePictureUploader = () => {
   const [uploadPicture, { loading: isUploading }] =
@@ -20,10 +21,29 @@ export const ProfilePictureUploader = () => {
   const [uploadController, setUploadController] =
     useState<AbortController | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [validPictureURL, setValidPictureURL] = useState<string | null>(null);
 
   const { updateOneRecord } = useUpdateOneRecord({
     objectNameSingular: CoreObjectNameSingular.WorkspaceMember,
   });
+
+  useEffect(() => {
+    const checkImageValidity = async () => {
+      const pictureURL = getImageAbsoluteURI({
+        imageUrl: currentWorkspaceMember?.avatarUrl || '',
+        baseUrl: REACT_APP_SERVER_BASE_URL,
+      });
+
+      const response = await fetch(pictureURL || '', {
+        method: 'HEAD',
+      });
+      if (response.ok) {
+        setValidPictureURL(pictureURL);
+      }
+    };
+
+    checkImageValidity();
+  }, [currentWorkspaceMember?.avatarUrl]);
 
   const handleUpload = async (file: File) => {
     if (isUndefinedOrNull(file)) {
@@ -96,6 +116,7 @@ export const ProfilePictureUploader = () => {
       });
 
       setCurrentWorkspaceMember({ ...currentWorkspaceMember, avatarUrl: null });
+      setValidPictureURL(null);
     } catch (error) {
       setErrorMessage('An error occurred while removing the picture.');
     }
@@ -103,7 +124,7 @@ export const ProfilePictureUploader = () => {
 
   return (
     <ImageInput
-      picture={currentWorkspaceMember?.avatarUrl}
+      picture={validPictureURL}
       onUpload={handleUpload}
       onRemove={handleRemove}
       onAbort={handleAbort}
