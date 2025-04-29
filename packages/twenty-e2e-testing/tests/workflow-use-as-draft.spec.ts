19f46a00915eb923cbeaf3c12d72c1727bacdde9
Baptiste Devessier <baptiste@devessier.fr>
2025-04-29 15:51:59 +0200

    Fix e2e tests (#11792)

--- packages/twenty-e2e-testing/tests/workflow-use-as-draft.spec.ts
+++ packages/twenty-e2e-testing/tests/workflow-use-as-draft.spec.ts
@@ -8,25 +8,19 @@ test('Use an old version as draft', async ({ workflowVisualizer, page }) => {
 
   await workflowVisualizer.background.click();
 
-  await Promise.all([
-    expect(workflowVisualizer.workflowStatus).toHaveText('Active'),
+  await workflowVisualizer.activateWorkflowButton.click();
 
-    workflowVisualizer.activateWorkflowButton.click(),
-  ]);
+  await expect(workflowVisualizer.workflowStatus).toHaveText('Active');
 
-  await Promise.all([
-    expect(workflowVisualizer.workflowStatus).toHaveText('Draft'),
+  await workflowVisualizer.createStep('delete-record');
 
-    workflowVisualizer.createStep('delete-record'),
-  ]);
+  await expect(workflowVisualizer.workflowStatus).toHaveText('Draft');
 
-  await workflowVisualizer.background.click();
+  await workflowVisualizer.closeSidePanel();
 
-  await Promise.all([
-    expect(workflowVisualizer.workflowStatus).toHaveText('Active'),
+  await workflowVisualizer.activateWorkflowButton.click();
 
-    workflowVisualizer.activateWorkflowButton.click(),
-  ]);
+  await expect(workflowVisualizer.workflowStatus).toHaveText('Active');
 
   await expect(workflowVisualizer.triggerNode).toContainText(
     'Record is Created',
@@ -41,6 +35,8 @@ test('Use an old version as draft', async ({ workflowVisualizer, page }) => {
   const workflowsLink = page.getByRole('link', { name: 'Workflows' });
   await workflowsLink.click();
 
+  await workflowVisualizer.setWorkflowsOpenInMode('record-page');
+
   const recordTableRowForWorkflow = page.getByRole('row', {
     name: workflowVisualizer.workflowName,
   });
@@ -48,7 +44,7 @@ test('Use an old version as draft', async ({ workflowVisualizer, page }) => {
   const linkToWorkflow = recordTableRowForWorkflow.getByRole('link', {
     name: workflowVisualizer.workflowName,
   });
-  expect(linkToWorkflow).toBeVisible();
+  await expect(linkToWorkflow).toBeVisible();
 
   const linkToFirstWorkflowVersion = recordTableRowForWorkflow.getByRole(
     'link',
@@ -69,11 +65,9 @@ test('Use an old version as draft', async ({ workflowVisualizer, page }) => {
   ]);
   await expect(workflowVisualizer.getAllStepNodes()).toHaveCount(1);
 
-  await Promise.all([
-    page.waitForURL(`/object/workflow/${workflowVisualizer.workflowId}`),
+  await workflowVisualizer.useAsDraftButton.click();
 
-    workflowVisualizer.useAsDraftButton.click(),
-  ]);
+  await page.waitForURL(`/object/workflow/${workflowVisualizer.workflowId}`);
 
   await expect(workflowVisualizer.workflowStatus).toHaveText('Draft');
   await expect(workflowVisualizer.useAsDraftButton).not.toBeVisible();
@@ -96,17 +90,13 @@ test('Use an old version as draft while having a pending draft version', async (
 
   await workflowVisualizer.background.click();
 
-  await Promise.all([
-    expect(workflowVisualizer.workflowStatus).toHaveText('Active'),
+  await workflowVisualizer.activateWorkflowButton.click();
 
-    workflowVisualizer.activateWorkflowButton.click(),
-  ]);
+  await expect(workflowVisualizer.workflowStatus).toHaveText('Active');
 
-  await Promise.all([
-    expect(workflowVisualizer.workflowStatus).toHaveText('Draft'),
+  await workflowVisualizer.createStep('delete-record');
 
-    workflowVisualizer.createStep('delete-record'),
-  ]);
+  await expect(workflowVisualizer.workflowStatus).toHaveText('Draft');
 
   await expect(workflowVisualizer.triggerNode).toContainText(
     'Record is Created',
@@ -130,7 +120,7 @@ test('Use an old version as draft while having a pending draft version', async (
   const linkToWorkflow = recordTableRowForWorkflow.getByRole('link', {
     name: workflowVisualizer.workflowName,
   });
-  expect(linkToWorkflow).toBeVisible();
+  await expect(linkToWorkflow).toBeVisible();
 
   const linkToFirstWorkflowVersion = recordTableRowForWorkflow.getByRole(
     'link',
@@ -151,17 +141,13 @@ test('Use an old version as draft while having a pending draft version', async (
   ]);
   await expect(workflowVisualizer.getAllStepNodes()).toHaveCount(1);
 
-  await Promise.all([
-    expect(workflowVisualizer.overrideDraftButton).toBeVisible(),
+  await workflowVisualizer.useAsDraftButton.click();
 
-    workflowVisualizer.useAsDraftButton.click(),
-  ]);
+  await expect(workflowVisualizer.overrideDraftButton).toBeVisible();
 
-  await Promise.all([
-    page.waitForURL(`/object/workflow/${workflowVisualizer.workflowId}`),
+  await workflowVisualizer.overrideDraftButton.click();
 
-    workflowVisualizer.overrideDraftButton.click(),
-  ]);
+  await page.waitForURL(`/object/workflow/${workflowVisualizer.workflowId}`);
 
   await expect(workflowVisualizer.workflowStatus).toHaveText('Draft');
   await expect(workflowVisualizer.useAsDraftButton).not.toBeVisible();
