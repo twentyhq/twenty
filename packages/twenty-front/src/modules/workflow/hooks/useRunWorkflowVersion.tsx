e1273ea745c17b476aaf71a473a484e2c7d6c736
Devessier <baptiste@devessier.fr>
2025-04-29 14:09:04 +0200

    revert: do not open the manually executed workflow runs in the side panel by default as it doesn't work properly yet

--- packages/twenty-front/src/modules/workflow/hooks/useRunWorkflowVersion.tsx
+++ packages/twenty-front/src/modules/workflow/hooks/useRunWorkflowVersion.tsx
@@ -1,22 +1,28 @@
-import { useOpenRecordInCommandMenu } from '@/command-menu/hooks/useOpenRecordInCommandMenu';
-import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { SnackBarVariant } from '@/ui/feedback/snack-bar-manager/components/SnackBar';
+import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
 import { RUN_WORKFLOW_VERSION } from '@/workflow/graphql/mutations/runWorkflowVersion';
 import { useApolloClient, useMutation } from '@apollo/client';
+import { useTheme } from '@emotion/react';
+import { useLingui } from '@lingui/react/macro';
 import {
   RunWorkflowVersionMutation,
   RunWorkflowVersionMutationVariables,
 } from '~/generated/graphql';
+import { IconSettingsAutomation } from 'twenty-ui/display';
 
 export const useRunWorkflowVersion = () => {
   const apolloClient = useApolloClient();
-  const [runWorkflowVersionMutate] = useMutation<
+  const [mutate] = useMutation<
     RunWorkflowVersionMutation,
     RunWorkflowVersionMutationVariables
   >(RUN_WORKFLOW_VERSION, {
     client: apolloClient,
   });
 
-  const { openRecordInCommandMenu } = useOpenRecordInCommandMenu();
+  const { enqueueSnackBar } = useSnackBar();
+
+  const theme = useTheme();
+  const { t } = useLingui();
 
   const runWorkflowVersion = async ({
     workflowVersionId,
@@ -25,15 +31,33 @@ export const useRunWorkflowVersion = () => {
     workflowVersionId: string;
     payload?: Record<string, any>;
   }) => {
-    const { data } = await runWorkflowVersionMutate({
+    const { data } = await mutate({
       variables: { input: { workflowVersionId, payload } },
     });
 
     const workflowRunId = data?.runWorkflowVersion?.workflowRunId;
 
-    openRecordInCommandMenu({
-      recordId: workflowRunId,
-      objectNameSingular: CoreObjectNameSingular.WorkflowRun,
+    if (!workflowRunId) {
+      enqueueSnackBar(t`Workflow run failed`, {
+        variant: SnackBarVariant.Error,
+      });
+      return;
+    }
+
+    const link = `/object/workflowRun/${workflowRunId}`;
+
+    enqueueSnackBar(t`Workflow is running...`, {
+      variant: SnackBarVariant.Success,
+      icon: (
+        <IconSettingsAutomation
+          size={16}
+          color={theme.snackBar.success.color}
+        />
+      ),
+      link: {
+        href: link,
+        text: t`View execution details`,
+      },
     });
   };
 
