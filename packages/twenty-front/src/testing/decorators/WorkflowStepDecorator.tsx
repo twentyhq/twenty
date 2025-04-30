91d1df083ca3d657be79f6a04630c9de7ee8ec41
Devessier <baptiste@devessier.fr>
2025-04-30 11:56:32 +0200

    test: fix stories

--- packages/twenty-front/src/testing/decorators/WorkflowStepDecorator.tsx
+++ packages/twenty-front/src/testing/decorators/WorkflowStepDecorator.tsx
@@ -1,56 +1,75 @@
-import { useSetRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentStateV2';
 import { useStepsOutputSchema } from '@/workflow/hooks/useStepsOutputSchema';
 import { WorkflowStepContextProvider } from '@/workflow/states/context/WorkflowStepContext';
 import { flowComponentState } from '@/workflow/states/flowComponentState';
 import { workflowVisualizerWorkflowIdComponentState } from '@/workflow/states/workflowVisualizerWorkflowIdComponentState';
 import { WorkflowVersion } from '@/workflow/types/Workflow';
+import { WorkflowVisualizerComponentInstanceContext } from '@/workflow/workflow-diagram/states/contexts/WorkflowVisualizerComponentInstanceContext';
 import { workflowSelectedNodeComponentState } from '@/workflow/workflow-diagram/states/workflowSelectedNodeComponentState';
 import { Decorator } from '@storybook/react';
 import { useEffect, useState } from 'react';
+import { useRecoilCallback } from 'recoil';
 import {
   getWorkflowMock,
   getWorkflowNodeIdMock,
 } from '~/testing/mock-data/workflow';
 
 export const WorkflowStepDecorator: Decorator = (Story) => {
-  const setWorkflowVisualizerWorkflowId = useSetRecoilComponentStateV2(
-    workflowVisualizerWorkflowIdComponentState,
-  );
-  const setWorkflowSelectedNode = useSetRecoilComponentStateV2(
-    workflowSelectedNodeComponentState,
-  );
-  const setFlow = useSetRecoilComponentStateV2(flowComponentState);
+  const workflowVisualizerComponentInstanceId = 'workflow-visualizer-test-id';
+
   const workflowVersion = getWorkflowMock().versions.edges[0]
     .node as WorkflowVersion;
   const { populateStepsOutputSchema } = useStepsOutputSchema();
   const [ready, setReady] = useState(false);
 
+  const handleMount = useRecoilCallback(
+    ({ set }) =>
+      () => {
+        set(
+          workflowVisualizerWorkflowIdComponentState.atomFamily({
+            instanceId: workflowVisualizerComponentInstanceId,
+          }),
+          getWorkflowMock().id,
+        );
+        set(
+          workflowSelectedNodeComponentState.atomFamily({
+            instanceId: workflowVisualizerComponentInstanceId,
+          }),
+          getWorkflowNodeIdMock(),
+        );
+        set(
+          flowComponentState.atomFamily({
+            instanceId: workflowVisualizerComponentInstanceId,
+          }),
+          {
+            workflowVersionId: workflowVersion.id,
+            trigger: workflowVersion.trigger,
+            steps: workflowVersion.steps,
+          },
+        );
+        populateStepsOutputSchema(workflowVersion);
+        setReady(true);
+      },
+    [populateStepsOutputSchema, workflowVersion],
+  );
+
   useEffect(() => {
-    setWorkflowVisualizerWorkflowId(getWorkflowMock().id);
-    setWorkflowSelectedNode(getWorkflowNodeIdMock());
-    setFlow({
-      workflowVersionId: workflowVersion.id,
-      trigger: workflowVersion.trigger,
-      steps: workflowVersion.steps,
-    });
-    populateStepsOutputSchema(workflowVersion);
-    setReady(true);
-  }, [
-    setWorkflowVisualizerWorkflowId,
-    setWorkflowSelectedNode,
-    populateStepsOutputSchema,
-    workflowVersion,
-    setFlow,
-  ]);
+    handleMount();
+  }, [handleMount]);
 
   return (
-    <WorkflowStepContextProvider
+    <WorkflowVisualizerComponentInstanceContext.Provider
       value={{
-        workflowVersionId: workflowVersion.id,
-        workflowRunId: '123',
+        instanceId: workflowVisualizerComponentInstanceId,
       }}
     >
-      {ready && <Story />}
-    </WorkflowStepContextProvider>
+      <WorkflowStepContextProvider
+        value={{
+          workflowVersionId: workflowVersion.id,
+          workflowRunId: '123',
+        }}
+      >
+        {ready && <Story />}
+      </WorkflowStepContextProvider>
+    </WorkflowVisualizerComponentInstanceContext.Provider>
   );
 };
