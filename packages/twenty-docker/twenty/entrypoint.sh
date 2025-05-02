2c57fb477fa74592750f66762c28337e5162a09d
prastoin <paul@twenty.com>
2025-05-02 11:58:46 +0200

    refactor: entrypoint sh

--- packages/twenty-docker/twenty/entrypoint.sh
+++ packages/twenty-docker/twenty/entrypoint.sh
@@ -1,36 +1,37 @@
 #!/bin/sh
 set -e
 
-# Check if the initialization has already been done and that we enabled automatic migration
-if [ "${DISABLE_DB_MIGRATIONS}" != "true" ] && [ ! -f /app/docker-data/db_status ]; then
-    echo "Running database setup and migrations..."
+setup_and_migrate_db() {
+    if [ "${DISABLE_DB_MIGRATIONS}" = "true" ]; then
+        return
+    fi
 
-    # Creating the database if it doesn't exist
-    PGUSER=$(echo $PG_DATABASE_URL | awk -F '//' '{print $2}' | awk -F ':' '{print $1}')
-    PGPASS=$(echo $PG_DATABASE_URL | awk -F ':' '{print $3}' | awk -F '@' '{print $1}')
-    PGHOST=$(echo $PG_DATABASE_URL | awk -F '@' '{print $2}' | awk -F ':' '{print $1}')
-    PGPORT=$(echo $PG_DATABASE_URL | awk -F ':' '{print $4}' | awk -F '/' '{print $1}')
-    PGDATABASE=$(echo $PG_DATABASE_URL | awk -F ':' '{print $4}' | awk -F '/' '{print $2}')
-    PGPASSWORD=${PGPASS} psql -h ${PGHOST} -p ${PGPORT} -U ${PGUSER} -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '${PGDATABASE}'" | grep -q 1 || \
-    PGPASSWORD=${PGPASS} psql -h ${PGHOST} -p ${PGPORT} -U ${PGUSER} -d postgres -c "CREATE DATABASE \"${PGDATABASE}\""
+    if [ ! -f /app/docker-data/db_status ]; then
+        echo "Running database setup and migrations..."
 
-    # Run setup and migration scripts
-    NODE_OPTIONS="--max-old-space-size=1500" tsx ./scripts/setup-db.ts
+        # Creating the database if it doesn't exist
+        PGUSER=$(echo $PG_DATABASE_URL | awk -F '//' '{print $2}' | awk -F ':' '{print $1}')
+        PGPASS=$(echo $PG_DATABASE_URL | awk -F ':' '{print $3}' | awk -F '@' '{print $1}')
+        PGHOST=$(echo $PG_DATABASE_URL | awk -F '@' '{print $2}' | awk -F ':' '{print $1}')
+        PGPORT=$(echo $PG_DATABASE_URL | awk -F ':' '{print $4}' | awk -F '/' '{print $1}')
+        PGDATABASE=$(echo $PG_DATABASE_URL | awk -F ':' '{print $4}' | awk -F '/' '{print $2}')
+        PGPASSWORD=${PGPASS} psql -h ${PGHOST} -p ${PGPORT} -U ${PGUSER} -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '${PGDATABASE}'" | grep -q 1 ||
+            PGPASSWORD=${PGPASS} psql -h ${PGHOST} -p ${PGPORT} -U ${PGUSER} -d postgres -c "CREATE DATABASE \"${PGDATABASE}\""
 
-    # Mark initialization as done
-    echo "Successfuly migrated DB!"
-    touch /app/docker-data/db_status
-fi
+        # Run setup and migration scripts
+        NODE_OPTIONS="--max-old-space-size=1500" tsx ./scripts/setup-db.ts
+    fi
 
-yarn database:migrate:prod
-yarn command:prod upgrade
+    yarn database:migrate:prod
+    yarn command:prod upgrade
 
-if [ "${DISABLE_DB_MIGRATIONS}" != "true" ] && [ ! -f /app/docker-data/db_status ]; then
-    # Mark initialization as done
-    echo "Successfuly migrated DB!"
-    touch /app/docker-data/db_status
-fi
-
+    if [ ! -f /app/docker-data/db_status ]; then
+        # Mark initialization as done
+        echo "Successfully migrated DB!"
+        touch /app/docker-data/db_status
+    fi
+}
+setup_and_migrate_db
 
 # Continue with the original Docker command
 exec "$@"
