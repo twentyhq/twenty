883891eb9a826fba48243d96249c8cc0ef0a60dd
bosiraphael <raphael.bosi@gmail.com>
2025-05-05 10:32:18 +0200

    add enter hotkey

--- packages/twenty-front/src/modules/object-record/record-table/record-table-row/components/RecordTableRowHotkeyEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-row/components/RecordTableRowHotkeyEffect.tsx
@@ -1,6 +1,8 @@
 import { useOpenRecordInCommandMenu } from '@/command-menu/hooks/useOpenRecordInCommandMenu';
 import { useRecordTableRowContextOrThrow } from '@/object-record/record-table/contexts/RecordTableRowContext';
+import { useSetRecordTableFocusPosition } from '@/object-record/record-table/hooks/internal/useSetRecordTableFocusPosition';
 import { useActiveRecordTableRow } from '@/object-record/record-table/hooks/useActiveRecordTableRow';
+import { useFocusedRecordTableRow } from '@/object-record/record-table/hooks/useFocusedRecordTableRow';
 import { useSetCurrentRowSelected } from '@/object-record/record-table/record-table-row/hooks/useSetCurrentRowSelected';
 import { TableHotkeyScope } from '@/object-record/record-table/types/TableHotkeyScope';
 import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
@@ -15,6 +17,10 @@ export const RecordTableRowHotkeyEffect = () => {
   const { openRecordInCommandMenu } = useOpenRecordInCommandMenu();
 
   const { activateRecordTableRow } = useActiveRecordTableRow();
+
+  const { unfocusRecordTableRow } = useFocusedRecordTableRow();
+
+  const setFocusPosition = useSetRecordTableFocusPosition();
 
   useScopedHotkeys(
     'x',
@@ -34,6 +40,18 @@ export const RecordTableRowHotkeyEffect = () => {
       });
 
       activateRecordTableRow(rowIndex);
+    },
+    TableHotkeyScope.TableFocus,
+  );
+
+  useScopedHotkeys(
+    Key.Enter,
+    () => {
+      unfocusRecordTableRow();
+      setFocusPosition({
+        row: rowIndex,
+        column: 0,
+      });
     },
     TableHotkeyScope.TableFocus,
   );
