72bd4917068327eb16b2845990e2f2d38f244a20
etiennejouan <jouan.etienne@gmail.com>
2025-05-05 17:00:45 +0200

    remove useless pre query hook

--- packages/twenty-server/src/modules/messaging/common/query-hooks/messaging-query-hook.module.ts
+++ packages/twenty-server/src/modules/messaging/common/query-hooks/messaging-query-hook.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
 import { ApplyMessagesVisibilityRestrictionsService } from 'src/modules/messaging/common/query-hooks/message/apply-messages-visibility-restrictions.service';
-import { CanAccessMessageThreadService } from 'src/modules/messaging/common/query-hooks/message/can-access-message-thread.service';
 import { MessageFindManyPostQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-many.post-query.hook';
-import { MessageFindManyPreQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-many.pre-query.hook';
 import { MessageFindOnePostQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-one.post-query.hook';
-import { MessageFindOnePreQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-one.pre-query-hook';
 import { WorkspaceMemberWorkspaceEntity } from 'src/modules/workspace-member/standard-objects/workspace-member.workspace-entity';
 
 @Module({
@@ -14,10 +11,7 @@ import { WorkspaceMemberWorkspaceEntity } from 'src/modules/workspace-member/sta
     ObjectMetadataRepositoryModule.forFeature([WorkspaceMemberWorkspaceEntity]),
   ],
   providers: [
-    CanAccessMessageThreadService,
     ApplyMessagesVisibilityRestrictionsService,
-    MessageFindOnePreQueryHook,
-    MessageFindManyPreQueryHook,
     MessageFindOnePostQueryHook,
     MessageFindManyPostQueryHook,
   ],
