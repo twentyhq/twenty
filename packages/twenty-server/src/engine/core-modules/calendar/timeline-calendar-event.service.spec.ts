75fedc4868a7d210a47e7e6e0af55e28ed3e8c45
etiennejouan <jouan.etienne@gmail.com>
2025-05-05 10:25:42 +0200

    add test on timeline calendar event service

---
+++ packages/twenty-server/src/engine/core-modules/calendar/timeline-calendar-event.service.spec.ts
@@ -0,0 +1,152 @@
+import { FIELD_RESTRICTED_ADDITIONAL_PERMISSIONS_REQUIRED } from 'twenty-shared/constants';
+
+import { TwentyORMManager } from 'src/engine/twenty-orm/twenty-orm.manager';
+
+import { TimelineCalendarEventService } from './timeline-calendar-event.service';
+
+describe('TimelineCalendarEventService', () => {
+  let service: TimelineCalendarEventService;
+  let twentyORMManager: TwentyORMManager;
+
+  const mockCalendarEvent = {
+    id: '1',
+    title: 'Test Event',
+    description: 'Test Description',
+    startsAt: new Date(),
+    endsAt: new Date(),
+    calendarEventParticipants: [],
+    calendarChannelEventAssociations: [],
+  };
+
+  const mockCalendarEventRepository = {
+    find: jest.fn(),
+    findAndCount: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    twentyORMManager = {
+      getRepository: jest.fn().mockResolvedValue(mockCalendarEventRepository),
+    } as any;
+
+    service = new TimelineCalendarEventService(twentyORMManager);
+  });
+
+  it('should return non-obfuscated calendar events if visibility is SHARE_EVERYTHING', async () => {
+    const currentWorkspaceMemberId = 'current-workspace-member-id';
+    const personIds = ['person-1'];
+
+    mockCalendarEventRepository.find.mockResolvedValue([
+      { id: '1', startsAt: new Date() },
+    ]);
+    mockCalendarEventRepository.findAndCount.mockResolvedValue([
+      [
+        {
+          ...mockCalendarEvent,
+          calendarChannelEventAssociations: [
+            {
+              calendarChannel: {
+                visibility: 'SHARE_EVERYTHING',
+                connectedAccount: {
+                  accountOwnerId: 'other-workspace-member-id',
+                },
+              },
+            },
+          ],
+        },
+      ],
+      1,
+    ]);
+
+    const result = await service.getCalendarEventsFromPersonIds({
+      currentWorkspaceMemberId,
+      personIds,
+      page: 1,
+      pageSize: 10,
+    });
+
+    expect(result.timelineCalendarEvents[0].title).toBe('Test Event');
+    expect(result.timelineCalendarEvents[0].description).toBe(
+      'Test Description',
+    );
+  });
+
+  it('should return obfuscated calendar events if visibility is METADATA', async () => {
+    const currentWorkspaceMemberId = 'current-workspace-member-id';
+    const personIds = ['person-1'];
+
+    mockCalendarEventRepository.find.mockResolvedValue([
+      { id: '1', startsAt: new Date() },
+    ]);
+    mockCalendarEventRepository.findAndCount.mockResolvedValue([
+      [
+        {
+          ...mockCalendarEvent,
+          calendarChannelEventAssociations: [
+            {
+              calendarChannel: {
+                visibility: 'METADATA',
+                connectedAccount: {
+                  accountOwnerId: 'other-workspace-member-id',
+                },
+              },
+            },
+          ],
+        },
+      ],
+      1,
+    ]);
+
+    const result = await service.getCalendarEventsFromPersonIds({
+      currentWorkspaceMemberId,
+      personIds,
+      page: 1,
+      pageSize: 10,
+    });
+
+    expect(result.timelineCalendarEvents[0].title).toBe(
+      FIELD_RESTRICTED_ADDITIONAL_PERMISSIONS_REQUIRED,
+    );
+    expect(result.timelineCalendarEvents[0].description).toBe(
+      FIELD_RESTRICTED_ADDITIONAL_PERMISSIONS_REQUIRED,
+    );
+  });
+
+  it('should return non-obfuscated calendar events if visibility is METADATA and user is calendar events owner', async () => {
+    const currentWorkspaceMemberId = 'current-workspace-member-id';
+    const personIds = ['person-1'];
+
+    mockCalendarEventRepository.find.mockResolvedValue([
+      { id: '1', startsAt: new Date() },
+    ]);
+    mockCalendarEventRepository.findAndCount.mockResolvedValue([
+      [
+        {
+          ...mockCalendarEvent,
+          calendarChannelEventAssociations: [
+            {
+              calendarChannel: {
+                visibility: 'METADATA',
+                connectedAccount: {
+                  accountOwnerId: 'current-workspace-member-id',
+                },
+              },
+            },
+          ],
+        },
+      ],
+      1,
+    ]);
+
+    const result = await service.getCalendarEventsFromPersonIds({
+      currentWorkspaceMemberId,
+      personIds,
+      page: 1,
+      pageSize: 10,
+    });
+
+    expect(result.timelineCalendarEvents[0].title).toBe('Test Event');
+    expect(result.timelineCalendarEvents[0].description).toBe(
+      'Test Description',
+    );
+  });
+});
