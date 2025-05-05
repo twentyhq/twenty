3a6e9246442c23fd39f928aaefe8e30f9c1ce415
etiennejouan <jouan.etienne@gmail.com>
2025-05-05 14:56:26 +0200

    add post hook + tests

--- packages/twenty-server/src/modules/messaging/common/query-hooks/messaging-query-hook.module.ts
+++ packages/twenty-server/src/modules/messaging/common/query-hooks/messaging-query-hook.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
+import { ApplyMessagesVisibilityRestrictionsService } from 'src/modules/messaging/common/query-hooks/message/apply-messages-visibility-restrictions.service';
 import { CanAccessMessageThreadService } from 'src/modules/messaging/common/query-hooks/message/can-access-message-thread.service';
+import { MessageFindManyPostQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-many.post-query.hook';
 import { MessageFindManyPreQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-many.pre-query.hook';
+import { MessageFindOnePostQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-one.post-query.hook';
 import { MessageFindOnePreQueryHook } from 'src/modules/messaging/common/query-hooks/message/message-find-one.pre-query-hook';
 import { WorkspaceMemberWorkspaceEntity } from 'src/modules/workspace-member/standard-objects/workspace-member.workspace-entity';
 
@@ -12,8 +15,11 @@ import { WorkspaceMemberWorkspaceEntity } from 'src/modules/workspace-member/sta
   ],
   providers: [
     CanAccessMessageThreadService,
+    ApplyMessagesVisibilityRestrictionsService,
     MessageFindOnePreQueryHook,
     MessageFindManyPreQueryHook,
+    MessageFindOnePostQueryHook,
+    MessageFindManyPostQueryHook,
   ],
 })
 export class MessagingQueryHookModule {}
