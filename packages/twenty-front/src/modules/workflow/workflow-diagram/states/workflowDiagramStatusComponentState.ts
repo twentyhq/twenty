102b3f12fc6dc2f3d7ee9d255d7de4a3d314436a
Devessier <baptiste@devessier.fr>
2025-05-05 10:45:43 +0200

    refactor: extract type to specific file

--- packages/twenty-front/src/modules/workflow/workflow-diagram/states/workflowDiagramStatusComponentState.ts
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/states/workflowDiagramStatusComponentState.ts
@@ -1,12 +1,12 @@
 import { createComponentStateV2 } from '@/ui/utilities/state/component-state/utils/createComponentStateV2';
 import { WorkflowRunVisualizerComponentInstanceContext } from '@/workflow/workflow-diagram/states/contexts/WorkflowRunVisualizerComponentInstanceContext';
+import { WorkflowDiagramStatus } from '@/workflow/workflow-diagram/types/WorkflowDiagramStatus';
 
 // This state must be fresh every time the Reactflow component is mounted.
 // We use another instance context whose instanceId is an id unique to the component hierarchy.
-export const workflowDiagramStatusComponentState = createComponentStateV2<
-  'computing-diagram' | 'computing-dimensions' | 'done'
->({
-  key: 'workflowDiagramStatusComponentState',
-  defaultValue: 'computing-diagram',
-  componentInstanceContext: WorkflowRunVisualizerComponentInstanceContext,
-});
+export const workflowDiagramStatusComponentState =
+  createComponentStateV2<WorkflowDiagramStatus>({
+    key: 'workflowDiagramStatusComponentState',
+    defaultValue: 'computing-diagram',
+    componentInstanceContext: WorkflowRunVisualizerComponentInstanceContext,
+  });
