7b291e92c8b61676960c0488d63e694746aaf644
martmull <martmull@hotmail.fr>
2025-05-05 11:22:20 +0200

    Fix condition on generate Field fake value

--- packages/twenty-server/src/modules/workflow/workflow-builder/workflow-schema/utils/should-generate-field-fake-value.ts
+++ packages/twenty-server/src/modules/workflow/workflow-builder/workflow-schema/utils/should-generate-field-fake-value.ts
@@ -1,8 +1,16 @@
+import { FieldMetadataType } from 'twenty-shared/types';
+
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
+
+const isManyToOneRelationField = (field: FieldMetadataEntity) =>
+  (field as FieldMetadataEntity<FieldMetadataType.RELATION>).settings
+    ?.relationType === 'MANY_TO_ONE';
 
 export const shouldGenerateFieldFakeValue = (field: FieldMetadataEntity) => {
   return (
+    field.isActive &&
     (!field.isSystem || field.name === 'id' || field.name === 'userEmail') &&
-    field.isActive
+    (field.type !== FieldMetadataType.RELATION ||
+      isManyToOneRelationField(field))
   );
 };
