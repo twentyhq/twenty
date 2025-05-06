f1d658bcb6be95530b128a5324c52d2ac76a08fa
Prajwal Dhule <prajwaldhule36@gmail.com>
2025-05-06 18:04:52 +0530

    Fix: Twenty-website docs same TOC ids #11865 (#11872)
    This PR fixes issue https://github.com/twentyhq/twenty/issues/11865.
    
    The highlight heading logic in TOC was checking the heading text which
    could be the same for multiple headings.
    The ids for these headings were also just the heading texts, leading to
    conflict in ids too.
    
    Fix:
    - Appended index of the heading item from the list of headings to the id
    of the heading. This fixed conflicting ids.
    - Used these unique ids to toggle the highlight style.
    
    Behaviour after the fix:
    
    
    https://github.com/user-attachments/assets/ab3bc205-0b0e-451d-b9cb-4fa852263efc
    
    
    Edit:
    close #11865
    
    ---------
    
    Co-authored-by: prastoin <paul@twenty.com>

--- packages/twenty-website/src/shared-utils/wrapHeadingsWithAnchor.tsx
+++ packages/twenty-website/src/shared-utils/wrapHeadingsWithAnchor.tsx
@@ -1,4 +1,4 @@
-import React, {
+import {
   Children,
   cloneElement,
   isValidElement,
@@ -12,6 +12,7 @@ export const wrapHeadingsWithAnchor = (children: ReactNode): ReactNode => {
   ): element is ReactElement<{ children: ReactNode }> => {
     return element.props.children !== undefined;
   };
+  const idCounts = new Map<string, number>();
 
   return Children.map(children, (child) => {
     if (
@@ -19,10 +20,15 @@ export const wrapHeadingsWithAnchor = (children: ReactNode): ReactNode => {
       typeof child.type === 'string' &&
       ['h1', 'h2', 'h3', 'h4'].includes(child.type)
     ) {
-      const id = child.props.children
+      const baseId = child.props.children
         .toString()
         .replace(/\s+/g, '-')
         .toLowerCase();
+      const idCount = idCounts.get(baseId) ?? 0;
+
+      const id = idCount === 0 ? baseId : `${baseId}-${idCount}`;
+      idCounts.set(baseId, idCount + 1);
+
       return cloneElement(child as ReactElement<any>, {
         id,
         className: 'anchor',
