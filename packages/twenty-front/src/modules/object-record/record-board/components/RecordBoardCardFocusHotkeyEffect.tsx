de62b9de5d8c3f09786e59e0f0fb6be032c36199
bosiraphael <raphael.bosi@gmail.com>
2025-05-07 15:01:56 +0200

    create RecordBoardCardFocusHotkeyEffect

--- packages/twenty-front/src/modules/object-record/record-board/components/RecordBoardCardFocusHotkeyEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-board/components/RecordBoardCardFocusHotkeyEffect.tsx
@@ -4,51 +4,43 @@ import { Key } from 'ts-key-enum';
 import { RecordBoardContext } from '@/object-record/record-board/contexts/RecordBoardContext';
 import { useBoardCardNavigation } from '@/object-record/record-board/hooks/useBoardCardNavigation';
 import { BoardHotkeyScope } from '@/object-record/record-board/types/BoardHotkeyScope';
-import { RecordIndexHotkeyScope } from '@/object-record/record-index/types/RecordIndexHotkeyScope';
-import { usePreviousHotkeyScope } from '@/ui/utilities/hotkey/hooks/usePreviousHotkeyScope';
 import { useScopedHotkeys } from '@/ui/utilities/hotkey/hooks/useScopedHotkeys';
 
-export const RecordBoardHotkeyEffect = () => {
+export const RecordBoardCardFocusHotkeyEffect = () => {
   const { recordBoardId } = useContext(RecordBoardContext);
 
   const { move } = useBoardCardNavigation(recordBoardId);
 
-  const { setHotkeyScopeAndMemorizePreviousScope } = usePreviousHotkeyScope();
-
   useScopedHotkeys(
     Key.ArrowLeft,
     () => {
-      setHotkeyScopeAndMemorizePreviousScope(BoardHotkeyScope.BoardFocus);
       move('left');
     },
-    RecordIndexHotkeyScope.RecordIndex,
+    BoardHotkeyScope.BoardFocus,
   );
 
   useScopedHotkeys(
     Key.ArrowRight,
     () => {
-      setHotkeyScopeAndMemorizePreviousScope(BoardHotkeyScope.BoardFocus);
       move('right');
     },
-    RecordIndexHotkeyScope.RecordIndex,
+    BoardHotkeyScope.BoardFocus,
   );
 
   useScopedHotkeys(
     Key.ArrowUp,
     () => {
-      setHotkeyScopeAndMemorizePreviousScope(BoardHotkeyScope.BoardFocus);
       move('up');
     },
-    RecordIndexHotkeyScope.RecordIndex,
+    BoardHotkeyScope.BoardFocus,
   );
 
   useScopedHotkeys(
     Key.ArrowDown,
     () => {
-      setHotkeyScopeAndMemorizePreviousScope(BoardHotkeyScope.BoardFocus);
       move('down');
     },
-    RecordIndexHotkeyScope.RecordIndex,
+    BoardHotkeyScope.BoardFocus,
   );
 
   return null;
