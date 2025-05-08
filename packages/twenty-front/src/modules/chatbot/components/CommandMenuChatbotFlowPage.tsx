3bc188c8a35b6bfd74d7d05a7848039aa5186484
Alessandro Lemos Jr <ablemosjunior@gmail.com>
2025-05-08 17:06:32 -0300

    ✨ feat: create new nodes

--- packages/twenty-front/src/modules/chatbot/components/CommandMenuChatbotFlowPage.tsx
+++ packages/twenty-front/src/modules/chatbot/components/CommandMenuChatbotFlowPage.tsx
@@ -3,43 +3,44 @@ import {
   NODE_ACTIONS,
   OTHER_NODE_ACTIONS,
 } from '@/chatbot/constants/NodeActions';
+import { useUpdateChatbotFlow } from '@/chatbot/hooks/useUpdateChatbotFlow';
 import { chatbotFlowState } from '@/chatbot/state/chatbotFlowState';
-import { ChatbotFlow } from '@/chatbot/types/chatbotFlow.type';
+import { createNode } from '@/chatbot/utils/createNode';
 import { RightDrawerStepListContainer } from '@/workflow/workflow-steps/components/RightDrawerWorkflowSelectStepContainer';
 import { RightDrawerWorkflowSelectStepTitle } from '@/workflow/workflow-steps/components/RightDrawerWorkflowSelectStepTitle';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useIcons } from 'twenty-ui/display';
 import { MenuItemCommand } from 'twenty-ui/navigation';
-import { v4 } from 'uuid';
 
 export const CommandMenuChatbotFlowPage = () => {
   const { getIcon } = useIcons();
 
   const chatbotFlow = useRecoilValue(chatbotFlowState);
-  console.log('chatbotFlow', chatbotFlow);
+  const setChatbotFlow = useSetRecoilState(chatbotFlowState);
+
+  const { updateFlow } = useUpdateChatbotFlow();
 
   const handleAddNode = (action: ChatbotAction) => {
-    // Change this code to add new node to the chatbot flow
-    // and update the chatbot flow state with the new node
-
     if (!chatbotFlow) {
       return;
     }
 
-    const newNode = {
-      ...action,
-      id: v4(),
-      type: action.type,
-      position: { x: 0, y: 0 },
-      data: {},
-    };
+    const newNode = createNode(action.type);
 
-    const updatedChatbotFlow: ChatbotFlow = {
-      ...chatbotFlow,
+    if (!newNode) {
+      console.error('Failed to create a new node');
+      return;
+    }
+
+    const { id, __typename, ...chatbotFlowWithoutId } = chatbotFlow;
+
+    const updatedChatbotFlow = {
+      ...chatbotFlowWithoutId,
       nodes: [...chatbotFlow.nodes, newNode],
     };
 
-    console.log('Updated Chatbot Flow:', updatedChatbotFlow);
+    setChatbotFlow(updatedChatbotFlow);
+    updateFlow(updatedChatbotFlow);
   };
 
   return (
---
+++ packages/twenty-front/src/modules/chatbot/utils/createNode.ts
@@ -0,0 +1,38 @@
+import { NewLogicNodeData } from '@/chatbot/types/LogicNodeDataType';
+import { v4 } from 'uuid';
+
+export const createNode = (type: string) => {
+  const baseNode = {
+    id: v4(),
+    type,
+    position: { x: 0, y: 0 },
+  };
+
+  switch (type) {
+    case 'text':
+      return {
+        ...baseNode,
+        data: {
+          nodeStart: false,
+        },
+      };
+    case 'condition': {
+      const initialLogicNode: NewLogicNodeData = {
+        option: '1',
+        comparison: '==',
+        sectorId: '',
+        conditionValue: '||',
+      };
+
+      return {
+        ...baseNode,
+        data: {
+          logic: {
+            logicNodes: [0],
+            logicNodeData: [initialLogicNode],
+          },
+        },
+      };
+    }
+  }
+};
