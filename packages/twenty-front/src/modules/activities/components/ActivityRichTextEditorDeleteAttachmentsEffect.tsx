c2ccffd399916f40c911e447cc8a154f0887bd1c
etiennejouan <jouan.etienne@gmail.com>
2025-05-09 12:22:33 +0200

    work better with recoil callback

--- packages/twenty-front/src/modules/activities/components/ActivityRichTextEditorDeleteAttachmentsEffect.tsx
+++ packages/twenty-front/src/modules/activities/components/ActivityRichTextEditorDeleteAttachmentsEffect.tsx
@@ -6,7 +6,7 @@ import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSi
 import { useDeleteManyRecords } from '@/object-record/hooks/useDeleteManyRecords';
 import { recordStoreFamilyState } from '@/object-record/record-store/states/recordStoreFamilyState';
 import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilCallback } from 'recoil';
 import { isDefined } from 'twenty-shared/utils';
 
 type ActivityRichTextEditorDeleteAttachmentsEffectProps = {
@@ -20,54 +20,61 @@ type Activity = (Task | Note) & {
 export const ActivityRichTextEditorDeleteAttachmentsEffect = ({
   activityId,
 }: ActivityRichTextEditorDeleteAttachmentsEffectProps) => {
-  const [activityInStore] = useRecoilState(recordStoreFamilyState(activityId));
-  const activity = activityInStore as Activity | null;
-  const [
-    isActivityAttachmentDeletionCheckNeeded,
-    setIsActivityAttachmentDeletionCheckNeeded,
-  ] = useRecoilState(isActivityAttachmentDeletionCheckNeededState);
-
   const { deleteManyRecords } = useDeleteManyRecords({
     objectNameSingular: CoreObjectNameSingular.Attachment,
   });
 
+  const checkAndDeleteAttachments = useRecoilCallback(
+    ({ snapshot, set }) =>
+      () => {
+        const isCheckNeeded = snapshot
+          .getLoadable(isActivityAttachmentDeletionCheckNeededState)
+          .getValue();
+
+        if (!isCheckNeeded) {
+          return;
+        }
+
+        set(isActivityAttachmentDeletionCheckNeededState, false);
+
+        const activity = snapshot
+          .getLoadable(recordStoreFamilyState(activityId))
+          .getValue() as Activity;
+
+        const activityAttachments = activity?.attachments;
+
+        if (
+          !isDefined(activityAttachments) ||
+          activityAttachments.length === 0
+        ) {
+          return;
+        }
+
+        const activityAttachmentIdsToDelete = activityAttachments
+          .filter((attachment) => {
+            return !activity?.bodyV2?.blocknote?.includes(
+              attachment.fullPath.split('?')[0],
+            );
+          })
+          .map((attachment) => attachment.id);
+
+        if (activityAttachmentIdsToDelete.length === 0) {
+          return;
+        }
+
+        deleteManyRecords({
+          recordIdsToDelete: activityAttachmentIdsToDelete,
+        });
+      },
+    [deleteManyRecords, activityId],
+  );
+
   useEffect(() => {
-    console.log('run mount');
-
     return () => {
-      console.log('run unmount');
-      if (!isActivityAttachmentDeletionCheckNeeded) {
-        console.log('no need to check for attachments to delete');
-        return;
-      }
-      console.log('setting isActivityAttachmentDeletionCheckNeeded to false');
-      setIsActivityAttachmentDeletionCheckNeeded(false);
-
-      const activityAttachments = activity?.attachments;
-      if (!isDefined(activityAttachments) || activityAttachments.length === 0) {
-        console.log('no attachments to delete 1');
-        return;
-      }
-
-      const activityAttachmentIdsToDelete = activityAttachments
-        .filter((attachment) => {
-          return !activity?.bodyV2?.blocknote?.includes(
-            attachment.fullPath.split('?')[0],
-          );
-        })
-        .map((attachment) => attachment.id);
-      if (activityAttachmentIdsToDelete.length === 0) {
-        console.log('no attachments to delete 2');
-        return;
-      }
-
-      console.log('deleteManyRecords', activityAttachmentIdsToDelete);
-      deleteManyRecords({
-        recordIdsToDelete: activityAttachmentIdsToDelete,
-      });
+      checkAndDeleteAttachments();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activityId]); // activityId in array only means this effect only runs on mount/unmount or
+  }, [activityId]); // activityId in array only means this effect only runs on mount/unmount or when activityId changes
 
   return <></>;
 };
