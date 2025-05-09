8216800a4a3aefdfbb26d696056469b69e087d75
Charles Bochet <charlesBochet@users.noreply.github.com>
2025-05-09 19:03:39 +0200

    Fix workspace relation sync (#11963)
    ## Context
    
    While deploying the IS_NEW_RELATION_ENABLED (we don't compute relation
    based on relationMetadata anymore) to existing workspace, I've tested to
    run a sync-metadata post feature flag activation. This has raised two
    issues:
    - the workspaceMigration generator (which is over-complex and should be
    refactored later) for fieldMetadata of type RELATION was not handling
    settings update properly ;
    - we need to delete existing fieldMetadata corresponding to the UUID
    foreignKey as they are not needed anymore. This is handled as a 0.53
    upgrade command as 0.53 will also come with the full removal of the old
    relation system
    
    ---------
    
    Co-authored-by: Etienne <45695613+etiennejouan@users.noreply.github.com>
    Co-authored-by: prastoin <paul@twenty.com>

--- packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
@@ -1,7 +1,7 @@
 import { InjectRepository } from '@nestjs/typeorm';
 
-import { Repository } from 'typeorm';
 import { Command } from 'nest-commander';
+import { Repository } from 'typeorm';
 
 import {
   AllCommands,
