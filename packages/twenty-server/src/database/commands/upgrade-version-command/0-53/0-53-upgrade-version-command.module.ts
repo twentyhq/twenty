6bb8fa8559bdd4bdd1c53c94fe1290b39d98c399
Charles Bochet <charlesBochet@users.noreply.github.com>
2025-05-09 19:03:39 +0200

    Fix workspace relation sync (#11963)
    While deploying the IS_NEW_RELATION_ENABLED (we don't compute relation
    based on relationMetadata anymore) to existing workspace, I've tested to
    run a sync-metadata post feature flag activation. This has raised two
    issues:
    - the workspaceMigration generator (which is over-complex and should be
    refactored later) for fieldMetadata of type RELATION was not handling
    settings update properly ;
    - we need to delete existing fieldMetadata corresponding to the UUID
    foreignKey as they are not needed anymore. This is handled as a 0.53
    upgrade command as 0.53 will also come with the full removal of the old
    relation system
    
    ---------
    
    Co-authored-by: Etienne <45695613+etiennejouan@users.noreply.github.com>
    Co-authored-by: prastoin <paul@twenty.com>

--- packages/twenty-server/src/database/commands/upgrade-version-command/0-53/0-53-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/0-53/0-53-upgrade-version-command.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
-import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
+import { BackfillWorkflowNextStepIdsCommand } from 'src/database/commands/upgrade-version-command/0-52/0-52-backfill-workflow-next-step-ids.command';
 import { MigrateWorkflowEventListenersToAutomatedTriggersCommand } from 'src/database/commands/upgrade-version-command/0-53/0-53-migrate-workflow-event-listeners-to-automated-triggers.command';
+import { RemoveRelationForeignKeyFieldMetadataCommand } from 'src/database/commands/upgrade-version-command/0-53/0-53-remove-relation-foreign-key-field-metadata.command';
+import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
+import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Workspace], 'core'),
+    TypeOrmModule.forFeature([FieldMetadataEntity], 'metadata'),
     WorkspaceDataSourceModule,
   ],
-  providers: [MigrateWorkflowEventListenersToAutomatedTriggersCommand],
-  exports: [MigrateWorkflowEventListenersToAutomatedTriggersCommand],
+  providers: [
+    MigrateWorkflowEventListenersToAutomatedTriggersCommand,
+    BackfillWorkflowNextStepIdsCommand,
+    RemoveRelationForeignKeyFieldMetadataCommand,
+  ],
+  exports: [
+    MigrateWorkflowEventListenersToAutomatedTriggersCommand,
+    RemoveRelationForeignKeyFieldMetadataCommand,
+    BackfillWorkflowNextStepIdsCommand,
+  ],
 })
 export class V0_53_UpgradeVersionCommandModule {}
