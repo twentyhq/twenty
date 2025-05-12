eba740b13fa49eca2fa519af7c3c8f2b39214374
guillim <guigloo@msn.com>
2025-05-12 12:17:24 +0200

    charles feedback

--- packages/twenty-server/src/modules/calendar/calendar-event-import-manager/drivers/utils/sanitizeCalendarEvent.ts
+++ packages/twenty-server/src/modules/calendar/calendar-event-import-manager/drivers/utils/sanitizeCalendarEvent.ts
@@ -1,3 +1,5 @@
+import { isDefined } from 'twenty-shared/utils';
+
 export const sanitizeCalendarEvent = <T extends Record<string, any>>(
   event: T,
   propertiesToSanitize: (keyof T)[],
@@ -5,18 +7,18 @@ export const sanitizeCalendarEvent = <T extends Record<string, any>>(
   const sanitizedEvent = { ...event };
 
   for (const property of propertiesToSanitize) {
+    if (!isDefined(sanitizedEvent[property])) {
+      continue;
+    }
+    if (typeof sanitizedEvent[property] !== 'string') {
+      continue;
+    }
     sanitizedEvent[property] = sanitizeString(sanitizedEvent[property]) as any;
   }
 
   return sanitizedEvent;
 };
 
-const sanitizeString = (
-  value: string | null | undefined,
-): string | null | undefined => {
-  if (value === null || value === undefined || typeof value !== 'string') {
-    return value;
-  }
-
+const sanitizeString = (value: string): string => {
   return value.replace('\u0000', '');
 };
