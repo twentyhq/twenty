977ceba5a0c4f84e061b57489596c6be66f9a527
Marie Stoppa <marie.stoppa@essec.edu>
2025-05-13 15:02:48 +0200

    intercept email verification exceptions

---
+++ packages/twenty-server/src/engine/core-modules/email-verification/email-verification-exception-handler.util.ts
@@ -0,0 +1,27 @@
+import {
+  EmailVerificationException,
+  EmailVerificationExceptionCode,
+} from 'src/engine/core-modules/email-verification/email-verification.exception';
+import {
+  ForbiddenError,
+  UserInputError,
+} from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
+import { CustomException } from 'src/utils/custom-exception';
+
+export const emailVerificationGraphqlApiExceptionHandler = (
+  error: EmailVerificationException,
+) => {
+  switch (error.code) {
+    case EmailVerificationExceptionCode.INVALID_TOKEN:
+    case EmailVerificationExceptionCode.INVALID_APP_TOKEN_TYPE:
+    case EmailVerificationExceptionCode.TOKEN_EXPIRED:
+    case EmailVerificationExceptionCode.RATE_LIMIT_EXCEEDED:
+      throw new ForbiddenError(error.message);
+    case EmailVerificationExceptionCode.EMAIL_MISSING:
+    case EmailVerificationExceptionCode.EMAIL_ALREADY_VERIFIED:
+    case EmailVerificationExceptionCode.EMAIL_VERIFICATION_NOT_REQUIRED:
+      throw new UserInputError(error.message);
+    default:
+      throw new CustomException(error.message, error.code);
+  }
+};
