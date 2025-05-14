00056e83dfef4e57489f13c1b8ad59398c969928
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-05-14 17:36:59 +0200

    Fix

--- packages/twenty-front/src/modules/object-record/record-store/components/RecordValueSetterEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-store/components/RecordValueSetterEffect.tsx
@@ -6,6 +6,7 @@ import {
   useSetRecordValue,
 } from '@/object-record/record-store/contexts/RecordFieldValueSelectorContext';
 import { recordStoreFamilyState } from '@/object-record/record-store/states/recordStoreFamilyState';
+import { isDeeplyEqual } from '~/utils/isDeeplyEqual';
 
 // TODO: should be optimized and put higher up
 export const RecordValueSetterEffect = ({ recordId }: { recordId: string }) => {
@@ -18,9 +19,9 @@ export const RecordValueSetterEffect = ({ recordId }: { recordId: string }) => {
   );
 
   useEffect(() => {
-    //if (!isDeeplyEqual(recordValueFromContextSelector, recordValueFromRecoil)) {
+    if (!isDeeplyEqual(recordValueFromContextSelector, recordValueFromRecoil)) {
       setRecordValueInContextSelector(recordId, recordValueFromRecoil);
-    //}
+    }
   }, [
     setRecordValueInContextSelector,
     recordValueFromRecoil,
