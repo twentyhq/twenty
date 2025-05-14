5be880ff2c35d64fdd9a2de6dddd496b56b1dc7d
bosiraphael <raphael.bosi@gmail.com>
2025-05-14 14:47:44 +0200

    create useModal hook

---
+++ packages/twenty-front/src/modules/ui/layout/modal/hooks/useModal.tsx
@@ -0,0 +1,87 @@
+import { useRecoilCallback } from 'recoil';
+
+import { isModalOpenedComponentState } from '@/ui/layout/modal/states/isModalOpenedComponentState';
+import { modalHotkeyScopeComponentState } from '@/ui/layout/modal/states/modalHotkeyScopeComponenState';
+import { usePreviousHotkeyScope } from '@/ui/utilities/hotkey/hooks/usePreviousHotkeyScope';
+import { HotkeyScope } from '@/ui/utilities/hotkey/types/HotkeyScope';
+import { isDefined } from 'twenty-shared/utils';
+
+export const useModal = () => {
+  const {
+    setHotkeyScopeAndMemorizePreviousScope,
+    goBackToPreviousHotkeyScope,
+  } = usePreviousHotkeyScope();
+
+  const closeModal = useRecoilCallback(
+    ({ set, snapshot }) =>
+      (modalId: string) => {
+        const isModalOpen = snapshot
+          .getLoadable(
+            isModalOpenedComponentState.atomFamily({ instanceId: modalId }),
+          )
+          .getValue();
+
+        if (isModalOpen) {
+          goBackToPreviousHotkeyScope();
+          set(
+            isModalOpenedComponentState.atomFamily({ instanceId: modalId }),
+            false,
+          );
+        }
+      },
+    [goBackToPreviousHotkeyScope],
+  );
+
+  const openModal = useRecoilCallback(
+    ({ set, snapshot }) =>
+      (modalId: string, customHotkeyScope?: HotkeyScope) => {
+        const modalHotkeyScope = snapshot
+          .getLoadable(
+            modalHotkeyScopeComponentState.atomFamily({ instanceId: modalId }),
+          )
+          .getValue();
+
+        set(
+          isModalOpenedComponentState.atomFamily({ instanceId: modalId }),
+          true,
+        );
+
+        if (isDefined(customHotkeyScope)) {
+          setHotkeyScopeAndMemorizePreviousScope(
+            customHotkeyScope.scope,
+            customHotkeyScope.customScopes,
+          );
+        } else if (isDefined(modalHotkeyScope)) {
+          setHotkeyScopeAndMemorizePreviousScope(
+            modalHotkeyScope.scope,
+            modalHotkeyScope.customScopes,
+          );
+        }
+      },
+    [setHotkeyScopeAndMemorizePreviousScope],
+  );
+
+  const toggleModal = useRecoilCallback(
+    ({ snapshot }) =>
+      (modalId: string, customHotkeyScope?: HotkeyScope) => {
+        const isModalOpen = snapshot
+          .getLoadable(
+            isModalOpenedComponentState.atomFamily({ instanceId: modalId }),
+          )
+          .getValue();
+
+        if (isModalOpen) {
+          closeModal(modalId);
+        } else {
+          openModal(modalId, customHotkeyScope);
+        }
+      },
+    [closeModal, openModal],
+  );
+
+  return {
+    closeModal,
+    openModal,
+    toggleModal,
+  };
+};
