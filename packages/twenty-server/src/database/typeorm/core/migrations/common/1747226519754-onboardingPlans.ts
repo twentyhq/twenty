0ad939df447004eafa573eab04337687c1912fa0
guilhermeveigaa <guilhermeveigafff@gmail.com>
2025-05-14 10:33:15 -0300

    fix: move onboardingPlans migration to correct folder

---
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1747226519754-onboardingPlans.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class OnboardingPlans1747226519754 implements MigrationInterface {
+  name = 'OnboardingPlans1747226519754';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "core"."onboardingPlans" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" text NOT NULL, "price" integer NOT NULL, "type" text NOT NULL, "features" text array NOT NULL, CONSTRAINT "UQ_6f0b46df0a4a842c5c43fe1c908" UNIQUE ("title"), CONSTRAINT "PK_ddf683f9fc505f9623079593c20" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "core"."onboardingPlans"`);
+  }
+}
