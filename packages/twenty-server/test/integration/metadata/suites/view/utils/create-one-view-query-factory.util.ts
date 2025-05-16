8f80859f95ac714b7724c071a693e805f56fd030
prastoin <paul@twenty.com>
2025-05-16 11:49:23 +0200

    test(server): create one view

---
+++ packages/twenty-server/test/integration/metadata/suites/view/utils/create-one-view-query-factory.util.ts
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+import { PerformMetadataQueryParams } from 'test/integration/metadata/types/perform-metadata-query.type';
+
+export type CreateOneViewFactoryInput = {
+  icon?: string;
+  id?: string;
+  isCompact?: boolean;
+  kanbanFieldMetadataId?: string;
+  key?: string;
+  name?: string;
+  objectMetadataId: string;
+  position?: number;
+  type?: string;
+};
+
+export const createOneViewQueryFactory = ({
+  input,
+  gqlFields = 'id',
+}: PerformMetadataQueryParams<CreateOneViewFactoryInput>) => ({
+  query: gql`
+        mutation CreateOneView($input: ViewCreateInput!) {
+          createView(data: $input) {
+            ${gqlFields}
+        }
+      }
+      `,
+  variables: {
+    input,
+  },
+});
