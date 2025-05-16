48f32aa2107072a0471aba8c27617ba23d474a0d
Abdul Rahman <ar5438376@gmail.com>
2025-05-16 15:51:38 +0530

    Remove unused loading state component for MultipleRecordPicker

--- packages/twenty-front/src/modules/object-record/record-picker/multiple-record-picker/states/multipleRecordPickerIsLoadingComponentState.ts
+++
@@ -1,9 +0,0 @@
-import { MultipleRecordPickerComponentInstanceContext } from '@/object-record/record-picker/multiple-record-picker/states/contexts/MultipleRecordPickerComponentInstanceContext';
-import { createComponentStateV2 } from '@/ui/utilities/state/component-state/utils/createComponentStateV2';
-
-export const multipleRecordPickerIsLoadingComponentState =
-  createComponentStateV2<boolean>({
-    key: 'multipleRecordPickerIsLoadingComponentState',
-    defaultValue: false,
-    componentInstanceContext: MultipleRecordPickerComponentInstanceContext,
-  });
