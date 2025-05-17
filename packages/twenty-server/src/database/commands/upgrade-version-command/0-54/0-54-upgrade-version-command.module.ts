c1b6ba67597b8399218c7ca2d11011af10a77569
etiennejouan <jouan.etienne@gmail.com>
2025-05-17 09:59:26 +0200

    add migration command

--- packages/twenty-server/src/database/commands/upgrade-version-command/0-54/0-54-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/0-54/0-54-upgrade-version-command.module.ts
@@ -5,6 +5,7 @@ import { CleanNotFoundFilesCommand } from 'src/database/commands/upgrade-version
 import { FixCreatedByDefaultValueCommand } from 'src/database/commands/upgrade-version-command/0-54/0-54-created-by-default-value.command';
 import { FixStandardSelectFieldsPositionCommand } from 'src/database/commands/upgrade-version-command/0-54/0-54-fix-standard-select-fields-position.command';
 import { LowercaseUserAndInvitationEmailsCommand } from 'src/database/commands/upgrade-version-command/0-54/0-54-lowercase-user-and-invitation-emails.command';
+import { MigrateDefaultAvatarUrlToUserWorkspaceCommand } from 'src/database/commands/upgrade-version-command/0-54/0-54-migrate-default-avatar-url-to-user-workspace.command';
 import { AppToken } from 'src/engine/core-modules/app-token/app-token.entity';
 import { FileModule } from 'src/engine/core-modules/file/file.module';
 import { User } from 'src/engine/core-modules/user/user.entity';
@@ -32,12 +33,14 @@ import { WorkspaceMigrationRunnerModule } from 'src/engine/workspace-manager/wor
     FixCreatedByDefaultValueCommand,
     CleanNotFoundFilesCommand,
     LowercaseUserAndInvitationEmailsCommand,
+    MigrateDefaultAvatarUrlToUserWorkspaceCommand,
   ],
   exports: [
     FixStandardSelectFieldsPositionCommand,
     FixCreatedByDefaultValueCommand,
     CleanNotFoundFilesCommand,
     LowercaseUserAndInvitationEmailsCommand,
+    MigrateDefaultAvatarUrlToUserWorkspaceCommand,
   ],
 })
 export class V0_54_UpgradeVersionCommandModule {}
