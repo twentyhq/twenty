230f7e6de1de334d52e1df1c5dedb644e5d359dd
Abdul Rahman <ar5438376@gmail.com>
2025-05-19 14:41:08 +0530

    Remove OBJECT_METADATA_MAP_NOT_FOUND from SearchException enum

--- packages/twenty-server/src/engine/core-modules/search/exceptions/search.exception.ts
+++ packages/twenty-server/src/engine/core-modules/search/exceptions/search.exception.ts
@@ -9,5 +9,4 @@ export class SearchException extends CustomException {
 
 export enum SearchExceptionCode {
   LABEL_IDENTIFIER_FIELD_NOT_FOUND = 'LABEL_IDENTIFIER_FIELD_NOT_FOUND',
-  OBJECT_METADATA_MAP_NOT_FOUND = 'OBJECT_METADATA_MAP_NOT_FOUND',
 }
