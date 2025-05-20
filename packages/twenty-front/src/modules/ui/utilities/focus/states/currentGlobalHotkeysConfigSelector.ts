29f7efbde8bdaebfa8f694de3ac09a5065b166a7
bosiraphael <raphael.bosi@gmail.com>
2025-05-20 16:35:13 +0200

    rename

--- packages/twenty-front/src/modules/ui/utilities/focus/states/currentGlobalHotkeysConfigSelector.ts
+++ packages/twenty-front/src/modules/ui/utilities/focus/states/currentGlobalHotkeysConfigSelector.ts
@@ -2,10 +2,10 @@ import { focusStackState } from '@/ui/utilities/focus/states/focusStackState';
 import { FocusGlobalHotkeysConfig } from '@/ui/utilities/focus/types/FocusGlobalHotkeysConfig';
 import { selector } from 'recoil';
 
-export const currentHotkeysConfigSelector = selector<
+export const currentGlobalHotkeysConfigSelector = selector<
   FocusGlobalHotkeysConfig | undefined
 >({
-  key: 'currentHotkeysConfigSelector',
+  key: 'currentGlobalHotkeysConfigSelector',
   get: ({ get }) => {
     const focusStack = get(focusStackState);
     return focusStack.at(-1)?.globalHotkeysConfig;
