073912123ef3469e170c3d579909f42e0067485f
prastoin <paul@twenty.com>
2025-05-20 15:15:31 +0200

    fix(server): snake case reversed operator

--- packages/twenty-server/src/utils/is-snake-case-string.ts
+++ packages/twenty-server/src/utils/is-snake-case-string.ts
@@ -1,3 +1,3 @@
 const SNAKE_CASE_REGEX = /^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$/;
 
-export const isSnakeCaseString = (str: string) => !SNAKE_CASE_REGEX.test(str);
+export const isSnakeCaseString = (str: string) => SNAKE_CASE_REGEX.test(str);
