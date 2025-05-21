fdde7651a4e86df45a4f7e461dabc503b8f77c3f
Marie <51697796+ijreilly@users.noreply.github.com>
2025-05-21 14:09:11 +0200

    Fix email verification exception are not caught (#12174)
    cf
    [sentry](https://twenty-v7.sentry.io/issues/6606375182/?project=4507072499810304&query=is%3Aunresolved%20%21issue.type%3A%5Bperformance_consecutive_db_queries%2Cperformance_consecutive_http%2Cperformance_file_io_main_thread%2Cperformance_db_main_thread%2Cperformance_n_plus_one_db_queries%2Cperformance_n_plus_one_api_calls%2Cperformance_p95_endpoint_regression%2Cperformance_slow_db_query%2Cperformance_render_blocking_asset_span%2Cperformance_uncompressed_assets%2Cperformance_http_overhead%2Cperformance_large_http_payload%5D&referrer=issue-stream&sort=date&stream_index=4)
    
    Email verification exception were not caught when thrown outside of
    email-verification resolver, which be the case at authentication step.

--- packages/twenty-server/src/engine/core-modules/email-verification/email-verification.resolver.ts
+++ packages/twenty-server/src/engine/core-modules/email-verification/email-verification.resolver.ts
@@ -1,3 +1,4 @@
+import { UseFilters } from '@nestjs/common';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
 import { SOURCE_LOCALE } from 'twenty-shared/translations';
@@ -5,13 +6,13 @@ import { SOURCE_LOCALE } from 'twenty-shared/translations';
 import { DomainManagerService } from 'src/engine/core-modules/domain-manager/services/domain-manager.service';
 import { ResendEmailVerificationTokenInput } from 'src/engine/core-modules/email-verification/dtos/resend-email-verification-token.input';
 import { ResendEmailVerificationTokenOutput } from 'src/engine/core-modules/email-verification/dtos/resend-email-verification-token.output';
-import { emailVerificationGraphqlApiExceptionHandler } from 'src/engine/core-modules/email-verification/email-verification-exception-handler.util';
-import { EmailVerificationException } from 'src/engine/core-modules/email-verification/email-verification.exception';
+import { EmailVerificationExceptionFilter } from 'src/engine/core-modules/email-verification/email-verification-exception-filter.util';
 import { EmailVerificationService } from 'src/engine/core-modules/email-verification/services/email-verification.service';
 import { I18nContext } from 'src/engine/core-modules/i18n/types/i18n-context.type';
 import { OriginHeader } from 'src/engine/decorators/auth/origin-header.decorator';
 
 @Resolver()
+@UseFilters(EmailVerificationExceptionFilter)
 export class EmailVerificationResolver {
   constructor(
     private readonly emailVerificationService: EmailVerificationService,
@@ -25,22 +26,15 @@ export class EmailVerificationResolver {
     @OriginHeader() origin: string,
     @Context() context: I18nContext,
   ): Promise<ResendEmailVerificationTokenOutput> {
-    try {
-      const workspace =
-        await this.domainManagerService.getWorkspaceByOriginOrDefaultWorkspace(
-          origin,
-        );
-
-      return await this.emailVerificationService.resendEmailVerificationToken(
-        resendEmailVerificationTokenInput.email,
-        workspace,
-        context.req.headers['x-locale'] ?? SOURCE_LOCALE,
+    const workspace =
+      await this.domainManagerService.getWorkspaceByOriginOrDefaultWorkspace(
+        origin,
       );
-    } catch (error) {
-      if (error instanceof EmailVerificationException) {
-        return emailVerificationGraphqlApiExceptionHandler(error);
-      }
-      throw error;
-    }
+
+    return await this.emailVerificationService.resendEmailVerificationToken(
+      resendEmailVerificationTokenInput.email,
+      workspace,
+      context.req.headers['x-locale'] ?? SOURCE_LOCALE,
+    );
   }
 }
