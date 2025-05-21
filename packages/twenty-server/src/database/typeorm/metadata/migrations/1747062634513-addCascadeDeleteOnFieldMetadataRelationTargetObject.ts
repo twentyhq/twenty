dddbcbf8cc20a2f4864df1492bee7a2b13d82b05
Félix Malfait <felix@twenty.com>
2025-05-21 11:14:05 +0200

    Make migration more robust

--- packages/twenty-server/src/database/typeorm/metadata/migrations/1747062634513-addCascadeDeleteOnFieldMetadataRelationTargetObject.ts
+++ packages/twenty-server/src/database/typeorm/metadata/migrations/1747062634513-addCascadeDeleteOnFieldMetadataRelationTargetObject.ts
@@ -7,7 +7,7 @@ export class AddCascadeDeleteOnFieldMetadataRelationTargetObject1747062634513
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "metadata"."fieldMetadata" DROP CONSTRAINT "FK_6f6c87ec32cca956d8be321071c"`,
+      `ALTER TABLE "metadata"."fieldMetadata" DROP CONSTRAINT IF EXISTS "FK_6f6c87ec32cca956d8be321071c"`,
     );
     await queryRunner.query(
       `ALTER TABLE "metadata"."fieldMetadata" ADD CONSTRAINT "FK_6f6c87ec32cca956d8be321071c" FOREIGN KEY ("relationTargetObjectMetadataId") REFERENCES "metadata"."objectMetadata"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
