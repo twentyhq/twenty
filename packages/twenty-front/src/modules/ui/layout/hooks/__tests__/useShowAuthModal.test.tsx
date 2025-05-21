ed11a45bc7989d9275ecff40f60c457ded17e556
bosiraphael <raphael.bosi@gmail.com>
2025-05-21 18:32:52 +0200

    fix test

--- packages/twenty-front/src/modules/ui/layout/hooks/__tests__/useShowAuthModal.test.tsx
+++ packages/twenty-front/src/modules/ui/layout/hooks/__tests__/useShowAuthModal.test.tsx
@@ -1,14 +1,22 @@
 import { renderHook } from '@testing-library/react';
+import * as reactRouterDom from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import { AppPath } from '@/types/AppPath';
 import { useShowAuthModal } from '@/ui/layout/hooks/useShowAuthModal';
 import { isMatchingLocation } from '~/utils/isMatchingLocation';
 
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn().mockReturnValue({ pathname: '' }),
+}));
+
+const mockUseLocation = reactRouterDom.useLocation as jest.Mock;
+
 jest.mock('~/utils/isMatchingLocation');
 const mockIsMatchingLocation = jest.mocked(isMatchingLocation);
 
 const setupMockIsMatchingLocation = (pathname: string) => {
+  mockUseLocation.mockReturnValue({ pathname });
   mockIsMatchingLocation.mockImplementation(
     (_location, path) => path === pathname,
   );
