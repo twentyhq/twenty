8b946a316be05f07c5b9ec1fba73e7b6edf27618
Félix Malfait <felix@twenty.com>
2025-05-26 18:46:50 +0200

    Set client config to be non-render blocking

--- packages/twenty-front/src/modules/client-config/components/ClientConfigProvider.tsx
+++ packages/twenty-front/src/modules/client-config/components/ClientConfigProvider.tsx
@@ -2,26 +2,11 @@ import { useRecoilValue } from 'recoil';
 
 import { clientConfigApiStatusState } from '@/client-config/states/clientConfigApiStatusState';
 import { AppFullScreenErrorFallback } from '@/error-handler/components/AppFullScreenErrorFallback';
-import { AppPath } from '@/types/AppPath';
-import { useLocation } from 'react-router-dom';
-import { isMatchingLocation } from '~/utils/isMatchingLocation';
 
 export const ClientConfigProvider: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const { isLoaded, isErrored, error } = useRecoilValue(
-    clientConfigApiStatusState,
-  );
-
-  const location = useLocation();
-
-  // TODO: Implement a better loading strategy
-  if (
-    !isLoaded &&
-    !isMatchingLocation(location, AppPath.Verify) &&
-    !isMatchingLocation(location, AppPath.VerifyEmail)
-  )
-    return null;
+  const { isErrored, error } = useRecoilValue(clientConfigApiStatusState);
 
   return isErrored && error instanceof Error ? (
     <AppFullScreenErrorFallback
