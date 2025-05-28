01d20714d4b9d2794baa6dbb81e9e36cdd8dbebe
Marie Stoppa <marie.stoppa@essec.edu>
2025-05-28 17:17:37 +0200

    remove irrelevant export

--- packages/twenty-server/src/modules/timeline/timeline-activity.module.ts
+++ packages/twenty-server/src/modules/timeline/timeline-activity.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { ObjectMetadataRepositoryModule } from 'src/engine/object-metadata-repository/object-metadata-repository.module';
 import { TwentyORMModule } from 'src/engine/twenty-orm/twenty-orm.module';
 import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
-import { TimelineActivityRepository } from 'src/modules/timeline/repositiories/timeline-activity.repository';
 import { TimelineActivityService } from 'src/modules/timeline/services/timeline-activity.service';
 import { TimelineActivityWorkspaceEntity } from 'src/modules/timeline/standard-objects/timeline-activity.workspace-entity';
 
@@ -15,7 +14,7 @@ import { TimelineActivityWorkspaceEntity } from 'src/modules/timeline/standard-o
     ]),
     TwentyORMModule,
   ],
-  providers: [TimelineActivityService, TimelineActivityRepository],
-  exports: [TimelineActivityService, TimelineActivityRepository],
+  providers: [TimelineActivityService],
+  exports: [TimelineActivityService],
 })
 export class TimelineActivityModule {}
