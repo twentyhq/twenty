d88b10aeb39f922b281bfe5e2b3f807411262368
Abdul Rahman <ar5438376@gmail.com>
2025-05-28 12:39:17 +0530

    Update date mask format for YEAR_FIRST to use hyphens instead of slashes

--- packages/twenty-front/src/modules/ui/input/components/internal/date/constants/DateMask.ts
+++ packages/twenty-front/src/modules/ui/input/components/internal/date/constants/DateMask.ts
@@ -5,7 +5,7 @@ export const getDateMask = (dateFormat: DateFormat): string => {
     case DateFormat.DAY_FIRST:
       return 'd`/m`/Y`';
     case DateFormat.YEAR_FIRST:
-      return 'Y`/m`/d`';
+      return 'Y`-m`-d`';
     case DateFormat.MONTH_FIRST:
     default:
       return 'm`/d`/Y`';
