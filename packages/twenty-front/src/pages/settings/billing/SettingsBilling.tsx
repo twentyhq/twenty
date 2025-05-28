295f85a950b43c88af7d643a8ccf68d12c7ba5af
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-05-28 17:36:35 -0300

    🐛 [fix] Duplicate 'Billing' menu itens: * Removed duplicated billing menu item; * Hide Ours Billing menu item when 'isBillingEnabled' is false; * Fix code identation in SettingsBilling page.

--- packages/twenty-front/src/pages/settings/billing/SettingsBilling.tsx
+++ packages/twenty-front/src/pages/settings/billing/SettingsBilling.tsx
@@ -1,52 +1,57 @@
-import react from 'react';
 import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
 import { SettingsPath } from '@/types/SettingsPath';
 import { SubMenuTopBarContainer } from '@/ui/layout/page/components/SubMenuTopBarContainer';
 import { H2Title } from 'twenty-ui/display';
 import { Section } from 'twenty-ui/layout';
 import { getSettingsPath } from '~/utils/navigation/getSettingsPath';
-import { BillingDetailsButton, ChangeSubscriptionButton, CancelSubscriptionButton } from '~/pages/settings/billing/SettingsBillingPlanButtons';
+import {
+  BillingDetailsButton,
+  ChangeSubscriptionButton,
+  CancelSubscriptionButton,
+} from '~/pages/settings/billing/SettingsBillingPlanButtons';
 
 export const SettingsBillingPlan = () => {
-    return (
-        <SubMenuTopBarContainer
-            title={'Billing'}
-                  links={[
-                    {
-                      children: 'Billing',
-                      href: getSettingsPath(SettingsPath.BillingPlan),
-                    },
-                    { children: 'Billing' },
-                  ]}
-        >
-            <SettingsPageContainer>
-                <Section>
-                    <H2Title
-                        title={'Manage your subscription'}
-                        description={'Edit payment method, see your invoices and more'}
-                    />
+  return (
+    <SubMenuTopBarContainer
+      title={'Billing'}
+      links={[
+        {
+          children: 'Billing',
+          href: getSettingsPath(SettingsPath.BillingPlan),
+        },
+        { children: 'Billing' },
+      ]}
+    >
+      <SettingsPageContainer>
+        <Section>
+          <H2Title
+            title={'Manage your subscription'}
+            description={'Edit payment method, see your invoices and more'}
+          />
 
-                <BillingDetailsButton />
-                </Section>
+          <BillingDetailsButton />
+        </Section>
 
-                <Section>
-                    <H2Title
-                        title={'Change subscription plan'}
-                        description={'Allows you to view and modify your current subscription plans'}
-                    />
+        <Section>
+          <H2Title
+            title={'Change subscription plan'}
+            description={
+              'Allows you to view and modify your current subscription plans'
+            }
+          />
 
-                    <ChangeSubscriptionButton />
-                </Section>
+          <ChangeSubscriptionButton />
+        </Section>
 
-                <Section>
-                    <H2Title
-                        title={'Cancel your subscription'}
-                        description={'Your workspace will be disabled'}
-                    />
+        <Section>
+          <H2Title
+            title={'Cancel your subscription'}
+            description={'Your workspace will be disabled'}
+          />
 
-                    <CancelSubscriptionButton />
-                </Section>
-            </SettingsPageContainer>
-        </SubMenuTopBarContainer>
-    );
-}
+          <CancelSubscriptionButton />
+        </Section>
+      </SettingsPageContainer>
+    </SubMenuTopBarContainer>
+  );
+};
