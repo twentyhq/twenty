f34691bfb84b0ca88981ba3c6a37e6c1aebea1f8
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-05-28 22:20:24 +0200

    fix(client-config): set `isLoaded` to false on API status update

--- packages/twenty-front/src/modules/client-config/hooks/useClientConfig.ts
+++ packages/twenty-front/src/modules/client-config/hooks/useClientConfig.ts
@@ -29,7 +29,7 @@ export const useClientConfig = (): UseClientConfigResult => {
           set(clientConfigApiStatusState, (prev) => ({
             ...prev,
             isLoading: false,
-            isLoaded: true,
+            isLoaded: false,
             data: { clientConfig },
           }));
         } catch (err) {
