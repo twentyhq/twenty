039a1c1debdb998a02aac90a0a45daa9b5f87ba0
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-05-28 18:13:57 +0200

    chore(workspace): add missing comma to useEffect dependencies in WorkspaceProviderEffect

--- packages/twenty-front/src/modules/workspace/components/WorkspaceProviderEffect.tsx
+++ packages/twenty-front/src/modules/workspace/components/WorkspaceProviderEffect.tsx
@@ -53,7 +53,7 @@ export const WorkspaceProviderEffect = () => {
     redirectToWorkspaceDomain,
     getPublicWorkspaceData,
     currentLocationHostname,
-    isWorkspaceHostnameMatchCurrentLocationHostname
+    isWorkspaceHostnameMatchCurrentLocationHostname,
   ]);
 
   useEffect(() => {
