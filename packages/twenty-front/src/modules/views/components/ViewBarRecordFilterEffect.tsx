0cd34153a1387f64476ebe7fc1c5471f12358647
prastoin <paul@twenty.com>
2025-05-28 13:58:07 +0200

    Revert "fix(front): evict view cache and allow state to get hydrated"
    This reverts commit ad9aefc617825783ca21f499f965720f0c511b81.

--- packages/twenty-front/src/modules/views/components/ViewBarRecordFilterEffect.tsx
+++ packages/twenty-front/src/modules/views/components/ViewBarRecordFilterEffect.tsx
@@ -3,13 +3,14 @@ import { useFilterableFieldMetadataItems } from '@/object-record/record-filter/h
 import { currentRecordFiltersComponentState } from '@/object-record/record-filter/states/currentRecordFiltersComponentState';
 import { useRecordIndexContextOrThrow } from '@/object-record/record-index/contexts/RecordIndexContext';
 import { prefetchViewFromViewIdFamilySelector } from '@/prefetch/states/selector/prefetchViewFromViewIdFamilySelector';
-import { useRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentStateV2';
+import { useRecoilComponentFamilyStateV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentFamilyStateV2';
 import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
+import { useSetRecoilComponentStateV2 } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentStateV2';
+import { hasInitializedCurrentRecordFiltersComponentFamilyState } from '@/views/states/hasInitializedCurrentRecordFiltersComponentFamilyState';
 import { mapViewFiltersToFilters } from '@/views/utils/mapViewFiltersToFilters';
 import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { isDefined } from 'twenty-shared/utils';
-import { isDeeplyEqual } from '~/utils/isDeeplyEqual';
 
 export const ViewBarRecordFilterEffect = () => {
   const currentViewId = useRecoilComponentValueV2(
@@ -24,25 +25,39 @@ export const ViewBarRecordFilterEffect = () => {
     }),
   );
 
-  const [currentRecordFilters, setCurrentRecordFilters] =
-    useRecoilComponentStateV2(currentRecordFiltersComponentState);
+  const [
+    hasInitializedCurrentRecordFilters,
+    setHasInitializedCurrentRecordFilters,
+  ] = useRecoilComponentFamilyStateV2(
+    hasInitializedCurrentRecordFiltersComponentFamilyState,
+    {
+      viewId: currentViewId ?? undefined,
+    },
+  );
+
+  const setCurrentRecordFilters = useSetRecoilComponentStateV2(
+    currentRecordFiltersComponentState,
+  );
 
   const { filterableFieldMetadataItems } = useFilterableFieldMetadataItems(
     objectMetadataItem.id,
   );
 
   useEffect(() => {
-    if (isDefined(currentView)) {
+    if (isDefined(currentView) && !hasInitializedCurrentRecordFilters) {
       if (currentView.objectMetadataId !== objectMetadataItem.id) {
         return;
       }
 
-      const newRecordFilters = mapViewFiltersToFilters(
-        currentView.viewFilters,
-        filterableFieldMetadataItems,
-      );
-      if (!isDeeplyEqual(currentRecordFilters, newRecordFilters)) {
-        setCurrentRecordFilters(newRecordFilters);
+      if (isDefined(currentView)) {
+        setCurrentRecordFilters(
+          mapViewFiltersToFilters(
+            currentView.viewFilters,
+            filterableFieldMetadataItems,
+          ),
+        );
+
+        setHasInitializedCurrentRecordFilters(true);
       }
     }
   }, [
@@ -50,6 +65,8 @@ export const ViewBarRecordFilterEffect = () => {
     currentRecordFilters,
     setCurrentRecordFilters,
     filterableFieldMetadataItems,
+    hasInitializedCurrentRecordFilters,
+    setHasInitializedCurrentRecordFilters,
     currentView,
     objectMetadataItem,
   ]);
