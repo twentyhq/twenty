cf5dd6e424442dbcf512efd8687c037664dee4de
Abdul Rahman <ar5438376@gmail.com>
2025-05-30 16:45:47 +0530

    Refactor TSVectorFilterType to simplify input fields by removing unused operators

--- packages/twenty-server/src/engine/api/graphql/workspace-schema-builder/graphql-types/input/ts-vector-filter.input-type.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-schema-builder/graphql-types/input/ts-vector-filter.input-type.ts
@@ -1,20 +1,8 @@
-import {
-  GraphQLInputObjectType,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLString,
-} from 'graphql';
-
-import { FilterIs } from './filter-is.input-type';
+import { GraphQLInputObjectType, GraphQLString } from 'graphql';
 
 export const TSVectorFilterType = new GraphQLInputObjectType({
   name: 'TSVectorFilter',
   fields: {
     contains: { type: GraphQLString },
-    matches: { type: GraphQLString },
-    containsAny: { type: new GraphQLList(new GraphQLNonNull(GraphQLString)) },
-    containsAll: { type: new GraphQLList(new GraphQLNonNull(GraphQLString)) },
-    fuzzy: { type: GraphQLString },
-    is: { type: FilterIs },
   },
 });
