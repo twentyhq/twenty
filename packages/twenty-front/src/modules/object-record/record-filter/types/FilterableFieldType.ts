93b6ca779b5d58857de12fa5eacbeea41d0b1d2d
Abdul Rahman <ar5438376@gmail.com>
2025-05-31 06:51:26 +0530

    Remove 'TS_VECTOR' from FILTERABLE_FIELD_TYPES in FilterableFieldType.ts

--- packages/twenty-front/src/modules/object-record/record-filter/types/FilterableFieldType.ts
+++ packages/twenty-front/src/modules/object-record/record-filter/types/FilterableFieldType.ts
@@ -20,7 +20,6 @@ export const FILTERABLE_FIELD_TYPES = [
   'ARRAY',
   'RAW_JSON',
   'BOOLEAN',
-  'TS_VECTOR',
 ] as const;
 
 type FilterableFieldTypeBaseLiteral = (typeof FILTERABLE_FIELD_TYPES)[number];
