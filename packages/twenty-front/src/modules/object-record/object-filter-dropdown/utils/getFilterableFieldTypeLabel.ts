476decda5c39b0654e551a2b3fee87d813073e8d
Abdul Rahman <ar5438376@gmail.com>
2025-06-01 20:04:26 +0530

    Update getFilterableFieldTypeLabel to use translation for 'Search'

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/utils/getFilterableFieldTypeLabel.ts
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/utils/getFilterableFieldTypeLabel.ts
@@ -1,11 +1,12 @@
 import { FilterableFieldType } from '@/object-record/record-filter/types/FilterableFieldType';
 import { SETTINGS_FIELD_TYPE_CONFIGS } from '@/settings/data-model/constants/SettingsFieldTypeConfigs';
+import { t } from '@lingui/core/macro';
 
 export const getFilterableFieldTypeLabel = (
   filterableFieldType: FilterableFieldType,
 ) => {
   if (filterableFieldType === 'TS_VECTOR') {
-    return 'Search';
+    return t`Search`;
   }
   return SETTINGS_FIELD_TYPE_CONFIGS[filterableFieldType].label;
 };
