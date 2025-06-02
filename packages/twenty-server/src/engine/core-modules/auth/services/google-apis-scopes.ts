2001041a4840a77b6c5b0c255b9887aca299299f
Guillim <guillim@users.noreply.github.com>
2025-06-02 12:28:55 +0200

    Google-scopes-handling (#12362)
    # Summary
    
    Enhanced the Google OAuth flow to better handle missing permissions and
    improved user experience by redirecting to settings/account page.
    
    
    ## Changes
    - Added new google-apis-scopes.ts service for better scope management
    - Updated Google APIs auth controller for better flow control
    - New tests for this logic
    
    ## User request
    From @bonapara email test and need to better handle user flow during the
    connect email flow
    
    Before :
    <img width="574" alt="Screenshot 2025-05-28 at 17 58 59"
    src="https://github.com/user-attachments/assets/fd54625b-e211-4b2f-b76a-48bcb08b5222"
    />
    
    After : 
    <img width="1143" alt="Screenshot 2025-05-28 at 16 29 05"
    src="https://github.com/user-attachments/assets/8f3d1f2c-9e02-4d25-b949-fe2b20f048f4"
    />
    
    ## Reference :
    For google specialities, I added this link in the `export const
    getGoogleApisOauthScopes` in order to keep that in mind
    
    https://developers.google.com/identity/protocols/oauth2/scopes

---
+++ packages/twenty-server/src/engine/core-modules/auth/services/google-apis-scopes.ts
@@ -0,0 +1,52 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+
+import {
+  AuthException,
+  AuthExceptionCode,
+} from 'src/engine/core-modules/auth/auth.exception';
+import { includesExpectedScopes } from 'src/engine/core-modules/auth/services/google-apis-scopes.service.util';
+import { getGoogleApisOauthScopes } from 'src/engine/core-modules/auth/utils/get-google-apis-oauth-scopes';
+
+interface TokenInfoResponse {
+  scope: string;
+  exp: string;
+  email: string;
+  email_verified: string;
+  access_type: string;
+  client_id: string;
+  user_id: string;
+  aud: string;
+  azp: string;
+  sub: string;
+  hd: string;
+}
+
+@Injectable()
+export class GoogleAPIScopesService {
+  constructor(private httpService: HttpService) {}
+
+  public async getScopesFromGoogleAccessTokenAndCheckIfExpectedScopesArePresent(
+    accessToken: string,
+  ): Promise<{ scopes: string[]; isValid: boolean }> {
+    try {
+      const response = await this.httpService.axiosRef.get<TokenInfoResponse>(
+        `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${accessToken}`,
+        { timeout: 600 },
+      );
+
+      const scopes = response.data.scope.split(' ');
+      const expectedScopes = getGoogleApisOauthScopes();
+
+      return {
+        scopes,
+        isValid: includesExpectedScopes(scopes, expectedScopes),
+      };
+    } catch (error) {
+      throw new AuthException(
+        'Google account connect error: cannot read scopes from token',
+        AuthExceptionCode.INSUFFICIENT_SCOPES,
+      );
+    }
+  }
+}
