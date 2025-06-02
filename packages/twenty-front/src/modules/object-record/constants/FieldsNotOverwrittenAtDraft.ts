0d03ff7b7bff669971c43e6e6420fcd3848b9ec0
Raphaël Bosi <71827178+bosiraphael@users.noreply.github.com>
2025-06-02 15:33:57 +0200

    Don't overwrite SELECT fields at draft (#12413)
    Fixes https://github.com/twentyhq/twenty/issues/12328

--- packages/twenty-front/src/modules/object-record/constants/FieldsNotOverwrittenAtDraft.ts
+++ packages/twenty-front/src/modules/object-record/constants/FieldsNotOverwrittenAtDraft.ts
@@ -4,4 +4,7 @@ export const FIELD_NOT_OVERWRITTEN_AT_DRAFT = [
   FieldMetadataType.ADDRESS,
   FieldMetadataType.PHONES,
   FieldMetadataType.LINKS,
+  FieldMetadataType.MULTI_SELECT,
+  FieldMetadataType.RATING,
+  FieldMetadataType.SELECT,
 ];
