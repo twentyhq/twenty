0dcc4123bb21c0ab201aad8b64ae79928c1ebbfd
Abdul Rahman <ar5438376@gmail.com>
2025-06-02 23:02:39 +0530

    Refactor DateTimeInput and getDateTimeMask to use updated getDateMask utility function

--- packages/twenty-front/src/modules/ui/input/components/internal/date/utils/getDateTimeMask.ts
+++ packages/twenty-front/src/modules/ui/input/components/internal/date/utils/getDateTimeMask.ts
@@ -1,7 +1,7 @@
 import { TIME_MASK } from '@/ui/input/components/internal/date/constants/TimeMask';
 
 import { DateFormat } from '@/localization/constants/DateFormat';
-import { getDateMask } from '../constants/DateMask';
+import { getDateMask } from './getDateMask';
 
 export const getDateTimeMask = (dateFormat: DateFormat): string => {
   return `${getDateMask(dateFormat)} ${TIME_MASK}`;
