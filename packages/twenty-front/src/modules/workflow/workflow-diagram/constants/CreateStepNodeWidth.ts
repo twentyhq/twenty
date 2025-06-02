14c2ba06ac91903076e7e39a9602d006bd8e7118
martmull <martmull@hotmail.fr>
2025-06-02 14:47:17 +0200

    Code review returns: greptile

--- packages/twenty-front/src/modules/workflow/workflow-diagram/constants/CreateStepNodeWidth.ts
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/constants/CreateStepNodeWidth.ts
@@ -1,3 +1,8 @@
 import { NODE_BORDER_WIDTH } from '@/workflow/workflow-diagram/constants/NodeBorderWidth';
 
-export const CREATE_STEP_NODE_WIDTH = 24 + 8 + NODE_BORDER_WIDTH; // New step icon width + step padding + border width
+const STEP_ICON_WIDTH = 24;
+
+const STEP_PADDING = 8;
+
+export const CREATE_STEP_NODE_WIDTH =
+  STEP_ICON_WIDTH + STEP_PADDING + NODE_BORDER_WIDTH;
