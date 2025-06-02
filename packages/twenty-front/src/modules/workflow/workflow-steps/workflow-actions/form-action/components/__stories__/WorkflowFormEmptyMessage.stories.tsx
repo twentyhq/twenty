a508f4a4fb7e8ebd5e4002b44f48ead770d3677f
Thomas Trompette <thomas.trompette@sfr.fr>
2025-06-02 18:04:39 +0200

    Add empty message for form actions (#12414)
    <img width="503" alt="Capture d’écran 2025-06-02 à 15 55 36"
    src="https://github.com/user-attachments/assets/9b3f60ae-7a13-45f8-aa87-ba32211e832f"
    />

---
+++ packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/form-action/components/__stories__/WorkflowFormEmptyMessage.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, within } from '@storybook/test';
+import { I18nFrontDecorator } from '~/testing/decorators/I18nFrontDecorator';
+import { WorkflowFormEmptyMessage } from '../WorkflowFormEmptyMessage';
+
+const meta: Meta<typeof WorkflowFormEmptyMessage> = {
+  title: 'Modules/Workflow/Actions/Form/WorkflowFormEmptyMessage',
+  component: WorkflowFormEmptyMessage,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [I18nFrontDecorator],
+};
+
+export default meta;
+type Story = StoryObj<typeof WorkflowFormEmptyMessage>;
+
+export const Default: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const messageTitleContainer = await canvas.findByTestId(
+      'empty-form-message-title',
+    );
+    const messageDescriptionContainer = await canvas.findByTestId(
+      'empty-form-message-description',
+    );
+
+    expect(messageTitleContainer).toBeVisible();
+    expect(messageDescriptionContainer).toBeVisible();
+  },
+};
