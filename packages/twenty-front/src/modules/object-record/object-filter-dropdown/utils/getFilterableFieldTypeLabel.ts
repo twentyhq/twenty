a2f63253043722b50bace6d74e451f9546e731dd
Abdul Rahman <ar5438376@gmail.com>
2025-06-02 20:09:42 +0530

    Remove 'TS_VECTOR' from FILTERABLE_FIELD_TYPES and update getFilterableFieldTypeLabel to eliminate the TS_VECTOR condition

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/utils/getFilterableFieldTypeLabel.ts
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/utils/getFilterableFieldTypeLabel.ts
@@ -1,12 +1,8 @@
 import { FilterableFieldType } from '@/object-record/record-filter/types/FilterableFieldType';
 import { SETTINGS_FIELD_TYPE_CONFIGS } from '@/settings/data-model/constants/SettingsFieldTypeConfigs';
-import { t } from '@lingui/core/macro';
 
 export const getFilterableFieldTypeLabel = (
   filterableFieldType: FilterableFieldType,
 ) => {
-  if (filterableFieldType === 'TS_VECTOR') {
-    return t`Search`;
-  }
   return SETTINGS_FIELD_TYPE_CONFIGS[filterableFieldType].label;
 };
