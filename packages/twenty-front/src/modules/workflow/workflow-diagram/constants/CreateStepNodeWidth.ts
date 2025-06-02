d662801bac64e945396b164db5dc955229237a72
martmull <martmull@hotmail.fr>
2025-06-02 14:10:19 +0200

    Fix edge x position

---
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/constants/CreateStepNodeWidth.ts
@@ -0,0 +1,3 @@
+import { NODE_BORDER_WIDTH } from '@/workflow/workflow-diagram/constants/NodeBorderWidth';
+
+export const CREATE_STEP_NODE_WIDTH = 24 + 8 + NODE_BORDER_WIDTH; // New step icon width + step padding + border width
