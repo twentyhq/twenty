8c2da395bbce46196e0b2cc00c4c460c1e47bab7
Félix Malfait <felix.malfait@gmail.com>
2025-06-03 08:50:44 +0200

    Fix Invalid input in Track Analytics (#12421)
    Fix #12231

---
+++ packages/twenty-server/src/engine/core-modules/audit/audit-exception-filter.ts
@@ -0,0 +1,23 @@
+import { Catch, ExceptionFilter } from '@nestjs/common';
+
+import {
+  AuditException,
+  AuditExceptionCode,
+} from 'src/engine/core-modules/audit/audit.exception';
+import { UserInputError } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
+
+@Catch(AuditException)
+export class AuditExceptionFilter implements ExceptionFilter {
+  catch(exception: AuditException) {
+    switch (exception.code) {
+      case AuditExceptionCode.INVALID_TYPE:
+      case AuditExceptionCode.INVALID_INPUT:
+        throw new UserInputError(exception.message);
+      default: {
+        const _exhaustiveCheck: never = exception.code;
+
+        throw exception;
+      }
+    }
+  }
+}
