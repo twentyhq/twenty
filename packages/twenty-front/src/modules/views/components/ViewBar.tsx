4ecfd4e3f7921fe66157b8899dcf00ad5cc63f32
Abdul Rahman <ar5438376@gmail.com>
2025-06-03 21:37:26 +0530

    Update ViewBar to use FiltersHotkeyScope for filter dropdown hotkey scope, replacing the previous RecordIndexHotkeyScope.

--- packages/twenty-front/src/modules/views/components/ViewBar.tsx
+++ packages/twenty-front/src/modules/views/components/ViewBar.tsx
@@ -14,7 +14,6 @@ import { ObjectFilterDropdownComponentInstanceContext } from '@/object-record/ob
 import { FiltersHotkeyScope } from '@/object-record/object-filter-dropdown/types/FiltersHotkeyScope';
 import { VIEW_SORT_DROPDOWN_ID } from '@/object-record/object-sort-dropdown/constants/ViewSortDropdownId';
 import { ObjectSortDropdownComponentInstanceContext } from '@/object-record/object-sort-dropdown/states/context/ObjectSortDropdownComponentInstanceContext';
-import { RecordIndexHotkeyScope } from '@/object-record/record-index/types/RecordIndexHotkeyScope';
 import { ViewBarFilterDropdown } from '@/views/components/ViewBarFilterDropdown';
 import { ViewBarFilterDropdownVectorSearchHotkeyEffect } from '@/views/components/ViewBarFilterDropdownVectorSearchHotkeyEffect';
 import { ViewBarRecordFilterEffect } from '@/views/components/ViewBarRecordFilterEffect';
@@ -64,7 +63,7 @@ export const ViewBar = ({
             >
               <ViewBarFilterDropdown
                 hotkeyScope={{
-                  scope: RecordIndexHotkeyScope.RecordIndex,
+                  scope: FiltersHotkeyScope.ObjectFilterDropdownButton,
                 }}
               />
             </ObjectFilterDropdownComponentInstanceContext.Provider>
