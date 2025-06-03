8f628953094680b97f652c23101c6ee1500257bc
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-06-03 11:33:54 -0300

    ⚡️ refactor: Inter billing email template improvements: * Removed plain text link from inter billing e-mail template; * Fix `Logo` component not showing correct logo; * Added pt-BR translation for the inter billing e-mail teamplate;

--- packages/twenty-emails/src/emails/inter-billing-charge-file.email.tsx
+++ packages/twenty-emails/src/emails/inter-billing-charge-file.email.tsx
@@ -2,7 +2,6 @@ import { i18n } from '@lingui/core';
 import { Trans } from '@lingui/react';
 import { BaseEmail } from 'src/components/BaseEmail';
 import { CallToAction } from 'src/components/CallToAction';
-import { Link } from 'src/components/Link';
 import { MainText } from 'src/components/MainText';
 import { Title } from 'src/components/Title';
 import { APP_LOCALES } from 'twenty-shared/translations';
@@ -27,7 +26,6 @@ export const InterBillingChargeFileEmail = ({
           values={{ duration }}
         />
         <br />
-        <Link href={link} value={link} />
       </MainText>
       <br />
       <CallToAction href={link} value={i18n._('Boleto Inter')} />
