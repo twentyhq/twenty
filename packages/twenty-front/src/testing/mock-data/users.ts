3438144f2ac3814b70713c255576832db7c11d4f
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-06-03 16:18:44 -0300

    ✏️ fix-typos: Fix front build type errors for test files

--- packages/twenty-front/src/testing/mock-data/users.ts
+++ packages/twenty-front/src/testing/mock-data/users.ts
@@ -13,6 +13,10 @@ import {
   WorkspaceMemberTimeFormatEnum,
 } from '~/generated/graphql';
 
+type MockedWorkspace = Omit<Workspace, 'stripeIntegrations' | 'billingPlans'>;
+
+type MockedWorkspaceMmeber = Omit<WorkspaceMember, 'agentId'>;
+
 type MockedUser = Pick<
   User,
   | 'id'
@@ -26,11 +30,13 @@ type MockedUser = Pick<
   | 'onboardingStatus'
   | 'userVars'
 > & {
-  workspaceMember: Omit<WorkspaceMember, 'agentId'> | null;
+  workspaceMember: MockedWorkspaceMmeber | null;
   locale: string;
-  currentWorkspace: Omit<Workspace, 'stripeIntegrations'>;
-  workspaces: Array<{ workspace: Omit<Workspace, 'stripeIntegrations'> }>;
-  workspaceMembers: Omit<WorkspaceMember, 'agentId'>[];
+  currentWorkspace: MockedWorkspace;
+  workspaces: Array<{
+    workspace: MockedWorkspace;
+  }>;
+  workspaceMembers: MockedWorkspaceMmeber[];
   currentUserWorkspace: CurrentUserWorkspace;
 };
 
@@ -39,7 +45,10 @@ export const avatarUrl =
 export const workspaceLogoUrl =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAACb0lEQVR4nO2VO4taQRTHr3AblbjxEVlwCwVhg7BoqqCIjy/gAyyFWNlYBOxsfH0KuxgQGwXRUkGuL2S7i1barGAgiwbdW93SnGOc4BonPiKahf3DwXFmuP/fPM4ZlvmlTxAhCBdzHnEQWYiv7Mr4C3NeuVYhQYDPzOUUQgDLBQGcLHNhvQK8DACPx8PTxiqVyvISG43GbyaT6Qfpn06n0m63e/tPAPF4vJ1MJu8kEsnWTCkWi1yr1RKGw+GDRqPBOTfr44vFQvD7/Q/lcpmaaVQAr9fLp1IpO22c47hGOBz+MB6PH+Vy+VYDAL8qlUoGtVotzOfzq4MAgsHgE/6KojiQyWR/bKVSqbSszHFM8Pl8z1YK48JsNltCOBwOnrYLO+8AAIjb+nHbycoTiUQfDJ7tFq4YAHiVSmXBxcD41u8flQU8z7fhzO0r83atVns3Go3u9Xr9x0O/RQXo9/tsIBBg6vX606a52Wz+bZ7P5/WwG29gxSJzhKgA6XTaDoFNF+krFAocmC//4yWEcSf2wTm7mCO19xFgSsKOLI16vV7b7XY7mRNoLwA0JymJ5uQIzgIAuX5PzDElT2m+E8BqtQ4ymcx7Yq7T6a6ZE4sKgOadTucaCwkxp1UzlEKh0GDxIXOwDWHAdi6Xe3swQDQa/Q7mywoolUpvsaptymazDWKxmBHTlWXZm405BFZoNpuGgwEmk4mE2SGtVivii4f1AO7J3ZopkQCQj7Ar1FeRChCJRJzVapX6DKNIfSc1Ax+wtQWQ55h6bH8FWDfYV4fO3wlwDr0C/BcADYiTPCxHqIEA2QsCZAkAKnRGkMbKN/sTX5YHPQ1e7SkAAAAASUVORK5CYII=';
 
-export const mockCurrentWorkspace: Omit<Workspace, 'stripeIntegrations'> = {
+export const mockCurrentWorkspace: Omit<
+  Workspace,
+  'stripeIntegrations' | 'billingPlans'
+> = {
   subdomain: 'acme.twenty.com',
   id: '7dfbc3f7-6e5e-4128-957e-8d86808cdf6w',
   displayName: 'Twenty',
@@ -92,7 +101,7 @@ export const mockCurrentWorkspace: Omit<Workspace, 'stripeIntegrations'> = {
   databaseUrl: '',
 };
 
-export const mockedWorkspaceMemberData: Omit<WorkspaceMember, 'agentId'> = {
+export const mockedWorkspaceMemberData: MockedWorkspaceMmeber = {
   __typename: 'WorkspaceMember',
   id: '7dfbc3f7-6e5e-4128-957e-8d86808cdf6b',
   colorScheme: 'Light',
