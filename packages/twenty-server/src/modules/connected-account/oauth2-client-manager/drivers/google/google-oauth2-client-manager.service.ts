a8eacd30a16b19bc2ef81885d9deecd56f377a0f
Guillim <guillim@users.noreply.github.com>
2025-06-03 14:23:36 +0200

    better logging of calendar failures (#12431)
    better logging to troubleshoot calendar events failure :
    
    ```
    [Nest] 1  -  [GoogleCalendarGetEventsService] Error: internal_failure
    ```

--- packages/twenty-server/src/modules/connected-account/oauth2-client-manager/drivers/google/google-oauth2-client-manager.service.ts
+++ packages/twenty-server/src/modules/connected-account/oauth2-client-manager/drivers/google/google-oauth2-client-manager.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 import { OAuth2Client } from 'google-auth-library';
 import { google } from 'googleapis';
@@ -7,7 +7,10 @@ import { TwentyConfigService } from 'src/engine/core-modules/twenty-config/twent
 
 @Injectable()
 export class GoogleOAuth2ClientManagerService {
-  constructor(private readonly twentyConfigService: TwentyConfigService) {}
+  constructor(
+    private readonly twentyConfigService: TwentyConfigService,
+    private readonly logger: Logger,
+  ) {}
 
   public async getOAuth2Client(refreshToken: string): Promise<OAuth2Client> {
     const gmailClientId = this.twentyConfigService.get('AUTH_GOOGLE_CLIENT_ID');
@@ -15,15 +18,24 @@ export class GoogleOAuth2ClientManagerService {
       'AUTH_GOOGLE_CLIENT_SECRET',
     );
 
-    const oAuth2Client = new google.auth.OAuth2(
-      gmailClientId,
-      gmailClientSecret,
-    );
+    try {
+      const oAuth2Client = new google.auth.OAuth2(
+        gmailClientId,
+        gmailClientSecret,
+      );
 
-    oAuth2Client.setCredentials({
-      refresh_token: refreshToken,
-    });
+      oAuth2Client.setCredentials({
+        refresh_token: refreshToken,
+      });
 
-    return oAuth2Client;
+      return oAuth2Client;
+    } catch (error) {
+      this.logger.error(
+        `Error in ${GoogleOAuth2ClientManagerService.name}`,
+        error,
+      );
+
+      throw error;
+    }
   }
 }
