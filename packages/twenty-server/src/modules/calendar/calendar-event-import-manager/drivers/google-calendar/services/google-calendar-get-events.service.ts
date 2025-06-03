9eda1b750ea57b92b55c05b3087e473bbb19607c
guillim <guigloo@msn.com>
2025-06-03 13:01:36 +0200

    better logging to troubleshoot calendar events failure "internal_error"

--- packages/twenty-server/src/modules/calendar/calendar-event-import-manager/drivers/google-calendar/services/google-calendar-get-events.service.ts
+++ packages/twenty-server/src/modules/calendar/calendar-event-import-manager/drivers/google-calendar/services/google-calendar-get-events.service.ts
@@ -46,6 +46,11 @@ export class GoogleCalendarGetEventsService {
           showDeleted: true,
         })
         .catch(async (error: GaxiosError) => {
+          this.logger.error(
+            `Error in ${GoogleCalendarGetEventsService.name} - getCalendarEvents`,
+            error,
+          );
+
           this.handleError(error);
 
           return {
