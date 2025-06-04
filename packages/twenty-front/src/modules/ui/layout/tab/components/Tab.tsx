da8afdff3f2995665713f92d5440baec14a5c8ec
ehconitin <nitinkoche03@gmail.com>
2025-06-04 10:59:18 +0530

    yagni - we did not need forward ref in tab

--- packages/twenty-front/src/modules/ui/layout/tab/components/Tab.tsx
+++ packages/twenty-front/src/modules/ui/layout/tab/components/Tab.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@emotion/react';
-import { ReactElement, forwardRef } from 'react';
+import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import { Pill } from 'twenty-ui/components';
 import { Avatar, IconComponent } from 'twenty-ui/display';
@@ -23,73 +23,65 @@ type TabProps = {
   logo?: string;
 };
 
-export const Tab = forwardRef<HTMLButtonElement, TabProps>(
-  (
-    {
-      id,
-      title,
-      Icon,
-      active = false,
-      onClick,
-      className,
-      disabled,
-      pill,
+export const Tab = ({
+  id,
+  title,
+  Icon,
+  active = false,
+  onClick,
+  className,
+  disabled,
+  pill,
+  to,
+  logo,
+}: TabProps) => {
+  const theme = useTheme();
+  const { onClick: handleClick, onMouseDown: handleMouseDown } =
+    useMouseDownNavigation({
       to,
-      logo,
-    },
-    ref,
-  ) => {
-    const theme = useTheme();
-    const { onClick: handleClick, onMouseDown: handleMouseDown } =
-      useMouseDownNavigation({
-        to,
-        onClick,
-        disabled,
-      });
+      onClick,
+      disabled,
+    });
 
-    const iconColor = active
-      ? theme.font.color.primary
-      : disabled
-        ? theme.font.color.light
-        : theme.font.color.secondary;
+  const iconColor = active
+    ? theme.font.color.primary
+    : disabled
+      ? theme.font.color.light
+      : theme.font.color.secondary;
 
-    return (
-      <StyledTabBase
-        ref={ref}
-        onClick={handleClick}
-        onMouseDown={handleMouseDown}
-        active={active}
-        className={className}
-        disabled={disabled}
-        data-testid={'tab-' + id}
-        as={to ? Link : 'button'}
-        to={to}
-      >
-        <StyledTabHover>
-          <StyledTabIconContainer>
-            {logo && (
-              <Avatar
-                avatarUrl={logo}
-                size="md"
-                placeholder={title}
-                iconColor={iconColor}
-              />
-            )}
-            {Icon && (
-              <Avatar
-                Icon={Icon}
-                size="md"
-                placeholder={title}
-                iconColor={iconColor}
-              />
-            )}
-          </StyledTabIconContainer>
-          {title}
-          {pill && typeof pill === 'string' ? <Pill label={pill} /> : pill}
-        </StyledTabHover>
-      </StyledTabBase>
-    );
-  },
-);
-
-Tab.displayName = 'Tab';
+  return (
+    <StyledTabBase
+      onClick={handleClick}
+      onMouseDown={handleMouseDown}
+      active={active}
+      className={className}
+      disabled={disabled}
+      data-testid={'tab-' + id}
+      as={to ? Link : 'button'}
+      to={to}
+    >
+      <StyledTabHover>
+        <StyledTabIconContainer>
+          {logo && (
+            <Avatar
+              avatarUrl={logo}
+              size="md"
+              placeholder={title}
+              iconColor={iconColor}
+            />
+          )}
+          {Icon && (
+            <Avatar
+              Icon={Icon}
+              size="md"
+              placeholder={title}
+              iconColor={iconColor}
+            />
+          )}
+        </StyledTabIconContainer>
+        {title}
+        {pill && typeof pill === 'string' ? <Pill label={pill} /> : pill}
+      </StyledTabHover>
+    </StyledTabBase>
+  );
+};
