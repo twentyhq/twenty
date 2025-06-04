3a3ccd945340820422c9f9761764011b98ad9272
Ronnie Feng <ronnie@ronniefeng.com>
2025-06-04 10:30:58 -0400

    admin panel and visibility adjustments (#26)

--- packages/twenty-front/src/modules/settings/integrations/hooks/useSettingsIntegrationCategories.ts
+++ packages/twenty-front/src/modules/settings/integrations/hooks/useSettingsIntegrationCategories.ts
@@ -1,12 +1,20 @@
+import { currentUserState } from '@/auth/states/currentUserState';
 import { MOCK_REMOTE_DATABASES } from '@/settings/integrations/constants/MockRemoteDatabases';
+import { SETTINGS_INTEGRATION_REQUEST_CATEGORY } from '@/settings/integrations/constants/SettingsIntegrationRequest';
+import { SETTINGS_INTEGRATION_WINDMILL_CATEGORY } from '@/settings/integrations/constants/SettingsIntegrationWindmill';
 import { SETTINGS_INTEGRATION_ZAPIER_CATEGORY } from '@/settings/integrations/constants/SettingsIntegrationZapier';
 import { SettingsIntegrationCategory } from '@/settings/integrations/types/SettingsIntegrationCategory';
+import { isInsurOSAdminAccount } from '@/settings/integrations/utils/AdminAccounts';
 import { getSettingsIntegrationAll } from '@/settings/integrations/utils/getSettingsIntegrationAll';
 import { useIsFeatureEnabled } from '@/workspace/hooks/useIsFeatureEnabled';
+import { useRecoilValue } from 'recoil';
 import { FeatureFlagKey } from '~/generated/graphql';
 
 export const useSettingsIntegrationCategories =
   (): SettingsIntegrationCategory[] => {
+    const currentUser = useRecoilValue(currentUserState);
+    const { email } = currentUser ?? {};
+    const showAdminIntegrations = isInsurOSAdminAccount(email ?? '');
     const isAirtableIntegrationEnabled = useIsFeatureEnabled(
       FeatureFlagKey.IS_AIRTABLE_INTEGRATION_ENABLED,
     );
@@ -14,39 +22,49 @@ export const useSettingsIntegrationCategories =
       ({ name }) => name === 'airtable',
     )?.isActive;
 
-    // const isPostgresqlIntegrationEnabled = useIsFeatureEnabled(
-    //   FeatureFlagKey.IS_POSTGRESQL_INTEGRATION_ENABLED,
-    // );
-
-    const isPostgresqlIntegrationEnabled = false;
+    const isPostgresqlIntegrationEnabled = useIsFeatureEnabled(
+      FeatureFlagKey.IS_POSTGRESQL_INTEGRATION_ENABLED,
+    );
 
     const isPostgresqlIntegrationActive = !!MOCK_REMOTE_DATABASES.find(
       ({ name }) => name === 'postgresql',
     )?.isActive;
 
-    // const isStripeIntegrationEnabled = useIsFeatureEnabled(
-    //   FeatureFlagKey.IS_STRIPE_INTEGRATION_ENABLED,
-    // );
-
-    const isStripeIntegrationEnabled = false;
+    const isStripeIntegrationEnabled = useIsFeatureEnabled(
+      FeatureFlagKey.IS_STRIPE_INTEGRATION_ENABLED,
+    );
 
     const isStripeIntegrationActive = !!MOCK_REMOTE_DATABASES.find(
       ({ name }) => name === 'stripe',
     )?.isActive;
 
-    const allIntegrations = getSettingsIntegrationAll({
-      isAirtableIntegrationEnabled,
-      isAirtableIntegrationActive,
-      isPostgresqlIntegrationEnabled,
-      isPostgresqlIntegrationActive,
-      isStripeIntegrationEnabled,
-      isStripeIntegrationActive,
-    });
+    const allIntegrations = showAdminIntegrations
+      ? getSettingsIntegrationAll({
+          isAirtableIntegrationEnabled,
+          isAirtableIntegrationActive,
+          isPostgresqlIntegrationEnabled,
+          isPostgresqlIntegrationActive,
+          isStripeIntegrationEnabled,
+          isStripeIntegrationActive,
+        })
+      : getSettingsIntegrationAll({
+          isAirtableIntegrationEnabled,
+          isAirtableIntegrationActive,
+          isPostgresqlIntegrationEnabled: false,
+          isPostgresqlIntegrationActive,
+          isStripeIntegrationEnabled: false,
+          isStripeIntegrationActive,
+        });
 
-    return [
-      ...(allIntegrations.integrations.length > 0 ? [allIntegrations] : []),
-      SETTINGS_INTEGRATION_ZAPIER_CATEGORY,
-      // SETTINGS_INTEGRATION_WINDMILL_CATEGORY,
-      // SETTINGS_INTEGRATION_REQUEST_CATEGORY,
-    ];
+    return showAdminIntegrations
+      ? [
+          ...(allIntegrations.integrations.length > 0 ? [allIntegrations] : []),
+          SETTINGS_INTEGRATION_ZAPIER_CATEGORY,
+          SETTINGS_INTEGRATION_WINDMILL_CATEGORY,
+          SETTINGS_INTEGRATION_REQUEST_CATEGORY,
+        ]
+      : [
+          ...(allIntegrations.integrations.length > 0 ? [allIntegrations] : []),
+          SETTINGS_INTEGRATION_ZAPIER_CATEGORY,
+        ];
   };
