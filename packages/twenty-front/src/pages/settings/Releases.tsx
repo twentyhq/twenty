204e4390d0b73a07625ff5d5d43cdcce603414ae
Félix Malfait <felix@twenty.com>
2025-06-05 11:54:06 +0200

    Begin refactoring AI module

--- packages/twenty-front/src/pages/settings/Releases.tsx
+++ packages/twenty-front/src/pages/settings/Releases.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
-import { unified } from 'unified';
+import { PluggableList, unified } from 'unified';
 import { visit } from 'unist-util-visit';
 
 import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
@@ -89,9 +89,7 @@ export const Releases = () => {
       for (const release of json) {
         release.html = String(
           await unified()
-            .use(remarkParse)
-            .use(remarkRehype)
-            .use(rehypeStringify)
+            .use([remarkParse, remarkRehype, rehypeStringify] as PluggableList)
             .use(() => (tree: any) => {
               visit(tree, (node) => {
                 if (node.tagName === 'h1' || node.tagName === 'h2') {
