c05f581879f6039ba3fc50169a539ea94530838a
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-06-06 18:41:03 -0300

    ✨ feat: Integrate ChargeType into BillingPortalCheckoutSessionParameters and update billing logic for subscription line items based on charge type. Imrpoved ChargeType enum description.

--- packages/twenty-server/src/engine/core-modules/billing/enums/billint-charge-type.enum.ts
+++ packages/twenty-server/src/engine/core-modules/billing/enums/billint-charge-type.enum.ts
@@ -1,3 +1,9 @@
+/**
+ * @description
+ * - ONE_TIME: One time charge, used for one time payments in every subscription
+ * - PER_SEAT: Per seat charge, used for subscriptions that ad up for every new workspace member
+ * - PRE_PAID: Pre paid charge, used for pre paid subscriptions
+ */
 export enum ChargeType {
   ONE_TIME = 'one_time',
   PER_SEAT = 'per_seat',
