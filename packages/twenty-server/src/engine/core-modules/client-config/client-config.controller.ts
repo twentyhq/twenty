dc21f03d67450d35dac045b8081fbb15d2c0f4fa
Félix Malfait <felix.malfait@gmail.com>
2025-06-09 14:14:32 +0200

    Fix workspace hydratation (#12452)
    We must separate the concept of hydratation which happens at the request
    level (take the token and pass auth/user context), from the concept of
    authorization which happens at the query/endpoint/mutation level.
    
    Previously, hydratation exemption happened at the operation name level
    which is not correct because the operation name is meaningless and
    optional. Still this gave an impression of security by enforcing a
    blacklist. So in this PR we introduce linting rule that aim to achieve a
    similar behavior, now every api method has to have a guard. That way if
    and endpoint is not protected by AuthUserGuard or AuthWorspaceGuard,
    then it has to be stated explicitly next to its code.
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- packages/twenty-server/src/engine/core-modules/client-config/client-config.controller.ts
+++ packages/twenty-server/src/engine/core-modules/client-config/client-config.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
 import { ClientConfig } from 'src/engine/core-modules/client-config/client-config.entity';
 import { ClientConfigService } from 'src/engine/core-modules/client-config/services/client-config.service';
+import { PublicEndpointGuard } from 'src/engine/guards/public-endpoint.guard';
 
 @Controller('/client-config')
 export class ClientConfigController {
   constructor(private readonly clientConfigService: ClientConfigService) {}
 
   @Get()
+  @UseGuards(PublicEndpointGuard)
   async getClientConfig(): Promise<ClientConfig> {
     return this.clientConfigService.getClientConfig();
   }
