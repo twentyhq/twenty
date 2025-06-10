1114b55a8a93f49712a74f4e8df91bd7f598179f
Abdul Rahman <ar5438376@gmail.com>
2025-06-10 18:53:20 +0530

    fix: refine body type definition in WorkflowHttpRequestActionInput

--- packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
+++ packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
@@ -2,6 +2,5 @@ export type WorkflowHttpRequestActionInput = {
   url: string;
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
   headers?: Record<string, string>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  body?: Record<string, any> | string;
+  body?: Record<string, string | number | boolean | null> | string;
 };
