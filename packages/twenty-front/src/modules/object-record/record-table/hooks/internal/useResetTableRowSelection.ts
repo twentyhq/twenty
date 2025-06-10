a90bbc00674a0620afa8bbd0f54fc44ee937a001
bosiraphael <raphael.bosi@gmail.com>
2025-06-10 17:31:04 +0200

    fix

--- packages/twenty-front/src/modules/object-record/record-table/hooks/internal/useResetTableRowSelection.ts
+++ packages/twenty-front/src/modules/object-record/record-table/hooks/internal/useResetTableRowSelection.ts
@@ -44,7 +44,7 @@ export const useResetTableRowSelection = (recordTableId?: string) => {
   const lastSelectedRowIndexComponentCallbackState =
     useRecoilComponentCallbackStateV2(
       lastSelectedRowIndexComponentState,
-      recordTableId,
+      recordTableIdFromContext,
     );
 
   return useRecoilCallback(
