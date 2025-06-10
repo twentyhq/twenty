2c046cc5d26aeb51c002571512a43fdac56997bd
Abdul Rahman <ar5438376@gmail.com>
2025-06-10 19:43:42 +0530

    fix: update body type definition in WorkflowHttpRequestActionInput to remove redundant string type

--- packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
+++ packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
@@ -2,5 +2,5 @@ export type WorkflowHttpRequestActionInput = {
   url: string;
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
   headers?: Record<string, string>;
-  body?: Record<string, string | number | boolean | null> | string;
+  body?: Record<string, string | number | boolean | null>;
 };
