bc982cfab848d0fb184e57f26f588c4d2e8645b8
Abdul Rahman <ar5438376@gmail.com>
2025-06-10 05:35:39 +0530

    Update WorkflowHttpRequestActionInput type to allow any type for body property

--- packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
+++ packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
@@ -2,5 +2,6 @@ export type WorkflowHttpRequestActionInput = {
   url: string;
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
   headers?: Record<string, string>;
-  body?: Record<string, unknown> | string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  body?: Record<string, any> | string;
 };
