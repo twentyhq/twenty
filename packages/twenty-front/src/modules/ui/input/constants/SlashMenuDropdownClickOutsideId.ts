a07a23b11492b387f1b00f230a5da34bc34536f0
omarNaifer12 <o.naifer7@gmail.com>
2025-06-11 20:08:15 +0000

    fix:lint

--- packages/twenty-front/src/modules/ui/input/constants/SlashMenuDropdownClickOutsideId.ts
+++ packages/twenty-front/src/modules/ui/input/constants/SlashMenuDropdownClickOutsideId.ts
@@ -1,1 +1,2 @@
-export const SLASH_MENU_DROPDOWN_CLICK_OUTSIDE_ID = 'slash-menu-dropdown-click-outside-id';
+export const SLASH_MENU_DROPDOWN_CLICK_OUTSIDE_ID =
+  'slash-menu-dropdown-click-outside-id';
