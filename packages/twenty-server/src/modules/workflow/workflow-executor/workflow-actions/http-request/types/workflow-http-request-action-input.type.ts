c2a574a8cd1a078579016c622468150dce9ab338
Abdul Rahman <ar5438376@gmail.com>
2025-06-11 10:04:09 +0530

    Enhance WorkflowHttpRequestActionInput type to allow undefined and array values in body property for improved flexibility

--- packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
+++ packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/http-request/types/workflow-http-request-action-input.type.ts
@@ -2,5 +2,13 @@ export type WorkflowHttpRequestActionInput = {
   url: string;
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
   headers?: Record<string, string>;
-  body?: Record<string, string | number | boolean | null>;
+  body?: Record<
+    string,
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | Array<string | number | boolean | null>
+  >;
 };
