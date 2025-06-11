6c4502fc89881c93e02458e1338bb626b272b3bd
Abdul Rahman <ar5438376@gmail.com>
2025-06-11 20:44:11 +0530

    Standardize action labels in getActionHeaderTypeOrThrow function for consistency

--- packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/utils/getActionHeaderTypeOrThrow.ts
+++ packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/utils/getActionHeaderTypeOrThrow.ts
@@ -11,9 +11,8 @@ export const getActionHeaderTypeOrThrow = (actionType: WorkflowActionType) => {
     case 'DELETE_RECORD':
     case 'FIND_RECORDS':
     case 'FORM':
-      return msg`Action`;
     case 'SEND_EMAIL':
-      return msg`Email`;
+      return msg`Action`;
     case 'HTTP_REQUEST':
       return msg`HTTP Request`;
     default:
