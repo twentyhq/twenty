6237da5e132ea2b721e7969781a0d0fd31d2e597
Abdul Rahman <ar5438376@gmail.com>
2025-06-11 10:16:01 +0530

    Refactor isMethodWithBody utility to improve type safety by using HttpMethodWithBody type

--- packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/http-request-action/utils/isMethodWithBody.ts
+++ packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/http-request-action/utils/isMethodWithBody.ts
@@ -1,11 +1,14 @@
-import { METHODS_WITH_BODY } from '@/workflow/workflow-steps/workflow-actions/http-request-action/constants/HttpRequest';
+import {
+  HttpMethodWithBody,
+  METHODS_WITH_BODY,
+} from '@/workflow/workflow-steps/workflow-actions/http-request-action/constants/HttpRequest';
 
-export const isMethodWithBody = (method: string | null): boolean => {
+export const isMethodWithBody = (
+  method: string | null,
+): method is HttpMethodWithBody => {
   if (!method) {
     return false;
   }
 
-  return METHODS_WITH_BODY.includes(
-    method as (typeof METHODS_WITH_BODY)[number],
-  );
+  return METHODS_WITH_BODY.includes(method as HttpMethodWithBody);
 };
