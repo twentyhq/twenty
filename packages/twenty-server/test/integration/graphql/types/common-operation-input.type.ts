b5beb342e47828e951dee0e675b1b7f4bd23d358
prastoin <paul@twenty.com>
2025-06-12 16:20:16 +0200

    chore(server): remove out of pr scope additions

--- packages/twenty-server/test/integration/graphql/types/common-operation-input.type.ts
+++
@@ -1,6 +0,0 @@
-import { MakeMetadataAPIRequestOptions } from "test/integration/graphql/utils/make-graphql-request.util";
-
-export type CommonOperationInput<T> = {
-  input: T;
-  options?: MakeMetadataAPIRequestOptions
-}
