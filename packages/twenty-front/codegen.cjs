e2e8cd88e998204bebf0ad78695b977888c77140
neo773 <neo773@protonmail.com>
2025-06-13 13:08:03 +0530

    ignore hooks folder instead of file in codegen

--- packages/twenty-front/codegen.cjs
+++ packages/twenty-front/codegen.cjs
@@ -9,7 +9,7 @@ module.exports = {
     '!./src/modules/object-metadata/**',
     '!./src/modules/object-record/**',
     '!./src/modules/settings/serverless-functions/**',
-    '!./src/modules/settings/accounts/hooks/useConnectedAccount.ts',
+    '!./src/modules/settings/accounts/hooks/**',
     './src/modules/**/*.tsx',
     './src/modules/**/*.ts',
     '!./src/**/*.test.tsx',
