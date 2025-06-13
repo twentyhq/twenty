12690274952197abcbef38e6bc1640232b355faf
Abdul Rahman <ar5438376@gmail.com>
2025-06-13 11:36:29 +0530

    Enhance workflow version step service to include HTTP request actions in enrichment check, clarifying OutputSchema handling for both code and HTTP request types.

--- packages/twenty-server/src/modules/workflow/workflow-builder/workflow-step/workflow-version-step.workspace-service.ts
+++ packages/twenty-server/src/modules/workflow/workflow-builder/workflow-step/workflow-version-step.workspace-service.ts
@@ -348,8 +348,14 @@ export class WorkflowVersionStepWorkspaceService {
     step: WorkflowAction;
     workspaceId: string;
   }): Promise<WorkflowAction> {
-    // We don't enrich on the fly for code workflow action. OutputSchema is computed and updated when testing the serverless function
-    if (step.type === WorkflowActionType.CODE) {
+    // We don't enrich on the fly for code and HTTP request workflow actions.
+    // For code actions, OutputSchema is computed and updated when testing the serverless function.
+    // For HTTP requests, OutputSchema is determined by the expamle response input
+    if (
+      [WorkflowActionType.CODE, WorkflowActionType.HTTP_REQUEST].includes(
+        step.type,
+      )
+    ) {
       return step;
     }
 
