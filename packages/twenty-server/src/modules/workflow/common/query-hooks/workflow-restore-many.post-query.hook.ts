842f63e1df19576d19103d08b3c603225fc933c7
martmull <martmull@hotmail.fr>
2025-06-13 14:33:56 +0200

    Fix flaky test (#12579)
    as title, add missing awaits
    # Conflicts:
    #	packages/twenty-server/src/modules/workflow/common/query-hooks/workflow-delete-many.post-query.hook.ts
    #	packages/twenty-server/src/modules/workflow/common/query-hooks/workflow-delete-one.post-query.hook.ts
    #	packages/twenty-server/src/modules/workflow/common/query-hooks/workflow-restore-many.post-query.hook.ts
    #	packages/twenty-server/src/modules/workflow/common/query-hooks/workflow-restore-one.post-query.hook.ts

--- packages/twenty-server/src/modules/workflow/common/query-hooks/workflow-restore-many.post-query.hook.ts
+++ packages/twenty-server/src/modules/workflow/common/query-hooks/workflow-restore-many.post-query.hook.ts
@@ -5,7 +5,6 @@ import { WorkspaceQueryHookType } from 'src/engine/api/graphql/workspace-query-r
 import { AuthContext } from 'src/engine/core-modules/auth/types/auth-context.type';
 import { WorkflowWorkspaceEntity } from 'src/modules/workflow/common/standard-objects/workflow.workspace-entity';
 import { WorkflowCommonWorkspaceService } from 'src/modules/workflow/common/workspace-services/workflow-common.workspace-service';
-import { workspaceValidator } from 'src/engine/core-modules/workspace/workspace.validate';
 
 @WorkspaceQueryHook({
   key: 'workflow.restoreMany',
@@ -23,13 +22,9 @@ export class WorkflowRestoreManyPostQueryHook
     _objectName: string,
     payload: WorkflowWorkspaceEntity[],
   ): Promise<void> {
-    const workspace = authContext.workspace;
-
-    workspaceValidator.assertIsDefinedOrThrow(workspace);
-
-    this.workflowCommonWorkspaceService.handleWorkflowSubEntities({
+    await this.workflowCommonWorkspaceService.handleWorkflowSubEntities({
       workflowIds: payload.map((workflow) => workflow.id),
-      workspaceId: workspace.id,
+      workspaceId: authContext.workspace.id,
       operation: 'restore',
     });
   }
