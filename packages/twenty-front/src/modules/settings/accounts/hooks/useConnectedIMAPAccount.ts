f29738bb6bba57dfef2ed42ee54d1a88be652ab6
neo773 <neo773@protonmail.com>
2025-06-16 07:10:45 +0530

    fix lint

--- packages/twenty-front/src/modules/settings/accounts/hooks/useConnectedIMAPAccount.ts
+++ packages/twenty-front/src/modules/settings/accounts/hooks/useConnectedIMAPAccount.ts
@@ -1,7 +1,9 @@
 import { ConnectedAccountProvider } from 'twenty-shared/types';
 import { useGetConnectedImapAccountQuery } from '~/generated/graphql';
 
-export const useConnectedIMAPAccount = (connectedAccountId: string | undefined) => {
+export const useConnectedIMAPAccount = (
+  connectedAccountId: string | undefined,
+) => {
   const { data, loading, error } = useGetConnectedImapAccountQuery({
     variables: { id: connectedAccountId ?? '' },
     skip: !connectedAccountId,
