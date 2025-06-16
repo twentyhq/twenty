3c1f7f64b15c3b97c0e29285b2d2bb0cd2e75a3b
prastoin <paul@twenty.com>
2025-06-16 11:13:07 +0200

    refactor(front): short select icon

--- packages/twenty-front/src/modules/settings/data-model/fields/forms/number/constants/NumberDataModelSelectOptions.ts
+++ packages/twenty-front/src/modules/settings/data-model/fields/forms/number/constants/NumberDataModelSelectOptions.ts
@@ -1,6 +1,15 @@
+import { FieldNumberVariant } from '@/object-record/record-field/types/FieldMetadata';
+import { MessageDescriptor } from '@lingui/core';
 import { msg } from '@lingui/core/macro';
+import { Icon, IconLetterK, IconProps } from '@tabler/icons-react';
+import { ForwardRefExoticComponent, RefAttributes } from 'react';
 import { IconNumber9, IconPercentage } from 'twenty-ui/display';
 
+type NumberDataModelSelectOptions = {
+  Icon: ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>;
+  label: MessageDescriptor;
+  value: FieldNumberVariant;
+}
 export const NUMBER_DATA_MODEL_SELECT_OPTIONS = [
   {
     Icon: IconNumber9,
@@ -8,7 +17,7 @@ export const NUMBER_DATA_MODEL_SELECT_OPTIONS = [
     value: 'number',
   },
   {
-    Icon: IconNumber9,
+    Icon: IconLetterK,
     label: msg`Short`,
     value: 'shortNumber',
   },
@@ -17,4 +26,4 @@ export const NUMBER_DATA_MODEL_SELECT_OPTIONS = [
     label: msg`Percentage`,
     value: 'percentage',
   },
-];
+] as const satisfies Array<NumberDataModelSelectOptions>;
