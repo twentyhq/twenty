5c8db64d6ed3ad565a7b773e7a31a25b6d3b85e2
neo773 <neo773@protonmail.com>
2025-06-16 06:14:15 +0530

    refactor(imap-get-messages): replace sanitizeString with removeNullCharacters for improved clarity

--- packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/services/imap-get-messages.service.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/services/imap-get-messages.service.ts
@@ -10,7 +10,7 @@ import { computeMessageDirection } from 'src/modules/messaging/message-import-ma
 import { ImapClientProvider } from 'src/modules/messaging/message-import-manager/drivers/imap/providers/imap-client.provider';
 import { ImapHandleErrorService } from 'src/modules/messaging/message-import-manager/drivers/imap/services/imap-handle-error.service';
 import { findSentMailbox } from 'src/modules/messaging/message-import-manager/drivers/imap/utils/find-sent-mailbox.util';
-import { sanitizeString } from 'src/modules/messaging/message-import-manager/drivers/imap/utils/sanitize-string.util';
+import { removeNullCharacters } from 'src/modules/messaging/message-import-manager/drivers/imap/utils/sanitize-string.util';
 import { EmailAddress } from 'src/modules/messaging/message-import-manager/types/email-address';
 import { MessageWithParticipants } from 'src/modules/messaging/message-import-manager/types/message';
 import { formatAddressObjectAsParticipants } from 'src/modules/messaging/message-import-manager/utils/format-address-object-as-participants.util';
@@ -174,7 +174,7 @@ export class ImapGetMessagesService {
       : '';
 
     const direction = computeMessageDirection(fromHandle, connectedAccount);
-    const text = sanitizeString(textWithoutReplyQuotations);
+    const text = removeNullCharacters(textWithoutReplyQuotations);
 
     return {
       externalId: messageId,
--- packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/utils/sanitize-string.util.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/utils/sanitize-string.util.ts
@@ -1,3 +1,6 @@
-export const sanitizeString = (str: string) => {
+/**
+ * Removes null characters (\0) from a string to prevent unexpected errors
+ */
+export const removeNullCharacters = (str: string) => {
   return str.replace(/\0/g, '');
 };
