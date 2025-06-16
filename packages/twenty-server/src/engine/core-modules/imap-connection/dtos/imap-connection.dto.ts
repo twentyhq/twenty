0f1cf8e77be6704c9455e8006f0def9eca41b2c5
neo773 <neo773@protonmail.com>
2025-06-16 17:56:56 +0530

    add comment for password

--- packages/twenty-server/src/engine/core-modules/imap-connection/dtos/imap-connection.dto.ts
+++ packages/twenty-server/src/engine/core-modules/imap-connection/dtos/imap-connection.dto.ts
@@ -11,6 +11,11 @@ export class ImapConnectionParams {
   @Field(() => Number)
   port: number;
 
+  /**
+   * Note: This field is stored in plain text in the database.
+   * While encrypting it could provide an extra layer of defense, we have decided not to,
+   * as database access implies a broader compromise. For context, see discussion in PR #12576.
+   */
   @Field(() => String)
   password: string;
 
