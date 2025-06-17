1f2bec4ac0aabd8bba1122d455d7d923e1765322
Marie Stoppa <marie.stoppa@essec.edu>
2025-06-17 16:58:47 +0200

    Adapt defaultPath to permissions

--- packages/twenty-front/src/modules/navigation/hooks/useDefaultHomePagePath.ts
+++ packages/twenty-front/src/modules/navigation/hooks/useDefaultHomePagePath.ts
@@ -2,6 +2,7 @@ import { currentUserState } from '@/auth/states/currentUserState';
 import { lastVisitedObjectMetadataItemIdState } from '@/navigation/states/lastVisitedObjectMetadataItemIdState';
 import { ObjectPathInfo } from '@/navigation/types/ObjectPathInfo';
 import { useFilteredObjectMetadataItems } from '@/object-metadata/hooks/useFilteredObjectMetadataItems';
+import { useObjectPermissions } from '@/object-record/hooks/useObjectPermissions';
 import { prefetchViewsState } from '@/prefetch/states/prefetchViewsState';
 import { AppPath } from '@/types/AppPath';
 import { useCallback, useMemo } from 'react';
@@ -11,10 +12,23 @@ import { getAppPath } from '~/utils/navigation/getAppPath';
 
 export const useDefaultHomePagePath = () => {
   const currentUser = useRecoilValue(currentUserState);
+  const { objectPermissionsByObjectMetadataId } = useObjectPermissions();
+
   const {
     activeNonSystemObjectMetadataItems,
     alphaSortedActiveNonSystemObjectMetadataItems,
   } = useFilteredObjectMetadataItems();
+
+  const readableAlphaSortedActiveNonSystemObjectMetadataItems = useMemo(() => {
+    return alphaSortedActiveNonSystemObjectMetadataItems.filter((item) => {
+      const objectPermissions = objectPermissionsByObjectMetadataId[item.id];
+      return objectPermissions?.canReadObjectRecords;
+    });
+  }, [
+    alphaSortedActiveNonSystemObjectMetadataItems,
+    objectPermissionsByObjectMetadataId,
+  ]);
+
   const prefetchViews = useRecoilValue(prefetchViewsState);
   const lastVisitedObjectMetadataItemId = useRecoilValue(
     lastVisitedObjectMetadataItemIdState,
@@ -39,7 +53,7 @@ export const useDefaultHomePagePath = () => {
 
   const firstObjectPathInfo = useMemo<ObjectPathInfo | null>(() => {
     const [firstObjectMetadataItem] =
-      alphaSortedActiveNonSystemObjectMetadataItems;
+      readableAlphaSortedActiveNonSystemObjectMetadataItems;
 
     if (!isDefined(firstObjectMetadataItem)) {
       return null;
@@ -48,10 +62,14 @@ export const useDefaultHomePagePath = () => {
     const view = getFirstView(firstObjectMetadataItem?.id);
 
     return { objectMetadataItem: firstObjectMetadataItem, view };
-  }, [alphaSortedActiveNonSystemObjectMetadataItems, getFirstView]);
+  }, [readableAlphaSortedActiveNonSystemObjectMetadataItems, getFirstView]);
 
   const defaultObjectPathInfo = useMemo<ObjectPathInfo | null>(() => {
-    if (!isDefined(lastVisitedObjectMetadataItemId)) {
+    if (
+      !isDefined(lastVisitedObjectMetadataItemId) ||
+      !objectPermissionsByObjectMetadataId[lastVisitedObjectMetadataItemId]
+        ?.canReadObjectRecords
+    ) {
       return firstObjectPathInfo;
     }
 
@@ -72,6 +90,7 @@ export const useDefaultHomePagePath = () => {
     getActiveObjectMetadataItemMatchingId,
     getFirstView,
     lastVisitedObjectMetadataItemId,
+    objectPermissionsByObjectMetadataId,
   ]);
 
   const defaultHomePagePath = useMemo(() => {
