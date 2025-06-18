ac2b36bc606fbd092f3d6a990e1f0ca26b65b3dc
prastoin <paul@twenty.com>
2025-06-18 15:42:01 +0200

    test(server): start testing

--- packages/twenty-server/test/integration/metadata/suites/field-metadata/phone/create-one-field-metadata-phone.integration-spec.ts
+++ packages/twenty-server/test/integration/metadata/suites/field-metadata/phone/create-one-field-metadata-phone.integration-spec.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker/.';
+import { EachTestingContext } from 'twenty-shared/testing';
+
+import { PhonesMetadata } from 'src/engine/metadata-modules/field-metadata/composite-types/phones.composite-type';
 import { createOneOperation } from 'test/integration/graphql/utils/create-one-operation.util';
-import { updateOneOperation } from 'test/integration/graphql/utils/update-one-operation.util';
 import { createOneFieldMetadata } from 'test/integration/metadata/suites/field-metadata/utils/create-one-field-metadata.util';
 import { createOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/create-one-object-metadata.util';
 import { deleteOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/delete-one-object-metadata.util';
@@ -8,41 +10,87 @@ import { FieldMetadataType } from 'twenty-shared/types';
 
 const FIELD_NAME = 'phonenumber';
 
+type CreatePhoneFieldMetadataTestCase = {
+  input: Partial<PhonesMetadata>;
+  expected: Partial<PhonesMetadata>
+};
+
+const SUCCESSFUL_TEST_CASES: EachTestingContext<CreatePhoneFieldMetadataTestCase>[] = [
+  {
+    title: 'create phone field with number only',
+    context: {
+      input: {
+        primaryPhoneNumber: '781037818',
+        additionalPhones: [],
+      },
+      expected: {
+        primaryPhoneNumber: '781037818',
+        additionalPhones: [],
+      },
+    },
+  },
+  {
+    title: 'create phone field with full international format',
+    context: {
+      input: {
+        primaryPhoneNumber: '+1781037818',
+        primaryPhoneCountryCode: 'US',
+        primaryPhoneCallingCode: '+1',
+        additionalPhones: null,
+      },
+      expected: {
+        primaryPhoneNumber: '781037818',
+        primaryPhoneCountryCode: 'US',
+        primaryPhoneCallingCode: '+1',
+        additionalPhones: null,
+      },
+    },
+  },
+    {
+    title: 'create phone field with empty payload',
+    context: {
+      input: {},
+      expected: {
+        primaryPhoneNumber: '',
+        primaryPhoneCountryCode: '' as any,
+        primaryPhoneCallingCode: '',
+        additionalPhones: null,
+      },
+    },
+  },
+];
+
 describe('Phone field metadata tests suite', () => {
   let createdObjectMetadataId: string;
 
   beforeEach(async () => {
-    const {
-      data: {
-        createOneObject: { id: objectMetadataId },
-      },
-    } = await createOneObjectMetadata({
+    const { data } = await createOneObjectMetadata({
       input: {
         nameSingular: 'myTestObject',
         namePlural: 'myTestObjects',
         labelSingular: 'My Test Object',
         labelPlural: 'My Test Objects',
         icon: 'Icon123',
+        isLabelSyncedWithName: false,
       },
     });
 
-    createdObjectMetadataId = objectMetadataId;
+    createdObjectMetadataId = data.createOneObject.id;
 
-    const createFieldInput = {
-      name: FIELD_NAME,
-      label: 'Phone number',
-      type: FieldMetadataType.PHONES,
-      objectMetadataId: createdObjectMetadataId,
-      isLabelSyncedWithName: false,
-    };
     await createOneFieldMetadata({
-      input: createFieldInput,
+      input: {
+        name: FIELD_NAME,
+        label: 'Phone number',
+        type: FieldMetadataType.PHONES,
+        objectMetadataId: createdObjectMetadataId,
+        isLabelSyncedWithName: false,
+      },
       gqlFields: `
-          id
-          name
-          label
-          isLabelSyncedWithName
-        `,
+        id
+        name
+        label
+        isLabelSyncedWithName
+      `,
     });
   });
 
@@ -51,123 +99,38 @@ describe('Phone field metadata tests suite', () => {
       input: { idToDelete: createdObjectMetadataId },
     });
   });
-  it('Stephanie reproduction', async () => {
-    const {
-      data: { createOneResponse },
-    } = await createOneOperation<{
-      id: string;
-      [FIELD_NAME]: any;
-    }>({
-      objectMetadataSingularName: 'myTestObject',
-      input: {
-        id: faker.string.uuid(),
-        [FIELD_NAME]: {
-          primaryPhoneNumber: '781037818',
-          additionalPhones: [],
+
+  test.each(SUCCESSFUL_TEST_CASES)(
+    '$title',
+    async ({ context: { input, expected } }) => {
+      const {
+        data: { createOneResponse },
+      } = await createOneOperation<{
+        id: string;
+        [FIELD_NAME]: any;
+      }>({
+        objectMetadataSingularName: 'myTestObject',
+        input: {
+          id: faker.string.uuid(),
+          [FIELD_NAME]: input,
         },
-      },
-      gqlFields: `
-        id
-        ${FIELD_NAME} {
-          primaryPhoneNumber
-          primaryPhoneCountryCode
-          primaryPhoneCallingCode
-          additionalPhones
-          __typename
-        }
-      `,
-    });
+        gqlFields: `
+          id
+          ${FIELD_NAME} {
+            primaryPhoneNumber
+            primaryPhoneCountryCode
+            primaryPhoneCallingCode
+            additionalPhones
+            __typename
+          }
+        `,
+      });
 
-    const { id, ...rest } = createOneResponse;
-    expect(rest).toMatchInlineSnapshot(`
-{
-  "phonenumber": {
-    "__typename": "Phones",
-    "additionalPhones": [],
-    "primaryPhoneCallingCode": "",
-    "primaryPhoneCountryCode": "",
-    "primaryPhoneNumber": "781037818",
-  },
-}
-`);
-    const createdRecordId = createOneResponse.id;
-
-    const {
-      data: { updateOneResponse },
-    } = await updateOneOperation({
-      input: {
-        [FIELD_NAME]: {
-          primaryPhoneNumber: '+1781037818',
-          primaryPhoneCountryCode: 'US',
-          primaryPhoneCallingCode: '+1',
-          additionalPhones: [],
-        },
-      },
-      objectMetadataSingularName: 'myTestObject',
-      recordId: createdRecordId,
-      gqlFields: `
-        id
-        ${FIELD_NAME} {
-          primaryPhoneNumber
-          primaryPhoneCountryCode
-          primaryPhoneCallingCode
-          additionalPhones
-          __typename
-        }
-      `,
-    });
-
-    {
-      const { id, ...rest } = updateOneResponse;
-      expect(rest).toMatchInlineSnapshot(`
-{
-  "phonenumber": {
-    "__typename": "Phones",
-    "additionalPhones": [],
-    "primaryPhoneCallingCode": "+1",
-    "primaryPhoneCountryCode": "US",
-    "primaryPhoneNumber": "781037818",
-  },
-}
-`);
-    }
-  });
-
-  it.skip('Empty phone', async () => {
-    const {
-      data: { createOneResponse },
-    } = await createOneOperation<{
-      id: string;
-      [FIELD_NAME]: any;
-    }>({
-      objectMetadataSingularName: 'myTestObject',
-      input: {
-        id: faker.string.uuid(),
-        [FIELD_NAME]: {},
-      },
-      gqlFields: `
-        id
-        ${FIELD_NAME} {
-          primaryPhoneNumber
-          primaryPhoneCountryCode
-          primaryPhoneCallingCode
-          additionalPhones
-          __typename
-        }
-      `,
-    });
-
-    const { id, ...rest } = createOneResponse;
-    expect(rest).toMatchInlineSnapshot(`
-{
-  "phonenumber": {
-    "__typename": "Phones",
-    "additionalPhones": null,
-    "primaryPhoneCallingCode": "",
-    "primaryPhoneCountryCode": "",
-    "primaryPhoneNumber": "",
-  },
-}
-`);
-  });
+      const { id, ...rest } = createOneResponse;
+      expect(rest[FIELD_NAME]).toMatchObject({
+        ...expected,
+        __typename: 'Phones',
+      });
+    },
+  );
 });
