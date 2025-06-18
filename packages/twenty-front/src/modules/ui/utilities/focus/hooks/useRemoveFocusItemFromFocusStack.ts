d9926405dcef2e64e116c9b930ea14be7d58019e
bosiraphael <raphael.bosi@gmail.com>
2025-06-18 17:15:09 +0200

    add debug tools to focus stack

--- packages/twenty-front/src/modules/ui/utilities/focus/hooks/useRemoveFocusItemFromFocusStack.ts
+++ packages/twenty-front/src/modules/ui/utilities/focus/hooks/useRemoveFocusItemFromFocusStack.ts
@@ -1,18 +1,28 @@
+import { DEBUG_FOCUS_STACK } from '@/ui/utilities/focus/constants/DebugFocusStack';
 import { focusStackState } from '@/ui/utilities/focus/states/focusStackState';
 import { usePreviousHotkeyScope } from '@/ui/utilities/hotkey/hooks/usePreviousHotkeyScope';
 import { useRecoilCallback } from 'recoil';
+import { logDebug } from '~/utils/logDebug';
 
 export const useRemoveFocusItemFromFocusStack = () => {
   const { goBackToPreviousHotkeyScope } = usePreviousHotkeyScope();
 
   const removeFocusItemFromFocusStack = useRecoilCallback(
-    ({ set }) =>
+    ({ snapshot, set }) =>
       ({ focusId, memoizeKey }: { focusId: string; memoizeKey: string }) => {
-        set(focusStackState, (previousFocusStack) =>
-          previousFocusStack.filter(
-            (focusStackItem) => focusStackItem.focusId !== focusId,
-          ),
+        const focusStack = snapshot.getLoadable(focusStackState).getValue();
+
+        const newFocusStack = focusStack.filter(
+          (focusStackItem) => focusStackItem.focusId !== focusId,
         );
+
+        set(focusStackState, newFocusStack);
+
+        if (DEBUG_FOCUS_STACK) {
+          logDebug(`DEBUG: removeFocusItemFromFocusStack ${focusId}`, {
+            newFocusStack,
+          });
+        }
 
         // TODO: Remove this once we've migrated hotkey scopes to the new api
         goBackToPreviousHotkeyScope(memoizeKey);
--- packages/twenty-front/src/modules/ui/utilities/focus/hooks/useResetFocusStackToFocusItem.ts
+++ packages/twenty-front/src/modules/ui/utilities/focus/hooks/useResetFocusStackToFocusItem.ts
@@ -1,9 +1,11 @@
+import { DEBUG_FOCUS_STACK } from '@/ui/utilities/focus/constants/DebugFocusStack';
 import { focusStackState } from '@/ui/utilities/focus/states/focusStackState';
 import { FocusStackItem } from '@/ui/utilities/focus/types/FocusStackItem';
 import { currentHotkeyScopeState } from '@/ui/utilities/hotkey/states/internal/currentHotkeyScopeState';
 import { previousHotkeyScopeFamilyState } from '@/ui/utilities/hotkey/states/internal/previousHotkeyScopeFamilyState';
 import { HotkeyScope } from '@/ui/utilities/hotkey/types/HotkeyScope';
 import { useRecoilCallback } from 'recoil';
+import { logDebug } from '~/utils/logDebug';
 
 export const useResetFocusStackToFocusItem = () => {
   const resetFocusStackToFocusItem = useRecoilCallback(
@@ -18,6 +20,12 @@ export const useResetFocusStackToFocusItem = () => {
         memoizeKey: string;
       }) => {
         set(focusStackState, [focusStackItem]);
+
+        if (DEBUG_FOCUS_STACK) {
+          logDebug(`DEBUG: reset focus stack to focus item`, {
+            focusStackItem,
+          });
+        }
 
         // TODO: Remove this once we've migrated hotkey scopes to the new api
         set(previousHotkeyScopeFamilyState(memoizeKey), null);
