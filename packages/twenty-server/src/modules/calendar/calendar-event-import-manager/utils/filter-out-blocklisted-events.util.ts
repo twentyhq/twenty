6d56b75962f79d31f0790558d68496bebadc8613
Guillim <guillim@users.noreply.github.com>
2025-06-19 17:04:21 +0200

    12690-error-unknown-error-importing-calendar-events-reafcto-required (#12711)
    Why : we had an issue impoting events du to CalendarEvents not yet
    existing while inserting the CalendarChannelAssociation due to inverted
    method in the service
    
    This PR refactors the calendar event import logic by 
    - renaming 
    - splitting utility functions for better clarity and maintainability. 
    - adding TSDoc comments to explain the purpose and uniqueness of the
    `eventExternalId` field in calendar event associations
    
    
    Fixes #12690
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- packages/twenty-server/src/modules/calendar/calendar-event-import-manager/utils/filter-out-blocklisted-events.util.ts
+++ packages/twenty-server/src/modules/calendar/calendar-event-import-manager/utils/filter-out-blocklisted-events.util.ts
@@ -1,9 +1,9 @@
 import { isEmailBlocklisted } from 'src/modules/blocklist/utils/is-email-blocklisted.util';
-import { CalendarEventWithParticipants } from 'src/modules/calendar/common/types/calendar-event';
+import { FetchedCalendarEvent } from 'src/modules/calendar/common/types/fetched-calendar-event';
 
 export const filterOutBlocklistedEvents = (
   calendarChannelHandles: string[],
-  events: CalendarEventWithParticipants[],
+  events: FetchedCalendarEvent[],
   blocklist: string[],
 ) => {
   return events.filter((event) => {
