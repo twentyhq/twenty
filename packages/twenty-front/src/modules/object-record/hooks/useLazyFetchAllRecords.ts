twentyhq/twenty

commit f3a8b849aa0009c74ab46b813e5ef765d0011023

Fix table loading (#12653)
As per title

packages/twenty-front/src/modules/object-record/hooks/useLazyFetchAllRecords.ts
@@ -34,7 +34,7 @@ export const useLazyFetchAllRecords = <T>({
   const [progress, setProgress] = useState<ExportProgress>({
     displayType: 'number',
   });
-  const { fetchMore, findManyRecords } = useLazyFindManyRecords({
+  const { fetchMoreRecordsLazy, findManyRecordsLazy } = useLazyFindManyRecords({
     objectNameSingular,
     filter,
     orderBy,
@@ -47,12 +47,12 @@ export const useLazyFetchAllRecords = <T>({
   });
 
   const fetchAllRecords = useCallback(async () => {
-    if (!isDefined(findManyRecords)) {
+    if (!isDefined(findManyRecordsLazy)) {
       return [];
     }
     setIsDownloading(true);
 
-    const findManyRecordsDataResult = await findManyRecords();
+    const findManyRecordsDataResult = await findManyRecordsLazy();
 
     const firstQueryResult =
       findManyRecordsDataResult?.data?.[objectMetadataItem.namePlural];
@@ -84,7 +84,7 @@ export const useLazyFetchAllRecords = <T>({
         break;
       }
 
-      if (!isDefined(fetchMore)) {
+      if (!isDefined(fetchMoreRecordsLazy)) {
         break;
       }
 
@@ -92,12 +92,7 @@ export const useLazyFetchAllRecords = <T>({
         await sleep(delayMs);
       }
 
-      const rawResult = await fetchMore({
-        variables: {
-          lastCursor: lastCursor,
-          limit,
-        },
-      });
+      const rawResult = await fetchMoreRecordsLazy();
 
       const fetchMoreResult = rawResult?.data?.[objectMetadataItem.namePlural];
 
@@ -126,8 +121,8 @@ export const useLazyFetchAllRecords = <T>({
     return records;
   }, [
     delayMs,
-    fetchMore,
-    findManyRecords,
+    fetchMoreRecordsLazy,
+    findManyRecordsLazy,
     objectMetadataItem.namePlural,
     limit,
     maximumRequests,

packages/twenty-front/src/modules/object-record/hooks/__tests__/useRecordIndexTableQuery.test.tsx
@@ -1,9 +1,8 @@
-import { expect } from '@storybook/test';
-import { renderHook } from '@testing-library/react';
-import { ReactNode, act } from 'react';
+import { renderHook, waitFor } from '@testing-library/react';
+import { ReactNode } from 'react';
 
 import { RecordGroupContext } from '@/object-record/record-group/states/context/RecordGroupContext';
-import { useLazyLoadRecordIndexTable } from '@/object-record/record-index/hooks/useLazyLoadRecordIndexTable';
+import { useRecordIndexTableQuery } from '@/object-record/record-index/hooks/useRecordIndexTableQuery';
 import { RecordTableComponentInstance } from '@/object-record/record-table/components/RecordTableComponentInstance';
 import { ViewComponentInstanceContext } from '@/views/states/contexts/ViewComponentInstanceContext';
 import { MockedResponse } from '@apollo/client/testing';
@@ -679,16 +678,14 @@ const Wrapper = ({ children }: { children: ReactNode }) => {
   );
 };
 
-describe('useLazyLoadRecordIndexTable', () => {
+describe('useRecordIndexTableQuery', () => {
   it('should fetch', async () => {
     const { result } = renderHook(
       () => {
-        const { findManyRecords, ...result } =
-          useLazyLoadRecordIndexTable(objectNameSingular);
+        const { records } = useRecordIndexTableQuery(objectNameSingular);
 
         return {
-          findManyRecords,
-          ...result,
+          records,
         };
       },
       {
@@ -696,12 +693,8 @@ describe('useLazyLoadRecordIndexTable', () => {
       },
     );
 
-    expect(result.current.loading).toBe(false);
-
-    await act(async () => {
-      await result.current.findManyRecords();
+    await waitFor(() => {
+      expect(result.current.records).toHaveLength(16);
     });
-
-    expect(result.current.records).toHaveLength(16);
   });
 });

packages/twenty-front/src/modules/object-record/hooks/useFindManyRecords.ts
@@ -119,7 +119,7 @@ export const useFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
     loading,
     error,
     fetchMoreRecords,
-    queryStateIdentifier: queryIdentifier,
+    queryIdentifier,
     hasNextPage,
     pageInfo,
   };

packages/twenty-front/src/modules/object-record/hooks/useHandleFindManyRecordsCompleted.ts
@@ -1,4 +1,4 @@
-import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 import { getRecordsFromRecordConnection } from '@/object-record/cache/utils/getRecordsFromRecordConnection';
@@ -6,6 +6,7 @@ import { RecordGqlOperationFindManyResult } from '@/object-record/graphql/types/
 import { cursorFamilyState } from '@/object-record/states/cursorFamilyState';
 import { hasNextPageFamilyState } from '@/object-record/states/hasNextPageFamilyState';
 import { OnFindManyRecordsCompleted } from '@/object-record/types/OnFindManyRecordsCompleted';
+import { useCallback } from 'react';
 import { isDefined } from 'twenty-shared/utils';
 
 export const useHandleFindManyRecordsCompleted = <T>({
@@ -17,35 +18,36 @@ export const useHandleFindManyRecordsCompleted = <T>({
   objectMetadataItem: ObjectMetadataItem;
   onCompleted?: OnFindManyRecordsCompleted<T>;
 }) => {
-  const [, setLastCursor] = useRecoilState(cursorFamilyState(queryIdentifier));
+  const setLastCursor = useSetRecoilState(cursorFamilyState(queryIdentifier));
 
-  const [, setHasNextPage] = useRecoilState(
+  const setHasNextPage = useSetRecoilState(
     hasNextPageFamilyState(queryIdentifier),
   );
 
-  const handleFindManyRecordsCompleted = (
-    data: RecordGqlOperationFindManyResult,
-  ) => {
-    if (!isDefined(data)) {
-      onCompleted?.([]);
-    }
+  const handleFindManyRecordsCompleted = useCallback(
+    (data: RecordGqlOperationFindManyResult) => {
+      if (!isDefined(data)) {
+        onCompleted?.([]);
+      }
 
-    const pageInfo = data?.[objectMetadataItem.namePlural]?.pageInfo;
+      const pageInfo = data?.[objectMetadataItem.namePlural]?.pageInfo;
 
-    const records = getRecordsFromRecordConnection({
-      recordConnection: data?.[objectMetadataItem.namePlural],
-    }) as T[];
+      const records = getRecordsFromRecordConnection({
+        recordConnection: data?.[objectMetadataItem.namePlural],
+      }) as T[];
 
-    onCompleted?.(records, {
-      pageInfo,
-      totalCount: data?.[objectMetadataItem.namePlural]?.totalCount,
-    });
+      onCompleted?.(records, {
+        pageInfo,
+        totalCount: data?.[objectMetadataItem.namePlural]?.totalCount,
+      });
 
-    if (isDefined(data?.[objectMetadataItem.namePlural])) {
-      setLastCursor(pageInfo.endCursor ?? '');
-      setHasNextPage(pageInfo.hasNextPage ?? false);
-    }
-  };
+      if (isDefined(data?.[objectMetadataItem.namePlural])) {
+        setLastCursor(pageInfo.endCursor ?? '');
+        setHasNextPage(pageInfo.hasNextPage ?? false);
+      }
+    },
+    [onCompleted, objectMetadataItem.namePlural, setLastCursor, setHasNextPage],
+  );
 
   return {
     handleFindManyRecordsCompleted,

packages/twenty-front/src/modules/object-record/hooks/useHandleFindManyRecordsError.ts
@@ -4,6 +4,7 @@ import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 import { SnackBarVariant } from '@/ui/feedback/snack-bar-manager/components/SnackBar';
 import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
 import { logError } from '~/utils/logError';
+import { useCallback } from 'react';
 
 export const useHandleFindManyRecordsError = ({
   handleError,
@@ -14,16 +15,19 @@ export const useHandleFindManyRecordsError = ({
 }) => {
   const { enqueueSnackBar } = useSnackBar();
 
-  const handleFindManyRecordsError = (error: ApolloError) => {
-    logError(
-      `useFindManyRecords for "${objectMetadataItem.namePlural}" error : ` +
-        error,
-    );
-    enqueueSnackBar(`${error.message}`, {
-      variant: SnackBarVariant.Error,
-    });
-    handleError?.(error);
-  };
+  const handleFindManyRecordsError = useCallback(
+    (error: ApolloError) => {
+      logError(
+        `useFindManyRecords for "${objectMetadataItem.namePlural}" error : ` +
+          error,
+      );
+      enqueueSnackBar(`${error.message}`, {
+        variant: SnackBarVariant.Error,
+      });
+      handleError?.(error);
+    },
+    [enqueueSnackBar, handleError, objectMetadataItem.namePlural],
+  );
 
   return {
     handleFindManyRecordsError,

packages/twenty-front/src/modules/object-record/hooks/useLazyFetchMoreRecordsWithPagination.ts
@@ -7,8 +7,7 @@ import {
 } from '@apollo/client';
 import { isNonEmptyArray } from '@apollo/client/utilities';
 import { isNonEmptyString } from '@sniptt/guards';
-import { useMemo } from 'react';
-import { useRecoilCallback, useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilCallback } from 'recoil';
 
 import { ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 import { ObjectMetadataItemIdentifier } from '@/object-metadata/types/ObjectMetadataItemIdentifier';
@@ -27,7 +26,6 @@ import { getQueryIdentifier } from '@/object-record/utils/getQueryIdentifier';
 import { capitalize, isDefined } from 'twenty-shared/utils';
 import { cursorFamilyState } from '../states/cursorFamilyState';
 import { hasNextPageFamilyState } from '../states/hasNextPageFamilyState';
-import { isFetchingMoreRecordsFamilyState } from '../states/isFetchingMoreRecordsFamilyState';
 
 export type UseFindManyRecordsParams<T> = ObjectMetadataItemIdentifier &
   RecordGqlOperationVariables & {
@@ -42,7 +40,7 @@ type UseFindManyRecordsStateParams<
   TData = RecordGqlOperationFindManyResult,
 > = Omit<
   UseFindManyRecordsParams<T>,
-  'skip' | 'recordGqlFields' | 'fetchPolicy'
+  'skip' | 'recordGqlFields' | 'fetchPolicy' | 'onCompleted'
 > & {
   data: RecordGqlOperationFindManyResult | undefined;
   error: ApolloError | undefined;
@@ -63,18 +61,16 @@ type UseFindManyRecordsStateParams<
   objectMetadataItem: ObjectMetadataItem;
 };
 
-export const useFetchMoreRecordsWithPagination = <
+export const useLazyFetchMoreRecordsWithPagination = <
   T extends ObjectRecord = ObjectRecord,
 >({
   objectNameSingular,
   filter,
   orderBy,
   limit,
-  data,
   error,
   fetchMore,
   objectMetadataItem,
-  onCompleted,
 }: UseFindManyRecordsStateParams<T>) => {
   const queryIdentifier = getQueryIdentifier({
     objectNameSingular,
@@ -83,19 +79,13 @@ export const useFetchMoreRecordsWithPagination = <
     orderBy,
   });
 
-  const [hasNextPage] = useRecoilState(hasNextPageFamilyState(queryIdentifier));
-
-  const setIsFetchingMoreObjects = useSetRecoilState(
-    isFetchingMoreRecordsFamilyState(queryIdentifier),
-  );
-
   const { handleFindManyRecordsError } = useHandleFindManyRecordsError({
     objectMetadataItem,
   });
 
   // TODO: put this into a util inspired from https://github.com/apollographql/apollo-client/blob/master/src/utilities/policies/pagination.ts
   // This function is equivalent to merge function + read function in field policy
-  const fetchMoreRecords = useRecoilCallback(
+  const fetchMoreRecordsLazy = useRecoilCallback(
     ({ snapshot, set }) =>
       async () => {
         const hasNextPageLocal = snapshot
@@ -111,8 +101,6 @@ export const useFetchMoreRecordsWithPagination = <
           hasNextPageLocal ||
           (!isAggregationEnabled(objectMetadataItem) && !error)
         ) {
-          setIsFetchingMoreObjects(true);
-
           try {
             const { data: fetchMoreDataResult } = await fetchMore({
               variables: {
@@ -152,20 +140,6 @@ export const useFetchMoreRecordsWithPagination = <
                   );
                 }
 
-                const records = getRecordsFromRecordConnection({
-                  recordConnection: {
-                    edges: newEdges,
-                    pageInfo,
-                  },
-                }) as T[];
-
-                onCompleted?.(records, {
-                  pageInfo,
-                  totalCount:
-                    fetchMoreResult?.[objectMetadataItem.namePlural]
-                      ?.totalCount,
-                });
-
                 return Object.assign({}, prev, {
                   [objectMetadataItem.namePlural]: {
                     __typename: `${capitalize(
@@ -184,46 +158,37 @@ export const useFetchMoreRecordsWithPagination = <
 
             return {
               data: fetchMoreDataResult?.[objectMetadataItem.namePlural],
+              totalCount:
+                fetchMoreDataResult?.[objectMetadataItem.namePlural]
+                  ?.totalCount,
+              records: getRecordsFromRecordConnection({
+                recordConnection: {
+                  edges:
+                    fetchMoreDataResult?.[objectMetadataItem.namePlural]?.edges,
+                  pageInfo:
+                    fetchMoreDataResult?.[objectMetadataItem.namePlural]
+                      ?.pageInfo,
+                },
+              }) as T[],
             };
           } catch (error) {
             handleFindManyRecordsError(error as ApolloError);
             return { error: error as ApolloError };
-          } finally {
-            setIsFetchingMoreObjects(false);
           }
         }
       },
     [
+      queryIdentifier,
       objectMetadataItem,
       error,
-      setIsFetchingMoreObjects,
       fetchMore,
       filter,
       orderBy,
-      onCompleted,
       handleFindManyRecordsError,
-      queryIdentifier,
     ],
   );
 
-  const totalCount = data?.[objectMetadataItem.namePlural]?.totalCount;
-
-  const recordConnection = data?.[objectMetadataItem.namePlural];
-
-  const records = useMemo(
-    () =>
-      isDefined(recordConnection)
-        ? getRecordsFromRecordConnection<T>({
-            recordConnection,
-          })
-        : [],
-    [recordConnection],
-  );
-
   return {
-    fetchMoreRecords,
-    totalCount,
-    records,
-    hasNextPage,
+    fetchMoreRecordsLazy,
   };
 };

packages/twenty-front/src/modules/object-record/hooks/useLazyFindManyRecords.ts
@@ -2,12 +2,13 @@ import { useLazyQuery } from '@apollo/client';
 import { useRecoilCallback } from 'recoil';
 
 import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
+import { getRecordsFromRecordConnection } from '@/object-record/cache/utils/getRecordsFromRecordConnection';
 import { RecordGqlOperationFindManyResult } from '@/object-record/graphql/types/RecordGqlOperationFindManyResult';
-import { useFetchMoreRecordsWithPagination } from '@/object-record/hooks/useFetchMoreRecordsWithPagination';
 import { UseFindManyRecordsParams } from '@/object-record/hooks/useFindManyRecords';
 import { useFindManyRecordsQuery } from '@/object-record/hooks/useFindManyRecordsQuery';
 import { useHandleFindManyRecordsCompleted } from '@/object-record/hooks/useHandleFindManyRecordsCompleted';
 import { useHandleFindManyRecordsError } from '@/object-record/hooks/useHandleFindManyRecordsError';
+import { useLazyFetchMoreRecordsWithPagination } from '@/object-record/hooks/useLazyFetchMoreRecordsWithPagination';
 import { useObjectPermissionsForObject } from '@/object-record/hooks/useObjectPermissionsForObject';
 import { cursorFamilyState } from '@/object-record/states/cursorFamilyState';
 import { hasNextPageFamilyState } from '@/object-record/states/hasNextPageFamilyState';
@@ -16,7 +17,7 @@ import { getQueryIdentifier } from '@/object-record/utils/getQueryIdentifier';
 
 type UseLazyFindManyRecordsParams<T> = Omit<
   UseFindManyRecordsParams<T>,
-  'skip'
+  'skip' | 'onCompleted' | 'onError'
 >;
 
 export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
@@ -25,9 +26,6 @@ export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
   orderBy,
   limit,
   recordGqlFields,
-  fetchPolicy,
-  onCompleted,
-  onError,
 }: UseLazyFindManyRecordsParams<T>) => {
   const { objectMetadataItem } = useObjectMetadataItem({
     objectNameSingular,
@@ -40,7 +38,6 @@ export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
 
   const { handleFindManyRecordsError } = useHandleFindManyRecordsError({
     objectMetadataItem,
-    handleError: onError,
   });
 
   const queryIdentifier = getQueryIdentifier({
@@ -53,7 +50,6 @@ export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
   const { handleFindManyRecordsCompleted } = useHandleFindManyRecordsCompleted({
     objectMetadataItem,
     queryIdentifier,
-    onCompleted,
   });
 
   const objectPermissions = useObjectPermissionsForObject(
@@ -62,30 +58,28 @@ export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
 
   const hasReadPermission = objectPermissions.canReadObjectRecords;
 
-  const [findManyRecords, { data, loading, error, fetchMore }] =
+  const [findManyRecords, { data, error, fetchMore }] =
     useLazyQuery<RecordGqlOperationFindManyResult>(findManyRecordsQuery, {
       variables: {
         filter,
         limit,
         orderBy,
       },
-      fetchPolicy: fetchPolicy,
+      fetchPolicy: 'network-only',
       onCompleted: handleFindManyRecordsCompleted,
       onError: handleFindManyRecordsError,
     });
 
-  const { fetchMoreRecords, totalCount, records, hasNextPage } =
-    useFetchMoreRecordsWithPagination<T>({
-      objectNameSingular,
-      filter,
-      orderBy,
-      limit,
-      onCompleted,
-      fetchMore,
-      data,
-      error,
-      objectMetadataItem,
-    });
+  const { fetchMoreRecordsLazy } = useLazyFetchMoreRecordsWithPagination<T>({
+    objectNameSingular,
+    filter,
+    orderBy,
+    limit,
+    fetchMore,
+    data,
+    error,
+    objectMetadataItem,
+  });
 
   const findManyRecordsLazy = useRecoilCallback(
     ({ set }) =>
@@ -96,9 +90,10 @@ export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
 
           return {
             data: null,
-            loading: false,
+            records: [],
+            totalCount: 0,
+            hasNextPage: false,
             error: undefined,
-            called: true,
           };
         }
 
@@ -115,7 +110,29 @@ export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
         set(hasNextPageFamilyState(queryIdentifier), hasNextPage);
         set(cursorFamilyState(queryIdentifier), lastCursor);
 
-        return result;
+        const records = getRecordsFromRecordConnection({
+          recordConnection: {
+            edges: result?.data?.[objectMetadataItem.namePlural]?.edges ?? [],
+            pageInfo: result?.data?.[objectMetadataItem.namePlural]
+              ?.pageInfo ?? {
+              hasNextPage: false,
+              hasPreviousPage: false,
+              startCursor: '',
+              endCursor: '',
+            },
+          },
+        });
+
+        const totalCount =
+          result?.data?.[objectMetadataItem.namePlural]?.totalCount ?? 0;
+
+        return {
+          data: result?.data,
+          records,
+          totalCount,
+          hasNextPage,
+          error: result?.error,
+        };
       },
     [
       hasReadPermission,
@@ -126,16 +143,8 @@ export const useLazyFindManyRecords = <T extends ObjectRecord = ObjectRecord>({
   );
 
   return {
-    objectMetadataItem,
-    records,
-    totalCount,
-    loading: hasReadPermission ? loading : false,
-    error: hasReadPermission ? error : undefined,
-    fetchMore,
-    fetchMoreRecords,
-    queryStateIdentifier: queryIdentifier,
-    findManyRecords: findManyRecordsLazy,
-    hasNextPage,
-    hasReadPermission,
+    findManyRecordsLazy,
+    fetchMoreRecordsLazy,
+    queryIdentifier,
   };
 };

packages/twenty-front/src/modules/object-record/record-board/record-board-column/components/RecordBoardColumnFetchMoreLoader.tsx
@@ -6,8 +6,6 @@ import { useRecoilValue } from 'recoil';
 import { RecordBoardColumnContext } from '@/object-record/record-board/record-board-column/contexts/RecordBoardColumnContext';
 import { isRecordBoardFetchingRecordsByColumnFamilyState } from '@/object-record/record-board/states/isRecordBoardFetchingRecordsByColumnFamilyState';
 import { recordBoardShouldFetchMoreInColumnComponentFamilyState } from '@/object-record/record-board/states/recordBoardShouldFetchMoreInColumnComponentFamilyState';
-import { isRecordIndexLoadMoreLockedComponentState } from '@/object-record/record-index/states/isRecordIndexLoadMoreLockedComponentState';
-import { useRecoilComponentValueV2 } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValueV2';
 import { useSetRecoilComponentFamilyStateV2 } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentFamilyStateV2';
 import { GRAY_SCALE } from 'twenty-ui/theme';
 
@@ -33,23 +31,11 @@ export const RecordBoardColumnFetchMoreLoader = () => {
     columnDefinition.id,
   );
 
-  const isLoadMoreLocked = useRecoilComponentValueV2(
-    isRecordIndexLoadMoreLockedComponentState,
-  );
-
   const { ref, inView } = useInView();
 
   useEffect(() => {
-    if (isLoadMoreLocked) {
-      return;
-    }
-
     setShouldFetchMore(inView);
-  }, [setShouldFetchMore, inView, isLoadMoreLocked]);
-
-  if (isLoadMoreLocked) {
-    return null;
-  }
+  }, [setShouldFetchMore, inView]);
 
   return (
     <div ref={ref}>

commit 4da0152c33afcede1219c0c748b9409d0d55a858

Fix`useLazyFetchAllRecords` any wrong infer (#12728)
# Introduction
closes https://github.com/twentyhq/twenty/issues/12721
Regression seems to be introduced by
https://github.com/twentyhq/twenty/pull/12653

## Post mortem
### Previously
No mapping on `namePlural` dynamic response data key
```ts
      const rawResult = await fetchMore({
        variables: {
          lastCursor: lastCursor,
          limit,
        },
      });
```

### Now
Mapping is done by the tool we're consuming already
```ts
      const rawResult = await fetchMoreRecordsLazy();

// 
const fetchMoreRecordsLazy = useRecoilCallback(
    ({ snapshot, set }) =>
      async () => {

          //...
           return {
              data: fetchMoreDataResult?.[objectMetadataItem.namePlural],
              totalCount:
                fetchMoreDataResult?.[objectMetadataItem.namePlural]
                  ?.totalCount,
              records: getRecordsFromRecordConnection({
                recordConnection: {
                  edges:
                    fetchMoreDataResult?.[objectMetadataItem.namePlural]?.edges,
                  pageInfo:
                    fetchMoreDataResult?.[objectMetadataItem.namePlural]
                      ?.pageInfo,
                },
              }) as T[],
```

## Concerns
TypeScript did not throw any errors because `RecordConnectionGql` is
typed as any, we should consider adding a generic type defaulted to
unknown
```ts
import { RecordGqlEdge } from '@/object-record/graphql/types/RecordGqlEdge';
import { Nullable } from 'twenty-ui/utilities';

export type RecordGqlConnection = {
  __typename?: string;
  edges: RecordGqlEdge[];
  pageInfo: {
    __typename?: string;
    hasNextPage?: boolean;
    hasPreviousPage?: boolean;
    startCursor?: Nullable<string>;
    endCursor?: Nullable<string>;
    totalCount?: number;
  };
  totalCount?: number;
  [aggregateFieldName: string]: any; // Any is problematic here should be unknown
};
```

Example:
```ts

packages/twenty-front/src/modules/object-record/hooks/useLazyFetchAllRecords.ts
@@ -94,9 +94,9 @@ export const useLazyFetchAllRecords = <T>({
 
       const rawResult = await fetchMoreRecordsLazy();
 
-      const fetchMoreResult = rawResult?.data?.[objectMetadataItem.namePlural];
+      const fetchMoreResult = rawResult?.data;
 
-      for (const edge of fetchMoreResult.edges) {
+      for (const edge of fetchMoreResult?.edges ?? []) {
         records.push(edge.node);
       }
 
@@ -106,11 +106,11 @@ export const useLazyFetchAllRecords = <T>({
         displayType: totalCount ? 'percentage' : 'number',
       });
 
-      if (fetchMoreResult.pageInfo.hasNextPage === false) {
+      if (fetchMoreResult?.pageInfo.hasNextPage === false) {
         break;
       }
 
-      lastCursor = fetchMoreResult.pageInfo.endCursor ?? null;
+      lastCursor = fetchMoreResult?.pageInfo.endCursor ?? null;
     }
 
     setIsDownloading(false);