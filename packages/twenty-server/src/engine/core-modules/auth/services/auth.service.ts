a7fe8ac3b1826adf084d52e6441283aa4a02aa2d
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-06-20 11:47:25 +0200

    fix(auth): social login

--- packages/twenty-server/src/engine/core-modules/auth/services/auth.service.ts
+++ packages/twenty-server/src/engine/core-modules/auth/services/auth.service.ts
@@ -779,7 +779,7 @@ export class AuthService {
         workspace: currentWorkspace,
         userData,
         authParams: {
-          provider: authProvider
+          provider: authProvider,
         },
         billingCheckoutSessionState,
       });
