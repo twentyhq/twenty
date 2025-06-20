0816c6e1dad400b957b6ea10eca71f042c54a1e6
bosiraphael <raphael.bosi@gmail.com>
2025-06-20 17:49:07 +0200

    fix

--- packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellHotkeysEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-cell/components/RecordTableCellHotkeysEffect.tsx
@@ -1,25 +1,25 @@
+import { useContext } from 'react';
+import { Key } from 'ts-key-enum';
+
 import { FieldContext } from '@/object-record/record-field/contexts/FieldContext';
 import { useClearField } from '@/object-record/record-field/hooks/useClearField';
 import { useIsFieldClearable } from '@/object-record/record-field/hooks/useIsFieldClearable';
 import { useIsFieldInputOnly } from '@/object-record/record-field/hooks/useIsFieldInputOnly';
 import { useToggleEditOnlyInput } from '@/object-record/record-field/hooks/useToggleEditOnlyInput';
+import { useCurrentlyFocusedRecordTableCellFocusId } from '@/object-record/record-table/record-table-cell/hooks/useCurrentlyFocusedRecordTableCellFocusId';
 import { useOpenRecordTableCellFromCell } from '@/object-record/record-table/record-table-cell/hooks/useOpenRecordTableCellFromCell';
 import { TableHotkeyScope } from '@/object-record/record-table/types/TableHotkeyScope';
 import { useHotkeysOnFocusedElement } from '@/ui/utilities/hotkey/hooks/useHotkeysOnFocusedElement';
 import { isNonTextWritingKey } from '@/ui/utilities/hotkey/utils/isNonTextWritingKey';
-import { useContext } from 'react';
-import { Key } from 'ts-key-enum';
 
-export const useRecordTableHotkeys = (focusId: string) => {
+export const RecordTableCellHotkeysEffect = () => {
   const { openTableCell } = useOpenRecordTableCellFromCell();
   const { isReadOnly } = useContext(FieldContext);
+  const cellFocusId = useCurrentlyFocusedRecordTableCellFocusId();
 
   const isFieldInputOnly = useIsFieldInputOnly();
-
   const isFieldClearable = useIsFieldClearable();
-
   const toggleEditOnlyInput = useToggleEditOnlyInput();
-
   const clearField = useClearField();
 
   const handleBackspaceOrDelete = () => {
@@ -66,7 +66,7 @@ export const useRecordTableHotkeys = (focusId: string) => {
   useHotkeysOnFocusedElement({
     keys: [Key.Backspace, Key.Delete],
     callback: handleBackspaceOrDelete,
-    focusId,
+    focusId: cellFocusId,
     scope: TableHotkeyScope.TableFocus,
     dependencies: [handleBackspaceOrDelete],
   });
@@ -74,7 +74,7 @@ export const useRecordTableHotkeys = (focusId: string) => {
   useHotkeysOnFocusedElement({
     keys: [Key.Enter],
     callback: handleEnter,
-    focusId,
+    focusId: cellFocusId,
     scope: TableHotkeyScope.TableFocus,
     dependencies: [handleEnter],
   });
@@ -82,8 +82,13 @@ export const useRecordTableHotkeys = (focusId: string) => {
   useHotkeysOnFocusedElement({
     keys: ['*'],
     callback: handleAnyKey,
-    focusId,
+    focusId: cellFocusId,
     scope: TableHotkeyScope.TableFocus,
     dependencies: [handleAnyKey],
+    options: {
+      preventDefault: false,
+    },
   });
+
+  return null;
 };
