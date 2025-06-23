248effd7eb12b260d9f916215b06c0c7c1d90dfa
Abdul Rahman <ar5438376@gmail.com>
2025-06-23 16:05:50 +0530

    Refactor fromRoleEntityToRoleDto utility to streamline property mapping from RoleEntity to RoleDTO, enhancing code readability and maintainability.

--- packages/twenty-server/src/engine/metadata-modules/role/utils/fromRoleEntityToRoleDto.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/role/utils/fromRoleEntityToRoleDto.util.ts
@@ -1,31 +1,24 @@
 import { RoleDTO } from 'src/engine/metadata-modules/role/dtos/role.dto';
 import { RoleEntity } from 'src/engine/metadata-modules/role/role.entity';
 
-export const fromRoleEntityToRoleDto = ({
-  id,
-  label,
-  canUpdateAllSettings,
-  description,
-  icon,
-  isEditable,
-  userWorkspaceRoles,
-  canReadAllObjectRecords,
-  canUpdateAllObjectRecords,
-  canSoftDeleteAllObjectRecords,
-  canDestroyAllObjectRecords,
-}: RoleEntity): RoleDTO => {
+export const fromRoleEntityToRoleDto = (role: RoleEntity): RoleDTO => {
   return {
-    id,
-    label,
-    canUpdateAllSettings,
-    description,
-    icon,
-    isEditable,
-    userWorkspaceRoles,
-    canReadAllObjectRecords,
-    canUpdateAllObjectRecords,
-    canSoftDeleteAllObjectRecords,
-    canDestroyAllObjectRecords,
+    id: role.id,
+    label: role.label,
+    canUpdateAllSettings: role.canUpdateAllSettings,
+    canReadAllObjectRecords: role.canReadAllObjectRecords,
+    canUpdateAllObjectRecords: role.canUpdateAllObjectRecords,
+    canSoftDeleteAllObjectRecords: role.canSoftDeleteAllObjectRecords,
+    canDestroyAllObjectRecords: role.canDestroyAllObjectRecords,
+    description: role.description,
+    icon: role.icon,
+    workspaceId: role.workspaceId,
+    createdAt: role.createdAt,
+    updatedAt: role.updatedAt,
+    isEditable: role.isEditable,
+    roleTargets: role.roleTargets,
+    objectPermissions: role.objectPermissions,
+    settingPermissions: role.settingPermissions,
   };
 };
 
