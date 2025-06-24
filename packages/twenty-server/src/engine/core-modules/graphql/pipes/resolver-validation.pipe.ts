19e2c0e19da1b1b87cd131f5d592ec6f314796d2
prastoin <paul@twenty.com>
2025-06-24 17:54:21 +0200

    fix(server): validation snake trap

--- packages/twenty-server/src/engine/core-modules/graphql/pipes/resolver-validation.pipe.ts
+++ packages/twenty-server/src/engine/core-modules/graphql/pipes/resolver-validation.pipe.ts
@@ -10,6 +10,16 @@ import { ValidationError, validate } from 'class-validator';
 
 import { UserInputError } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
 
+const safeClassValidatorValidateWrapper = async (
+  object: object,
+): Promise<ValidationError[]> => {
+  try {
+    return await validate(object);
+  } catch (error) {
+    return [];
+  }
+};
+
 @Injectable()
 export class ResolverValidationPipe implements PipeTransform {
   async transform(value: unknown, metadata: ArgumentMetadata) {
@@ -20,21 +30,13 @@ export class ResolverValidationPipe implements PipeTransform {
     }
 
     const object = plainToInstance(metatype, value);
-
-    try {
-      const errors = await validate(object);
-
-      if (errors.length > 0) {
-        const errorMessage = this.formatErrorMessage(errors);
-
-        throw new UserInputError(errorMessage);
-      }
-    } catch (error) {
-      // If the element is not a class, we can't validate it
+    const errors = await safeClassValidatorValidateWrapper(object);
+    if (errors.length === 0) {
       return value;
     }
 
-    return value;
+    const errorMessage = this.formatErrorMessage(errors);
+    throw new UserInputError(errorMessage);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
