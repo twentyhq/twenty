4e094bbca28f2b5be67df9dd81646edf7219a460
prastoin <paul@twenty.com>
2025-06-24 15:13:48 +0200

    fix(front): remove number from label identifier list

--- packages/twenty-front/src/modules/object-metadata/constants/LabelIdentifierFieldMetadataTypes.ts
+++ packages/twenty-front/src/modules/object-metadata/constants/LabelIdentifierFieldMetadataTypes.ts
@@ -1,7 +1,6 @@
 import { FieldMetadataType } from '~/generated-metadata/graphql';
 
 export const LABEL_IDENTIFIER_FIELD_METADATA_TYPES = [
-  FieldMetadataType.NUMBER,
   FieldMetadataType.TEXT,
   FieldMetadataType.FULL_NAME,
 ];
