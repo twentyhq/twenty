ce55b20faaea96b2803a8accf1af965204d9c841
martmull <martmull@hotmail.fr>
2025-06-24 15:29:00 +0200

    Use main dataSource to query CRON jobs (#12830)
    As title

--- packages/twenty-server/src/modules/workflow/workflow-trigger/automated-trigger/automated-trigger.module.ts
+++ packages/twenty-server/src/modules/workflow/workflow-trigger/automated-trigger/automated-trigger.module.ts
@@ -7,11 +7,13 @@ import { AutomatedTriggerWorkspaceService } from 'src/modules/workflow/workflow-
 import { CronTriggerCronCommand } from 'src/modules/workflow/workflow-trigger/automated-trigger/crons/commands/cron-trigger.cron.command';
 import { CronTriggerCronJob } from 'src/modules/workflow/workflow-trigger/automated-trigger/crons/jobs/cron-trigger.cron.job';
 import { DatabaseEventTriggerListener } from 'src/modules/workflow/workflow-trigger/automated-trigger/listeners/database-event-trigger.listener';
+import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Workspace], 'core'),
     WorkflowCommonModule,
+    WorkspaceDataSourceModule,
   ],
   providers: [
     AutomatedTriggerWorkspaceService,
