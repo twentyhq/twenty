14121c75761af2beaf38bfc730711386357a3191
Abdul Rahman <ar5438376@gmail.com>
2025-06-26 02:23:57 +0530

    Update migration to rename userWorkspaceRole to roleTargets, modifying foreign key constraints for improved data integrity and consistency.

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1749000000000-rename-user-workspace-role-to-role-targets.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1749000000000-rename-user-workspace-role-to-role-targets.ts
@@ -15,6 +15,10 @@ export class RenameUserWorkspaceRoleToRoleTargets1749000000000
     );
 
     await queryRunner.query(
+      `ALTER TABLE "core"."userWorkspaceRole" DROP CONSTRAINT "FK_0b70755f23a3705f1bea0ddc7d4"`,
+    );
+
+    await queryRunner.query(
       `ALTER TABLE "core"."userWorkspaceRole" ADD CONSTRAINT "FK_8febe85bd7aac55de81b1c51140" FOREIGN KEY ("agentId") REFERENCES "core"."agent"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
 
@@ -43,9 +47,23 @@ export class RenameUserWorkspaceRoleToRoleTargets1749000000000
     await queryRunner.query(
       `ALTER TABLE "core"."roleTargets" ADD CONSTRAINT "IDX_ROLE_TARGETS_UNIQUE" UNIQUE ("userWorkspaceId", "roleId", "agentId")`,
     );
+
+    await queryRunner.query(
+      `ALTER TABLE "core"."roleTargets" ADD CONSTRAINT "FK_d5838ba43033ee6266d8928d7d7" FOREIGN KEY ("roleId") REFERENCES "core"."role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."roleTargets" ADD CONSTRAINT "FK_15066df4c077d783a5e21934909" FOREIGN KEY ("userWorkspaceId") REFERENCES "core"."userWorkspace"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "core"."roleTargets" DROP CONSTRAINT "FK_15066df4c077d783a5e21934909"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."roleTargets" DROP CONSTRAINT "FK_d5838ba43033ee6266d8928d7d7"`,
+    );
+
     await queryRunner.query(`DROP INDEX "core"."IDX_ROLE_TARGETS_AGENT_ID"`);
 
     await queryRunner.query(
@@ -72,6 +90,10 @@ export class RenameUserWorkspaceRoleToRoleTargets1749000000000
 
     await queryRunner.query(
       `ALTER TABLE "core"."userWorkspaceRole" DROP CONSTRAINT "FK_8febe85bd7aac55de81b1c51140"`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "core"."userWorkspaceRole" ADD CONSTRAINT "FK_0b70755f23a3705f1bea0ddc7d4" FOREIGN KEY ("roleId") REFERENCES "core"."role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
 
     await queryRunner.query(
