6061184361d84f35157278e0341da769133f396a
Paul Rastoin <45004772+prastoin@users.noreply.github.com>
2025-06-26 15:25:20 +0200

    fix(server): incr metadata version (#12907)
    Following https://github.com/twentyhq/twenty/pull/12883

--- packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
@@ -21,6 +21,7 @@ import { FixStandardSelectFieldsPositionCommand } from 'src/database/commands/up
 import { LowercaseUserAndInvitationEmailsCommand } from 'src/database/commands/upgrade-version-command/0-54/0-54-lowercase-user-and-invitation-emails.command';
 import { MigrateDefaultAvatarUrlToUserWorkspaceCommand } from 'src/database/commands/upgrade-version-command/0-54/0-54-migrate-default-avatar-url-to-user-workspace.command';
 import { DeduplicateIndexedFieldsCommand } from 'src/database/commands/upgrade-version-command/0-55/0-55-deduplicate-indexed-fields.command';
+import { FixUpdateStandardFieldsIsLabelSyncedWithName } from 'src/database/commands/upgrade-version-command/1-1/1-1-fix-update-standard-field-is-label-synced-with-name.command';
 import { TwentyConfigService } from 'src/engine/core-modules/twenty-config/twenty-config.service';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
@@ -135,6 +136,9 @@ export class UpgradeCommand extends UpgradeCommandRunner {
 
     // 0.55 Commands
     protected readonly deduplicateIndexedFieldsCommand: DeduplicateIndexedFieldsCommand,
+
+    // 1.1  Commands
+    protected readonly fixUpdateStandardFieldsIsLabelSyncedWithNameCommand: FixUpdateStandardFieldsIsLabelSyncedWithName,
   ) {
     super(
       workspaceRepository,
@@ -176,8 +180,10 @@ export class UpgradeCommand extends UpgradeCommandRunner {
     };
 
     const commands_110: VersionCommands = {
+      beforeSyncMetadata: [
+        this.fixUpdateStandardFieldsIsLabelSyncedWithNameCommand,
+      ],
       afterSyncMetadata: [],
-      beforeSyncMetadata: [],
     };
 
     this.allCommands = {
