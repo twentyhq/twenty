4c6d51b61b330832e00dea3dcdb29af75edcb78a
etiennejouan <jouan.etienne@gmail.com>
2025-06-26 15:45:03 +0200

    fix test

--- packages/twenty-shared/src/utils/url/lowercaseUrlAndRemoveTrailingSlash.ts
+++ packages/twenty-shared/src/utils/url/lowercaseUrlAndRemoveTrailingSlash.ts
@@ -1,7 +1,7 @@
 export const lowercaseUrlAndRemoveTrailingSlash = (url: string) => {
   try {
-    return new URL(url).toString().replace(/\/$/, '');
+    return new URL(url).toString().toLowerCase().replace(/\/$/, '');
   } catch {
-    return url;
+    return url.toLowerCase();
   }
 };
