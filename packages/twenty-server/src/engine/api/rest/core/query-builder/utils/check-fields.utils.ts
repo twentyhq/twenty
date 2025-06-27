b6fa427c1508e3ba8f363f20e6234f9a19fc1ff7
prastoin <paul@twenty.com>
2025-06-27 16:16:26 +0200

    test(server): refactor using test each

--- packages/twenty-server/src/engine/api/rest/core/query-builder/utils/check-fields.utils.ts
+++ packages/twenty-server/src/engine/api/rest/core/query-builder/utils/check-fields.utils.ts
@@ -1,19 +1,20 @@
 import { BadRequestException } from '@nestjs/common';
 
+import { FieldMetadataType } from 'twenty-shared/types';
+import { isDefined } from 'twenty-shared/utils';
+
 import { compositeTypeDefinitions } from 'src/engine/metadata-modules/field-metadata/composite-types';
 import { isCompositeFieldMetadataType } from 'src/engine/metadata-modules/field-metadata/utils/is-composite-field-metadata-type.util';
 import { ObjectMetadataItemWithFieldMaps } from 'src/engine/metadata-modules/types/object-metadata-item-with-field-maps';
 import { computeObjectTargetTable } from 'src/engine/utils/compute-object-target-table.util';
 import { isFieldMetadataInterfaceOfType } from 'src/engine/utils/is-field-metadata-of-type.util';
-import { FieldMetadataType } from 'twenty-shared/types';
-import { isDefined } from 'twenty-shared/utils';
 
 export const checkFields = (
-  objectMetadataItem: ObjectMetadataItemWithFieldMaps,
+  objectMetadataItemWithFieldsMaps: ObjectMetadataItemWithFieldMaps,
   fieldNames: string[],
 ): void => {
   const fieldMetadataNames: string[] = Object.values(
-    objectMetadataItem.fieldsById,
+    objectMetadataItemWithFieldsMaps.fieldsById,
   )
     .flatMap((field) => {
       if (isCompositeFieldMetadataType(field.type)) {
@@ -47,7 +48,7 @@ export const checkFields = (
     if (!fieldMetadataNames.includes(fieldName)) {
       throw new BadRequestException(
         `field '${fieldName}' does not exist in '${computeObjectTargetTable(
-          objectMetadataItem,
+          objectMetadataItemWithFieldsMaps,
         )}' object`,
       );
     }
