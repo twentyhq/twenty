d5bd980dce3391eeb3460ab7103d0ab5e696a3b7
Hemanthkumar Krishnakumar <hemanthkumar2k04@gmail.com>
2025-06-29 16:21:37 +0530

    Fixed the visibility of  labels "Visible fields" and "hidden fields" for both Filter and Sort dropDownMenuSection

--- packages/twenty-front/src/modules/ui/layout/dropdown/components/DropdownMenuSectionLabel.tsx
+++ packages/twenty-front/src/modules/ui/layout/dropdown/components/DropdownMenuSectionLabel.tsx
@@ -2,16 +2,18 @@ import styled from '@emotion/styled';
 
 const StyledDropdownMenuSectionLabel = styled.div`
   background-color: ${({ theme }) => theme.background.transparent.lighter};
-  color: ${({ theme }) => theme.font.color.light};
+  color: ${({ theme }) => theme.font.color.primary};
   min-height: 20px;
   width: auto;
-  font-size: ${({ theme }) => theme.font.size.xxs};
+  font-size: ${({ theme }) => theme.font.size.xs};
   display: flex;
   align-items: center;
   justify-content: flex-start;
   padding-left: ${({ theme }) => theme.spacing(1)};
   user-select: none;
+  font-weight: ${({ theme }) => theme.font.weight.semiBold};
 `;
+
 
 export type DropdownMenuSectionLabelProps = {
   label: string;
