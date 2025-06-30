04412ae987f8e4e4e3dc55475db24c5a65209013
prastoin <paul@twenty.com>
2025-06-30 17:50:26 +0200

    chore(sdk): dependencies and ci

---
+++ packages/twenty-sdk/src/__tests__/index.test.ts
@@ -0,0 +1,4 @@
+import { addNum } from '@/index';
+it('Should run successfully', () => {
+  expect(addNum(1, 1)).toBe(2);
+});
--- packages/twenty-sdk/src/index.ts
+++ packages/twenty-sdk/src/index.ts
@@ -1,10 +1,1 @@
-/*
- * _____                    _
- *|_   _|_      _____ _ __ | |_ _   _
- *  | | \ \ /\ / / _ \ '_ \| __| | | | Auto-genreated file
- *  | |  \ V  V /  __/ | | | |_| |_| | Any edition to this will be override
- *  |_|   \_/\_/ \___|_| |_|\__|\__, |
- *                              |___/
- */
-
-export default {};
+export const addNum = (a: number, b: number) => a + b;
