8c71baeff64adf5286558ecba291b2a5be8518bc
Marie Stoppa <marie.stoppa@essec.edu>
2025-07-02 17:27:14 +0200

    adapt tests

--- packages/twenty-server/test/integration/metadata/suites/field-metadata/update-one-field-metadata.integration-spec.ts
+++ packages/twenty-server/test/integration/metadata/suites/field-metadata/update-one-field-metadata.integration-spec.ts
@@ -181,7 +181,7 @@ describe('updateOne', () => {
   {
     "extensions": {
       "code": "BAD_USER_INPUT",
-      "displayedErrorMessage": "An error occurred.",
+      "displayedErrorMessage": "Default value "'OPTION_2'" must be one of the option values",
     },
     "message": "Default value "'OPTION_2'" must be one of the option values",
     "name": "UserInputError",
