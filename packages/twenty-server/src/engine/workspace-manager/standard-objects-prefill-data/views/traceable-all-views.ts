2c5172f1ec97279abfaf29d7228ed59526b1e46f
Eliezer Rodrigues <eliezer_gt.02@hotmail.com>
2025-07-04 14:52:35 -0300

    fix: Missing standard object views
    * Added missing views for `traceable` and `product` standard objects;
    * Refactored `traceable` and `product` all view to follow new 1.0 standard;

--- packages/twenty-server/src/engine/workspace-manager/standard-objects-prefill-data/views/traceable-all-views.ts
+++ packages/twenty-server/src/engine/workspace-manager/standard-objects-prefill-data/views/traceable-all-views.ts
@@ -1,14 +1,21 @@
-import { ObjectMetadataStandardIdToIdMap } from 'src/engine/metadata-modules/object-metadata/interfaces/object-metadata-standard-id-to-id-map';
-
+import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 import { TRACEABLE_STANDARD_FIELD_IDS } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-field-ids';
 import { STANDARD_OBJECT_IDS } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-object-ids';
+
 export const tracaebleAllView = (
-  objectMetadataStandardIdToIdMap: ObjectMetadataStandardIdToIdMap,
+  objectMetadataItems: ObjectMetadataEntity[],
 ) => {
+  const traceableObjectMetadata = objectMetadataItems.find(
+    (object) => object.standardId === STANDARD_OBJECT_IDS.traceable,
+  );
+
+  if (!traceableObjectMetadata) {
+    throw new Error('Traceable object metadata not found');
+  }
+
   return {
     name: 'All',
-    objectMetadataId:
-      objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].id,
+    objectMetadataId: traceableObjectMetadata.id,
     type: 'table',
     key: 'INDEX',
     position: 0,
@@ -18,36 +25,39 @@ export const tracaebleAllView = (
     fields: [
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.name
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) => field.standardId === TRACEABLE_STANDARD_FIELD_IDS.name,
+          )?.id ?? '',
         position: 0,
         isVisible: true,
         size: 150,
       },
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.websiteUrl
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) =>
+              field.standardId === TRACEABLE_STANDARD_FIELD_IDS.websiteUrl,
+          )?.id ?? '',
         position: 1,
         isVisible: true,
         size: 150,
       },
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.campaignName
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) =>
+              field.standardId === TRACEABLE_STANDARD_FIELD_IDS.campaignName,
+          )?.id ?? '',
         position: 2,
         isVisible: true,
         size: 150,
       },
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.campaignSource
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) =>
+              field.standardId === TRACEABLE_STANDARD_FIELD_IDS.campaignSource,
+          )?.id ?? '',
         position: 3,
         isVisible: true,
         size: 150,
@@ -55,36 +65,41 @@ export const tracaebleAllView = (
 
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.meansOfCommunication
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) =>
+              field.standardId ===
+              TRACEABLE_STANDARD_FIELD_IDS.meansOfCommunication,
+          )?.id ?? '',
         position: 4,
         isVisible: true,
         size: 150,
       },
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.keyword
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) =>
+              field.standardId === TRACEABLE_STANDARD_FIELD_IDS.keyword,
+          )?.id ?? '',
         position: 5,
         isVisible: true,
         size: 150,
       },
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.campaignContent
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) =>
+              field.standardId === TRACEABLE_STANDARD_FIELD_IDS.campaignContent,
+          )?.id ?? '',
         position: 6,
         isVisible: true,
         size: 150,
       },
       {
         fieldMetadataId:
-          objectMetadataStandardIdToIdMap[STANDARD_OBJECT_IDS.traceable].fields[
-            TRACEABLE_STANDARD_FIELD_IDS.generatedUrl
-          ],
+          traceableObjectMetadata.fields.find(
+            (field) =>
+              field.standardId === TRACEABLE_STANDARD_FIELD_IDS.generatedUrl,
+          )?.id ?? '',
         position: 7,
         isVisible: false,
         size: 150,
