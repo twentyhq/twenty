4272bc51c5330d11936dc97375d8078e9295bc84
omarNaifer12 <o.naifer7@gmail.com>
2025-07-04 20:17:07 +0000

    fix: allow outside click on date input

--- packages/twenty-front/src/modules/ui/field/input/components/DateInput.tsx
+++ packages/twenty-front/src/modules/ui/field/input/components/DateInput.tsx
@@ -88,7 +88,6 @@ export const DateInput = ({
         if (hotkeyScope?.scope === TableHotkeyScope.CellEditMode) {
           closeDropdownYearSelect(MONTH_AND_YEAR_DROPDOWN_YEAR_SELECT_ID);
           closeDropdownMonthSelect(MONTH_AND_YEAR_DROPDOWN_MONTH_SELECT_ID);
-          onEscape(internalValue);
           onClickOutside(event, internalValue);
         }
       },
