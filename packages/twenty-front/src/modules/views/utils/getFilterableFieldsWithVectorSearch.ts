97e0c1015e12d2ee9852231e50773e66dd67f150
Abdul Rahman <ar5438376@gmail.com>
2025-07-06 01:09:44 +0530

    Refactor getFilterableFieldsWithVectorSearch to remove isJsonFilterEnabled parameter from getFilterFilterableFieldMetadataItems function call.

--- packages/twenty-front/src/modules/views/utils/getFilterableFieldsWithVectorSearch.ts
+++ packages/twenty-front/src/modules/views/utils/getFilterableFieldsWithVectorSearch.ts
@@ -12,7 +12,7 @@ export const getFilterableFieldsWithVectorSearch = (
 
   return [
     ...objectMetadataItem.fields.filter(
-      getFilterFilterableFieldMetadataItems({ isJsonFilterEnabled: true }),
+      getFilterFilterableFieldMetadataItems(),
     ),
     ...(vectorSearchField ? [vectorSearchField] : []),
   ];
