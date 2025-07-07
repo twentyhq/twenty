600ea8768a04abd1486813cd94ff4b235d74d2d9
Abdul Rahman <ar5438376@gmail.com>
2025-07-08 00:39:37 +0530

    Update getFilterableFieldsWithVectorSearch to enable JSON filter in field metadata retrieval

--- packages/twenty-front/src/modules/views/utils/getFilterableFieldsWithVectorSearch.ts
+++ packages/twenty-front/src/modules/views/utils/getFilterableFieldsWithVectorSearch.ts
@@ -12,7 +12,7 @@ export const getFilterableFieldsWithVectorSearch = (
 
   return [
     ...objectMetadataItem.fields.filter(
-      getFilterFilterableFieldMetadataItems(),
+      getFilterFilterableFieldMetadataItems({ isJsonFilterEnabled: true }),
     ),
     ...(vectorSearchField ? [vectorSearchField] : []),
   ];
