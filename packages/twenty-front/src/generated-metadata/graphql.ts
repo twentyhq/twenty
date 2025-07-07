29f7b747562efc1c6f1097b218c8815cf2729d70
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-07-07 18:06:25 +0200

    fix(ci): reorganize workflow steps and move cache saving to correct s… (#13083)
    …tage

--- packages/twenty-front/src/generated-metadata/graphql.ts
+++ packages/twenty-front/src/generated-metadata/graphql.ts
@@ -2561,6 +2561,7 @@ export type UserWorkspace = {
   createdAt: Scalars['DateTime'];
   deletedAt?: Maybe<Scalars['DateTime']>;
   id: Scalars['UUID'];
+  locale: Scalars['String'];
   objectPermissions?: Maybe<Array<ObjectPermission>>;
   /** @deprecated Use objectPermissions instead */
   objectRecordsPermissions?: Maybe<Array<PermissionsOnAllObjectRecords>>;
