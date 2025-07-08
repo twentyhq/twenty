09778d93977e10b8392a4b5e77f601512218d529
Dmitry Moiseenko <cxdimafeed@gmail.com>
2025-07-08 14:32:51 -0400

    fix lint error with Prettier

--- packages/twenty-front/src/modules/workflow/workflow-trigger/utils/__tests__/getSentenceCaseLabels.test.ts
+++ packages/twenty-front/src/modules/workflow/workflow-trigger/utils/__tests__/getSentenceCaseLabels.test.ts
@@ -8,18 +8,49 @@ const labels = [
 
   ...OTHER_TRIGGER_TYPES.map((t) => t.defaultLabel),
 
-  getTriggerStepName({ type: 'CRON', settings: { /* schedule dummy */ type: 'DAYS', schedule: { day: 1, hour: 0, minute: 0 }, outputSchema: {} } }),
-  getTriggerStepName({ type: 'MANUAL', settings: { objectType: 'company', outputSchema: {}, icon: 'IconHandMove' } }),
-  getTriggerStepName({ type: 'WEBHOOK', settings: { httpMethod: 'GET', authentication: null, outputSchema: {} } }),
+  getTriggerStepName({
+    type: 'CRON',
+    settings: {
+      /* schedule dummy */ type: 'DAYS',
+      schedule: { day: 1, hour: 0, minute: 0 },
+      outputSchema: {},
+    },
+  }),
+  getTriggerStepName({
+    type: 'MANUAL',
+    settings: { objectType: 'company', outputSchema: {}, icon: 'IconHandMove' },
+  }),
+  getTriggerStepName({
+    type: 'WEBHOOK',
+    settings: { httpMethod: 'GET', authentication: null, outputSchema: {} },
+  }),
 
-  getTriggerDefaultLabel({ type: 'CRON', name: '', settings: { type: 'DAYS', schedule: { day: 1, hour: 0, minute: 0 }, outputSchema: {} } }),
-  getTriggerDefaultLabel({ type: 'MANUAL', name: '', settings: { objectType: 'company', outputSchema: {}, icon: 'IconHandMove' } }),
-  getTriggerDefaultLabel({ type: 'WEBHOOK', name: '', settings: { httpMethod: 'GET', authentication: null, outputSchema: {} } }),
-
+  getTriggerDefaultLabel({
+    type: 'CRON',
+    name: '',
+    settings: {
+      type: 'DAYS',
+      schedule: { day: 1, hour: 0, minute: 0 },
+      outputSchema: {},
+    },
+  }),
+  getTriggerDefaultLabel({
+    type: 'MANUAL',
+    name: '',
+    settings: { objectType: 'company', outputSchema: {}, icon: 'IconHandMove' },
+  }),
+  getTriggerDefaultLabel({
+    type: 'WEBHOOK',
+    name: '',
+    settings: { httpMethod: 'GET', authentication: null, outputSchema: {} },
+  }),
 ];
 
 describe('All UI labels use sentence case', () => {
-  it.each(labels)('"%s" should only have the first letter uppercase', (label) => {
-    expect(label).toMatch(/^[A-Z][^A-Z]*$/);
-  });
+  it.each(labels)(
+    '"%s" should only have the first letter uppercase',
+    (label) => {
+      expect(label).toMatch(/^[A-Z][^A-Z]*$/);
+    },
+  );
 });
