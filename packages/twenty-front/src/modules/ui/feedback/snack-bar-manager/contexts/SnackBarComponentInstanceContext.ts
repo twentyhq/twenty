efb417394cedfdb333461a44a157e7e63493c2bd
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-07-11 17:55:36 +0200

    Fixed lint

--- packages/twenty-front/src/modules/ui/feedback/snack-bar-manager/contexts/SnackBarComponentInstanceContext.ts
+++ packages/twenty-front/src/modules/ui/feedback/snack-bar-manager/contexts/SnackBarComponentInstanceContext.ts
@@ -1,3 +1,4 @@
 import { createComponentInstanceContext } from '@/ui/utilities/state/component-state/utils/createComponentInstanceContext';
 
-export const SnackBarComponentInstanceContext = createComponentInstanceContext();
+export const SnackBarComponentInstanceContext =
+  createComponentInstanceContext();
