997460d5f57b3c12439048310f9b1b3d520d667b
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-07-15 12:30:52 +0200

    Ok

--- packages/twenty-front/src/modules/keyboard-shortcut-menu/components/KeyboardShortcutMenu.tsx
+++ packages/twenty-front/src/modules/keyboard-shortcut-menu/components/KeyboardShortcutMenu.tsx
@@ -21,7 +21,7 @@ export const KeyboardShortcutMenu = () => {
       closeCommandMenu();
       toggleKeyboardShortcutMenu();
     },
-    true,
+    false,
     [toggleKeyboardShortcutMenu],
   );
 
