c3277ffecccd9f8480f10b27a10b0e78712a6781
neo773 <neo773@protonmail.com>
2025-07-15 20:40:30 +0530

    run twenty-front:graphql:generate

--- packages/twenty-front/src/generated/graphql.ts
+++ packages/twenty-front/src/generated/graphql.ts
@@ -1,5 +1,5 @@
-import * as Apollo from '@apollo/client';
 import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -440,6 +440,7 @@ export type ConnectionParametersOutput = {
   password: Scalars['String'];
   port: Scalars['Float'];
   secure?: Maybe<Scalars['Boolean']>;
+  username?: Maybe<Scalars['String']>;
 };
 
 export type CreateApiKeyDto = {
