9698ce1c2832fb1b93afc14820a704db061a3f71
martmull <martmull@hotmail.fr>
2025-07-21 15:41:38 +0200

    Add migration command

--- packages/twenty-server/src/database/commands/upgrade-version-command/1-2/1-2-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-2/1-2-upgrade-version-command.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { RemoveWorkflowRunsWithoutState } from 'src/database/commands/upgrade-version-command/1-2/1-2-remove-workflow-runs-without-state.command';
+import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 
 @Module({
-  imports: [],
-  providers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([Workspace], 'core')],
+  providers: [RemoveWorkflowRunsWithoutState],
+  exports: [RemoveWorkflowRunsWithoutState],
 })
 export class V1_2_UpgradeVersionCommandModule {}
