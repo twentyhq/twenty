593b7211a47fc325ff3e5267354e6ce49f1d09d0
Abdul Rahman <ar5438376@gmail.com>
2025-07-23 00:54:36 +0530

    Rename `settingPermissionRepository` to `permissionFlagRepository` for clarity in PermissionFlagService.

--- packages/twenty-server/src/engine/metadata-modules/permission-flag/permission-flag.service.ts
+++ packages/twenty-server/src/engine/metadata-modules/permission-flag/permission-flag.service.ts
@@ -17,7 +17,7 @@ import { WorkspacePermissionsCacheService } from 'src/engine/metadata-modules/wo
 export class PermissionFlagService {
   constructor(
     @InjectRepository(PermissionFlagEntity, 'core')
-    private readonly settingPermissionRepository: Repository<PermissionFlagEntity>,
+    private readonly permissionFlagRepository: Repository<PermissionFlagEntity>,
     @InjectRepository(RoleEntity, 'core')
     private readonly roleRepository: Repository<RoleEntity>,
     @InjectDataSource('core')
