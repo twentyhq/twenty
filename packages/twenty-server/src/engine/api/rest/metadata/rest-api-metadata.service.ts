4ad26824952043cd78b99c7c7124df2d3f75b22f
Weiko <corentin@twenty.com>
2025-07-22 15:54:20 +0200

    fix

--- packages/twenty-server/src/engine/api/rest/metadata/rest-api-metadata.service.ts
+++ packages/twenty-server/src/engine/api/rest/metadata/rest-api-metadata.service.ts
@@ -8,18 +8,21 @@ import {
   RestApiService,
 } from 'src/engine/api/rest/rest-api.service';
 import { RequestContext } from 'src/engine/api/rest/types/RequestContext';
+import { AccessTokenService } from 'src/engine/core-modules/auth/token/services/access-token.service';
 import { TwentyConfigService } from 'src/engine/core-modules/twenty-config/twenty-config.service';
 import { getServerUrl } from 'src/utils/get-server-url';
 
 @Injectable()
 export class RestApiMetadataService {
   constructor(
+    private readonly accessTokenService: AccessTokenService,
     private readonly metadataQueryBuilderFactory: MetadataQueryBuilderFactory,
     private readonly restApiService: RestApiService,
     private readonly twentyConfigService: TwentyConfigService,
   ) {}
 
   async get(request: Request) {
+    await this.accessTokenService.validateTokenByRequest(request);
     const requestContext = this.getRequestContext(request);
     const data = await this.metadataQueryBuilderFactory.get(requestContext);
 
@@ -31,6 +34,7 @@ export class RestApiMetadataService {
   }
 
   async create(request: Request) {
+    await this.accessTokenService.validateTokenByRequest(request);
     const requestContext = this.getRequestContext(request);
     const data = await this.metadataQueryBuilderFactory.create(requestContext);
 
@@ -42,6 +46,7 @@ export class RestApiMetadataService {
   }
 
   async update(request: Request) {
+    await this.accessTokenService.validateTokenByRequest(request);
     const requestContext = this.getRequestContext(request);
     const data = await this.metadataQueryBuilderFactory.update(requestContext);
 
@@ -53,6 +58,7 @@ export class RestApiMetadataService {
   }
 
   async delete(request: Request) {
+    await this.accessTokenService.validateTokenByRequest(request);
     const requestContext = this.getRequestContext(request);
     const data = await this.metadataQueryBuilderFactory.delete(requestContext);
 
