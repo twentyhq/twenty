557398f582c44c631f8f828cfa3f43e195e0a1a8
bosiraphael <raphael.bosi@gmail.com>
2025-07-22 15:59:41 +0200

    fix imports

--- packages/twenty-server/src/database/commands/database-command.module.ts
+++ packages/twenty-server/src/database/commands/database-command.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CronRegisterAllCommand } from 'src/database/commands/cron-register-all.command';
 import { ConfirmationQuestion } from 'src/database/commands/questions/confirmation.question';
@@ -8,9 +9,16 @@ import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 import { ApiKeyModule } from 'src/engine/core-modules/api-key/api-key.module';
 import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-flag.module';
 import { FileModule } from 'src/engine/core-modules/file/file.module';
+import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
 import { FieldMetadataModule } from 'src/engine/metadata-modules/field-metadata/field-metadata.module';
 import { ObjectMetadataModule } from 'src/engine/metadata-modules/object-metadata/object-metadata.module';
+import { ViewField } from 'src/engine/metadata-modules/view/view-field.entity';
+import { ViewFilterGroup } from 'src/engine/metadata-modules/view/view-filter-group.entity';
+import { ViewFilter } from 'src/engine/metadata-modules/view/view-filter.entity';
+import { ViewGroup } from 'src/engine/metadata-modules/view/view-group.entity';
+import { ViewSort } from 'src/engine/metadata-modules/view/view-sort.entity';
+import { View } from 'src/engine/metadata-modules/view/view.entity';
 import { WorkspaceCacheStorageModule } from 'src/engine/workspace-cache-storage/workspace-cache-storage.module';
 import { DevSeederModule } from 'src/engine/workspace-manager/dev-seeder/dev-seeder.module';
 import { WorkspaceManagerModule } from 'src/engine/workspace-manager/workspace-manager.module';
@@ -23,6 +31,19 @@ import { DataSeedWorkspaceCommand } from './data-seed-dev-workspace.command';
 @Module({
   imports: [
     UpgradeVersionCommandModule,
+
+    TypeOrmModule.forFeature(
+      [
+        Workspace,
+        View,
+        ViewField,
+        ViewFilter,
+        ViewSort,
+        ViewGroup,
+        ViewFilterGroup,
+      ],
+      'core',
+    ),
 
     // Cron command dependencies
     MessagingImportManagerModule,
