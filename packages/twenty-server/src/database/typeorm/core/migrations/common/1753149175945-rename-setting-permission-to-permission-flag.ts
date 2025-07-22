fef06003adbe87d3e3bb1a5291ff5f51a8842633
Abdul Rahman <ar5438376@gmail.com>
2025-07-22 07:27:52 +0530

    Implement migration to rename 'settingPermission' table to 'permissionFlag' and update associated index name

---
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1753149175945-rename-setting-permission-to-permission-flag.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class RenameSettingPermissionToPermissionFlag1753149175945
+  implements MigrationInterface
+{
+  name = 'RenameSettingPermissionToPermissionFlag1753149175945';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "core"."settingPermission" RENAME TO "permissionFlag"`,
+    );
+    await queryRunner.query(
+      `ALTER INDEX "core"."IDX_SETTING_PERMISSION_SETTING_ROLE_ID_UNIQUE" RENAME TO "IDX_PERMISSION_FLAG_PERMISSION_FLAG_ROLE_ID_UNIQUE"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "core"."permissionFlag" RENAME TO "settingPermission"`,
+    );
+    await queryRunner.query(
+      `ALTER INDEX "core"."IDX_PERMISSION_FLAG_PERMISSION_FLAG_ROLE_ID_UNIQUE" RENAME TO "IDX_SETTING_PERMISSION_SETTING_ROLE_ID_UNIQUE"`,
+    );
+  }
+}
