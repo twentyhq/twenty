3e8c22a773ff0dd0ebc06a1a8c93e13b4daeaad9
Antoine Moreaux <moreaux.antoine@gmail.com>
2025-07-22 16:44:51 +0200

    feat(ai): add context to ai chat

---
+++ packages/twenty-ui/src/components/avatar-chip/__stories__/AvatarChip.stories.tsx
@@ -0,0 +1,81 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { ComponentDecorator } from '@ui/testing';
+import { IconBuildingSkyscraper, IconUser } from '@ui/display';
+import { AvatarChip } from '../AvatarChip';
+import { AvatarType } from '@ui/display/avatar/types/AvatarType';
+
+const meta: Meta<typeof AvatarChip> = {
+  title: 'UI/Components/AvatarChip',
+  component: AvatarChip,
+  decorators: [ComponentDecorator],
+};
+
+export default meta;
+type Story = StoryObj<typeof AvatarChip>;
+
+export const Default: Story = {
+  args: {
+    placeholder: 'JD',
+    placeholderColorSeed: 'John Doe',
+  },
+};
+
+export const WithAvatar: Story = {
+  args: {
+    avatarUrl: 'https://i.pravatar.cc/300',
+    placeholder: 'JD',
+    placeholderColorSeed: 'John Doe',
+    avatarType: AvatarType.Person,
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    Icon: IconUser,
+  },
+};
+
+export const WithIconBackground: Story = {
+  args: {
+    Icon: IconBuildingSkyscraper,
+    isIconInverted: true,
+  },
+};
+
+export const WithInvertedIcon: Story = {
+  args: {
+    Icon: IconUser,
+    isIconInverted: true,
+  },
+};
+
+export const WithRightDivider: Story = {
+  args: {
+    placeholder: 'JD',
+    placeholderColorSeed: 'John Doe',
+    divider: 'right',
+  },
+};
+
+export const WithLeftDivider: Story = {
+  args: {
+    Icon: IconUser,
+    divider: 'left',
+  },
+};
+
+export const Clickable: Story = {
+  args: {
+    placeholder: 'JD',
+    placeholderColorSeed: 'John Doe',
+    onClick: () => alert('AvatarChip clicked'),
+  },
+};
+
+export const ClickableIcon: Story = {
+  args: {
+    Icon: IconBuildingSkyscraper,
+    isIconInverted: true,
+    onClick: () => alert('Icon AvatarChip clicked'),
+  },
+};
