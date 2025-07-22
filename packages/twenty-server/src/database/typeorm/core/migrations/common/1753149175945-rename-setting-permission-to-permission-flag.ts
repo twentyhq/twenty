21f72394492fc1a8d441d7a08db82c92f791c383
Abdul Rahman <ar5438376@gmail.com>
2025-07-22 08:10:07 +0530

    Update migration to rename 'settingPermission' to 'permissionFlag' and adjust related index and column names for consistency in permission handling

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1753149175945-rename-setting-permission-to-permission-flag.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1753149175945-rename-setting-permission-to-permission-flag.ts
@@ -7,19 +7,25 @@ export class RenameSettingPermissionToPermissionFlag1753149175945
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "core"."settingPermission" RENAME TO "flag"`,
+      `ALTER TABLE "core"."settingPermission" RENAME TO "permissionFlag"`,
     );
+
     await queryRunner.query(
-      `ALTER INDEX "core"."IDX_SETTING_PERMISSION_SETTING_ROLE_ID_UNIQUE" RENAME TO "IDX_PERMISSION_FLAG_PERMISSION_FLAG_ROLE_ID_UNIQUE"`,
+      `ALTER TABLE "core"."permissionFlag" RENAME COLUMN "setting" TO "flag"`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "core"."permissionFlag" RENAME CONSTRAINT "IDX_SETTING_PERMISSION_SETTING_ROLE_ID_UNIQUE" TO "IDX_SETTING_PERMISSION_SETTING_ROLE_ID_UNIQUE"`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "core"."flag" RENAME TO "settingPermission"`,
+      `ALTER TABLE "core"."permissionFlag" RENAME COLUMN "flag" TO "setting"`,
     );
+
     await queryRunner.query(
-      `ALTER INDEX "core"."IDX_PERMISSION_FLAG_PERMISSION_FLAG_ROLE_ID_UNIQUE" RENAME TO "IDX_SETTING_PERMISSION_SETTING_ROLE_ID_UNIQUE"`,
+      `ALTER TABLE "core"."permissionFlag" RENAME TO "settingPermission"`,
     );
   }
 }
