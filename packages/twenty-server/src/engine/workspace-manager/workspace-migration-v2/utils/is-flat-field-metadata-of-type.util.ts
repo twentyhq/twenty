73322a04f13f3f9c397047fdef6abf2ab1be13b3
prastoin <paul@twenty.com>
2025-07-23 15:48:15 +0200

    refactor(server): assign type if is relation type

---
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/utils/is-flat-field-metadata-of-type.util.ts
@@ -0,0 +1,12 @@
+import { FlatFieldMetadata } from 'src/engine/workspace-manager/workspace-migration-v2/types/flat-field-metadata';
+import { FieldMetadataType } from 'twenty-shared/types';
+
+export function isFlatFieldMetadataEntityOfType<
+  Field extends FlatFieldMetadata<FieldMetadataType>,
+  Type extends FieldMetadataType,
+>(
+  fieldMetadata: Pick<Field, 'type'>,
+  type: Type,
+): fieldMetadata is Field & FlatFieldMetadata<Type> {
+  return fieldMetadata.type === type;
+}
