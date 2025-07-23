602e446337a48b1bdf0a64efbb939fd69f4d2162
Guillim <guillim@users.noreply.github.com>
2025-07-23 13:30:08 +0200

    Fix: Infinite "ONGOING" Status in Messaging Message List Fetch (#13370)
    ## Problem
    Previously, the newly created "message list fetch process" left the
    message channel in an "ONGOING" status indefinitely.
    
    ## Why
    Before only "fullSync" messageChannel were concerned by this method.
    What happened it sometimes the providers gave empty messageExternalIds
    during the initial fetch. Happens for a newly created email account on
    gmail or microsoft (not sure which one)
    
    Now that we gather both of sync methods (partial and full) we cannot use
    this anly longer. Because after the first sync, if no new messages
    arrived in the last 5 minutes, there will be none, so it was consiedered
    as emptyMailbox (which is wrong)
    
    ## Solution
    Removed this logic from the messageChannel since now we will rely on
    each messageFolder (messageList) to handle the logic of going for a full
    or not sync.
    
    ## Question
    We might see some bugs in case some newly created email account without
    messageList in case the provider does not give a nextSyncCursor at the
    messageList level. Not easy to test
    
    ## Bonus
    We also cleant a useless service method called getCursor that was not
    used anywhere.
    
    ---------
    
    Co-authored-by: prastoin <paul@twenty.com>

--- packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-message-list-fetch.service.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-message-list-fetch.service.ts
@@ -43,20 +43,11 @@ export class MessagingMessageListFetchService {
           messageChannel,
         );
 
-      const isEmptyMailbox = messageLists.some(
-        (messageList) => messageList.messageExternalIds.length === 0,
-      );
-
-      if (isEmptyMailbox) {
-        await this.messageChannelSyncStatusService.resetAndScheduleMessageListFetch(
-          [messageChannel.id],
-          workspaceId,
-        );
-
-        return;
-      }
-
       for (const messageList of messageLists) {
+        if (messageList.messageExternalIds.length === 0) {
+          continue;
+        }
+
         const {
           messageExternalIds,
           nextSyncCursor,
