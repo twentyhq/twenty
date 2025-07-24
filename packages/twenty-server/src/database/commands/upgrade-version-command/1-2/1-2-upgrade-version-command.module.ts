b331656122f2cdde40f31d8fef50c3aaf6350c01
martmull <martmull@hotmail.fr>
2025-07-24 15:40:21 +0200

    Add nextStepIds to trigger

--- packages/twenty-server/src/database/commands/upgrade-version-command/1-2/1-2-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-2/1-2-upgrade-version-command.module.ts
@@ -3,10 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RemoveWorkflowRunsWithoutState } from 'src/database/commands/upgrade-version-command/1-2/1-2-remove-workflow-runs-without-state.command';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import { AddNextStepIdsToWorkflowVersionTriggers } from 'src/database/commands/upgrade-version-command/1-2/1-2-add-next-step-ids-to-workflow-version-triggers.command';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Workspace], 'core')],
-  providers: [RemoveWorkflowRunsWithoutState],
-  exports: [RemoveWorkflowRunsWithoutState],
+  providers: [
+    RemoveWorkflowRunsWithoutState,
+    AddNextStepIdsToWorkflowVersionTriggers,
+  ],
+  exports: [
+    RemoveWorkflowRunsWithoutState,
+    AddNextStepIdsToWorkflowVersionTriggers,
+  ],
 })
 export class V1_2_UpgradeVersionCommandModule {}
