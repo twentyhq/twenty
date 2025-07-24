3c6697eeeef7960c838b15bc7fe1ab8429bb80b4
prastoin <paul@twenty.com>
2025-07-24 16:29:48 +0200

    feat(server): object metadata create refactor api metadata wip

--- packages/twenty-server/src/engine/metadata-modules/object-metadata/dtos/create-object.input.ts
+++ packages/twenty-server/src/engine/metadata-modules/object-metadata/dtos/create-object.input.ts
@@ -9,6 +9,10 @@ import { FieldMetadataSettings } from 'src/engine/metadata-modules/field-metadat
 
 import { IsValidMetadataName } from 'src/engine/decorators/metadata/is-valid-metadata-name.decorator';
 import { BeforeCreateOneObject } from 'src/engine/metadata-modules/object-metadata/hooks/before-create-one-object.hook';
+import { buildDefaultFlatFieldMetadataForCustomObject } from 'src/engine/metadata-modules/object-metadata/utils/build-default-fields-for-custom-object.util';
+import { FlatObjectMetadata } from 'src/engine/workspace-manager/workspace-migration-v2/types/flat-object-metadata';
+import { trimAndRemoveDuplicatedWhitespacesFromObjectStringProperties } from 'src/utils/trim-and-remove-duplicated-whitespaces-from-object-string-properties';
+import { v4 } from 'uuid';
 
 @InputType()
 @BeforeCreateOne(BeforeCreateOneObject)
@@ -71,6 +75,68 @@ export class CreateObjectInput {
 
   @IsBoolean()
   @IsOptional()
-  @Field({ nullable: true })
+  @Field({ nullable: true }) // Not nullable to me
   isLabelSyncedWithName?: boolean;
 }
+
+export const fromCreateObjectInputToFlatObjectMetadata = (
+  rawCreateObjectInput: CreateObjectInput,
+): FlatObjectMetadata => {
+  if (rawCreateObjectInput.isRemote) {
+    throw new Error('Remote objects are not supported yet');
+  }
+
+  const createObjectInput =
+    trimAndRemoveDuplicatedWhitespacesFromObjectStringProperties(
+      rawCreateObjectInput,
+      [
+        'description',
+        'icon',
+        'labelPlural',
+        'labelSingular',
+        'namePlural',
+        'nameSingular',
+        'shortcut',
+      ],
+    );
+
+  const objectMetadataId = v4();
+  const createdAt = new Date();
+  const baseCustomFlatFieldMetadatas =
+    buildDefaultFlatFieldMetadataForCustomObject({
+      createdAt,
+      objectMetadataId,
+      workspaceId: createObjectInput.workspaceId,
+    });
+
+  return {
+    createdAt,
+    updatedAt: createdAt,
+    dataSourceId: '', // TODO
+    description: createObjectInput.description ?? null,
+    duplicateCriteria: [], // IDK
+    flatFieldMetadatas: Object.values(baseCustomFlatFieldMetadatas),
+    flatIndexMetadatas: [], // IDK
+    icon: createObjectInput.icon ?? null,
+    id: objectMetadataId,
+    imageIdentifierFieldMetadataId: null,
+    isActive: true,
+    isAuditLogged: true,
+    isCustom: true,
+    isLabelSyncedWithName: createObjectInput.isLabelSyncedWithName ?? false,
+    isRemote: false,
+    isSearchable: true,
+    isSystem: false,
+    labelIdentifierFieldMetadataId: baseCustomFlatFieldMetadatas.nameField.id,
+    labelPlural: createObjectInput.labelPlural ?? null,
+    labelSingular: createObjectInput.labelSingular ?? null,
+    namePlural: createObjectInput.namePlural ?? null,
+    nameSingular: createObjectInput.nameSingular ?? null,
+    shortcut: createObjectInput.shortcut ?? null,
+    standardId: null,
+    standardOverrides: null,
+    uniqueIdentifier: objectMetadataId,
+    targetTableName: 'DEPRECATED',
+    workspaceId: createObjectInput.workspaceId,
+  };
+};
