d593f4cafa9ff7f5ddcea3042a6e1be0a62c9800
prastoin <paul@twenty.com>
2025-07-25 14:47:08 +0200

    feat(server): flat field metadata validation first lines

---
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/services/flat-field-metadata-validator.service.ts
@@ -0,0 +1,139 @@
+import { Injectable } from '@nestjs/common';
+import { FeatureFlagKey } from 'src/engine/core-modules/feature-flag/enums/feature-flag-key.enum';
+import { FeatureFlagService } from 'src/engine/core-modules/feature-flag/services/feature-flag.service';
+import {
+    FieldMetadataException,
+    FieldMetadataExceptionCode,
+} from 'src/engine/metadata-modules/field-metadata/field-metadata.exception';
+import { FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
+import { Expect } from 'twenty-shared/testing';
+import { FieldMetadataType } from 'twenty-shared/types';
+import { isDefined } from 'twenty-shared/utils';
+
+// What about update ? FromTo<> ?
+type ValidateOneFieldMetadataArgs = {
+  existingFlatObjectMetadata: FlatObjectMetadata[];
+  othersFlatObjectMetadataToValidate?: FlatObjectMetadata[];
+  flatFieldMetadataToValidate: FlatFieldMetadata;
+  // flatFieldMetadataToValidateRelatedFlatObjectMetadata lol
+  workspaceId: string;
+};
+
+type FailedFlatFieldMetadataValidation = {
+  status: 'fail';
+  error: FieldMetadataException;
+};
+
+type SuccessfulFlatFieldMetadataValidation = {
+  status: 'success';
+};
+
+type FlatFieldMetadataValidationResult =
+  | FailedFlatFieldMetadataValidation
+  | SuccessfulFlatFieldMetadataValidation;
+
+// We need to infer if this is going to be an udpate or not
+
+@Injectable()
+export class FlatFieldMetadataValidatorService {
+  constructor(private readonly featureFlagService: FeatureFlagService) {}
+
+  // This should return a status but never throw directly ?
+  async validateOneFlatFieldMetadata({
+    existingFlatObjectMetadata,
+    flatFieldMetadataToValidate,
+    othersFlatObjectMetadataToValidate,
+    workspaceId,
+  }: ValidateOneFieldMetadataArgs): Promise<FlatFieldMetadataValidationResult> {
+    const allFlatObjectMetadata = [
+      ...existingFlatObjectMetadata,
+      ...(othersFlatObjectMetadataToValidate ?? []),
+    ];
+    // Common TODO facto in own scope
+    const parentFlatObjectMetadata = allFlatObjectMetadata.find(
+      (existingFlatObjectMetadata) =>
+        // Should this be unique identifier ?? I'm confused :thinking:
+        existingFlatObjectMetadata.id ===
+        flatFieldMetadataToValidate.objectMetadataId,
+    );
+
+    if (!isDefined(parentFlatObjectMetadata)) {
+      return {
+        status: 'fail',
+        error: new FieldMetadataException(
+          isDefined(othersFlatObjectMetadataToValidate)
+            ? 'Object metadata does not exist in both existing and about to be created object metadatass'
+            : 'Object metadata does not exist',
+          FieldMetadataExceptionCode.OBJECT_METADATA_NOT_FOUND,
+        ),
+      };
+    }
+    /// End of common
+
+    switch (flatFieldMetadataToValidate.type) {
+      case FieldMetadataType.UUID:
+      case FieldMetadataType.TEXT:
+      case FieldMetadataType.PHONES:
+      case FieldMetadataType.EMAILS:
+      case FieldMetadataType.DATE_TIME:
+      case FieldMetadataType.DATE:
+      case FieldMetadataType.BOOLEAN:
+      case FieldMetadataType.NUMBER:
+      case FieldMetadataType.NUMERIC:
+      case FieldMetadataType.LINKS:
+      case FieldMetadataType.CURRENCY:
+      case FieldMetadataType.FULL_NAME:
+      case FieldMetadataType.RATING:
+      case FieldMetadataType.SELECT:
+      case FieldMetadataType.MULTI_SELECT:
+      case FieldMetadataType.RELATION:
+      case FieldMetadataType.MORPH_RELATION: {
+        // Facto in dedicated service
+        const isMorphRelationEnabled =
+          await this.featureFlagService.isFeatureEnabled(
+            FeatureFlagKey.IS_MORPH_RELATION_ENABLED,
+            workspaceId,
+          );
+
+        if (!isMorphRelationEnabled) {
+          return {
+            status: 'fail',
+            error: new FieldMetadataException(
+              'Object metadata does not exist',
+              FieldMetadataExceptionCode.OBJECT_METADATA_NOT_FOUND,
+            ),
+          };
+        }
+        ///
+      }
+      case FieldMetadataType.POSITION:
+      case FieldMetadataType.ADDRESS:
+      case FieldMetadataType.RAW_JSON:
+      case FieldMetadataType.RICH_TEXT:
+      case FieldMetadataType.RICH_TEXT_V2:
+      case FieldMetadataType.ACTOR:
+      case FieldMetadataType.ARRAY:
+      case FieldMetadataType.TS_VECTOR: {
+        break;
+      }
+      default: {
+        const _staticTypeCheck: Expect<
+          typeof flatFieldMetadataToValidate.type extends never ? true : false
+        > = true;
+
+        return {
+          status: 'fail',
+          error: new FieldMetadataException(
+            'Object metadata does not exist',
+            FieldMetadataExceptionCode.UNCOVERED_FIELD_METADATA_TYPE_VALIDATION,
+          ),
+        };
+      }
+    }
+
+    return {
+      status: 'success',
+    };
+  }
+}
