ccd1b54ae297831b2ef4b7c82ea3bcc81a49181a
martmull <martmull@hotmail.fr>
2025-07-28 23:10:51 +0200

    Fix tidyUp

--- packages/twenty-front/src/modules/workflow/workflow-version/hooks/useTidyUpWorkflowVersion.ts
+++ packages/twenty-front/src/modules/workflow/workflow-version/hooks/useTidyUpWorkflowVersion.ts
@@ -1,13 +1,23 @@
-import { WorkflowWithCurrentVersion } from '@/workflow/types/Workflow';
+import {
+  WorkflowVersion,
+  WorkflowWithCurrentVersion,
+} from '@/workflow/types/Workflow';
 import { useGetUpdatableWorkflowVersion } from '@/workflow/hooks/useGetUpdatableWorkflowVersion';
 import { isDefined } from 'twenty-shared/utils';
 import { useMutation } from '@apollo/client';
 import {
   UpdateDraftWorkflowVersionPositionsMutation,
   UpdateDraftWorkflowVersionPositionsMutationVariables,
+  WorkflowAction,
 } from '~/generated-metadata/graphql';
 import { UPDATE_DRAFT_WORKFLOW_VERSION_POSITIONS } from '@/workflow/workflow-version/graphql/mutations/updateDraftWorkflowVersionPositions';
 import { useApolloCoreClient } from '@/object-metadata/hooks/useApolloCoreClient';
+import { updateRecordFromCache } from '@/object-record/cache/utils/updateRecordFromCache';
+import { useObjectMetadataItems } from '@/object-metadata/hooks/useObjectMetadataItems';
+import { useObjectPermissions } from '@/object-record/hooks/useObjectPermissions';
+import { useObjectMetadataItem } from '@/object-metadata/hooks/useObjectMetadataItem';
+import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
+import { useGetRecordFromCache } from '@/object-record/cache/hooks/useGetRecordFromCache';
 
 export const useTidyUpWorkflowVersion = ({
   workflow,
@@ -15,6 +25,16 @@ export const useTidyUpWorkflowVersion = ({
   workflow?: WorkflowWithCurrentVersion;
 }) => {
   const apolloCoreClient = useApolloCoreClient();
+
+  const { objectMetadataItems } = useObjectMetadataItems();
+  const { objectPermissionsByObjectMetadataId } = useObjectPermissions();
+
+  const { objectMetadataItem } = useObjectMetadataItem({
+    objectNameSingular: CoreObjectNameSingular.WorkflowVersion,
+  });
+  const getRecordFromCache = useGetRecordFromCache({
+    objectNameSingular: CoreObjectNameSingular.WorkflowVersion,
+  });
 
   const [mutate] = useMutation<
     UpdateDraftWorkflowVersionPositionsMutation,
@@ -33,6 +53,50 @@ export const useTidyUpWorkflowVersion = ({
     const workflowVersionId = await getUpdatableWorkflowVersion(workflow);
 
     await mutate({ variables: { input: { workflowVersionId, positions } } });
+
+    const cachedRecord = getRecordFromCache<WorkflowVersion>(workflowVersionId);
+
+    if (!isDefined(cachedRecord)) {
+      return;
+    }
+
+    const triggerPosition = positions.find(
+      (position) => position.id === 'trigger',
+    );
+
+    const updatedTrigger = isDefined(triggerPosition)
+      ? { ...cachedRecord.trigger, position: triggerPosition.position }
+      : cachedRecord.trigger;
+
+    const updatedSteps = cachedRecord.steps?.map((step: WorkflowAction) => {
+      const stepPosition = positions.find(
+        (position) => position.id === step.id,
+      );
+      if (!isDefined(stepPosition)) {
+        return step;
+      }
+      return { ...step, position: stepPosition.position };
+    });
+
+    const newCachedRecord = {
+      ...cachedRecord,
+      trigger: updatedTrigger,
+      steps: updatedSteps,
+    };
+
+    const recordGqlFields = {
+      trigger: true,
+      steps: true,
+    };
+
+    updateRecordFromCache({
+      objectMetadataItems,
+      objectMetadataItem,
+      cache: apolloCoreClient.cache,
+      record: newCachedRecord,
+      recordGqlFields,
+      objectPermissionsByObjectMetadataId,
+    });
   };
 
   return { tidyUpWorkflowVersion };
