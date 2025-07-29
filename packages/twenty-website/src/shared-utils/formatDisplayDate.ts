7e283437d4e485bc77bf7b75ef0e30211ff0fc55
Félix Malfait <felix@twenty.com>
2025-07-29 15:29:31 +0200

    Lint

--- packages/twenty-website/src/shared-utils/formatDisplayDate.ts
+++ packages/twenty-website/src/shared-utils/formatDisplayDate.ts
@@ -3,12 +3,12 @@ export const formatGithubPublishedAtDisplayDate = (
 ): string => {
   const date = new Date(dateString);
   const currentYear = new Date().getFullYear();
-  
+
   const formatter = new Intl.DateTimeFormat('en-US', {
     month: 'short',
     day: 'numeric',
     ...(date.getFullYear() !== currentYear && { year: 'numeric' }),
   });
-  
+
   return formatter.format(date);
 };
