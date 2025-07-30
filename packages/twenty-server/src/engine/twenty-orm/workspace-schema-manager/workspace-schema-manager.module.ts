bd99b3b68a04a839b1e62ccc870cacf93695c815
Weiko <corentin@twenty.com>
2025-07-30 18:42:22 +0200

    fix

--- packages/twenty-server/src/engine/twenty-orm/workspace-schema-manager/workspace-schema-manager.module.ts
+++ packages/twenty-server/src/engine/twenty-orm/workspace-schema-manager/workspace-schema-manager.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import {
-  WorkspaceSchemaColumnManagerService,
-  WorkspaceSchemaEnumManagerService,
-  WorkspaceSchemaForeignKeyManagerService,
-  WorkspaceSchemaIndexManagerService,
-  WorkspaceSchemaTableManagerService,
-} from './services';
+import { WorkspaceSchemaColumnManagerService } from 'src/engine/twenty-orm/workspace-schema-manager/services/workspace-schema-column-manager.service';
+import { WorkspaceSchemaEnumManagerService } from 'src/engine/twenty-orm/workspace-schema-manager/services/workspace-schema-enum-manager.service';
+import { WorkspaceSchemaForeignKeyManagerService } from 'src/engine/twenty-orm/workspace-schema-manager/services/workspace-schema-foreign-key-manager.service';
+import { WorkspaceSchemaIndexManagerService } from 'src/engine/twenty-orm/workspace-schema-manager/services/workspace-schema-index-manager.service';
+import { WorkspaceSchemaTableManagerService } from 'src/engine/twenty-orm/workspace-schema-manager/services/workspace-schema-table-manager.service';
+
 import { WorkspaceSchemaManagerService } from './workspace-schema-manager.service';
 
 @Module({
