b96f034cb74f0416d4eb5ccb7a8ea964e3f1d83d
prastoin <paul@twenty.com>
2025-07-30 17:35:31 +0200

    feat(server): handle flat field metadata validation enum

--- packages/twenty-shared/src/types/index.ts
+++ packages/twenty-shared/src/types/index.ts
@@ -12,6 +12,7 @@ export { ConnectedAccountProvider } from './ConnectedAccountProvider';
 export { FieldMetadataType } from './FieldMetadataType';
 export type { FromTo } from './FromToType';
 export type { IsExactly } from './IsExactly';
+export type { NonNullableRequired } from './NonNullableRequired';
 export type { NullablePartial } from './NullablePartial';
 export type { ObjectRecordsPermissions } from './ObjectRecordsPermissions';
 export type { ObjectRecordsPermissionsByRoleId } from './ObjectRecordsPermissionsByRoleId';
