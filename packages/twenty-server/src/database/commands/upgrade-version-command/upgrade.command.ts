2d0a985f278f64ce6c448e7171955bbc5f94eba0
martmull <martmull@hotmail.fr>
2025-07-31 16:00:32 +0200

    Add again 1.2 migration in 1.3

--- packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
@@ -216,7 +216,9 @@ export class UpgradeCommand extends UpgradeCommandRunner {
     };
 
     const commands_130: VersionCommands = {
-      beforeSyncMetadata: [],
+      beforeSyncMetadata: [
+        this.addNextStepIdsToWorkflowVersionTriggers, // We add that command again because nextStepIds where not added on freshly created triggers. It will be done in 1.3
+      ],
       afterSyncMetadata: [],
     };
 
