da1049e3f6d56e30946d8b0b06c26a6d14a8d23a
prastoin <paul@twenty.com>
2025-07-31 10:32:48 +0200

    review(server):greptile 1

---
+++ packages/twenty-shared/src/types/EnumFieldMetadataType.ts
@@ -0,0 +1,6 @@
+import { FieldMetadataType } from "@/types/FieldMetadataType";
+
+export type EnumFieldMetadataType =
+  | FieldMetadataType.RATING
+  | FieldMetadataType.SELECT
+  | FieldMetadataType.MULTI_SELECT;
