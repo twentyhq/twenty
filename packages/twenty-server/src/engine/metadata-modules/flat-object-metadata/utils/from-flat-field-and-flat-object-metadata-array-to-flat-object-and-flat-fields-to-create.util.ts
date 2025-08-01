3a5ded3289565fe0ce1789f51ca511c0cfc3601a
prastoin <paul@twenty.com>
2025-08-01 19:40:33 +0200

    nitpicks(server): naming and stuff

--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/from-flat-field-and-flat-object-metadata-array-to-flat-object-and-flat-fields-to-create.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/from-flat-field-and-flat-object-metadata-array-to-flat-object-and-flat-fields-to-create.util.ts
@@ -1,41 +1,52 @@
 import { FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
 import { FlatFieldAndFlatObjectMetadata } from 'src/engine/metadata-modules/flat-field-metadata/utils/from-create-field-input-to-flat-field-and-its-flat-object-metadata.util';
+import { mergeTwoFlatFieldMetadatas } from 'src/engine/metadata-modules/flat-field-metadata/utils/merge-two-flat-field-metadatas.util';
 import { FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
 
 export type FlatObjectAndFlatFieldsToCreate = {
   flatObjectMetadata: FlatObjectMetadata;
   toCreateFlatFieldMetadata: FlatFieldMetadata[];
 };
+
+type FlatObjectAndFlatFieldsToCreateByFlatObjectId = Record<
+  string,
+  FlatObjectAndFlatFieldsToCreate
+>;
+
 export const fromFlatFieldAndFlatObjectMetadataArrayToFlatObjectAndFlatFieldsToCreate =
   (
     flatFieldAndFLatObjectArray: FlatFieldAndFlatObjectMetadata[],
   ): FlatObjectAndFlatFieldsToCreate[] => {
-    const record = flatFieldAndFLatObjectArray.reduce<
-      Record<string, FlatObjectAndFlatFieldsToCreate>
-    >((acc, { flatFieldMetadata, parentFlatObjectMetadata }) => {
-      const occurrence = acc[parentFlatObjectMetadata.id];
+    const initialAccumulator: FlatObjectAndFlatFieldsToCreateByFlatObjectId =
+      {};
+    const flatObjectAndFlatFieldsToCreateByFlatObjectId =
+      flatFieldAndFLatObjectArray.reduce(
+        (acc, { flatFieldMetadata, parentFlatObjectMetadata }) => {
+          const occurrence = acc[parentFlatObjectMetadata.id];
 
-      if (occurrence) {
-        return {
-          ...acc,
-          [parentFlatObjectMetadata.id]: {
-            ...occurrence,
-            toCreateFlatFieldMetadata: [
-              ...occurrence.toCreateFlatFieldMetadata,
-              flatFieldMetadata,
-            ],
-          },
-        };
-      }
+          if (occurrence) {
+            return {
+              ...acc,
+              [parentFlatObjectMetadata.id]: {
+                ...occurrence,
+                toCreateFlatFieldMetadata: mergeTwoFlatFieldMetadatas({
+                  destFlatFieldMetadatas: occurrence.toCreateFlatFieldMetadata,
+                  toMergeFlatFieldMetadatas: [flatFieldMetadata],
+                }),
+              },
+            };
+          }
 
-      return {
-        ...acc,
-        [parentFlatObjectMetadata.id]: {
-          flatObjectMetadata: parentFlatObjectMetadata,
-          toCreateFlatFieldMetadata: [flatFieldMetadata],
+          return {
+            ...acc,
+            [parentFlatObjectMetadata.id]: {
+              flatObjectMetadata: parentFlatObjectMetadata,
+              toCreateFlatFieldMetadata: [flatFieldMetadata],
+            },
+          };
         },
-      };
-    }, {});
+        initialAccumulator,
+      );
 
-    return Object.values(record);
+    return Object.values(flatObjectAndFlatFieldsToCreateByFlatObjectId);
   };
