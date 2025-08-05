3d7e4db6962effbba98633a443c02b4c10cdcbbb
prastoin <paul@twenty.com>
2025-08-05 17:50:24 +0200

    feat(server): from flat object metadatas to flat object metadata maps

--- packages/twenty-server/src/codegen/flat-object-metadata-maps.mock.ts
+++ packages/twenty-server/src/codegen/flat-object-metadata-maps.mock.ts
@@ -1,17 +1,7 @@
 import { ALL_FLAT_OBJECT_METADATA_MOCKS } from 'src/codegen/all-flat-object-metadatas.mock';
-import { FlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/types/flat-object-metadata-maps.type';
-import { addFlatObjectMetadataToFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/add-flat-object-metadata-to-flat-object-metadata-maps.util';
+import { fromFlatObjectMetadatasToFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata/utils/from-flat-object-metadatas-to-flat-object-metadata-maps.util';
 
-const emptyFlatObjectMetadataMaps: FlatObjectMetadataMaps = {
-  byId: {},
-  idByNameSingular: {},
-};
-export const FLAT_OBJECT_METADATA_MAPS_MOCKS: FlatObjectMetadataMaps =
-  ALL_FLAT_OBJECT_METADATA_MOCKS.reduce(
-    (flatObjectMetadataMaps, flatObjectMetadata) =>
-      addFlatObjectMetadataToFlatObjectMetadataMaps({
-        flatObjectMetadata,
-        flatObjectMetadataMaps,
-      }),
-    emptyFlatObjectMetadataMaps,
-  );
+export const FLAT_OBJECT_METADATA_MAPS_MOCKS =
+  fromFlatObjectMetadatasToFlatObjectMetadataMaps([
+    ...ALL_FLAT_OBJECT_METADATA_MOCKS,
+  ]);
