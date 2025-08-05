12c83f0f39764bfbd7cce86b635d75b67c3ec866
prastoin <paul@twenty.com>
2025-08-05 10:49:38 +0200

    feat(server): handle create object

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-optimistic-migration-runner.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-optimistic-migration-runner.ts
@@ -1,6 +1,7 @@
 import { assertUnreachable } from 'twenty-shared/utils';
 
 import { FlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/types/flat-object-metadata-maps.type';
+import { addFlatObjectMetadataToFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/add-flat-object-metadata-to-flat-object-metadata-maps.util';
 import { deleteFieldFromFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/delete-field-from-flat-object-metadata-maps.util';
 import { deleteObjectFromFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/delete-object-from-flat-object-metadata-maps.util';
 import { WorkspaceMigrationRunnerArgs } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/types/workspace-migration-runner-args.type';
@@ -20,7 +21,19 @@ export const runWorkspaceMigrationActionOptimistically = ({
       });
     }
     case 'create_object': {
-      return flatObjectMetadataMaps;
+      const flatObjectMetadataWithoutFields =
+        action.flatObjectMetadataWithoutFields;
+      const flatFieldMetadatas = action.createFieldActions.map(
+        (createFieldAction) => createFieldAction.flatFieldMetadata,
+      );
+      return addFlatObjectMetadataToFlatObjectMetadataMaps({
+        flatObjectMetadata: {
+          ...flatObjectMetadataWithoutFields,
+          flatIndexMetadatas: [], // TODO prastoin handle indexes ? could this be a problem here ?
+          flatFieldMetadatas,
+        },
+        flatObjectMetadataMaps,
+      });
     }
     case 'update_object': {
       return flatObjectMetadataMaps;
