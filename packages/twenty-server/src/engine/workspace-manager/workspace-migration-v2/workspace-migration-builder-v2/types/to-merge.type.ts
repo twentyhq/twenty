d7baf921eb6a1cb52d7d55c421284c2b0873cc16
prastoin <paul@twenty.com>
2025-08-06 14:48:37 +0200

    chore: simplify types

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/to-merge.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/to-merge.type.ts
@@ -1,3 +1,3 @@
 export type ToMerge<T, K extends string> = {
-  [P in ['toMerge', 'dest'][number] as `${P}${Capitalize<K>}`]: T;
+  [P in 'toMerge' | 'dest' as `${P}${Capitalize<K>}`]: T;
 };
--- packages/twenty-shared/src/types/FromToType.ts
+++ packages/twenty-shared/src/types/FromToType.ts
@@ -1,3 +1,3 @@
 export type FromTo<T, K extends string = ''> = {
-  [P in ['from', 'to'][number] as `${P}${Capitalize<K>}`]: T;
+  [P in 'from' | 'to' as `${P}${Capitalize<K>}`]: T;
 };
