d42b0e3e148186377489331b563b5b1e928e7bc5
prastoin <paul@twenty.com>
2025-08-06 10:29:10 +0200

    test(server): dispatch and add flat field metadata

--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata-maps/utils/__tests__/dispatch-and-add-flat-field-metadata-in-flat-object-metadata-maps.spec.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata-maps/utils/__tests__/dispatch-and-add-flat-field-metadata-in-flat-object-metadata-maps.spec.ts
@@ -5,24 +5,56 @@ import {
 import { FieldMetadataType } from 'twenty-shared/types';
 
 import { FLAT_OBJECT_METADATA_MAPS_MOCKS } from 'src/codegen/flat-object-metadata-maps.mock';
+import { PET_FLAT_OBJECT_MOCK } from 'src/codegen/pet-flat-object.mock';
+import { ROCKET_FLAT_OBJECT_MOCK } from 'src/codegen/rocket-flat-object.mock';
+import { RelationType } from 'src/engine/metadata-modules/field-metadata/interfaces/relation-type.interface';
 import { getFlatFieldMetadataMock } from 'src/engine/metadata-modules/flat-field-metadata/__mocks__/get-flat-field-metadata.mock';
+import { FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { expectFlatFieldMetadataToBeInFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/__tests__/utils/expect-flat-field-metadata-to-be-in-flat-object-metadata-maps.util';
 import {
   DispatchAndAddFlatFieldMetadataInFlatObjectMetadataMapsArgs,
   dispatchAndAddFlatFieldMetadataInFlatObjectMetadataMaps,
 } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/dispatch-and-add-flat-field-metadata-in-flat-object-metadata-maps.util';
+import { fromFlatObjectMetadatasToFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata/utils/from-flat-object-metadatas-to-flat-object-metadata-maps.util';
+import { RelationOnDeleteAction } from 'src/engine/metadata-modules/relation-metadata/relation-on-delete-action.type';
+import { isDefined } from 'twenty-shared/utils';
 
 type DispatchAndAddFlatFieldMetadataInFlatObjectMetadataMapsTestCase = {
   input: DispatchAndAddFlatFieldMetadataInFlatObjectMetadataMapsArgs;
-  expected: ReturnType<
-    typeof dispatchAndAddFlatFieldMetadataInFlatObjectMetadataMaps
-  >;
+  expected:
+    | ((
+        arg: ReturnType<
+          typeof dispatchAndAddFlatFieldMetadataInFlatObjectMetadataMaps
+        >,
+      ) => void)
+    | undefined;
 };
+
+const FLAT_OBJECT_METADATA_MAPS_MOCKS_WITH_ROCKET_AND_PET =
+  fromFlatObjectMetadatasToFlatObjectMetadataMaps([
+    PET_FLAT_OBJECT_MOCK,
+    ROCKET_FLAT_OBJECT_MOCK,
+  ]);
+const fieldMetadataToRemoveId = 'field-metadata-id';
+const newCustomRelationFlatFieldMetadata = getFlatFieldMetadataMock({
+  objectMetadataId: PET_FLAT_OBJECT_MOCK.id,
+  type: FieldMetadataType.RELATION,
+  uniqueIdentifier: fieldMetadataToRemoveId,
+  id: fieldMetadataToRemoveId,
+  settings: {
+    relationType: RelationType.MANY_TO_ONE,
+    joinColumnName: 'whateverId',
+    onDelete: RelationOnDeleteAction.CASCADE,
+  },
+  relationTargetObjectMetadataId: PET_FLAT_OBJECT_MOCK.id,
+  relationTargetFieldMetadataId: 'does-not-matter',
+}) as FlatFieldMetadata<FieldMetadataType.RELATION>;
 
 describe('dispatchAndAddFlatFieldMetadataInFlatObjectMetadataMaps', () => {
   const testCases: EachTestingContext<DispatchAndAddFlatFieldMetadataInFlatObjectMetadataMapsTestCase>[] =
     [
       {
-        title: 'should return same maps when object metadata id does not exist',
+        title: 'should return undefined when object metadata id does not exist',
         context: {
           input: {
             flatObjectMetadataMaps: FLAT_OBJECT_METADATA_MAPS_MOCKS,
@@ -36,6 +68,27 @@ describe('dispatchAndAddFlatFieldMetadataInFlatObjectMetadataMaps', () => {
           expected: undefined,
         },
       },
+      {
+        only: true,
+        title: 'should add a new relation field to pet object',
+        context: {
+          input: {
+            flatObjectMetadataMaps:
+              FLAT_OBJECT_METADATA_MAPS_MOCKS_WITH_ROCKET_AND_PET,
+            flatFieldMetadata: newCustomRelationFlatFieldMetadata,
+          },
+          expected: (flatObjectMetadataMaps) => {
+            expect(flatObjectMetadataMaps).toBeDefined();
+            if (!isDefined(flatObjectMetadataMaps)) {
+              throw new Error('Should never occur, typecheck');
+            }
+            expectFlatFieldMetadataToBeInFlatObjectMetadataMaps({
+              flatFieldMetadata: newCustomRelationFlatFieldMetadata,
+              flatObjectMetadataMaps,
+            });
+          },
+        },
+      },
     ];
 
   const filteredTestCases = eachTestingContextFilter(testCases);
@@ -44,6 +97,10 @@ describe('dispatchAndAddFlatFieldMetadataInFlatObjectMetadataMaps', () => {
     const result =
       dispatchAndAddFlatFieldMetadataInFlatObjectMetadataMaps(input);
 
-    expect(result).toEqual(expected);
+    if (isDefined(expected)) {
+      expected(result);
+    } else {
+      expect(result).toEqual(expected);
+    }
   });
 });
