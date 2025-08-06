3ee0bea2a0d87a36762be8bfc2408b332c852f3a
prastoin <paul@twenty.com>
2025-08-06 15:59:57 +0200

    fix(server): import post rebase

--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/from-flat-object-metadatas-to-flat-object-metadata-maps.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/from-flat-object-metadatas-to-flat-object-metadata-maps.util.ts
@@ -1,5 +1,5 @@
 import { FlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/types/flat-object-metadata-maps.type';
-import { addFlatObjectMetadataToFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/add-flat-object-metadata-to-flat-object-metadata-maps.util';
+import { addFlatObjectMetadataToFlatObjectMetadataMapsOrThrow } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/add-flat-object-metadata-to-flat-object-metadata-maps-or-throw.util';
 import { FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
 
 export const fromFlatObjectMetadatasToFlatObjectMetadataMaps = (
@@ -12,7 +12,7 @@ export const fromFlatObjectMetadatasToFlatObjectMetadataMaps = (
 
   return flatObjectMetadatas.reduce(
     (flatObjectMetadataMaps, flatObjectMetadata) =>
-      addFlatObjectMetadataToFlatObjectMetadataMaps({
+      addFlatObjectMetadataToFlatObjectMetadataMapsOrThrow({
         flatObjectMetadata,
         flatObjectMetadataMaps,
       }),
