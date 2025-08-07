090dcdca68c9a9cc090bd320528877eeafe99d1a
Charles Bochet <charles@twenty.com>
2025-08-07 13:41:57 +0200

    Fix

---
+++ packages/twenty-front/src/modules/object-metadata/hooks/useRefreshObjectMetadataItems.ts
@@ -0,0 +1,44 @@
+import { useMutation } from '@apollo/client';
+
+import {
+  CreateFieldInput,
+  CreateOneFieldMetadataItemMutation,
+  CreateOneFieldMetadataItemMutationVariables,
+} from '~/generated-metadata/graphql';
+
+import { CREATE_ONE_FIELD_METADATA_ITEM } from '../graphql/mutations';
+
+import { useRefreshObjectMetadataItems } from '@/object-metadata/hooks/useRefreshObjectMetadataItems';
+import { useRefreshCachedViews } from '@/views/hooks/useRefreshViews';
+
+export const useCreateOneFieldMetadataItem = () => {
+  const { refreshObjectMetadataItems } =
+    useRefreshObjectMetadataItems('network-only');
+
+  const [mutate] = useMutation<
+    CreateOneFieldMetadataItemMutation,
+    CreateOneFieldMetadataItemMutationVariables
+  >(CREATE_ONE_FIELD_METADATA_ITEM);
+
+  const { refreshCachedViews } = useRefreshCachedViews();
+
+  const createOneFieldMetadataItem = async (input: CreateFieldInput) => {
+    const result = await mutate({
+      variables: {
+        input: {
+          field: input,
+        },
+      },
+    });
+
+    await refreshObjectMetadataItems();
+
+    await refreshCachedViews();
+
+    return result;
+  };
+
+  return {
+    createOneFieldMetadataItem,
+  };
+};
