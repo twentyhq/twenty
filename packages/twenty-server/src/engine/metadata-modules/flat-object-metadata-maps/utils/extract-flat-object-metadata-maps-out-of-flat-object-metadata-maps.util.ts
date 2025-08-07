df73d87274617ab661a043e3d9fe68c8f2b234d3
prastoin <paul@twenty.com>
2025-08-07 11:33:05 +0200

    feat(server): new util extract flat object metadata maps out of flat object metadata maps

---
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata-maps/utils/extract-flat-object-metadata-maps-out-of-flat-object-metadata-maps.util.ts
@@ -0,0 +1,45 @@
+import {
+  FlatObjectMetadataMapsException,
+  FlatObjectMetadataMapsExceptionCode,
+} from 'src/engine/metadata-modules/flat-object-metadata-maps/flat-object-metadata-maps.exception';
+import { FlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/types/flat-object-metadata-maps.type';
+import { addFlatObjectMetadataWithFlatFieldMapsToFlatObjectMetadataMapsOrThrow } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/add-flat-object-metadata-with-field-maps-to-flat-object-metadata-maps-or-throw.util';
+import { isDefined } from 'twenty-shared/utils';
+
+export type ExtractFlatObjectMetadataMapsOutOfFlatObjectMetadataMapsOrThrowArgs =
+  {
+    objectMetadataIds: string[];
+    flatObjectMetadataMaps: FlatObjectMetadataMaps;
+  };
+export const extractFlatObjectMetadataMapsOutOfFlatObjectMetadataMapsOrThrow =
+  ({
+    flatObjectMetadataMaps: sourceFlatObjectMetadataMaps,
+    objectMetadataIds,
+  }: ExtractFlatObjectMetadataMapsOutOfFlatObjectMetadataMapsOrThrowArgs): FlatObjectMetadataMaps => {
+    const emptyFlatObjectMetadataMaps: FlatObjectMetadataMaps = {
+      byId: {},
+      idByNameSingular: {},
+    };
+
+    return objectMetadataIds.reduce(
+      (flatObjectMetadataMaps, objectMetadataId) => {
+        const flatObjectMetadataWithFlatFieldMaps =
+          sourceFlatObjectMetadataMaps.byId[objectMetadataId];
+
+        if (!isDefined(flatObjectMetadataWithFlatFieldMaps)) {
+          throw new FlatObjectMetadataMapsException(
+            'extractFlatObjectMetadataMapsOutOfFlatObjectMetadataMapsOrThrow object metadata not found',
+            FlatObjectMetadataMapsExceptionCode.OBJECT_METADATA_NOT_FOUND,
+          );
+        }
+
+        return addFlatObjectMetadataWithFlatFieldMapsToFlatObjectMetadataMapsOrThrow(
+          {
+            flatObjectMetadataMaps,
+            flatObjectMetadataWithFlatFieldMaps,
+          },
+        );
+      },
+      emptyFlatObjectMetadataMaps,
+    );
+  };
