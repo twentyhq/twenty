b96bfe3b58188b027a89532bab840bb773ead397
etiennejouan <jouan.etienne@gmail.com>
2025-08-07 11:51:31 +0200

    improve friendly error message

--- packages/twenty-server/src/engine/twenty-orm/relation-nested-queries/utils/formatConnectRecordNotFoundErrorMessage.util.ts
+++ packages/twenty-server/src/engine/twenty-orm/relation-nested-queries/utils/formatConnectRecordNotFoundErrorMessage.util.ts
@@ -13,6 +13,6 @@ export const formatConnectRecordNotFoundErrorMessage = (
 
   return {
     errorMessage: `Expected 1 record to connect to ${connectFieldName}, but found ${recordToConnectTotal} for ${formattedConnectCondition}`,
-    userFriendlyMessage: t`Expected 1 record to connect to ${connectFieldName}, but found ${recordToConnectTotal} for ${formattedConnectCondition}`,
+    userFriendlyMessage: t`Can't connect to ${connectFieldName}. No unique record found with condition: ${formattedConnectCondition}`,
   };
 };
