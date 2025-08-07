636b146a1a3778e3d262860930c7babbd1bec458
Abdul Rahman <ar5438376@gmail.com>
2025-08-07 18:56:26 +0530

    Refactor role standardId migration and remove obsolete population script
    This commit updates the migration to add a "standardId" column to the "core.role" table and sets a default value for the 'Admin' role. Additionally, the previous migration script for populating "standardId" for existing roles has been removed as it is no longer needed.

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1754480368129-addStandardIdToRole.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1754480368129-addStandardIdToRole.ts
@@ -5,9 +5,15 @@ export class AddStandardIdToRole1754480368129 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "core"."role" ADD "standardId" uuid`);
+
+    await queryRunner.query(
+      `UPDATE "core"."role" SET "standardId" = '20202020-0001-0001-0001-000000000001' WHERE "label" = 'Admin'`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`UPDATE "core"."role" SET "standardId" = NULL`);
+
     await queryRunner.query(
       `ALTER TABLE "core"."role" DROP COLUMN "standardId"`,
     );
