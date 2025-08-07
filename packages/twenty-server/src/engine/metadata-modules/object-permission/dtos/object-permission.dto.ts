f5c3954cc7e6a8df565ae55dc79b68115bdeb34b
Marie Stoppa <marie.stoppa@essec.edu>
2025-08-07 14:31:34 +0200

    fix

--- packages/twenty-server/src/engine/metadata-modules/object-permission/dtos/object-permission.dto.ts
+++ packages/twenty-server/src/engine/metadata-modules/object-permission/dtos/object-permission.dto.ts
@@ -1,9 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 
 import GraphQLJSON from 'graphql-type-json';
+import { RestrictedFieldsPermissions } from 'twenty-shared/types';
 
 import { UUIDScalarType } from 'src/engine/api/graphql/workspace-schema-builder/graphql-types/scalars';
-import { RestrictedFieldsPermissions } from 'twenty-shared/types';
 
 @ObjectType('ObjectPermission')
 export class ObjectPermissionDTO {
