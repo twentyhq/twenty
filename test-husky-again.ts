65fef7bdb80db126182fd528fd6238919c930072
phuth <phuth@phanmemmkt.vn>
2025-08-08 10:37:35 +0700

    test: verify husky pre-commit hook again
    Testing Husky functionality:
    - Pre-commit hook should trigger
    - Prettier should format the code
    - Lint-staged should process the file
    
    Closes #husky-test-2

---
+++ test-husky-again.ts
@@ -0,0 +1,8 @@
+// Test Husky functionality again
+const huskyTest = () => {
+  console.log('Testing Husky hooks');
+  const message = 'Hello World';
+  return message;
+};
+
+export default huskyTest;
