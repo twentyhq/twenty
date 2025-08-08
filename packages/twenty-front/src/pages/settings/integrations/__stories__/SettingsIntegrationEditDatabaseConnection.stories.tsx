f1055c38470a8e2cf5fa87e95bf3d91a49fba243
Félix Malfait <felix@twenty.com>
2025-08-08 21:50:47 +0200

    Fix

--- packages/twenty-front/src/pages/settings/integrations/__stories__/SettingsIntegrationEditDatabaseConnection.stories.tsx
+++ packages/twenty-front/src/pages/settings/integrations/__stories__/SettingsIntegrationEditDatabaseConnection.stories.tsx
@@ -1,12 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react-vite';
 import { within } from 'storybook/test';
 
+import { SettingsPath } from '@/types/SettingsPath';
 import { SettingsIntegrationEditDatabaseConnection } from '~/pages/settings/integrations/SettingsIntegrationEditDatabaseConnection';
 import {
   PageDecorator,
   PageDecoratorArgs,
 } from '~/testing/decorators/PageDecorator';
 import { graphqlMocks } from '~/testing/graphqlMocks';
+import { getSettingsPath } from '~/utils/navigation/getSettingsPath';
 import { sleep } from '~/utils/sleep';
 
 const meta: Meta<PageDecoratorArgs> = {
@@ -15,7 +17,7 @@ const meta: Meta<PageDecoratorArgs> = {
   component: SettingsIntegrationEditDatabaseConnection,
   decorators: [PageDecorator],
   args: {
-    routePath: '/settings/integrations/:databaseKey/edit',
+    routePath: getSettingsPath(SettingsPath.IntegrationEditDatabaseConnection),
     routeParams: {
       ':databaseKey': 'postgresql',
       ':connectionId': '67cbfd35-8dd4-4591-b9d4-c1906281a5da',
