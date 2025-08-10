eac3c3f53ca079c5b8d275a55e452712888bbd16
Félix Malfait <felix@twenty.com>
2025-08-10 22:38:00 +0200

    Fix

--- packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/http-request-action/constants/HttpRequest.ts
+++ packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/http-request-action/constants/HttpRequest.ts
@@ -1,11 +1,31 @@
-const HTTP_METHODS = [
-  { label: 'GET', value: 'GET' },
-  { label: 'POST', value: 'POST' },
-  { label: 'PUT', value: 'PUT' },
-  { label: 'PATCH', value: 'PATCH' },
-  { label: 'DELETE', value: 'DELETE' },
-] as const,
-METHODS_WITH_BODY = ['POST', 'PUT', 'PATCH'] as const;
+const {
+  HTTP_METHODS,
+  METHODS_WITH_BODY,
+  DEFAULT_JSON_BODY_PLACEHOLDER,
+  JSON_RESPONSE_PLACEHOLDER,
+  DEFAULT_HTTP_REQUEST_OUTPUT_VALUE,
+} = {
+  HTTP_METHODS: [
+    { label: 'GET', value: 'GET' },
+    { label: 'POST', value: 'POST' },
+    { label: 'PUT', value: 'PUT' },
+    { label: 'PATCH', value: 'PATCH' },
+    { label: 'DELETE', value: 'DELETE' },
+  ] as const,
+  METHODS_WITH_BODY: ['POST', 'PUT', 'PATCH'] as const,
+  DEFAULT_JSON_BODY_PLACEHOLDER:
+    '{\n  "key": "value"\n "another_key": "{{workflow.variable}}" \n}',
+  JSON_RESPONSE_PLACEHOLDER:
+    '{\n  Paste expected call response here to use its keys later in the workflow \n}',
+  DEFAULT_HTTP_REQUEST_OUTPUT_VALUE: {
+    data: 'Configure your request above, then press "Test"',
+    status: undefined,
+    statusText: undefined,
+    headers: {},
+    duration: undefined,
+    error: undefined,
+  },
+};
 
 export type HttpMethodWithBody = (typeof METHODS_WITH_BODY)[number];
 
@@ -18,19 +38,6 @@ export type HttpRequestFormData = {
   method: HttpMethod;
   headers: Record<string, string>;
   body?: HttpRequestBody | string;
-};
-
-DEFAULT_JSON_BODY_PLACEHOLDER =
-  '{\n  "key": "value"\n "another_key": "{{workflow.variable}}" \n}',
-  JSON_RESPONSE_PLACEHOLDER =
-  '{\n  Paste expected call response here to use its keys later in the workflow \n}',
-  DEFAULT_HTTP_REQUEST_OUTPUT_VALUE = {
-  data: 'Configure your request above, then press "Test"',
-  status: undefined,
-  statusText: undefined,
-  headers: {},
-  duration: undefined,
-  error: undefined,
 };
 
 export {
