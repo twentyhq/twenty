3f3b994501b41939bd3c27d63e29e4235067adf8
prastoin <paul@twenty.com>
2025-08-11 15:15:47 +0200

    lint

--- packages/twenty-server/src/engine/metadata-modules/utils/__tests__/validate-metadata-name.spec.ts
+++ packages/twenty-server/src/engine/metadata-modules/utils/__tests__/validate-metadata-name.spec.ts
@@ -2,7 +2,6 @@ import { type EachTestingContext } from 'twenty-shared/testing';
 
 import { validateMetadataNameOrThrow } from 'src/engine/metadata-modules/utils/validate-metadata-name-or-throw.utils';
 
-
 type ValidateMetadataNameTestContext = EachTestingContext<{
   input: string;
   shouldNotThrow?: true;
