75a15260adc3f4d7f40de9e43ce4c7fc0389fbe1
nghuyan304 <nghuyan304@gmail.com>
2025-08-11 11:04:02 +0700

    fix: reorder MKT_PREFILLS to include prefillMktContracts

--- packages/twenty-server/src/mkt-core/enums/mkt-objects-prefill-data.ts
+++ packages/twenty-server/src/mkt-core/enums/mkt-objects-prefill-data.ts
@@ -16,6 +16,7 @@ export const MKT_PREFILLS = [
   prefillMktVariants,
   prefillMktVariantAttributes,
   // order prefills
+  prefillMktContracts,
   prefillMktOrders,
   prefillMktLicenses,
   prefillMktContracts,
