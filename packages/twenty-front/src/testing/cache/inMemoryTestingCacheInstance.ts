7be0bc09d5217e10cfe5c3c7e92b3555855b81eb
Félix Malfait <felix@twenty.com>
2025-08-11 10:36:07 +0200

    Fix typecheck?

--- packages/twenty-front/src/testing/cache/inMemoryTestingCacheInstance.ts
+++ packages/twenty-front/src/testing/cache/inMemoryTestingCacheInstance.ts
@@ -103,7 +103,7 @@ export class InMemoryTestingCacheInstance {
     if (isDefined(matchObject)) {
       expect(cachedRecord).toMatchObject(matchObject);
     }
-    expect(cachedRecord).toMatchSnapshot(snapshotPropertyMatchers ?? {});
+    expect(cachedRecord).toMatchSnapshot(snapshotPropertyMatchers);
   };
 
   public restoreCacheToInitialState = async () => {
