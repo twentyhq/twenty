ff00a918a105effaf8bb011a765f87fb7da04036
Abdul Rahman <ar5438376@gmail.com>
2025-08-11 20:07:51 +0530

    Add migration to add standardId column to Agent table

---
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1754923039348-AddStandardIdToAgent.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddStandardIdToAgent1754923039348 implements MigrationInterface {
+    name = 'AddStandardIdToAgent1754923039348'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."agent" ADD "standardId" uuid`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."agent" DROP COLUMN "standardId"`);
+    }
+
+}
