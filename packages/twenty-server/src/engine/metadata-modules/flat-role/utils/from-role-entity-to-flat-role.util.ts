9b11735c57e615e7d1a9d5ac5579553e1ce38f2b
Abdul Rahman <81605929+abdulrahmancodes@users.noreply.github.com>
2025-08-11 16:44:45 +0530

    Code first roles sync (#13667)

--- packages/twenty-server/src/engine/metadata-modules/flat-role/utils/from-role-entity-to-flat-role.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-role/utils/from-role-entity-to-flat-role.util.ts
@@ -1,23 +1,21 @@
-import { type RoleDTO } from 'src/engine/metadata-modules/role/dtos/role.dto';
+import { type FlatRole } from 'src/engine/metadata-modules/flat-role/types/flat-role.type';
 import { type RoleEntity } from 'src/engine/metadata-modules/role/role.entity';
 
-export const fromRoleEntityToRoleDto = (role: RoleEntity): RoleDTO => {
+export const fromRoleEntityToFlatRole = (role: RoleEntity): FlatRole => {
   return {
     id: role.id,
     standardId: role.standardId,
     label: role.label,
-    canUpdateAllSettings: role.canUpdateAllSettings,
-    canAccessAllTools: role.canAccessAllTools,
     description: role.description,
     icon: role.icon,
     isEditable: role.isEditable,
+    canUpdateAllSettings: role.canUpdateAllSettings,
+    canAccessAllTools: role.canAccessAllTools,
     canReadAllObjectRecords: role.canReadAllObjectRecords,
     canUpdateAllObjectRecords: role.canUpdateAllObjectRecords,
     canSoftDeleteAllObjectRecords: role.canSoftDeleteAllObjectRecords,
     canDestroyAllObjectRecords: role.canDestroyAllObjectRecords,
-    roleTargets: role.roleTargets,
+    workspaceId: role.workspaceId,
+    uniqueIdentifier: role.standardId || role.id,
   };
 };
-
-export const fromRoleEntitiesToRoleDtos = (roleEntities: RoleEntity[]) =>
-  roleEntities.map(fromRoleEntityToRoleDto);
