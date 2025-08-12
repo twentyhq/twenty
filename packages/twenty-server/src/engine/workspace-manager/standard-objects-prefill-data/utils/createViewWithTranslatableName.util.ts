bcbbfd7105b643820ed229e82d5fdfbbd525d4b4
ehconitin <nitinkoche03@gmail.com>
2025-08-12 13:35:11 +0530

    remove comment

--- packages/twenty-server/src/engine/workspace-manager/standard-objects-prefill-data/utils/createViewWithTranslatableName.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/standard-objects-prefill-data/utils/createViewWithTranslatableName.util.ts
@@ -3,7 +3,6 @@ import { type MessageDescriptor } from '@lingui/core';
 import { type ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 import { type ViewDefinition } from 'src/engine/workspace-manager/standard-objects-prefill-data/types/view-definition.interface';
 
-// Extracts message from MessageDescriptor for view name
 export const createViewWithTranslatableName = (
   name: MessageDescriptor,
   viewBuilder: (
