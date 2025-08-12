7500eac50d19d661a3f22f586cedb816d86733eb
neo773 <neo773@protonmail.com>
2025-08-13 01:20:58 +0530

    minor fix

--- packages/twenty-front/src/modules/ui/theme/states/persistedColorSchemeState.ts
+++ packages/twenty-front/src/modules/ui/theme/states/persistedColorSchemeState.ts
@@ -4,7 +4,7 @@ import { type ColorScheme } from '@/workspace-member/types/WorkspaceMember';
 import { localStorageEffect } from '~/utils/recoil-effects';
 
 export const persistedColorSchemeState = atom<ColorScheme>({
-  key: 'colorSchemeState',
+  key: 'persistedColorSchemeState',
   default: 'System',
   effects: [localStorageEffect()],
 });
