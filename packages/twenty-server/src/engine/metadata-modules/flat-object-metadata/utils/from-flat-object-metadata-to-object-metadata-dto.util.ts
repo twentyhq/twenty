c7cf703144f9946b5970cde05c4d6c10647e16b7
prastoin <paul@twenty.com>
2025-08-12 17:25:02 +0200

    feat(server): from flat to dto

---
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/from-flat-object-metadata-to-object-metadata-dto.util.ts
@@ -0,0 +1,26 @@
+import { FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
+import { type ObjectMetadataDTO } from 'src/engine/metadata-modules/object-metadata/dtos/object-metadata.dto';
+
+export const fromFlatObjectMetadataToObjectMetadataDto = (
+  flatObjectMetadata: FlatObjectMetadata,
+): ObjectMetadataDTO => {
+  const {
+    createdAt,
+    updatedAt,
+    description,
+    icon,
+    standardOverrides,
+    shortcut,
+    ...rest
+  } = flatObjectMetadata;
+
+  return {
+    ...rest,
+    createdAt: new Date(createdAt),
+    updatedAt: new Date(updatedAt),
+    description: description ?? undefined,
+    icon: icon ?? undefined,
+    standardOverrides: standardOverrides ?? undefined,
+    shortcut: shortcut ?? undefined,
+  };
+};
