9dbba8172ff3734add24188c7fd2add7e8c82418
prastoin <paul@twenty.com>
2025-08-13 15:26:57 +0200

    refactor(server): flat object comparator to handle standard overrides

--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/compare-two-flat-object-metadata.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/compare-two-flat-object-metadata.util.ts
@@ -1,7 +1,7 @@
 import omit from 'lodash.omit';
 import diff from 'microdiff';
 import { type FromTo } from 'twenty-shared/types';
-import { assertUnreachable } from 'twenty-shared/utils';
+import { assertUnreachable, isDefined } from 'twenty-shared/utils';
 
 import { type FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
 import { type UpdateObjectAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-object-action-v2';
@@ -21,6 +21,12 @@ export const flatObjectMetadataPropertiesToCompare = [
 
 export type FlatObjectMetadataPropertiesToCompare =
   (typeof flatObjectMetadataPropertiesToCompare)[number];
+
+export const flatObjectdMetadataEntityJsonbProperties = [
+  'standardOverrides',
+] as const satisfies (keyof FlatObjectMetadata)[];
+export type FlatObjectMetadataEntityJsonbProperties =
+  (typeof flatObjectdMetadataEntityJsonbProperties)[number];
 
 /**
  * This comparator handles update on colliding uniqueIdentifier flatObjectMetadata
@@ -42,30 +48,24 @@ export const compareTwoFlatObjectMetadata = ({
   >((difference) => {
     switch (difference.type) {
       case 'CHANGE': {
-        if (
-          difference.oldValue === null &&
-          (difference.value === null || difference.value === undefined)
-        ) {
-          return [];
-        }
-        const property = difference.path[0];
+        const { oldValue, path, value } = difference;
+        const property = path[0] as FlatObjectMetadataPropertiesToCompare;
+        const isJsonb = flatObjectdMetadataEntityJsonbProperties.includes(
+          property as FlatObjectMetadataEntityJsonbProperties,
+        );
 
-        if (typeof property === 'number') {
-          return [];
-        }
-
-        if (
-          !flatObjectMetadataPropertiesToCompare.includes(
-            property as FlatObjectMetadataPropertiesToCompare,
-          )
-        ) {
-          return [];
+        if (isJsonb) {
+          return {
+            from: isDefined(oldValue) ? JSON.parse(oldValue) : oldValue,
+            to: isDefined(value) ? JSON.parse(value) : value,
+            property,
+          };
         }
 
         return {
-          property: property as FlatObjectMetadataPropertiesToCompare,
-          from: difference.oldValue,
-          to: difference.value,
+          from: oldValue,
+          to: value,
+          property,
         };
       }
       case 'CREATE':
