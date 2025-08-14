b0f3aa7468ad52cbc6d8e5ed5c9a40216f05fcb6
bosiraphael <raphael.bosi@gmail.com>
2025-08-14 16:26:58 +0200

    fix import

--- packages/twenty-front/src/modules/object-record/record-field-list/anchored-portal/components/RecordFieldListCellEditModePortalContent.tsx
+++ packages/twenty-front/src/modules/object-record/record-field-list/anchored-portal/components/RecordFieldListCellEditModePortalContent.tsx
@@ -1,4 +1,4 @@
-import { FieldInput } from '@/object-record/record-field/components/FieldInput';
+import { FieldInput } from '@/object-record/record-field/ui/components/FieldInput';
 import { RecordInlineCellEditMode } from '@/object-record/record-inline-cell/components/RecordInlineCellEditMode';
 
 export const RecordFieldListCellEditModePortalContent = () => {
