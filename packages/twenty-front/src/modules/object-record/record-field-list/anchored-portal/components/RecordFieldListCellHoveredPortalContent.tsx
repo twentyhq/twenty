cd0f9aebc046cb033e234e67807a4ff22b95f692
Charles Bochet <charlesBochet@users.noreply.github.com>
2025-08-14 11:06:37 +0200

    Refactor inline-cell inputs (#13914)
    Continue the work so that inline-cell do not embed their own input which
    is inefficient when we have thousands of inline-cell on the table

--- packages/twenty-front/src/modules/object-record/record-field-list/anchored-portal/components/RecordFieldListCellHoveredPortalContent.tsx
+++ packages/twenty-front/src/modules/object-record/record-field-list/anchored-portal/components/RecordFieldListCellHoveredPortalContent.tsx
@@ -1,63 +1,60 @@
-import { useRecoilComponentValue } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValue';
-
-import { type ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
-import { useFieldListFieldMetadataFromPosition } from '@/object-record/record-field-list/hooks/useFieldListFieldMetadataFromPosition';
+import { recordFieldListCellEditModePositionComponentState } from '@/object-record/record-field-list/states/recordFieldListCellEditModePositionComponentState';
 import { recordFieldListHoverPositionComponentState } from '@/object-record/record-field-list/states/recordFieldListHoverPositionComponentState';
 import { FieldDisplay } from '@/object-record/record-field/ui/components/FieldDisplay';
 import { FieldInput } from '@/object-record/record-field/ui/components/FieldInput';
 import { FieldContext } from '@/object-record/record-field/ui/contexts/FieldContext';
+import { useOpenFieldInputEditMode } from '@/object-record/record-field/ui/hooks/useOpenFieldInputEditMode';
 import { useRecordInlineCellContext } from '@/object-record/record-inline-cell/components/RecordInlineCellContext';
 import { RecordInlineCellDisplayMode } from '@/object-record/record-inline-cell/components/RecordInlineCellDisplayMode';
 import { RecordInlineCellHoveredPortalContent } from '@/object-record/record-inline-cell/components/RecordInlineCellHoveredPortalContent';
 import { useInlineCell } from '@/object-record/record-inline-cell/hooks/useInlineCell';
+import { useRecoilComponentState } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentState';
 import { useSetRecoilComponentState } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentState';
 import { useContext } from 'react';
-import { isDefined } from 'twenty-shared/utils';
 
-type RecordFieldListCellHoveredPortalContentProps = {
-  objectMetadataItem: ObjectMetadataItem;
-};
-
-export const RecordFieldListCellHoveredPortalContent = ({
-  objectMetadataItem,
-}: RecordFieldListCellHoveredPortalContentProps) => {
-  const hoverPosition = useRecoilComponentValue(
-    recordFieldListHoverPositionComponentState,
-  );
-
+export const RecordFieldListCellHoveredPortalContent = () => {
   const { editModeContentOnly, isCentered } = useRecordInlineCellContext();
 
-  const { hoveredFieldMetadataItem } = useFieldListFieldMetadataFromPosition({
-    objectNameSingular: objectMetadataItem.nameSingular,
-  });
-
-  const { isRecordFieldReadOnly } = useContext(FieldContext);
+  const { isRecordFieldReadOnly, recordId, fieldDefinition } =
+    useContext(FieldContext);
   const { openInlineCell } = useInlineCell();
 
   const shouldContainerBeClickable =
     !isRecordFieldReadOnly && !editModeContentOnly;
 
-  const setRecordFieldListHoverPosition = useSetRecoilComponentState(
-    recordFieldListHoverPositionComponentState,
-    'fields-card',
+  const [recordFieldListHoverPosition, setRecordFieldListHoverPosition] =
+    useRecoilComponentState(recordFieldListHoverPositionComponentState);
+
+  const setRecordFieldListCellEditModePosition = useSetRecoilComponentState(
+    recordFieldListCellEditModePositionComponentState,
   );
+  const { openFieldInput } = useOpenFieldInputEditMode();
+
+  const handleClick = () => {
+    if (shouldContainerBeClickable) {
+      openInlineCell();
+      setRecordFieldListCellEditModePosition(recordFieldListHoverPosition);
+
+      openFieldInput({
+        fieldDefinition,
+        recordId,
+        prefix: 'inline-cell',
+      });
+    }
+  };
 
   const handleMouseLeave = () => {
     setRecordFieldListHoverPosition(null);
   };
 
-  if (!isDefined(hoverPosition) || !isDefined(hoveredFieldMetadataItem)) {
-    return null;
-  }
-
   return (
     <RecordInlineCellHoveredPortalContent
       readonly={isRecordFieldReadOnly}
       isCentered={isCentered}
-      onClick={shouldContainerBeClickable ? openInlineCell : undefined}
+      onClick={handleClick}
       onMouseLeave={handleMouseLeave}
     >
-      <RecordInlineCellDisplayMode>
+      <RecordInlineCellDisplayMode isHovered={true}>
         {editModeContentOnly ? <FieldInput /> : <FieldDisplay />}
       </RecordInlineCellDisplayMode>
     </RecordInlineCellHoveredPortalContent>
