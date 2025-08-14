36fcb29e86b4de1c66088318948d98b5957ead2c
Baptiste Devessier <baptiste@devessier.fr>
2025-08-14 10:05:42 +0200

    Implement proper arrows between nodes for workflows and workflow versions (#13909)
    > [!IMPORTANT]
    > We still have to implement the new arrows for **workflow runs**. I
    kept the old arrows because they currently indicate the success state of
    an execution. In the new version, arrows will all look the same,
    regardless of the execution state of the previous node, so we'll be able
    to use the same arrows everywhere.
    
    ## Workflows
    
    You can see that arrows start from the right spot and end just above the
    target node. **We will soon remove the round top handle, and the arrow
    will gently touch the top of the node.**
    
    
    https://github.com/user-attachments/assets/febd5f49-393f-4d97-addc-bede5cf6944a
    
    ## Workflow versions
    
    
    https://github.com/user-attachments/assets/e519850f-abd9-4d75-8337-c4c44a2dbfed
    
    Closes https://github.com/twentyhq/core-team-issues/issues/1369

--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowRunDiagramBaseEdge.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowRunDiagramBaseEdge.tsx
@@ -1,9 +1,9 @@
-import { EDGE_GRAY_CIRCLE_MARKED_ID } from '@/workflow/workflow-diagram/constants/EdgeGrayCircleMarkedId';
 import { EDGE_GREEN_CIRCLE_MARKED_ID } from '@/workflow/workflow-diagram/constants/EdgeGreenCircleMarkedId';
 import { EDGE_GREEN_ROUNDED_ARROW_MARKER_ID } from '@/workflow/workflow-diagram/constants/EdgeGreenRoundedArrowMarkerId';
 import { EDGE_ROUNDED_ARROW_MARKER_ID } from '@/workflow/workflow-diagram/constants/EdgeRoundedArrowMarkerId';
 import { type Theme, useTheme } from '@emotion/react';
 import { BaseEdge } from '@xyflow/react';
+import { isDefined } from 'twenty-shared/utils';
 import { StepStatus } from 'twenty-shared/workflow';
 
 const toMarkerId = (id: string) => `url(#${id})`;
@@ -13,7 +13,7 @@ const getMarkerStart = (edgeExecutionStatus: StepStatus | undefined) => {
     return EDGE_GREEN_CIRCLE_MARKED_ID;
   }
 
-  return EDGE_GRAY_CIRCLE_MARKED_ID;
+  return undefined;
 };
 
 const getMarkerEnd = (edgeExecutionStatus: StepStatus | undefined) => {
@@ -49,9 +49,11 @@ export const WorkflowRunDiagramBaseEdge = ({
 }: WorkflowRunDiagramBaseEdgeProps) => {
   const theme = useTheme();
 
+  const markerStart = getMarkerStart(edgeExecutionStatus);
+
   return (
     <BaseEdge
-      markerStart={toMarkerId(getMarkerStart(edgeExecutionStatus))}
+      markerStart={isDefined(markerStart) ? toMarkerId(markerStart) : undefined}
       markerEnd={toMarkerId(getMarkerEnd(edgeExecutionStatus))}
       path={edgePath}
       style={{
