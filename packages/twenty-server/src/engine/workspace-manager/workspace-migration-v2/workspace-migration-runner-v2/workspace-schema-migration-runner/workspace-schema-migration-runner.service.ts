1707e00ad48aa8af4980f2bbeabe5be257256e75
Weiko <corentin@twenty.com>
2025-08-14 15:58:25 +0200

    Workspace schema migration runner v2 (#13899)

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-schema-migration-runner/workspace-schema-migration-runner.service.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-schema-migration-runner/workspace-schema-migration-runner.service.ts
@@ -18,53 +18,54 @@ export class WorkspaceSchemaMigrationRunnerService {
   runWorkspaceSchemaMigration = async ({
     action,
     queryRunner,
+    flatObjectMetadataMaps,
   }: WorkspaceMigrationRunnerArgs) => {
     switch (action.type) {
       case 'delete_object': {
         await this.workspaceSchemaObjectMigrationRunnerService.runDeleteObjectSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
       case 'create_object': {
         await this.workspaceSchemaObjectMigrationRunnerService.runCreateObjectSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
       case 'update_object': {
         await this.workspaceSchemaObjectMigrationRunnerService.runUpdateObjectSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
       case 'create_field': {
         await this.workspaceSchemaFieldMigrationRunnerService.runCreateFieldSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
       case 'update_field': {
         await this.workspaceSchemaFieldMigrationRunnerService.runUpdateFieldSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
       case 'delete_field': {
         await this.workspaceSchemaFieldMigrationRunnerService.runDeleteFieldSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
       case 'create_index': {
         await this.workspaceSchemaIndexMigrationRunnerService.runCreateIndexSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
       case 'delete_index': {
         await this.workspaceSchemaIndexMigrationRunnerService.runDeleteIndexSchemaMigration(
-          { action, queryRunner },
+          { action, queryRunner, flatObjectMetadataMaps },
         );
         break;
       }
