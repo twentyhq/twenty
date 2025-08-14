f0a5649ab4c1b35e7c6b2bcb6df9a70442683e07
prastoin <paul@twenty.com>
2025-08-14 17:25:16 +0200

    test(server): renaming mocks

--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/__mocks__/task-target-flat-object.mock.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/__mocks__/task-target-flat-object.mock.ts
@@ -1,7 +1,7 @@
 import { TASKTARGET_FLAT_FIELDS_MOCK } from 'src/engine/metadata-modules/flat-field-metadata/__mocks__/tasktarget-flat-fields.mock';
 import { getFlatObjectMetadataMock } from 'src/engine/metadata-modules/flat-object-metadata/__mocks__/get-flat-object-metadata.mock';
 
-export const TASKTARGET_FLAT_OBJECT_MOCK = getFlatObjectMetadataMock({
+export const TASK_TARGET_FLAT_OBJECT_MOCK = getFlatObjectMetadataMock({
   id: 'bb2b29b8-7f46-4106-a8ae-3a32df9c9166',
   standardId: '20202020-5a9a-44e8-95df-771cd06d0fb1',
   nameSingular: 'taskTarget',
