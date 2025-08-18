801c402df6530f126096890e49a74d245367e93a
Charles Bochet <charles@twenty.com>
2025-08-18 15:38:28 +0200

    Fix

--- packages/twenty-server/src/engine/workspace-manager/standard-objects-prefill-data/prefill-workflows.ts
+++ packages/twenty-server/src/engine/workspace-manager/standard-objects-prefill-data/prefill-workflows.ts
@@ -5,7 +5,6 @@ import { type ObjectMetadataEntity } from 'src/engine/metadata-modules/object-me
 import { generateObjectMetadataMaps } from 'src/engine/metadata-modules/utils/generate-object-metadata-maps.util';
 import { type WorkspaceEntityManager } from 'src/engine/twenty-orm/entity-manager/workspace-entity-manager';
 import { generateObjectRecordFields } from 'src/modules/workflow/workflow-builder/workflow-schema/utils/generate-object-record-fields';
-import { type WorkflowCreateRecordAction } from 'src/modules/workflow/workflow-executor/workflow-actions/types/workflow-action.type';
 
 const QUICK_LEAD_WORKFLOW_ID = '8b213cac-a68b-4ffe-817a-3ec994e9932d';
 const QUICK_LEAD_WORKFLOW_VERSION_ID = 'ac67974f-c524-4288-9d88-af8515400b68';
