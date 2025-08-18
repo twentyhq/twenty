46d25a7541fa0b95bb071f5ff1565f61d3f4c2cc
Devessier <baptiste@devessier.fr>
2025-08-18 15:23:33 +0200

    feat: implement new design for workflow runs and clean things a bit

--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramCustomMarkers.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowDiagramCustomMarkers.tsx
@@ -1,8 +1,4 @@
 import { EDGE_BRANCH_ARROW_MARKER_ID } from '@/workflow/workflow-diagram/constants/EdgeBranchArrowMarkerId';
-import { EDGE_GREEN_CIRCLE_MARKED_ID } from '@/workflow/workflow-diagram/constants/EdgeGreenCircleMarkedId';
-import { EDGE_GREEN_ROUNDED_ARROW_MARKER_ID } from '@/workflow/workflow-diagram/constants/EdgeGreenRoundedArrowMarkerId';
-import { EDGE_GREEN_ROUNDED_ARROW_MARKER_WIDTH_PX } from '@/workflow/workflow-diagram/constants/EdgeGreenRoundedArrowMarkerWidthPx';
-import { EDGE_ROUNDED_ARROW_MARKER_ID } from '@/workflow/workflow-diagram/constants/EdgeRoundedArrowMarkerId';
 import { useTheme } from '@emotion/react';
 
 export const WorkflowDiagramCustomMarkers = () => {
@@ -11,32 +7,6 @@ export const WorkflowDiagramCustomMarkers = () => {
   return (
     <svg style={{ position: 'absolute', top: 0, left: 0 }}>
       <defs>
-        <marker
-          id={EDGE_ROUNDED_ARROW_MARKER_ID}
-          markerHeight={5}
-          markerWidth={6}
-          refX={3}
-          refY={2.5}
-        >
-          <path
-            d="M0.31094 1.1168C0.178029 0.917434 0.320947 0.650391 0.560555 0.650391H5.43945C5.67905 0.650391 5.82197 0.917434 5.68906 1.1168L3.62404 4.21433C3.32717 4.65963 2.67283 4.65963 2.37596 4.21433L0.31094 1.1168Z"
-            fill={theme.border.color.strong}
-          />
-        </marker>
-
-        <marker
-          id={EDGE_GREEN_ROUNDED_ARROW_MARKER_ID}
-          markerHeight={5}
-          markerWidth={EDGE_GREEN_ROUNDED_ARROW_MARKER_WIDTH_PX}
-          refX={EDGE_GREEN_ROUNDED_ARROW_MARKER_WIDTH_PX / 2}
-          refY={2.5}
-        >
-          <path
-            d="M0.31094 1.1168C0.178029 0.917434 0.320947 0.650391 0.560555 0.650391H5.43945C5.67905 0.650391 5.82197 0.917434 5.68906 1.1168L3.62404 4.21433C3.32717 4.65963 2.67283 4.65963 2.37596 4.21433L0.31094 1.1168Z"
-            fill={theme.tag.text.turquoise}
-          />
-        </marker>
-
         <marker
           id={EDGE_BRANCH_ARROW_MARKER_ID}
           markerHeight={8}
@@ -48,25 +18,6 @@ export const WorkflowDiagramCustomMarkers = () => {
             d="M1.7915 1.38672H8.18311C8.57541 1.38705 8.81458 1.81852 8.60693 2.15137L5.41064 7.26465C5.21481 7.57798 4.75882 7.57798 4.56299 7.26465L1.3667 2.15137C1.15906 1.81841 1.39896 1.38672 1.7915 1.38672Z"
             stroke={theme.border.color.strong}
             fill={theme.background.primary}
-          />
-        </marker>
-
-        <marker
-          markerHeight={5}
-          markerWidth={5}
-          refX={2}
-          refY={2.5}
-          id={EDGE_GREEN_CIRCLE_MARKED_ID}
-        >
-          <rect
-            x={0.5}
-            y={0.5}
-            height={3}
-            width={3}
-            rx="1.5"
-            fill="white"
-            stroke={theme.tag.text.turquoise}
-            strokeWidth={1}
           />
         </marker>
       </defs>
--- packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowRunDiagramStepNode.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/components/WorkflowRunDiagramStepNode.tsx
@@ -5,25 +5,181 @@ import { useRecoilComponentValue } from '@/ui/utilities/state/component-state/ho
 import { useSetRecoilComponentState } from '@/ui/utilities/state/component-state/hooks/useSetRecoilComponentState';
 import { useWorkflowRunIdOrThrow } from '@/workflow/hooks/useWorkflowRunIdOrThrow';
 import { workflowVisualizerWorkflowIdComponentState } from '@/workflow/states/workflowVisualizerWorkflowIdComponentState';
-import { WorkflowDiagramStepNodeBase } from '@/workflow/workflow-diagram/components/WorkflowDiagramStepNodeBase';
-import { WorkflowDiagramStepNodeIcon } from '@/workflow/workflow-diagram/components/WorkflowDiagramStepNodeIcon';
+import { WorkflowDiagramHandleReadonly } from '@/workflow/workflow-diagram/components/WorkflowDiagramHandleReadonly';
+import { WorkflowDiagramStepNodeBigIcon } from '@/workflow/workflow-diagram/components/WorkflowDiagramStepNodeBigIcon';
+import { WORKFLOW_DIAGRAM_STEP_NODE_BASE_CLICK_OUTSIDE_ID } from '@/workflow/workflow-diagram/constants/WorkflowDiagramStepNodeClickOutsideId';
 import { workflowSelectedNodeComponentState } from '@/workflow/workflow-diagram/states/workflowSelectedNodeComponentState';
 import { type WorkflowRunDiagramStepNodeData } from '@/workflow/workflow-diagram/types/WorkflowDiagram';
-import { getNodeVariantFromStepRunStatus } from '@/workflow/workflow-diagram/utils/getNodeVariantFromStepRunStatus';
 import { getWorkflowNodeIconKey } from '@/workflow/workflow-diagram/utils/getWorkflowNodeIconKey';
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Position } from '@xyflow/react';
 import { useContext } from 'react';
 import { useSetRecoilState } from 'recoil';
-import { isDefined } from 'twenty-shared/utils';
-import { useIcons } from 'twenty-ui/display';
+import { capitalize, isDefined } from 'twenty-shared/utils';
+import { StepStatus } from 'twenty-shared/workflow';
+import { IconCheck, IconX, Label, useIcons } from 'twenty-ui/display';
+import { Loader } from 'twenty-ui/feedback';
+
+const StyledNodeContainer = styled.div`
+  align-items: center;
+  display: flex;
+  gap: ${({ theme }) => theme.spacing(2)};
+  max-width: 240px;
+  min-width: 44px;
+  padding: ${({ theme }) => theme.spacing(2)};
+  border-radius: ${({ theme }) => theme.border.radius.md};
+  border: 1px solid ${({ theme }) => theme.border.color.strong};
+  background: ${({ theme }) => theme.background.secondary};
+  box-sizing: border-box;
+  cursor: pointer;
+  position: relative;
+
+  &:hover {
+    background: linear-gradient(
+        0deg,
+        ${({ theme }) => theme.background.transparent.lighter} 0%,
+        ${({ theme }) => theme.background.transparent.lighter} 100%
+      ),
+      ${({ theme }) => theme.background.secondary};
+  }
+
+  .selected & {
+    border-color: ${({ theme }) => theme.color.blue};
+    background: ${({ theme }) => theme.adaptiveColors.blue1};
+  }
+
+  .selected &[data-status='RUNNING'],
+  .selected &[data-status='PENDING'] {
+    border-color: ${({ theme }) => theme.color.yellow};
+    background: ${({ theme }) => theme.adaptiveColors.yellow1};
+  }
+
+  .selected &[data-status='FAILED'] {
+    border-color: ${({ theme }) => theme.color.red};
+    background: ${({ theme }) => theme.adaptiveColors.red1};
+  }
+
+  .selected &[data-status='SUCCESS'] {
+    border-color: ${({ theme }) => theme.color.turquoise};
+    background: ${({ theme }) => theme.adaptiveColors.turquoise1};
+  }
+`;
+
+const StyledNodeIconContainer = styled.div`
+  align-items: center;
+  background: ${({ theme }) => theme.background.transparent.light};
+  border-radius: 4px;
+  box-sizing: border-box;
+  display: flex;
+  height: 32px;
+  justify-content: center;
+  width: 32px;
+`;
+
+const StyledNodeRightPart = styled.div`
+  align-items: flex-start;
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  max-width: 184px;
+  box-sizing: border-box;
+`;
+
+const StyledNodeLabelWithCounterPart = styled.div`
+  align-items: center;
+  align-self: stretch;
+  display: flex;
+  column-gap: ${({ theme }) => theme.spacing(2)};
+  height: 14px;
+  justify-content: space-between;
+  box-sizing: border-box;
+`;
+
+const StyledNodeLabel = styled(Label)`
+  box-sizing: border-box;
+  color: ${({ theme }) => theme.font.color.tertiary};
+  flex: 1 0 0;
+
+  .selected & {
+    color: ${({ theme }) => theme.tag.text.blue};
+  }
+
+  &[data-status='RUNNING'],
+  &[data-status='PENDING'] {
+    color: ${({ theme }) => theme.tag.text.yellow};
+  }
+
+  &[data-status='FAILED'] {
+    color: ${({ theme }) => theme.tag.text.red};
+  }
+
+  &[data-status='SUCCESS'] {
+    color: ${({ theme }) => theme.tag.text.green};
+  }
+`;
+
+const StyledNodeTitle = styled.div`
+  box-sizing: border-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  align-self: stretch;
+  color: ${({ theme }) => theme.font.color.light};
+  display: -webkit-box;
+  font-family: Inter;
+  font-size: ${({ theme }) => theme.font.size.md};
+  font-style: normal;
+  font-weight: ${({ theme }) => theme.font.weight.medium};
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  &[data-status='RUNNING'],
+  &[data-status='PENDING'],
+  &[data-status='FAILED'],
+  &[data-status='SUCCESS'],
+  .selected & {
+    color: ${({ theme }) => theme.font.color.primary};
+  }
+`;
+
+const StyledNodeCounter = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 4px;
+  justify-content: flex-end;
+  box-sizing: border-box;
+`;
+
+const StyledColorIcon = styled.div`
+  display: flex;
+  width: 14px;
+  height: 14px;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  border-radius: ${({ theme }) => theme.border.radius.sm};
+
+  &[data-status='FAILED'] {
+    background: ${({ theme }) => theme.tag.background.red};
+  }
+
+  &[data-status='SUCCESS'] {
+    background: ${({ theme }) => theme.tag.background.turquoise};
+  }
+`;
 
 export const WorkflowRunDiagramStepNode = ({
   id,
   data,
+  selected,
 }: {
   id: string;
   data: WorkflowRunDiagramStepNodeData;
+  selected: boolean;
 }) => {
   const { getIcon } = useIcons();
+  const theme = useTheme();
 
   const workflowId = useRecoilComponentValue(
     workflowVisualizerWorkflowIdComponentState,
@@ -42,34 +198,87 @@ export const WorkflowRunDiagramStepNode = ({
     commandMenuNavigationStackState,
   );
 
+  const handleClick = () => {
+    if (!isDefined(workflowId)) {
+      throw new Error('Workflow ID must be defined');
+    }
+
+    if (!isInRightDrawer) {
+      setCommandMenuNavigationStack([]);
+    }
+
+    setWorkflowSelectedNode(id);
+
+    openWorkflowRunViewStepInCommandMenu({
+      workflowId,
+      workflowRunId,
+      title: data.name,
+      icon: getIcon(getWorkflowNodeIconKey(data)),
+      workflowSelectedNode: id,
+      stepExecutionStatus: data.runStatus,
+    });
+  };
+
   return (
-    <WorkflowDiagramStepNodeBase
-      id={id}
-      name={data.name}
-      variant={getNodeVariantFromStepRunStatus(data.runStatus)}
-      nodeType={data.nodeType}
-      Icon={<WorkflowDiagramStepNodeIcon data={data} />}
-      displayHandle={false}
-      onClick={() => {
-        if (!isDefined(workflowId)) {
-          throw new Error('Workflow ID must be defined');
-        }
+    <>
+      {data.nodeType !== 'trigger' && (
+        <WorkflowDiagramHandleReadonly
+          type="target"
+          position={Position.Top}
+          selected={false}
+        />
+      )}
 
-        if (!isInRightDrawer) {
-          setCommandMenuNavigationStack([]);
-        }
+      <StyledNodeContainer
+        data-click-outside-id={WORKFLOW_DIAGRAM_STEP_NODE_BASE_CLICK_OUTSIDE_ID}
+        data-status={data.runStatus}
+        onClick={handleClick}
+      >
+        <StyledNodeIconContainer>
+          <WorkflowDiagramStepNodeBigIcon data={data} />
+        </StyledNodeIconContainer>
 
-        setWorkflowSelectedNode(id);
+        <StyledNodeRightPart>
+          <StyledNodeLabelWithCounterPart>
+            <StyledNodeLabel data-status={data.runStatus}>
+              {capitalize(data.nodeType)}
+            </StyledNodeLabel>
 
-        openWorkflowRunViewStepInCommandMenu({
-          workflowId,
-          workflowRunId,
-          title: data.name,
-          icon: getIcon(getWorkflowNodeIconKey(data)),
-          workflowSelectedNode: id,
-          stepExecutionStatus: data.runStatus,
-        });
-      }}
-    />
+            {data.runStatus === StepStatus.SUCCESS && (
+              <StyledNodeCounter>
+                <StyledColorIcon data-status={StepStatus.SUCCESS}>
+                  <IconCheck color={theme.tag.text.turquoise} size={14} />
+                </StyledColorIcon>
+              </StyledNodeCounter>
+            )}
+
+            {data.runStatus === StepStatus.FAILED && (
+              <StyledNodeCounter>
+                <StyledColorIcon data-status={StepStatus.FAILED}>
+                  <IconX color={theme.tag.text.red} size={14} />
+                </StyledColorIcon>
+              </StyledNodeCounter>
+            )}
+
+            {(data.runStatus === StepStatus.RUNNING ||
+              data.runStatus === StepStatus.PENDING) && (
+              <StyledNodeCounter>
+                <Loader color="yellow" />
+              </StyledNodeCounter>
+            )}
+          </StyledNodeLabelWithCounterPart>
+
+          <StyledNodeTitle data-status={data.runStatus}>
+            {data.name}
+          </StyledNodeTitle>
+        </StyledNodeRightPart>
+      </StyledNodeContainer>
+
+      <WorkflowDiagramHandleReadonly
+        type="source"
+        position={Position.Bottom}
+        selected={selected}
+      />
+    </>
   );
 };
