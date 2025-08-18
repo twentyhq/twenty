26cfbee79b2bdaf4a3bac8a91e81d380fb76fa23
prastoin <paul@twenty.com>
2025-08-18 14:47:29 +0200

    chore(server): remove validation from service

--- packages/twenty-server/src/engine/metadata-modules/field-metadata/services/field-metadata.service-v2.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/services/field-metadata.service-v2.ts
@@ -8,7 +8,6 @@ import {
 } from 'twenty-shared/utils';
 import { In, Repository } from 'typeorm';
 
-import { MultipleMetadataValidationErrors } from 'src/engine/core-modules/error/multiple-metadata-validation-errors';
 import { type CreateFieldInput } from 'src/engine/metadata-modules/field-metadata/dtos/create-field.input';
 import { type DeleteOneFieldInput } from 'src/engine/metadata-modules/field-metadata/dtos/delete-field.input';
 import { FieldMetadataDTO } from 'src/engine/metadata-modules/field-metadata/dtos/field-metadata.dto';
@@ -18,8 +17,6 @@ import {
   FieldMetadataException,
   FieldMetadataExceptionCode,
 } from 'src/engine/metadata-modules/field-metadata/field-metadata.exception';
-import { FlatFieldMetadataValidatorService } from 'src/engine/metadata-modules/flat-field-metadata/services/flat-field-metadata-validator.service';
-import { type FailedFlatFieldMetadataValidationExceptions } from 'src/engine/metadata-modules/flat-field-metadata/types/failed-flat-field-metadata-validation.type';
 import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
 import { fromCreateFieldInputToFlatFieldMetadatasToCreate } from 'src/engine/metadata-modules/flat-field-metadata/utils/from-create-field-input-to-flat-field-metadatas-to-create.util';
 import { fromDeleteFieldInputToFlatFieldMetadatasToDelete } from 'src/engine/metadata-modules/flat-field-metadata/utils/from-delete-field-input-to-flat-field-metadatas-to-delete.util';
@@ -28,7 +25,6 @@ import { fromUpdateFieldInputToFlatFieldMetadata } from 'src/engine/metadata-mod
 import { isFlatFieldMetadataEntityOfType } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-flat-field-metadata-of-type.util';
 import { throwOnFieldInputTranspilationsError } from 'src/engine/metadata-modules/flat-field-metadata/utils/throw-on-field-input-transpilations-error.util';
 import { type FlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/types/flat-object-metadata-maps.type';
-import { addFlatFieldMetadataInFlatObjectMetadataMapsOrThrow } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/add-flat-field-metadata-in-flat-object-metadata-maps-or-throw.util';
 import { addFlatFieldMetadataInFlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/add-flat-field-metadata-in-flat-object-metadata-maps.util';
 import { deleteFieldFromFlatObjectMetadataMapsOrThrow } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/delete-field-from-flat-object-metadata-maps-or-throw.util';
 import { getSubFlatObjectMetadataMapsOrThrow } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/get-sub-flat-object-metadata-maps-or-throw.util';
@@ -44,7 +40,6 @@ export class FieldMetadataServiceV2 {
     private readonly fieldMetadataRepository: Repository<FieldMetadataEntity>,
     private readonly workspaceMetadataCacheService: WorkspaceMetadataCacheService,
     private readonly workspaceMigrationValidateBuildAndRunService: WorkspaceMigrationValidateBuildAndRunService,
-    private readonly flatFieldMetadataValidatorService: FlatFieldMetadataValidatorService,
   ) {}
 
   async createOne({
@@ -88,23 +83,6 @@ export class FieldMetadataServiceV2 {
         deleteOneFieldInput,
         existingFlatObjectMetadataMaps,
       });
-
-    const validationErrors = flatFieldMetadatasToDelete.flatMap(
-      (flatFieldMetadataToDelete) =>
-        this.flatFieldMetadataValidatorService.validateFlatFieldMetadataDeletion(
-          {
-            existingFlatObjectMetadataMaps,
-            flatFieldMetadataToDelete,
-          },
-        ),
-    );
-
-    if (validationErrors.length > 0) {
-      throw new MultipleMetadataValidationErrors(
-        validationErrors,
-        'Multiple validation errors occurred while deleting field',
-      );
-    }
 
     const flatObjectMetadataMapsWithImpactedObject =
       getSubFlatObjectMetadataMapsOrThrow({
@@ -219,22 +197,6 @@ export class FieldMetadataServiceV2 {
     const optimisticiallyUpdatedFlatFieldMetadata =
       inputTranspilationResult.result;
 
-    const validationErrors =
-      await this.flatFieldMetadataValidatorService.validateFlatFieldMetadataUpdate(
-        {
-          existingFlatObjectMetadataMaps,
-          flatFieldMetadataToValidate: optimisticiallyUpdatedFlatFieldMetadata,
-          workspaceId,
-        },
-      );
-
-    if (validationErrors.length > 0) {
-      throw new MultipleMetadataValidationErrors(
-        validationErrors,
-        'Multiple validation errors occurred while updating field',
-      );
-    }
-
     const fromFlatObjectMetadataMaps = getSubFlatObjectMetadataMapsOrThrow({
       flatObjectMetadataMaps: existingFlatObjectMetadataMaps,
       objectMetadataIds: [
@@ -306,56 +268,6 @@ export class FieldMetadataServiceV2 {
       ({ result }) => result,
     );
 
-    const allValidationErrors: FailedFlatFieldMetadataValidationExceptions[] =
-      [];
-    let optimisticFlatObjectMetadataMaps = structuredClone(
-      existingFlatObjectMetadataMaps,
-    );
-
-    for (const flatFieldMetadataToCreate of flatFieldMetadatasToCreate) {
-      const otherFlatObjectMetadataMapsToValidate =
-        this.computeOtherFlatObjectMetadataMapsToValidate({
-          flatObjectMetadataMaps: optimisticFlatObjectMetadataMaps,
-          flatFieldMetadatasToCreate,
-          flatFieldMetadataToCreate,
-        });
-
-      const validationErrors =
-        await this.flatFieldMetadataValidatorService.validateFlatFieldMetadataCreation(
-          {
-            existingFlatObjectMetadataMaps: optimisticFlatObjectMetadataMaps,
-            flatFieldMetadataToValidate: flatFieldMetadataToCreate,
-            workspaceId,
-            otherFlatObjectMetadataMapsToValidate,
-          },
-        );
-
-      if (validationErrors.length > 0) {
-        allValidationErrors.push(...validationErrors);
-        continue;
-      }
-
-      try {
-        optimisticFlatObjectMetadataMaps =
-          addFlatFieldMetadataInFlatObjectMetadataMapsOrThrow({
-            flatFieldMetadata: flatFieldMetadataToCreate,
-            flatObjectMetadataMaps: optimisticFlatObjectMetadataMaps,
-          });
-      } catch {
-        throw new FieldMetadataException(
-          'Optimistic cache manipulation failed, should never occur',
-          FieldMetadataExceptionCode.INTERNAL_SERVER_ERROR,
-        );
-      }
-    }
-
-    if (allValidationErrors.length > 0) {
-      throw new MultipleMetadataValidationErrors(
-        allValidationErrors,
-        'Multiple validation errors occurred while creating field',
-      );
-    }
-
     const impactedObjectMetadataIds = Array.from(
       new Set(
         flatFieldMetadatasToCreate.map(
@@ -369,7 +281,7 @@ export class FieldMetadataServiceV2 {
       objectMetadataIds: impactedObjectMetadataIds,
     });
     const toFlatObjectMetadataMaps = getSubFlatObjectMetadataMapsOrThrow({
-      flatObjectMetadataMaps: optimisticFlatObjectMetadataMaps,
+      flatObjectMetadataMaps: existingFlatObjectMetadataMaps,
       objectMetadataIds: impactedObjectMetadataIds,
     });
 
@@ -387,7 +299,6 @@ export class FieldMetadataServiceV2 {
       },
     );
 
-    // In the best of the world could consume runner returned value instead of searching in db here
     return this.fieldMetadataRepository.find({
       where: {
         name: In(
