03ce32285683e48aa6ad69a5d303bb106b5ad290
guillim <guigloo@msn.com>
2025-08-19 11:04:57 +0200

    composition pattern on MultiSelectControl

--- packages/twenty-front/src/modules/ui/input/components/MultiSelectControl.tsx
+++ packages/twenty-front/src/modules/ui/input/components/MultiSelectControl.tsx
@@ -1,83 +1,27 @@
-import { type SelectSizeVariant } from '@/ui/input/components/Select';
-import { css, useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
+import {
+  type SelectControlProps,
+  StyledControlContainer,
+  StyledSelectControlIconChevronDown,
+} from '@/ui/input/components/SelectControl';
+import { useTheme } from '@emotion/react';
 import { t } from '@lingui/core/macro';
 import pluralize from 'pluralize';
 import React from 'react';
 import { isDefined } from 'twenty-shared/utils';
 import {
-  IconChevronDown,
   type IconComponent,
   OverflowingTextWithTooltip,
 } from 'twenty-ui/display';
-
-export type SelectControlTextAccent = 'default' | 'placeholder';
-
-const StyledControlContainer = styled.div<{
-  disabled?: boolean;
-  hasIcon: boolean;
-  selectSizeVariant?: SelectSizeVariant;
-  textAccent: SelectControlTextAccent;
-  hasRightElement?: boolean;
-}>`
-  display: grid;
-  grid-template-columns: ${({ hasIcon }) =>
-    hasIcon ? 'auto 1fr auto' : '1fr auto'};
-  align-items: center;
-  gap: ${({ theme }) => theme.spacing(1)};
-  box-sizing: border-box;
-  height: ${({ selectSizeVariant, theme }) =>
-    selectSizeVariant === 'small' ? theme.spacing(6) : theme.spacing(8)};
-  max-width: 100%;
-  padding: 0 ${({ theme }) => theme.spacing(2)};
-  background-color: ${({ theme }) => theme.background.transparent.lighter};
-  border: 1px solid ${({ theme }) => theme.border.color.medium};
-  border-top-left-radius: ${({ theme }) => theme.border.radius.sm};
-  border-bottom-left-radius: ${({ theme }) => theme.border.radius.sm};
-
-  ${({ hasRightElement, theme }) =>
-    !hasRightElement
-      ? css`
-          border-right: auto;
-          border-bottom-right-radius: ${theme.border.radius.sm};
-          border-top-right-radius: ${theme.border.radius.sm};
-        `
-      : css`
-          border-right: none;
-          border-bottom-right-radius: none;
-          border-top-right-radius: none;
-        `}
-
-  color: ${({ disabled, theme, textAccent }) =>
-    disabled
-      ? theme.font.color.tertiary
-      : textAccent === 'default'
-        ? theme.font.color.primary
-        : theme.font.color.tertiary};
-  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
-  text-align: left;
-`;
-
-const StyledIconChevronDown = styled(IconChevronDown)<{
-  disabled?: boolean;
-}>`
-  color: ${({ disabled, theme }) =>
-    disabled ? theme.font.color.extraLight : theme.font.color.tertiary};
-`;
 
 type MultiSelectOptionType = {
   label: string;
   Icon: IconComponent;
 };
 
-type MultiSelectControlProps = {
+type MultiSelectControlProps = Omit<SelectControlProps, 'selectedOption'> & {
   fixedIcon?: IconComponent;
   fixedText?: string;
   selectedOptions: MultiSelectOptionType[];
-  isDisabled?: boolean;
-  selectSizeVariant?: SelectSizeVariant;
-  textAccent?: SelectControlTextAccent;
-  hasRightElement?: boolean;
 };
 
 export const MultiSelectControl = ({
@@ -128,7 +72,10 @@ export const MultiSelectControl = ({
         <OverflowingTextWithTooltip text={firstSelectedOption.label} />
       )}
 
-      <StyledIconChevronDown disabled={isDisabled} size={theme.icon.size.md} />
+      <StyledSelectControlIconChevronDown
+        disabled={isDisabled}
+        size={theme.icon.size.md}
+      />
     </StyledControlContainer>
   );
 };
