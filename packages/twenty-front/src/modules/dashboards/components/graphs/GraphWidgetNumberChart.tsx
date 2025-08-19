478f1fdf97910aac15eaf7744b4b5cf781e31585
ehconitin <nitinkoche03@gmail.com>
2025-08-20 01:43:17 +0530

    DONE

--- packages/twenty-front/src/modules/dashboards/components/graphs/GraphWidgetNumberChart.tsx
+++ packages/twenty-front/src/modules/dashboards/components/graphs/GraphWidgetNumberChart.tsx
@@ -1,36 +1,66 @@
-import { IconTrendingDown, IconTrendingUp } from '@tabler/icons-react';
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import {
+  H1Title,
+  H1TitleFontColor,
+  IconTrendingDown,
+  IconTrendingUp,
+} from 'twenty-ui/display';
 
+// props are subjected to change
 type GraphWidgetNumberChartProps = {
-  label: string;
   value: string;
   trendPercentage: number;
 };
 
-const TrendIndicator = ({ trendPercentage }: { trendPercentage: number }) => {
-  const trendIcon =
-    trendPercentage > 0 ? (
-      <IconTrendingUp color="green" />
-    ) : (
-      <IconTrendingDown color="red" />
-    );
-  return (
-    <div>
-      <span>{trendPercentage}%</span>
-      {trendIcon}
-    </div>
-  );
-};
+const StyledTrendPercentageValue = styled.span`
+  color: ${({ theme }) => theme.font.color.secondary};
+  font-size: ${({ theme }) => theme.font.size.xs};
+  font-weight: ${({ theme }) => theme.font.weight.regular};
+  margin-right: ${({ theme }) => theme.spacing(2)};
+`;
+
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const StyledTrendIconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledH1Title = styled(H1Title)`
+  margin: 0;
+`;
 
 export const GraphWidgetNumberChart = ({
-  label,
   value,
   trendPercentage,
 }: GraphWidgetNumberChartProps) => {
+  const theme = useTheme();
+  const formattedPercentage =
+    trendPercentage >= 0 ? `+${trendPercentage}` : `${trendPercentage}`;
   return (
-    <div>
-      <h3>{label}</h3>
-      <p>{value}</p>
-      <TrendIndicator trendPercentage={trendPercentage} />
-    </div>
+    <StyledContainer>
+      <StyledH1Title title={value} fontColor={H1TitleFontColor.Primary} />
+      <StyledTrendIconContainer>
+        <StyledTrendPercentageValue>
+          {formattedPercentage}%
+        </StyledTrendPercentageValue>
+        {trendPercentage >= 0 ? (
+          <IconTrendingUp
+            color={theme.color.turquoise40}
+            size={theme.icon.size.md}
+          />
+        ) : (
+          // question for product - whats the exact red here? cant see it on figma
+          <IconTrendingDown color={theme.color.red} size={theme.icon.size.md} />
+        )}
+      </StyledTrendIconContainer>
+    </StyledContainer>
   );
 };
