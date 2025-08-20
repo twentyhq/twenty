914388883093895e7e20995c8a315af69d14c948
Charles Bochet <charles@twenty.com>
2025-08-20 16:19:33 +0200

    Fix

--- packages/twenty-front/src/modules/object-metadata/hooks/useGetFieldMetadataItemById.ts
+++ packages/twenty-front/src/modules/object-metadata/hooks/useGetFieldMetadataItemById.ts
@@ -1,27 +1,23 @@
 import { objectMetadataItemsState } from '@/object-metadata/states/objectMetadataItemsState';
-import { useCallback } from 'react';
 import { useRecoilValue } from 'recoil';
 import { isDefined } from 'twenty-shared/utils';
 
 export const useGetFieldMetadataItemById = () => {
   const objectMetadataItems = useRecoilValue(objectMetadataItemsState);
 
-  const getFieldMetadataItemById = useCallback(
-    (fieldMetadataId: string) => {
-      const fieldMetadataItem = objectMetadataItems
-        .flatMap((objectMetadataItem) => objectMetadataItem.fields)
-        .find((field) => field.id === fieldMetadataId);
+  const getFieldMetadataItemById = (fieldMetadataId: string) => {
+    const fieldMetadataItem = objectMetadataItems
+      .flatMap((objectMetadataItem) => objectMetadataItem.fields)
+      .find((field) => field.id === fieldMetadataId);
 
-      if (!isDefined(fieldMetadataItem)) {
-        throw new Error(
-          `Field metadata item not found for id ${fieldMetadataId}`,
-        );
-      }
+    if (!isDefined(fieldMetadataItem)) {
+      throw new Error(
+        `Field metadata item not found for id ${fieldMetadataId}`,
+      );
+    }
 
-      return fieldMetadataItem;
-    },
-    [objectMetadataItems],
-  );
+    return fieldMetadataItem;
+  };
 
   return { getFieldMetadataItemById };
 };
