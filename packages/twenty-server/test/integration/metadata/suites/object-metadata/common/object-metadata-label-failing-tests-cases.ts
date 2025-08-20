7881758c4e8c8d212409b39227985631baae5ea3
prastoin <paul@twenty.com>
2025-08-20 17:16:04 +0200

    chore(server): centralize tests cases

---
+++ packages/twenty-server/test/integration/metadata/suites/object-metadata/common/object-metadata-label-failing-tests-cases.ts
@@ -0,0 +1,45 @@
+import { CreateObjectInput } from 'src/engine/metadata-modules/object-metadata/dtos/create-object.input';
+import { EachTestingContext } from 'twenty-shared/testing';
+
+export const OBJECT_METADATA_LABEL_FAILING_TEST_CASES: EachTestingContext<
+  Partial<Omit<CreateObjectInput, 'workspaceId' | 'dataSourceId'>>
+>[] = [
+  {
+    title: 'when labelSingular is empty',
+    context: { labelSingular: '' },
+  },
+  {
+    title: 'when labelPlural is empty',
+    context: { labelPlural: '' },
+  },
+  {
+    title: 'when labelSingular exceeds maximum length',
+    context: { labelSingular: 'A'.repeat(64) },
+  },
+  {
+    title: 'when labelPlural exceeds maximum length',
+    context: { labelPlural: 'A'.repeat(64) },
+  },
+  {
+    title: 'when labelSingular contains only whitespace',
+    context: { labelSingular: '   ' },
+  },
+  {
+    title: 'when labelPlural contains only whitespace',
+    context: { labelPlural: '   ' },
+  },
+  {
+    title: 'when labels are identical',
+    context: {
+      labelPlural: 'fooBar',
+      labelSingular: 'fooBar',
+    },
+  },
+  {
+    title: 'when labels with whitespaces result to be identical',
+    context: {
+      labelPlural: '      fooBar               ',
+      labelSingular: 'fooBar',
+    },
+  },
+];
