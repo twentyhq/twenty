6698bc00a60b2574d772c32dfae1f4e70494940b
Charles Bochet <charles@twenty.com>
2025-08-20 17:30:15 +0200

    Fix

--- packages/twenty-shared/src/utils/parseJson.ts
+++ packages/twenty-shared/src/utils/parseJson.ts
@@ -1,5 +1,4 @@
 import { isDefined } from "@/utils/validation";
-import { isString } from "@sniptt/guards";
 
 export const parseJson = <T>(rawJson: string | boolean | null | number): T | null => {
   try {
@@ -7,13 +6,9 @@ export const parseJson = <T>(rawJson: string | boolean | null | number): T | nul
       return null;
     }
 
-    if (isString(rawJson)) {
-      return JSON.parse(rawJson);
-    }
-
     // This is a hack to handle the case where the value is a scalar value which is part of JSON spec but not implemented before ES2019
-    return JSON.parse("[" + rawJson + ']')[0];
+    return JSON.parse("[" + rawJson + "]")[0];
   } catch {
-     return JSON.parse("[" + rawJson + "]")[0];
+     return null;
   }
 };
