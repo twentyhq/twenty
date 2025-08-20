d0862f09149949ec0b6ec7d2051a536254f78d44
Weiko <corentin@twenty.com>
2025-08-20 11:17:38 +0200

    fix after merge

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-schema-migration-runner/action-handlers/object/services/object-delete-action.service.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-schema-migration-runner/action-handlers/object/services/object-delete-action.service.ts
@@ -1,6 +1,7 @@
 import { type WorkspaceMigrationActionService } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-schema-migration-runner/core/interfaces/workspace-migration-action-service.interface';
 
 import { FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { isCompositeFlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-composite-flat-field-metadata.util';
 import { isEnumFlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-enum-flat-field-metadata.util';
 import { WorkspaceSchemaManagerService } from 'src/engine/twenty-orm/workspace-schema-manager/workspace-schema-manager.service';
 import { type DeleteObjectAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-object-action-v2';
@@ -39,14 +40,17 @@ export class ObjectDeleteActionService
       tableName,
     });
 
-    const enumFlatFieldMetadatas = Object.values(
+    const enumOrCompositeFlatFieldMetadatas = Object.values(
       flatObjectMetadataWithFlatFieldMaps.fieldsById,
     )
       .filter((field): field is FlatFieldMetadata => field != null)
-      .filter((field) => isEnumFlatFieldMetadata(field));
+      .filter(
+        (field) =>
+          isEnumFlatFieldMetadata(field) || isCompositeFlatFieldMetadata(field),
+      );
 
     const enumOperations = collectEnumOperationsForObject({
-      flatFieldMetadatas: enumFlatFieldMetadatas,
+      flatFieldMetadatas: enumOrCompositeFlatFieldMetadatas,
       tableName,
       operation: EnumOperation.DROP,
     });
