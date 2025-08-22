ed137d6c3cf8611987ba39eb6a86fc2b420170ad
Weiko <corentin@twenty.com>
2025-08-22 11:07:56 +0200

    Create related records after object creation

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/action-handlers/object/services/create-object-action-handler.service.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/action-handlers/object/services/create-object-action-handler.service.ts
@@ -4,10 +4,12 @@ import { WorkspaceMigrationRunnerActionHandler } from 'src/engine/workspace-mana
 
 import { DataSourceEntity } from 'src/engine/metadata-modules/data-source/data-source.entity';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
-import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
 import { isCompositeFlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-composite-flat-field-metadata.util';
 import { isEnumFlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-enum-flat-field-metadata.util';
+import { FlatObjectMetadataWithoutFields } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
 import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
+import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
 import { WorkspaceSchemaManagerService } from 'src/engine/twenty-orm/workspace-schema-manager/workspace-schema-manager.service';
 import { type CreateObjectAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-object-action-v2';
 import { type WorkspaceMigrationActionRunnerArgs } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/types/workspace-migration-action-runner-args.type';
@@ -18,6 +20,9 @@ import {
   EnumOperation,
   executeBatchEnumOperations,
 } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/utils/workspace-schema-enum-operations.util';
+import { FavoriteWorkspaceEntity } from 'src/modules/favorite/standard-objects/favorite.workspace-entity';
+import { ViewFieldWorkspaceEntity } from 'src/modules/view/standard-objects/view-field.workspace-entity';
+import { ViewWorkspaceEntity } from 'src/modules/view/standard-objects/view.workspace-entity';
 
 @Injectable()
 export class CreateObjectActionHandlerService extends WorkspaceMigrationRunnerActionHandler(
@@ -25,6 +30,7 @@ export class CreateObjectActionHandlerService extends WorkspaceMigrationRunnerAc
 ) {
   constructor(
     private readonly workspaceSchemaManagerService: WorkspaceSchemaManagerService,
+    private readonly twentyORMGlobalManager: TwentyORMGlobalManager,
   ) {
     super();
   }
@@ -74,27 +80,26 @@ export class CreateObjectActionHandlerService extends WorkspaceMigrationRunnerAc
     const { action, queryRunner, workspaceId } = context;
     const { flatObjectMetadataWithoutFields, createFieldActions } = action;
 
-    const flatObjectMetadata = flatObjectMetadataWithoutFields;
-
     const { schemaName, tableName } = getWorkspaceSchemaContextForMigration({
       workspaceId,
-      flatObjectMetadata: flatObjectMetadata,
+      flatObjectMetadata: flatObjectMetadataWithoutFields,
     });
 
-    const columnDefinitions = createFieldActions.flatMap((createFieldAction) =>
+    const flatFieldMetadatas = createFieldActions.map(
+      (createFieldAction) => createFieldAction.flatFieldMetadata,
+    );
+
+    const columnDefinitions = flatFieldMetadatas.flatMap((flatFieldMetadata) =>
       generateColumnDefinitions({
-        flatFieldMetadata: createFieldAction.flatFieldMetadata,
-        flatObjectMetadataWithoutFields: flatObjectMetadataWithoutFields,
+        flatFieldMetadata,
+        flatObjectMetadataWithoutFields,
       }),
     );
 
-    const enumOrCompositeFlatFieldMetadatas = createFieldActions
-      .map((createFieldAction) => createFieldAction.flatFieldMetadata)
-      .filter((field): field is FlatFieldMetadata => field != null)
-      .filter(
-        (field) =>
-          isEnumFlatFieldMetadata(field) || isCompositeFlatFieldMetadata(field),
-      );
+    const enumOrCompositeFlatFieldMetadatas = flatFieldMetadatas.filter(
+      (field) =>
+        isEnumFlatFieldMetadata(field) || isCompositeFlatFieldMetadata(field),
+    );
 
     const enumOperations = collectEnumOperationsForObject({
       flatFieldMetadatas: enumOrCompositeFlatFieldMetadatas,
@@ -115,5 +120,90 @@ export class CreateObjectActionHandlerService extends WorkspaceMigrationRunnerAc
       tableName,
       columnDefinitions,
     });
+
+    await this.createObjectRelatedRecords(
+      flatObjectMetadataWithoutFields,
+      flatFieldMetadatas,
+      workspaceId,
+    );
+  }
+
+  public async createObjectRelatedRecords(
+    flatObjectMetadataWithoutFields: FlatObjectMetadataWithoutFields,
+    flatFieldMetadatas: FlatFieldMetadata[],
+    workspaceId: string,
+  ) {
+    const view = await this.createView(
+      flatObjectMetadataWithoutFields,
+      workspaceId,
+    );
+
+    await this.createViewFields(flatFieldMetadatas, view.id, workspaceId);
+    await this.createViewWorkspaceFavorite(workspaceId, view.id);
+  }
+
+  private async createView(
+    flatObjectMetadataWithoutFields: FlatObjectMetadataWithoutFields,
+    workspaceId: string,
+  ): Promise<ViewWorkspaceEntity> {
+    const viewRepository =
+      await this.twentyORMGlobalManager.getRepositoryForWorkspace<ViewWorkspaceEntity>(
+        workspaceId,
+        'view',
+      );
+
+    const createdView = await viewRepository.save({
+      objectMetadataId: flatObjectMetadataWithoutFields.id,
+      type: 'table',
+      name: `All ${flatObjectMetadataWithoutFields.labelPlural}`,
+      key: 'INDEX',
+      icon: 'IconList',
+    });
+
+    return createdView;
+  }
+
+  private async createViewFields(
+    flatFieldMetadatas: FlatFieldMetadata[],
+    viewId: string,
+    workspaceId: string,
+  ): Promise<void> {
+    const viewFieldRepository =
+      await this.twentyORMGlobalManager.getRepositoryForWorkspace<ViewFieldWorkspaceEntity>(
+        workspaceId,
+        'viewField',
+      );
+
+    const viewFields = flatFieldMetadatas
+      .filter((field) => field.name !== 'id' && field.name !== 'deletedAt')
+      .map((field, index) => ({
+        fieldMetadataId: field.id,
+        position: index,
+        isVisible: true,
+        size: 180,
+        viewId: viewId,
+      }));
+
+    await viewFieldRepository.insert(viewFields);
+  }
+
+  private async createViewWorkspaceFavorite(
+    workspaceId: string,
+    viewId: string,
+  ): Promise<void> {
+    const favoriteRepository =
+      await this.twentyORMGlobalManager.getRepositoryForWorkspace<FavoriteWorkspaceEntity>(
+        workspaceId,
+        'favorite',
+      );
+
+    const favoriteCount = await favoriteRepository.count();
+
+    await favoriteRepository.insert(
+      favoriteRepository.create({
+        viewId: viewId,
+        position: favoriteCount,
+      }),
+    );
   }
 }
