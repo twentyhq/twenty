2bcf26e8b0f9a4750637c6aa9f9995919a52b655
omarNaifer12 <o.naifer7@gmail.com>
2025-08-24 12:33:46 +0000

    fix conflict

--- packages/twenty-shared/src/workflow/index.ts
+++ packages/twenty-shared/src/workflow/index.ts
@@ -9,16 +9,20 @@
 
 export { CONTENT_TYPE_VALUES_HTTP_REQUEST } from './constants/contentTypeValuesHttpRequest';
 export { TRIGGER_STEP_ID } from './constants/TriggerStepId';
+<<<<<<< HEAD
 export {
   baseTriggerSchema, baseWorkflowActionSchema, baseWorkflowActionSettingsSchema, objectRecordSchema, workflowActionSchema, workflowAiAgentActionSchema, workflowAiAgentActionSettingsSchema, workflowCodeActionSchema, workflowCodeActionSettingsSchema, workflowCreateRecordActionSchema, workflowCreateRecordActionSettingsSchema, workflowCronTriggerSchema, workflowDatabaseEventTriggerSchema, workflowDeleteRecordActionSchema, workflowDeleteRecordActionSettingsSchema, workflowFilterActionSchema, workflowFilterActionSettingsSchema, workflowFindRecordsActionSchema, workflowFindRecordsActionSettingsSchema, workflowFormActionSchema, workflowFormActionSettingsSchema, workflowHttpRequestActionSchema, workflowHttpRequestActionSettingsSchema, workflowManualTriggerSchema, workflowRunSchema, workflowRunStateSchema, workflowRunStateStepInfoSchema,
   workflowRunStateStepInfosSchema, workflowRunStatusSchema, workflowRunStepStatusSchema, workflowSendEmailActionSchema, workflowSendEmailActionSettingsSchema, workflowTriggerSchema, workflowUpdateRecordActionSchema, workflowUpdateRecordActionSettingsSchema, workflowWebhookTriggerSchema
 } from './schemas/workflow.schema';
 export type { BodyType } from './types/worflowHttpRequestStep';
 export { StepStatus } from './types/WorkflowRunStateStepInfos';
+=======
+export type { BodyType } from './types/workflowHttpRequestStep';
+>>>>>>> 18e9e06dca (fix lint)
 export type {
   WorkflowRunStepInfo,
-  WorkflowRunStepInfos
+  WorkflowRunStepInfos,
 } from './types/WorkflowRunStateStepInfos';
+export { StepStatus } from './types/WorkflowRunStateStepInfos';
 export { bodyParsersHttpRequestStep } from './utils/bodyParsersHttpRequestStep';
 export { getWorkflowRunContext } from './utils/getWorkflowRunContext';
-
