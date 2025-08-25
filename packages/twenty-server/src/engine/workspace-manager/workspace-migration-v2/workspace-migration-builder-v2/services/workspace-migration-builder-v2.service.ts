51749ffecd87bd11ab007df7363c7114ff8a8757
Weiko <corentin@twenty.com>
2025-08-25 16:15:12 +0200

    add favorite creation

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/services/workspace-migration-builder-v2.service.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/services/workspace-migration-builder-v2.service.ts
@@ -10,6 +10,7 @@ import { deletedCreatedUpdatedMatrixDispatcher } from 'src/engine/workspace-mana
 import { WorkspaceMigrationV2FieldActionsBuilderService } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/field/services/workspace-migration-v2-field-actions-builder.service';
 import { buildWorkspaceMigrationIndexActions } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/index/workspace-migration-v2-index-actions-builder';
 import { WorkspaceMigrationV2ObjectActionsBuilderService } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/object/services/workspace-migration-v2-object-actions-builder.service';
+import { buildWorkspaceMigrationViewActions } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/view/workspace-migration-v2-view-actions-builder';
 import { WorkspaceMigrationV2 } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-v2';
 import { computeUpdatedObjectMetadataDeletedCreatedUpdatedFieldMatrix } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/utils/compute-updated-object-metadata-deleted-created-updated-field-matrix.util';
 import { computeUpdatedObjectMetadataDeletedCreatedUpdatedIndexMatrix } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/utils/compute-updated-object-metadata-deleted-created-updated-index-matrix.util';
@@ -118,6 +119,11 @@ export class WorkspaceMigrationBuilderV2Service {
         buildOptions.inferDeletionFromMissingObjectFieldIndex,
     });
 
+    // TODO: Remove this from here when we have a proper builder that handles views diffing independently
+    const viewWorkspaceMigrationActions = buildWorkspaceMigrationViewActions({
+      createdFlatObjectMetadatas, // should be flatViews, not objects.
+    });
+
     const allValidateAndBuildResultFailures = [
       ...objectActionsValidateAndBuildResult.failed,
       ...fieldActionsValidateAndBuildResult.failed,
@@ -143,6 +149,7 @@ export class WorkspaceMigrationBuilderV2Service {
           ...fieldActionsValidateAndBuildResult.updated,
           ...createdObjectMetadataCreateIndexActions,
           ...indexWorkspaceMigrationActions,
+          ...viewWorkspaceMigrationActions,
         ],
       },
     };
