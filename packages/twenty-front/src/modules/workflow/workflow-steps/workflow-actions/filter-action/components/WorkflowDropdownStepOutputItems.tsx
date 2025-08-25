a24ffa8aa160772e00867026ada71b73d08bc8ae
Thomas Trompette <thomas.trompette@sfr.fr>
2025-08-25 19:31:09 +0200

    Create a separated dropdown for step field selection

--- packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/filter-action/components/WorkflowDropdownStepOutputItems.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-steps/workflow-actions/filter-action/components/WorkflowDropdownStepOutputItems.tsx
@@ -4,35 +4,125 @@ import { DropdownMenuSearchInput } from '@/ui/layout/dropdown/components/Dropdow
 import { DropdownMenuSeparator } from '@/ui/layout/dropdown/components/DropdownMenuSeparator';
 import { type StepOutputSchema } from '@/workflow/workflow-variables/types/StepOutputSchema';
 
+import { useGetFieldMetadataItemByIdOrThrow } from '@/object-metadata/hooks/useGetFieldMetadataItemById';
 import { DropdownContent } from '@/ui/layout/dropdown/components/DropdownContent';
 import { DropdownMenuHeaderLeftComponent } from '@/ui/layout/dropdown/components/DropdownMenuHeader/internal/DropdownMenuHeaderLeftComponent';
 import { GenericDropdownContentWidth } from '@/ui/layout/dropdown/constants/GenericDropdownContentWidth';
+import { useWorkflowVersionIdOrThrow } from '@/workflow/hooks/useWorkflowVersionIdOrThrow';
+import { stepsOutputSchemaFamilySelector } from '@/workflow/states/selectors/stepsOutputSchemaFamilySelector';
+import { useUpsertStepFilterSettings } from '@/workflow/workflow-steps/workflow-actions/filter-action/hooks/useUpsertStepFilterSettings';
+import { getViewFilterOperands } from '@/workflow/workflow-steps/workflow-actions/filter-action/utils/getStepFilterOperands';
+import { useVariableDropdown } from '@/workflow/workflow-variables/hooks/useVariableDropdown';
+import { extractRawVariableNamePart } from '@/workflow/workflow-variables/utils/extractRawVariableNamePart';
 import { getCurrentSubStepFromPath } from '@/workflow/workflow-variables/utils/getCurrentSubStepFromPath';
 import { getStepHeaderLabel } from '@/workflow/workflow-variables/utils/getStepHeaderLabel';
 import { getVariableTemplateFromPath } from '@/workflow/workflow-variables/utils/getVariableTemplateFromPath';
 import { isRecordOutputSchema } from '@/workflow/workflow-variables/utils/isRecordOutputSchema';
+import { searchVariableThroughOutputSchema } from '@/workflow/workflow-variables/utils/searchVariableThroughOutputSchema';
 import { useLingui } from '@lingui/react/macro';
+import { useRecoilCallback } from 'recoil';
+import { type StepFilter } from 'twenty-shared/types';
+import { isDefined } from 'twenty-shared/utils';
 import {
   IconChevronLeft,
   OverflowingTextWithTooltip,
   useIcons,
 } from 'twenty-ui/display';
 import { MenuItemSelect } from 'twenty-ui/navigation';
-import { useVariableDropdown } from '../hooks/useVariableDropdown';
 
-type WorkflowVariablesDropdownAllItemsProps = {
+type WorkflowDropdownStepOutputItemsProps = {
+  stepFilter: StepFilter;
   step: StepOutputSchema;
-  onSelect: (value: string) => void;
+  onSelect: () => void;
   onBack: () => void;
 };
 
-export const WorkflowVariablesDropdownAllItems = ({
+export const WorkflowDropdownStepOutputItems = ({
+  stepFilter,
   step,
   onSelect,
   onBack,
-}: WorkflowVariablesDropdownAllItemsProps) => {
+}: WorkflowDropdownStepOutputItemsProps) => {
   const { t } = useLingui();
   const { getIcon } = useIcons();
+
+  const { upsertStepFilterSettings } = useUpsertStepFilterSettings();
+  const { getFieldMetadataItemByIdOrThrow } =
+    useGetFieldMetadataItemByIdOrThrow();
+
+  const workflowVersionId = useWorkflowVersionIdOrThrow();
+
+  const updateStepFilter = useRecoilCallback(
+    ({ snapshot }) =>
+      ({
+        variableName,
+        isFullRecord,
+      }: {
+        variableName: string;
+        isFullRecord: boolean;
+      }) => {
+        const stepId = extractRawVariableNamePart({
+          rawVariableName: variableName,
+          part: 'stepId',
+        });
+        const [currentStepOutputSchema] = snapshot
+          .getLoadable(
+            stepsOutputSchemaFamilySelector({
+              workflowVersionId,
+              stepIds: [stepId],
+            }),
+          )
+          .getValue();
+
+        const { variableType, fieldMetadataId, compositeFieldSubFieldName } =
+          searchVariableThroughOutputSchema({
+            stepOutputSchema: currentStepOutputSchema,
+            rawVariableName: variableName,
+            isFullRecord: false,
+          });
+
+        const { fieldMetadataItem: filterFieldMetadataItem } = isDefined(
+          fieldMetadataId,
+        )
+          ? getFieldMetadataItemByIdOrThrow(fieldMetadataId)
+          : { fieldMetadataItem: undefined };
+
+        const filterType = isDefined(fieldMetadataId)
+          ? (filterFieldMetadataItem?.type ?? 'unknown')
+          : variableType;
+
+        upsertStepFilterSettings({
+          stepFilterToUpsert: {
+            ...stepFilter,
+            stepOutputKey: variableName,
+            isFullRecord,
+            type: filterType ?? 'unknown',
+            value: '',
+            fieldMetadataId,
+            compositeFieldSubFieldName,
+            operand: getViewFilterOperands({
+              filterType,
+              subFieldName: compositeFieldSubFieldName,
+            })?.[0],
+          },
+        });
+      },
+    [
+      workflowVersionId,
+      getFieldMetadataItemByIdOrThrow,
+      upsertStepFilterSettings,
+      stepFilter,
+    ],
+  );
+
+  const handleStepFilterFieldSelect = (key: string) => {
+    updateStepFilter({
+      variableName: key,
+      isFullRecord: false,
+    });
+    onSelect();
+  };
+
   const {
     searchInputValue,
     setSearchInputValue,
@@ -42,7 +132,7 @@ export const WorkflowVariablesDropdownAllItems = ({
     currentPath,
   } = useVariableDropdown({
     step,
-    onSelect,
+    onSelect: handleStepFilterFieldSelect,
     onBack,
   });
 
@@ -63,12 +153,26 @@ export const WorkflowVariablesDropdownAllItems = ({
       return;
     }
 
-    onSelect(
-      getVariableTemplateFromPath({
-        stepId: step.id,
-        path: [...currentPath, currentSubStep.object.fieldIdName],
-      }),
-    );
+    const isRelationField = currentSubStep.object.isRelationField ?? false;
+
+    if (isRelationField) {
+      updateStepFilter({
+        variableName: getVariableTemplateFromPath({
+          stepId: step.id,
+          path: currentPath,
+        }),
+        isFullRecord: true,
+      });
+    } else {
+      updateStepFilter({
+        variableName: getVariableTemplateFromPath({
+          stepId: step.id,
+          path: [...currentPath, currentSubStep.object.fieldIdName],
+        }),
+        isFullRecord: true,
+      });
+    }
+    onSelect();
   };
 
   const displayedSubStepObject = getDisplayedSubStepObject();
