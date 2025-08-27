4401cedc1adf56f24a57b600ed6e8126e40cc8a6
prastoin <paul@twenty.com>
2025-08-27 11:29:18 +0200

    Revert "chore(server): remove is jsonb tool"
    This reverts commit 3f7f8fd43fbee01bb06d35541751a62923cae9b9.

--- packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/utils/compare-two-flat-field-metadata.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/utils/compare-two-flat-field-metadata.util.ts
@@ -8,6 +8,7 @@ import { FLAT_FIELD_METADATA_RELATION_PROPERTIES_TO_COMPARE } from 'src/engine/m
 import { type FlatFieldMetadataPropertiesToCompare } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata-properties-to-compare.type';
 import { type FlatFieldMetadataRelationPropertiesToCompare } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata-relation-properties-to-compare.type';
 import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { isFlatFieldMetadataJsonbProperty } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-flat-field-metadata-jsonb-property.util';
 import { isStandardMetadata } from 'src/engine/metadata-modules/utils/is-standard-metadata.util';
 import { type UpdateFieldAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-field-action-v2';
 import { transformMetadataForComparison } from 'src/engine/workspace-manager/workspace-sync-metadata/comparators/utils/transform-metadata-for-comparison.util';
@@ -80,15 +81,22 @@ export const compareTwoFlatFieldMetadata = ({
       case 'CHANGE': {
         const { oldValue, path, value } = difference;
         const property = path[0] as FlatFieldMetadataPropertiesToCompare;
+        const isJsonb = isFlatFieldMetadataJsonbProperty({
+          flatFieldMetadata: toFlatFieldMetadata,
+          property,
+        });
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const from = parseJson<any>(oldValue);
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const to = parseJson<any>(value);
+        if (isJsonb) {
+          return {
+            from: parseJson(oldValue),
+            to: parseJson(value),
+            property,
+          };
+        }
 
         return {
-          from,
-          to,
+          from: oldValue,
+          to: value,
           property,
         };
       }
--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/compare-two-flat-object-metadata.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/compare-two-flat-object-metadata.util.ts
@@ -5,6 +5,7 @@ import { assertUnreachable, parseJson } from 'twenty-shared/utils';
 
 import { FLAT_OBJECT_METADATA_JSONB_PROPERTIES } from 'src/engine/metadata-modules/flat-object-metadata/constants/flat-object-metadata-jsonb-properties.constant';
 import { FLAT_OBJECT_METADATA_PROPERTIES_TO_COMPARE } from 'src/engine/metadata-modules/flat-object-metadata/constants/flat-object-metadata-properties-to-compare.constant';
+import { type FlatObjectMetadataEntityJsonbProperties } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata-jsonb-properties.type';
 import { type FlatObjectMetadataPropertiesToCompare } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata-properties-to-compare.type';
 import { type FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
 import { isStandardMetadata } from 'src/engine/metadata-modules/utils/is-standard-metadata.util';
@@ -56,15 +57,21 @@ export const compareTwoFlatObjectMetadata = ({
       case 'CHANGE': {
         const { oldValue, path, value } = difference;
         const property = path[0] as FlatObjectMetadataPropertiesToCompare;
+        const isJsonb = FLAT_OBJECT_METADATA_JSONB_PROPERTIES.includes(
+          property as FlatObjectMetadataEntityJsonbProperties,
+        );
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const from = parseJson<any>(oldValue);
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const to = parseJson<any>(value);
+        if (isJsonb) {
+          return {
+            from: parseJson(oldValue),
+            to: parseJson(value),
+            property,
+          };
+        }
 
         return {
-          from,
-          to,
+          from: oldValue,
+          to: value,
           property,
         };
       }
