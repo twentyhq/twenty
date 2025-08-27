a20a748e4c9d287e8871908313153af1c6fd3772
bosiraphael <raphael.bosi@gmail.com>
2025-08-27 12:23:19 +0200

    fix

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/types/metadata-entities-relation-targets.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/types/metadata-entities-relation-targets.type.ts
@@ -1,4 +1,3 @@
-import { type PageLayoutEntity } from 'src/engine/core-modules/page-layout/entities/page-layout.entity';
 import { type DataSourceEntity } from 'src/engine/metadata-modules/data-source/data-source.entity';
 import { type FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { type IndexFieldMetadataEntity } from 'src/engine/metadata-modules/index-metadata/index-field-metadata.entity';
@@ -14,5 +13,4 @@ export type MetadataEntitiesRelationTarget =
   | FieldPermissionEntity
   | DataSourceEntity
   | IndexMetadataEntity
-  | ObjectPermissionEntity
-  | PageLayoutEntity;
+  | ObjectPermissionEntity;
