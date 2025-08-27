fc0a803449921e805e3fc92eef6125e4d8967b82
bosiraphael <raphael.bosi@gmail.com>
2025-08-27 14:23:10 +0200

    Create PageLayoutTabEntity

--- packages/twenty-server/src/engine/core-modules/page-layout/page-layout.module.ts
+++ packages/twenty-server/src/engine/core-modules/page-layout/page-layout.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { PageLayoutTabEntity } from 'src/engine/core-modules/page-layout/entities/page-layout-tab.entity';
 import { PageLayoutEntity } from 'src/engine/core-modules/page-layout/entities/page-layout.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PageLayoutEntity], 'core')],
+  imports: [
+    TypeOrmModule.forFeature([PageLayoutEntity, PageLayoutTabEntity], 'core'),
+  ],
   exports: [],
 })
 export class PageLayoutModule {}
