58db0c9b0ad9f68d3ef5a906ce00bc6d9599b77a
Raphaël Bosi <71827178+bosiraphael@users.noreply.github.com>
2025-08-27 16:12:54 +0200

    Create PageLayoutWidgetEntity (#14106)
    Closes https://github.com/twentyhq/core-team-issues/issues/1391

--- packages/twenty-server/src/engine/core-modules/page-layout/page-layout.module.ts
+++ packages/twenty-server/src/engine/core-modules/page-layout/page-layout.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PageLayoutTabEntity } from 'src/engine/core-modules/page-layout/entities/page-layout-tab.entity';
+import { PageLayoutWidgetEntity } from 'src/engine/core-modules/page-layout/entities/page-layout-widget.entity';
 import { PageLayoutEntity } from 'src/engine/core-modules/page-layout/entities/page-layout.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PageLayoutEntity, PageLayoutTabEntity], 'core'),
+    TypeOrmModule.forFeature(
+      [PageLayoutEntity, PageLayoutTabEntity, PageLayoutWidgetEntity],
+      'core',
+    ),
   ],
   exports: [],
 })
