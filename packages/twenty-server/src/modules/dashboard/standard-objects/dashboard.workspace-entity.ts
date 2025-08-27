d48f25232ae552ecc0fecd579c34b9cf19b95ba3
bosiraphael <raphael.bosi@gmail.com>
2025-08-27 16:33:01 +0200

    create dashboard workspace entity

---
+++ packages/twenty-server/src/modules/dashboard/standard-objects/dashboard.workspace-entity.ts
@@ -0,0 +1,38 @@
+import { msg } from '@lingui/core/macro';
+import { FieldMetadataType } from 'twenty-shared/types';
+
+import { BaseWorkspaceEntity } from 'src/engine/twenty-orm/base.workspace-entity';
+import { WorkspaceEntity } from 'src/engine/twenty-orm/decorators/workspace-entity.decorator';
+import { WorkspaceField } from 'src/engine/twenty-orm/decorators/workspace-field.decorator';
+import { DASHBOARD_STANDARD_FIELD_IDS } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-field-ids';
+import { STANDARD_OBJECT_ICONS } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-object-icons';
+import { STANDARD_OBJECT_IDS } from 'src/engine/workspace-manager/workspace-sync-metadata/constants/standard-object-ids';
+
+@WorkspaceEntity({
+  standardId: STANDARD_OBJECT_IDS.dashboard,
+  namePlural: 'dashboards',
+  labelSingular: msg`Dashboard`,
+  labelPlural: msg`Dashboards`,
+  description: msg`A dashboard`,
+  icon: STANDARD_OBJECT_ICONS.dashboard,
+  labelIdentifierStandardId: DASHBOARD_STANDARD_FIELD_IDS.title,
+})
+export class DashboardWorkspaceEntity extends BaseWorkspaceEntity {
+  @WorkspaceField({
+    standardId: DASHBOARD_STANDARD_FIELD_IDS.title,
+    type: FieldMetadataType.TEXT,
+    label: msg`Title`,
+    description: msg`Dashboard title`,
+    icon: 'IconNotes',
+  })
+  title: string;
+
+  @WorkspaceField({
+    standardId: DASHBOARD_STANDARD_FIELD_IDS.pageLayoutId,
+    type: FieldMetadataType.UUID,
+    label: msg`Page Layout ID`,
+    description: msg`Dashboard page layout`,
+    icon: 'IconLayout',
+  })
+  pageLayoutId: string;
+}
