ecf819f39e1a413923ee8bb18155137ae7bcac4b
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-08-28 13:41:50 +0200

    Should fail

--- packages/twenty-front/src/modules/settings/accounts/components/__stories__/SettingsAccountsCalendarChannelsGeneral.stories.tsx
+++ packages/twenty-front/src/modules/settings/accounts/components/__stories__/SettingsAccountsCalendarChannelsGeneral.stories.tsx
@@ -1,21 +1,12 @@
 import { type Meta, type StoryObj } from '@storybook/react';
 
 import { SettingsAccountsCalendarChannelsGeneral } from '@/settings/accounts/components/SettingsAccountsCalendarChannelsGeneral';
-import { ComponentDecorator } from 'twenty-ui/testing';
-import { I18nFrontDecorator } from '~/testing/decorators/I18nFrontDecorator';
-import { ObjectMetadataItemsDecorator } from '~/testing/decorators/ObjectMetadataItemsDecorator';
-import { SnackBarDecorator } from '~/testing/decorators/SnackBarDecorator';
 
 const meta: Meta<typeof SettingsAccountsCalendarChannelsGeneral> = {
   title:
     'Modules/Settings/Accounts/CalendarChannels/SettingsAccountsCalendarChannelsGeneral',
   component: SettingsAccountsCalendarChannelsGeneral,
-  decorators: [
-    ComponentDecorator,
-    ObjectMetadataItemsDecorator,
-    SnackBarDecorator,
-    I18nFrontDecorator,
-  ],
+  decorators: [],
 };
 
 export default meta;
