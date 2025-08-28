9530b725332913e81686165f3e29eeb62be28b49
prastoin <paul@twenty.com>
2025-08-28 17:30:46 +0200

    feat(server): morph flat field validation

--- packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/validators/utils/validate-relation-or-morph-relation-flat-field-metadata.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/validators/utils/validate-relation-or-morph-relation-flat-field-metadata.util.ts
@@ -1,16 +1,16 @@
 import { t } from '@lingui/core/macro';
-import { type FieldMetadataType } from 'twenty-shared/types';
 import { isDefined, isValidUuid } from 'twenty-shared/utils';
 
 import { FieldMetadataExceptionCode } from 'src/engine/metadata-modules/field-metadata/field-metadata.exception';
+import { RelationFieldMetadataType } from 'src/engine/metadata-modules/field-metadata/types/relation-field-metadata-type.type';
 import { type ValidateOneFieldMetadataArgs } from 'src/engine/metadata-modules/flat-field-metadata/services/flat-field-metadata-validator.service';
 import { type FlatFieldMetadataValidationError } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata-validation-error.type';
 
-export const validateRelationFlatFieldMetadata = async ({
+export const validateRelationOrMorphRelationFlatFieldMetadata = async ({
   existingFlatObjectMetadataMaps,
   flatFieldMetadataToValidate,
   otherFlatObjectMetadataMapsToValidate,
-}: ValidateOneFieldMetadataArgs<FieldMetadataType.RELATION>): Promise<
+}: ValidateOneFieldMetadataArgs<RelationFieldMetadataType>): Promise<
   FlatFieldMetadataValidationError[]
 > => {
   const { relationTargetFieldMetadataId, relationTargetObjectMetadataId } =
