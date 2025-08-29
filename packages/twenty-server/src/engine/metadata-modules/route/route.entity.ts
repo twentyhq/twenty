f189879d680424016991c66264fce255f73ddbaf
martmull <martmull@hotmail.fr>
2025-08-29 15:56:14 +0200

    Add route table

---
+++ packages/twenty-server/src/engine/metadata-modules/route/route.entity.ts
@@ -0,0 +1,65 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+  UpdateDateColumn,
+  Unique,
+} from 'typeorm';
+
+import { SyncableEntity } from 'src/engine/workspace-manager/workspace-sync/interfaces/syncable-entity.interface';
+
+import { ServerlessFunctionEntity } from 'src/engine/metadata-modules/serverless-function/serverless-function.entity';
+
+enum HTTPMethod {
+  Get = 'Get',
+  Post = 'Post',
+  Put = 'Put',
+  Patch = 'Patch',
+  Delete = 'Delete',
+}
+
+@Entity({ name: 'route', schema: 'core' })
+@Unique('IDX_ROUTE_PATH_HTTP_METHOD_WORKSPACE_ID_UNIQUE', [
+  'path',
+  'httpMethod',
+  'workspaceId',
+])
+export class Route extends SyncableEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  path: string;
+
+  @Column({ nullable: false, default: true })
+  isAuthRequired: boolean;
+
+  @Column({
+    type: 'enum',
+    enum: HTTPMethod,
+    default: HTTPMethod.Get,
+    nullable: false,
+  })
+  httpMethod: HTTPMethod;
+
+  @ManyToOne(
+    () => ServerlessFunctionEntity,
+    (serverlessFunction) => serverlessFunction.routes,
+    { onDelete: 'CASCADE' },
+  )
+  @JoinColumn({ name: 'serverlessFunctionId' })
+  serverlessFunction: Relation<ServerlessFunctionEntity>;
+
+  @Column({ nullable: false, type: 'uuid' })
+  workspaceId: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
