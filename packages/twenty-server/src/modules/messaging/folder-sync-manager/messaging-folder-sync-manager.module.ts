58ad46837c7689de60c3d5e012552c042e1ef276
neo773 <neo773@protonmail.com>
2025-08-29 19:50:58 +0530

    remove cron job

--- packages/twenty-server/src/modules/messaging/folder-sync-manager/messaging-folder-sync-manager.module.ts
+++ packages/twenty-server/src/modules/messaging/folder-sync-manager/messaging-folder-sync-manager.module.ts
@@ -5,9 +5,6 @@ import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
 import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
-import { MessagingFolderSyncCronCommand } from 'src/modules/messaging/folder-sync-manager/crons/commands/messaging-folder-sync.cron.command';
-import { MessagingFolderSyncCronJob } from 'src/modules/messaging/folder-sync-manager/crons/jobs/messaging-folder-sync.cron.job';
-import { MessagingFolderSyncJob } from 'src/modules/messaging/folder-sync-manager/jobs/messaging-folder-sync.job';
 import { SyncMessageFoldersService } from 'src/modules/messaging/folder-sync-manager/services/sync-message-folders.service';
 import { MessagingGmailDriverModule } from 'src/modules/messaging/message-import-manager/drivers/gmail/messaging-gmail-driver.module';
 import { MessagingIMAPDriverModule } from 'src/modules/messaging/message-import-manager/drivers/imap/messaging-imap-driver.module';
@@ -23,12 +20,7 @@ import { MessagingMicrosoftDriverModule } from 'src/modules/messaging/message-im
     MessagingMicrosoftDriverModule,
     MessagingIMAPDriverModule,
   ],
-  providers: [
-    SyncMessageFoldersService,
-    MessagingFolderSyncJob,
-    MessagingFolderSyncCronJob,
-    MessagingFolderSyncCronCommand,
-  ],
-  exports: [SyncMessageFoldersService, MessagingFolderSyncCronCommand],
+  providers: [SyncMessageFoldersService],
+  exports: [SyncMessageFoldersService],
 })
 export class MessagingFolderSyncManagerModule {}
