04f1e73d30996cdcb5699a81cc9cc86b9de7baca
ehconitin <nitinkoche03@gmail.com>
2025-09-02 00:25:34 +0530

    fix

--- packages/twenty-front/src/modules/settings/page-layout/hooks/usePageLayoutHandleLayoutChange.ts
+++ packages/twenty-front/src/modules/settings/page-layout/hooks/usePageLayoutHandleLayoutChange.ts
@@ -1,30 +1,32 @@
 import { type Layout, type Layouts } from 'react-grid-layout';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilCallback } from 'recoil';
 import { pageLayoutCurrentLayoutsState } from '../states/pageLayoutCurrentLayoutsState';
 import { pageLayoutDraftState } from '../states/pageLayoutDraftState';
 import { pageLayoutWidgetsState } from '../states/pageLayoutWidgetsState';
 import { convertLayoutsToWidgets } from '../utils/convertLayoutsToWidgets';
 
 export const usePageLayoutHandleLayoutChange = () => {
-  const [, setPageLayoutCurrentLayouts] = useRecoilState(
-    pageLayoutCurrentLayoutsState,
+  const handleLayoutChange = useRecoilCallback(
+    ({ snapshot, set }) =>
+      (_: Layout[], allLayouts: Layouts) => {
+        set(pageLayoutCurrentLayoutsState, allLayouts);
+
+        const pageLayoutWidgets = snapshot
+          .getLoadable(pageLayoutWidgetsState)
+          .getValue();
+
+        const updatedWidgets = convertLayoutsToWidgets(
+          pageLayoutWidgets,
+          allLayouts,
+        );
+
+        set(pageLayoutDraftState, (prev) => ({
+          ...prev,
+          widgets: updatedWidgets,
+        }));
+      },
+    [],
   );
-  const pageLayoutWidgets = useRecoilValue(pageLayoutWidgetsState);
-  const setPageLayoutDraft = useSetRecoilState(pageLayoutDraftState);
-
-  const handleLayoutChange = (_: Layout[], allLayouts: Layouts) => {
-    setPageLayoutCurrentLayouts(allLayouts);
-
-    const updatedWidgets = convertLayoutsToWidgets(
-      pageLayoutWidgets,
-      allLayouts,
-    );
-
-    setPageLayoutDraft((prev) => ({
-      ...prev,
-      widgets: updatedWidgets,
-    }));
-  };
 
   return { handleLayoutChange };
 };
