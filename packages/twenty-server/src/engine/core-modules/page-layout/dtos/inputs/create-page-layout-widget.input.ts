414026d9252ba8aa326c423ac200768965114ed8
bosiraphael <raphael.bosi@gmail.com>
2025-09-05 15:49:28 +0200

    extract GridPositionInput

--- packages/twenty-server/src/engine/core-modules/page-layout/dtos/inputs/create-page-layout-widget.input.ts
+++ packages/twenty-server/src/engine/core-modules/page-layout/dtos/inputs/create-page-layout-widget.input.ts
@@ -4,46 +4,17 @@ import { Type } from 'class-transformer';
 import {
   IsEnum,
   IsNotEmpty,
-  IsNumber,
   IsObject,
   IsOptional,
   IsString,
   IsUUID,
-  Min,
   ValidateNested,
 } from 'class-validator';
 import { GraphQLJSON } from 'graphql-type-json';
 
 import { UUIDScalarType } from 'src/engine/api/graphql/workspace-schema-builder/graphql-types/scalars';
+import { GridPositionInput } from 'src/engine/core-modules/page-layout/dtos/inputs/grid-position.input';
 import { WidgetType } from 'src/engine/core-modules/page-layout/enums/widget-type.enum';
-import { GridPosition } from 'src/engine/core-modules/page-layout/types/grid-position.type';
-
-@InputType('GridPositionInput')
-export class GridPositionInput implements GridPosition {
-  @Field()
-  @IsNumber()
-  @Min(0)
-  @IsNotEmpty()
-  row: number;
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  @IsNotEmpty()
-  column: number;
-
-  @Field()
-  @IsNumber()
-  @Min(1)
-  @IsNotEmpty()
-  rowSpan: number;
-
-  @Field()
-  @IsNumber()
-  @Min(1)
-  @IsNotEmpty()
-  columnSpan: number;
-}
 
 @InputType()
 export class CreatePageLayoutWidgetInput {
