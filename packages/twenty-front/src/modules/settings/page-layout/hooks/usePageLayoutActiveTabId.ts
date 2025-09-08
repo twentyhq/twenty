8fd924bcdf087f1cb46b537a3fe9eb53d33f4dc6
ehconitin <nitinkoche03@gmail.com>
2025-09-08 16:55:08 +0530

    seperate file for const

--- packages/twenty-front/src/modules/settings/page-layout/hooks/usePageLayoutActiveTabId.ts
+++ packages/twenty-front/src/modules/settings/page-layout/hooks/usePageLayoutActiveTabId.ts
@@ -1,18 +1,17 @@
+import { SETTINGS_PAGE_LAYOUT_TABS_INSTANCE_ID } from '@/settings/page-layout/constants/SettingsPageLayoutTabsInstanceId';
 import { activeTabIdComponentState } from '@/ui/layout/tab-list/states/activeTabIdComponentState';
 import { useRecoilComponentValue } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValue';
 import { useSetRecoilState } from 'recoil';
 
-const PAGE_LAYOUT_TABS_INSTANCE_ID = 'page-layout-tabs';
-
 export const usePageLayoutActiveTabId = () => {
   const activeTabId = useRecoilComponentValue(
     activeTabIdComponentState,
-    PAGE_LAYOUT_TABS_INSTANCE_ID,
+    SETTINGS_PAGE_LAYOUT_TABS_INSTANCE_ID,
   );
 
   const setActiveTabId = useSetRecoilState(
     activeTabIdComponentState.atomFamily({
-      instanceId: PAGE_LAYOUT_TABS_INSTANCE_ID,
+      instanceId: SETTINGS_PAGE_LAYOUT_TABS_INSTANCE_ID,
     }),
   );
 
