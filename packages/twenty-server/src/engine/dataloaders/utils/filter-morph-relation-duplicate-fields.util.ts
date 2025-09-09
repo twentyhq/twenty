f21e1a581be7e7f9e14f5ba11708c883a30cf8c7
prastoin <paul@twenty.com>
2025-09-09 12:07:08 +0200

    fix(server): morph relation fields filtering

--- packages/twenty-server/src/engine/dataloaders/utils/filter-morph-relation-duplicate-fields.util.ts
+++ packages/twenty-server/src/engine/dataloaders/utils/filter-morph-relation-duplicate-fields.util.ts
@@ -1,16 +1,54 @@
+import { FieldMetadataType } from 'twenty-shared/types';
+
+import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { isFlatFieldMetadataOfType } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-flat-field-metadata-of-type.util';
 import { transpileToDateIfNot } from 'src/utils/transpile-to-date-if-not.util';
 
-export const filterMorphRelationDuplicateFields = <
-  T extends { createdAt: Date; morphId: string | null },
->(
-  fields: T[],
-) => {
-  return fields.filter((currentField) => {
-    return !fields.some(
-      (otherField) =>
-        otherField.morphId === currentField.morphId &&
-        transpileToDateIfNot(otherField.createdAt).getTime() >
-          transpileToDateIfNot(currentField.createdAt).getTime(),
-    );
-  });
+export const filterMorphRelationDuplicateFields = (
+  flatFieldMetadatas: FlatFieldMetadata[],
+): FlatFieldMetadata[] => {
+  const initialAccumulator: {
+    morphFlatFieldMetadatas: FlatFieldMetadata<FieldMetadataType.MORPH_RELATION>[];
+    otherFlatFieldMetadatas: FlatFieldMetadata[];
+  } = {
+    morphFlatFieldMetadatas: [],
+    otherFlatFieldMetadatas: [],
+  };
+  const { morphFlatFieldMetadatas, otherFlatFieldMetadatas } =
+    flatFieldMetadatas.reduce((acc, flatFieldMetadata) => {
+      if (
+        isFlatFieldMetadataOfType(
+          flatFieldMetadata,
+          FieldMetadataType.MORPH_RELATION,
+        )
+      ) {
+        return {
+          ...acc,
+          morphFlatFieldMetadata: [
+            ...acc.morphFlatFieldMetadatas,
+            flatFieldMetadata,
+          ],
+        };
+      }
+
+      return {
+        ...acc,
+        otherFlatFieldMetadatas: [
+          ...acc.otherFlatFieldMetadatas,
+          flatFieldMetadata,
+        ],
+      };
+    }, initialAccumulator);
+
+  const filteredMorphFlatFieldMetadatas = morphFlatFieldMetadatas.filter(
+    (currentField) =>
+      !morphFlatFieldMetadatas.some(
+        (otherField) =>
+          otherField.morphId === currentField.morphId &&
+          transpileToDateIfNot(otherField.createdAt).getTime() >
+            transpileToDateIfNot(currentField.createdAt).getTime(),
+      ),
+  );
+
+  return [...otherFlatFieldMetadatas, ...filteredMorphFlatFieldMetadatas];
 };
