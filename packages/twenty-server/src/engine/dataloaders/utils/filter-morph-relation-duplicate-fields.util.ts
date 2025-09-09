96057b55177f58866106de3d3cc0dc11b2b6a99b
prastoin <paul@twenty.com>
2025-09-09 09:44:00 +0200

    fix(server): get datetime

--- packages/twenty-server/src/engine/dataloaders/utils/filter-morph-relation-duplicate-fields.util.ts
+++ packages/twenty-server/src/engine/dataloaders/utils/filter-morph-relation-duplicate-fields.util.ts
@@ -1,13 +1,19 @@
+const transpileAsDateIfNot = (dateToVerify: string | Date): Date =>
+  dateToVerify instanceof Date ? dateToVerify : new Date(dateToVerify);
+
 export const filterMorphRelationDuplicateFields = <
   T extends { createdAt: Date; morphId: string | null },
 >(
   fields: T[],
 ) => {
   return fields.filter((currentField) => {
-    return !fields.some(
-      (otherField) =>
+    return !fields.some((otherField) => {
+      console.log(otherField.createdAt);
+      return (
         otherField.morphId === currentField.morphId &&
-        otherField.createdAt.getTime() > currentField.createdAt.getTime(),
-    );
+        transpileAsDateIfNot(otherField.createdAt).getTime() >
+          transpileAsDateIfNot(currentField.createdAt).getTime()
+      );
+    });
   });
 };
