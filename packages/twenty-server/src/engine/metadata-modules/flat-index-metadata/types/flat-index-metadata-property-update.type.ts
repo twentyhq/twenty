dc17854406bb1d632795b85209e0eb58d564e878
prastoin <paul@twenty.com>
2025-09-09 15:28:53 +0200

    refactor(server): wip

---
+++ packages/twenty-server/src/engine/metadata-modules/flat-index-metadata/types/flat-index-metadata-property-update.type.ts
@@ -0,0 +1,10 @@
+import { type FromTo } from 'twenty-shared/types';
+
+import { type FlatIndexMetadataPropertiesToCompare } from 'src/engine/metadata-modules/flat-index-metadata/types/flat-index-metadata-properites-to-compare.type';
+import { type FlatIndexMetadata } from 'src/engine/metadata-modules/flat-index-metadata/types/flat-index-metadata.type';
+
+export type FlatIndexMetadataPropertyUpdate<
+  P extends FlatIndexMetadataPropertiesToCompare,
+> = {
+  property: P;
+} & FromTo<FlatIndexMetadata[P]>;
--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-index-action-v2.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-index-action-v2.ts
@@ -1,3 +1,5 @@
+import { type FlatIndexMetadataPropertiesToCompare } from 'src/engine/metadata-modules/flat-index-metadata/types/flat-index-metadata-properites-to-compare.type';
+import { type FlatIndexMetadataPropertyUpdate } from 'src/engine/metadata-modules/flat-index-metadata/types/flat-index-metadata-property-update.type';
 import { type FlatIndexMetadata } from 'src/engine/metadata-modules/flat-index-metadata/types/flat-index-metadata.type';
 
 export type CreateIndexAction = {
@@ -10,9 +12,20 @@ export type DeleteIndexAction = {
   flatIndexMetadataId: string;
 };
 
+export type UpdateIndexAction = {
+  type: 'update_index';
+  flatIndexMetadataId: string;
+  updates: Array<
+    {
+      [P in FlatIndexMetadataPropertiesToCompare]: FlatIndexMetadataPropertyUpdate<P>;
+    }[FlatIndexMetadataPropertiesToCompare]
+  >;
+};
+
 export type WorkspaceMigrationIndexActionV2 =
   | CreateIndexAction
   | DeleteIndexAction;
+// | UpdateIndexAction; // TODO incoming
 
 export type WorkspaceMigrationIndexActionTypeV2 =
   WorkspaceMigrationIndexActionV2['type'];
