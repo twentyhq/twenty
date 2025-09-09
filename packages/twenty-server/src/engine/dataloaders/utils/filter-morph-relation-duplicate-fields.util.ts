bbd26cc964a9243c169addcc7e31d72444c660fb
prastoin <paul@twenty.com>
2025-09-09 12:12:32 +0200

    fix

--- packages/twenty-server/src/engine/dataloaders/utils/filter-morph-relation-duplicate-fields.util.ts
+++ packages/twenty-server/src/engine/dataloaders/utils/filter-morph-relation-duplicate-fields.util.ts
@@ -24,7 +24,7 @@ export const filterMorphRelationDuplicateFields = (
       ) {
         return {
           ...acc,
-          morphFlatFieldMetadata: [
+          morphFlatFieldMetadatas: [
             ...acc.morphFlatFieldMetadatas,
             flatFieldMetadata,
           ],
