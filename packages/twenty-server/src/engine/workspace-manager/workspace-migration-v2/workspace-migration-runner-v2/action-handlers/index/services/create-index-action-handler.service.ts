a20af40441ab831e45e7c068b50ff988383308d9
prastoin <paul@twenty.com>
2025-09-09 18:16:36 +0200

    feat(server): handle index schema runners

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/action-handlers/index/services/create-index-action-handler.service.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/action-handlers/index/services/create-index-action-handler.service.ts
@@ -4,9 +4,8 @@ import { WorkspaceMigrationRunnerActionHandler } from 'src/engine/workspace-mana
 
 import { findFlatObjectMetadataInFlatObjectMetadataMapsOrThrow } from 'src/engine/metadata-modules/flat-object-metadata-maps/utils/find-flat-object-metadata-in-flat-object-metadata-maps-or-throw.util';
 import { IndexMetadataEntity } from 'src/engine/metadata-modules/index-metadata/index-metadata.entity';
-import { IndexType } from 'src/engine/metadata-modules/index-metadata/types/indexType.types';
-import { FlatIndexFieldMetadata } from 'src/engine/workspace-manager/workspace-migration-v2/types/flat-index-field-metadata';
 import { type CreateIndexAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-index-action-v2';
+import { computeIndexCreationQuery } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/action-handlers/index/utils/compute-index-creation-query.util';
 import { type WorkspaceMigrationActionRunnerArgs } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/types/workspace-migration-action-runner-args.type';
 import { getWorkspaceSchemaContextForMigration } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/utils/get-workspace-schema-context-for-migration.util';
 
@@ -47,24 +46,13 @@ export class CreateIndexActionHandlerService extends WorkspaceMigrationRunnerAct
       workspaceId,
       flatObjectMetadata,
     });
-    // TODO find in cache
-    const relatedFlatIndexFieldMetadatas = [] as FlatIndexFieldMetadata[];
-    const quotedColumns = relatedFlatIndexFieldMetadatas.map(
-      (column) => `"${column}"`,
-    );
-    if (flatIndexMetadata.indexType === IndexType.BTREE) {
-      await queryRunner.query(`
-              CREATE INDEX IF NOT EXISTS "${flatIndexMetadata.name}" ON "${schemaName}"."${tableName}" USING ${flatIndexMetadata.indexType} (${quotedColumns.join(', ')})
-            `);
-    }
+    const createIndexQuery = await computeIndexCreationQuery({
+      flatIndexMetadata,
+      flatObjectMetadataMaps,
+      schemaName,
+      tableName,
+    });
 
-    const isUnique = flatIndexMetadata.isUnique ? 'UNIQUE' : '';
-    const whereClause = flatIndexMetadata.indexWhereClause
-      ? `WHERE ${flatIndexMetadata.indexWhereClause}`
-      : '';
-
-    await queryRunner.query(`
-              CREATE ${isUnique} INDEX IF NOT EXISTS "${flatIndexMetadata.name}" ON "${schemaName}"."${tableName}" (${quotedColumns.join(', ')}) ${whereClause}
-            `);
+    await queryRunner.query(createIndexQuery);
   }
 }
---
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/action-handlers/index/utils/compute-index-creation-query.util.ts
@@ -0,0 +1,30 @@
+import { FlatIndexMetadata } from 'src/engine/metadata-modules/flat-index-metadata/types/flat-index-metadata.type';
+import { FlatObjectMetadataMaps } from 'src/engine/metadata-modules/flat-object-metadata-maps/types/flat-object-metadata-maps.type';
+import { IndexType } from 'src/engine/metadata-modules/index-metadata/types/indexType.types';
+import { FlatIndexFieldMetadata } from 'src/engine/workspace-manager/workspace-migration-v2/types/flat-index-field-metadata';
+import { WorkspaceSchemaContextForMigration } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/utils/get-workspace-schema-context-for-migration.util';
+
+type ComputeIndexCreationQueryArgs = {
+  flatObjectMetadataMaps: FlatObjectMetadataMaps;
+  flatIndexMetadata: FlatIndexMetadata;
+} & WorkspaceSchemaContextForMigration;
+export const computeIndexCreationQuery = async ({
+  flatIndexMetadata,
+  flatObjectMetadataMaps: _,
+  schemaName,
+  tableName,
+}: ComputeIndexCreationQueryArgs): Promise<string> => {
+  const flatIndexFieldMetadatas = [] as FlatIndexFieldMetadata[];
+  // shouldn't we sanitize this ? we can assume it has been validated tho
+  const quotedColumns = flatIndexFieldMetadatas.map((column) => `"${column}"`);
+  if (flatIndexMetadata.indexType === IndexType.BTREE) {
+    return `\nCREATE INDEX IF NOT EXISTS "${flatIndexMetadata.name}" ON "${schemaName}"."${tableName}" USING ${flatIndexMetadata.indexType} (${quotedColumns.join(', ')})\n`;
+  }
+
+  const isUnique = flatIndexMetadata.isUnique ? 'UNIQUE' : '';
+  const whereClause = flatIndexMetadata.indexWhereClause
+    ? `WHERE ${flatIndexMetadata.indexWhereClause}`
+    : '';
+
+  return `\nCREATE ${isUnique} INDEX IF NOT EXISTS "${flatIndexMetadata.name}" ON "${schemaName}"."${tableName}" (${quotedColumns.join(', ')}) ${whereClause}\n`;
+};
