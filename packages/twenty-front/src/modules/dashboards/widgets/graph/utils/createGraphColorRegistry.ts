ceffc82b9cc75d62c9bffee84fbaeead6ff36834
nitin <142569587+ehconitin@users.noreply.github.com>
2025-09-11 14:09:17 +0530

    [Dashboard]- Add GraphWidgetLineChart (#14386)
    closes https://github.com/twentyhq/core-team-issues/issues/1371

--- packages/twenty-front/src/modules/dashboards/widgets/graph/utils/createGraphColorRegistry.ts
+++ packages/twenty-front/src/modules/dashboards/widgets/graph/utils/createGraphColorRegistry.ts
@@ -48,4 +48,36 @@ export const createGraphColorRegistry = (
     },
     solid: theme.color.pink,
   },
+  yellow: {
+    name: 'yellow',
+    gradient: {
+      normal: [theme.adaptiveColors.yellow1, theme.adaptiveColors.yellow2],
+      hover: [theme.adaptiveColors.yellow3, theme.adaptiveColors.yellow4],
+    },
+    solid: theme.color.yellow,
+  },
+  red: {
+    name: 'red',
+    gradient: {
+      normal: [theme.adaptiveColors.red1, theme.adaptiveColors.red2],
+      hover: [theme.adaptiveColors.red3, theme.adaptiveColors.red4],
+    },
+    solid: theme.color.red,
+  },
+  green: {
+    name: 'green',
+    gradient: {
+      normal: [theme.adaptiveColors.green1, theme.adaptiveColors.green2],
+      hover: [theme.adaptiveColors.green3, theme.adaptiveColors.green4],
+    },
+    solid: theme.color.green,
+  },
+  sky: {
+    name: 'sky',
+    gradient: {
+      normal: [theme.adaptiveColors.sky1, theme.adaptiveColors.sky2],
+      hover: [theme.adaptiveColors.sky3, theme.adaptiveColors.sky4],
+    },
+    solid: theme.color.sky,
+  },
 });
