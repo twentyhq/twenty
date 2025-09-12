f63cccfa34215cdd601e772a4f17feabbf072bfc
Marie Stoppa <marie.stoppa@essec.edu>
2025-09-12 14:53:08 +0200

    Remove objectsRecordsPermissions + introduce objectsPermissions

--- packages/twenty-server/src/engine/metadata-modules/permissions/types/user-workspace-permissions.ts
+++ packages/twenty-server/src/engine/metadata-modules/permissions/types/user-workspace-permissions.ts
@@ -1,14 +1,8 @@
-import { type PermissionsOnAllObjectRecords } from 'twenty-shared/constants';
-import {
-  type ObjectsPermissions,
-  type ObjectsPermissionsDeprecated,
-} from 'twenty-shared/types';
+import { type ObjectsPermissions } from 'twenty-shared/types';
 
 import { type PermissionFlagType } from 'src/engine/metadata-modules/permissions/constants/permission-flag-type.constants';
 
 export type UserWorkspacePermissions = {
   permissionFlags: Record<PermissionFlagType, boolean>;
-  objectRecordsPermissions: Record<PermissionsOnAllObjectRecords, boolean>;
-  objectPermissions: ObjectsPermissionsDeprecated;
-  objectsPermissions: ObjectsPermissions; // replaces ObjectsPermissionsDeprecated - deprecation ongoing
+  objectsPermissions: ObjectsPermissions;
 };
