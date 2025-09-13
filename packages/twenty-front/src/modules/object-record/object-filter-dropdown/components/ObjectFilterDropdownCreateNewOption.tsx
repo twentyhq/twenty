c6972c73971c375aeb2ec17d53ce1b744a543a92
animesh65432 <kiranduttta234@gmail.com>
2025-09-14 01:43:01 +0530

    fix types path

--- packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/ObjectFilterDropdownCreateNewOption.tsx
+++ packages/twenty-front/src/modules/object-record/object-filter-dropdown/components/ObjectFilterDropdownCreateNewOption.tsx
@@ -1,7 +1,7 @@
-import { SettingsPath } from '@/types/SettingsPath';
 import { DropdownMenuSeparator } from '@/ui/layout/dropdown/components/DropdownMenuSeparator';
 import { t } from '@lingui/core/macro';
 import { useParams } from 'react-router-dom';
+import { SettingsPath } from 'twenty-shared/types';
 import { IconPlus } from 'twenty-ui/display';
 import { MenuItem } from 'twenty-ui/navigation';
 import { useNavigateSettings } from '~/hooks/useNavigateSettings';
