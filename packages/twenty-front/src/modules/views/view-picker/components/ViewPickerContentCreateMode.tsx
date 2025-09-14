9ec7887d45791fcde5f4ec88deadb0bff33f23d9
Weiko <corentin@twenty.com>
2025-09-14 17:19:24 +0200

    Add calendar view type in picker

--- packages/twenty-front/src/modules/views/view-picker/components/ViewPickerContentCreateMode.tsx
+++ packages/twenty-front/src/modules/views/view-picker/components/ViewPickerContentCreateMode.tsx
@@ -33,9 +33,11 @@ import { viewPickerIsPersistingComponentState } from '@/views/view-picker/states
 import { viewPickerKanbanFieldMetadataIdComponentState } from '@/views/view-picker/states/viewPickerKanbanFieldMetadataIdComponentState';
 import { viewPickerSelectedIconComponentState } from '@/views/view-picker/states/viewPickerSelectedIconComponentState';
 import { viewPickerTypeComponentState } from '@/views/view-picker/states/viewPickerTypeComponentState';
+import { useFeatureFlagsMap } from '@/workspace/hooks/useFeatureFlagsMap';
 import { useLingui } from '@lingui/react/macro';
 import { useMemo, useState } from 'react';
 import { IconX } from 'twenty-ui/display';
+import { FeatureFlagKey } from '~/generated/graphql';
 
 const StyledNoKanbanFieldAvailableContainer = styled.div`
   color: ${({ theme }) => theme.font.color.light};
@@ -82,6 +84,14 @@ export const ViewPickerContentCreateMode = () => {
 
   const { availableFieldsForKanban } = useGetAvailableFieldsForKanban();
 
+  const featureFlags = useFeatureFlagsMap();
+
+  const hasCalendarViewEnabled = featureFlags[FeatureFlagKey.IS_CALENDAR_VIEW_ENABLED];
+
+  const viewPickerTypeOptions = VIEW_PICKER_TYPE_SELECT_OPTIONS.filter((option) => {
+    return option.value !== ViewType.Calendar || hasCalendarViewEnabled;
+  });
+  
   useHotkeysOnFocusedElement({
     keys: [Key.Enter],
     callback: async () => {
@@ -164,7 +174,7 @@ export const ViewPickerContentCreateMode = () => {
               setViewPickerIsDirty(true);
               setViewPickerType(value);
             }}
-            options={VIEW_PICKER_TYPE_SELECT_OPTIONS.map((option) => ({
+            options={viewPickerTypeOptions.map((option) => ({
               ...option,
               label: t(option.label),
             }))}
