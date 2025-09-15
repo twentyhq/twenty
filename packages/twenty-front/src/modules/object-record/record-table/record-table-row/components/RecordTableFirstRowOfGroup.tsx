8fcc6ad1f1066c858bfa7a67234688ce7ce28789
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-09-16 00:02:53 +0200

    Fix lint

--- packages/twenty-front/src/modules/object-record/record-table/record-table-row/components/RecordTableFirstRowOfGroup.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-row/components/RecordTableFirstRowOfGroup.tsx
@@ -26,7 +26,6 @@ export const RecordTableFirstRowOfGroup = forwardRef<
       children,
       recordId,
       isNextRowActiveOrFocused,
-      focusIndex,
       isDragging = false,
       isRowFocusActive,
       isFocused,
