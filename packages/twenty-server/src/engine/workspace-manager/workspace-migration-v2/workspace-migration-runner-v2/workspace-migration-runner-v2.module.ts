fb8b9622e1fac99b4a4c890d45370051cd1c6afa
prastoin <paul@twenty.com>
2025-09-15 18:14:46 +0200

    fix(server): module injection

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-migration-runner-v2.module.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/workspace-migration-runner-v2.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DiscoveryModule } from '@nestjs/core';
 
 import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
+import { FlatEntityMapsCacheModule } from 'src/engine/core-modules/common/services/flat-entity-maps-cache.module';
 import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-flag.module';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
 import { WorkspaceMetadataCacheModule } from 'src/engine/metadata-modules/workspace-metadata-cache/workspace-metadata-cache.module';
@@ -20,6 +21,7 @@ import { WorkspaceMigrationRunnerV2Service } from 'src/engine/workspace-manager/
     WorkspacePermissionsCacheModule,
     WorkspaceMetadataCacheModule,
     WorkspaceSchemaMigrationRunnerActionHandlersModule,
+    FlatEntityMapsCacheModule,
     DiscoveryModule,
   ],
   providers: [
