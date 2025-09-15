7816ace653b9974e3210f9eebeac4dae5031bbc5
prastoin <paul@twenty.com>
2025-09-15 11:03:03 +0200

    remove unused types

--- packages/twenty-server/src/engine/core-modules/view/flat-view/types/failed-flat-view-field-validation-minimal-information.type.ts
+++
@@ -1,5 +0,0 @@
-import { type FlatViewField } from 'src/engine/core-modules/view/flat-view/types/flat-view-field.type';
-
-export type FailedFlatViewFieldValidationMinimalInformation = Partial<
-  Pick<FlatViewField, 'id' | 'fieldMetadataId' | 'viewId'>
->;
