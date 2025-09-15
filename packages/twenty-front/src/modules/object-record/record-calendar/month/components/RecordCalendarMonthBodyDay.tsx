bd0f51811b2699d670b02c68ca9e5a35707efbb9
Weiko <corentin@twenty.com>
2025-09-15 18:17:21 +0200

    Add grid to calendar view (#14516)
    <img width="1254" height="786" alt="Screenshot 2025-09-15 at 18 15 40"
    src="https://github.com/user-attachments/assets/3e95b8b9-f877-4527-9793-2f738939ce19"
    />
    
    Next step: fix padding issue with cards

--- packages/twenty-front/src/modules/object-record/record-calendar/month/components/RecordCalendarMonthBodyDay.tsx
+++ packages/twenty-front/src/modules/object-record/record-calendar/month/components/RecordCalendarMonthBodyDay.tsx
@@ -1,20 +1,36 @@
 import { RecordCalendarCard } from '@/object-record/record-calendar/record-calendar-card/components/RecordCalendarCard';
+import { recordCalendarSelectedDateComponentState } from '@/object-record/record-calendar/states/recordCalendarSelectedDateComponentState';
 import { calendarDayRecordIdsComponentFamilySelector } from '@/object-record/record-calendar/states/selectors/calendarDayRecordsComponentFamilySelector';
 import { useRecoilComponentFamilyValue } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentFamilyValue';
+import { useRecoilComponentValue } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValue';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-const StyledContainer = styled.div`
+const StyledContainer = styled.div<{ isOtherMonth: boolean }>`
   display: flex;
   width: calc(100% / 7);
   flex-direction: column;
   min-height: 122px;
+  padding: ${({ theme }) => theme.spacing(0.5)};
+  background: ${({ theme }) => theme.background.primary};
+
+  &:not(:last-child) {
+    border-right: 0.5px solid ${({ theme }) => theme.border.color.light};
+  }
+
+  ${({ isOtherMonth, theme }) =>
+    isOtherMonth &&
+    css`
+      background: ${theme.background.secondary};
+      color: ${theme.font.color.light};
+    `}
 `;
 
 const StyledDayHeader = styled.div`
   display: flex;
-  width: 100%;
   text-align: right;
   justify-content: flex-end;
+  padding: 7px 5px;
 `;
 
 const StyledCardsContainer = styled.div`
@@ -29,13 +45,19 @@ type RecordCalendarMonthBodyDayProps = {
 export const RecordCalendarMonthBodyDay = ({
   day,
 }: RecordCalendarMonthBodyDayProps) => {
+  const recordCalendarSelectedDate = useRecoilComponentValue(
+    recordCalendarSelectedDateComponentState,
+  );
+
   const recordIds = useRecoilComponentFamilyValue(
     calendarDayRecordIdsComponentFamilySelector,
     day.toDateString(),
   );
 
+  const isOtherMonth = day.getMonth() !== recordCalendarSelectedDate.getMonth();
+
   return (
-    <StyledContainer>
+    <StyledContainer isOtherMonth={isOtherMonth}>
       <StyledDayHeader>{day.getDate()}</StyledDayHeader>
       <StyledCardsContainer>
         {recordIds.slice(0, 5).map((recordId) => (
