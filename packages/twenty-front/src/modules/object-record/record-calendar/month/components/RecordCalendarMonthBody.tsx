106da85899ac9df158d6a549b9e3de0d57d44552
Weiko <corentin@twenty.com>
2025-09-16 17:00:01 +0200

    Add dnd to calendar view (#14539)

--- packages/twenty-front/src/modules/object-record/record-calendar/month/components/RecordCalendarMonthBody.tsx
+++ packages/twenty-front/src/modules/object-record/record-calendar/month/components/RecordCalendarMonthBody.tsx
@@ -1,6 +1,7 @@
 import { RecordCalendarMonthBodyWeek } from '@/object-record/record-calendar/month/components/RecordCalendarMonthBodyWeek';
 import { useRecordCalendarMonthContextOrThrow } from '@/object-record/record-calendar/month/contexts/RecordCalendarMonthContext';
 import styled from '@emotion/styled';
+import { format } from 'date-fns';
 
 const StyledContainer = styled.div`
   display: flex;
@@ -17,7 +18,7 @@ export const RecordCalendarMonthBody = () => {
     <StyledContainer>
       {weekFirstDays.map((weekFirstDay) => (
         <RecordCalendarMonthBodyWeek
-          key={`week-${weekFirstDay.toDateString()}`}
+          key={`week-${format(weekFirstDay, 'yyyy-MM-dd')}`}
           startDayOfWeek={weekFirstDay}
         />
       ))}
