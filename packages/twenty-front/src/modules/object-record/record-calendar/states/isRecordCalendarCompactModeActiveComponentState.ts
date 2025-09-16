e93b32c1e036855c28d2aa479999bb908cc7be64
Weiko <corentin@twenty.com>
2025-09-16 11:46:07 +0200

    remove isRecordCalendarCompactModeActiveComponentState

--- packages/twenty-front/src/modules/object-record/record-calendar/states/isRecordCalendarCompactModeActiveComponentState.ts
+++
@@ -1,9 +0,0 @@
-import { RecordCalendarComponentInstanceContext } from '@/object-record/record-calendar/states/contexts/RecordCalendarComponentInstanceContext';
-import { createComponentState } from '@/ui/utilities/state/component-state/utils/createComponentState';
-
-export const isRecordCalendarCompactModeActiveComponentState =
-  createComponentState<boolean>({
-    key: 'isRecordCalendarCompactModeActiveComponentState',
-    defaultValue: false,
-    componentInstanceContext: RecordCalendarComponentInstanceContext,
-  });
