7fa93a51509e77f11c097b21c48b246b3642e741
neo773 <neo773@protonmail.com>
2025-09-17 17:29:13 +0530

    greptile

--- packages/twenty-server/src/engine/core-modules/outbound-message-domain/outbound-message-domain.module.ts
+++ packages/twenty-server/src/engine/core-modules/outbound-message-domain/outbound-message-domain.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { NestjsQueryTypeOrmModule } from '@ptc-org/nestjs-query-typeorm';
 
+import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 import { AwsSesClientProvider } from 'src/engine/core-modules/outbound-message-domain/drivers/aws-ses/providers/aws-ses-client.provider';
 import { AwsSesHandleErrorService } from 'src/engine/core-modules/outbound-message-domain/drivers/aws-ses/services/aws-ses-handle-error.service';
 import { OutboundMessageDomainDriverFactory } from 'src/engine/core-modules/outbound-message-domain/drivers/outbound-message-domain-driver.factory';
@@ -11,7 +12,8 @@ import { OutboundMessageDomainService } from 'src/engine/core-modules/outbound-m
 
 @Module({
   imports: [
-    NestjsQueryTypeOrmModule.forFeature([OutboundMessageDomain], 'core'),
+    TypeORMModule,
+    NestjsQueryTypeOrmModule.forFeature([OutboundMessageDomain]),
   ],
   exports: [OutboundMessageDomainService],
   providers: [
