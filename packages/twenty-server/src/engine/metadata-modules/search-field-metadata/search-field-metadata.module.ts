b355ec063aa72b752c55a818a0835144288a4bae
Abdullah. <125115953+mabdullahabaid@users.noreply.github.com>
2025-09-19 03:39:56 +0500

    fix: remove search-field-metadata service as requested

--- packages/twenty-server/src/engine/metadata-modules/search-field-metadata/search-field-metadata.module.ts
+++ packages/twenty-server/src/engine/metadata-modules/search-field-metadata/search-field-metadata.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { SearchFieldMetadataEntity } from 'src/engine/metadata-modules/search-field-metadata/search-field-metadata.entity';
-import { SearchFieldMetadataService } from 'src/engine/metadata-modules/search-field-metadata/search-field-metadata.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SearchFieldMetadataEntity])],
-  providers: [SearchFieldMetadataService],
-  exports: [SearchFieldMetadataService],
+  providers: [],
+  exports: [],
 })
 export class SearchFieldMetadataModule {}
