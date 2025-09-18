844f2d2f6527788c165c60e32ad2d5ce721e2ce4
Nguyen Tinh <tinhnq@phanmemmkt.vn>
2025-09-18 10:59:24 +0700

    refactor: enhance i18n middleware with locale handling comments
    - Added comments to clarify locale handling logic in the I18nMiddleware.
    - Explained the priority of user-specific locales and default language settings for logged-in and non-logged-in users.

--- packages/twenty-server/src/engine/core-modules/i18n/i18n.middleware.ts
+++ packages/twenty-server/src/engine/core-modules/i18n/i18n.middleware.ts
@@ -7,14 +7,15 @@ import { APP_LOCALES, SOURCE_LOCALE } from 'twenty-shared/translations';
 @Injectable()
 export class I18nMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    //  Khi đã login dùng local trong table userWorkspace,workspaceMember (Độ ưu tiên sẽ cao hơn x-locale)
+    // Ngôn ngữ mặc định là en
+    //  Khi chưa login ngôn ngữ được set bằng x-locale gửi kèm trong header (Định dạng: vi-VN) 
     const locale = req.locale;
-
     if (locale && Object.values(APP_LOCALES).includes(locale)) {
       i18n.activate(locale);
     } else {
       i18n.activate(SOURCE_LOCALE);
     }
-
     next();
   }
 }
