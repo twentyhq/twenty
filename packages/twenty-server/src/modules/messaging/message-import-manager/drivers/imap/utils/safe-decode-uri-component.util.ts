0d4327caea4b20095f21eb9991f167ee418dc4df
neo773 <neo773@protonmail.com>
2025-09-20 22:27:10 +0530

    remove eslint line

--- packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/utils/safe-decode-uri-component.util.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/utils/safe-decode-uri-component.util.ts
@@ -1,8 +1,7 @@
 export const safeDecodeURIComponent = (text: string): string => {
   try {
     return decodeURIComponent(text);
-    // eslint-disable-next-line unused-imports/no-unused-vars
-  } catch (error) {
+  } catch {
     return text;
   }
 };
