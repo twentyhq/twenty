7bc597c36203d1802811a86687ce5f244310a9ba
Weiko <corentin@twenty.com>
2025-09-22 16:20:30 +0200

    Enable workspace migration v2 for dev workspace

--- packages/twenty-server/src/engine/workspace-manager/dev-seeder/core/utils/seed-feature-flags.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/dev-seeder/core/utils/seed-feature-flags.util.ts
@@ -53,7 +53,7 @@ export const seedFeatureFlags = async (
       {
         key: FeatureFlagKey.IS_WORKSPACE_MIGRATION_V2_ENABLED,
         workspaceId: workspaceId,
-        value: false,
+        value: true,
       },
       {
         key: FeatureFlagKey.IS_PAGE_LAYOUT_ENABLED,
