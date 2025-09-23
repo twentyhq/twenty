7cb294169dc8eefbb0901608707b427a281eebe3
Raphaël Bosi <71827178+bosiraphael@users.noreply.github.com>
2025-09-23 11:00:36 +0200

    Add a new graphql mutation in the pageLayout resolver to handle page layout update with tabs and widgets (#14612)
    Add a new graphql mutation in the pageLayout resolver to handle page
    layout update with tabs and widgets.
    
    Later we will add validation inside the service to check if the page
    layout tab structure is correct (for instance check widgets don't
    overlap or aren't out of the grid, or to check that they have a correct
    configuration ...).
    
    This mutation will be plugged to the save action of the dashboards in
    the frontend.

--- packages/twenty-front/src/generated/graphql.ts
+++ packages/twenty-front/src/generated/graphql.ts
@@ -1544,6 +1544,7 @@ export type Mutation = {
   updatePageLayout: PageLayout;
   updatePageLayoutTab: PageLayoutTab;
   updatePageLayoutWidget: PageLayoutWidget;
+  updatePageLayoutWithTabsAndWidgets: PageLayout;
   updatePasswordViaResetToken: InvalidatePassword;
   updateWebhook?: Maybe<Webhook>;
   updateWorkflowRunStep: WorkflowAction;
@@ -2217,6 +2218,12 @@ export type MutationUpdatePageLayoutTabArgs = {
 export type MutationUpdatePageLayoutWidgetArgs = {
   id: Scalars['String'];
   input: UpdatePageLayoutWidgetInput;
+};
+
+
+export type MutationUpdatePageLayoutWithTabsAndWidgetsArgs = {
+  id: Scalars['String'];
+  input: UpdatePageLayoutWithTabsInput;
 };
 
 
@@ -3448,12 +3455,36 @@ export type UpdatePageLayoutTabInput = {
   title?: InputMaybe<Scalars['String']>;
 };
 
+export type UpdatePageLayoutTabWithWidgetsInput = {
+  id: Scalars['UUID'];
+  position: Scalars['Float'];
+  title: Scalars['String'];
+  widgets: Array<UpdatePageLayoutWidgetWithIdInput>;
+};
+
 export type UpdatePageLayoutWidgetInput = {
   configuration?: InputMaybe<Scalars['JSON']>;
   gridPosition?: InputMaybe<GridPositionInput>;
   objectMetadataId?: InputMaybe<Scalars['UUID']>;
   title?: InputMaybe<Scalars['String']>;
   type?: InputMaybe<WidgetType>;
+};
+
+export type UpdatePageLayoutWidgetWithIdInput = {
+  configuration: Scalars['JSON'];
+  gridPosition: GridPositionInput;
+  id: Scalars['UUID'];
+  objectMetadataId?: InputMaybe<Scalars['UUID']>;
+  pageLayoutTabId: Scalars['UUID'];
+  title: Scalars['String'];
+  type: WidgetType;
+};
+
+export type UpdatePageLayoutWithTabsInput = {
+  name: Scalars['String'];
+  objectMetadataId: Scalars['UUID'];
+  tabs: Array<UpdatePageLayoutTabWithWidgetsInput>;
+  type: PageLayoutType;
 };
 
 export type UpdateRoleInput = {
