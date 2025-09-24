01c4d187850f42c80023f509e8ede2e259e0c6d9
Devessier <baptiste@devessier.fr>
2025-09-24 11:51:26 +0200

    feat: add label and loop handle

--- packages/twenty-front/src/modules/workflow/workflow-diagram/workflow-edges/components/WorkflowDiagramDefaultEdgeRun.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/workflow-edges/components/WorkflowDiagramDefaultEdgeRun.tsx
@@ -1,6 +1,11 @@
+import { WorkflowDiagramEdgeLabel } from '@/workflow/workflow-diagram/workflow-edges/components/WorkflowDiagramEdgeLabel';
+import { WorkflowDiagramEdgeLabelContainer } from '@/workflow/workflow-diagram/workflow-edges/components/WorkflowDiagramEdgeLabelContainer';
 import { WorkflowRunDiagramBaseEdge } from '@/workflow/workflow-diagram/workflow-edges/components/WorkflowRunDiagramBaseEdge';
 import { type WorkflowDiagramEdgeComponentProps } from '@/workflow/workflow-diagram/workflow-edges/types/WorkflowDiagramEdgeComponentProps';
 import { getEdgePath } from '@/workflow/workflow-diagram/workflow-edges/utils/getEdgePath';
+import { useLingui } from '@lingui/react/macro';
+import { EdgeLabelRenderer } from '@xyflow/react';
+import { isDefined } from 'twenty-shared/utils';
 
 type WorkflowDiagramDefaultEdgeRunProps = WorkflowDiagramEdgeComponentProps;
 
@@ -15,6 +20,8 @@ export const WorkflowDiagramDefaultEdgeRun = ({
   markerEnd,
   data,
 }: WorkflowDiagramDefaultEdgeRunProps) => {
+  const { i18n } = useLingui();
+
   const { segments } = getEdgePath({
     sourceX,
     sourceY,
@@ -37,6 +44,18 @@ export const WorkflowDiagramDefaultEdgeRun = ({
           markerEnd={segment.markerEnd}
         />
       ))}
+
+      <EdgeLabelRenderer>
+        {isDefined(data?.labelOptions) && (
+          <WorkflowDiagramEdgeLabelContainer
+            sourceX={sourceX}
+            sourceY={sourceY}
+            position={data.labelOptions.position}
+          >
+            <WorkflowDiagramEdgeLabel label={i18n._(data.labelOptions.label)} />
+          </WorkflowDiagramEdgeLabelContainer>
+        )}
+      </EdgeLabelRenderer>
     </>
   );
 };
