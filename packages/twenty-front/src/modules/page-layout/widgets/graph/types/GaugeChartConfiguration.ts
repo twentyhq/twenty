da2417db11545e3e45a8e57acf13716c5450e6af
ehconitin <nitinkoche03@gmail.com>
2025-09-25 18:33:58 +0530

    grep

--- packages/twenty-front/src/modules/page-layout/widgets/graph/types/GaugeChartConfiguration.ts
+++ packages/twenty-front/src/modules/page-layout/widgets/graph/types/GaugeChartConfiguration.ts
@@ -1,13 +1,9 @@
-import { type RecordGqlOperationFilter } from '@/object-record/graphql/types/RecordGqlOperationFilter';
 import { type AggregateOperations } from '@/object-record/record-table/constants/AggregateOperations';
+import { type BaseGraphConfiguration } from '@/page-layout/widgets/graph/types/BaseGraphConfiguration';
 import { type GraphType } from '@/page-layout/widgets/graph/types/GraphType';
 
-export type GaugeChartConfiguration = {
+export type GaugeChartConfiguration = Omit<BaseGraphConfiguration, 'color'> & {
   graphType: GraphType.GAUGE;
-  aggregateOperation: AggregateOperations;
-  aggregateFieldMetadataId: string;
   aggregateOperationTotal: AggregateOperations;
   aggregateFieldMetadataIdTotal: string;
-  filter?: RecordGqlOperationFilter;
-  description?: string;
 };
