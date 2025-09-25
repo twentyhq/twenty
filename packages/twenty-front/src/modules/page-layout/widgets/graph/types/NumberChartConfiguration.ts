a07f016b354d9d53ac238b751dd90c83311bed4a
ehconitin <nitinkoche03@gmail.com>
2025-09-25 18:30:07 +0530

    missing

--- packages/twenty-front/src/modules/page-layout/widgets/graph/types/NumberChartConfiguration.ts
+++ packages/twenty-front/src/modules/page-layout/widgets/graph/types/NumberChartConfiguration.ts
@@ -1,6 +1,6 @@
 import { type RecordGqlOperationFilter } from '@/object-record/graphql/types/RecordGqlOperationFilter';
 import { type AggregateOperations } from '@/object-record/record-table/constants/AggregateOperations';
-import { GraphType } from '@/page-layout/widgets/graph/types/GraphType';
+import { type GraphType } from '@/page-layout/widgets/graph/types/GraphType';
 
 export type NumberChartConfiguration = {
   graphType: GraphType.NUMBER;
