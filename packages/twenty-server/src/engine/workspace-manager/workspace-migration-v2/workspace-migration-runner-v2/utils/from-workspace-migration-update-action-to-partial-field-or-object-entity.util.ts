188d66909d5ab9d542c27477572256533e3e09f1
Weiko <corentin@twenty.com>
2025-09-25 14:05:01 +0200

    Add ServerlessFunction to migration v2 (#14698)

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/utils/from-workspace-migration-update-action-to-partial-field-or-object-entity.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-runner-v2/utils/from-workspace-migration-update-action-to-partial-field-or-object-entity.util.ts
@@ -1,14 +1,5 @@
-import { type UpdateFieldAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-field-action-v2';
-import { type UpdateObjectAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-object-action-v2';
-import { type UpdateViewAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-view-action-v2.type';
-import { type UpdateViewFieldAction } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-view-field-action-v2.type';
-
 export const fromWorkspaceMigrationUpdateActionToPartialEntity = <
-  T extends
-    | UpdateFieldAction
-    | UpdateObjectAction
-    | UpdateViewAction
-    | UpdateViewFieldAction,
+  T extends { updates: Array<{ property: string; to: unknown }> },
 >(
   action: T,
 ) => {
