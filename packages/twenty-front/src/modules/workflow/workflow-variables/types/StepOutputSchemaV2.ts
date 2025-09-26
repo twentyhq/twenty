051df4835f511f7896392626ab1e53423b918a02
Thomas Trompette <thomas.trompette@sfr.fr>
2025-09-26 15:24:55 +0200

    Add search for iterators

--- packages/twenty-front/src/modules/workflow/workflow-variables/types/StepOutputSchemaV2.ts
+++ packages/twenty-front/src/modules/workflow/workflow-variables/types/StepOutputSchemaV2.ts
@@ -6,6 +6,7 @@ import { type BaseOutputSchemaV2 } from '@/workflow/workflow-variables/types/Bas
 import { type CodeOutputSchema } from '@/workflow/workflow-variables/types/CodeOutputSchema';
 import { type FindRecordsOutputSchema } from '@/workflow/workflow-variables/types/FindRecordsOutputSchema';
 import { type FormOutputSchema } from '@/workflow/workflow-variables/types/FormOutputSchema';
+import { type IteratorOutputSchema } from '@/workflow/workflow-variables/types/IteratorOutputSchema';
 import { type ManualTriggerOutputSchema } from '@/workflow/workflow-variables/types/ManualTriggerOutputSchema';
 import { type RecordOutputSchemaV2 } from '@/workflow/workflow-variables/types/RecordOutputSchemaV2';
 
@@ -15,7 +16,8 @@ export type OutputSchemaV2 =
   | FindRecordsOutputSchema
   | FormOutputSchema
   | RecordOutputSchemaV2
-  | ManualTriggerOutputSchema;
+  | ManualTriggerOutputSchema
+  | IteratorOutputSchema;
 
 export type StepOutputSchemaV2 = {
   id: string;
