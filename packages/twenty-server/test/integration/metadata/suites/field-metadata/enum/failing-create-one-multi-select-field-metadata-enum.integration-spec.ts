43cc3e8ff8d51fc6fc8a46a317832d98f2423a60
prastoin <paul@twenty.com>
2025-09-26 12:11:52 +0200

    restore wrongly removed file

--- packages/twenty-server/test/integration/metadata/suites/field-metadata/enum/failing-create-one-multi-select-field-metadata-enum.integration-spec.ts
+++ packages/twenty-server/test/integration/metadata/suites/field-metadata/enum/failing-create-one-multi-select-field-metadata-enum.integration-spec.ts
@@ -1,10 +1,10 @@
 import { CREATE_ENUM_FIELD_METADATA_TEST_CASES } from 'test/integration/metadata/suites/field-metadata/enum/common/create-enum-field-metadata-test-cases';
 import { createOneFieldMetadata } from 'test/integration/metadata/suites/field-metadata/utils/create-one-field-metadata.util';
+import { createOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/create-one-object-metadata.util';
 import { deleteOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/delete-one-object-metadata.util';
 import { updateFeatureFlag } from 'test/integration/metadata/suites/utils/update-feature-flag.util';
-import { isDefined } from 'twenty-shared/utils';
-import { createOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/create-one-object-metadata.util';
 import { FieldMetadataType } from 'twenty-shared/types';
+import { isDefined } from 'twenty-shared/utils';
 
 import { FeatureFlagKey } from 'src/engine/core-modules/feature-flag/enums/feature-flag-key.enum';
 
