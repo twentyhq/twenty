301f15311be2307b63c1d89d4df3c041743fabf6
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-09-26 15:02:39 +0200

    Fixed body loading

--- packages/twenty-front/src/modules/object-record/record-table/record-table-body/components/RecordTableNoRecordGroupBody.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/record-table-body/components/RecordTableNoRecordGroupBody.tsx
@@ -1,7 +1,9 @@
+import { recordIndexHasRecordsComponentSelector } from '@/object-record/record-index/states/selectors/recordIndexHasRecordsComponentSelector';
 import { RecordTableNoRecordGroupBodyContextProvider } from '@/object-record/record-table/components/RecordTableNoRecordGroupBodyContextProvider';
 import { RecordTableNoRecordGroupRows } from '@/object-record/record-table/components/RecordTableNoRecordGroupRows';
 import { RecordTableBodyDragDropContextProvider } from '@/object-record/record-table/record-table-body/components/RecordTableBodyDragDropContextProvider';
 import { RecordTableBodyDroppable } from '@/object-record/record-table/record-table-body/components/RecordTableBodyDroppable';
+import { RecordTableBodyLoading } from '@/object-record/record-table/record-table-body/components/RecordTableBodyLoading';
 import { RecordTableCellPortals } from '@/object-record/record-table/record-table-cell/components/RecordTableCellPortals';
 import { RecordTableAggregateFooter } from '@/object-record/record-table/record-table-footer/components/RecordTableAggregateFooter';
 import { isRecordTableInitialLoadingComponentState } from '@/object-record/record-table/states/isRecordTableInitialLoadingComponentState';
@@ -9,17 +11,17 @@ import { RecordTableVirtualizationEffect } from '@/object-record/record-table/vi
 import { useRecoilComponentValue } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValue';
 
 export const RecordTableNoRecordGroupBody = () => {
-  // const recordTableHasRecords = useRecoilComponentValue(
-  //   recordIndexHasRecordsComponentSelector,
-  // );
+  const recordTableHasRecords = useRecoilComponentValue(
+    recordIndexHasRecordsComponentSelector,
+  );
 
   const isRecordTableInitialLoading = useRecoilComponentValue(
     isRecordTableInitialLoadingComponentState,
   );
 
-  // if (isRecordTableInitialLoading && !recordTableHasRecords) {
-  //   return <RecordTableBodyLoading />;
-  // }
+  if (isRecordTableInitialLoading && !recordTableHasRecords) {
+    return <RecordTableBodyLoading />;
+  }
 
   return (
     <RecordTableNoRecordGroupBodyContextProvider>
