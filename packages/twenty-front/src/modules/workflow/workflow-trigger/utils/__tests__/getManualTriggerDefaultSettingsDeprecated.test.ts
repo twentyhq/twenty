dafe02b28f20ec43cca31359362e98fce3cb3648
Thomas Trompette <thomas.trompette@sfr.fr>
2025-09-26 16:28:03 +0200

    Allow switch between previous and old availability

--- packages/twenty-front/src/modules/workflow/workflow-trigger/utils/__tests__/getManualTriggerDefaultSettingsDeprecated.test.ts
+++ packages/twenty-front/src/modules/workflow/workflow-trigger/utils/__tests__/getManualTriggerDefaultSettingsDeprecated.test.ts
@@ -3,57 +3,71 @@ import { COMMAND_MENU_DEFAULT_ICON } from '@/workflow/workflow-trigger/constants
 import { generatedMockObjectMetadataItems } from '~/testing/utils/generatedMockObjectMetadataItems';
 import { getManualTriggerDefaultSettingsDeprecated } from '../getManualTriggerDefaultSettingsDeprecated';
 
-it('returns settings for a manual trigger that can be activated from any where', () => {
-  expect(
-    getManualTriggerDefaultSettingsDeprecated({
-      availability: 'EVERYWHERE',
-      activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
-    }),
-  ).toStrictEqual({
-    objectType: undefined,
-    outputSchema: {},
-    icon: COMMAND_MENU_DEFAULT_ICON,
-    isPinned: false,
+describe('getManualTriggerDefaultSettingsDeprecated', () => {
+  it('returns settings for a manual trigger that can be activated from any where', () => {
+    expect(
+      getManualTriggerDefaultSettingsDeprecated({
+        availability: 'EVERYWHERE',
+        activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
+      }),
+    ).toStrictEqual({
+      objectType: undefined,
+      outputSchema: {},
+      icon: COMMAND_MENU_DEFAULT_ICON,
+      isPinned: false,
+      availability: {
+        type: 'GLOBAL',
+        locations: [],
+      },
+    });
   });
-});
 
-it('returns settings for a manual trigger that can be activated from any where', () => {
-  expect(
-    getManualTriggerDefaultSettingsDeprecated({
-      availability: 'WHEN_RECORD_SELECTED',
-      activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
+  it('returns settings for a manual trigger that can be activated from any where', () => {
+    expect(
+      getManualTriggerDefaultSettingsDeprecated({
+        availability: 'WHEN_RECORD_SELECTED',
+        activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
+        icon: 'IconTest',
+      }),
+    ).toStrictEqual({
+      objectType: generatedMockObjectMetadataItems[0].nameSingular,
+      outputSchema: {},
       icon: 'IconTest',
-    }),
-  ).toStrictEqual({
-    objectType: generatedMockObjectMetadataItems[0].nameSingular,
-    outputSchema: {},
-    icon: 'IconTest',
-    isPinned: false,
+      isPinned: false,
+      availability: {
+        type: 'SINGLE_RECORD',
+        objectNameSingular: generatedMockObjectMetadataItems[0].nameSingular,
+      },
+    });
   });
-});
 
-it('returns settings for WHEN_RECORD_SELECTED with default icon when no custom icon provided', () => {
-  expect(
-    getManualTriggerDefaultSettingsDeprecated({
-      availability: 'WHEN_RECORD_SELECTED',
-      activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
-    }),
-  ).toStrictEqual({
-    objectType: generatedMockObjectMetadataItems[0].nameSingular,
-    outputSchema: {},
-    icon: COMMAND_MENU_DEFAULT_ICON,
-    isPinned: false,
+  it('returns settings for WHEN_RECORD_SELECTED with default icon when no custom icon provided', () => {
+    expect(
+      getManualTriggerDefaultSettingsDeprecated({
+        availability: 'WHEN_RECORD_SELECTED',
+        activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
+      }),
+    ).toStrictEqual({
+      objectType: generatedMockObjectMetadataItems[0].nameSingular,
+      outputSchema: {},
+      icon: COMMAND_MENU_DEFAULT_ICON,
+      isPinned: false,
+      availability: {
+        type: 'SINGLE_RECORD',
+        objectNameSingular: generatedMockObjectMetadataItems[0].nameSingular,
+      },
+    });
   });
-});
 
-it('throws error for unsupported availability type', () => {
-  const invalidAvailability =
-    'INVALID_AVAILABILITY' as WorkflowManualTriggerAvailability;
+  it('throws error for unsupported availability type', () => {
+    const invalidAvailability =
+      'INVALID_AVAILABILITY' as WorkflowManualTriggerAvailability;
 
-  expect(() =>
-    getManualTriggerDefaultSettingsDeprecated({
-      availability: invalidAvailability,
-      activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
-    }),
-  ).toThrow("Didn't expect to get here.");
+    expect(() =>
+      getManualTriggerDefaultSettingsDeprecated({
+        availability: invalidAvailability,
+        activeNonSystemObjectMetadataItems: generatedMockObjectMetadataItems,
+      }),
+    ).toThrow("Didn't expect to get here.");
+  });
 });
