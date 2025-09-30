f7cd18820adad2590e89ebaf75d992a36efa9587
guillim <guigloo@msn.com>
2025-09-30 13:58:28 +0200

    renaming + picker fix

--- packages/twenty-front/src/modules/object-record/record-field/ui/meta-types/input/hooks/useOpenMorphRelationOneToManyFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/ui/meta-types/input/hooks/useOpenMorphRelationOneToManyFieldInput.tsx
@@ -16,13 +16,13 @@ import { FocusComponentType } from '@/ui/utilities/focus/types/FocusComponentTyp
 import { useRecoilCallback } from 'recoil';
 import { isDefined } from 'twenty-shared/utils';
 
-export const useOpenMorphRelationFromManyFieldInput = () => {
+export const useOpenMorphRelationOneToManyFieldInput = () => {
   const { performSearch } = useMultipleRecordPickerPerformSearch();
   const { openMultipleRecordPicker } = useMultipleRecordPickerOpen();
 
   const { pushFocusItemToFocusStack } = usePushFocusItemToFocusStack();
 
-  const openMorphRelationFromManyFieldInput = useRecoilCallback(
+  const openMorphRelationOneToManyFieldInput = useRecoilCallback(
     ({ set, snapshot }) =>
       ({
         recordId,
@@ -136,5 +136,7 @@ export const useOpenMorphRelationFromManyFieldInput = () => {
     [openMultipleRecordPicker, performSearch, pushFocusItemToFocusStack],
   );
 
-  return { openMorphRelationFromManyFieldInput };
+  return {
+    openMorphRelationOneToManyFieldInput,
+  };
 };
