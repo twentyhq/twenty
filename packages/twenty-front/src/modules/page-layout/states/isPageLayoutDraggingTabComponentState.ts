6adf5bdb1b2d5e02cf47ec2cb564785c2f3d590a
ehconitin <nitinkoche03@gmail.com>
2025-09-30 19:17:39 +0530

    cleanup

--- packages/twenty-front/src/modules/page-layout/states/isPageLayoutDraggingTabComponentState.ts
+++
@@ -1,9 +0,0 @@
-import { createComponentState } from '@/ui/utilities/state/component-state/utils/createComponentState';
-import { PageLayoutComponentInstanceContext } from './contexts/PageLayoutComponentInstanceContext';
-
-export const isPageLayoutDraggingTabComponentState =
-  createComponentState<boolean>({
-    key: 'isPageLayoutDraggingTabComponentState',
-    defaultValue: false,
-    componentInstanceContext: PageLayoutComponentInstanceContext,
-  });
