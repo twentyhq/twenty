a3104967d3df0ad8b06b75e9c29bff809c8fdc74
martmull <martmull@hotmail.fr>
2025-10-01 20:57:24 +0200

    Update serverless base code

--- packages/twenty-cli/src/commands/app-add.command.ts
+++ packages/twenty-cli/src/commands/app-add.command.ts
@@ -73,7 +73,19 @@ export class AppAddCommand {
 
         await fs.writeFile(
           path.join(srcPath, 'index.ts'),
-          `export const handler = async () => {}`,
+          `export const main = async (params: {
+  a: string;
+  b: number;
+}): Promise<object> => {
+  const { a, b } = params;
+
+  // Rename the parameters and code below with your own logic
+  // This is just an example
+  const message = \`Hello, input: \${a} and \${b}\`;
+
+  return { message };
+};
+`,
         );
 
         return;
