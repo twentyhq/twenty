aac1314f5be44b65bbf9a607995641c9c27a6f90
Marie <51697796+ijreilly@users.noreply.github.com>
2025-10-02 13:08:24 +0200

    [Fix] Handle deprecated viewFilterOperand in workflows run & on interface (#14837)
    In this PR https://github.com/twentyhq/twenty/pull/14785 we deprecated
    viewFilterOperand (from camelCase to capital snakeCase values), but we
    had not migrated the existing workflow steps values. Hence they cannot
    be executed!
    
    Let's fix that by dealing with both operands, new and deprecated, to
    mitigate the issue. Then we will add a command to migrate the values.

---
+++ packages/twenty-shared/src/types/ViewFilterOperandDeprecated.ts
@@ -0,0 +1,19 @@
+export enum ViewFilterOperandDeprecated {
+    Is = 'is',
+    IsNotNull = 'isNotNull',
+    IsNot = 'isNot',
+    LessThanOrEqual = 'lessThanOrEqual',
+    GreaterThanOrEqual = 'greaterThanOrEqual',
+    IsBefore = 'isBefore',
+    IsAfter = 'isAfter',
+    Contains = 'contains',
+    DoesNotContain = 'doesNotContain',
+    IsEmpty = 'isEmpty',
+    IsNotEmpty = 'isNotEmpty',
+    IsRelative = 'isRelative',
+    IsInPast = 'isInPast',
+    IsInFuture = 'isInFuture',
+    IsToday = 'isToday',
+    VectorSearch = 'vectorSearch',
+}
+  
