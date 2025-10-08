f8076269ef9701bc6d88d5717e38d07579362811
Charles Bochet <charles@twenty.com>
2025-10-08 17:05:04 +0200

    Reduce relation loading overload on FE graphql queries

--- packages/twenty-front/src/modules/object-record/graphql/utils/generateDepthOneRecordGqlFields.ts
+++ packages/twenty-front/src/modules/object-record/graphql/utils/generateDepthOneRecordGqlFields.ts
@@ -1,4 +1,3 @@
-import { CoreObjectNameSingular } from '@/object-metadata/types/CoreObjectNameSingular';
 import { type ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
 import { type RecordGqlFields } from '@/object-record/graphql/types/RecordGqlFields';
 import { FieldMetadataType, RelationType } from 'twenty-shared/types';
@@ -11,8 +10,8 @@ export type GenerateDepthOneRecordGqlFields = {
 export const generateDepthOneRecordGqlFields = ({
   objectMetadataItem,
 }: GenerateDepthOneRecordGqlFields) => {
-  const gqlFields = objectMetadataItem.readableFields.flatMap(
-    (fieldMetadata) => {
+  const gqlFieldsAsArray: RecordGqlFields[] =
+    objectMetadataItem.readableFields.flatMap((fieldMetadata) => {
       const isManyToOneMorphRelation =
         fieldMetadata.type === FieldMetadataType.MORPH_RELATION &&
         fieldMetadata.settings.relationType === RelationType.MANY_TO_ONE;
@@ -22,7 +21,10 @@ export const generateDepthOneRecordGqlFields = ({
         fieldMetadata.settings.relationType === RelationType.MANY_TO_ONE;
 
       if (isManyToOneRelation) {
-        return [`${fieldMetadata.name}Id`, `${fieldMetadata.name}`];
+        return [
+          { [`${fieldMetadata.name}Id`]: true },
+          { [`${fieldMetadata.name}`]: { id: true, name: true } },
+        ];
       }
 
       if (isManyToOneMorphRelation) {
@@ -46,28 +48,13 @@ export const generateDepthOneRecordGqlFields = ({
           }),
         );
         return morphGqlFields.flatMap((morphGqlField) => [
-          morphGqlField.gqlField,
-          `${morphGqlField.gqlField}Id`,
+          { [`${morphGqlField.gqlField}`]: { id: true, name: true } },
+          { [`${morphGqlField.gqlField}Id`]: true },
         ]);
       }
 
-      return [fieldMetadata.name];
-    },
-  );
+      return [{ [fieldMetadata.name]: true }];
+    });
 
-  return gqlFields.reduce<RecordGqlFields>((acc, field) => {
-    return {
-      ...acc,
-      [field]:
-      // TODO: Remove once we have made the workflows lighter
-        (objectMetadataItem.nameSingular === CoreObjectNameSingular.Workflow ||
-          objectMetadataItem.nameSingular ===
-            CoreObjectNameSingular.WorkflowVersion ||
-          objectMetadataItem.nameSingular ===
-            CoreObjectNameSingular.WorkflowRun) &&
-        (field === 'versions' || field === 'runs')
-          ? { id: true, name: true }
-          : true,
-    };
-  }, {});
+  return Object.assign({}, ...gqlFieldsAsArray);
 };
