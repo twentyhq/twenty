127c6f78990548d1bf734a3ea1038fbc5a64d1e4
etiennejouan <jouan.etienne@gmail.com>
2025-10-08 14:32:22 +0200

    this one the one !

--- packages/twenty-server/test/integration/graphql/suites/args-validation/filter-validation/address-field-filter-input-validation.integration-spec.ts
+++ packages/twenty-server/test/integration/graphql/suites/args-validation/filter-validation/address-field-filter-input-validation.integration-spec.ts
@@ -1,70 +1,70 @@
-// import { successfulFilterInputByFieldMetadataType } from 'test/integration/graphql/suites/args-validation/filter-validation/constants/successful-filter-input-by-field-metadata-type.constant';
-// import { testGqlSuccessfulScenario } from 'test/integration/graphql/suites/args-validation/filter-validation/utils/test-gql-successful-scenario.util';
-// import { testRestSuccessfulScenario } from 'test/integration/graphql/suites/args-validation/filter-validation/utils/test-rest-successful-scenario.util';
-// import { destroyManyObjectsMetadata } from 'test/integration/graphql/suites/args-validation/utils/destroy-many-objects-metadata';
-// import { setupTestObjectsWithAllFieldTypes } from 'test/integration/graphql/suites/args-validation/utils/setup-test-objects-with-all-field-types.util';
-// import { FieldMetadataType } from 'twenty-shared/types';
+import { successfulFilterInputByFieldMetadataType } from 'test/integration/graphql/suites/args-validation/filter-validation/constants/successful-filter-input-by-field-metadata-type.constant';
+import { testGqlSuccessfulScenario } from 'test/integration/graphql/suites/args-validation/filter-validation/utils/test-gql-successful-scenario.util';
+import { testRestSuccessfulScenario } from 'test/integration/graphql/suites/args-validation/filter-validation/utils/test-rest-successful-scenario.util';
+import { destroyManyObjectsMetadata } from 'test/integration/graphql/suites/args-validation/utils/destroy-many-objects-metadata';
+import { setupTestObjectsWithAllFieldTypes } from 'test/integration/graphql/suites/args-validation/utils/setup-test-objects-with-all-field-types.util';
+import { FieldMetadataType } from 'twenty-shared/types';
 
-// const FIELD_METADATA_TYPE = FieldMetadataType.ADDRESS;
-// const successfulTestCases =
-//   successfulFilterInputByFieldMetadataType[FIELD_METADATA_TYPE];
+const FIELD_METADATA_TYPE = FieldMetadataType.ADDRESS;
+const successfulTestCases =
+  successfulFilterInputByFieldMetadataType[FIELD_METADATA_TYPE];
 
-// describe(`Filter args validation - ${FIELD_METADATA_TYPE}`, () => {
-//   let objectMetadataId: string;
-//   let objectMetadataSingularName: string;
-//   let objectMetadataPluralName: string;
-//   let targetObjectMetadataId: string;
+describe(`Filter args validation - ${FIELD_METADATA_TYPE}`, () => {
+  let objectMetadataId: string;
+  let objectMetadataSingularName: string;
+  let objectMetadataPluralName: string;
+  let targetObjectMetadataId: string;
 
-//   beforeAll(async () => {
-//     const setupTest = await setupTestObjectsWithAllFieldTypes();
+  beforeAll(async () => {
+    const setupTest = await setupTestObjectsWithAllFieldTypes();
 
-//     objectMetadataId = setupTest.objectMetadataId;
-//     objectMetadataSingularName = setupTest.objectMetadataSingularName;
-//     objectMetadataPluralName = setupTest.objectMetadataPluralName;
-//     targetObjectMetadataId = setupTest.targetObjectMetadataId;
-//   });
+    objectMetadataId = setupTest.objectMetadataId;
+    objectMetadataSingularName = setupTest.objectMetadataSingularName;
+    objectMetadataPluralName = setupTest.objectMetadataPluralName;
+    targetObjectMetadataId = setupTest.targetObjectMetadataId;
+  });
 
-//   afterAll(async () => {
-//     await destroyManyObjectsMetadata([
-//       objectMetadataId,
-//       targetObjectMetadataId,
-//     ]);
-//   });
+  afterAll(async () => {
+    await destroyManyObjectsMetadata([
+      objectMetadataId,
+      targetObjectMetadataId,
+    ]);
+  });
 
-//   describe('Gql filterInput - success', () => {
-//     it.each(
-//       successfulTestCases.map((testCase) => ({
-//         ...testCase,
-//         stringifiedFilter: JSON.stringify(testCase.gqlFilterInput),
-//       })),
-//     )(
-//       `${FIELD_METADATA_TYPE} field type - should succeed with filter : $stringifiedFilter`,
-//       async ({ gqlFilterInput: filter, validateFilter }) => {
-//         await testGqlSuccessfulScenario(
-//           objectMetadataSingularName,
-//           objectMetadataPluralName,
-//           filter,
-//           validateFilter,
-//         );
-//       },
-//     );
-//   });
+  describe('Gql filterInput - success', () => {
+    it.each(
+      successfulTestCases.map((testCase) => ({
+        ...testCase,
+        stringifiedFilter: JSON.stringify(testCase.gqlFilterInput),
+      })),
+    )(
+      `${FIELD_METADATA_TYPE} field type - should succeed with filter : $stringifiedFilter`,
+      async ({ gqlFilterInput: filter, validateFilter }) => {
+        await testGqlSuccessfulScenario(
+          objectMetadataSingularName,
+          objectMetadataPluralName,
+          filter,
+          validateFilter,
+        );
+      },
+    );
+  });
 
-//   describe('Rest filterInput - success', () => {
-//     it.each(
-//       successfulTestCases.map((testCase) => ({
-//         ...testCase,
-//         stringifiedFilter: JSON.stringify(testCase.restFilterInput),
-//       })),
-//     )(
-//       `${FIELD_METADATA_TYPE} field type - should succeed with filter : $stringifiedFilter`,
-//       async ({ restFilterInput, validateFilter }) => {
-//         await testRestSuccessfulScenario(
-//           objectMetadataPluralName,
-//           restFilterInput,
-//           validateFilter,
-//         );
-//       },
-//     );
-//   });
-// });
+  describe('Rest filterInput - success', () => {
+    it.each(
+      successfulTestCases.map((testCase) => ({
+        ...testCase,
+        stringifiedFilter: JSON.stringify(testCase.restFilterInput),
+      })),
+    )(
+      `${FIELD_METADATA_TYPE} field type - should succeed with filter : $stringifiedFilter`,
+      async ({ restFilterInput, validateFilter }) => {
+        await testRestSuccessfulScenario(
+          objectMetadataPluralName,
+          restFilterInput,
+          validateFilter,
+        );
+      },
+    );
+  });
+});
