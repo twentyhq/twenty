7bfbc48d0c339918df490c79e34cf362f1ebf6b6
Abdullah <125115953+mabdullahabaid@users.noreply.github.com>
2025-10-10 01:38:29 +0500

    chore: move command from 1.8 to 1.10

--- packages/twenty-server/src/database/commands/upgrade-version-command/1-10/1-10-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-10/1-10-upgrade-version-command.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { MigrateWorkflowStepFilterOperandValueCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-migrate-workflow-step-filter-operand-value';
+import { RegeneratePersonSearchVectorWithPhonesCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-regenerate-person-search-vector-with-phones.command';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import { WorkspaceSchemaManagerModule } from 'src/engine/twenty-orm/workspace-schema-manager/workspace-schema-manager.module';
 import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Workspace]), WorkspaceDataSourceModule],
-  providers: [MigrateWorkflowStepFilterOperandValueCommand],
-  exports: [MigrateWorkflowStepFilterOperandValueCommand],
+  imports: [
+    TypeOrmModule.forFeature([Workspace]),
+    WorkspaceDataSourceModule,
+    WorkspaceSchemaManagerModule,
+  ],
+  providers: [
+    MigrateWorkflowStepFilterOperandValueCommand,
+    RegeneratePersonSearchVectorWithPhonesCommand,
+  ],
+  exports: [
+    MigrateWorkflowStepFilterOperandValueCommand,
+    RegeneratePersonSearchVectorWithPhonesCommand,
+  ],
 })
 export class V1_10_UpgradeVersionCommandModule {}
