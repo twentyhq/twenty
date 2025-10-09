d25f2873da87d62403a197b096fc6bae59795d23
prastoin <paul@twenty.com>
2025-10-09 18:16:45 +0200

    refactor(server): creating type files

---
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/successful-flat-entity-validate-and-build.type.ts
@@ -0,0 +1,18 @@
+import { AllFlatEntities } from "src/engine/core-modules/common/types/all-flat-entities.type";
+import { AllFlatEntityMaps } from "src/engine/core-modules/common/types/all-flat-entity-maps.type";
+import { FlatEntityMaps } from "src/engine/core-modules/common/types/flat-entity-maps.type";
+import { CreatedDeletedUpdatedActions } from "src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/created-deleted-updated-actions.type";
+import { WorkspaceMigrationActionV2 } from "src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-action-common-v2";
+
+    export type SuccessfulFlatEntityValidateAndBuild<
+      TActions extends WorkspaceMigrationActionV2,
+      TFlatEntity extends AllFlatEntities,
+      TRelatedFlatEntityMaps extends
+        | Partial<AllFlatEntityMaps>
+        | undefined = undefined,
+    > = {
+      status: 'success';
+      actions: CreatedDeletedUpdatedActions<TActions>;
+      optimisticFlatEntityMaps: FlatEntityMaps<TFlatEntity>;
+      dependencyOptimisticFlatEntityMaps: TRelatedFlatEntityMaps;
+    };
