436a0e26681118d82b03e79cce58f47d828e4a54
Harshit Singh <harshitsingh467@gmail.com>
2025-10-09 18:31:58 +0530

    refactor: frontend to new updated type

--- packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/delay/types/workflow-delay-action-input.type.ts
+++ packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/delay/types/workflow-delay-action-input.type.ts
@@ -1,3 +1,10 @@
 export type WorkflowDelayActionInput = {
+  delayType: 'schedule_date' | 'duration';
   scheduledDateTime?: string | null;
+  duration?: {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
 };
