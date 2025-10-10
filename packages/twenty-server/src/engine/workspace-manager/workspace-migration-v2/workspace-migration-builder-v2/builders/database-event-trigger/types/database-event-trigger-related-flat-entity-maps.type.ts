5aff167c10559772d903c09aedd2f350058ccb32
prastoin <paul@twenty.com>
2025-10-10 11:06:44 +0200

    wip

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/database-event-trigger/types/database-event-trigger-related-flat-entity-maps.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/database-event-trigger/types/database-event-trigger-related-flat-entity-maps.type.ts
@@ -1,6 +1,7 @@
+import { ALL_FLAT_ENTITY_CONSTANTS } from 'src/engine/metadata-modules/flat-entity/constant/all-flat-entity-contants.constant';
 import { type AllFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entity-maps.type';
 
 export type DatabaseEventTriggerRelatedFlatEntityMaps = Pick<
   AllFlatEntityMaps,
-  'flatServerlessFunctionMaps'
+  (typeof ALL_FLAT_ENTITY_CONSTANTS.databaseEventTrigger.relatedFlatEntityMapsKeys)[number]
 >;
--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/view-field/types/view-field-related-flat-entity-maps.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/builders/view-field/types/view-field-related-flat-entity-maps.type.ts
@@ -1,6 +1,7 @@
+import { ALL_FLAT_ENTITY_CONSTANTS } from 'src/engine/metadata-modules/flat-entity/constant/all-flat-entity-contants.constant';
 import { type AllFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entity-maps.type';
 
 export type ViewFieldRelatedFlatEntityMaps = Pick<
   AllFlatEntityMaps,
-  'flatViewMaps' | 'flatFieldMetadataMaps' | 'flatObjectMetadataMaps'
+  (typeof ALL_FLAT_ENTITY_CONSTANTS.viewField.relatedFlatEntityMapsKeys)[number]
 >;
