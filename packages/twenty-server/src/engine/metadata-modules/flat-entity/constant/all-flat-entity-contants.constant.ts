208eb9332ad424be41859b86b46b4e929f41e59f
prastoin <paul@twenty.com>
2025-10-10 12:10:00 +0200

    wip deprecating old type

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/constant/all-flat-entity-contants.constant.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/constant/all-flat-entity-contants.constant.ts
@@ -1,7 +1,6 @@
 import { FLAT_CRON_TRIGGER_EDITABLE_PROPERTIES } from 'src/engine/metadata-modules/cron-trigger/constants/flat-cron-trigger-editable-properties.constant';
 import { FLAT_DATABASE_EVENT_TRIGGER_EDITABLE_PROPERTIES } from 'src/engine/metadata-modules/database-event-trigger/constants/flat-database-event-trigger-editable-properties.constant';
-import { type AllFlatEntitiesByMetadataEngineName } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entities-by-metadata-engine-name.type';
-import { type AllFlatEntities } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entities.type';
+import { AllFlatEntitiesByMetadataEngineNameV2 } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entities-by-metadata-engine-name.type';
 import { type AllFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entity-maps.type';
 import { type FromMetadataEngineNameToFlatEntityMapsKey } from 'src/engine/metadata-modules/flat-entity/types/from-metadata-engine-name-to-flat-entity-maps-key.type';
 import { FLAT_FIELD_METADATA_EDITABLE_PROPERTIES } from 'src/engine/metadata-modules/flat-field-metadata/constants/flat-field-metadata-editable-properties.constant';
@@ -11,13 +10,12 @@ import { FLAT_VIEW_EDITABLE_PROPERTIES } from 'src/engine/metadata-modules/flat-
 import { FLAT_ROUTE_TRIGGER_EDITABLE_PROPERTIES } from 'src/engine/metadata-modules/route-trigger/constants/flat-route-trigger-editable-properties.constant';
 import { FLAT_SERVERLESS_FUNCTION_EDITABLE_PROPERTIES } from 'src/engine/metadata-modules/serverless-function/constants/flat-serverless-function-editable-properties.constant';
 
-type OneFlatEntityConstants<
-  TFlatEntity extends AllFlatEntities,
-  TFlatEntityName extends keyof AllFlatEntitiesByMetadataEngineName,
+type OneFlatEntityConfiguration<
+  P extends keyof AllFlatEntitiesByMetadataEngineNameV2,
 > = {
-  propertiesToCompare: (keyof TFlatEntity)[];
-  propertiesToStringify: (keyof TFlatEntity)[];
-  flatEntityMapsKey: FromMetadataEngineNameToFlatEntityMapsKey<TFlatEntityName>;
+  propertiesToCompare: (keyof AllFlatEntitiesByMetadataEngineNameV2[P]['flatEntity'])[];
+  propertiesToStringify: (keyof AllFlatEntitiesByMetadataEngineNameV2[P]['flatEntity'])[];
+  flatEntityMapsKey: FromMetadataEngineNameToFlatEntityMapsKey<P>;
   relatedFlatEntityMapsKeys: (keyof AllFlatEntityMaps)[];
 };
 
@@ -122,8 +120,5 @@ export const ALL_FLAT_ENTITY_CONSTANTS = {
     relatedFlatEntityMapsKeys: ['flatViewMaps', 'flatFieldMetadataMaps'],
   },
 } as const satisfies {
-  [P in keyof AllFlatEntitiesByMetadataEngineName]: OneFlatEntityConstants<
-    AllFlatEntitiesByMetadataEngineName[P],
-    P
-  >;
+  [P in keyof AllFlatEntitiesByMetadataEngineNameV2]: OneFlatEntityConfiguration<P>;
 };
