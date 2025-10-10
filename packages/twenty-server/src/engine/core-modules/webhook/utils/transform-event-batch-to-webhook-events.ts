b52e483ec6749dc435d6eb4a11ab7b12ce1e3e5a
martmull <martmull@hotmail.fr>
2025-10-10 21:14:53 +0200

    Refactor with subscription events

--- packages/twenty-server/src/engine/core-modules/webhook/utils/transform-event-batch-to-webhook-events.ts
+++ packages/twenty-server/src/engine/core-modules/webhook/utils/transform-event-batch-to-webhook-events.ts
@@ -1,10 +1,8 @@
-import { isDefined } from 'twenty-shared/utils';
-
 import { type WorkspaceEventBatch } from 'src/engine/workspace-event-emitter/types/workspace-event-batch.type';
 import { type CallWebhookJobData } from 'src/engine/core-modules/webhook/jobs/call-webhook.job';
 import { type Webhook } from 'src/engine/core-modules/webhook/webhook.entity';
-import { removeSecretFromWebhookRecord } from 'src/utils/remove-secret-from-webhook-record';
 import type { ObjectRecordEvent } from 'src/engine/core-modules/event-emitter/types/object-record-event.event';
+import { transformEventToWebhookEvent } from 'src/engine/core-modules/webhook/utils/transform-event-to-webhook-event';
 
 export const transformEventBatchToWebhookEvents = ({
   workspaceEventBatch,
@@ -27,27 +25,11 @@ export const transformEventBatchToWebhookEvents = ({
     const eventDate = new Date();
     const secret = webhook.secret;
 
-    const [nameSingular, _] = workspaceEventBatch.name.split('.');
-
     for (const eventData of workspaceEventBatch.events) {
-      const record =
-        'after' in eventData.properties && isDefined(eventData.properties.after)
-          ? eventData.properties.after
-          : 'before' in eventData.properties &&
-              isDefined(eventData.properties.before)
-            ? eventData.properties.before
-            : {};
-      const updatedFields =
-        'updatedFields' in eventData.properties
-          ? eventData.properties.updatedFields
-          : undefined;
-
-      const isWebhookEvent = nameSingular === 'webhook';
-
-      const sanitizedRecord = removeSecretFromWebhookRecord(
-        record,
-        isWebhookEvent,
-      );
+      const { record, updatedFields } = transformEventToWebhookEvent({
+        eventName: workspaceEventBatch.name,
+        event: eventData,
+      });
 
       result.push({
         targetUrl,
@@ -56,7 +38,7 @@ export const transformEventBatchToWebhookEvents = ({
         workspaceId,
         webhookId,
         eventDate,
-        record: sanitizedRecord,
+        record,
         ...(updatedFields && { updatedFields }),
         secret,
       });
