365120c4905d85f66bebb73393704efab7a45d38
prastoin <paul@twenty.com>
2025-10-11 09:01:33 +0200

    fixup

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/constant/empty-orchestrator-actions-report.constant.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/constant/empty-orchestrator-actions-report.constant.ts
@@ -2,7 +2,9 @@ import { ALL_METADATA_NAME } from 'src/engine/metadata-modules/flat-entity/const
 import { type OrchestratorActionsReport } from 'src/engine/workspace-manager/workspace-migration-v2/types/workspace-migration-orchestrator.type';
 import { getMetadataEmptyWorkspaceMigrationActionRecord } from 'src/engine/workspace-manager/workspace-migration-v2/utils/get-metadata-empty-workspace-migration-action-record.util';
 
-export const EMPTY_ORCHESTRATOR_ACTIONS_REPORT = ALL_METADATA_NAME.reduce(
+export const EMPTY_ORCHESTRATOR_ACTIONS_REPORT = (
+  Object.keys(ALL_METADATA_NAME) as (keyof typeof ALL_METADATA_NAME)[]
+).reduce(
   (orchestratorReport, metadataName) => ({
     ...orchestratorReport,
     [metadataName]:
