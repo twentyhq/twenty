9c6e0c805c09116a3305de52d6c44e4187f50bd2
ehconitin <nitinkoche03@gmail.com>
2025-10-11 13:06:13 +0530

    done

--- packages/twenty-front/src/modules/command-menu/pages/page-layout/components/CommandMenuPageLayoutGraphTypeSelect.tsx
+++ packages/twenty-front/src/modules/command-menu/pages/page-layout/components/CommandMenuPageLayoutGraphTypeSelect.tsx
@@ -2,6 +2,7 @@ import { SidePanelHeader } from '@/command-menu/components/SidePanelHeader';
 import { ChartSettings } from '@/command-menu/pages/page-layout/components/ChartSettings';
 import { GRAPH_TYPE_INFORMATION } from '@/command-menu/pages/page-layout/constants/GraphTypeInformation';
 import { usePageLayoutIdFromContextStoreTargetedRecord } from '@/command-menu/pages/page-layout/hooks/usePageLayoutFromContextStoreTargetedRecord';
+import { useUpdatePageLayoutWidget } from '@/page-layout/hooks/useUpdatePageLayoutWidget';
 import { pageLayoutDraftComponentState } from '@/page-layout/states/pageLayoutDraftComponentState';
 import { pageLayoutEditingWidgetIdComponentState } from '@/page-layout/states/pageLayoutEditingWidgetIdComponentState';
 import { useRecoilComponentValue } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValue';
@@ -21,6 +22,12 @@ export const CommandMenuPageLayoutGraphTypeSelect = () => {
     pageLayoutEditingWidgetIdComponentState,
     pageLayoutId,
   );
+
+  const { updatePageLayoutWidget } = useUpdatePageLayoutWidget(pageLayoutId);
+
+  if (!isDefined(pageLayoutEditingWidgetId)) {
+    throw new Error('Widget ID must be present while editing the widget');
+  }
 
   const widgetInEditMode = draftPageLayout.tabs
     .flatMap((tab) => tab.widgets)
@@ -43,9 +50,13 @@ export const CommandMenuPageLayoutGraphTypeSelect = () => {
       <SidePanelHeader
         Icon={GRAPH_TYPE_INFORMATION[currentGraphType].icon}
         iconColor={theme.font.color.tertiary}
-        initialTitle={t`Chart`}
+        initialTitle={widgetInEditMode.title}
         headerType={t`${graphTypeLabel} Chart`}
-        onTitleChange={() => {}}
+        onTitleChange={(newTitle) => {
+          updatePageLayoutWidget(pageLayoutEditingWidgetId, {
+            title: newTitle,
+          });
+        }}
       />
 
       <ChartSettings widget={widgetInEditMode} />
