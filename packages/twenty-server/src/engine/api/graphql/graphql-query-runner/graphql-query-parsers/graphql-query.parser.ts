4c7e8444cf797b4cad20ab0243e52f3c2bed8cb7
Marie <51697796+ijreilly@users.noreply.github.com>
2025-10-13 11:51:42 +0200

    Inter-groups orderBy on groupByDimension values (#15024)
    Closes https://github.com/twentyhq/core-team-issues/issues/1563.
    
    We now have two inter-group orderBy criteria: one on the aggregate
    values, the other on the dimension values.
    Ex: I am grouping companies by city and querying the average number of
    employees for each group. I can either order the groups by the city
    name, or by the average number of employees, or by one then the other.
    I could actually also order groups by an aggregated value that I did not
    ask for (ex: average ARR), but I cannot order groups by a field value I
    did not group records by (ex: country).
    [See discord
    discussion](https://discord.com/channels/1130383047699738754/1425438213555753050/1425438223097921659)
    
    An example of query variables: 
    ```
    {
      "groupBy": [
        {
          "createdAt": {
            "granularity": "QUARTER_OF_THE_YEAR"
          }
        },
        {"city": true}
      ],
      "orderBy": [
        { 
          "aggregate": {
            "avgEmployees": "DescNullsLast"
          }
        },
        { 
          "aggregate": {
            "percentyEmptyEmployees": "DescNullsLast"
          }
        },
        {
          "city": "AscNullsLast"
        },
        {
          "createdAt": {
            "orderBy": "AscNullsLast",
            "granularity": "QUARTER_OF_THE_YEAR",
          }
        }
      ]
    }
    ```
    
    The aggregate orderBy criteria had already been implemented, but I
    updated the implementation to add an "aggregate" key to prefix them, in
    order to avoid confusion between the two + for the schema generation not
    to break (otherwise we would have an issue when generating
    OrderByWithGroupByInput if a user has created a field that has the same
    name as an aggregate field, such as avgEmployees).

--- packages/twenty-server/src/engine/api/graphql/graphql-query-runner/graphql-query-parsers/graphql-query.parser.ts
+++ packages/twenty-server/src/engine/api/graphql/graphql-query-runner/graphql-query-parsers/graphql-query.parser.ts
@@ -3,6 +3,7 @@ import { type FindOptionsWhere, type ObjectLiteral } from 'typeorm';
 import {
   type ObjectRecordFilter,
   type ObjectRecordOrderBy,
+  type OrderByWithGroupBy,
 } from 'src/engine/api/graphql/workspace-query-builder/interfaces/object-record.interface';
 
 import {
@@ -15,6 +16,7 @@ import {
   GraphqlQuerySelectedFieldsParser,
   type GraphqlQuerySelectedFieldsResult,
 } from 'src/engine/api/graphql/graphql-query-runner/graphql-query-parsers/graphql-query-selected-fields/graphql-selected-fields.parser';
+import { type GroupByField } from 'src/engine/api/graphql/graphql-query-runner/group-by/resolvers/types/group-by-field.types';
 import { type ObjectMetadataItemWithFieldMaps } from 'src/engine/metadata-modules/types/object-metadata-item-with-field-maps';
 import { type ObjectMetadataMaps } from 'src/engine/metadata-modules/types/object-metadata-maps';
 import { getObjectMetadataMapItemByNameSingular } from 'src/engine/metadata-modules/utils/get-object-metadata-map-item-by-name-singular.util';
@@ -97,38 +99,43 @@ export class GraphqlQueryParser {
   public applyOrderToBuilder(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     queryBuilder: WorkspaceSelectQueryBuilder<any>,
-    orderBy: ObjectRecordOrderBy,
+    orderBy: ObjectRecordOrderBy | OrderByWithGroupBy,
     objectNameSingular: string,
     isForwardPagination = true,
-    isGroupBy = false,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ): WorkspaceSelectQueryBuilder<any> {
     const parsedOrderBys = this.orderFieldParser.parse(
-      orderBy,
+      orderBy as ObjectRecordOrderBy,
       objectNameSingular,
       isForwardPagination,
-      isGroupBy,
     );
 
-    if (isGroupBy) {
-      Object.entries(parsedOrderBys).forEach(
-        ([expression, direction], index) => {
-          if (index === 0) {
-            queryBuilder.orderBy(expression, direction.order, direction.nulls);
-          } else {
-            queryBuilder.addOrderBy(
-              expression,
-              direction.order,
-              direction.nulls,
-            );
-          }
-        },
-      );
-
-      return queryBuilder;
-    }
-
     return queryBuilder.orderBy(parsedOrderBys);
+  }
+
+  public applyGroupByOrderToBuilder(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    queryBuilder: WorkspaceSelectQueryBuilder<any>,
+    orderBy: ObjectRecordOrderBy | OrderByWithGroupBy,
+    groupByFields: GroupByField[],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ): WorkspaceSelectQueryBuilder<any> {
+    const parsedOrderBys = this.orderFieldParser.parseForGroupBy({
+      orderBy,
+      groupByFields,
+    });
+
+    parsedOrderBys.forEach((orderByField, index) => {
+      Object.entries(orderByField).forEach(([expression, direction]) => {
+        if (index === 0) {
+          queryBuilder.orderBy(expression, direction.order, direction.nulls);
+        } else {
+          queryBuilder.addOrderBy(expression, direction.order, direction.nulls);
+        }
+      });
+    });
+
+    return queryBuilder;
   }
 
   public parseSelectedFields(
