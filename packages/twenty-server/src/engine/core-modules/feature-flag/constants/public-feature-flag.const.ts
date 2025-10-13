3686434b6807a6e0a09a1100ab0b1015c568e289
martmull <martmull@hotmail.fr>
2025-10-13 14:06:23 +0200

    Add application featureFlag in labs

--- packages/twenty-server/src/engine/core-modules/feature-flag/constants/public-feature-flag.const.ts
+++ packages/twenty-server/src/engine/core-modules/feature-flag/constants/public-feature-flag.const.ts
@@ -48,6 +48,14 @@ export const PUBLIC_FEATURE_FLAGS: PublicFeatureFlag[] = [
       imagePath: 'https://twenty.com/images/lab/is-calendar-view-enabled.png',
     },
   },
+  {
+    key: FeatureFlagKey.IS_APPLICATION_ENABLED,
+    metadata: {
+      label: 'Application Settings',
+      description: 'View and manage installed applications',
+      imagePath: 'https://twenty.com/images/lab/is-application-enabled.png',
+    },
+  },
   ...(process.env.CLOUDFLARE_API_KEY
     ? [
         // {
