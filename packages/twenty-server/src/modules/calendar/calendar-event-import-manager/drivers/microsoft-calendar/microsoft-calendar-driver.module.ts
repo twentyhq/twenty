6701311689ebc935ccb0aa874c2ced5dc85884a3
neo773 <neo773@protonmail.com>
2025-10-14 20:20:40 +0530

    fix comp

--- packages/twenty-server/src/modules/calendar/calendar-event-import-manager/drivers/microsoft-calendar/microsoft-calendar-driver.module.ts
+++ packages/twenty-server/src/modules/calendar/calendar-event-import-manager/drivers/microsoft-calendar/microsoft-calendar-driver.module.ts
@@ -4,10 +4,12 @@ import { TwentyConfigModule } from 'src/engine/core-modules/twenty-config/twenty
 import { MicrosoftCalendarGetEventsService } from 'src/modules/calendar/calendar-event-import-manager/drivers/microsoft-calendar/services/microsoft-calendar-get-events.service';
 import { MicrosoftCalendarImportEventsService } from 'src/modules/calendar/calendar-event-import-manager/drivers/microsoft-calendar/services/microsoft-calendar-import-events.service';
 import { MicrosoftOAuth2ClientManagerService } from 'src/modules/connected-account/oauth2-client-manager/drivers/microsoft/microsoft-oauth2-client-manager.service';
+import { MicrosoftClientProvider } from 'src/modules/messaging/message-import-manager/drivers/microsoft/providers/microsoft-client.provider';
 
 @Module({
   imports: [TwentyConfigModule],
   providers: [
+    MicrosoftClientProvider,
     MicrosoftCalendarGetEventsService,
     MicrosoftCalendarImportEventsService,
     MicrosoftOAuth2ClientManagerService,
