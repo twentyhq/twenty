f915abc4ac9fc70150bf8e748dd65df0ae4f3395
neo773 <neo773@protonmail.com>
2025-10-14 20:22:25 +0530

    changes

--- packages/twenty-server/src/modules/connected-account/oauth2-client-manager/drivers/microsoft/microsoft-oauth2-client-manager.service.ts
+++ packages/twenty-server/src/modules/connected-account/oauth2-client-manager/drivers/microsoft/microsoft-oauth2-client-manager.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { Client } from '@microsoft/microsoft-graph-client';
 
-import { MicrosoftOAuth2ClientAuthProvider } from 'src/modules/connected-account/oauth2-client-manager/drivers/microsoft/microsoft-oauth2-client-auth-provider.service';
+import { MicrosoftOAuth2ClientAuthProvider } from 'src/modules/connected-account/oauth2-client-manager/drivers/microsoft/microsoft-oauth2-client-auth-provider';
 
 @Injectable()
 export class MicrosoftOAuth2ClientManagerService {
