23a14ee5670e1505b46b9fa5f06011c8a457281c
martmull <martmull@hotmail.fr>
2025-10-14 15:46:02 +0200

    Handle no application

--- packages/twenty-front/src/pages/settings/applications/SettingsApplications.tsx
+++ packages/twenty-front/src/pages/settings/applications/SettingsApplications.tsx
@@ -7,11 +7,17 @@ import { Section } from 'twenty-ui/layout';
 import { SettingsApplicationsTable } from '~/pages/settings/applications/components/SettingsApplicationsTable';
 import { useFindManyApplicationsQuery } from '~/generated-metadata/graphql';
 import { TabList } from '@/ui/layout/tab-list/components/TabList';
+import styled from '@emotion/styled';
+import { LinkDisplay } from '@/ui/field/display/components/LinkDisplay';
 
 const APPLICATIONS_ID = 'applications';
 
+const StyledNoApplicationContainer = styled.div``;
+
 export const SettingsApplications = () => {
   const { data } = useFindManyApplicationsQuery();
+
+  const applications = data?.findManyApplications ?? [];
 
   const tabs = [{ id: 'inUsed', title: 'In used' }];
 
@@ -28,14 +34,26 @@ export const SettingsApplications = () => {
     >
       <SettingsPageContainer>
         <Section>
-          <TabList
-            tabs={tabs}
-            behaveAsLinks={false}
-            componentInstanceId={APPLICATIONS_ID}
-          />
-          <SettingsApplicationsTable
-            applications={data?.findManyApplications ?? []}
-          />
+          {applications.length > 0 ? (
+            <>
+              <TabList
+                tabs={tabs}
+                behaveAsLinks={false}
+                componentInstanceId={APPLICATIONS_ID}
+              />
+              <SettingsApplicationsTable applications={applications} />
+            </>
+          ) : (
+            <StyledNoApplicationContainer>
+              No installed application. Please check our{' '}
+              <LinkDisplay
+                value={{
+                  url: 'https://www.npmjs.com/package/twenty-cli',
+                  label: 'twenty-cli',
+                }}
+              />
+            </StyledNoApplicationContainer>
+          )}
         </Section>
       </SettingsPageContainer>
     </SubMenuTopBarContainer>
