f7c784f627fe946e1dc18c0c34bb3bfc8926bcc6
ehconitin <nitinkoche03@gmail.com>
2025-10-15 13:39:10 +0530

    add min height and width

--- packages/twenty-front/src/modules/page-layout/utils/convertPageLayoutToTabLayouts.ts
+++ packages/twenty-front/src/modules/page-layout/utils/convertPageLayoutToTabLayouts.ts
@@ -1,5 +1,6 @@
 import { type PageLayout } from '@/page-layout/types/PageLayout';
 import { type TabLayouts } from '@/page-layout/types/tab-layouts';
+import { getWidgetMinimumSize } from '@/page-layout/utils/getWidgetMinimumSize';
 
 export const convertPageLayoutToTabLayouts = (
   pageLayout: PageLayout,
@@ -11,13 +12,19 @@ export const convertPageLayoutToTabLayouts = (
   const tabLayouts: TabLayouts = {};
 
   pageLayout.tabs.forEach((tab) => {
-    const layouts = tab.widgets.map((widget) => ({
-      i: widget.id,
-      x: widget.gridPosition.column,
-      y: widget.gridPosition.row,
-      w: widget.gridPosition.columnSpan,
-      h: widget.gridPosition.rowSpan,
-    }));
+    const layouts = tab.widgets.map((widget) => {
+      const { minW, minH } = getWidgetMinimumSize(widget);
+
+      return {
+        i: widget.id,
+        x: widget.gridPosition.column,
+        y: widget.gridPosition.row,
+        w: widget.gridPosition.columnSpan,
+        h: widget.gridPosition.rowSpan,
+        minW,
+        minH,
+      };
+    });
 
     tabLayouts[tab.id] = {
       desktop: layouts,
