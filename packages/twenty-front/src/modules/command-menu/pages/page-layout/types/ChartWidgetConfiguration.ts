146ba08b3a0fa2f278d06900e5e668e90804bf2e
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-10-16 16:35:34 +0200

    Fix

--- packages/twenty-front/src/modules/command-menu/pages/page-layout/types/ChartWidgetConfiguration.ts
+++ packages/twenty-front/src/modules/command-menu/pages/page-layout/types/ChartWidgetConfiguration.ts
@@ -1,9 +1,14 @@
 import {
-  type IframeConfiguration,
-  type WidgetConfiguration,
+  type BarChartConfiguration,
+  type GaugeChartConfiguration,
+  type LineChartConfiguration,
+  type NumberChartConfiguration,
+  type PieChartConfiguration,
 } from '~/generated/graphql';
 
-export type ChartWidgetConfiguration = Exclude<
-  WidgetConfiguration,
-  IframeConfiguration
->;
+export type ChartWidgetConfiguration =
+  | BarChartConfiguration
+  | GaugeChartConfiguration
+  | LineChartConfiguration
+  | NumberChartConfiguration
+  | PieChartConfiguration;
