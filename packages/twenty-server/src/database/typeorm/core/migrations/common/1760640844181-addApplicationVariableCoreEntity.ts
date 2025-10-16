9ef1018bc8b75ab0e249d5f99ea7282c4f05dba5
martmull <martmull@hotmail.fr>
2025-10-16 20:54:34 +0200

    Fix migration file

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1760640844181-addApplicationVariableCoreEntity.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1760640844181-addApplicationVariableCoreEntity.ts
@@ -1,13 +1,13 @@
 import { type MigrationInterface, type QueryRunner } from 'typeorm';
 
-export class AddApplicationVariableCoreEntity1760554366699
+export class AddApplicationVariableCoreEntity1760640844181
   implements MigrationInterface
 {
-  name = 'AddApplicationVariableCoreEntity1760554366699';
+  name = 'AddApplicationVariableCoreEntity1760640844181';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "core"."applicationVariable" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "key" text NOT NULL, "value" text NOT NULL DEFAULT '', "description" text, "isSecret" boolean NOT NULL DEFAULT false, "applicationId" uuid NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "IDX_APPLICATION_VARIABLE_KEY_APPLICATION_ID_UNIQUE" UNIQUE ("key", "applicationId"), CONSTRAINT "PK_62f7823eb5f1e416c9d60614dfb" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "core"."applicationVariable" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "key" text NOT NULL, "value" text NOT NULL DEFAULT '', "description" text NOT NULL DEFAULT '', "isSecret" boolean NOT NULL DEFAULT false, "applicationId" uuid, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "IDX_APPLICATION_VARIABLE_KEY_APPLICATION_ID_UNIQUE" UNIQUE ("key", "applicationId"), CONSTRAINT "PK_62f7823eb5f1e416c9d60614dfb" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "core"."applicationVariable" ADD CONSTRAINT "FK_51adb49e7f8df35dd23e01c4830" FOREIGN KEY ("applicationId") REFERENCES "core"."application"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
