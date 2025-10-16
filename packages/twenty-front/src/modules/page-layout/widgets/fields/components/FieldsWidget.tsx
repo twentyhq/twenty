d4dbfd011dce14240b299254a3da3a6d0c51a81c
Devessier <baptiste@devessier.fr>
2025-10-16 15:36:50 +0200

    refactor: nitpicks

--- packages/twenty-front/src/modules/page-layout/widgets/fields/components/FieldsWidget.tsx
+++ packages/twenty-front/src/modules/page-layout/widgets/fields/components/FieldsWidget.tsx
@@ -1,31 +1,34 @@
 import { RecordFieldList } from '@/object-record/record-field-list/components/RecordFieldList';
-import { type FieldCardConfiguration } from '@/object-record/record-show/types/CardConfiguration';
 import { useLayoutRenderingContext } from '@/ui/layout/contexts/LayoutRenderingContext';
 import { useTargetRecord } from '@/ui/layout/contexts/useTargetRecord';
 import { RightDrawerProvider } from '@/ui/layout/right-drawer/contexts/RightDrawerContext';
+import styled from '@emotion/styled';
 import { type PageLayoutWidget } from '~/generated/graphql';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 type FieldsWidgetProps = {
   widget: PageLayoutWidget;
 };
 
-export const FieldsWidget = ({ widget }: FieldsWidgetProps) => {
+export const FieldsWidget = ({ widget: _widget }: FieldsWidgetProps) => {
   const targetRecord = useTargetRecord();
   const { isInRightDrawer } = useLayoutRenderingContext();
 
-  // Parse configuration from widget
-  const configuration = widget.configuration as FieldCardConfiguration | null;
-
   return (
     <RightDrawerProvider value={{ isInRightDrawer }}>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <StyledContainer>
         <RecordFieldList
           instanceId={`fields-widget-${targetRecord.id}-${isInRightDrawer ? 'right-drawer' : ''}`}
           objectNameSingular={targetRecord.targetObjectNameSingular}
           objectRecordId={targetRecord.id}
-          showDuplicatesSection={configuration?.showDuplicatesSection ?? true}
+          // TODO: pick the value from the widget configuration
+          showDuplicatesSection={true}
         />
-      </div>
+      </StyledContainer>
     </RightDrawerProvider>
   );
 };
