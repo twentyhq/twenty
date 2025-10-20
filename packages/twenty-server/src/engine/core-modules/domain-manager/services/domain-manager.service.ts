5a805f19cdf683fb7e1b0f83c64bf20b14ae9da2
Naineel Soyantar <naineelsoyantar@gmail.com>
2025-10-20 18:43:07 +0000

    fix: reserved subdomain check to be case-insensitive in `generateSubdomain`

--- packages/twenty-server/src/engine/core-modules/domain-manager/services/domain-manager.service.ts
+++ packages/twenty-server/src/engine/core-modules/domain-manager/services/domain-manager.service.ts
@@ -8,10 +8,10 @@ import { type WorkspaceSubdomainCustomDomainAndIsCustomDomainEnabledType } from 
 import { generateRandomSubdomain } from 'src/engine/core-modules/domain-manager/utils/generate-random-subdomain';
 import { getSubdomainFromEmail } from 'src/engine/core-modules/domain-manager/utils/get-subdomain-from-email';
 import { getSubdomainNameFromDisplayName } from 'src/engine/core-modules/domain-manager/utils/get-subdomain-name-from-display-name';
+import { PublicDomain } from 'src/engine/core-modules/public-domain/public-domain.entity';
 import { TwentyConfigService } from 'src/engine/core-modules/twenty-config/twenty-config.service';
 import { RESERVED_SUBDOMAINS } from 'src/engine/core-modules/workspace/constants/reserved-subdomains.constant';
 import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
-import { PublicDomain } from 'src/engine/core-modules/public-domain/public-domain.entity';
 import { WorkspaceNotFoundDefaultError } from 'src/engine/core-modules/workspace/workspace.exception';
 
 @Injectable()
@@ -210,7 +210,7 @@ export class DomainManagerService {
     let subdomain = this.extractSubdomain(params);
 
     // If extracted subdomain is reserved, use random instead
-    if (subdomain && RESERVED_SUBDOMAINS.includes(subdomain)) {
+    if (subdomain && RESERVED_SUBDOMAINS.includes(subdomain.toLowerCase())) {
       subdomain = undefined;
     }
 
