b2807e73d81d87a81dac8de45a62d083423e4163
prastoin <paul@twenty.com>
2025-10-21 15:19:27 +0200

    lint

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1761052489394-calendarFieldMetadataRelation.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1761052489394-calendarFieldMetadataRelation.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { type MigrationInterface, type QueryRunner } from 'typeorm';
 
 export class CalendarFieldMetadataRelation1761052489394
   implements MigrationInterface
