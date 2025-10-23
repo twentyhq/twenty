95466265bc3cbde5673f05e06dc73f4878d12cf8
prastoin <paul@twenty.com>
2025-10-23 15:47:43 +0200

    lint

--- packages/twenty-server/test/integration/rest/suites/rest-api-core-update.integration-spec.ts
+++ packages/twenty-server/test/integration/rest/suites/rest-api-core-update.integration-spec.ts
@@ -3,9 +3,7 @@ import {
   NOT_EXISTING_TEST_PERSON_ID,
   TEST_PERSON_1_ID,
 } from 'test/integration/constants/test-person-ids.constants';
-import {
-  TEST_PRIMARY_LINK_URL
-} from 'test/integration/constants/test-primary-link-url.constant';
+import { TEST_PRIMARY_LINK_URL } from 'test/integration/constants/test-primary-link-url.constant';
 import { makeRestAPIRequest } from 'test/integration/rest/utils/make-rest-api-request.util';
 import { deleteAllRecords } from 'test/integration/utils/delete-all-records';
 import { generateRecordName } from 'test/integration/utils/generate-record-name';
