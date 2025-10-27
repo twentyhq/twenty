b978b2876016f9fc363f322c31ef9df6bfa03050
neo773 <62795688+neo773@users.noreply.github.com>
2025-10-27 17:51:38 +0530

    fix gmail access token (#15374)

--- packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/connected-account-refresh-tokens-manager.module.ts
+++ packages/twenty-server/src/modules/connected-account/refresh-tokens-manager/connected-account-refresh-tokens-manager.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { JwtModule } from 'src/engine/core-modules/jwt/jwt.module';
+import { GoogleAPIRefreshAccessTokenModule } from 'src/modules/connected-account/refresh-tokens-manager/drivers/google/google-api-refresh-access-token.module';
 import { MicrosoftAPIRefreshAccessTokenModule } from 'src/modules/connected-account/refresh-tokens-manager/drivers/microsoft/microsoft-api-refresh-access-token.module';
 import { ConnectedAccountRefreshTokensService } from 'src/modules/connected-account/refresh-tokens-manager/services/connected-account-refresh-tokens.service';
 
 @Module({
-  imports: [JwtModule, MicrosoftAPIRefreshAccessTokenModule],
+  imports: [
+    JwtModule,
+    GoogleAPIRefreshAccessTokenModule,
+    MicrosoftAPIRefreshAccessTokenModule,
+  ],
   providers: [ConnectedAccountRefreshTokensService],
   exports: [ConnectedAccountRefreshTokensService],
 })
