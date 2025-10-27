7aedd1843e7dcdce06a6b7a461d7e125e493e6f2
prastoin <paul@twenty.com>
2025-10-27 11:49:53 +0100

    test(front): fix

--- packages/twenty-front/src/modules/object-metadata/hooks/__tests__/useDeleteOneObjectMetadataItem.test.tsx
+++ packages/twenty-front/src/modules/object-metadata/hooks/__tests__/useDeleteOneObjectMetadataItem.test.tsx
@@ -1,7 +1,4 @@
-import { MockedProvider } from '@apollo/client/testing';
 import { act, renderHook } from '@testing-library/react';
-import { type ReactNode } from 'react';
-import { RecoilRoot } from 'recoil';
 
 import { useDeleteOneObjectMetadataItem } from '@/object-metadata/hooks/useDeleteOneObjectMetadataItem';
 
@@ -14,6 +11,7 @@ import {
 import { jestExpectSuccessfulMetadataRequestResult } from '@/object-metadata/hooks/__tests__/utils/jest-expect-metadata-request-status.util';
 import { GET_CURRENT_USER } from '@/users/graphql/queries/getCurrentUser';
 import { FIND_ALL_CORE_VIEWS } from '@/views/graphql/queries/findAllCoreViews';
+import { getJestMetadataAndApolloMocksWrapper } from '~/testing/jest/getJestMetadataAndApolloMocksWrapper';
 import { mockedUserData } from '~/testing/mock-data/users';
 import { mockedCoreViewsData } from '~/testing/mock-data/views';
 import {
@@ -66,13 +64,9 @@ const mocks = [
   },
 ];
 
-const Wrapper = ({ children }: { children: ReactNode }) => (
-  <RecoilRoot>
-    <MockedProvider mocks={mocks} addTypename={false}>
-      {children}
-    </MockedProvider>
-  </RecoilRoot>
-);
+const Wrapper = getJestMetadataAndApolloMocksWrapper({
+  apolloMocks: mocks,
+});
 
 describe('useDeleteOneObjectMetadataItem', () => {
   it('should work as expected', async () => {
@@ -85,7 +79,7 @@ describe('useDeleteOneObjectMetadataItem', () => {
         await result.current.deleteOneObjectMetadataItem('idToDelete');
 
       jestExpectSuccessfulMetadataRequestResult(res);
-      expect(res.response).toEqual({ deleteOneObject: responseData });
+      expect(res.response).toEqual({ data: { deleteOneObject: responseData } });
     });
   });
 });
