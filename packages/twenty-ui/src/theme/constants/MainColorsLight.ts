4c5057db54f7acaad63ef56a204f49e4a3adc502
bosiraphael <raphael.bosi@gmail.com>
2025-10-28 19:06:10 +0100

    add new colors

--- packages/twenty-ui/src/theme/constants/MainColorsLight.ts
+++ packages/twenty-ui/src/theme/constants/MainColorsLight.ts
@@ -2,14 +2,45 @@ import * as RadixColors from '@radix-ui/colors';
 import { GRAY_SCALE_LIGHT } from './GrayScaleLight';
 
 export const MAIN_COLORS_LIGHT = {
-  green: RadixColors.greenP3.green9,
+  // Grays
+  gray: GRAY_SCALE_LIGHT.gray7,
+  mauve: RadixColors.mauveP3.mauve9,
+  slate: RadixColors.slateP3.slate9,
+  sage: RadixColors.sageP3.sage9,
+  olive: RadixColors.oliveP3.olive9,
+  sand: RadixColors.sandP3.sand9,
+
+  // Reds
+  tomato: RadixColors.tomatoP3.tomato9,
+  red: RadixColors.redP3.red9,
+  ruby: RadixColors.rubyP3.ruby9,
+  crimson: RadixColors.crimsonP3.crimson9,
+
+  // Pinks & Purples
+  pink: RadixColors.pinkP3.pink9,
+  plum: RadixColors.plumP3.plum9,
+  purple: RadixColors.purpleP3.purple9,
+  violet: RadixColors.violetP3.violet9,
+  iris: RadixColors.irisP3.iris9,
+
+  // Cyans & Blues
+  cyan: RadixColors.cyanP3.cyan9,
   turquoise: RadixColors.tealP3.teal9,
   sky: RadixColors.skyP3.sky9,
   blue: RadixColors.indigoP3.indigo9,
-  purple: RadixColors.violetP3.violet9,
-  pink: RadixColors.pinkP3.pink9,
-  red: RadixColors.redP3.red9,
+
+  // Greens
+  jade: RadixColors.jadeP3.jade9,
+  green: RadixColors.greenP3.green9,
+  grass: RadixColors.grassP3.grass9,
+  mint: RadixColors.mintP3.mint9,
+  lime: RadixColors.limeP3.lime9,
+
+  // Oranges & Yellows
+  bronze: RadixColors.bronzeP3.bronze9,
+  gold: RadixColors.goldP3.gold9,
+  brown: RadixColors.brownP3.brown9,
   orange: RadixColors.orangeP3.orange9,
+  amber: RadixColors.amberP3.amber9,
   yellow: RadixColors.yellowP3.yellow9,
-  gray: GRAY_SCALE_LIGHT.gray7,
 };
