1590751131f2647da81b41fe545b396c4fe5bdb4
Thomas Trompette <thomas.trompette@sfr.fr>
2025-10-28 17:13:39 +0100

    Set formatted before instead of after (#15401)
    Soft deletion events use before in event properties. But we set the
    after instead, that only contains a few data.
    
    Fixes https://github.com/twentyhq/twenty/issues/15120
    
    Debugging raised an additional issue. UpdateMany, RestoreMany and
    SoftDeleteMany were broken for custom objects. The `where` clause was
    using "_objectName.id" instead of "objectName.id" during select. That's
    why we need to use different where clause between the select of the
    existing value and the actual mutation.
    
    ---------
    
    Co-authored-by: Charles Bochet <charles@twenty.com>

--- packages/twenty-server/src/engine/twenty-orm/repository/workspace-soft-delete-query-builder.ts
+++ packages/twenty-server/src/engine/twenty-orm/repository/workspace-soft-delete-query-builder.ts
@@ -6,6 +6,7 @@ import {
   type UpdateResult,
 } from 'typeorm';
 import { SoftDeleteQueryBuilder } from 'typeorm/query-builder/SoftDeleteQueryBuilder';
+import { type WhereClause } from 'typeorm/query-builder/WhereClause';
 
 import { type FeatureFlagMap } from 'src/engine/core-modules/feature-flag/interfaces/feature-flag-map.interface';
 import { type WorkspaceInternalContext } from 'src/engine/twenty-orm/interfaces/workspace-internal-context.interface';
@@ -19,11 +20,13 @@ import {
 } from 'src/engine/twenty-orm/exceptions/twenty-orm.exception';
 import { validateQueryIsPermittedOrThrow } from 'src/engine/twenty-orm/repository/permissions.utils';
 import { type WorkspaceDeleteQueryBuilder } from 'src/engine/twenty-orm/repository/workspace-delete-query-builder';
-import { type WorkspaceSelectQueryBuilder } from 'src/engine/twenty-orm/repository/workspace-select-query-builder';
+import { WorkspaceSelectQueryBuilder } from 'src/engine/twenty-orm/repository/workspace-select-query-builder';
 import { type WorkspaceUpdateQueryBuilder } from 'src/engine/twenty-orm/repository/workspace-update-query-builder';
+import { applyTableAliasOnWhereCondition } from 'src/engine/twenty-orm/utils/apply-table-alias-on-where-condition';
 import { formatResult } from 'src/engine/twenty-orm/utils/format-result.util';
 import { formatTwentyOrmEventToDatabaseBatchEvent } from 'src/engine/twenty-orm/utils/format-twenty-orm-event-to-database-batch-event.util';
 import { getObjectMetadataFromEntityTarget } from 'src/engine/twenty-orm/utils/get-object-metadata-from-entity-target.util';
+import { computeTableName } from 'src/engine/utils/compute-table-name.util';
 
 export class WorkspaceSoftDeleteQueryBuilder<
   T extends ObjectLiteral,
@@ -78,10 +81,43 @@ export class WorkspaceSoftDeleteQueryBuilder<
         this.internalContext,
       );
 
+      const eventSelectQueryBuilder = new WorkspaceSelectQueryBuilder(
+        this as unknown as WorkspaceSelectQueryBuilder<T>,
+        this.objectRecordsPermissions,
+        this.internalContext,
+        true,
+        this.authContext,
+        this.featureFlagMap,
+      );
+
+      const tableName = computeTableName(
+        objectMetadata.nameSingular,
+        objectMetadata.isCustom,
+      );
+
+      eventSelectQueryBuilder.expressionMap.wheres = this.expressionMap.wheres;
+      eventSelectQueryBuilder.expressionMap.aliases =
+        this.expressionMap.aliases;
+      eventSelectQueryBuilder.setParameters(this.getParameters());
+
+      const before = await eventSelectQueryBuilder.getMany();
+
+      this.expressionMap.wheres = applyTableAliasOnWhereCondition({
+        condition: this.expressionMap.wheres,
+        tableName,
+        aliasName: objectMetadata.nameSingular,
+      }) as WhereClause[];
+
       const after = await super.execute();
 
       const formattedAfter = formatResult<T[]>(
         after.raw,
+        objectMetadata,
+        this.internalContext.objectMetadataMaps,
+      );
+
+      const formattedBefore = formatResult<T[]>(
+        before,
         objectMetadata,
         this.internalContext.objectMetadataMaps,
       );
@@ -91,7 +127,7 @@ export class WorkspaceSoftDeleteQueryBuilder<
           action: DatabaseEventAction.DELETED,
           objectMetadataItem: objectMetadata,
           workspaceId: this.internalContext.workspaceId,
-          entities: formattedAfter,
+          entities: formattedBefore,
           authContext: this.authContext,
         }),
       );
