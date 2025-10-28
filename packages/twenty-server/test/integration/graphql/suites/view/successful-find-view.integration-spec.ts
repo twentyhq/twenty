07444c21d9eeac96db3ef4472a1ff9bd630225f9
prastoin <paul@twenty.com>
2025-10-28 10:57:45 +0100

    test(server): successful view find

--- packages/twenty-server/test/integration/graphql/suites/view/successful-find-view.integration-spec.ts
+++ packages/twenty-server/test/integration/graphql/suites/view/successful-find-view.integration-spec.ts
@@ -67,7 +67,6 @@ describe.skip('successful find view with all sub-relations (e2e)', () => {
       const testView = viewsData.getCoreViews.find(
         (view) => view.key === ViewKey.INDEX,
       );
-
       jestExpectToBeDefined(testView);
 
       expect(testView).toMatchSnapshot(
