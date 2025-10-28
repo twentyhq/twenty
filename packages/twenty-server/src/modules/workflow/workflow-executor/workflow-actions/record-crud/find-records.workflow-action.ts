be08233fa4df8866d1e343e31101efe5330e08bb
Thomas Trompette <thomas.trompette@sfr.fr>
2025-10-28 09:54:47 +0100

    Add sort section in find records action (#15340)
    https://github.com/user-attachments/assets/020f3e97-316b-41db-b95d-7b938a7f82cf

--- packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/record-crud/find-records.workflow-action.ts
+++ packages/twenty-server/src/modules/workflow/workflow-executor/workflow-actions/record-crud/find-records.workflow-action.ts
@@ -67,7 +67,7 @@ export class FindRecordsWorkflowAction implements WorkflowAction {
     const toolOutput = await this.findRecordsService.execute({
       objectName: workflowActionInput.objectName,
       filter: workflowActionInput.filter?.gqlOperationFilter,
-      orderBy: workflowActionInput.orderBy,
+      orderBy: workflowActionInput.orderBy?.gqlOperationOrderBy,
       limit: workflowActionInput.limit,
       workspaceId,
       rolePermissionConfig: executionContext.rolePermissionConfig,
