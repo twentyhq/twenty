473efb5dc51c4656ba304b54b681a0874dd9f562
Marie <51697796+ijreilly@users.noreply.github.com>
2025-10-29 14:49:10 +0100

    [groupBy] Order by within records (#15404)
    Closes https://github.com/twentyhq/core-team-issues/issues/1727
    
    <img width="1415" height="625" alt="image"
    src="https://github.com/user-attachments/assets/cd132582-4a95-463c-8946-87b77670e023"
    />

--- packages/twenty-server/src/engine/api/rest/input-request-parsers/order-by-with-group-by-parser-utils/parse-order-by-for-records-rest-request.util.ts
+++ packages/twenty-server/src/engine/api/rest/input-request-parsers/order-by-with-group-by-parser-utils/parse-order-by-for-records-rest-request.util.ts
@@ -1,14 +1,12 @@
-//TODO : Refacto-common - remove this comment - This parser is a copy of the OrderByInputFactory without objectMetadata dependency. Validation will be done in common layer
-
 import { type ObjectRecordOrderBy } from 'src/engine/api/graphql/workspace-query-builder/interfaces/object-record.interface';
 
 import { parseOrderByRestRequestCommon } from 'src/engine/api/rest/input-request-parsers/order-by-parser-utils/utils/parse-order-by-rest-request-common.util';
 import { type AuthenticatedRequest } from 'src/engine/api/rest/types/authenticated-request';
 
-export const parseOrderByRestRequest = (
+export const parseOrderByForRecordsWithGroupByRestRequest = (
   request: AuthenticatedRequest,
-): ObjectRecordOrderBy => {
-  const orderByQuery = request.query.order_by;
+): ObjectRecordOrderBy | undefined => {
+  const orderByForRecordsWithGroupByQuery = request.query.order_by_for_records;
 
-  return parseOrderByRestRequestCommon(orderByQuery);
+  return parseOrderByRestRequestCommon(orderByForRecordsWithGroupByQuery);
 };
