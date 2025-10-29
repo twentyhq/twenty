fcd259133eff96796fe0853ad2a349ad68482e50
bosiraphael <raphael.bosi@gmail.com>
2025-10-29 18:38:01 +0100

    fix accent color

--- packages/twenty-ui/src/theme/constants/AccentDark.ts
+++ packages/twenty-ui/src/theme/constants/AccentDark.ts
@@ -4,7 +4,7 @@ export const ACCENT_DARK = {
   primary: COLOR_DARK.blue5,
   secondary: COLOR_DARK.blue5,
   tertiary: COLOR_DARK.blue3,
-  quaternary: COLOR_DARK.blue3,
+  quaternary: COLOR_DARK.blue2,
   accent3570: COLOR_DARK.blue8,
   accent4060: COLOR_DARK.blue8,
 };
--- packages/twenty-ui/src/theme/constants/AccentLight.ts
+++ packages/twenty-ui/src/theme/constants/AccentLight.ts
@@ -4,7 +4,7 @@ export const ACCENT_LIGHT = {
   primary: COLOR_LIGHT.blue5,
   secondary: COLOR_LIGHT.blue5,
   tertiary: COLOR_LIGHT.blue3,
-  quaternary: COLOR_LIGHT.blue3,
+  quaternary: COLOR_LIGHT.blue2,
   accent3570: COLOR_LIGHT.blue8,
   accent4060: COLOR_LIGHT.blue8,
 };
