e9fc80b05b46e873e0456250880991b485a2b9d7
BOHEUS <nothin_spe@2abc.hub.pl>
2025-11-02 04:55:11 +0100

    Added proper error handling, optimizations and linting

--- packages/twenty-apps/updated-by/README.md
+++ packages/twenty-apps/updated-by/README.md
@@ -6,10 +6,33 @@ Updates Updated by field with details of person behind newest update
 - twenty-cli `npm install -g twenty-cli`
 - an `apiKey`. Go to `https://twenty.com/settings/api-webhooks` to generate one
 
+## Quick start
 
-## Install to your Twenty workspace
-
+1. Add application
 ```bash
 twenty auth login
+cd packages/twenty-apps/updated-by
 twenty app sync
 ```
+
+2. Configure **TWENTY_API_KEY**
+
+Go to Settings > Applications > Updated by > Settings and add Twenty API key used to
+send requests to Twenty. 
+
+**If you're using self-hosted, you have to add also URL to your workspace.**
+
+## Flow
+
+1. Check if Twenty API key is added, if not, exit
+2. Check if updated record belongs to an object which shouldn't have a updatedBy field (like blocklists or messages), if yes, exit
+3. Check if updated record has updatedBy field, if not, create it
+4. Check if updated field in record is updatedBy field, if yes, return preemptively
+5. Update record with workspace member ID
+
+## Notes
+
+- Updated by field shouldn't be changed by users, only by extension 
+- Amount of API requests is reduced to possible minimum
+- New version of app will come out once type of object to which updated record belongs to will be exposed in properties 
+(more optimizations regarding amount of requests and computation will be done)
--- packages/twenty-apps/updated-by/serverlessFunctions/updated/src/index.ts
+++ packages/twenty-apps/updated-by/serverlessFunctions/updated/src/index.ts
@@ -1,69 +1,70 @@
 import axios from 'axios';
 
-const TWENTY_API_KEY: string = process.env.TWENTY_API_KEY !== "" && process.env.TWENTY_API_KEY !== undefined ? process.env.TWENTY_API_KEY : "";
-const TWENTY_API_URL: string = process.env.TWENTY_API_URL !== "" && process.env.TWENTY_API_URL !== undefined ? process.env.TWENTY_API_URL : "https://api.twenty.com";
+const TWENTY_API_KEY: string = process.env.TWENTY_API_KEY ?? '';
+const TWENTY_API_URL: string =
+  process.env.TWENTY_API_URL ?? 'https://api.twenty.com';
 const objectsNotForUpdate: string[] = [
-  "messageParticipants",
-  "blocklists",
-  "favoriteFolders",
-  "calendarEvents",
-  "attachments",
-  "workflowRuns",
-  "workflowAutomatedTriggers",
-  "messages",
-  "timelineActivities",
-  "workflows",
-  "calendarChannels",
-  "calendarChannelEventAssociations",
-  "viewFilters",
-  "viewFields",
-  "calendarEventParticipants",
-  "messageChannels",
-  "workspaceMembers",
-  "messageFolders",
-  "connectedAccounts",
-  "viewFilterGroups",
-  "noteTargets",
-  "views",
-  "workflowVersions",
-  "taskTargets",
-  "viewSorts",
-  "messageThreads",
-  "favorites",
-  "messageChannelMessageAssociations",
-  "viewGroups"
+  'messageParticipants',
+  'blocklists',
+  'favoriteFolders',
+  'calendarEvents',
+  'attachments',
+  'workflowRuns',
+  'workflowAutomatedTriggers',
+  'messages',
+  'timelineActivities',
+  'workflows',
+  'calendarChannels',
+  'calendarChannelEventAssociations',
+  'viewFilters',
+  'viewFields',
+  'calendarEventParticipants',
+  'messageChannels',
+  'workspaceMembers',
+  'messageFolders',
+  'connectedAccounts',
+  'viewFilterGroups',
+  'noteTargets',
+  'views',
+  'workflowVersions',
+  'taskTargets',
+  'viewSorts',
+  'messageThreads',
+  'favorites',
+  'messageChannelMessageAssociations',
+  'viewGroups',
 ];
 
-const fetchObjectId = async (objectName: string): Promise<string> => {
+const fetchAllObjects = async (): Promise<Record<string, string>> => {
   const options = {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${TWENTY_API_KEY}`,
     },
-    url: `${TWENTY_API_URL}/rest/metadata/objects`
+    url: `${TWENTY_API_URL}/rest/metadata/objects`,
   };
   try {
     const response = await axios(options);
-    if (response.status === 200) {
-      return response.data.data.objects.find(
-        (object: any) => object.nameSingular === objectName,
-      ).id !== undefined
-        ? response.data.data.objects.find(
-            (object: any) => object.nameSingular === objectName,
-          ).id
-        : '';
+    let objects: Record<string, string> = {};
+    const objectsToParse: Record<string, any>[] = response.data.data.objects;
+    objectsToParse.forEach((object) => {
+      objects[object.namePlural] = object.id;
+    });
+    return objects;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error;
     }
-    return "";
+    throw error;
   }
-  catch (error) {
-    console.error(error);
-    return "";
-  }
-}
+};
 
-const createUpdatedByField = async (objectName: string): Promise<boolean> => {
-  const sourceObjectId: string = await fetchObjectId(objectName);
-  const targetObjectId: string = await fetchObjectId("workspaceMember");
+const createUpdatedByField = async (
+  twentyObjects: Record<string, string>,
+  objectName: string,
+): Promise<boolean> => {
+  const sourceObjectId: string = twentyObjects[objectName];
+  const targetObjectId: string = twentyObjects['workspaceMembers'];
   // taken directly from Network tab
   const GraphQLQuery: string = `mutation CreateOneFieldMetadataItem($input: CreateOneFieldMetadataInput!) {
   createOneField(input: $input) {
@@ -92,157 +93,163 @@ const createUpdatedByField = async (objectName: string): Promise<boolean> => {
 }`;
 
   const variables = {
-    "input": {
-      "field": {
-        "description": "",
-        "icon": "IconRelationOneToMany",
-        "label": "Updated by",
-        "name": "updatedBy",
-        "isLabelSyncedWithName": true,
-        "objectMetadataId": `${sourceObjectId}`,
-        "type": "RELATION",
-        "relationCreationPayload": {
-          "type": "MANY_TO_ONE",
-          "targetObjectMetadataId": `${targetObjectId}`,
-          "targetFieldLabel": `Updated by ${objectName}`,
-          "targetFieldIcon": "IconUsers"
-        }
-      }
-    }
+    input: {
+      field: {
+        description: '',
+        icon: 'IconRelationOneToMany',
+        label: 'Updated by',
+        name: 'updatedBy',
+        isLabelSyncedWithName: true,
+        objectMetadataId: `${sourceObjectId}`,
+        type: 'RELATION',
+        relationCreationPayload: {
+          type: 'MANY_TO_ONE',
+          targetObjectMetadataId: `${targetObjectId}`,
+          targetFieldLabel: `Updated by ${objectName}`,
+          targetFieldIcon: 'IconUsers',
+        },
+      },
+    },
   };
 
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${TWENTY_API_KEY}`
+      Authorization: `Bearer ${TWENTY_API_KEY}`,
     },
     url: `${TWENTY_API_URL}/graphql`,
     data: {
       query: GraphQLQuery,
-      variables: variables
-    }
-  }
-  try {
-    const temp = await axios.request(options);
-    return temp.status === 200;
-  }
-  catch (error) {
-    console.error(error);
-    return false;
-  }
-}
-
-const updateUpdatedByField = async (objectName: string, userId: string, recordId: string): Promise<boolean> => {
-  const options = {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${TWENTY_API_KEY}`
+      variables: variables,
     },
-    url: `${TWENTY_API_URL}/rest/${objectName}/${recordId}`,
-    data: {
-      updatedBy: `${userId}`
-    }
   };
   try {
     const response = await axios.request(options);
     return response.status === 200;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error;
+    }
+    throw error;
   }
-  catch (error) {
-    console.error(error);
-    return false;
-  }
-}
+};
 
-const helperFinderObjectName = async (objectName: string, objectId: string): Promise<string> => {
+const updateUpdatedByField = async (
+  objectName: string,
+  userId: string,
+  recordId: string,
+): Promise<boolean> => {
   const options = {
-    method: 'GET',
+    method: 'PATCH',
     headers: {
-      Authorization: `Bearer ${TWENTY_API_KEY}`
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TWENTY_API_KEY}`,
     },
-    url: `${TWENTY_API_URL}/rest/${objectName}/${objectId}`,
-  }
-  try {
-    const response = await axios(options);
-    return response.status === 200 ? objectName : "";
-  }
-  catch (error) {
-    console.error(error);
-    return "";
-  }
-}
-
-const findObjectName = async (objectId: string): Promise<string> => {
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${TWENTY_API_KEY}`
+    url: `${TWENTY_API_URL}/rest/${objectName}/${recordId}`,
+    data: {
+      updatedBy: `${userId}`,
     },
-    url: `${TWENTY_API_URL}/rest/metadata/objects`,
-  }
+  };
   try {
     const response = await axios.request(options);
-    const all_objects: string[] = [];
-    response.data.data.objects.forEach((object: any) => {all_objects.push(object.namePlural)});
-    const changeable_objects: string[] = all_objects.filter((o) => objectsNotForUpdate.indexOf(o) === -1);
-    for (const o of changeable_objects) {
-      const temp: string = await helperFinderObjectName(o, objectId);
+    return response.status === 200;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error;
+    }
+    throw error;
+  }
+};
+
+const helperFinderObjectName = async (
+  objectName: string,
+  objectId: string,
+): Promise<string> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${TWENTY_API_KEY}`,
+    },
+    url: `${TWENTY_API_URL}/rest/${objectName}/${objectId}`,
+  };
+  try {
+  const response = await axios.request(options);
+  return response.status === 200 ? objectName : '';
+  } catch (error) {
+    return "";
+  }
+};
+
+const findObjectName = async (
+  twentyObjects: Record<string, string>,
+  objectId: string,
+): Promise<string> => {
+  const changeable_objects: string[] = Object.keys(twentyObjects).filter(
+    (key: string) => objectsNotForUpdate.indexOf(key) === -1,
+  );
+  console.log(changeable_objects.toString());
+    for (const object of changeable_objects) {
+      const temp: string = await helperFinderObjectName(object, objectId);
+      console.log(temp);
       if (temp !== '') {
         return temp;
       }
     }
-    return "";
-  }
-  catch (error) {
-    console.error(error);
-    return "";
-  }
-}
+    return '';
+};
 
 export const main = async (params: {
   properties: Record<string, any>;
   recordId: string;
   userId: string;
 }): Promise<object> => {
-  if (TWENTY_API_KEY === "" || TWENTY_API_URL === "") {
-    console.error("You must specify a valid TWENTY_API_URL");
+  if (TWENTY_API_KEY === '') {
+    console.error('You must specify a valid TWENTY_API_KEY');
     return {};
   }
+  try {
+    const { properties, recordId, userId } = params;
 
-  const { properties, recordId, userId } = params;
-
-  // const objectName = properties.after; //TODO: uncomment once object type is exposed in properties
-  const objectName = await findObjectName(recordId);
-
-  // Check if updated record belongs to an object which shouldn't have a updatedBy field
-  // Check if updated record has updatedBy field, if not, create it
-  // Check if updated field in record is updatedBy field, if yes, return preemptively
-  // Update record with person ID
-
-  if (objectsNotForUpdate.includes(objectName)) {
-    return {};
-  }
-
-  if (properties.updatedFields.length === 1 && properties.updatedFields.includes("updatedBy")) {
-    return {}; // if last update was updatedBy field, don't update
-  }
-
-  if (properties.after.updatedBy === undefined) {
-    const isFieldCreated: boolean = await createUpdatedByField(objectName);
-    if (!isFieldCreated) {
-      console.error("Creation of updated by field failed");
-      return {};
+    if (
+      properties.updatedFields?.length === 1 &&
+      properties.updatedFields.includes('updatedBy')
+    ) {
+      return {}; // if last update was updatedBy field, don't update
     }
-  }
+    const twentyObjects: Record<string, string> = await fetchAllObjects();
 
-  const isObjectUpdated = await updateUpdatedByField(objectName, userId, recordId);
+    // const objectName = properties.after; //TODO: uncomment once object type is exposed in properties
+    const objectName: string = await findObjectName(twentyObjects, recordId);
 
-  if (isObjectUpdated) {
-    console.log(`Field updatedBy in object ${objectName} has been updated`);
+    if (properties.after.updatedBy === undefined) {
+      const isFieldCreated: boolean = await createUpdatedByField(
+        twentyObjects,
+        objectName,
+      );
+      if (!isFieldCreated) {
+        console.error('Creation of updated by field failed');
+        return {};
+      }
+    }
+
+    const isObjectUpdated: boolean = await updateUpdatedByField(
+      objectName,
+      userId,
+      recordId,
+    );
+
+    if (isObjectUpdated) {
+      console.log(`Field updatedBy in object ${objectName} has been updated`);
+    }
+    return {};
+  } catch (error) {
+    console.error('Exiting because of error');
+    if (axios.isAxiosError(error)) {
+      console.error(error.message);
+    } else {
+      console.error(error);
+    }
+    return {};
   }
-  else {
-    console.log("Error")
-  }
-  return {  };
 };
