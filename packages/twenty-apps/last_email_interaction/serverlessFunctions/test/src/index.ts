38fe304fbd733248e97fbcb33ed7d9fa0c18355d
BOHEUS <nothin_spe@2abc.hub.pl>
2025-11-03 10:57:00 +0100

    Fix README and change logic

--- packages/twenty-apps/last_email_interaction/serverlessFunctions/test/src/index.ts
+++ packages/twenty-apps/last_email_interaction/serverlessFunctions/test/src/index.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { setTimeout } from 'timers/promises';
 
-const TWENTY_API_KEY = process.env.TWENTY_API_KEY !== "" && process.env.TWENTY_API_KEY !== undefined ? process.env.TWENTY_API_KEY : "";
-const TWENTY_URL = process.env.TWENTY_API_URL !== "" && process.env.TWENTY_API_URL !== undefined ? `${process.env.TWENTY_API_URL}/rest` : "https://api.twenty.com/rest";
+const TWENTY_API_KEY = process.env.TWENTY_API_KEY ?? '';
+const TWENTY_URL =
+  process.env.TWENTY_API_URL !== '' && process.env.TWENTY_API_URL !== undefined
+    ? `${process.env.TWENTY_API_URL}/rest`
+    : 'https://api.twenty.com/rest';
 const DELAY = 500;
 
 const create_last_interaction = (id: string) => {
@@ -11,21 +14,21 @@ const create_last_interaction = (id: string) => {
     url: `${TWENTY_URL}/metadata/fields`,
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${TWENTY_API_KEY}`
+      Authorization: `Bearer ${TWENTY_API_KEY}`,
     },
     data: {
-      "type": "DATE_TIME",
-      "objectMetadataId": `${id}`,
-      "name": "lastInteraction",
-      "label": "Last interaction",
-      "description": "Date when the last interaction happened",
-      "icon": "IconCalendarClock",
-      "defaultValue": null,
-      "isNullable": false,
-      "settings": {}
-    }
+      type: 'DATE_TIME',
+      objectMetadataId: `${id}`,
+      name: 'lastInteraction',
+      label: 'Last interaction',
+      description: 'Date when the last interaction happened',
+      icon: 'IconCalendarClock',
+      defaultValue: null,
+      isNullable: true,
+      settings: {},
+    },
   };
-}
+};
 
 const create_interaction_status = (id: string) => {
   return {
@@ -33,71 +36,75 @@ const create_interaction_status = (id: string) => {
     url: `${TWENTY_URL}/metadata/fields`,
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${TWENTY_API_KEY}`
+      Authorization: `Bearer ${TWENTY_API_KEY}`,
     },
     data: {
-      "type": "SELECT",
-      "objectMetadataId": `${id}`,
-      "name": "interactionStatus",
-      "label": "Interaction status",
-      "description": "Indicates the health of relation",
-      "icon": "IconProgress",
-      "defaultValue": null,
-      "isNullable": false,
-      "settings": {},
-      "options": [
+      type: 'SELECT',
+      objectMetadataId: `${id}`,
+      name: 'interactionStatus',
+      label: 'Interaction status',
+      description: 'Indicates the health of relation',
+      icon: 'IconProgress',
+      defaultValue: null,
+      isNullable: true,
+      settings: {},
+      options: [
         {
-          "color": "green",
-          "label": "Recent",
-          "value": "RECENT",
-          "position": 1
+          color: 'green',
+          label: 'Recent',
+          value: 'RECENT',
+          position: 1,
         },
         {
-          "color": "yellow",
-          "label": "Active",
-          "value": "ACTIVE",
-          "position": 2
+          color: 'yellow',
+          label: 'Active',
+          value: 'ACTIVE',
+          position: 2,
         },
         {
-          "color": "sky",
-          "label": "Cooling",
-          "value": "COOLING",
-          "position": 3
+          color: 'sky',
+          label: 'Cooling',
+          value: 'COOLING',
+          position: 3,
         },
         {
-          "color": "gray",
-          "label": "Dormant",
-          "value": "DORMANT",
-          "position": 4
+          color: 'gray',
+          label: 'Dormant',
+          value: 'DORMANT',
+          position: 4,
         },
-      ]
-    }
+      ],
+    },
   };
-}
+};
 
 const calculateStatus = (date: string) => {
   const day = 1000 * 60 * 60 * 24;
   const now = Date.now();
   const messageDate = Date.parse(date);
   const deltaTime = now - messageDate;
-  return deltaTime < (7 * day) ? 'RECENT' : (deltaTime < (30 * day) ? 'ACTIVE' : (deltaTime < (90 * day) ? 'COOLING' : 'DORMANT'));
-}
+  return deltaTime < 7 * day
+    ? 'RECENT'
+    : deltaTime < 30 * day
+      ? 'ACTIVE'
+      : deltaTime < 90 * day
+        ? 'COOLING'
+        : 'DORMANT';
+};
 
 const interactionData = (date: string, status: string) => {
   return {
-    "lastInteraction": date,
-    "interactionStatus": [
-      status
-    ],
-  }
-}
+    lastInteraction: date,
+    interactionStatus: status,
+  };
+};
 
 export const main = async (params: {
-  properties: Record<string, any>,
-  recordId: string,
-  userId: string
+  properties: Record<string, any>;
+  recordId: string;
+  userId: string;
 }): Promise<object> => {
-  if (TWENTY_API_KEY === "" || TWENTY_URL === ""){
+  if (TWENTY_API_KEY === '') {
     console.log("Function exited as API key or URL hasn't been set properly");
     return {};
   }
@@ -108,44 +115,63 @@ export const main = async (params: {
     url: `${TWENTY_URL}/metadata/objects`,
     headers: {
       Authorization: `Bearer ${TWENTY_API_KEY}`,
-    }
+    },
   };
   try {
     const response = await axios.request(options);
-    await setTimeout(1000);
     const objects = response.data.data.objects;
-    const company_object = objects.find(object=>object.nameSingular==='company');
-    const company_last_interaction = company_object.fields.find(field=>field.name==='lastInteraction');
-    const company_interaction_status = company_object.fields.find(field=>field.name==='interactionStatus');
-    const person_object = objects.find(object=>object.nameSingular==='person');
-    const person_last_interaction = person_object.fields.find(field=>field.name==='lastInteraction');
-    const person_interaction_status = person_object.fields.find(field=>field.name==='interactionStatus');
+    const company_object = objects.find(
+      (object) => object.nameSingular === 'company',
+    );
+    const company_last_interaction = company_object.fields.find(
+      (field) => field.name === 'lastInteraction',
+    );
+    const company_interaction_status = company_object.fields.find(
+      (field) => field.name === 'interactionStatus',
+    );
+    const person_object = objects.find(
+      (object) => object.nameSingular === 'person',
+    );
+    const person_last_interaction = person_object.fields.find(
+      (field) => field.name === 'lastInteraction',
+    );
+    const person_interaction_status = person_object.fields.find(
+      (field) => field.name === 'interactionStatus',
+    );
     // If not, create them
     if (company_last_interaction === undefined) {
-      const response2 = await axios.request(create_last_interaction(company_object.id));
-      if (response2.status === 200) {
-        console.log("Successfully created company last interaction field");
+      const response2 = await axios.request(
+        create_last_interaction(company_object.id),
+      );
+      if (response2.status === 201) {
+        console.log('Successfully created company last interaction field');
       }
       await setTimeout(DELAY);
     }
     if (company_interaction_status === undefined) {
-      const response2 = await axios.request(create_interaction_status(company_object.id));
-      if (response2.status === 200) {
-        console.log("Successfully created company interaction status field");
+      const response2 = await axios.request(
+        create_interaction_status(company_object.id),
+      );
+      if (response2.status === 201) {
+        console.log('Successfully created company interaction status field');
       }
       await setTimeout(DELAY);
     }
     if (person_last_interaction === undefined) {
-      const response2 = await axios.request(create_last_interaction(person_object.id));
-      if (response2.status === 200) {
-        console.log("Successfully created person last interaction field");
+      const response2 = await axios.request(
+        create_last_interaction(person_object.id),
+      );
+      if (response2.status === 201) {
+        console.log('Successfully created person last interaction field');
       }
       await setTimeout(DELAY);
     }
     if (person_interaction_status === undefined) {
-      const response2 = await axios.request(create_interaction_status(person_object.id));
-      if (response2.status === 200) {
-        console.log("Successfully created person interaction status field");
+      const response2 = await axios.request(
+        create_interaction_status(person_object.id),
+      );
+      if (response2.status === 201) {
+        console.log('Successfully created person interaction status field');
       }
       await setTimeout(DELAY);
     }
@@ -159,13 +185,14 @@ export const main = async (params: {
       method: 'GET',
       url: `${TWENTY_URL}/messages/${recordId}?depth=1`,
       headers: {
-        Authorization: `Bearer ${TWENTY_API_KEY}`
-      }
-    }
+        Authorization: `Bearer ${TWENTY_API_KEY}`,
+      },
+    };
     const messageDetails = await axios.request(messageOptions);
     await setTimeout(DELAY);
     const peopleIds = [];
-    for (const participant of messageDetails.data.messages.messageParticipants) {
+    for (const participant of messageDetails.data.messages
+      .messageParticipants) {
       peopleIds.push(participant.personId);
     }
 
@@ -175,11 +202,19 @@ export const main = async (params: {
         method: 'GET',
         url: `${TWENTY_URL}/people/${id}`,
         headers: {
-          Authorization: `Bearer ${TWENTY_API_KEY}`
-        }
+          Authorization: `Bearer ${TWENTY_API_KEY}`,
+        },
       };
-      const req = await axios.request(options);
-      companiesIds.push(req.data.person.companyId);
+      try {
+        const req = await axios.request(options);
+        companiesIds.push(req.data.person.companyId);
+        await setTimeout(DELAY);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          throw error;
+        }
+        throw error;
+      }
     }
     // Update the field value depending on the timestamp
     for (const id of peopleIds) {
@@ -187,15 +222,22 @@ export const main = async (params: {
         method: 'PATCH',
         url: `${TWENTY_URL}/people/${id}`,
         headers: {
-          'Content-Type': "application/json",
-          Authorization: `Bearer ${TWENTY_API_KEY}`
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${TWENTY_API_KEY}`,
         },
-        data: interactionData(messageDate, interactionStatus)
-      }
-      const req = await axios.request(peopleOptions);
-      if (req.status === 200) {
-        console.log(`Successfully updated person with ID ${id}`);
-        await setTimeout(DELAY);
+        data: interactionData(messageDate, interactionStatus),
+      };
+      try {
+        const response = await axios.request(options);
+        if (response.status === 200) {
+          console.log('Successfully updated company last interaction field');
+          await setTimeout(DELAY);
+        }
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          throw error;
+        }
+        throw error;
       }
     }
 
@@ -205,18 +247,29 @@ export const main = async (params: {
         url: `${TWENTY_URL}/companies/${id}`,
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${TWENTY_API_KEY}`
+          Authorization: `Bearer ${TWENTY_API_KEY}`,
         },
-        data: interactionData(messageDate, interactionStatus)
-      }
-      const req = await axios.request(companiesOptions);
-      if (req.status === 200) {
-        console.log(`Successfully updated company with ID ${id}`);
-        await setTimeout(1000);
+        data: interactionData(messageDate, interactionStatus),
+      };
+      try {
+        const req = await axios.request(companiesOptions);
+        if (req.status === 200) {
+          console.log(`Successfully updated company with ID ${id}`);
+          await setTimeout(DELAY);
+        }
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          throw error;
+        }
+        throw error;
       }
     }
   } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error);
+      return {};
+    }
     console.log(error);
-    return error;
+    return {};
   }
 };
