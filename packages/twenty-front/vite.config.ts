50eb8c55586a052a7686b70811380d6dbc1db5f2
Charles Bochet <charlesBochet@users.noreply.github.com>
2025-11-03 12:36:46 +0100

    Add command to make sure v1.8 workspaces are not using FULL or PARTIAL sync stages (that should be already deprecated) (#15545)
    In v1.8, we have already run a command to deprecate FULL or PARTIAL sync
    stages.
    
    However the code was fully deprecated in v1.10 and some workspaces might
    still have this status used. This is to double check

--- packages/twenty-front/vite.config.ts
+++ packages/twenty-front/vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig(({ command, mode }) => {
   // Please don't increase this limit for main index chunk
   // If it gets too big then find modules in the code base
   // that can be loaded lazily, there are more!
-  const MAIN_CHUNK_SIZE_LIMIT = 5.7 * 1024 * 1024; // 5.5MB for main index chunk
+  const MAIN_CHUNK_SIZE_LIMIT = 5.9 * 1024 * 1024; // 5.5MB for main index chunk
   const OTHER_CHUNK_SIZE_LIMIT = 5 * 1024 * 1024; // 5MB for other chunks
 
   const checkers: Checkers = {
@@ -224,7 +224,7 @@ export default defineConfig(({ command, mode }) => {
             /*
             {
               name: 'add-prefetched-modules',
-              transformIndexHtml(html: string, 
+              transformIndexHtml(html: string,
                 ctx: {
                   path: string;
                   filename: string;
@@ -239,13 +239,13 @@ export default defineConfig(({ command, mode }) => {
                     (bundle) => bundle.endsWith('.map') === false
                   );
 
-                  
+
                   // Remove existing files and concatenate them into link tags
                   const prefechBundlesString = modernBundles
                     .filter((bundle) => html.includes(bundle) === false)
                     .map((bundle) => `<link rel="prefetch" href="${ctx.server?.config.base}${bundle}">`)
                     .join('');
-            
+
                   // Use regular expression to get the content within <head> </head>
                   const headContent = html.match(/<head>([\s\S]*)<\/head>/)?.[1] ?? '';
                   // Insert the content of prefetch into the head
@@ -255,10 +255,10 @@ export default defineConfig(({ command, mode }) => {
                     /<head>([\s\S]*)<\/head>/,
                     `<head>${newHeadContent}</head>`
                   );
-            
+
                   return html;
-            
-             
+
+
               },
             }*/
           ],
