a84b1c11d50f3b9e474028f0218d8d93b40d23c8
prastoin <paul@twenty.com>
2025-11-03 12:16:36 +0100

    refactor(server): builder to use new global utils

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/flat-entity-validation-args.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/flat-entity-validation-args.type.ts
@@ -2,13 +2,12 @@ import { type AllMetadataName } from 'twenty-shared/metadata';
 
 import { type MetadataFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/metadata-flat-entity-maps.type';
 import { type MetadataFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-flat-entity.type';
-import { type MetadataValidationRelatedFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/metadata-related-types.type';
+import { MetadataFlatEntityAndRelatedFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/metadata-related-types.type';
 import { type WorkspaceMigrationBuilderOptions } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/workspace-migration-builder-options.type';
 
 export type FlatEntityValidationArgs<T extends AllMetadataName> = {
   flatEntityToValidate: MetadataFlatEntity<T>;
-  optimisticFlatEntityMaps: MetadataFlatEntityMaps<T>;
-  mutableDependencyOptimisticFlatEntityMaps: MetadataValidationRelatedFlatEntityMaps<T>;
+  optimisticFlatEntityMapsAndRelatedFlatEntityMaps: MetadataFlatEntityAndRelatedFlatEntityMaps<T>;
   workspaceId: string;
   remainingFlatEntityMapsToValidate: MetadataFlatEntityMaps<T>;
   buildOptions: WorkspaceMigrationBuilderOptions;
