e64603e61a8cde65da253764385b4078f3bb2d59
Guillim <guillim@users.noreply.github.com>
2025-11-04 17:29:06 +0100

    release 1.10 flush cache command (#15610)

--- packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/upgrade.command.ts
@@ -12,6 +12,7 @@ import {
 import { AddWorkflowRunStopStatusesCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-add-workflow-run-stop-statuses.command';
 import { CleanOrphanedKanbanAggregateOperationFieldMetadataIdCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-clean-orphaned-kanban-aggregate-operation-field-metadata-id.command';
 import { CreateViewKanbanFieldMetadataIdForeignKeyMigrationCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-create-view-kanban-field-metadata-id-foreign-key-migration.command';
+import { FlushCacheCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-flush-cache.command';
 import { MakeSureDashboardNamingAvailableCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-make-sure-dashboard-naming-available.command';
 import { MigrateAttachmentAuthorToCreatedByCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-migrate-attachment-author-to-created-by.command';
 import { MigrateAttachmentTypeToFileCategoryCommand } from 'src/database/commands/upgrade-version-command/1-10/1-10-migrate-attachment-type-to-file-category.command';
@@ -67,6 +68,7 @@ export class UpgradeCommand extends UpgradeCommandRunner {
     protected readonly makeSureDashboardNamingAvailableCommand: MakeSureDashboardNamingAvailableCommand,
     protected readonly seedDashboardViewCommand: SeedDashboardViewCommand,
     protected readonly createViewKanbanFieldMetadataIdForeignKeyMigrationCommand: CreateViewKanbanFieldMetadataIdForeignKeyMigrationCommand,
+    protected readonly flushWorkspaceCacheCommand: FlushCacheCommand,
   ) {
     super(
       workspaceRepository,
@@ -111,6 +113,7 @@ export class UpgradeCommand extends UpgradeCommandRunner {
         this.migrateAttachmentAuthorToCreatedByCommand,
         this.migrateAttachmentTypeToFileCategoryCommand,
         this.seedDashboardViewCommand,
+        this.flushWorkspaceCacheCommand,
       ],
     };
 
