99e2f046f9f8dec96d3ba0c3d6feab4f2122a689
martmull <martmull@hotmail.fr>
2025-11-04 18:03:57 +0100

    Fix linkedin-applicaiton

--- packages/twenty-apps/hacktoberfest-2025/linkedin-browser-extension/application/serverlessFunctions/get-company/src/index.ts
+++ packages/twenty-apps/hacktoberfest-2025/linkedin-browser-extension/application/serverlessFunctions/get-company/src/index.ts
@@ -1,3 +1,5 @@
+import { type ServerlessFunctionConfig } from 'twenty-sdk/application';
+
 export const main = async (params: {
   a: string;
   b: number;
@@ -8,7 +10,19 @@ export const main = async (params: {
   // This is just an example
   const message = `Hello, input: ${a} and ${b}`;
 
-
-
   return { message };
+};
+
+export const config: ServerlessFunctionConfig = {
+  universalIdentifier: '8e43b96b-49a1-4e21-b257-e432a757b09f',
+  name: 'get-company',
+  triggers: [
+    {
+      universalIdentifier: '7a2bb8ad-6366-49ac-9f73-db9c4713c5af',
+      type: 'route',
+      path: '/get/company',
+      httpMethod: 'GET',
+      isAuthRequired: true,
+    },
+  ],
 };
--- packages/twenty-apps/hacktoberfest-2025/linkedin-browser-extension/application/serverlessFunctions/get-person/src/index.ts
+++ packages/twenty-apps/hacktoberfest-2025/linkedin-browser-extension/application/serverlessFunctions/get-person/src/index.ts
@@ -1,3 +1,5 @@
+import { type ServerlessFunctionConfig } from 'twenty-sdk/application';
+
 export const main = async (params: {
   a: string;
   b: number;
@@ -8,7 +10,19 @@ export const main = async (params: {
   // This is just an example
   const message = `Hello, input: ${a} and ${b}`;
 
-
-
   return { message };
+};
+
+export const config: ServerlessFunctionConfig = {
+  universalIdentifier: '87ea9816-c9e5-4860-b49f-a5f0759800f7',
+  name: 'get-person',
+  triggers: [
+    {
+      universalIdentifier: '54aec609-0518-4fb0-bd90-7cd21507fe11',
+      type: 'route',
+      path: '/get/person',
+      httpMethod: 'GET',
+      isAuthRequired: true,
+    },
+  ],
 };
