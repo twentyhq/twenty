016948033b741979ee8b278348f591d8e733d0a5
prastoin <paul@twenty.com>
2025-11-05 15:18:47 +0100

    test(server): integration testing workspace creation

---
+++ packages/twenty-server/test/integration/graphql/utils/sign-up-on-new-workspace.util.ts
@@ -0,0 +1,63 @@
+import gql from 'graphql-tag';
+
+import { type CommonResponseBody } from 'test/integration/metadata/types/common-response-body.type';
+import { warnIfErrorButNotExpectedToFail } from 'test/integration/metadata/utils/warn-if-error-but-not-expected-to-fail.util';
+import { warnIfNoErrorButExpectedToFail } from 'test/integration/metadata/utils/warn-if-no-error-but-expected-to-fail.util';
+
+import { SignUpOutput } from 'src/engine/core-modules/auth/dto/sign-up.output';
+import { makeGraphqlAPIRequest } from 'test/integration/graphql/utils/make-graphql-api-request.util';
+
+type SignUpOnNewWorkspaceUtilArgs = {
+  accessToken: string;
+  expectToFail?: boolean;
+};
+
+export const signUpOnNewWorkspace = async ({
+  accessToken,
+  expectToFail,
+}: SignUpOnNewWorkspaceUtilArgs): CommonResponseBody<{
+  signUpInNewWorkspace: SignUpOutput;
+}> => {
+  const mutation = gql`
+    mutation SignUpInNewWorkspace {
+      signUpInNewWorkspace {
+        loginToken {
+          token
+          expiresAt
+        }
+        workspace {
+          id
+          workspaceUrls {
+            subdomain
+            redirectUrl
+            workspaceUrl
+          }
+        }
+      }
+    }
+  `;
+
+  const response = await makeGraphqlAPIRequest(
+    {
+      query: mutation,
+      variables: {},
+    },
+    accessToken,
+  ).expect(200);
+
+  if (expectToFail === true) {
+    warnIfNoErrorButExpectedToFail({
+      response,
+      errorMessage: 'Sign up on new workspace should have failed but did not',
+    });
+  }
+
+  if (expectToFail === false) {
+    warnIfErrorButNotExpectedToFail({
+      response,
+      errorMessage: 'Sign up on new workspace has failed but should not',
+    });
+  }
+
+  return { data: response.body.data, errors: response.body.errors };
+};
