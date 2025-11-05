54aede89bc2db84de53b2e515b466c880fe47a0f
Devessier <baptiste@devessier.fr>
2025-11-05 17:50:34 +0100

    refactor: delete unused files

--- packages/twenty-front/src/modules/page-layout/hooks/useCurrentPageLayoutActiveTabs.ts
+++
@@ -1,9 +0,0 @@
-import { useCurrentPageLayout } from '@/page-layout/hooks/useCurrentPageLayout';
-
-export const useCurrentPageLayoutActiveTabs = () => {
-  const { currentPageLayout } = useCurrentPageLayout();
-
-  const activeTab = currentPageLayout?.tabs.find((tab) => tab.id === tabId);
-
-  return { activeTab };
-};
