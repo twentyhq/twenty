2f7185319ef0d0b038a327afbd69354f2b6472d7
bosiraphael <raphael.bosi@gmail.com>
2025-11-05 10:32:47 +0100

    animate actions

--- packages/twenty-front/src/modules/action-menu/components/PageHeaderActionMenuButtons.tsx
+++ packages/twenty-front/src/modules/action-menu/components/PageHeaderActionMenuButtons.tsx
@@ -1,13 +1,39 @@
 import { ActionComponent } from '@/action-menu/actions/display/components/ActionComponent';
 import { ActionMenuContext } from '@/action-menu/contexts/ActionMenuContext';
-
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
 import { useContext } from 'react';
+
+const StyledActionContainer = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const PageHeaderActionMenuButtons = () => {
   const { actions } = useContext(ActionMenuContext);
+  const theme = useTheme();
 
   const pinnedActions = actions.filter((entry) => entry.isPinned);
 
-  return pinnedActions.map((action) => (
-    <ActionComponent key={action.key} action={action} />
-  ));
+  return (
+    <>
+      {pinnedActions.map((action, index) => (
+        <StyledActionContainer
+          key={pinnedActions.length - index - 1}
+          layout
+          initial={{ width: 0, opacity: 0 }}
+          animate={{ width: 'unset', opacity: 1 }}
+          exit={{ width: 0, opacity: 0 }}
+          transition={{
+            duration: theme.animation.duration.instant,
+            ease: 'easeInOut',
+          }}
+        >
+          <ActionComponent action={action} />
+        </StyledActionContainer>
+      ))}
+    </>
+  );
 };
