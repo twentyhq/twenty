89d6e119dfc73f0201c829980aef5c8d2274c83e
prastoin <paul@twenty.com>
2025-11-06 15:15:15 +0100

    feat(server): finalize migrations

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1762343994716-add-workspace-custom-application-id-column.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1762343994716-add-workspace-custom-application-id-column.ts
@@ -7,7 +7,7 @@ export class AddWorkspaceCustomApplicationIdColumn1762343994716
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "core"."workspace" ADD "workspaceCustomApplicationId" uuid NOT NULL`,
+      `ALTER TABLE "core"."workspace" ADD "workspaceCustomApplicationId" uuid`,
     );
     await queryRunner.query(
       `DROP INDEX "core"."IDX_APPLICATION_UNIVERSAL_IDENTIFIER_WORKSPACE_ID_UNIQUE"`,
@@ -18,16 +18,9 @@ export class AddWorkspaceCustomApplicationIdColumn1762343994716
     await queryRunner.query(
       `CREATE UNIQUE INDEX "IDX_APPLICATION_UNIVERSAL_IDENTIFIER_WORKSPACE_ID_UNIQUE" ON "core"."application" ("universalIdentifier", "workspaceId") WHERE "deletedAt" IS NULL AND "universalIdentifier" IS NOT NULL`,
     );
-
-    await queryRunner.query(
-      `ALTER TABLE "core"."workspace" ADD CONSTRAINT "FK_workspace_workspaceCustomApplicationId" FOREIGN KEY ("workspaceCustomApplicationId") REFERENCES "core"."application"("id") ON DELETE RESTRICT ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "core"."workspace" DROP CONSTRAINT IF EXISTS "FK_workspace_workspaceCustomApplicationId"`,
-    );
     await queryRunner.query(
       `DROP INDEX "core"."IDX_APPLICATION_UNIVERSAL_IDENTIFIER_WORKSPACE_ID_UNIQUE"`,
     );
