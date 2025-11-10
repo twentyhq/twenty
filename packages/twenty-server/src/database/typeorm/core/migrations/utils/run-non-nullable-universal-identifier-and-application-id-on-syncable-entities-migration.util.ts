b58aac217fc8efd36ddcc1bf768772b25f45b5d2
prastoin <paul@twenty.com>
2025-11-10 15:05:29 +0100

    feat(server): upgrade command to handle migration too

---
+++ packages/twenty-server/src/database/typeorm/core/migrations/utils/run-non-nullable-universal-identifier-and-application-id-on-syncable-entities-migration.util.ts
@@ -0,0 +1,140 @@
+import { QueryRunner } from "typeorm";
+
+export const runNonNullableUniversalIdentifierAndApplicationIdOnSyncableEntitiesMigration =
+  async ({ queryRunner }: { queryRunner: QueryRunner }) => {
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_0cc4d03dbcc269e77ba4d297fb"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."agent" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_b27c681286ac581f81498c5d4b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."indexMetadata" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_3b7ff27925c0959777682c1adc"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."role" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_cd4588bfc9ad73345b3953a039"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."viewFilter" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_e6ed40a61e4584e98584019a47"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."viewFilterGroup" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_a44e3b03f0eca32d0504d5ef73"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."viewGroup" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_38232fc0c6567ed029c2b1a12c"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."viewSort" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_552aa6908966e980099b3e5ebf"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."view" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_b86af4ea24cae518dee8eae996"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."viewField" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_f1c88fdfc3ad8910b17fc1fd73"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."fieldMetadata" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_3a00d35710f4227ded320fd96d"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."objectMetadata" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_8adc1fd6cb0dad2fbfd945954d"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."cronTrigger" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_960465af116edf9ac501bfb3db"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."databaseEventTrigger" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_e9c53b9ac5035d3202a8737020"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."routeTrigger" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "core"."IDX_5b43e65e322d516c9307bed97a"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "core"."serverlessFunction" ALTER COLUMN "universalIdentifier" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_0cc4d03dbcc269e77ba4d297fb" ON "core"."agent" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_b27c681286ac581f81498c5d4b" ON "core"."indexMetadata" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_3b7ff27925c0959777682c1adc" ON "core"."role" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_cd4588bfc9ad73345b3953a039" ON "core"."viewFilter" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_e6ed40a61e4584e98584019a47" ON "core"."viewFilterGroup" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_a44e3b03f0eca32d0504d5ef73" ON "core"."viewGroup" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_38232fc0c6567ed029c2b1a12c" ON "core"."viewSort" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_552aa6908966e980099b3e5ebf" ON "core"."view" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_b86af4ea24cae518dee8eae996" ON "core"."viewField" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_f1c88fdfc3ad8910b17fc1fd73" ON "core"."fieldMetadata" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_3a00d35710f4227ded320fd96d" ON "core"."objectMetadata" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_8adc1fd6cb0dad2fbfd945954d" ON "core"."cronTrigger" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_960465af116edf9ac501bfb3db" ON "core"."databaseEventTrigger" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_e9c53b9ac5035d3202a8737020" ON "core"."routeTrigger" ("workspaceId", "universalIdentifier") `,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_5b43e65e322d516c9307bed97a" ON "core"."serverlessFunction" ("workspaceId", "universalIdentifier") `,
+    );
+  };
