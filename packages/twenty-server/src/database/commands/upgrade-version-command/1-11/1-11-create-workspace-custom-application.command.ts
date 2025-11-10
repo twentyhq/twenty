fe79999401ef5257f28a5147140aa86f11ac74a7
prastoin <paul@twenty.com>
2025-11-10 15:40:19 +0100

    feat(server): create twenty custom workspace application

---
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-11/1-11-create-workspace-custom-application.command.ts
@@ -0,0 +1,69 @@
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Command } from 'nest-commander';
+import { Repository } from 'typeorm';
+
+import {
+  ActiveOrSuspendedWorkspacesMigrationCommandRunner,
+  type RunOnWorkspaceArgs,
+} from 'src/database/commands/command-runners/active-or-suspended-workspaces-migration.command-runner';
+import { ApplicationService } from 'src/engine/core-modules/application/application.service';
+import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
+import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
+import { computeWorkspaceCustomCreateApplicationInput } from 'src/engine/workspace-manager/workspace-sync-metadata/utils/compute-workspace-custom-create-application-input';
+import { isDefined } from 'twenty-shared/utils';
+import { v4 } from 'uuid';
+
+@Command({
+  name: 'upgrade:1-11:create-workspace-custom-application',
+  description:
+    'Create workspace-custom application for workspaces that do not have them',
+})
+export class CreateWorkspaceCustomApplicationCommand extends ActiveOrSuspendedWorkspacesMigrationCommandRunner {
+  constructor(
+    @InjectRepository(WorkspaceEntity)
+    protected readonly workspaceRepository: Repository<WorkspaceEntity>,
+    protected readonly twentyORMGlobalManager: TwentyORMGlobalManager,
+    private readonly applicationService: ApplicationService,
+  ) {
+    super(workspaceRepository, twentyORMGlobalManager);
+  }
+
+  override async runOnWorkspace({
+    workspaceId,
+  }: RunOnWorkspaceArgs): Promise<void> {
+    this.logger.log(
+      `Checking standard applications for workspace ${workspaceId}`,
+    );
+
+    const workspace = await this.workspaceRepository.findOne({
+      where: { id: workspaceId },
+    });
+    if (!isDefined(workspace)) {
+      throw new Error(`${workspaceId} workspace not found`);
+    }
+
+    if (isDefined(workspace.workspaceCustomApplicationId)) {
+      this.logger.log(
+        `${workspaceId} skipping custom workspace application creation as already exists`,
+      );
+      return;
+    }
+
+    const customWorkspaceApplicationInput =
+      computeWorkspaceCustomCreateApplicationInput({
+        workspace,
+        applicationId: v4(),
+      });
+    const customWorkspaceApplication = await this.applicationService.create({
+      ...customWorkspaceApplicationInput,
+      serverlessFunctionLayerId: null,
+    });
+
+    await this.workspaceRepository.update(workspace.id, {
+      workspaceCustomApplicationId: customWorkspaceApplication.id,
+    });
+
+    this.logger.log(`Successfully create workpsace custom application`);
+  }
+}
