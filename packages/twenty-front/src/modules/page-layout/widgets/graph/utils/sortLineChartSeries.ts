b3b12e51cffc69837e638660bff169742d715d17
ehconitin <nitinkoche03@gmail.com>
2025-11-12 18:45:01 +0530

    fix

--- packages/twenty-front/src/modules/page-layout/widgets/graph/utils/sortLineChartSeries.ts
+++ packages/twenty-front/src/modules/page-layout/widgets/graph/utils/sortLineChartSeries.ts
@@ -10,10 +10,9 @@ export const sortLineChartSeries = ({
 }): LineChartSeries[] => {
   switch (orderByY) {
     case GraphOrderBy.FIELD_ASC:
-      return [...series].sort((a, b) => a.id.localeCompare(b.id));
-    case GraphOrderBy.FIELD_DESC:
       return [...series].sort((a, b) => b.id.localeCompare(a.id));
-    // VALUE_ASC/DESC not applicable for series-level sorting
+    case GraphOrderBy.FIELD_DESC:
+      return [...series].sort((a, b) => a.id.localeCompare(b.id));
     default:
       return series;
   }
