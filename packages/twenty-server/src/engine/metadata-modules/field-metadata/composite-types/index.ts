616a8e60782cc17f72f05ac0647bbfd7f41301ef
Connorbelez <connor.belez@gmail.com>
2025-11-12 21:36:32 -0500

    Import composite types from twenty-shared

--- packages/twenty-server/src/engine/metadata-modules/field-metadata/composite-types/index.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/composite-types/index.ts
@@ -1,17 +1,18 @@
-import { FieldMetadataType } from 'twenty-shared/types';
+import {
+  FieldMetadataType,
+  actorCompositeType,
+  addressCompositeType,
+  currencyCompositeType,
+  emailsCompositeType,
+  fullNameCompositeType,
+  imageCompositeType,
+  linksCompositeType,
+  pdfCompositeType,
+  phonesCompositeType,
+  richTextV2CompositeType,
+} from 'twenty-shared/types';
 
 import { type CompositeType } from 'src/engine/metadata-modules/field-metadata/interfaces/composite-type.interface';
-
-import { actorCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/actor.composite-type';
-import { addressCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/address.composite-type';
-import { currencyCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/currency.composite-type';
-import { emailsCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/emails.composite-type';
-import { fullNameCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/full-name.composite-type';
-import { imageCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/image.composite-type';
-import { linksCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/links.composite-type';
-import { pdfCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/pdf.composite-type';
-import { phonesCompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/phones.composite-type';
-import { richTextV2CompositeType } from 'src/engine/metadata-modules/field-metadata/composite-types/rich-text-v2.composite-type';
 
 export const compositeTypeDefinitions = new Map<
   FieldMetadataType,
