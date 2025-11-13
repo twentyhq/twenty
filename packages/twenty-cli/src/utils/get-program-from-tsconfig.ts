4353f96acc2e65c6499a700ad410898c23642bb7
prastoin <paul@twenty.com>
2025-11-13 18:57:28 +0100

    lint

--- packages/twenty-cli/src/utils/get-program-from-tsconfig.ts
+++ packages/twenty-cli/src/utils/get-program-from-tsconfig.ts
@@ -1,10 +1,10 @@
 import { join } from 'path';
 import {
-    createProgram,
-    formatDiagnosticsWithColorAndContext,
-    parseJsonConfigFileContent,
-    readConfigFile,
-    sys,
+  createProgram,
+  formatDiagnosticsWithColorAndContext,
+  parseJsonConfigFileContent,
+  readConfigFile,
+  sys,
 } from 'typescript';
 
 export const getProgramFromTsconfig = ({
