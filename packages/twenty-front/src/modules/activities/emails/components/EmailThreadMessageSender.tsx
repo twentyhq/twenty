61e99e868a2423230910a842184b77cd37341706
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2025-11-14 21:51:33 +0530

    feat: use relative and absolute dates in email

--- packages/twenty-front/src/modules/activities/emails/components/EmailThreadMessageSender.tsx
+++ packages/twenty-front/src/modules/activities/emails/components/EmailThreadMessageSender.tsx
@@ -3,8 +3,12 @@ import { useRecoilValue } from 'recoil';
 
 import { ParticipantChip } from '@/activities/components/ParticipantChip';
 import { type EmailThreadMessageParticipant } from '@/activities/emails/types/EmailThreadMessageParticipant';
+import { useState } from 'react';
 import { dateLocaleState } from '~/localization/states/dateLocaleState';
-import { beautifyPastDateRelativeToNow } from '~/utils/date-utils';
+import {
+  beautifyPastDateRelativeToNow,
+  formatToHumanReadableDate,
+} from '~/utils/date-utils';
 
 const StyledEmailThreadMessageSender = styled.div`
   display: flex;
@@ -16,6 +20,12 @@ const StyledThreadMessageSentAt = styled.div`
   display: flex;
   color: ${({ theme }) => theme.font.color.tertiary};
   font-size: ${({ theme }) => theme.font.size.sm};
+  padding: ${({ theme }) => theme.spacing(1)};
+  &:hover {
+    background-color: ${({ theme }) => theme.background.transparent.light};
+    border-radius: ${({ theme }) => theme.border.radius.md};
+  }
+  user-select: none;
 `;
 
 type EmailThreadMessageSenderProps = {
@@ -28,12 +38,25 @@ export const EmailThreadMessageSender = ({
   sentAt,
 }: EmailThreadMessageSenderProps) => {
   const { localeCatalog } = useRecoilValue(dateLocaleState);
+  const [dateFormat, setDateFormat] = useState<'relative' | 'absolute'>(
+    'relative',
+  );
+  const relativeDate = beautifyPastDateRelativeToNow(sentAt, localeCatalog);
+  const absoluteDate = formatToHumanReadableDate(sentAt);
+  const flipDateFormat = () => {
+    setDateFormat((cur) => (cur === 'absolute' ? 'relative' : 'absolute'));
+  };
 
   return (
     <StyledEmailThreadMessageSender>
       <ParticipantChip participant={sender} variant="bold" />
-      <StyledThreadMessageSentAt>
-        {beautifyPastDateRelativeToNow(sentAt, localeCatalog)}
+      <StyledThreadMessageSentAt
+        onClick={(event) => {
+          event.stopPropagation();
+          flipDateFormat();
+        }}
+      >
+        {dateFormat === 'absolute' ? absoluteDate : relativeDate}
       </StyledThreadMessageSentAt>
     </StyledEmailThreadMessageSender>
   );
