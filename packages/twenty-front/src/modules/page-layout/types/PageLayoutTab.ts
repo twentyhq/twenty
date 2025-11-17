ad13fb38df7f51a3c82728d39c6e2560b56e0041
Devessier <baptiste@devessier.fr>
2025-11-17 17:21:13 +0100

    feat: add icons to default record page layouts

--- packages/twenty-front/src/modules/page-layout/types/PageLayoutTab.ts
+++ packages/twenty-front/src/modules/page-layout/types/PageLayoutTab.ts
@@ -14,4 +14,5 @@ export type PageLayoutTab = Omit<PageLayoutTabGenerated, 'widgets'> & {
    * Only available behind IS_RECORD_PAGE_LAYOUT_ENABLED for now.
    */
   layoutMode?: PageLayoutTabLayoutMode;
+  icon?: string;
 };
