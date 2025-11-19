c4b108b9e013bfc929db74cc5f468311dea9eb1b
Félix Malfait <felix@twenty.com>
2025-11-19 13:38:12 +0100

    Update extract-file-info-from-request test to match new implementation

--- packages/twenty-server/src/engine/core-modules/file/utils/__tests__/extract-file-info-from-request.utils.spec.ts
+++ packages/twenty-server/src/engine/core-modules/file/utils/__tests__/extract-file-info-from-request.utils.spec.ts
@@ -16,7 +16,7 @@ jest.mock(
   'src/engine/core-modules/file/interfaces/file-folder.interface',
   () => ({
     fileFolderConfigs: {
-      'some-folder': { ignoreExpirationToken: true },
+      attachment: { ignoreExpirationToken: true },
     },
   }),
 );
@@ -25,24 +25,25 @@ describe('extractFileInfoFromRequest', () => {
   it('should extract all file info correctly from request', () => {
     const mockRequest = {
       params: {
+        folder: 'attachment',
+        token: 'filesig123',
         filename: 'myfile.txt',
-        '0': 'some-folder/some-subfolder/filesig123',
       },
     } as unknown as Request;
 
     (checkFilename as jest.Mock).mockReturnValue('validated-file.txt');
-    (checkFileFolder as jest.Mock).mockReturnValue('some-folder');
+    (checkFileFolder as jest.Mock).mockReturnValue('attachment');
 
     const result = extractFileInfoFromRequest(mockRequest);
 
     expect(checkFilename).toHaveBeenCalledWith('myfile.txt');
-    expect(checkFileFolder).toHaveBeenCalledWith('some-folder/some-subfolder');
+    expect(checkFileFolder).toHaveBeenCalledWith('attachment');
 
     expect(result).toEqual({
       filename: 'validated-file.txt',
       fileSignature: 'filesig123',
-      rawFolder: 'some-folder/some-subfolder',
-      fileFolder: 'some-folder',
+      rawFolder: 'attachment',
+      fileFolder: 'attachment',
       ignoreExpirationToken: true,
     });
   });
