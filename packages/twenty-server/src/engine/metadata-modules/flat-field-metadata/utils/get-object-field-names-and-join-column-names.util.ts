a00e0eb93b93a2dd5fa5dbb2bcbf5c5ee07313b6
prastoin <paul@twenty.com>
2025-11-19 14:43:35 +0100

    chore(server): remove debug and be strict on find

--- packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/utils/get-object-field-names-and-join-column-names.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/utils/get-object-field-names-and-join-column-names.util.ts
@@ -1,10 +1,8 @@
-import { writeFileSync } from 'fs';
 
 import { RelationType } from 'twenty-shared/types';
-import { isDefined } from 'twenty-shared/utils';
 
 import { type FlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-maps.type';
-import { findFlatEntityByIdInFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/utils/find-flat-entity-by-id-in-flat-entity-maps.util';
+import { findFlatEntityByIdInFlatEntityMapsOrThrow } from 'src/engine/metadata-modules/flat-entity/utils/find-flat-entity-by-id-in-flat-entity-maps-or-throw.util';
 import { findManyFlatEntityByIdInFlatEntityMapsOrThrow } from 'src/engine/metadata-modules/flat-entity/utils/find-many-flat-entity-by-id-in-flat-entity-maps-or-throw.util';
 import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
 import { isMorphOrRelationFlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/utils/is-morph-or-relation-flat-field-metadata.util';
@@ -52,34 +50,11 @@ export const getObjectFieldNamesAndJoinColumnNames = ({
         remainingFlatEntityMapsToValidate?.byId[
           flatFieldMetadata.relationTargetFieldMetadataId
         ] ??
-        findFlatEntityByIdInFlatEntityMaps({
+        findFlatEntityByIdInFlatEntityMapsOrThrow({
           flatEntityId: flatFieldMetadata.relationTargetFieldMetadataId,
           flatEntityMaps: flatFieldMetadataMaps,
         });
 
-      if (
-        !isDefined(targetFlatFieldMetadata) ||
-        !isMorphOrRelationFlatFieldMetadata(targetFlatFieldMetadata) ||
-        !isDefined(targetFlatFieldMetadata.settings.joinColumnName)
-      ) {
-        writeFileSync(
-          `${Date.now()}-not-found.json`,
-          JSON.stringify(
-            {
-              flatFieldMetadata,
-              flatFieldMetadataMaps,
-              remainingFlatEntityMapsToValidate,
-            },
-            null,
-            2,
-          ),
-        );
-
-        return {
-          ...acc,
-          names: [...acc.names, flatFieldMetadata.name],
-        };
-      }
 
       return {
         ...acc,
