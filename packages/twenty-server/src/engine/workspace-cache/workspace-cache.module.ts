f919ead833a46e1879da8dca5b00bad8a6f5fb59
Weiko <corentin@twenty.com>
2025-11-20 18:24:22 +0100

    Fix after review

--- packages/twenty-server/src/engine/workspace-cache/workspace-cache.module.ts
+++ packages/twenty-server/src/engine/workspace-cache/workspace-cache.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { DiscoveryModule } from '@nestjs/core';
 
 import { CacheStorageModule } from 'src/engine/core-modules/cache-storage/cache-storage.module';
-import { WorkspaceContextCacheService } from 'src/engine/workspace-context-cache/services/workspace-context-cache.service';
+import { WorkspaceCacheService } from 'src/engine/workspace-cache/services/workspace-cache.service';
 
 @Module({
   imports: [CacheStorageModule, DiscoveryModule],
-  providers: [WorkspaceContextCacheService],
-  exports: [WorkspaceContextCacheService],
+  providers: [WorkspaceCacheService],
+  exports: [WorkspaceCacheService],
 })
 export class WorkspaceContextCacheModule {}
--- packages/twenty-server/src/engine/workspace-context-cache/decorators/workspace-context-cache.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const WORKSPACE_CONTEXT_CACHE_KEY = 'WORKSPACE_CONTEXT_CACHE_KEY';
-
-export const WorkspaceContextCache = (cacheKey: string) =>
-  SetMetadata(WORKSPACE_CONTEXT_CACHE_KEY, cacheKey);
