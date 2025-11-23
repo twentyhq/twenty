3dafb2c9989ab3003aaab99b73094bb43e4109cb
prastoin <paul@twenty.com>
2025-11-23 11:44:32 +0100

    nitpicks

--- packages/twenty-front/src/modules/metadata-error-handler/hooks/useMetadataErrorHandler.ts
+++ packages/twenty-front/src/modules/metadata-error-handler/hooks/useMetadataErrorHandler.ts
@@ -24,6 +24,7 @@ export const useMetadataErrorHandler = () => {
     databaseEventTrigger: t`database trigger`,
     routeTrigger: t`route trigger`,
     role: t`role`,
+    roleTarget: t`role target`,
   } as const satisfies Record<AllMetadataName, string>;
 
   const handleMetadataError = useCallback(
