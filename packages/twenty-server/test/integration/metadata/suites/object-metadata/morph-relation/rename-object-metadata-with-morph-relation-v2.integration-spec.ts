4848bc03f389340a7ffa8c9af140c496163ee53c
Guillim <guillim@users.noreply.github.com>
2025-11-23 21:45:30 +0100

    Update Name of relation fieldMetadata (#15749)
    UpdateOne of a Relation that involves a CustomObject, because the
    nameSingular needs to be updated in the fieldMetadata
    - nameSingular and namePlural must be provided since they are necessary
    for morph name computation
      - label sync should be false
    
    
    Interesting files to look at:
    
    -
    packages/twenty-server/src/engine/metadata-modules/object-metadata/utils/build-default-relation-flat-field-metadatas-for-custom-object.util.ts
    
    - UPDATE =>
    packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/utils/rename-related-morph-field-on-object-names-update.util.ts
    ( also update relation indexes ) needs v2 refactor to handle field
    relation name update
    - CREATE =>
    packages/twenty-server/src/engine/metadata-modules/object-metadata/utils/build-default-relation-flat-field-metadatas-for-custom-object.util.ts
    ( handle morph instead of previous classic relation )
    - DELETE => DONE
    
    Edit:
    closes https://github.com/twentyhq/core-team-issues/issues/1897
    
    ---------
    
    Co-authored-by: prastoin <paul@twenty.com>

--- packages/twenty-server/test/integration/metadata/suites/object-metadata/morph-relation/rename-object-metadata-with-morph-relation-v2.integration-spec.ts
+++ packages/twenty-server/test/integration/metadata/suites/object-metadata/morph-relation/rename-object-metadata-with-morph-relation-v2.integration-spec.ts
@@ -1,12 +1,94 @@
-import { findManyFieldsMetadataQueryFactory } from 'test/integration/metadata/suites/field-metadata/utils/find-many-fields-metadata-query-factory.util';
+import { findManyFieldsMetadata } from 'test/integration/metadata/suites/field-metadata/utils/find-many-fields-metadata.util';
 import { createMorphRelationBetweenObjects } from 'test/integration/metadata/suites/object-metadata/utils/create-morph-relation-between-objects.util';
 import { createOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/create-one-object-metadata.util';
 import { deleteOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/delete-one-object-metadata.util';
+import { findManyObjectMetadataWithIndexes } from 'test/integration/metadata/suites/object-metadata/utils/find-many-object-metadata-with-indexes.util';
 import { updateOneObjectMetadata } from 'test/integration/metadata/suites/object-metadata/utils/update-one-object-metadata.util';
-import { makeMetadataAPIRequest } from 'test/integration/metadata/suites/utils/make-metadata-api-request.util';
+import { extractRecordIdsAndDatesAsExpectAny } from 'test/utils/extract-record-ids-and-dates-as-expect-any';
+import { jestExpectToBeDefined } from 'test/utils/jest-expect-to-be-defined.util.test';
+import {
+  eachTestingContextFilter,
+  type EachTestingContext,
+} from 'twenty-shared/testing';
 import { FieldMetadataType } from 'twenty-shared/types';
 
 import { RelationType } from 'src/engine/metadata-modules/field-metadata/interfaces/relation-type.interface';
+
+import { type IndexFieldMetadataDTO } from 'src/engine/metadata-modules/index-metadata/dtos/index-field-metadata.dto';
+import { type IndexMetadataDTO } from 'src/engine/metadata-modules/index-metadata/dtos/index-metadata.dto';
+
+const findFieldMetadata = async ({
+  fieldMetadataId,
+}: {
+  fieldMetadataId: string;
+}) => {
+  const { fields } = await findManyFieldsMetadata({
+    gqlFields: `
+      id
+      name
+      object { id nameSingular }
+      relation { type targetFieldMetadata { id } targetObjectMetadata { id } }
+      settings
+    `,
+    input: {
+      filter: { id: { eq: fieldMetadataId } },
+      paging: { first: 1 },
+    },
+    expectToFail: false,
+  });
+
+  return fields[0]?.node;
+};
+
+const allTestsUseCases: EachTestingContext<{
+  nameSingular: string;
+  namePlural: string;
+  labelSingular: string;
+  labelPlural: string;
+  isLabelSyncedWithName: boolean;
+  newJoinColumnName: string | undefined;
+  relationType: RelationType;
+}>[] = [
+  {
+    title:
+      'should rename custom object, and update both the field name and join column name of the morph relation that contains the object name',
+    context: {
+      nameSingular: 'personForRenameSecond2',
+      namePlural: 'peopleForRenameSecond2',
+      labelSingular: 'Person For Rename2',
+      labelPlural: 'People For Rename2',
+      isLabelSyncedWithName: false,
+      newJoinColumnName: 'ownerPersonForRenameSecond2Id',
+      relationType: RelationType.MANY_TO_ONE,
+    },
+  },
+  {
+    title:
+      'should rename custom object, and update both the field name and join column name of the morph relation that contains the object name if label is sync with name',
+    context: {
+      nameSingular: 'personForRenameSecond3',
+      namePlural: 'peopleForRenameSecond3',
+      labelSingular: 'person For Rename Second3',
+      labelPlural: 'people For Rename Second3',
+      isLabelSyncedWithName: true,
+      newJoinColumnName: 'ownerPersonForRenameSecond3Id',
+      relationType: RelationType.MANY_TO_ONE,
+    },
+  },
+  {
+    title:
+      'should rename custom object, and update both the field name and join column name of the morph relation that contains the object name with ONE_TO_MANY relation type',
+    context: {
+      nameSingular: 'personForRenameSecond4',
+      namePlural: 'peopleForRenameSecond4',
+      labelSingular: 'Person For Rename Second4',
+      labelPlural: 'People For Rename Second4',
+      isLabelSyncedWithName: true,
+      newJoinColumnName: undefined,
+      relationType: RelationType.ONE_TO_MANY,
+    },
+  },
+];
 
 describe('Rename an object metadata with morph relation should succeed', () => {
   let createdObjectMetadataPersonId: string;
@@ -83,16 +165,26 @@ describe('Rename an object metadata with morph relation should succeed', () => {
     }
   });
 
-  it.failing(
-    'should rename custom object, and update both the field name and join column name of the morph relation that contains the object name',
-    async () => {
+  it.each(eachTestingContextFilter(allTestsUseCases))(
+    '$title',
+    async ({ context }) => {
+      const {
+        nameSingular,
+        namePlural,
+        labelSingular,
+        labelPlural,
+        isLabelSyncedWithName,
+        newJoinColumnName,
+        relationType,
+      } = context;
+
       const morphRelationField = await createMorphRelationBetweenObjects({
         name: 'owner',
         objectMetadataId: createdObjectMetadataOpportunityId,
         firstTargetObjectMetadataId: createdObjectMetadataPersonId,
         secondTargetObjectMetadataId: createdObjectMetadataCompanyId,
         type: FieldMetadataType.MORPH_RELATION,
-        relationType: RelationType.MANY_TO_ONE,
+        relationType,
       });
 
       expect(morphRelationField.morphRelations.length).toBe(2);
@@ -100,23 +192,24 @@ describe('Rename an object metadata with morph relation should succeed', () => {
       const { data } = await updateOneObjectMetadata({
         expectToFail: false,
         gqlFields: `
-      nameSingular
-      labelSingular
-      namePlural
-      labelPlural
-      `,
+    nameSingular
+    labelSingular
+    namePlural
+    labelPlural
+    `,
         input: {
           idToUpdate: createdObjectMetadataPersonId,
           updatePayload: {
-            nameSingular: 'personForRenameSecond2',
-            namePlural: 'peopleForRenameSecond2',
-            labelSingular: 'Person For Rename2',
-            labelPlural: 'People For Rename2',
+            nameSingular,
+            namePlural,
+            labelSingular,
+            labelPlural,
+            isLabelSyncedWithName,
           },
         },
       });
 
-      expect(data.updateOneObject.nameSingular).toBe('personForRenameSecond2');
+      expect(data.updateOneObject.nameSingular).toBe(nameSingular);
 
       const ownerFieldMetadataOnPersonId =
         morphRelationField.morphRelations.find(
@@ -136,32 +229,273 @@ describe('Rename an object metadata with morph relation should succeed', () => {
       });
 
       expect(fieldAfterRenaming.settings.joinColumnName).toBe(
-        'ownerPersonForRenameSecond2Id',
+        newJoinColumnName,
       );
     },
   );
-});
 
-const findFieldMetadata = async ({
-  fieldMetadataId,
-}: {
-  fieldMetadataId: string;
-}) => {
-  const operation = findManyFieldsMetadataQueryFactory({
-    gqlFields: `
-      id
-      name
-      object { id nameSingular }
-      relation { type targetFieldMetadata { id } targetObjectMetadata { id } }
-      settings
-    `,
-    input: {
-      filter: { id: { eq: fieldMetadataId } },
-      paging: { first: 1 },
-    },
+  it('should update indexes on ONE_TO_MANY morph field related target object name update', async () => {
+    const morphRelationField = await createMorphRelationBetweenObjects({
+      name: 'owner',
+      objectMetadataId: createdObjectMetadataOpportunityId,
+      firstTargetObjectMetadataId: createdObjectMetadataPersonId,
+      secondTargetObjectMetadataId: createdObjectMetadataCompanyId,
+      type: FieldMetadataType.MORPH_RELATION,
+      relationType: RelationType.ONE_TO_MANY,
+    });
+
+    expect(morphRelationField.morphRelations.length).toBe(2);
+
+    const objects = await findManyObjectMetadataWithIndexes({
+      expectToFail: false,
+    });
+
+    let relationIndexByFieldId: Record<
+      string,
+      IndexMetadataDTO & {
+        indexFieldMetadataList: IndexFieldMetadataDTO[];
+      }
+    > = {};
+
+    const morphParentObject = objects.find(
+      (object) => object.id === createdObjectMetadataOpportunityId,
+    );
+
+    jestExpectToBeDefined(morphParentObject);
+
+    for (const {
+      targetObjectMetadata,
+      targetFieldMetadata,
+      sourceFieldMetadata,
+    } of morphRelationField.morphRelations) {
+      const relatedObject = objects.find(
+        (object) => object.id === targetObjectMetadata.id,
+      );
+
+      jestExpectToBeDefined(relatedObject);
+
+      const objectRelatedIndexes = relatedObject.indexMetadataList.filter(
+        (index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId === targetFieldMetadata.id,
+          ),
+      );
+
+      expect(objectRelatedIndexes.length).toBe(1);
+      const [relationIndex] = objectRelatedIndexes;
+
+      jestExpectToBeDefined(relationIndex);
+      expect(relationIndex).toMatchSnapshot(
+        extractRecordIdsAndDatesAsExpectAny({ ...relationIndex }),
+      );
+
+      relationIndexByFieldId[targetFieldMetadata.id] = relationIndex;
+
+      const parentRelationIndex = morphParentObject.indexMetadataList.filter(
+        (index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId == sourceFieldMetadata.id,
+          ),
+      );
+
+      expect(parentRelationIndex.length).toBe(0);
+    }
+
+    await updateOneObjectMetadata({
+      expectToFail: false,
+      input: {
+        idToUpdate: createdObjectMetadataPersonId,
+        updatePayload: {
+          nameSingular: 'personForRenameSecondUpdated',
+          namePlural: 'peopleForRenameSecondUpdated',
+          labelSingular: 'Person For Rename Updated',
+          labelPlural: 'People For Rename Updated',
+        },
+      },
+    });
+
+    const updatedObjects = await findManyObjectMetadataWithIndexes({
+      expectToFail: false,
+    });
+
+    const morphParentObjectAfterUpdate = objects.find(
+      (object) => object.id === createdObjectMetadataOpportunityId,
+    );
+
+    jestExpectToBeDefined(morphParentObjectAfterUpdate);
+    for (const {
+      targetObjectMetadata,
+      targetFieldMetadata,
+      sourceFieldMetadata,
+    } of morphRelationField.morphRelations) {
+      const relatedObject = updatedObjects.find(
+        (object) => object.id === targetObjectMetadata.id,
+      );
+
+      jestExpectToBeDefined(relatedObject);
+
+      const objectRelatedIndexes = relatedObject.indexMetadataList.filter(
+        (index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId === targetFieldMetadata.id,
+          ),
+      );
+
+      expect(objectRelatedIndexes.length).toBe(1);
+      const [relationIndex] = objectRelatedIndexes;
+
+      jestExpectToBeDefined(relationIndex);
+      expect(relationIndex).toMatchSnapshot(
+        extractRecordIdsAndDatesAsExpectAny({ ...relationIndex }),
+      );
+      const previousIndex = relationIndexByFieldId[targetFieldMetadata.id];
+
+      jestExpectToBeDefined(previousIndex);
+      if (targetObjectMetadata.id === createdObjectMetadataPersonId) {
+        expect(previousIndex.name).not.toBe(relationIndex.name);
+      } else {
+        expect(previousIndex.name).toBe(relationIndex.name);
+      }
+
+      const parentRelationIndex =
+        morphParentObjectAfterUpdate.indexMetadataList.filter((index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId == sourceFieldMetadata.id,
+          ),
+        );
+
+      expect(parentRelationIndex.length).toBe(0);
+    }
   });
-  const fields = await makeMetadataAPIRequest(operation);
-  const field = fields.body.data.fields.edges?.[0]?.node;
 
-  return field;
-};
+  it('should not update morph index on MANY_TO_ONE morph field related target object name update', async () => {
+    const morphRelationField = await createMorphRelationBetweenObjects({
+      name: 'owner',
+      objectMetadataId: createdObjectMetadataOpportunityId,
+      firstTargetObjectMetadataId: createdObjectMetadataPersonId,
+      secondTargetObjectMetadataId: createdObjectMetadataCompanyId,
+      type: FieldMetadataType.MORPH_RELATION,
+      relationType: RelationType.MANY_TO_ONE,
+    });
+
+    expect(morphRelationField.morphRelations.length).toBe(2);
+
+    const objects = await findManyObjectMetadataWithIndexes({
+      expectToFail: false,
+    });
+
+    let relationIndexByFieldId: Record<
+      string,
+      IndexMetadataDTO & {
+        indexFieldMetadataList: IndexFieldMetadataDTO[];
+      }
+    > = {};
+
+    const morphParentObject = objects.find(
+      (object) => object.id === createdObjectMetadataOpportunityId,
+    );
+
+    jestExpectToBeDefined(morphParentObject);
+
+    for (const {
+      targetObjectMetadata,
+      targetFieldMetadata,
+      sourceFieldMetadata,
+    } of morphRelationField.morphRelations) {
+      const relatedObject = objects.find(
+        (object) => object.id === targetObjectMetadata.id,
+      );
+
+      jestExpectToBeDefined(relatedObject);
+
+      const objectRelatedIndexes = relatedObject.indexMetadataList.filter(
+        (index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId === targetFieldMetadata.id,
+          ),
+      );
+
+      expect(objectRelatedIndexes.length).toBe(0);
+
+      const parentRelationIndex = morphParentObject.indexMetadataList.filter(
+        (index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId == sourceFieldMetadata.id,
+          ),
+      );
+
+      expect(parentRelationIndex.length).toBe(1);
+      const [relationIndex] = parentRelationIndex;
+
+      relationIndexByFieldId[
+        relationIndex.indexFieldMetadataList[0].fieldMetadataId
+      ] = relationIndex;
+    }
+
+    await updateOneObjectMetadata({
+      expectToFail: false,
+      input: {
+        idToUpdate: createdObjectMetadataPersonId,
+        updatePayload: {
+          nameSingular: 'personForRenameSecondUpdated',
+          namePlural: 'peopleForRenameSecondUpdated',
+          labelSingular: 'Person For Rename Updated',
+          labelPlural: 'People For Rename Updated',
+        },
+      },
+    });
+
+    const updatedObjects = await findManyObjectMetadataWithIndexes({
+      expectToFail: false,
+    });
+
+    const morphParentObjectAfterUpdate = objects.find(
+      (object) => object.id === createdObjectMetadataOpportunityId,
+    );
+
+    jestExpectToBeDefined(morphParentObjectAfterUpdate);
+
+    for (const {
+      targetObjectMetadata,
+      targetFieldMetadata,
+      sourceFieldMetadata,
+    } of morphRelationField.morphRelations) {
+      const relatedObject = updatedObjects.find(
+        (object) => object.id === targetObjectMetadata.id,
+      );
+
+      jestExpectToBeDefined(relatedObject);
+
+      const objectRelatedIndexes = relatedObject.indexMetadataList.filter(
+        (index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId === targetFieldMetadata.id,
+          ),
+      );
+
+      expect(objectRelatedIndexes.length).toBe(0);
+
+      const parentRelationIndex =
+        morphParentObjectAfterUpdate.indexMetadataList.filter((index) =>
+          index.indexFieldMetadataList.some(
+            (indexField) =>
+              indexField.fieldMetadataId == sourceFieldMetadata.id,
+          ),
+        );
+
+      expect(parentRelationIndex.length).toBe(1);
+
+      const [relationIndex] = parentRelationIndex;
+      const previousIndex = relationIndexByFieldId[sourceFieldMetadata.id];
+
+      expect(previousIndex.name).toBe(relationIndex.name);
+    }
+  });
+});
