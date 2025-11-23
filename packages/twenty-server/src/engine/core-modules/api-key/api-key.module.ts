35e91bb652dddf02e39c04a9b2356d58cd7fa6c6
prastoin <paul@twenty.com>
2025-11-23 16:53:55 +0100

    fix(server): module injection

--- packages/twenty-server/src/engine/core-modules/api-key/api-key.module.ts
+++ packages/twenty-server/src/engine/core-modules/api-key/api-key.module.ts
@@ -1,21 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ApiKeyRoleService } from 'src/engine/core-modules/api-key/api-key-role.service';
 import { ApiKeyEntity } from 'src/engine/core-modules/api-key/api-key.entity';
 import { ApiKeyResolver } from 'src/engine/core-modules/api-key/api-key.resolver';
 import { ApiKeyService } from 'src/engine/core-modules/api-key/api-key.service';
-import { TokenModule } from 'src/engine/core-modules/auth/token/token.module';
 import { FeatureFlagModule } from 'src/engine/core-modules/feature-flag/feature-flag.module';
 import { JwtModule } from 'src/engine/core-modules/jwt/jwt.module';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { PermissionsModule } from 'src/engine/metadata-modules/permissions/permissions.module';
+import { RoleTargetV2Module } from 'src/engine/metadata-modules/role-target/role-target-v2.module';
 import { RoleTargetsEntity } from 'src/engine/metadata-modules/role/role-targets.entity';
 import { RoleEntity } from 'src/engine/metadata-modules/role/role.entity';
-import { RoleTargetV2Module } from 'src/engine/metadata-modules/role-target/role-target-v2.module';
 import { WorkspacePermissionsCacheModule } from 'src/engine/metadata-modules/workspace-permissions-cache/workspace-permissions-cache.module';
 import { WorkspaceCacheStorageModule } from 'src/engine/workspace-cache-storage/workspace-cache-storage.module';
 
+import { TokenModule } from 'src/engine/core-modules/auth/token/token.module';
 import { ApiKeyController } from './controllers/api-key.controller';
 
 @Module({
@@ -27,12 +27,12 @@ import { ApiKeyController } from './controllers/api-key.controller';
       WorkspaceEntity,
     ]),
     JwtModule,
-    TokenModule,
     WorkspacePermissionsCacheModule,
     WorkspaceCacheStorageModule,
-    PermissionsModule,
     FeatureFlagModule,
     RoleTargetV2Module,
+    TokenModule,
+    forwardRef(() => PermissionsModule),
   ],
   providers: [ApiKeyService, ApiKeyResolver, ApiKeyRoleService],
   controllers: [ApiKeyController],
