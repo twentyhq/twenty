c0690350afe8d12f358e9de953cd12deaf01fc52
Etienne <45695613+etiennejouan@users.noreply.github.com>
2025-11-24 18:38:39 +0100

    fix

--- packages/twenty-server/src/engine/core-modules/record-transformer/utils/transform-emails-value.util.ts
+++ packages/twenty-server/src/engine/core-modules/record-transformer/utils/transform-emails-value.util.ts
@@ -1,5 +1,4 @@
-import { isNonEmptyString, isNull } from '@sniptt/guards';
-import { isDefined } from 'twenty-shared/utils';
+import { isNonEmptyString } from '@sniptt/guards';
 
 export const transformEmailsValue = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -11,26 +10,31 @@ export const transformEmailsValue = (
     return value;
   }
 
-  let additionalEmails = value?.additionalEmails;
+  let additionalEmails: string | null = value?.additionalEmails;
   const primaryEmail = value?.primaryEmail
     ? value.primaryEmail.toLowerCase()
     : isNullEquivalenceEnabled
       ? null
       : '';
 
-  if (isDefined(additionalEmails)) {
+  if (additionalEmails) {
     try {
       const emailArray = (
         isNonEmptyString(additionalEmails)
           ? JSON.parse(additionalEmails)
-          : isNullEquivalenceEnabled
-            ? null
-            : additionalEmails
+          : additionalEmails
       ) as string[];
 
-      additionalEmails = isNull(emailArray)
-        ? null
-        : JSON.stringify(emailArray.map((email) => email.toLowerCase()));
+      additionalEmails = JSON.stringify(
+        emailArray.map((email) => email.toLowerCase()),
+      );
+
+      if (isNullEquivalenceEnabled) {
+        additionalEmails =
+          Array.isArray(emailArray) && emailArray.length === 0
+            ? null
+            : additionalEmails;
+      }
     } catch {
       /* empty */
     }
