2b415023be8ca1a61e8df83c37de6810a5c5d330
Félix Malfait <felix@twenty.com>
2025-11-25 11:34:44 +0100

    Missing migration

---
+++ packages/twenty-server/1764066845539-core-migration-check.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CoreMigrationCheck1764066845539 implements MigrationInterface {
+    name = 'CoreMigrationCheck1764066845539'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."agent" ALTER COLUMN "modelId" SET DEFAULT 'default-smart-model'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."agent" ALTER COLUMN "modelId" SET DEFAULT 'auto'`);
+    }
+
+}
