0a844f4876f8cc930180567fb39d232a773b206b
Etienne <45695613+etiennejouan@users.noreply.github.com>
2025-11-25 18:24:45 +0100

    add complexity ccompute

--- packages/twenty-server/src/engine/core-modules/graphql/hooks/use-compute-complexity.hook.ts
+++ packages/twenty-server/src/engine/core-modules/graphql/hooks/use-compute-complexity.hook.ts
@@ -1,22 +1,29 @@
-import queryComplexity, { simpleEstimator } from 'graphql-query-complexity';
+import { type ValidationContext } from 'graphql';
 import { type Plugin } from 'graphql-yoga';
 
 import { UserInputError } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
 
 export const useComputeComplexity = (maximumComplexity: number): Plugin => ({
   onValidate: ({ addValidationRule }) => {
-    addValidationRule(
-      queryComplexity({
-        maximumComplexity,
-        estimators: [simpleEstimator({ defaultComplexity: 1 })],
-        onComplete: (complexity: number) => {
-          if (complexity > maximumComplexity) {
-            throw new UserInputError(
-              `Query complexity is too high: ${complexity} - Too many fields requested`,
-            );
-          }
+    addValidationRule((context: ValidationContext) => {
+      let complexity = 0;
+
+      return {
+        Field() {
+          complexity++;
         },
-      }),
-    );
+        Document: {
+          leave() {
+            if (complexity > maximumComplexity) {
+              context.reportError(
+                new UserInputError(
+                  `Query complexity is too high: ${complexity} - Too many fields requested`,
+                ),
+              );
+            }
+          },
+        },
+      };
+    });
   },
 });
