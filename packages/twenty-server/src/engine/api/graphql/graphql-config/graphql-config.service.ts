1eb1ce98400821a25893e8f568b85b7b0ae9909f
Etienne <45695613+etiennejouan@users.noreply.github.com>
2025-11-25 16:22:09 +0100

    add query complexity estimation

--- packages/twenty-server/src/engine/api/graphql/graphql-config/graphql-config.service.ts
+++ packages/twenty-server/src/engine/api/graphql/graphql-config/graphql-config.service.ts
@@ -33,6 +33,7 @@ import {
 import { CoreEngineModule } from 'src/engine/core-modules/core-engine.module';
 import { ExceptionHandlerService } from 'src/engine/core-modules/exception-handler/exception-handler.service';
 import { useSentryTracing } from 'src/engine/core-modules/exception-handler/hooks/use-sentry-tracing';
+import { useComputeComplexity } from 'src/engine/core-modules/graphql/hooks/use-compute-complexity.hook';
 import { useDisableIntrospectionForUnauthenticatedUsers } from 'src/engine/core-modules/graphql/hooks/use-disable-introspection-for-unauthenticated-users.hook';
 import { useGraphQLErrorHandlerHook } from 'src/engine/core-modules/graphql/hooks/use-graphql-error-handler.hook';
 import { I18nService } from 'src/engine/core-modules/i18n/i18n.service';
@@ -75,6 +76,7 @@ export class GraphQLConfigService
       useDisableIntrospectionForUnauthenticatedUsers(
         this.twentyConfigService.get('NODE_ENV') === NodeEnvironment.PRODUCTION,
       ),
+      useComputeComplexity(1000),
     ];
 
     if (Sentry.isInitialized()) {
---
+++ packages/twenty-server/src/engine/core-modules/graphql/hooks/use-compute-complexity.hook.ts
@@ -0,0 +1,22 @@
+import queryComplexity, { simpleEstimator } from 'graphql-query-complexity';
+import { type Plugin } from 'graphql-yoga';
+
+import { UserInputError } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
+
+export const useComputeComplexity = (maximumComplexity = 1000): Plugin => ({
+  onValidate: ({ addValidationRule }) => {
+    addValidationRule(
+      queryComplexity({
+        maximumComplexity,
+        estimators: [simpleEstimator({ defaultComplexity: 1 })],
+        onComplete: (complexity: number) => {
+          if (complexity > maximumComplexity) {
+            throw new UserInputError(
+              `Query complexity is too high: ${complexity} - Too many fields requested`,
+            );
+          }
+        },
+      }),
+    );
+  },
+});
