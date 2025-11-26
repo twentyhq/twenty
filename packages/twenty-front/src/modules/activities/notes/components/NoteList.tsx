04c7300d36296c2908fec04c2683de5fb906e57b
Thomas des Francs <tdesfrancs@gmail.com>
2025-11-26 19:09:50 +0100

    Let single notes take the full width (both side panel & desktop

--- packages/twenty-front/src/modules/activities/notes/components/NoteList.tsx
+++ packages/twenty-front/src/modules/activities/notes/components/NoteList.tsx
@@ -1,5 +1,5 @@
-import { type ReactElement } from 'react';
 import styled from '@emotion/styled';
+import { type ReactElement } from 'react';
 
 import { type Note } from '@/activities/types/Note';
 
