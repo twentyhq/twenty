74eab775395fc29acf708ed0d9c2ccbecbdb8118
Paul Rastoin <45004772+prastoin@users.noreply.github.com>
2025-11-26 12:53:33 +0100

    Refactor upgrade devx to allow configuring workspaces status to pass over (#16066)
    # Introduction
    We need to be able to create custom workspace application on all
    workspaces, even pending and ongoing etc
    Right now the upgrade devx only allows and expect active or suspended
    workspace to be passed to runOnWorkspace.
    
    ## WorkspacesMigrationRunner
    Created an intermediate class `WorkspacesMigrationRunner` that expect an
    array `WorkspaceStatus` to be fetched for the current command to be run
    on
    The `ActiveOrSuspendedCommandRunner` statically passes both `SUSPENDED`
    and `ACTIVE`, whereas the create workspace custom application passed all
    the enum values
    
    ## DataSource
    Workspace that are not fully init don't have a `workspace_schema` so
    they don't have `dataSource`
    Made a not very elegant check to see if current workspace we're about to
    create dataSource on has one historically
    Which means that dataSource is now optional, it had only one impact on
    an existing command and the desired devx will become consuming existing
    services that do not expect dataSource ( or at least yet )

--- packages/twenty-server/src/database/commands/upgrade-version-command/1-12/1-12-add-calendar-events-import-scheduled-sync-stage.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-12/1-12-add-calendar-events-import-scheduled-sync-stage.command.ts
@@ -4,11 +4,10 @@ import { Command } from 'nest-commander';
 import { STANDARD_OBJECT_IDS } from 'twenty-shared/metadata';
 import { DataSource, Repository } from 'typeorm';
 
-import {
-  ActiveOrSuspendedWorkspacesMigrationCommandRunner,
-  type RunOnWorkspaceArgs,
-} from 'src/database/commands/command-runners/active-or-suspended-workspaces-migration.command-runner';
+import { ActiveOrSuspendedWorkspacesMigrationCommandRunner } from 'src/database/commands/command-runners/active-or-suspended-workspaces-migration.command-runner';
+import { RunOnWorkspaceArgs } from 'src/database/commands/command-runners/workspaces-migration.command-runner';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
+import { DataSourceService } from 'src/engine/metadata-modules/data-source/data-source.service';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
 import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
@@ -26,6 +25,7 @@ export class AddCalendarEventsImportScheduledSyncStageCommand extends ActiveOrSu
     @InjectRepository(WorkspaceEntity)
     protected readonly workspaceRepository: Repository<WorkspaceEntity>,
     protected readonly twentyORMGlobalManager: TwentyORMGlobalManager,
+    protected readonly dataSourceService: DataSourceService,
     @InjectRepository(ObjectMetadataEntity)
     private readonly objectMetadataRepository: Repository<ObjectMetadataEntity>,
     @InjectRepository(FieldMetadataEntity)
@@ -33,7 +33,7 @@ export class AddCalendarEventsImportScheduledSyncStageCommand extends ActiveOrSu
     @InjectDataSource()
     private readonly coreDataSource: DataSource,
   ) {
-    super(workspaceRepository, twentyORMGlobalManager);
+    super(workspaceRepository, twentyORMGlobalManager, dataSourceService);
   }
 
   override async runOnWorkspace({
