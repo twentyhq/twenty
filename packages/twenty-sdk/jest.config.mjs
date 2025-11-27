a7bed7010bf3512fe38521c8c749b22d96a28810
martmull <martmull@hotmail.fr>
2025-11-27 10:37:25 +0100

    Add test and lint command

---
+++ packages/twenty-sdk/jest.config.mjs
@@ -0,0 +1,40 @@
+const jestConfig = {
+  displayName: 'twenty-cli',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transformIgnorePatterns: ['../../node_modules/'],
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', tsx: false },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  extensionsToTreatAsEsm: ['.ts'],
+  coverageDirectory: './coverage',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.(test|spec).{js,ts}',
+    '<rootDir>/src/**/?(*.)(test|spec).{js,ts}',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.d.ts',
+    '!src/cli/cli.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 1,
+      lines: 1,
+      functions: 1,
+    },
+  },
+};
+
+export default jestConfig;
