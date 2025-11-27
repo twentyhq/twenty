f99493509dcb9c3df834be4782cd39ff87f3343e
bosiraphael <raphael.bosi@gmail.com>
2025-11-27 15:44:20 +0100

    Add default order by and date granularity when switching field

---
+++ packages/twenty-front/src/modules/command-menu/pages/page-layout/utils/buildChartGroupByFieldConfigUpdate.ts
@@ -0,0 +1,111 @@
+import { type ChartConfiguration } from '@/command-menu/pages/page-layout/types/ChartConfiguration';
+import { ObjectRecordGroupByDateGranularity } from 'twenty-shared/types';
+import { isDefined } from 'twenty-shared/utils';
+import { BarChartGroupMode, GraphOrderBy } from '~/generated/graphql';
+
+type BuildChartGroupByFieldConfigUpdateArgs<T extends ChartConfiguration> = {
+  configuration: T;
+  fieldMetadataIdKey: keyof T;
+  subFieldNameKey: keyof T;
+  fieldId: string | null;
+  subFieldName: string | null;
+};
+
+export const buildChartGroupByFieldConfigUpdate = <
+  T extends ChartConfiguration,
+>({
+  configuration,
+  fieldMetadataIdKey,
+  subFieldNameKey,
+  fieldId,
+  subFieldName,
+}: BuildChartGroupByFieldConfigUpdateArgs<T>) => {
+  const isPrimaryAxis =
+    fieldMetadataIdKey === 'primaryAxisGroupByFieldMetadataId';
+  const isSecondaryAxis =
+    fieldMetadataIdKey === 'secondaryAxisGroupByFieldMetadataId';
+  const isPieChartGroupBy = fieldMetadataIdKey === 'groupByFieldMetadataId';
+
+  const baseConfig = {
+    [fieldMetadataIdKey]: fieldId,
+    [subFieldNameKey]: subFieldName,
+  };
+
+  const isBarChart = configuration.__typename === 'BarChartConfiguration';
+  const isLineChart = configuration.__typename === 'LineChartConfiguration';
+  const isPieChart = configuration.__typename === 'PieChartConfiguration';
+
+  if (isPrimaryAxis) {
+    const existingOrderBy =
+      isBarChart || isLineChart ? configuration.primaryAxisOrderBy : null;
+
+    const existingDateGranularity =
+      isBarChart || isLineChart
+        ? configuration.primaryAxisDateGranularity
+        : null;
+
+    return {
+      ...baseConfig,
+      primaryAxisOrderBy: isDefined(fieldId)
+        ? (existingOrderBy ?? GraphOrderBy.FIELD_ASC)
+        : null,
+      primaryAxisDateGranularity: isDefined(fieldId)
+        ? (existingDateGranularity ?? ObjectRecordGroupByDateGranularity.DAY)
+        : null,
+    };
+  }
+
+  if (isPieChartGroupBy) {
+    const existingOrderBy = isPieChart ? configuration.orderBy : null;
+
+    const existingDateGranularity = isPieChart
+      ? configuration.dateGranularity
+      : null;
+
+    return {
+      ...baseConfig,
+      orderBy: isDefined(fieldId)
+        ? (existingOrderBy ?? GraphOrderBy.FIELD_ASC)
+        : null,
+      dateGranularity: isDefined(fieldId)
+        ? (existingDateGranularity ?? ObjectRecordGroupByDateGranularity.DAY)
+        : null,
+    };
+  }
+
+  if (!isSecondaryAxis) {
+    return baseConfig;
+  }
+
+  if (isBarChart) {
+    return {
+      ...baseConfig,
+      secondaryAxisOrderBy: isDefined(fieldId)
+        ? (configuration.secondaryAxisOrderBy ?? GraphOrderBy.FIELD_ASC)
+        : null,
+      secondaryAxisGroupByDateGranularity: isDefined(fieldId)
+        ? (configuration.secondaryAxisGroupByDateGranularity ??
+          ObjectRecordGroupByDateGranularity.DAY)
+        : null,
+      groupMode: isDefined(fieldId)
+        ? (configuration.groupMode ?? BarChartGroupMode.STACKED)
+        : null,
+    };
+  }
+
+  if (isLineChart) {
+    return {
+      ...baseConfig,
+      secondaryAxisOrderBy: isDefined(fieldId)
+        ? (configuration.secondaryAxisOrderBy ?? GraphOrderBy.FIELD_ASC)
+        : null,
+      secondaryAxisGroupByDateGranularity: isDefined(fieldId)
+        ? (configuration.secondaryAxisGroupByDateGranularity ??
+          ObjectRecordGroupByDateGranularity.DAY)
+        : null,
+      isStacked: isDefined(fieldId) ? (configuration.isStacked ?? true) : null,
+    };
+  }
+
+  return baseConfig;
+};
