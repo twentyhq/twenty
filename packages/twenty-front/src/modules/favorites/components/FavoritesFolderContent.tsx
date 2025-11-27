9b2889aadc12c7c1b717a9f812c2b3a7fd4c84b5
Anton <a.ambaroff@gmail.com>
2025-11-27 17:25:04 +0500

    fix(#15950): mobile favorites folder navigation with proper back button (return the provider)

--- packages/twenty-front/src/modules/favorites/components/FavoritesFolderContent.tsx
+++ packages/twenty-front/src/modules/favorites/components/FavoritesFolderContent.tsx
@@ -1,5 +1,6 @@
 import { FavoriteIcon } from '@/favorites/components/FavoriteIcon';
 import { FavoritesBackButton } from '@/favorites/components/FavoritesBackButton';
+import { FavoritesDragProvider } from '@/favorites/components/FavoritesDragProvider';
 import { useDeleteFavorite } from '@/favorites/hooks/useDeleteFavorite';
 import { getFavoriteSecondaryLabel } from '@/favorites/utils/getFavoriteSecondaryLabel';
 import { type ProcessedFavorite } from '@/favorites/utils/sortFavorites';
@@ -28,44 +29,46 @@ export const FavoritesFolderContent = ({
   return (
     <>
       <FavoritesBackButton folderName={folderName} />
-      <Droppable droppableId={`folder-${folderId}`}>
-        {(provided) => (
-          <div
-            ref={provided.innerRef}
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            {...provided.droppableProps}
-          >
-            {favorites.map((favorite, index) => (
-              <DraggableItem
-                key={favorite.id}
-                draggableId={favorite.id}
-                index={index}
-                isInsideScrollableContainer
-                itemComponent={
-                  <NavigationDrawerItem
-                    secondaryLabel={getFavoriteSecondaryLabel({
-                      objectMetadataItems,
-                      favoriteObjectNameSingular: favorite.objectNameSingular,
-                    })}
-                    label={favorite.labelIdentifier}
-                    Icon={() => <FavoriteIcon favorite={favorite} />}
-                    rightOptions={
-                      <LightIconButton
-                        Icon={IconHeartOff}
-                        onClick={() => deleteFavorite(favorite.id)}
-                        accent="tertiary"
-                      />
-                    }
-                    triggerEvent="CLICK"
-                    to={favorite.link}
-                  />
-                }
-              />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+      <FavoritesDragProvider>
+        <Droppable droppableId={`folder-${folderId}`}>
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...provided.droppableProps}
+            >
+              {favorites.map((favorite, index) => (
+                <DraggableItem
+                  key={favorite.id}
+                  draggableId={favorite.id}
+                  index={index}
+                  isInsideScrollableContainer
+                  itemComponent={
+                    <NavigationDrawerItem
+                      secondaryLabel={getFavoriteSecondaryLabel({
+                        objectMetadataItems,
+                        favoriteObjectNameSingular: favorite.objectNameSingular,
+                      })}
+                      label={favorite.labelIdentifier}
+                      Icon={() => <FavoriteIcon favorite={favorite} />}
+                      rightOptions={
+                        <LightIconButton
+                          Icon={IconHeartOff}
+                          onClick={() => deleteFavorite(favorite.id)}
+                          accent="tertiary"
+                        />
+                      }
+                      triggerEvent="CLICK"
+                      to={favorite.link}
+                    />
+                  }
+                />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </FavoritesDragProvider>
     </>
   );
 };
