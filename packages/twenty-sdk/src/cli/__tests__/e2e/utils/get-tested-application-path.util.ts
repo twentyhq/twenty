92b104419e42f753a6dee086ed1e8d15fb42d83f
martmull <martmull@hotmail.fr>
2025-11-27 12:28:29 +0100

    Add e2e tests

--- packages/twenty-sdk/src/cli/__tests__/e2e/utils/get-tested-application-path.util.ts
+++ packages/twenty-sdk/src/cli/__tests__/e2e/utils/get-tested-application-path.util.ts
@@ -1,11 +1,9 @@
 import path from 'path';
 
 export const getTestedApplicationPath = (relativePath: string): string => {
-  const currentFileDir = __dirname;
-
   const twentyAppsPath = path.resolve(
-    currentFileDir,
-    '../../../../../twenty-apps',
+    __dirname,
+    '../../../../../../twenty-apps',
   );
 
   return path.join(twentyAppsPath, relativePath);
