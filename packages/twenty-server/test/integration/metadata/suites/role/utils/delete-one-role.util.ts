cea85d6ecd5c09009916444971e75c88cca86c48
prastoin <paul@twenty.com>
2025-11-28 15:05:06 +0100

    fix

--- packages/twenty-server/test/integration/metadata/suites/role/utils/delete-one-role.util.ts
+++ packages/twenty-server/test/integration/metadata/suites/role/utils/delete-one-role.util.ts
@@ -1,24 +1,28 @@
-import { makeGraphqlAPIRequest } from 'test/integration/graphql/utils/make-graphql-api-request.util';
 import {
   type DeleteOneRoleFactoryInput,
   deleteOneRoleQueryFactory,
 } from 'test/integration/metadata/suites/role/utils/delete-one-role-query-factory.util';
+import { makeMetadataAPIRequest } from 'test/integration/metadata/suites/utils/make-metadata-api-request.util';
 import { CommonResponseBody } from 'test/integration/metadata/types/common-response-body.type';
 import { type PerformMetadataQueryParams } from 'test/integration/metadata/types/perform-metadata-query.type';
 import { warnIfErrorButNotExpectedToFail } from 'test/integration/metadata/utils/warn-if-error-but-not-expected-to-fail.util';
+
 import { warnIfNoErrorButExpectedToFail } from 'test/integration/metadata/utils/warn-if-no-error-but-expected-to-fail.util';
 
 export const deleteOneRole = async ({
   input,
+  gqlFields,
   expectToFail = false,
+  token,
 }: PerformMetadataQueryParams<DeleteOneRoleFactoryInput>): CommonResponseBody<{
   deleteOneRole: string;
 }> => {
   const graphqlOperation = deleteOneRoleQueryFactory({
     input,
+    gqlFields,
   });
 
-  const response = await makeGraphqlAPIRequest(graphqlOperation);
+  const response = await makeMetadataAPIRequest(graphqlOperation, token);
 
   if (expectToFail === true) {
     warnIfNoErrorButExpectedToFail({
