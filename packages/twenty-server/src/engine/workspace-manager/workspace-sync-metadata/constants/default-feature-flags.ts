638d4015a1e85a3e257322322fc73740b3aa5049
Etienne <45695613+etiennejouan@users.noreply.github.com>
2025-12-01 15:43:28 +0100

    Null equivalence - Activate FF for all (#16209)

--- packages/twenty-server/src/engine/workspace-manager/workspace-sync-metadata/constants/default-feature-flags.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-sync-metadata/constants/default-feature-flags.ts
@@ -2,4 +2,5 @@ import { FeatureFlagKey } from 'src/engine/core-modules/feature-flag/enums/featu
 
 export const DEFAULT_FEATURE_FLAGS = [
   FeatureFlagKey.IS_WORKSPACE_MIGRATION_V2_ENABLED,
+  FeatureFlagKey.IS_NULL_EQUIVALENCE_ENABLED,
 ] as const satisfies FeatureFlagKey[];
