fa8b9adc5bb208a688551ce07dd503f5c01d060d
bosiraphael <raphael.bosi@gmail.com>
2025-12-02 11:21:01 +0100

    update cumulative icon

--- packages/twenty-front/src/modules/command-menu/pages/page-layout/constants/settings/CumulativeSetting.ts
+++ packages/twenty-front/src/modules/command-menu/pages/page-layout/constants/settings/CumulativeSetting.ts
@@ -1,11 +1,11 @@
 import { CHART_CONFIGURATION_SETTING_LABELS } from '@/command-menu/pages/page-layout/constants/settings/ChartConfigurationSettingLabels';
 import { CHART_CONFIGURATION_SETTING_IDS } from '@/command-menu/pages/page-layout/types/ChartConfigurationSettingIds';
 import { type ChartSettingsItem } from '@/command-menu/pages/page-layout/types/ChartSettingsGroup';
-import { IconSum } from 'twenty-ui/display';
+import { IconChartBarPopular } from 'twenty-ui/display';
 
 export const CUMULATIVE_SETTING: ChartSettingsItem = {
   isBoolean: true,
-  Icon: IconSum,
+  Icon: IconChartBarPopular,
   label: CHART_CONFIGURATION_SETTING_LABELS.CUMULATIVE,
   id: CHART_CONFIGURATION_SETTING_IDS.CUMULATIVE,
 };
