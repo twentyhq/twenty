81b1d2f9a5ddf69025426b59bb8e3fcab6ee1811
Lucas Bordeau <bordeau.lucas@gmail.com>
2025-12-02 11:19:11 +0100

    Ok

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableColumnWidthEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableColumnWidthEffect.tsx
@@ -1,11 +1,20 @@
+import { RECORD_TABLE_COLUMN_ADD_COLUMN_BUTTON_WIDTH } from '@/object-record/record-table/constants/RecordTableColumnAddColumnButtonWidth';
+import { RECORD_TABLE_COLUMN_CHECKBOX_WIDTH } from '@/object-record/record-table/constants/RecordTableColumnCheckboxWidth';
+import { RECORD_TABLE_COLUMN_DRAG_AND_DROP_WIDTH } from '@/object-record/record-table/constants/RecordTableColumnDragAndDropWidth';
 import { RECORD_TABLE_COLUMN_LAST_EMPTY_COLUMN_WIDTH_VARIABLE_NAME } from '@/object-record/record-table/constants/RecordTableColumnLastEmptyColumnWidthVariableName';
 import { RECORD_TABLE_COLUMN_WITH_GROUP_LAST_EMPTY_COLUMN_WIDTH_VARIABLE_NAME } from '@/object-record/record-table/constants/RecordTableColumnWithGroupLastEmptyColumnWidthVariableName';
+import { RECORD_TABLE_LABEL_IDENTIFIER_COLUMN_WIDTH_ON_MOBILE } from '@/object-record/record-table/constants/RecordTableLabelIdentifierColumnWidthOnMobile';
 import { useRecordTableContextOrThrow } from '@/object-record/record-table/contexts/RecordTableContext';
-import { useRecordTableLastColumnWidthToFill } from '@/object-record/record-table/hooks/useRecordTableLastColumnWidthToFill';
+import { recordTableWidthComponentState } from '@/object-record/record-table/states/recordTableWidthComponentState';
 import { resizedFieldMetadataIdComponentState } from '@/object-record/record-table/states/resizedFieldMetadataIdComponentState';
+import { shouldCompactRecordTableFirstColumnComponentState } from '@/object-record/record-table/states/shouldCompactRecordTableFirstColumnComponentState';
+import { computeLastRecordTableColumnWidth } from '@/object-record/record-table/utils/computeLastRecordTableColumnWidth';
+import { computeVisibleRecordFieldsWidthOnTable } from '@/object-record/record-table/utils/computeVisibleRecordFieldsWidthOnTable';
 import { getRecordTableColumnFieldWidthCSSVariableName } from '@/object-record/record-table/utils/getRecordTableColumnFieldWidthCSSVariableName';
 import { updateRecordTableCSSVariable } from '@/object-record/record-table/utils/updateRecordTableCSSVariable';
+import { RECORD_TABLE_VIRTUALIZATION_BODY_PLACEHOLDER_WIDTH_CSS_VARIABLE_NAME } from '@/object-record/record-table/virtualization/components/RecordTableVirtualizedBodyPlaceholder';
 import { useRecoilComponentState } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentState';
+import { useRecoilComponentValue } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentValue';
 
 import { useEffect } from 'react';
 import { isDefined } from 'twenty-shared/utils';
@@ -15,14 +24,46 @@ export const RecordTableColumnWidthEffect = () => {
     resizedFieldMetadataIdComponentState,
   );
 
-  const { lastColumnWidth } = useRecordTableLastColumnWidthToFill();
-
   const { visibleRecordFields } = useRecordTableContextOrThrow();
+
+  const shouldCompactRecordTableFirstColumn = useRecoilComponentValue(
+    shouldCompactRecordTableFirstColumnComponentState,
+  );
+
+  const recordTableWidth = useRecoilComponentValue(
+    recordTableWidthComponentState,
+  );
 
   useEffect(() => {
     if (isDefined(resizedFieldMetadataItemId)) {
       return;
     }
+
+    const { lastColumnWidth } = computeLastRecordTableColumnWidth({
+      recordFields: visibleRecordFields,
+      shouldCompactFirstColumn: shouldCompactRecordTableFirstColumn,
+      tableWidth: recordTableWidth,
+    });
+
+    const { visibleRecordFieldsWidth } = computeVisibleRecordFieldsWidthOnTable(
+      {
+        shouldCompactFirstColumn: shouldCompactRecordTableFirstColumn,
+        visibleRecordFields,
+      },
+    );
+
+    const totalTableBodyWidth =
+      visibleRecordFieldsWidth +
+      RECORD_TABLE_COLUMN_DRAG_AND_DROP_WIDTH +
+      RECORD_TABLE_COLUMN_CHECKBOX_WIDTH +
+      RECORD_TABLE_COLUMN_ADD_COLUMN_BUTTON_WIDTH +
+      lastColumnWidth +
+      visibleRecordFields.length;
+
+    updateRecordTableCSSVariable(
+      RECORD_TABLE_VIRTUALIZATION_BODY_PLACEHOLDER_WIDTH_CSS_VARIABLE_NAME,
+      `${totalTableBodyWidth}px`,
+    );
 
     updateRecordTableCSSVariable(
       RECORD_TABLE_COLUMN_LAST_EMPTY_COLUMN_WIDTH_VARIABLE_NAME,
@@ -40,7 +81,24 @@ export const RecordTableColumnWidthEffect = () => {
         `${recordField.size}px`,
       );
     }
-  }, [lastColumnWidth, resizedFieldMetadataItemId, visibleRecordFields]);
+
+    if (shouldCompactRecordTableFirstColumn) {
+      updateRecordTableCSSVariable(
+        getRecordTableColumnFieldWidthCSSVariableName(0),
+        `${RECORD_TABLE_LABEL_IDENTIFIER_COLUMN_WIDTH_ON_MOBILE}px`,
+      );
+    } else {
+      updateRecordTableCSSVariable(
+        getRecordTableColumnFieldWidthCSSVariableName(0),
+        `${visibleRecordFields[0].size}px`,
+      );
+    }
+  }, [
+    resizedFieldMetadataItemId,
+    visibleRecordFields,
+    recordTableWidth,
+    shouldCompactRecordTableFirstColumn,
+  ]);
 
   return null;
 };
