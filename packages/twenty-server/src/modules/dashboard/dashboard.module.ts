f946f7610c0b4bb4eef7f459e4b00bcde20f5942
bosiraphael <raphael.bosi@gmail.com>
2025-12-03 17:11:31 +0100

    create duplicate controller

--- packages/twenty-server/src/modules/dashboard/dashboard.module.ts
+++ packages/twenty-server/src/modules/dashboard/dashboard.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { PageLayoutModule } from 'src/engine/core-modules/page-layout/page-layout.module';
 import { TwentyORMModule } from 'src/engine/twenty-orm/twenty-orm.module';
+import { DashboardController } from 'src/modules/dashboard/controllers/dashboard.controller';
 import { DashboardResolver } from 'src/modules/dashboard/resolvers/dashboard.resolver';
 import { DashboardDuplicationService } from 'src/modules/dashboard/services/dashboard-duplication.service';
 
 @Module({
   imports: [PageLayoutModule, TwentyORMModule],
+  controllers: [DashboardController],
   providers: [DashboardDuplicationService, DashboardResolver],
   exports: [DashboardDuplicationService],
 })
