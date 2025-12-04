3e5860d2181a634bee521aab0979bad94e91d245
prastoin <paul@twenty.com>
2025-12-04 14:11:23 +0100

    feat(server): upgrade command

--- packages/twenty-server/src/database/commands/upgrade-version-command/1-13/1-13-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-13/1-13-upgrade-version-command.module.ts
@@ -4,8 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackfillViewMainGroupByFieldMetadataIdCommand } from 'src/database/commands/upgrade-version-command/1-13/1-13-backfill-view-main-group-by-field-metadata-id.command';
 import { CleanEmptyStringNullInTextFieldsCommand } from 'src/database/commands/upgrade-version-command/1-13/1-13-clean-empty-string-null-in-text-fields.command';
 import { DeduplicateRoleTargetsCommand } from 'src/database/commands/upgrade-version-command/1-13/1-13-deduplicate-role-targets.command';
+import { MigratePageLayoutTabApplicationIdCommand } from 'src/database/commands/upgrade-version-command/1-13/1-13-migrate-page-layout-tab-application-id.command';
 import { UpdateRoleTargetsUniqueConstraintMigrationCommand } from 'src/database/commands/upgrade-version-command/1-13/1-13-update-role-targets-unique-constraint-migration.command';
 import { FeatureFlagEntity } from 'src/engine/core-modules/feature-flag/feature-flag.entity';
+import { PageLayoutTabEntity } from 'src/engine/core-modules/page-layout/entities/page-layout-tab.entity';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
@@ -24,6 +26,7 @@ import { ViewEntity } from 'src/engine/metadata-modules/view/entities/view.entit
       ViewGroupEntity,
       FeatureFlagEntity,
       RoleTargetEntity,
+      PageLayoutTabEntity,
     ]),
     DataSourceModule,
   ],
@@ -32,12 +35,14 @@ import { ViewEntity } from 'src/engine/metadata-modules/view/entities/view.entit
     BackfillViewMainGroupByFieldMetadataIdCommand,
     DeduplicateRoleTargetsCommand,
     UpdateRoleTargetsUniqueConstraintMigrationCommand,
+    MigratePageLayoutTabApplicationIdCommand,
   ],
   exports: [
     CleanEmptyStringNullInTextFieldsCommand,
     BackfillViewMainGroupByFieldMetadataIdCommand,
     DeduplicateRoleTargetsCommand,
     UpdateRoleTargetsUniqueConstraintMigrationCommand,
+    MigratePageLayoutTabApplicationIdCommand,
   ],
 })
 export class V1_13_UpgradeVersionCommandModule {}
