a036cc867cafac69b01c60500847528286f526e5
Marie Stoppa <marie.stoppa@essec.edu>
2025-12-04 18:31:14 +0100

    fix

--- packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/create-standard-relation-field-flat-metadata.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/create-standard-relation-field-flat-metadata.util.ts
@@ -108,7 +108,6 @@ export const createStandardRelationFieldFlatMetadata = <
     morphId: null,
     viewFieldIds: [],
     viewFilterIds: [],
-    viewGroupIds: [],
     kanbanAggregateOperationViewIds: [],
     calendarViewIds: [],
     mainGroupByFieldMetadataViewIds: [],
