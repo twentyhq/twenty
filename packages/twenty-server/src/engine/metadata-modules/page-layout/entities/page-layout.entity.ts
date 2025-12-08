37ce02344d046ae7bdda70b96168d15f6acfef6e
bosiraphael <raphael.bosi@gmail.com>
2025-12-08 14:04:45 +0100

    Revert "remove migration"
    This reverts commit 0386eeb602e6b0390c1996e67abdb939ed629110.

--- packages/twenty-server/src/engine/metadata-modules/page-layout/entities/page-layout.entity.ts
+++ packages/twenty-server/src/engine/metadata-modules/page-layout/entities/page-layout.entity.ts
@@ -14,12 +14,12 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-import { SyncableEntity } from 'src/engine/workspace-manager/workspace-sync/interfaces/syncable-entity.interface';
+import { StrictSyncableEntity } from 'src/engine/workspace-manager/workspace-sync/interfaces/strict-syncable-entity.interface';
 
-import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
-import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 import { PageLayoutTabEntity } from 'src/engine/metadata-modules/page-layout/entities/page-layout-tab.entity';
 import { PageLayoutType } from 'src/engine/metadata-modules/page-layout/enums/page-layout-type.enum';
+import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
+import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 
 @Entity({ name: 'pageLayout', schema: 'core' })
 @ObjectType('PageLayout')
@@ -29,7 +29,7 @@ import { PageLayoutType } from 'src/engine/metadata-modules/page-layout/enums/pa
   { where: '"deletedAt" IS NULL' },
 )
 export class PageLayoutEntity
-  extends SyncableEntity
+  extends StrictSyncableEntity
   implements Required<PageLayoutEntity>
 {
   @PrimaryGeneratedColumn('uuid')
