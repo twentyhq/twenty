c792729e2ec365c6e0b6b954e52e3eef16cdc59c
ehconitin <nitinkoche03@gmail.com>
2025-12-08 18:42:44 +0530

    add tests

--- packages/twenty-front/src/modules/page-layout/widgets/graph/__tests__/GraphWidgetTestWrapper.tsx
+++ packages/twenty-front/src/modules/page-layout/widgets/graph/__tests__/GraphWidgetTestWrapper.tsx
@@ -1,15 +1,6 @@
 import { GraphWidgetComponentInstanceContext } from '@/page-layout/widgets/graph/states/contexts/GraphWidgetComponentInstanceContext';
-import { i18n } from '@lingui/core';
-import { I18nProvider } from '@lingui/react';
 import { type ReactNode } from 'react';
 import { RecoilRoot, type MutableSnapshot } from 'recoil';
-import { messages as enMessages } from '~/locales/generated/en';
-import { SOURCE_LOCALE } from 'twenty-shared/translations';
-
-i18n.load({
-  [SOURCE_LOCALE]: enMessages,
-});
-i18n.activate(SOURCE_LOCALE);
 
 export const GRAPH_WIDGET_TEST_INSTANCE_ID =
   '30303030-f244-4ae0-906b-78958aa07642';
@@ -26,12 +17,10 @@ export const GraphWidgetTestWrapper = ({
   const instanceId = instanceIdFromProps ?? GRAPH_WIDGET_TEST_INSTANCE_ID;
 
   return (
-    <I18nProvider i18n={i18n}>
-      <RecoilRoot initializeState={initializeState}>
-        <GraphWidgetComponentInstanceContext.Provider value={{ instanceId }}>
-          {children}
-        </GraphWidgetComponentInstanceContext.Provider>
-      </RecoilRoot>
-    </I18nProvider>
+    <RecoilRoot initializeState={initializeState}>
+      <GraphWidgetComponentInstanceContext.Provider value={{ instanceId }}>
+        {children}
+      </GraphWidgetComponentInstanceContext.Provider>
+    </RecoilRoot>
   );
 };
