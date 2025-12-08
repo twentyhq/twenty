c941b043d78e751708892143a94891dc9646dcdc
Charles Bochet <charles@twenty.com>
2025-12-08 14:50:06 +0100

    Fix

--- packages/twenty-server/src/modules/connected-account/channel-sync/channel-sync.module.ts
+++ packages/twenty-server/src/modules/connected-account/channel-sync/channel-sync.module.ts
@@ -4,6 +4,7 @@ import { PermissionsModule } from 'src/engine/metadata-modules/permissions/permi
 import { WorkspaceDataSourceModule } from 'src/engine/workspace-datasource/workspace-datasource.module';
 import { ChannelSyncResolver } from 'src/modules/connected-account/channel-sync/channel-sync.resolver';
 import { ChannelSyncService } from 'src/modules/connected-account/channel-sync/services/channel-sync.service';
+import { MessagingCommonModule } from 'src/modules/messaging/common/messaging-common.module';
 
 @Module({
   imports: [
