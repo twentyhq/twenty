790a8d6f939d92901011843c4eeaa98c385824d5
Marie <51697796+ijreilly@users.noreply.github.com>
2025-12-09 11:42:00 +0100

    Fixes on views (#16407)
    Fixes
    
    - Fixes https://github.com/twentyhq/twenty/issues/15640 : we have some
    viewGroups that were in the past wrongly migrated; eg deleteing an
    enum's option did not lead to deleting the associated viewGroup. We have
    not cleaned that (we could do a command), but we can identify them and
    choose not to display them - otherwise currently they are shown as a
    duplicate of "No value" column
    - Fixes buggy edge case introduced by
    https://github.com/twentyhq/twenty/pull/16382 : after updating a view
    from Table/Calendar to Kanban, then visiting another view, when coming
    back to the newly updated kanban view the view groups were empty. This
    is due to to the view groups being optimistically created with
    on-the-fly computed ids when the view is updated, while we call
    refreshCoreViewsByObjectMetadataId() after. The view groups we get from
    the refresh obviously have different ids. It is not possible to indicate
    the ids of the view group through the update of the view as they are a
    side effect of the view update.

--- packages/twenty-front/src/modules/object-record/record-board/states/isSwitchingToKanbanViewTypeComponentState.ts
+++
@@ -1,9 +0,0 @@
-import { ContextStoreComponentInstanceContext } from '@/context-store/states/contexts/ContextStoreComponentInstanceContext';
-import { createComponentState } from '@/ui/utilities/state/component-state/utils/createComponentState';
-
-export const isSwitchingToKanbanViewTypeComponentState =
-  createComponentState<boolean>({
-    key: 'isSwitchingToKanbanViewTypeComponentState',
-    componentInstanceContext: ContextStoreComponentInstanceContext,
-    defaultValue: false,
-  });
