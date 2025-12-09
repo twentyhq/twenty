2be49e41f2ed3442126367832f849ed2d513d5a6
Charles Bochet <charles@twenty.com>
2025-12-09 15:55:56 +0100

    Fix lint

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/utils/replace-flat-entity-in-flat-entity-maps-or-throw.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/utils/replace-flat-entity-in-flat-entity-maps-or-throw.util.ts
@@ -3,13 +3,16 @@ import { type SyncableFlatEntity } from 'src/engine/metadata-modules/flat-entity
 import { addFlatEntityToFlatEntityMapsOrThrow } from 'src/engine/metadata-modules/flat-entity/utils/add-flat-entity-to-flat-entity-maps-or-throw.util';
 import { deleteFlatEntityFromFlatEntityMapsOrThrow } from 'src/engine/metadata-modules/flat-entity/utils/delete-flat-entity-from-flat-entity-maps-or-throw.util';
 
-export type ReplaceFlatEntityInFlatEntityMapsOrThrowArgs<T extends SyncableFlatEntity> =
-  {
-    flatEntity: T;
-    flatEntityMaps: FlatEntityMaps<T>;
-  };
+export type ReplaceFlatEntityInFlatEntityMapsOrThrowArgs<
+  T extends SyncableFlatEntity,
+> = {
+  flatEntity: T;
+  flatEntityMaps: FlatEntityMaps<T>;
+};
 
-export const replaceFlatEntityInFlatEntityMapsOrThrow = <T extends SyncableFlatEntity>({
+export const replaceFlatEntityInFlatEntityMapsOrThrow = <
+  T extends SyncableFlatEntity,
+>({
   flatEntity,
   flatEntityMaps,
 }: ReplaceFlatEntityInFlatEntityMapsOrThrowArgs<T>): FlatEntityMaps<T> => {
