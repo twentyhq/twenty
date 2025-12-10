333b1f42414264554103909be9e62642b068776a
Félix Malfait <felix@twenty.com>
2025-12-10 14:43:18 +0100

    fix: prettier formatting

--- packages/twenty-front/src/modules/object-record/record-table/virtualization/components/RecordTableRowVirtualizedDebugRowHelper.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/virtualization/components/RecordTableRowVirtualizedDebugRowHelper.tsx
@@ -59,8 +59,7 @@ export const RecordTableRowVirtualizedDebugRowHelper = ({
     (RECORD_TABLE_ROW_HEIGHT + 1);
 
   const record = useRecoilValue(recordStoreFamilyState(recordId ?? ''));
-  const { objectMetadataItem } =
-    useRecordTableContextOrThrow();
+  const { objectMetadataItem } = useRecordTableContextOrThrow();
   const labelIdentifierFieldMetadataItem =
     getLabelIdentifierFieldMetadataItem(objectMetadataItem);
 
