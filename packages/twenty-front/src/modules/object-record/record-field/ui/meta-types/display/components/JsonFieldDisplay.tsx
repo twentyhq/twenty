43345a3de57b592a9cf64013a0072450240b53e6
Etienne <45695613+etiennejouan@users.noreply.github.com>
2025-12-10 15:23:45 +0100

    add json tree view at click in jsonfield

--- packages/twenty-front/src/modules/object-record/record-field/ui/meta-types/display/components/JsonFieldDisplay.tsx
+++ packages/twenty-front/src/modules/object-record/record-field/ui/meta-types/display/components/JsonFieldDisplay.tsx
@@ -1,9 +1,18 @@
 import { useJsonFieldDisplay } from '@/object-record/record-field/ui/meta-types/hooks/useJsonFieldDisplay';
 import { JsonDisplay } from '@/ui/field/display/components/JsonDisplay';
+import { t } from '@lingui/core/macro';
+import { useState } from 'react';
 import { isDefined } from 'twenty-shared/utils';
+import { JsonTree } from 'twenty-ui/json-visualizer';
 
 export const JsonFieldDisplay = () => {
   const { fieldValue, maxWidth } = useJsonFieldDisplay();
+
+  const [isJsonTreeViewOpen, setIsJsonTreeViewOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsJsonTreeViewOpen(true);
+  };
 
   if (!isDefined(fieldValue)) {
     return <></>;
@@ -11,5 +20,20 @@ export const JsonFieldDisplay = () => {
 
   const value = JSON.stringify(fieldValue);
 
-  return <JsonDisplay text={value} maxWidth={maxWidth} />;
+  return (
+    <div onClick={handleClick}>
+      <JsonDisplay text={value} maxWidth={maxWidth} />
+      {isJsonTreeViewOpen && (
+        <JsonTree
+          value={fieldValue}
+          shouldExpandNodeInitially={() => true}
+          emptyArrayLabel={t`Empty Array`}
+          emptyObjectLabel={t`Empty Object`}
+          emptyStringLabel={t`[empty string]`}
+          arrowButtonCollapsedLabel={t`Expand`}
+          arrowButtonExpandedLabel={t`Collapse`}
+        />
+      )}
+    </div>
+  );
 };
