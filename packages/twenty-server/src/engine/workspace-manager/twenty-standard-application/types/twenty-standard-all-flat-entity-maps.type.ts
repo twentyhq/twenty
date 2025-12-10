09648be68dc05707bbac02af2e5070c0bc5cf176
prastoin <paul@twenty.com>
2025-12-10 11:55:57 +0100

    feat(server): plug

---
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/types/twenty-standard-all-flat-entity-maps.type.ts
@@ -0,0 +1,12 @@
+import { AllFlatEntityMaps } from "src/engine/metadata-modules/flat-entity/types/all-flat-entity-maps.type";
+
+export type TwentyStandardAllFlatEntityMaps = Pick<
+  AllFlatEntityMaps,
+  | 'flatIndexMaps'
+  | 'flatObjectMetadataMaps'
+  | 'flatFieldMetadataMaps'
+  | 'flatViewFieldMaps'
+  | 'flatViewFilterMaps'
+  | 'flatViewGroupMaps'
+  | 'flatViewMaps'
+>
