62754e2e6c86cdcb3bdd11027e22c2f5a2a3ae1d
Félix Malfait <felix@twenty.com>
2025-12-10 20:59:21 +0100

    fix: remove barrel exports from tool-provider module

--- packages/twenty-server/src/engine/core-modules/tool-provider/interfaces/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './tool-provider.interface';
--- packages/twenty-server/src/engine/core-modules/tool-provider/providers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './action-tool.provider';
-export * from './database-tool.provider';
-export * from './metadata-tool.provider';
-export * from './workflow-tool.provider';
