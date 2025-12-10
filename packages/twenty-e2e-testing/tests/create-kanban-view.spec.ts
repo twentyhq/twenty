5946f064ce25364f109c8ceba3ca273855c0814d
Marie Stoppa <marie.stoppa@essec.edu>
2025-12-10 11:44:56 +0100

    wip

--- packages/twenty-e2e-testing/tests/create-kanban-view.spec.ts
+++ packages/twenty-e2e-testing/tests/create-kanban-view.spec.ts
@@ -1,10 +1,11 @@
-import { test } from '../lib/fixtures/screenshot';
+import { expect, test } from '../lib/fixtures/screenshot';
 
 if (process.env.LINK) {
   const baseURL = new URL(process.env.LINK).origin;
   test.use({ baseURL });
 }
-test('Create kanban view', async ({ page }) => {
+test.describe.serial('Create Kanban View', () => {
+test('Create Industry Select Field', async ({ page }) => {
     await page.getByRole('link', { name: 'Settings' }).click();
     await page.getByRole('link', { name: 'Data model' }).click();
     await page.getByRole('link', { name: 'Opportunities' }).click();
@@ -20,14 +21,20 @@ test('Create kanban view', async ({ page }) => {
     await page.getByRole('button', { name: 'Add option' }).click();
     await page.getByRole('button', { name: 'Option 3' }).getByRole('textbox').fill('Travel');
     await page.getByRole('button', { name: 'Save' }).click();
-    await page.getByRole('button', { name: 'Exit Settings' }).click();
-    await page.getByRole('link', { name: 'Opportunities' }).click();
-    await page.getByRole('button', { name: 'All Opportunities ·' }).click();
-    await page.getByText('Add view').click();
-    await page.getByRole('textbox').press('ControlOrMeta+a');
-    await page.getByRole('textbox').fill('By industry');
-    await page.getByRole('button', { name: 'Table', exact: true }).click();
-    await page.getByText('Kanban').click();
-    await page.getByRole('button', { name: 'select', exact: true }).click();
-    await page.locator('div').filter({ hasText: /^Industry$/ }).nth(2).click();
+    await page.waitForURL('**/objects/opportunities');
+    await page.waitForSelector('text=Industry');
+    await expect(page.getByText('Industry')).toBeVisible();
 });
+
+test('Create Kanban View from Industry Select Field', async ({ page }) => {
+  await page.getByRole('link', { name: 'Opportunities' }).click();
+  await page.getByRole('button', { name: 'All Opportunities ·' }).click();
+  await page.getByText('Add view').click();
+  await page.getByRole('textbox').press('ControlOrMeta+a');
+  await page.getByRole('textbox').fill('By industry');
+  await page.getByRole('button', { name: 'Table', exact: true }).click();
+  await page.getByText('Kanban').click();
+  await page.getByRole('button', { name: 'select', exact: true }).click();
+  await page.locator('div').filter({ hasText: /^Industry$/ }).nth(2).click();
+});
+})
