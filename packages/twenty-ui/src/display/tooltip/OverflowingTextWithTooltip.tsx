7464ea98f48230856688c4b410e6cd8aeb3d5ace
Devessier <baptiste@devessier.fr>
2025-12-10 14:50:59 +0100

    refactor: use useId to generate component id

--- packages/twenty-ui/src/display/tooltip/OverflowingTextWithTooltip.tsx
+++ packages/twenty-ui/src/display/tooltip/OverflowingTextWithTooltip.tsx
@@ -1,5 +1,5 @@
 import { styled } from '@linaria/react';
-import { useRef, useState } from 'react';
+import { useId, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import { THEME_COMMON } from '@ui/theme';
@@ -83,7 +83,7 @@ export const OverflowingTextWithTooltip = ({
   displayedMaxRows?: number;
   hideTooltip?: boolean;
 }) => {
-  const textElementId = `title-id-${+new Date()}`;
+  const textElementId = useId();
 
   const textRef = useRef<HTMLDivElement>(null);
 
