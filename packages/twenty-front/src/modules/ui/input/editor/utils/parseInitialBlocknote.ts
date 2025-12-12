f02623e6421ec7fac128c547fd72f4d94b5f9f5c
ehconitin <nitinkoche03@gmail.com>
2025-12-12 16:43:56 +0530

    ad back legacy comments

--- packages/twenty-front/src/modules/ui/input/editor/utils/parseInitialBlocknote.ts
+++ packages/twenty-front/src/modules/ui/input/editor/utils/parseInitialBlocknote.ts
@@ -8,11 +8,14 @@ export const parseInitialBlocknote = (
   if (isNonEmptyString(blocknote) && blocknote !== '{}') {
     let parsedBody: PartialBlock[] | undefined = undefined;
 
+    // TODO: Remove this once we have removed the old rich text
     try {
       parsedBody = JSON.parse(blocknote);
     } catch {
       // eslint-disable-next-line no-console
       console.warn(logContext ?? `Failed to parse blocknote body`);
+      // eslint-disable-next-line no-console
+      console.warn(blocknote);
     }
 
     if (!isArray(parsedBody) || parsedBody.length === 0) {
