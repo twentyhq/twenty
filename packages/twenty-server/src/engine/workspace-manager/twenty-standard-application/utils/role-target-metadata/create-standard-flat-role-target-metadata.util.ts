636eff603a363a708426150270b2c2658c5ef23d
prastoin <paul@twenty.com>
2025-12-12 14:24:18 +0100

    lint

--- packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-flat-role-target-metadata.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-flat-role-target-metadata.util.ts
@@ -1,9 +1,7 @@
 import { type FlatRoleTarget } from 'src/engine/metadata-modules/flat-role-target/types/flat-role-target.type';
 import { type AllStandardAgentName } from 'src/engine/workspace-manager/twenty-standard-application/types/all-standard-agent-name.type';
 import { type AllStandardRoleTargetTypeName } from 'src/engine/workspace-manager/twenty-standard-application/types/all-standard-role-target-type-name.type';
-import {
-  type CreateStandardRoleTargetArgs
-} from 'src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-role-target-flat-metadata.util';
+import { type CreateStandardRoleTargetArgs } from 'src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-role-target-flat-metadata.util';
 
 type StandardRoleTargetBuilder<T extends AllStandardRoleTargetTypeName> = (
   args: Omit<CreateStandardRoleTargetArgs<T>, 'context'>,
