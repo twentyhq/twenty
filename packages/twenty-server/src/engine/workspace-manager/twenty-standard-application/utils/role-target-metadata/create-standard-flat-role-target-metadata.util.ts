141e73e99f7361927c968d4a01a31ede5690a66b
prastoin <paul@twenty.com>
2025-12-15 15:33:19 +0100

    helper role target

--- packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-flat-role-target-metadata.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-flat-role-target-metadata.util.ts
@@ -1,7 +1,10 @@
 import { type FlatRoleTarget } from 'src/engine/metadata-modules/flat-role-target/types/flat-role-target.type';
 import { type AllStandardAgentName } from 'src/engine/workspace-manager/twenty-standard-application/types/all-standard-agent-name.type';
 import { type AllStandardRoleTargetTypeName } from 'src/engine/workspace-manager/twenty-standard-application/types/all-standard-role-target-type-name.type';
-import { type CreateStandardRoleTargetArgs } from 'src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-role-target-flat-metadata.util';
+import {
+  type CreateStandardRoleTargetArgs,
+  createStandardRoleTargetFlatMetadata,
+} from 'src/engine/workspace-manager/twenty-standard-application/utils/role-target-metadata/create-standard-role-target-flat-metadata.util';
 
 type StandardRoleTargetBuilder<T extends AllStandardRoleTargetTypeName> = (
   args: Omit<CreateStandardRoleTargetArgs<T>, 'context'>,
@@ -10,10 +13,17 @@ type StandardRoleTargetBuilder<T extends AllStandardRoleTargetTypeName> = (
 export const STANDARD_FLAT_ROLE_TARGET_METADATA_BUILDERS = {
   agent: {
     helper: (
-      _args: Omit<CreateStandardRoleTargetArgs<'agent'>, 'context'>,
-    ): undefined => {
-      // Helper agent does not have a standard role assigned
-      return undefined;
+      args: Omit<CreateStandardRoleTargetArgs<'agent'>, 'context'>,
+    ): FlatRoleTarget => {
+      return createStandardRoleTargetFlatMetadata({
+        ...args,
+        context: {
+          roleTargetTypeName: 'agent',
+          roleTargetName: 'helper',
+          roleName: 'dataManipulator',
+          agentName: 'helper',
+        },
+      });
     },
   },
 } satisfies {
