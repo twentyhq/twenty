231705dce54d18bb4aeac2235e6c1941b6601fa8
prastoin <paul@twenty.com>
2025-12-16 18:09:02 +0100

    lint

--- packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/twenty-standard-application.module.ts
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/twenty-standard-application.module.ts
@@ -3,8 +3,8 @@ import { Module } from '@nestjs/common';
 import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
 import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
 import { WorkspaceMigrationV2Module } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-v2.module';
-
 import { GlobalWorkspaceDataSourceModule } from 'src/engine/twenty-orm/global-workspace-datasource/global-workspace-datasource.module';
+
 import { TwentyStandardApplicationService } from './services/twenty-standard-application.service';
 
 @Module({
