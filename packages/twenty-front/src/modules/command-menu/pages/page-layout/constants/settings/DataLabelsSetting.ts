71156f7dccb9ce34b169f9a56c0972d6c0879252
bosiraphael <raphael.bosi@gmail.com>
2025-12-16 15:55:08 +0100

    update icons

--- packages/twenty-front/src/modules/command-menu/pages/page-layout/constants/settings/DataLabelsSetting.ts
+++ packages/twenty-front/src/modules/command-menu/pages/page-layout/constants/settings/DataLabelsSetting.ts
@@ -1,11 +1,11 @@
 import { CHART_CONFIGURATION_SETTING_LABELS } from '@/command-menu/pages/page-layout/constants/settings/ChartConfigurationSettingLabels';
 import { CHART_CONFIGURATION_SETTING_IDS } from '@/command-menu/pages/page-layout/types/ChartConfigurationSettingIds';
 import { type ChartSettingsItem } from '@/command-menu/pages/page-layout/types/ChartSettingsGroup';
-import { IconTag } from 'twenty-ui/display';
+import { IconBadge2k } from 'twenty-ui/display';
 
 export const DATA_LABELS_SETTING: ChartSettingsItem = {
   isBoolean: true,
-  Icon: IconTag,
+  Icon: IconBadge2k,
   label: CHART_CONFIGURATION_SETTING_LABELS.DATA_LABELS,
   id: CHART_CONFIGURATION_SETTING_IDS.DATA_LABELS,
 };
