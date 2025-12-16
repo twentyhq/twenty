f1370bcdc163dc7f573494457b3935f44bc80aff
prastoin <paul@twenty.com>
2025-12-16 11:13:36 +0100

    lint

--- packages/twenty-server/src/database/commands/upgrade-version-command/upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/upgrade-version-command.module.ts
@@ -6,7 +6,6 @@ import { V1_14_UpgradeVersionCommandModule } from 'src/database/commands/upgrade
 import { UpgradeCommand } from 'src/database/commands/upgrade-version-command/upgrade.command';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module';
-import { V1_14_UpgradeVersionCommandModule } from 'src/database/commands/upgrade-version-command/1-14/1-14-upgrade-version-command.module';
 
 @Module({
   imports: [
