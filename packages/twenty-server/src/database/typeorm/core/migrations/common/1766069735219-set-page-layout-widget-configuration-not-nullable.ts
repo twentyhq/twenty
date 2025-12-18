6d69d41b031ae95ad10098bcfdb852ff16acfe3e
prastoin <paul@twenty.com>
2025-12-18 15:57:24 +0100

    typeorm

---
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1766069735219-set-page-layout-widget-configuration-not-nullable.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CoreMigrationCheck1766069735219 implements MigrationInterface {
+    name = 'CoreMigrationCheck1766069735219'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."pageLayoutWidget" ALTER COLUMN "configuration" SET NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."pageLayoutWidget" ALTER COLUMN "configuration" DROP NOT NULL`);
+    }
+
+}
+
