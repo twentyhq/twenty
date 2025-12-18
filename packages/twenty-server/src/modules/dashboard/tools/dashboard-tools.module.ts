7b0723b5a544d3feb3cfc93136c48bb497b8a5eb
prastoin <paul@twenty.com>
2025-12-18 12:04:45 +0100

    module injection

--- packages/twenty-server/src/modules/dashboard/tools/dashboard-tools.module.ts
+++ packages/twenty-server/src/modules/dashboard/tools/dashboard-tools.module.ts
@@ -3,6 +3,8 @@ import { Global, Module } from '@nestjs/common';
 import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
 import { RecordPositionModule } from 'src/engine/core-modules/record-position/record-position.module';
 import { DASHBOARD_TOOL_SERVICE_TOKEN } from 'src/engine/core-modules/tool-provider/constants/dashboard-tool-service.token';
+import { PageLayoutTabModule } from 'src/engine/metadata-modules/page-layout-tab/page-layout-tab.module';
+import { PageLayoutWidgetModule } from 'src/engine/metadata-modules/page-layout-widget/page-layout-widget.module';
 import { PageLayoutModule } from 'src/engine/metadata-modules/page-layout/page-layout.module';
 import { TwentyORMModule } from 'src/engine/twenty-orm/twenty-orm.module';
 
@@ -12,6 +14,8 @@ import { DashboardToolWorkspaceService } from './services/dashboard-tool.workspa
 @Module({
   imports: [
     PageLayoutModule,
+    PageLayoutTabModule,
+    PageLayoutWidgetModule,
     RecordPositionModule,
     TwentyORMModule,
     ApplicationModule,
