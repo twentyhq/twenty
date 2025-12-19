0bdd5f3e3985702c1ba0bbc2ead1a026ba61f378
prastoin <paul@twenty.com>
2025-12-19 07:23:42 +0100

    feat(server): types universal identifier

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/universal-flat-entity.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/universal-flat-entity.type.ts
@@ -1,12 +1,24 @@
-import { CastRecordTypeOrmDatePropertiesToString } from "src/engine/metadata-modules/flat-entity/types/cast-record-typeorm-date-properties-to-string.type";
+import { CastRecordTypeOrmDatePropertiesToString } from 'src/engine/metadata-modules/flat-entity/types/cast-record-typeorm-date-properties-to-string.type';
+import { FromMetadataEntityToMetadataName } from 'src/engine/metadata-modules/flat-entity/types/from-metadata-entity-to-metadata-name.type';
+import { MetadataManyToOneJoinColumn } from 'src/engine/metadata-modules/flat-entity/types/metadata-many-to-one-join-column.type';
+import { ExtractEntityRelatedSyncableEntityProperties } from 'src/engine/workspace-manager/workspace-migration-v2/types/extract-entity-related-syncable-entity-properties.type';
+import { RemoveSuffix } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/remove-suffix.type';
+import { SyncableEntity } from 'src/engine/workspace-manager/workspace-sync/interfaces/syncable-entity.interface';
 
 export type UniversalFlatEntityFrom<
-  TEntity,
-  TEntityRelationProperties extends keyof TEntity,
+  TEntity extends SyncableEntity,
+  TMetadataName extends
+    FromMetadataEntityToMetadataName<TEntity> = FromMetadataEntityToMetadataName<TEntity>,
 > = Omit<
   TEntity,
-  | TEntityRelationProperties
+  | ExtractEntityRelatedSyncableEntityProperties<TEntity>
   | 'application'
+  | 'workspaceId'
+  | 'applicationId'
   | keyof CastRecordTypeOrmDatePropertiesToString<TEntity>
+  | MetadataManyToOneJoinColumn<TMetadataName>
 > &
-  CastRecordTypeOrmDatePropertiesToString<TEntity>;
+  CastRecordTypeOrmDatePropertiesToString<TEntity> & {
+    [P in MetadataManyToOneJoinColumn<TMetadataName> &
+      string as `${RemoveSuffix<P, 'Id'>}UniversalIdentifier`]: P;
+  };
