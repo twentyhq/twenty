0bc027c1ea3abacff6e28c4ce512b137f84d8152
neo773 <neo773@protonmail.com>
2025-12-19 21:24:04 +0530

    extract to utils + test

--- packages/twenty-server/src/modules/messaging/message-folder-manager/services/sync-message-folders.service.ts
+++ packages/twenty-server/src/modules/messaging/message-folder-manager/services/sync-message-folders.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
 
-import { isNonEmptyString } from '@sniptt/guards';
-import deepEqual from 'deep-equal';
 import { ConnectedAccountProvider } from 'twenty-shared/types';
 
 import {
@@ -21,6 +19,10 @@ import {
 import { GmailGetAllFoldersService } from 'src/modules/messaging/message-folder-manager/drivers/gmail/services/gmail-get-all-folders.service';
 import { ImapGetAllFoldersService } from 'src/modules/messaging/message-folder-manager/drivers/imap/services/imap-get-all-folders.service';
 import { MicrosoftGetAllFoldersService } from 'src/modules/messaging/message-folder-manager/drivers/microsoft/services/microsoft-get-all-folders.service';
+import { computeFolderIdsToDelete } from 'src/modules/messaging/message-folder-manager/utils/compute-folder-ids-to-delete.util';
+import { computeFoldersToCreate } from 'src/modules/messaging/message-folder-manager/utils/compute-folders-to-create.util';
+import { computeFoldersToUpdate } from 'src/modules/messaging/message-folder-manager/utils/compute-folders-to-update.util';
+import { computeUpdatedFolders } from 'src/modules/messaging/message-folder-manager/utils/compute-updated-folders.util';
 
 @Injectable()
 export class SyncMessageFoldersService {
@@ -112,67 +114,21 @@ export class SyncMessageFoldersService {
     messageChannelId: string,
     workspaceId: string,
   ): Promise<MessageFolder[]> {
-    const existingFoldersByExternalId = new Map(
-      existingFolders.map((folder) => [folder.externalId, folder]),
-    );
+    const foldersToCreate = computeFoldersToCreate({
+      discoveredFolders,
+      existingFolders,
+      messageChannelId,
+    });
 
-    const foldersToCreate: Partial<MessageFolderWorkspaceEntity>[] = [];
-    const foldersToUpdate = new Map<
-      string,
-      Partial<MessageFolderWorkspaceEntity>
-    >();
+    const foldersToUpdate = computeFoldersToUpdate({
+      discoveredFolders,
+      existingFolders,
+    });
 
-    for (const discoveredFolder of discoveredFolders) {
-      const existingFolder = existingFoldersByExternalId.get(
-        discoveredFolder.externalId,
-      );
-
-      if (!existingFolder) {
-        foldersToCreate.push({
-          name: discoveredFolder.name,
-          externalId: discoveredFolder.externalId,
-          messageChannelId,
-          isSentFolder: discoveredFolder.isSentFolder,
-          isSynced: discoveredFolder.isSynced,
-          syncCursor: null,
-          parentFolderId: isNonEmptyString(discoveredFolder.parentFolderId)
-            ? discoveredFolder.parentFolderId
-            : null,
-        });
-        continue;
-      }
-
-      const discoveredFolderData = {
-        name: discoveredFolder.name,
-        isSentFolder: discoveredFolder.isSentFolder,
-        parentFolderId: isNonEmptyString(discoveredFolder.parentFolderId)
-          ? discoveredFolder.parentFolderId
-          : null,
-      };
-
-      const existingFolderData = {
-        name: existingFolder.name,
-        isSentFolder: existingFolder.isSentFolder,
-        parentFolderId: isNonEmptyString(existingFolder.parentFolderId)
-          ? existingFolder.parentFolderId
-          : null,
-      };
-
-      if (!deepEqual(discoveredFolderData, existingFolderData)) {
-        foldersToUpdate.set(existingFolder.id, discoveredFolderData);
-      }
-    }
-
-    const discoveredExternalIds = new Set(
-      discoveredFolders.map((discoveredFolder) => discoveredFolder.externalId),
-    );
-
-    const folderIdsToDelete = existingFolders
-      .filter(
-        (existingFolder) =>
-          !discoveredExternalIds.has(existingFolder.externalId),
-      )
-      .map((existingFolder) => existingFolder.id);
+    const folderIdsToDelete = computeFolderIdsToDelete({
+      discoveredFolders,
+      existingFolders,
+    });
 
     const authContext = buildSystemAuthContext(workspaceId);
 
@@ -222,44 +178,16 @@ export class SyncMessageFoldersService {
                   )
                 : [];
 
-            const updatedExistingFolders = this.computeUpdatedFolders(
+            const updatedExistingFolders = computeUpdatedFolders({
               existingFolders,
               foldersToUpdate,
               folderIdsToDelete,
-            );
+            });
 
             return [...updatedExistingFolders, ...createdFolders];
           },
         );
       },
     );
-  }
-
-  private computeUpdatedFolders(
-    existingFolders: MessageFolder[],
-    foldersToUpdate: Map<string, Partial<MessageFolderWorkspaceEntity>>,
-    folderIdsToDelete: string[],
-  ): MessageFolder[] {
-    return existingFolders.map((existingFolder) => {
-      const update = foldersToUpdate.get(existingFolder.id);
-      const isMarkedForDeletion = folderIdsToDelete.includes(existingFolder.id);
-
-      const pendingSyncAction = isMarkedForDeletion
-        ? MessageFolderPendingSyncAction.FOLDER_DELETION
-        : MessageFolderPendingSyncAction.NONE;
-
-      if (update) {
-        return {
-          ...existingFolder,
-          ...update,
-          pendingSyncAction,
-        };
-      }
-
-      return {
-        ...existingFolder,
-        pendingSyncAction,
-      };
-    });
   }
 }
