50c53d1dc996534c21945cdc3fbd1cdcfbbd9643
prastoin <paul@twenty.com>
2025-12-19 08:27:17 +0100

    naming

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/extract-entity-many-to-one-syncable-entity-relation-properties.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/extract-entity-many-to-one-syncable-entity-relation-properties.type.ts
@@ -1,10 +1,10 @@
 import { SyncableEntity } from "src/engine/workspace-manager/workspace-sync/interfaces/syncable-entity.interface";
 import { Relation } from "typeorm";
 
-export type ExtractEntityOneToManyRelationProperties<T> =
+export type ExtractEntityManyToOneSyncableEntityRelationProperties<T> =
   NonNullable<
     {
-      [P in keyof T]: NonNullable<T[P]> extends Array<Relation<SyncableEntity>>
+      [P in keyof T]: NonNullable<T[P]> extends Relation<SyncableEntity>
         ? P
         : never;
     }[keyof T]
--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity.type.ts
@@ -1,8 +1,8 @@
 import { type SyncableEntity } from 'src/engine/workspace-manager/workspace-sync/interfaces/syncable-entity.interface';
 
 import { type CastRecordTypeOrmDatePropertiesToString } from 'src/engine/metadata-modules/flat-entity/types/cast-record-typeorm-date-properties-to-string.type';
-import { ExtractEntityManyToOneRelationProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-many-to-one-relation-properties.type';
-import { ExtractEntityOneToManyRelationProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-one-to-many-relation-properties.type';
+import { ExtractEntityManyToOneSyncableEntityRelationProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-many-to-one-syncable-entity-relation-properties.type';
+import { ExtractEntityOneToManySyncableEntityRelationProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-one-to-many-syncabl-entity-relation-properties.type';
 import { ExtractEntityRelatedSyncableEntityProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-related-syncable-entity-properties.type';
 import { RemoveSuffix } from 'src/engine/workspace-manager/workspace-migration-v2/workspace-migration-builder-v2/types/remove-suffix.type';
 import { type NonNullableProperties } from 'src/types/non-nullable-properties.type';
@@ -21,9 +21,9 @@ export type FlatEntityFrom<TEntity> = Omit<
   | keyof CastRecordTypeOrmDatePropertiesToString<TEntity>
 > &
   CastRecordTypeOrmDatePropertiesToString<TEntity> & {
-    [P in ExtractEntityManyToOneRelationProperties<TEntity> &
+    [P in ExtractEntityManyToOneSyncableEntityRelationProperties<TEntity> &
       string as RemoveSuffix<P, 's'>]: string;
   } & {
-    [P in ExtractEntityOneToManyRelationProperties<TEntity> &
+    [P in ExtractEntityOneToManySyncableEntityRelationProperties<TEntity> &
       string as `${RemoveSuffix<P, 's'>}Ids`]: string[];
   };
