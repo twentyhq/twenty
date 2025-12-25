c36039eacf403234ad1fabdcfecd3a900597f5d0
Abdullah <125115953+mabdullahabaid@users.noreply.github.com>
2025-12-25 05:37:48 +0500

    fix: simplify fieldsWidgetSectionContainer

--- packages/twenty-front/src/modules/page-layout/widgets/fields/components/FieldsWidgetSectionContainer.tsx
+++ packages/twenty-front/src/modules/page-layout/widgets/fields/components/FieldsWidgetSectionContainer.tsx
@@ -1,6 +1,4 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Section } from 'twenty-ui/layout';
 
 const StyledFieldsWidgetSectionContainer = styled(Section)`
@@ -9,75 +7,32 @@ const StyledFieldsWidgetSectionContainer = styled(Section)`
   width: auto;
 `;
 
-const StyledHeader = styled.header<{
-  isDropdownOpen?: boolean;
-  areRecordsAvailable?: boolean;
-  ariaLabel?: string;
-}>`
+const StyledHeader = styled.header`
   align-items: center;
   display: flex;
   height: 24px;
   justify-content: space-between;
-  margin-bottom: ${({ theme, areRecordsAvailable }) =>
-    areRecordsAvailable && theme.spacing(2)};
   padding-left: ${({ theme }) => theme.spacing(3)};
   padding-right: ${({ theme }) => theme.spacing(2)};
-`;
-
-const StyledTitle = styled.div`
-  align-items: flex-end;
-  display: flex;
-  gap: ${({ theme }) => theme.spacing(1)};
 `;
 
 const StyledTitleLabel = styled.div`
   font-weight: ${({ theme }) => theme.font.weight.medium};
 `;
 
-const StyledLink = styled(Link)`
-  color: ${({ theme }) => theme.font.color.light};
-  text-decoration: none;
-  font-size: ${({ theme }) => theme.font.size.sm};
-  :hover {
-    color: ${({ theme }) => theme.font.color.secondary};
-  }
-`;
-
 type FieldsWidgetSectionContainerProps = {
   children: React.ReactNode;
   title: string;
-  link?: { to: string; label: string };
-  rightAdornment?: React.ReactNode;
-  hideRightAdornmentOnMouseLeave?: boolean;
-  areRecordsAvailable?: boolean;
-  dataTestId?: string;
 };
 
 export const FieldsWidgetSectionContainer = ({
   children,
   title,
-  link,
-  rightAdornment,
-  hideRightAdornmentOnMouseLeave = true,
-  areRecordsAvailable = false,
-  dataTestId,
 }: FieldsWidgetSectionContainerProps) => {
-  const [isHovered, setIsHovered] = useState(false);
   return (
     <StyledFieldsWidgetSectionContainer>
-      <StyledHeader
-        areRecordsAvailable={areRecordsAvailable}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        data-testid={dataTestId}
-      >
-        <StyledTitle>
-          <StyledTitleLabel>{title}</StyledTitleLabel>
-          {link && <StyledLink to={link.to}>{link.label}</StyledLink>}
-        </StyledTitle>
-        {hideRightAdornmentOnMouseLeave && !isHovered && areRecordsAvailable
-          ? null
-          : rightAdornment}
+      <StyledHeader>
+        <StyledTitleLabel>{title}</StyledTitleLabel>
       </StyledHeader>
       {children}
     </StyledFieldsWidgetSectionContainer>
