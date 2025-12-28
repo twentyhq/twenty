f710ec07aa0a7ec280619f954cb11941ddc12d8c
alvhix <alvhix@gmail.com>
2025-12-28 19:36:12 +0100

    fix(16819): Add NoteRestoreManyPostQueryHook for restoring note targets as well

--- packages/twenty-server/src/modules/note/query-hooks/note-query-hook.module.ts
+++ packages/twenty-server/src/modules/note/query-hooks/note-query-hook.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { NoteDeleteOnePostQueryHook } from 'src/modules/note/query-hooks/note-delete-one.post-query.hook';
+import { NoteRestoreManyPostQueryHook } from 'src/modules/note/query-hooks/note-restore-many.post-query.hook';
 
 @Module({
-  providers: [NoteDeleteOnePostQueryHook],
+  providers: [NoteDeleteOnePostQueryHook, NoteRestoreManyPostQueryHook],
 })
 export class NoteQueryHookModule {}
