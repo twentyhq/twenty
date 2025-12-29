8b8857e804432de50eec95d2623451b6a5f438a6
prastoin <paul@twenty.com>
2025-12-29 08:50:02 +0100

    fix(server): flat role and object

--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type.ts
@@ -1,9 +1,8 @@
 import { type FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity.type';
 import { type ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 
-export type FlatObjectMetadata = Omit<
-  FlatEntityFrom<ObjectMetadataEntity>,
-  'fieldIds'
+export type FlatObjectMetadata = FlatEntityFrom<
+  Omit<ObjectMetadataEntity, 'targetRelationFields' | 'dataSourceId' | 'fields'>
 > & {
   fieldMetadataIds: string[];
 };
--- packages/twenty-server/src/engine/metadata-modules/flat-role/types/flat-role.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-role/types/flat-role.type.ts
@@ -1,4 +1,9 @@
 import { type FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity.type';
 import { type RoleEntity } from 'src/engine/metadata-modules/role/role.entity';
 
-export type FlatRole = FlatEntityFrom<RoleEntity>;
+export type FlatRole = FlatEntityFrom<RoleEntity> & {
+  // TOOD remove once objectPermission permissionFlag fieldPermission have been migrated to v2
+  objectPermissionIds: string[];
+  permissionFlagIds: string[];
+  fieldPermissionIds: string[];
+};
