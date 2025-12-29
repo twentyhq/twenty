8fae408e9e286cf49db599a5044b6218c34677e4
prastoin <paul@twenty.com>
2025-12-29 09:20:32 +0100

    lint

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/extract-entity-related-entity-properties.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/extract-entity-related-entity-properties.type.ts
@@ -1,8 +1,9 @@
+import { type AllMetadataName } from 'twenty-shared/metadata';
+
 import { type ExtractEntityManyToOneEntityRelationProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-many-to-one-entity-relation-properties.type';
 import { type ExtractEntityOneToManyEntityRelationProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-one-to-many-entity-relation-properties.type';
-import { MetadataEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-entity.type';
-import { AllNonSyncableEntity } from 'src/engine/workspace-manager/workspace-sync/types/all-non-syncable-entities.type';
-import { AllMetadataName } from 'twenty-shared/metadata';
+import { type MetadataEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-entity.type';
+import { type AllNonSyncableEntity } from 'src/engine/workspace-manager/workspace-sync/types/all-non-syncable-entities.type';
 
 export type ExtractEntityRelatedEntityProperties<
   T,
