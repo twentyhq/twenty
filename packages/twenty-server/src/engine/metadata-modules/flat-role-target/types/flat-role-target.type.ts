5900e05671c0c60d62b38c021b01470efab7db24
prastoin <paul@twenty.com>
2025-12-29 08:20:01 +0100

    chore

--- packages/twenty-server/src/engine/metadata-modules/flat-role-target/types/flat-role-target.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-role-target/types/flat-role-target.type.ts
@@ -1,8 +1,4 @@
 import { type FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity.type';
 import { type RoleTargetEntity } from 'src/engine/metadata-modules/role-target/role-target.entity';
 
-type NonSyncableRoleTargetEntityRelationProperties = 'apiKey';
-
-export type FlatRoleTarget = FlatEntityFrom<
-  Omit<RoleTargetEntity, NonSyncableRoleTargetEntityRelationProperties>
->;
+export type FlatRoleTarget = FlatEntityFrom<RoleTargetEntity>;
--- packages/twenty-server/src/engine/metadata-modules/flat-role/types/flat-role.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-role/types/flat-role.type.ts
@@ -1,16 +1,4 @@
 import { type FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity.type';
 import { type RoleEntity } from 'src/engine/metadata-modules/role/role.entity';
 
-type NonSyncableRoleEntityRelationProperties =
-  | 'objectPermissions'
-  | 'permissionFlags'
-  | 'fieldPermissions';
-
-export type FlatRole = FlatEntityFrom<
-  Omit<RoleEntity, NonSyncableRoleEntityRelationProperties>
-> & {
-  roleTargetIds: string[];
-  objectPermissionIds: string[];
-  permissionFlagIds: string[];
-  fieldPermissionIds: string[];
-};
+export type FlatRole = FlatEntityFrom<RoleEntity>;
