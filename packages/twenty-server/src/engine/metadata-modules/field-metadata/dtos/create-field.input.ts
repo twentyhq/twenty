e4509cdd2504f78e6e16abab7c5dd94e278641e6
prastoin <paul@twenty.com>
2025-12-30 15:49:26 +0100

    lint

--- packages/twenty-server/src/engine/metadata-modules/field-metadata/dtos/create-field.input.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/dtos/create-field.input.ts
@@ -11,7 +11,13 @@ import { FieldMetadataDTO } from 'src/engine/metadata-modules/field-metadata/dto
 @InputType()
 export class CreateFieldInput extends OmitType(
   FieldMetadataDTO,
-  ['id', 'createdAt', 'updatedAt', 'standardOverrides', 'applicationId'] as const,
+  [
+    'id',
+    'createdAt',
+    'updatedAt',
+    'standardOverrides',
+    'applicationId',
+  ] as const,
   InputType,
 ) {
   @IsUUID()
