d5d827b3bbb5efbe11131050e40f5cbb8bc6b99c
bosiraphael <raphael.bosi@gmail.com>
2025-12-30 17:07:07 +0100

    create util

--- packages/twenty-front/src/modules/page-layout/widgets/graph/graphWidgetBarChart/utils/transformTwoDimensionalGroupByToBarChartData.ts
+++ packages/twenty-front/src/modules/page-layout/widgets/graph/graphWidgetBarChart/utils/transformTwoDimensionalGroupByToBarChartData.ts
@@ -1,19 +1,16 @@
 import { type FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { type ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
-import { BAR_CHART_CONSTANTS } from '@/page-layout/widgets/graph/graphWidgetBarChart/constants/BarChartConstants';
 import { type BarChartSeries } from '@/page-layout/widgets/graph/graphWidgetBarChart/types/BarChartSeries';
+import { applyCumulativeTransformToTwoDimensionalBarChartData } from '@/page-layout/widgets/graph/graphWidgetBarChart/utils/applyCumulativeTransformToTwoDimensionalBarChartData';
+import { buildTwoDimensionalBarChartData } from '@/page-layout/widgets/graph/graphWidgetBarChart/utils/buildTwoDimensionalBarChartDataMap';
 import { sortTwoDimensionalBarChartData } from '@/page-layout/widgets/graph/graphWidgetBarChart/utils/sortTwoDimensionalBarChartData';
 import { type GroupByRawResult } from '@/page-layout/widgets/graph/types/GroupByRawResult';
 import { type RawDimensionValue } from '@/page-layout/widgets/graph/types/RawDimensionValue';
-import { applyCumulativeTransformToTwoDimensionalBarChartData } from '@/page-layout/widgets/graph/graphWidgetBarChart/utils/applyCumulativeTransformToTwoDimensionalBarChartData';
 import { getFieldKey } from '@/page-layout/widgets/graph/utils/getFieldKey';
 import { processTwoDimensionalGroupByResults } from '@/page-layout/widgets/graph/utils/processTwoDimensionalGroupByResults';
 import { type BarDatum } from '@nivo/bar';
 import { type FirstDayOfTheWeek } from 'twenty-shared/utils';
-import {
-  BarChartGroupMode,
-  type BarChartConfiguration,
-} from '~/generated/graphql';
+import { type BarChartConfiguration } from '~/generated/graphql';
 
 type TransformTwoDimensionalGroupByToBarChartDataParams = {
   rawResults: GroupByRawResult[];
@@ -68,54 +65,12 @@ export const transformTwoDimensionalGroupByToBarChartData = ({
       firstDayOfTheWeek,
     });
 
-  const dataMap = new Map<string, BarDatum>();
-  const xValues = new Set<string>();
-  const yValues = new Set<string>();
-  let hasTooManyGroups = false;
-
-  // TODO: Add a limit to the query instead of checking here (issue: twentyhq/core-team-issues#1600)
-  for (const { xValue, yValue, aggregateValue } of processedDataPoints) {
-    const isNewX = !xValues.has(xValue);
-    const isNewY = !yValues.has(yValue);
-
-    if (configuration.groupMode === BarChartGroupMode.STACKED) {
-      if (
-        isNewX &&
-        xValues.size >= BAR_CHART_CONSTANTS.MAXIMUM_NUMBER_OF_BARS
-      ) {
-        hasTooManyGroups = true;
-        continue;
-      }
-    }
-
-    if (configuration.groupMode === BarChartGroupMode.GROUPED) {
-      const totalUniqueDimensions = xValues.size * yValues.size;
-      const additionalDimensions =
-        (isNewX ? 1 : 0) * yValues.size + (isNewY ? 1 : 0) * xValues.size;
-
-      if (
-        totalUniqueDimensions + additionalDimensions >
-        BAR_CHART_CONSTANTS.MAXIMUM_NUMBER_OF_BARS
-      ) {
-        hasTooManyGroups = true;
-        continue;
-      }
-    }
-
-    xValues.add(xValue);
-    yValues.add(yValue);
-
-    if (!dataMap.has(xValue)) {
-      dataMap.set(xValue, {
-        [indexByKey]: xValue,
-      });
-    }
-
-    const dataItem = dataMap.get(xValue)!;
-    dataItem[yValue] = aggregateValue;
-  }
-
-  const unsortedData = Array.from(dataMap.values());
+  const { unsortedData, yValues, hasTooManyGroups } =
+    buildTwoDimensionalBarChartData({
+      processedDataPoints,
+      indexByKey,
+      groupMode: configuration.groupMode,
+    });
 
   const { sortedData, sortedKeys, sortedSeries } =
     sortTwoDimensionalBarChartData({
--- packages/twenty-front/src/modules/page-layout/widgets/graph/graphWidgetLineChart/utils/transformTwoDimensionalGroupByToLineChartData.ts
+++ packages/twenty-front/src/modules/page-layout/widgets/graph/graphWidgetLineChart/utils/transformTwoDimensionalGroupByToLineChartData.ts
@@ -1,13 +1,12 @@
 import { type FieldMetadataItem } from '@/object-metadata/types/FieldMetadataItem';
 import { type ObjectMetadataItem } from '@/object-metadata/types/ObjectMetadataItem';
-import { LINE_CHART_CONSTANTS } from '@/page-layout/widgets/graph/graphWidgetLineChart/constants/LineChartConstants';
-import { type LineChartDataPoint } from '@/page-layout/widgets/graph/graphWidgetLineChart/types/LineChartDataPoint';
 import { type LineChartSeries } from '@/page-layout/widgets/graph/graphWidgetLineChart/types/LineChartSeries';
+import { applyCumulativeTransformToLineChartData } from '@/page-layout/widgets/graph/graphWidgetLineChart/utils/applyCumulativeTransformToLineChartData';
+import { buildTwoDimensionalLineChartSeries } from '@/page-layout/widgets/graph/graphWidgetLineChart/utils/buildTwoDimensionalLineChartSeriesMap';
 import { sortTwoDimensionalLineChartData } from '@/page-layout/widgets/graph/graphWidgetLineChart/utils/sortTwoDimensionalLineChartData';
 import { type GraphColor } from '@/page-layout/widgets/graph/types/GraphColor';
 import { type GroupByRawResult } from '@/page-layout/widgets/graph/types/GroupByRawResult';
 import { type RawDimensionValue } from '@/page-layout/widgets/graph/types/RawDimensionValue';
-import { applyCumulativeTransformToLineChartData } from '@/page-layout/widgets/graph/graphWidgetLineChart/utils/applyCumulativeTransformToLineChartData';
 import { processTwoDimensionalGroupByResults } from '@/page-layout/widgets/graph/utils/processTwoDimensionalGroupByResults';
 import { type FirstDayOfTheWeek } from 'twenty-shared/utils';
 import { type LineChartConfiguration } from '~/generated/graphql';
@@ -57,50 +56,11 @@ export const transformTwoDimensionalGroupByToLineChartData = ({
       firstDayOfTheWeek,
     });
 
-  const seriesMap = new Map<string, Map<string, number>>();
-  const allXValues: string[] = [];
-  const xValueSet = new Set<string>();
-  let hasTooManyGroups = false;
-
-  // TODO: Add a limit to the query instead of checking here (issue: twentyhq/core-team-issues#1600)
-  for (const { xValue, yValue, aggregateValue } of processedDataPoints) {
-    const isNewX = !xValueSet.has(xValue);
-
-    if (
-      isNewX &&
-      xValueSet.size >= LINE_CHART_CONSTANTS.MAXIMUM_NUMBER_OF_DATA_POINTS
-    ) {
-      hasTooManyGroups = true;
-      continue;
-    }
-
-    if (isNewX) {
-      xValueSet.add(xValue);
-      allXValues.push(xValue);
-    }
-
-    if (!seriesMap.has(yValue)) {
-      seriesMap.set(yValue, new Map());
-    }
-
-    seriesMap.get(yValue)!.set(xValue, aggregateValue);
-  }
-
-  const unsortedSeries: LineChartSeries[] = Array.from(seriesMap.entries()).map(
-    ([seriesKey, xToYMap]) => {
-      const data: LineChartDataPoint[] = allXValues.map((xValue) => ({
-        x: xValue,
-        y: xToYMap.get(xValue) ?? 0,
-      }));
-
-      return {
-        id: seriesKey,
-        label: seriesKey,
-        color: configuration.color as GraphColor,
-        data,
-      };
-    },
-  );
+  const { unsortedSeries, hasTooManyGroups } =
+    buildTwoDimensionalLineChartSeries({
+      processedDataPoints,
+      color: configuration.color as GraphColor,
+    });
 
   const { sortedSeries } = sortTwoDimensionalLineChartData({
     series: unsortedSeries,
