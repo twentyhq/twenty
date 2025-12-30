173c3f46bca1d3f7872d43488721bd3f540126eb
Weiko <corentin@twenty.com>
2025-12-30 17:13:46 +0100

    fix

--- packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/utils/create-query-runner-context.util.ts
+++ packages/twenty-server/src/engine/api/graphql/workspace-resolver-builder/utils/create-query-runner-context.util.ts
@@ -19,6 +19,7 @@ export const createQueryRunnerContext = ({
     userWorkspace,
     authProvider,
     impersonationContext,
+    workspaceMember,
   } = request;
 
   return {
@@ -32,6 +33,7 @@ export const createQueryRunnerContext = ({
       userWorkspace,
       authProvider,
       impersonationContext,
+      workspaceMember,
     },
   };
 };
