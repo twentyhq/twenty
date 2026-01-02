7a3cf9ba5e7b4d8dd975f03349c4d295030edf02
prastoin <paul@twenty.com>
2026-01-02 18:44:59 +0100

    feat(server): new utils

---
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/utils/get-metadata-related-metadata-names.util.ts
@@ -0,0 +1,25 @@
+import { ALL_METADATA_RELATIONS } from 'src/engine/metadata-modules/flat-entity/constant/all-metadata-relations.constant';
+import { MetadataManyToOneRelatedMetadataNames } from 'src/engine/metadata-modules/flat-entity/types/metadata-many-to-one-related-metadata-names.type';
+import { MetadataOneToManyRelatedMetadataNames } from 'src/engine/metadata-modules/flat-entity/types/metadata-one-to-many-related-metadata-names.type';
+import { AllMetadataName } from 'twenty-shared/metadata';
+
+export const getMetadataRelatedMetadataNames = <T extends AllMetadataName>(
+  metadataName: T,
+) => {
+  const relationProperties = ALL_METADATA_RELATIONS[metadataName];
+
+  const manyToOneMetadataNames = Object.values(relationProperties.manyToOne)
+    .filter((relation) => relation !== null)
+    .map((relation) => relation.metadataName);
+
+  const oneToManyMetadataNames = Object.values(relationProperties.oneToMany)
+    .filter((relation) => relation !== null)
+    .map((relation) => relation.metadataName);
+
+  return [
+    ...new Set([...manyToOneMetadataNames, ...oneToManyMetadataNames]),
+  ] as (
+    | MetadataOneToManyRelatedMetadataNames<T>
+    | MetadataManyToOneRelatedMetadataNames<T>
+  )[];
+};
