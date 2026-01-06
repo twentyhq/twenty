2f2437e42f1a3eb7912e22ce88dd930eacd1c2d5
Thomas Trompette <thomas.trompette@sfr.fr>
2026-01-06 18:24:49 +0100

    Add metrics for completed and failed jobs (#16969)
    Add a counter for completed and failed jobs

--- packages/twenty-server/src/engine/core-modules/message-queue/interfaces/message-queue-module-options.interface.ts
+++ packages/twenty-server/src/engine/core-modules/message-queue/interfaces/message-queue-module-options.interface.ts
@@ -1,4 +1,5 @@
 import { type BullMQDriverOptions } from 'src/engine/core-modules/message-queue/drivers/bullmq.driver';
+import { type MetricsService } from 'src/engine/core-modules/metrics/metrics.service';
 
 export enum MessageQueueDriverType {
   BullMQ = 'bull-mq',
@@ -8,6 +9,7 @@ export enum MessageQueueDriverType {
 export interface BullMQDriverFactoryOptions {
   type: MessageQueueDriverType.BullMQ;
   options: BullMQDriverOptions;
+  metricsService: MetricsService;
 }
 
 export interface SyncDriverFactoryOptions {
