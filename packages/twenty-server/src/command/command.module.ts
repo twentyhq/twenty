148b73960fa9f038a04cd05919a447ffa83e39a1
prastoin <paul@twenty.com>
2026-01-07 19:07:43 +0100

    chore(server): remove pending migration command

--- packages/twenty-server/src/command/command.module.ts
+++ packages/twenty-server/src/command/command.module.ts
@@ -5,7 +5,6 @@ import { DatabaseCommandModule } from 'src/database/commands/database-command.mo
 import { FieldMetadataModule } from 'src/engine/metadata-modules/field-metadata/field-metadata.module';
 import { ObjectMetadataModule } from 'src/engine/metadata-modules/object-metadata/object-metadata.module';
 import { WorkspaceCleanerModule } from 'src/engine/workspace-manager/workspace-cleaner/workspace-cleaner.module';
-import { WorkspaceMigrationRunnerCommandsModule } from 'src/engine/workspace-manager/workspace-migration-runner/commands/workspace-migration-runner-commands.module';
 import { MessagingMessageCleanerModule } from 'src/modules/messaging/message-cleaner/messaging-message-cleaner.module';
 
 @Module({
@@ -13,7 +12,6 @@ import { MessagingMessageCleanerModule } from 'src/modules/messaging/message-cle
     AppModule,
     DatabaseCommandModule,
     MessagingMessageCleanerModule,
-    WorkspaceMigrationRunnerCommandsModule,
     ObjectMetadataModule,
     FieldMetadataModule,
     WorkspaceCleanerModule,
