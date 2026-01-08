a568c771168156588fc37d8fd1339a32a31d209f
martmull <martmull@hotmail.fr>
2026-01-08 14:32:57 +0100

    Fix ci

--- packages/twenty-sdk/src/application/functions/function-config.ts
+++ packages/twenty-sdk/src/application/functions/function-config.ts
@@ -3,7 +3,6 @@ import {
   type ServerlessFunctionTriggerManifest,
 } from 'twenty-shared/application';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type FunctionHandler = (...args: any[]) => any | Promise<any>;
 
 export type FunctionConfig = Omit<
