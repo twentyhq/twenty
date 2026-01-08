d653b0a168edbe5a97bb3bb2b664c09f91fc5266
nitin <142569587+ehconitin@users.noreply.github.com>
2026-01-08 14:36:44 +0530

    Change bar chart tooltips interaction from bar to slice  (#16938)
    https://github.com/user-attachments/assets/90baca7e-2c32-448d-a458-e95ced00fdf4
    
    
    https://github.com/user-attachments/assets/53dd5f8b-1c22-4600-b582-46ba594b966a
    
    
    
    
    https://github.com/user-attachments/assets/0b0b15c1-3ff8-4059-bc10-969d49852545

--- packages/twenty-front/src/modules/page-layout/widgets/graph/graphWidgetBarChart/states/graphWidgetHoveredSliceIndexComponentState.ts
+++ packages/twenty-front/src/modules/page-layout/widgets/graph/graphWidgetBarChart/states/graphWidgetHoveredSliceIndexComponentState.ts
@@ -1,13 +1,10 @@
-import { type BarChartSlice } from '@/page-layout/widgets/graph/graphWidgetBarChart/types/BarChartSlice';
 import { WidgetComponentInstanceContext } from '@/page-layout/widgets/states/contexts/WidgetComponentInstanceContext';
 import { createComponentState } from '@/ui/utilities/state/component-state/utils/createComponentState';
 
-export const graphWidgetBarTooltipComponentState = createComponentState<{
-  slice: BarChartSlice;
-  offsetLeft: number;
-  offsetTop: number;
-} | null>({
-  key: 'graphWidgetBarTooltipComponentState',
+export const graphWidgetHoveredSliceIndexComponentState = createComponentState<
+  string | null
+>({
+  key: 'graphWidgetHoveredSliceIndexComponentState',
   defaultValue: null,
   componentInstanceContext: WidgetComponentInstanceContext,
 });
