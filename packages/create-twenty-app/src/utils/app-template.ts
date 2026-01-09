503f4dec4e8a7bd8f4f46c23582b7a61c074dc9a
martmull <martmull@hotmail.fr>
2026-01-09 15:48:34 +0100

    Deploy new cli version

--- packages/create-twenty-app/src/utils/app-template.ts
+++ packages/create-twenty-app/src/utils/app-template.ts
@@ -168,7 +168,7 @@ const createPackageJson = async ({
       'lint-fix': 'eslint --fix',
     },
     dependencies: {
-      'twenty-sdk': '0.3.0-beta',
+      'twenty-sdk': '0.3.0',
     },
     devDependencies: {
       typescript: '^5.9.3',
