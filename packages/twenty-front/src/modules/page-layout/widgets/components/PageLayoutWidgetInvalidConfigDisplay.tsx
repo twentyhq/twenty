d1e70f3da9aaa6bdfa4cf0b0820be4cb2667daf2
ehconitin <nitinkoche03@gmail.com>
2026-01-10 00:03:03 +0530

    Add error boundary to widget renderer to prevent page crashes

--- packages/twenty-front/src/modules/page-layout/widgets/components/PageLayoutWidgetInvalidConfigDisplay.tsx
+++ packages/twenty-front/src/modules/page-layout/widgets/components/PageLayoutWidgetInvalidConfigDisplay.tsx
@@ -2,25 +2,22 @@ import { useCurrentWidget } from '@/page-layout/widgets/hooks/useCurrentWidget';
 import styled from '@emotion/styled';
 import { t } from '@lingui/core/macro';
 import { AppTooltip, Status } from 'twenty-ui/display';
-import { WidgetType } from '~/generated/graphql';
 
-const StyledNoDataContainer = styled.div`
+const StyledInvalidConfigContainer = styled.div`
   align-items: center;
   display: flex;
   justify-content: center;
 `;
-export const PageLayoutWidgetNoDataDisplay = () => {
-  const widget = useCurrentWidget();
-  const tooltipId = `widget-incomplete-tooltip-${widget.id}`;
 
-  const text = widget.type === WidgetType.IFRAME ? t`Invalid URL` : t`No Data`;
-  const tooltipContent =
-    widget.type === WidgetType.IFRAME
-      ? t`Invalid URL. Click edit to configure this widget.`
-      : t`No data available. Click edit to configure this widget.`;
+export const PageLayoutWidgetInvalidConfigDisplay = () => {
+  const widget = useCurrentWidget();
+  const tooltipId = `widget-invalid-config-tooltip-${widget.id}`;
+
+  const text = t`Invalid Configuration`;
+  const tooltipContent = t`Invalid configuration. Click edit to configure this widget.`;
 
   return (
-    <StyledNoDataContainer>
+    <StyledInvalidConfigContainer>
       <div id={tooltipId}>
         <Status color="red" text={text} />
       </div>
@@ -29,6 +26,6 @@ export const PageLayoutWidgetNoDataDisplay = () => {
         content={tooltipContent}
         place="top"
       />
-    </StyledNoDataContainer>
+    </StyledInvalidConfigContainer>
   );
 };
