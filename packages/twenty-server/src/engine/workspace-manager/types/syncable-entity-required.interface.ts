55a7345c186e2fb68e1b84e36d5458b0fb79fc6b
prastoin <paul@twenty.com>
2026-01-09 15:55:32 +0100

    fix

--- packages/twenty-server/src/engine/workspace-manager/types/syncable-entity-required.interface.ts
+++ packages/twenty-server/src/engine/workspace-manager/types/syncable-entity-required.interface.ts
@@ -1,7 +1,7 @@
 import { Column, Index, JoinColumn, ManyToOne, type Relation } from 'typeorm';
 
 import type { ApplicationEntity } from 'src/engine/core-modules/application/application.entity';
-import { WorkspaceRelatedEntity } from 'src/engine/workspace-manager/workspace-sync/types/workspace-related-entity';
+import { WorkspaceRelatedEntity } from 'src/engine/workspace-manager/types/workspace-related-entity';
 
 @Index(['workspaceId', 'universalIdentifier'], {
   unique: true,
