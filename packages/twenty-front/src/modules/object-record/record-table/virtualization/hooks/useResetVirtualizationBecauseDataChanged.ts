9b95d28cb346f27c82abbea569a24930634c747b
Lucas Bordeau <bordeau.lucas@gmail.com>
2026-01-12 12:41:59 +0100

    Refactored table virtualization real index state (#17074)
    This PR refactors the table virtualization real index main state that
    was creating performance problems.
    
    The problem was that we kept track of all the real rows even if we only
    print 200 at a time in the application.
    
    This caused a crash when some users with 10k+ rows on a table tried to
    iterate over this state 10.000 times or more.
    
    The trade-off is that now we keep all real indices in a Map, and each
    virtual row uses a selector to go to this Map.
    
    This way if we want to reset the full Map, we just have to overwrite the
    base state that contains the map, and the 200 selectors will recompute.
    This happens for example when we delete a row. This now has a limited
    and negligible performance impact.

--- packages/twenty-front/src/modules/object-record/record-table/virtualization/hooks/useResetVirtualizationBecauseDataChanged.ts
+++ packages/twenty-front/src/modules/object-record/record-table/virtualization/hooks/useResetVirtualizationBecauseDataChanged.ts
@@ -4,11 +4,18 @@ import { useRecordsFieldVisibleGqlFields } from '@/object-record/record-field/ho
 
 import { useFindManyRecordIndexTableParams } from '@/object-record/record-index/hooks/useFindManyRecordIndexTableParams';
 import { useTriggerFetchPages } from '@/object-record/record-table/virtualization/hooks/useTriggerFetchPages';
+import { dataLoadingStatusByRealIndexComponentState } from '@/object-record/record-table/virtualization/states/dataLoadingStatusByRealIndexComponentState';
 import { dataPagesLoadedComponentState } from '@/object-record/record-table/virtualization/states/dataPagesLoadedComponentState';
+import { lastScrollPositionComponentState } from '@/object-record/record-table/virtualization/states/lastScrollPositionComponentState';
+import { recordIdByRealIndexComponentState } from '@/object-record/record-table/virtualization/states/recordIdByRealIndexComponentState';
 import { totalNumberOfRecordsToVirtualizeComponentState } from '@/object-record/record-table/virtualization/states/totalNumberOfRecordsToVirtualizeComponentState';
+import { getVirtualizationOverscanWindow } from '@/object-record/record-table/virtualization/utils/getVirtualizationOverscanWindow';
+import { useScrollWrapperHTMLElement } from '@/ui/utilities/scroll/hooks/useScrollWrapperHTMLElement';
 import { useRecoilComponentCallbackState } from '@/ui/utilities/state/component-state/hooks/useRecoilComponentCallbackState';
+import { getSnapshotValue } from '@/ui/utilities/state/utils/getSnapshotValue';
 import { useCallback } from 'react';
 import { useRecoilCallback } from 'recoil';
+import { isDefined } from 'twenty-shared/utils';
 import { sleep } from '~/utils/sleep';
 
 export const useResetVirtualizationBecauseDataChanged = (
@@ -17,6 +24,8 @@ export const useResetVirtualizationBecauseDataChanged = (
   const { objectMetadataItem } = useObjectMetadataItem({
     objectNameSingular,
   });
+
+  const { scrollWrapperHTMLElement } = useScrollWrapperHTMLElement();
 
   const params = useFindManyRecordIndexTableParams(objectNameSingular);
 
@@ -43,18 +52,101 @@ export const useResetVirtualizationBecauseDataChanged = (
 
   const { triggerFetchPagesWithoutDebounce } = useTriggerFetchPages();
 
+  const lastScrollPositionCallbackState = useRecoilComponentCallbackState(
+    lastScrollPositionComponentState,
+  );
+
+  const recordIdByRealIndexCallbackState = useRecoilComponentCallbackState(
+    recordIdByRealIndexComponentState,
+  );
+
+  const dataLoadingStatusByRealIndexCallbackState =
+    useRecoilComponentCallbackState(dataLoadingStatusByRealIndexComponentState);
+
   const resetVirtualization = useRecoilCallback(
-    ({ set }) =>
+    ({ set, snapshot }) =>
       async () => {
         const { totalCount } = await findManyRecordsLazy();
+
+        const tableScrollWrapperHeight =
+          scrollWrapperHTMLElement?.clientHeight ?? 0;
+
+        const lastScrollPosition = getSnapshotValue(
+          snapshot,
+          lastScrollPositionCallbackState,
+        );
+
+        const totalNumberOfRecordsToVirtualize =
+          getSnapshotValue(
+            snapshot,
+            totalNumberOfRecordsToVirtualizeCallbackState,
+          ) ?? 0;
+
+        const {
+          firstRealIndexInOverscanWindow,
+          lastRealIndexInOverscanWindow,
+        } = getVirtualizationOverscanWindow(
+          lastScrollPosition,
+          tableScrollWrapperHeight,
+          totalNumberOfRecordsToVirtualize,
+        );
+
+        const recordIdByRealIndex = getSnapshotValue(
+          snapshot,
+          recordIdByRealIndexCallbackState,
+        );
+
+        const dataLoadingStatusByRealIndex = getSnapshotValue(
+          snapshot,
+          dataLoadingStatusByRealIndexCallbackState,
+        );
+
+        const lengthOfOverscanWindow =
+          lastRealIndexInOverscanWindow - firstRealIndexInOverscanWindow + 1;
+
+        const newRecordIdByRealIndex = new Map<number, string>();
+        const newDataLoadingStatusByRealIndex = new Map<
+          number,
+          'loaded' | 'not-loaded'
+        >();
+
+        for (let i = 0; i < lengthOfOverscanWindow; i++) {
+          const realIndex = firstRealIndexInOverscanWindow + i;
+
+          const existingRecordId = recordIdByRealIndex.get(realIndex);
+
+          if (isDefined(existingRecordId)) {
+            newRecordIdByRealIndex.set(realIndex, existingRecordId);
+          }
+
+          const existingDataLoadingStatus =
+            dataLoadingStatusByRealIndex.get(realIndex);
+
+          if (isDefined(existingDataLoadingStatus)) {
+            newDataLoadingStatusByRealIndex.set(
+              realIndex,
+              existingDataLoadingStatus,
+            );
+          }
+        }
+
+        set(recordIdByRealIndexCallbackState, newRecordIdByRealIndex);
+        set(
+          dataLoadingStatusByRealIndexCallbackState,
+          newDataLoadingStatusByRealIndex,
+        );
 
         set(dataPagesLoadedCallbackState, []);
         set(totalNumberOfRecordsToVirtualizeCallbackState, totalCount);
       },
     [
-      dataPagesLoadedCallbackState,
-      totalNumberOfRecordsToVirtualizeCallbackState,
       findManyRecordsLazy,
+      scrollWrapperHTMLElement?.clientHeight,
+      lastScrollPositionCallbackState,
+      totalNumberOfRecordsToVirtualizeCallbackState,
+      dataPagesLoadedCallbackState,
+      dataLoadingStatusByRealIndexCallbackState,
+      recordIdByRealIndexCallbackState,
     ],
   );
 
