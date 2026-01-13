af4e334e4b465aa4dbef9b7300a9507e5771dd00
Abdul Rahman <ar5438376@gmail.com>
2026-01-14 03:20:07 +0530

    refactor: improve side panel closing logic for record deactivation
    - Enhanced the handling of side panel closing events by introducing a check for the command menu's open and closing states.
    - Utilized Recoil's snapshot to conditionally deactivate the record table row only when the command menu is not active, improving the overall user experience and preventing unintended deactivations.
    - Refactored the side panel closing listener to use a callback for better state management.

--- packages/twenty-front/src/modules/object-record/record-table/components/RecordTableDeactivateRecordTableRowEffect.tsx
+++ packages/twenty-front/src/modules/object-record/record-table/components/RecordTableDeactivateRecordTableRowEffect.tsx
@@ -1,12 +1,32 @@
+import { isCommandMenuClosingState } from '@/command-menu/states/isCommandMenuClosingState';
+import { isCommandMenuOpenedState } from '@/command-menu/states/isCommandMenuOpenedState';
 import { useActiveRecordTableRow } from '@/object-record/record-table/hooks/useActiveRecordTableRow';
 import { useListenToSidePanelClosing } from '@/ui/layout/right-drawer/hooks/useListenToSidePanelClosing';
+import { useRecoilCallback } from 'recoil';
 
 export const RecordTableDeactivateRecordTableRowEffect = () => {
   const { deactivateRecordTableRow } = useActiveRecordTableRow();
 
-  useListenToSidePanelClosing(() => {
-    deactivateRecordTableRow();
-  });
+  const handleSidePanelClosing = useRecoilCallback(
+    ({ snapshot }) =>
+      () => {
+        const isCommandMenuOpened = snapshot
+          .getLoadable(isCommandMenuOpenedState)
+          .getValue();
+        const isCommandMenuClosing = snapshot
+          .getLoadable(isCommandMenuClosingState)
+          .getValue();
+
+        if (isCommandMenuOpened || isCommandMenuClosing) {
+          return;
+        }
+
+        deactivateRecordTableRow();
+      },
+    [deactivateRecordTableRow],
+  );
+
+  useListenToSidePanelClosing(handleSidePanelClosing);
 
   return null;
 };
