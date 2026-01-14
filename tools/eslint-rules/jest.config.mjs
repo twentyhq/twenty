775d956850289d7b49f6aace32917c4621ee046a
Félix Malfait <felix@twenty.com>
2026-01-14 13:25:46 +0100

    fix: keep tools/eslint-rules at original location
    @nx/eslint-plugin requires workspace rules to be in tools/eslint-rules.
    Moving eslint.config.react.mjs to tools/eslint-rules instead.
    Also removed jest.config.mjs from root (use npx nx test instead).

--- tools/eslint-rules/jest.config.mjs
+++ tools/eslint-rules/jest.config.mjs
@@ -1,11 +1,11 @@
 
 export default {
-  displayName: 'twenty-eslint-rules',
+  displayName: 'eslint-rules',
   silent: false,
   preset: '@nx/jest/preset',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/twenty-eslint-rules',
+  coverageDirectory: '../../coverage/tools/eslint-rules',
 };
