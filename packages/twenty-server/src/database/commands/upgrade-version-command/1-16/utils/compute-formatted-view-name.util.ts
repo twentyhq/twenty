1f50a8fe2b7a464bc6c76a36246847b31a2b3c1a
prastoin <paul@twenty.com>
2026-01-14 10:11:27 +0100

    lint

--- packages/twenty-server/src/database/commands/upgrade-version-command/1-16/utils/compute-formatted-view-name.util.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/1-16/utils/compute-formatted-view-name.util.ts
@@ -1,6 +1,6 @@
 import { capitalize, uncapitalize } from 'twenty-shared/utils';
 
-import { FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
+import { type FlatObjectMetadata } from 'src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type';
 
 export const ALL_ENTITY_VIEW_NAME = 'All {objectLabelPlural}';
 
