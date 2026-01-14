c0cf95d9fdf07bf54b86ea76a4206b100b39409b
neo773 <neo773@protonmail.com>
2026-01-14 06:43:48 +0530

    seerbot

--- packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/utils/is-imap-network-error.util.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/utils/is-imap-network-error.util.ts
@@ -1,34 +1,51 @@
 import { isDefined } from 'twenty-shared/utils';
 
-const TIMEOUT_ERROR_CODES = [
+import { MessageNetworkExceptionCode } from 'src/modules/messaging/message-import-manager/drivers/exceptions/message-network.exception';
+
+const IMAPFLOW_TIMEOUT_ERROR_CODES = [
   'ETIMEOUT',
   'UPGRADE_TIMEOUT',
   'CONNECT_TIMEOUT',
   'GREETING_TIMEOUT',
 ];
 
-const CONNECTION_ERROR_CODES = [
+const IMAPFLOW_CONNECTION_ERROR_CODES = [
   'NoConnection',
   'EConnectionClosed',
   'ProxyError',
   'ClosedAfterConnectTLS',
   'ClosedAfterConnectText',
-  'ECONNREFUSED',
+];
+
+const NODEJS_NETWORK_ERROR_CODES = [
+  MessageNetworkExceptionCode.ECONNREFUSED,
+  MessageNetworkExceptionCode.ECONNRESET,
+  MessageNetworkExceptionCode.ENOTFOUND,
+  MessageNetworkExceptionCode.ECONNABORTED,
+  MessageNetworkExceptionCode.ETIMEDOUT,
+  MessageNetworkExceptionCode.ERR_NETWORK,
+  MessageNetworkExceptionCode.EHOSTUNREACH,
 ];
 
 export const isImapNetworkError = (error: Error): boolean => {
   const errorWithCode = error as { code?: string };
 
-  if (
-    isDefined(errorWithCode.code) &&
-    TIMEOUT_ERROR_CODES.includes(errorWithCode.code)
-  ) {
+  if (!isDefined(errorWithCode.code)) {
+    return false;
+  }
+
+  if (IMAPFLOW_TIMEOUT_ERROR_CODES.includes(errorWithCode.code)) {
+    return true;
+  }
+
+  if (IMAPFLOW_CONNECTION_ERROR_CODES.includes(errorWithCode.code)) {
     return true;
   }
 
   if (
-    isDefined(errorWithCode.code) &&
-    CONNECTION_ERROR_CODES.includes(errorWithCode.code)
+    NODEJS_NETWORK_ERROR_CODES.includes(
+      errorWithCode.code as MessageNetworkExceptionCode,
+    )
   ) {
     return true;
   }
