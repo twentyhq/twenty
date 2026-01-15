14d583cd9d342a4e6f60d7f2fab0e88e7a7c64f4
Félix Malfait <felix@twenty.com>
2026-01-15 18:14:09 +0100

    fix: correct indentation in test utility imports

--- packages/twenty-server/test/integration/metadata/suites/serverless-function/utils/delete-serverless-function.util.ts
+++ packages/twenty-server/test/integration/metadata/suites/serverless-function/utils/delete-serverless-function.util.ts
@@ -1,6 +1,6 @@
 import {
-    type DeleteServerlessFunctionFactoryInput,
-    deleteServerlessFunctionQueryFactory,
+  type DeleteServerlessFunctionFactoryInput,
+  deleteServerlessFunctionQueryFactory,
 } from 'test/integration/metadata/suites/serverless-function/utils/delete-serverless-function-query-factory.util';
 import { makeMetadataAPIRequest } from 'test/integration/metadata/suites/utils/make-metadata-api-request.util';
 import { type CommonResponseBody } from 'test/integration/metadata/types/common-response-body.type';
--- packages/twenty-server/test/integration/metadata/suites/serverless-function/utils/publish-serverless-function.util.ts
+++ packages/twenty-server/test/integration/metadata/suites/serverless-function/utils/publish-serverless-function.util.ts
@@ -1,6 +1,6 @@
 import {
-    type PublishServerlessFunctionFactoryInput,
-    publishServerlessFunctionQueryFactory,
+  type PublishServerlessFunctionFactoryInput,
+  publishServerlessFunctionQueryFactory,
 } from 'test/integration/metadata/suites/serverless-function/utils/publish-serverless-function-query-factory.util';
 import { makeMetadataAPIRequest } from 'test/integration/metadata/suites/utils/make-metadata-api-request.util';
 import { type CommonResponseBody } from 'test/integration/metadata/types/common-response-body.type';
