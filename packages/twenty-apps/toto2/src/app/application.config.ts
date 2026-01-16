b3ab747805a8384f4a3b2f501bcb4144c5a4a57b
martmull <martmull@hotmail.fr>
2026-01-16 22:24:32 +0100

    Remove toto app

--- packages/twenty-apps/toto2/src/app/application.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineApp } from 'twenty-sdk';
-import { DEFAULT_FUNCTION_ROLE_UNIVERSAL_IDENTIFIER } from './default-function.role';
-
-export default defineApp({
-  universalIdentifier: 'b13892d9-f951-47ef-890d-aa3bd1bdc4b8',
-  displayName: 'Toto 2',
-  description: '',
-  functionRoleUniversalIdentifier: DEFAULT_FUNCTION_ROLE_UNIVERSAL_IDENTIFIER,
-});
