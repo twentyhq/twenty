6a959baa61c821b3180eec885b28ac753becca96
Devessier <baptiste@devessier.fr>
2026-01-16 15:50:16 +0100

    fix: remove padding only for rpl

--- packages/twenty-front/src/modules/object-record/record-field-list/record-detail-section/components/RecordDetailRecordsListItemContainer.tsx
+++ packages/twenty-front/src/modules/object-record/record-field-list/record-detail-section/components/RecordDetailRecordsListItemContainer.tsx
@@ -1,11 +1,47 @@
+import { useLayoutRenderingContext } from '@/ui/layout/contexts/LayoutRenderingContext';
+import isPropValid from '@emotion/is-prop-valid';
 import styled from '@emotion/styled';
+import { PageLayoutType } from '~/generated/graphql';
 
-const StyledListItem = styled.div`
+const StyledListItem = styled('div', {
+  shouldForwardProp: (prop) =>
+    isPropValid(prop) && prop !== 'noHorizontalPadding',
+})<{ noHorizontalPadding?: boolean }>`
   align-items: center;
   justify-content: space-between;
   gap: ${({ theme }) => theme.spacing(1)};
   display: flex;
   height: ${({ theme }) => theme.spacing(10)};
+  padding-left: ${({ theme, noHorizontalPadding }) =>
+    noHorizontalPadding ? 0 : theme.spacing(3)};
+  padding-right: ${({ theme, noHorizontalPadding }) =>
+    noHorizontalPadding ? 0 : theme.spacing(2)};
 `;
 
-export { StyledListItem as RecordDetailRecordsListItemContainer };
+type RecordDetailRecordsListItemContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+/**
+ * TODO: Remove noHorizontalPadding logic once the traditional record show page is removed.
+ */
+export const RecordDetailRecordsListItemContainer = ({
+  children,
+  className,
+}: RecordDetailRecordsListItemContainerProps) => {
+  const layoutRenderingContext = useLayoutRenderingContext();
+
+  const isInRecordPageLayout =
+    layoutRenderingContext?.layoutType === PageLayoutType.RECORD_PAGE &&
+    !layoutRenderingContext?.isLegacyRecordShowPage;
+
+  return (
+    <StyledListItem
+      className={className}
+      noHorizontalPadding={isInRecordPageLayout}
+    >
+      {children}
+    </StyledListItem>
+  );
+};
