15ec1e1079fa8bec285b219c8785feeb15064dee
prastoin <paul@twenty.com>
2026-01-18 16:36:10 +0100

    lint

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1768750308557-foreignKeyIndexStandardization.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1768750308557-foreignKeyIndexStandardization.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { type MigrationInterface, type QueryRunner } from 'typeorm';
 
 export class ForeignKeyIndexStandardization1768750308557
   implements MigrationInterface
