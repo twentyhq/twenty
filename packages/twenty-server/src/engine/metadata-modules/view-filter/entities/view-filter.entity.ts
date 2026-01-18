905708f10546c554a10e3828fd3c93bc99e9d934
prastoin <paul@twenty.com>
2026-01-18 15:45:39 +0100

    feat(server): improve indexing coverage

--- packages/twenty-server/src/engine/metadata-modules/view-filter/entities/view-filter.entity.ts
+++ packages/twenty-server/src/engine/metadata-modules/view-filter/entities/view-filter.entity.ts
@@ -20,9 +20,7 @@ import { SyncableEntityRequired } from 'src/engine/workspace-manager/types/synca
 
 @Entity({ name: 'viewFilter', schema: 'core' })
 @Index('IDX_VIEW_FILTER_WORKSPACE_ID_VIEW_ID', ['workspaceId', 'viewId'])
-@Index('IDX_VIEW_FILTER_VIEW_ID', ['viewId'], {
-  where: '"deletedAt" IS NULL',
-})
+@Index('IDX_VIEW_FILTER_VIEW_ID', ['viewId'])
 @Index('IDX_VIEW_FILTER_FIELD_METADATA_ID', ['fieldMetadataId'])
 export class ViewFilterEntity
   extends SyncableEntityRequired
