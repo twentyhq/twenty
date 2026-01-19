4f8ef7a180a9bc7a89e1bf16d13ad644915dcb7c
Devessier <baptiste@devessier.fr>
2026-01-19 10:17:13 +0100

    fix: use light button component

--- packages/twenty-front/src/modules/page-layout/widgets/field/components/FieldWidgetShowMoreButton.tsx
+++ packages/twenty-front/src/modules/page-layout/widgets/field/components/FieldWidgetShowMoreButton.tsx
@@ -1,28 +1,7 @@
-import { useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
 import { t } from '@lingui/core/macro';
 
 import { IconChevronDown } from 'twenty-ui/display';
-
-const StyledShowMoreButton = styled.div`
-  align-items: center;
-  color: ${({ theme }) => theme.font.color.tertiary};
-  cursor: pointer;
-  display: flex;
-  gap: ${({ theme }) => theme.spacing(1)};
-  height: 24px;
-  box-sizing: border-box;
-  border-radius: ${({ theme }) => theme.border.radius.sm};
-
-  &:hover {
-    background: ${({ theme }) => theme.background.transparent.lighter};
-  }
-`;
-
-const StyledShowMoreText = styled.span`
-  font-size: ${({ theme }) => theme.font.size.md};
-  font-weight: ${({ theme }) => theme.font.weight.regular};
-`;
+import { LightButton } from 'twenty-ui/input';
 
 type FieldWidgetShowMoreButtonProps = {
   remainingCount: number;
@@ -33,19 +12,12 @@ export const FieldWidgetShowMoreButton = ({
   remainingCount,
   onClick,
 }: FieldWidgetShowMoreButtonProps) => {
-  const theme = useTheme();
-
   return (
-    <StyledShowMoreButton
+    <LightButton
+      title={t`More (${remainingCount})`}
+      Icon={IconChevronDown}
       onClick={onClick}
-      data-testid="field-widget-show-more-button"
-    >
-      <IconChevronDown
-        size={theme.icon.size.md}
-        stroke={theme.icon.stroke.sm}
-        color={theme.font.color.tertiary}
-      />
-      <StyledShowMoreText>{t`More (${remainingCount})`}</StyledShowMoreText>
-    </StyledShowMoreButton>
+      accent="tertiary"
+    />
   );
 };
