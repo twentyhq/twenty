ae51b87bbcb9d7306f317f150bbd3c2e0f5fb037
Abdul Rahman <ar5438376@gmail.com>
2026-01-19 23:47:06 +0530

    fix: reorder imports and add QueryNavigationMenuItemArgs type in graphql.ts
    - Moved the Apollo import statement to improve organization.
    - Introduced a new type definition for QueryNavigationMenuItemArgs to enhance GraphQL query capabilities.
    - Minor formatting adjustments for consistency with existing code.

--- packages/twenty-front/src/generated-metadata/graphql.ts
+++ packages/twenty-front/src/generated-metadata/graphql.ts
@@ -1,5 +1,5 @@
-import * as Apollo from '@apollo/client';
 import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -3960,6 +3960,11 @@ export type QueryIndexMetadatasArgs = {
 
 export type QueryLineChartDataArgs = {
   input: LineChartDataInput;
+};
+
+
+export type QueryNavigationMenuItemArgs = {
+  id: Scalars['UUID'];
 };
 
 
