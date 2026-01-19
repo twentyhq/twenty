583f1eea108c7f7f75b803faaa671f08284a5685
Charles Bochet <charles@twenty.com>
2026-01-19 16:56:00 +0100

    Fix tests

--- packages/twenty-sdk/src/cli/utilities/manifest/utils/__tests__/build-manifest.spec.ts
+++ packages/twenty-sdk/src/cli/utilities/manifest/utils/__tests__/build-manifest.spec.ts
@@ -1,12 +1,12 @@
 import { DEFAULT_FUNCTION_ROLE_UNIVERSAL_IDENTIFIER } from '@/cli/__tests__/test-app/src/app/default-function.role';
 import {
-    POST_CARD_EXTENSION_CATEGORY_FIELD_ID,
-    POST_CARD_EXTENSION_PRIORITY_FIELD_ID,
+  POST_CARD_EXTENSION_CATEGORY_FIELD_ID,
+  POST_CARD_EXTENSION_PRIORITY_FIELD_ID,
 } from '@/cli/__tests__/test-app/src/app/postCard.object-extension';
 import { buildManifest, type BuildManifestResult } from '@/cli/utilities/manifest/utils/manifest-build';
 import { join } from 'path';
 
-const TEST_APP_PATH = join(__dirname, '../../__tests__/test-app');
+const TEST_APP_PATH = join(__dirname, '../../../../__tests__/test-app');
 
 describe('buildManifest with test-app', () => {
   let manifest: BuildManifestResult['manifest'];
