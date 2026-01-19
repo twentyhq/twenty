bf7e682a38ca0c8fc00a262f712ae5856719895e
Abdul Rahman <ar5438376@gmail.com>
2026-01-19 12:56:31 +0530

    Add migration for NavigationMenuItem entity
    - Created a new migration to add the `navigationMenuItem` table in the core schema.
    - Defined columns, primary key, and foreign key constraints.
    - Added necessary indexes for improved query performance.

---
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1768807499350-addNavigationMenuItemEntity.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddNavigationMenuItemEntity1768807499350 implements MigrationInterface {
+    name = 'AddNavigationMenuItemEntity1768807499350'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "core"."navigationMenuItem" ("workspaceId" uuid NOT NULL, "universalIdentifier" uuid NOT NULL, "applicationId" uuid NOT NULL, "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "forWorkspaceMemberId" uuid, "targetRecordId" uuid NOT NULL, "targetObjectMetadataId" uuid NOT NULL, "favoriteFolderId" uuid, "position" integer NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_d8689756f55769faea7dc0ae968" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_4d8beaebdfcd5d82ebe6e8b58f" ON "core"."navigationMenuItem" ("workspaceId", "universalIdentifier") `);
+        await queryRunner.query(`CREATE INDEX "IDX_NAVIGATION_MENU_ITEM_FAVORITE_FOLDER_ID_WORKSPACE_ID" ON "core"."navigationMenuItem" ("favoriteFolderId", "workspaceId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_NAVIGATION_MENU_ITEM_TARGET_RECORD_OBJECT_METADATA_WORKSPACE_ID" ON "core"."navigationMenuItem" ("targetRecordId", "targetObjectMetadataId", "workspaceId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_NAVIGATION_MENU_ITEM_FOR_WORKSPACE_MEMBER_ID_WORKSPACE_ID" ON "core"."navigationMenuItem" ("forWorkspaceMemberId", "workspaceId") `);
+        await queryRunner.query(`ALTER TABLE "core"."navigationMenuItem" ADD CONSTRAINT "FK_03c63a0b00ddc3ade21ed0b1a80" FOREIGN KEY ("workspaceId") REFERENCES "core"."workspace"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "core"."navigationMenuItem" ADD CONSTRAINT "FK_6fd84a774fe4ea4daa9aeeee5ed" FOREIGN KEY ("applicationId") REFERENCES "core"."application"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "core"."navigationMenuItem" ADD CONSTRAINT "FK_62d47d14b50b67a03f832481de7" FOREIGN KEY ("targetObjectMetadataId") REFERENCES "core"."objectMetadata"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "core"."navigationMenuItem" DROP CONSTRAINT "FK_62d47d14b50b67a03f832481de7"`);
+        await queryRunner.query(`ALTER TABLE "core"."navigationMenuItem" DROP CONSTRAINT "FK_6fd84a774fe4ea4daa9aeeee5ed"`);
+        await queryRunner.query(`ALTER TABLE "core"."navigationMenuItem" DROP CONSTRAINT "FK_03c63a0b00ddc3ade21ed0b1a80"`);
+        await queryRunner.query(`DROP INDEX "core"."IDX_NAVIGATION_MENU_ITEM_FOR_WORKSPACE_MEMBER_ID_WORKSPACE_ID"`);
+        await queryRunner.query(`DROP INDEX "core"."IDX_NAVIGATION_MENU_ITEM_TARGET_RECORD_OBJECT_METADATA_WORKSPACE_ID"`);
+        await queryRunner.query(`DROP INDEX "core"."IDX_NAVIGATION_MENU_ITEM_FAVORITE_FOLDER_ID_WORKSPACE_ID"`);
+        await queryRunner.query(`DROP INDEX "core"."IDX_4d8beaebdfcd5d82ebe6e8b58f"`);
+        await queryRunner.query(`DROP TABLE "core"."navigationMenuItem"`);
+    }
+
+}
