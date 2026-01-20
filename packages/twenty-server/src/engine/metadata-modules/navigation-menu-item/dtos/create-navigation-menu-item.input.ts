21f6c8943bd96ed1faea632843336ec323402ed7
Abdul Rahman <ar5438376@gmail.com>
2026-01-21 01:59:31 +0530

    refactor: remove unnecessary validation for name property in navigation menu item DTO
    - Removed the `@IsNotEmpty()` validator from the `CreateNavigationMenuItemInput` DTO, streamlining the validation process for the optional `name` property.
    - This change enhances the flexibility of the input while maintaining data integrity across navigation menu item creation.

--- packages/twenty-server/src/engine/metadata-modules/navigation-menu-item/dtos/create-navigation-menu-item.input.ts
+++ packages/twenty-server/src/engine/metadata-modules/navigation-menu-item/dtos/create-navigation-menu-item.input.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
 
-import { IsInt, IsNotEmpty, IsOptional, IsString, IsUUID, Min } from 'class-validator';
+import { IsInt, IsOptional, IsString, IsUUID, Min } from 'class-validator';
 
 import { UUIDScalarType } from 'src/engine/api/graphql/workspace-schema-builder/graphql-types/scalars';
 
