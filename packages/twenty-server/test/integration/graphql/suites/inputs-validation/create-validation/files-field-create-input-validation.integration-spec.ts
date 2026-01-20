132a7975d7a4e008c7ae6ea71d9deb83e092bed2
Etienne <45695613+etiennejouan@users.noreply.github.com>
2026-01-20 17:53:27 +0100

    fix

--- packages/twenty-server/test/integration/graphql/suites/inputs-validation/create-validation/files-field-create-input-validation.integration-spec.ts
+++ packages/twenty-server/test/integration/graphql/suites/inputs-validation/create-validation/files-field-create-input-validation.integration-spec.ts
@@ -108,6 +108,7 @@ describe(`Create input validation - ${FIELD_METADATA_TYPE}`, () => {
           objectMetadataSingularName,
           input,
           validateInput,
+          true,
         );
       },
     );
