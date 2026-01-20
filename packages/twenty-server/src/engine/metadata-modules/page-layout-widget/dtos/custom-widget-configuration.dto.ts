ab3628a0c6335716bd6c393c423ddc6d95b15c44
bosiraphael <raphael.bosi@gmail.com>
2026-01-20 13:39:10 +0100

    update custom widget configuration

--- packages/twenty-server/src/engine/metadata-modules/page-layout-widget/dtos/custom-widget-configuration.dto.ts
+++ packages/twenty-server/src/engine/metadata-modules/page-layout-widget/dtos/custom-widget-configuration.dto.ts
@@ -1,7 +1,8 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 
-import { IsIn, IsNotEmpty } from 'class-validator';
+import { IsIn, IsNotEmpty, IsUUID } from 'class-validator';
 
+import { UUIDScalarType } from 'src/engine/api/graphql/workspace-schema-builder/graphql-types/scalars';
 import { WidgetConfigurationType } from 'src/engine/metadata-modules/page-layout-widget/enums/widget-configuration-type.type';
 import { PageLayoutWidgetConfigurationBase } from 'src/engine/metadata-modules/page-layout-widget/types/page-layout-widget-configurationt-base.type';
 
@@ -13,4 +14,9 @@ export class CustomWidgetConfigurationDTO
   @IsIn([WidgetConfigurationType.CUSTOM_WIDGET])
   @IsNotEmpty()
   configurationType: WidgetConfigurationType.CUSTOM_WIDGET;
+
+  @Field(() => UUIDScalarType)
+  @IsNotEmpty()
+  @IsUUID()
+  frontComponentId: string;
 }
