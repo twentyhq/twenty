578988eaa69839e7fcbfff154cde6da8e4dc33cc
prastoin <paul@twenty.com>
2026-01-21 18:16:01 +0100

    feat(server): wip cache invalidation and rollback

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-runner/services/workspace-migration-runner.service.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-runner/services/workspace-migration-runner.service.ts
@@ -11,14 +11,15 @@ import { LoggerService } from 'src/engine/core-modules/logger/logger.service';
 import { WorkspaceManyOrAllFlatEntityMapsCacheService } from 'src/engine/metadata-modules/flat-entity/services/workspace-many-or-all-flat-entity-maps-cache.service';
 import { AllFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entity-maps.type';
 import { getMetadataFlatEntityMapsKey } from 'src/engine/metadata-modules/flat-entity/utils/get-metadata-flat-entity-maps-key.util';
-import { getMetadataNameFromFlatEntityMapsKey } from 'src/engine/metadata-modules/flat-entity/utils/get-metadata-name-from-flat-entity-maps-key.util';
 import { getMetadataRelatedMetadataNames } from 'src/engine/metadata-modules/flat-entity/utils/get-metadata-related-metadata-names.util';
 import { FIND_ALL_CORE_VIEWS_GRAPHQL_OPERATION } from 'src/engine/metadata-modules/view/constants/find-all-core-views-graphql-operation.constant';
 import { WorkspaceMetadataVersionService } from 'src/engine/metadata-modules/workspace-metadata-version/services/workspace-metadata-version.service';
 import { WorkspaceCacheStorageService } from 'src/engine/workspace-cache-storage/workspace-cache-storage.service';
 import { WorkspaceCacheService } from 'src/engine/workspace-cache/services/workspace-cache.service';
 import { WorkspaceMigration } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/workspace-migration';
+import { WorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/workspace-migration-action-common';
 import { WorkspaceMigrationRunnerActionHandlerRegistryService } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-runner/registry/workspace-migration-runner-action-handler-registry.service';
+import { AllMetadataName } from 'twenty-shared/metadata';
 
 @Injectable()
 export class WorkspaceMigrationRunnerService {
@@ -32,6 +33,66 @@ export class WorkspaceMigrationRunnerService {
     private readonly workspaceCacheService: WorkspaceCacheService,
     private readonly logger: LoggerService,
   ) {}
+
+  private async invalidateCachePostExecution({
+    impactedMetadataNames,
+    workspaceId,
+    actions,
+  }: {
+    impactedMetadataNames: AllMetadataName[];
+    workspaceId: string;
+    actions: WorkspaceMigrationAction[];
+  }): Promise<void> {
+    const metadataAndRelatedMetadataNames = [
+      ...new Set([
+        ...impactedMetadataNames,
+        ...impactedMetadataNames.flatMap(getMetadataRelatedMetadataNames),
+      ]),
+    ];
+    const flatEntitiesCacheToInvalidate = metadataAndRelatedMetadataNames.map(
+      getMetadataFlatEntityMapsKey,
+    );
+
+    this.logger.time(
+      'Runner',
+      `Cache invalidation ${flatEntitiesCacheToInvalidate.join()}`,
+    );
+
+    await this.flatEntityMapsCacheService.invalidateFlatEntityMaps({
+      workspaceId,
+      flatMapsKeys: flatEntitiesCacheToInvalidate,
+    });
+
+    const invalidationResults = await Promise.allSettled(
+      this.getLegacyCacheInvalidationPromises({
+        workspaceMigration: {
+          actions,
+          workspaceId,
+        },
+      }),
+    );
+
+    const invalidationFailures = invalidationResults.filter(
+      (result) => result.status === 'rejected',
+    );
+
+    if (invalidationFailures.length > 0) {
+      invalidationFailures.forEach((err) =>
+        this.logger.error(
+          `Failed to invalidate a legacy cache ${err.reason}`,
+          'Runner',
+        ),
+      );
+      throw new Error(
+        `Failed to invalidate ${invalidationFailures.length} cache operations`,
+      );
+    }
+
+    this.logger.timeEnd(
+      'Runner',
+      `Cache invalidation ${flatEntitiesCacheToInvalidate.join()}`,
+    );
+  }
 
   private getLegacyCacheInvalidationPromises({
     workspaceMigration: { actions, workspaceId },
@@ -128,11 +189,12 @@ export class WorkspaceMigrationRunnerService {
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
-    let flatEntityMapsToInvalidate: (keyof AllFlatEntityMaps)[] = [];
+    let impactedMetadataNames: AllMetadataName[] = [];
 
+    const successfullyExecutedActions: WorkspaceMigrationAction[] = [];
     try {
       for (const action of actions) {
-        const partialOptimisticCache =
+        const result =
           await this.workspaceMigrationRunnerActionHandlerRegistry.executeActionHandler(
             {
               action,
@@ -144,105 +206,64 @@ export class WorkspaceMigrationRunnerService {
               },
             },
           );
-        const optimisticallyUpdatedFlatEntityMapsKeys = Object.keys(
-          partialOptimisticCache,
-        ) as (keyof AllFlatEntityMaps)[];
 
-        flatEntityMapsToInvalidate = [
-          ...new Set([
-            ...optimisticallyUpdatedFlatEntityMapsKeys,
-            ...flatEntityMapsToInvalidate,
-          ]),
-        ];
+        if (result.status === 'failure') {
+          if (queryRunner.isTransactionActive) {
+            // TODO before merge shouldn't we throw ?
+            await queryRunner
+              .rollbackTransaction()
+              .catch((error) =>
+                console.trace(
+                  `Failed to rollback transaction: ${error.message}`,
+                ),
+              );
+          }
+
+          const invertedActions = successfullyExecutedActions.reverse();
+
+          for (const invertedAction of invertedActions) {
+            await this.workspaceMigrationRunnerActionHandlerRegistry.executeActionRollbackHandler(
+              {
+                action: invertedAction,
+                context: {
+                  action: invertedAction,
+                  allFlatEntityMaps: allFlatEntityMaps,
+                  queryRunner,
+                  workspaceId,
+                },
+              },
+            );
+          }
+
+          return; // TODO before merge prastoin return failure record
+        }
+
+        const { partialOptimisticCache } = result;
 
         allFlatEntityMaps = {
           ...allFlatEntityMaps,
           ...partialOptimisticCache,
         };
+
+        impactedMetadataNames = [
+          ...new Set([action.metadataName, ...impactedMetadataNames]),
+        ];
       }
 
       await queryRunner.commitTransaction();
 
       this.logger.timeEnd('Runner', 'Transaction execution');
 
-      const flatEntitiesCacheToInvalidate = [
-        ...new Set([
-          ...flatEntityMapsToInvalidate,
-          ...flatEntityMapsToInvalidate
-            .map(getMetadataNameFromFlatEntityMapsKey)
-            .flatMap(getMetadataRelatedMetadataNames)
-            .map(getMetadataFlatEntityMapsKey),
-        ]),
-      ];
-
-      this.logger.time(
-        'Runner',
-        `Cache invalidation ${flatEntitiesCacheToInvalidate.join()}`,
-      );
-
-      await this.flatEntityMapsCacheService.invalidateFlatEntityMaps({
+      await this.invalidateCachePostExecution({
+        impactedMetadataNames,
         workspaceId,
-        flatMapsKeys: flatEntitiesCacheToInvalidate,
+        actions,
       });
 
-      const invalidationResults = await Promise.allSettled(
-        this.getLegacyCacheInvalidationPromises({
-          workspaceMigration: {
-            actions,
-            workspaceId,
-          },
-        }),
-      );
-
-      const invalidationFailures = invalidationResults.filter(
-        (result) => result.status === 'rejected',
-      );
-
-      if (invalidationFailures.length > 0) {
-        invalidationFailures.forEach((err) =>
-          this.logger.error(
-            `Failed to invalidate a legacy cache ${err.reason}`,
-            'Runner',
-          ),
-        );
-        throw new Error(
-          `Failed to invalidate ${invalidationFailures.length} cache operations`,
-        );
-      }
-
-      this.logger.timeEnd(
-        'Runner',
-        `Cache invalidation ${flatEntitiesCacheToInvalidate.join()}`,
-      );
       this.logger.timeEnd('Runner', 'Total execution');
 
       return allFlatEntityMaps;
     } catch (error) {
-      if (queryRunner.isTransactionActive) {
-        try {
-          await queryRunner.rollbackTransaction();
-        } catch (error) {
-          // eslint-disable-next-line no-console
-          console.trace(`Failed to rollback transaction: ${error.message}`);
-        }
-      }
-
-      const invertedActions = actions.reverse();
-
-      for (const invertedAction of invertedActions) {
-        await this.workspaceMigrationRunnerActionHandlerRegistry.executeActionRollbackHandler(
-          {
-            action: invertedAction,
-            context: {
-              action: invertedAction,
-              allFlatEntityMaps: allFlatEntityMaps,
-              queryRunner,
-              workspaceId,
-            },
-          },
-        );
-      }
-
       throw new WorkspaceQueryRunnerException(
         error.message,
         WorkspaceQueryRunnerExceptionCode.INTERNAL_SERVER_ERROR,
