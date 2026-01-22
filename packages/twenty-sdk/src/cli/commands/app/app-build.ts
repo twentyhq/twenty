4830cfcb6d3bea79298525c5719df66dfe6ee5a1
Charles Bochet <charles@twenty.com>
2026-01-22 14:00:45 +0100

    Fix

--- packages/twenty-sdk/src/cli/commands/app/app-build.ts
+++ packages/twenty-sdk/src/cli/commands/app/app-build.ts
@@ -3,6 +3,7 @@ import { createLogger } from '@/cli/utilities/build/common/logger';
 import { FrontComponentsWatcher } from '@/cli/utilities/build/front-components/front-component-watcher';
 import { FunctionsWatcher } from '@/cli/utilities/build/functions/function-watcher';
 import { runManifestBuild, type ManifestBuildResult } from '@/cli/utilities/build/manifest/manifest-build';
+import { manifestExtractFromFileServer } from '@/cli/utilities/build/manifest/manifest-extract-from-file-server';
 import { CURRENT_EXECUTION_DIRECTORY } from '@/cli/utilities/config/constants/current-execution-directory';
 
 const initLogger = createLogger('init');
@@ -45,6 +46,8 @@ export class AppBuildCommand {
     await this.buildFunctions(buildResult);
     await this.buildFrontComponents(buildResult);
 
+    await this.cleanup();
+
     return buildResult;
   }
 
@@ -66,5 +69,11 @@ export class AppBuildCommand {
     });
 
     await this.frontComponentsBuilder.start();
+  }
+
+  private async cleanup(): Promise<void> {
+    await this.functionsBuilder?.close();
+    await this.frontComponentsBuilder?.close();
+    await manifestExtractFromFileServer.closeViteServer();
   }
 }
