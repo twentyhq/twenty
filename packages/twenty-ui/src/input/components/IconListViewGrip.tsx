1c5defb89f98f3ff325e47e030a19b1efe5694b2
Félix Malfait <felix@twenty.com>
2026-01-23 10:51:02 +0100

    revert(twenty-ui): restore IconListViewGrip original props type
    The Partial wrapper was unnecessary since IconComponentProps
    properties are already optional.

--- packages/twenty-ui/src/input/components/IconListViewGrip.tsx
+++ packages/twenty-ui/src/input/components/IconListViewGrip.tsx
@@ -1,9 +1,7 @@
 import IconListViewGripRaw from '@assets/misc/list-view-grip.svg?react';
 import { type IconComponentProps } from '@ui/display/icon/types/IconComponent';
 
-type IconListViewGripProps = Partial<
-  Pick<IconComponentProps, 'size' | 'stroke'>
->;
+type IconListViewGripProps = Pick<IconComponentProps, 'size' | 'stroke'>;
 
 export const IconListViewGrip = (props: IconListViewGripProps) => {
   const width = props.size ?? 8;
