acc038ee1abadcef9e74b4a828ecaab60ed04556
martmull <martmull@hotmail.fr>
2026-01-23 11:31:05 +0100

    update eslint config

--- packages/twenty-sdk/eslint.config.mjs
+++ packages/twenty-sdk/eslint.config.mjs
@@ -3,13 +3,13 @@ import baseConfig from '../../eslint.config.mjs';
 export default [
   ...baseConfig,
   {
+    ignores: ['**/dist/**'],
+  },
+  {
     files: ['*/.{js,jsx,ts,tsx}'],
     rules: {
       'prettier/prettier': 'error',
     },
-  },
-  {
-    ignores: ['**/dist/**'],
   },
   {
     rules: {
