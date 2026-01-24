4e284c64014a9ac4d68b70d982d211150a718dc9
prastoin <paul@twenty.com>
2026-01-24 20:51:53 +0100

    simply

--- packages/twenty-shared/src/types/ExtractSerializedRelationProperties.type.ts
+++ packages/twenty-shared/src/types/ExtractSerializedRelationProperties.type.ts
@@ -1,13 +1,13 @@
 import { SERIALIZED_RELATION_BRAND } from './SerializedRelation.type';
 
 export type ExtractSerializedRelationProperties<T> = T extends unknown
-  ? NonNullable<
-      {
+  ? T extends object
+    ? {
         [P in keyof T]-?: [NonNullable<T[P]>] extends [never]
           ? never
           : typeof SERIALIZED_RELATION_BRAND extends keyof NonNullable<T[P]>
             ? P
             : never;
       }[keyof T]
-    >
+    : never
   : never;
