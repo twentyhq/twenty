e2bae5bee62480b599cb31cbe6076f20f7bc20ec
BOHEUS <nothin_spe@2abc.hub.pl>
2026-01-24 11:01:58 +0100

    Disable sync in related message channels

--- packages/twenty-server/src/modules/connected-account/jobs/delete-workspace-member-connected-accounts.job.ts
+++ packages/twenty-server/src/modules/connected-account/jobs/delete-workspace-member-connected-accounts.job.ts
@@ -4,6 +4,7 @@ import { MessageQueue } from 'src/engine/core-modules/message-queue/message-queu
 import { GlobalWorkspaceOrmManager } from 'src/engine/twenty-orm/global-workspace-datasource/global-workspace-orm.manager';
 import { buildSystemAuthContext } from 'src/engine/twenty-orm/utils/build-system-auth-context.util';
 import { type ConnectedAccountWorkspaceEntity } from 'src/modules/connected-account/standard-objects/connected-account.workspace-entity';
+import { MessageChannelWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-channel.workspace-entity';
 
 export type DeleteWorkspaceMemberConnectedAccountsCleanupJobData = {
   workspaceId: string;
@@ -33,11 +34,32 @@ export class DeleteWorkspaceMemberConnectedAccountsCleanupJob {
             'connectedAccount',
           );
 
+        const connectedAccount = await connectedAccountRepository.findOneBy({
+          accountOwnerId: workspaceMemberId,
+        });
+
         await connectedAccountRepository.update(
           {
             accountOwnerId: workspaceMemberId,
           },
           { state: 'ARCHIVED' },
+        );
+
+        const messageChannelRepository =
+          await this.globalWorkspaceOrmManager.getRepository<MessageChannelWorkspaceEntity>(
+            workspaceId,
+            'messageChannel',
+          );
+
+        const relatedMessageChannels = await messageChannelRepository.findBy({
+          connectedAccountId: connectedAccount?.id,
+        });
+
+        await messageChannelRepository.updateMany(
+          relatedMessageChannels.map((messageChannel) => ({
+            criteria: messageChannel.id,
+            partialEntity: { isSyncEnabled: false },
+          })),
         );
       },
     );
