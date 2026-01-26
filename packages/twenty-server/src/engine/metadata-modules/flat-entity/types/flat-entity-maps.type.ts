0c60a547839d797448e3904f5ca91e6d9698616f
prastoin <paul@twenty.com>
2026-01-26 11:49:11 +0100

    fixup

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-maps.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-maps.type.ts
@@ -1,6 +1,7 @@
-// TODO
-export type UniversalFlatEntityMaps<T extends object> = {
-  byUniversalIdentifier: Partial<Record<string, T>>;
-  universalIdentifierById: Partial<Record<string, string>>;
+import { type SyncableFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-from.type';
+
+export type FlatEntityMaps<T extends SyncableFlatEntity> = {
+  byId: Partial<Record<string, T>>;
+  idByUniversalIdentifier: Partial<Record<string, string>>;
   universalIdentifiersByApplicationId: Partial<Record<string, string[]>>;
 };
