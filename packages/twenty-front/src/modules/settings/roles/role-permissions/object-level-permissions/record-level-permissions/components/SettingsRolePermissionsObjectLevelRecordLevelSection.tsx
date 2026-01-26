e24e0c2538def7e5cf6a043feaae2d8507ef83f7
Weiko <corentin@twenty.com>
2026-01-26 18:45:20 +0100

    lint

--- packages/twenty-front/src/modules/settings/roles/role-permissions/object-level-permissions/record-level-permissions/components/SettingsRolePermissionsObjectLevelRecordLevelSection.tsx
+++ packages/twenty-front/src/modules/settings/roles/role-permissions/object-level-permissions/record-level-permissions/components/SettingsRolePermissionsObjectLevelRecordLevelSection.tsx
@@ -62,16 +62,18 @@ export const SettingsRolePermissionsObjectLevelRecordLevelSection = ({
             Icon={IconLock}
             title={t`Upgrade to access`}
             description={t`This feature is part of the Organization Plan`}
-            Button={isBillingEnabled && (
-              <Button
-                title={t`Upgrade`}
-                variant="primary"
-                accent="blue"
-                size="small"
-                Icon={IconArrowUp}
-                onClick={() => navigateSettings(SettingsPath.Billing)}
-              />
-            )}
+            Button={
+              isBillingEnabled && (
+                <Button
+                  title={t`Upgrade`}
+                  variant="primary"
+                  accent="blue"
+                  size="small"
+                  Icon={IconArrowUp}
+                  onClick={() => navigateSettings(SettingsPath.Billing)}
+                />
+              )
+            }
           />
         </StyledCard>
       </Section>
