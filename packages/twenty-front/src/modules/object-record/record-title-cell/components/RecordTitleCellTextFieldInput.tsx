42ebc2708bf1005193b85c0e6dffddb84cf65d9d
Daniel <daniel@example.com>
2026-01-26 14:03:15 +0100

    fix: format destructuring to match prettier rules (single line)

--- packages/twenty-front/src/modules/object-record/record-title-cell/components/RecordTitleCellTextFieldInput.tsx
+++ packages/twenty-front/src/modules/object-record/record-title-cell/components/RecordTitleCellTextFieldInput.tsx
@@ -16,12 +16,8 @@ export const RecordTitleCellTextFieldInput = ({
   instanceId,
   sizeVariant,
 }: RecordTitleCellTextFieldInputProps) => {
-  const {
-    fieldDefinition,
-    draftValue,
-    setDraftValue,
-    fieldValue,
-  } = useTextField();
+  const { fieldDefinition, draftValue, setDraftValue, fieldValue } =
+    useTextField();
 
   const wrapperRef = useRef<HTMLInputElement>(null);
 
