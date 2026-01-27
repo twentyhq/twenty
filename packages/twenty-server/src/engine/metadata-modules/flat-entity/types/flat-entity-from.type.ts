34a7e0774bd6d9d072149cd66b2fa6e942ab8ab0
prastoin <paul@twenty.com>
2026-01-27 13:51:33 +0100

    fix

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-from.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-from.type.ts
@@ -3,7 +3,6 @@ import { type CastRecordTypeOrmDatePropertiesToString } from 'src/engine/metadat
 import { type ExtractEntityRelatedEntityProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-related-entity-properties.type';
 import { FromMetadataEntityToMetadataName } from 'src/engine/metadata-modules/flat-entity/types/from-metadata-entity-to-metadata-name.type';
 import { type SyncableEntity } from 'src/engine/workspace-manager/types/syncable-entity.interface';
-import { type ExtractJsonbProperties } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/extract-jsonb-properties.type';
 import { UniversalFlatEntityExtraProperties } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-from.type';
 import { AllMetadataName } from 'twenty-shared/metadata';
 
@@ -22,7 +21,6 @@ export type FlatEntityFrom<
   TEntity,
   | ExtractEntityRelatedEntityProperties<TEntity>
   | keyof CastRecordTypeOrmDatePropertiesToString<TEntity>
-  | ExtractJsonbProperties<TEntity>
 > &
   CastRecordTypeOrmDatePropertiesToString<TEntity> &
   AddSuffixToEntityOneToManyProperties<TEntity, 'ids'> & {
