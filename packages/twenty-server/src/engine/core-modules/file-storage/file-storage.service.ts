12cfbc4c8d3811b6e5f64bea35d1448e664c5ce6
Etienne <45695613+etiennejouan@users.noreply.github.com>
2026-01-28 14:32:15 +0100

    fix

--- packages/twenty-server/src/engine/core-modules/file-storage/file-storage.service.ts
+++ packages/twenty-server/src/engine/core-modules/file-storage/file-storage.service.ts
@@ -8,7 +8,7 @@ import { Like, Repository } from 'typeorm';
 
 import { FileStorageDriverFactory } from 'src/engine/core-modules/file-storage/file-storage-driver.factory';
 import { FileEntity } from 'src/engine/core-modules/file/entities/file.entity';
-import { FileInfo } from 'src/engine/core-modules/file/types/file-settings.types';
+import { FileSettings } from 'src/engine/core-modules/file/types/file-settings.types';
 
 @Injectable()
 //TODO: Implement storage driver interface when removing v1
@@ -57,7 +57,7 @@ export class FileStorageService {
     applicationId: string;
     workspaceId: string;
     fileId?: string;
-    settings: FileInfo;
+    settings: FileSettings;
   }): Promise<FileEntity> {
     const driver = this.fileStorageDriverFactory.getCurrentDriver();
 
