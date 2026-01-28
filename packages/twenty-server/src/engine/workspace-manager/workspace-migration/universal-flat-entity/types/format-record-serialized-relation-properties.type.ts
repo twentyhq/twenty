9d9cda382a441f70f90fafecbf0c000799da6309
prastoin <paul@twenty.com>
2026-01-28 16:22:03 +0100

    comment

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/format-record-serialized-relation-properties.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/format-record-serialized-relation-properties.type.ts
@@ -21,7 +21,8 @@ export type FormatRecordSerializedRelationProperties<T> = T extends unknown
   ? T extends (infer U)[]
     ? FormatRecordSerializedRelationProperties<U>[]
     : T extends string
-      ? // By definition we assume that any SerializedRelation is not pg synched, which means stored id might resolve to null
+      ? // By definition we assume that any SerializedRelation are not enforced at pg scope through an FK
+        // Which mean that SerializedRelation might resolve to non-existent entities ( null )
         IsSerializedRelation<T> extends true
         ? T | null
         : T
