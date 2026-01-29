d7b6e8cd8dd76a23dfcae860d48e38f1b276f43c
prastoin <paul@twenty.com>
2026-01-29 12:23:08 +0100

    refactor(server): object

--- packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-object-metadata/types/flat-object-metadata.type.ts
@@ -1,13 +1,16 @@
 import { type FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-from.type';
 import { type ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
+import { ExtractJsonbProperties } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/extract-jsonb-properties.type';
 
 type BaseFlatObjectMetadata = FlatEntityFrom<
   Omit<ObjectMetadataEntity, 'targetRelationFields' | 'dataSourceId'>
 >;
-export type FlatObjectMetadata = Omit<BaseFlatObjectMetadata, '__universal'> & {
-  __universal?: BaseFlatObjectMetadata['__universal'] & {
+export type FlatObjectMetadata = Omit<
+  BaseFlatObjectMetadata,
+  '__universal' | ExtractJsonbProperties<ObjectMetadataEntity>
+> &
+  BaseFlatObjectMetadata['__universal'] & {
     // TODO remove once https://github.com/twentyhq/core-team-issues/issues/2172 has been resolved
     labelIdentifierFieldMetadataUniversalIdentifier: string | null;
     imageIdentifierFieldMetadataUniversalIdentifier: string | null;
   };
-};
