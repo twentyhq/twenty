6989b1b717c760a4ef700e1fb257c046ce0c51a2
prastoin <paul@twenty.com>
2026-01-29 18:02:22 +0100

    chore(server): simplify flatEntityFromV12

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-from-v2.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-from-v2.type.ts
@@ -1,32 +1,15 @@
 import { type AllMetadataName } from 'twenty-shared/metadata';
 
-import { type AddSuffixToEntityOneToManyProperties } from 'src/engine/metadata-modules/flat-entity/types/add-suffix-to-entity-one-to-many-properties.type';
-import { type CastRecordTypeOrmDatePropertiesToString } from 'src/engine/metadata-modules/flat-entity/types/cast-record-typeorm-date-properties-to-string.type';
-import { type ExtractEntityRelatedEntityProperties } from 'src/engine/metadata-modules/flat-entity/types/extract-entity-related-entity-properties.type';
-import { type FromMetadataEntityToMetadataName } from 'src/engine/metadata-modules/flat-entity/types/from-metadata-entity-to-metadata-name.type';
-import { type SyncableEntity } from 'src/engine/workspace-manager/types/syncable-entity.interface';
-import { type UniversalFlatEntityExtraProperties } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-from.type';
+import { type FlatEntityFrom } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-from.type';
 
 /**
  * Currently under migration but aims to replace FlatEntity afterwards
  */
 export type FlatEntityFromV2<
   TEntity,
-  // Required to be passed for narrowed type
   TMetadataName extends AllMetadataName | undefined = undefined,
-> = Omit<
-  TEntity,
-  | ExtractEntityRelatedEntityProperties<TEntity>
-  | keyof CastRecordTypeOrmDatePropertiesToString<TEntity>
-> &
-  CastRecordTypeOrmDatePropertiesToString<TEntity> &
-  AddSuffixToEntityOneToManyProperties<TEntity, 'ids'> &
-  (TEntity extends SyncableEntity
-    ? UniversalFlatEntityExtraProperties<
-        TEntity,
-        TMetadataName extends undefined
-          ? FromMetadataEntityToMetadataName<TEntity>
-          : TMetadataName
-      > & { universalIdentifier: string }
-    : // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-      {});
+  TInnerFlatEntity extends { __universal?: unknown } = FlatEntityFrom<
+    TEntity,
+    TMetadataName
+  >,
+> = Omit<TInnerFlatEntity, '__universal'> & TInnerFlatEntity['__universal'];
