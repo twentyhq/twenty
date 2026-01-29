5ec33182499a3e6221e0cca6f3fe456c9adc42f0
prastoin <paul@twenty.com>
2026-01-29 17:48:55 +0100

    lint

--- packages/twenty-server/src/engine/metadata-modules/field-metadata/field-metadata.exception.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/field-metadata.exception.ts
@@ -4,8 +4,8 @@ import { assertUnreachable } from 'twenty-shared/utils';
 
 import { STANDARD_ERROR_MESSAGE } from 'src/engine/api/common/common-query-runners/errors/standard-error-message.constant';
 import {
-    appendCommonExceptionCode,
-    CustomException,
+  appendCommonExceptionCode,
+  CustomException,
 } from 'src/utils/custom-exception';
 
 export const FieldMetadataExceptionCode = appendCommonExceptionCode({
