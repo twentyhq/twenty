c159c0f142a450046ded9c4311b93d9065162dd6
prastoin <paul@twenty.com>
2026-01-30 18:09:05 +0100

    fixup

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-property-update.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-property-update.type.ts
@@ -2,18 +2,12 @@ import { type AllMetadataName } from 'twenty-shared/metadata';
 
 import { type FlatEntityPropertiesToCompare } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-properties-to-compare.type';
 import { type MetadataFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-flat-entity.type';
-import { type PropertyUpdate } from 'src/engine/workspace-manager/workspace-migration/types/property-update.type';
+import { PropertyUpdate } from 'src/engine/workspace-manager/workspace-migration/types/property-update.type';
 
 export type FlatEntityPropertyUpdate<
   T extends AllMetadataName,
   K extends FlatEntityPropertiesToCompare<T> = FlatEntityPropertiesToCompare<T>,
-> =
-  MetadataBuilderHasBeenMigratedToUniversal<T> extends true
-    ? PropertyUpdate<
-        MetadataFlatEntity<T>,
-        Extract<K, keyof MetadataFlatEntity<T>>
-      >
-    : PropertyUpdate<
-        MetadataFlatEntity<T>,
-        Extract<K, keyof MetadataFlatEntity<T>>
-      >;
+> = PropertyUpdate<
+  MetadataFlatEntity<T>,
+  Extract<K, keyof MetadataFlatEntity<T>>
+>;
