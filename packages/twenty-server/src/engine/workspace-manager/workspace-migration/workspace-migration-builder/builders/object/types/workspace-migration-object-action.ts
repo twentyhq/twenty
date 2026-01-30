7a1e8240388e4dd921f43081ee58bc3dc3c8818c
prastoin <paul@twenty.com>
2026-01-30 11:33:22 +0100

    feat(server): types

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/builders/object/types/workspace-migration-object-action.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/builders/object/types/workspace-migration-object-action.ts
@@ -1,12 +1,14 @@
-import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { UniversalFlatFieldMetadata } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-field-metadata.type';
 import { type BaseCreateWorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-create-workspace-migration-action.type';
 import { type BaseDeleteWorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-delete-workspace-migration-action.type';
 import { type BaseUpdateWorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-update-workspace-migration-action.type';
 
-export type CreateObjectAction =
-  BaseCreateWorkspaceMigrationAction<'objectMetadata'> & {
-    flatFieldMetadatas: FlatFieldMetadata[];
-  };
+export type CreateObjectAction = BaseCreateWorkspaceMigrationAction<
+  'objectMetadata',
+  true
+> & {
+  universalFlatFieldMetadatas: UniversalFlatFieldMetadata[];
+};
 
 export type UpdateObjectAction =
   BaseUpdateWorkspaceMigrationAction<'objectMetadata'>;
