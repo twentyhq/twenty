4edeb5e4b64789ab46a7bb4b078188f335b649c3
neo773 <neo773@protonmail.com>
2026-01-30 19:45:24 +0530

    minor changes

--- packages/twenty-server/src/engine/metadata-modules/object-metadata/constants/object-metadata-translatable-properties.constant.ts
+++ packages/twenty-server/src/engine/metadata-modules/object-metadata/constants/object-metadata-translatable-properties.constant.ts
@@ -1,8 +1,10 @@
+import { type ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
+
 export const OBJECT_METADATA_TRANSLATABLE_PROPERTIES = [
   'labelSingular',
   'labelPlural',
   'description',
-] as const;
+] as const satisfies (keyof ObjectMetadataEntity)[];
 
 export type ObjectMetadataTranslatableProperty =
   (typeof OBJECT_METADATA_TRANSLATABLE_PROPERTIES)[number];
--- packages/twenty-server/src/engine/metadata-modules/object-metadata/object-metadata.service.ts
+++ packages/twenty-server/src/engine/metadata-modules/object-metadata/object-metadata.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { TypeOrmQueryService } from '@ptc-org/nestjs-query-typeorm';
-import { type APP_LOCALES } from 'twenty-shared/translations';
+import { AppLocale } from 'twenty-shared/translations';
 import { fromArrayToUniqueKeyRecord, isDefined } from 'twenty-shared/utils';
 import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
 
@@ -61,7 +61,7 @@ export class ObjectMetadataService extends TypeOrmQueryService<ObjectMetadataEnt
   }: {
     workspaceId: string;
     updateObjectInput: UpdateOneObjectInput;
-    locale?: keyof typeof APP_LOCALES;
+    locale?: AppLocale;
   }): Promise<FlatObjectMetadata> {
     const {
       flatObjectMetadataMaps: existingFlatObjectMetadataMaps,
