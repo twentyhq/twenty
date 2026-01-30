ef87797de8f3ddee8f50cd2750f26e698dcf1690
Abdul Rahman <ar5438376@gmail.com>
2026-01-30 19:57:04 +0530

    Refactor CommandMenuItemResolver to Use isDefined Utility
    Updated the CommandMenuItemResolver to utilize the isDefined utility for checking the presence of frontComponentId. This change enhances code clarity and ensures that the check for frontComponentId is more explicit, improving overall maintainability.

--- packages/twenty-server/src/engine/metadata-modules/command-menu-item/command-menu-item.resolver.ts
+++ packages/twenty-server/src/engine/metadata-modules/command-menu-item/command-menu-item.resolver.ts
@@ -8,6 +8,8 @@ import {
   Resolver,
 } from '@nestjs/graphql';
 
+import { isDefined } from 'twenty-shared/utils';
+
 import { UUIDScalarType } from 'src/engine/api/graphql/workspace-schema-builder/graphql-types/scalars';
 import { FeatureFlagKey } from 'src/engine/core-modules/feature-flag/enums/feature-flag-key.enum';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
@@ -20,11 +22,11 @@ import { NoPermissionGuard } from 'src/engine/guards/no-permission.guard';
 import { WorkspaceAuthGuard } from 'src/engine/guards/workspace-auth.guard';
 import { CommandMenuItemService } from 'src/engine/metadata-modules/command-menu-item/command-menu-item.service';
 import { CommandMenuItemDTO } from 'src/engine/metadata-modules/command-menu-item/dtos/command-menu-item.dto';
-import { FrontComponentDTO } from 'src/engine/metadata-modules/front-component/dtos/front-component.dto';
-import { FrontComponentService } from 'src/engine/metadata-modules/front-component/front-component.service';
 import { CreateCommandMenuItemInput } from 'src/engine/metadata-modules/command-menu-item/dtos/create-command-menu-item.input';
 import { UpdateCommandMenuItemInput } from 'src/engine/metadata-modules/command-menu-item/dtos/update-command-menu-item.input';
 import { CommandMenuItemGraphqlApiExceptionInterceptor } from 'src/engine/metadata-modules/command-menu-item/interceptors/command-menu-item-graphql-api-exception.interceptor';
+import { FrontComponentDTO } from 'src/engine/metadata-modules/front-component/dtos/front-component.dto';
+import { FrontComponentService } from 'src/engine/metadata-modules/front-component/front-component.service';
 import { WorkspaceMigrationGraphqlApiExceptionInterceptor } from 'src/engine/workspace-manager/workspace-migration/interceptors/workspace-migration-graphql-api-exception.interceptor';
 
 @UseGuards(WorkspaceAuthGuard, FeatureFlagGuard)
@@ -44,7 +46,7 @@ export class CommandMenuItemResolver {
     @Parent() commandMenuItem: CommandMenuItemDTO,
     @AuthWorkspace() workspace: WorkspaceEntity,
   ) {
-    if (!commandMenuItem.frontComponentId) {
+    if (!isDefined(commandMenuItem.frontComponentId)) {
       return null;
     }
 
