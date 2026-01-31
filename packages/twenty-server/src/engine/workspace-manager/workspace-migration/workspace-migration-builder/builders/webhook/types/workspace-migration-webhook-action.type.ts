6379f41ce45f28646db04154edfbf099250f0f9a
prastoin <paul@twenty.com>
2026-01-31 14:57:40 +0100

    refactor(server): migrate all delete action to flat

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/builders/webhook/types/workspace-migration-webhook-action.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/builders/webhook/types/workspace-migration-webhook-action.type.ts
@@ -1,5 +1,6 @@
 import { type BaseCreateWorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-create-workspace-migration-action.type';
 import { type BaseDeleteWorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-delete-workspace-migration-action.type';
+import { type BaseFlatDeleteWorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-flat-delete-workspace-migration-action.type';
 import { type BaseUpdateWorkspaceMigrationAction } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-update-workspace-migration-action.type';
 
 export type CreateWebhookAction = BaseCreateWorkspaceMigrationAction<'webhook'>;
@@ -7,3 +8,6 @@ export type CreateWebhookAction = BaseCreateWorkspaceMigrationAction<'webhook'>;
 export type UpdateWebhookAction = BaseUpdateWorkspaceMigrationAction<'webhook'>;
 
 export type DeleteWebhookAction = BaseDeleteWorkspaceMigrationAction<'webhook'>;
+
+export type FlatDeleteWebhookAction =
+  BaseFlatDeleteWorkspaceMigrationAction<'webhook'>;
