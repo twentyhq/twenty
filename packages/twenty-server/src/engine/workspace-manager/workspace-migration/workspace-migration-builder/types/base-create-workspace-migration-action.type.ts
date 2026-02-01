d25aaa57f10400d2b76cf19775a9e295be977ff5
prastoin <paul@twenty.com>
2026-02-01 11:00:23 +0100

    chore(server): remove universal flat entity maps

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-create-workspace-migration-action.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-create-workspace-migration-action.type.ts
@@ -1,18 +1,10 @@
 import { type AllMetadataName } from 'twenty-shared/metadata';
 
-import { type AllFlatEntityTypesByMetadataName } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entity-types-by-metadata-name';
 import { type MetadataFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-flat-entity.type';
-import { type MetadataUniversalFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-universal-flat-entity.type';
 import { type WORKSPACE_MIGRATION_ACTION_TYPE } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/constants/workspace-migration-action-type.constant';
 
 export type BaseCreateWorkspaceMigrationAction<T extends AllMetadataName> = {
-  flatEntity: AllFlatEntityTypesByMetadataName[T] extends {
-    universalMigrated: {
-      runner: true;
-    };
-  }
-    ? MetadataUniversalFlatEntity<T>
-    : MetadataFlatEntity<T>;
+  flatEntity: MetadataFlatEntity<T>;
   type: typeof WORKSPACE_MIGRATION_ACTION_TYPE.create;
   metadataName: T;
 };
