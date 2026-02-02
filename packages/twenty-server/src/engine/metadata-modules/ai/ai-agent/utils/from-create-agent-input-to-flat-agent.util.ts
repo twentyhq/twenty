3fa37b56e141339379b9d6b7c51e116381646e2b
prastoin <paul@twenty.com>
2026-02-02 16:55:36 +0100

    more standardid cleaning

--- packages/twenty-server/src/engine/metadata-modules/ai/ai-agent/utils/from-create-agent-input-to-flat-agent.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/ai/ai-agent/utils/from-create-agent-input-to-flat-agent.util.ts
@@ -32,7 +32,6 @@ export const fromCreateAgentInputToFlatAgent = ({
         'description',
         'prompt',
         'modelId',
-        'standardId',
         'applicationId',
         'roleId',
       ],
