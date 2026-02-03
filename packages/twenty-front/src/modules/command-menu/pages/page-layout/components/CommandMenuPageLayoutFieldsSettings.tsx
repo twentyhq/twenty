09c2c1fb620ca893b107d8d30b5bb255ae91a4bc
Devessier <baptiste@devessier.fr>
2026-02-03 15:12:17 +0100

    feat: drop unwanted options

--- packages/twenty-front/src/modules/command-menu/pages/page-layout/components/CommandMenuPageLayoutFieldsSettings.tsx
+++ packages/twenty-front/src/modules/command-menu/pages/page-layout/components/CommandMenuPageLayoutFieldsSettings.tsx
@@ -1,6 +1,5 @@
 import { CommandGroup } from '@/command-menu/components/CommandGroup';
 import { CommandMenuItem } from '@/command-menu/components/CommandMenuItem';
-import { CommandMenuItemToggle } from '@/command-menu/components/CommandMenuItemToggle';
 import { CommandMenuList } from '@/command-menu/components/CommandMenuList';
 import { WidgetSettingsFooter } from '@/command-menu/pages/page-layout/components/WidgetSettingsFooter';
 import { useNavigatePageLayoutCommandMenu } from '@/command-menu/pages/page-layout/hooks/useNavigatePageLayoutCommandMenu';
@@ -11,17 +10,8 @@ import { useTemporaryFieldsConfiguration } from '@/page-layout/hooks/useTemporar
 import { SelectableListItem } from '@/ui/layout/selectable-list/components/SelectableListItem';
 import styled from '@emotion/styled';
 import { useLingui } from '@lingui/react/macro';
-import { useState } from 'react';
 import { isDefined } from 'twenty-shared/utils';
-import {
-  IconChevronDown,
-  IconChevronUp,
-  IconCircleOff,
-  IconCirclePlus,
-  IconLayoutSidebarRight,
-  IconSwitchHorizontal,
-  IconTrash,
-} from 'twenty-ui/display';
+import { IconLayoutSidebarRight } from 'twenty-ui/display';
 
 const StyledContainer = styled.div`
   display: flex;
@@ -46,8 +36,6 @@ export const CommandMenuPageLayoutFieldsSettings = () => {
   const fieldsConfiguration =
     useTemporaryFieldsConfiguration(objectNameSingular);
 
-  const [showMoreFieldsButton, setShowMoreFieldsButton] = useState(true);
-
   if (!isDefined(widgetInEditMode)) {
     return null;
   }
@@ -61,10 +49,6 @@ export const CommandMenuPageLayoutFieldsSettings = () => {
     navigatePageLayoutCommandMenu({
       commandMenuPage: CommandMenuPages.PageLayoutFieldsLayout,
     });
-  };
-
-  const handleToggleMoreFieldsButton = () => {
-    setShowMoreFieldsButton(!showMoreFieldsButton);
   };
 
   const selectableItemIds = [
@@ -100,76 +84,6 @@ export const CommandMenuPageLayoutFieldsSettings = () => {
                 description={t`${totalFieldsCount} fields`}
                 contextualTextPosition="right"
               />
-            </SelectableListItem>
-
-            <SelectableListItem
-              itemId="display-more-fields-button"
-              onEnter={handleToggleMoreFieldsButton}
-            >
-              <CommandMenuItemToggle
-                id="display-more-fields-button"
-                LeftIcon={IconCircleOff}
-                text={t`Display "More fields" button`}
-                toggled={showMoreFieldsButton}
-                onToggleChange={handleToggleMoreFieldsButton}
-              />
-            </SelectableListItem>
-
-            <SelectableListItem itemId="action-button">
-              <CommandMenuItem
-                id="action-button"
-                label={t`Action Button`}
-                Icon={IconCirclePlus}
-                hasSubMenu
-                description={t`None`}
-                contextualTextPosition="right"
-              />
-            </SelectableListItem>
-          </CommandGroup>
-
-          <CommandGroup heading={t`More`}>
-            <SelectableListItem itemId="move-down">
-              <CommandMenuItem
-                id="move-down"
-                label={t`Move Down`}
-                Icon={IconChevronDown}
-              />
-            </SelectableListItem>
-
-            <SelectableListItem itemId="move-up">
-              <CommandMenuItem
-                id="move-up"
-                label={t`Move Up`}
-                Icon={IconChevronUp}
-              />
-            </SelectableListItem>
-
-            <SelectableListItem itemId="move-to-tab">
-              <CommandMenuItem
-                id="move-to-tab"
-                label={t`Move to another tab`}
-                Icon={IconSwitchHorizontal}
-              />
-            </SelectableListItem>
-
-            <SelectableListItem itemId="add-widget-above">
-              <CommandMenuItem
-                id="add-widget-above"
-                label={t`Add widget above`}
-                Icon={IconCirclePlus}
-              />
-            </SelectableListItem>
-
-            <SelectableListItem itemId="add-widget-below">
-              <CommandMenuItem
-                id="add-widget-below"
-                label={t`Add widget below`}
-                Icon={IconCirclePlus}
-              />
-            </SelectableListItem>
-
-            <SelectableListItem itemId="delete">
-              <CommandMenuItem id="delete" label={t`Delete`} Icon={IconTrash} />
             </SelectableListItem>
           </CommandGroup>
         </CommandMenuList>
