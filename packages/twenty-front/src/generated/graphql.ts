ef81450af9651c99b135e831828068f1c4aaed13
Devessier <baptiste@devessier.fr>
2026-02-03 17:42:50 +0100

    chore: regenerate graphql types

--- packages/twenty-front/src/generated/graphql.ts
+++ packages/twenty-front/src/generated/graphql.ts
@@ -3445,7 +3445,7 @@ export type PublicFeatureFlag = {
 export type PublicFeatureFlagMetadata = {
   __typename?: 'PublicFeatureFlagMetadata';
   description: Scalars['String'];
-  imagePath: Scalars['String'];
+  imagePath?: Maybe<Scalars['String']>;
   label: Scalars['String'];
 };
 
