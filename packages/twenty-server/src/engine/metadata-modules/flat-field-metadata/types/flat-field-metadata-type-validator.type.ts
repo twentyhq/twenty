8914eb317cfc9fa0d4a7b7a8be2167782c78b56d
prastoin <paul@twenty.com>
2026-02-05 16:47:36 +0100

    refactor(server): field metadata validator

--- packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata-type-validator.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata-type-validator.type.ts
@@ -2,14 +2,14 @@ import { type FieldMetadataType } from 'twenty-shared/types';
 
 import { type GenericValidateFlatFieldMetadataTypeSpecificitiesArgs } from 'src/engine/metadata-modules/flat-field-metadata/services/flat-field-metadata-type-validator.service';
 import { type FlatFieldMetadataValidationError } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata-validation-error.type';
-import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { type UniversalFlatFieldMetadata } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-field-metadata.type';
 
 export type FlatFieldMetadataTypeValidationArgs<T extends FieldMetadataType> =
   Omit<
     GenericValidateFlatFieldMetadataTypeSpecificitiesArgs,
     'flatEntityToValidate'
   > & {
-    flatEntityToValidate: FlatFieldMetadata<T>;
+    flatEntityToValidate: UniversalFlatFieldMetadata<T>;
   };
 
 export type FlatFieldMetadataTypeValidator = {
