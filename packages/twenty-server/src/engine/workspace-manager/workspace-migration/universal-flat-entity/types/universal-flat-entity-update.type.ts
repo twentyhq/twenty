d8f9f2ec92e3177b20d96212f2cdac745a3ce1ad
prastoin <paul@twenty.com>
2026-02-05 13:11:18 +0100

    refactor(server): exlcude universal flat entity update props out of flat entity update

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-update.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-update.type.ts
@@ -4,7 +4,7 @@ import { ALL_ENTITY_PROPERTIES_CONFIGURATION_BY_METADATA_NAME } from 'src/engine
 import { MetadataFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-flat-entity.type';
 import { MetadataUniversalFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-universal-flat-entity.type';
 import { MetadataUniversalFlatEntityPropertiesToCompare } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/metadata-universal-flat-entity-properties-to-compare.type';
-import { Expect } from 'twenty-shared/testing';
+import { Equal, Expect } from 'twenty-shared/testing';
 
 export type UniversalFlatEntityUpdate<T extends AllMetadataName> = Partial<
   Pick<
@@ -13,15 +13,29 @@ export type UniversalFlatEntityUpdate<T extends AllMetadataName> = Partial<
   >
 >;
 
-export type FlatEntityUpdate<T extends AllMetadataName> = Partial<
+type IsUniversalMappedProperty<
+  MetadataConfig,
+  P extends keyof MetadataConfig,
+> = MetadataConfig[P] extends { universalProperty: string }
+  ? MetadataConfig[P]['universalProperty']
+  : never;
+
+export type FlatEntityUpdate<
+  T extends AllMetadataName,
+  MetadataPropertyConfig = (typeof ALL_ENTITY_PROPERTIES_CONFIGURATION_BY_METADATA_NAME)[T],
+> = Partial<
   Pick<
     MetadataFlatEntity<T>,
-    Extract<
-      keyof MetadataFlatEntity<T>,
-      keyof (typeof ALL_ENTITY_PROPERTIES_CONFIGURATION_BY_METADATA_NAME)[T]
-    >
+    Extract<keyof MetadataFlatEntity<T>, keyof MetadataPropertyConfig>
   >
->;
+> & {
+  [K in keyof MetadataPropertyConfig as [never] extends IsUniversalMappedProperty<
+    MetadataPropertyConfig,
+    K
+  >
+    ? never
+    : IsUniversalMappedProperty<MetadataPropertyConfig, K>]?: never;
+};
 
 // TODO ignore eslint
 type Assertions = [
@@ -29,9 +43,10 @@ type Assertions = [
     'settings' extends keyof FlatEntityUpdate<'fieldMetadata'> ? true : false
   >,
   Expect<
-    'universalSettings' extends keyof FlatEntityUpdate<'fieldMetadata'>
-      ? false
-      : true
+    Equal<
+      FlatEntityUpdate<'fieldMetadata'>['universalSettings'],
+      never | undefined
+    >
   >,
 
   Expect<
--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-flat-update-workspace-migration-action.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration-builder/types/base-flat-update-workspace-migration-action.type.ts
@@ -1,6 +1,8 @@
 import { type AllMetadataName } from 'twenty-shared/metadata';
 
-import { FlatEntityUpdate } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-update.type';
+import {
+  FlatEntityUpdate
+} from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-update.type';
 import { type WORKSPACE_MIGRATION_ACTION_TYPE } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/constants/workspace-migration-action-type.constant';
 
 export type BaseFlatUpdateWorkspaceMigrationAction<T extends AllMetadataName> =
