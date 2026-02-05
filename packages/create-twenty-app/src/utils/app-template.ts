19ac5a5c9c9dafc52ef79402d2ef9d570882255d
martmull <martmull@hotmail.fr>
2026-02-05 14:39:30 +0100

    Increate twenty-sdk version

--- packages/create-twenty-app/src/utils/app-template.ts
+++ packages/create-twenty-app/src/utils/app-template.ts
@@ -282,7 +282,7 @@ const createPackageJson = async ({
       'lint:fix': 'eslint --fix',
     },
     dependencies: {
-      'twenty-sdk': '0.4.4',
+      'twenty-sdk': '0.4.5',
     },
     devDependencies: {
       typescript: '^5.9.3',
