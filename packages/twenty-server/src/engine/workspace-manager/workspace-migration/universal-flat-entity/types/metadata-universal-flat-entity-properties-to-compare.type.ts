e7fcf8a7bfa46ac7117863888eb4ea974365f550
prastoin <paul@twenty.com>
2026-02-05 10:45:20 +0100

    feat(server): metadata universal flat entity properties to compare

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/metadata-universal-flat-entity-properties-to-compare.type.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/metadata-universal-flat-entity-properties-to-compare.type.ts
@@ -1,9 +1,38 @@
 import { type AllMetadataName } from 'twenty-shared/metadata';
 
 import { type ALL_FLAT_ENTITY_PROPERTIES_TO_COMPARE_BY_METADATA_NAME } from 'src/engine/metadata-modules/flat-entity/constant/all-flat-entity-properties-to-compare-by-metadata-name.constant';
-import { MetadataUniversalFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-universal-flat-entity.type';
+import { Equal, Expect } from 'twenty-shared/testing';
 
-export type MetadataUniversalFlatEntityPropertiesToCompare<T extends AllMetadataName> = Extract<
-  keyof (typeof ALL_FLAT_ENTITY_PROPERTIES_TO_COMPARE_BY_METADATA_NAME)[T],
-  keyof MetadataUniversalFlatEntity<T>
->;
+type ExtractPropertyToCompare<
+  MetadataConfig,
+  P extends keyof MetadataConfig,
+> = MetadataConfig[P] extends { universalProperty: string }
+  ? MetadataConfig[P]['universalProperty']
+  : P;
+
+export type MetadataUniversalFlatEntityPropertiesToCompare<
+  T extends AllMetadataName,
+  MetadataConfig = (typeof ALL_FLAT_ENTITY_PROPERTIES_TO_COMPARE_BY_METADATA_NAME)[T],
+> = {
+  [P in keyof MetadataConfig]: ExtractPropertyToCompare<MetadataConfig, P>;
+}[keyof MetadataConfig];
+
+// TODO ignore eslint
+type Assertions = [
+  Expect<
+    Equal<
+      MetadataUniversalFlatEntityPropertiesToCompare<'fieldMetadata'>,
+      | 'name'
+      | 'label'
+      | 'icon'
+      | 'description'
+      | 'isActive'
+      | 'defaultValue'
+      | 'standardOverrides'
+      | 'options'
+      | 'isUnique'
+      | 'isLabelSyncedWithName'
+      | 'universalSettings'
+    >
+  >,
+];
