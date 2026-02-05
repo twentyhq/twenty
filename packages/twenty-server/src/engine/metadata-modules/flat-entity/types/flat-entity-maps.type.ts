1ea0af7d49d51e7e3aee1dcd84fcf5d7419f49db
prastoin <paul@twenty.com>
2026-02-05 13:42:37 +0100

    feat(server): find flat entity by uniserval

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-maps.type.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-entity/types/flat-entity-maps.type.ts
@@ -1,7 +1,8 @@
 import { type SyncableFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-from.type';
+import { UniversalFlatEntityMaps } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-maps.type';
 
-export type FlatEntityMaps<T extends SyncableFlatEntity> = {
-  byUniversalIdentifier: Partial<Record<string, T>>;
-  universalIdentifierById: Partial<Record<string, string>>;
-  universalIdentifiersByApplicationId: Partial<Record<string, string[]>>;
-};
+export type FlatEntityMaps<T extends SyncableFlatEntity> =
+  UniversalFlatEntityMaps<T> & {
+    universalIdentifierById: Partial<Record<string, string>>;
+    universalIdentifiersByApplicationId: Partial<Record<string, string[]>>;
+  };
