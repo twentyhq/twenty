2f643181479c6604c5f06631f6d2220dc670d5a4
ehconitin <nitinkoche03@gmail.com>
2026-02-09 15:18:24 +0530

    add gauge chart in type

--- packages/twenty-server/src/engine/metadata-modules/page-layout-widget/constants/graph-configuration-types.constant.ts
+++ packages/twenty-server/src/engine/metadata-modules/page-layout-widget/constants/graph-configuration-types.constant.ts
@@ -3,6 +3,7 @@ import { WidgetConfigurationType } from 'src/engine/metadata-modules/page-layout
 export const GRAPH_CONFIGURATION_TYPES = new Set<WidgetConfigurationType>([
   WidgetConfigurationType.AGGREGATE_CHART,
   WidgetConfigurationType.BAR_CHART,
+  WidgetConfigurationType.GAUGE_CHART,
   WidgetConfigurationType.LINE_CHART,
   WidgetConfigurationType.PIE_CHART,
 ]);
