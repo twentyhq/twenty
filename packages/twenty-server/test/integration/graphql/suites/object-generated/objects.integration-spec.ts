157873da3e6816727e9092f6852e92894f5dac65
Charles Bochet <charles@twenty.com>
2026-02-11 00:46:25 +0100

    Fix

--- packages/twenty-server/test/integration/graphql/suites/object-generated/objects.integration-spec.ts
+++ packages/twenty-server/test/integration/graphql/suites/object-generated/objects.integration-spec.ts
@@ -33,7 +33,7 @@ describe('objectsResolver (e2e)', () => {
     };
 
     return client
-      .post('/graphql')
+      .post('/metadata')
       .set('Authorization', `Bearer ${APPLE_JANE_ADMIN_ACCESS_TOKEN}`)
       .send(queryData)
       .expect(200)
