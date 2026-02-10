b340f1b84508abf26e19a5f9a2e2d1d5801ed618
prastoin <paul@twenty.com>
2026-02-10 18:17:28 +0100

    lint

--- packages/twenty-server/src/engine/metadata-modules/field-metadata/constants/field-metadata-standard-overrides-properties.constant.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/constants/field-metadata-standard-overrides-properties.constant.ts
@@ -1,4 +1,4 @@
-import { MetadataUniversalFlatEntityPropertiesToCompare } from "src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/metadata-universal-flat-entity-properties-to-compare.type";
+import { type MetadataUniversalFlatEntityPropertiesToCompare } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/metadata-universal-flat-entity-properties-to-compare.type';
 
 export const FIELD_METADATA_STANDARD_OVERRIDES_PROPERTIES = [
   'label',
