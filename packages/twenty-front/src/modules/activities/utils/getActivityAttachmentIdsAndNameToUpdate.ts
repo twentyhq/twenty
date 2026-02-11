9731c40826e0dbc8ee2f282341bdf1b01136e475
Etienne <45695613+etiennejouan@users.noreply.github.com>
2026-02-11 18:04:52 +0100

    fix-lint

--- packages/twenty-front/src/modules/activities/utils/getActivityAttachmentIdsAndNameToUpdate.ts
+++ packages/twenty-front/src/modules/activities/utils/getActivityAttachmentIdsAndNameToUpdate.ts
@@ -1,10 +1,10 @@
 import { type Attachment } from '@/activities/files/types/Attachment';
-import { getAttachmentUrl } from '@/activities/utils/getAttachmentUrl';
 import { compareUrls } from '@/activities/utils/compareUrls';
 import {
   type AttachmentInfo,
   getActivityAttachmentPathsAndName,
 } from '@/activities/utils/getActivityAttachmentPathsAndName';
+import { getAttachmentUrl } from '@/activities/utils/getAttachmentUrl';
 import { isDefined } from 'twenty-shared/utils';
 
 export const getActivityAttachmentIdsAndNameToUpdate = (
@@ -19,7 +19,10 @@ export const getActivityAttachmentIdsAndNameToUpdate = (
   return activityAttachmentsNameAndPaths.reduce(
     (acc: Partial<Attachment>[], activity: AttachmentInfo) => {
       const foundActivity = oldActivityAttachments.find((attachment) =>
-        compareUrls(getAttachmentUrl(attachment, isFilesFieldMigrated), activity.path),
+        compareUrls(
+          getAttachmentUrl(attachment, isFilesFieldMigrated),
+          activity.path,
+        ),
       );
       if (isDefined(foundActivity) && foundActivity.name !== activity.name) {
         acc.push({ id: foundActivity.id, name: activity.name });
