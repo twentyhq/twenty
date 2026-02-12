06ab3d69794f14c30e597617cb2b84e02c8b82e5
martmull <martmull@hotmail.fr>
2026-02-12 17:39:57 +0100

    Fix twenty-sdk build

--- packages/twenty-sdk/vite.config.ts
+++ packages/twenty-sdk/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import packageJson from './package.json';
+import { type PackageJson } from 'type-fest';
 
 const entries = [
   'src/index.ts',
@@ -78,7 +79,9 @@ export default defineConfig(() => {
           warn(warning);
         },
         external: (id: string) => {
-          if (/^node:/.test(id)) return true;
+          if (/^node:/.test(id)) {
+            return true;
+          }
 
           const builtins = [
             'path',
@@ -91,11 +94,18 @@ export default defineConfig(() => {
             'os',
             'module',
           ];
-          if (builtins.includes(id)) return true;
 
-          const deps = Object.keys((packageJson as any).dependencies || {});
+          if (builtins.includes(id)) {
+            return true;
+          }
 
-          return deps.some((dep) => id === dep || id.startsWith(dep + '/'));
+          const deps = Object.entries(
+            (packageJson as PackageJson).dependencies || {},
+          ).filter(([_, version]) => !version?.startsWith('workspace:'));
+
+          return deps.some(
+            ([dep, _]) => id === dep || id.startsWith(dep + '/'),
+          );
         },
         output: [
           {
