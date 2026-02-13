a72bb3497104d9fc3a5af4a1daa1601b719cfdf7
Devessier <baptiste@devessier.fr>
2026-02-13 12:13:42 +0100

    chore: regenerate barrel

--- packages/twenty-ui/src/navigation/index.ts
+++ packages/twenty-ui/src/navigation/index.ts
@@ -7,83 +7,93 @@
  *                              |___/
  */
 
-export { AdvancedSettingsToggle } from './link/components/AdvancedSettingsToggle';
 export type { AdvancedSettingsToggleProps } from './link/components/AdvancedSettingsToggle';
-export { ClickToActionLink } from './link/components/ClickToActionLink';
+export { AdvancedSettingsToggle } from './link/components/AdvancedSettingsToggle';
 export type { ClickToActionLinkProps } from './link/components/ClickToActionLink';
-export { ContactLink } from './link/components/ContactLink';
+export { ClickToActionLink } from './link/components/ClickToActionLink';
 export type { ContactLinkProps } from './link/components/ContactLink';
-export { GithubVersionLink } from './link/components/GithubVersionLink';
+export { ContactLink } from './link/components/ContactLink';
 export type { GithubVersionLinkProps } from './link/components/GithubVersionLink';
-export { RawLink } from './link/components/RawLink';
+export { GithubVersionLink } from './link/components/GithubVersionLink';
 export type { RawLinkProps } from './link/components/RawLink';
-export { RoundedLink } from './link/components/RoundedLink';
+export { RawLink } from './link/components/RawLink';
 export type { RoundedLinkProps } from './link/components/RoundedLink';
-export { LinkType, SocialLink } from './link/components/SocialLink';
+export { RoundedLink } from './link/components/RoundedLink';
 export type { SocialLinkProps } from './link/components/SocialLink';
-export { UndecoratedLink } from './link/components/UndecoratedLink';
+export { LinkType, SocialLink } from './link/components/SocialLink';
 export type { UndecoratedLinkProps } from './link/components/UndecoratedLink';
+export { UndecoratedLink } from './link/components/UndecoratedLink';
 export { CAL_LINK } from './link/constants/Cal';
 export { GITHUB_LINK } from './link/constants/GithubLink';
 export { TWENTY_PRICING_LINK } from './link/constants/TwentyPricingLink';
-export { MenuPicker } from './menu/components/MenuPicker';
 export type { MenuPickerProps } from './menu/components/MenuPicker';
-export { MenuItem } from './menu/menu-item/components/MenuItem';
+export { MenuPicker } from './menu/components/MenuPicker';
 export type {
   MenuItemIconButton,
-  MenuItemProps
+  MenuItemProps,
 } from './menu/menu-item/components/MenuItem';
-export { MenuItemAvatar } from './menu/menu-item/components/MenuItemAvatar';
+export { MenuItem } from './menu/menu-item/components/MenuItem';
 export type { MenuItemAvatarProps } from './menu/menu-item/components/MenuItemAvatar';
-export { MenuItemDraggable } from './menu/menu-item/components/MenuItemDraggable';
+export { MenuItemAvatar } from './menu/menu-item/components/MenuItemAvatar';
 export type { MenuItemDraggableProps } from './menu/menu-item/components/MenuItemDraggable';
-export { MenuItemHotKeys } from './menu/menu-item/components/MenuItemHotKeys';
+export { MenuItemDraggable } from './menu/menu-item/components/MenuItemDraggable';
 export type { MenuItemHotKeysProps } from './menu/menu-item/components/MenuItemHotKeys';
-export { MenuItemMultiSelect } from './menu/menu-item/components/MenuItemMultiSelect';
+export { MenuItemHotKeys } from './menu/menu-item/components/MenuItemHotKeys';
 export type { MenuItemMultiSelectProps } from './menu/menu-item/components/MenuItemMultiSelect';
-export { MenuItemMultiSelectAvatar } from './menu/menu-item/components/MenuItemMultiSelectAvatar';
+export { MenuItemMultiSelect } from './menu/menu-item/components/MenuItemMultiSelect';
 export type { MenuItemMultiSelectAvatarProps } from './menu/menu-item/components/MenuItemMultiSelectAvatar';
-export { MenuItemMultiSelectTag } from './menu/menu-item/components/MenuItemMultiSelectTag';
+export { MenuItemMultiSelectAvatar } from './menu/menu-item/components/MenuItemMultiSelectAvatar';
 export type { MenuItemMultiSelectTagProps } from './menu/menu-item/components/MenuItemMultiSelectTag';
-export { MenuItemNavigate } from './menu/menu-item/components/MenuItemNavigate';
+export { MenuItemMultiSelectTag } from './menu/menu-item/components/MenuItemMultiSelectTag';
 export type { MenuItemNavigateProps } from './menu/menu-item/components/MenuItemNavigate';
-export {
-  MenuItemSelect, StyledMenuItemSelect
-} from './menu/menu-item/components/MenuItemSelect';
+export { MenuItemNavigate } from './menu/menu-item/components/MenuItemNavigate';
 export type { MenuItemSelectProps } from './menu/menu-item/components/MenuItemSelect';
-export { MenuItemSelectAvatar } from './menu/menu-item/components/MenuItemSelectAvatar';
+export {
+  StyledMenuItemSelect,
+  MenuItemSelect,
+} from './menu/menu-item/components/MenuItemSelect';
 export type { MenuItemSelectAvatarProps } from './menu/menu-item/components/MenuItemSelectAvatar';
-export { MenuItemSelectColor } from './menu/menu-item/components/MenuItemSelectColor';
+export { MenuItemSelectAvatar } from './menu/menu-item/components/MenuItemSelectAvatar';
 export type {
   ColorLabels,
-  MenuItemSelectColorProps
+  MenuItemSelectColorProps,
 } from './menu/menu-item/components/MenuItemSelectColor';
-export { MenuItemSelectTag } from './menu/menu-item/components/MenuItemSelectTag';
+export { MenuItemSelectColor } from './menu/menu-item/components/MenuItemSelectColor';
 export type { MenuItemSelectTagProps } from './menu/menu-item/components/MenuItemSelectTag';
-export { MenuItemSuggestion } from './menu/menu-item/components/MenuItemSuggestion';
+export { MenuItemSelectTag } from './menu/menu-item/components/MenuItemSelectTag';
 export type { MenuItemSuggestionProps } from './menu/menu-item/components/MenuItemSuggestion';
-export { MenuItemToggle } from './menu/menu-item/components/MenuItemToggle';
+export { MenuItemSuggestion } from './menu/menu-item/components/MenuItemSuggestion';
 export type { MenuItemToggleProps } from './menu/menu-item/components/MenuItemToggle';
-export { MenuItemIcon } from './menu/menu-item/internals/components/MenuItemIcon';
+export { MenuItemToggle } from './menu/menu-item/components/MenuItemToggle';
 export type { MenuItemIconProps } from './menu/menu-item/internals/components/MenuItemIcon';
-export { MenuItemIconWithGripSwap } from './menu/menu-item/internals/components/MenuItemIconWithGripSwap';
-export type { MenuItemIconWithGripSwapProps } from './menu/menu-item/internals/components/MenuItemIconWithGripSwap';
-export { MenuItemLeftContent } from './menu/menu-item/internals/components/MenuItemLeftContent';
-export type { MenuItemLeftContentProps } from './menu/menu-item/internals/components/MenuItemLeftContent';
-export { StyledIconContainer } from './menu/menu-item/internals/components/StyledIconContainer';
+export { MenuItemIcon } from './menu/menu-item/internals/components/MenuItemIcon';
 export {
-  StyledDraggableItem,
-  StyledHoverableMenuItemBase, StyledMenuItemBase, StyledMenuItemContextualText, StyledMenuItemIconCheck, StyledMenuItemLabel,
-  StyledMenuItemLabelLight, StyledMenuItemLeftContent,
-  StyledMenuItemRightContent, StyledNoIconFiller, StyledRightMenuItemContextualText
-} from './menu/menu-item/internals/components/StyledMenuItemBase';
+  StyledIconContainer,
+  MenuItemIconBoxContainer,
+} from './menu/menu-item/internals/components/MenuItemIconBoxContainer';
+export type { MenuItemIconWithGripSwapProps } from './menu/menu-item/internals/components/MenuItemIconWithGripSwap';
+export { MenuItemIconWithGripSwap } from './menu/menu-item/internals/components/MenuItemIconWithGripSwap';
+export type { MenuItemLeftContentProps } from './menu/menu-item/internals/components/MenuItemLeftContent';
+export { MenuItemLeftContent } from './menu/menu-item/internals/components/MenuItemLeftContent';
 export type { MenuItemBaseProps } from './menu/menu-item/internals/components/StyledMenuItemBase';
+export {
+  StyledMenuItemBase,
+  StyledMenuItemLabel,
+  StyledMenuItemLabelLight,
+  StyledNoIconFiller,
+  StyledMenuItemLeftContent,
+  StyledMenuItemRightContent,
+  StyledDraggableItem,
+  StyledHoverableMenuItemBase,
+  StyledMenuItemIconCheck,
+  StyledMenuItemContextualText,
+  StyledRightMenuItemContextualText,
+} from './menu/menu-item/internals/components/StyledMenuItemBase';
 export type { MenuItemAccent } from './menu/menu-item/types/MenuItemAccent';
 export type { MenuItemDraggableGripMode } from './menu/menu-item/types/MenuItemDraggableGripMode';
-export { NavigationBar } from './navigation-bar/components/NavigationBar';
 export type { NavigationBarProps } from './navigation-bar/components/NavigationBar';
-export { NavigationBarItem } from './navigation-bar/components/NavigationBarItem';
+export { NavigationBar } from './navigation-bar/components/NavigationBar';
 export type { NavigationBarItemProps } from './navigation-bar/components/NavigationBarItem';
-export { NotificationCounter } from './notification-counter/components/NotificationCounter';
+export { NavigationBarItem } from './navigation-bar/components/NavigationBarItem';
 export type { NotificationCounterProps } from './notification-counter/components/NotificationCounter';
-
+export { NotificationCounter } from './notification-counter/components/NotificationCounter';
