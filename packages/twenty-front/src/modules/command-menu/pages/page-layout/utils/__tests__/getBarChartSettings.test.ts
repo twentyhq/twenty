fa15b90d6e040d8a82f35c938433509a3411c301
ehconitin <nitinkoche03@gmail.com>
2026-02-13 16:02:01 +0530

    fix test

--- packages/twenty-front/src/modules/command-menu/pages/page-layout/utils/__tests__/getBarChartSettings.test.ts
+++ packages/twenty-front/src/modules/command-menu/pages/page-layout/utils/__tests__/getBarChartSettings.test.ts
@@ -31,7 +31,7 @@ describe('getBarChartSettings', () => {
       );
 
       expect(xAxisGroup).toBeDefined();
-      expect(xAxisGroup?.items).toHaveLength(4);
+      expect(xAxisGroup?.items).toHaveLength(5);
       expect(xAxisGroup?.items[0].id).toBe(DATA_DISPLAY_X_SETTING.id);
       expect(xAxisGroup?.items[0].label).toBe(DATA_DISPLAY_X_SETTING.label);
       expect(xAxisGroup?.items[0].Icon).toBe(IconAxisX);
@@ -100,7 +100,7 @@ describe('getBarChartSettings', () => {
       );
 
       expect(yAxisGroup).toBeDefined();
-      expect(yAxisGroup?.items).toHaveLength(4);
+      expect(yAxisGroup?.items).toHaveLength(5);
       expect(yAxisGroup?.items[0].id).toBe(DATA_DISPLAY_X_SETTING.id);
       expect(yAxisGroup?.items[0].label).toBe(DATA_DISPLAY_X_SETTING.label);
       expect(yAxisGroup?.items[0].Icon).toBe(IconAxisY);
