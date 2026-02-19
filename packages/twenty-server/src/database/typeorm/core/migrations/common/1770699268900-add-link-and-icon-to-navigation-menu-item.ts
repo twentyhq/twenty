ab6839d9bb1963d70a51e394bb5e1e6b5da69c50
Abdul Rahman <ar5438376@gmail.com>
2026-02-19 15:38:42 +0530

    Rename migration class for adding link to navigation menu item for clarity

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1770699268900-add-link-and-icon-to-navigation-menu-item.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1770699268900-add-link-and-icon-to-navigation-menu-item.ts
@@ -1,9 +1,9 @@
 import { type MigrationInterface, type QueryRunner } from 'typeorm';
 
-export class AddLinkAndIconToNavigationMenuItem1770699268900
+export class AddLinkToNavigationMenuItem1770256542802
   implements MigrationInterface
 {
-  name = 'AddLinkAndIconToNavigationMenuItem1770699268900';
+  name = 'AddLinkToNavigationMenuItem1770256542802';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
