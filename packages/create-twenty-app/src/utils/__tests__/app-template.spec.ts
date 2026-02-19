f99b074742e5acf9c3a9d86696b5d95f768d40dd
prastoin <paul@twenty.com>
2026-02-19 15:53:48 +0100

    fix(create): tsc

--- packages/create-twenty-app/src/utils/__tests__/app-template.spec.ts
+++ packages/create-twenty-app/src/utils/__tests__/app-template.spec.ts
@@ -1,8 +1,8 @@
-import * as fs from 'fs-extra';
-import { join } from 'path';
-import { tmpdir } from 'os';
-import { copyBaseApplicationProject } from '@/utils/app-template';
 import { type ExampleOptions } from '@/types/scaffolding-options';
+import { copyBaseApplicationProject } from '@/utils/app-template';
+import * as fs from 'fs-extra';
+import { tmpdir } from 'os';
+import { join } from 'path';
 
 // Mock fs-extra's copy function to skip copying base template (not available during tests)
 jest.mock('fs-extra', () => {
@@ -23,11 +23,13 @@ const ALL_EXAMPLES: ExampleOptions = {
   includeExampleFrontComponent: true,
   includeExampleView: true,
   includeExampleNavigationMenuItem: true,
+  includeExampleSkill: true,
 };
 
 const NO_EXAMPLES: ExampleOptions = {
   includeExampleObject: false,
   includeExampleField: false,
+  includeExampleSkill: false,
   includeExampleLogicFunction: false,
   includeExampleFrontComponent: false,
   includeExampleView: false,
@@ -437,6 +439,7 @@ describe('copyBaseApplicationProject', () => {
           exampleOptions: {
             includeExampleObject: false,
             includeExampleField: false,
+            includeExampleSkill: false,
             includeExampleLogicFunction: false,
             includeExampleFrontComponent: true,
             includeExampleView: false,
@@ -472,6 +475,7 @@ describe('copyBaseApplicationProject', () => {
           appDirectory: testAppDirectory,
           exampleOptions: {
             includeExampleObject: false,
+            includeExampleSkill: false,
             includeExampleField: false,
             includeExampleLogicFunction: true,
             includeExampleFrontComponent: false,
