6d05beb64ed5a80023a792f1c479c4316befed4c
Devessier <baptiste@devessier.fr>
2026-02-24 17:14:41 +0100

    feat: clean

--- packages/twenty-apps/internal/twentyfortwenty/src/application-config.ts
+++ packages/twenty-apps/internal/twentyfortwenty/src/application-config.ts
@@ -1,11 +1,9 @@
-import { defineApplication } from 'twenty-sdk';
 import { DEFAULT_ROLE_UNIVERSAL_IDENTIFIER } from 'src/roles/default-role';
-import { POST_INSTALL_UNIVERSAL_IDENTIFIER } from 'src/logic-functions/post-install';
+import { defineApplication } from 'twenty-sdk';
 
 export default defineApplication({
   universalIdentifier: '0fac8de4-9d11-492b-9e6a-577e11e1c442',
   displayName: 'Twenty for Twenty',
   description: '',
   defaultRoleUniversalIdentifier: DEFAULT_ROLE_UNIVERSAL_IDENTIFIER,
-  postInstallLogicFunctionUniversalIdentifier: POST_INSTALL_UNIVERSAL_IDENTIFIER,
 });
