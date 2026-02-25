23d21d3a7001c18ba65c2e5d615481a183eaaf1e
martmull <martmull@hotmail.fr>
2026-02-25 11:08:21 +0100

    Improve create-twenty-app command

--- packages/create-twenty-app/src/utils/install.ts
+++ packages/create-twenty-app/src/utils/install.ts
@@ -6,7 +6,8 @@ const execPromise = promisify(exec);
 
 export const install = async (root: string) => {
   try {
-    await execPromise('yarn', { cwd: root });
+    await execPromise('corepack enable', { cwd: root });
+    await execPromise('yarn install', { cwd: root });
   } catch (error: any) {
     console.error(chalk.red('yarn install failed:'), error.stdout);
   }
