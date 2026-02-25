b51945b3d6cc0dbff4e0a01d435f4bcd73aec644
martmull <martmull@hotmail.fr>
2026-02-25 14:03:50 +0100

    Isolate install steps

--- packages/create-twenty-app/src/utils/install.ts
+++ packages/create-twenty-app/src/utils/install.ts
@@ -7,6 +7,11 @@ const execPromise = promisify(exec);
 export const install = async (root: string) => {
   try {
     await execPromise('corepack enable', { cwd: root });
+  } catch (error: any) {
+    console.warn(chalk.yellow('corepack enabled failed:'), error.stderr);
+  }
+
+  try {
     await execPromise('yarn install', { cwd: root });
   } catch (error: any) {
     console.error(chalk.red('yarn install failed:'), error.stdout);
