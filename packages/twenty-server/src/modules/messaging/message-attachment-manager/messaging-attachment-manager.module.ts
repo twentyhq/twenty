68a65913367b2bc8a4c07173d12f486592d8f3ed
neo773 <neo773@protonmail.com>
2026-02-26 05:46:00 +0530

    wip

--- packages/twenty-server/src/modules/messaging/message-attachment-manager/messaging-attachment-manager.module.ts
+++ packages/twenty-server/src/modules/messaging/message-attachment-manager/messaging-attachment-manager.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 
+import { TokenModule } from 'src/engine/core-modules/auth/token/token.module';
+import { WorkspaceCacheStorageModule } from 'src/engine/workspace-cache-storage/workspace-cache-storage.module';
 import { OAuth2ClientManagerModule } from 'src/modules/connected-account/oauth2-client-manager/oauth2-client-manager.module';
 import { MessageAttachmentController } from 'src/modules/messaging/message-attachment-manager/controllers/message-attachment.controller';
 import { GmailAttachmentDownloadService } from 'src/modules/messaging/message-attachment-manager/drivers/gmail-attachment-download.service';
@@ -9,7 +11,12 @@ import { MessagingAttachmentDownloadService } from 'src/modules/messaging/messag
 import { MessagingIMAPDriverModule } from 'src/modules/messaging/message-import-manager/drivers/imap/messaging-imap-driver.module';
 
 @Module({
-  imports: [OAuth2ClientManagerModule, MessagingIMAPDriverModule],
+  imports: [
+    TokenModule,
+    WorkspaceCacheStorageModule,
+    OAuth2ClientManagerModule,
+    MessagingIMAPDriverModule,
+  ],
   controllers: [MessageAttachmentController],
   providers: [
     MessagingAttachmentDownloadService,
