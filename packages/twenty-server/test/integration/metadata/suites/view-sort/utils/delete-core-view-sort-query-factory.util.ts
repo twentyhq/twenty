f0cf9a1b668731733cfc988bbcd80bcf7a993b6a
prastoin <paul@twenty.com>
2026-03-04 11:27:18 +0100

    lint

--- packages/twenty-server/test/integration/metadata/suites/view-sort/utils/delete-core-view-sort-query-factory.util.ts
+++ packages/twenty-server/test/integration/metadata/suites/view-sort/utils/delete-core-view-sort-query-factory.util.ts
@@ -1,5 +1,4 @@
 import gql from 'graphql-tag';
-import { VIEW_SORT_GQL_FIELDS } from 'test/integration/constants/view-gql-fields.constants';
 import { type PerformMetadataQueryParams } from 'test/integration/metadata/types/perform-metadata-query.type';
 
 import { type DeleteViewSortInput } from 'src/engine/metadata-modules/view-sort/dtos/inputs/delete-view-sort.input';
