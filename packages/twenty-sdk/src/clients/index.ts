b392d250fdb781c3b310e8d33cc544105c3d1932
prastoin <paul@twenty.com>
2026-03-05 19:28:52 +0100

    chore(sdk): commit client

--- packages/twenty-sdk/src/clients/index.ts
+++ packages/twenty-sdk/src/clients/index.ts
@@ -1,5 +1,5 @@
 export { CoreApiClient } from './generated/core/index';
-export { CoreSchema } from './generated/core/schema';
+export * as CoreSchema from './generated/core/schema';
 export { MetadataApiClient } from './generated/metadata/index';
-export { MetadataSchema } from './generated/metadata/schema';
+export * as MetadataSchema from './generated/metadata/schema';
 
