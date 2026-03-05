1a56bc92689e775d976bb63c103579bbe906951d
prastoin <paul@twenty.com>
2026-03-05 15:37:13 +0100

    fix kebab

--- packages/twenty-sdk/src/cli/utilities/string/kebab-case.ts
+++ packages/twenty-sdk/src/cli/utilities/string/kebab-case.ts
@@ -1,6 +1,8 @@
 export const kebabCase = (input: string): string =>
   input
-    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
     .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .replace(/([a-zA-Z])(\d)/g, '$1-$2')
+    .replace(/(\d)([a-zA-Z])/g, '$1-$2')
     .replace(/[\s_]+/g, '-')
     .toLowerCase();
