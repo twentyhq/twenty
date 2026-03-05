468e3365ef59096611bf947cdead4ba61f8e43af
bosiraphael <raphael.bosi@gmail.com>
2026-03-05 13:37:37 +0100

    create standard command menu items

---
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/command-menu-item/build-standard-flat-command-menu-item-maps.util.ts
@@ -0,0 +1,50 @@
+import { v4 } from 'uuid';
+
+import { type FlatCommandMenuItemMaps } from 'src/engine/metadata-modules/flat-command-menu-item/types/flat-command-menu-item-maps.type';
+import { createEmptyFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/constant/create-empty-flat-entity-maps.constant';
+import { type FlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-maps.type';
+import { type FlatFrontComponent } from 'src/engine/metadata-modules/flat-front-component/types/flat-front-component.type';
+import { STANDARD_COMMAND_MENU_ITEMS } from 'src/engine/workspace-manager/twenty-standard-application/constants/standard-command-menu-item.constant';
+import { createStandardCommandMenuItemFlatMetadata } from 'src/engine/workspace-manager/twenty-standard-application/utils/command-menu-item/create-standard-command-menu-item-flat-metadata.util';
+import { addFlatEntityToFlatEntityMapsThroughMutationOrThrow } from 'src/engine/workspace-manager/workspace-migration/utils/add-flat-entity-to-flat-entity-maps-through-mutation-or-throw.util';
+
+const STANDARD_COMMAND_MENU_ITEM_NAMES = Object.keys(
+  STANDARD_COMMAND_MENU_ITEMS,
+) as Array<keyof typeof STANDARD_COMMAND_MENU_ITEMS>;
+
+export const buildStandardFlatCommandMenuItemMaps = ({
+  now,
+  workspaceId,
+  twentyStandardApplicationId,
+  dependencyFlatEntityMaps: { flatFrontComponentMaps },
+}: {
+  now: string;
+  workspaceId: string;
+  twentyStandardApplicationId: string;
+  dependencyFlatEntityMaps: {
+    flatFrontComponentMaps: FlatEntityMaps<FlatFrontComponent>;
+  };
+}): FlatCommandMenuItemMaps => {
+  const flatCommandMenuItemMaps: FlatCommandMenuItemMaps =
+    createEmptyFlatEntityMaps();
+
+  for (const commandMenuItemName of STANDARD_COMMAND_MENU_ITEM_NAMES) {
+    const flatCommandMenuItem = createStandardCommandMenuItemFlatMetadata({
+      commandMenuItemName,
+      commandMenuItemId: v4(),
+      workspaceId,
+      twentyStandardApplicationId,
+      dependencyFlatEntityMaps: {
+        flatFrontComponentMaps,
+      },
+      now,
+    });
+
+    addFlatEntityToFlatEntityMapsThroughMutationOrThrow({
+      flatEntity: flatCommandMenuItem,
+      flatEntityMapsToMutate: flatCommandMenuItemMaps,
+    });
+  }
+
+  return flatCommandMenuItemMaps;
+};
---
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/command-menu-item/create-standard-command-menu-item-flat-metadata.util.ts
@@ -0,0 +1,62 @@
+import { isDefined } from 'twenty-shared/utils';
+
+import { type FlatCommandMenuItem } from 'src/engine/metadata-modules/flat-command-menu-item/types/flat-command-menu-item.type';
+import { type FlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/flat-entity-maps.type';
+import { findFlatEntityByUniversalIdentifier } from 'src/engine/metadata-modules/flat-entity/utils/find-flat-entity-by-universal-identifier.util';
+import { type FlatFrontComponent } from 'src/engine/metadata-modules/flat-front-component/types/flat-front-component.type';
+import { STANDARD_COMMAND_MENU_ITEMS } from 'src/engine/workspace-manager/twenty-standard-application/constants/standard-command-menu-item.constant';
+import { TWENTY_STANDARD_APPLICATION } from 'src/engine/workspace-manager/twenty-standard-application/constants/twenty-standard-applications';
+
+export const createStandardCommandMenuItemFlatMetadata = ({
+  commandMenuItemName,
+  commandMenuItemId,
+  workspaceId,
+  twentyStandardApplicationId,
+  dependencyFlatEntityMaps: { flatFrontComponentMaps },
+  now,
+}: {
+  commandMenuItemName: keyof typeof STANDARD_COMMAND_MENU_ITEMS;
+  commandMenuItemId: string;
+  workspaceId: string;
+  twentyStandardApplicationId: string;
+  dependencyFlatEntityMaps: {
+    flatFrontComponentMaps: FlatEntityMaps<FlatFrontComponent>;
+  };
+  now: string;
+}): FlatCommandMenuItem => {
+  const definition = STANDARD_COMMAND_MENU_ITEMS[commandMenuItemName];
+
+  const flatFrontComponent = findFlatEntityByUniversalIdentifier({
+    flatEntityMaps: flatFrontComponentMaps,
+    universalIdentifier: definition.frontComponentUniversalIdentifier,
+  });
+
+  if (!isDefined(flatFrontComponent)) {
+    throw new Error(
+      `Front component not found for universal identifier ${definition.frontComponentUniversalIdentifier}`,
+    );
+  }
+
+  return {
+    id: commandMenuItemId,
+    universalIdentifier: definition.universalIdentifier,
+    applicationId: twentyStandardApplicationId,
+    applicationUniversalIdentifier:
+      TWENTY_STANDARD_APPLICATION.universalIdentifier,
+    workspaceId,
+    label: definition.label,
+    shortLabel: definition.shortLabel,
+    icon: definition.icon,
+    position: definition.position,
+    isPinned: definition.isPinned,
+    availabilityType: definition.availabilityType,
+    conditionalAvailabilityExpression: null,
+    frontComponentId: flatFrontComponent.id,
+    frontComponentUniversalIdentifier: flatFrontComponent.universalIdentifier,
+    workflowVersionId: null,
+    availabilityObjectMetadataId: null,
+    availabilityObjectMetadataUniversalIdentifier: null,
+    createdAt: now,
+    updatedAt: now,
+  };
+};
