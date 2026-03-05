7f591a6c2be9dbd9b4910951e5ddc59396c61537
Félix Malfait <felix@twenty.com>
2026-03-05 09:29:50 +0100

    Improve style

--- packages/twenty-front/src/pages/settings/applications/tabs/SettingsApplicationsInstalledTab.tsx
+++ packages/twenty-front/src/pages/settings/applications/tabs/SettingsApplicationsInstalledTab.tsx
@@ -1,24 +1,31 @@
+import { Dropdown } from '@/ui/layout/dropdown/components/Dropdown';
+import { DropdownContent } from '@/ui/layout/dropdown/components/DropdownContent';
+import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/DropdownMenuItemsContainer';
+import { useCloseDropdown } from '@/ui/layout/dropdown/hooks/useCloseDropdown';
+import { useModal } from '@/ui/layout/modal/hooks/useModal';
 import { styled } from '@linaria/react';
 import { useLingui } from '@lingui/react/macro';
-import { IconDownload, IconUpload } from 'twenty-ui/display';
-import { Button } from 'twenty-ui/input';
+import { IconChevronDown, IconDownload, IconUpload } from 'twenty-ui/display';
+import { Button, ButtonGroup, IconButton } from 'twenty-ui/input';
 import { Section } from 'twenty-ui/layout';
+import { MenuItem } from 'twenty-ui/navigation';
 import { themeCssVariables } from 'twenty-ui/theme-constants';
-import { useModal } from '@/ui/layout/modal/hooks/useModal';
 import { useFindManyApplicationsQuery } from '~/generated-metadata/graphql';
 import { SettingsApplicationsTable } from '~/pages/settings/applications/components/SettingsApplicationsTable';
 import {
-  SettingsInstallNpmAppModal,
   INSTALL_NPM_APP_MODAL_ID,
+  SettingsInstallNpmAppModal,
 } from '~/pages/settings/applications/components/SettingsInstallNpmAppModal';
 import {
   SettingsUploadTarballModal,
   UPLOAD_TARBALL_MODAL_ID,
 } from '~/pages/settings/applications/components/SettingsUploadTarballModal';
 
-const StyledButtonGroup = styled.div`
+const INSTALL_APP_DROPDOWN_ID = 'install-app-dropdown';
+
+const StyledButtonGroupContainer = styled.div`
   display: flex;
-  gap: ${themeCssVariables.spacing[2]};
+  justify-content: flex-end;
   margin-bottom: ${themeCssVariables.spacing[4]};
 `;
 
@@ -26,33 +33,64 @@ export const SettingsApplicationsInstalledTab = () => {
   const { t } = useLingui();
   const { data } = useFindManyApplicationsQuery();
   const { openModal } = useModal();
+  const { closeDropdown } = useCloseDropdown();
 
   const applications = data?.findManyApplications ?? [];
 
+  const handleInstallFromNpm = () => {
+    closeDropdown(INSTALL_APP_DROPDOWN_ID);
+    openModal(INSTALL_NPM_APP_MODAL_ID);
+  };
+
+  const handleUploadTarball = () => {
+    closeDropdown(INSTALL_APP_DROPDOWN_ID);
+    openModal(UPLOAD_TARBALL_MODAL_ID);
+  };
+
   return (
     <>
-      <Section>
-        <StyledButtonGroup>
-          <Button
-            Icon={IconDownload}
-            title={t`Install from npm`}
-            variant="secondary"
-            size="small"
-            onClick={() => openModal(INSTALL_NPM_APP_MODAL_ID)}
-          />
-          <Button
-            Icon={IconUpload}
-            title={t`Upload tarball`}
-            variant="secondary"
-            size="small"
-            onClick={() => openModal(UPLOAD_TARBALL_MODAL_ID)}
-          />
-        </StyledButtonGroup>
-      </Section>
-
       {applications.length > 0 && (
         <SettingsApplicationsTable applications={applications} />
       )}
+
+      <Section>
+        <StyledButtonGroupContainer>
+          <ButtonGroup size="small" variant="secondary">
+            <Button
+              Icon={IconDownload}
+              title={t`Install app`}
+              onClick={handleInstallFromNpm}
+            />
+            <Dropdown
+              dropdownId={INSTALL_APP_DROPDOWN_ID}
+              clickableComponent={
+                <IconButton
+                  size="small"
+                  variant="secondary"
+                  Icon={IconChevronDown}
+                  position="right"
+                />
+              }
+              dropdownComponents={
+                <DropdownContent>
+                  <DropdownMenuItemsContainer>
+                    <MenuItem
+                      LeftIcon={IconDownload}
+                      text={t`Install from npm`}
+                      onClick={handleInstallFromNpm}
+                    />
+                    <MenuItem
+                      LeftIcon={IconUpload}
+                      text={t`Upload tarball`}
+                      onClick={handleUploadTarball}
+                    />
+                  </DropdownMenuItemsContainer>
+                </DropdownContent>
+              }
+            />
+          </ButtonGroup>
+        </StyledButtonGroupContainer>
+      </Section>
 
       <SettingsInstallNpmAppModal />
       <SettingsUploadTarballModal />
