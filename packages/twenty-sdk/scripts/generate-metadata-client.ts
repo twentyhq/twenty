06fe42f99c44b1007b93c83345635c64bb36170e
prastoin <paul@twenty.com>
2026-03-06 12:07:30 +0100

    chore

--- packages/twenty-sdk/scripts/generate-metadata-client.ts
+++ packages/twenty-sdk/scripts/generate-metadata-client.ts
@@ -13,7 +13,7 @@ const main = async () => {
   );
 
   const serverUrl = process.env.TWENTY_API_URL ?? 'http://localhost:3000';
-  const token = process.env.TWENTY_API_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMDIwMjAyMC1lNmI1LTQ2ODAtOGEzMi1iODIwOTczNzE1NmIiLCJ1c2VySWQiOiIyMDIwMjAyMC1lNmI1LTQ2ODAtOGEzMi1iODIwOTczNzE1NmIiLCJ3b3Jrc3BhY2VJZCI6IjIwMjAyMDIwLTFjMjUtNGQwMi1iZjI1LTZhZWNjZjdlYTQxOSIsIndvcmtzcGFjZU1lbWJlcklkIjoiMjAyMDIwMjAtNDYzZi00MzViLTgyOGMtMTA3ZTAwN2EyNzExIiwidXNlcldvcmtzcGFjZUlkIjoiMjAyMDIwMjAtMWU3Yy00M2Q5LWE1ZGItNjg1YjUwNjlkODE2IiwidHlwZSI6IkFDQ0VTUyIsImF1dGhQcm92aWRlciI6InBhc3N3b3JkIiwiaWF0IjoxNzUxMjgxNzA0LCJleHAiOjIwNjY4NTc3MDR9.HMGqCsVlOAPVUBhKSGlD1X86VoHKt4LIUtET3CGIdik';
+  const token = process.env.TWENTY_API_KEY;
 
   const clientService = new ClientService({ serverUrl, token });
 
