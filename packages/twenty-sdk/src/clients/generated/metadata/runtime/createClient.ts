546c34854dc65a98e818a81533d6700a2c5d5295
prastoin <paul@twenty.com>
2026-03-09 15:44:06 +0100

    lint

--- packages/twenty-sdk/src/clients/generated/metadata/runtime/createClient.ts
+++ packages/twenty-sdk/src/clients/generated/metadata/runtime/createClient.ts
@@ -1,68 +1,67 @@
 // @ts-nocheck
 
-import  { type BatchOptions, createFetcher } from './fetcher'
-import type { ExecutionResult, LinkedType } from './types'
+import { type BatchOptions, createFetcher } from './fetcher';
+import type { ExecutionResult, LinkedType } from './types';
 import {
-    generateGraphqlOperation,
-    type GraphqlOperation,
-} from './generateGraphqlOperation'
+  generateGraphqlOperation,
+  type GraphqlOperation,
+} from './generateGraphqlOperation';
 
 export type Headers =
-    | HeadersInit
-    | (() => HeadersInit)
-    | (() => Promise<HeadersInit>)
+  | HeadersInit
+  | (() => HeadersInit)
+  | (() => Promise<HeadersInit>);
 
 export type BaseFetcher = (
-    operation: GraphqlOperation | GraphqlOperation[],
-) => Promise<ExecutionResult | ExecutionResult[]>
+  operation: GraphqlOperation | GraphqlOperation[],
+) => Promise<ExecutionResult | ExecutionResult[]>;
 
 export type ClientOptions = Omit<RequestInit, 'body' | 'headers'> & {
-    url?: string
-    batch?: BatchOptions | boolean
-    fetcher?: BaseFetcher
-    fetch?: Function
-    headers?: Headers
-}
+  url?: string;
+  batch?: BatchOptions | boolean;
+  fetcher?: BaseFetcher;
+  fetch?: Function;
+  headers?: Headers;
+};
 
 export const createClient = ({
-    queryRoot,
-    mutationRoot,
-    subscriptionRoot,
-    ...options
+  queryRoot,
+  mutationRoot,
+  subscriptionRoot,
+  ...options
 }: ClientOptions & {
-    queryRoot?: LinkedType
-    mutationRoot?: LinkedType
-    subscriptionRoot?: LinkedType
+  queryRoot?: LinkedType;
+  mutationRoot?: LinkedType;
+  subscriptionRoot?: LinkedType;
 }) => {
-    const fetcher = createFetcher(options)
-    const client: {
-        query?: Function
-        mutation?: Function
-    } = {}
+  const fetcher = createFetcher(options);
+  const client: {
+    query?: Function;
+    mutation?: Function;
+  } = {};
 
-    if (queryRoot) {
-        client.query = (request: any) => {
-            if (!queryRoot) throw new Error('queryRoot argument is missing')
+  if (queryRoot) {
+    client.query = (request: any) => {
+      if (!queryRoot) throw new Error('queryRoot argument is missing');
 
-            const resultPromise = fetcher(
-                generateGraphqlOperation('query', queryRoot, request),
-            )
+      const resultPromise = fetcher(
+        generateGraphqlOperation('query', queryRoot, request),
+      );
 
-            return resultPromise
-        }
-    }
-    if (mutationRoot) {
-        client.mutation = (request: any) => {
-            if (!mutationRoot)
-                throw new Error('mutationRoot argument is missing')
+      return resultPromise;
+    };
+  }
+  if (mutationRoot) {
+    client.mutation = (request: any) => {
+      if (!mutationRoot) throw new Error('mutationRoot argument is missing');
 
-            const resultPromise = fetcher(
-                generateGraphqlOperation('mutation', mutationRoot, request),
-            )
+      const resultPromise = fetcher(
+        generateGraphqlOperation('mutation', mutationRoot, request),
+      );
 
-            return resultPromise
-        }
-    }
+      return resultPromise;
+    };
+  }
 
-    return client as any
-}
+  return client as any;
+};
--- packages/twenty-sdk/src/clients/generated/metadata/runtime/types.ts
+++ packages/twenty-sdk/src/clients/generated/metadata/runtime/types.ts
@@ -1,69 +1,69 @@
 // @ts-nocheck
 
 export interface ExecutionResult<TData = { [key: string]: any }> {
-    errors?: Array<Error>
-    data?: TData | null
+  errors?: Array<Error>;
+  data?: TData | null;
 }
 
 export interface ArgMap<keyType = number> {
-    [arg: string]: [keyType, string] | [keyType] | undefined
+  [arg: string]: [keyType, string] | [keyType] | undefined;
 }
 
 export type CompressedField<keyType = number> = [
-    type: keyType,
-    args?: ArgMap<keyType>,
-]
+  type: keyType,
+  args?: ArgMap<keyType>,
+];
 
 export interface CompressedFieldMap<keyType = number> {
-    [field: string]: CompressedField<keyType> | undefined
+  [field: string]: CompressedField<keyType> | undefined;
 }
 
-export type CompressedType<keyType = number> = CompressedFieldMap<keyType>
+export type CompressedType<keyType = number> = CompressedFieldMap<keyType>;
 
 export interface CompressedTypeMap<keyType = number> {
-    scalars: Array<keyType>
-    types: {
-        [type: string]: CompressedType<keyType> | undefined
-    }
+  scalars: Array<keyType>;
+  types: {
+    [type: string]: CompressedType<keyType> | undefined;
+  };
 }
 
 // normal types
 export type Field<keyType = number> = {
-    type: keyType
-    args?: ArgMap<keyType>
-}
+  type: keyType;
+  args?: ArgMap<keyType>;
+};
 
 export interface FieldMap<keyType = number> {
-    [field: string]: Field<keyType> | undefined
+  [field: string]: Field<keyType> | undefined;
 }
 
-export type Type<keyType = number> = FieldMap<keyType>
+export type Type<keyType = number> = FieldMap<keyType>;
 
 export interface TypeMap<keyType = number> {
-    scalars: Array<keyType>
-    types: {
-        [type: string]: Type<keyType> | undefined
-    }
+  scalars: Array<keyType>;
+  types: {
+    [type: string]: Type<keyType> | undefined;
+  };
 }
 
 export interface LinkedArgMap {
-    [arg: string]: [LinkedType, string] | undefined
+  [arg: string]: [LinkedType, string] | undefined;
 }
 export interface LinkedField {
-    type: LinkedType
-    args?: LinkedArgMap
+  type: LinkedType;
+  args?: LinkedArgMap;
 }
 
 export interface LinkedFieldMap {
-    [field: string]: LinkedField | undefined
+  [field: string]: LinkedField | undefined;
 }
 
 export interface LinkedType {
-    name: string
-    fields?: LinkedFieldMap
-    scalar?: string[]
+  name: string;
+  fields?: LinkedFieldMap;
+  scalar?: string[];
 }
 
 export interface LinkedTypeMap {
-    [type: string]: LinkedType | undefined
+  [type: string]: LinkedType | undefined;
 }
