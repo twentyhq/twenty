dfe4b956b9515caa45293548667919d03cd4be1a
Devessier <baptiste@devessier.fr>
2026-03-10 19:20:56 +0100

    refactor: restrict which pages are considered sub pages

--- packages/twenty-front/src/modules/side-panel/constants/SidePanelSubPages.ts
+++ packages/twenty-front/src/modules/side-panel/constants/SidePanelSubPages.ts
@@ -1,11 +1,6 @@
 import { SidePanelPages } from 'twenty-shared/types';
 
 export const SIDE_PANEL_SUB_PAGES = new Set<SidePanelPages>([
-  SidePanelPages.PageLayoutWidgetTypeSelect,
-  SidePanelPages.PageLayoutGraphTypeSelect,
   SidePanelPages.PageLayoutGraphFilter,
-  SidePanelPages.PageLayoutIframeSettings,
-  SidePanelPages.PageLayoutTabSettings,
   SidePanelPages.PageLayoutFieldsSettings,
-  SidePanelPages.PageLayoutFieldsLayout,
 ]);
