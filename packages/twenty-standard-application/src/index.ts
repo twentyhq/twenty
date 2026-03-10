246ec1da1ed3f382496401a3f68e8efa38effc1b
bosiraphael <raphael.bosi@gmail.com>
2026-03-10 17:24:38 +0100

    fix build

--- packages/twenty-standard-application/src/index.ts
+++ packages/twenty-standard-application/src/index.ts
@@ -1,1 +1,1 @@
-export { STANDARD_FRONT_COMPONENT_BUILD_MANIFEST } from './standard-front-component-build-manifest.ts';
+export { STANDARD_FRONT_COMPONENT_BUILD_MANIFEST } from './standard-front-component-build-manifest';
