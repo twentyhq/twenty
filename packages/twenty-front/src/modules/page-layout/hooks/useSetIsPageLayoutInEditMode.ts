67f8a0b0c7922559c87ffdffbd6517fbaf3a9c8a
Devessier <baptiste@devessier.fr>
2026-03-10 11:33:15 +0100

    feat: clear draft states so that last data are shown when starting to edit the fields widget

--- packages/twenty-front/src/modules/page-layout/hooks/useSetIsPageLayoutInEditMode.ts
+++ packages/twenty-front/src/modules/page-layout/hooks/useSetIsPageLayoutInEditMode.ts
@@ -1,10 +1,17 @@
-import { SIDE_PANEL_COMPONENT_INSTANCE_ID } from '@/side-panel/constants/SidePanelComponentInstanceId';
-import { isSidePanelOpenedState } from '@/side-panel/states/isSidePanelOpenedState';
 import { MAIN_CONTEXT_STORE_INSTANCE_ID } from '@/context-store/constants/MainContextStoreInstanceId';
 import { contextStoreIsPageInEditModeComponentState } from '@/context-store/states/contextStoreIsPageInEditModeComponentState';
 import { PageLayoutComponentInstanceContext } from '@/page-layout/states/contexts/PageLayoutComponentInstanceContext';
 import { currentPageLayoutIdState } from '@/page-layout/states/currentPageLayoutIdState';
+import { fieldsWidgetEditorModeDraftComponentState } from '@/page-layout/states/fieldsWidgetEditorModeDraftComponentState';
+import { fieldsWidgetEditorModePersistedComponentState } from '@/page-layout/states/fieldsWidgetEditorModePersistedComponentState';
+import { fieldsWidgetGroupsDraftComponentState } from '@/page-layout/states/fieldsWidgetGroupsDraftComponentState';
+import { fieldsWidgetGroupsPersistedComponentState } from '@/page-layout/states/fieldsWidgetGroupsPersistedComponentState';
+import { fieldsWidgetUngroupedFieldsDraftComponentState } from '@/page-layout/states/fieldsWidgetUngroupedFieldsDraftComponentState';
+import { fieldsWidgetUngroupedFieldsPersistedComponentState } from '@/page-layout/states/fieldsWidgetUngroupedFieldsPersistedComponentState';
+import { hasInitializedFieldsWidgetGroupsDraftComponentState } from '@/page-layout/states/hasInitializedFieldsWidgetGroupsDraftComponentState';
 import { isPageLayoutInEditModeComponentState } from '@/page-layout/states/isPageLayoutInEditModeComponentState';
+import { SIDE_PANEL_COMPONENT_INSTANCE_ID } from '@/side-panel/constants/SidePanelComponentInstanceId';
+import { isSidePanelOpenedState } from '@/side-panel/states/isSidePanelOpenedState';
 import { useAvailableComponentInstanceIdOrThrow } from '@/ui/utilities/state/component-state/hooks/useAvailableComponentInstanceIdOrThrow';
 import { useAtomComponentStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateCallbackState';
 import { useStore } from 'jotai';
@@ -27,10 +34,59 @@ export const useSetIsPageLayoutInEditMode = (pageLayoutIdFromProps: string) => {
       MAIN_CONTEXT_STORE_INSTANCE_ID,
     );
 
+  const fieldsWidgetGroupsDraftState = useAtomComponentStateCallbackState(
+    fieldsWidgetGroupsDraftComponentState,
+    pageLayoutId,
+  );
+
+  const fieldsWidgetGroupsPersistedState = useAtomComponentStateCallbackState(
+    fieldsWidgetGroupsPersistedComponentState,
+    pageLayoutId,
+  );
+
+  const fieldsWidgetUngroupedFieldsDraftState =
+    useAtomComponentStateCallbackState(
+      fieldsWidgetUngroupedFieldsDraftComponentState,
+      pageLayoutId,
+    );
+
+  const fieldsWidgetUngroupedFieldsPersistedState =
+    useAtomComponentStateCallbackState(
+      fieldsWidgetUngroupedFieldsPersistedComponentState,
+      pageLayoutId,
+    );
+
+  const fieldsWidgetEditorModeDraftState = useAtomComponentStateCallbackState(
+    fieldsWidgetEditorModeDraftComponentState,
+    pageLayoutId,
+  );
+
+  const fieldsWidgetEditorModePersistedState =
+    useAtomComponentStateCallbackState(
+      fieldsWidgetEditorModePersistedComponentState,
+      pageLayoutId,
+    );
+
+  const hasInitializedFieldsWidgetGroupsDraftState =
+    useAtomComponentStateCallbackState(
+      hasInitializedFieldsWidgetGroupsDraftComponentState,
+      pageLayoutId,
+    );
+
   const store = useStore();
 
   const setIsPageLayoutInEditMode = useCallback(
     (value: boolean) => {
+      if (value) {
+        store.set(fieldsWidgetGroupsDraftState, {});
+        store.set(fieldsWidgetGroupsPersistedState, {});
+        store.set(fieldsWidgetUngroupedFieldsDraftState, {});
+        store.set(fieldsWidgetUngroupedFieldsPersistedState, {});
+        store.set(fieldsWidgetEditorModeDraftState, {});
+        store.set(fieldsWidgetEditorModePersistedState, {});
+        store.set(hasInitializedFieldsWidgetGroupsDraftState, {});
+      }
+
       store.set(isPageLayoutInEditModeState, value);
 
       store.set(contextStoreIsFullTabWidgetInEditModeState, value);
@@ -51,6 +107,13 @@ export const useSetIsPageLayoutInEditMode = (pageLayoutIdFromProps: string) => {
     [
       isPageLayoutInEditModeState,
       contextStoreIsFullTabWidgetInEditModeState,
+      fieldsWidgetGroupsDraftState,
+      fieldsWidgetGroupsPersistedState,
+      fieldsWidgetUngroupedFieldsDraftState,
+      fieldsWidgetUngroupedFieldsPersistedState,
+      fieldsWidgetEditorModeDraftState,
+      fieldsWidgetEditorModePersistedState,
+      hasInitializedFieldsWidgetGroupsDraftState,
       pageLayoutId,
       store,
     ],
