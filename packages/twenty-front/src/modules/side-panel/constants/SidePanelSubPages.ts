922bb91f7a24a54b55ad2ef160879c97f6d93ab5
Devessier <baptiste@devessier.fr>
2026-03-10 19:41:51 +0100

    fix: use correct fields widget page

--- packages/twenty-front/src/modules/side-panel/constants/SidePanelSubPages.ts
+++ packages/twenty-front/src/modules/side-panel/constants/SidePanelSubPages.ts
@@ -2,5 +2,5 @@ import { SidePanelPages } from 'twenty-shared/types';
 
 export const SIDE_PANEL_SUB_PAGES = new Set<SidePanelPages>([
   SidePanelPages.PageLayoutGraphFilter,
-  SidePanelPages.PageLayoutFieldsSettings,
+  SidePanelPages.PageLayoutFieldsLayout,
 ]);
