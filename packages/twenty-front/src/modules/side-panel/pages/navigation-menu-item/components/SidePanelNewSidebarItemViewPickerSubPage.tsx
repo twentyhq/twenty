fd3737842b76cc4f158d04969acb815748fe03b7
Devessier <baptiste@devessier.fr>
2026-03-12 11:56:00 +0100

    fix: fix bug make it impossible to see views

--- packages/twenty-front/src/modules/side-panel/pages/navigation-menu-item/components/SidePanelNewSidebarItemViewPickerSubPage.tsx
+++ packages/twenty-front/src/modules/side-panel/pages/navigation-menu-item/components/SidePanelNewSidebarItemViewPickerSubPage.tsx
@@ -1,28 +1,14 @@
-import { useSidePanelSubPageHistory } from '@/side-panel/hooks/useSidePanelSubPageHistory';
 import { SidePanelNewSidebarItemViewPickerSubView } from '@/side-panel/pages/navigation-menu-item/components/SidePanelNewSidebarItemViewPickerSubView';
 import { selectedObjectMetadataIdForViewFlowState } from '@/side-panel/states/selectedObjectMetadataIdForViewFlowState';
 import { useAtomComponentStateValue } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateValue';
-import { useSetAtomComponentState } from '@/ui/utilities/state/jotai/hooks/useSetAtomComponentState';
-import { useEffect } from 'react';
 import { isDefined } from 'twenty-shared/utils';
 
 export const SidePanelNewSidebarItemViewPickerSubPage = () => {
   const selectedObjectMetadataIdForViewFlow = useAtomComponentStateValue(
     selectedObjectMetadataIdForViewFlowState,
   );
-  const setSelectedObjectMetadataIdForViewFlow = useSetAtomComponentState(
-    selectedObjectMetadataIdForViewFlowState,
-  );
-  const { goBackFromSidePanelSubPage } = useSidePanelSubPageHistory();
-
-  useEffect(() => {
-    return () => {
-      setSelectedObjectMetadataIdForViewFlow(null);
-    };
-  }, [setSelectedObjectMetadataIdForViewFlow]);
 
   if (!isDefined(selectedObjectMetadataIdForViewFlow)) {
-    goBackFromSidePanelSubPage();
     return null;
   }
 
