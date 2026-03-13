9030def1bd383a9f45ac3e3cf863f739f351664f
Thomas Trompette <thomas.trompette@sfr.fr>
2026-03-13 17:29:01 +0100

    Fix timeline activities + breadcrumb

--- packages/twenty-front/src/modules/activities/timeline-activities/rows/components/EventCard.tsx
+++ packages/twenty-front/src/modules/activities/timeline-activities/rows/components/EventCard.tsx
@@ -14,10 +14,11 @@ const StyledCardContainer = styled.div`
   gap: ${themeCssVariables.spacing[2]};
   padding: ${themeCssVariables.spacing[2]} 0px ${themeCssVariables.spacing[1]}
     0px;
-  width: 400px;
+  max-width: 400px;
+  width: 100%;
 
   @media (max-width: ${MOBILE_VIEWPORT}px) {
-    width: 300px;
+    max-width: 300px;
   }
 `;
 
@@ -32,6 +33,7 @@ const StyledCardInnerContainer = styled.div`
   flex-direction: column;
   gap: ${themeCssVariables.spacing[2]};
   justify-content: center;
+  overflow: hidden;
   padding: ${themeCssVariables.spacing[2]};
 `;
 
