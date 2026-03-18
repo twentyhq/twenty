aa7a38639e96051294f54c7d8922b08955b134a9
bosiraphael <raphael.bosi@gmail.com>
2026-03-18 10:22:45 +0100

    rename directory

--- packages/twenty-shared/src/utils/index.ts
+++ packages/twenty-shared/src/utils/index.ts
@@ -23,11 +23,11 @@ export { upsertIntoArrayOfObjectsComparingId } from './array/upsertIntoArrayOfOb
 export { upsertPropertiesOfItemIntoArrayOfObjectsComparingId } from './array/upsertPropertiesOfItemIntoArrayOfObjectsComparingId';
 export { assertUnreachable } from './assertUnreachable';
 export { base64UrlEncode } from './base64UrlEncode';
+export { conditionalAvailabilityParser } from './command-menu-items/conditionalAvailabilityParser';
+export { evaluateConditionalAvailabilityExpression } from './command-menu-items/evaluateConditionalAvailabilityExpression';
+export { interpolateCommandMenuItemLabel } from './command-menu-items/interpolateCommandMenuItemLabel';
+export { safeGetNestedProperty } from './command-menu-items/safeGetNestedProperty';
 export { computeDiffBetweenObjects } from './compute-diff-between-objects';
-export { conditionalAvailabilityParser } from './conditional-availability/conditionalAvailabilityParser';
-export { evaluateConditionalAvailabilityExpression } from './conditional-availability/evaluateConditionalAvailabilityExpression';
-export { interpolateCommandMenuItemLabel } from './conditional-availability/interpolateCommandMenuItemLabel';
-export { safeGetNestedProperty } from './conditional-availability/safeGetNestedProperty';
 export { isPlainDateAfter } from './date/isPlainDateAfter';
 export { isPlainDateBefore } from './date/isPlainDateBefore';
 export { isPlainDateBeforeOrEqual } from './date/isPlainDateBeforeOrEqual';
