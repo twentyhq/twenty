c5a1ce5e514c522b991c8630842e102e496f09de
bosiraphael <raphael.bosi@gmail.com>
2026-03-18 13:44:14 +0100

    lint

--- packages/twenty-front/src/modules/command-menu-item/states/commandMenuItemProgressFamilyState.ts
+++ packages/twenty-front/src/modules/command-menu-item/states/commandMenuItemProgressFamilyState.ts
@@ -1,7 +1,9 @@
 import { createAtomFamilyState } from '@/ui/utilities/state/jotai/utils/createAtomFamilyState';
 
-export const commandMenuItemProgressFamilyState =
-  createAtomFamilyState<number | undefined, string>({
-    key: 'commandMenuItemProgressFamilyState',
-    defaultValue: undefined,
-  });
+export const commandMenuItemProgressFamilyState = createAtomFamilyState<
+  number | undefined,
+  string
+>({
+  key: 'commandMenuItemProgressFamilyState',
+  defaultValue: undefined,
+});
