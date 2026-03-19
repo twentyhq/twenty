7e16bd221676d4ce89acd19e771d7c712e82bded
victorjzq <118996546+victorjzq@users.noreply.github.com>
2026-03-19 19:53:21 +0700

    feat: add IconApple component

---
+++ packages/twenty-ui/src/display/icon/components/IconApple.tsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+
+import IconAppleRaw from '@assets/icons/apple.svg?react';
+import { type IconComponentProps } from '@ui/display/icon/types/IconComponent';
+import { ThemeContext } from '@ui/theme-constants';
+
+type IconAppleProps = Pick<IconComponentProps, 'size'>;
+
+export const IconApple = (props: IconAppleProps) => {
+  const { theme } = useContext(ThemeContext);
+  const size = props.size ?? theme.icon.size.lg;
+
+  return <IconAppleRaw height={size} width={size} fill="currentColor" />;
+};
