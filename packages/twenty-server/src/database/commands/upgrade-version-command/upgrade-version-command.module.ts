2d1cd24441c2ca966d8896b3d9147edd2aa2f25f
Charles Bochet <charles@twenty.com>
2026-03-31 12:07:17 +0200

    More

--- packages/twenty-server/src/database/commands/upgrade-version-command/upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/upgrade-version-command.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { V1_20_UpgradeVersionCommandModule } from 'src/database/commands/upgrade-version-command/1-20/1-20-upgrade-version-command.module';
+import { V1_21_UpgradeVersionCommandModule } from 'src/database/commands/upgrade-version-command/1-21/1-21-upgrade-version-command.module';
 import { UpgradeCommand } from 'src/database/commands/upgrade-version-command/upgrade.command';
 import { CoreEngineVersionModule } from 'src/engine/core-engine-version/core-engine-version.module';
 import { CoreMigrationRunnerModule } from 'src/database/commands/core-migration-runner/core-migration-runner.module';
@@ -13,6 +14,7 @@ import { WorkspaceVersionModule } from 'src/engine/workspace-manager/workspace-v
   imports: [
     TypeOrmModule.forFeature([WorkspaceEntity]),
     V1_20_UpgradeVersionCommandModule,
+    V1_21_UpgradeVersionCommandModule,
     DataSourceModule,
     CoreEngineVersionModule,
     CoreMigrationRunnerModule,
