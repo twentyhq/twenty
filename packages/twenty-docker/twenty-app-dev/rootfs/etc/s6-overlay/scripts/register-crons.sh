46d5757ef11274a2b603ac89f424a1683b0feb3e
martmull <martmull@hotmail.fr>
2026-04-01 19:25:00 +0200

    Fix laucnh cron

--- packages/twenty-docker/twenty-app-dev/rootfs/etc/s6-overlay/scripts/register-crons.sh
+++ packages/twenty-docker/twenty-app-dev/rootfs/etc/s6-overlay/scripts/register-crons.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 set -e
 
-cd /app/packages/twenty-server
-
 echo "==> START Registering cron jobs"
 
-yarn command:prod cron:trigger:start-cron-trigger
-yarn command:prod cron:marketplace-catalog-sync
-yarn command:prod cron:app-version-check
-yarn command:prod cron:stale-registration-cleanup
+cd /app/packages/twenty-server
+yarn command:prod cron:register:all --dev-mode
 
 echo "==> DONE"
