7ca109327e12bef3661fbc53cde9fb58303515a4
Etienne <45695613+etiennejouan@users.noreply.github.com>
2026-04-02 13:40:41 +0200

    fix

--- packages/twenty-server/src/engine/api/graphql/core-graphql-api.module.ts
+++ packages/twenty-server/src/engine/api/graphql/core-graphql-api.module.ts
@@ -4,10 +4,15 @@ import { ScalarsExplorerService } from 'src/engine/api/graphql/services/scalars-
 import { WorkspaceGraphqlSchemaSDLModule } from 'src/engine/api/graphql/workspace-graphql-schema-sdl/workspace-graphql-schema-sdl.module';
 import { WorkspaceResolverBuilderModule } from 'src/engine/api/graphql/workspace-resolver-builder/workspace-resolver-builder.module';
 
+import { MetricsModule } from 'src/engine/core-modules/metrics/metrics.module';
 import { WorkspaceSchemaFactory } from './workspace-schema.factory';
 
 @Module({
-  imports: [WorkspaceResolverBuilderModule, WorkspaceGraphqlSchemaSDLModule],
+  imports: [
+    WorkspaceResolverBuilderModule,
+    WorkspaceGraphqlSchemaSDLModule,
+    MetricsModule,
+  ],
   providers: [WorkspaceSchemaFactory, ScalarsExplorerService],
   exports: [WorkspaceSchemaFactory],
 })
