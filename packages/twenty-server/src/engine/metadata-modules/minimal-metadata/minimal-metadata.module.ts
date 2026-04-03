93938c85c1ff3c809fdf417a8863898ff1e37fa9
David Condrey <davidcondrey@me.com>
2026-04-03 00:39:05 -0700

    style(server): reorder imports to follow alphabetical convention

--- packages/twenty-server/src/engine/metadata-modules/minimal-metadata/minimal-metadata.module.ts
+++ packages/twenty-server/src/engine/metadata-modules/minimal-metadata/minimal-metadata.module.ts
@@ -4,9 +4,14 @@ import { WorkspaceManyOrAllFlatEntityMapsCacheModule } from 'src/engine/metadata
 import { MinimalMetadataResolver } from 'src/engine/metadata-modules/minimal-metadata/minimal-metadata.resolver';
 import { MinimalMetadataService } from 'src/engine/metadata-modules/minimal-metadata/minimal-metadata.service';
 import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
+import { WorkspaceModule } from 'src/engine/core-modules/workspace/workspace.module';
 
 @Module({
-  imports: [WorkspaceManyOrAllFlatEntityMapsCacheModule, WorkspaceCacheModule],
+  imports: [
+    WorkspaceManyOrAllFlatEntityMapsCacheModule,
+    WorkspaceCacheModule,
+    WorkspaceModule,
+  ],
   providers: [MinimalMetadataResolver, MinimalMetadataService],
   exports: [MinimalMetadataService],
 })
--- packages/twenty-server/src/engine/metadata-modules/minimal-metadata/minimal-metadata.resolver.ts
+++ packages/twenty-server/src/engine/metadata-modules/minimal-metadata/minimal-metadata.resolver.ts
@@ -1,10 +1,11 @@
 import { UseGuards } from '@nestjs/common';
-import { Query } from '@nestjs/graphql';
+import { Query, ResolveField } from '@nestjs/graphql';
 
 import { MetadataResolver } from 'src/engine/api/graphql/graphql-config/decorators/metadata-resolver.decorator';
-import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { AuthUserWorkspaceId } from 'src/engine/decorators/auth/auth-user-workspace-id.decorator';
 import { AuthWorkspace } from 'src/engine/decorators/auth/auth-workspace.decorator';
+import { WorkspaceService } from 'src/engine/core-modules/workspace/services/workspace.service';
+import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { NoPermissionGuard } from 'src/engine/guards/no-permission.guard';
 import { WorkspaceAuthGuard } from 'src/engine/guards/workspace-auth.guard';
 import { MinimalMetadataDTO } from 'src/engine/metadata-modules/minimal-metadata/dtos/minimal-metadata.dto';
@@ -15,6 +16,7 @@ import { MinimalMetadataService } from 'src/engine/metadata-modules/minimal-meta
 export class MinimalMetadataResolver {
   constructor(
     private readonly minimalMetadataService: MinimalMetadataService,
+    private readonly workspaceService: WorkspaceService,
   ) {}
 
   @Query(() => MinimalMetadataDTO)
@@ -27,5 +29,18 @@ export class MinimalMetadataResolver {
       workspace.id,
       userWorkspaceId,
     );
+  }
+
+  @ResolveField(() => WorkspaceEntity)
+  async currentWorkspace(
+    @AuthWorkspace() { id }: WorkspaceEntity,
+  ): Promise<WorkspaceEntity> {
+    const workspace = await this.workspaceService.findById(id);
+
+    if (!workspace) {
+      throw new Error('Workspace not found');
+    }
+
+    return workspace;
   }
 }
