feb3d15ee7e3006d2c0503336b4e57d02e4903dd
David Condrey <davidcondrey@me.com>
2026-04-02 23:24:26 -0700

    style(front): reorder imports to follow convention

--- packages/twenty-front/src/modules/object-record/record-board/components/RecordBoardColumns.tsx
+++ packages/twenty-front/src/modules/object-record/record-board/components/RecordBoardColumns.tsx
@@ -1,11 +1,11 @@
 import { styled } from '@linaria/react';
 
+import { themeCssVariables } from 'twenty-ui/theme-constants';
+
 import { RecordBoardColumn } from '@/object-record/record-board/record-board-column/components/RecordBoardColumn';
 import { visibleRecordGroupIdsComponentFamilySelector } from '@/object-record/record-group/states/selectors/visibleRecordGroupIdsComponentFamilySelector';
 import { useAtomComponentFamilySelectorValue } from '@/ui/utilities/state/jotai/hooks/useAtomComponentFamilySelectorValue';
 import { ViewType } from '@/views/types/ViewType';
-
-import { themeCssVariables } from 'twenty-ui/theme-constants';
 
 const StyledColumnContainer = styled.div`
   display: flex;
