1c1b6d094c1737c80d639a87a0018401c6bb8ba2
BOHEUS <nothin_spe@2abc.hub.pl>
2026-04-04 13:57:26 +0200

    Rewrite to 0.8.0 SDK

---
+++ packages/twenty-apps/community/last-email-interaction/src/application.config.ts
@@ -0,0 +1,10 @@
+import { defineApplication } from 'twenty-sdk';
+
+export default defineApplication({
+  universalIdentifier: '718ed9ab-53fc-49c8-8deb-0cff78ecf0d2',
+  displayName: 'Last email interaction',
+  description:
+    'Updates Last interaction and Interaction status fields based on last received email',
+  icon: "IconMailFast",
+  defaultRoleUniversalIdentifier: '7a66af97-5056-45b2-96a9-c89f0fd181d1',
+});
