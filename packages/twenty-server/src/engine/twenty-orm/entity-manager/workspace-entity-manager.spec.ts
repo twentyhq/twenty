7fb5c823a7c3b7bc7b23ed67dca6d82168e167d4
neo773 <neo773@protonmail.com>
2026-04-07 15:07:36 +0530

    add messaging upgrade command 1.21

--- packages/twenty-server/src/engine/twenty-orm/entity-manager/workspace-entity-manager.spec.ts
+++ packages/twenty-server/src/engine/twenty-orm/entity-manager/workspace-entity-manager.spec.ts
@@ -240,6 +240,7 @@ describe('WorkspaceEntityManager', () => {
         IS_EMAILING_DOMAIN_ENABLED: false,
         IS_JUNCTION_RELATIONS_ENABLED: false,
         IS_DRAFT_EMAIL_ENABLED: false,
+        IS_CONNECTED_ACCOUNT_MIGRATED: false,
         IS_USAGE_ANALYTICS_ENABLED: false,
         IS_RICH_TEXT_V1_MIGRATED: false,
         IS_RECORD_PAGE_LAYOUT_GLOBAL_EDITION_ENABLED: false,
