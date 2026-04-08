5c77b1b5a17d81c0d25a7a58d482f431beafa988
Weiko <corentin@twenty.com>
2026-04-08 16:06:55 +0200

    Improve FE hooks

--- packages/twenty-front/src/modules/page-layout/hooks/useResetPageLayoutTabToDefault.ts
+++ packages/twenty-front/src/modules/page-layout/hooks/useResetPageLayoutTabToDefault.ts
@@ -1,98 +1,34 @@
-import { useApolloClient } from '@apollo/client/react';
 import { CombinedGraphQLErrors } from '@apollo/client/errors';
 import { useMutation } from '@apollo/client/react';
 import { t } from '@lingui/core/macro';
-import { useStore } from 'jotai';
 import { useCallback } from 'react';
 import { CrudOperationType } from 'twenty-shared/types';
-import { isDefined } from 'twenty-shared/utils';
-import { FindOnePageLayoutDocument } from '~/generated-metadata/graphql';
 
-import { useExitLayoutCustomizationMode } from '@/layout-customization/hooks/useExitLayoutCustomizationMode';
-import { useInvalidateMetadataStore } from '@/metadata-store/hooks/useInvalidateMetadataStore';
 import { useMetadataErrorHandler } from '@/metadata-error-handler/hooks/useMetadataErrorHandler';
 import { RESET_PAGE_LAYOUT_TAB_TO_DEFAULT } from '@/page-layout/graphql/mutations/resetPageLayoutTabToDefault';
-import { useSetIsPageLayoutInEditMode } from '@/page-layout/hooks/useSetIsPageLayoutInEditMode';
-import { PageLayoutComponentInstanceContext } from '@/page-layout/states/contexts/PageLayoutComponentInstanceContext';
-import { pageLayoutCurrentLayoutsComponentState } from '@/page-layout/states/pageLayoutCurrentLayoutsComponentState';
-import { pageLayoutDraftComponentState } from '@/page-layout/states/pageLayoutDraftComponentState';
-import { pageLayoutPersistedComponentState } from '@/page-layout/states/pageLayoutPersistedComponentState';
-import { convertPageLayoutToTabLayouts } from '@/page-layout/utils/convertPageLayoutToTabLayouts';
-import { transformPageLayout } from '@/page-layout/utils/transformPageLayout';
+import { useRefreshPageLayoutAfterReset } from '@/page-layout/hooks/useRefreshPageLayoutAfterReset';
+import { collectViewIdsFromWidgets } from '@/page-layout/utils/collectViewIdsFromWidgets';
 import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
-import { useAvailableComponentInstanceIdOrThrow } from '@/ui/utilities/state/component-state/hooks/useAvailableComponentInstanceIdOrThrow';
-import { useAtomComponentStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateCallbackState';
 
 export const useResetPageLayoutTabToDefault = (
   pageLayoutIdFromProps: string,
 ) => {
-  const pageLayoutId = useAvailableComponentInstanceIdOrThrow(
-    PageLayoutComponentInstanceContext,
-    pageLayoutIdFromProps,
-  );
-
-  const client = useApolloClient();
   const [resetMutation] = useMutation(RESET_PAGE_LAYOUT_TAB_TO_DEFAULT);
-
   const { handleMetadataError } = useMetadataErrorHandler();
   const { enqueueErrorSnackBar } = useSnackBar();
-  const { invalidateMetadataStore } = useInvalidateMetadataStore();
-  const { setIsPageLayoutInEditMode } =
-    useSetIsPageLayoutInEditMode(pageLayoutId);
-  const { exitLayoutCustomizationMode } = useExitLayoutCustomizationMode();
-
-  const store = useStore();
-
-  const pageLayoutPersistedState = useAtomComponentStateCallbackState(
-    pageLayoutPersistedComponentState,
-    pageLayoutId,
-  );
-
-  const pageLayoutDraftState = useAtomComponentStateCallbackState(
-    pageLayoutDraftComponentState,
-    pageLayoutId,
-  );
-
-  const pageLayoutCurrentLayoutsState = useAtomComponentStateCallbackState(
-    pageLayoutCurrentLayoutsComponentState,
-    pageLayoutId,
+  const { refreshPageLayoutAfterReset } = useRefreshPageLayoutAfterReset(
+    pageLayoutIdFromProps,
   );
 
   const resetPageLayoutTabToDefault = useCallback(
     async (tabId: string) => {
       try {
-        await resetMutation({
-          variables: { id: tabId },
-        });
-
-        const { data } = await client.query({
-          query: FindOnePageLayoutDocument,
-          variables: { id: pageLayoutId },
-          fetchPolicy: 'network-only',
-        });
-
-        if (isDefined(data?.getPageLayout)) {
-          const freshLayout = transformPageLayout(data.getPageLayout);
-
-          store.set(pageLayoutPersistedState, freshLayout);
-          store.set(pageLayoutDraftState, {
-            id: freshLayout.id,
-            name: freshLayout.name,
-            type: freshLayout.type,
-            objectMetadataId: freshLayout.objectMetadataId,
-            tabs: freshLayout.tabs,
-            defaultTabToFocusOnMobileAndSidePanelId:
-              freshLayout.defaultTabToFocusOnMobileAndSidePanelId,
-          });
-          store.set(
-            pageLayoutCurrentLayoutsState,
-            convertPageLayoutToTabLayouts(freshLayout),
-          );
-        }
-
-        setIsPageLayoutInEditMode(false);
-        exitLayoutCustomizationMode();
-        invalidateMetadataStore();
+        await resetMutation({ variables: { id: tabId } });
+        await refreshPageLayoutAfterReset((layout) =>
+          collectViewIdsFromWidgets(
+            layout.tabs.find((tab) => tab.id === tabId)?.widgets ?? [],
+          ),
+        );
       } catch (error) {
         if (CombinedGraphQLErrors.is(error)) {
           handleMetadataError(error, {
@@ -106,17 +42,9 @@ export const useResetPageLayoutTabToDefault = (
     },
     [
       resetMutation,
-      client,
-      pageLayoutId,
-      pageLayoutPersistedState,
-      pageLayoutDraftState,
-      pageLayoutCurrentLayoutsState,
-      setIsPageLayoutInEditMode,
-      exitLayoutCustomizationMode,
-      invalidateMetadataStore,
+      refreshPageLayoutAfterReset,
       handleMetadataError,
       enqueueErrorSnackBar,
-      store,
     ],
   );
 
--- packages/twenty-front/src/modules/page-layout/hooks/useResetPageLayoutWidgetToDefault.ts
+++ packages/twenty-front/src/modules/page-layout/hooks/useResetPageLayoutWidgetToDefault.ts
@@ -1,100 +1,36 @@
-import { useApolloClient } from '@apollo/client/react';
 import { CombinedGraphQLErrors } from '@apollo/client/errors';
 import { useMutation } from '@apollo/client/react';
 import { t } from '@lingui/core/macro';
-import { useStore } from 'jotai';
 import { useCallback } from 'react';
 import { CrudOperationType } from 'twenty-shared/types';
-import { isDefined } from 'twenty-shared/utils';
-import {
-  FindOnePageLayoutDocument,
-  ResetPageLayoutWidgetToDefaultDocument,
-} from '~/generated-metadata/graphql';
+import { ResetPageLayoutWidgetToDefaultDocument } from '~/generated-metadata/graphql';
 
-import { useExitLayoutCustomizationMode } from '@/layout-customization/hooks/useExitLayoutCustomizationMode';
-import { useInvalidateMetadataStore } from '@/metadata-store/hooks/useInvalidateMetadataStore';
 import { useMetadataErrorHandler } from '@/metadata-error-handler/hooks/useMetadataErrorHandler';
-import { useSetIsPageLayoutInEditMode } from '@/page-layout/hooks/useSetIsPageLayoutInEditMode';
-import { PageLayoutComponentInstanceContext } from '@/page-layout/states/contexts/PageLayoutComponentInstanceContext';
-import { pageLayoutCurrentLayoutsComponentState } from '@/page-layout/states/pageLayoutCurrentLayoutsComponentState';
-import { pageLayoutDraftComponentState } from '@/page-layout/states/pageLayoutDraftComponentState';
-import { pageLayoutPersistedComponentState } from '@/page-layout/states/pageLayoutPersistedComponentState';
-import { convertPageLayoutToTabLayouts } from '@/page-layout/utils/convertPageLayoutToTabLayouts';
-import { transformPageLayout } from '@/page-layout/utils/transformPageLayout';
+import { useRefreshPageLayoutAfterReset } from '@/page-layout/hooks/useRefreshPageLayoutAfterReset';
+import { collectViewIdsFromWidgets } from '@/page-layout/utils/collectViewIdsFromWidgets';
 import { useSnackBar } from '@/ui/feedback/snack-bar-manager/hooks/useSnackBar';
-import { useAvailableComponentInstanceIdOrThrow } from '@/ui/utilities/state/component-state/hooks/useAvailableComponentInstanceIdOrThrow';
-import { useAtomComponentStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateCallbackState';
 
 export const useResetPageLayoutWidgetToDefault = (
   pageLayoutIdFromProps: string,
 ) => {
-  const pageLayoutId = useAvailableComponentInstanceIdOrThrow(
-    PageLayoutComponentInstanceContext,
-    pageLayoutIdFromProps,
-  );
-
-  const client = useApolloClient();
   const [resetMutation] = useMutation(ResetPageLayoutWidgetToDefaultDocument);
-
   const { handleMetadataError } = useMetadataErrorHandler();
   const { enqueueErrorSnackBar } = useSnackBar();
-  const { invalidateMetadataStore } = useInvalidateMetadataStore();
-  const { setIsPageLayoutInEditMode } =
-    useSetIsPageLayoutInEditMode(pageLayoutId);
-  const { exitLayoutCustomizationMode } = useExitLayoutCustomizationMode();
-
-  const store = useStore();
-
-  const pageLayoutPersistedState = useAtomComponentStateCallbackState(
-    pageLayoutPersistedComponentState,
-    pageLayoutId,
-  );
-
-  const pageLayoutDraftState = useAtomComponentStateCallbackState(
-    pageLayoutDraftComponentState,
-    pageLayoutId,
-  );
-
-  const pageLayoutCurrentLayoutsState = useAtomComponentStateCallbackState(
-    pageLayoutCurrentLayoutsComponentState,
-    pageLayoutId,
+  const { refreshPageLayoutAfterReset } = useRefreshPageLayoutAfterReset(
+    pageLayoutIdFromProps,
   );
 
   const resetPageLayoutWidgetToDefault = useCallback(
     async (widgetId: string) => {
       try {
-        await resetMutation({
-          variables: { id: widgetId },
-        });
-
-        const { data } = await client.query({
-          query: FindOnePageLayoutDocument,
-          variables: { id: pageLayoutId },
-          fetchPolicy: 'network-only',
-        });
-
-        if (isDefined(data?.getPageLayout)) {
-          const freshLayout = transformPageLayout(data.getPageLayout);
-
-          store.set(pageLayoutPersistedState, freshLayout);
-          store.set(pageLayoutDraftState, {
-            id: freshLayout.id,
-            name: freshLayout.name,
-            type: freshLayout.type,
-            objectMetadataId: freshLayout.objectMetadataId,
-            tabs: freshLayout.tabs,
-            defaultTabToFocusOnMobileAndSidePanelId:
-              freshLayout.defaultTabToFocusOnMobileAndSidePanelId,
-          });
-          store.set(
-            pageLayoutCurrentLayoutsState,
-            convertPageLayoutToTabLayouts(freshLayout),
-          );
-        }
-
-        setIsPageLayoutInEditMode(false);
-        exitLayoutCustomizationMode();
-        invalidateMetadataStore();
+        await resetMutation({ variables: { id: widgetId } });
+        await refreshPageLayoutAfterReset((layout) =>
+          collectViewIdsFromWidgets(
+            layout.tabs
+              .flatMap((tab) => tab.widgets)
+              .filter((widget) => widget.id === widgetId),
+          ),
+        );
       } catch (error) {
         if (CombinedGraphQLErrors.is(error)) {
           handleMetadataError(error, {
@@ -108,17 +44,9 @@ export const useResetPageLayoutWidgetToDefault = (
     },
     [
       resetMutation,
-      client,
-      pageLayoutId,
-      pageLayoutPersistedState,
-      pageLayoutDraftState,
-      pageLayoutCurrentLayoutsState,
-      setIsPageLayoutInEditMode,
-      exitLayoutCustomizationMode,
-      invalidateMetadataStore,
+      refreshPageLayoutAfterReset,
       handleMetadataError,
       enqueueErrorSnackBar,
-      store,
     ],
   );
 
