67780365917702a151dd590deddb69fe5bab4b52
Devessier <baptiste@devessier.fr>
2026-04-09 19:00:21 +0200

    chore: remove parts of the migration

--- packages/twenty-server/src/database/typeorm/core/migrations/common/1775654781000-addConditionalAvailabilityExpressionToPageLayoutWidget.ts
+++ packages/twenty-server/src/database/typeorm/core/migrations/common/1775654781000-addConditionalAvailabilityExpressionToPageLayoutWidget.ts
@@ -9,14 +9,6 @@ export class AddConditionalAvailabilityExpressionToPageLayoutWidget1775654781000
     await queryRunner.query(
       `ALTER TABLE "core"."pageLayoutWidget" ADD "conditionalAvailabilityExpression" varchar`,
     );
-
-    await queryRunner.query(
-      `UPDATE "core"."pageLayoutWidget" SET "conditionalAvailabilityExpression" = 'device == "MOBILE"' WHERE "conditionalDisplay"::text = '{"and":[{"===":[{"var":"device"},"MOBILE"]}]}'`,
-    );
-
-    await queryRunner.query(
-      `UPDATE "core"."pageLayoutWidget" SET "conditionalAvailabilityExpression" = 'device == "DESKTOP"' WHERE "conditionalDisplay"::text = '{"and":[{"===":[{"var":"device"},"DESKTOP"]}]}'`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
