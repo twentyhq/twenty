a6977df552b06f42b3373e13beeb50cc354c59f4
neo773 <neo773@protonmail.com>
2026-04-09 15:57:33 +0530

    le lint

--- packages/twenty-server/src/engine/metadata-modules/connected-account/dtos/connected-account-public.dto.ts
+++ packages/twenty-server/src/engine/metadata-modules/connected-account/dtos/connected-account-public.dto.ts
@@ -6,10 +6,9 @@ import { ConnectionParametersDTO } from 'src/engine/core-modules/imap-smtp-calda
 import { ConnectedAccountDTO } from 'src/engine/metadata-modules/connected-account/dtos/connected-account.dto';
 
 @ObjectType('PublicConnectionParametersOutput')
-class PublicConnectionParametersDTO extends OmitType(
-  ConnectionParametersDTO,
-  ['password'] as const,
-) {}
+class PublicConnectionParametersDTO extends OmitType(ConnectionParametersDTO, [
+  'password',
+] as const) {}
 
 @ObjectType('PublicImapSmtpCaldavConnectionParameters')
 class PublicImapSmtpCaldavConnectionParametersDTO {
@@ -24,10 +23,9 @@ class PublicImapSmtpCaldavConnectionParametersDTO {
 }
 
 @ObjectType('ConnectedAccountPublicDTO')
-export class ConnectedAccountPublicDTO extends OmitType(
-  ConnectedAccountDTO,
-  ['connectionParameters'] as const,
-) {
+export class ConnectedAccountPublicDTO extends OmitType(ConnectedAccountDTO, [
+  'connectionParameters',
+] as const) {
   @IsOptional()
   @Field(() => PublicImapSmtpCaldavConnectionParametersDTO, { nullable: true })
   connectionParameters: PublicImapSmtpCaldavConnectionParametersDTO | null;
