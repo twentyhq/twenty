aa8329e7e7ab0679177c7212c9e3966c0c5e2719
Abdullah <125115953+mabdullahabaid@users.noreply.github.com>
2026-04-10 09:02:36 +0500

    fix: some copy on pricing and partner page

--- packages/twenty-website-new/src/app/pricing/page.tsx
+++ packages/twenty-website-new/src/app/pricing/page.tsx
@@ -67,7 +67,7 @@ export default async function PricingPage() {
             <LinkButton
               color="secondary"
               href="https://app.twenty.com/welcome"
-              label="Read our case studies"
+              label="Contact us"
               type="anchor"
               variant="contained"
             />
