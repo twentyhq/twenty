77ab1efcaac4240e6c5a687b12f21683338beec2
Weiko <corentin@twenty.com>
2026-04-13 16:11:52 +0200

    Fix delete widget

--- packages/twenty-front/src/modules/page-layout/widgets/record-table/hooks/useDeleteViewForRecordTableWidget.ts
+++ packages/twenty-front/src/modules/page-layout/widgets/record-table/hooks/useDeleteViewForRecordTableWidget.ts
@@ -1,14 +1,21 @@
 import { usePerformViewAPIPersist } from '@/views/hooks/internal/usePerformViewAPIPersist';
+import { useGetViewFromState } from '@/views/hooks/useGetViewFromState';
 import { useCallback } from 'react';
+import { isDefined } from 'twenty-shared/utils';
 
 export const useDeleteViewForRecordTableWidget = () => {
   const { performViewAPIDestroy } = usePerformViewAPIPersist();
+  const { getViewFromState } = useGetViewFromState();
 
   const deleteViewForRecordTableWidget = useCallback(
     async (viewId: string) => {
+      if (!isDefined(getViewFromState(viewId))) {
+        return;
+      }
+
       await performViewAPIDestroy({ id: viewId });
     },
-    [performViewAPIDestroy],
+    [performViewAPIDestroy, getViewFromState],
   );
 
   return { deleteViewForRecordTableWidget };
