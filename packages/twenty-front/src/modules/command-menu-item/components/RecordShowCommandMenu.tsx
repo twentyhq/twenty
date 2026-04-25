e3a5edb65b2da8706f4ef518e26edece45f3080e
ehconitin <nitinkoche03@gmail.com>
2026-04-25 15:18:00 +0530

    fix

--- packages/twenty-front/src/modules/command-menu-item/components/RecordShowCommandMenu.tsx
+++ packages/twenty-front/src/modules/command-menu-item/components/RecordShowCommandMenu.tsx
@@ -1,10 +1,13 @@
 import { CommandMenuContextProvider } from '@/command-menu-item/contexts/CommandMenuContextProvider';
 import { PinnedCommandMenuItemButtons } from '@/command-menu-item/display/components/PinnedCommandMenuItemButtons';
 import { CommandMenuItemEditButton } from '@/command-menu-item/edit/components/CommandMenuItemEditButton';
+import { PinnedCommandMenuItemButtonsEditMode } from '@/command-menu-item/edit/components/PinnedCommandMenuItemButtonsEditMode';
 import { MAIN_CONTEXT_STORE_INSTANCE_ID } from '@/context-store/constants/MainContextStoreInstanceId';
 import { contextStoreCurrentObjectMetadataItemIdComponentState } from '@/context-store/states/contextStoreCurrentObjectMetadataItemIdComponentState';
 import { contextStoreTargetedRecordsRuleComponentState } from '@/context-store/states/contextStoreTargetedRecordsRuleComponentState';
+import { isLayoutCustomizationModeEnabledState } from '@/layout-customization/states/isLayoutCustomizationModeEnabledState';
 import { useAtomComponentStateValue } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateValue';
+import { useAtomStateValue } from '@/ui/utilities/state/jotai/hooks/useAtomStateValue';
 import { useIsMobile } from 'twenty-ui/utilities';
 
 export const RecordShowCommandMenu = () => {
@@ -23,18 +26,27 @@ export const RecordShowCommandMenu = () => {
     contextStoreTargetedRecordsRule.selectedRecordIds.length === 1;
 
   const isMobile = useIsMobile();
+  const isLayoutCustomizationModeEnabled = useAtomStateValue(
+    isLayoutCustomizationModeEnabledState,
+  );
+
+  const showEditModePinnedButtons = isLayoutCustomizationModeEnabled;
 
   return (
     <>
       {hasSelectedRecord && contextStoreCurrentObjectMetadataItemId && (
         <>
-          <CommandMenuContextProvider
-            isInSidePanel={false}
-            displayType="button"
-            containerType="show-page-header"
-          >
-            {!isMobile && <PinnedCommandMenuItemButtons />}
-          </CommandMenuContextProvider>
+          {!isMobile && showEditModePinnedButtons ? (
+            <PinnedCommandMenuItemButtonsEditMode />
+          ) : (
+            <CommandMenuContextProvider
+              isInSidePanel={false}
+              displayType="button"
+              containerType="show-page-header"
+            >
+              {!isMobile && <PinnedCommandMenuItemButtons />}
+            </CommandMenuContextProvider>
+          )}
           <CommandMenuItemEditButton />
         </>
       )}
