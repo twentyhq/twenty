16970d625f43f958c7d2372546ccb3900e41bc39
neo773 <neo773@protonmail.com>
2026-04-29 19:11:54 +0530

    feat(messaging): add 2.2.0 workspace command to drop message.direction field

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-2/2-2-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-2/2-2-upgrade-version-command.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 
+import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
+import { DropMessageDirectionFieldCommand } from 'src/database/commands/upgrade-version-command/2-2/2-2-workspace-command-1777400000000-drop-message-direction-field.command';
+import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
+import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
+import { WorkspaceMigrationModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration.module';
+
 @Module({
-  imports: [],
-  providers: [],
+  imports: [
+    ApplicationModule,
+    WorkspaceCacheModule,
+    WorkspaceIteratorModule,
+    WorkspaceMigrationModule,
+  ],
+  providers: [DropMessageDirectionFieldCommand],
 })
 export class V2_2_UpgradeVersionCommandModule {}
