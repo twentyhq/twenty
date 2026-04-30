bff6b21b5d9d4f8dff05ffceea68dc8f72134270
ehconitin <nitinkoche03@gmail.com>
2026-05-01 01:14:25 +0530

    [Dashboards] Remove gauge chart support and delete existing widgets

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-4/2-4-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-4/2-4-upgrade-version-command.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 
+import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
+import { DeleteGaugeWidgetsCommand } from 'src/database/commands/upgrade-version-command/2-4/2-4-workspace-command-1798000000000-delete-gauge-widgets.command';
+import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
+import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
+import { WorkspaceMigrationModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration.module';
+
 @Module({
-  imports: [],
-  providers: [],
+  imports: [
+    ApplicationModule,
+    WorkspaceCacheModule,
+    WorkspaceIteratorModule,
+    WorkspaceMigrationModule,
+  ],
+  providers: [DeleteGaugeWidgetsCommand],
 })
 export class V2_4_UpgradeVersionCommandModule {}
