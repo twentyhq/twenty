bdabaeaca55af3fe66fe029a3ae5233d9e9a9751
ratishjain12 <ratishjain6@gmail.com>
2026-05-01 22:52:16 +0530

    feat(email-verification): add EMAIL_UPDATE support to resend flow

--- packages/twenty-server/src/engine/core-modules/email-verification/dtos/resend-email-verification-token.input.ts
+++ packages/twenty-server/src/engine/core-modules/email-verification/dtos/resend-email-verification-token.input.ts
@@ -2,10 +2,18 @@ import { ArgsType, Field } from '@nestjs/graphql';
 
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
+import { EmailVerificationTrigger } from 'src/engine/core-modules/email-verification/email-verification.constants';
+
 @ArgsType()
 export class ResendEmailVerificationTokenInput {
   @Field(() => String)
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
+  @Field(() => EmailVerificationTrigger, {
+    nullable: true,
+    defaultValue: EmailVerificationTrigger.SIGN_UP,
+  })
+  verificationTrigger?: EmailVerificationTrigger;
 }
