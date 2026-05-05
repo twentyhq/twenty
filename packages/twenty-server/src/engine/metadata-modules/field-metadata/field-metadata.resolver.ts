f0ee64fccc7828edef23c9d6785ad8802246866c
Abdul Rahman <ar5438376@gmail.com>
2026-05-05 12:25:26 +0530

    Enhance FieldMetadataResolver to include i18nService for improved localization support
    - Updated the FieldMetadataResolver constructor to inject the I18nService, enabling better handling of internationalization within field metadata operations.
    - This change aims to enhance the user experience by providing localized labels and messages in the GraphQL responses.

--- packages/twenty-server/src/engine/metadata-modules/field-metadata/field-metadata.resolver.ts
+++ packages/twenty-server/src/engine/metadata-modules/field-metadata/field-metadata.resolver.ts
@@ -8,6 +8,8 @@ import { MetadataResolver } from 'src/engine/api/graphql/graphql-config/decorato
 import { PreventNestToAutoLogGraphqlErrorsFilter } from 'src/engine/core-modules/graphql/filters/prevent-nest-to-auto-log-graphql-errors.filter';
 import { ResolverValidationPipe } from 'src/engine/core-modules/graphql/pipes/resolver-validation.pipe';
 import { ForbiddenError } from 'src/engine/core-modules/graphql/utils/graphql-errors.util';
+import { I18nService } from 'src/engine/core-modules/i18n/i18n.service';
+import { I18nContext } from 'src/engine/core-modules/i18n/types/i18n-context.type';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { type IDataloaders } from 'src/engine/dataloaders/dataloader.interface';
 import { AuthWorkspace } from 'src/engine/decorators/auth/auth-workspace.decorator';
@@ -38,7 +40,10 @@ type FieldMetadataStandardOverrideParent = Parameters<
   PreventNestToAutoLogGraphqlErrorsFilter,
 )
 export class FieldMetadataResolver {
-  constructor(private readonly fieldMetadataService: FieldMetadataService) {}
+  constructor(
+    private readonly fieldMetadataService: FieldMetadataService,
+    private readonly i18nService: I18nService,
+  ) {}
 
   @ResolveField(() => String, { nullable: true })
   async label(
