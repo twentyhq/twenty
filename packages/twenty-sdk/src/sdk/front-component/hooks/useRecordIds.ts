1ecbfc12ebb41982f384b45493c2ca899769dbb8
prastoin <paul@twenty.com>
2026-05-05 09:42:47 +0200

    lint

--- packages/twenty-sdk/src/sdk/front-component/hooks/useRecordIds.ts
+++ packages/twenty-sdk/src/sdk/front-component/hooks/useRecordIds.ts
@@ -1,9 +1,8 @@
 import { type FrontComponentExecutionContext } from '../types/FrontComponentExecutionContext';
 import { useFrontComponentExecutionContext } from './useFrontComponentExecutionContext';
 
-const selectRecordIds = (
-  context: FrontComponentExecutionContext,
-): string[] => context.recordIds;
+const selectRecordIds = (context: FrontComponentExecutionContext): string[] =>
+  context.recordIds;
 
 export const useRecordIds = (): string[] => {
   return useFrontComponentExecutionContext(selectRecordIds);
