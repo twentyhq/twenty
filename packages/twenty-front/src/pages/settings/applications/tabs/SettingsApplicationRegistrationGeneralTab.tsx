f0d8985ce72268752f8f2dcc456c6ff1b08ba7d3
martmull <martmull@hotmail.fr>
2026-05-06 15:46:18 +0200

    Use isConfigured in admin panel

--- packages/twenty-front/src/pages/settings/applications/tabs/SettingsApplicationRegistrationGeneralTab.tsx
+++ packages/twenty-front/src/pages/settings/applications/tabs/SettingsApplicationRegistrationGeneralTab.tsx
@@ -1,10 +1,21 @@
 import { type ApplicationRegistration } from '~/generated-metadata/graphql';
 
-import { SettingsApplicationRegistrationGeneralInfo } from '~/pages/settings/applications/components/SettingsApplicationRegistrationGeneralInfo';
+import { useLingui } from '@lingui/react/macro';
+import { useNavigate } from 'react-router-dom';
+import { InlineBanner } from 'twenty-ui/display';
+import {
+  SettingsApplicationRegistrationGeneralInfo
+} from '~/pages/settings/applications/components/SettingsApplicationRegistrationGeneralInfo';
 
-import { SettingsAdminApplicationRegistrationDangerZone } from '~/pages/settings/admin-panel/SettingsAdminApplicationRegistrationDangerZone';
-import { SettingsApplicationRegistrationGeneralStats } from '~/pages/settings/applications/components/SettingsApplicationRegistrationGeneralStats';
-import { SettingsAdminApplicationRegistrationGeneralToggles } from '~/pages/settings/admin-panel/SettingsAdminApplicationRegistrationGeneralToggles';
+import {
+  SettingsAdminApplicationRegistrationDangerZone
+} from '~/pages/settings/admin-panel/SettingsAdminApplicationRegistrationDangerZone';
+import {
+  SettingsApplicationRegistrationGeneralStats
+} from '~/pages/settings/applications/components/SettingsApplicationRegistrationGeneralStats';
+import {
+  SettingsAdminApplicationRegistrationGeneralToggles
+} from '~/pages/settings/admin-panel/SettingsAdminApplicationRegistrationGeneralToggles';
 
 export const SettingsApplicationRegistrationGeneralTab = ({
   registration,
@@ -13,8 +24,21 @@ export const SettingsApplicationRegistrationGeneralTab = ({
   registration: ApplicationRegistration;
   fromAdmin?: boolean;
 }) => {
+  const { t } = useLingui();
+  const navigate = useNavigate();
+
   return (
     <>
+      {!registration.isConfigured && fromAdmin && (
+        <InlineBanner
+          color="danger"
+          message={t`This app has required server variables that are not configured. Users won't be able to install it until all required variables are set.`}
+          button={{
+            title: t`Configure`,
+            onClick: () => navigate('#config'),
+          }}
+        />
+      )}
       <SettingsApplicationRegistrationGeneralInfo registration={registration} />
       {fromAdmin && (
         <SettingsAdminApplicationRegistrationGeneralToggles
