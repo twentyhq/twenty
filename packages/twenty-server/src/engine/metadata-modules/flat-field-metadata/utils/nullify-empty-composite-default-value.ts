6ce469540f6610502a215363979b7f282b0e2525
Etienne <45695613+etiennejouan@users.noreply.github.com>
2026-05-12 09:17:15 +0200

    add

--- packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/utils/nullify-empty-composite-default-value.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/utils/nullify-empty-composite-default-value.ts
@@ -6,14 +6,14 @@ import { assertUnreachable } from 'twenty-shared/utils';
 
 import { CompositeFieldMetadataType } from 'src/engine/metadata-modules/field-metadata/types/composite-field-metadata-type.type';
 
-import { nullifyEmptyActorDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-actor-default-value.util';
-import { nullifyEmptyAddressDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-address-default-value.util';
-import { nullifyEmptyCurrencyDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-currency-default-value.util';
-import { nullifyEmptyEmailsDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-emails-default-value.util';
-import { nullifyEmptyFullNameDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-full-name-default-value.util';
-import { nullifyEmptyLinksDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-links-default-value.util';
-import { nullifyEmptyPhonesDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-phones-default-value.util';
-import { nullifyEmptyRichTextDefaultValue } from './nullify-empty-composite-default-value/nullify-empty-rich-text-default-value.util';
+import { nullifyEmptyActorDefaultValue } from './nullify-empty-actor-default-value.util';
+import { nullifyEmptyAddressDefaultValue } from './nullify-empty-address-default-value.util';
+import { nullifyEmptyCurrencyDefaultValue } from './nullify-empty-currency-default-value.util';
+import { nullifyEmptyEmailsDefaultValue } from './nullify-empty-emails-default-value.util';
+import { nullifyEmptyFullNameDefaultValue } from './nullify-empty-full-name-default-value.util';
+import { nullifyEmptyLinksDefaultValue } from './nullify-empty-links-default-value.util';
+import { nullifyEmptyPhonesDefaultValue } from './nullify-empty-phones-default-value.util';
+import { nullifyEmptyRichTextDefaultValue } from './nullify-empty-rich-text-default-value.util';
 
 export const nullifyEmptyCompositeDefaultValue = ({
   defaultValue,
