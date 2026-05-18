4da11c066fa784b52f7b83335bf359e3fe9ec4ab
martmull <martmull@hotmail.fr>
2026-05-18 14:08:01 +0200

    Revert "Increate cli version"
    This reverts commit 6be6000c30fe77ef243060b611168a198667fc36.

--- packages/create-twenty-app/src/constants/template/src/navigation-menu-items/main-page.navigation-menu-item.ts
+++ packages/create-twenty-app/src/constants/template/src/navigation-menu-items/main-page.navigation-menu-item.ts
@@ -1,13 +1,17 @@
-import { defineNavigationMenuItem, NavigationMenuItemType } from 'twenty-sdk/define';
+import {
+  defineNavigationMenuItem,
+  NavigationMenuItemType,
+} from 'twenty-sdk/define';
 
 import {
+  APP_DISPLAY_NAME,
   MAIN_PAGE_LAYOUT_UNIVERSAL_IDENTIFIER,
   MAIN_PAGE_NAVIGATION_MENU_ITEM_UNIVERSAL_IDENTIFIER,
 } from 'src/constants/universal-identifiers';
 
 export default defineNavigationMenuItem({
   universalIdentifier: MAIN_PAGE_NAVIGATION_MENU_ITEM_UNIVERSAL_IDENTIFIER,
-  name: 'main-page',
+  name: `${APP_DISPLAY_NAME} app`,
   icon: 'IconApps',
   position: 0,
   type: NavigationMenuItemType.PAGE_LAYOUT,
