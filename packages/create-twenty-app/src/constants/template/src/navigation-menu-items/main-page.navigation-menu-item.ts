bdf77cba3a9cb8cf66caf5556871e8f5e87768c8
martmull <martmull@hotmail.fr>
2026-05-18 18:00:29 +0200

    Fix scaffolded front component

--- packages/create-twenty-app/src/constants/template/src/navigation-menu-items/main-page.navigation-menu-item.ts
+++ packages/create-twenty-app/src/constants/template/src/navigation-menu-items/main-page.navigation-menu-item.ts
@@ -11,9 +11,9 @@ import {
 
 export default defineNavigationMenuItem({
   universalIdentifier: MAIN_PAGE_NAVIGATION_MENU_ITEM_UNIVERSAL_IDENTIFIER,
-  name: `${APP_DISPLAY_NAME} app`,
-  icon: 'IconApps',
-  position: 99999,
+  name: APP_DISPLAY_NAME,
+  icon: 'IconFile',
+  position: -1,
   type: NavigationMenuItemType.PAGE_LAYOUT,
   pageLayoutUniversalIdentifier: MAIN_PAGE_LAYOUT_UNIVERSAL_IDENTIFIER,
 });
