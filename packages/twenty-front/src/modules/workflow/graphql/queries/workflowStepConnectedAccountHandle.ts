d6622c64b1bf471714ac92ee105a2f42ba220c2e
prastoin <paul@twenty.com>
2026-05-19 18:01:39 +0200

    lint

--- packages/twenty-front/src/modules/workflow/graphql/queries/workflowStepConnectedAccountHandle.ts
+++ packages/twenty-front/src/modules/workflow/graphql/queries/workflowStepConnectedAccountHandle.ts
@@ -2,7 +2,9 @@ import { gql } from '@apollo/client';
 
 export const WORKFLOW_STEP_CONNECTED_ACCOUNT_HANDLE = gql`
   query WorkflowStepConnectedAccountHandle($connectedAccountId: UUID!) {
-    workflowStepConnectedAccountHandle(connectedAccountId: $connectedAccountId) {
+    workflowStepConnectedAccountHandle(
+      connectedAccountId: $connectedAccountId
+    ) {
       id
       handle
       provider
