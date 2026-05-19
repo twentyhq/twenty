30a21d28f17b66b89724fd0e922206ffa4544c91
martmull <martmull@hotmail.fr>
2026-05-19 17:23:47 +0200

    Fix label

--- packages/create-twenty-app/src/constants/template/src/front-components/main-page.tsx
+++ packages/create-twenty-app/src/constants/template/src/front-components/main-page.tsx
@@ -37,7 +37,7 @@ const CATEGORIES = [
         href: `${DOCS_BASE_URL}/logic/logic-functions`,
       },
       {
-        label: 'SERVERLESS FUNCT.',
+        label: 'LOGIC FUNCTION',
         href: `${DOCS_BASE_URL}/logic/logic-functions`,
       },
       {
