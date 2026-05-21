195f3a9dc14155e8662dfb3253b1eb6da9c44bcc
Charles Bochet <charles@twenty.com>
2026-05-21 23:08:27 +0200

    chore: oxfmt files merged from main

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-7/2-7-instance-command-fast-1779600000000-finalize-role-permission-flag-cutover.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-7/2-7-instance-command-fast-1779600000000-finalize-role-permission-flag-cutover.ts
@@ -4,9 +4,7 @@ import { RegisteredInstanceCommand } from 'src/engine/core-modules/upgrade/decor
 import { FastInstanceCommand } from 'src/engine/core-modules/upgrade/interfaces/fast-instance-command.interface';
 
 @RegisteredInstanceCommand('2.7.0', 1779600000000)
-export class FinalizeRolePermissionFlagCutoverFastInstanceCommand
-  implements FastInstanceCommand
-{
+export class FinalizeRolePermissionFlagCutoverFastInstanceCommand implements FastInstanceCommand {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "core"."rolePermissionFlag"
