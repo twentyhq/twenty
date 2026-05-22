1b1860b038a1b9679e72763290fbc01f8cf283f2
martmull <martmull@hotmail.fr>
2026-05-22 09:46:34 +0200

    Fix missing db update

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-8/2-8-instance-command-fast-1779387505162-add-logo-to-application-registration.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-8/2-8-instance-command-fast-1779387505162-add-logo-to-application-registration.ts
@@ -14,9 +14,21 @@ export class AddLogoToApplicationRegistrationFastInstanceCommand
     await queryRunner.query(
       'ALTER TABLE "core"."applicationRegistration" ADD "logoFileId" uuid',
     );
+    await queryRunner.query(
+      'ALTER TABLE "core"."applicationRegistration" ADD CONSTRAINT "UQ_796819fb23559c233e6ebd49f34" UNIQUE ("logoFileId")',
+    );
+    await queryRunner.query(
+      'ALTER TABLE "core"."applicationRegistration" ADD CONSTRAINT "FK_796819fb23559c233e6ebd49f34" FOREIGN KEY ("logoFileId") REFERENCES "core"."file"("id") ON DELETE SET NULL ON UPDATE NO ACTION',
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE "core"."applicationRegistration" DROP CONSTRAINT "FK_796819fb23559c233e6ebd49f34"',
+    );
+    await queryRunner.query(
+      'ALTER TABLE "core"."applicationRegistration" DROP CONSTRAINT "UQ_796819fb23559c233e6ebd49f34"',
+    );
     await queryRunner.query(
       'ALTER TABLE "core"."applicationRegistration" DROP COLUMN "logoFileId"',
     );
