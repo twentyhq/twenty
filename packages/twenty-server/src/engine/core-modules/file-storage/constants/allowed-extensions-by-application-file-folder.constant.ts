350fdc16c7ff0ac2973dbe185b2a1cbe83f80c8b
prastoin <paul@twenty.com>
2026-05-22 15:20:24 +0200

    add json to source

--- packages/twenty-server/src/engine/core-modules/file-storage/constants/allowed-extensions-by-application-file-folder.constant.ts
+++ packages/twenty-server/src/engine/core-modules/file-storage/constants/allowed-extensions-by-application-file-folder.constant.ts
@@ -3,7 +3,7 @@ import { FileFolder } from 'twenty-shared/types';
 export const ALLOWED_EXTENSIONS_BY_APPLICATION_FILE_FOLDER = {
   [FileFolder.BuiltLogicFunction]: { '.mjs': true },
   [FileFolder.BuiltFrontComponent]: { '.mjs': true },
-  [FileFolder.Source]: { '.ts': true, '.tsx': true },
+  [FileFolder.Source]: { '.ts': true, '.tsx': true, '.json': true },
   [FileFolder.Dependencies]: { '.json': true, '.lock': true },
   [FileFolder.PublicAsset]: {
     '.png': true,
