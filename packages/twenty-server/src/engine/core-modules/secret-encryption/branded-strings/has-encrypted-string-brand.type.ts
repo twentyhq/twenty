2817b486bb075f6a54b47fa19d688a55db2b1d0e
prastoin <paul@twenty.com>
2026-05-31 13:36:29 +0200

    deadcode

--- packages/twenty-server/src/engine/core-modules/secret-encryption/branded-strings/has-encrypted-string-brand.type.ts
+++
@@ -1,5 +0,0 @@
-import { type EncryptedString } from 'src/engine/core-modules/secret-encryption/branded-strings/encrypted-string.type';
-
-export type HasEncryptedStringBrand<T> = T extends EncryptedString
-  ? true
-  : false;
--- packages/twenty-server/src/engine/core-modules/secret-encryption/branded-strings/index.ts
+++ packages/twenty-server/src/engine/core-modules/secret-encryption/branded-strings/index.ts
@@ -1,6 +1,5 @@
 export { type ContainsEncryptedString } from 'src/engine/core-modules/secret-encryption/branded-strings/contains-encrypted-string.type';
 export { type EncryptedString } from 'src/engine/core-modules/secret-encryption/branded-strings/encrypted-string.type';
 export { type ExtractEncryptedColumns } from 'src/engine/core-modules/secret-encryption/branded-strings/extract-encrypted-columns.type';
-export { type HasEncryptedStringBrand } from 'src/engine/core-modules/secret-encryption/branded-strings/has-encrypted-string-brand.type';
 export { isEncryptedString } from 'src/engine/core-modules/secret-encryption/branded-strings/is-encrypted-string.util';
 export { type PlaintextString } from 'src/engine/core-modules/secret-encryption/branded-strings/plaintext-string.type';
