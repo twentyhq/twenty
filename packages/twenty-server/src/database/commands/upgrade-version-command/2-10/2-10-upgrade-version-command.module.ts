5672a090f81d3ad5394a26602a4f805b07fe6d7b
ehconitin <nitinkoche03@gmail.com>
2026-06-04 19:30:34 +0530

    fix(call-recording): resequence upgrade command timestamp

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-10/2-10-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-10/2-10-upgrade-version-command.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
-import { SyncCallRecordingStandardObjectsCommand } from 'src/database/commands/upgrade-version-command/2-10/2-10-workspace-command-1800000000000-sync-call-recording-standard-objects.command';
+import { SyncCallRecordingStandardObjectsCommand } from 'src/database/commands/upgrade-version-command/2-10/2-10-workspace-command-1799000040000-sync-call-recording-standard-objects.command';
 import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
 import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
 import { WorkspaceMigrationModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration.module';
