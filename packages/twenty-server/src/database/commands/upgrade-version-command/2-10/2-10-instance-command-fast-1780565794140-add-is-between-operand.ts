8f81d2e2420d586c20085b53387b649a786bd7ea
DeviSriSaiCharan <devisrisaicharan@gmail.com>
2026-06-05 10:32:55 +0530

    chore: move IS_BETWEEN migration to 2.10.0

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-10/2-10-instance-command-fast-1780565794140-add-is-between-operand.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-10/2-10-instance-command-fast-1780565794140-add-is-between-operand.ts
@@ -3,7 +3,7 @@ import { QueryRunner } from 'typeorm';
 import { RegisteredInstanceCommand } from 'src/engine/core-modules/upgrade/decorators/registered-instance-command.decorator';
 import { FastInstanceCommand } from 'src/engine/core-modules/upgrade/interfaces/fast-instance-command.interface';
 
-@RegisteredInstanceCommand('2.9.0', 1780565794140)
+@RegisteredInstanceCommand('2.10.0', 1780565794140)
 export class AddIsBetweenOperandFastInstanceCommand implements FastInstanceCommand {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TYPE "core"."viewFilter_operand_enum" ADD VALUE IF NOT EXISTS 'IS_BETWEEN'`);
