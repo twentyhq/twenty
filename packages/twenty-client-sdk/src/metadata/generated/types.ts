64624481bcf3854cce2f77a2665256d3cb62ebb6
neo773 <62795688+neo773@users.noreply.github.com>
2026-06-06 18:19:55 +0530

    revert #21177 (#21284)

--- packages/twenty-client-sdk/src/metadata/generated/types.ts
+++ packages/twenty-client-sdk/src/metadata/generated/types.ts
@@ -6235,6 +6235,9 @@ export default {
                     ]
                 }
             ],
+            "myConnectedAccounts": [
+                271
+            ],
             "myCalendarChannels": [
                 305,
                 {
@@ -7916,6 +7919,15 @@ export default {
             ],
             "deleteEmailGroupChannel": [
                 310,
+                {
+                    "id": [
+                        3,
+                        "UUID!"
+                    ]
+                }
+            ],
+            "deleteConnectedAccount": [
+                271,
                 {
                     "id": [
                         3,
