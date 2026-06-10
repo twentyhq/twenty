83482cd95fc5984671ef030a33da37da57f216b2
Marie Stoppa <marie@twenty.com>
2026-06-10 10:50:30 +0200

    fix typecheck

--- packages/twenty-server/src/engine/api/common/common-query-runners/common-create-many-query-runner/common-create-many-query-runner.service.ts
+++ packages/twenty-server/src/engine/api/common/common-query-runners/common-create-many-query-runner/common-create-many-query-runner.service.ts
@@ -329,7 +329,7 @@ export class CommonCreateManyQueryRunnerService extends CommonBaseQueryRunnerSer
       partialRecordInputs: recordsToInsert,
       workspaceId,
       objectMetadata: {
-        isCustom: flatObjectMetadata.isCustom,
+        isCustom: flatObjectMetadata.isCustom ?? false,
         nameSingular: flatObjectMetadata.nameSingular,
         fieldIdByName,
       },
