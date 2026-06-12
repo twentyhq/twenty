e2f96de7a53dae862e092764ff25e81dc9eea23c
bosiraphael <raphael.bosi@gmail.com>
2026-06-12 13:56:43 +0200

    remove

--- packages/twenty-server/src/engine/core-modules/usage/enums/usage-resource-type.enum.ts
+++ packages/twenty-server/src/engine/core-modules/usage/enums/usage-resource-type.enum.ts
@@ -4,7 +4,6 @@ export enum UsageResourceType {
   AI = 'AI',
   WORKFLOW = 'WORKFLOW',
   APP = 'APP',
-  APPLICATION = 'APPLICATION',
   STORAGE = 'STORAGE',
   API = 'API',
   LOGIC_FUNCTION = 'LOGIC_FUNCTION',
