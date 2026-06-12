7292e51726bed24c83f72680acad82c52214ad19
bosiraphael <raphael.bosi@gmail.com>
2026-06-12 12:05:28 +0200

    add application to the billing enum

--- packages/twenty-server/src/engine/core-modules/usage/enums/usage-resource-type.enum.ts
+++ packages/twenty-server/src/engine/core-modules/usage/enums/usage-resource-type.enum.ts
@@ -4,6 +4,7 @@ export enum UsageResourceType {
   AI = 'AI',
   WORKFLOW = 'WORKFLOW',
   APP = 'APP',
+  APPLICATION = 'APPLICATION',
   STORAGE = 'STORAGE',
   API = 'API',
   LOGIC_FUNCTION = 'LOGIC_FUNCTION',
