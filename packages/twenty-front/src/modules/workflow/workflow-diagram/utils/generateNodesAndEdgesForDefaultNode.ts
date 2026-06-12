b0272108cf75620982a746b35f97385fb2a110f3
bosiraphael <raphael.bosi@gmail.com>
2026-06-12 14:29:25 +0200

    Only set logicFunctionId on logic function nodes to fix diagram snapshots

--- packages/twenty-front/src/modules/workflow/workflow-diagram/utils/generateNodesAndEdgesForDefaultNode.ts
+++ packages/twenty-front/src/modules/workflow/workflow-diagram/utils/generateNodesAndEdgesForDefaultNode.ts
@@ -47,10 +47,9 @@ export const generateNodesAndEdgesForDefaultNode = ({
       nodeType: 'action',
       actionType: step.type,
       name: step.name,
-      logicFunctionId:
-        step.type === 'LOGIC_FUNCTION'
-          ? step.settings.input.logicFunctionId
-          : undefined,
+      ...(step.type === 'LOGIC_FUNCTION'
+        ? { logicFunctionId: step.settings.input.logicFunctionId }
+        : {}),
       hasNextStepIds:
         isDefined(step.nextStepIds) && step.nextStepIds.length > 0,
       stepId: step.id,
