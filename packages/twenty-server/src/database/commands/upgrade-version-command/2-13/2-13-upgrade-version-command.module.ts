0da56fbe86d746e240decfa170153e89b4bd6557
Félix Malfait <felix@twenty.com>
2026-06-13 18:24:00 +0200

    chore(emailing): defer existing-workspace backfill commands to a separate PR

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-13/2-13-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-13/2-13-upgrade-version-command.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 
 import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
-import { BackfillMessageStandardObjectsCommand } from 'src/database/commands/upgrade-version-command/2-13/2-13-workspace-command-1799000031000-backfill-message-standard-objects.command';
-import { AddComposeCampaignCommandMenuItemCommand } from 'src/database/commands/upgrade-version-command/2-13/2-13-workspace-command-1799000040000-add-compose-campaign-command-menu-item.command';
 import { SyncStandardUiCapabilityFlagsCommand } from 'src/database/commands/upgrade-version-command/2-13/2-13-workspace-command-1781277460000-sync-standard-ui-capability-flags.command';
 import { SyncCreateRecordCommandAvailabilityExpressionCommand } from 'src/database/commands/upgrade-version-command/2-13/2-13-workspace-command-1781277470000-sync-create-record-command-availability-expression.command';
 import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
@@ -17,8 +15,6 @@ import { WorkspaceMigrationModule } from 'src/engine/workspace-manager/workspace
     WorkspaceMigrationModule,
   ],
   providers: [
-    BackfillMessageStandardObjectsCommand,
-    AddComposeCampaignCommandMenuItemCommand,
     SyncStandardUiCapabilityFlagsCommand,
     SyncCreateRecordCommandAvailabilityExpressionCommand,
   ],
