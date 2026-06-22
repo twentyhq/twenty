7b7abc10fbac7a518920c5a4308e673eebac3f49
jubinsoni <jubinkumarsoni@gmail.com>
2026-06-22 12:56:19 +0530

    suport non aws providers 1

--- packages/twenty-server/src/engine/core-modules/twenty-config/decorators/is-aws-region.decorator.ts
+++ packages/twenty-server/src/engine/core-modules/twenty-config/decorators/is-aws-region.decorator.ts
@@ -1,5 +1,6 @@
 import {
   registerDecorator,
+  type ValidationArguments,
   type ValidationOptions,
   ValidatorConstraint,
   type ValidatorConstraintInterface,
@@ -7,7 +8,13 @@ import {
 
 @ValidatorConstraint({ async: true })
 export class IsAWSRegionConstraint implements ValidatorConstraintInterface {
-  validate(region: string) {
+  validate(region: string, args?: ValidationArguments) {
+    const object = args?.object as any;
+
+    if (args?.property === 'STORAGE_S3_REGION' && object?.STORAGE_S3_ENDPOINT) {
+      return typeof region === 'string' && region.length > 0;
+    }
+
     const regex = /^[a-z]{2}-[a-z]+-\d{1}$/;
 
     return regex.test(region); // Returns true if region matches regex
