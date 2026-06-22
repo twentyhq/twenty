0282e8995a18ab0f6f28921f36b3da6153f05257
martmull <martmull@hotmail.fr>
2026-06-22 09:45:54 +0200

    Add twenty-discord on internal ci apps

--- packages/twenty-apps/internal/twenty-discord/src/logic-functions/discord-update-message.ts
+++ packages/twenty-apps/internal/twenty-discord/src/logic-functions/discord-update-message.ts
@@ -1,5 +1,5 @@
 import { defineLogicFunction } from 'twenty-sdk/define';
-import { jsonSchemaToInputSchema } from 'twenty-shared/logic-function';
+import { jsonSchemaToInputSchema } from 'src/logic-functions/utils/json-schema-to-input-schema';
 
 import { DISCORD_UPDATE_MESSAGE_UNIVERSAL_IDENTIFIER } from 'src/constants/universal-identifiers';
 import { discordUpdateMessageHandler } from 'src/logic-functions/handlers/discord-update-message-handler';
