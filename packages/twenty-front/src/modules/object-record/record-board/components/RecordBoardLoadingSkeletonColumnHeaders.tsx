73e3624fc7029b22d695f04e5cb6bd4c07c137f9
Abdul Rahman <ar5438376@gmail.com>
2026-06-24 15:10:56 +0530

    refactor: enhance loading skeleton for record board column headers
    - Integrated the react-loading-skeleton library to improve the visual representation of loading states in the RecordBoardLoadingSkeletonColumnHeaders component.
    - Utilized theme context for dynamic styling, ensuring consistency with the application's theme.
    - Streamlined the rendering logic for skeleton headers, enhancing code clarity and maintainability.

--- packages/twenty-front/src/modules/object-record/record-board/components/RecordBoardLoadingSkeletonColumnHeaders.tsx
+++ packages/twenty-front/src/modules/object-record/record-board/components/RecordBoardLoadingSkeletonColumnHeaders.tsx
@@ -1,10 +1,10 @@
-import { styled } from '@linaria/react';
-import { themeCssVariables } from 'twenty-ui/theme-constants';
-
 import { RECORD_BOARD_COLUMN_WIDTH } from '@/object-record/record-board/constants/RecordBoardColumnWidth';
 import { RECORD_BOARD_COLUMN_WIDTH_CSS_VARIABLE_NAME } from '@/object-record/record-board/constants/RecordBoardColumnWidthCssVariableName';
 import { RECORD_BOARD_LOADING_SKELETON_COLUMNS_COUNT } from '@/object-record/record-board/constants/RecordBoardLoadingSkeletonColumnsCount';
-
+import { styled } from '@linaria/react';
+import { useContext } from 'react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import { ThemeContext, themeCssVariables } from 'twenty-ui/theme-constants';
 const StyledHeaderCell = styled.div`
   align-items: center;
   display: flex;
@@ -20,20 +20,25 @@ const StyledHeaderCell = styled.div`
   padding: 0 ${themeCssVariables.spacing[2]};
 `;
 
-const StyledTitleSkeleton = styled.div`
-  background-color: ${themeCssVariables.background.tertiary};
-  border-radius: ${themeCssVariables.border.radius.sm};
-  height: 16px;
-  width: 96px;
-`;
-
 export const RecordBoardLoadingSkeletonColumnHeaders = () => {
-  return Array.from(
-    { length: RECORD_BOARD_LOADING_SKELETON_COLUMNS_COUNT },
-    (_, index) => (
-      <StyledHeaderCell key={`record-board-skeleton-column-header-${index}`}>
-        <StyledTitleSkeleton />
-      </StyledHeaderCell>
-    ),
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <SkeletonTheme
+      baseColor={theme.background.tertiary}
+      highlightColor={theme.background.transparent.lighter}
+      borderRadius={4}
+    >
+      {Array.from(
+        { length: RECORD_BOARD_LOADING_SKELETON_COLUMNS_COUNT },
+        (_, index) => (
+          <StyledHeaderCell
+            key={`record-board-skeleton-column-header-${index}`}
+          >
+            <Skeleton width={96} height={16} />
+          </StyledHeaderCell>
+        ),
+      )}
+    </SkeletonTheme>
   );
 };
