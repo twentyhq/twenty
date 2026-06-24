22daae834fe4c2644b3928c10253e5c10e5459e9
Abdul Rahman <ar5438376@gmail.com>
2026-06-24 22:09:59 +0530

    refactor(view-group): improve view groups filter mapping logic
    - Enhanced the `useGetViewGroupsFilters` hook by refining the mapping logic for view groups, introducing a conditional check for relation groups with no field value.
    - This change ensures that the correct operand is applied based on the presence of field values, improving the accuracy of the filters returned.
    - The modifications contribute to better maintainability and clarity in the filter logic for view groups, particularly in relation to many-to-one fields.

--- packages/twenty-front/src/modules/views/hooks/useGetViewGroupsFilters.ts
+++ packages/twenty-front/src/modules/views/hooks/useGetViewGroupsFilters.ts
@@ -32,19 +32,30 @@ export const useGetViewGroupsFilters = (): RecordFilter[] => {
 
   return (currentView?.viewGroups ?? [])
     .filter((recordGroup) => !recordGroup.isVisible)
-    .filter(
-      (recordGroup) =>
-        !isRelationGroupBy ||
-        (isDefined(recordGroup.fieldValue) && recordGroup.fieldValue !== ''),
-    )
-    .map((recordGroup) => ({
-      id: recordGroup.id,
-      fieldMetadataId: mainGroupByFieldMetadataId,
-      value: JSON.stringify([recordGroup.fieldValue]),
-      operand: ViewFilterOperand.IS_NOT,
-      displayValue: '',
-      type: filterType,
-      label: '',
-    }))
-    .filter(isDefined);
+    .map((recordGroup) => {
+      const isNoValueGroup =
+        !isDefined(recordGroup.fieldValue) || recordGroup.fieldValue === '';
+
+      if (isRelationGroupBy && isNoValueGroup) {
+        return {
+          id: recordGroup.id,
+          fieldMetadataId: mainGroupByFieldMetadataId,
+          value: '',
+          operand: ViewFilterOperand.IS_NOT_EMPTY,
+          displayValue: '',
+          type: filterType,
+          label: '',
+        };
+      }
+
+      return {
+        id: recordGroup.id,
+        fieldMetadataId: mainGroupByFieldMetadataId,
+        value: JSON.stringify([recordGroup.fieldValue]),
+        operand: ViewFilterOperand.IS_NOT,
+        displayValue: '',
+        type: filterType,
+        label: '',
+      };
+    });
 };
