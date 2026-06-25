79907c99fa8838589861c255ddf7e32dd1d16a16
Abdul Rahman <ar5438376@gmail.com>
2026-06-25 06:28:38 +0530

    refactor(useSetRecordIdsForColumn): improve record filtering logic
    - Reordered import statements for better organization and clarity.
    - Updated the record filtering logic to use the `getRecordGroupByFieldColumnName` utility, enhancing code readability and maintainability.
    - This change ensures that the filtering is based on the correct column name derived from the field metadata, improving the accuracy of record selection.

--- packages/twenty-front/src/modules/object-record/record-board/hooks/useSetRecordIdsForColumn.ts
+++ packages/twenty-front/src/modules/object-record/record-board/hooks/useSetRecordIdsForColumn.ts
@@ -1,13 +1,14 @@
-import { useCallback } from 'react';
 import { useStore } from 'jotai';
+import { useCallback } from 'react';
 
 import { emptyRecordGroupByIdComponentFamilyState } from '@/object-record/record-group/states/emptyRecordGroupByIdComponentFamilyState';
 import { recordGroupDefinitionFamilyState } from '@/object-record/record-group/states/recordGroupDefinitionFamilyState';
+import { getRecordGroupByFieldColumnName } from '@/object-record/record-group/utils/getRecordGroupByFieldColumnName';
 import { recordIndexGroupFieldMetadataItemComponentState } from '@/object-record/record-index/states/recordIndexGroupFieldMetadataComponentState';
 import { recordIndexRecordIdsByGroupComponentFamilyState } from '@/object-record/record-index/states/recordIndexRecordIdsByGroupComponentFamilyState';
 import { type ObjectRecord } from '@/object-record/types/ObjectRecord';
-import { useAtomComponentStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateCallbackState';
 import { useAtomComponentFamilyStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentFamilyStateCallbackState';
+import { useAtomComponentStateCallbackState } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateCallbackState';
 import { isDefined } from 'twenty-shared/utils';
 import { isDeeplyEqual } from '~/utils/isDeeplyEqual';
 
@@ -48,10 +49,13 @@ export const useSetRecordIdsForColumn = (recordBoardId?: string) => {
         return;
       }
 
+      const recordGroupColumnName = getRecordGroupByFieldColumnName(
+        currentRecordGroupFieldMetadata,
+      );
+
       const recordGroupRowIds = records
         .filter(
-          (record) =>
-            record[currentRecordGroupFieldMetadata.name] === recordGroup?.value,
+          (record) => record[recordGroupColumnName] === recordGroup?.value,
         )
         .map((record) => record.id);
 
