2a8ace3706483de22f9ad54b36032562d83ce369
bosiraphael <raphael.bosi@gmail.com>
2026-06-26 17:56:41 +0200

    Use isUIEditable for jobTitle in upgrade command

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-17/2-17-workspace-command-1801000010000-add-workspace-member-job-title-field.command.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-17/2-17-workspace-command-1801000010000-add-workspace-member-job-title-field.command.ts
@@ -85,7 +85,7 @@ export class AddWorkspaceMemberJobTitleFieldCommand extends ActiveOrSuspendedWor
       description: 'Workspace member job title',
       icon: 'IconBriefcase',
       isNullable: true,
-      isUIReadOnly: true,
+      isUIEditable: false,
       isSystem: true,
       isActive: true,
       universalIdentifier: JOB_TITLE_FIELD_UNIVERSAL_IDENTIFIER,
