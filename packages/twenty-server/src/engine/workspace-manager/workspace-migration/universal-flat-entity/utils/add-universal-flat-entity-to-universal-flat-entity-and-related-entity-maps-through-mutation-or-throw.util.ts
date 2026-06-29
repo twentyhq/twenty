f4db35d06492e7733a8f109fc96988d6500c6fd9
prastoin <paul@twenty.com>
2026-06-29 16:01:05 +0200

    fix(server): action handler optimistic rendering

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/utils/add-universal-flat-entity-to-universal-flat-entity-and-related-entity-maps-through-mutation-or-throw.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/utils/add-universal-flat-entity-to-universal-flat-entity-and-related-entity-maps-through-mutation-or-throw.util.ts
@@ -11,6 +11,7 @@ import { type MetadataRelatedFlatEntityMapsKeys } from 'src/engine/metadata-modu
 import { type MetadataUniversalFlatEntityAndRelatedUniversalFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/metadata-related-types.type';
 import { type MetadataUniversalFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-universal-flat-entity.type';
 import { findFlatEntityByUniversalIdentifierOrThrow } from 'src/engine/metadata-modules/flat-entity/utils/find-flat-entity-by-universal-identifier-or-throw.util';
+import { findFlatEntityByUniversalIdentifier } from 'src/engine/metadata-modules/flat-entity/utils/find-flat-entity-by-universal-identifier.util';
 import { getMetadataFlatEntityMapsKey } from 'src/engine/metadata-modules/flat-entity/utils/get-metadata-flat-entity-maps-key.util';
 import { type UniversalFlatEntityMaps } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-maps.type';
 import { addUniversalFlatEntityToUniversalFlatEntityMapsThroughMutationOrThrow } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/utils/add-universal-flat-entity-to-universal-flat-entity-maps-through-mutation-or-throw.util';
@@ -22,6 +23,7 @@ type AddUniversalFlatEntityToUniversalFlatEntityAndRelatedEntityMapsThroughMutat
   metadataName: T;
   universalFlatEntity: MetadataUniversalFlatEntity<T>;
   universalFlatEntityAndRelatedMapsToMutate: MetadataUniversalFlatEntityAndRelatedUniversalFlatEntityMaps<T>;
+  skipMissingRelatedEntities?: boolean;
 };
 
 export const addUniversalFlatEntityToUniversalFlatEntityAndRelatedEntityMapsThroughMutationOrThrow =
@@ -29,6 +31,7 @@ export const addUniversalFlatEntityToUniversalFlatEntityAndRelatedEntityMapsThro
     metadataName,
     universalFlatEntity,
     universalFlatEntityAndRelatedMapsToMutate,
+    skipMissingRelatedEntities = false,
   }: AddUniversalFlatEntityToUniversalFlatEntityAndRelatedEntityMapsThroughMutationOrThrowArgs<T>) => {
     const flatEntityMapsKey = getMetadataFlatEntityMapsKey(metadataName);
 
@@ -94,6 +97,18 @@ export const addUniversalFlatEntityToUniversalFlatEntityAndRelatedEntityMapsThro
 
       if (!isDefined(universalForeignKeyValue)) {
         continue;
+      }
+
+      if (skipMissingRelatedEntities) {
+        const maybeRelatedUniversalFlatEntity =
+          findFlatEntityByUniversalIdentifier({
+            universalIdentifier: universalForeignKeyValue,
+            flatEntityMaps: relatedUniversalFlatEntityMaps,
+          });
+
+        if (!isDefined(maybeRelatedUniversalFlatEntity)) {
+          continue;
+        }
       }
 
       const relatedUniversalFlatEntity =
