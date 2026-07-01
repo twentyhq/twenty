c5f44012eabee6609f836a3c6a96c08b99fbf340
prastoin <paul@twenty.com>
2026-07-01 16:04:50 +0200

    move commands to 2.19

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-19/2-19-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-19/2-19-upgrade-version-command.module.ts
@@ -3,15 +3,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
 import { BackfillWorkspaceCustomApplicationRegistrationCommand } from 'src/database/commands/upgrade-version-command/2-19/2-19-workspace-command-1820000000000-backfill-workspace-custom-application-registration.command';
-import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
 import { ApplicationEntity } from 'src/engine/core-modules/application/application.entity';
+import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
+import { IndexMetadataEntity } from 'src/engine/metadata-modules/index-metadata/index-metadata.entity';
+import { WorkspaceMetadataVersionModule } from 'src/engine/metadata-modules/workspace-metadata-version/workspace-metadata-version.module';
+import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
+import { WorkspaceMigrationModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration.module';
 
 @Module({
   imports: [
     ApplicationModule,
-    TypeOrmModule.forFeature([WorkspaceEntity, ApplicationEntity]),
+    TypeOrmModule.forFeature([
+      WorkspaceEntity,
+      ApplicationEntity,
+      IndexMetadataEntity,
+    ]),
     WorkspaceIteratorModule,
+    WorkspaceCacheModule,
+    WorkspaceIteratorModule,
+    WorkspaceMetadataVersionModule,
+    WorkspaceMigrationModule,
   ],
   providers: [BackfillWorkspaceCustomApplicationRegistrationCommand],
 })
