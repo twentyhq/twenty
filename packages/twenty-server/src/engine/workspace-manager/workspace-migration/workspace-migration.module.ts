1a8a282f4a33e41838a0e31edfa6c6114ccb9113
prastoin <paul@twenty.com>
2026-07-02 14:02:59 +0200

    feat(server): workspace migration flat entity maps service

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration.module.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/workspace-migration.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { MetadataSideEffectModule } from 'src/engine/metadata-modules/metadata-side-effect/metadata-side-effect.module';
 import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
 import { WorkspaceMigrationBuildOrchestratorService } from 'src/engine/workspace-manager/workspace-migration/services/workspace-migration-build-orchestrator.service';
+import { WorkspaceMigrationFlatEntityMapsService } from 'src/engine/workspace-manager/workspace-migration/services/workspace-migration-flat-entity-maps.service';
 import { WorkspaceMigrationValidateBuildAndRunService } from 'src/engine/workspace-manager/workspace-migration/services/workspace-migration-validate-build-and-run-service';
 import { WorkspaceMigrationBuilderModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-builder/workspace-migration-builder.module';
 import { WorkspaceMigrationRunnerModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-runner/workspace-migration-runner.module';
@@ -17,6 +18,7 @@ import { WorkspaceMigrationRunnerModule } from 'src/engine/workspace-manager/wor
   providers: [
     WorkspaceMigrationValidateBuildAndRunService,
     WorkspaceMigrationBuildOrchestratorService,
+    WorkspaceMigrationFlatEntityMapsService,
   ],
   exports: [
     WorkspaceMigrationValidateBuildAndRunService,
