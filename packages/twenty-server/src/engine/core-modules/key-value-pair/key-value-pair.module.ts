93accb9b8dc419166698b3c062b7648441759bb3
Abdul Rahman <ar5438376@gmail.com>
2026-07-06 15:13:03 +0530

    refactor(key-value-pair): reorder module imports for improved clarity

--- packages/twenty-server/src/engine/core-modules/key-value-pair/key-value-pair.module.ts
+++ packages/twenty-server/src/engine/core-modules/key-value-pair/key-value-pair.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { KeyValuePairService } from 'src/engine/core-modules/key-value-pair/key-value-pair.service';
-import { KeyValuePairEntity } from 'src/engine/core-modules/key-value-pair/key-value-pair.entity';
 import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
+import { KeyValuePairEntity } from 'src/engine/core-modules/key-value-pair/key-value-pair.entity';
+import { KeyValuePairService } from 'src/engine/core-modules/key-value-pair/key-value-pair.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([KeyValuePairEntity]), TypeORMModule],
