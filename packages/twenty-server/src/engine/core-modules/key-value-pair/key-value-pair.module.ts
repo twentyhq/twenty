0320821f94f20eeec38cb956afad8627813bd512
Abdul Rahman <ar5438376@gmail.com>
2026-07-06 15:11:18 +0530

    refactor(key-value-pair): streamline module imports by removing GraphQL dependencies

--- packages/twenty-server/src/engine/core-modules/key-value-pair/key-value-pair.module.ts
+++ packages/twenty-server/src/engine/core-modules/key-value-pair/key-value-pair.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
-
-import { NestjsQueryGraphQLModule } from '@ptc-org/nestjs-query-graphql';
-import { NestjsQueryTypeOrmModule } from '@ptc-org/nestjs-query-typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { KeyValuePairService } from 'src/engine/core-modules/key-value-pair/key-value-pair.service';
 import { KeyValuePairEntity } from 'src/engine/core-modules/key-value-pair/key-value-pair.entity';
 import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 
 @Module({
-  imports: [
-    NestjsQueryGraphQLModule.forFeature({
-      imports: [
-        NestjsQueryTypeOrmModule.forFeature([KeyValuePairEntity]),
-        TypeORMModule,
-      ],
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([KeyValuePairEntity]), TypeORMModule],
   exports: [KeyValuePairService],
   providers: [KeyValuePairService],
 })
