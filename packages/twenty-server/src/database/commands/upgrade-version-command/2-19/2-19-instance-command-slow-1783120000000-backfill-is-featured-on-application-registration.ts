11169b918e9efcc78800afb5779456b6a34ea8c3
martmull <martmull@hotmail.fr>
2026-07-07 16:54:59 +0200

    Code review returns

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-19/2-19-instance-command-slow-1783120000000-backfill-is-featured-on-application-registration.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-19/2-19-instance-command-slow-1783120000000-backfill-is-featured-on-application-registration.ts
@@ -1,12 +1,13 @@
 import { DataSource, QueryRunner } from 'typeorm';
 
-import { MARKETPLACE_FEATURED_APPLICATIONS } from 'src/engine/core-modules/application/application-marketplace/constants/marketplace-featured-applications.constant';
 import { RegisteredInstanceCommand } from 'src/engine/core-modules/upgrade/decorators/registered-instance-command.decorator';
 import { SlowInstanceCommand } from 'src/engine/core-modules/upgrade/interfaces/slow-instance-command.interface';
 
-const FEATURED_UNIVERSAL_IDENTIFIERS = MARKETPLACE_FEATURED_APPLICATIONS.map(
-  (application) => application.universalIdentifier,
-);
+const FEATURED_UNIVERSAL_IDENTIFIERS = [
+  '8da4b8b5-5edf-4880-b51f-ab6e679ec617',
+  '4a1178c1-3535-4a47-b592-231d3216b36f',
+  '66a504cc-0a75-410e-a43f-cdeae1db1522'
+];
 
 @RegisteredInstanceCommand('2.19.0', 1783120000000, { type: 'slow' })
 export class BackfillIsFeaturedOnApplicationRegistrationSlowInstanceCommand
