96407b2382904ab3ec1bf1759118cab0299d41a9
Abdul Rahman <ar5438376@gmail.com>
2026-07-08 15:23:20 +0530

    Refactor avatar file ID retrieval in SearchService for improved clarity and type safety
    - Simplified access to the avatar file ID by removing unnecessary type casting.
    - Updated the validation check to ensure the file ID is a non-empty string, enhancing type safety.

--- packages/twenty-server/src/engine/core-modules/search/services/search.service.ts
+++ packages/twenty-server/src/engine/core-modules/search/services/search.service.ts
@@ -14,7 +14,6 @@ import { Brackets, type ObjectLiteral } from 'typeorm';
 
 import { type ObjectRecordFilter } from 'src/engine/api/graphql/workspace-query-builder/interfaces/object-record.interface';
 
-import { FileOutput } from 'src/engine/api/common/common-args-processors/data-arg-processor/types/file-item.type';
 import { GraphqlQueryParser } from 'src/engine/api/graphql/graphql-query-runner/graphql-query-parsers/graphql-query.parser';
 import {
   decodeCursor,
@@ -658,11 +657,9 @@ export class SearchService {
 
     switch (imageIdentifierField.type) {
       case FieldMetadataType.FILES: {
-        const avatarFileId = (
-          record[imageIdentifierField.name] as FileOutput[]
-        )?.[0]?.fileId;
+        const avatarFileId = record[imageIdentifierField.name]?.[0]?.fileId;
 
-        if (!isDefined(avatarFileId)) {
+        if (!isNonEmptyString(avatarFileId)) {
           return '';
         }
 
