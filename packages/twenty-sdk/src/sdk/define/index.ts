7a54527d2edcee57d61176dbdc6e50bc4cb7dcfe
martmull <martmull@hotmail.fr>
2026-07-09 12:46:09 +0200

    Revert "Test ci red"
    This reverts commit bb7d2a6339c263acf53287e5cdcc1e398f1ffea0.

--- packages/twenty-sdk/src/sdk/define/index.ts
+++ packages/twenty-sdk/src/sdk/define/index.ts
@@ -1,5 +1,8 @@
 export { defineAgent } from '@/sdk/define/agents/define-agent';
 
+export type { ApplicationConfig } from '@/sdk/define/application/application-config';
+export { defineApplication } from '@/sdk/define/application/define-application';
+
 export type {
   DefinableEntity,
   DefineEntity,
