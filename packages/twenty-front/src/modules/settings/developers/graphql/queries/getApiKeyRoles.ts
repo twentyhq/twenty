a1bc6f05cba08cdefaf8b3e7c83b49b592953566
Marie Stoppa <marie@twenty.com>
2026-07-21 15:17:45 +0200

    update GET_API_KEY_ROLES

--- packages/twenty-front/src/modules/settings/developers/graphql/queries/getApiKeyRoles.ts
+++ packages/twenty-front/src/modules/settings/developers/graphql/queries/getApiKeyRoles.ts
@@ -1,11 +1,12 @@
-import { ROLE_FRAGMENT } from '@/settings/roles/graphql/fragments/roleFragment';
 import gql from 'graphql-tag';
 
 export const GET_API_KEY_ROLES = gql`
   query GetApiKeyRoles {
     getApiKeyRoles {
-      ...RoleFragment
+      id
+      label
+      icon
+      canBeAssignedToApiKeys
     }
   }
-  ${ROLE_FRAGMENT}
 `;
