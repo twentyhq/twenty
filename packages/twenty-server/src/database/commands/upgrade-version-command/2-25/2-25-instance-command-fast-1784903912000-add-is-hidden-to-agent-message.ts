49c54224e83e532d34080045133e5a3f8e98dfeb
bosiraphael <raphael.bosi@gmail.com>
2026-07-24 16:45:31 +0200

    Move add-is-hidden instance command to the current 2.25 version directory

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-25/2-25-instance-command-fast-1784903912000-add-is-hidden-to-agent-message.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-25/2-25-instance-command-fast-1784903912000-add-is-hidden-to-agent-message.ts
@@ -3,7 +3,7 @@ import { type QueryRunner } from 'typeorm';
 import { RegisteredInstanceCommand } from 'src/engine/core-modules/upgrade/decorators/registered-instance-command.decorator';
 import { type FastInstanceCommand } from 'src/engine/core-modules/upgrade/interfaces/fast-instance-command.interface';
 
-@RegisteredInstanceCommand('2.24.0', 1785000000000)
+@RegisteredInstanceCommand('2.25.0', 1784903912000)
 export class AddIsHiddenToAgentMessageFastInstanceCommand
   implements FastInstanceCommand
 {
