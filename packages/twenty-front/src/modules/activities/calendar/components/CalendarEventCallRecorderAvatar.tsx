715a29730d3c8b8a413a4b2a46660edffcda183b
martmull <martmull@hotmail.fr>
2026-07-28 15:15:31 +0200

    Update style

--- packages/twenty-front/src/modules/activities/calendar/components/CalendarEventCallRecorderAvatar.tsx
+++ packages/twenty-front/src/modules/activities/calendar/components/CalendarEventCallRecorderAvatar.tsx
@@ -4,12 +4,12 @@ import { themeCssVariables } from 'twenty-ui/theme-constants';
 import { CallRecordingStatus } from '~/generated/graphql';
 
 const CALL_RECORDER_STATUS_BORDERS: Record<CallRecordingStatus, string> = {
-  [CallRecordingStatus.SCHEDULED]: `2px solid ${themeCssVariables.tag.text.blue}`,
-  [CallRecordingStatus.JOINING]: `2px solid ${themeCssVariables.tag.text.orange}`,
-  [CallRecordingStatus.RECORDING]: `2px solid ${themeCssVariables.tag.text.orange}`,
-  [CallRecordingStatus.PROCESSING]: `2px solid ${themeCssVariables.tag.text.orange}`,
-  [CallRecordingStatus.COMPLETED]: `2px solid ${themeCssVariables.tag.text.green}`,
-  [CallRecordingStatus.FAILED]: `2px solid ${themeCssVariables.tag.text.red}`,
+  [CallRecordingStatus.SCHEDULED]: `1px solid ${themeCssVariables.tag.text.blue}`,
+  [CallRecordingStatus.JOINING]: `1px solid ${themeCssVariables.tag.text.orange}`,
+  [CallRecordingStatus.RECORDING]: `1px solid ${themeCssVariables.tag.text.orange}`,
+  [CallRecordingStatus.PROCESSING]: `1px solid ${themeCssVariables.tag.text.orange}`,
+  [CallRecordingStatus.COMPLETED]: `1px solid ${themeCssVariables.tag.text.green}`,
+  [CallRecordingStatus.FAILED]: `1px solid ${themeCssVariables.tag.text.red}`,
 };
 
 type CalendarEventCallRecorderAvatarProps = {
