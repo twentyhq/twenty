d0c04c7f088fe5573e51d87aaf366466475e8015
Félix Malfait <felix.malfait@gmail.com>
2026-07-28 09:16:14 +0000

    Re-slot 2-25 campaign commands above main's 1785173910915

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-25/2-25-instance-command-fast-1785229950000-add-message-campaign-widget-type.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-25/2-25-instance-command-fast-1785229950000-add-message-campaign-widget-type.ts
@@ -3,7 +3,7 @@ import { type QueryRunner } from 'typeorm';
 import { RegisteredInstanceCommand } from 'src/engine/core-modules/upgrade/decorators/registered-instance-command.decorator';
 import { type FastInstanceCommand } from 'src/engine/core-modules/upgrade/interfaces/fast-instance-command.interface';
 
-@RegisteredInstanceCommand('2.25.0', 1785168110000)
+@RegisteredInstanceCommand('2.25.0', 1785229950000)
 export class AddMessageCampaignWidgetTypeFastInstanceCommand
   implements FastInstanceCommand
 {
