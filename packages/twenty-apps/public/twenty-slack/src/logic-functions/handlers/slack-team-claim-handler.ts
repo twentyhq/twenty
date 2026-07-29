b4d2d63626e9d38e66a007a63f3f8244116c64a0
Abdul Rahman <ar5438376@gmail.com>
2026-07-29 19:00:22 +0530

    refactor(twenty-slack): simplify slackTeamClaimHandler by removing async keyword
    The slackTeamClaimHandler function has been refactored to remove the async keyword, directly calling the claimSlackTeam utility function. This change streamlines the code and improves readability.

--- packages/twenty-apps/public/twenty-slack/src/logic-functions/handlers/slack-team-claim-handler.ts
+++ packages/twenty-apps/public/twenty-slack/src/logic-functions/handlers/slack-team-claim-handler.ts
@@ -1,7 +1,7 @@
 import { type SlackTeamClaimPayload } from 'src/logic-functions/types/slack-team-claim-payload.type';
 import { claimSlackTeam } from 'src/logic-functions/utils/claim-slack-team';
 
-export const slackTeamClaimHandler = async (payload: SlackTeamClaimPayload) =>
+export const slackTeamClaimHandler = (payload: SlackTeamClaimPayload) =>
   claimSlackTeam({
     connectedAccountId: payload.connectedAccountId,
   });
