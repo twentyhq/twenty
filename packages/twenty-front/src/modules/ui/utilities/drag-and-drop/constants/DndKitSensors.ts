0ea602a49449b4d5aab45e55cd18efc2752b2ed5
Priyanshu Bartwal <bartwalpriyanshu00@gmail.com>
2026-08-02 16:09:57 +0530

    chore: update comment msg

--- packages/twenty-front/src/modules/ui/utilities/drag-and-drop/constants/DndKitSensors.ts
+++ packages/twenty-front/src/modules/ui/utilities/drag-and-drop/constants/DndKitSensors.ts
@@ -3,6 +3,8 @@ import { KeyboardSensor, PointerSensor } from '@dnd-kit/react';
 
 import { shouldPreventDragActivation } from '@/ui/utilities/drag-and-drop/utils/shouldPreventDragActivation';
 
+// Mouse/pen drags start past 8px so clicks still register. Touch needs a 200ms
+// press-and-hold instead, otherwise every scroll swipe would start a drag.
 export const DND_KIT_SENSORS = [
   PointerSensor.configure({
     activationConstraints: (event) =>
