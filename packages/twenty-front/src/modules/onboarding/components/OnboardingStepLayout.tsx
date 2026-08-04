2eeb04a14943f2d6a54ae7e7e955103998192c2d
bosiraphael <raphael.bosi@gmail.com>
2026-08-04 17:58:08 +0200

    Warm the Cal embed before the book-call step is reached

--- packages/twenty-front/src/modules/onboarding/components/OnboardingStepLayout.tsx
+++ packages/twenty-front/src/modules/onboarding/components/OnboardingStepLayout.tsx
@@ -1,5 +1,6 @@
 import { OnboardingLayout } from '@/onboarding/components/OnboardingLayout';
 import { OnboardingTransitionOutlet } from '@/onboarding/components/OnboardingTransitionOutlet';
+import { PrefetchBookCallStepEffect } from '@/onboarding/effect-components/PrefetchBookCallStepEffect';
 import { PrefetchPlanRequiredStepEffect } from '@/onboarding/effect-components/PrefetchPlanRequiredStepEffect';
 import { useOnboardingFreeCreditsTotal } from '@/onboarding/hooks/useOnboardingFreeCreditsTotal';
 
@@ -8,6 +9,7 @@ export const OnboardingStepLayout = () => {
 
   return (
     <OnboardingLayout freeCredits={freeCredits}>
+      <PrefetchBookCallStepEffect />
       <PrefetchPlanRequiredStepEffect />
       <OnboardingTransitionOutlet />
     </OnboardingLayout>
