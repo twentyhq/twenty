3b90243c912ecd1282238bb8ec67761ea3ab4428
BOHEUS <nothin_spe@2abc.hub.pl>
2026-08-04 17:06:25 +0200

    Prastoin review 2

--- packages/twenty-server/src/engine/api/common/common-args-processors/data-arg-processor/utils/is-null-equivalent-array-field-value.util.ts
+++ packages/twenty-server/src/engine/api/common/common-args-processors/data-arg-processor/utils/is-null-equivalent-array-field-value.util.ts
@@ -1,14 +1,8 @@
 export const isNullEquivalentArrayFieldValue = (value: unknown): boolean => {
-  if (value === null) {
-    return true;
-  }
-
-  if (Array.isArray(value)) {
-    return (
-      value.length === 0 ||
-      value.every((item) => isNullEquivalentArrayFieldValue(item))
-    );
-  }
-
-  return typeof value === 'object' && Object.keys(value).length === 0;
+  return value === null ||
+    (
+      Array.isArray(value)) &&
+      (value.length === 0 || value.every((item) => isNullEquivalentArrayFieldValue(item))
+    ) ||
+    (typeof value === 'object' && Object.keys(value).length === 0);
 };
