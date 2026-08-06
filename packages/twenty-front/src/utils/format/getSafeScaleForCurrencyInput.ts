0607b8189c598c40f05bc52cdeb8de7b11abcecc
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-08-06 17:04:33 +0530

    style: format getSafeScaleForCurrencyInput

--- packages/twenty-front/src/utils/format/getSafeScaleForCurrencyInput.ts
+++ packages/twenty-front/src/utils/format/getSafeScaleForCurrencyInput.ts
@@ -2,9 +2,12 @@ type SafeScaleProps = {
   value: string;
   decimals?: number;
 };
-export const getSafeScaleForCurrencyInput = ({value,decimals}:SafeScaleProps):number => {
-    const decimalPart = value.split(".")[1];
-    const decimalCount = decimalPart?.length ?? 0
+export const getSafeScaleForCurrencyInput = ({
+  value,
+  decimals,
+}: SafeScaleProps): number => {
+  const decimalPart = value.split('.')[1];
+  const decimalCount = decimalPart?.length ?? 0;
 
-    return Math.max(decimalCount , decimals ?? 0)
-}
+  return Math.max(decimalCount, decimals ?? 0);
+};
