691eadffe41e71c895983a67637904f7f7161487
Félix Malfait <felix.malfait@gmail.com>
2026-08-14 15:11:59 +0000

    Format viewport-unit compensations

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawer.tsx
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/components/NavigationDrawer.tsx
@@ -47,7 +47,8 @@ const StyledAnimatedContainer = styled.div<{
       : `${NAVIGATION_DRAWER_COLLAPSED_WIDTH}px`};
 
   @media (max-width: ${MOBILE_VIEWPORT}px) {
-    width: ${({ isExpanded }) => (isExpanded ? 'calc(100vw / var(--t-zoom, 1))' : '0')};
+    width: ${({ isExpanded }) =>
+      isExpanded ? 'calc(100vw / var(--t-zoom, 1))' : '0'};
   }
 `;
 
