a7678ffd2aab1d99a775c844bd8e75a83c482de7
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-19 13:28:19 -0300

    test(messaging): update isBulkMail unit test expectations (#24332)

--- packages/twenty-server/src/modules/messaging/message-import-manager/utils/__tests__/is-bulk-mail.util.spec.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/utils/__tests__/is-bulk-mail.util.spec.ts
@@ -1,29 +1,9 @@
 import { isBulkMail } from 'src/modules/messaging/message-import-manager/utils/is-bulk-mail.util';
 
 describe('isBulkMail', () => {
-  it('should detect a List-Unsubscribe header regardless of casing', () => {
-    expect(
-      isBulkMail([
-        { name: 'List-Unsubscribe', value: '<https://example.com/unsub>' },
-      ]),
-    ).toBe(true);
-
-    expect(
-      isBulkMail([
-        { name: 'list-unsubscribe', value: '<mailto:unsub@example.com>' },
-      ]),
-    ).toBe(true);
-  });
-
-  it('should detect a List-Id header', () => {
-    expect(
-      isBulkMail([{ name: 'List-Id', value: '<newsletter.example.com>' }]),
-    ).toBe(true);
-  });
-
   it('should detect bulk precedence values only', () => {
     expect(isBulkMail([{ name: 'Precedence', value: 'bulk' }])).toBe(true);
-    expect(isBulkMail([{ name: 'Precedence', value: ' List ' }])).toBe(true);
+    expect(isBulkMail([{ name: 'Precedence', value: 'junk' }])).toBe(true);
     expect(isBulkMail([{ name: 'Precedence', value: 'urgent' }])).toBe(false);
   });
 
