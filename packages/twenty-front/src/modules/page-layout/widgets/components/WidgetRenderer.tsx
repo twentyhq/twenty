f81944dc7d40fd7a4ce77ac12af882bf313a588c
Thomas des Francs <tdesfrancs@gmail.com>
2026-08-20 11:50:43 +0200

    Simplify widget renderer hierarchy

--- packages/twenty-front/src/modules/page-layout/widgets/components/WidgetRenderer.tsx
+++ packages/twenty-front/src/modules/page-layout/widgets/components/WidgetRenderer.tsx
@@ -1,19 +1,10 @@
 import { type PageLayoutWidget } from '@/page-layout/types/PageLayoutWidget';
-import { RecordPageWidgetRenderer } from '@/page-layout/widgets/components/RecordPageWidgetRenderer';
 import { WidgetRendererContent } from '@/page-layout/widgets/components/WidgetRendererContent';
-import { useLayoutRenderingContext } from '@/ui/layout/contexts/LayoutRenderingContext';
-import { PageLayoutType } from '~/generated-metadata/graphql';
 
 type WidgetRendererProps = {
   widget: PageLayoutWidget;
 };
 
-export const WidgetRenderer = ({ widget }: WidgetRendererProps) => {
-  const { layoutType } = useLayoutRenderingContext();
-
-  if (layoutType === PageLayoutType.RECORD_PAGE) {
-    return <RecordPageWidgetRenderer widget={widget} />;
-  }
-
-  return <WidgetRendererContent widget={widget} />;
-};
+export const WidgetRenderer = ({ widget }: WidgetRendererProps) => (
+  <WidgetRendererContent widget={widget} />
+);
