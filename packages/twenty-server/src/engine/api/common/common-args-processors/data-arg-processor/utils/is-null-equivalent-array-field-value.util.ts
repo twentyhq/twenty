8ec0a3c246e974e826a17c59c5055303aaab6a9b
BOHEUS <nothin_spe@2abc.hub.pl>
2026-08-20 13:24:15 +0200

    Add isEmptyObject

--- packages/twenty-server/src/engine/api/common/common-args-processors/data-arg-processor/utils/is-null-equivalent-array-field-value.util.ts
+++ packages/twenty-server/src/engine/api/common/common-args-processors/data-arg-processor/utils/is-null-equivalent-array-field-value.util.ts
@@ -1,9 +1,11 @@
+import { isEmptyObject } from "twenty-shared/utils";
+
 export const isNullEquivalentArrayFieldValue = (value: unknown): boolean => {
   return (
     value === null ||
     (Array.isArray(value) &&
       (value.length === 0 ||
         value.every((item) => isNullEquivalentArrayFieldValue(item)))) ||
-    (typeof value === 'object' && Object.keys(value).length === 0)
+    isEmptyObject(value)
   );
 };
