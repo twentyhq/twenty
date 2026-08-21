a5dbc48809b02a16a5f45daa19f487608add8d6a
Félix Malfait <felix.malfait@gmail.com>
2026-08-21 09:57:27 +0000

    fix(server) - reslot the timeline rule upgrade commands above main's latest

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-33/2-33-instance-command-fast-1787305882000-create-timeline-activity-rule-core-table.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-33/2-33-instance-command-fast-1787305882000-create-timeline-activity-rule-core-table.ts
@@ -3,7 +3,7 @@ import { type QueryRunner } from 'typeorm';
 import { RegisteredInstanceCommand } from 'src/engine/core-modules/upgrade/decorators/registered-instance-command.decorator';
 import { type FastInstanceCommand } from 'src/engine/core-modules/upgrade/interfaces/fast-instance-command.interface';
 
-@RegisteredInstanceCommand('2.33.0', 1787125000000)
+@RegisteredInstanceCommand('2.33.0', 1787305882000)
 export class CreateTimelineActivityRuleCoreTableFastInstanceCommand
   implements FastInstanceCommand
 {
