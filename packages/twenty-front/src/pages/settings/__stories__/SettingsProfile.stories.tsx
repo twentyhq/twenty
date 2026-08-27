d078f2b17654e9fabfa10e54a46f554899170dc0
Thomas des Francs <tdesfrancs@gmail.com>
2026-08-27 16:02:18 +0200

    test: initialize profile story workspace member

--- packages/twenty-front/src/pages/settings/__stories__/SettingsProfile.stories.tsx
+++ packages/twenty-front/src/pages/settings/__stories__/SettingsProfile.stories.tsx
@@ -1,11 +1,14 @@
 import { type Meta, type StoryObj } from '@storybook/react-vite';
 import { expect, within } from 'storybook/test';
 
+import { currentWorkspaceMemberState } from '@/auth/states/currentWorkspaceMemberState';
+import { jotaiStore } from '@/ui/utilities/state/jotai/jotaiStore';
 import {
   PageDecorator,
   type PageDecoratorArgs,
 } from '~/testing/decorators/PageDecorator';
 import { graphqlMocks } from '~/testing/graphqlMocks';
+import { mockedWorkspaceMemberData } from '~/testing/mock-data/users';
 
 import { SettingsProfile } from '~/pages/settings/profile/SettingsProfile';
 
@@ -16,6 +19,13 @@ const meta: Meta<PageDecoratorArgs> = {
   args: {
     routePath: '/settings/profile',
     additionalRoutes: ['/welcome'],
+  },
+  beforeEach: () => {
+    jotaiStore.set(currentWorkspaceMemberState.atom, mockedWorkspaceMemberData);
+
+    return () => {
+      jotaiStore.set(currentWorkspaceMemberState.atom, null);
+    };
   },
   parameters: {
     msw: graphqlMocks,
