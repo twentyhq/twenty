12c7a96c3d1d745d808a1b5c8f0b24d9dc82cec0
Abdul Rahman <ar5438376@gmail.com>
2026-08-31 14:12:40 +0000

    refactor(slack): finish the isDefined guard sweep

--- packages/twenty-apps/public/slack/src/logic-functions/utils/is-consented-slack-user-link.ts
+++ packages/twenty-apps/public/slack/src/logic-functions/utils/is-consented-slack-user-link.ts
@@ -1,8 +1,10 @@
+import { isDefined } from 'twenty-sdk/utils';
+
 import { SLACK_USER_LINK_CONSENT_STATE } from 'src/logic-functions/constants/slack-user-link-consent-state';
 
 export const isConsentedSlackUserLink = (
   consentState: string | undefined,
 ): boolean =>
-  consentState === undefined ||
+  !isDefined(consentState) ||
   consentState === SLACK_USER_LINK_CONSENT_STATE.ACTIVE ||
   consentState === SLACK_USER_LINK_CONSENT_STATE.ADMIN_SET;
