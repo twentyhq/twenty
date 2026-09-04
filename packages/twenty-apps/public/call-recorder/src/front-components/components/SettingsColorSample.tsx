4e6d2e208a1cd211e8ded81438024cc54ddecd4e
ehconitin <nitinkoche03@gmail.com>
2026-09-04 16:59:18 +0530

    Restore call recorder color pill radius

--- packages/twenty-apps/public/call-recorder/src/front-components/components/SettingsColorSample.tsx
+++ packages/twenty-apps/public/call-recorder/src/front-components/components/SettingsColorSample.tsx
@@ -1,11 +1,20 @@
 import styled from '@emotion/styled';
 import { ColorSample, type ColorSampleProps } from 'twenty-ui/data-display';
+import { themeCssVariables } from 'twenty-ui/theme-constants';
 
+type SettingsColorSampleProps = Omit<ColorSampleProps, 'variant'>;
+
+// The front-component style bridge does not preserve ColorSample's pill radius.
 const StyledContainer = styled.span`
   display: flex;
+
+  > div {
+    border-radius: ${() => themeCssVariables.border.radius.pill};
+    corner-shape: round;
+  }
 `;
 
-export const SettingsColorSample = (props: ColorSampleProps) => (
+export const SettingsColorSample = (props: SettingsColorSampleProps) => (
   <StyledContainer>
     <ColorSample {...props} />
   </StyledContainer>
