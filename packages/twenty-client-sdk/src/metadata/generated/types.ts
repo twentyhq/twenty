979962816420d5ce398df5d28026abb24354a4ad
neo773 <62795688+neo773@users.noreply.github.com>
2026-09-07 23:19:56 +0530

    Regenerate SDK metadata client for campaign scheduledAt

--- packages/twenty-client-sdk/src/metadata/generated/types.ts
+++ packages/twenty-client-sdk/src/metadata/generated/types.ts
@@ -12714,6 +12714,9 @@ export default {
             "campaignId": [
                 1
             ],
+            "scheduledAt": [
+                4
+            ],
             "__typename": [
                 1
             ]
