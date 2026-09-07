d32e526412d00e95c2db5e7da74e03b2cde9064e
bosiraphael <raphael.bosi@gmail.com>
2026-09-07 17:05:04 +0200

    Restore already hidden globals when hiding one of them fails

--- packages/twenty-ui/test-utilities/conformance/utils/hideGlobals.ts
+++ packages/twenty-ui/test-utilities/conformance/utils/hideGlobals.ts
@@ -1,9 +1,11 @@
 import { isDefined } from '@ui/utilities/utils/isDefined';
 
-const restoreGlobal = (
-  globalName: string,
-  descriptor: PropertyDescriptor | undefined,
-) => {
+type HiddenGlobal = {
+  globalName: string;
+  descriptor: PropertyDescriptor | undefined;
+};
+
+const restoreGlobal = ({ globalName, descriptor }: HiddenGlobal) => {
   if (isDefined(descriptor)) {
     Object.defineProperty(globalThis, globalName, descriptor);
 
@@ -13,23 +15,36 @@ const restoreGlobal = (
   Reflect.deleteProperty(globalThis, globalName);
 };
 
+const hideGlobal = (globalName: string): HiddenGlobal => {
+  const descriptor = Object.getOwnPropertyDescriptor(globalThis, globalName);
+
+  Object.defineProperty(globalThis, globalName, {
+    configurable: true,
+    writable: true,
+    value: undefined,
+  });
+
+  return { globalName, descriptor };
+};
+
 export const hideGlobals = (globalNames: readonly string[]) => {
-  const descriptors = globalNames.map((globalName) => ({
-    globalName,
-    descriptor: Object.getOwnPropertyDescriptor(globalThis, globalName),
-  }));
+  const hiddenGlobals: HiddenGlobal[] = [];
 
-  for (const { globalName } of descriptors) {
-    Object.defineProperty(globalThis, globalName, {
-      configurable: true,
-      writable: true,
-      value: undefined,
-    });
-  }
-
-  return () => {
-    for (const { globalName, descriptor } of descriptors) {
-      restoreGlobal(globalName, descriptor);
+  const restoreGlobals = () => {
+    for (const hiddenGlobal of hiddenGlobals) {
+      restoreGlobal(hiddenGlobal);
     }
   };
+
+  try {
+    for (const globalName of globalNames) {
+      hiddenGlobals.push(hideGlobal(globalName));
+    }
+  } catch (error) {
+    restoreGlobals();
+
+    throw error;
+  }
+
+  return restoreGlobals;
 };
