19d0d4cb961c131d10c183f90b4f2ecaaf8a68d0
prastoin <paul@twenty.com>
2026-09-07 11:35:46 +0200

    findTsVectorFlatFieldMetadataForObject

--- packages/twenty-server/src/engine/metadata-modules/flat-search-field-metadata/utils/find-ts-vector-flat-field-metadata-for-object.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-search-field-metadata/utils/find-ts-vector-flat-field-metadata-for-object.util.ts
@@ -1,23 +1,20 @@
 import { FieldMetadataType } from 'twenty-shared/types';
 import { isDefined } from 'twenty-shared/utils';
 
-import { type FlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata.type';
+import { type MetadataUniversalFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-universal-flat-entity.type';
 import { SEARCH_VECTOR_FIELD } from 'src/engine/metadata-modules/search-field-metadata/constants/search-vector-field.constants';
+import { type MetadataUniversalFlatEntityMaps } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/metadata-universal-flat-entity-maps.type';
 
 // Resolves the object's single system TS_VECTOR field (the 'searchVector' field) that
 // every searchFieldMetadata row contributes to.
 // Could be refactored once https://github.com/twentyhq/twenty/pull/21949 has been merged.
-export const findTsVectorFlatFieldMetadataForObject = <
-  TFlatFieldMetadata extends Pick<FlatFieldMetadata, 'type' | 'name'>,
->({
+export const findTsVectorFlatFieldMetadataForObject = ({
   fieldUniversalIdentifiers,
   flatFieldMetadataMaps,
 }: {
   fieldUniversalIdentifiers: string[];
-  flatFieldMetadataMaps: {
-    byUniversalIdentifier: Partial<Record<string, TFlatFieldMetadata>>;
-  };
-}): TFlatFieldMetadata | undefined =>
+  flatFieldMetadataMaps: MetadataUniversalFlatEntityMaps<'fieldMetadata'>;
+}): MetadataUniversalFlatEntity<'fieldMetadata'> | undefined =>
   fieldUniversalIdentifiers
     .map(
       (fieldUniversalIdentifier) =>
