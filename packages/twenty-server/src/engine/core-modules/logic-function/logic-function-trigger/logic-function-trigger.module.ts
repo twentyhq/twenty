62becbe1a71e5d7d3e5972ff1d2e8f3021065461
martmull <martmull@hotmail.fr>
2026-09-08 20:35:01 +0200

    Revert "Record sharing 5/14: evaluate record shares in memory for events" (#25649)
    Reverts twentyhq/twenty#25429
    merged too quickly my bad

--- packages/twenty-server/src/engine/core-modules/logic-function/logic-function-trigger/logic-function-trigger.module.ts
+++ packages/twenty-server/src/engine/core-modules/logic-function/logic-function-trigger/logic-function-trigger.module.ts
@@ -12,7 +12,6 @@ import { LogicFunctionTriggerService } from 'src/engine/core-modules/logic-funct
 import { RouteTriggerService } from 'src/engine/core-modules/logic-function/logic-function-trigger/triggers/route/route-trigger.service';
 import { WorkspaceEntity } from 'src/engine/core-modules/workspace/workspace.entity';
 import { LogicFunctionEntity } from 'src/engine/metadata-modules/logic-function/logic-function.entity';
-import { RecordShareModule } from 'src/engine/record-share/record-share.module';
 import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
 
 @Module({
@@ -22,7 +21,6 @@ import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache
     WorkspaceDomainsModule,
     WorkspaceCacheModule,
     CronModule,
-    RecordShareModule,
   ],
   providers: [
     LogicFunctionTriggerJob,
