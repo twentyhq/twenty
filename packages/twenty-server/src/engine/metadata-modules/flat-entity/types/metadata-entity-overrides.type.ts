ade9c6204673438480af649e0412dda8c3f98d4c
prastoin <paul@twenty.com>
2026-09-10 17:10:31 +0200

    Remove override types superseded by the overridable properties constant

--- packages/twenty-server/src/engine/metadata-modules/flat-entity/types/metadata-entity-overrides.type.ts
+++
@@ -1,8 +0,0 @@
-import { type AllMetadataName } from 'twenty-shared/metadata';
-
-import { type MetadataEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-entity.type';
-
-export type MetadataEntityOverrides<T extends AllMetadataName> =
-  MetadataEntity<T> extends { overrides: infer TOverrides }
-    ? NonNullable<TOverrides>
-    : never;
