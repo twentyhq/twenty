ee480d7453e82bb7c6f55a652c278f70d33272bd
Thomas des Francs <tdesfrancs@gmail.com>
2026-09-10 18:32:14 +0200

    fix: resolve viewport stylesheet from the source root

--- packages/twenty-front/src/testing/decorators/RootDecorator.tsx
+++ packages/twenty-front/src/testing/decorators/RootDecorator.tsx
@@ -1,4 +1,4 @@
-import '@/viewport-zoom.css';
+import '~/viewport-zoom.css';
 import { ApolloProvider } from '@apollo/client/react';
 import { type Decorator } from '@storybook/react-vite';
 import { Provider as JotaiProvider } from 'jotai';
