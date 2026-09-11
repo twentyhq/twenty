87a0e008d6f44a15cfa7be45fc1ee9243a968864
bosiraphael <raphael.bosi@gmail.com>
2026-09-11 17:25:12 +0200

    Use complete fixtures for select color validation tests

--- packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/validators/utils/__tests__/validate-enum-flat-field-metadata.util.spec.ts
+++ packages/twenty-server/src/engine/metadata-modules/flat-field-metadata/validators/utils/__tests__/validate-enum-flat-field-metadata.util.spec.ts
@@ -5,6 +5,9 @@ import {
 } from 'twenty-shared/types';
 
 import { FieldMetadataExceptionCode } from 'src/engine/metadata-modules/field-metadata/field-metadata.exception';
+import { createEmptyAllFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/constant/create-empty-all-flat-entity-maps.constant';
+import { getFlatFieldMetadataMock } from 'src/engine/metadata-modules/flat-field-metadata/__mocks__/get-flat-field-metadata.mock';
+import { type FlatFieldMetadataTypeValidationArgs } from 'src/engine/metadata-modules/flat-field-metadata/types/flat-field-metadata-type-validator.type';
 import { validateEnumSelectFlatFieldMetadata } from 'src/engine/metadata-modules/flat-field-metadata/validators/utils/validate-enum-flat-field-metadata.util';
 
 const OPTION_WITHOUT_COLOR = {
@@ -21,13 +24,24 @@ const validateOptionColor = ({
 }: {
   type: EnumFieldMetadataType;
   colorProperties: { color?: unknown };
-  update?: Parameters<typeof validateEnumSelectFlatFieldMetadata>[0]['update'];
-}) =>
-  validateEnumSelectFlatFieldMetadata({
+  update?: FlatFieldMetadataTypeValidationArgs<EnumFieldMetadataType>['update'];
+}) => {
+  const flatFieldMetadata = getFlatFieldMetadataMock({
+    type,
+    universalIdentifier: 'ab0e6d76-67d8-466f-918a-4b8a8d044131',
+    objectMetadataId: '6450cd8f-c202-498f-8be4-65e1b1c93e32',
+  });
+
+  return validateEnumSelectFlatFieldMetadata({
     update,
     flatEntityToValidate: {
+      ...flatFieldMetadata,
       type,
       defaultValue: null,
+      morphId: null,
+      relationTargetFieldMetadataUniversalIdentifier: null,
+      relationTargetObjectMetadataUniversalIdentifier: null,
+      universalSettings: null,
       options: [
         {
           ...OPTION_WITHOUT_COLOR,
@@ -35,7 +49,36 @@ const validateOptionColor = ({
         },
       ],
     },
-  } as Parameters<typeof validateEnumSelectFlatFieldMetadata>[0]);
+    additionalCacheDataMaps: {
+      featureFlagsMap: {
+        IS_APP_CLAIMING_ENABLED: false,
+        IS_UNIQUE_INDEXES_ENABLED: false,
+        IS_CONFIGURABLE_SEARCH_FIELDS_ENABLED: false,
+        IS_JSON_FILTER_ENABLED: false,
+        IS_EMAIL_GROUP_ENABLED: false,
+        IS_JUNCTION_RELATIONS_ENABLED: false,
+        IS_REST_METADATA_API_NEW_FORMAT_DIRECT: false,
+        IS_LOGIC_FUNCTION_PREBUILT_MODE_ENABLED: false,
+        IS_WORKFLOW_CORE_INDEX_PAGE_ENABLED: false,
+        IS_API_RATE_LIMIT_V2_ENABLED: false,
+        IS_MESSAGE_CALENDAR_TARGET_READ_ENABLED: false,
+        IS_QUOTA_ENGINE_CREDIT_BOUND_ENABLED: false,
+        IS_RECORD_CREATION_FORM_ENABLED: false,
+        IS_RECORD_SHARING_ENABLED: false,
+        IS_WEBHOOK_RATE_LIMIT_ENABLED: false,
+      },
+    },
+    optimisticFlatEntityMapsAndRelatedFlatEntityMaps:
+      createEmptyAllFlatEntityMaps(),
+    remainingFlatEntityMapsToValidate: { byUniversalIdentifier: {} },
+    workspaceId: flatFieldMetadata.workspaceId,
+    buildOptions: {
+      isSystemBuild: false,
+      applicationUniversalIdentifier:
+        flatFieldMetadata.applicationUniversalIdentifier,
+    },
+  });
+};
 
 describe('validateEnumSelectFlatFieldMetadata', () => {
   describe.each([
