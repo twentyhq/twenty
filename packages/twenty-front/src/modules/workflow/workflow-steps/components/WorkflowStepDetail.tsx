c292114f4151849ff7484bae16e234a33f5ec23b
Abhinav A P <asok0968@gmail.com>
2026-09-14 16:48:18 +0530

    feat(workflow): wire RUN_WORKFLOW into editable step-detail panel
    - Add RUN_WORKFLOW case to WorkflowStepDetail.tsx's action switch
    - Renders WorkflowEditActionRunWorkflow in editable mode (actionOptions=props)

--- packages/twenty-front/src/modules/workflow/workflow-steps/components/WorkflowStepDetail.tsx
+++ packages/twenty-front/src/modules/workflow/workflow-steps/components/WorkflowStepDetail.tsx
@@ -21,6 +21,7 @@ import { WorkflowEditActionIfElse } from '@/workflow/workflow-steps/workflow-act
 import { WorkflowEditActionIterator } from '@/workflow/workflow-steps/workflow-actions/iterator-action/components/WorkflowEditActionIterator';
 import { WorkflowEditActionLogicFunction } from '@/workflow/workflow-steps/workflow-actions/logic-function-action/components/WorkflowEditActionLogicFunction';
 import { WorkflowEditActionPickRecord } from '@/workflow/workflow-steps/workflow-actions/pick-record-action/components/WorkflowEditActionPickRecord';
+import { WorkflowEditActionRunWorkflow } from '@/workflow/workflow-steps/workflow-actions/run-workflow-action/components/WorkflowEditActionRunWorkflow';
 import { WorkflowEditTriggerCronForm } from '@/workflow/workflow-trigger/components/WorkflowEditTriggerCronForm';
 import { WorkflowEditTriggerDatabaseEventForm } from '@/workflow/workflow-trigger/components/WorkflowEditTriggerDatabaseEventForm';
 import { WorkflowEditTriggerManual } from '@/workflow/workflow-trigger/components/WorkflowEditTriggerManual';
@@ -268,6 +269,15 @@ export const WorkflowStepDetail = ({
         case 'DELAY': {
           return (
             <WorkflowEditActionDelay
+              key={stepId}
+              action={stepDefinition.definition}
+              actionOptions={props}
+            />
+          );
+        }
+        case 'RUN_WORKFLOW': {
+          return (
+            <WorkflowEditActionRunWorkflow
               key={stepId}
               action={stepDefinition.definition}
               actionOptions={props}
