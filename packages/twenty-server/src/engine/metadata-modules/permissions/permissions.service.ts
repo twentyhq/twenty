2e2ac7841d53c4d0bdb15f8d76a576b43deef20d
Thomas Trompette <thomas.trompette@sfr.fr>
2026-09-14 09:36:53 +0200

    Add the monitoring flag to the default permission map

--- packages/twenty-server/src/engine/metadata-modules/permissions/permissions.service.ts
+++ packages/twenty-server/src/engine/metadata-modules/permissions/permissions.service.ts
@@ -126,6 +126,7 @@ export class PermissionsService {
         [PermissionFlagType.VIEWS]: false,
         [PermissionFlagType.BILLING]: false,
         [PermissionFlagType.AI_SETTINGS]: false,
+        [PermissionFlagType.MONITORING]: false,
         [PermissionFlagType.AI]: false,
         [PermissionFlagType.UPLOAD_FILE]: false,
         [PermissionFlagType.DOWNLOAD_FILE]: false,
