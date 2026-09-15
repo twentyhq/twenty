a6ea15bd2c15ccdc92b6b0d22c234cf2a20adb58
bosiraphael <raphael.bosi@gmail.com>
2026-09-15 15:55:57 +0200

    Use Twenty UI components in presentation stories

--- packages/twenty-ui/src/input/Checkbox/__stories__/Checkbox.stories.tsx
+++ packages/twenty-ui/src/input/Checkbox/__stories__/Checkbox.stories.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { expect, fn, userEvent, within } from 'storybook/test';
 
 import { Checkbox } from '@ui/input/Checkbox/Checkbox';
+import { Button } from '@ui/input/Button/Button';
 import { type CheckboxColor } from '@ui/input/Checkbox/types/CheckboxColor';
 import { type CheckboxProps } from '@ui/input/Checkbox/types/CheckboxProps';
 import { type CheckboxSize } from '@ui/input/Checkbox/types/CheckboxSize';
@@ -27,9 +28,12 @@ const ControlledExample = (props: CheckboxProps) => {
   return (
     <>
       <Checkbox {...props} checked={checked} />
-      <button type="button" onClick={() => setChecked(true)}>
-        Apply selection
-      </button>
+      <Button
+        type="button"
+        title="Apply selection"
+        ariaLabel="Apply selection"
+        onClick={() => setChecked(true)}
+      />
     </>
   );
 };
