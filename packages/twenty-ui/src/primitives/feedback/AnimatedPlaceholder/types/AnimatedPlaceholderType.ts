bfc74f11aed710d07c864d9591a36ff035101c73
Thomas des Francs <tdesfrancs@gmail.com>
2026-09-15 16:46:00 +0200

    Update placeholder type imports for UI primitive families

--- packages/twenty-ui/src/primitives/feedback/AnimatedPlaceholder/types/AnimatedPlaceholderType.ts
+++ packages/twenty-ui/src/primitives/feedback/AnimatedPlaceholder/types/AnimatedPlaceholderType.ts
@@ -1,5 +1,5 @@
-import { type BACKGROUND } from '@ui/feedback/AnimatedPlaceholder/constants/Background';
-import { type MOVING_IMAGE } from '@ui/feedback/AnimatedPlaceholder/constants/MovingImage';
+import { type BACKGROUND } from '@ui/primitives/feedback/AnimatedPlaceholder/constants/Background';
+import { type MOVING_IMAGE } from '@ui/primitives/feedback/AnimatedPlaceholder/constants/MovingImage';
 
 export type AnimatedPlaceholderType =
   | keyof typeof BACKGROUND
