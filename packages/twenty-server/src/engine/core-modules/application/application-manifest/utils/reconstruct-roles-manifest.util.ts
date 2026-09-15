6f40812a4b4c73eb7439dcdd9aede654155075d5
Weiko <corentin@twenty.com>
2026-09-15 18:01:57 +0200

    Export row-level permission groups only within their own role and object

--- packages/twenty-server/src/engine/core-modules/application/application-manifest/utils/reconstruct-roles-manifest.util.ts
+++ packages/twenty-server/src/engine/core-modules/application/application-manifest/utils/reconstruct-roles-manifest.util.ts
@@ -3,17 +3,12 @@ import {
   type ObjectPermissionManifest,
   type PermissionFlagManifest,
   type RoleManifest,
-  type RowLevelPermissionPredicateGroupManifest,
-  type RowLevelPermissionPredicateManifest,
 } from 'twenty-shared/application';
-import { isDefined } from 'twenty-shared/utils';
 
 import { fromFlatFieldPermissionToFieldPermissionManifest } from 'src/engine/core-modules/application/application-manifest/converters/from-flat-field-permission-to-field-permission-manifest.util';
 import { fromFlatObjectPermissionToObjectPermissionManifest } from 'src/engine/core-modules/application/application-manifest/converters/from-flat-object-permission-to-object-permission-manifest.util';
 import { fromFlatPermissionFlagToPermissionFlagManifest } from 'src/engine/core-modules/application/application-manifest/converters/from-flat-permission-flag-to-permission-flag-manifest.util';
 import { fromFlatRoleToRoleManifest } from 'src/engine/core-modules/application/application-manifest/converters/from-flat-role-to-role-manifest.util';
-import { fromFlatRowLevelPermissionPredicateGroupToRowLevelPermissionPredicateGroupManifest } from 'src/engine/core-modules/application/application-manifest/converters/from-flat-row-level-permission-predicate-group-to-row-level-permission-predicate-group-manifest.util';
-import { fromFlatRowLevelPermissionPredicateToRowLevelPermissionPredicateManifest } from 'src/engine/core-modules/application/application-manifest/converters/from-flat-row-level-permission-predicate-to-row-level-permission-predicate-manifest.util';
 import { type ApplicationExportCoverageEntry } from 'src/engine/core-modules/application/application-manifest/types/application-export.type';
 import {
   type ChildMetadataName,
@@ -22,28 +17,22 @@ import {
 import { buildExportedCoverageEntry } from 'src/engine/core-modules/application/application-manifest/utils/build-exported-coverage-entry.util';
 import { compareByCodePoint } from 'src/engine/core-modules/application/application-manifest/utils/compare-by-code-point.util';
 import { createChildDecider } from 'src/engine/core-modules/application/application-manifest/utils/create-child-decider.util';
-import { MANIFEST_ENTITY_REGISTRY } from 'src/engine/core-modules/application/application-manifest/utils/find-manifest-entity-descriptor-by-universal-identifier.util';
 import { getUnresolvableReferenceReason } from 'src/engine/core-modules/application/application-manifest/utils/get-unresolvable-reference-reason.util';
+import { reconstructRowLevelPermissionPredicatesManifest } from 'src/engine/core-modules/application/application-manifest/utils/reconstruct-row-level-permission-predicates-manifest.util';
 import { sortFlatEntitiesByUniversalIdentifier } from 'src/engine/core-modules/application/application-manifest/utils/sort-flat-entities-by-universal-identifier.util';
 import { type AllFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/all-flat-entity-maps.type';
 import { type UniversalFlatFieldPermission } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-field-permission.type';
 import { type UniversalFlatObjectPermission } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-object-permission.type';
 import { type UniversalFlatRolePermissionFlag } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-role-permission-flag.type';
-import { type UniversalFlatRowLevelPermissionPredicateGroup } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-row-level-permission-predicate-group.type';
-import { type UniversalFlatRowLevelPermissionPredicate } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-row-level-permission-predicate.type';
 
 type FlatRoleChild =
   | UniversalFlatObjectPermission
   | UniversalFlatFieldPermission
-  | UniversalFlatRolePermissionFlag
-  | UniversalFlatRowLevelPermissionPredicateGroup
-  | UniversalFlatRowLevelPermissionPredicate;
+  | UniversalFlatRolePermissionFlag;
 
 type RoleChildren = {
   objectPermissions: ObjectPermissionManifest[];
   fieldPermissions: FieldPermissionManifest[];
-  rowLevelPermissionPredicateGroups: RowLevelPermissionPredicateGroupManifest[];
-  rowLevelPermissionPredicates: RowLevelPermissionPredicateManifest[];
   permissionFlagUniversalIdentifiers: string[];
 };
 
@@ -97,8 +86,6 @@ export const reconstructRolesManifest = ({
       {
         objectPermissions: [],
         fieldPermissions: [],
-        rowLevelPermissionPredicateGroups: [],
-        rowLevelPermissionPredicates: [],
         permissionFlagUniversalIdentifiers: [],
       },
     ]),
@@ -228,156 +215,19 @@ export const reconstructRolesManifest = ({
     }
   }
 
-  const getUnsupportedPredicateGroupReason = ({
-    flatRowLevelPermissionPredicateGroup,
-    visitedPredicateGroupUniversalIdentifiers,
-  }: {
-    flatRowLevelPermissionPredicateGroup: UniversalFlatRowLevelPermissionPredicateGroup;
-    visitedPredicateGroupUniversalIdentifiers: ReadonlySet<string>;
-  }): string | undefined =>
-    getUnresolvableObjectReason({
-      metadataName: 'rowLevelPermissionPredicateGroup',
-      objectMetadataUniversalIdentifier:
-        flatRowLevelPermissionPredicateGroup.objectMetadataUniversalIdentifier,
-    }) ??
-    getUnexportedPredicateGroupReason({
-      metadataName: 'rowLevelPermissionPredicateGroup',
-      roleUniversalIdentifier:
-        flatRowLevelPermissionPredicateGroup.roleUniversalIdentifier,
-      predicateGroupUniversalIdentifier:
-        flatRowLevelPermissionPredicateGroup.parentRowLevelPermissionPredicateGroupUniversalIdentifier,
-      visitedPredicateGroupUniversalIdentifiers: new Set([
-        ...visitedPredicateGroupUniversalIdentifiers,
-        flatRowLevelPermissionPredicateGroup.universalIdentifier,
-      ]),
-    });
-  const isPredicateGroupExported = ({
-    roleUniversalIdentifier,
-    predicateGroupUniversalIdentifier,
-    visitedPredicateGroupUniversalIdentifiers,
-  }: {
-    roleUniversalIdentifier: string;
-    predicateGroupUniversalIdentifier: string;
-    visitedPredicateGroupUniversalIdentifiers: ReadonlySet<string>;
-  }): boolean => {
-    const flatRowLevelPermissionPredicateGroup =
-      applicationAllFlatEntityMaps.flatRowLevelPermissionPredicateGroupMaps
-        .byUniversalIdentifier[predicateGroupUniversalIdentifier];
+  const {
+    predicateGroupsByRoleUniversalIdentifier,
+    predicatesByRoleUniversalIdentifier,
+    coverage: rowLevelPermissionCoverage,
+  } = reconstructRowLevelPermissionPredicatesManifest({
+    applicationAllFlatEntityMaps,
+    allFlatEntityMaps,
+    exportedObjectUniversalIdentifiers,
+    resolvableFieldUniversalIdentifiers,
+    parentRoleStatusByUniversalIdentifier,
+  });
 
-    return (
-      isDefined(flatRowLevelPermissionPredicateGroup) &&
-      !isDefined(flatRowLevelPermissionPredicateGroup.deletedAt) &&
-      !visitedPredicateGroupUniversalIdentifiers.has(
-        predicateGroupUniversalIdentifier,
-      ) &&
-      flatRowLevelPermissionPredicateGroup.roleUniversalIdentifier ===
-        roleUniversalIdentifier &&
-      parentRoleStatusByUniversalIdentifier.get(roleUniversalIdentifier) ===
-        'exported' &&
-      !isDefined(
-        getUnsupportedPredicateGroupReason({
-          flatRowLevelPermissionPredicateGroup,
-          visitedPredicateGroupUniversalIdentifiers,
-        }),
-      )
-    );
-  };
-  const getUnexportedPredicateGroupReason = ({
-    metadataName,
-    roleUniversalIdentifier,
-    predicateGroupUniversalIdentifier,
-    visitedPredicateGroupUniversalIdentifiers,
-  }: {
-    metadataName: Extract<
-      ChildMetadataName,
-      'rowLevelPermissionPredicate' | 'rowLevelPermissionPredicateGroup'
-    >;
-    roleUniversalIdentifier: string;
-    predicateGroupUniversalIdentifier: string | null;
-    visitedPredicateGroupUniversalIdentifiers: ReadonlySet<string>;
-  }): string | undefined =>
-    !isDefined(predicateGroupUniversalIdentifier) ||
-    isPredicateGroupExported({
-      roleUniversalIdentifier,
-      predicateGroupUniversalIdentifier,
-      visitedPredicateGroupUniversalIdentifiers,
-    })
-      ? undefined
-      : `${MANIFEST_ENTITY_REGISTRY[metadataName].entityKind} in a ${MANIFEST_ENTITY_REGISTRY.rowLevelPermissionPredicateGroup.entityKind} that is not exported`;
-
-  for (const flatRowLevelPermissionPredicateGroup of sortFlatEntitiesByUniversalIdentifier(
-    applicationAllFlatEntityMaps.flatRowLevelPermissionPredicateGroupMaps,
-  )) {
-    if (isDefined(flatRowLevelPermissionPredicateGroup.deletedAt)) {
-      continue;
-    }
-
-    if (
-      decideRoleChild({
-        metadataName: 'rowLevelPermissionPredicateGroup',
-        flatEntity: flatRowLevelPermissionPredicateGroup,
-        unsupportedReason: getUnsupportedPredicateGroupReason({
-          flatRowLevelPermissionPredicateGroup,
-          visitedPredicateGroupUniversalIdentifiers: new Set(),
-        }),
-      }) === 'nested'
-    ) {
-      childrenOf(
-        flatRowLevelPermissionPredicateGroup.roleUniversalIdentifier,
-      )?.rowLevelPermissionPredicateGroups.push(
-        fromFlatRowLevelPermissionPredicateGroupToRowLevelPermissionPredicateGroupManifest(
-          { flatRowLevelPermissionPredicateGroup },
-        ),
-      );
-    }
-  }
-
-  for (const flatRowLevelPermissionPredicate of sortFlatEntitiesByUniversalIdentifier(
-    applicationAllFlatEntityMaps.flatRowLevelPermissionPredicateMaps,
-  )) {
-    if (isDefined(flatRowLevelPermissionPredicate.deletedAt)) {
-      continue;
-    }
-
-    if (
-      decideRoleChild({
-        metadataName: 'rowLevelPermissionPredicate',
-        flatEntity: flatRowLevelPermissionPredicate,
-        unsupportedReason:
-          getUnresolvableObjectReason({
-            metadataName: 'rowLevelPermissionPredicate',
-            objectMetadataUniversalIdentifier:
-              flatRowLevelPermissionPredicate.objectMetadataUniversalIdentifier,
-          }) ??
-          getUnresolvableFieldReferenceReason({
-            metadataName: 'rowLevelPermissionPredicate',
-            fieldMetadataUniversalIdentifier:
-              flatRowLevelPermissionPredicate.fieldMetadataUniversalIdentifier,
-          }) ??
-          getUnresolvableFieldReferenceReason({
-            metadataName: 'rowLevelPermissionPredicate',
-            fieldMetadataUniversalIdentifier:
-              flatRowLevelPermissionPredicate.workspaceMemberFieldMetadataUniversalIdentifier,
-          }) ??
-          getUnexportedPredicateGroupReason({
-            metadataName: 'rowLevelPermissionPredicate',
-            roleUniversalIdentifier:
-              flatRowLevelPermissionPredicate.roleUniversalIdentifier,
-            predicateGroupUniversalIdentifier:
-              flatRowLevelPermissionPredicate.rowLevelPermissionPredicateGroupUniversalIdentifier,
-            visitedPredicateGroupUniversalIdentifiers: new Set(),
-          }),
-      }) === 'nested'
-    ) {
-      childrenOf(
-        flatRowLevelPermissionPredicate.roleUniversalIdentifier,
-      )?.rowLevelPermissionPredicates.push(
-        fromFlatRowLevelPermissionPredicateToRowLevelPermissionPredicateManifest(
-          { flatRowLevelPermissionPredicate },
-        ),
-      );
-    }
-  }
+  coverage.push(...rowLevelPermissionCoverage);
 
   const roles = flatRoles.map((flatRole) => {
     coverage.push(
@@ -395,8 +245,13 @@ export const reconstructRolesManifest = ({
         objectPermissions: children?.objectPermissions,
         fieldPermissions: children?.fieldPermissions,
         rowLevelPermissionPredicateGroups:
-          children?.rowLevelPermissionPredicateGroups,
-        rowLevelPermissionPredicates: children?.rowLevelPermissionPredicates,
+          predicateGroupsByRoleUniversalIdentifier.get(
+            flatRole.universalIdentifier,
+          ) ?? [],
+        rowLevelPermissionPredicates:
+          predicatesByRoleUniversalIdentifier.get(
+            flatRole.universalIdentifier,
+          ) ?? [],
         permissionFlagUniversalIdentifiers:
           children?.permissionFlagUniversalIdentifiers.sort(compareByCodePoint),
       },
