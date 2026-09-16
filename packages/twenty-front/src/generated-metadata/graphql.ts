e130fd3c6289184f89e7e0c446c61d0e333e9e15
neo773 <62795688+neo773@users.noreply.github.com>
2026-09-16 15:37:00 +0530

    Match codegen field order for the open tracking switch

--- packages/twenty-front/src/generated-metadata/graphql.ts
+++ packages/twenty-front/src/generated-metadata/graphql.ts
@@ -6714,6 +6714,7 @@ export type UpdateWorkspaceInput = {
   inviteHash?: InputMaybe<Scalars['String']['input']>;
   isAutoModelSelectionEnabled?: InputMaybe<Scalars['Boolean']['input']>;
   isCampaignClickTrackingEnabled?: InputMaybe<Scalars['Boolean']['input']>;
+  isCampaignOpenTrackingEnabled?: InputMaybe<Scalars['Boolean']['input']>;
   isGoogleAuthBypassEnabled?: InputMaybe<Scalars['Boolean']['input']>;
   isGoogleAuthEnabled?: InputMaybe<Scalars['Boolean']['input']>;
   isInternalMessagesImportEnabled?: InputMaybe<Scalars['Boolean']['input']>;
@@ -6722,7 +6723,6 @@ export type UpdateWorkspaceInput = {
   isPasswordAuthBypassEnabled?: InputMaybe<Scalars['Boolean']['input']>;
   isPasswordAuthEnabled?: InputMaybe<Scalars['Boolean']['input']>;
   isPublicInviteLinkEnabled?: InputMaybe<Scalars['Boolean']['input']>;
-  isCampaignOpenTrackingEnabled?: InputMaybe<Scalars['Boolean']['input']>;
   isTwoFactorAuthenticationEnforced?: InputMaybe<Scalars['Boolean']['input']>;
   logo?: InputMaybe<Scalars['String']['input']>;
   subdomain?: InputMaybe<Scalars['String']['input']>;
@@ -7398,6 +7398,7 @@ export type Workspace = {
   inviteHash?: Maybe<Scalars['String']['output']>;
   isAutoModelSelectionEnabled: Scalars['Boolean']['output'];
   isCampaignClickTrackingEnabled: Scalars['Boolean']['output'];
+  isCampaignOpenTrackingEnabled: Scalars['Boolean']['output'];
   isCustomDomainEnabled: Scalars['Boolean']['output'];
   isGoogleAuthBypassEnabled: Scalars['Boolean']['output'];
   isGoogleAuthEnabled: Scalars['Boolean']['output'];
@@ -7407,7 +7408,6 @@ export type Workspace = {
   isPasswordAuthBypassEnabled: Scalars['Boolean']['output'];
   isPasswordAuthEnabled: Scalars['Boolean']['output'];
   isPublicInviteLinkEnabled: Scalars['Boolean']['output'];
-  isCampaignOpenTrackingEnabled: Scalars['Boolean']['output'];
   isTwoFactorAuthenticationEnforced: Scalars['Boolean']['output'];
   logo?: Maybe<Scalars['String']['output']>;
   logoFileId?: Maybe<Scalars['UUID']['output']>;
