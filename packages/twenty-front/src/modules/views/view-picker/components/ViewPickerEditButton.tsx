30475810c41ef34b8215e8851d06e17339edcc53
Thomas Trompette <thomas.trompette@sfr.fr>
2026-09-16 15:28:49 +0200

    fix(view): apply the visible view count to the picker edit mode too

--- packages/twenty-front/src/modules/views/view-picker/components/ViewPickerEditButton.tsx
+++ packages/twenty-front/src/modules/views/view-picker/components/ViewPickerEditButton.tsx
@@ -3,6 +3,8 @@ import { useAtomComponentStateValue } from '@/ui/utilities/state/jotai/hooks/use
 import { useAtomFamilySelectorValue } from '@/ui/utilities/state/jotai/hooks/useAtomFamilySelectorValue';
 import { viewsFromObjectMetadataItemFamilySelector } from '@/views/states/selectors/viewsFromObjectMetadataItemFamilySelector';
 import { ViewType } from '@/views/types/ViewType';
+import { useGetCurrentViewOnly } from '@/views/hooks/useGetCurrentViewOnly';
+import { computeViewPickerVisibleViews } from '@/views/view-picker/utils/computeViewPickerVisibleViews';
 import { useCreateViewFromCurrentState } from '@/views/view-picker/hooks/useCreateViewFromCurrentState';
 import { useDestroyViewFromCurrentState } from '@/views/view-picker/hooks/useDestroyViewFromCurrentState';
 import { useGetAvailableFieldsToGroupRecordsBy } from '@/views/view-picker/hooks/useGetAvailableFieldsToGroupRecordsBy';
@@ -10,8 +12,10 @@ import { useViewPickerMode } from '@/views/view-picker/hooks/useViewPickerMode';
 import { viewPickerIsPersistingComponentState } from '@/views/view-picker/states/viewPickerIsPersistingComponentState';
 import { viewPickerMainGroupByFieldMetadataIdComponentState } from '@/views/view-picker/states/viewPickerMainGroupByFieldMetadataIdComponentState';
 import { viewPickerTypeComponentState } from '@/views/view-picker/states/viewPickerTypeComponentState';
+import { useIsFeatureEnabled } from '@/workspace/hooks/useIsFeatureEnabled';
 import { t } from '@lingui/core/macro';
 import { Button } from 'twenty-ui/primitives/input';
+import { FeatureFlagKey } from '~/generated-metadata/graphql';
 
 export const ViewPickerEditButton = () => {
   const { availableFieldsForGrouping, navigateToSelectSettings } =
@@ -24,7 +28,19 @@ export const ViewPickerEditButton = () => {
     { objectMetadataItemId: objectMetadataItem.id },
   );
 
-  const isLastView = viewsOnCurrentObject.length <= 1;
+  const { currentView } = useGetCurrentViewOnly();
+
+  const isInitialObjectViewEnabled = useIsFeatureEnabled(
+    FeatureFlagKey.IS_INITIAL_OBJECT_VIEW_ENABLED,
+  );
+
+  const visibleViews = computeViewPickerVisibleViews({
+    views: viewsOnCurrentObject,
+    currentViewId: currentView?.id,
+    isInitialObjectViewEnabled,
+  });
+
+  const isLastView = visibleViews.length <= 1;
 
   const { viewPickerMode } = useViewPickerMode();
   const viewPickerType = useAtomComponentStateValue(
