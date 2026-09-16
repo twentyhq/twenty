4c915ea72eba1c2c511d76ec8c986c0e17331e90
Thomas des Francs <tdesfrancs@gmail.com>
2026-09-16 13:44:40 +0200

    Clarify nested folder restriction wording

--- packages/twenty-front/src/modules/navigation-menu-item/edit/components/NavigationMenuItemAddDropdownContent.tsx
+++ packages/twenty-front/src/modules/navigation-menu-item/edit/components/NavigationMenuItemAddDropdownContent.tsx
@@ -211,7 +211,7 @@ export const NavigationMenuItemAddDropdownContent = ({
         {
           id: 'folder',
           label: t`Folder`,
-          contextualText: folderId ? t`No nested folders` : undefined,
+          contextualText: folderId ? t`Cannot nest folders` : undefined,
           Icon: folderId ? IconFolder : undefined,
           icon: folderId ? undefined : (
             <ColoredIcon
