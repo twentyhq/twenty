db34d5d16fa9b3d51b234db03f3eeea0e01f00bf
Charles Bochet <charles@twenty.com>
2026-09-19 18:42:53 +0200

    Drop the comment restating the input min-width

--- packages/twenty-front/src/modules/ui/field/input/components/FieldInputContainer.tsx
+++ packages/twenty-front/src/modules/ui/field/input/components/FieldInputContainer.tsx
@@ -2,7 +2,6 @@ import { styled } from '@linaria/react';
 
 import { FIELD_INPUT_ANCHOR_WIDTH_CSS_VARIABLE } from '@/ui/field/input/constants/FieldInputAnchorWidthCssVariable';
 
-// Inputs that fill their container should also fill the field they float over, when that width is known.
 // oxlint-disable-next-line twenty/styled-components-prefixed-with-styled
 export const FieldInputContainer = styled.div`
   align-items: center;
