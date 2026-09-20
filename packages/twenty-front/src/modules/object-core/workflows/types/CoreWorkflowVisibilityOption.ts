e6f00595c2b08031f07176a730fc90156a993615
Félix Malfait <felix.malfait@gmail.com>
2026-09-20 15:34:35 +0000

    fix(front): import WorkflowVisibility as a type in the option type file

--- packages/twenty-front/src/modules/object-core/workflows/types/CoreWorkflowVisibilityOption.ts
+++ packages/twenty-front/src/modules/object-core/workflows/types/CoreWorkflowVisibilityOption.ts
@@ -1,7 +1,7 @@
 import { type MessageDescriptor } from '@lingui/core';
 import { type IconComponent } from 'twenty-ui/icon';
 
-import { WorkflowVisibility } from '~/generated/graphql';
+import { type WorkflowVisibility } from '~/generated/graphql';
 
 export type CoreWorkflowVisibilityOption = {
   value: WorkflowVisibility;
