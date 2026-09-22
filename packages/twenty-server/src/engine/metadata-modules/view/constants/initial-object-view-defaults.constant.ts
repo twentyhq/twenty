ac8d6d498213de704c38bd4317e485367fbda953
Thomas Trompette <thomas.trompette@sfr.fr>
2026-09-22 20:40:10 +0200

    feat(view): make the campaign index view a list

--- packages/twenty-server/src/engine/metadata-modules/view/constants/initial-object-view-defaults.constant.ts
+++ packages/twenty-server/src/engine/metadata-modules/view/constants/initial-object-view-defaults.constant.ts
@@ -1,16 +1,9 @@
 import { STANDARD_OBJECT_UNIVERSAL_IDENTIFIERS } from 'twenty-shared/metadata';
 import { ViewType } from 'twenty-shared/types';
 
-export type InitialObjectViewType = ViewType.TABLE | ViewType.LIST;
-
 export const INITIAL_OBJECT_VIEW_POSITION = 1;
 
 export const INITIAL_OBJECT_VIEW_TYPE = ViewType.TABLE;
-
-export const INITIAL_OBJECT_VIEW_TYPE_BY_OBJECT_UNIVERSAL_IDENTIFIER = new Map<
-  string,
-  InitialObjectViewType
->([[STANDARD_OBJECT_UNIVERSAL_IDENTIFIERS.messageCampaign, ViewType.LIST]]);
 
 export const OBJECT_UNIVERSAL_IDENTIFIERS_WITHOUT_INITIAL_VIEW =
   new Set<string>([STANDARD_OBJECT_UNIVERSAL_IDENTIFIERS.opportunity]);
--- packages/twenty-server/src/engine/metadata-modules/view/utils/compute-initial-object-view-to-create.util.ts
+++ packages/twenty-server/src/engine/metadata-modules/view/utils/compute-initial-object-view-to-create.util.ts
@@ -2,8 +2,10 @@ import { getInitialObjectViewUniversalIdentifier } from 'src/engine/metadata-mod
 import { VIEW_TYPE_DEFAULT_ICONS } from 'twenty-shared/constants';
 
 import { buildBaseUniversalFlatView } from 'src/engine/metadata-modules/view/utils/build-base-universal-flat-view.util';
-import { INITIAL_OBJECT_VIEW_POSITION } from 'src/engine/metadata-modules/view/constants/initial-object-view-defaults.constant';
-import { getInitialObjectViewType } from 'src/engine/metadata-modules/view/utils/get-initial-object-view-type.util';
+import {
+  INITIAL_OBJECT_VIEW_POSITION,
+  INITIAL_OBJECT_VIEW_TYPE,
+} from 'src/engine/metadata-modules/view/constants/initial-object-view-defaults.constant';
 import { type UniversalFlatObjectMetadata } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-object-metadata.type';
 import { type UniversalFlatView } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-view.type';
 
@@ -18,10 +20,8 @@ export const computeInitialObjectViewToCreate = ({
 }: {
   applicationUniversalIdentifier: string;
   objectMetadata: InitialObjectViewObjectMetadata;
-}): UniversalFlatView & { id: string } => {
-  const type = getInitialObjectViewType(objectMetadata.universalIdentifier);
-
-  return buildBaseUniversalFlatView({
+}): UniversalFlatView & { id: string } =>
+  buildBaseUniversalFlatView({
     objectMetadataUniversalIdentifier: objectMetadata.universalIdentifier,
     applicationUniversalIdentifier,
     universalIdentifier: getInitialObjectViewUniversalIdentifier({
@@ -30,9 +30,8 @@ export const computeInitialObjectViewToCreate = ({
     }),
     name: `All ${objectMetadata.labelPlural}`,
     key: null,
-    icon: VIEW_TYPE_DEFAULT_ICONS[type],
-    type,
+    icon: VIEW_TYPE_DEFAULT_ICONS[INITIAL_OBJECT_VIEW_TYPE],
+    type: INITIAL_OBJECT_VIEW_TYPE,
     position: INITIAL_OBJECT_VIEW_POSITION,
     isSystemSideEffect: false,
   });
-};
