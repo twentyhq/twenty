6b30ba9f282bb98d7566d7ddf9ed263c5550cde3
Thomas des Francs <tdesfrancs@gmail.com>
2026-09-23 10:35:41 +0200

    Move currency rounding comment to the PR description

--- packages/twenty-front/src/utils/convertCurrencyToCurrencyMicros.ts
+++ packages/twenty-front/src/utils/convertCurrencyToCurrencyMicros.ts
@@ -1,7 +1,6 @@
 export const convertCurrencyAmountToCurrencyMicros = (
   currencyAmount: number,
 ) => {
-  // Float multiplication drifts (8.2 * 1e6 === 8199999.999999999) and micros are whole numbers
   return Math.round(currencyAmount * 1000000);
 };
 
