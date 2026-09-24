a58e643ecbe6b9aa463a5de8fd122c284575c222
bosiraphael <raphael.bosi@gmail.com>
2026-09-24 11:00:23 +0200

    Return the unauthenticated check result without a boolean coercion

--- packages/twenty-front/src/modules/front-components/utils/isUnauthenticatedMetadataGraphqlResponse.ts
+++ packages/twenty-front/src/modules/front-components/utils/isUnauthenticatedMetadataGraphqlResponse.ts
@@ -5,4 +5,4 @@ export const isUnauthenticatedMetadataGraphqlResponse = (
   response: MetadataGraphqlResponse<unknown>,
 ): boolean =>
   response.status === 401 ||
-  response.payload?.errors?.some(isUnauthenticatedGraphQLError) === true;
+  (response.payload?.errors ?? []).some(isUnauthenticatedGraphQLError);
