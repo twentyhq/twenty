a7e6c41eabf0675ff9b7bbe85862f982f248f6e6
bosiraphael <raphael.bosi@gmail.com>
2026-09-24 13:00:34 +0200

    Limit :open to details and dialog elements

--- packages/twenty-front-component-renderer/src/polyfills/selectors/constants/PseudoClassMatcherByName.ts
+++ packages/twenty-front-component-renderer/src/polyfills/selectors/constants/PseudoClassMatcherByName.ts
@@ -6,12 +6,12 @@ import { isElementEmpty } from '@/polyfills/selectors/css-select/utils/isElement
 import { type CompiledSelectorMatcher } from '@/polyfills/selectors/types/CompiledSelectorMatcher';
 import { canElementBeDisabled } from '@/polyfills/selectors/utils/canElementBeDisabled';
 import { canElementBeRequired } from '@/polyfills/selectors/utils/canElementBeRequired';
-import { hasElementAttributeIgnoringCase } from '@/polyfills/selectors/utils/hasElementAttributeIgnoringCase';
 import { isDocumentNode } from '@/polyfills/selectors/utils/isDocumentNode';
 import { isElementChecked } from '@/polyfills/selectors/utils/isElementChecked';
 import { isElementDisabled } from '@/polyfills/selectors/utils/isElementDisabled';
 import { isElementIndeterminate } from '@/polyfills/selectors/utils/isElementIndeterminate';
 import { isElementLink } from '@/polyfills/selectors/utils/isElementLink';
+import { isElementOpen } from '@/polyfills/selectors/utils/isElementOpen';
 import { isElementPlaceholderShown } from '@/polyfills/selectors/utils/isElementPlaceholderShown';
 import { isElementReadWrite } from '@/polyfills/selectors/utils/isElementReadWrite';
 import { isElementRequired } from '@/polyfills/selectors/utils/isElementRequired';
@@ -80,7 +80,7 @@ export const PSEUDO_CLASS_MATCHER_BY_NAME = new Map<
       !hasSiblingElement({ element, direction: 'preceding', isOfType: true }) &&
       !hasSiblingElement({ element, direction: 'following', isOfType: true }),
   ],
-  ['open', (element) => hasElementAttributeIgnoringCase(element, 'open')],
+  ['open', isElementOpen],
   [
     'optional',
     (element) => canElementBeRequired(element) && !isElementRequired(element),
