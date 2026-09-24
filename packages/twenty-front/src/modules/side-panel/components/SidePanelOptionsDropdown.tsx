2dddfebc898610519687d649d7bde8a0a7f9b4b7
bosiraphael <raphael.bosi@gmail.com>
2026-09-24 12:06:32 +0200

    Inline the side panel options hotkeys

--- packages/twenty-front/src/modules/side-panel/components/SidePanelOptionsDropdown.tsx
+++ packages/twenty-front/src/modules/side-panel/components/SidePanelOptionsDropdown.tsx
@@ -1,8 +1,10 @@
-import { useSidePanelOptionsHotkeys } from '@/side-panel/hooks/useSidePanelOptionsHotkeys';
+import { SIDE_PANEL_FOCUS_ID } from '@/side-panel/constants/SidePanelFocusId';
 import { DropdownRoot } from '@/ui/layout/dropdown/components/DropdownRoot';
 import { DropdownRootContent } from '@/ui/layout/dropdown/components/DropdownRootContent';
 import { DROPDOWN_OFFSET_Y } from '@/ui/layout/dropdown/constants/DropdownOffsetY';
 import { OPTIONS_DROPDOWN_GLOBAL_HOTKEYS_CONFIG } from '@/ui/layout/dropdown/constants/OptionsDropdownGlobalHotkeysConfig';
+import { useToggleDropdown } from '@/ui/layout/dropdown/hooks/useToggleDropdown';
+import { useHotkeysOnFocusedElement } from '@/ui/utilities/hotkey/hooks/useHotkeysOnFocusedElement';
 import { useLingui } from '@lingui/react/macro';
 import { type ReactNode } from 'react';
 import { Dropdown, IconButton } from 'twenty-ui/components';
@@ -18,8 +20,27 @@ export const SidePanelOptionsDropdown = ({
   children,
 }: SidePanelOptionsDropdownProps) => {
   const { t } = useLingui();
+  const { toggleDropdown } = useToggleDropdown();
 
-  useSidePanelOptionsHotkeys(dropdownId);
+  const toggleOptionsHotkeysConfig = {
+    keys: ['ctrl+o', 'meta+o'],
+    callback: () =>
+      toggleDropdown({
+        dropdownComponentInstanceIdFromProps: dropdownId,
+        globalHotkeysConfig: OPTIONS_DROPDOWN_GLOBAL_HOTKEYS_CONFIG,
+      }),
+    dependencies: [toggleDropdown, dropdownId],
+  };
+
+  useHotkeysOnFocusedElement({
+    ...toggleOptionsHotkeysConfig,
+    focusId: SIDE_PANEL_FOCUS_ID,
+  });
+
+  useHotkeysOnFocusedElement({
+    ...toggleOptionsHotkeysConfig,
+    focusId: dropdownId,
+  });
 
   return (
     <DropdownRoot
