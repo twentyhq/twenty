9874a15a9d5946424ca127d99b49890f82076fa7
bosiraphael <raphael.bosi@gmail.com>
2026-09-24 13:00:12 +0200

    Match :required and :placeholder-shown only on input types that support them

--- packages/twenty-front-component-renderer/src/polyfills/selectors/utils/isElementPlaceholderShown.ts
+++ packages/twenty-front-component-renderer/src/polyfills/selectors/utils/isElementPlaceholderShown.ts
@@ -3,13 +3,38 @@ import { isNonEmptyString, isString } from '@sniptt/guards';
 import { type SelectorElementLike } from '@/polyfills/selectors/types/SelectorElementLike';
 import { readElementAttributeIgnoringCase } from '@/polyfills/selectors/utils/readElementAttributeIgnoringCase';
 import { resolveHtmlTagNameOfElement } from '@/polyfills/selectors/utils/resolveHtmlTagNameOfElement';
+import { resolveInputTypeOfElement } from '@/polyfills/selectors/utils/resolveInputTypeOfElement';
 
 const TAG_NAMES_WITH_PLACEHOLDER = new Set(['input', 'textarea']);
+
+const INPUT_TYPES_WITHOUT_PLACEHOLDER = new Set([
+  'button',
+  'checkbox',
+  'color',
+  'date',
+  'datetime-local',
+  'file',
+  'hidden',
+  'image',
+  'month',
+  'radio',
+  'range',
+  'reset',
+  'submit',
+  'time',
+  'week',
+]);
 
 export const isElementPlaceholderShown = (
   element: SelectorElementLike,
 ): boolean => {
-  if (!TAG_NAMES_WITH_PLACEHOLDER.has(resolveHtmlTagNameOfElement(element))) {
+  const tagName = resolveHtmlTagNameOfElement(element);
+
+  if (
+    !TAG_NAMES_WITH_PLACEHOLDER.has(tagName) ||
+    (tagName === 'input' &&
+      INPUT_TYPES_WITHOUT_PLACEHOLDER.has(resolveInputTypeOfElement(element)))
+  ) {
     return false;
   }
 
--- packages/twenty-front-component-renderer/src/polyfills/selectors/utils/isElementRequired.ts
+++ packages/twenty-front-component-renderer/src/polyfills/selectors/utils/isElementRequired.ts
@@ -1,7 +1,23 @@
 import { type SelectorElementLike } from '@/polyfills/selectors/types/SelectorElementLike';
 import { canElementBeRequired } from '@/polyfills/selectors/utils/canElementBeRequired';
 import { hasElementAttributeIgnoringCase } from '@/polyfills/selectors/utils/hasElementAttributeIgnoringCase';
+import { resolveHtmlTagNameOfElement } from '@/polyfills/selectors/utils/resolveHtmlTagNameOfElement';
+import { resolveInputTypeOfElement } from '@/polyfills/selectors/utils/resolveInputTypeOfElement';
+
+const INPUT_TYPES_WITHOUT_REQUIRED = new Set([
+  'button',
+  'color',
+  'hidden',
+  'image',
+  'range',
+  'reset',
+  'submit',
+]);
 
 export const isElementRequired = (element: SelectorElementLike): boolean =>
   canElementBeRequired(element) &&
+  !(
+    resolveHtmlTagNameOfElement(element) === 'input' &&
+    INPUT_TYPES_WITHOUT_REQUIRED.has(resolveInputTypeOfElement(element))
+  ) &&
   hasElementAttributeIgnoringCase(element, 'required');
