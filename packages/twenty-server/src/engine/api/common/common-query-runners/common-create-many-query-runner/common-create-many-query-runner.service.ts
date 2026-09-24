215e27a72c151340957cda4da1ea21120f3f3238
Félix Malfait <felix@twenty.com>
2026-09-24 13:38:11 +0200

    style: format record creation enforcement predicate

--- packages/twenty-server/src/engine/api/common/common-query-runners/common-create-many-query-runner/common-create-many-query-runner.service.ts
+++ packages/twenty-server/src/engine/api/common/common-query-runners/common-create-many-query-runner/common-create-many-query-runner.service.ts
@@ -645,7 +645,8 @@ export class CommonCreateManyQueryRunnerService extends CommonBaseQueryRunnerSer
   private isRecordSharingEnforced(
     queryRunnerContext: CommonExtendedQueryRunnerContext,
   ): boolean {
-    return !queryRunnerContext.repository.internalContext.isLegacyRecordAccessOpen;
+    return !queryRunnerContext.repository.internalContext
+      .isLegacyRecordAccessOpen;
   }
 
   private resolveNestedRelationsForCreate({
